05ecf34e987f5b47d2aa4bffbd19f2e29aa673fe
mrroundhill <dan@automattic.com>
2009-12-09 00:37:45 +0000

    changes namespace to org.wordpress.android and added better date formatting to the UI

--- src/com/roundhill/androidWP/ServiceUpdateUIListener.java
+++
@@ -1,7 +0,0 @@
-package com.roundhill.androidWP;
-
-public interface ServiceUpdateUIListener {
-
-	public void updateUI(String accountID, String accountName);
-
-	}//end interface ServiceUpdateUIListener
