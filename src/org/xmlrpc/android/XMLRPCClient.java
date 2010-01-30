215127877792c70566550284e997d38f5ca6359e
mrroundhill <dan@automattic.com>
2010-01-30 17:46:00 +0000

    changed the user agent to 1.0.0 for release

--- src/org/xmlrpc/android/XMLRPCClient.java
+++ src/org/xmlrpc/android/XMLRPCClient.java
@@ -88,7 +88,7 @@ public class XMLRPCClient {
 		postMethod = new HttpPost(uri);
 		postMethod.addHeader("Content-Type", "text/xml");		
 		//UPDATE THE VERSION NUMBER BEFORE RELEASE!
-		postMethod.addHeader("User-Agent", "wp-android/0.9.1");
+		postMethod.addHeader("User-Agent", "wp-android/1.0.0");
 		
 		// WARNING
 		// I had to disable "Expect: 100-Continue" header since I had 
