6f305d6a8f39905c1894671f2ab3c2779137f62b
mrroundhill <dan@automattic.com>
2011-11-11 16:34:47 +0000

    should fix #210, not allow taps on the header rows.

--- src/org/wordpress/android/ViewPosts.java
+++ src/org/wordpress/android/ViewPosts.java
@@ -310,7 +310,7 @@ public class ViewPosts extends ListFragment {
 
 					public void onItemClick(AdapterView<?> arg0, View v,
 							int position, long id) {
-						if (v != null) {
+						if (v != null && !postIDs[position].equals("draftsHeader") && !postIDs[position].equals("postsHeader")) {
 							selectedPosition = position;
 							selectedID = v.getId();
 							Post post = new Post(WordPress.currentBlog.getId(),
