5489ec71031202fee771b13c942c25e86038603a
mrroundhill <dan@automattic.com>
2012-01-10 15:40:57 +0000

    version updated.

--- src/org/wordpress/android/ViewStats.java
+++ src/org/wordpress/android/ViewStats.java
@@ -535,7 +535,7 @@ public class ViewStats extends Activity {
 
 		postMethod.addHeader("charset", "UTF-8");
 		// UPDATE THE VERSION NUMBER BEFORE RELEASE! <3 Dan
-		postMethod.addHeader("User-Agent", "wp-android/2.0.3");
+		postMethod.addHeader("User-Agent", "wp-android/2.0.4");
 
 		httpParams = postMethod.getParams();
 		HttpProtocolParams.setUseExpectContinue(httpParams, false);
