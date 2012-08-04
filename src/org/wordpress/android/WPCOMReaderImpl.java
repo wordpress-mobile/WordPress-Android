0c20f413220abe6ea8565498a7a79837faf111a9
mrroundhill <dan@automattic.com>
2012-08-04 15:03:16 +0000

    Removing unused import

--- src/org/wordpress/android/WPCOMReaderImpl.java
+++ src/org/wordpress/android/WPCOMReaderImpl.java
@@ -15,7 +15,6 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
