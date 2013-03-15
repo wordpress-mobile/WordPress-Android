e2e84195e30bcd2b83034fbf4c234296099309c9
mrroundhill <dan@automattic.com>
2013-03-15 21:39:39 +0000

    Use standard ok text for AlertDialog. Fixes #448. Also fixed bug where tapping the new post button would open the camera

--- src/org/wordpress/android/ui/posts/EditPostActivity.java
+++ src/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -205,7 +205,7 @@ public class EditPostActivity extends SherlockActivity implements OnClickListene
                     if (savedInstanceState.getString("mediaCapturePath") != null)
                         mMediaCapturePath = savedInstanceState.getString("mediaCapturePath");
                 } else {
-                    mQuickMediaType = extras.getInt("quick-media");
+                    mQuickMediaType = extras.getInt("quick-media", -1);
                 }
 
                 if (extras.getBoolean("isQuickPress")) {
@@ -1401,7 +1401,7 @@ public class EditPostActivity extends SherlockActivity implements OnClickListene
             AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(EditPostActivity.this);
             dialogBuilder.setTitle(getResources().getText(R.string.empty_fields));
             dialogBuilder.setMessage(getResources().getText(R.string.title_post_required));
-            dialogBuilder.setPositiveButton(getString(R.id.ok), new DialogInterface.OnClickListener() {
+            dialogBuilder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
                     dialog.dismiss();
                 }
