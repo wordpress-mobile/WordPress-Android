13ce80490ded9fa8ff8cde414e3dff6512b3a0a7
Beau Collins <beaucollins@gmail.com>
2013-04-04 12:14:50 -0700

    Cleaning up Oauth test until we can test async requests

--- test/src/com/wordpress/rest/OauthTest.java
+++ test/src/com/wordpress/rest/OauthTest.java
@@ -5,17 +5,6 @@ import com.wordpress.rest.Oauth;
 
 import com.wordpress.util.TestExecutorService;
 
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.HttpRequest;
-import org.apache.http.impl.client.AbstractHttpClient;
-import org.apache.http.protocol.HttpContext;
-
-import com.loopj.android.http.AsyncHttpClient;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.ArrayBlockingQueue;
-
 import android.util.Log;
     
 public class OauthTest extends AndroidTestCase {
@@ -30,18 +19,6 @@ public class OauthTest extends AndroidTestCase {
     @Override
     public void setUp(){
         mClient = new Oauth(mAppId, mAppSecret, mAppRedirectURI);
-        Log.d(TAG, "Preparing Client");
-        AsyncHttpClient client = mClient.getClient();
-        //     public TestExecutorService(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
-
-        // client.setThreadPool(new TestExecutorService(1, 1, (long) 30, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(1)));
-        AbstractHttpClient httpClient = (AbstractHttpClient) mClient.getHttpClient();
-        // client.addRequestInterceptor( new HttpRequestInterceptor(){
-        //     @Override
-        //     public void process(HttpRequest request, HttpContext context) {
-        //         Log.d(TAG, String.format("Process request %s", request));
-        //     }
-        // });
     }
     
     public void testRequestAuthorizationURL(){
@@ -51,20 +28,5 @@ public class OauthTest extends AndroidTestCase {
         assertEquals(expected, url);
     }
     
-    public void testRequestAccessTokenWithCode() throws InterruptedException {
-        final CountDownLatch signal = new CountDownLatch(1);
-        mClient.requestAccessToken("acode", new Oauth.AccessTokenResponseHandler(){
-            @Override
-            public void onPreflight(){
-                signal.countDown();
-            }
-        });
-        signal.await();
-        fail("Did not respond");
-    }
-    
-    public void testRequestAccessTokenWithUsernameAndPassword(){
-        mClient.requestAccessToken("username", "password", new Oauth.AccessTokenResponseHandler());
-    }
     
 }
