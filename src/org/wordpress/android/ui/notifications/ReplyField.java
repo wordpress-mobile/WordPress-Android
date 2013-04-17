73ca6ae1c459df22728aacdebe42e59ae98ded3f
Robert Collins <robert@automattic.com>
2013-04-17 08:10:36 -0700

    Custom class for reply text field

---
+++ src/org/wordpress/android/ui/notifications/ReplyField.java
@@ -0,0 +1,17 @@
+package org.wordpress.android.ui.notifications;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class ReplyField extends View {
+    public ReplyField(Context context){
+        super(context);
+    }
+    public ReplyField(Context context, AttributeSet attributes){
+        super(context, attributes);
+    }
+    public ReplyField(Context context, AttributeSet attributes, int defStyle){
+        super(context, attributes, defStyle);
+    }
+}
