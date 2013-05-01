c223b2e34ff63fe9a7a265b2e56432bdb06f05a0
Robert Collins <robert@automattic.com>
2013-05-01 15:37:43 -0700

    Moving JSON querying to JSONUtil class to make it reusable

--- src/org/wordpress/android/models/Note.java
+++ src/org/wordpress/android/models/Note.java
@@ -14,6 +14,8 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import org.wordpress.android.util.JSONUtil;
+
 public class Note {
     protected static final String TAG="NoteModel";
     public static final String UNKNOWN_TYPE="unknown";
@@ -24,15 +26,8 @@ public class Note {
     public static final String SINGLE_LINE_LIST_TEMPLATE="single-line-list";
     public static final String MULTI_LINE_LIST_TEMPLATE="multi-line-list";
     public static final String BIG_BADGE_TEMPLATE="big-badge";
-    
-    private static String QUERY_SEPERATOR=".";
-    private static String QUERY_ARRAY_INDEX_START="[";
-    private static String QUERY_ARRAY_INDEX_END="]";
-    private static String QUERY_ARRAY_FIRST="first";
-    private static String QUERY_ARRAY_LAST="last";
-    
-    private Map<String,JSONObject> mActions;
 
+    private Map<String,JSONObject> mActions;
     private JSONObject mNoteJSON;
     /**
      * Create a note using JSON from REST API
@@ -142,7 +137,7 @@ public class Note {
                 mActions = new HashMap<String,JSONObject>(actions.length());
                 for (int i=0; i<actions.length(); i++) {
                     JSONObject action = actions.getJSONObject(i);
-                    String actionType = queryJSON(action, "type", "");
+                    String actionType = JSONUtil.queryJSON(action, "type", "");
                     if (!actionType.equals("")) {
                         mActions.put(actionType, action);
                     }
@@ -156,112 +151,13 @@ public class Note {
     }
     protected Object queryJSON(String query){
         Object defaultObject = "";
-        return queryJSON(query, defaultObject);
+        return JSONUtil.queryJSON(this.toJSONObject(), query, defaultObject);
     }
     /**
      * Rudimentary system for pulling an item out of a JSON object hierarchy
      */
-    public <U extends Object> U queryJSON(String query, U defaultObject){
-        return queryJSON(this.toJSONObject(), query, defaultObject);
-    }
-    
-    public static <U extends Object> U queryJSON(JSONObject source, String query, U defaultObject){
-        int nextSeperator = query.indexOf(QUERY_SEPERATOR);
-        int nextIndexStart = query.indexOf(QUERY_ARRAY_INDEX_START);
-        if (nextSeperator == -1 && nextIndexStart == -1) {
-            // last item let's get it
-            try {
-                Object result = source.get(query);
-                if (result.getClass().isAssignableFrom(defaultObject.getClass())) {
-                    return (U) result;
-                } else {
-                    return defaultObject;
-                }
-            } catch (JSONException e) {
-                Log.e(TAG, String.format("Could not complete query %s", query), e);
-                return defaultObject;
-            } catch (ClassCastException e) {
-                Log.e(TAG, String.format("Could not cast object %s", query), e);
-                return defaultObject;
-            }
-        }
-        int endQuery;
-        if (nextSeperator == -1 || nextIndexStart == -1) {
-            endQuery = Math.max(nextSeperator, nextIndexStart);
-        } else {
-            endQuery = Math.min(nextSeperator, nextIndexStart);
-        }
-        String nextQuery = query.substring(endQuery);
-        String key = query.substring(0, endQuery);
-        try {
-            if (nextQuery.indexOf(QUERY_SEPERATOR) == 0) {
-                return queryJSON(source.getJSONObject(key), nextQuery.substring(1), defaultObject);
-            } else if (nextQuery.indexOf(QUERY_ARRAY_INDEX_START) == 0){
-                return queryJSON(source.getJSONArray(key), nextQuery, defaultObject);
-            } else if (!nextQuery.equals("")){
-                return defaultObject;
-            }
-            Object result = source.get(key);
-            if (result.getClass().isAssignableFrom(defaultObject.getClass())) {
-                return (U) result;
-            } else {
-                return defaultObject;
-            }
-        } catch (java.lang.ClassCastException e) {
-            Log.e(TAG, String.format("Could not cast object at %s", query), e);
-            return defaultObject;
-        } catch (JSONException e) {
-            Log.e(TAG, String.format("Could not complete query %s", query), e);
-            return defaultObject;
-        }
-    }
-    
-    public static <U extends Object> U queryJSON(JSONArray source, String query, U defaultObject){
-        // query must start with [ have an index and then have ]
-        int indexStart = query.indexOf(QUERY_ARRAY_INDEX_START);
-        int indexEnd = query.indexOf(QUERY_ARRAY_INDEX_END);
-        if (indexStart == -1 || indexEnd == -1 || indexStart > indexEnd) {
-            return defaultObject;
-        }
-        // get "index" from "[index]"
-        String indexStr = query.substring(indexStart + 1, indexEnd);
-        int index;
-        if (indexStr.equals(QUERY_ARRAY_FIRST)) {
-            index = 0;
-        } else if (indexStr.equals(QUERY_ARRAY_LAST)){
-            index = -1;
-        } else {
-            index = Integer.parseInt(indexStr);
-        }
-        if(index < 0){
-            index = source.length() + index;
-        }
-        // copy remaining query
-        String remainingQuery = query.substring(indexEnd + 1);
-        try {
-            if (remainingQuery.indexOf(QUERY_ARRAY_INDEX_START) == 0) {
-                return queryJSON(source.getJSONArray(index), remainingQuery, defaultObject);
-            } else if(remainingQuery.indexOf(QUERY_SEPERATOR) == 0){
-                return queryJSON(source.getJSONObject(index), remainingQuery.substring(1), defaultObject);
-            } else if(!remainingQuery.equals("")){
-                // TODO throw an exception since the query isn't valid?
-                Log.d(TAG, String.format("Incorrect query for next object %s", remainingQuery));
-                return defaultObject;
-            }
-            Object result = source.get(index);
-            if (result.getClass().isAssignableFrom(defaultObject.getClass())) {
-                return (U) result;
-            } else {
-                return defaultObject;
-            }
-        } catch(java.lang.ClassCastException e){
-            Log.e(TAG, String.format("Could not cast object at %s", query), e);
-            return defaultObject;
-        } catch (JSONException e) {
-            Log.e(TAG, String.format("Could not complete query %s", query), e);
-            return defaultObject;
-        }
-
+    public <U> U queryJSON(String query, U defaultObject){
+        return JSONUtil.queryJSON(this.toJSONObject(), query, defaultObject);
     }
 
 }
