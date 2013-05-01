cc3b7b74d5e21ceb5266ed68c0f8ed4939745cea
Dan Roundhill <dan@automattic.com>
2013-05-01 15:06:32 -0700

    Refresh all blog content when refreshing the posts list. Fixes #519.

--- src/org/wordpress/android/ui/posts/PostsActivity.java
+++ src/org/wordpress/android/ui/posts/PostsActivity.java
@@ -278,7 +278,7 @@ public class PostsActivity extends WPActionBarActivity implements OnPostSelected
         int itemId = item.getItemId();
         if (itemId == R.id.menu_refresh) {
             postList.refreshPosts(false);
-            new ApiHelper.RefreshBlogContentTask(this, WordPress.currentBlog).execute(true);
+            new ApiHelper.RefreshBlogContentTask(this, WordPress.currentBlog).execute(false);
             return true;
         } else if (itemId == R.id.menu_new_post) {
             Intent i = new Intent(this, EditPostActivity.class);
