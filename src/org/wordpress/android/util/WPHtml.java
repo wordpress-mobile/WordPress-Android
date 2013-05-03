37c6a127ea8700a60766cad10c4735cae54783b0
Robert Collins <robert@automattic.com>
2013-05-03 10:56:25 -0700

    Only apply custom WPQuoteSpan to ICS and newer
    For older devices just change the line color. They don't correctly
    increase the indent.

--- src/org/wordpress/android/util/WPHtml.java
+++ src/org/wordpress/android/util/WPHtml.java
@@ -56,6 +56,7 @@ import android.view.WindowManager;
 import android.text.Layout;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Build;
 
 import org.ccil.cowan.tagsoup.HTMLSchema;
 import org.ccil.cowan.tagsoup.Parser;
@@ -81,26 +82,47 @@ public class WPHtml {
     public static class WPQuoteSpan extends QuoteSpan {
         private static final int STRIPE_WIDTH=5;
         private static final int GAP_WIDTH=20;
+        public static final int STRIPE_COLOR=0xFFCCCCCC;
+        private static final boolean IS_ICS= Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+
+        public WPQuoteSpan(){
+            super(STRIPE_COLOR);
+        }
+
         @Override
         public int getLeadingMargin(boolean first){
-            int margin = GAP_WIDTH * 2 + STRIPE_WIDTH;
-            return margin;
+            if (IS_ICS) {
+                int margin = GAP_WIDTH * 2 + STRIPE_WIDTH;
+                return margin;
+            } else {
+                return super.getLeadingMargin(first);
+            }
         }
+        /**
+         * Draw a nice thick gray bar if Ice Cream Sandwhich or newer. There's a
+         * bug on older devices that does not respect the increased margin.
+         */
         @Override
         public void drawLeadingMargin(Canvas c, Paint p, int x, int dir,
                                       int top, int baseline, int bottom,
                                       CharSequence text, int start, int end,
                                       boolean first, Layout layout) {
-            Paint.Style style = p.getStyle();
-            int color = p.getColor();
 
-            p.setStyle(Paint.Style.FILL);
-            p.setColor(0xFFCCCCCC);
+            if (IS_ICS) {
+                Paint.Style style = p.getStyle();
+                int color = p.getColor();
 
-            c.drawRect(GAP_WIDTH + x, top, x + dir * STRIPE_WIDTH, bottom, p);
+                p.setStyle(Paint.Style.FILL);
+                p.setColor(STRIPE_COLOR);
 
-            p.setStyle(style);
-            p.setColor(color);
+                c.drawRect(GAP_WIDTH + x, top, x + dir * STRIPE_WIDTH, bottom, p);
+
+                p.setStyle(style);
+                p.setColor(color);
+            } else {
+                super.drawLeadingMargin(c, p, x, dir, top, baseline, bottom,
+                                        text, start, end, first, layout);
+            }
         }
     }
     
