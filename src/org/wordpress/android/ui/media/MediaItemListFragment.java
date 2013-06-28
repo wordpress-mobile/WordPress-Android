75efe26245336c7ff18c648ba4815964399b692d
FredrichO/AkifH <fredrich.ombico@xtremelabs.com>
2013-06-28 11:08:51 -0400

    FredrichO/AkifH - [#52310947] Made images show up on the media browser. Integrated the XL Image utils library.

--- src/org/wordpress/android/ui/media/MediaItemListFragment.java
+++ src/org/wordpress/android/ui/media/MediaItemListFragment.java
@@ -1,10 +1,18 @@
 package org.wordpress.android.ui.media;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v4.widget.CursorAdapter;
@@ -12,7 +20,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.xtremelabs.imageutils.BitmapListener;
+import com.xtremelabs.imageutils.ImageLoader;
+import com.xtremelabs.imageutils.ImageReturnedFrom;
 
 import org.xmlrpc.android.ApiHelper;
 import org.xmlrpc.android.ApiHelper.GetMediaTask.Callback;
@@ -26,7 +40,21 @@ public class MediaItemListFragment extends ListFragment {
     private ApiHelper.GetMediaTask mGetMediaTask;
     private MediaCursorAdapter mAdapter;
     private Cursor mCursor;
+    
+    private ImageLoader mImageLoader;
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mImageLoader = ImageLoader.buildImageLoaderForSupportFragment(this);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mImageLoader.destroy();
+    }
+    
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -72,9 +100,70 @@ public class MediaItemListFragment extends ListFragment {
         }
 
         @Override
-        public void bindView(View view, Context context, Cursor cursor) {
+        public void bindView(final View view, Context context, Cursor cursor) {
             TextView title = (TextView) view.findViewById(R.id.media_listitem_title);
             title.setText(cursor.getString(cursor.getColumnIndex("title")));
+            
+            String fileURL = cursor.getString(cursor.getColumnIndex("fileURL"));
+            ImageView thumbnail = (ImageView) view.findViewById(R.id.media_listitem_thumbnail);
+            if(isValidImage(fileURL))
+                loadThumbnail(thumbnail, context, cursor);
+            else
+                thumbnail.setImageDrawable(null);
+                
+        }
+
+        private void loadThumbnail(final ImageView thumbnail, Context context, Cursor cursor) {
+            
+            
+            Blog blog = WordPress.getCurrentBlog(); 
+            if(blog != null) {
+                String blogId = String.valueOf(blog.getBlogId());
+                File blogDir = context.getDir(blogId, Context.MODE_PRIVATE);
+                final String imgPath = blogDir.getAbsolutePath() + File.separator + cursor.getString(cursor.getColumnIndex("uuid"));
+                
+                Uri imageUri;
+                
+                final File file = new File(imgPath);
+                if(file.exists()) {
+                    Log.d("WordPress", "MCA: File " + imgPath + " exists");
+                    // show image
+                    imageUri = Uri.fromFile(file);
+                    mImageLoader.loadImage(thumbnail, imageUri.toString());
+                } else {
+                    Log.d("WordPress", "MCA: File " + imgPath + " does not exist");
+                    imageUri = Uri.parse(cursor.getString(cursor.getColumnIndex("thumbnailURL")));
+                    
+
+                    mImageLoader.loadImage(imageUri.toString(), new BitmapListener() {
+                        
+                        @Override
+                        public void onImageLoadError(String error) {
+                            
+                        }
+                        
+                        @Override
+                        public void onImageAvailable(Bitmap bitmap, ImageReturnedFrom arg1) {
+                            thumbnail.setImageBitmap(bitmap);
+                            saveBitmapToFile(file, bitmap);
+                            
+                        }
+                       
+                    });
+                }
+                
+            }
+        }
+
+        private void saveBitmapToFile(File file, Bitmap bitmap) {
+            try {
+               OutputStream out = new FileOutputStream(file);
+               bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+               out.flush();
+               out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -86,6 +175,12 @@ public class MediaItemListFragment extends ListFragment {
             return view;
         }
         
+    }
+    
+    private boolean isValidImage(String url) {
+        if(url.endsWith(".png") || url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".gif"))
+            return true;
+        return false;
     }
 
     private Callback mCallback = new Callback() {
