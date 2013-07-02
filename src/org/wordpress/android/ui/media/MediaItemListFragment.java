642eae1b355ec0a981701f54d6bac85858fd0d22
FredrichO/AkifH <fredrich.ombico@xtremelabs.com>
2013-07-02 10:18:34 -0400

    FredrichO - [#52310947] Fixed bug where images sometimes wouldn't load when using volley image cache.

--- src/org/wordpress/android/ui/media/MediaItemListFragment.java
+++ src/org/wordpress/android/ui/media/MediaItemListFragment.java
@@ -3,61 +3,31 @@ package org.wordpress.android.ui.media;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.content.Context;
 import android.database.Cursor;
-import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.support.v4.widget.CursorAdapter;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.android.volley.toolbox.NetworkImageView;
 
 import org.xmlrpc.android.ApiHelper;
 import org.xmlrpc.android.ApiHelper.GetMediaTask.Callback;
 
-import org.wordpress.android.R;
 import org.wordpress.android.WordPress;
 import org.wordpress.android.models.Blog;
 
 public class MediaItemListFragment extends ListFragment {
 
     private ApiHelper.GetMediaTask mGetMediaTask;
-    private MediaCursorAdapter mAdapter;
+    private MediaItemListAdapter mAdapter;
     private Cursor mCursor;
     
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
-    
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
-    
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
     @Override
     public void onResume() {
         super.onResume();
         
-        Log.d("WordPress", "MediaItemListFragment onResume()");
-        
-        if(WordPress.getCurrentBlog() != null)
-            refreshMedia();
-        
         loadCursor();
-        
-        mAdapter = new MediaCursorAdapter(getActivity(), mCursor, 0);
+        mAdapter = new MediaItemListAdapter(getActivity(), mCursor, 0);
         setListAdapter(mAdapter);
         
+        if(WordPress.getCurrentBlog() != null)
+            refreshMediaFromServer();
     }
 
     private void loadCursor() {
@@ -68,55 +38,12 @@ public class MediaItemListFragment extends ListFragment {
         }
     }
     
-    private void refreshMedia() {
+    private void refreshMediaFromServer() {
         List<Object> apiArgs = new ArrayList<Object>();
         apiArgs.add(WordPress.getCurrentBlog());
         
         mGetMediaTask = new ApiHelper.GetMediaTask(mCallback);
         mGetMediaTask.execute(apiArgs);
-    }
-
-    
-    class MediaCursorAdapter extends CursorAdapter {
-
-        public MediaCursorAdapter(Context context, Cursor c, int flags) {
-            super(context, c, flags);
-        }
-
-        @Override
-        public void bindView(final View view, Context context, Cursor cursor) {
-            TextView title = (TextView) view.findViewById(R.id.media_listitem_title);
-            title.setText(cursor.getString(cursor.getColumnIndex("title")));
-            
-            
-            NetworkImageView thumbnail = (NetworkImageView) view.findViewById(R.id.media_listitem_thumbnail);
-            thumbnail.setDefaultImageResId(R.drawable.placeholder);
-            
-            String fileURL = cursor.getString(cursor.getColumnIndex("fileURL"));
-            String thumbnailURL = cursor.getString(cursor.getColumnIndex("thumbnailURL"));
-            
-            if(isValidImage(fileURL))
-                thumbnail.setImageUrl(thumbnailURL, WordPress.imageLoader);
-            else
-                thumbnail.setImageDrawable(null);
-                
-        }
-
-        @Override
-        public View newView(Context context, Cursor cursor, ViewGroup root) {
-            
-            LayoutInflater inflater = LayoutInflater.from(context);
-            View view = inflater.inflate(R.layout.media_listitem, root, false);
-            
-            return view;
-        }
-        
-    }
-    
-    private boolean isValidImage(String url) {
-        if(url.endsWith(".png") || url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".gif"))
-            return true;
-        return false;
     }
 
     private Callback mCallback = new Callback() {
