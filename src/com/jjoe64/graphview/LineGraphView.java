98340c1fbae818f29554a501c4a09baedca07d41
Jonas Gehring <g.jjoe64@googlemail.com>
2013-08-01 06:59:33 +0200

    license

--- src/com/jjoe64/graphview/LineGraphView.java
+++ src/com/jjoe64/graphview/LineGraphView.java
@@ -1,3 +1,22 @@
+/**
+ * This file is part of GraphView.
+ *
+ * GraphView is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GraphView is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GraphView.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ * Copyright Jonas Gehring
+ */
+
 package com.jjoe64.graphview;
 
 import android.content.Context;
@@ -10,11 +29,6 @@ import com.jjoe64.graphview.GraphViewSeries.GraphViewSeriesStyle;
 
 /**
  * Line Graph View. This draws a line chart.
- * @author jjoe64 - jonas gehring - http://www.jjoe64.com
- *
- * Copyright (C) 2011 Jonas Gehring
- * Licensed under the GNU Lesser General Public License (LGPL)
- * http://www.gnu.org/licenses/lgpl.html
  */
 public class LineGraphView extends GraphView {
 	private final Paint paintBackground;
--- src/com/jjoe64/graphview/compatible/ScaleGestureDetector.java
+++ src/com/jjoe64/graphview/compatible/ScaleGestureDetector.java
@@ -1,3 +1,22 @@
+/**
+ * This file is part of GraphView.
+ *
+ * GraphView is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GraphView is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GraphView.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ * Copyright Jonas Gehring
+ */
+
 package com.jjoe64.graphview.compatible;
 
 import java.lang.reflect.Constructor;
@@ -7,11 +26,6 @@ import android.content.Context;
 import android.util.Log;
 import android.view.MotionEvent;
 
-/**
- * Copyright (C) 2011 Jonas Gehring
- * Licensed under the GNU Lesser General Public License (LGPL)
- * http://www.gnu.org/licenses/lgpl.html
- */
 public class ScaleGestureDetector {
 	public interface SimpleOnScaleGestureListener {
 		boolean onScale(ScaleGestureDetector detector);
