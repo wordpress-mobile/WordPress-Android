5838ab1c061216d95195bcde8716d26504c8a313
Fredrich Ombico <fredrich.ombico@xtremelabs.com>
2013-08-06 17:33:58 -0400

    FredrichO - highlighting on tablet

--- src/org/wordpress/android/ui/stats/StatsCursorFragment.java
+++ src/org/wordpress/android/ui/stats/StatsCursorFragment.java
@@ -129,8 +129,11 @@ public class StatsCursorFragment extends SherlockFragment implements LoaderManag
         
         // limit number of items to show otherwise it would cause performance issues on the linearlayout
         int count = Math.min(mAdapter.getCount(), MAX_ITEMS_ON_TABLET);
-        for (int i=0; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             View view = mAdapter.getView(i, null, mLinearLayout);
+            view.findViewById(R.id.stats_list_cell_div).setVisibility(View.VISIBLE);
+            if (i % 2 == 1)
+                view.setBackgroundColor(getResources().getColor(R.color.stats_alt_row));
             mLinearLayout.addView(view);
         }
         
