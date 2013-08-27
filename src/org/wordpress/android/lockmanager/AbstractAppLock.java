5c7bc77de06bc044b0eb0a63fd32472f0642d64b
Danilo Ercoli <ercoli@gmail.com>
2013-08-27 22:27:22 +0200

    Set the default timeout of the PassLock to 2 secs

--- src/org/wordpress/android/lockmanager/AbstractAppLock.java
+++ src/org/wordpress/android/lockmanager/AbstractAppLock.java
@@ -3,7 +3,7 @@ package org.wordpress.android.lockmanager;
 import android.app.Application;
 
 public abstract class AbstractAppLock implements Application.ActivityLifecycleCallbacks {
-    public static final int DEFAULT_TIMEOUT = 5; //5 seconds
+    public static final int DEFAULT_TIMEOUT = 2; //2 seconds
     public static final int EXTENDED_TIMEOUT = 60; //60 seconds
     
     protected static final String APP_LOCK_PASSWORD_PREF_KEY = "wp_app_lock_password_key";
