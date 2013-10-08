c99734b54072fa147f06d35d7b66693bb84b9b05
Maxime Biais <maxime.biais@gmail.com>
2013-10-08 18:26:48 +0200

    Clean strings and unused import

--- src/org/wordpress/android/ui/accounts/WelcomeActivity.java
+++ src/org/wordpress/android/ui/accounts/WelcomeActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
