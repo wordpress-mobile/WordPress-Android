b6e2f554b8407262c864884e6a3a2f3ac02f2b12
Beau Collins <beaucollins@gmail.com>
2013-11-16 00:06:09 -0800

    Start buckets once user has been authenticated

--- src/org/wordpress/android/util/SimperiumUtils.java
+++ src/org/wordpress/android/util/SimperiumUtils.java
@@ -21,31 +21,44 @@ public class SimperiumUtils {
         Simperium simperium = Simperium.newClient(Config.SIMPERIUM_APP_NAME,
             Config.SIMPERIUM_APP_SECRET, context);
 
-        if (token != null) {
-            authorizeUser(simperium, token);
-        }
 
         try {
-            Bucket<Note> notesBucket = simperium.bucket(new Note.Schema());
-            Bucket<BucketObject> metaBucket = simperium.bucket("meta");
-
-            notesBucket.start();
-            metaBucket.start();
+            final Bucket<Note> notesBucket = simperium.bucket(new Note.Schema());
+            final Bucket<BucketObject> metaBucket = simperium.bucket("meta");
 
             WordPress.simperium = simperium;
             WordPress.notesBucket = notesBucket;
             WordPress.metaBucket = metaBucket;
 
+            simperium.setUserStatusChangeListener( new User.StatusChangeListener() {
+
+                @Override
+                public void onUserStatusChange(User.Status status) {
+                    switch (status) {
+                        case AUTHORIZED:
+                            notesBucket.start();
+                            metaBucket.start();
+                            break;
+                        default:
+                            android.util.Log.d("WordPress", "User not authorized yet");
+                            break;
+                    }
+                }
+
+            });
+
         } catch (BucketNameInvalid e) {
             throw new RuntimeException("Failed to configure simperium", e);
         }
 
+        authorizeUser(simperium, token);
 
         return simperium;
     }
 
     public static void authorizeUser(Simperium simperium, String token){
         // for now we're going to hardcod mdawaffe's
+        if (token == null) return;
 
         User user = simperium.getUser();
 
