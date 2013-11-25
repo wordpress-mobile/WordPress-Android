47fff8425b599ecb5ce38b0bdf2fa9cd912c97f3
Maxime Biais <maxime.biais@gmail.com>
2013-11-25 21:47:40 +0100

    NUX sign up: Use EditText.setError on network errors instead of a modal error

--- src/org/wordpress/android/ui/accounts/NewAccountAbstractPageFragment.java
+++ src/org/wordpress/android/ui/accounts/NewAccountAbstractPageFragment.java
@@ -33,6 +33,8 @@ public abstract class NewAccountAbstractPageFragment extends SherlockFragment {
     protected static RequestQueue requestQueue = null;
     protected static WPRestClient restClient = null;
 
+    protected enum ErrorType {USERNAME, PASSWORD, SITE_URL, EMAIL, TITLE, UNDEFINED}
+
     /**
      * The fragment's page number, which is set to the argument value for {@link #ARG_PAGE}.
      */
@@ -69,103 +71,162 @@ public abstract class NewAccountAbstractPageFragment extends SherlockFragment {
     protected class ErrorListener implements RestRequest.ErrorListener {
         @Override
         public void onErrorResponse(VolleyError error) {
-            Log.d("ErrorListener", String.format("Error type: %s", error));
             String message = null;
-
+            int messageId;
+            Log.d("ErrorListener", String.format("Error type: %s", error));
             if (error.networkResponse != null && error.networkResponse.data != null) {
-                Log.d("ErrorListener", String.format("Error message: %s", new String(error.networkResponse.data)));
+                Log.d("ErrorListener", String.format("Error message: %s",
+                        new String(error.networkResponse.data)));
                 String jsonString = new String(error.networkResponse.data);
                 try {
                     JSONObject errorObj = new JSONObject(jsonString);
-                    message = getErrorMessageForErrorCode((String) errorObj.get("error"));
-                    if (message == null) { // Not one of our common errors. Show the error message from the server.
+                    messageId = getErrorMessageForErrorCode((String) errorObj.get("error"));
+                    if (messageId == 0) { // Not one of our common errors. Show the error message
+                                           // from the server.
                         message = (String) errorObj.get("message");
                     }
                 } catch (JSONException e) {
-                    Log.d("ErrorListener", String.format("Error parsing the error message :( : %s", e));
-                    message = getString(R.string.error_generic);
+                    Log.d("ErrorListener", String.format("Error parsing the error message :( : %s",
+                            e));
+                    messageId = R.string.error_generic;
                 }
             } else {
                 if (error.getMessage() != null) {
                     if (error.getMessage().contains("Limit reached")) {
-                        message = getString(R.string.limit_reached);
+                        messageId = R.string.limit_reached;
                     } else {
-                        message = getString(R.string.error_generic);
+                        messageId = R.string.error_generic;
                     }
                 } else {
-                    message = getString(R.string.error_generic);
+                    messageId = R.string.error_generic;
                 }
             }
-            showError(message);
+            endProgress();
+            if (messageId == 0) {
+                showError(message);
+            } else {
+                showError(messageId);
+            }
         }
     }
 
+    protected boolean specificShowError(int messageId) {
+        return false;
+    }
+
+    protected void showError(int messageId) {
+        if (specificShowError(messageId)) {
+            return ;
+        }
+        // Failback if it's not a specific error
+        showError(getString(messageId));
+    }
+
     protected void showError(String message) {
-        endProgress();
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         NUXDialogFragment nuxAlert = NUXDialogFragment.newInstance(getString(R.string.error),
-                message, getString(R.string.nux_tap_continue), R.drawable.nux_icon_alert);
+                message, getString(R.string.nux_tap_continue),
+                R.drawable.nux_icon_alert);
         nuxAlert.show(ft, "alert");
     }
-    
-    protected String getErrorMessageForErrorCode(String errorCode) {
-        if(errorCode.equals("username_only_lowercase_letters_and_numbers"))
-            return getString(R.string.username_only_lowercase_letters_and_numbers);
-        if(errorCode.equals("username_required"))
-            return getString(R.string.username_required);
-        if(errorCode.equals("username_not_allowed"))
-            return getString(R.string.username_not_allowed);
-        if(errorCode.equals("email_cant_be_used_to_signup"))
-            return getString(R.string.email_cant_be_used_to_signup);
-        if(errorCode.equals("username_must_be_at_least_four_characters"))
-            return getString(R.string.username_must_be_at_least_four_characters);
-        if(errorCode.equals("username_contains_invalid_characters"))
-            return getString(R.string.username_contains_invalid_characters);
-        if(errorCode.equals("username_must_include_letters"))
-            return getString(R.string.username_must_include_letters);
-        if(errorCode.equals("email_invalid"))
-            return getString(R.string.email_invalid);
-        if(errorCode.equals("email_not_allowed"))
-            return getString(R.string.email_not_allowed);
-        if(errorCode.equals("username_exists"))
-            return getString(R.string.username_exists);
-        if(errorCode.equals("email_exists"))
-            return getString(R.string.email_exists);
-        if(errorCode.equals("username_reserved_but_may_be_available"))
-            return getString(R.string.username_reserved_but_may_be_available);
-        if(errorCode.equals("email_reserved"))
-            return getString(R.string.email_reserved);
-        if(errorCode.equals("blog_name_required"))    
-            return getString(R.string.blog_name_required);
-        if(errorCode.equals("blog_name_not_allowed"))
-            return getString(R.string.blog_name_not_allowed);
-        if(errorCode.equals("blog_name_must_be_at_least_four_characters"))
-            return getString(R.string.blog_name_must_be_at_least_four_characters);
-        if(errorCode.equals("blog_name_must_be_less_than_sixty_four_characters"))
-            return getString(R.string.blog_name_must_be_less_than_sixty_four_characters);
-        if(errorCode.equals("blog_name_contains_invalid_characters"))
-            return getString(R.string.blog_name_contains_invalid_characters);
-        if(errorCode.equals("blog_name_cant_be_used"))
-            return getString(R.string.blog_name_cant_be_used);
-        if(errorCode.equals("blog_name_only_lowercase_letters_and_numbers"))
-            return getString(R.string.blog_name_only_lowercase_letters_and_numbers);
-        if(errorCode.equals("blog_name_must_include_letters"))
-            return getString(R.string.blog_name_must_include_letters);
-        if(errorCode.equals("blog_name_exists"))
-            return getString(R.string.blog_name_exists);
-        if(errorCode.equals("blog_name_reserved"))
-            return getString(R.string.blog_name_reserved);
-        if(errorCode.equals("blog_name_reserved_but_may_be_available"))
-            return getString(R.string.blog_name_reserved_but_may_be_available);
-        if(errorCode.equals("password_invalid"))
-            return getString(R.string.password_invalid);
-        if(errorCode.equals("blog_name_invalid"))
-            return getString(R.string.blog_name_invalid);
-        if(errorCode.equals("blog_title_invalid"))
-            return getString(R.string.blog_title_invalid);
-        if(errorCode.equals("username_invalid"))
-            return getString(R.string.username_invalid);
+
+    protected ErrorType getErrorType(int messageId) {
+        switch (messageId) {
+            case R.string.username_only_lowercase_letters_and_numbers:
+            case R.string.username_required:
+            case R.string.username_not_allowed:
+            case R.string.username_must_be_at_least_four_characters:
+            case R.string.username_contains_invalid_characters:
+            case R.string.username_must_include_letters:
+            case R.string.username_exists:
+            case R.string.username_reserved_but_may_be_available:
+            case R.string.username_invalid:
+                return ErrorType.USERNAME;
+            case R.string.password_invalid:
+                return ErrorType.PASSWORD;
+            case R.string.email_cant_be_used_to_signup:
+            case R.string.email_invalid:
+            case R.string.email_not_allowed:
+            case R.string.email_exists:
+            case R.string.email_reserved:
+                return ErrorType.EMAIL;
+            case R.string.blog_name_required:
+            case R.string.blog_name_not_allowed:
+            case R.string.blog_name_must_be_at_least_four_characters:
+            case R.string.blog_name_must_be_less_than_sixty_four_characters:
+            case R.string.blog_name_contains_invalid_characters:
+            case R.string.blog_name_cant_be_used:
+            case R.string.blog_name_only_lowercase_letters_and_numbers:
+            case R.string.blog_name_must_include_letters:
+            case R.string.blog_name_exists:
+            case R.string.blog_name_reserved:
+            case R.string.blog_name_reserved_but_may_be_available:
+            case R.string.blog_name_invalid:
+                return ErrorType.SITE_URL;
+            case R.string.blog_title_invalid:
+                return ErrorType.TITLE;
+        }
+        return ErrorType.UNDEFINED;
+    }
+
+    protected int getErrorMessageForErrorCode(String errorCode) {
+        if (errorCode.equals("username_only_lowercase_letters_and_numbers"))
+            return R.string.username_only_lowercase_letters_and_numbers;
+        if (errorCode.equals("username_required"))
+            return R.string.username_required;
+        if (errorCode.equals("username_not_allowed"))
+            return R.string.username_not_allowed;
+        if (errorCode.equals("email_cant_be_used_to_signup"))
+            return R.string.email_cant_be_used_to_signup;
+        if (errorCode.equals("username_must_be_at_least_four_characters"))
+            return R.string.username_must_be_at_least_four_characters;
+        if (errorCode.equals("username_contains_invalid_characters"))
+            return R.string.username_contains_invalid_characters;
+        if (errorCode.equals("username_must_include_letters"))
+            return R.string.username_must_include_letters;
+        if (errorCode.equals("email_invalid"))
+            return R.string.email_invalid;
+        if (errorCode.equals("email_not_allowed"))
+            return R.string.email_not_allowed;
+        if (errorCode.equals("username_exists"))
+            return R.string.username_exists;
+        if (errorCode.equals("email_exists"))
+            return R.string.email_exists;
+        if (errorCode.equals("username_reserved_but_may_be_available"))
+            return R.string.username_reserved_but_may_be_available;
+        if (errorCode.equals("email_reserved"))
+            return R.string.email_reserved;
+        if (errorCode.equals("blog_name_required"))
+            return R.string.blog_name_required;
+        if (errorCode.equals("blog_name_not_allowed"))
+            return R.string.blog_name_not_allowed;
+        if (errorCode.equals("blog_name_must_be_at_least_four_characters"))
+            return R.string.blog_name_must_be_at_least_four_characters;
+        if (errorCode.equals("blog_name_must_be_less_than_sixty_four_characters"))
+            return R.string.blog_name_must_be_less_than_sixty_four_characters;
+        if (errorCode.equals("blog_name_contains_invalid_characters"))
+            return R.string.blog_name_contains_invalid_characters;
+        if (errorCode.equals("blog_name_cant_be_used"))
+            return R.string.blog_name_cant_be_used;
+        if (errorCode.equals("blog_name_only_lowercase_letters_and_numbers"))
+            return R.string.blog_name_only_lowercase_letters_and_numbers;
+        if (errorCode.equals("blog_name_must_include_letters"))
+            return R.string.blog_name_must_include_letters;
+        if (errorCode.equals("blog_name_exists"))
+            return R.string.blog_name_exists;
+        if (errorCode.equals("blog_name_reserved"))
+            return R.string.blog_name_reserved;
+        if (errorCode.equals("blog_name_reserved_but_may_be_available"))
+            return R.string.blog_name_reserved_but_may_be_available;
+        if (errorCode.equals("password_invalid"))
+            return R.string.password_invalid;
+        if (errorCode.equals("blog_name_invalid"))
+            return R.string.blog_name_invalid;
+        if (errorCode.equals("blog_title_invalid"))
+            return R.string.blog_title_invalid;
+        if (errorCode.equals("username_invalid"))
+            return R.string.username_invalid;
         
-        return null;
+        return 0;
     }
 }
