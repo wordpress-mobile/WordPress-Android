5fb92534206b4157d94d802aabde67839f9d7826
Danilo Ercoli <ercoli@gmail.com>
2013-11-27 12:41:18 +0100

    Hide zoom buttons, and add the "up" button to the ActionBar.

--- src/org/wordpress/android/ui/notifications/NotificationsWebViewActivity.java
+++ src/org/wordpress/android/ui/notifications/NotificationsWebViewActivity.java
@@ -3,24 +3,30 @@ package org.wordpress.android.ui.notifications;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 import org.wordpress.android.Constants;
 import org.wordpress.android.R;
 import org.wordpress.android.ui.AuthenticatedWebViewActivity;
+import org.wordpress.android.util.StringUtils;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class NotificationsWebViewActivity extends AuthenticatedWebViewActivity {
     
     public static final String URL_TO_LOAD = "external_url";
     
-    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setUserAgentString(Constants.USER_AGENT);
+        mWebView.getSettings().setDisplayZoomControls(false);
      
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        
         // load URL if one was provided in the intent
         String url = getIntent().getStringExtra(URL_TO_LOAD);
         if (url != null) {
@@ -34,5 +40,18 @@ public class NotificationsWebViewActivity extends AuthenticatedWebViewActivity {
         menu.findItem(R.id.menu_signout).setVisible(false);
         menu.findItem(R.id.menu_settings).setVisible(false);
         return true;
+    }
+    
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemID = item.getItemId();
+        if (itemID == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
