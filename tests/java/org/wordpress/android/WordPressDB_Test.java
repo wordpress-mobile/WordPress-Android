7079f464aab9308f9ed0a5e01dc74a37dcd937a5
Maxime Biais <maxime.biais@gmail.com>
2013-12-02 14:37:35 +0100

    new test reproducing #387

---
+++ tests/java/org/wordpress/android/WordPressDB_Test.java
@@ -0,0 +1,41 @@
+package org.wordpress;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.test.InstrumentationTestCase;
+import android.test.RenamingDelegatingContext;
+import android.util.Log;
+import org.wordpress.android.TestUtils;
+import org.wordpress.android.WordPress;
+import org.wordpress.android.WordPressDB;
+import org.wordpress.android.models.CategoryNode;
+
+public class WordPressDB_Test extends InstrumentationTestCase {
+    protected Context testContext;
+    protected Context targetContext;
+
+    @Override
+    protected void setUp() {
+        // Run tests in an isolated context
+        targetContext = new RenamingDelegatingContext(getInstrumentation().getTargetContext(),
+                "test_");
+        testContext = getInstrumentation().getContext();
+        SQLiteDatabase db = TestUtils.loadDBFromDump(targetContext, testContext,
+                "string-escaping-test.sql");
+    }
+
+    // This test failed before fixing #387
+    public void testEscaping() {
+        SQLiteDatabase db = TestUtils.loadDBFromDump(targetContext, testContext,
+                "string-escaping-test.sql");
+        String blogName = "say: \"pouet\"";
+        String blogURL = "pouet.com";
+        String username = "pouet";
+        String password = "";
+
+        boolean blogExists = WordPress.wpDB.checkForExistingBlog(blogName, blogURL, username, password);
+        assertTrue(blogExists);
+        db.close();
+    }
+}
