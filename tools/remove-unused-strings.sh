bd312374a63183da01edb89d117d43e7111900cd
Maxime Biais <maxime.biais@gmail.com>
2013-12-12 18:12:40 +0100

    fix an issue with remove-unused-string.sh script

--- tools/remove-unused-strings.sh
+++ tools/remove-unused-strings.sh
@@ -5,12 +5,14 @@ unused_strings=$(lint --check  UnusedResources . \
     | grep -o "R\.string\.[^ ]*" \
     | sed "s/R.string.//" \
     | tr "\n" "|" \
-    | sed "s/|$//")
+    | sed 's/|/"|"/g' \
+    | sed 's/^/"/' \
+    | sed 's/|"$//')
 
 if [ "$unused_strings"x = x ]; then
     echo res/values/strings.xml is already clean
 else
     grep -E -v "$unused_strings" res/values/strings.xml > tmp.xml
     mv tmp.xml res/values/strings.xml
-    echo res/values/strings.xml has been cleaned
+    echo $(echo "$unused_strings" | sed "s/[^|]//g" | wc -c) strings removed
 fi
