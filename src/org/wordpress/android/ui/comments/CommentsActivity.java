a66de52cffdba28f15839714c6ebe5436bd06b0d
ShiftLeftLogical <johnoneil@gmail.com>
2013-12-13 05:03:56 -0700

    Added the use of the MessageBarUtils ... very awesome. I might add the ability to customize it a bit more for my needs but for now it is pretty slick!

--- src/org/wordpress/android/ui/comments/CommentsActivity.java
+++ src/org/wordpress/android/ui/comments/CommentsActivity.java
@@ -21,6 +21,7 @@ import org.wordpress.android.ui.WPActionBarActivity;
 import org.wordpress.android.ui.comments.CommentListFragment.CommentAsyncModerationReturnListener;
 import org.wordpress.android.ui.comments.CommentListFragment.CommentListFragmentListener;
 import org.wordpress.android.ui.comments.CommentListFragment.OnAnimateRefreshButtonListener;
+import org.wordpress.android.util.MessageBarUtils;
 
 import java.util.ArrayList;
 
@@ -242,6 +243,7 @@ public class CommentsActivity extends WPActionBarActivity implements CommentAsyn
                 || commentModerationStatusType == CommentStatus.TRASH) {
             if (mActionMode != null) { mActionMode.finish(); }
         }
+        MessageBarUtils.hideMessageBar(this, null, false);
     }
 
     @Override
@@ -259,6 +261,7 @@ public class CommentsActivity extends WPActionBarActivity implements CommentAsyn
         } else {
             //TODO: JCO (Resolve by 12/13/13) This would be a programming error. Possibly server?
         }
+        MessageBarUtils.hideMessageBar(this, null, false);
     }
 
     @Override
