e871a065d1b586507b8f900835b08bbd894a54e0
Maxime Biais <maxime.biais@gmail.com>
2013-12-19 12:34:39 +0100

    fix #334: modify our WPHtmlTagHandler to support paragraph tag in a list item

--- tests/java/org/wordpress/android/ui/notifications/NotesParseTest.java
+++ tests/java/org/wordpress/android/ui/notifications/NotesParseTest.java
@@ -3,15 +3,16 @@ package org.wordpress.android.ui.notifications;
 import android.text.Html;
 import android.text.SpannableStringBuilder;
 
-import org.wordpress.android.util.WPHtmlTagHandler;
-
 import junit.framework.TestCase;
+
+import org.wordpress.android.util.WPHtmlTagHandler;
 
 public class NotesParseTest extends TestCase {
 
     public void testParagraphInListItem() {
         String text = "<li><p>Paragraph in li</p></li>";
-        SpannableStringBuilder html = (SpannableStringBuilder) Html.fromHtml(text, null, new WPHtmlTagHandler());
+        SpannableStringBuilder html = (SpannableStringBuilder) Html.fromHtml(text, null,
+                new WPHtmlTagHandler());
         // if this didn't throw a RuntimeException we're ok
         assert(true);
     }
