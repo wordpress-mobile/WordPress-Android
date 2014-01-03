a8e594e27a41f9940049d33c3f572a2192aa8df5
Maxime Biais <maxime.biais@gmail.com>
2014-01-03 16:39:26 +0100

    fix #619: replace update-translations.rb by update-translations.sh the original ruby script was 95% pure shell, and languages-codes.sh were also used in dump-po-files.sh

--- tools/dump-po-files.sh
+++ tools/dump-po-files.sh
@@ -1,4 +1,4 @@
-LANGS=$(grep "=>" update-translations.rb|cut -d "'" -f 2)
+LANGS=$(cat tools/language-codes.csv|cut -d "," -f1)
 mkdir po-files
 for lang in $LANGS; do
     curl "http://translate.wordpress.org/projects/android/dev/$lang/default/export-translations" > po-files/"$lang".po
