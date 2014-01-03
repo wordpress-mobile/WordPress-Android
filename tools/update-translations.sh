84dd27eb92ad4e1b97d9873710a3b138174fe835
Maxime Biais <maxime.biais@gmail.com>
2014-01-03 17:06:38 +0100

    Display an error when server replies with 404

--- tools/update-translations.sh
+++ tools/update-translations.sh
@@ -6,7 +6,6 @@ for line in $(cat tools/language-codes.csv) ; do
     echo updating $local - $code
     test -d res/values-$local/ || mkdir res/values-$local/
     test -f res/values-$local/strings.xml && cp res/values-$local/strings.xml res/values-$local/strings.xml.bak
-    curl --globoff -so res/values-$local/strings.xml "http://translate.wordpress.org/projects/android/dev/$code/default/export-translations?filters[status]=current&format=android" || echo Error downloading $code
+    curl -sSf --globoff -o res/values-$local/strings.xml "http://translate.wordpress.org/projects/android/dev/$code/default/export-translations?filters[status]=current&format=android" || (echo Error downloading $code && rm -rf res/values-$local/)
     test -f res/values-$local/strings.xml.bak && rm res/values-$local/strings.xml.bak
-    grep -a '\\x00\\x22\\x00\\x22' res/values-$local/strings.xml
 done
