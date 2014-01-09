09a12ed6173c3d7dec24632f2aeed6a3ff5b7296
Maxime Biais <maxime.biais@gmail.com>
2014-01-09 11:05:49 +0100

    Fix an errorType check

--- src/org/wordpress/android/ui/media/MediaGalleryPickerActivity.java
+++ src/org/wordpress/android/ui/media/MediaGalleryPickerActivity.java
@@ -239,7 +239,7 @@ public class MediaGalleryPickerActivity extends SherlockActivity implements Mult
 
                 @Override
                 public void onFailure(ApiHelper.ErrorType errorType, String errorMessage, Throwable throwable) {
-                    if (errorType == ApiHelper.ErrorType.NO_ERROR) {
+                    if (errorType != ApiHelper.ErrorType.NO_ERROR) {
                         String message = errorType == ApiHelper.ErrorType.NO_UPLOAD_FILES_CAP
                                 ? getString(R.string.media_error_no_permission)
                                 : getString(R.string.error_refresh_media);
