6c5fcc022238b5c9bf87147ed015bd5e294fd7ca
Nick Bradbury <nick.bradbury@gmail.com>
2014-02-02 17:29:46 -0500

    Rewrote comment saving to use a compiled statement for performance

--- src/org/wordpress/android/datasets/CommentTable.java
+++ src/org/wordpress/android/datasets/CommentTable.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteStatement;
 
 import org.wordpress.android.WordPress;
 import org.wordpress.android.WordPressDB;
@@ -22,7 +23,7 @@ public class CommentTable {
 
     public static void createTables(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS " + COMMENTS_TABLE + " ("
-                 + "    blog_id      INTEGER DEFAULT 0," // <- TODO: this is the local blog id, need to store remote blog_id as well
+                 + "    blog_id      INTEGER DEFAULT 0,"
                  + "    post_id      INTEGER DEFAULT 0,"
                  + "    comment_id   INTEGER DEFAULT 0,"
                  + "    comment      TEXT,"
@@ -133,6 +134,11 @@ public class CommentTable {
         return (count > 0);
     }
 
+    /**
+     * nbradbury - get all comments for a blog
+     * @param localBlogId - unique id in account table for this blog
+     * @return list of comments for this blog
+     */
     public static CommentList getCommentsForBlog(int localBlogId) {
         CommentList comments = new CommentList();
 
@@ -153,68 +159,55 @@ public class CommentTable {
         }
     }
 
-    public static void deleteCommentsForBlog(int localBlogId) {
-        getWritableDb().delete(COMMENTS_TABLE, "blog_id=?", new String[]{Integer.toString(localBlogId)});
+    /**
+    * nbradbury - delete all comments for a blog
+    * @param localBlogId - unique id in account table for this blog
+    * @return number of comments deleted
+     */
+    public static int deleteCommentsForBlog(int localBlogId) {
+        return getWritableDb().delete(COMMENTS_TABLE, "blog_id=?", new String[]{Integer.toString(localBlogId)});
     }
 
-    /*public static boolean saveComments(List<?> commentValues) {
-        SQLiteDatabase db = getWritableDb();
-        db.beginTransaction();
-        try {
-            try {
-                for (int i = 0; i < commentValues.size(); i++) {
-                    ContentValues values = new ContentValues();
-                    Map<?, ?> thisHash = (Map<?, ?>) commentValues.get(i);
-                    values.put("blog_id", thisHash.get("blog_id").toString());
-                    values.put("post_id", thisHash.get("post_id").toString());
-                    values.put("comment_id", thisHash.get("comment_id").toString());
-                    values.put("author_name", thisHash.get("author_name").toString());
-                    values.put("comment", thisHash.get("comment").toString());
-                    values.put("commentDate", thisHash.get("commentDate").toString());
-                    values.put("published",
-                            thisHash.get("published").toString());
-                    values.put("status", thisHash.get("status").toString());
-                    values.put("author_url", thisHash.get("author_url").toString());
-                    values.put("author_email", thisHash.get("author_email").toString());
-                    values.put("post_title", thisHash.get("post_title").toString());
-
-                    db.insertWithOnConflict(COMMENTS_TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
-                }
-
-                db.setTransactionSuccessful();
-                return true;
-            } catch (SQLiteException e) {
-                AppLog.e(AppLog.T.COMMENTS, e);
-                return false;
-            }
-        } finally {
-            db.endTransaction();
-        }
-    }*/
-
-    public static boolean saveComments(int localBlogId, CommentList comments) {
+    /**
+     * nbradbury - saves comments for passed blog to local db, overwriting existing ones if necessary
+     * @param localBlogId - unique id in account table for this blog
+     * @param comments - list of comments to save
+     * @return true if saved, false on failure
+     */
+    public static boolean saveComments(int localBlogId, final CommentList comments) {
         if (comments == null || comments.size() == 0)
             return false;
 
+        final String sql = " INSERT OR REPLACE INTO " + COMMENTS_TABLE + "("
+                         + " blog_id,"       // 1
+                         + " post_id,"       // 2
+                         + " comment_id,"    // 3
+                         + " comment,"       // 4
+                         + " published,"     // 5
+                         + " status,"        // 6
+                         + " author_name,"   // 7
+                         + " author_url,"    // 8
+                         + " author_email,"  // 9
+                         + " post_title"     // 10
+                         + " ) VALUES (?1,?2,?3,?4,?5,?6,?7,?8,?9,?10)";
+
         SQLiteDatabase db = getWritableDb();
+        SQLiteStatement stmt = db.compileStatement(sql);
         db.beginTransaction();
         try {
             try {
                 for (Comment comment: comments) {
-                    ContentValues values = new ContentValues();
-
-                    values.put("blog_id",       localBlogId);
-                    values.put("post_id",       comment.postID);
-                    values.put("comment_id",    comment.commentID);
-                    values.put("comment",       comment.getCommentText());
-                    values.put("published",  comment.getPublished());
-                    values.put("status",        comment.getStatus());
-                    values.put("author_name",   comment.getAuthorName());
-                    values.put("author_url",    comment.getAuthorUrl());
-                    values.put("author_email",  comment.getAuthorEmail());
-                    values.put("post_title",    comment.getPostTitle());
-
-                    db.insertWithOnConflict(COMMENTS_TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+                    stmt.bindLong  (1, localBlogId);
+                    stmt.bindLong  (2, comment.postID);
+                    stmt.bindLong  (3, comment.commentID);
+                    stmt.bindString(4, comment.getCommentText());
+                    stmt.bindString(5, comment.getPublished());
+                    stmt.bindString(6, comment.getStatus());
+                    stmt.bindString(7, comment.getAuthorName());
+                    stmt.bindString(8, comment.getAuthorUrl());
+                    stmt.bindString(9, comment.getAuthorEmail());
+                    stmt.bindString(10, comment.getPostTitle());
+                    stmt.execute();
                 }
 
                 db.setTransactionSuccessful();
@@ -225,9 +218,17 @@ public class CommentTable {
             }
         } finally {
             db.endTransaction();
+            SqlUtils.closeStatement(stmt);
         }
     }
 
+    /**
+     * nbradbury - updates the passed comment
+     * @param localBlogId - unique id in account table for this blog
+     * @param commentId - id of comment (returned by api)
+     * @param commentHash - values to store
+     */
+    // TODO: replace commentHash with Comment object
     public static void updateComment(int localBlogId, int commentId, Map<?, ?> commentHash) {
         ContentValues values = new ContentValues();
 
@@ -242,6 +243,12 @@ public class CommentTable {
 
     }
 
+    /**
+     * nbradbury - updates the status for the passed comment
+     * @param localBlogId - unique id in account table for this blog
+     * @param commentId - id of comment (returned by api)
+     * @param newStatus - status to change to
+     */
     public static void updateCommentStatus(int localBlogId, int commentId, String newStatus) {
         ContentValues values = new ContentValues();
         values.put("status", newStatus);
@@ -254,6 +261,10 @@ public class CommentTable {
 
     }
 
+    /**
+     * nbradbury - returns the number of unmoderated comments for a specific blog
+     * @param localBlogId - unique id in account table for this blog
+     */
     public static int getUnmoderatedCommentCount(int localBlogId) {
         String sql = "SELECT COUNT(*) FROM " + COMMENTS_TABLE + " WHERE blog_id=? AND status=?";
         String[] args = {Integer.toString(localBlogId), "hold"};
