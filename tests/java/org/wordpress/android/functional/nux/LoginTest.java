94a434d1c835f2b36998277c538da71071186326
Maxime Biais <maxime.biais@gmail.com>
2014-02-17 09:53:31 +0100

    Factorize robotium test cases

--- tests/java/org/wordpress/android/functional/nux/LoginTest.java
+++ tests/java/org/wordpress/android/functional/nux/LoginTest.java
@@ -1,59 +1,14 @@
 package org.wordpress.android.functional.nux;
 
-import android.content.Context;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.RenamingDelegatingContext;
-
-import com.robotium.solo.Solo;
-
-import org.wordpress.android.TestUtils;
-import org.wordpress.android.functional.FactoryUtils;
+import org.wordpress.android.ActivityRobotiumTestCase;
+import org.wordpress.android.R;
 import org.wordpress.android.mocks.RestClientFactoryTest;
 import org.wordpress.android.mocks.XMLRPCFactoryTest;
 import org.wordpress.android.ui.accounts.WelcomeActivity;
-import org.wordpress.android.util.AppLog;
-import org.wordpress.android.util.AppLog.T;
 
-public class LoginTest extends ActivityInstrumentationTestCase2<WelcomeActivity> {
-    private Solo mSolo;
-    private Context mTargetContext;
-
+public class LoginTest extends ActivityRobotiumTestCase<WelcomeActivity> {
     public LoginTest() {
         super(WelcomeActivity.class);
-        FactoryUtils.initWithTestFactories();
-    }
-
-    @Override
-    public void setUp() throws Exception {
-        // Clean application state
-        mTargetContext = new RenamingDelegatingContext(getInstrumentation().getTargetContext(), "test_");
-        TestUtils.clearApplicationState(mTargetContext);
-
-        // setUp() is run before a test case is started.
-        // This is where the mSolo object is created.
-        mSolo = new Solo(getInstrumentation(), getActivity());
-
-        // Init contexts
-        XMLRPCFactoryTest.sContext = getInstrumentation().getContext();
-        RestClientFactoryTest.sContext = getInstrumentation().getContext();
-        AppLog.v(T.TESTS, "Contexts set");
-
-        // Set mode to Customizable
-        XMLRPCFactoryTest.sMode = XMLRPCFactoryTest.Mode.CUSTOMIZABLE;
-        RestClientFactoryTest.sMode = RestClientFactoryTest.Mode.CUSTOMIZABLE;
-        AppLog.v(T.TESTS, "Modes set to customizable");
-    }
-
-    @Override
-    public void tearDown() throws Exception {
-        // tearDown() is run after a test case has finished.
-        // finishOpenedActivities() will finish all the activities that have been opened during the test execution.
-        mSolo.finishOpenedActivities();
-    }
-
-    public void assertLoggedIn() {
-        mSolo.waitForActivity("WPActionBar");
-        assertTrue(true);
     }
 
     public void testGoodCredentials() throws Exception {
@@ -61,8 +16,7 @@ public class LoginTest extends ActivityInstrumentationTestCase2<WelcomeActivity>
         XMLRPCFactoryTest.setPrefixAllInstances("default");
         mSolo.enterText(0, "test");
         mSolo.enterText(1, "test");
-        mSolo.clickOnText("Sign in");
-        assertLoggedIn();
+        mSolo.clickOnText(mSolo.getString(R.string.sign_in));
     }
 
     public void testBadCredentials() throws Exception {
@@ -70,71 +24,70 @@ public class LoginTest extends ActivityInstrumentationTestCase2<WelcomeActivity>
         XMLRPCFactoryTest.setPrefixAllInstances("login-failure");
         mSolo.enterText(0, "test");
         mSolo.enterText(1, "test");
-        mSolo.clickOnText("Sign in");
-        boolean errorMessageFound = mSolo.searchText("username or password");
+        mSolo.clickOnText(mSolo.getString(R.string.sign_in));
+        boolean errorMessageFound = mSolo.searchText(mSolo.getString(R.string.username_or_password_incorrect));
         assertTrue("Error message not found", errorMessageFound);
     }
 
     public void testCreateAccountInvalidEmail() throws Exception {
-        mSolo.clickOnText("Create account");
-        mSolo.waitForText(".*Create an account.*");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        mSolo.waitForText(mSolo.getString(R.string.create_account_wpcom));
         mSolo.clearEditText(0);
         mSolo.enterText(0, "test");
         mSolo.enterText(1, "test");
         mSolo.enterText(2, "test");
-        mSolo.clickOnText("Create account");
-        boolean errorMessageFound = mSolo.searchText("address isn't valid");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        boolean errorMessageFound = mSolo.searchText(mSolo.getString(R.string.invalid_email_message));
         assertTrue("Error message not found", errorMessageFound);
     }
 
     public void testCreateAccountUsernameExists() throws Exception {
         RestClientFactoryTest.setPrefixAllInstances("username-exists");
-        mSolo.clickOnText("Create account");
-        mSolo.waitForText(".*Create an account.*");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        mSolo.waitForText(mSolo.getString(R.string.create_account_wpcom));
         mSolo.clearEditText(0);
         mSolo.enterText(0, "test@test.com");
         mSolo.enterText(1, "test");
         mSolo.enterText(2, "test");
-        mSolo.clickOnText("Create account");
-        boolean errorMessageFound = mSolo.searchText("username already exists");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        boolean errorMessageFound = mSolo.searchText(mSolo.getString(R.string.username_exists));
         assertTrue("Error message not found", errorMessageFound);
     }
 
     public void testCreateAccountPasswordTooShort() throws Exception {
-        mSolo.clickOnText("Create account");
-        mSolo.waitForText(".*Create an account.*");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        mSolo.waitForText(mSolo.getString(R.string.create_account_wpcom));
         mSolo.clearEditText(0);
         mSolo.enterText(0, "test@test.com");
         mSolo.enterText(1, "test");
         mSolo.enterText(2, "tes");
-        mSolo.clickOnText("Create account");
-        boolean errorMessageFound = mSolo.searchText("must contain at least 4");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        boolean errorMessageFound = mSolo.searchText(mSolo.getString(R.string.invalid_password_message));
         assertTrue("Error message not found", errorMessageFound);
     }
 
     public void testCreateAccountPasswordTooWeak() throws Exception {
         RestClientFactoryTest.setPrefixAllInstances("password-invalid");
-        mSolo.clickOnText("Create account");
-        mSolo.waitForText(".*Create an account.*");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        mSolo.waitForText(mSolo.getString(R.string.create_account_wpcom));
         mSolo.clearEditText(0);
         mSolo.enterText(0, "test@test.com");
         mSolo.enterText(1, "test");
         mSolo.enterText(2, "test");
-        mSolo.clickOnText("Create account");
-        boolean errorMessageFound = mSolo.searchText("more secure");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        boolean errorMessageFound = mSolo.searchText(mSolo.getString(R.string.password_invalid));
         assertTrue("Error message not found", errorMessageFound);
     }
 
     public void testCreateAccountOk() throws Exception {
         RestClientFactoryTest.setPrefixAllInstances("default");
         XMLRPCFactoryTest.setPrefixAllInstances("default");
-        mSolo.clickOnText("Create account");
-        mSolo.waitForText(".*Create an account.*");
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
+        mSolo.waitForText(mSolo.getString(R.string.create_account_wpcom));
         mSolo.clearEditText(0);
         mSolo.enterText(0, "test@test.com");
         mSolo.enterText(1, "test");
         mSolo.enterText(2, "test");
-        mSolo.clickOnText("Create account");
-        assertLoggedIn();
+        mSolo.clickOnText(mSolo.getString(R.string.nux_welcome_create_account));
     }
 }
--- tests/java/org/wordpress/android/functional/posts/PostListTest.java
+++ tests/java/org/wordpress/android/functional/posts/PostListTest.java
@@ -1,69 +1,24 @@
 package org.wordpress.android.functional.posts;
 
-import android.content.Context;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.RenamingDelegatingContext;
-
-import com.robotium.solo.Solo;
-
-import org.wordpress.android.TestUtils;
-import org.wordpress.android.functional.FactoryUtils;
-import org.wordpress.android.mocks.RestClientFactoryTest;
-import org.wordpress.android.mocks.XMLRPCFactoryTest;
+import org.wordpress.android.ActivityRobotiumTestCase;
+import org.wordpress.android.R;
 import org.wordpress.android.ui.posts.PostsActivity;
-import org.wordpress.android.util.AppLog;
-import org.wordpress.android.util.AppLog.T;
 
-public class PostListTest extends ActivityInstrumentationTestCase2<PostsActivity> {
-    private Solo mSolo;
-    private Context mTargetContext;
-
+public class PostListTest extends ActivityRobotiumTestCase<PostsActivity> {
     public PostListTest() {
         super(PostsActivity.class);
-        FactoryUtils.initWithTestFactories();
     }
 
-    @Override
-    public void setUp() throws Exception {
-        // Clean application state
-        mTargetContext = new RenamingDelegatingContext(getInstrumentation().getTargetContext(), "test_");
-        TestUtils.clearApplicationState(mTargetContext);
-
-        // setUp() is run before a test case is started.
-        // This is where the mSolo object is created.
-        mSolo = new Solo(getInstrumentation(), getActivity());
-
-        // Init contexts
-        XMLRPCFactoryTest.sContext = getInstrumentation().getContext();
-        RestClientFactoryTest.sContext = getInstrumentation().getContext();
-        AppLog.v(T.TESTS, "Contexts set");
-
-        // Set mode to Customizable
-        XMLRPCFactoryTest.sMode = XMLRPCFactoryTest.Mode.CUSTOMIZABLE;
-        RestClientFactoryTest.sMode = RestClientFactoryTest.Mode.CUSTOMIZABLE;
-        AppLog.v(T.TESTS, "Modes set to customizable");
-    }
-
-    @Override
-    public void tearDown() throws Exception {
-        // tearDown() is run after a test case has finished.
-        // finishOpenedActivities() will finish all the activities that have been opened during the test execution.
-        mSolo.finishOpenedActivities();
-    }
-
-    public void login() throws Exception {
-        RestClientFactoryTest.setPrefixAllInstances("default");
-        XMLRPCFactoryTest.setPrefixAllInstances("default");
-        mSolo.enterText(0, "test");
-        mSolo.enterText(1, "test");
-        mSolo.clickOnText("Sign in");
+    public void testLoadPosts() throws Exception {
+        login();
+        mSolo.clickOnText(mSolo.getString(R.string.posts));
+        mSolo.scrollDown();
     }
 
     public void testCreateNewPost() throws Exception {
         login();
-        mSolo.clickOnText("Posts");
-        //mSolo.clickOnActionBarItem(R.id.menu_new_post);
-        //mSolo.clickOnActionBarItem(R.id.menu_save_post);
-        mSolo.sleep(100000);
+        mSolo.clickOnText(mSolo.getString(R.string.posts));
+        mSolo.clickOnActionBarItem(R.id.menu_new_post);
+        mSolo.clickOnActionBarItem(R.id.menu_save_post);
     }
 }
