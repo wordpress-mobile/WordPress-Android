750f6eda3a81bc5f54591390aa3f1f50eb43157c
Nick Bradbury <nick.bradbury@gmail.com>
2014-02-25 05:35:26 -0500

    Added view holder pattern to StatsReferrersFragment

--- src/org/wordpress/android/ui/stats/StatsReferrersFragment.java
+++ src/org/wordpress/android/ui/stats/StatsReferrersFragment.java
@@ -23,7 +23,6 @@ import org.wordpress.android.WordPress;
 import org.wordpress.android.datasets.StatsReferrerGroupsTable;
 import org.wordpress.android.datasets.StatsReferrersTable;
 import org.wordpress.android.providers.StatsContentProvider;
-import org.wordpress.android.ui.HorizontalTabView.TabListener;
 import org.wordpress.android.util.WPLinkMovementMethod;
 
 import java.text.DecimalFormat;
@@ -84,7 +83,21 @@ public class StatsReferrersFragment extends StatsAbsPagedViewFragment {
         fragment.setListAdapter(adapter);
         return fragment;
     }
-    
+
+    private static class GroupViewHolder {
+        TextView entryTextView;
+        TextView totalsTextView;
+        View imageFrame;
+        NetworkImageView networkImageView;
+        ImageView errorImageView;
+    }
+
+    private class ChildViewHolder {
+        TextView entryTextView;
+        TextView totalsTextView;
+    }
+
+
     public class CustomAdapter extends CursorTreeAdapter {
 
         private StatsCursorLoaderCallback mCallback;
@@ -98,32 +111,63 @@ public class StatsReferrersFragment extends StatsAbsPagedViewFragment {
         }
 
         @Override
+        protected View newChildView(Context context, Cursor cursor, boolean isLastChild, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View view = inflater.inflate(R.layout.stats_list_cell, parent, false);
+
+            ChildViewHolder holder = new ChildViewHolder();
+            holder.entryTextView = (TextView) view.findViewById(R.id.stats_list_cell_entry);
+            holder.totalsTextView = (TextView) view.findViewById(R.id.stats_list_cell_total);
+            view.setTag(holder);
+
+            return view;
+        }
+
+        @Override
         protected void bindChildView(View view, Context context, Cursor cursor, boolean isLastChild) {
+            ChildViewHolder holder = (ChildViewHolder) view.getTag();
+
             String name = cursor.getString(cursor.getColumnIndex(StatsReferrersTable.Columns.NAME));
             int total = cursor.getInt(cursor.getColumnIndex(StatsReferrersTable.Columns.TOTAL));
 
             // name, url
-            TextView entryTextView = (TextView) view.findViewById(R.id.stats_list_cell_entry);
             if (name.startsWith("http")) {
                 Spanned link = Html.fromHtml("<a href=\"" + name + "\">" + name + "</a>");
-                entryTextView.setText(link);
-                entryTextView.setMovementMethod(WPLinkMovementMethod.getInstance());
+                holder.entryTextView.setText(link);
+                holder.entryTextView.setMovementMethod(WPLinkMovementMethod.getInstance());
             } else {
-                entryTextView.setText(name);
-                entryTextView.setMovementMethod(null);
+                holder.entryTextView.setText(name);
+                holder.entryTextView.setMovementMethod(null);
             }
 
             DecimalFormat formatter = (DecimalFormat) DecimalFormat.getInstance(Locale.getDefault());
             
             // totals
-            TextView totalsTextView = (TextView) view.findViewById(R.id.stats_list_cell_total);
-            totalsTextView.setText(formatter.format(total));
+            holder.totalsTextView.setText(formatter.format(total));
 
             // no icon
         }
 
         @Override
+        protected View newGroupView(Context context, Cursor cursor, boolean isExpanded, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View view = inflater.inflate(R.layout.stats_group_cell, parent, false);
+
+            GroupViewHolder holder = new GroupViewHolder();
+            holder.entryTextView = (TextView) view.findViewById(R.id.stats_group_cell_entry);
+            holder.errorImageView = (ImageView) view.findViewById(R.id.stats_group_cell_blank_image);
+            holder.imageFrame = view.findViewById(R.id.stats_group_cell_image_frame);
+            holder.networkImageView = (NetworkImageView) view.findViewById(R.id.stats_group_cell_image);
+            holder.totalsTextView = (TextView) view.findViewById(R.id.stats_group_cell_total);
+            view.setTag(holder);
+
+            return view;
+        }
+
+        @Override
         protected void bindGroupView(View view, Context context, Cursor cursor, boolean isExpanded) {
+            GroupViewHolder holder = (GroupViewHolder) view.getTag();
+
             String name = cursor.getString(cursor.getColumnIndex(StatsReferrerGroupsTable.Columns.NAME));
             int total = cursor.getInt(cursor.getColumnIndex(StatsReferrerGroupsTable.Columns.TOTAL));
             String url = cursor.getString(cursor.getColumnIndex(StatsReferrerGroupsTable.Columns.URL));
@@ -136,35 +180,31 @@ public class StatsReferrersFragment extends StatsAbsPagedViewFragment {
             toggleChevrons(children > 0, isExpanded, view);
             
             // name, url
-            TextView entryTextView = (TextView) view.findViewById(R.id.stats_group_cell_entry);
             if (urlValid) {
                 Spanned link = Html.fromHtml("<a href=\"" + url + "\">" + name + "</a>");
-                entryTextView.setText(link);
-                entryTextView.setMovementMethod(WPLinkMovementMethod.getInstance());
+                holder.entryTextView.setText(link);
+                holder.entryTextView.setMovementMethod(WPLinkMovementMethod.getInstance());
             } else {
-                entryTextView.setText(name);
-                entryTextView.setMovementMethod(null);
+                holder.entryTextView.setText(name);
+                holder.entryTextView.setMovementMethod(null);
             }
 
             DecimalFormat formatter = (DecimalFormat) DecimalFormat.getInstance(Locale.getDefault());
             
             // totals
-            TextView totalsTextView = (TextView) view.findViewById(R.id.stats_group_cell_total);
-            totalsTextView.setText(formatter.format(total));
+            holder.totalsTextView.setText(formatter.format(total));
 
             // icon
-            view.findViewById(R.id.stats_group_cell_image_frame).setVisibility(View.VISIBLE);
-            NetworkImageView imageView = (NetworkImageView) view.findViewById(R.id.stats_group_cell_image);
-            ImageView errorImageView = (ImageView) view.findViewById(R.id.stats_group_cell_blank_image);
+            holder.imageFrame.setVisibility(View.VISIBLE);
             if (icon != null && icon.length() > 0) {
-                imageView.setErrorImageResId(R.drawable.stats_blank_image);
-                imageView.setDefaultImageResId(R.drawable.stats_blank_image);
-                imageView.setImageUrl(icon, WordPress.imageLoader);
-                imageView.setVisibility(View.VISIBLE);
-                errorImageView.setVisibility(View.GONE);
+                holder.networkImageView.setErrorImageResId(R.drawable.stats_blank_image);
+                holder.networkImageView.setDefaultImageResId(R.drawable.stats_blank_image);
+                holder.networkImageView.setImageUrl(icon, WordPress.imageLoader);
+                holder.networkImageView.setVisibility(View.VISIBLE);
+                holder.errorImageView.setVisibility(View.GONE);
             } else {
-                imageView.setVisibility(View.GONE);
-                errorImageView.setVisibility(View.VISIBLE);
+                holder.networkImageView.setVisibility(View.GONE);
+                holder.errorImageView.setVisibility(View.VISIBLE);
             }   
         }
 
@@ -175,18 +215,6 @@ public class StatsReferrersFragment extends StatsAbsPagedViewFragment {
             bundle.putString(StatsCursorLoaderCallback.BUNDLE_GROUP_ID, groupCursor.getString(groupCursor.getColumnIndex("groupId")));
             mCallback.onUriRequested(groupCursor.getPosition(), STATS_REFERRERS_URI, bundle);
             return null;
-        }
-        
-        @Override
-        protected View newChildView(Context context, Cursor cursor, boolean isLastChild, ViewGroup parent) {
-            LayoutInflater inflater = LayoutInflater.from(context);
-            return inflater.inflate(R.layout.stats_list_cell, parent, false);
-        }
-
-        @Override
-        protected View newGroupView(Context context, Cursor cursor, boolean isExpanded, ViewGroup parent) {
-            LayoutInflater inflater = LayoutInflater.from(context);
-            return inflater.inflate(R.layout.stats_group_cell, parent, false);
         }
 
         private void toggleChevrons(boolean isVisible, boolean isExpanded, View view) {
