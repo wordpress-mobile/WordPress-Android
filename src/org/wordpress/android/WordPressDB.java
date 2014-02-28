cac0dd68fe6fd9deef31d76dfc03ed0b1e899a5b
Dan Roundhill <dan@automattic.com>
2014-02-28 14:13:12 -0800

    Temporary magic number fix to get the correct value for 'isLocalChange' I have this fixed in another branch as part of #968

--- src/org/wordpress/android/WordPressDB.java
+++ src/org/wordpress/android/WordPressDB.java
@@ -1154,7 +1154,7 @@ public class WordPressDB {
                 values.add(c.getInt(26));
                 values.add(c.getInt(27));
                 values.add(c.getInt(28));
-                values.add(c.getInt(29));
+                values.add(c.getInt(31));
             }
         }
         c.close();
