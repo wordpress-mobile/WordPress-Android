36bd4e76bd574c79f8777ed7ae3c5bc69dd1e439
Maxime Biais <maxime.biais@gmail.com>
2014-03-11 17:13:04 +0100

    show a tip when the user never started a pull to refresh action

--- src/org/wordpress/android/ui/PullToRefreshHelper.java
+++ src/org/wordpress/android/ui/PullToRefreshHelper.java
@@ -1,7 +1,13 @@
 package org.wordpress.android.ui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
 import android.view.View;
+
+import org.wordpress.android.R;
 
 import uk.co.senab.actionbarpulltorefresh.extras.actionbarsherlock.PullToRefreshLayout;
 import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
@@ -11,9 +17,13 @@ import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
 import uk.co.senab.actionbarpulltorefresh.library.viewdelegates.ViewDelegate;
 
 public class PullToRefreshHelper implements OnRefreshListener {
+    private static String NEED_PTR_TIP = "NEED_PTR_TIP";
     private PullToRefreshHeaderTransformer mHeaderTransformer;
     private PullToRefreshLayout mPullToRefreshLayout;
     private RefreshListener mRefreshListener;
+    private OnTopMessage mOnTopMessage;
+    private boolean mShowTip;
+    private Context mContext;
 
     public PullToRefreshHelper(Activity activity, PullToRefreshLayout pullToRefreshLayout, RefreshListener listener) {
         init(activity, pullToRefreshLayout, listener, null);
@@ -26,6 +36,7 @@ public class PullToRefreshHelper implements OnRefreshListener {
 
     public void init(Activity activity, PullToRefreshLayout pullToRefreshLayout, RefreshListener listener,
                      java.lang.Class<?> viewClass) {
+        mContext = activity;
         mRefreshListener = listener;
         mPullToRefreshLayout = pullToRefreshLayout;
         mHeaderTransformer = new PullToRefreshHeaderTransformer();
@@ -41,6 +52,7 @@ public class PullToRefreshHelper implements OnRefreshListener {
             );
         }
         setupWizard.setup(mPullToRefreshLayout);
+        showTip(activity);
     }
 
     public void setRefreshing(boolean refreshing) {
@@ -51,9 +63,30 @@ public class PullToRefreshHelper implements OnRefreshListener {
     @Override
     public void onRefreshStarted(View view) {
         mRefreshListener.onRefreshStarted(view);
+        hideTip();
     }
 
     public interface RefreshListener {
         public void onRefreshStarted(View view);
+    }
+
+    private void hideTip() {
+        if (mShowTip) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+            Editor editor = preferences.edit();
+            editor.putBoolean(NEED_PTR_TIP, false);
+            editor.commit();
+            mOnTopMessage.hide();
+            mShowTip = false;
+        }
+    }
+
+    private void showTip(Activity activity) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        mShowTip = preferences.getBoolean(NEED_PTR_TIP, true);
+        if (mShowTip) {
+            mOnTopMessage = new OnTopMessage(activity, mPullToRefreshLayout);
+            mOnTopMessage.show(activity.getString(R.string.ptr_tip_message));
+        }
     }
 }
