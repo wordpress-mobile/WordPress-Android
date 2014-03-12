f5490dc9354f3b3e50308ed2fb8a584871ef55d6
Dan Roundhill <dan@automattic.com>
2014-03-12 15:44:15 -0700

    Moved some mime type methods to MediaUtils, and moved MediaUtils class to the 'util' folder.

--- src/org/wordpress/android/util/MediaUtils.java
+++ src/org/wordpress/android/util/MediaUtils.java
@@ -1,23 +1,7 @@
-package org.wordpress.android.ui.media;
-
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Random;
-import java.util.TimeZone;
+package org.wordpress.android.util;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -31,14 +15,26 @@ import android.support.v4.content.CursorLoader;
 import android.text.TextUtils;
 import android.webkit.MimeTypeMap;
 
-import org.wordpress.android.models.MediaFile;
-import org.wordpress.android.util.ImageHelper;
-import org.wordpress.android.util.WPImageSpan;
-import org.wordpress.passcodelock.AppLockManager;
-
 import org.wordpress.android.R;
 import org.wordpress.android.WordPress;
-import org.wordpress.android.util.Version;
+import org.wordpress.android.models.MediaFile;
+import org.wordpress.passcodelock.AppLockManager;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class MediaUtils {
 
@@ -405,5 +401,79 @@ public class MediaUtils {
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeStream(stream, null, options);
         return options.outMimeType;
+    }
+
+    public static String getMediaFileMimeType(File mediaFile) {
+        String originalFileName = mediaFile.getName().toLowerCase();
+        String mimeType = UrlUtils.getUrlMimeType(originalFileName);
+
+        if (TextUtils.isEmpty(mimeType)) {
+            try {
+                String filePathForGuessingMime = mediaFile.getPath().contains("://") ? mediaFile.getPath() : "file://"+mediaFile.getPath();
+                URL urlForGuessingMime = new URL(filePathForGuessingMime);
+                URLConnection uc = urlForGuessingMime.openConnection();
+                String guessedContentType = uc.getContentType(); //internally calls guessContentTypeFromName(url.getFile()); and guessContentTypeFromStream(is);
+                // check if returned "content/unknown"
+                if (!TextUtils.isEmpty(guessedContentType) && !guessedContentType.equals("content/unknown")) {
+                    mimeType = guessedContentType;
+                }
+            } catch (MalformedURLException e) {
+                AppLog.e(AppLog.T.API, "MalformedURLException while trying to guess the content type for the file here " + mediaFile.getPath() + " with URLConnection", e);
+            }
+            catch (IOException e) {
+                AppLog.e(AppLog.T.API, "Error while trying to guess the content type for the file here " + mediaFile.getPath() +" with URLConnection", e);
+            }
+        }
+
+        // No mimeType yet? Try to decode the image and get the mimeType from there
+        if (TextUtils.isEmpty(mimeType)) {
+            try {
+                DataInputStream inputStream = new DataInputStream(new FileInputStream(mediaFile));
+                String mimeTypeFromStream = getMimeTypeOfInputStream(inputStream);
+                if (!TextUtils.isEmpty(mimeTypeFromStream)) {
+                    mimeType = mimeTypeFromStream;
+                }
+                inputStream.close();
+            } catch (FileNotFoundException e) {
+                AppLog.e(AppLog.T.API, "FileNotFoundException while trying to guess the content type for the file " + mediaFile.getPath(), e);
+            } catch (IOException e) {
+                AppLog.e(AppLog.T.API, "IOException while trying to guess the content type for the file " + mediaFile.getPath(), e);
+            }
+        }
+
+        if (TextUtils.isEmpty(mimeType)) {
+            mimeType = "";
+        } else {
+            if (mimeType.equalsIgnoreCase("video/mp4v-es")) { //Fixes #533. See: http://tools.ietf.org/html/rfc3016
+                mimeType = "video/mp4";
+            }
+        }
+
+        return mimeType;
+    }
+
+    public static String getMediaFileName(File mediaFile, String mimeType) {
+        String originalFileName = mediaFile.getName().toLowerCase();
+        String extension = MimeTypeMap.getFileExtensionFromUrl(originalFileName);
+        if (!TextUtils.isEmpty(extension))  //File name already has the extension in it
+            return originalFileName;
+
+        if (!TextUtils.isEmpty(mimeType)) { //try to get the extension from mimeType
+            MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+            String fileExtensionFromMimeType = mimeTypeMap.getExtensionFromMimeType(mimeType);
+            if (!TextUtils.isEmpty(fileExtensionFromMimeType)) {
+                originalFileName += "." + fileExtensionFromMimeType.toLowerCase();
+            } else {
+                //We're still without an extension - split the mime type and retrieve it
+                String[] split = mimeType.split("/");
+                String guessedExt = split.length > 1 ? split[1] : split[0];
+                originalFileName += "." + guessedExt;
+            }
+        } else {
+            //No mimetype and no extension!!
+            AppLog.e(AppLog.T.API, "No mimetype and no extension for " + mediaFile.getPath());
+        }
+
+        return originalFileName;
     }
 }
