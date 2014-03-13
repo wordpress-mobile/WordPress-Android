57dbd35885c1bb48380a5c3fc1fe67430326a4ae
Nick Bradbury <nick.bradbury@gmail.com>
2014-03-13 09:20:42 -0400

    MediaImageLoader no longer extends ImageLoader

--- src/org/wordpress/android/ui/media/MediaImageLoader.java
+++ src/org/wordpress/android/ui/media/MediaImageLoader.java
@@ -15,18 +15,22 @@ import org.wordpress.android.util.VolleyUtils;
  * provides the ImageLoader and backing RequestQueue for media image requests - necessary because
  * images in protected blogs need to be authenticated, which requires a separate RequestQueue
  */
-class MediaImageLoader extends ImageLoader {
+class MediaImageLoader {
+    private MediaImageLoader() {
+        throw new AssertionError();
+    }
+
     static ImageLoader getInstance() {
         return getInstance(WordPress.getCurrentBlog());
     }
 
-    private static ImageLoader getInstance(Blog blog) {
+    static ImageLoader getInstance(Blog blog) {
         if (blog != null && VolleyUtils.isCustomHTTPClientStackNeeded(blog)) {
             // use ImageLoader with authenticating request queue for protected blogs
             AppLog.d(AppLog.T.MEDIA, "using custom imageLoader");
             Context context = WordPress.getContext();
             RequestQueue authRequestQueue = Volley.newRequestQueue(context, VolleyUtils.getHTTPClientStack(context, blog));
-            MediaImageLoader imageLoader = new MediaImageLoader(authRequestQueue, WordPress.getBitmapCache());
+            ImageLoader imageLoader = new ImageLoader(authRequestQueue, WordPress.getBitmapCache());
             imageLoader.setBatchedResponseDelay(0);
             return imageLoader;
         } else {
@@ -34,9 +38,5 @@ class MediaImageLoader extends ImageLoader {
             AppLog.d(AppLog.T.MEDIA, "using default imageLoader");
             return WordPress.imageLoader;
         }
-    }
-
-    private MediaImageLoader(RequestQueue queue, ImageCache imageCache) {
-        super(queue, imageCache);
     }
 }
