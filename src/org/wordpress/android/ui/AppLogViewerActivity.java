d4ce0af733b3e9f59f97e6a9f183bc9ea8abfa48
Nick Bradbury <nick.bradbury@gmail.com>
2014-03-14 07:00:28 -0400

    Removed share button, added share icon to ActionBar

--- src/org/wordpress/android/ui/AppLogViewerActivity.java
+++ src/org/wordpress/android/ui/AppLogViewerActivity.java
@@ -2,11 +2,14 @@ package org.wordpress.android.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.text.Html;
-import android.view.View;
-import android.widget.Button;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 import org.wordpress.android.R;
 import org.wordpress.android.util.AppLog;
@@ -16,24 +19,25 @@ import org.wordpress.android.util.ToastUtils;
  * Created by nbradbury on 7/16/13.
  * views the activity log (see utils/AppLog.java)
  */
-public class AppLogViewerActivity extends FragmentActivity {
+public class AppLogViewerActivity extends SherlockFragmentActivity {
     private TextView mTxtLogViewer;
+    private static final int ID_SHARE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.reader_activity_logviewer);
+        setContentView(R.layout.logviewer_activity);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         mTxtLogViewer = (TextView) findViewById(R.id.text_log);
         mTxtLogViewer.setText(Html.fromHtml(AppLog.toHtml()));
 
-        Button btnShare = (Button) findViewById(R.id.btn_share);
-        btnShare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                shareAppLog();
-            }
-        });
+        // this is necessary to enable the textView to scroll vertically
+        mTxtLogViewer.setMovementMethod(ScrollingMovementMethod.getInstance());
     }
 
     private void shareAppLog() {
@@ -45,6 +49,29 @@ public class AppLogViewerActivity extends FragmentActivity {
             startActivity(Intent.createChooser(intent, getString(R.string.reader_btn_share)));
         } catch (android.content.ActivityNotFoundException ex) {
             ToastUtils.showToast(this, R.string.reader_toast_err_share_intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuItem item = menu.add(Menu.NONE, ID_SHARE, Menu.NONE, R.string.reader_btn_share);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        item.setIcon(R.drawable.ab_icon_share);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case ID_SHARE:
+                shareAppLog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
