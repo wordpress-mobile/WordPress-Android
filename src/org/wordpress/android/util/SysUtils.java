dcc61dd2e082d282fe1234999aa101039cb02552
Maxime Biais <maxime.biais@gmail.com>
2014-05-01 07:42:43 +0800

    isGteAndroid4 now test if api >= 14 - in case we still need it later

--- src/org/wordpress/android/util/SysUtils.java
+++ src/org/wordpress/android/util/SysUtils.java
@@ -7,19 +7,18 @@ public class SysUtils {
         throw new AssertionError();
     }
 
-    /*
+    /**
      * returns true if device is running Android 4.0 (ICS) or later
      */
     public static boolean isGteAndroid4() {
-        return (Build.VERSION.SDK_INT >= 15);
+        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH);
     }
 
-    /*
+    /**
      * returns true on API 11 and above - called to determine whether
      * AsyncTask.executeOnExecutor() can be used
      */
     public static boolean canUseExecuteOnExecutor() {
         return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB);
     }
-
 }
