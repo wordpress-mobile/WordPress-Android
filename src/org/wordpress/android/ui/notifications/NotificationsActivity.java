3c98ad22c441553b6e396e78591c73dc4a81a06b
Maxime Biais <maxime.biais@gmail.com>
2014-04-30 10:18:20 +0800

    fix #1320: onPostClicked can be called from a separate thread, this thread can run after the activity onStop() and in this case, commiting the fragment will crash the app.
    Replacing a call to `commit()` by a call to `commitAllowingStateLoss()` in
    this particular case, will avoid a crash but also lose the fragment state:
    the selected comment or post won't be recreated.

--- src/org/wordpress/android/ui/notifications/NotificationsActivity.java
+++ src/org/wordpress/android/ui/notifications/NotificationsActivity.java
@@ -637,7 +637,7 @@ public class NotificationsActivity extends WPActionBarActivity
         ft.replace(R.id.layout_fragment_container, readerFragment, tagForFragment)
           .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
           .addToBackStack(tagForFragment)
-          .commit();
+          .commitAllowingStateLoss();
     }
 
     /**
@@ -653,7 +653,7 @@ public class NotificationsActivity extends WPActionBarActivity
         ft.replace(R.id.layout_fragment_container, commentFragment, tagForFragment)
           .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
           .addToBackStack(tagForFragment)
-          .commit();
+          .commitAllowingStateLoss();
     }
 
     private interface LoadNotesCallback {
