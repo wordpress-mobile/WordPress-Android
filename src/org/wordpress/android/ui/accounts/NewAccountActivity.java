4870df7d0994516327615a73c5f701745424dd3f
Maxime Biais <maxime.biais@gmail.com>
2014-06-05 19:42:30 +0800

    fix #1469: crash on account creation

--- src/org/wordpress/android/ui/accounts/NewAccountActivity.java
+++ src/org/wordpress/android/ui/accounts/NewAccountActivity.java
@@ -1,13 +1,13 @@
 package org.wordpress.android.ui.accounts;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Window;
 
 import org.wordpress.android.R;
 
 // TODO: merge it with WelcomeFragmentSignIn
-public class NewAccountActivity extends Activity {
+public class NewAccountActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
