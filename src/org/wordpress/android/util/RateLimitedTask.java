1f7748e66b37a1031fedb2812d3b966d75b4d34c
Maxime Biais <maxime.biais@gmail.com>
2014-06-16 12:11:37 +0200

    fix init state

--- src/org/wordpress/android/util/RateLimitedTask.java
+++ src/org/wordpress/android/util/RateLimitedTask.java
@@ -24,7 +24,7 @@ public abstract class RateLimitedTask {
 
     public synchronized boolean runIfNotLimited() {
         Date now = new Date();
-        if (DateTimeUtils.secondsBetween(now, mLastUpdate) >= mMinRateInSeconds) {
+        if (mLastUpdate == null || DateTimeUtils.secondsBetween(now, mLastUpdate) >= mMinRateInSeconds) {
             if (run()) {
                 mLastUpdate = now;
                 return true;
