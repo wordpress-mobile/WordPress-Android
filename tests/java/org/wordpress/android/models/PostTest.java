395303ddd9fa6880066901397f53029c84acc1c0
Maxime Biais <maxime.biais@gmail.com>
2014-06-27 14:48:26 +0200

    fix PostTest tests

--- tests/java/org/wordpress/android/models/PostTest.java
+++ tests/java/org/wordpress/android/models/PostTest.java
@@ -1,34 +1,42 @@
 package org.wordpress.android.models;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.InstrumentationTestCase;
+import android.test.RenamingDelegatingContext;
 
-import org.wordpress.android.WordPressDB;
+import org.wordpress.android.TestUtils;
+import org.wordpress.android.WordPress;
 
 public class PostTest extends InstrumentationTestCase {
-    private WordPressDB mDB;
+    protected Context mTestContext;
+    protected Context mTargetContext;
 
     @Override
     protected void setUp() throws Exception {
-        mDB = new WordPressDB(getInstrumentation().getContext());
+
+        mTargetContext = new RenamingDelegatingContext(getInstrumentation().getTargetContext(), "test_");
+        mTestContext = getInstrumentation().getContext();
 
         super.setUp();
     }
 
     public void testInvalidPostIdLoad() {
-        Post post = mDB.getPostForLocalTablePostId(-1);
+        SQLiteDatabase db = TestUtils.loadDBFromDump(mTargetContext, mTestContext, "taliwutt-blogs-sample.sql");
+        Post post = WordPress.wpDB.getPostForLocalTablePostId(-1);
 
         assertNull(post);
     }
 
     public void testPostSaveAndLoad() {
+        SQLiteDatabase db = TestUtils.loadDBFromDump(mTargetContext, mTestContext, "taliwutt-blogs-sample.sql");
         Post post = new Post(1, false);
         post.setTitle("test-post");
-        mDB.savePost(post);
+        WordPress.wpDB.savePost(post);
 
-        Post loadedPost = mDB.getPostForLocalTablePostId(post.getLocalTablePostId());
+        Post loadedPost = WordPress.wpDB.getPostForLocalTablePostId(post.getLocalTablePostId());
 
         assertNotNull(loadedPost);
         assertEquals(loadedPost.getTitle(), post.getTitle());
     }
-
 }
