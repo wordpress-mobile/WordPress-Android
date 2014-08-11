355056936b5521d2534a0b47d1adbc51021257b7
Nick Bradbury <nick.bradbury@gmail.com>
2014-08-11 10:58:19 -0400

    Rewrote the AppLog viewer to use a ListView+adapter

--- WordPress/src/main/java/org/wordpress/android/ui/AppLogViewerActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/AppLogViewerActivity.java
@@ -2,23 +2,29 @@ package org.wordpress.android.ui;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.wordpress.android.R;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.ToastUtils;
 
+import java.util.ArrayList;
+
 /**
  * views the activity log (see utils/AppLog.java)
  */
 public class AppLogViewerActivity extends Activity {
-    private TextView mTxtLogViewer;
     private static final int ID_SHARE = 1;
 
     @Override
@@ -32,11 +38,70 @@ public class AppLogViewerActivity extends Activity {
             actionBar.setDisplayShowTitleEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-        mTxtLogViewer = (TextView) findViewById(R.id.text_log);
-        mTxtLogViewer.setText(Html.fromHtml(AppLog.toHtml(this)));
 
-        // this is necessary to enable the textView to scroll vertically
-        mTxtLogViewer.setMovementMethod(ScrollingMovementMethod.getInstance());
+        final ListView listView = (ListView) findViewById(android.R.id.list);
+        listView.setAdapter(new LogAdapter(this));
+    }
+
+    private class LogAdapter extends BaseAdapter {
+        private final ArrayList<String> mEntries;
+        private final LayoutInflater mInflater;
+
+        private LogAdapter(Context context) {
+            mEntries = AppLog.toHtmlList(context);
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return mEntries.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mEntries.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final LogViewHolder holder;
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.logviewer_listitem, parent, false);
+                holder = new LogViewHolder(convertView);
+                convertView.setTag(holder);
+            } else {
+                holder = (LogViewHolder) convertView.getTag();
+            }
+
+            // take the header lines (app version & device name) into account or else the
+            // line numbers shown here won't match the line numbers when the log is shared
+            int lineNum = position - AppLog.HEADER_LINE_COUNT + 1;
+            if (lineNum > 0) {
+                holder.txtLineNumber.setText(String.format("%02d", lineNum));
+                holder.txtLineNumber.setVisibility(View.VISIBLE);
+            } else {
+                holder.txtLineNumber.setVisibility(View.GONE);
+            }
+
+            holder.txtLogEntry.setText(Html.fromHtml(mEntries.get(position)));
+
+            return convertView;
+        }
+
+        private class LogViewHolder {
+            private final TextView txtLineNumber;
+            private final TextView txtLogEntry;
+
+            LogViewHolder(View view) {
+                txtLineNumber = (TextView) view.findViewById(R.id.text_line);
+                txtLogEntry = (TextView) view.findViewById(R.id.text_log);
+            }
+        }
     }
 
     private void shareAppLog() {
