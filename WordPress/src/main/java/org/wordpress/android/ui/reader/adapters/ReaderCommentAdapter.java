2edc4900808e870a58d216046b7db843b4084f99
Nick Bradbury <nick.bradbury@gmail.com>
2014-08-25 19:37:50 -0400

    Cleanup WPImageGetter based on code analysis

--- WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderCommentAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderCommentAdapter.java
@@ -46,7 +46,6 @@ public class ReaderCommentAdapter extends BaseAdapter {
     private final int mBgColorHighlight;
     private final int mLinkColor;
     private final int mNoLinkColor;
-    private final int mMaxImageSz;
 
     private final String mLike;
     private final String mLikedBy;
@@ -76,7 +75,6 @@ public class ReaderCommentAdapter extends BaseAdapter {
         mInflater = LayoutInflater.from(context);
         mIndentPerLevel = (context.getResources().getDimensionPixelSize(R.dimen.reader_comment_indent_per_level) / 2);
         mAvatarSz = context.getResources().getDimensionPixelSize(R.dimen.avatar_sz_small);
-        mMaxImageSz = context.getResources().getDimensionPixelSize(R.dimen.reader_comment_max_image_size);
 
         mBgColorNormal = context.getResources().getColor(R.color.grey_extra_light);
         mBgColorHighlight = context.getResources().getColor(R.color.grey_light);
@@ -136,7 +134,7 @@ public class ReaderCommentAdapter extends BaseAdapter {
 
         holder.txtAuthor.setText(comment.getAuthorName());
         holder.imgAvatar.setImageUrl(PhotonUtils.fixAvatar(comment.getAuthorAvatar(), mAvatarSz), WPNetworkImageView.ImageType.AVATAR);
-        CommentUtils.displayHtmlComment(holder.txtText, comment.getText(), mMaxImageSz);
+        CommentUtils.displayHtmlComment(holder.txtText, comment.getText(), parent.getWidth());
 
         java.util.Date dtPublished = DateTimeUtils.iso8601ToJavaDate(comment.getPublished());
         holder.txtDate.setText(DateTimeUtils.javaDateToTimeSpan(dtPublished));
