b40578b2bbbb291b2d64e6545e7065e35a99f806
Nick Bradbury <nick.bradbury@gmail.com>
2014-09-02 09:24:59 -0400

    Removed unused context from previous merge

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostDetailFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostDetailFragment.java
@@ -238,7 +238,6 @@ public class ReaderPostDetailFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.reader_fragment_post_detail, container, false);
-        final Context context = container.getContext();
 
         // locate & init listView
         mListView = (WPListView) view.findViewById(android.R.id.list);
