cc66d426be5db513c9c1e3c322547e066940a087
Maxime Biais <maxime.biais@gmail.com>
2014-10-29 16:24:00 +0100

    new Autolink tests to check for existing <a href=>

--- WordPress/src/androidTest/java/org/wordpress/android/util/AutolinkUtilsTest.java
+++ WordPress/src/androidTest/java/org/wordpress/android/util/AutolinkUtilsTest.java
@@ -13,7 +13,9 @@ public class AutolinkUtilsTest extends InstrumentationTestCase {
     }
 
     public void testEmptyString() {
-        AutolinkUtils.autoCreateLinks("");
+        String sourceTest = "";
+        String output = AutolinkUtils.autoCreateLinks(sourceTest);
+        assertEquals(sourceTest, output);
     }
 
     public void testNonBlacklistedUrl1() {
@@ -23,23 +25,55 @@ public class AutolinkUtilsTest extends InstrumentationTestCase {
         assertEquals(expected, output);
     }
 
+    public void testNonBlacklistedUrl2() {
+        String sourceTest = "http://test.com http://test.com";
+        String output = AutolinkUtils.autoCreateLinks(sourceTest);
+        String expected = "<a href=\"http://test.com\">http://test.com</a> <a href=\"http://test.com\">http://test.com</a>";
+        assertEquals(expected, output);
+    }
+
+    public void testNonBlacklistedUrl3() {
+        String sourceTest = "http://test.com\nhttp://test.com";
+        String output = AutolinkUtils.autoCreateLinks(sourceTest);
+        String expected = "<a href=\"http://test.com\">http://test.com</a>\n<a href=\"http://test.com\">http://test.com</a>";
+        assertEquals(expected, output);
+    }
+
     public void testBlacklistedUrl1() {
-        String sourceTest = "http://youtube.com/xxx";
+        String sourceTest = "http://youtube.com/watch?test";
         String output = AutolinkUtils.autoCreateLinks(sourceTest);
         assertEquals(sourceTest, output);
     }
 
     public void testMixedUrls1() {
-        String sourceTest = "hey http://youtube.com/watch salut http://test.com hello";
+        String sourceTest = "hey http://youtube.com/watch?test salut http://test.com hello";
         String output = AutolinkUtils.autoCreateLinks(sourceTest);
-        String expected = "hey http://youtube.com/watch salut <a href=\"http://test.com\">http://test.com</a> hello";
+        String expected = "hey http://youtube.com/watch?test salut <a href=\"http://test.com\">http://test.com</a> hello";
         assertEquals(expected, output);
     }
 
-    public void testMixedUrls2() {
-        String sourceTest = "http://test.com http://www.youtube.com/wat http://test.com http://youtu.be/wat";
+    public void testExistingAHref1() {
+        String sourceTest = "<a href=\"http://test.com\">http://test.com</a>";
         String output = AutolinkUtils.autoCreateLinks(sourceTest);
-        String expected = "<a href=\"http://test.com\">http://test.com</a> http://www.youtube.com/wat <a href=\"http://test.com\">http://test.com</a> http://youtu.be/wat";
+        assertEquals(sourceTest, output);
+    }
+
+    public void testUndetectable1() {
+        String sourceTest = "testhttp://test.com";
+        String output = AutolinkUtils.autoCreateLinks(sourceTest);
+        assertEquals(sourceTest, output);
+    }
+
+    public void testUndetectable2() {
+        String sourceTest = "\"http://test.com\"";
+        String output = AutolinkUtils.autoCreateLinks(sourceTest);
+        assertEquals(sourceTest, output);
+    }
+
+    public void testMixedUrls2() {
+        String sourceTest = "http://test.com http://www.youtube.com/watch?test http://test.com http://youtu.be/wat";
+        String output = AutolinkUtils.autoCreateLinks(sourceTest);
+        String expected = "<a href=\"http://test.com\">http://test.com</a> http://www.youtube.com/watch?test <a href=\"http://test.com\">http://test.com</a> http://youtu.be/wat";
         assertEquals(expected, output);
     }
 }
