316af9f8fad13306f874e9ec7ef4a165ed5a8437
Tony Rankin <tonyr@tonyrankin.com>
2014-11-10 23:05:50 -0800

    Implementing Parcelable on MediaContent types.
    This will allow MediaContent to be serialized and returned as a result to the content selection activity.

--- WordPress/src/main/java/org/wordpress/android/ui/media/content/CaptureMediaContent.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/content/CaptureMediaContent.java
@@ -1,5 +1,7 @@
 package org.wordpress.android.ui.media.content;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 /**
  * Represents media content that will be captured. Can be of image or video type.
  */
@@ -11,8 +13,18 @@ public class CaptureMediaContent extends MediaContent {
 
     private int mCaptureType;
 
-    private CaptureMediaContent() {
-        mCaptureType = CAPTURE_TYPE_NONE;
+    public static final Parcelable.Creator<CaptureMediaContent> CREATOR =
+    new Parcelable.Creator<CaptureMediaContent>() {
+        public CaptureMediaContent createFromParcel(Parcel in) {
+            return new CaptureMediaContent(in);
+        }
+
+        public CaptureMediaContent[] newArray(int size) {
+            return new CaptureMediaContent[size];
+        }
+    };
+
+    public CaptureMediaContent(Parcel parcel) {
     }
 
     public CaptureMediaContent(int captureType) {
@@ -20,8 +32,32 @@ public class CaptureMediaContent extends MediaContent {
     }
 
     @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+    @Override
     public MEDIA_TYPE getType() {
         return MEDIA_TYPE.CAPTURE;
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return (mCaptureType == CAPTURE_TYPE_IMAGE) ? "Take a picture" : "Shoot a video";
+    }
+
+    @Override
+    public String getData() {
+        return "";
     }
 
     public void setCaptureType(int type) {
--- WordPress/src/main/java/org/wordpress/android/ui/media/content/DeviceImageMediaContent.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/content/DeviceImageMediaContent.java
@@ -1,16 +1,70 @@
 package org.wordpress.android.ui.media.content;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents images stored on the device.
  */
 
 public class DeviceImageMediaContent extends MediaContent {
+    private String mImageId;
     private String mDisplayName;
     private String mImageUri;
     private String mThumbUri;
 
-    public DeviceImageMediaContent(String uri) {
-        mImageUri = uri;
+    public static final Parcelable.Creator<DeviceImageMediaContent> CREATOR =
+    new Parcelable.Creator<DeviceImageMediaContent>() {
+        public DeviceImageMediaContent createFromParcel(Parcel in) {
+            return new DeviceImageMediaContent(in);
+        }
+
+        public DeviceImageMediaContent[] newArray(int size) {
+            return new DeviceImageMediaContent[size];
+        }
+    };
+
+    public DeviceImageMediaContent(Parcel parcel) {
+        List<String> data = new ArrayList<String>();
+        parcel.readStringList(data);
+
+        while(data.size() > 0) {
+            String contentData = data.remove(0);
+            String key = contentData.substring(0, contentData.indexOf('='));
+            String value = contentData.substring(contentData.indexOf('=') + 1, contentData.length());
+            writeDataFromParcel(key, value);
+        }
+    }
+
+    public DeviceImageMediaContent(String imageId) {
+        mImageId = imageId;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        List<String> stringList = new ArrayList<String>();
+        if (mImageId != null) {
+            stringList.add("mImageId=" + mImageId);
+        }
+        if (mDisplayName != null) {
+            stringList.add("mDisplayName=" + mDisplayName);
+        }
+        if (mImageUri != null) {
+            stringList.add("mImageUri=" + mImageUri);
+        }
+        if (mThumbUri != null) {
+            stringList.add("mThumbUri=" + mThumbUri);
+        }
+
+        dest.writeStringList(stringList);
     }
 
     @Override
@@ -19,13 +73,17 @@ public class DeviceImageMediaContent extends MediaContent {
     }
 
     @Override
-    public String getName() {
-        return mDisplayName;
+    public String getId() {
+        return mImageId;
     }
 
     @Override
     public String getData() {
         return mImageUri;
+    }
+
+    public String getName() {
+        return mDisplayName;
     }
 
     public void setName(String name) {
@@ -42,5 +100,17 @@ public class DeviceImageMediaContent extends MediaContent {
 
     public String getThumbUri() {
         return mThumbUri;
+    }
+
+    private void writeDataFromParcel(String key, String value) {
+        if (key.equals("mImageId")) {
+            mImageId = value;
+        } else if (key.equals("mImageUri")) {
+            mImageUri = value;
+        } else if (key.equals("mDisplayName")) {
+            mDisplayName = value;
+        } else if (key.equals("mThumbUri")) {
+            mThumbUri = value;
+        }
     }
 }
