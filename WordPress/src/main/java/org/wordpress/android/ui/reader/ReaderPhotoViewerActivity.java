f1656cdf5a48e3ae2ea634ba7831117c887b2314
Nick Bradbury <nick.bradbury@gmail.com>
2014-11-12 19:39:49 -0500

    Fixed reader photo viewer crash

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPhotoViewerActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPhotoViewerActivity.java
@@ -1,6 +1,5 @@
 package org.wordpress.android.ui.reader;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
@@ -9,7 +8,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 
 import org.wordpress.android.R;
@@ -39,8 +37,6 @@ public class ReaderPhotoViewerActivity extends ActionBarActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.reader_activity_photo_viewer);
 
         mViewPager = (ReaderViewPager) findViewById(R.id.viewpager);
