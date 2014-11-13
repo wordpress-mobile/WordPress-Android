457f4876791973377306da575c8870b552693b59
Nick Bradbury <nick.bradbury@gmail.com>
2014-11-13 12:03:02 -0500

    Corrected merge conflict in ReaderCommentListActivity

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderCommentListActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderCommentListActivity.java
@@ -6,7 +6,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.TextUtils;
