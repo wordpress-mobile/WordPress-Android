dc9d1ef1d5b3003434b98839013c517099ba0db0
Nick Bradbury <nick.bradbury@gmail.com>
2014-11-18 17:26:29 -0500

    Fixed startup crash in NewAccountActivity

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/NewAccountActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/NewAccountActivity.java
@@ -11,7 +11,6 @@ public class NewAccountActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_new_account);
     }
 }
