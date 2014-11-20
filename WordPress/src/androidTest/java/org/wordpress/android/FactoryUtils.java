c01f6d68206887222a49abd31e84ac4e8289e5e7
Maxime Biais <maxime.biais@gmail.com>
2014-11-20 17:44:28 +0100

    fix #985: maks sFactory private - force injection in test module

--- WordPress/src/androidTest/java/org/wordpress/android/FactoryUtils.java
+++ WordPress/src/androidTest/java/org/wordpress/android/FactoryUtils.java
@@ -11,13 +11,26 @@ import org.wordpress.android.util.AppLog.T;
 import org.wordpress.android.util.SystemServiceFactory;
 import org.xmlrpc.android.XMLRPCFactory;
 
+import java.lang.reflect.Field;
+
 public class FactoryUtils {
     public static void initWithTestFactories() {
         // create test factories
-        XMLRPCFactory.sFactory = new XMLRPCFactoryTest();
-        RestClientFactory.sFactory = new RestClientFactoryTest();
-        OAuthAuthenticatorFactory.sFactory = new OAuthAuthenticatorFactoryTest();
-        SystemServiceFactory.sFactory = new SystemServiceFactoryTest();
+        forceFactoryInjection(XMLRPCFactory.class, new XMLRPCFactoryTest());
+        forceFactoryInjection(RestClientFactory.class, new RestClientFactoryTest());
+        forceFactoryInjection(OAuthAuthenticatorFactory.class, new OAuthAuthenticatorFactoryTest());
+        forceFactoryInjection(SystemServiceFactory.class, new SystemServiceFactoryTest());
         AppLog.v(T.TESTS, "Mocks factories instantiated");
+    }
+
+    private static void forceFactoryInjection(Class klass, Object factory) {
+        try {
+            Field field = klass.getDeclaredField("sFactory");
+            field.setAccessible(true);
+            field.set(null, factory);
+            AppLog.v(T.TESTS, "Factory " + klass + " injected");
+        } catch (Exception e) {
+            AppLog.e(T.TESTS, "Can't inject test factory " + klass);
+        }
     }
 }
