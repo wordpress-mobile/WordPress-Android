cb8321b16a20b2c860c7bf0e89e140c37a4626c7
Maxime Biais <maxime.biais@gmail.com>
2014-11-20 17:44:28 +0100

    fix #985: maks sFactory private - force injection in test module

--- WordPressUtils/src/main/java/org/wordpress/android/util/SystemServiceFactory.java
+++ WordPressUtils/src/main/java/org/wordpress/android/util/SystemServiceFactory.java
@@ -2,16 +2,13 @@ package org.wordpress.android.util;
 
 import android.content.Context;
 
-import org.wordpress.android.util.AppLog.T;
-
 public class SystemServiceFactory {
-    public static SystemServiceFactoryAbstract sFactory;
+    private static SystemServiceFactoryAbstract sFactory;
 
     public static Object get(Context context, String name) {
         if (sFactory == null) {
             sFactory = new SystemServiceFactoryDefault();
         }
-        AppLog.v(T.UTILS, "instantiate SystemService using sFactory: " + sFactory.getClass());
         return sFactory.get(context, name);
     }
 }
