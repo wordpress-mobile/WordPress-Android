d7234f2e2c556523a1510cf4b3b21a7655876cbe
Nick Bradbury <nick.bradbury@gmail.com>
2014-11-23 14:56:50 -0500

    Fix bug that caused ReaderBlogInfoView to overlap post list

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderBlogInfoView.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderBlogInfoView.java
@@ -45,6 +45,8 @@ public class ReaderBlogInfoView extends LinearLayout {
         super(context);
 
         View view = LayoutInflater.from(context).inflate(R.layout.reader_blog_info_view, this, true);
+        view.setId(R.id.layout_blog_info_view);
+
         mImageMshot = (WPNetworkImageView) view.findViewById(R.id.image_mshot);
 
         // set mshot width based on display width
