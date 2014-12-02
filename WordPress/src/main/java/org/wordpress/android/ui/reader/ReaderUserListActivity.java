c98bf6d147c80d310568fdc0c367e84caa3b0032
Nick Bradbury <nick.bradbury@gmail.com>
2014-12-02 05:48:55 -0500

    User list now correctly restores position after rotation

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderUserListActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderUserListActivity.java
@@ -2,6 +2,7 @@ package org.wordpress.android.ui.reader;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -20,13 +21,10 @@ import org.wordpress.android.util.DisplayUtils;
  */
 public class ReaderUserListActivity extends ActionBarActivity {
 
+    private static final String KEY_RESTORE_POSITION = "restore_position";
+    private ReaderRecyclerView mRecyclerView;
     private ReaderUserAdapter mAdapter;
-    private ReaderUserAdapter getAdapter() {
-        if (mAdapter == null) {
-            mAdapter = new ReaderUserAdapter(this);
-        }
-        return mAdapter;
-    }
+    private int mRestorePosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +38,46 @@ public class ReaderUserListActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        if (savedInstanceState != null) {
+            mRestorePosition = savedInstanceState.getInt(KEY_RESTORE_POSITION);
+        }
+
+        int spacingHorizontal = getResources().getDimensionPixelSize(R.dimen.reader_detail_margin);
+        int spacingVertical = DisplayUtils.dpToPx(this, 1);
+        mRecyclerView = (ReaderRecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new ReaderRecyclerView.ReaderItemDecoration(spacingHorizontal, spacingVertical));
+
         long blogId = getIntent().getLongExtra(ReaderConstants.ARG_BLOG_ID, 0);
         long postId = getIntent().getLongExtra(ReaderConstants.ARG_POST_ID, 0);
         long commentId = getIntent().getLongExtra(ReaderConstants.ARG_COMMENT_ID, 0);
-
-        ReaderRecyclerView recyclerView = (ReaderRecyclerView) findViewById(R.id.recycler_view);
-        int spacingHorizontal = getResources().getDimensionPixelSize(R.dimen.reader_detail_margin);
-        int spacingVertical = DisplayUtils.dpToPx(this, 1);
-        recyclerView.addItemDecoration(new ReaderRecyclerView.ReaderItemDecoration(spacingHorizontal, spacingVertical));
-
-        recyclerView.setAdapter(getAdapter());
         loadUsers(blogId, postId, commentId);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        int position = ((LinearLayoutManager) mRecyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+        if (position > 0) {
+            outState.putInt(KEY_RESTORE_POSITION, position);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private ReaderUserAdapter getAdapter() {
+        if (mAdapter == null) {
+            mAdapter = new ReaderUserAdapter(this);
+            mAdapter.setDataLoadedListener(new ReaderInterfaces.DataLoadedListener() {
+                @Override
+                public void onDataLoaded(boolean isEmpty) {
+                    if (!isEmpty && mRestorePosition > 0) {
+                        mRecyclerView.scrollToPosition(mRestorePosition);
+                    }
+                    mRestorePosition = 0;
+                }
+            });
+            mRecyclerView.setAdapter(mAdapter);
+        }
+        return mAdapter;
     }
 
     @Override
--- WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderUserAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderUserAdapter.java
@@ -134,4 +134,8 @@ public class ReaderUserAdapter  extends RecyclerView.Adapter<ReaderUserAdapter.U
             }
         }.start();
     }
+
+    public int indexOfUserId(long userId) {
+        return (mUsers != null ? mUsers.indexOfUserId(userId) : -1);
+    }
 }
