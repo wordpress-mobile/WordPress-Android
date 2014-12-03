536d20a4c71572540628563c20f747fc33deb4b4
Maxime Biais <maxime.biais@gmail.com>
2014-12-03 15:26:03 +0100

    auto save and retrieve text onAttach and onDetach

--- WordPress/src/main/java/org/wordpress/android/widgets/SuggestionAutoCompleteText.java
+++ WordPress/src/main/java/org/wordpress/android/widgets/SuggestionAutoCompleteText.java
@@ -1,7 +1,9 @@
 package org.wordpress.android.widgets;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.MultiAutoCompleteTextView;
 
 import org.wordpress.android.ui.suggestion.util.SuggestionTokenizer;
@@ -26,5 +28,32 @@ public class SuggestionAutoCompleteText extends MultiAutoCompleteTextView {
         TypefaceCache.setCustomTypeface(context, this, attrs);
         this.setTokenizer(new SuggestionTokenizer());
         this.setThreshold(1);
+    }
+
+    private String getViewPathId(View view) {
+        StringBuilder sb = new StringBuilder();
+        for (View currentView = view; currentView != null && currentView.getParent() != null
+                && currentView.getParent() instanceof View; currentView = (View) currentView.getParent()) {
+            sb.append(currentView.getId());
+        }
+        return sb.toString();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("TODO", Context.MODE_PRIVATE);
+        String text = sharedPreferences.getString(getViewPathId(this), "");
+        setText(text);
+        setSelection(text.length());
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("TODO", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(getViewPathId(this), getText().toString());
+        editor.apply();
     }
 }
