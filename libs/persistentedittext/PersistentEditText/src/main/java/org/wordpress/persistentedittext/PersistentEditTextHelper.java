5f11818ec2086aa05e60c3b4a39f3faf9c8240b8
Maxime Biais <maxime.biais@gmail.com>
2014-12-08 15:42:47 +0100

    add a comment to explain setUniqueId method

--- libs/persistentedittext/PersistentEditText/src/main/java/org/wordpress/persistentedittext/PersistentEditTextHelper.java
+++ libs/persistentedittext/PersistentEditText/src/main/java/org/wordpress/persistentedittext/PersistentEditTextHelper.java
@@ -12,21 +12,20 @@ public class PersistentEditTextHelper {
         mPersistentEditTextDatabase = new PersistentEditTextDatabase(context);
     }
 
+    /**
+     * Set a unique id. This is only needed to identify a text edit from another if they share the same layout
+     * path and id.
+     *
+     * For instance if you want to use the same fragment with one text field to display different content and want
+     * the saved text in the EditField to be differentiated, you'll have to set that id. Another example: if you have
+     * a listview with an EditText in each rows, you might want to set a different id for each row.
+     */
     public void setUniqueId(String uniqueId) {
         mUniqueId = uniqueId;
     }
 
     public String getUniqueId() {
         return mUniqueId;
-    }
-
-    private static String getViewPathId(View view) {
-        StringBuilder sb = new StringBuilder();
-        for (View currentView = view; currentView != null && currentView.getParent() != null
-                && currentView.getParent() instanceof View; currentView = (View) currentView.getParent()) {
-            sb.append(currentView.getId());
-        }
-        return sb.toString();
     }
 
     public void clearSavedText(View view) {
@@ -50,5 +49,14 @@ public class PersistentEditTextHelper {
 
     public void clearSavedText(View view, String uniqueId) {
         mPersistentEditTextDatabase.remove(getViewPathId(view) + uniqueId);
+    }
+
+    protected static String getViewPathId(View view) {
+        StringBuilder sb = new StringBuilder();
+        for (View currentView = view; currentView != null && currentView.getParent() != null
+                && currentView.getParent() instanceof View; currentView = (View) currentView.getParent()) {
+            sb.append(currentView.getId());
+        }
+        return sb.toString();
     }
 }
