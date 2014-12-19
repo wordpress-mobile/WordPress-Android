929ab966cfa0336a89b44b616c9f02546a342c0a
Nick Bradbury <nick.bradbury@gmail.com>
2014-12-19 05:35:59 -0500

    Added SuppressLint("NewApi")

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListFragment.java
@@ -1,6 +1,7 @@
 package org.wordpress.android.ui.reader;
 
 import android.animation.Animator;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
@@ -347,6 +348,7 @@ public class ReaderPostListFragment extends Fragment {
     /*
      * animate in the blog/tag info header after a brief delay
      */
+    @SuppressLint("NewApi")
     private void animateHeader() {
         new Handler().postDelayed(new Runnable() {
             @Override
