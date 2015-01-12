b48038c3489e2ec9cf087e6789e8be354e1c0bbb
Nick Bradbury <nick.bradbury@gmail.com>
2015-01-12 18:24:48 -0500

    Removed paging between groups of recommended blogs, now showing all 20 at once in a single list

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderConstants.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderConstants.java
@@ -1,14 +1,12 @@
 package org.wordpress.android.ui.reader;
 
 public class ReaderConstants {
-    public static final int  READER_MAX_POSTS_TO_REQUEST      = 20;     // max # posts to request when updating posts
-    public static final int  READER_MAX_POSTS_TO_DISPLAY      = 200;    // max # posts to display
-    public static final int  READER_MAX_COMMENTS_TO_REQUEST   = 20;     // max # top-level comments to request when updating comments
-    public static final int  READER_MAX_USERS_TO_DISPLAY      = 500;    // max # users to show in ReaderUserListActivity
-    public static final long READER_AUTO_UPDATE_DELAY_MINUTES = 10;     // 10 minute delay between automatic updates
-
-    public static final int  READER_MAX_RECOMMENDED_TO_REQUEST = 40;     // max # of recommended blogs to request
-    public static final int  READER_MAX_RECOMMENDED_TO_DISPLAY = 4;      // max # of recommended blogs to display
+    public static final int  READER_MAX_POSTS_TO_REQUEST       = 20;     // max # posts to request when updating posts
+    public static final int  READER_MAX_POSTS_TO_DISPLAY       = 200;    // max # posts to display
+    public static final int  READER_MAX_COMMENTS_TO_REQUEST    = 20;     // max # top-level comments to request when updating comments
+    public static final int  READER_MAX_USERS_TO_DISPLAY       = 500;    // max # users to show in ReaderUserListActivity
+    public static final long READER_AUTO_UPDATE_DELAY_MINUTES  = 10;     // 10 minute delay between automatic updates
+    public static final int  READER_MAX_RECOMMENDED_TO_REQUEST = 20;     // max # of recommended blogs to request
 
     public static final int MIN_FEATURED_IMAGE_WIDTH = 640;              // min width for an image to be suitable featured image
 
