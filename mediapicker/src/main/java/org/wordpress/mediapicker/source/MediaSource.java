6def305ed1aa02c4ad45d7f38b1b5ca8511311df
Tony Rankin <tonyr@tonyrankin.com>
2015-01-30 15:44:01 -0800

    Updating MediaSource interface and defining capture items.

--- mediapicker/src/main/java/org/wordpress/mediapicker/source/MediaSource.java
+++ mediapicker/src/main/java/org/wordpress/mediapicker/source/MediaSource.java
@@ -1,9 +1,26 @@
 package org.wordpress.mediapicker.source;
 
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.toolbox.ImageLoader;
+
 import org.wordpress.mediapicker.MediaItem;
 
-import java.util.List;
+/**
+ * MediaSource's are used to gather {@link org.wordpress.mediapicker.MediaItem}'s and create
+ * {@link android.view.View}'s for displaying them.
+ */
 
-public interface MediaSource {
-    public List<MediaItem> getMediaItems();
+public interface MediaSource extends Parcelable {
+    // Get the number of MediaItems accessible through the source
+    public int getCount();
+    // Get the MediaItem at the specified position
+    public MediaItem getMedia(int position);
+    // Get the View to display the MediaItem at the specified position TODO: remove cache from here
+    public View getView(int position, View convertView, ViewGroup parent, LayoutInflater inflater, ImageLoader.ImageCache cache);
+    // Callback when MediaItem is selected; return true if handled internally, false to propagate
+    public boolean onMediaItemSelected(final MediaItem mediaItem, boolean selected);
 }
--- mediapicker/src/main/java/org/wordpress/mediapicker/source/MediaSourceCaptureVideo.java
+++ mediapicker/src/main/java/org/wordpress/mediapicker/source/MediaSourceCaptureVideo.java
@@ -1,14 +1,32 @@
 package org.wordpress.mediapicker.source;
 
-import org.wordpress.mediapicker.MediaItem;
+import android.os.Parcel;
 
-import java.util.List;
+import org.wordpress.mediapicker.R;
 
-public class MediaSourceCaptureVideo implements MediaSource {
-    public static final String TAG_CAPTURE_VIDEO = "capture-video";
+public class MediaSourceCaptureVideo extends MediaSourceCaptureImage {
+    @Override
+    protected int getOverlayResource() {
+        return R.drawable.video;
+    }
+
+    public static final Creator<MediaSourceCaptureVideo> CREATOR =
+            new Creator<MediaSourceCaptureVideo>() {
+                public MediaSourceCaptureVideo createFromParcel(Parcel in) {
+                    return new MediaSourceCaptureVideo();
+                }
+
+                public MediaSourceCaptureVideo[] newArray(int size) {
+                    return new MediaSourceCaptureVideo[size];
+                }
+            };
 
     @Override
-    public List<MediaItem> getMediaItems() {
-        return null;
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
     }
 }
