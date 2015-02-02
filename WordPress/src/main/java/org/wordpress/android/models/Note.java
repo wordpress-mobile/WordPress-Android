13ddc9876eada99fd0ef44d8e90833bc2833ae9d
Dan Roundhill <dan@automattic.com>
2015-02-02 15:56:36 -0800

    Adds support for showing the recently added comment subject noticon in the notifications list view.
    Fixes #2276

--- WordPress/src/main/java/org/wordpress/android/models/Note.java
+++ WordPress/src/main/java/org/wordpress/android/models/Note.java
@@ -5,6 +5,7 @@ package org.wordpress.android.models;
 
 import android.text.Html;
 import android.text.Spannable;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.simperium.client.BucketSchema;
@@ -160,6 +161,24 @@ public class Note extends Syncable {
                 return commentSubject;
             }
 
+        }
+
+        return "";
+    }
+
+    private String getCommentSubjectNoticon() {
+        JSONArray subjectRanges = queryJSON("subject[0].ranges", new JSONArray());
+        if (subjectRanges != null) {
+            for (int i=0; i < subjectRanges.length(); i++) {
+                try {
+                    JSONObject rangeItem = subjectRanges.getJSONObject(i);
+                    if (rangeItem.has("type") && rangeItem.optString("type").equals("noticon")) {
+                        return rangeItem.optString("value", "");
+                    }
+                } catch (JSONException e) {
+                    return "";
+                }
+            }
         }
 
         return "";
@@ -426,6 +445,7 @@ public class Note extends Syncable {
         static public final String NOTICON_INDEX = "noticon";
         static public final String ICON_URL_INDEX = "icon";
         static public final String IS_UNAPPROVED_INDEX = "unapproved";
+        static public final String COMMENT_SUBJECT_NOTICON = "comment_subject_noticon";
         static public final String LOCAL_STATUS = "local_status";
 
         private static final Indexer<Note> sNoteIndexer = new Indexer<Note>() {
@@ -447,6 +467,7 @@ public class Note extends Syncable {
                 indexes.add(new Index(NOTICON_INDEX, note.getNoticonCharacter()));
                 indexes.add(new Index(ICON_URL_INDEX, note.getIconURL()));
                 indexes.add(new Index(IS_UNAPPROVED_INDEX, note.getCommentStatus() == CommentStatus.UNAPPROVED));
+                indexes.add(new Index(COMMENT_SUBJECT_NOTICON, note.getCommentSubjectNoticon()));
                 indexes.add(new Index(LOCAL_STATUS, note.getLocalStatus()));
 
                 return indexes;
