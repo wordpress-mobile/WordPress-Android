cfc3ff0bff65d18b78bcdedc2bd87dab636688f3
Maxime Biais <maxime.biais@gmail.com>
2015-02-04 17:21:53 +0100

    use title/content params in the editor

--- editor/src/main/java/org/wordpress/android/editor/EditorFragment.java
+++ editor/src/main/java/org/wordpress/android/editor/EditorFragment.java
@@ -21,30 +21,18 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class EditorFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM_TITLE = "param_title";
+    private static final String ARG_PARAM_CONTENT = "param_content";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mParamTitle;
+    private String mParamContent;
     private WebView mWebView;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditorFragment newInstance(String param1, String param2) {
+    public static EditorFragment newInstance(String title, String content) {
         EditorFragment fragment = new EditorFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM_TITLE, title);
+        args.putString(ARG_PARAM_CONTENT, content);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +44,8 @@ public class EditorFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParamTitle = getArguments().getString(ARG_PARAM_TITLE);
+            mParamContent = getArguments().getString(ARG_PARAM_CONTENT);
         }
     }
 
