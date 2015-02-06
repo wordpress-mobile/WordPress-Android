7c08749cf0afde80267beb2313e1476169e83f70
Maxime Biais <maxime.biais@gmail.com>
2015-02-06 17:55:17 +0100

    move resources from main project to editor

---
+++ libs/editor/editor/src/main/java/org/wordpress/android/editor/LegacyEditorActivity.java
@@ -0,0 +1,12 @@
+package org.wordpress.android.editor;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class LegacyEditorActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
