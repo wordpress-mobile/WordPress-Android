27525be54d0c0062cda10f19e3ffdedd8856f402
Nick Bradbury <nick.bradbury@gmail.com>
2015-02-16 17:56:15 -0500

    Removed unnecessary semicolons

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderEvents.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderEvents.java
@@ -4,9 +4,9 @@ package org.wordpress.android.ui.reader;
  * Reader-related EventBus event classes
  */
 public class ReaderEvents {
-    public static class FollowedTagsChanged {};
-    public static class RecommendedTagsChanged{};
+    public static class FollowedTagsChanged {}
+    public static class RecommendedTagsChanged{}
 
-    public static class FollowedBlogsChanged {};
-    public static class RecommendedBlogsChanged {};
+    public static class FollowedBlogsChanged {}
+    public static class RecommendedBlogsChanged {}
 }
