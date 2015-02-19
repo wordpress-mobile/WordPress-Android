9b68570fa0c680f553a044c7784172dce27deb34
Dan Roundhill <dan@automattic.com>
2015-02-19 10:53:10 -0800

    Simplified authenticate method to fall back to using the token stored in the shared preferences.

--- WordPress/src/main/java/org/wordpress/android/networking/OAuthAuthenticator.java
+++ WordPress/src/main/java/org/wordpress/android/networking/OAuthAuthenticator.java
@@ -11,38 +11,20 @@ public class OAuthAuthenticator implements Authenticator {
     @Override
     public void authenticate(final AuthenticatorRequest request) {
         String siteId = request.getSiteId();
-        String token = null;
-        Blog blog;
 
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(WordPress.getContext());
-        if (siteId == null) {
-            // Use the global access token
-            token = settings.getString(WordPress.ACCESS_TOKEN_PREFERENCE, null);
-        } else {
-            blog = WordPress.wpDB.getBlogForDotComBlogId(siteId);
+        String token = settings.getString(WordPress.ACCESS_TOKEN_PREFERENCE, null);
+
+        if (siteId != null) {
+            // Get the token for a Jetpack site if needed
+            Blog blog = WordPress.wpDB.getBlogForDotComBlogId(siteId);
 
             if (blog != null) {
-                // get the access token from api key field. Jetpack blogs linked with a different wpcom
-                // account have the token stored here.
-                token = blog.getApi_key();
+                String jetpackToken = blog.getApi_key();
 
-                // valid oauth tokens are 64 chars
-                if (token != null && token.length() < 64 && !blog.isDotcomFlag()) {
-                    token = null;
-                }
-
-                // if there is no access token, we need to check if it is a dotcom blog, or a jetpack
-                // blog linked with the main wpcom account.
-                if (token == null) {
-                    if (blog.isDotcomFlag() && blog.getUsername().equals(settings.getString(
-                            WordPress.WPCOM_USERNAME_PREFERENCE, ""))) {
-                        token = settings.getString(WordPress.ACCESS_TOKEN_PREFERENCE, null);
-                    } else if (blog.isJetpackPowered()) {
-                        if (blog.getDotcom_username() == null || blog.getDotcom_username().equals(settings.getString(
-                                WordPress.WPCOM_USERNAME_PREFERENCE, ""))) {
-                            token = settings.getString(WordPress.ACCESS_TOKEN_PREFERENCE, null);
-                        }
-                    }
+                // valid OAuth tokens are 64 chars
+                if (jetpackToken != null && jetpackToken.length() == 64 && !blog.isDotcomFlag()) {
+                    token = jetpackToken;
                 }
             }
         }
