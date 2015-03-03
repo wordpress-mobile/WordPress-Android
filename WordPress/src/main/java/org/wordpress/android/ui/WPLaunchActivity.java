06c63ba6ceaf10bdf9534cf4fdf27250a19c5d0b
Tony Rankin <tonyr@tonyrankin.com>
2015-03-03 14:07:49 -0800

    Applying locale preference on app launch.

--- WordPress/src/main/java/org/wordpress/android/ui/WPLaunchActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/WPLaunchActivity.java
@@ -1,17 +1,24 @@
 package org.wordpress.android.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 
 import org.wordpress.android.R;
 import org.wordpress.android.WordPress;
 import org.wordpress.android.ui.posts.PostsActivity;
 import org.wordpress.android.ui.prefs.AppPrefs;
+import org.wordpress.android.ui.prefs.SettingsFragment;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.AppLog.T;
 import org.wordpress.android.util.ToastUtils;
 import org.wordpress.android.util.WPActivityUtils;
+
+import java.util.Locale;
 
 public class WPLaunchActivity extends ActionBarActivity {
 
@@ -23,6 +30,9 @@ public class WPLaunchActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        applyLocale();
+
         setContentView(R.layout.activity_launch);
 
         if (WordPress.wpDB == null) {
@@ -42,5 +52,24 @@ public class WPLaunchActivity extends ActionBarActivity {
         }
         startActivity(intent);
         finish();
+    }
+
+    private void applyLocale() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        if (sharedPreferences.contains(SettingsFragment.SETTINGS_PREFERENCES)) {
+            String locale = sharedPreferences.getString(SettingsFragment.SETTINGS_PREFERENCES, "");
+
+            if (!locale.equals(Locale.getDefault().getDisplayLanguage())) {
+                Resources resources = getResources();
+                Configuration conf = resources.getConfiguration();
+                conf.locale = new Locale(locale);
+                resources.updateConfiguration(conf, resources.getDisplayMetrics());
+
+                Intent refresh = new Intent(this, WPLaunchActivity.class);
+                startActivity(refresh);
+                finish();
+            }
+        }
     }
 }
