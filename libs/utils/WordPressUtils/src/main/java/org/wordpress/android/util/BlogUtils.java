5b709e2fd33c66094fabbf5b6889082dc2acb4a1
Maxime Biais <maxime.biais@gmail.com>
2015-03-06 17:04:55 +0100

    fix #2328: sort blogs with an empty title by their hostname

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/BlogUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/BlogUtils.java
@@ -6,19 +6,10 @@ import java.util.Map;
 public class BlogUtils {
     public static Comparator<Object> BlogNameComparator = new Comparator<Object>() {
         public int compare(Object blog1, Object blog2) {
-            Map<Object, Object> blogMap1 = (Map<Object, Object>) blog1;
-            Map<Object, Object> blogMap2 = (Map<Object, Object>) blog2;
-
-            String blogName1 = MapUtils.getMapStr(blogMap1, "blogName");
-            if (blogName1.length() == 0) {
-                blogName1 = MapUtils.getMapStr(blogMap1, "url");
-            }
-
-            String blogName2 = MapUtils.getMapStr(blogMap2, "blogName");
-            if (blogName2.length() == 0) {
-                blogName2 = MapUtils.getMapStr(blogMap2, "url");
-            }
-
+            Map<String, Object> blogMap1 = (Map<String, Object>) blog1;
+            Map<String, Object> blogMap2 = (Map<String, Object>) blog2;
+            String blogName1 = getBlogNameOrHostNameFromAccountMap(blogMap1);
+            String blogName2 = getBlogNameOrHostNameFromAccountMap(blogMap2);
             return blogName1.compareToIgnoreCase(blogName2);
         }
     };
