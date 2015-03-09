a007ee51d27ecb8284054a8bc97adb05d9f0f1c7
Nick Bradbury <nick.bradbury@gmail.com>
2015-03-09 16:18:45 -0400

    Site picker now slides in

--- WordPress/src/main/java/org/wordpress/android/ui/ActivityLauncher.java
+++ WordPress/src/main/java/org/wordpress/android/ui/ActivityLauncher.java
@@ -2,13 +2,21 @@ package org.wordpress.android.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+
+import org.wordpress.android.R;
 
 public class ActivityLauncher {
 
     public static void showSitePickerForResult(Activity activity, boolean visibleAccountsOnly) {
         Intent intent = new Intent(activity, SitePickerActivity.class);
         intent.putExtra(SitePickerActivity.ARG_VISIBLE_ONLY, visibleAccountsOnly);
-        activity.startActivityForResult(intent, RequestCodes.SITE_PICKER);
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeCustomAnimation(
+                activity,
+                R.anim.activity_slide_in_from_left,
+                R.anim.do_nothing);
+        ActivityCompat.startActivityForResult(activity, intent, RequestCodes.SITE_PICKER, options.toBundle());
     }
 
 }
