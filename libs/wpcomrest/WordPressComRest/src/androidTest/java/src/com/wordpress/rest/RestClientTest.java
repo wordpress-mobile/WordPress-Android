362037101b0c644167f8d5398125c01566871db1
Maxime Biais <maxime.biais@gmail.com>
2015-04-22 17:58:08 +0200

    fix wpcomrest test: testGetAbsoluteURLWithLeadingSlash

--- libs/wpcomrest/WordPressComRest/src/androidTest/java/src/com/wordpress/rest/RestClientTest.java
+++ libs/wpcomrest/WordPressComRest/src/androidTest/java/src/com/wordpress/rest/RestClientTest.java
@@ -5,7 +5,8 @@ import android.test.AndroidTestCase;
 public class RestClientTest extends AndroidTestCase {
     public void testGetAbsoluteURLWithLeadingSlash(){
         String path = "/sites/mobileprojects.wordpress.com/posts";
-        String url = RestClient.getAbsoluteURL(path);
+        RestClient restClient = new RestClient(null);
+        String url = restClient.getAbsoluteURL(path);
         String expected = String.format("https://public-api.wordpress.com/rest/v1%s", path);
         assertEquals(expected, url);
     }
