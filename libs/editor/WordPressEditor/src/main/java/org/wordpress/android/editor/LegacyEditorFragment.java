e7c25e68ffeb81171282ae61fd4a95ddfe2a555f
Maxime Biais <maxime.biais@gmail.com>
2015-05-06 13:05:11 +0200

    fix #2635: check if data is null

--- libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/LegacyEditorFragment.java
+++ libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/LegacyEditorFragment.java
@@ -321,7 +321,7 @@ public class LegacyEditorFragment extends EditorFragmentAbstract implements Text
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == LegacyEditorFragment.ACTIVITY_REQUEST_CODE_CREATE_LINK) {
+        if (requestCode == LegacyEditorFragment.ACTIVITY_REQUEST_CODE_CREATE_LINK && data != null) {
             Bundle extras = data.getExtras();
             if (extras == null) {
                 return;
