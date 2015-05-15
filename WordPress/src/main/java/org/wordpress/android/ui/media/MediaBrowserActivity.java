d1cb5291924e11eff82fcafdaf71af5245fde8b7
Tony Rankin <tonyr@tonyrankin.com>
2015-05-14 19:06:05 -0700

    Showing image after it downloads.

--- WordPress/src/main/java/org/wordpress/android/ui/media/MediaBrowserActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/MediaBrowserActivity.java
@@ -40,6 +40,7 @@ import org.wordpress.android.ui.media.MediaGridFragment.Filter;
 import org.wordpress.android.ui.media.MediaGridFragment.MediaGridListener;
 import org.wordpress.android.ui.media.MediaItemFragment.MediaItemFragmentCallback;
 import org.wordpress.android.ui.media.services.MediaDeleteService;
+import org.wordpress.android.ui.media.services.MediaUploadEvents;
 import org.wordpress.android.util.ActivityUtils;
 import org.wordpress.android.util.NetworkUtils;
 import org.wordpress.android.widgets.WPAlertDialogFragment;
@@ -50,6 +51,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * The main activity in which the user can browse their media.
@@ -125,11 +128,13 @@ public class MediaBrowserActivity extends ActionBarActivity implements MediaGrid
     @Override
     public void onStart() {
         super.onStart();
+        EventBus.getDefault().register(this);
         registerReceiver(mReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
     }
 
     @Override
     public void onStop() {
+        EventBus.getDefault().unregister(this);
         unregisterReceiver(mReceiver);
         super.onStop();
     }
@@ -495,6 +500,11 @@ public class MediaBrowserActivity extends ActionBarActivity implements MediaGrid
         if (cursor != null) {
             cursor.close();
         }
+    }
+
+    @SuppressWarnings("unused")
+    public void onEventMainThread(MediaUploadEvents.MediaUploadSucceed event) {
+        onMediaAdded(event.mLocalId);
     }
 
     @Override
