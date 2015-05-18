8fa45c81948fdd352bfd44d1e069144d92804368
Nick Bradbury <nick.bradbury@gmail.com>
2015-05-18 19:27:39 -0400

    Use `x` rather than back arrow in site picker ActionBar

--- WordPress/src/main/java/org/wordpress/android/ui/main/SitePickerActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/SitePickerActivity.java
@@ -40,6 +40,7 @@ public class SitePickerActivity extends ActionBarActivity
     private SitePickerAdapter mAdapter;
     private ActionMode mActionMode;
     private int mCurrentLocalId;
+    private boolean mDidUserSelectSite;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +50,7 @@ public class SitePickerActivity extends ActionBarActivity
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
@@ -116,7 +118,9 @@ public class SitePickerActivity extends ActionBarActivity
     @Override
     public void finish() {
         super.finish();
-        overridePendingTransition(R.anim.do_nothing, R.anim.activity_slide_out_to_left);
+        if (mDidUserSelectSite) {
+            overridePendingTransition(R.anim.do_nothing, R.anim.activity_slide_out_to_left);
+        }
     }
 
     @Override
@@ -241,6 +245,7 @@ public class SitePickerActivity extends ActionBarActivity
             WordPress.setCurrentBlog(site.localId);
             WordPress.wpDB.updateLastBlogId(site.localId);
             setResult(RESULT_OK);
+            mDidUserSelectSite = true;
             finish();
         }
     }
