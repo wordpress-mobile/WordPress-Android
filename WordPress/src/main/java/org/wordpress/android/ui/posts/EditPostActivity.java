c7ade142f114cbc1279f568a1d1485941d17f1f6
Alex Forcier <alex.forcier@automattic.com>
2015-05-26 11:34:54 -0400

    Fixed issue where preview was not being updated when saveAndFinish() was called on an empty (non-new) post

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -615,25 +615,30 @@ public class EditPostActivity extends ActionBarActivity implements EditorFragmen
             // new and empty post? delete it
             if (mIsNewPost) {
                 WordPress.wpDB.deletePost(mPost);
+                finish();
+                return;
             }
         } else if (mOriginalPost != null && !mPost.hasChanges(mOriginalPost)) {
             // if no changes have been made to the post, set it back to the original don't save it
             WordPress.wpDB.updatePost(mOriginalPost);
             WordPress.currentPost = mOriginalPost;
+            finish();
+            return;
         } else {
             // changes have been made, save the post and ask for the post list to refresh.
             // We consider this being "manual save", it will replace some Android "spans" by an html
             // or a shortcode replacement (for instance for images and galleries)
             savePost(false);
-            WordPress.currentPost = mPost;
-            Intent i = new Intent();
-            i.putExtra(EXTRA_SHOULD_REFRESH, true);
-            i.putExtra(EXTRA_SAVED_AS_LOCAL_DRAFT, true);
-            i.putExtra(EXTRA_IS_PAGE, mIsPage);
-            setResult(RESULT_OK, i);
-
-            ToastUtils.showToast(this, R.string.editor_toast_changes_saved);
         }
+        WordPress.currentPost = mPost;
+        Intent i = new Intent();
+        i.putExtra(EXTRA_SHOULD_REFRESH, true);
+        i.putExtra(EXTRA_SAVED_AS_LOCAL_DRAFT, true);
+        i.putExtra(EXTRA_IS_PAGE, mIsPage);
+        setResult(RESULT_OK, i);
+
+        ToastUtils.showToast(this, R.string.editor_toast_changes_saved);
+
         finish();
     }
 
