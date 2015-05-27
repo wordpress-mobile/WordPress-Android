2f3bb7ab1f8771646e28edf1af97f183d46b9dc8
Will Kwon <will@willkwon.com>
2015-05-26 19:25:35 -0700

    Adding tests for SitePickerSearchView

---
+++ WordPress/src/androidTest/java/org/wordpress/android/ui/main/SitePickerSearchViewTest.java
@@ -0,0 +1,46 @@
+package org.wordpress.android.ui.main;
+
+import android.test.InstrumentationTestCase;
+import android.view.MenuItem;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SitePickerSearchViewTest extends InstrumentationTestCase {
+    private SitePickerSearchView mSitePickerSearchView;
+    private SitePickerActivity mMockSitePickerActivity;
+    private MenuItem mMockMenuItem;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mSitePickerSearchView = new SitePickerSearchView(getInstrumentation().getContext());
+        mMockSitePickerActivity = mock(SitePickerActivity.class);
+        mMockMenuItem = mock(MenuItem.class);
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        mSitePickerSearchView = null;
+        mMockSitePickerActivity = null;
+        mMockMenuItem = null;
+    }
+
+    public void testConstructorSetsInputMethodManager() {
+        assertNotNull(mSitePickerSearchView.getInputMethodManager());
+    }
+
+    public void testConfigureSetsIconifiedByDefaultToFalse() {
+        mSitePickerSearchView.configure(mMockSitePickerActivity, mMockMenuItem);
+
+        assertFalse(mSitePickerSearchView.isIconfiedByDefault());
+    }
+
+    public void testGetInputMethodManagerReturnsNotNull() {
+        assertNotNull(mSitePickerSearchView.getInputMethodManager());
+    }
+}
