2eb03815c40ed69d6392e7b53f9922092f517f8e
Danilo Ercoli <ercoli@gmail.com>
2015-06-16 12:03:44 +0200

    Make sure the context passed to checkConnection is not null

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/NetworkUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/NetworkUtils.java
@@ -77,6 +77,9 @@ public class NetworkUtils {
      * and returns false
      */
     public static boolean checkConnection(Context context) {
+        if(context == null) {
+            return false;
+        }
         if (isNetworkAvailable(context)) {
             return true;
         }
