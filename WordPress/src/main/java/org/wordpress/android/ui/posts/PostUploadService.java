e1ea69c26f9c373809cee7373c7fd169164ce351
Maxime Biais <maxime.biais@gmail.com>
2015-06-19 18:22:01 +0200

    add crashlytics non-fatal exception logging

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostUploadService.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostUploadService.java
@@ -37,6 +37,7 @@ import org.wordpress.android.ui.posts.PostUploadEvents.PostUploadFailed;
 import org.wordpress.android.ui.posts.PostUploadEvents.PostUploadSucceed;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.AppLog.T;
+import org.wordpress.android.util.CrashlyticsUtils;
 import org.wordpress.android.util.DisplayUtils;
 import org.wordpress.android.util.ImageUtils;
 import org.wordpress.android.util.MediaUtils;
@@ -947,6 +948,8 @@ public class PostUploadService extends Service {
             try {
                 mNotificationManager.notify(mNotificationId, mNotificationBuilder.build());
             } catch (RuntimeException runtimeException) {
+                CrashlyticsUtils.logException(runtimeException, CrashlyticsUtils.ExceptionType.SPECIFIC,
+                        AppLog.T.UTILS, "See issue #2858");
                 AppLog.d(T.POSTS, "See issue #2858; notify failed with:" + runtimeException);
             }
         }
