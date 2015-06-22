571aba094bee2d437524f74da08063d15d814992
Dan Roundhill <dan@automattic.com>
2015-06-22 13:32:30 -0600

    Placed NotificationsSettingsFragment in a wrapper activity.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/NotificationsSettingsActivity.java
@@ -0,0 +1,51 @@
+package org.wordpress.android.ui.prefs;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import org.wordpress.android.R;
+import org.wordpress.android.ui.ActivityLauncher;
+
+// Simple wrapper activity for NotificationsSettingsFragment
+public class NotificationsSettingsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setElevation(0.0f);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        setContentView(R.layout.settings_activity);
+
+        setTitle(R.string.notifications);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if (savedInstanceState == null) {
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, new NotificationsSettingsFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        ActivityLauncher.slideOutToRight(this);
+    }
+}
