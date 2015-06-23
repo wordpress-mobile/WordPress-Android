723e4a033591b8df92e1413b95d763d482b022d2
Danilo Ercoli <ercoli@gmail.com>
2015-06-23 12:54:02 +0200

    Fix #2911 by checking that Extras is not null

--- WordPress/src/main/java/org/wordpress/android/ui/stats/StatsSingleItemDetailsActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/StatsSingleItemDetailsActivity.java
@@ -182,7 +182,7 @@ public class StatsSingleItemDetailsActivity extends AppCompatActivity
             if (savedInstanceState.containsKey(ARG_YEARS_EXPANDED_ROWS)) {
                 mYearsIdToExpandedMap = savedInstanceState.getParcelable(ARG_YEARS_EXPANDED_ROWS);
             }
-        } else if (getIntent() != null) {
+        } else if (getIntent() != null && getIntent().getExtras() != null) {
             Bundle extras = getIntent().getExtras();
             mRemoteItemID = extras.getString(ARG_REMOTE_ITEM_ID);
             mRemoteBlogID = extras.getString(ARG_REMOTE_BLOG_ID);
