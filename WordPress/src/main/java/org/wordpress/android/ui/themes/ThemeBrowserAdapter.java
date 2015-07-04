4b1bd36bb50040f2c3c8a01a07134170e493ae6b
Will Kwon <will@willkwon.com>
2015-07-03 21:12:39 -0700

    First pass at new theme with cardview

--- WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeBrowserAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeBrowserAdapter.java
@@ -19,36 +19,24 @@ import org.wordpress.android.WordPress;
 import org.wordpress.android.util.DisplayUtils;
 
 /**
- * Adapter for the {@link ThemeBrowserFragment}'s gridview
+ * Adapter for the {@link ThemeBrowserFragment}'s listview
  *
  */
 public class ThemeBrowserAdapter extends CursorAdapter {
     private final LayoutInflater mInflater;
-    private final int mColumnWidth;
-    private final int mColumnHeight;
-    private final Drawable mIconPremium;
-    private final Drawable mIconCurrent;
-    private final int m32DpToPx;
 
     public ThemeBrowserAdapter(Context context, Cursor c, boolean autoRequery) {
         super(context, c, autoRequery);
         mInflater = LayoutInflater.from(context);
-        mColumnWidth = getColumnWidth(context);
-        mColumnHeight = (int) (0.75f * mColumnWidth);
-        mIconPremium = context.getResources().getDrawable(R.drawable.theme_icon_tag_premium);
-        mIconCurrent = context.getResources().getDrawable(R.drawable.theme_icon_tag_current);
-        m32DpToPx = DisplayUtils.dpToPx(context, 32);
     }
 
     private static class ThemeViewHolder {
         private final NetworkImageView imageView;
         private final TextView nameView;
-        private final ImageView themeAttr;
 
         ThemeViewHolder(View view) {
             imageView = (NetworkImageView) view.findViewById(R.id.theme_grid_item_image);
             nameView = (TextView) view.findViewById(R.id.theme_grid_item_name);
-            themeAttr = (ImageView) view.findViewById(R.id.theme_grid_attributes);
         }
     }
 
@@ -59,11 +47,6 @@ public class ThemeBrowserAdapter extends CursorAdapter {
         ThemeViewHolder themeViewHolder = new ThemeViewHolder(view);
         view.setTag(themeViewHolder);
 
-        // size the imageView to fit the column - image will be requested at this same width
-        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) themeViewHolder.imageView.getLayoutParams();
-        params.width = mColumnWidth;
-        params.height = mColumnHeight;
-
         return view;
     }
 
@@ -73,20 +56,8 @@ public class ThemeBrowserAdapter extends CursorAdapter {
 
         final String screenshotURL =  cursor.getString(cursor.getColumnIndex("screenshotURL"));
         final String name = cursor.getString(cursor.getColumnIndex("name"));
-        final int isPremiumTheme = cursor.getInt(cursor.getColumnIndex("isPremium"));
-        final int isCurrentTheme = cursor.getInt(cursor.getColumnIndex("isCurrent"));
 
         themeViewHolder.nameView.setText(name);
-
-        if (isCurrentTheme != 0) {
-            themeViewHolder.themeAttr.setVisibility(View.VISIBLE);
-            themeViewHolder.themeAttr.setImageDrawable(mIconCurrent);
-        } else if (isPremiumTheme != 0) {
-            themeViewHolder.themeAttr.setVisibility(View.VISIBLE);
-            themeViewHolder.themeAttr.setImageDrawable(mIconPremium);
-        } else {
-            themeViewHolder.themeAttr.setVisibility(View.GONE);
-        }
 
         ScreenshotHolder urlHolder = (ScreenshotHolder) themeViewHolder.imageView.getTag();
         if (urlHolder == null) {
@@ -100,20 +71,7 @@ public class ThemeBrowserAdapter extends CursorAdapter {
             urlHolder.requestURL = screenshotURL;
         }
 
-        themeViewHolder.imageView.setImageUrl(screenshotURL + "?w=" + mColumnWidth, WordPress.imageLoader);
-        view.setLayoutParams(new GridView.LayoutParams(mColumnWidth, mColumnHeight + m32DpToPx));
-    }
-
-    // The theme previews are 600x450 px, resulting in a ratio of 0.75
-    // We'll try to max the width, while keeping the padding ratio correct.
-    // Then we'll determine the height based on the width and the 0.75 ratio
-    private static int getColumnWidth(Context context) {
-        // Padding is 4 dp between the grid columns and on the outside
-        int columnCount = context.getResources().getInteger(R.integer.themes_grid_num_columns);
-        int dp4 = DisplayUtils.dpToPx(context, 4);
-        int padding = (columnCount + 1) * dp4;
-        int maxWidth = context.getResources().getDisplayMetrics().widthPixels;
-        return (maxWidth - padding) / columnCount;
+        themeViewHolder.imageView.setImageUrl(screenshotURL + "?w=" + 500, WordPress.imageLoader);
     }
 
     static class ScreenshotHolder {
