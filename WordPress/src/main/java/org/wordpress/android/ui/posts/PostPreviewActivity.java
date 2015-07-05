f4d363bb3d1255388d0e93bb5cab551ccbc5e48b
Nick Bradbury <nick.bradbury@gmail.com>
2015-07-05 15:27:03 -0400

    Title is now shown in webView, dropped title textView

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostPreviewActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostPreviewActivity.java
@@ -9,7 +9,6 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
-import android.widget.TextView;
 
 import org.wordpress.android.R;
 import org.wordpress.android.WordPress;
@@ -28,7 +27,6 @@ public class PostPreviewActivity extends AppCompatActivity {
     public static final String ARG_LOCAL_BLOG_ID = "local_blog_id";
     public static final String ARG_IS_PAGE = "is_page";
 
-    private TextView mTitleTextView;
     private WebView mWebView;
     private long mLocalPostId;
     private int mLocalBlogId;
@@ -47,7 +45,6 @@ public class PostPreviewActivity extends AppCompatActivity {
             actionBar.setDisplayShowTitleEnabled(true);
         }
 
-        mTitleTextView = (TextView) findViewById(R.id.text_post_title);
         mWebView = (WebView) findViewById(R.id.webView);
 
         if (savedInstanceState != null) {
@@ -114,13 +111,8 @@ public class PostPreviewActivity extends AppCompatActivity {
             return;
         }
 
-        String title = (TextUtils.isEmpty(post.getTitle())
-                ? "(" + getResources().getText(R.string.untitled) + ")"
-                : StringUtils.unescapeHTML(post.getTitle()));
-        mTitleTextView.setText(title);
-
         mWebView.loadDataWithBaseURL(
-                "file:///android_asset/",
+                null,
                 formatPostContentForWebView(post),
                 "text/html",
                 "utf-8",
@@ -128,30 +120,41 @@ public class PostPreviewActivity extends AppCompatActivity {
     }
 
     private String formatPostContentForWebView(Post post) {
-        String content = post.getDescription();
+        String title = (TextUtils.isEmpty(post.getTitle())
+                ? "(" + getResources().getText(R.string.untitled) + ")"
+                : StringUtils.unescapeHTML(post.getTitle()));
+
+        String postContent = post.getDescription();
         if (!TextUtils.isEmpty(post.getMoreText())) {
-            content += "\n\n" + post.getMoreText();
+            postContent += "\n\n" + post.getMoreText();
         }
 
         // if this is a local draft, remove src="null" from image tags then replace the "android-uri"
         // tag added for local image with a valid "src" tag so local images can be viewed
         if (post.isLocalDraft()) {
-            content = content.replace("src=\"null\"", "").replace("android-uri=", "src=");
+            postContent = postContent.replace("src=\"null\"", "").replace("android-uri=", "src=");
         }
 
         String textColorStr = HtmlUtils.colorResToHtmlColor(this, R.color.grey_dark);
         String linkColorStr = HtmlUtils.colorResToHtmlColor(this, R.color.reader_hyperlink);
 
+        int contentMargin = getResources().getDimensionPixelSize(R.dimen.content_margin);
+        String marginStr =  Integer.toString(contentMargin) + "px";
+
         return "<!DOCTYPE html><html><head><meta charset='UTF-8' />"
                 + "<meta name='viewport' content='width=device-width, initial-scale=1'>"
-                + "<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Merriweather:300' />"
+                + "<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Merriweather:300,700' />"
                 + "<style type='text/css'>"
-                + "  body { font-family: Merriweather, serif; margin: 0px; padding: 0px; width: 100%; color: " + textColorStr + "; }"
+                + "  html { margin-left: " + marginStr + "; margin-right: " + marginStr + "; }"
+                + "  body { font-family: Merriweather, serif; font-weight: 300; padding: 0px; width: 100%; color: " + textColorStr + "; }"
                 + "  body, p, div { max-width: 100% !important; word-wrap: break-word; }"
                 + "  p, div { line-height: 1.6em; font-size: 0.95em; }"
-                + "  h1, h2 { line-height: 1.2em; }"
+                + "  h1 { font-size: 1.2em; font-family: Merriweather, serif; font-weight: 700; }"
                 + "  img { max-width: 100%; }"
                 + "  a { text-decoration: none; color: " + linkColorStr + "; }"
-                + "</style></head><body>" + StringUtils.addPTags(content) + "</body></html>";
+                + "</style></head><body>"
+                + "<h1>" + title + "</h1>"
+                + StringUtils.addPTags(postContent)
+                + "</body></html>";
     }
 }
