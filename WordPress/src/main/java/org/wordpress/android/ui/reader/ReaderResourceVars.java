d3831f7377010f17b65b335eea7977fe28791360
Nick Bradbury <nick.bradbury@gmail.com>
2015-08-06 17:37:05 -0400

    Fixed full-size image width and cleaned up ReaderResourceVars

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderResourceVars.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderResourceVars.java
@@ -11,23 +11,16 @@ import org.wordpress.android.util.HtmlUtils;
  * class which holds all resource-based variables used when rendering post detail
  */
 class ReaderResourceVars {
-    final int displayWidthPx;
-    final int likeAvatarSizePx;
     final int extraSmallAvatarSizePx;
 
-    final int marginLargePx;
     final int marginSmallPx;
     final int marginExtraSmallPx;
-    final int detailMarginWidthPx;
 
     final int fullSizeImageWidthPx;
     final int featuredImageHeightPx;
 
     final int videoWidthPx;
     final int videoHeightPx;
-
-    final int colorGreyExtraLight;
-    final int mediumAnimTime;
 
     final String linkColorStr;
     final String greyMediumDarkStr;
@@ -37,28 +30,23 @@ class ReaderResourceVars {
     ReaderResourceVars(Context context) {
         Resources resources = context.getResources();
 
-        displayWidthPx = DisplayUtils.getDisplayPixelWidth(context);
-        likeAvatarSizePx = resources.getDimensionPixelSize(R.dimen.avatar_sz_small);
+        int displayWidthPx = DisplayUtils.getDisplayPixelWidth(context);
+        int marginLargePx = resources.getDimensionPixelSize(R.dimen.margin_large);
+        int detailMarginWidthPx = resources.getDimensionPixelOffset(R.dimen.reader_detail_margin);
+
         extraSmallAvatarSizePx = resources.getDimensionPixelSize(R.dimen.avatar_sz_extra_small);
         featuredImageHeightPx = resources.getDimensionPixelSize(R.dimen.reader_featured_image_height);
 
-        marginLargePx = resources.getDimensionPixelSize(R.dimen.margin_large);
         marginSmallPx = resources.getDimensionPixelSize(R.dimen.margin_small);
         marginExtraSmallPx = resources.getDimensionPixelSize(R.dimen.margin_extra_small);
-        detailMarginWidthPx = resources.getDimensionPixelOffset(R.dimen.reader_detail_margin);
-
-        colorGreyExtraLight = resources.getColor(R.color.grey_lighten_30);
-        mediumAnimTime = resources.getInteger(android.R.integer.config_mediumAnimTime);
 
         linkColorStr = HtmlUtils.colorResToHtmlColor(context, R.color.reader_hyperlink);
         greyMediumDarkStr = HtmlUtils.colorResToHtmlColor(context, R.color.grey_darken_30);
         greyLightStr = HtmlUtils.colorResToHtmlColor(context, R.color.grey_light);
         greyExtraLightStr = HtmlUtils.colorResToHtmlColor(context, R.color.grey_lighten_30);
 
-        // full-size image width must take margin and padding into account
-        int listPadding = resources.getDimensionPixelOffset(R.dimen.margin_large);
-        int imageWidth = displayWidthPx - (detailMarginWidthPx * 2) - (listPadding * 2);
-        fullSizeImageWidthPx = imageWidth;
+        // full-size image width must take margin into account
+        fullSizeImageWidthPx = displayWidthPx - (detailMarginWidthPx * 2);
 
         // 16:9 ratio (YouTube standard)
         videoWidthPx = fullSizeImageWidthPx - (marginLargePx * 2);
