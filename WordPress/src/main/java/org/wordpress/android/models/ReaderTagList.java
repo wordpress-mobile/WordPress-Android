601bc0a073b39969ec48b2acbe62dfda8b5151a0
Nick Bradbury <nick.bradbury@gmail.com>
2015-08-24 11:23:59 -0400

    Changed tag adapters to perform isSameList() comparison on main thread

--- WordPress/src/main/java/org/wordpress/android/models/ReaderTagList.java
+++ WordPress/src/main/java/org/wordpress/android/models/ReaderTagList.java
@@ -4,12 +4,7 @@ import java.util.ArrayList;
 
 public class ReaderTagList extends ArrayList<ReaderTag> {
 
-    @Override
-    public Object clone() {
-        return super.clone();
-    }
-
-    public int indexOfTagName(final String tagName) {
+    public int indexOfTagName(String tagName) {
         if (tagName == null || isEmpty()) {
             return -1;
         }
