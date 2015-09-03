732f7a5395db899b3642ea959a070fd681a14e3b
Will Kwon <will@willkwon.com>
2015-09-02 23:21:00 -0700

    Convert Spinner to ImageButton with PopupMenu

--- WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeBrowserAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeBrowserAdapter.java
@@ -3,17 +3,18 @@ package org.wordpress.android.ui.themes;
 import android.content.Context;
 import android.database.Cursor;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.NetworkImageView;
 
 import org.wordpress.android.R;
 import org.wordpress.android.WordPress;
-import org.wordpress.android.widgets.HeaderGridView;
 
 /**
  * Adapter for the {@link ThemeBrowserFragment}'s listview
@@ -21,25 +22,25 @@ import org.wordpress.android.widgets.HeaderGridView;
  */
 public class ThemeBrowserAdapter extends CursorAdapter {
     private final LayoutInflater mInflater;
-    private final HeaderGridView mGridView;
+    private final ThemeBrowserFragment.ThemeBrowserFragmentCallback mCallback;
 
-    public ThemeBrowserAdapter(Context context, Cursor c, boolean autoRequery, HeaderGridView gridView) {
+    public ThemeBrowserAdapter(Context context, Cursor c, boolean autoRequery, ThemeBrowserFragment.ThemeBrowserFragmentCallback callback) {
         super(context, c, autoRequery);
         mInflater = LayoutInflater.from(context);
-        mGridView = gridView;
+        mCallback = callback;
     }
 
     private static class ThemeViewHolder {
         private final NetworkImageView imageView;
         private final TextView nameView;
         private final TextView priceView;
-        private final Spinner spinner;
+        private final ImageButton imageButton;
 
         ThemeViewHolder(View view) {
             imageView = (NetworkImageView) view.findViewById(R.id.theme_grid_item_image);
             nameView = (TextView) view.findViewById(R.id.theme_grid_item_name);
             priceView = (TextView) view.findViewById(R.id.theme_grid_item_price);
-            spinner = (Spinner) view.findViewById(R.id.theme_grid_item_spinner);
+            imageButton = (ImageButton) view.findViewById(R.id.theme_grid_item_image_button);
         }
     }
 
@@ -60,10 +61,16 @@ public class ThemeBrowserAdapter extends CursorAdapter {
         final String screenshotURL = cursor.getString(cursor.getColumnIndex("screenshot"));
         final String name = cursor.getString(cursor.getColumnIndex("name"));
         final String price = cursor.getString(cursor.getColumnIndex("price"));
+        final String themeId = cursor.getString(cursor.getColumnIndex("id"));
 
         themeViewHolder.nameView.setText(name);
         themeViewHolder.priceView.setText(price);
 
+        configureImageView(themeViewHolder, screenshotURL);
+        configureImageButton(context, themeViewHolder, themeId);
+    }
+
+    private void configureImageView(ThemeViewHolder themeViewHolder, String screenshotURL) {
         ScreenshotHolder urlHolder = (ScreenshotHolder) themeViewHolder.imageView.getTag();
         if (urlHolder == null) {
             urlHolder = new ScreenshotHolder();
@@ -77,12 +84,43 @@ public class ThemeBrowserAdapter extends CursorAdapter {
             urlHolder.requestURL = screenshotURL;
         }
 
-        themeViewHolder.imageView.setImageUrl(screenshotURL + "?w=" + mGridView.getWidth(), WordPress.imageLoader);
         themeViewHolder.imageView.setImageUrl(screenshotURL + "?w=" + 500, WordPress.imageLoader);
+    }
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context, R.array.themes_more_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        themeViewHolder.spinner.setAdapter(adapter);
+    private void configureImageButton(Context context, ThemeViewHolder themeViewHolder, final String themeId) {
+        final PopupMenu popupMenu = new PopupMenu(context, themeViewHolder.imageButton);
+        popupMenu.getMenuInflater().inflate(R.menu.theme_more, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.string.activate:
+                        mCallback.onActivateSelected(themeId);
+                        break;
+                    case R.string.preview:
+                        mCallback.onPreviewSelected(themeId);
+                        break;
+                    case R.string.demo:
+                        mCallback.onDemoSelected(themeId);
+                        break;
+                    case R.string.details:
+                        mCallback.onDetailsSelected(themeId);
+                        break;
+                    case R.string.support:
+                    default:
+                        mCallback.onSupportSelected(themeId);
+                        break;
+                }
+
+                return true;
+            }
+        });
+        themeViewHolder.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupMenu.show();
+            }
+        });
     }
 
     static class ScreenshotHolder {
