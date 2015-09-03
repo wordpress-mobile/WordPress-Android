1f59bde39309b908884a62e92d77c4d48e8a2745
Nick Bradbury <nick.bradbury@gmail.com>
2015-09-03 15:02:25 -0400

    Added unit test

---
+++ WordPress/src/androidTest/java/org/wordpress/android/ui/posts/PostUtilsTest.java
@@ -0,0 +1,23 @@
+package org.wordpress.android.ui.posts;
+
+import android.test.AndroidTestCase;
+
+public class PostUtilsTest extends AndroidTestCase {
+
+    public void testCollapseShortcodes() {
+        String postContent = "Text before first gallery [gallery number=\"one\"]"
+                + " text between galleries"
+                + " [gallery number=\"two\"]"
+                + " text after second gallery"
+                + " [unknown shortcode].";
+        String collapsedContent = PostUtils.collapseShortcodes(postContent);
+
+        // make sure [gallery] now exists and [gallery number] does not
+        assertTrue(collapsedContent.contains("[gallery]"));
+        assertFalse(collapsedContent.contains("[gallery number]"));
+
+        // make sure the unknown shortcode is intact
+        assertTrue(collapsedContent.contains("[unknown shortcode]"));
+    }
+
+}
