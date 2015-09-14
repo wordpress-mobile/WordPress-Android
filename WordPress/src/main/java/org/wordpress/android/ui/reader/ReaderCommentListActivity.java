e79251b5aa9d19f9a44909729fe183d9d3125a78
Dan Roundhill <dan@automattic.com>
2015-09-14 11:05:46 -0700

    Restores enabling the post title click in the reader comments list.
    Fixes #3180

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderCommentListActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderCommentListActivity.java
@@ -264,6 +264,11 @@ public class ReaderCommentListActivity extends AppCompatActivity {
                 }
             });
 
+            // Enable post title click if we came from notifications with a commentId
+            if (mCommentId > 0) {
+                mCommentAdapter.setTitleClickEnabled(true);
+            }
+
             // adapter calls this when data has been loaded & displayed
             mCommentAdapter.setDataLoadedListener(new ReaderInterfaces.DataLoadedListener() {
                 @Override
