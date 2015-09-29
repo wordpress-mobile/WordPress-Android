71e3c4efb82316c8633c41d35ffb4877f1ddb894
Nick Bradbury <nick.bradbury@gmail.com>
2015-09-29 15:55:29 -0400

    Set correct db version

--- WordPress/src/main/java/org/wordpress/android/datasets/ReaderDatabase.java
+++ WordPress/src/main/java/org/wordpress/android/datasets/ReaderDatabase.java
@@ -19,7 +19,7 @@ import java.io.OutputStream;
  */
 public class ReaderDatabase extends SQLiteOpenHelper {
     protected static final String DB_NAME = "wpreader.db";
-    private static final int DB_VERSION = 107;
+    private static final int DB_VERSION = 108;
 
     /*
      * version history
