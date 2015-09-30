83ffc00e7ff2e9e95f9c4fb1ba2422222f0c6b18
Tony Rankin <tonyr@tonyrankin.com>
2015-09-30 15:28:29 -0700

    Using string resources

--- WordPress/src/main/java/org/wordpress/android/widgets/WPSwitch.java
+++ WordPress/src/main/java/org/wordpress/android/widgets/WPSwitch.java
@@ -5,8 +5,17 @@ import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
 
 public class WPSwitch extends SwitchCompat {
+    public WPSwitch(Context context) {
+        super(context);
+    }
+
     public WPSwitch(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypefaceCache.setCustomTypeface(context, this, attrs);
+    }
+
+    public WPSwitch(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
         TypefaceCache.setCustomTypeface(context, this, attrs);
     }
 }
