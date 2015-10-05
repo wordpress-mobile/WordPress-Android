25b8ba856787414c90ecbb908f20282e699e5d10
Will Kwon <will@willkwon.com>
2015-10-05 11:23:48 -0700

    Fixing lifecycle issue where Search rotate would make a new copy every time

--- WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeBrowserActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeBrowserActivity.java
@@ -277,6 +277,7 @@ public class ThemeBrowserActivity extends AppCompatActivity implements ThemeBrow
 
     private void addFragment() {
         if (mIsInSearchMode) {
+            addBrowserFragment();
             addSearchFragment();
         } else {
             addBrowserFragment();
@@ -286,7 +287,6 @@ public class ThemeBrowserActivity extends AppCompatActivity implements ThemeBrow
     private void addBrowserFragment() {
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.theme_browser_container, mThemeBrowserFragment);
-        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
 
