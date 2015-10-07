b1d60268c6608cb286644f33684272a18fb5a626
Oguz Kocer <oguz.kocer@automattic.com>
2015-10-07 19:07:38 +0300

    People management activity introduced with dummy data

---
+++ WordPress/src/main/java/org/wordpress/android/ui/people/PeopleAdapter.java
@@ -0,0 +1,90 @@
+package org.wordpress.android.ui.people;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.wordpress.android.R;
+import org.wordpress.android.models.Person;
+import org.wordpress.android.models.Role;
+import org.wordpress.android.util.GravatarUtils;
+import org.wordpress.android.widgets.WPNetworkImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeopleAdapter extends BaseAdapter {
+    private final LayoutInflater mInflater;
+    private List<Person> mPersonList;
+    private int mAvatarSz;
+
+    public PeopleAdapter(Context context) {
+        mAvatarSz = context.getResources().getDimensionPixelSize(R.dimen.avatar_sz_medium);
+        mInflater = LayoutInflater.from(context);
+        mPersonList = new ArrayList<>();
+        mPersonList.add(new Person(1, "beaulebens", "Beau", "Lebens", "Beau", "http://lorempixum.com/76/76", Role.ADMIN));
+        mPersonList.add(new Person(2, "ebinnion", "Eric", "Binnion", "Eric", "http://lorempixum.com/76/76", Role.AUTHOR));
+        mPersonList.add(new Person(3, "javialvarez", "Javi", "Alvarez", "Javi", "http://lorempixum.com/76/76", Role.CONTRIBUTOR));
+        mPersonList.add(new Person(4, "oguzkocer", "Oguz", "Kocer", "Oguz", "http://lorempixum.com/76/76", Role.EDITOR));
+    }
+
+    @Override
+    public int getCount() {
+        return mPersonList.size();
+    }
+
+    @Override
+    public Person getItem(int position) {
+        if (mPersonList == null) {
+            return null;
+        }
+        return mPersonList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).personID;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final PeopleViewHolder holder;
+
+        if (convertView == null || convertView.getTag() == null) {
+            convertView = mInflater.inflate(R.layout.people_list_row, parent, false);
+            holder = new PeopleViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (PeopleViewHolder) convertView.getTag();
+        }
+
+        Person person = getItem(position);
+
+        if (person != null) {
+            String avatarUrl = GravatarUtils.fixGravatarUrl(person.getImageUrl(), mAvatarSz);
+            holder.imgAvatar.setImageUrl(avatarUrl, WPNetworkImageView.ImageType.AVATAR);
+            holder.txtDisplayName.setText(person.getDisplayName());
+        }
+
+        // hide the divider for the last item
+        boolean isLastItem = (position == getCount() - 1);
+        holder.divider.setVisibility(isLastItem ?  View.INVISIBLE : View.VISIBLE);
+
+        return convertView;
+    }
+
+    private class PeopleViewHolder {
+        private final WPNetworkImageView imgAvatar;
+        private final TextView txtDisplayName;
+        private final View divider;
+
+        PeopleViewHolder(View row) {
+            imgAvatar = (WPNetworkImageView) row.findViewById(R.id.people_list_row_avatar);
+            txtDisplayName = (TextView) row.findViewById(R.id.people_list_row_display_name);
+            divider = row.findViewById(R.id.divider);
+        }
+    }
+}
---
+++ WordPress/src/main/java/org/wordpress/android/ui/people/PeopleManagementActivity.java
@@ -0,0 +1,45 @@
+package org.wordpress.android.ui.people;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import org.wordpress.android.R;
+import org.wordpress.android.ui.ActivityLauncher;
+
+public class PeopleManagementActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        setContentView(R.layout.people_management);
+
+        setTitle(R.string.people);
+
+        ListView listView = (ListView)findViewById(android.R.id.list);
+        listView.setAdapter(new PeopleAdapter(this));
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        ActivityLauncher.slideOutToRight(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
