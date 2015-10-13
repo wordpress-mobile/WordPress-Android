f99ca927a2088a44599c35f20a07edf2f3e8869f
Oguz Kocer <oguz.kocer@automattic.com>
2015-10-13 11:22:02 -0600

    PersonActivity introduced

---
+++ WordPress/src/main/java/org/wordpress/android/ui/people/PersonActivity.java
@@ -0,0 +1,39 @@
+package org.wordpress.android.ui.people;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import org.wordpress.android.R;
+import org.wordpress.android.ui.ActivityLauncher;
+
+public class PersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        setContentView(R.layout.activity_person);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        ActivityLauncher.slideOutToRight(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
