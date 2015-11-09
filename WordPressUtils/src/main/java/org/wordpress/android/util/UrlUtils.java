b7cf0424212c84436f6ae0921a54feddb60d2e2f
Dan Roundhill <dan@automattic.com>
2015-11-09 08:09:27 -0800

    Revert "Fixes issue where private blog images wouldn't load."
    This reverts commit c37fd9027cfe0eefd49320eadebcad14696c4856.

--- WordPressUtils/src/main/java/org/wordpress/android/util/UrlUtils.java
+++ WordPressUtils/src/main/java/org/wordpress/android/util/UrlUtils.java
@@ -231,14 +231,6 @@ public class UrlUtils {
                 cleanedUrl.endsWith("gif") || cleanedUrl.endsWith("png");
     }
 
-    /**
-     * Returns true if the url is on wordpress.com
-     */
-    public static boolean isWPComUrl(String url) {
-        return !TextUtils.isEmpty(url) && url.contains("wordpress.com");
-    }
-
-
     public static String appendUrlParameter(String url, String paramName, String paramValue) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put(paramName, paramValue);
