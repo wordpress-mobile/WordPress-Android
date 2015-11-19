f004610b03477357a7b00e15d2de9d4ef0e7d309
Oguz Kocer <oguz.kocer@automattic.com>
2015-11-19 21:31:59 +0200

    Basic input dialog added to my profile for first name

--- WordPress/src/main/java/org/wordpress/android/ui/me/MyProfileActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/me/MyProfileActivity.java
@@ -1,12 +1,17 @@
 package org.wordpress.android.ui.me;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import org.wordpress.android.R;
 import org.wordpress.android.ui.ActivityLauncher;
+import org.wordpress.android.widgets.OpenSansEditText;
 
 public class MyProfileActivity extends AppCompatActivity {
     @Override
@@ -21,6 +26,13 @@ public class MyProfileActivity extends AppCompatActivity {
         setContentView(R.layout.my_profile_activity);
 
         setTitle(R.string.my_profile);
+
+        findViewById(R.id.first_name).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInputDialog();
+            }
+        });
     }
 
     @Override
@@ -36,5 +48,28 @@ public class MyProfileActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void showInputDialog() {
+        LayoutInflater layoutInflater = LayoutInflater.from(MyProfileActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.my_profile_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MyProfileActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final OpenSansEditText editText = (OpenSansEditText) promptView.findViewById(R.id.edittext);
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                })
+                .setNegativeButton(R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 }
