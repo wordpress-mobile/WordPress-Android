dafb88d5e06159cc09706f3700f0e6acdfaf2e55
unknown <filbry_92@hotmail.com>
2015-11-23 20:23:29 +0100

    Moved the check for video to within ImageUtils

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -1203,8 +1203,7 @@ public class EditPostActivity extends AppCompatActivity implements EditorFragmen
         mediaFile.setTitle(mediaTitle);
         mediaFile.setFilePath(imageUri.toString());
         if (imageUri.getEncodedPath() != null) {
-            mediaFile.setVideo(MediaUtils.isVideo(imageUri.toString())
-                    || mediaTitle.equals(getResources().getString(R.string.video)));
+            mediaFile.setVideo(MediaUtils.isVideo(imageUri.toString()));
         }
         WordPress.wpDB.saveMediaFile(mediaFile);
         mEditorFragment.appendMediaFile(mediaFile, mediaFile.getFilePath(), WordPress.imageLoader);
