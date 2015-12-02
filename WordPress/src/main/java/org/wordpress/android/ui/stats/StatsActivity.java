d519b0620e077f5ae70e82a63c8d3d6f548eeb76
Danilo Ercoli <ercoli@gmail.com>
2015-12-02 10:51:26 +0100

    Remove unused imports.

--- WordPress/src/main/java/org/wordpress/android/ui/stats/StatsActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/StatsActivity.java
@@ -12,8 +12,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
