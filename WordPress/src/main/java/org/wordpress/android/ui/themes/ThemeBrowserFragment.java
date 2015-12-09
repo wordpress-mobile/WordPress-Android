a216cf873a0321f0bf45a4ca41b68b0164cc2179
Will Kwon <will@willkwon.com>
2015-12-09 15:52:57 -0600

    Cleaning imports

--- WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeBrowserFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeBrowserFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.widget.ContentLoadingProgressBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
