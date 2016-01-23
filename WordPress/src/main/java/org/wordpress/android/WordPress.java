c5f752abf473430358f2cb24da1ad40dd962d4be
Will Kwon <will@kwon.co>
2016-01-23 11:12:49 -0800

    Changing WordPress.getCurrentBlog() to also attempt looking at hidden blogs.

--- WordPress/src/main/java/org/wordpress/android/WordPress.java
+++ WordPress/src/main/java/org/wordpress/android/WordPress.java
@@ -386,8 +386,9 @@ public class WordPress extends Application {
      * Get the currently active blog.
      * <p/>
      * If the current blog is not already set, try and determine the last active blog from the last
-     * time the application was used. If we're not able to determine the last active blog, just
-     * select the first one.
+     * time the application was used. If we're not able to determine the last active blog, try to
+     * select the first visible blog. If there are no more visible blogs, try to select the first
+     * hidden blog. If there are no blogs at all, return null.
      */
     public static Blog getCurrentBlog() {
         if (currentBlog == null || !wpDB.isDotComBlogVisible(currentBlog.getRemoteBlogId())) {
@@ -644,7 +645,18 @@ public class WordPress extends Application {
                 int id = Integer.valueOf(accounts.get(0).get("id").toString());
                 setCurrentBlog(id);
                 wpDB.updateLastBlogId(id);
+            } else {
+                attemptToRestoreHiddenBlog();
             }
+        }
+    }
+
+    private static void attemptToRestoreHiddenBlog() {
+        List<Map<String, Object>> accounts = WordPress.wpDB.getAllBlogs();
+        if (accounts.size() > 0) {
+            Blog firstHiddenBlog = (Blog) accounts.get(0);
+            firstHiddenBlog.setHidden(false);
+            setCurrentBlog(firstHiddenBlog.getLocalTableBlogId());
         }
     }
 
