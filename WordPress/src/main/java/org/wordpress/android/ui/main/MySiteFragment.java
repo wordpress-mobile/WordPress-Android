2d11cedfc056fb99243a79ba4a0e795cc23a00a1
Nick Bradbury <nick.bradbury@gmail.com>
2016-01-28 05:46:29 -0500

    Don't use mBlog in my site

--- WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
@@ -203,7 +203,7 @@ public class MySiteFragment extends Fragment
         mSharingView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ActivityLauncher.viewBlogSharing(getActivity(), mBlog);
+                ActivityLauncher.viewBlogSharing(getActivity(), WordPress.getBlog(mBlogLocalId));
             }
         });
 
@@ -322,7 +322,7 @@ public class MySiteFragment extends Fragment
         mThemesContainer.setVisibility(themesVisibility);
 
         // sharing is only exposed for wp.com blogs
-        int sharingVisibility = mBlog.isDotcomFlag() ? View.VISIBLE : View.GONE;
+        int sharingVisibility = blog.isDotcomFlag() ? View.VISIBLE : View.GONE;
         mSharingView.setVisibility(sharingVisibility);
 
         // show settings for all self-hosted to expose Delete Site
