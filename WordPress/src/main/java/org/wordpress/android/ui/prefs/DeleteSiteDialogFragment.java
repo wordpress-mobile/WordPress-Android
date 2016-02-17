ef491a2e7dfba099864ec1163c2a0d8278bc3847
Will Kwon <will@kwon.co>
2016-02-17 14:12:24 -0800

    Formatting prompt and also making text similar to other platforms

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/DeleteSiteDialogFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/DeleteSiteDialogFragment.java
@@ -6,16 +6,19 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.TextWatcher;
+import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import org.wordpress.android.R;
-import org.wordpress.android.WordPress;
-import org.wordpress.android.util.UrlUtils;
 
 public class DeleteSiteDialogFragment extends DialogFragment implements TextWatcher, DialogInterface.OnShowListener {
     public static final String SITE_DOMAIN_KEY = "site-domain";
@@ -23,10 +26,12 @@ public class DeleteSiteDialogFragment extends DialogFragment implements TextWatc
     private AlertDialog mDeleteSiteDialog;
     private EditText mUrlConfirmation;
     private Button mDeleteButton;
+    private String mSiteDomain;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        retrieveSiteDomain();
         configureAlertViewBuilder(builder);
 
         mDeleteSiteDialog = builder.create();
@@ -59,10 +64,8 @@ public class DeleteSiteDialogFragment extends DialogFragment implements TextWatc
     }
 
     private void configureAlertViewBuilder(AlertDialog.Builder builder) {
-        builder.setTitle(R.string.delete_site_question);
-        String url = WordPress.getCurrentBlog().getHomeURL();
-        String deletePrompt = String.format(getString(R.string.confirm_delete_site_prompt), UrlUtils.getHost(url));
-        builder.setMessage(deletePrompt);
+        builder.setTitle(R.string.confirm_delete_site);
+        builder.setMessage(confirmationPromptString());
 
         configureUrlConfirmation(builder);
         configureButtons(builder);
@@ -88,21 +91,30 @@ public class DeleteSiteDialogFragment extends DialogFragment implements TextWatc
         });
     }
 
+    private Spannable confirmationPromptString() {
+        String deletePrompt = String.format(getString(R.string.confirm_delete_site_prompt), mSiteDomain);
+        Spannable promptSpannable = new SpannableString(deletePrompt);
+        int beginning = deletePrompt.indexOf(mSiteDomain);
+        int end = beginning + mSiteDomain.length();
+        promptSpannable.setSpan(new StyleSpan(Typeface.BOLD), beginning, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return promptSpannable;
+    }
+
     private void configureUrlConfirmation(AlertDialog.Builder builder) {
         View view = getActivity().getLayoutInflater().inflate(R.layout.delete_site_dialog, null);
         mUrlConfirmation = (EditText) view.findViewById(R.id.url_confirmation);
-        setSiteDomainHint();
+        mUrlConfirmation.setHint(mSiteDomain);
         mUrlConfirmation.addTextChangedListener(this);
         builder.setView(view);
     }
 
-    private void setSiteDomainHint() {
+    private void retrieveSiteDomain() {
         Bundle args = getArguments();
-        String siteDomain = getString(R.string.delete).toLowerCase();
+        mSiteDomain = getString(R.string.delete).toLowerCase();
         if (args != null) {
-            siteDomain = args.getString(SITE_DOMAIN_KEY);
+            mSiteDomain = args.getString(SITE_DOMAIN_KEY);
         }
-        mUrlConfirmation.setHint(siteDomain);
     }
 
     private boolean isUrlConfirmationTextValid() {
