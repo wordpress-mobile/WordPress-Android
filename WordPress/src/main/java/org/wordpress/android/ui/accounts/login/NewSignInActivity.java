372fb46624d825bde7d86ffbe5ef0626e6527191
Will Kwon <will@kwon.co>
2016-02-24 11:32:40 -0800

    Adding new activity for sign in

---
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivity.java
@@ -0,0 +1,19 @@
+package org.wordpress.android.ui.accounts.login;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.app.Activity;
+
+import org.wordpress.android.R;
+
+public class NewSignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_sign_in_activity);
+
+
+    }
+}
---
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivityFragment.java
@@ -0,0 +1,24 @@
+package org.wordpress.android.ui.accounts.login;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.wordpress.android.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NewSignInActivityFragment extends Fragment {
+
+    public NewSignInActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_sign_in2, container, false);
+    }
+}
