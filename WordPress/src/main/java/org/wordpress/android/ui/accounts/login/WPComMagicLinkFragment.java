889ac0076cf80cedacb9979f183a895e32e24ec3
Will Kwon <will@kwon.co>
2016-02-24 15:00:03 -0800

    Adding magic link request button

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/WPComMagicLinkFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/WPComMagicLinkFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import org.wordpress.android.R;
 
@@ -23,6 +24,8 @@ public class WPComMagicLinkFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private Button mMagicLinkButton;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,7 +64,16 @@ public class WPComMagicLinkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wpcom_magic_link, container, false);
+        View view = inflater.inflate(R.layout.fragment_wpcom_magic_link, container, false);
+        mMagicLinkButton = (Button) view.findViewById(R.id.magic_button);
+        mMagicLinkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonPressed(true);
+            }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
