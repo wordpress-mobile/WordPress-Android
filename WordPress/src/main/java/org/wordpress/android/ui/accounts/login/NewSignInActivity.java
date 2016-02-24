8fa3533c574066a2c7184c95339d8481f31e7cd9
Will Kwon <will@kwon.co>
2016-02-24 11:33:34 -0800

    Adding onClicklistener and handling different fragment transactions

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivity.java
@@ -1,19 +1,45 @@
 package org.wordpress.android.ui.accounts.login;
 
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 import org.wordpress.android.R;
 
-public class NewSignInActivity extends Activity {
+public class NewSignInActivity extends AppCompatActivity {
+    public String email = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_sign_in_activity);
 
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        NewSignInActivityFragment fragment = new NewSignInActivityFragment();
+        fragmentTransaction.add(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void logIn() {
+        // Logic for checking if WordPress.com or self-hosted
+        boolean isWPCom = true;
+        if (isWPCom) {
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            WPComMagicLinkFragment fragment = WPComMagicLinkFragment.newInstance(email);
+            fragmentTransaction.add(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        } else {
+            // self-hosted flow
+        }
+    }
+
+    public void requestMagicLink() {
 
     }
 }
--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivityFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.wordpress.android.R;
 
@@ -19,6 +21,20 @@ public class NewSignInActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_new_sign_in2, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_new_sign_in2, container, false);
+        final EditText email = (EditText) view.findViewById(R.id.email_address);
+        Button button = (Button) view.findViewById(R.id.magic_link_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NewSignInActivity newSignInActivity = (NewSignInActivity) getActivity();
+                newSignInActivity.email = email.getText().toString();
+                newSignInActivity.logIn();
+            }
+        });
+
+        return view;
     }
 }
