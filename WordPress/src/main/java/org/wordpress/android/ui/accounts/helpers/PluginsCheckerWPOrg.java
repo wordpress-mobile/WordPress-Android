60e4d235c0602d8da093ab83b44555cbb0162714
Danilo Ercoli <ercoli@gmail.com>
2016-03-03 13:17:55 +0100

    Remove any external dependencies from the PluginsChecker. Just use Android platform classes and methods.

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/helpers/PluginsCheckerWPOrg.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/helpers/PluginsCheckerWPOrg.java
@@ -3,17 +3,13 @@ package org.wordpress.android.ui.accounts.helpers;
 import android.text.TextUtils;
 import android.webkit.URLUtil;
 
-import com.android.volley.TimeoutError;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.wordpress.android.WordPress;
 import org.wordpress.android.util.AppLog;
-import org.wordpress.android.util.WPRestClient;
-import org.xmlrpc.android.ApiHelper;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -23,6 +19,12 @@ public class PluginsCheckerWPOrg {
     //TODO: update the address with a valid .org site
     private final static String BB_PLUGINS_LIST_URL = "http://xmlrpc.eritreo.it/test.json";
 
+    // Do not use the WP-APP user agent. Requests could be blocked if made from our app UA.
+    private final static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36";
+
+    /** Socket timeout in milliseconds for the requests */
+    private static final int REQUEST_TIMEOUT_MS = 30000;
+
     String mOriginalURL;
 
     public PluginsCheckerWPOrg(String url) {
@@ -30,16 +32,12 @@ public class PluginsCheckerWPOrg {
     }
 
     public List<Plugin> checkForPlugins() {
-        String responseHTML = null;
-        try {
-            responseHTML = ApiHelper.getResponse(BB_PLUGINS_LIST_URL);
-        } catch (IOException | TimeoutError e) {
-            AppLog.e(AppLog.T.NUX, "Can't download the plugins list from the server!", e);
-        }
+        String responseHTML = downloadPluginsList();
         if (TextUtils.isEmpty(responseHTML)) {
             AppLog.w(AppLog.T.NUX, "Without the list we can't check if the host has some BB plugins installed on it.");
             return null;
         }
+
         JSONArray listOfPlugins;
         try {
             listOfPlugins = new JSONArray(responseHTML);
@@ -55,25 +53,33 @@ public class PluginsCheckerWPOrg {
             baseURL = baseURL + "/wp-content/plugins/";
         }
 
+        AppLog.i(AppLog.T.NUX, "The calculated plugins URL is the following: " + baseURL);
+
         if (!URLUtil.isValidUrl(baseURL)) {
+            AppLog.w(AppLog.T.NUX, "The calculated plugins URL isn't a valid URL. Returning now.");
             return null;
         }
 
         int respCode = openConnection(baseURL);
         if (respCode != HttpURLConnection.HTTP_OK && respCode != 401 && respCode != 403) {
+            AppLog.w(AppLog.T.NUX, "The request to plugins URL returned with an unexpected HTTP error code. Returning now.");
             return null;
         }
 
+        AppLog.i(AppLog.T.NUX, "Start checking the plugins list..");
         ArrayList<Plugin> listOfBBPlugins = new ArrayList<>();
         for (int i=0; i<listOfPlugins.length(); i++) {
             try {
                 JSONObject currentObject = listOfPlugins.getJSONObject(i);
-                respCode = openConnection(baseURL + currentObject.getString("name") + "/");
+                String currentPluginURL = baseURL + currentObject.getString("name") + "/";
+                AppLog.i(AppLog.T.NUX, "Testing the following plugin " + currentPluginURL);
+                respCode = openConnection(currentPluginURL);
                 if (respCode != HttpURLConnection.HTTP_NOT_FOUND) {
                     Plugin currentBBPLugin = new Plugin();
                     currentBBPLugin.name = currentObject.getString("name");
                     currentBBPLugin.url = currentObject.getString("url");
                     listOfBBPlugins.add(currentBBPLugin);
+                    AppLog.i(AppLog.T.NUX, "Plugin found on the server: " + currentObject.getString("name"));
                 }
             } catch (JSONException e) {
                 AppLog.e(AppLog.T.NUX, "Error while parsing the " + i + " in the list of plugins returned from the server.", e);
@@ -83,14 +89,44 @@ public class PluginsCheckerWPOrg {
         return listOfBBPlugins;
     }
 
+    private String downloadPluginsList() {
+        HttpURLConnection conn = null;
+        String response = "";
+        try {
+            URL url = new URL(BB_PLUGINS_LIST_URL);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(REQUEST_TIMEOUT_MS);
+            conn.setConnectTimeout(REQUEST_TIMEOUT_MS);
+            conn.setRequestProperty("User-Agent", USER_AGENT);
+            conn.setUseCaches(false);
+            conn.setRequestProperty("Connection", "close");
+            conn.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            String inLine;
+            while ((inLine = in.readLine()) != null) {
+                response += inLine;
+            }
+        } catch (Exception e) {
+            AppLog.e(AppLog.T.NUX, "Error while downloading the plugins list from the server...", e);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            } catch (Exception e) {
+            }
+        }
+        return response;
+    }
+
     private int openConnection(String url) {
         HttpURLConnection conn = null;
         try {
             URL requestURL = new URL(url);
             conn = (HttpURLConnection) requestURL.openConnection();
-            conn.setReadTimeout(WPRestClient.REST_TIMEOUT_MS);
-            conn.setConnectTimeout(WPRestClient.REST_TIMEOUT_MS);
-            conn.setRequestProperty("User-Agent", WordPress.getUserAgent());
+            conn.setReadTimeout(REQUEST_TIMEOUT_MS);
+            conn.setConnectTimeout(REQUEST_TIMEOUT_MS);
+            conn.setRequestProperty("User-Agent", USER_AGENT);
             conn.setUseCaches(false);
             conn.setRequestProperty("Connection", "close");
             conn.setRequestMethod("GET");
