b063e624e912c74f9acf4675d9e8d73e538f0777
Will Kwon <will@kwon.co>
2016-03-08 10:41:38 -0800

    Cleaning up listener

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivityFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivityFragment.java
@@ -5,37 +5,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 
 import org.wordpress.android.R;
 import org.wordpress.android.widgets.WPTextView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class NewSignInActivityFragment extends Fragment {
+    private String mEmail = "";
+    private OnEmailCheckListener mListener;
+
+    public interface OnEmailCheckListener {
+        void onEmailChecked(Boolean isWPCom);
+    }
 
     public NewSignInActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_sign_in2, container, false);
         final EditText email = (EditText) view.findViewById(R.id.email_address);
         WPTextView button = (WPTextView) view.findViewById(R.id.magic_link_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NewSignInActivity newSignInActivity = (NewSignInActivity) getActivity();
-                newSignInActivity.email = email.getText().toString();
-                newSignInActivity.logIn();
+                mEmail = email.getText().toString();
+                checkEmail();
             }
         });
 
         return view;
+    }
+
+    private void checkEmail() {
+        // request email check to server
+
+        boolean isWPCom = true;
+        mListener.onEmailChecked(isWPCom);
     }
 }
