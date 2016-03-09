65daeb6c5adde2caea0b6161f8ad66f0b0909a4f
Will Kwon <will@kwon.co>
2016-03-09 14:51:53 -0800

    Showing magic link or password based on toggling of entering password

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/WPComMagicLinkFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/WPComMagicLinkFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import org.wordpress.android.R;
 import org.wordpress.android.widgets.WPTextView;
@@ -20,6 +22,7 @@ public class WPComMagicLinkFragment extends Fragment {
     private static final String ARG_EMAIL_ADDRESS = "arg_email_address";
 
     private WPTextView mMagicLinkButton;
+    private RelativeLayout mPasswordSection;
     private String mEmail;
     private OnMagicLinkFragmentInteraction mListener;
 
@@ -46,10 +49,19 @@ public class WPComMagicLinkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_wpcom_magic_link, container, false);
         mMagicLinkButton = (WPTextView) view.findViewById(R.id.magic_button);
+        mPasswordSection = (RelativeLayout) view.findViewById(R.id.nux_password_layout);
         mMagicLinkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendMagicLinkRequest();
+            }
+        });
+
+        TextView forgotPassword = (TextView) view.findViewById(R.id.forgot_password);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                togglePasswordField();
             }
         });
 
@@ -70,6 +82,14 @@ public class WPComMagicLinkFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    private void togglePasswordField() {
+        if (mPasswordSection.getVisibility() == View.GONE) {
+            mPasswordSection.setVisibility(View.VISIBLE);
+        } else {
+            mPasswordSection.setVisibility(View.GONE);
+        }
     }
 
     private void sendMagicLinkRequest() {
