e0ac745cc553e2508f7110ae2c06db119d73534a
Danilo Ercoli <ercoli@gmail.com>
2016-03-10 14:56:51 +0100

    Fix an issue where the "erroneous SSL cert" flag was not set to true after a SSLHanshakeException on self hosted sites. Props @hypest

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/helpers/FetchBlogListWPOrg.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/helpers/FetchBlogListWPOrg.java
@@ -465,7 +465,7 @@ public class FetchBlogListWPOrg extends FetchBlogListAbstract {
                 AppLog.e(T.NUX, "XMLRPCException received from XMLRPC call wp.getUsersBlogs", xmlRpcException);
                 mErrorMsgId = org.wordpress.android.R.string.no_site_error;
             } catch (SSLHandshakeException e) {
-                if (WPUrlUtils.isWordPressCom(xmlrpcUri)) {
+                if (!WPUrlUtils.isWordPressCom(xmlrpcUri)) {
                     mErroneousSslCertificate = true;
                 }
                 AppLog.w(T.NUX, "SSLHandshakeException failed. Erroneous SSL certificate detected.");
