858e7569ae4e1e83c9659e668cac53d83923a434
Alex Forcier <alex.forcier@automattic.com>
2016-03-10 08:43:31 -0500

    Added unit test support with example unit test for SiteStore
    - Also added helper class for initializing WellSql with single stores

---
+++ fluxc/src/test/java/org/wordpress/android/stores/SiteStoreUnitTest.java
@@ -0,0 +1,45 @@
+package org.wordpress.android.stores;
+
+import android.content.Context;
+
+import com.yarolegovich.wellsql.WellSql;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.wordpress.android.stores.model.SiteModel;
+import org.wordpress.android.stores.network.rest.wpcom.site.SiteRestClient;
+import org.wordpress.android.stores.network.xmlrpc.site.SiteXMLRPCClient;
+import org.wordpress.android.stores.persistence.WellSqlConfig;
+import org.wordpress.android.stores.store.SiteStore;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class SiteStoreUnitTest {
+    private SiteStore mSiteStore = new SiteStore(new Dispatcher(), Mockito.mock(SiteRestClient.class),
+            Mockito.mock(SiteXMLRPCClient.class));
+
+    @Before
+    public void setUp() {
+        Context appContext = RuntimeEnvironment.application.getApplicationContext();
+
+        WellSqlConfig config = new SingleStoreWellSqlConfigForTests(appContext, SiteModel.class);
+        WellSql.init(config);
+        config.reset();
+    }
+
+    @Test
+    public void testSimpleInsertionAndRetrieval() {
+        SiteModel siteModel = new SiteModel();
+        siteModel.setSiteId(42);
+        WellSql.insert(siteModel).execute();
+
+        assertEquals(1, mSiteStore.getSitesCount());
+
+        assertEquals(42, mSiteStore.getSites().get(0).getSiteId());
+    }
+}
