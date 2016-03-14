1214840a13ae05b0a03e3a148da162b176248677
Maxime Biais <maxime.biais@gmail.com>
2016-03-14 17:16:34 +0100

    remove workaround on UpdateAllExceptId since we updated to WellSQL 1.0.6

--- WordPressStores/src/main/java/org/wordpress/android/stores/persistence/UpdateAllExceptId.java
+++ WordPressStores/src/main/java/org/wordpress/android/stores/persistence/UpdateAllExceptId.java
@@ -6,32 +6,12 @@ import com.yarolegovich.wellsql.WellSql;
 import com.yarolegovich.wellsql.mapper.InsertMapper;
 import com.yarolegovich.wellsql.mapper.SQLiteMapper;
 
-import org.wordpress.android.util.AppLog;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public class UpdateAllExceptId<T> implements InsertMapper<T> {
     @Override
     public ContentValues toCv(T item) {
-        try {
-            // Reflection equivalent for: SQLiteMapper<T> mapper = WellSql.mapperFor((Class<T>) item.getClass());
-            // This will be useless once the new wellsql version (1.0.6) is deployed
-            Method mapperFor = WellSql.class.getDeclaredMethod("mapperFor", Class.class);
-            mapperFor.setAccessible(true); // if security settings allow this
-            SQLiteMapper<T> mapper = (SQLiteMapper<T>) mapperFor.invoke(null, (Class<T>) item.getClass());
-            ContentValues cv = mapper.toCv(item);
-            cv.remove("_id");
-            return cv;
-        } catch (IllegalAccessException e) {
-            AppLog.e(AppLog.T.API, "can't invoke WellSql.mapperFor", e);
-            return null;
-        } catch (InvocationTargetException e) {
-            AppLog.e(AppLog.T.API, "can't invoke WellSql.mapperFor", e);
-            return null;
-        } catch (NoSuchMethodException e) {
-            AppLog.e(AppLog.T.API, "can't invoke WellSql.mapperFor", e);
-            return null;
-        }
+        SQLiteMapper<T> mapper = WellSql.mapperFor((Class<T>) item.getClass());
+        ContentValues cv = mapper.toCv(item);
+        cv.remove("_id");
+        return cv;
     }
 }
