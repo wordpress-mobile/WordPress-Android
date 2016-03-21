9d5cc3b695e3e398946e2d2fe00e09077e200cb2
Maxime Biais <maxime.biais@gmail.com>
2016-03-21 10:01:25 +0100

    check for a clean working directory

--- tools/build-all-apks.sh
+++ tools/build-all-apks.sh
@@ -44,6 +44,15 @@ function build_apk {
   return $version_code
 }
 
+function check_clean_working_dir {
+  if [ "`git status --porcelain`"x \!= x ]; then
+    git status
+    echo "Your working directory must be clean before you run this script"
+    exit 1
+  fi
+}
+
+check_clean_working_dir
 date > $LOGFILE
 build_apk $release_branch Vanilla
 release_code=$?
@@ -53,10 +62,10 @@ build_apk $alpha_branch Zalpha
 alpha_code=$?
 git checkout $current_branch
 
-echo "Version codes - release: $release_code, beta: $beta_code, alpha: $alpha_code"
-echo "Full log in: $LOGFILE"
+echo "Version codes - release: $release_code, beta: $beta_code, alpha: $alpha_code" | tee -a $LOGFILE
 if [ $release_code -ge $beta_code -o $beta_code -ge $alpha_code ]; then
   echo "(ಥ﹏ಥ) (ಥ﹏ಥ) (ಥ﹏ಥ) (ಥ﹏ಥ) (ಥ﹏ಥ) (ಥ﹏ಥ) (ಥ﹏ಥ) (ಥ﹏ಥ) (ಥ﹏ಥ)"
   echo "Wrong version codes (╯°□°）╯︵ ┻━┻"
+  echo "Full log in: $LOGFILE"
   exit 2
 fi
