6764a94cfb74894a1afb768e9f3574b7a09ddf86
Will Kwon <will@kwon.co>
2016-03-23 17:34:53 -0700

    Making Fragment non-support version

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/WPComMagicLinkFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/WPComMagicLinkFragment.java
@@ -1,9 +1,9 @@
 package org.wordpress.android.ui.accounts.login;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
