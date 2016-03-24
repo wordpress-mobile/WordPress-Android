474faea2581d6e45293af9d4a0286401d20fe752
Will Kwon <will@kwon.co>
2016-03-23 18:07:21 -0700

    Inherting functionality from SignInActivity

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/NewSignInActivity.java
@@ -2,40 +2,22 @@ package org.wordpress.android.ui.accounts.login;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import org.wordpress.android.R;
 import org.wordpress.android.ui.accounts.SignInActivity;
 import org.wordpress.android.ui.accounts.SignInFragment;
 
-public class NewSignInActivity extends AppCompatActivity implements WPComMagicLinkFragment.OnMagicLinkFragmentInteraction, NewSignInFragment.OnMagicLinkRequestListener {
-    public String mEmail = "";
+public class NewSignInActivity extends SignInActivity implements WPComMagicLinkFragment.OnMagicLinkFragmentInteraction, NewSignInFragment.OnMagicLinkRequestListener {
+    private String mEmail = "";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_sign_in_activity);
-
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        NewSignInFragment fragment = new NewSignInFragment();
-        fragmentTransaction.add(R.id.fragment_container, fragment);
-        fragmentTransaction.commit();
+    public SignInFragment getSignInFragment() {
+        return new NewSignInFragment();
     }
 
     @Override
     public void onMagicLinkSent() {
-        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        MagicLinkSentFragment fragment = new MagicLinkSentFragment();
-        fragmentTransaction.replace(R.id.fragment_container, fragment);
-        fragmentTransaction.setTransition(android.support.v4.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
     }
 
     @Override
@@ -45,13 +27,11 @@ public class NewSignInActivity extends AppCompatActivity implements WPComMagicLi
 
     @Override
     public void onMagicLinkRequestSuccess() {
-        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        WPComMagicLinkFragment fragment = WPComMagicLinkFragment.newInstance(mEmail);
-        fragmentTransaction.replace(R.id.fragment_container, fragment);
-        fragmentTransaction.setTransition(android.support.v4.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        fragmentTransaction.addToBackStack(null);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        WPComMagicLinkFragment wpComMagicLinkFragment = WPComMagicLinkFragment.newInstance(mEmail);
+        fragmentTransaction.replace(R.id.fragment_container, wpComMagicLinkFragment);
+        fragmentTransaction.addToBackStack("sign_in");
         fragmentTransaction.commit();
     }
 }
