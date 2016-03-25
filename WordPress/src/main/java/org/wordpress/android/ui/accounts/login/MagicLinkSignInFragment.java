32e089870b6f2b710f34d6dc28a6837f1d024e5c
Will Kwon <will@kwon.co>
2016-03-24 20:59:32 -0700

    Reimplementing logic of what to show while removing a member variable

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/MagicLinkSignInFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/MagicLinkSignInFragment.java
@@ -4,15 +4,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import org.wordpress.android.R;
 import org.wordpress.android.ui.accounts.SignInFragment;
 import org.wordpress.android.util.EditTextUtils;
-import org.wordpress.android.util.WPActivityUtils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,11 +25,9 @@ public class MagicLinkSignInFragment extends SignInFragment {
     }
 
     private OnMagicLinkRequestListener mListener;
-    private boolean mIsMagicLink;
 
     public MagicLinkSignInFragment() {
         super();
-        mIsMagicLink = true;
     }
 
     @Override
@@ -43,62 +43,83 @@ public class MagicLinkSignInFragment extends SignInFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
-        if (mIsMagicLink) {
-            hideForMagicLink();
-        } else {
-            hideForNonMagicLink();
-        }
+
+        configureMagicLinkUI();
+        mUsernameEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (didPressNextKey(actionId, event) && !isEnterPasswordMode()) {
+                    signIn();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
 
         return view;
     }
 
-    public void setIsMagicLink(boolean isMagicLink) {
-        mIsMagicLink = isMagicLink;
+    private boolean didPressNextKey(int actionId, KeyEvent event) {
+        return actionId == EditorInfo.IME_ACTION_NEXT || event != null && (event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_NAVIGATE_NEXT);
     }
 
-    private void hideForMagicLink() {
-        mUrlButtonLayout.setVisibility(View.GONE);
+    private void configureMagicLinkUI() {
+        showDotComSignInForm();
         mPasswordLayout.setVisibility(View.GONE);
         mForgotPassword.setVisibility(View.GONE);
         mSignInButton.setText(getString(R.string.button_next));
     }
 
-    private void hideForNonMagicLink() {
-        mUrlButtonLayout.setVisibility(View.GONE);
-        mCreateAccountButton.setVisibility(View.GONE);
-        mAddSelfHostedButton.setVisibility(View.GONE);
-        mPasswordEditText.requestFocus();
-        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+    protected void showSelfHostedSignInForm(){
+        super.showSelfHostedSignInForm();
+        showPasswordField();
     }
 
     @Override
-    protected void toggleSignInMode() {
-        super.toggleSignInMode();
-
-        mIsMagicLink = !mSelfHosted;
-        if (mSelfHosted) {
-            mPasswordLayout.setVisibility(View.VISIBLE);
-            mForgotPassword.setVisibility(View.VISIBLE);
+    protected void toggleSignInMode(){
+        if (mUrlButtonLayout.getVisibility() == View.VISIBLE) {
+            configureMagicLinkUI();
+            mSelfHosted = false;
         } else {
-            mPasswordLayout.setVisibility(View.GONE);
-            mForgotPassword.setVisibility(View.GONE);
+            showSelfHostedSignInForm();
+            mSelfHosted = true;
         }
+    }
+
+    private boolean isEnterPasswordMode() {
+        return mPasswordLayout.getVisibility() == View.VISIBLE;
     }
 
     @Override
     protected void signIn() {
-        if (mIsMagicLink) {
-            boolean isValidWPComEmail = true;
-            if (isValidWPComEmail) { // OK RESPONSE
-                mListener.onMagicLinkRequestSuccess();
-            } else { // ERROR RESPONSE
-                mPasswordLayout.setVisibility(View.VISIBLE);
-                mForgotPassword.setVisibility(View.VISIBLE);
-            }
-        } else {
+        if (mSelfHosted || isEnterPasswordMode()) {
             super.signIn();
+        } else {
+            if (isUsernameEmail()) {
+                boolean isValidWPComEmail = false;
+                if (isValidWPComEmail) { // OK RESPONSE
+                    mListener.onMagicLinkRequestSuccess();
+                } else { // ERROR RESPONSE
+                    showPasswordFieldAndFocus();
+                }
+            } else {
+                showPasswordFieldAndFocus();
+            }
         }
+    }
+
+    private void showPasswordFieldAndFocus() {
+        showPasswordField();
+        mPasswordEditText.requestFocus();
+        mSignInButton.setText(getString(R.string.sign_in));
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(mPasswordEditText, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    private void showPasswordField() {
+        mPasswordLayout.setVisibility(View.VISIBLE);
+        mForgotPassword.setVisibility(View.VISIBLE);
     }
 
     private boolean isUsernameEmail() {
