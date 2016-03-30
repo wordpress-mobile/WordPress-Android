0eecf5f65d0326ae88a3ceec7bc3082179c0fcec
Oguz Kocer <oguz.kocer@automattic.com>
2016-03-30 12:55:30 +0300

    Introduced PeopleUtils with first iteration of fetchUsers implemented

---
+++ WordPress/src/main/java/org/wordpress/android/ui/people/utils/PeopleUtils.java
@@ -0,0 +1,40 @@
+package org.wordpress.android.ui.people.utils;
+
+import com.android.volley.VolleyError;
+import com.wordpress.rest.RestRequest;
+
+import org.json.JSONObject;
+import org.wordpress.android.WordPress;
+import org.wordpress.android.util.AppLog;
+import org.wordpress.android.util.AppLog.T;
+
+public class PeopleUtils {
+
+    public static void fetchUsers(long siteId, final PeopleUtils.Callback callback) {
+        com.wordpress.rest.RestRequest.Listener listener = new RestRequest.Listener() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                if (jsonObject != null) {
+                    callback.onSuccess(jsonObject);
+                }
+            }
+        };
+
+        RestRequest.ErrorListener errorListener = new RestRequest.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                AppLog.e(T.API, volleyError);
+                callback.onError(volleyError);
+            }
+        };
+
+        String path = String.format("sites/%s/users", siteId);
+        WordPress.getRestClientUtilsV1_1().get(path, listener, errorListener);
+    }
+
+    public interface Callback {
+        void onSuccess(JSONObject jsonObject);
+
+        void onError(VolleyError error);
+    }
+}
