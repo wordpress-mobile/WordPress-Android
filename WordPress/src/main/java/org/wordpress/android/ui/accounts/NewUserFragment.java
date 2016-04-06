6ed74b76aaeb583c2dd0a064f1dbd715ee91de93
Maxime Biais <max@bia.is>
2016-04-06 14:49:18 +0200

    Revert "Update to emailchecker2"

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/NewUserFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/NewUserFragment.java
@@ -32,7 +32,7 @@ import org.wordpress.android.util.AppLog.T;
 import org.wordpress.android.util.EditTextUtils;
 import org.wordpress.android.util.UserEmailUtils;
 import org.wordpress.android.widgets.WPTextView;
-import org.wordpress.emailchecker2.EmailChecker;
+import org.wordpress.emailchecker.EmailChecker;
 import org.wordpress.persistentedittext.PersistentEditTextHelper;
 
 import java.util.regex.Matcher;
@@ -46,8 +46,13 @@ public class NewUserFragment extends AbstractFragment implements TextWatcher {
     private WPTextView mSignupButton;
     private WPTextView mProgressTextSignIn;
     private RelativeLayout mProgressBarSignIn;
+    private EmailChecker mEmailChecker;
     private boolean mEmailAutoCorrected;
     private boolean mAutoCompleteUrl;
+
+    public NewUserFragment() {
+        mEmailChecker = new EmailChecker();
+    }
 
     @Override
     public void afterTextChanged(Editable s) {
@@ -318,7 +323,7 @@ public class NewUserFragment extends AbstractFragment implements TextWatcher {
             return;
         }
         final String email = EditTextUtils.getText(mEmailTextField).trim();
-        String suggest = EmailChecker.suggestDomainCorrection(email);
+        String suggest = mEmailChecker.suggestDomainCorrection(email);
         if (suggest.compareTo(email) != 0) {
             mEmailAutoCorrected = true;
             mEmailTextField.setText(suggest);
