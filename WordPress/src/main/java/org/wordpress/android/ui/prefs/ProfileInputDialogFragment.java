c7ad850a83068a615956fdd6eb2827e0c769b43d
Oguz Kocer <oguz.kocer@automattic.com>
2016-04-08 12:35:48 +0300

    Remove the custom hint from my profile dialog and use dialog message instead

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/ProfileInputDialogFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/ProfileInputDialogFragment.java
@@ -50,7 +50,6 @@ public class ProfileInputDialogFragment extends DialogFragment {
 
         final WPTextView textView = (WPTextView) promptView.findViewById(R.id.my_profile_dialog_label);
         final WPEditText editText = (WPEditText) promptView.findViewById(R.id.my_profile_dialog_input);
-        final WPTextView hintView = (WPTextView) promptView.findViewById(R.id.my_profile_dialog_hint);
 
         Bundle args = getArguments();
         String title = args.getString(TITLE_TAG);
@@ -61,9 +60,7 @@ public class ProfileInputDialogFragment extends DialogFragment {
 
         textView.setText(title);
         if (!TextUtils.isEmpty(hint)) {
-            hintView.setText(hint);
-        } else {
-            hintView.setVisibility(View.GONE);
+            alertDialogBuilder.setMessage(hint);
         }
 
         if (!isMultiline) {
