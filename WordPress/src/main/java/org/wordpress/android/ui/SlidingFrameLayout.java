dbcea61153a0ee137e63065dac951c56a9d5bb7a
Oguz Kocer <oguz.kocer@automattic.com>
2016-04-13 16:50:31 +0300

    Use XFraction instead of X for animation fragments

---
+++ WordPress/src/main/java/org/wordpress/android/ui/SlidingFrameLayout.java
@@ -0,0 +1,26 @@
+package org.wordpress.android.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class SlidingFrameLayout extends FrameLayout {
+
+    public SlidingFrameLayout(Context context) {
+        super(context);
+    }
+
+    public SlidingFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public float getXFraction() {
+        int width = getWidth();
+        return (width == 0) ? 0 : getX() / (float) width;
+    }
+
+    public void setXFraction(float xFraction) {
+        int width = getWidth();
+        setX((width > 0) ? (xFraction * width) : 0);
+    }
+}
