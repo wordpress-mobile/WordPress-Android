964b907da40a29eddd8bdab88a931456c3a0ac75
Oguz Kocer <oguz.kocer@automattic.com>
2016-04-14 16:26:23 +0300

    Refactor SlidingRelativeLayout so the first animation frame is drawn correctly

--- WordPress/src/main/java/org/wordpress/android/ui/SlidingRelativeLayout.java
+++ WordPress/src/main/java/org/wordpress/android/ui/SlidingRelativeLayout.java
@@ -2,9 +2,12 @@ package org.wordpress.android.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
 
 public class SlidingRelativeLayout extends RelativeLayout {
+
+    private float xFraction = 0;
 
     public SlidingRelativeLayout(Context context) {
         super(context);
@@ -14,13 +17,32 @@ public class SlidingRelativeLayout extends RelativeLayout {
         super(context, attrs);
     }
 
-    public float getXFraction() {
-        int width = getWidth();
-        return (width == 0) ? 0 : getX() / (float) width;
+    private ViewTreeObserver.OnPreDrawListener preDrawListener = null;
+
+    // This implementation fixes the first frame not being translated: http://trickyandroid.com/fragments-translate-animation/
+    public void setXFraction(float fraction) {
+        this.xFraction = fraction;
+
+        if (getWidth() == 0) {
+            if (preDrawListener == null) {
+                preDrawListener = new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        getViewTreeObserver().removeOnPreDrawListener(preDrawListener);
+                        setXFraction(xFraction);
+                        return true;
+                    }
+                };
+                getViewTreeObserver().addOnPreDrawListener(preDrawListener);
+            }
+            return;
+        }
+
+        float translationX = getWidth() * fraction;
+        setTranslationX(translationX);
     }
 
-    public void setXFraction(float xFraction) {
-        int width = getWidth();
-        setX((width > 0) ? (xFraction * width) : 0);
+    public float getXFraction() {
+        return this.xFraction;
     }
 }
