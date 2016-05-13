d50852b539ff3e345e99ddbb86c8ee3bb9c68722
Danilo Ercoli <ercoli@gmail.com>
2016-05-13 16:11:02 +0200

    Add a comment that states when `COMMENT_ID_UNKNOWN` is used.

--- WordPress/src/main/java/org/wordpress/android/ui/comments/CommentActionResult.java
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/CommentActionResult.java
@@ -3,7 +3,7 @@ package org.wordpress.android.ui.comments;
 public class CommentActionResult {
 
     public static final int COMMENT_ID_ON_ERRORS = -1;
-    public static final int COMMENT_ID_UNKNOWN = -2;
+    public static final int COMMENT_ID_UNKNOWN = -2; // This is used primarily for replies, when the commentID isn't known.
 
     private long mCommentID = COMMENT_ID_UNKNOWN;
     private String mMessage;
