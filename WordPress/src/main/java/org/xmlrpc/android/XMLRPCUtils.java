ab3cb9e6f78abd74988875dc7fa8a83279acf11d
Maxime Biais <maxime.biais@gmail.com>
2016-05-26 11:08:38 +0200

    minor comment update

--- WordPress/src/main/java/org/xmlrpc/android/XMLRPCUtils.java
+++ WordPress/src/main/java/org/xmlrpc/android/XMLRPCUtils.java
@@ -305,7 +305,7 @@ public class XMLRPCUtils {
     // whole process.
     private static String discoverSelfHostedXmlrpcUrl(String siteUrl, String httpUsername, String httpPassword) throws
             XMLRPCUtilsException {
-        // Array of Strings that contains the URLs we want to try
+        // Ordered set of Strings that contains the URLs we want to try
         final Set<String> urlsToTry = new LinkedHashSet<>();
 
         // add the url as provided by the user
