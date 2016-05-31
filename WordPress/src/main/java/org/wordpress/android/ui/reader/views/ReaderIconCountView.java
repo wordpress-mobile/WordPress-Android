d624df2c3ec6cc00e76a573f7ff298ebffa8fbed
Nick Bradbury <nick.bradbury@gmail.com>
2016-05-31 15:39:48 +0200

    Suppress getDrawable warning in ReaderIconCountView

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderIconCountView.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderIconCountView.java
@@ -2,6 +2,8 @@ package org.wordpress.android.ui.reader.views;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -53,10 +55,10 @@ public class ReaderIconCountView extends LinearLayout {
                 mIconType = a.getInteger(R.styleable.ReaderIconCountView_readerIcon, ICON_LIKE);
                 switch (mIconType) {
                     case ICON_LIKE :
-                        mImageView.setImageDrawable(context.getResources().getDrawable(R.drawable.reader_button_like));
+                        mImageView.setImageDrawable(getDrawableResource(context, R.drawable.reader_button_like));
                         break;
                     case ICON_COMMENT :
-                        mImageView.setImageDrawable(context.getResources().getDrawable(R.drawable.reader_button_comment));
+                        mImageView.setImageDrawable(getDrawableResource(context, R.drawable.reader_button_comment));
                         break;
                 }
 
@@ -66,6 +68,11 @@ public class ReaderIconCountView extends LinearLayout {
         }
 
         ReaderUtils.setBackgroundToRoundRipple(mImageView);
+    }
+
+    @SuppressWarnings("deprecation")
+    private Drawable getDrawableResource(Context context, @DrawableRes int drawableResId) {
+        return context.getResources().getDrawable(drawableResId);
     }
 
     public ImageView getImageView() {
