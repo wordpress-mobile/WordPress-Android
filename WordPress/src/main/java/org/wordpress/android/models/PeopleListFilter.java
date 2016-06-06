5a5d548dd574a69be70e7be77559b9df2e8b327d
Oguz Kocer <oguz.kocer@automattic.com>
2016-06-06 18:49:37 +0300

    PeopleListFilter introduced

---
+++ WordPress/src/main/java/org/wordpress/android/models/PeopleListFilter.java
@@ -0,0 +1,24 @@
+package org.wordpress.android.models;
+
+import android.support.annotation.StringRes;
+
+import org.wordpress.android.R;
+import org.wordpress.android.WordPress;
+
+public enum PeopleListFilter implements FilterCriteria {
+    TEAM(R.string.people_dropdown_item_team),
+    FOLLOWERS(R.string.people_dropdown_item_followers),
+    EMAIL_FOLLOWERS(R.string.people_dropdown_item_email_followers),
+    VIEWERS(R.string.people_dropdown_item_viewers);
+
+    private final int mLabelResId;
+
+    PeopleListFilter(@StringRes int labelResId) {
+        mLabelResId = labelResId;
+    }
+
+    @Override
+    public String getLabel() {
+        return WordPress.getContext().getString(mLabelResId);
+    }
+}
