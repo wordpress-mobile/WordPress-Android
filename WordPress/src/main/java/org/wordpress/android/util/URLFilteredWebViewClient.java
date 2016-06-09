de1b7bdbef4e04907663e037c782c2fd1d322023
Danilo Ercoli <ercoli@gmail.com>
2016-06-09 18:47:39 +0200

    Add a custom version of the WebViewClient class that adds the ability of disabling links in the web page.

---
+++ WordPress/src/main/java/org/wordpress/android/util/URLFilteredWebViewClient.java
@@ -0,0 +1,49 @@
+package org.wordpress.android.util;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * WebViewClient that is capable of handling HTTP authentication requests using the HTTP
+ * username and password of the blog configured for this activity.
+ */
+public class URLFilteredWebViewClient extends WebViewClient {
+    ArrayList<String> allowedURLs = new ArrayList<>();
+
+    public URLFilteredWebViewClient() {
+    }
+
+    public URLFilteredWebViewClient(String url) {
+       allowedURLs.add(url);
+    }
+
+    public URLFilteredWebViewClient(List<String> urls) {
+        if (urls == null || urls.size() == 0) {
+            AppLog.w(AppLog.T.UTILS, "No valid URLs passed to URLFilteredWebViewClient! " +
+                    "HTTP Links in the page are NOT disabled, and ALL URLs could be loaded by the user!!");
+            return;
+        }
+        allowedURLs.addAll(urls);
+    }
+
+    protected boolean isAllURLsAllowed() {
+        return allowedURLs.size() == 0;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        // Found a bug on some pages where there is an incorrect
+        // auto-redirect to file:///android_asset/webkit/.
+        if (url.equals("file:///android_asset/webkit/")) {
+            return true;
+        }
+
+        if (isAllURLsAllowed() || allowedURLs.contains(url)) {
+            view.loadUrl(url);
+        }
+        return true;
+    }
+}
