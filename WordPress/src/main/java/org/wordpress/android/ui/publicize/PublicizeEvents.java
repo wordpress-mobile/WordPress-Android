9187d49aadde0829dd7feacf04e25f8fee980c14
Will Kwon <will@kwon.co>
2016-06-22 12:51:07 -0700

    Adding new Event, ConnectionChooserRequired with an array of Connections that the dialog needs

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeEvents.java
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeEvents.java
@@ -1,7 +1,11 @@
 package org.wordpress.android.ui.publicize;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.wordpress.android.ui.publicize.PublicizeConstants.ConnectAction;
+
+import java.net.URI;
 
 /**
  * Publicize-related EventBus event classes
@@ -31,20 +35,87 @@ public class PublicizeEvents {
             return mSucceeded;
         }
     }
+
+    public static class ConnectionChooserRequired {
+        private JSONObject mJsonObject;
+        private Connection[] mConnections;
+
+        public ConnectionChooserRequired(JSONObject jsonObject) {
             mJsonObject = jsonObject;
-            mAction = action;
+            try {
+                mConnections = convertJsonToConnections();
+            } catch (JSONException e) {
+                mConnections = new Connection[0];
+                e.printStackTrace();
+            }
         }
 
-        public JSONObject getJsonObject() {
-            return mJsonObject;
+        public Connection[] getConnections() {
+            return mConnections;
         }
 
-        public ConnectAction getAction() {
-            return mAction;
+        private Connection[] convertJsonToConnections() throws JSONException {
+            JSONArray jsonArray = mJsonObject.getJSONArray("connections");
+            Connection[] connectionArray = new Connection[jsonArray.length()];
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject currentJsonConnection = jsonArray.getJSONObject(i);
+                URI profilePictureUrl = URI.create(currentJsonConnection.getString("external_profile_picture"));
+                String displayName = currentJsonConnection.getString("external_display");
+                int keychainId = currentJsonConnection.getInt("ID");
+                String connectionName = currentJsonConnection.getString("label");
+                JSONArray jsonSitesArray = currentJsonConnection.getJSONArray("sites");
+                int[] sitesArray = getSitesArrayFromJson(jsonSitesArray);
+
+                Connection connection = new Connection(profilePictureUrl, displayName, keychainId, connectionName, sitesArray);
+                connectionArray[i] = connection;
+            }
+
+            return connectionArray;
         }
 
-        public boolean didSucceed() {
-            return mSucceeded;
+        private int[] getSitesArrayFromJson(JSONArray jsonArray) throws JSONException {
+            int[] sitesArray = new int[jsonArray.length()];
+            for (int i = 0; i < jsonArray.length(); i++) {
+                sitesArray[i] = jsonArray.getInt(i);
+            }
+
+            return sitesArray;
+        }
+    }
+
+    public static class Connection {
+        private URI mProfilePictureUrl;
+        private String mDisplayName;
+        private int mKeychainId;
+        private String mServiceName;
+        private int[] mSites;
+
+        public Connection(URI profilePictureUrl, String displayName, int keychainId, String serviceName, int[] sites) {
+            mProfilePictureUrl = profilePictureUrl;
+            mDisplayName = displayName;
+            mKeychainId = keychainId;
+            mServiceName = serviceName;
+            mSites = sites;
+        }
+
+        public URI getProfilePictureUrl() {
+            return mProfilePictureUrl;
+        }
+
+        public String getDisplayName() {
+            return mDisplayName;
+        }
+
+        public int getKeychainId() {
+            return mKeychainId;
+        }
+
+        public String getServiceName() {
+            return mServiceName;
+        }
+
+        public int[] getSites() {
+            return mSites;
         }
     }
 }
