521c9ec38517dcdf8856b143dd7fe9bd57daca93
Klymentiy Haykov <forsver@gmail.com>
2016-06-23 16:52:19 -0700

    Added new comments state to moderation events.

--- WordPress/src/main/java/org/wordpress/android/ui/comments/CommentEvents.java
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/CommentEvents.java
@@ -1,16 +1,19 @@
 package org.wordpress.android.ui.comments;
 
 import org.wordpress.android.models.CommentList;
+import org.wordpress.android.models.CommentStatus;
 
 class CommentEvents {
 
     public static class CommentsBatchModerationFinishedEvent {
         private CommentList mComments;
         private boolean mIsDeleted;
+        private CommentStatus mNewStatus;
 
-        public CommentsBatchModerationFinishedEvent(CommentList comments, boolean isDeleted) {
+        public CommentsBatchModerationFinishedEvent(CommentList comments, boolean isDeleted, CommentStatus newStatus) {
             mComments = comments;
             mIsDeleted = isDeleted;
+            mNewStatus = newStatus;
         }
 
         public CommentList getComments() {
@@ -20,18 +23,24 @@ class CommentEvents {
         public boolean isDeleted() {
             return mIsDeleted;
         }
-    }
 
+        public CommentStatus getNewStatus() {
+            return mNewStatus;
+        }
+
+    }
 
     public static class CommentModerationFinishedEvent {
         private boolean mIsSuccess;
         private boolean mIsCommentsRefreshRequired;
         private long mCommentId;
+        private CommentStatus mNewStatus;
 
-        public CommentModerationFinishedEvent(boolean isSuccess, boolean isCommentsRefreshRequired, long commentId) {
+        public CommentModerationFinishedEvent(boolean isSuccess, boolean isCommentsRefreshRequired, long commentId, CommentStatus newStatus) {
             mIsSuccess = isSuccess;
             mIsCommentsRefreshRequired = isCommentsRefreshRequired;
             mCommentId = commentId;
+            mNewStatus = newStatus;
         }
 
         public boolean isSuccess() {
@@ -44,6 +53,10 @@ class CommentEvents {
 
         public long getCommentId() {
             return mCommentId;
+        }
+
+        public CommentStatus getNewStatus() {
+            return mNewStatus;
         }
     }
 }
