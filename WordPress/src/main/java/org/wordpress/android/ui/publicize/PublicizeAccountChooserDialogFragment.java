19da55a909a1e0de4acb05ed917da968f37d7610
Will Kwon <will@kwon.co>
2016-06-23 17:32:54 -0700

    Retrieving list item binding from data binder

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeAccountChooserDialogFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeAccountChooserDialogFragment.java
@@ -1,10 +1,20 @@
 package org.wordpress.android.ui.publicize;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.ListViewCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ListView;
+
+import org.wordpress.android.R;
+import org.wordpress.android.databinding.PublicizeConnectionListItemBinding;
 
 /**
  * Created by Will on 6/19/16.
@@ -14,6 +24,26 @@ public class PublicizeAccountChooserDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.publicize_account_chooser_dialog, null);
+        builder.setView(view);
+        builder.setPositiveButton(R.string.share_btn_connect, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                PublicizeActions.connectStepTwo(1, 1);
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+        PublicizeConnectionListItemBinding binding = DataBindingUtil.inflate(inflater, R.layout.publicize_connection_list_item, listView, false);
+
 
         return super.onCreateDialog(savedInstanceState);
     }
