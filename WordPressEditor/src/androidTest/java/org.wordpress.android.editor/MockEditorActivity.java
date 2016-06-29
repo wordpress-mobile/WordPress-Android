ced263752a32fb381556cee59b6a2031f5b4f380
Maxime Biais <maxime.biais@gmail.com>
2016-06-29 14:52:00 +0200

    Squashed 'libs/editor/' changes from a92ad9d..260a0d5
    260a0d5 Merge pull request #428 from wordpress-mobile/issue/204-start-post-formatted-text
    fb29391 Merge pull request #430 from wordpress-mobile/issue/429-html-regex-crash
    438932b Fix #429 HTML mode styling crash
    445938d Merge pull request #427 from wordpress-mobile/issue/417-format-current-word
    d38b4ab Merge branch 'develop' into issue/204-start-post-formatted-text
    709b82f Merge branch 'develop' into issue/417-format-current-word
    4758e2a Fix #204 by persisting inline styles when typing in an empty post
    4b86c90 Merge pull request #426 from wordpress-mobile/sync-wpandroid
    e4b4565 When applying inline formatting, apply the style to the entire word adjacent to the cursor
    cb9f57c Bumped targetSdkVersion to 24
    13043cb Bumped support library versions to 24
    60fb931 Updated Travis config for Android 24
    72ae111 Merge commit '7cee07f51ba62c3415d15fb71b0d83c7f507f843' into sync-editor
    9faeee7 Merge pull request #425 from wordpress-mobile/fix-example-image
    7a44419 Remove unused strings from the editor and merge them with the main strings.xml file for i18n
    5031c47 Fixed issue where partial text selections would be expanded to the full word when linking
    314e706 Extracted cursor expansion part of getSelectedTextToLinkify into a separate method
    9005ef3 Merge branch 'release/5.5' into develop
    fc4c730 Fix broken image link in example post
    61b592c Merge commit 'd0d8dd46c2b8223d66ec3e80578788d1b6cd8b85' into release/5.5
    e7289ce Adjusted spacing and styling for text expansion JS methods
    e6a29b5 Merge branch 'develop' into issue/3924-method-count
    a008e48 Merge branch 'release/5.5' into develop
    a7395aa Merge commit '49ac39827552c17c8d5b65eca236a55961cf6814' into release/5.5
    0c7e9f1 Merge commit '1cf141b3362c5c44f27a8b65775d3db27ac5756a' into release/5.5
    5123e3c Merge branch 'release/5.5' into develop
    b15d049 Merge commit '65eec31755233d054ff01bf729eea6d233f6d7df' into release/5.5
    9e8fc97 Merge commit '2fc5d53d4ba45ed45c806a5ac0772458a4ef403e' into release/5.5
    aab8458 update compileSdkVersion and buildToolsVersion in every subtrees
    3c57356 update gradle wrapper to 2.14
    3d748d5 onFeaturedImageChanged signature changed: long instead of int
    a3a53d3 Merge branch 'hotfix/5.4.2' into release/5.5
    19170dc Use a long for featured image IDs
    
    git-subtree-dir: libs/editor
    git-subtree-split: 260a0d5ba0eae70b44d4ccc826d2a2025f344280

--- WordPressEditor/src/androidTest/java/org.wordpress.android.editor/MockEditorActivity.java
+++ WordPressEditor/src/androidTest/java/org.wordpress.android.editor/MockEditorActivity.java
@@ -34,7 +34,7 @@ public class MockEditorActivity extends AppCompatActivity implements EditorFragm
     @Override
     public void onEditorFragmentInitialized() {
         mEditorFragment.setTitle("A title");
-        mEditorFragment.setContent(Utils.getHtmlFromFile(this, "example-content.html"));
+        mEditorFragment.setContent(Utils.getHtmlFromFile(this, "example/example-content.html"));
     }
 
     @Override
@@ -58,7 +58,7 @@ public class MockEditorActivity extends AppCompatActivity implements EditorFragm
     }
 
     @Override
-    public void onFeaturedImageChanged(int mediaId) {
+    public void onFeaturedImageChanged(long mediaId) {
 
     }
 
