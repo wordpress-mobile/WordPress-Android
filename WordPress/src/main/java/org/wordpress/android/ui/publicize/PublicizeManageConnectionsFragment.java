a56a29223167c55427da9bc51c06cde2ae0e7908
Will Kwon <will@kwon.co>
2016-07-10 16:23:51 -0500

    Adding listening for settings change and updating sharing label settings when appropriate

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeManageConnectionsFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeManageConnectionsFragment.java
@@ -2,20 +2,29 @@ package org.wordpress.android.ui.publicize;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import org.wordpress.android.R;
+import org.wordpress.android.WordPress;
+import org.wordpress.android.models.Blog;
+import org.wordpress.android.ui.prefs.BlogPreferencesActivity;
 import org.wordpress.android.ui.prefs.SiteSettingsFragment;
+import org.wordpress.android.ui.prefs.SiteSettingsInterface;
 import org.wordpress.android.ui.prefs.SummaryEditTextPreference;
+import org.wordpress.android.util.NetworkUtils;
 import org.wordpress.android.util.StringUtils;
 import org.wordpress.android.util.WPPrefUtils;
 
 
-public class PublicizeManageConnectionsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+public class PublicizeManageConnectionsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, SiteSettingsInterface.SiteSettingsListener {
     private PublicizePreferenceListener mListener;
     private SummaryEditTextPreference mLabelPreference;
+    private SiteSettingsInterface mSiteSettings;
+    private Blog mBlog;
 
     public PublicizeManageConnectionsFragment() {
     }
@@ -25,7 +34,32 @@ public class PublicizeManageConnectionsFragment extends PreferenceFragment imple
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.publicize_preferences);
 
+        mBlog = WordPress.currentBlog;
+        if (mBlog == null || !NetworkUtils.checkConnection(getActivity())) {
+            getActivity().finish();
+            return;
+        }
+
         mLabelPreference = (SummaryEditTextPreference) getChangePref(R.string.publicize_label);
+        mSiteSettings = SiteSettingsInterface.getInterface(getActivity(), mBlog, this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // always load cached settings
+        mSiteSettings.init(false);
+
+        if (true) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // initialize settings with locally cached values, fetch remote on first pass
+                    mSiteSettings.init(true);
+                }
+            }, 1000);
+        }
     }
 
     @Override
@@ -56,6 +90,10 @@ public class PublicizeManageConnectionsFragment extends PreferenceFragment imple
         return false;
     }
 
+    private void syncSettings() {
+
+    }
+
     private void changeEditTextPreferenceValue(EditTextPreference pref, String newValue) {
         if (newValue == null || pref == null || pref.getEditText().isInEditMode()) return;
 
@@ -65,6 +103,63 @@ public class PublicizeManageConnectionsFragment extends PreferenceFragment imple
             pref.setText(formattedValue);
             pref.setSummary(formattedValue);
         }
+    }
+
+    private void setPreferencesFromSiteSettings() {
+        changeEditTextPreferenceValue(mLabelPreference, mSiteSettings.getSharingLabel());
+//        mLocationPref.setChecked(mSiteSettings.getLocation());
+//        changeEditTextPreferenceValue(mTitlePref, mSiteSettings.getTitle());
+//        changeEditTextPreferenceValue(mTaglinePref, mSiteSettings.getTagline());
+//        changeEditTextPreferenceValue(mAddressPref, mSiteSettings.getAddress());
+//        changeEditTextPreferenceValue(mUsernamePref, mSiteSettings.getUsername());
+//        changeEditTextPreferenceValue(mPasswordPref, mSiteSettings.getPassword());
+//        changeLanguageValue(mSiteSettings.getLanguageCode());
+//        setDetailListPreferenceValue(mPrivacyPref,
+//                String.valueOf(mSiteSettings.getPrivacy()),
+//                mSiteSettings.getPrivacyDescription());
+//        setDetailListPreferenceValue(mImageWidthPref,
+//                mBlog.getMaxImageWidth(),
+//                mBlog.getMaxImageWidth());
+//        setCategories();
+//        setPostFormats();
+//        setAllowComments(mSiteSettings.getAllowComments());
+//        setSendPingbacks(mSiteSettings.getSendPingbacks());
+//        setReceivePingbacks(mSiteSettings.getReceivePingbacks());
+//        setDetailListPreferenceValue(mSortByPref,
+//                String.valueOf(mSiteSettings.getCommentSorting()),
+//                mSiteSettings.getSortingDescription());
+//        int approval = mSiteSettings.getManualApproval() ?
+//                mSiteSettings.getUseCommentWhitelist() ? 0
+//                        : -1 : 1;
+//        setDetailListPreferenceValue(mWhitelistPref, String.valueOf(approval), getWhitelistSummary(approval));
+//        String s = StringUtils.getQuantityString(getActivity(), R.string.site_settings_multiple_links_summary_zero,
+//                R.string.site_settings_multiple_links_summary_one,
+//                R.string.site_settings_multiple_links_summary_other, mSiteSettings.getMultipleLinks());
+//        mMultipleLinksPref.setSummary(s);
+//        mUploadAndLinkPref.setChecked(mBlog.isFullSizeImage());
+//        mIdentityRequiredPreference.setChecked(mSiteSettings.getIdentityRequired());
+//        mUserAccountRequiredPref.setChecked(mSiteSettings.getUserAccountRequired());
+//        mThreadingPref.setSummary(mSiteSettings.getThreadingDescription());
+//        mCloseAfterPref.setSummary(mSiteSettings.getCloseAfterDescriptionForPeriod());
+//        mPagingPref.setSummary(mSiteSettings.getPagingDescription());
+//        mRelatedPostsPref.setSummary(mSiteSettings.getRelatedPostsDescription());
+//        mModerationHoldPref.setSummary(mSiteSettings.getModerationHoldDescription());
+//        mBlacklistPref.setSummary(mSiteSettings.getBlacklistDescription());
+    }
+
+    @Override
+    public void onSettingsUpdated(Exception error) {
+        if (isAdded()) setPreferencesFromSiteSettings();
+    }
+
+    @Override
+    public void onSettingsSaved(Exception error) {
+
+    }
+
+    @Override
+    public void onCredentialsValidated(Exception error) {
+
     }
 
     public interface PublicizePreferenceListener {
