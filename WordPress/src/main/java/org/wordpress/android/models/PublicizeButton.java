8f8aabeb866d7601dd4a30262cb3c04125d90e9a
Will Kwon <will@kwon.co>
2016-07-18 20:10:51 -0500

    Adding SharingButton model

---
+++ WordPress/src/main/java/org/wordpress/android/models/PublicizeButton.java
@@ -0,0 +1,89 @@
+package org.wordpress.android.models;
+
+import org.json.JSONObject;
+
+public class PublicizeButton {
+    private static final String ID_KEY = "ID";
+    private static final String NAME_KEY = "name";
+    private static final String SHORT_NAME_KEY = "shortname";
+    private static final String CUSTOM_KEY = "custom";
+    private static final String ENABLED_KEY = "enabled";
+    private static final String VISIBILITY_KEY = "visibility";
+    private static final String GENERICON_KEY = "genericon";
+
+    private String mId;
+    private String mName;
+    private String mShortName;
+    private boolean mIsCustom;
+    private boolean mIsEnabled;
+    private String mVisibility;
+    private String mGenericon;
+
+    public PublicizeButton(JSONObject jsonObject) {
+        mId = jsonObject.optString(ID_KEY, "");
+        mName = jsonObject.optString(NAME_KEY, "");
+        mShortName = jsonObject.optString(SHORT_NAME_KEY, "");
+        mIsCustom = jsonObject.optBoolean(CUSTOM_KEY, false);
+        mIsEnabled = jsonObject.optBoolean(ENABLED_KEY, false);
+        mVisibility = jsonObject.optString(VISIBILITY_KEY, "visible");
+        mGenericon = jsonObject.optString(GENERICON_KEY, "");
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        this.mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    public String getShortName() {
+        return mShortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.mShortName = shortName;
+    }
+
+    public boolean isCustom() {
+        return mIsCustom;
+    }
+
+    public void setCustom(boolean custom) {
+        mIsCustom = custom;
+    }
+
+    public boolean isEnabled() {
+        return mIsEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        mIsEnabled = enabled;
+    }
+
+    public String getVisibility() {
+        return mVisibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.mVisibility = visibility;
+    }
+
+    public String getGenericon() {
+        return mGenericon;
+    }
+
+    public void setGenericon(String genericon) {
+        this.mGenericon = genericon;
+    }
+
+
+}
