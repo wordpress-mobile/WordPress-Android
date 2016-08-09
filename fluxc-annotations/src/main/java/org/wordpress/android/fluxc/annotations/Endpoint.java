7592b3381bef0062866ba8c8a624dcb0f68e2d8c
Alex Forcier <alex.forcier@automattic.com>
2016-08-09 14:05:29 -0400

    Added @Documented annotation to Endpoint annotation to include it in Javadocs

--- fluxc-annotations/src/main/java/org/wordpress/android/fluxc/annotations/Endpoint.java
+++ fluxc-annotations/src/main/java/org/wordpress/android/fluxc/annotations/Endpoint.java
@@ -1,11 +1,13 @@
 package org.wordpress.android.fluxc.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
  * Declares a valid REST endpoint.
  */
+@Documented
 @Target({ElementType.FIELD, ElementType.METHOD})
 public @interface Endpoint {
     String value();
