ce9ca599cdd1ecd3c7e13d38ee6f546fd6aa7a34
Maxime Biais <maxime.biais@gmail.com>
2016-08-23 16:33:23 +0200

    Remove unused code

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/site/SiteXMLRPCClientTest.java
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/site/SiteXMLRPCClientTest.java
@@ -35,7 +35,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.wordpress.android.fluxc.utils.SiteUtils.generateDotComSite;
+import static org.wordpress.android.fluxc.utils.SiteUtils.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class SiteXMLRPCClientTest {
