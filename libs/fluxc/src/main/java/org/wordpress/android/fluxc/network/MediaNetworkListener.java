667a75381754e7dc3d386f1d66da62ac43647105
Tony Rankin <tonyr@tonyrankin.com>
2016-08-22 19:16:47 -0700

    Updating error handling to simplify network listener

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/MediaNetworkListener.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/MediaNetworkListener.java
@@ -6,25 +6,32 @@ import org.wordpress.android.fluxc.model.MediaModel;
 import java.util.List;
 
 public interface MediaNetworkListener {
+    enum MediaNetworkError {
+        NONE,
+        MEDIA_NOT_FOUND,
+        UNKNOWN;
+        public Exception exception;
+    }
+
     /**
      * Notifies that an error has occurred while attempting to interface with network media.
      */
-    void onMediaError(MediaAction cause, Exception error);
+    void onMediaError(MediaAction cause, MediaModel media, MediaNetworkError error);
 
     /**
      * Media has been pulled successfully from a remote source.
      */
-    void onMediaPulled(MediaAction cause, List<MediaModel> pulledMedia, List<Exception> errors);
+    void onMediaPulled(MediaAction cause, List<MediaModel> pulledMedia);
 
     /**
      * Media changes have been successfully pushed to remote.
      */
-    void onMediaPushed(MediaAction cause, List<MediaModel> pushedMedia, List<Exception> errors);
+    void onMediaPushed(MediaAction cause, List<MediaModel> pushedMedia);
 
     /**
      * Media has been successfully deleted from remote.
      */
-    void onMediaDeleted(MediaAction cause, List<MediaModel> deletedMedia, List<Exception> errors);
+    void onMediaDeleted(MediaAction cause, List<MediaModel> deletedMedia);
 
     /**
      * Reports media upload progress as it is transmitted over the network.
