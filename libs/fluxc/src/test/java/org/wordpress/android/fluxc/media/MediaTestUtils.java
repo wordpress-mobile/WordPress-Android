fec543ef4ae0170462853bdfb65c5e967f83a3b8
Tony Rankin <tonyr@tonyrankin.com>
2016-08-26 16:20:57 -0700

    Adding MediaStore unit test file (plus utils class) with minimal tests

---
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/media/MediaTestUtils.java
@@ -0,0 +1,50 @@
+package org.wordpress.android.fluxc.media;
+
+import org.wordpress.android.fluxc.model.MediaModel;
+import org.wordpress.android.fluxc.persistence.MediaSqlUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
+
+public class MediaTestUtils {
+    public static List<MediaModel> insertRandomMediaIntoDatabase(long siteId, int count) {
+        List<MediaModel> insertedMedia = generateRandomizedMediaList(count, siteId);
+        for (MediaModel media : insertedMedia) {
+            assertTrue(MediaSqlUtils.insertOrUpdateMedia(media) == 0);
+        }
+        return insertedMedia;
+    }
+
+    public static MediaModel generateMedia(String title, String desc, String caption, String alt) {
+        MediaModel media = new MediaModel();
+        media.setTitle(title);
+        media.setDescription(desc);
+        media.setCaption(caption);
+        media.setAlt(alt);
+        return media;
+    }
+
+    public static MediaModel generateRandomizedMedia(long siteId) {
+        MediaModel media = generateMedia(randomStr(5), randomStr(5), randomStr(5), randomStr(5));
+        media.setBlogId(siteId);
+        return media;
+    }
+
+    public static List<MediaModel> generateRandomizedMediaList(int size, long siteId) {
+        List<MediaModel> mediaList = new ArrayList<>();
+        for (int i = 0; i < size; ++i) {
+            MediaModel newMedia = generateRandomizedMedia(siteId);
+            newMedia.setMediaId(i);
+            mediaList.add(newMedia);
+        }
+        return mediaList;
+    }
+
+    public static String randomStr(int length) {
+        String randomString = UUID.randomUUID().toString();
+        return length > randomString.length() ? randomString : randomString.substring(0, length);
+    }
+}
