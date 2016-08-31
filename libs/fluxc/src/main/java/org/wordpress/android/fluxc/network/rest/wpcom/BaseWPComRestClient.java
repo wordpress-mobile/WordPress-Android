df910a254cf1f9bc3eb8d9e0d37f32a1b70e6fc4
Maxime Biais <maxime.biais@gmail.com>
2016-08-31 15:55:19 +0200

    Add app context to our wpcom rest clients

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/BaseWPComRestClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/BaseWPComRestClient.java
@@ -1,29 +1,34 @@
 package org.wordpress.android.fluxc.network.rest.wpcom;
 
+import android.content.Context;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 
-import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
-import org.wordpress.android.fluxc.network.UserAgent;
-import org.wordpress.android.fluxc.network.rest.wpcom.auth.Authenticator.AuthenticateErrorPayload;
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
 import org.wordpress.android.fluxc.network.BaseRequest.OnAuthFailedListener;
+import org.wordpress.android.fluxc.network.UserAgent;
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken;
+import org.wordpress.android.fluxc.network.rest.wpcom.auth.Authenticator.AuthenticateErrorPayload;
+import org.wordpress.android.util.LanguageUtils;
 
 public class BaseWPComRestClient {
     private AccessToken mAccessToken;
     private final RequestQueue mRequestQueue;
     protected final Dispatcher mDispatcher;
+    protected final Context mAppContext;
     private UserAgent mUserAgent;
 
     protected OnAuthFailedListener mOnAuthFailedListener;
 
-    public BaseWPComRestClient(Dispatcher dispatcher, RequestQueue requestQueue,
+    public BaseWPComRestClient(Context appContext, Dispatcher dispatcher, RequestQueue requestQueue,
                                AccessToken accessToken, UserAgent userAgent) {
         mRequestQueue = requestQueue;
         mDispatcher = dispatcher;
         mAccessToken = accessToken;
         mUserAgent = userAgent;
+        mAppContext = appContext;
         mOnAuthFailedListener = new OnAuthFailedListener() {
             @Override
             public void onAuthFailed(AuthenticateErrorPayload authError) {
