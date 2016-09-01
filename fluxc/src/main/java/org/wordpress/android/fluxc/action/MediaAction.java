c334490e483c1d48a5776762d7b59d42ca04ba99
Tony Rankin <tonyr@tonyrankin.com>
2016-08-31 21:10:53 -0700

    Replacing usages of pull to fetch to adhere to guidelines

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/MediaAction.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/MediaAction.java
@@ -4,14 +4,14 @@ import org.wordpress.android.fluxc.annotations.Action;
 import org.wordpress.android.fluxc.annotations.ActionEnum;
 import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.store.MediaStore.ChangeMediaPayload;
-import org.wordpress.android.fluxc.store.MediaStore.PullMediaPayload;
+import org.wordpress.android.fluxc.store.MediaStore.FetchMediaPayload;
 import org.wordpress.android.fluxc.store.MediaStore.UploadMediaPayload;
 
 @ActionEnum
 public enum MediaAction implements IAction {
     // Remote actions
-    @Action(payloadType = PullMediaPayload.class)   PULL_ALL_MEDIA,
-    @Action(payloadType = PullMediaPayload.class)   PULL_MEDIA,
+    @Action(payloadType = FetchMediaPayload.class)  FETCH_ALL_MEDIA,
+    @Action(payloadType = FetchMediaPayload.class)  FETCH_MEDIA,
     @Action(payloadType = ChangeMediaPayload.class) PUSH_MEDIA,
     @Action(payloadType = UploadMediaPayload.class) UPLOAD_MEDIA,
     @Action(payloadType = ChangeMediaPayload.class) DELETE_MEDIA,
