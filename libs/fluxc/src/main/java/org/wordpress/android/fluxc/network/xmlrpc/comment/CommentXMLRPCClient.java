36c12437a63e7befbc8683bd16ac466ce97005b7
Maxime Biais <maxime.biais@gmail.com>
2016-09-12 15:04:40 +0200

    First step at implementing _fetchComments_ in CommentXMLRPCClient

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/comment/CommentXMLRPCClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/comment/CommentXMLRPCClient.java
@@ -1,19 +1,92 @@
 package org.wordpress.android.fluxc.network.xmlrpc.comment;
 
+import android.support.annotation.NonNull;
+
 import com.android.volley.RequestQueue;
+import com.android.volley.Response.Listener;
 
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.generated.CommentActionBuilder;
+import org.wordpress.android.fluxc.generated.endpoint.XMLRPC;
+import org.wordpress.android.fluxc.model.CommentModel;
+import org.wordpress.android.fluxc.model.CommentStatus;
+import org.wordpress.android.fluxc.model.SiteModel;
+import org.wordpress.android.fluxc.network.BaseRequest.BaseErrorListener;
+import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError;
 import org.wordpress.android.fluxc.network.HTTPAuthManager;
 import org.wordpress.android.fluxc.network.UserAgent;
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken;
 import org.wordpress.android.fluxc.network.xmlrpc.BaseXMLRPCClient;
+import org.wordpress.android.fluxc.network.xmlrpc.XMLRPCRequest;
+import org.wordpress.android.fluxc.store.CommentStore.FetchCommentsResponsePayload;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 public class CommentXMLRPCClient extends BaseXMLRPCClient {
+    public static final int DEFAULT_NUMBER_COMMENTS = 50;
+
     @Inject
     public CommentXMLRPCClient(Dispatcher dispatcher, RequestQueue requestQueue, AccessToken accessToken,
-                             UserAgent userAgent, HTTPAuthManager httpAuthManager) {
+                               UserAgent userAgent, HTTPAuthManager httpAuthManager) {
         super(dispatcher, requestQueue, accessToken, userAgent, httpAuthManager);
+    }
+
+    public void fetchComments(final SiteModel site, int offset, CommentStatus status) {
+        List<Object> params = new ArrayList<>(2);
+        Map<String, Object> commentParams = new HashMap<>();
+        commentParams.put("number", DEFAULT_NUMBER_COMMENTS);
+        commentParams.put("offset", offset);
+
+        params.add(site.getSiteId());
+        params.add(site.getUsername());
+        params.add(site.getPassword());
+        params.add(commentParams);
+        final XMLRPCRequest request = new XMLRPCRequest(
+                site.getXmlRpcUrl(), XMLRPC.GET_COMMENTS, params,
+                new Listener<Object>() {
+                    @Override
+                    public void onResponse(Object response) {
+                        List<CommentModel> comments = commentsResponseToCommentList(response, site);
+                        FetchCommentsResponsePayload payload = new FetchCommentsResponsePayload(comments);
+                        mDispatcher.dispatch(CommentActionBuilder.newFetchedCommentsAction(payload));
+                    }
+                },
+                new BaseErrorListener() {
+                    @Override
+                    public void onErrorResponse(@NonNull BaseNetworkError error) {
+                    }
+                }
+        );
+        add(request);
+    }
+
+    private List<CommentModel> commentsResponseToCommentList(Object response, SiteModel site) {
+        List<CommentModel> comments = new ArrayList<>();
+        if (!(response instanceof Object[])) {
+            return comments;
+        }
+        Object[] responseArray = (Object[]) response;
+        for (Object commentObject: responseArray) {
+            CommentModel commentModel = commentResponseToComment(commentObject, site);
+            if (commentModel != null) {
+                comments.add(commentModel);
+            }
+        }
+        return comments;
+    }
+
+    private CommentModel commentResponseToComment(Object commentObject, SiteModel site) {
+        if (!(commentObject instanceof HashMap)) {
+            return null;
+        }
+        HashMap<String, ?> commentMap = (HashMap<String, ?>) commentObject;
+        CommentModel comment = new CommentModel();
+
+        return comment;
     }
 }
