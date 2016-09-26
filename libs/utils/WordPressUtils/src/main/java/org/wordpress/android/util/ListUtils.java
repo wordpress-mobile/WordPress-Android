f25d0206d0fbc991938189e6379efac366c95498
Oguz Kocer <oguz.kocer@automattic.com>
2016-09-26 18:28:38 +0300

    ListUtils helper methods checks for null input

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/ListUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/ListUtils.java
@@ -1,5 +1,7 @@
 package org.wordpress.android.util;
 
+import android.support.annotation.Nullable;
+
 import org.apache.commons.lang.ArrayUtils;
 
 import java.util.ArrayList;
@@ -7,12 +9,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ListUtils {
+    @Nullable
     public static ArrayList<Long> toLongList(long[] array) {
+        if (array == null) {
+            return null;
+        }
         Long[] longObjects = ArrayUtils.toObject(array);
         return new ArrayList<>(Arrays.asList(longObjects));
     }
 
+    @Nullable
     public static long[] toLongArray(List<Long> list) {
+        if (list == null) {
+            return null;
+        }
         Long[] array = (Long[]) list.toArray();
         return ArrayUtils.toPrimitive(array);
     }
