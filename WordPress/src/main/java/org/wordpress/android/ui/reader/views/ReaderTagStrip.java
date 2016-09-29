6f09250f05a11fc19c2338da523786734359cb9c
Nick Bradbury <nick.bradbury@gmail.com>
2016-09-29 18:43:43 -0400

    Don't add secondary tag if it will take up too much space

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderTagStrip.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderTagStrip.java
@@ -2,6 +2,8 @@ package org.wordpress.android.ui.reader.views;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
@@ -17,14 +19,16 @@ import org.wordpress.android.models.ReaderTagType;
 import org.wordpress.android.ui.reader.ReaderActivityLauncher;
 import org.wordpress.android.ui.reader.utils.ReaderUtils;
 import org.wordpress.android.util.AniUtils;
+import org.wordpress.android.util.DisplayUtils;
 
 /**
- * used by the detail view to display a row of tags from a reader post
+ * used by the detail view to display the primary and secondary tags from a reader post
  */
 public class ReaderTagStrip extends LinearLayout {
 
     private LinearLayout mView;
     private int mNumTags;
+    private String mAllTags;
 
     public ReaderTagStrip(Context context) {
         super(context);
@@ -70,13 +74,37 @@ public class ReaderTagStrip extends LinearLayout {
     }
 
     private void addTag(final String tagName) {
+        String tagDisplayName = mNumTags > 0 ? ", " + ReaderUtils.makeHashTag(tagName) : ReaderUtils.makeHashTag(tagName);
+
+        // inflate a new textView to show this tag
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View tagView = inflater.inflate(R.layout.reader_tag_strip_label, mView, false);
-
         TextView txtTag = (TextView) tagView.findViewById(R.id.text_tag);
-        String tagDisplayName = mNumTags > 0 ? ", " + ReaderUtils.makeHashTag(tagName) : ReaderUtils.makeHashTag(tagName);
-        txtTag.setText(tagDisplayName);
+        mView.addView(tagView);
 
+        // skip showing this tag if it will make the view wider than the max
+        if (mNumTags > 0) {
+            // determine the width of the passed tag in the new textView
+            Rect rect = new Rect();
+            Paint paint = txtTag.getPaint();
+            paint.getTextBounds(tagDisplayName, 0, tagDisplayName.length(), rect);
+            int tagWidth = rect.width();
+
+            // determine the width of the tag already displayed
+            paint.getTextBounds(mAllTags, 0, mAllTags.length(), rect);
+            int currentWidth = rect.width();
+
+            int maxWidth = DisplayUtils.getDisplayPixelWidth(getContext());
+            if (currentWidth + tagWidth > maxWidth) {
+                return;
+            }
+        }
+
+        txtTag.setText(tagDisplayName);
+        mAllTags += tagDisplayName;
+        mNumTags++;
+
+        // show all posts with this tag when the tag is clicked
         txtTag.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,8 +112,5 @@ public class ReaderTagStrip extends LinearLayout {
                 ReaderActivityLauncher.showReaderTagPreview(v.getContext(), tag);
             }
         });
-
-        mView.addView(tagView);
-        mNumTags++;
     }
 }
