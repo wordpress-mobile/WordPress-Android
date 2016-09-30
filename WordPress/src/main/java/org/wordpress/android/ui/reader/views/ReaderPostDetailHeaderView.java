ab930c9e17aa9409988a49f6646bbb7344ebee56
Nick Bradbury <nick.bradbury@gmail.com>
2016-09-30 08:23:06 -0400

    First pass at header view

---
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderPostDetailHeaderView.java
@@ -0,0 +1,134 @@
+package org.wordpress.android.ui.reader.views;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.wordpress.android.R;
+import org.wordpress.android.models.ReaderPost;
+import org.wordpress.android.ui.reader.actions.ReaderActions;
+import org.wordpress.android.ui.reader.actions.ReaderBlogActions;
+import org.wordpress.android.ui.reader.utils.ReaderUtils;
+import org.wordpress.android.util.NetworkUtils;
+import org.wordpress.android.util.ToastUtils;
+import org.wordpress.android.widgets.WPNetworkImageView;
+
+/**
+ * topmost view in post detail
+ */
+public class ReaderPostDetailHeaderView extends LinearLayout {
+
+    private ReaderPost mPost;
+    private ReaderFollowButton mFollowButton;
+
+    public ReaderPostDetailHeaderView(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public ReaderPostDetailHeaderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    public ReaderPostDetailHeaderView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context);
+    }
+
+    private void initView(Context context) {
+        View view = inflate(context, R.layout.reader_post_detail_header_view, this);
+        mFollowButton = (ReaderFollowButton) view.findViewById(R.id.follow_button);
+    }
+
+    public void setPost(@NonNull ReaderPost post) {
+        mPost = post;
+
+        TextView txtTitle = (TextView) findViewById(R.id.text_title);
+        TextView txtAuthorName = (TextView) findViewById(R.id.text_author_name);
+        TextView txtFollowCount = (TextView) findViewById(R.id.text_blog_follow_count);
+        WPNetworkImageView imgAvatar = (WPNetworkImageView) findViewById(R.id.image_avatar);
+
+        boolean hasBlogName = mPost.hasBlogName();
+        boolean hasAuthorName = mPost.hasAuthorName();
+
+        if (hasBlogName && hasAuthorName) {
+            txtTitle.setText(mPost.getBlogName());
+            // don't show author name if it's the same as the blog name
+            if (mPost.getAuthorName().equals(mPost.getBlogName())) {
+                txtAuthorName.setVisibility(View.GONE);
+            } else {
+                txtAuthorName.setText(mPost.getAuthorName());
+                txtAuthorName.setVisibility(View.VISIBLE);
+            }
+        } else if (hasBlogName) {
+            txtTitle.setText(mPost.getBlogName());
+            txtAuthorName.setVisibility(View.GONE);
+        } else if (hasAuthorName) {
+            txtTitle.setText(mPost.getAuthorName());
+            txtAuthorName.setVisibility(View.GONE);
+        } else {
+            txtTitle.setText(R.string.untitled);
+            txtAuthorName.setVisibility(View.GONE);
+        }
+
+        // TODO: get follower count
+        //txtFollowCount.setText(String.format(getContext().getString(R.string.reader_label_follow_count), blogInfo.numSubscribers));
+
+        if (ReaderUtils.isLoggedOutReader()) {
+            mFollowButton.setVisibility(View.GONE);
+        } else {
+            mFollowButton.setVisibility(View.VISIBLE);
+            mFollowButton.setIsFollowed(mPost.isFollowedByCurrentUser);
+            mFollowButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    toggleFollowStatus();
+                }
+            });
+        }
+
+        // TODO: show avatar/blavatar
+        int avatarSize = getContext().getResources().getDimensionPixelSize(R.dimen.avatar_sz_large);
+    }
+
+    private void toggleFollowStatus() {
+        if (!NetworkUtils.checkConnection(getContext())) {
+            return;
+        }
+
+        final boolean isAskingToFollow = !mPost.isFollowedByCurrentUser;
+
+        ReaderActions.ActionListener listener = new ReaderActions.ActionListener() {
+            @Override
+            public void onActionResult(boolean succeeded) {
+                if (getContext() == null) {
+                    return;
+                }
+                mFollowButton.setEnabled(true);
+                if (!succeeded) {
+                    int errResId = isAskingToFollow ? R.string.reader_toast_err_follow_blog : R.string.reader_toast_err_unfollow_blog;
+                    ToastUtils.showToast(getContext(), errResId);
+                    mFollowButton.setIsFollowed(!isAskingToFollow);
+                }
+            }
+        };
+
+        // disable follow button until API call returns
+        mFollowButton.setEnabled(false);
+
+        boolean result;
+        if (mPost.feedId != 0) {
+            result = ReaderBlogActions.followFeedById(mPost.feedId, isAskingToFollow, listener);
+        } else {
+            result = ReaderBlogActions.followBlogById(mPost.blogId, isAskingToFollow, listener);
+        }
+
+        if (result) {
+            mFollowButton.setIsFollowedAnimated(isAskingToFollow);
+        }
+    }
+}
