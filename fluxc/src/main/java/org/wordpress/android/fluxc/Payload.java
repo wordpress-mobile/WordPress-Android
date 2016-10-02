0e95a2f267ca9f5eca5904a2a7ce14fbfab3fac0
Alex Forcier <alex.forcier@automattic.com>
2016-10-02 17:51:54 +0300

    Make Payload abstract

--- fluxc/src/main/java/org/wordpress/android/fluxc/Payload.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/Payload.java
@@ -2,7 +2,7 @@ package org.wordpress.android.fluxc;
 
 import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError;
 
-public class Payload {
+public abstract class Payload {
     public BaseNetworkError error;
     public boolean isError() {
         return error != null;
