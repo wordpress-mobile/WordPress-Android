9ac97c1c76572b6b4d00b674aea8a24da73ec1cc
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2016-10-05 17:55:36 +0300

    Navigate to Feed viewver on pressing Up

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostPagerActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostPagerActivity.java
@@ -2,10 +2,13 @@ package org.wordpress.android.ui.reader;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -170,6 +173,17 @@ public class ReaderPostPagerActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
+            if (mIsFeed) {
+                Intent feedListIntent = ReaderActivityLauncher.getReaderFeedPreviewIntent(this, mBlogId);
+
+                if (NavUtils.shouldUpRecreateTask(this, feedListIntent) || isTaskRoot()) {
+                    TaskStackBuilder.create(this)
+                            .addNextIntentWithParentStack(feedListIntent)
+                            .startActivities();
+                    return true;
+                }
+            }
+
             onBackPressed();
             return true;
         }
