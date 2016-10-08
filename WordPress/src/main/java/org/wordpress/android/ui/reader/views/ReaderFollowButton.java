fabbe9d3d89be4310e2e9b515871741c00ef6afc
Nick Bradbury <nick.bradbury@gmail.com>
2016-10-08 10:22:41 -0400

    Modify follow button to have no caption

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderFollowButton.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderFollowButton.java
@@ -6,6 +6,7 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -22,31 +23,48 @@ public class ReaderFollowButton extends LinearLayout {
     private TextView mTextFollow;
     private ImageView mImageFollow;
     private boolean mIsFollowed;
+    private boolean mShowCaption;
 
     public ReaderFollowButton(Context context){
         super(context);
-        initView(context);
+        initView(context, null);
     }
 
     public ReaderFollowButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initView(context);
+        initView(context, attrs);
     }
 
     public ReaderFollowButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        initView(context);
+        initView(context, attrs);
     }
 
-    private void initView(Context context) {
+    private void initView(Context context, AttributeSet attrs) {
         inflate(context, R.layout.reader_follow_button, this);
         mTextFollow = (TextView) findViewById(R.id.text_follow_button);
         mImageFollow = (ImageView) findViewById(R.id.image_follow_button);
+
+        // default to showing caption, then read the value from passed attributes
+        mShowCaption = true;
+        if (attrs != null) {
+            TypedArray array = context.getTheme().obtainStyledAttributes(attrs, R.styleable.ReaderFollowButton, 0, 0);
+            if (array != null) {
+                mShowCaption = array.getBoolean(R.styleable.ReaderFollowButton_wpShowCaption, true);
+            }
+        }
+
+        if (!mShowCaption) {
+            mTextFollow.setText(null);
+            mTextFollow.setVisibility(View.GONE);
+        }
     }
 
     private void updateFollowText() {
+        if (mShowCaption) {
+            mTextFollow.setText(mIsFollowed ? R.string.reader_btn_unfollow : R.string.reader_btn_follow);
+        }
         mTextFollow.setSelected(mIsFollowed);
-        mTextFollow.setText(mIsFollowed ? R.string.reader_btn_unfollow : R.string.reader_btn_follow);
         mImageFollow.setImageResource(mIsFollowed ? R.drawable.reader_following : R.drawable.reader_follow);
     }
 
