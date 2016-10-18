54495e8e9b8b3ffc78f54ec5717ac989e7c6e716
Oguz Kocer <oguz.kocer@automattic.com>
2016-10-18 17:23:59 +0300

    Introduces getPostsForSiteWithFormat to PostStore

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/PostStore.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/PostStore.java
@@ -213,6 +213,13 @@ public class PostStore extends Store {
     }
 
     /**
+     * Returns posts with given format in the store for the given site as a {@link PostModel} list.
+     */
+    public List<PostModel> getPostsForSiteWithFormat(SiteModel site, String postFormat) {
+        return PostSqlUtils.getPostsForSiteWithFormat(site, postFormat, false);
+    }
+
+    /**
      * Returns all pages in the store for the given site as a {@link PostModel} list.
      */
     public List<PostModel> getPagesForSite(SiteModel site) {
