f5c24d4d29dbce800242a7b6f48a8f545ba86136
Alex Forcier <alex.forcier@automattic.com>
2016-10-20 11:09:27 +0300

    Only emit OnPostUploaded when remote post is synced
    - For XML-RPC, this means fetching the newly uploaded post from the
    server in a new request and storing it in the db before emitting
    OnPostUploaded
    - For REST API, the new post is returned in the upload call - the
    only change is to emit OnPostUploaded after the post has been stored
    - Updated tests to reflect the expectation that OnPostUploaded is the
    final signal that a post is uploaded and the local copy updated

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/PostAction.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/PostAction.java
@@ -4,6 +4,7 @@ import org.wordpress.android.fluxc.annotations.Action;
 import org.wordpress.android.fluxc.annotations.ActionEnum;
 import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.model.PostModel;
+import org.wordpress.android.fluxc.store.PostStore.FetchPostResponsePayload;
 import org.wordpress.android.fluxc.store.PostStore.RemotePostPayload;
 import org.wordpress.android.fluxc.store.PostStore.FetchPostsPayload;
 import org.wordpress.android.fluxc.store.PostStore.FetchPostsResponsePayload;
@@ -19,7 +20,7 @@ public enum PostAction implements IAction {
     FETCHED_POSTS,
     @Action(payloadType = RemotePostPayload.class)
     FETCH_POST,
-    @Action(payloadType = RemotePostPayload.class)
+    @Action(payloadType = FetchPostResponsePayload.class)
     FETCHED_POST,
     @Action(payloadType = InstantiatePostPayload.class)
     INSTANTIATE_POST,
