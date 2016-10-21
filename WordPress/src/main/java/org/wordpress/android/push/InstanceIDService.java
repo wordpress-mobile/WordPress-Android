57c9f3636d59b4201c5fe28fc1dfdb91f00184b8
Mario Zorz <mariozorz@gmail.com>
2016-10-21 14:29:01 -0300

    removed unused import

--- WordPress/src/main/java/org/wordpress/android/push/InstanceIDService.java
+++ WordPress/src/main/java/org/wordpress/android/push/InstanceIDService.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 
 import com.google.android.gms.iid.InstanceIDListenerService;
 
-import org.wordpress.android.push.GCMRegistrationIntentService;
-
 public class InstanceIDService extends InstanceIDListenerService {
     @Override
     public void onTokenRefresh() {
