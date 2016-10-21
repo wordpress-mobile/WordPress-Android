8ff852f5ef3cca54bccd58ec510906fc66c7171e
Maxime Biais <maxime.biais@gmail.com>
2016-10-21 12:31:58 +0200

    Group comment actions

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/CommentAction.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/CommentAction.java
@@ -14,32 +14,37 @@ import org.wordpress.android.fluxc.store.CommentStore.RemoteCreateCommentPayload
 
 @ActionEnum
 public enum CommentAction implements IAction {
+    // Remote actions
     @Action(payloadType = FetchCommentsPayload.class)
     FETCH_COMMENTS,
-    @Action(payloadType = FetchCommentsResponsePayload.class)
-    FETCHED_COMMENTS,
     @Action(payloadType = RemoteCommentPayload.class)
     FETCH_COMMENT,
-    @Action(payloadType = RemoteCommentResponsePayload.class)
-    FETCHED_COMMENT,
-    @Action(payloadType = InstantiateCommentPayload.class)
-    INSTANTIATE_COMMENT,
     @Action(payloadType = RemoteCreateCommentPayload.class)
     CREATE_NEW_COMMENT,
-    @Action(payloadType = RemoteCommentResponsePayload.class)
-    CREATED_NEW_COMMENT,
-    @Action(payloadType = CommentModel.class)
-    UPDATE_COMMENT,
     @Action(payloadType = RemoteCommentPayload.class)
     PUSH_COMMENT,
+    @Action(payloadType = RemoteCommentPayload.class)
+    DELETE_COMMENT,
+
+    // Remote responses
+    @Action(payloadType = FetchCommentsResponsePayload.class)
+    FETCHED_COMMENTS,
+    @Action(payloadType = RemoteCommentResponsePayload.class)
+    FETCHED_COMMENT,
+    @Action(payloadType = RemoteCommentResponsePayload.class)
+    CREATED_NEW_COMMENT,
     @Action(payloadType = RemoteCommentResponsePayload.class)
     PUSHED_COMMENT,
+    @Action(payloadType = RemoteCommentResponsePayload.class)
+    DELETED_COMMENT,
+
+    // Locale actions
+    @Action(payloadType = InstantiateCommentPayload.class)
+    INSTANTIATE_COMMENT,
+    @Action(payloadType = CommentModel.class)
+    UPDATE_COMMENT,
     @Action(payloadType = SiteModel.class)
     REMOVE_COMMENTS,
     @Action(payloadType = CommentModel.class)
     REMOVE_COMMENT,
-    @Action(payloadType = RemoteCommentPayload.class)
-    DELETE_COMMENT,
-    @Action(payloadType = RemoteCommentResponsePayload.class)
-    DELETED_COMMENT,
 }
