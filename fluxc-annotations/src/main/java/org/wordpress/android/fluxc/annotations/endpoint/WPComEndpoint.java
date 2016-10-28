01fdebd23f2edd2eb0f0c4a5b3e2b33e600ce2e2
Alex Forcier <alex.forcier@automattic.com>
2016-10-28 23:36:42 +0300

    Support WP.com availability and magic link email endpoints

--- fluxc-annotations/src/main/java/org/wordpress/android/fluxc/annotations/endpoint/WPComEndpoint.java
+++ fluxc-annotations/src/main/java/org/wordpress/android/fluxc/annotations/endpoint/WPComEndpoint.java
@@ -1,11 +1,12 @@
 package org.wordpress.android.fluxc.annotations.endpoint;
 
 public class WPComEndpoint {
-    private static final String WPCOM_REST_PREFIX = "https://public-api.wordpress.com/rest";
-    private static final String WPCOM_PREFIX_V1 = WPCOM_REST_PREFIX + "/v1";
-    private static final String WPCOM_PREFIX_V1_1 = WPCOM_REST_PREFIX + "/v1.1";
-    private static final String WPCOM_PREFIX_V1_2 = WPCOM_REST_PREFIX + "/v1.2";
-    private static final String WPCOM_PREFIX_V1_3 = WPCOM_REST_PREFIX + "/v1.3";
+    private static final String WPCOM_REST_PREFIX = "https://public-api.wordpress.com";
+    private static final String WPCOM_PREFIX_V1 = WPCOM_REST_PREFIX + "/rest/v1";
+    private static final String WPCOM_PREFIX_V1_1 = WPCOM_REST_PREFIX + "/rest/v1.1";
+    private static final String WPCOM_PREFIX_V1_2 = WPCOM_REST_PREFIX + "/rest/v1.2";
+    private static final String WPCOM_PREFIX_V1_3 = WPCOM_REST_PREFIX + "/rest/v1.3";
+    private static final String WPCOM_PREFIX_V0 = WPCOM_REST_PREFIX;
 
     private final String mEndpoint;
 
@@ -39,5 +40,9 @@ public class WPComEndpoint {
 
     public String getUrlV1_3() {
         return WPCOM_PREFIX_V1_3 + mEndpoint;
+    }
+
+    public String getUrlV0() {
+        return WPCOM_PREFIX_V0 + mEndpoint;
     }
 }
