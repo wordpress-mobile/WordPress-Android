b14baa9752e29eb32f918be0d2d90aecf885a1fe
Alex Forcier <alex.forcier@automattic.com>
2016-11-01 02:33:28 +0200

    Implement /is-available/ endpoints in AccountStore

---
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/account/IsAvailableResponse.java
@@ -0,0 +1,9 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.account;
+
+import org.wordpress.android.fluxc.network.rest.JsonObjectOrFalse;
+
+public class IsAvailableResponse extends JsonObjectOrFalse {
+    public String error;
+    public String message;
+    public String status;
+}
