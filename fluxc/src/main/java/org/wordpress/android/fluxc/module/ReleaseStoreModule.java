79714bf2026afc00b52400a0a2264bbe8cc0d061
Alex Forcier <alex.forcier@automattic.com>
2016-11-02 08:34:55 +0200

    Add basic TaxonomyStore

--- fluxc/src/main/java/org/wordpress/android/fluxc/module/ReleaseStoreModule.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/module/ReleaseStoreModule.java
@@ -6,18 +6,21 @@ import org.wordpress.android.fluxc.network.rest.wpcom.account.AccountRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken;
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.Authenticator;
 import org.wordpress.android.fluxc.network.rest.wpcom.comment.CommentRestClient;
+import org.wordpress.android.fluxc.network.rest.wpcom.media.MediaRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.post.PostRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.site.SiteRestClient;
-import org.wordpress.android.fluxc.network.rest.wpcom.media.MediaRestClient;
-import org.wordpress.android.fluxc.network.xmlrpc.media.MediaXMLRPCClient;
-import org.wordpress.android.fluxc.network.xmlrpc.site.SiteXMLRPCClient;
-import org.wordpress.android.fluxc.store.AccountStore;
-import org.wordpress.android.fluxc.store.MediaStore;
-import org.wordpress.android.fluxc.network.xmlrpc.post.PostXMLRPCClient;
+import org.wordpress.android.fluxc.network.rest.wpcom.taxonomy.TaxonomyRestClient;
 import org.wordpress.android.fluxc.network.xmlrpc.comment.CommentXMLRPCClient;
+import org.wordpress.android.fluxc.network.xmlrpc.media.MediaXMLRPCClient;
+import org.wordpress.android.fluxc.network.xmlrpc.post.PostXMLRPCClient;
+import org.wordpress.android.fluxc.network.xmlrpc.site.SiteXMLRPCClient;
+import org.wordpress.android.fluxc.network.xmlrpc.taxonomy.TaxonomyXMLRPCClient;
+import org.wordpress.android.fluxc.store.AccountStore;
 import org.wordpress.android.fluxc.store.CommentStore;
+import org.wordpress.android.fluxc.store.MediaStore;
 import org.wordpress.android.fluxc.store.PostStore;
 import org.wordpress.android.fluxc.store.SiteStore;
+import org.wordpress.android.fluxc.store.TaxonomyStore;
 
 import javax.inject.Singleton;
 
@@ -60,5 +63,12 @@ public class ReleaseStoreModule {
     public CommentStore provideCommentStore(Dispatcher dispatcher, CommentRestClient restClient,
                                             CommentXMLRPCClient xmlrpcClient) {
         return new CommentStore(dispatcher, restClient, xmlrpcClient);
+    }
+
+    @Provides
+    @Singleton
+    public TaxonomyStore provideTaxonomyStore(Dispatcher dispatcher, TaxonomyRestClient taxonomyRestClient,
+                                              TaxonomyXMLRPCClient taxonomyXMLRPCClient) {
+        return new TaxonomyStore(dispatcher, taxonomyRestClient, taxonomyXMLRPCClient);
     }
 }
