15862a75d6126a8a95b30ef03ee87dedd588dc22
Mario Zorz <mariozorz@gmail.com>
2016-11-16 16:21:54 -0800

    removed unused import

--- WordPress/src/main/java/org/wordpress/android/ui/notifications/NotificationsDetailActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/notifications/NotificationsDetailActivity.java
@@ -8,7 +8,6 @@ import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
