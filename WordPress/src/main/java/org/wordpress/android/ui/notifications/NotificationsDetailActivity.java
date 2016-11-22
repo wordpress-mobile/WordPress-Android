b935380ce50375adce2301a77928dc1ead9c2349
Mario Zorz <mariozorz@gmail.com>
2016-11-22 15:11:55 -0300

    removed unused import

--- WordPress/src/main/java/org/wordpress/android/ui/notifications/NotificationsDetailActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/notifications/NotificationsDetailActivity.java
@@ -12,7 +12,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
 
 import org.wordpress.android.R;
