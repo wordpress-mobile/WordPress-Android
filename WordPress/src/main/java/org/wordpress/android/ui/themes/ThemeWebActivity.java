e7fbe4da76a339e72c9ae1f2c28c5a9b4dc61559
Maxime Biais <maxime.biais@gmail.com>
2016-12-05 11:48:57 +0100

    Fix #4798: force the use of WPCom credentials when opening a WPCom url

--- WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeWebActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/themes/ThemeWebActivity.java
@@ -104,11 +104,10 @@ public class ThemeWebActivity extends WPWebViewActivity {
 
         String authURL = ThemeWebActivity.getBlogLoginUrl(blog);
         Intent intent = new Intent(activity, ThemeWebActivity.class);
-        intent.putExtra(ThemeWebActivity.AUTHENTICATION_USER, blog.getUsername());
-        intent.putExtra(ThemeWebActivity.AUTHENTICATION_PASSWD, blog.getPassword());
         intent.putExtra(ThemeWebActivity.URL_TO_LOAD, url);
         intent.putExtra(ThemeWebActivity.AUTHENTICATION_URL, authURL);
         intent.putExtra(ThemeWebActivity.LOCAL_BLOG_ID, blog.getLocalTableBlogId());
+        intent.putExtra(WPWebViewActivity.USE_GLOBAL_WPCOM_USER, true);
         intent.putExtra(IS_PREMIUM_THEME, currentTheme.isPremium());
         intent.putExtra(IS_CURRENT_THEME, isCurrentTheme);
         intent.putExtra(THEME_NAME, currentTheme.getName());
