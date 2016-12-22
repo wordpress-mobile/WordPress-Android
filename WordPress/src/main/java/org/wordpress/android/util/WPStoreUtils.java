b3ef7d5bc67911ae5cb02ddf7b5c51c2edb49c36
Tony Rankin <tonyr@automattic.com>
2016-12-21 22:14:58 -0800

    Add utility method to convert MediaFile to MediaModel

--- WordPress/src/main/java/org/wordpress/android/util/WPStoreUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/WPStoreUtils.java
@@ -1,7 +1,9 @@
 package org.wordpress.android.util;
 
+import org.wordpress.android.fluxc.model.MediaModel;
 import org.wordpress.android.fluxc.store.AccountStore;
 import org.wordpress.android.fluxc.store.SiteStore;
+import org.wordpress.android.util.helpers.MediaFile;
 
 public class WPStoreUtils {
     /**
@@ -10,5 +12,21 @@ public class WPStoreUtils {
      */
     public static boolean isSignedInWPComOrHasWPOrgSite(AccountStore accountStore, SiteStore siteStore) {
         return accountStore.hasAccessToken() || siteStore.hasSelfHostedSite();
+    }
+
+    public static MediaModel fromMediaFile(MediaFile file) {
+        MediaModel mediaModel = new MediaModel();
+        mediaModel.setFileName(file.getFileName());
+        mediaModel.setFilePath(file.getFilePath());
+        mediaModel.setFileExtension(org.wordpress.android.fluxc.utils.MediaUtils.getExtension(file.getFilePath()));
+        mediaModel.setMimeType(file.getMimeType());
+        mediaModel.setThumbnailUrl(file.getThumbnailURL());
+        mediaModel.setTitle(file.getTitle());
+        mediaModel.setDescription(file.getDescription());
+        mediaModel.setCaption(file.getCaption());
+        mediaModel.setMediaId(Long.valueOf(file.getMediaId()));
+        mediaModel.setUploadState(file.getUploadState());
+        mediaModel.setSiteId(Long.valueOf(file.getBlogId()));
+        return mediaModel;
     }
 }
