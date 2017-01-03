ffb49f09464ede49c805bc0a5a5f9055e3e27d6d
Mario Zorz <mariozorz@gmail.com>
2017-01-03 17:09:56 -0300

    changed quick action icon for a PNG resource to avoid crashes on Android 4.x

--- WordPress/src/main/java/org/wordpress/android/ui/notifications/services/NotificationsPendingDraftsService.java
+++ WordPress/src/main/java/org/wordpress/android/ui/notifications/services/NotificationsPendingDraftsService.java
@@ -286,7 +286,7 @@ public class NotificationsPendingDraftsService extends Service {
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, openDraftIntent,
                 PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.addAction(R.drawable.gridicons_pencil, getText(R.string.edit),
+        builder.addAction(R.drawable.ic_edit_icon, getText(R.string.edit),
                 pendingIntent);
     }
 
