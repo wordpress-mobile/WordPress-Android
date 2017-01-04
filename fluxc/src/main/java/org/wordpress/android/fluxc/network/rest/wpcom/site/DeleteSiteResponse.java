ce0e8b9ee8d6e7a51f4cac233522089a942b989e
Oguz Kocer <oguz.kocer@automattic.com>
2017-01-04 18:06:01 +0300

    deleteSite network call skeleton implemented

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/DeleteSiteResponse.java
@@ -0,0 +1,9 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.site;
+
+import org.wordpress.android.fluxc.network.Response;
+
+public class DeleteSiteResponse implements Response {
+    public boolean success;
+    public String error;
+    public String message;
+}
