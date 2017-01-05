10e9f8f1ac2b21432702ea2e83b75998d4ed2f3b
Oguz Kocer <oguz.kocer@automattic.com>
2017-01-05 17:29:00 +0300

    Use SiteWPComRestResponse instead of DeleteSiteResponse

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/DeleteSiteResponse.java
+++
@@ -1,9 +0,0 @@
-package org.wordpress.android.fluxc.network.rest.wpcom.site;
-
-import org.wordpress.android.fluxc.network.Response;
-
-public class DeleteSiteResponse implements Response {
-    public boolean success;
-    public String error;
-    public String message;
-}
