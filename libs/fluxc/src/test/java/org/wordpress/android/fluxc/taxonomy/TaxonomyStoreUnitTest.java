25b2593eea70af7cf12eb3f098620f33acb0bf39
Alex Forcier <alex.forcier@automattic.com>
2017-01-09 06:45:04 -0500

    Add unit test for retrieving tags from a post

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/taxonomy/TaxonomyStoreUnitTest.java
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/taxonomy/TaxonomyStoreUnitTest.java
@@ -215,4 +215,42 @@ public class TaxonomyStoreUnitTest {
 
         assertEquals(2, TaxonomySqlUtils.getTermsFromRemoteIdList(idList, site, DEFAULT_TAXONOMY_CATEGORY).size());
     }
+
+    @Test
+    public void testGetTagsFromPost() {
+        SiteModel site = new SiteModel();
+        site.setId(6);
+
+        TermModel tag = TaxonomyTestUtils.generateSampleTag();
+        TaxonomySqlUtils.insertOrUpdateTerm(tag);
+
+        TermModel tag2 = TaxonomyTestUtils.generateSampleTag();
+        tag2.setRemoteTermId(6);
+        tag2.setName("Something");
+        TaxonomySqlUtils.insertOrUpdateTerm(tag2);
+
+        List<String> nameList = new ArrayList<>();
+        nameList.add(tag.getName());
+        nameList.add(tag2.getName());
+
+        assertEquals(2, TaxonomySqlUtils.getTermsFromRemoteNameList(nameList, site, DEFAULT_TAXONOMY_TAG).size());
+
+        // Unsynced tag ID should be ignored in the final list
+        TermModel unsyncedTag = TaxonomyTestUtils.generateSampleTag();
+        unsyncedTag.setRemoteTermId(66);
+        unsyncedTag.setName("More");
+        nameList.add(unsyncedTag.getName());
+
+        assertEquals(2, TaxonomySqlUtils.getTermsFromRemoteNameList(nameList, site, DEFAULT_TAXONOMY_TAG).size());
+
+        // Empty list should return empty tag list
+        nameList.clear();
+
+        assertEquals(0, TaxonomySqlUtils.getTermsFromRemoteNameList(nameList, site, DEFAULT_TAXONOMY_TAG).size());
+
+        // List with only unsynced tags should return empty tag list
+        nameList.add(unsyncedTag.getName());
+
+        assertEquals(0, TaxonomySqlUtils.getTermsFromRemoteNameList(nameList, site, DEFAULT_TAXONOMY_TAG).size());
+    }
 }
