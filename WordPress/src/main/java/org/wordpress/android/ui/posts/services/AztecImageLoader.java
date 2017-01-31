9c55339d048f401e14c5d43f334c200c7ab14a79
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-01-31 16:45:20 +0200

    Load bitmap directly if local file

--- WordPress/src/main/java/org/wordpress/android/ui/posts/services/AztecImageLoader.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/services/AztecImageLoader.java
@@ -1,6 +1,8 @@
 package org.wordpress.android.ui.posts.services;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 
 import com.android.volley.VolleyError;
@@ -8,6 +10,8 @@ import com.android.volley.toolbox.ImageLoader;
 
 import org.wordpress.android.WordPress;
 import org.wordpress.aztec.Html;
+
+import java.io.File;
 
 public class AztecImageLoader implements Html.ImageGetter {
 
@@ -18,6 +22,14 @@ public class AztecImageLoader implements Html.ImageGetter {
     }
     @Override
     public void loadImage(String url, final Callbacks callbacks, int maxWidth) {
+        // TODO: if a local file then load it directly. This is a quick fix though.
+        if (new File(url).exists()) {
+            Bitmap bitmap = BitmapFactory.decodeFile(url);
+            BitmapDrawable bitmapDrawable = new BitmapDrawable(context.getResources(), bitmap);
+            callbacks.onImageLoaded(bitmapDrawable);
+            return;
+        }
+
         WordPress.imageLoader.get(url, new ImageLoader.ImageListener() {
             @Override
             public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
