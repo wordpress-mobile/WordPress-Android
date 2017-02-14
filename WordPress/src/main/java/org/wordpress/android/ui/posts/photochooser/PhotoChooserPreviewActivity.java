0cf80d44bbcec3c75f951bfb0f9a4878d35f21a0
Nick Bradbury <nick.bradbury@gmail.com>
2017-02-14 15:14:58 -0500

    2nd pass at video preview

--- WordPress/src/main/java/org/wordpress/android/ui/posts/photochooser/PhotoChooserPreviewActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/photochooser/PhotoChooserPreviewActivity.java
@@ -1,11 +1,16 @@
 package org.wordpress.android.ui.posts.photochooser;
 
+import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import org.wordpress.android.R;
@@ -45,8 +50,7 @@ public class PhotoChooserPreviewActivity extends AppCompatActivity {
         mVideoView.setVisibility(mIsVideo ? View.VISIBLE : View.GONE);
 
         if (mIsVideo) {
-            mVideoView.setVideoURI(mMediaUri);
-            mVideoView.start();
+            playVideo();
         } else {
             mImageView.setImageURI(mMediaUri);
 
@@ -72,4 +76,50 @@ public class PhotoChooserPreviewActivity extends AppCompatActivity {
         outState.putString(ARG_MEDIA_URI, mMediaUri.toString());
         outState.putBoolean(ARG_IS_VIDEO, mIsVideo);
     }
+
+    private void playVideo() {
+        MediaController controls = new MediaController(this);
+        mVideoView.setMediaController(controls);
+
+        String videoPath = getRealPathFromVideoURI();
+        mVideoView.setVideoPath(videoPath);
+        mVideoView.requestFocus();
+        mVideoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                }, 1500);
+                return false;
+            }
+        });
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+    }
+
+    public String getRealPathFromVideoURI() {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = getContentResolver().query(mMediaUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            if (cursor.moveToFirst()) {
+                return cursor.getString(column_index);
+            } else {
+                return null;
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
 }
