9aac40b6100d1374bc02bffd2f469807d01b5230
Danilo Ercoli <ercoli@gmail.com>
2017-03-02 12:06:47 +0100

    Add DB migration code + helper method to compare Strings

--- WordPress/src/main/java/org/wordpress/android/models/SiteSettingsModel.java
+++ WordPress/src/main/java/org/wordpress/android/models/SiteSettingsModel.java
@@ -29,7 +29,7 @@ public class SiteSettingsModel {
     public static final String LANGUAGE_COLUMN_NAME = "language";
     public static final String PRIVACY_COLUMN_NAME = "privacy";
     public static final String LOCATION_COLUMN_NAME = "location";
-    public static final String OPTIMIZE_IMAGE_COLUMN_NAME = "optimizeImage";
+    public static final String OPTIMIZED_IMAGE_COLUMN_NAME = "optimizeImage";
     public static final String DEF_CATEGORY_COLUMN_NAME = "defaultCategory";
     public static final String DEF_POST_FORMAT_COLUMN_NAME = "defaultPostFormat";
     public static final String CATEGORIES_COLUMN_NAME = "categories";
@@ -54,6 +54,8 @@ public class SiteSettingsModel {
     public static final String BLACKLIST_KEYS_COLUMN_NAME = "blacklistKeys";
 
     public static final String SETTINGS_TABLE_NAME = "site_settings";
+    public static final String ADD_OPTIMIZED_IMAGE = "alter table " + SETTINGS_TABLE_NAME +
+            " add " + OPTIMIZED_IMAGE_COLUMN_NAME + " BOOLEAN;";
     public static final String CREATE_SETTINGS_TABLE_SQL =
             "CREATE TABLE IF NOT EXISTS " +
                     SETTINGS_TABLE_NAME +
@@ -67,7 +69,6 @@ public class SiteSettingsModel {
                     LANGUAGE_COLUMN_NAME + " INTEGER, " +
                     PRIVACY_COLUMN_NAME + " INTEGER, " +
                     LOCATION_COLUMN_NAME + " BOOLEAN, " +
-                    OPTIMIZE_IMAGE_COLUMN_NAME + " BOOLEAN, " +
                     DEF_CATEGORY_COLUMN_NAME + " TEXT, " +
                     DEF_POST_FORMAT_COLUMN_NAME + " TEXT, " +
                     CATEGORIES_COLUMN_NAME + " TEXT, " +
@@ -136,21 +137,16 @@ public class SiteSettingsModel {
         SiteSettingsModel otherModel = (SiteSettingsModel) other;
 
         return localTableId == otherModel.localTableId &&
-                ((address == null && otherModel.address == null) ||
-                        (address != null && otherModel.address != null && address.equals(otherModel.address))) &&
-                ((username == null && otherModel.username == null) ||
-                        (username != null && otherModel.username != null && username.equals(otherModel.username))) &&
-                ((password == null && otherModel.password == null) ||
-                        (password != null && otherModel.password != null && password.equals(otherModel.password))) &&
-                ((title == null && otherModel.title == null) ||
-                        (title != null && otherModel.title != null && title.equals(otherModel.title))) &&
-                ((tagline == null && otherModel.tagline == null) ||
-                        (tagline != null && otherModel.tagline != null && tagline.equals(otherModel.tagline))) &&
+                equals(address, otherModel.address) &&
+                equals(username, otherModel.username) &&
+                equals(password, otherModel.password) &&
+                equals(title, otherModel.title) &&
+                equals(tagline, otherModel.tagline) &&
+                equals(defaultPostFormat, otherModel.defaultPostFormat) &&
                 languageId == otherModel.languageId &&
                 privacy == otherModel.privacy &&
                 location == otherModel.location &&
                 optimizeImage == otherModel.optimizeImage &&
-                defaultPostFormat.equals(otherModel.defaultPostFormat) &&
                 defaultCategory == otherModel.defaultCategory &&
                 showRelatedPosts == otherModel.showRelatedPosts &&
                 showRelatedPostHeader == otherModel.showRelatedPostHeader &&
@@ -167,9 +163,10 @@ public class SiteSettingsModel {
                 commentsRequireUserAccount == otherModel.commentsRequireUserAccount &&
                 commentAutoApprovalKnownUsers == otherModel.commentAutoApprovalKnownUsers &&
                 maxLinks == otherModel.maxLinks &&
-                holdForModeration != null && otherModel.holdForModeration != null
+                equals(defaultPostFormat, otherModel.defaultPostFormat) &&
+                holdForModeration != null
                     && holdForModeration.equals(otherModel.holdForModeration) &&
-                blacklist != null && otherModel.blacklist != null && blacklist.equals(otherModel.blacklist);
+                blacklist != null && blacklist.equals(otherModel.blacklist);
     }
 
     /**
@@ -238,7 +235,7 @@ public class SiteSettingsModel {
         defaultCategory = getIntFromCursor(cursor, DEF_CATEGORY_COLUMN_NAME);
         defaultPostFormat = getStringFromCursor(cursor, DEF_POST_FORMAT_COLUMN_NAME);
         location = getBooleanFromCursor(cursor, LOCATION_COLUMN_NAME);
-        optimizeImage = getBooleanFromCursor(cursor, OPTIMIZE_IMAGE_COLUMN_NAME);
+        optimizeImage = getBooleanFromCursor(cursor, OPTIMIZED_IMAGE_COLUMN_NAME);
         hasVerifiedCredentials = getBooleanFromCursor(cursor, CREDS_VERIFIED_COLUMN_NAME);
         allowComments = getBooleanFromCursor(cursor, ALLOW_COMMENTS_COLUMN_NAME);
         sendPingbacks = getBooleanFromCursor(cursor, SEND_PINGBACKS_COLUMN_NAME);
@@ -309,7 +306,7 @@ public class SiteSettingsModel {
         values.put(PRIVACY_COLUMN_NAME, privacy);
         values.put(LANGUAGE_COLUMN_NAME, languageId);
         values.put(LOCATION_COLUMN_NAME, location);
-        values.put(OPTIMIZE_IMAGE_COLUMN_NAME, optimizeImage);
+        values.put(OPTIMIZED_IMAGE_COLUMN_NAME, optimizeImage);
         values.put(DEF_CATEGORY_COLUMN_NAME, defaultCategory);
         values.put(CATEGORIES_COLUMN_NAME, categoryIdList(categories));
         values.put(DEF_POST_FORMAT_COLUMN_NAME, defaultPostFormat);
@@ -427,5 +424,13 @@ public class SiteSettingsModel {
     private boolean getBooleanFromCursor(Cursor cursor, String columnName) {
         int columnIndex = cursor.getColumnIndex(columnName);
         return columnIndex != -1 && cursor.getInt(columnIndex) != 0;
+    }
+
+    /**
+     * Helper method to check if two String are equals or not
+     */
+    private boolean equals(String first, String second) {
+        return (first == null && second == null) ||
+                (first != null  && first.equals(second));
     }
 }
