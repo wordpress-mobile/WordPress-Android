a04c8811dfe134d47a91b1a8b356e761f711a76f
Alex Forcier <alex.forcier@automattic.com>
2017-03-13 09:07:04 -0400

    Report XML-RPC parse errors in OnUnexpectedError event

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/BaseXMLRPCClient.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/BaseXMLRPCClient.java
@@ -3,17 +3,19 @@ package org.wordpress.android.fluxc.network.xmlrpc;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 
-import org.wordpress.android.fluxc.network.HTTPAuthManager;
-import org.wordpress.android.fluxc.network.UserAgent;
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
 import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.network.BaseRequest;
 import org.wordpress.android.fluxc.network.BaseRequest.OnAuthFailedListener;
+import org.wordpress.android.fluxc.network.BaseRequest.OnParseErrorListener;
+import org.wordpress.android.fluxc.network.HTTPAuthManager;
+import org.wordpress.android.fluxc.network.UserAgent;
 import org.wordpress.android.fluxc.network.discovery.DiscoveryRequest;
 import org.wordpress.android.fluxc.network.discovery.DiscoveryXMLRPCRequest;
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken;
 import org.wordpress.android.fluxc.store.AccountStore.AuthenticateErrorPayload;
+import org.wordpress.android.fluxc.utils.ErrorUtils.OnUnexpectedError;
 
 public class BaseXMLRPCClient {
     private AccessToken mAccessToken;
@@ -21,8 +23,10 @@ public class BaseXMLRPCClient {
     private final RequestQueue mRequestQueue;
     protected final Dispatcher mDispatcher;
     protected UserAgent mUserAgent;
-    protected OnAuthFailedListener mOnAuthFailedListener;
     protected HTTPAuthManager mHTTPAuthManager;
+
+    protected OnAuthFailedListener mOnAuthFailedListener;
+    protected OnParseErrorListener mOnParseErrorListener;
 
     public BaseXMLRPCClient(Dispatcher dispatcher, RequestQueue requestQueue, AccessToken accessToken,
                             UserAgent userAgent, HTTPAuthManager httpAuthManager) {
@@ -35,6 +39,12 @@ public class BaseXMLRPCClient {
             @Override
             public void onAuthFailed(AuthenticateErrorPayload authError) {
                 mDispatcher.dispatch(AuthenticationActionBuilder.newAuthenticateErrorAction(authError));
+            }
+        };
+        mOnParseErrorListener = new OnParseErrorListener() {
+            @Override
+            public void onParseError(OnUnexpectedError event) {
+                mDispatcher.emitChange(event);
             }
         };
     }
@@ -53,6 +63,7 @@ public class BaseXMLRPCClient {
 
     private BaseRequest setRequestAuthParams(BaseRequest request) {
         request.setOnAuthFailedListener(mOnAuthFailedListener);
+        request.setOnParseErrorListener(mOnParseErrorListener);
         request.setUserAgent(mUserAgent.getUserAgent());
         request.setHTTPAuthHeaderOnMatchingURL(mHTTPAuthManager);
         return request;
