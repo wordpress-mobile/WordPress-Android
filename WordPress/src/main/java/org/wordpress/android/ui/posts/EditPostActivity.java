5cc731958ad4ad3ffcf7aeb7b257040ff33c596a
Ondrej Ruttkay <ondrej@automattic.com>
2017-03-17 18:12:22 +0100

    Fix ImageUtils call

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -1771,7 +1771,7 @@ public class EditPostActivity extends AppCompatActivity implements EditorFragmen
                !NetworkUtils.isWiFiConnected(this)) {
             // Not on WiFi and optimize image is set to ON
             // Max picture size will be 3000px wide. That's the maximum resolution you can set in the current picker.
-            String optimizedPath = ImageUtils.createResizedImageWithMaxWidth(this, path, 3000);
+            String optimizedPath = ImageUtils.optimizeImage(this, path, 3000, 85);
 
             if (optimizedPath == null) {
                 AppLog.e(T.EDITOR, "Optimized picture was null!");
