e5c9f10952242c443b06ec3fec00151096118576
Danilo Ercoli <ercoli@gmail.com>
2017-03-24 19:52:59 +0100

    Make sure to optimize pictures only on mobile data connections. Props Nick!

--- WordPress/src/main/java/org/wordpress/android/util/WPMediaUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/WPMediaUtils.java
@@ -8,22 +8,26 @@ import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.ui.prefs.SiteSettingsInterface;
 
 public class WPMediaUtils {
-    public static Uri getOptimizedMedia(Activity activity, SiteModel siteModel, String path, boolean isVideo) {
-        if (!NetworkUtils.isWiFiConnected(activity) && !isVideo) {
-            SiteSettingsInterface siteSettings = SiteSettingsInterface.getInterface(activity, siteModel, null);
-            // Site Settings are implemented on .com/Jetpack sites only
-            if (siteSettings != null && siteSettings.init(false).getOptimizedImage()) {
-                // Not on WiFi and optimize image is set to ON
-                // Max picture size will be 3000px wide. That's the maximum resolution you can set in the current picker.
-                String optimizedPath = ImageUtils.optimizeImage(activity, path, 3000, 85);
 
-                if (optimizedPath == null) {
-                    AppLog.e(AppLog.T.EDITOR, "Optimized picture was null!");
-                    AnalyticsTracker.track(AnalyticsTracker.Stat.MEDIA_PHOTO_OPTIMIZE_ERROR);
-                } else {
-                    AnalyticsTracker.track(AnalyticsTracker.Stat.MEDIA_PHOTO_OPTIMIZED);
-                    return Uri.parse(optimizedPath);
-                }
+    // Max picture size will be 3000px wide. That's the maximum resolution you can set in the current picker.
+    private static final int OPTIMIZE_IMAGE_MAX_WIDTH = 3000;
+    private static final int OPTIMIZE_IMAGE_ENCODER_QUALITY = 85;
+
+    public static Uri getOptimizedMedia(Activity activity, SiteModel siteModel, String path, boolean isVideo) {
+        if (isVideo || !NetworkUtils.isMobileConnected(activity)) {
+            // Not on mobile data. Skip optimization.
+            return null;
+        }
+        SiteSettingsInterface siteSettings = SiteSettingsInterface.getInterface(activity, siteModel, null);
+        // Site Settings are implemented on .com/Jetpack sites only
+        if (siteSettings != null && siteSettings.init(false).getOptimizedImage()) {
+            String optimizedPath = ImageUtils.optimizeImage(activity, path, OPTIMIZE_IMAGE_MAX_WIDTH, OPTIMIZE_IMAGE_ENCODER_QUALITY);
+            if (optimizedPath == null) {
+                AppLog.e(AppLog.T.EDITOR, "Optimized picture was null!");
+                AnalyticsTracker.track(AnalyticsTracker.Stat.MEDIA_PHOTO_OPTIMIZE_ERROR);
+            } else {
+                AnalyticsTracker.track(AnalyticsTracker.Stat.MEDIA_PHOTO_OPTIMIZED);
+                return Uri.parse(optimizedPath);
             }
         }
         return null;
