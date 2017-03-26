8aa91d1dffd69712ebbc7e10d46c3690c3fd1529
Mario Zorz <mariozorz@gmail.com>
2017-03-26 16:02:34 -0300

    added TODO comment for adding more Error Types

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/MediaRestClient.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/MediaRestClient.java
@@ -336,6 +336,8 @@ public class MediaRestClient extends BaseWPComRestClient implements ProgressList
             public void onFailure(Call call, IOException e) {
                 AppLog.w(T.MEDIA, "media upload failed: " + e);
                 if (!media.isUploadCancelled()) {
+                    // TODO it would be great to raise some more fine grained errors here, for
+                    // instance timeouts should be raised
                     MediaStore.MediaError error = new MediaError(MediaErrorType.GENERIC_ERROR);
                     notifyMediaUploaded(media, error);
                 }
