100d78a215cb24523ee054bf13602611c481ee6d
Tony Rankin <tonyr@tonyrankin.com>
2017-03-28 12:57:06 -0700

    Use more generic class name for response class

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/account/AccountBoolResponse.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/account/AccountBoolResponse.java
@@ -2,7 +2,7 @@ package org.wordpress.android.fluxc.network.rest.wpcom.account;
 
 import org.wordpress.android.fluxc.network.Response;
 
-public class NewAccountResponse implements Response {
+public class AccountBoolResponse implements Response {
     public boolean success;
     public String error;
     public String message;
