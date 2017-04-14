912101b0b424973c6a97f666a97f3a63a327cc56
Maxime Biais <maxime.biais@gmail.com>
2017-04-14 12:39:42 +0200

    New SiteStore action to get domain name suggestions

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/DomainSuggestionResponse.java
@@ -0,0 +1,13 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.site;
+
+import org.wordpress.android.fluxc.network.Response;
+
+public class DomainSuggestionResponse implements Response {
+    public String cost;
+    public String domain_name;
+    public boolean is_free;
+
+    public int product_id;
+    public String product_slug;
+    public float relevance;
+}
