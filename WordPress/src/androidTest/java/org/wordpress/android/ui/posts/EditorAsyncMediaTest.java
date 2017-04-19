90764678240fec5cc375e7d79896eca2eae6972e
Alex Forcier <alex.forcier@automattic.com>
2017-04-19 12:49:34 -0400

    Add test cases for uploading images on API<19

--- WordPress/src/androidTest/java/org/wordpress/android/ui/posts/EditorAsyncMediaTest.java
+++ WordPress/src/androidTest/java/org/wordpress/android/ui/posts/EditorAsyncMediaTest.java
@@ -88,6 +88,76 @@ public class EditorAsyncMediaTest extends AndroidTestCase {
         assertEquals("Some text\n" + expectedTag + "\nMore text" + expectedSecondTag, modifiedContent);
     }
 
+    public void testHybridEditorUploadingImageSwapOldApis() {
+        MediaFile mediaFile = generateSampleUploadedMediaFile1();
+
+        String expectedTag = String.format(Locale.US, "<a href=\"%s\"><img src=\"%s\" alt=\"\" class=\"wp-image-%s " +
+                "alignnone size-full\" width=\"%d\" height=\"%d\"></a>", mediaFile.getFileURL(), mediaFile.getFileURL(),
+                mediaFile.getMediaId(), mediaFile.getWidth(), mediaFile.getHeight());
+
+        // Pre-API19, we use nested spans for an 'Uploading...' overlay instead of a progress element
+        String uploadingImageHtmlOldApis = "<span id=\"img_container_54\" class=\"img_container compat\"><span " +
+                "class=\"upload-overlay\">Uploading…</span><span class=\"upload-overlay-bg\"></span><img " +
+                "data-wpid=\"54\" src=\"/storage/emulated/0/Android/data/image.jpg\" alt=\"\" class=\"uploading\">" +
+                "</span>";
+
+        // --- Single image post with no other content ---
+        String originalContent = "Some text\n" + uploadingImageHtmlOldApis + "\nMore text";
+        String modifiedContent = EditorFragment.replaceMediaFileWithUrl(originalContent, mediaFile);
+
+        assertEquals("Some text\n" + expectedTag + "\nMore text", modifiedContent);
+
+        // --- Single image with surrounding text ---
+        originalContent = "Some text\n" + uploadingImageHtmlOldApis + "\nMore text";
+        modifiedContent = EditorFragment.replaceMediaFileWithUrl(originalContent, mediaFile);
+
+        assertEquals("Some text\n" + expectedTag + "\nMore text", modifiedContent);
+    }
+
+    public void testHybridEditorUploadingImageSwapOldApisMultiple() {
+        MediaFile mediaFile = generateSampleUploadedMediaFile1();
+
+        String expectedTag = String.format(Locale.US, "<a href=\"%s\"><img src=\"%s\" alt=\"\" class=\"wp-image-%s " +
+                "alignnone size-full\" width=\"%d\" height=\"%d\"></a>", mediaFile.getFileURL(), mediaFile.getFileURL(),
+                mediaFile.getMediaId(), mediaFile.getWidth(), mediaFile.getHeight());
+
+        // Pre-API19, we use nested spans for an 'Uploading...' overlay instead of a progress element
+        String uploadingImageHtmlOldApis = "<span id=\"img_container_54\" class=\"img_container compat\"><span " +
+                "class=\"upload-overlay\">Uploading…</span><span class=\"upload-overlay-bg\"></span><img " +
+                "data-wpid=\"54\" src=\"/storage/emulated/0/Android/data/image.jpg\" alt=\"\" class=\"uploading\">" +
+                "</span>";
+
+        // --- Post with two uploading images ---
+        // -- Replace first image --
+        String secondUploadingImageHtml = uploadingImageHtmlOldApis.replaceAll("54", "65")
+                .replaceAll("image.jpg", "image2.jpg");
+        String originalContent = "Some text\n" + uploadingImageHtmlOldApis + "\nMore text" + secondUploadingImageHtml;
+        String modifiedContent = EditorFragment.replaceMediaFileWithUrl(originalContent, mediaFile);
+
+        assertEquals("Some text\n" + expectedTag + "\nMore text" + secondUploadingImageHtml, modifiedContent);
+
+        // -- Replace second image --
+        MediaFile mediaFile2 = generateSampleUploadedMediaFile2();
+        modifiedContent = EditorFragment.replaceMediaFileWithUrl(modifiedContent, mediaFile2);
+
+        String expectedSecondTag = String.format(Locale.US, "<a href=\"%s\"><img src=\"%s\" alt=\"\"" +
+                " class=\"wp-image-%s alignnone size-full\" width=\"%d\" height=\"%d\"></a>", mediaFile2.getFileURL(),
+                mediaFile2.getFileURL(), mediaFile2.getMediaId(), mediaFile2.getWidth(), mediaFile2.getHeight());
+        assertEquals("Some text\n" + expectedTag + "\nMore text" + expectedSecondTag, modifiedContent);
+
+        // --- Post with two uploading images, update in reverse order ---
+        // -- Replace second image --
+        originalContent = "Some text\n" + uploadingImageHtmlOldApis + "\nMore text" + secondUploadingImageHtml;
+        modifiedContent = EditorFragment.replaceMediaFileWithUrl(originalContent, mediaFile2);
+
+        assertEquals("Some text\n" + uploadingImageHtmlOldApis + "\nMore text" + expectedSecondTag, modifiedContent);
+
+        // -- Replace first image --
+        modifiedContent = EditorFragment.replaceMediaFileWithUrl(modifiedContent, mediaFile);
+
+        assertEquals("Some text\n" + expectedTag + "\nMore text" + expectedSecondTag, modifiedContent);
+    }
+
     private static MediaFile generateSampleUploadedMediaFile1() {
         MediaFile mediaFile = new MediaFile();
         mediaFile.setId(54);
