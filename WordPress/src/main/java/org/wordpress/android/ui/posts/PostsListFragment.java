cd233beea5241e43851dbabf25a46477de17b84b
Oguz Kocer <oguz.kocer@automattic.com>
2017-04-24 15:29:16 +0300

    Adds missing case for saving a scheduled post

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostsListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostsListFragment.java
@@ -469,6 +469,7 @@ public class PostsListFragment extends Fragment
                 ActivityLauncher.editPostOrPageForResult(getActivity(), mSite, post);
                 break;
             case PostListButton.BUTTON_SUBMIT:
+            case PostListButton.BUTTON_SAVE:
             case PostListButton.BUTTON_PUBLISH:
                 publishPost(post);
                 break;
