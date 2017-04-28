af48b5ef5c966edeb2304d220f23b0c38b988ec2
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-04-28 13:23:15 +0300

    Add basic setup for Login Roboloectric tests

---
+++ WordPress/src/test/java/org/wordpress/android/LoginLogicTest.java
@@ -0,0 +1,27 @@
+package org.wordpress.android;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Testing Login related logic
+ */
+@RunWith(RobolectricTestRunner.class)
+public class LoginLogicTest {
+
+    /**
+     * Initialize variables.
+     */
+    @Before
+    public void init() {
+        // nothing to be setup yet
+    }
+
+    @Test
+    public void prologueTest() {
+        Assert.assertTrue(true);
+    }
+}
