9788e75bc563f9bb3e0850dd31a481e1ccd24743
Nick Bradbury <nick.bradbury@gmail.com>
2017-04-28 17:19:21 -0400

    Replace column selection with seekbar

--- WordPress/src/main/java/org/wordpress/android/ui/media/GallerySettingsDialog.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/GallerySettingsDialog.java
@@ -3,42 +3,35 @@ package org.wordpress.android.ui.media;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import org.wordpress.android.R;
-import org.wordpress.android.ui.ExpandableHeightGridView;
-
-import java.util.ArrayList;
 
 /**
  * The fragment containing the settings for the media gallery
  */
 public class GallerySettingsDialog extends DialogFragment {
-    private static final int DEFAULT_THUMBNAIL_COLUMN_COUNT = 3;
+    private static final int DEFAULT_COLUMN_COUNT = 3;
+    private static final int MAX_COLUMN_COUNT = 9;
 
     private static final String STATE_NUM_COLUMNS = "STATE_NUM_COLUMNS";
     private static final String STATE_GALLERY_TYPE_ORD = "GALLERY_TYPE_ORD";
 
-    private View mNumColumnsContainer;
+    private ViewGroup mNumColumnsContainer;
     private RadioGroup mGalleryRadioGroup;
+    private SeekBar mNumColumnsSeekBar;
 
-    private GalleryType mType;
+    private GalleryType mGalleryType;
     private int mNumColumns;
-
-    private boolean mAllowCheckChanged;
-
-    private CustomGridAdapter mGridAdapter;
 
     private enum GalleryType {
         DEFAULT,
@@ -58,9 +51,27 @@ public class GallerySettingsDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.media_gallery_settings_dialog, container, false);
 
-        mNumColumnsContainer = view.findViewById(R.id.media_gallery_settings_num_columns_container);
-        mGalleryRadioGroup = (RadioGroup) view.findViewById(R.id.radio_group_gallery_type);
+        mNumColumnsContainer = (ViewGroup) view.findViewById(R.id.num_columns_container);
+        mNumColumnsSeekBar = (SeekBar) view.findViewById(R.id.seekbar_num_columns);
+        mNumColumnsSeekBar.setMax(MAX_COLUMN_COUNT - 1);
+        mNumColumnsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    setNumColumns(progress + 1);
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // noop
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // noop
+            }
+        });
 
+        mGalleryRadioGroup = (RadioGroup) view.findViewById(R.id.radio_group_gallery_type);
         mGalleryRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
@@ -82,19 +93,9 @@ public class GallerySettingsDialog extends DialogFragment {
                         galleryType = GalleryType.DEFAULT;
                         break;
                 }
-                setType(galleryType);
+                setGalleryType(galleryType);
             }
         });
-
-        ExpandableHeightGridView numColumnsGrid = (ExpandableHeightGridView) view.findViewById(R.id.media_gallery_num_columns_grid);
-        numColumnsGrid.setExpanded(true);
-        ArrayList<String> list = new ArrayList<String>(9);
-        for (int i = 1; i <= 9; i++) {
-            list.add(i + "");
-        }
-
-        mGridAdapter = new CustomGridAdapter(mNumColumns);
-        numColumnsGrid.setAdapter(mGridAdapter);
 
         return view;
     }
@@ -103,103 +104,35 @@ public class GallerySettingsDialog extends DialogFragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(STATE_NUM_COLUMNS, mNumColumns);
-        outState.putInt(STATE_GALLERY_TYPE_ORD, mType.ordinal());
+        outState.putInt(STATE_GALLERY_TYPE_ORD, mGalleryType.ordinal());
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         if (savedInstanceState != null) {
-            mNumColumns = savedInstanceState.getInt(STATE_NUM_COLUMNS);
+            setNumColumns(savedInstanceState.getInt(STATE_NUM_COLUMNS));
             int galleryTypeOrdinal = savedInstanceState.getInt(STATE_GALLERY_TYPE_ORD);
-            mType = GalleryType.values()[galleryTypeOrdinal];
+            setGalleryType(GalleryType.values()[galleryTypeOrdinal]);
         } else {
-            mAllowCheckChanged = true;
-            mType = GalleryType.DEFAULT;
-            mNumColumns = DEFAULT_THUMBNAIL_COLUMN_COUNT;
-        }
-        setType(mType);
-    }
-
-    private class CustomGridAdapter extends BaseAdapter implements OnCheckedChangeListener {
-        private boolean mAllowCheckChanged;
-        private final SparseBooleanArray mCheckedPositions;
-
-        public CustomGridAdapter(int selection) {
-            mAllowCheckChanged = true;
-            mCheckedPositions = new SparseBooleanArray(9);
-            setSelection(selection);
-        }
-
-        // when a number of columns is checked, the numbers less than
-        // the one chose are also set to checked on the ui.
-        // e.g. when 3 is checked, 1 and 2 are as well.
-        private void setSelection(int selection) {
-            for (int i = 0; i < 9; i++) {
-                if (i + 1 <= selection)
-                    mCheckedPositions.put(i, true);
-                else
-                    mCheckedPositions.put(i, false);
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return 9;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            CheckBox checkbox = (CheckBox) inflater.inflate(R.layout.media_gallery_column_checkbox, parent, false);
-            checkbox.setChecked(mCheckedPositions.get(position));
-            checkbox.setTag(position);
-            checkbox.setText(String.valueOf(position + 1));
-            checkbox.setOnCheckedChangeListener(this);
-
-            return checkbox;
-        }
-
-        @Override
-        public void onCheckedChanged(CompoundButton button, boolean checked) {
-            if (mAllowCheckChanged) {
-                mAllowCheckChanged = false;
-
-                int position = (Integer) button.getTag();
-                mNumColumns = position + 1;
-
-                int count = mCheckedPositions.size();
-                for (int i = 0; i < count; i++) {
-                    if (i <= position)
-                        mCheckedPositions.put(i, true);
-                    else
-                        mCheckedPositions.put(i, false);
-                }
-                notifyDataSetChanged();
-                mAllowCheckChanged = true;
-            }
+            setGalleryType(GalleryType.DEFAULT);
+            setNumColumns(DEFAULT_COLUMN_COUNT);
         }
     }
 
-    public void setType(GalleryType galleryType) {
-        mType = galleryType;
+    private void setGalleryType(@NonNull GalleryType galleryType) {
+        if (galleryType == mGalleryType) {
+            return;
+        }
 
-        boolean showNumColumns = (mType == GalleryType.DEFAULT);
+        mGalleryType = galleryType;
+
+        boolean showNumColumns = (galleryType == GalleryType.DEFAULT);
         mNumColumnsContainer.setVisibility(showNumColumns ? View.VISIBLE : View.GONE);
 
         @IdRes int resId;
         @DrawableRes int drawableId;
-        switch (mType) {
+        switch (galleryType) {
             case CIRCLES:
                 resId = R.id.radio_circles;
                 drawableId = R.drawable.gallery_icon_circles;
@@ -233,8 +166,11 @@ public class GallerySettingsDialog extends DialogFragment {
 
     public void setNumColumns(int numColumns) {
         mNumColumns = numColumns;
-        mGridAdapter.setSelection(numColumns);
-        mGridAdapter.notifyDataSetChanged();
+        if (isAdded()) {
+            mNumColumnsSeekBar.setProgress(numColumns - 1);
+            TextView textValue = (TextView) getView().findViewById(R.id.text_num_columns_value);
+            textValue.setText(Integer.toString(numColumns));
+        }
     }
 
     public int getNumColumns() {
