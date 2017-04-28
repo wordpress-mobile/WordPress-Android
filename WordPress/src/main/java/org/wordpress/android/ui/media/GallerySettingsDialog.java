f50bd99e4472d9b02a4891047f580044975a5d49
Nick Bradbury <nick.bradbury@gmail.com>
2017-04-28 16:21:05 -0400

    Added radio buttons (incomplete)

--- WordPress/src/main/java/org/wordpress/android/ui/media/GallerySettingsDialog.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/GallerySettingsDialog.java
@@ -1,7 +1,7 @@
 package org.wordpress.android.ui.media;
 
-import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.util.SparseBooleanArray;
@@ -12,8 +12,9 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.ScrollView;
-import android.widget.TextView;
 
 import org.wordpress.android.R;
 import org.wordpress.android.ui.ExpandableHeightGridView;
@@ -23,29 +24,20 @@ import java.util.ArrayList;
 /**
  * The fragment containing the settings for the media gallery
  */
-public class GallerySettingsDialog extends DialogFragment implements OnCheckedChangeListener {
+public class GallerySettingsDialog extends DialogFragment {
     private static final int DEFAULT_THUMBNAIL_COLUMN_COUNT = 3;
 
     private static final String STATE_NUM_COLUMNS = "STATE_NUM_COLUMNS";
     private static final String STATE_GALLERY_TYPE_ORD = "GALLERY_TYPE_ORD";
 
-    private CheckBox mThumbnailCheckbox;
-    private CheckBox mSquaresCheckbox;
-    private CheckBox mTiledCheckbox;
-    private CheckBox mCirclesCheckbox;
-    private CheckBox mSlideshowCheckbox;
+    private View mNumColumnsContainer;
+    private ScrollView mScrollView;
+    private RadioGroup mGalleryRadioGroup;
 
     private GalleryType mType;
     private int mNumColumns;
 
-    private View mNumColumnsContainer;
-    private View mHeader;
-
     private boolean mAllowCheckChanged;
-
-    private TextView mTitleView;
-
-    private ScrollView mScrollView;
 
     private CustomGridAdapter mGridAdapter;
 
@@ -92,13 +84,10 @@ public class GallerySettingsDialog extends DialogFragment implements OnCheckedCh
         mType = GalleryType.DEFAULT;
         mNumColumns = DEFAULT_THUMBNAIL_COLUMN_COUNT;
 
-        restoreState(savedInstanceState);
+        View view = inflater.inflate(R.layout.media_gallery_settings_dialog, container, false);
 
-        View view = inflater.inflate(R.layout.media_gallery_settings_fragment, container, false);
-
-        mHeader = view.findViewById(R.id.media_gallery_settings_header);
         mScrollView = (ScrollView) view.findViewById(R.id.media_gallery_settings_content_container);
-        mTitleView = (TextView) view.findViewById(R.id.media_gallery_settings_title);
+        mGalleryRadioGroup = (RadioGroup) view.findViewById(R.id.radio_group_gallery_type);
 
         mNumColumnsContainer = view.findViewById(R.id.media_gallery_settings_num_columns_container);
         int visible = (mType == GalleryType.DEFAULT) ? View.VISIBLE : View.GONE;
@@ -114,22 +103,36 @@ public class GallerySettingsDialog extends DialogFragment implements OnCheckedCh
         mGridAdapter = new CustomGridAdapter(mNumColumns);
         numColumnsGrid.setAdapter(mGridAdapter);
 
-        mThumbnailCheckbox = (CheckBox) view.findViewById(R.id.media_gallery_type_thumbnail_grid);
-        mTiledCheckbox = (CheckBox) view.findViewById(R.id.media_gallery_type_tiled);
-        mSquaresCheckbox = (CheckBox) view.findViewById(R.id.media_gallery_type_squares);
-        mCirclesCheckbox = (CheckBox) view.findViewById(R.id.media_gallery_type_circles);
-        mSlideshowCheckbox = (CheckBox) view.findViewById(R.id.media_gallery_type_slideshow);
-
+        restoreState(savedInstanceState);
         setType(mType.getTag());
 
-        mThumbnailCheckbox.setOnCheckedChangeListener(this);
-        mTiledCheckbox.setOnCheckedChangeListener(this);
-        mSquaresCheckbox.setOnCheckedChangeListener(this);
-        mCirclesCheckbox.setOnCheckedChangeListener(this);
-
-        mSlideshowCheckbox.setOnCheckedChangeListener(this);
-
         return view;
+    }
+
+    private RadioButton getRadioButtonForType(@NonNull GalleryType type) {
+        if (!isAdded()) {
+            return null;
+        }
+
+        @IdRes int resId;
+        switch (type) {
+            case CIRCLES:
+                resId = R.id.radio_circles;
+                break;
+            case SLIDESHOW:
+                resId = R.id.radio_slideshow;
+                break;
+            case SQUARES:
+                resId = R.id.radio_squares;
+                break;
+            case TILED:
+                resId = R.id.radio_tiled;
+                break;
+            default:
+                resId = R.id.radio_thumbnail_grid;
+                break;
+        }
+        return (RadioButton) mGalleryRadioGroup.findViewById(resId);
     }
 
     private void restoreState(Bundle savedInstanceState) {
@@ -148,62 +151,6 @@ public class GallerySettingsDialog extends DialogFragment implements OnCheckedCh
         outState.putInt(STATE_GALLERY_TYPE_ORD, mType.ordinal());
     }
 
-    @Override
-    public void onCheckedChanged(CompoundButton button, boolean checked) {
-        if (!mAllowCheckChanged)
-            return;
-
-        // the checkboxes for types are mutually exclusive, so when one is set,
-        // the others must be unset. Disable the checkChange listener during this time,
-        // and re-enable it once done.
-        mAllowCheckChanged = false;
-
-        int numColumnsContainerVisible = View.GONE;
-        int i = button.getId();
-        if (i == R.id.media_gallery_type_thumbnail_grid) {
-            numColumnsContainerVisible = View.VISIBLE;
-
-            mType = GalleryType.DEFAULT;
-            mThumbnailCheckbox.setChecked(true);
-            mSquaresCheckbox.setChecked(false);
-            mTiledCheckbox.setChecked(false);
-            mCirclesCheckbox.setChecked(false);
-            mSlideshowCheckbox.setChecked(false);
-        } else if (i == R.id.media_gallery_type_tiled) {
-            mType = GalleryType.TILED;
-            mThumbnailCheckbox.setChecked(false);
-            mTiledCheckbox.setChecked(true);
-            mSquaresCheckbox.setChecked(false);
-            mCirclesCheckbox.setChecked(false);
-            mSlideshowCheckbox.setChecked(false);
-        } else if (i == R.id.media_gallery_type_squares) {
-            mType = GalleryType.SQUARES;
-            mThumbnailCheckbox.setChecked(false);
-            mTiledCheckbox.setChecked(false);
-            mSquaresCheckbox.setChecked(true);
-            mCirclesCheckbox.setChecked(false);
-            mSlideshowCheckbox.setChecked(false);
-        } else if (i == R.id.media_gallery_type_circles) {
-            mType = GalleryType.CIRCLES;
-            mThumbnailCheckbox.setChecked(false);
-            mSquaresCheckbox.setChecked(false);
-            mTiledCheckbox.setChecked(false);
-            mCirclesCheckbox.setChecked(true);
-            mSlideshowCheckbox.setChecked(false);
-        } else if (i == R.id.media_gallery_type_slideshow) {
-            mType = GalleryType.SLIDESHOW;
-            mThumbnailCheckbox.setChecked(false);
-            mSquaresCheckbox.setChecked(false);
-            mTiledCheckbox.setChecked(false);
-            mCirclesCheckbox.setChecked(false);
-            mSlideshowCheckbox.setChecked(true);
-        }
-
-        mNumColumnsContainer.setVisibility(numColumnsContainerVisible);
-
-        mAllowCheckChanged = true;
-    }
-
     private class CustomGridAdapter extends BaseAdapter implements OnCheckedChangeListener {
         private boolean mAllowCheckChanged;
         private final SparseBooleanArray mCheckedPositions;
@@ -218,7 +165,7 @@ public class GallerySettingsDialog extends DialogFragment implements OnCheckedCh
         // the one chose are also set to checked on the ui.
         // e.g. when 3 is checked, 1 and 2 are as well.
         private void setSelection(int selection) {
-            for (int i = 0; i < 9; i++){
+            for (int i = 0; i < 9; i++) {
                 if (i + 1 <= selection)
                     mCheckedPositions.put(i, true);
                 else
@@ -272,40 +219,15 @@ public class GallerySettingsDialog extends DialogFragment implements OnCheckedCh
                 mAllowCheckChanged = true;
             }
         }
-
-    }
-
-    public View getDragView() {
-        return mHeader;
-    }
-
-    public void onPanelExpanded() {
-        mTitleView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_chevron_down_grey_100_35dp, 0);
-        mScrollView.scrollTo(0, 0);
-    }
-
-    public void onPanelCollapsed() {
-        mTitleView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_chevron_up_grey_100_35dp, 0);
     }
 
     public void setType(String type) {
         mType = GalleryType.getTypeFromTag(type);
-        switch (mType) {
-            case CIRCLES:
-                mCirclesCheckbox.setChecked(true);
-                break;
-            case DEFAULT:
-                mThumbnailCheckbox.setChecked(true);
-                break;
-            case SLIDESHOW:
-                mSlideshowCheckbox.setChecked(true);
-                break;
-            case SQUARES:
-                mSquaresCheckbox.setChecked(true);
-                break;
-            case TILED:
-                mTiledCheckbox.setChecked(true);
-                break;
+        RadioButton radio = getRadioButtonForType(mType);
+        if (radio != null) {
+            radio.setChecked(true);
+            boolean showNumColumns = (mType == GalleryType.DEFAULT);
+            mNumColumnsContainer.setVisibility(showNumColumns ? View.VISIBLE : View.GONE);
         }
     }
 
