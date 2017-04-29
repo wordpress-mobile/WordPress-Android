e0ee641d5931220eac292f36b559384d60fa86aa
Alex Forcier <alex.forcier@automattic.com>
2017-04-28 20:14:01 -0400

    Add unit test for safeGetMapValue with a Date entry

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/utils/XMLRPCUtilsTest.java
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/utils/XMLRPCUtilsTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.wordpress.android.fluxc.network.xmlrpc.XMLRPCUtils;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -88,8 +89,22 @@ public class XMLRPCUtilsTest {
         assertThat(XMLRPCUtils.safeGetMapValue(getTestMapForValue("false"), 42.42), equalTo(42.42));
     }
 
+    @Test
+    public void testGetValueDate() {
+        Date theDate = new Date();
+        assertThat(XMLRPCUtils.safeGetMapValue(getTestMapForValue(theDate), new Date(4244)), equalTo(theDate));
+    }
+
     private static Map<String, String> getTestMapForValue(String value) {
         Map<String, String> map = new HashMap<>();
+        map.put("key", "test-key");
+        map.put("id", "42");
+        map.put("value", value);
+        return map;
+    }
+
+    private static Map<String, Object> getTestMapForValue(Object value) {
+        Map<String, Object> map = new HashMap<>();
         map.put("key", "test-key");
         map.put("id", "42");
         map.put("value", value);
