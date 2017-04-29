361c5a2525a851f7b7af8c310204494e3d84ea6c
Alex Forcier <alex.forcier@automattic.com>
2017-04-28 20:18:42 -0400

    Add unit tests for safeGetMapValue with non-standard types

--- fluxc/src/test/java/org/wordpress/android/fluxc/utils/XMLRPCUtilsTest.java
+++ fluxc/src/test/java/org/wordpress/android/fluxc/utils/XMLRPCUtilsTest.java
@@ -5,9 +5,11 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.wordpress.android.fluxc.network.xmlrpc.XMLRPCUtils;
 
+import java.sql.Time;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -93,6 +95,23 @@ public class XMLRPCUtilsTest {
     public void testGetValueDate() {
         Date theDate = new Date();
         assertThat(XMLRPCUtils.safeGetMapValue(getTestMapForValue(theDate), new Date(4244)), equalTo(theDate));
+    }
+
+    @Test(expected = Exception.class)
+    public void testGetValueInvalidType() {
+        // Something bad should happen if we try this - Random isn't a possible type the XML-RPC deserializer would
+        // parse into, so it's guaranteed to always return the default value, without us ever knowing that we're
+        // asking safeGetMapValue() for an impossible type
+        Random thing = new Random();
+        assertThat(XMLRPCUtils.safeGetMapValue(getTestMapForValue("text"), thing), equalTo(thing));
+    }
+
+    @Test
+    public void testGetValueInvalidTypeSubclass() {
+        // If we pass a Date subclass as the default value, we might expect it to work as a match for a Date entry
+        // But it doesn't - we'll always receive the default value
+        Date theDate = new Date();
+        assertThat(XMLRPCUtils.safeGetMapValue(getTestMapForValue(theDate), new Time(4244)), equalTo(theDate));
     }
 
     private static Map<String, String> getTestMapForValue(String value) {
