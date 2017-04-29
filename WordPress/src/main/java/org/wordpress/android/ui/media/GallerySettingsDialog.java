923519f6eda18fd657d694258fb0818578da7e76
Nick Bradbury <nick.bradbury@gmail.com>
2017-04-29 10:37:05 -0400

    Added insert type, fade out to View.INVISIBLE

--- WordPress/src/main/java/org/wordpress/android/ui/media/GallerySettingsDialog.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/GallerySettingsDialog.java
@@ -21,26 +21,35 @@ import org.wordpress.android.util.AniUtils;
  * The fragment containing the settings for the media gallery
  */
 public class GallerySettingsDialog extends AppCompatDialogFragment {
-    private static final int DEFAULT_COLUMN_COUNT = 3;
-    private static final int MAX_COLUMN_COUNT = 9;
 
-    private static final String STATE_NUM_COLUMNS = "STATE_NUM_COLUMNS";
-    private static final String STATE_GALLERY_TYPE_ORD = "GALLERY_TYPE_ORD";
+    public enum InsertType {
+        INDIVIDUALLY,
+        GALLERY
+    }
 
-    private ViewGroup mNumColumnsContainer;
-    private RadioGroup mGalleryRadioGroup;
-    private SeekBar mNumColumnsSeekBar;
-
-    private GalleryType mGalleryType;
-    private int mNumColumns;
-
-    private enum GalleryType {
+    public enum GalleryType {
         DEFAULT,
         TILED,
         SQUARES,
         CIRCLES,
         SLIDESHOW
     }
+
+    private static final int DEFAULT_COLUMN_COUNT = 3;
+    private static final int MAX_COLUMN_COUNT = 9;
+
+    private static final String STATE_INSERT_TYPE = "STATE_INSERT_TYPE";
+    private static final String STATE_NUM_COLUMNS = "STATE_NUM_COLUMNS";
+    private static final String STATE_GALLERY_TYPE_ORD = "GALLERY_TYPE_ORD";
+
+    private RadioGroup mInsertRadioGroup;
+    private RadioGroup mGalleryRadioGroup;
+    private ViewGroup mNumColumnsContainer;
+    private SeekBar mNumColumnsSeekBar;
+
+    private GalleryType mGalleryType;
+    private InsertType mInsertType;
+    private int mNumColumns;
 
     public static GallerySettingsDialog newInstance() {
         GallerySettingsDialog dialog = new GallerySettingsDialog();
@@ -52,23 +61,15 @@ public class GallerySettingsDialog extends AppCompatDialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.media_gallery_settings_dialog, container, false);
 
-        mNumColumnsContainer = (ViewGroup) view.findViewById(R.id.num_columns_container);
-        mNumColumnsSeekBar = (SeekBar) mNumColumnsContainer.findViewById(R.id.seekbar_num_columns);
-        mNumColumnsSeekBar.setMax(MAX_COLUMN_COUNT - 1);
-        mNumColumnsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        mInsertRadioGroup = (RadioGroup) view.findViewById(R.id.radio_group_insert_type);
+        mInsertRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser) {
-                    setNumColumns(progress, true);
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if (checkedId == R.id.radio_insert_as_gallery) {
+                    setInsertType(InsertType.GALLERY);
+                } else {
+                    setInsertType(InsertType.INDIVIDUALLY);
                 }
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // noop
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // noop
             }
         });
 
@@ -98,26 +99,73 @@ public class GallerySettingsDialog extends AppCompatDialogFragment {
             }
         });
 
+        mNumColumnsContainer = (ViewGroup) view.findViewById(R.id.num_columns_container);
+        mNumColumnsSeekBar = (SeekBar) mNumColumnsContainer.findViewById(R.id.seekbar_num_columns);
+        mNumColumnsSeekBar.setMax(MAX_COLUMN_COUNT - 1);
+        mNumColumnsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    setNumColumns(progress, true);
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // noop
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // noop
+            }
+        });
+
         return view;
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt(STATE_NUM_COLUMNS, mNumColumns);
+        outState.putInt(STATE_INSERT_TYPE, mInsertType.ordinal());
         outState.putInt(STATE_GALLERY_TYPE_ORD, mGalleryType.ordinal());
+        outState.putInt(STATE_NUM_COLUMNS, mNumColumns);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         if (savedInstanceState != null) {
+            int insertTypeOrdinal = savedInstanceState.getInt(STATE_INSERT_TYPE);
+            setInsertType(InsertType.values()[insertTypeOrdinal]);
             int galleryTypeOrdinal = savedInstanceState.getInt(STATE_GALLERY_TYPE_ORD);
             setGalleryType(GalleryType.values()[galleryTypeOrdinal]);
             setNumColumns(savedInstanceState.getInt(STATE_NUM_COLUMNS), false);
         } else {
+            setInsertType(InsertType.INDIVIDUALLY);
             setGalleryType(GalleryType.DEFAULT);
             setNumColumns(DEFAULT_COLUMN_COUNT, false);
+        }
+    }
+
+    private void setInsertType(@NonNull InsertType insertType) {
+        if (insertType == mInsertType) {
+            return;
+        }
+
+        mInsertType = insertType;
+
+        @IdRes int radioId = insertType == InsertType.GALLERY
+                ? R.id.radio_insert_as_gallery : R.id.radio_insert_individually;
+        RadioButton radio = (RadioButton) getView().findViewById(radioId);
+        if (!radio.isChecked()) {
+            radio.setChecked(true);
+        }
+
+        View container = getView().findViewById(R.id.container_gallery_settings);
+        boolean showGalleryTypes = insertType == InsertType.GALLERY;
+        if (showGalleryTypes && container.getVisibility() != View.VISIBLE) {
+            AniUtils.fadeIn(container, AniUtils.Duration.SHORT);
+        } else if (!showGalleryTypes && container.getVisibility() == View.VISIBLE) {
+            AniUtils.fadeOut(container, AniUtils.Duration.SHORT, View.INVISIBLE);
         }
     }
 
@@ -133,7 +181,7 @@ public class GallerySettingsDialog extends AppCompatDialogFragment {
         if (showNumColumns && mNumColumnsContainer.getVisibility() != View.VISIBLE) {
             AniUtils.fadeIn(mNumColumnsContainer, AniUtils.Duration.SHORT);
         } else if (!showNumColumns && mNumColumnsContainer.getVisibility() == View.VISIBLE) {
-            AniUtils.fadeOut(mNumColumnsContainer, AniUtils.Duration.SHORT);
+            AniUtils.fadeOut(mNumColumnsContainer, AniUtils.Duration.SHORT, View.INVISIBLE);
         }
 
         @IdRes final int resId;
