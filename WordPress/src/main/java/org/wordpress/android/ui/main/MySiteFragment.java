e3baa0390ff4b7aa59d68739f58d24c5ef308d8a
Will Kwon <will@kwon.co>
2017-05-04 13:54:26 -0700

    Showing to .com sites only

--- WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
@@ -357,7 +357,7 @@ public class MySiteFragment extends Fragment
         mThemesContainer.setVisibility(themesVisibility);
 
         // sharing is only exposed for wp.com blogs
-        int sharingVisibility = blog.isDotcomFlag() ? View.VISIBLE : View.GONE;
+        int sharingVisibility = getSelectedSite().isWPCom() ? View.VISIBLE : View.GONE;
         mSharingView.setVisibility(sharingVisibility);
 
         // show settings for all self-hosted to expose Delete Site
