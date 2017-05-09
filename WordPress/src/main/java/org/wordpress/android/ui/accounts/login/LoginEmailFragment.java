0db361debdf06eb8428f2c656d486f7de8018387
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-05-09 13:23:00 +0300

    No need for an email instance variable

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginEmailFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginEmailFragment.java
@@ -55,7 +55,6 @@ public class LoginEmailFragment extends AbstractFragment implements TextWatcher 
     protected EditText mEmailEditText;
 
     protected boolean mEmailAutoCorrected;
-    protected String mEmail;
 
     protected Button mNextButton;
     protected View mUsernamePasswordButton;
@@ -63,10 +62,6 @@ public class LoginEmailFragment extends AbstractFragment implements TextWatcher 
     protected @Inject Dispatcher mDispatcher;
 
     private LoginNav.InputEmail mLoginNavInputEmail;
-
-    public String getEmail() {
-        return mEmail;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -103,7 +98,7 @@ public class LoginEmailFragment extends AbstractFragment implements TextWatcher 
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((didPressNextKey(actionId, event) || didPressEnterKey(actionId, event))) {
-                    next();
+                    next(getEmail());
                     return true;
                 } else {
                     return false;
@@ -204,7 +199,7 @@ public class LoginEmailFragment extends AbstractFragment implements TextWatcher 
     }
 
     protected void onDoneAction() {
-        next();
+        next(getEmail());
     }
 
     private boolean checkNetworkConnectivity() {
@@ -222,31 +217,34 @@ public class LoginEmailFragment extends AbstractFragment implements TextWatcher 
         return true;
     }
 
-    protected void next() {
+    protected void next(String email) {
         if (!checkNetworkConnectivity()) {
             return;
         }
 
-        if (isUsernameEmail()) {
+        if (isValidEmail(email)) {
             startProgress(getActivity().getString(R.string.checking_email));
-            mDispatcher.dispatch(AccountActionBuilder.newIsAvailableEmailAction(mEmail));
+            mDispatcher.dispatch(AccountActionBuilder.newIsAvailableEmailAction(email));
         } else {
             showEmailError(R.string.email_invalid);
         }
     }
 
-    private boolean isUsernameEmail() {
-        mEmail = EditTextUtils.getText(mEmailEditText).trim();
-        Pattern emailRegExPattern = Patterns.EMAIL_ADDRESS;
-        Matcher matcher = emailRegExPattern.matcher(mEmail);
+    private String getEmail() {
+        return EditTextUtils.getText(mEmailEditText).trim();
+    }
 
-        return matcher.find() && mEmail.length() <= MAX_EMAIL_LENGTH;
+    private boolean isValidEmail(String email) {
+        Pattern emailRegExPattern = Patterns.EMAIL_ADDRESS;
+        Matcher matcher = emailRegExPattern.matcher(email);
+
+        return matcher.find() && email.length() <= MAX_EMAIL_LENGTH;
     }
 
     private final OnClickListener mNextClickListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
-            next();
+            next(getEmail());
         }
     };
 
@@ -336,7 +334,7 @@ public class LoginEmailFragment extends AbstractFragment implements TextWatcher 
             // TODO: Email address exists in WordPress.com so, goto magic link offer screen
             // Email address exists in WordPress.com
             if (mLoginNavInputEmail != null) {
-                mLoginNavInputEmail.gotEmail(mEmail);
+                mLoginNavInputEmail.gotEmail(event.value);
             }
         } else {
             showEmailError(R.string.email_not_registered_wpcom);
