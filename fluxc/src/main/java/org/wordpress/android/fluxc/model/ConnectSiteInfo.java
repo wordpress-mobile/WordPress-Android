85cc345e4a1e77cb3839b9df59265618417018da
aerych <aerychj@gmail.com>
2017-05-09 17:30:03 -0500

    Iterating on connect/site-info endpoint addition.

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/ConnectSiteInfo.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/ConnectSiteInfo.java
@@ -1,13 +1,14 @@
-package org.wordpress.android.fluxc.network.rest.wpcom.site;
+package org.wordpress.android.fluxc.model;
 
 import org.wordpress.android.fluxc.Payload;
-import org.wordpress.android.fluxc.network.Response;
 
-public class ConnectSiteInfoResponse implements Response {
+public class ConnectSiteInfo extends Payload {
+    public String url = "";
     public boolean exists = false;
     public boolean isWordPress = false;
     public boolean hasJetpack = false;
     public boolean isJetpackActive = false;
     public boolean isJetpackConnected = false;
     public boolean isWordPressDotCom = false;
+
 }
