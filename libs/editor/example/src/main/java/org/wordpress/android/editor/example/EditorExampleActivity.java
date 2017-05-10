fd1e412c98cb18325f6ddb6b87681197a8b28097
Mario Zorz <mariozorz@gmail.com>
2017-05-10 12:09:58 -0300

    fixed method signature in onMediaUploadProgress calls within EditorExampleActivity

--- libs/editor/example/src/main/java/org/wordpress/android/editor/example/EditorExampleActivity.java
+++ libs/editor/example/src/main/java/org/wordpress/android/editor/example/EditorExampleActivity.java
@@ -252,7 +252,7 @@ public class EditorExampleActivity extends AppCompatActivity implements EditorFr
                     while (count < 1.1) {
                         sleep(500);
 
-                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId, false, count);
+                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId, count);
 
                         count += 0.1;
                     }
@@ -284,7 +284,7 @@ public class EditorExampleActivity extends AppCompatActivity implements EditorFr
                     while (count < 0.6) {
                         sleep(500);
 
-                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId, false, count);
+                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId, count);
 
                         count += 0.1;
                     }
@@ -312,7 +312,7 @@ public class EditorExampleActivity extends AppCompatActivity implements EditorFr
                     while (count < 1.1) {
                         sleep(2000);
 
-                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId, false, count);
+                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId, count);
 
                         count += 0.1;
                     }
