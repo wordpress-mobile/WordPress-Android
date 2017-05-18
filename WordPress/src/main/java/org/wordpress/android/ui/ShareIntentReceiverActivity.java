eebb515831dd68687a5f058202e71190174ec8f5
Anthony Rankin <tonyr@automattic.com>
2017-05-18 16:26:27 -0700

    Fix condition logic

--- WordPress/src/main/java/org/wordpress/android/ui/ShareIntentReceiverActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/ShareIntentReceiverActivity.java
@@ -149,7 +149,7 @@ public class ShareIntentReceiverActivity extends AppCompatActivity implements On
 
     private void finishIfNoVisibleBlogs() {
         // If not logged in, then ask to log in, else inform the user to set at least one blog visible
-        if (FluxCUtils.isSignedInWPComOrHasWPOrgSite(mAccountStore, mSiteStore)) {
+        if (!FluxCUtils.isSignedInWPComOrHasWPOrgSite(mAccountStore, mSiteStore)) {
             ToastUtils.showToast(getBaseContext(), R.string.no_account, ToastUtils.Duration.LONG);
             startActivity(new Intent(this, SignInActivity.class));
             finish();
