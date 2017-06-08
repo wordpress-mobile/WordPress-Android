391e62a4f2eff60a4372fe67aed191c9b6bc524a
Tony Rankin <tonyr@tonyrankin.com>
2017-06-08 15:58:23 -0700

    Correctly annotate adapter getter with NonNull

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostsListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostsListFragment.java
@@ -8,7 +8,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -285,7 +284,7 @@ public class PostsListFragment extends Fragment
                 });
     }
 
-    private @Nullable PostsListAdapter getPostListAdapter() {
+    private @NonNull PostsListAdapter getPostListAdapter() {
         if (mPostsListAdapter == null) {
             mPostsListAdapter = new PostsListAdapter(getActivity(), mSite, mIsPage);
             mPostsListAdapter.setOnLoadMoreListener(this);
