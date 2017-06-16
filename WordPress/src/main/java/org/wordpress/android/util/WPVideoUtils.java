08d95afef5d32b3fa4555a55c6e9bbfe6b639083
Danilo Ercoli <ercoli@gmail.com>
2017-06-16 10:42:22 +0200

    Add video optimization step one

---
+++ WordPress/src/main/java/org/wordpress/android/util/WPVideoUtils.java
@@ -0,0 +1,95 @@
+package org.wordpress.android.util;
+
+import android.content.Context;
+import android.media.MediaCodecInfo;
+
+import org.m4m.AudioFormat;
+import org.m4m.MediaComposer;
+import org.m4m.MediaFileInfo;
+import org.m4m.Uri;
+import org.m4m.android.AndroidMediaObjectFactory;
+import org.m4m.android.AudioFormatAndroid;
+import org.m4m.android.VideoFormatAndroid;
+
+import java.io.IOException;
+
+public class WPVideoUtils {
+
+    // Default parameters for the video encoder
+    private static final String VIDEO_MIME_TYPE = "video/avc";    // H.264 Advanced Video Coding
+    private static final int FRAME_RATE = 30;               // 30fps
+    private static final int IFRAME_INTERVAL = 2;           // 2 seconds between I-frames
+
+    private static final int OUTPUT_WIDTH = 1280;
+    private static final int OUTPUT_HEIGHT = 720;
+    private static final int OUTPUT_BITRATE_KB = 2000;
+
+    // Default parameters for the audio encoder
+    private static final String AUDIO_MIME_TYPE = "audio/mp4a-latm";
+    private static final int AUDIO_OUTPUT_BIT_RATE = 96 * 1024;
+
+    public static org.m4m.MediaComposer getVideoOptimizationComposer(Context ctx, String inputFile, String outFile, org.m4m.IProgressListener listener) {
+        if (ctx == null || inputFile == null || listener == null) {
+            AppLog.e(AppLog.T.MEDIA, "Input parameters must be not null!!");
+            return null;
+        }
+
+        AndroidMediaObjectFactory factory = new AndroidMediaObjectFactory(ctx);
+
+        Uri m4mUri = new Uri(inputFile);
+        MediaFileInfo mediaFileInfo = new MediaFileInfo(factory);
+        try {
+            mediaFileInfo.setUri(m4mUri);
+        } catch (IOException e) {
+            AppLog.e(AppLog.T.EDITOR, "Cannot access the input file at " + inputFile, e);
+            return null;
+        }
+
+        AudioFormat audioFormat = (org.m4m.AudioFormat) mediaFileInfo.getAudioFormat();
+        boolean isAudioAvailable = audioFormat != null;
+
+        MediaComposer mediaComposer = new MediaComposer(factory, listener);
+        try{
+            mediaComposer.addSourceFile(inputFile);
+        } catch (IOException e) {
+            AppLog.e(AppLog.T.EDITOR, "Cannot access the input file at " + inputFile, e);
+            return null;
+        }
+
+        try {
+            mediaComposer.setTargetFile(outFile, mediaFileInfo.getRotation());
+        } catch (IOException e) {
+            AppLog.e(AppLog.T.EDITOR, "Cannot access/write the output file at " + outFile, e);
+            return null;
+        }
+
+        configureVideoEncoderWithDefaults(mediaComposer);
+
+        if (isAudioAvailable) {
+            configureAudioEncoder(mediaComposer, audioFormat);
+        }
+
+        return mediaComposer;
+    }
+
+    private static void configureVideoEncoderWithDefaults(MediaComposer mediaComposer) {
+        VideoFormatAndroid videoFormat = new VideoFormatAndroid(VIDEO_MIME_TYPE, OUTPUT_WIDTH, OUTPUT_HEIGHT);
+        videoFormat.setVideoBitRateInKBytes(OUTPUT_BITRATE_KB);
+        videoFormat.setVideoFrameRate(FRAME_RATE);
+        videoFormat.setVideoIFrameInterval(IFRAME_INTERVAL);
+        mediaComposer.setTargetVideoFormat(videoFormat);
+    }
+
+    private static void configureAudioEncoder(org.m4m.MediaComposer mediaComposer, AudioFormat audioFormat) {
+        /**
+         * TODO: Audio resampling is unsupported by current m4m release
+         * Output sample rate and channel count are the same as for input.
+         */
+        AudioFormatAndroid aFormat = new AudioFormatAndroid(AUDIO_MIME_TYPE, audioFormat.getAudioSampleRateInHz(), audioFormat.getAudioChannelCount());
+
+        aFormat.setAudioBitrateInBytes(AUDIO_OUTPUT_BIT_RATE);
+        aFormat.setAudioProfile(MediaCodecInfo.CodecProfileLevel.AACObjectLC);
+
+        mediaComposer.setTargetAudioFormat(aFormat);
+    }
+}
