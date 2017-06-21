aa5c412e7f693067dca6f0573ffcd1b971ecbb85
Danilo Ercoli <ercoli@gmail.com>
2017-06-21 16:11:21 +0200

    Add settings for video resizing

--- WordPress/src/main/java/org/wordpress/android/util/WPMediaUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/WPMediaUtils.java
@@ -15,6 +15,8 @@ public class WPMediaUtils {
     // Max picture size will be 3000px wide. That's the maximum resolution you can set in the current picker.
     public static final int OPTIMIZE_IMAGE_MAX_WIDTH = 3000;
     public static final int OPTIMIZE_IMAGE_ENCODER_QUALITY = 85;
+    public static final int OPTIMIZE_VIDEO_MAX_WIDTH = 1280;
+    public static final int OPTIMIZE_VIDEO_ENCODER_BITRATE_KB = 3000;
 
     public static Uri getOptimizedMedia(Activity activity, SiteModel siteModel, String path, boolean isVideo) {
         if (isVideo) {
@@ -56,7 +58,10 @@ public class WPMediaUtils {
         return null;
     }
 
-    public static boolean isVideoOptimizationAvailable() {
-        return BuildConfig.VIDEO_OPTIMIZATION_AVAILABLE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
+    public static boolean isVideoOptimizationAvailable(Activity activity, SiteModel siteModel) {
+        SiteSettingsInterface siteSettings = SiteSettingsInterface.getInterface(activity, siteModel, null);
+        return BuildConfig.VIDEO_OPTIMIZATION_AVAILABLE
+                && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2
+                && siteSettings != null && siteSettings.init(false).getOptimizedVideo();
     }
 }
