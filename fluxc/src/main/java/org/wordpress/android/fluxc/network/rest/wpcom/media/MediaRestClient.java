883e1f51297a8a99d7f42bfb9a3569f231f7978e
Nick Bradbury <nick.bradbury@gmail.com>
2017-06-28 17:59:32 -0400

    Removed unused import to fix Travis

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/MediaRestClient.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/MediaRestClient.java
@@ -2,7 +2,6 @@ package org.wordpress.android.fluxc.network.rest.wpcom.media;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.android.volley.RequestQueue;
