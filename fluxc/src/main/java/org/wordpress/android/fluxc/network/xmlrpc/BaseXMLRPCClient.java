1c4a744907a281018d7659964e3d5783115e7903
Alex Forcier <alex.forcier@automattic.com>
2017-06-29 09:53:48 -0400

    Drop unused AccessToken from XML-RPC clients

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/BaseXMLRPCClient.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/BaseXMLRPCClient.java
@@ -5,7 +5,6 @@ import com.android.volley.RequestQueue;
 
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
-import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.network.BaseRequest;
 import org.wordpress.android.fluxc.network.BaseRequest.OnAuthFailedListener;
 import org.wordpress.android.fluxc.network.BaseRequest.OnParseErrorListener;
@@ -13,13 +12,10 @@ import org.wordpress.android.fluxc.network.HTTPAuthManager;
 import org.wordpress.android.fluxc.network.UserAgent;
 import org.wordpress.android.fluxc.network.discovery.DiscoveryRequest;
 import org.wordpress.android.fluxc.network.discovery.DiscoveryXMLRPCRequest;
-import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken;
 import org.wordpress.android.fluxc.store.AccountStore.AuthenticateErrorPayload;
 import org.wordpress.android.fluxc.utils.ErrorUtils.OnUnexpectedError;
 
 public class BaseXMLRPCClient {
-    private AccessToken mAccessToken;
-    private SiteModel mSiteModel;
     private final RequestQueue mRequestQueue;
     protected final Dispatcher mDispatcher;
     protected UserAgent mUserAgent;
@@ -28,11 +24,10 @@ public class BaseXMLRPCClient {
     protected OnAuthFailedListener mOnAuthFailedListener;
     protected OnParseErrorListener mOnParseErrorListener;
 
-    public BaseXMLRPCClient(Dispatcher dispatcher, RequestQueue requestQueue, AccessToken accessToken,
-                            UserAgent userAgent, HTTPAuthManager httpAuthManager) {
+    public BaseXMLRPCClient(Dispatcher dispatcher, RequestQueue requestQueue, UserAgent userAgent,
+                            HTTPAuthManager httpAuthManager) {
         mRequestQueue = requestQueue;
         mDispatcher = dispatcher;
-        mAccessToken = accessToken;
         mUserAgent = userAgent;
         mHTTPAuthManager = httpAuthManager;
         mOnAuthFailedListener = new OnAuthFailedListener() {
