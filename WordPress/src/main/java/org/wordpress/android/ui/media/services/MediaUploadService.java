8df477d4c025c0a650d3a7223a631c25c3ffd06d
Mario Zorz <mariozorz@gmail.com>
2017-07-07 12:40:27 -0300

    using ConcurrentHashMap to fix ConcurrentModificationException

--- WordPress/src/main/java/org/wordpress/android/ui/media/services/MediaUploadService.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/services/MediaUploadService.java
@@ -36,6 +36,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.inject.Inject;
 
@@ -53,7 +54,7 @@ public class MediaUploadService extends Service {
 
     // To avoid conflicts by editing the post each time a single upload completes, this map tracks completed media by
     // the post they're attached to, allowing us to update the post with the media URLs in a single batch at the end
-    private static HashMap<Integer, List<MediaModel>> mCompletedUploadsByPost = new HashMap<>();
+    private static ConcurrentHashMap<Integer, List<MediaModel>> mCompletedUploadsByPost = new ConcurrentHashMap<>();
 
     @Inject Dispatcher mDispatcher;
     @Inject MediaStore mMediaStore;
