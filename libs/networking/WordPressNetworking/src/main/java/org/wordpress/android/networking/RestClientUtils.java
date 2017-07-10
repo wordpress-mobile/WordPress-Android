9414205b67cb94ef668db766e0466cc7db0ff36d
Tony Rankin <tonyr@tonyrankin.com>
2017-07-10 14:57:48 -0700

    Add utility methods for Jetpack settings REST calls

--- libs/networking/WordPressNetworking/src/main/java/org/wordpress/android/networking/RestClientUtils.java
+++ libs/networking/WordPressNetworking/src/main/java/org/wordpress/android/networking/RestClientUtils.java
@@ -239,6 +239,18 @@ public class RestClientUtils {
         post(path, params, null, listener, errorListener);
     }
 
+    public void getJetpackSettings(long siteId, Listener listener, ErrorListener errorListener) {
+        String path = String.format(Locale.US, "jetpack-blogs/%d", siteId);
+        Map<String, String> params = new HashMap<>();
+        get(path, params, null, listener, errorListener);
+    }
+
+    public void setJetpackSettings(long siteId, Listener listener, ErrorListener errorListener,
+                                       Map<String, String> params) {
+        String path = String.format(Locale.US, "jetpack-blogs/%d", siteId);
+        post(path, params, null, listener, errorListener);
+    }
+
     /**
      * Delete a site
      */
