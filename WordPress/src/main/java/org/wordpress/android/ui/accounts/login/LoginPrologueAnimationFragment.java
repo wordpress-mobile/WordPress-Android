5c9971629660a7993b0f2705d9a80b19e51cb25f
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-07-12 10:12:39 +0300

    Reset anim on promo anim page hide

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginPrologueAnimationFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginPrologueAnimationFragment.java
@@ -83,6 +83,7 @@ public class LoginPrologueAnimationFragment extends Fragment {
             mLottieAnimationView.playAnimation();
         } else {
             mLottieAnimationView.cancelAnimation();
+            mLottieAnimationView.setProgress(0);
         }
     }
 }
