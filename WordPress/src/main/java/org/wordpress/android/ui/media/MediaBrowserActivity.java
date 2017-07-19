e993e9d574fa8d733c0de038279ba63435269402
Nick Bradbury <nick.bradbury@gmail.com>
2017-07-19 15:49:23 -0400

    Restored missing import

--- WordPress/src/main/java/org/wordpress/android/ui/media/MediaBrowserActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/MediaBrowserActivity.java
@@ -18,6 +18,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
