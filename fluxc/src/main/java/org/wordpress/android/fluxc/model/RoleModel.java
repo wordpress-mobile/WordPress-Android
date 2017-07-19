b62daa45e5894b4d2dba6004a682f78f671b6507
Oguz Kocer <oguz.kocer@automattic.com>
2017-07-19 17:46:32 +0300

    RoleModel introduced

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/RoleModel.java
@@ -0,0 +1,50 @@
+package org.wordpress.android.fluxc.model;
+
+import com.yarolegovich.wellsql.core.Identifiable;
+import com.yarolegovich.wellsql.core.annotation.Column;
+import com.yarolegovich.wellsql.core.annotation.PrimaryKey;
+import com.yarolegovich.wellsql.core.annotation.Table;
+
+@Table
+public class RoleModel implements Identifiable {
+    @PrimaryKey @Column private int mId;
+
+    // Site Id Foreign Key
+    @Column private int mSiteId;
+
+    // Role attributes
+    @Column private String mName;
+    @Column private String mDisplayName;
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public int getSiteId() {
+        return mSiteId;
+    }
+
+    public void setSiteId(int siteId) {
+        mSiteId = siteId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getDisplayName() {
+        return mDisplayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        mDisplayName = displayName;
+    }
+}
