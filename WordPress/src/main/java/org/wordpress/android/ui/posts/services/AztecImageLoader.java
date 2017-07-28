32922bf1078ec54e820290cbbb0c8b0fc9d0ffdd
Danilo Ercoli <ercoli@gmail.com>
2017-07-28 16:58:07 +0200

    Use custom loadin images with proper size when a post is loaded from the blog. HTML to visual editor

--- WordPress/src/main/java/org/wordpress/android/ui/posts/services/AztecImageLoader.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/services/AztecImageLoader.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 
+import org.wordpress.android.R;
 import org.wordpress.android.WordPress;
 import org.wordpress.android.util.ImageUtils;
 import org.wordpress.aztec.Html;
@@ -25,11 +27,13 @@ public class AztecImageLoader implements Html.ImageGetter {
 
     @Override
     public void loadImage(String url, final Callbacks callbacks, int maxWidth) {
-        // TODO: if a local file then load it directly. This is a quick fix though.
+        // Ignore the maxWidth passed from Aztec, since it's the MAX of screen width/height
+        final int maxWidthForEditor = ImageUtils.getMaximumThumbnailWidthForEditor(context);
+
         if (new File(url).exists()) {
             int orientation = ImageUtils.getImageOrientation(this.context, url);
             byte[] bytes = ImageUtils.createThumbnailFromUri(
-                   context, Uri.parse(url), maxWidth, null, orientation);
+                   context, Uri.parse(url), maxWidthForEditor, null, orientation);
             if (bytes != null) {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                 BitmapDrawable bitmapDrawable = new BitmapDrawable(context.getResources(), bitmap);
@@ -40,13 +44,18 @@ public class AztecImageLoader implements Html.ImageGetter {
             return;
         }
 
+        Drawable drawable = context.getResources().getDrawable(R.drawable.ic_gridicons_image);
+        drawable.setBounds(0, 0, 196, 196);
+        callbacks.onImageLoading(drawable);
+
         WordPress.sImageLoader.get(url, new ImageLoader.ImageListener() {
             @Override
             public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
                 Bitmap bitmap = response.getBitmap();
 
-                if (bitmap == null) {
-                    callbacks.onImageLoading(null);
+                if (bitmap == null && !isImmediate) {
+                    // isImmediate is true as soon as the request starts.
+                    callbacks.onImageFailed();
                 } else {
                     BitmapDrawable bitmapDrawable = new BitmapDrawable(context.getResources(), bitmap);
                     callbacks.onImageLoaded(bitmapDrawable);
@@ -57,6 +66,6 @@ public class AztecImageLoader implements Html.ImageGetter {
             public void onErrorResponse(VolleyError error) {
                 callbacks.onImageFailed();
             }
-        }, maxWidth, 0);
+        }, maxWidthForEditor, 0);
     }
 }
--- WordPress/src/main/java/org/wordpress/android/ui/posts/services/AztecVideoLoader.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/services/AztecVideoLoader.java
@@ -3,11 +3,13 @@ package org.wordpress.android.ui.posts.services;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 
+import org.wordpress.android.R;
 import org.wordpress.android.util.ImageUtils;
 import org.wordpress.aztec.Html;
 
@@ -22,22 +24,26 @@ public class AztecVideoLoader implements Html.VideoThumbnailGetter {
     }
 
     public void loadVideoThumbnail(final String url, final Html.VideoThumbnailGetter.Callbacks callbacks, final int maxWidth) {
-        if (TextUtils.isEmpty(url) || maxWidth <= 0) {
+        // Ignore the maxWidth passed from Aztec, since it's the MAX of screen width/height
+        final int maxWidthForEditor = ImageUtils.getMaximumThumbnailWidthForEditor(context);
+        if (TextUtils.isEmpty(url) || maxWidthForEditor <= 0) {
             callbacks.onThumbnailFailed();
             return;
         }
 
+        Drawable drawable = context.getResources().getDrawable(R.drawable.ic_gridicons_video_camera);
+        drawable.setBounds(0, 0, 196, 196);
+        callbacks.onThumbnailLoading(drawable);
+
         new AsyncTask<Void, Void, Bitmap>() {
             protected Bitmap doInBackground(Void... params) {
-                callbacks.onThumbnailLoading(null);
-
                 // If local file
                 if (new File(url).exists()) {
                     Bitmap thumb = ThumbnailUtils.createVideoThumbnail(url, MediaStore.Images.Thumbnails.FULL_SCREEN_KIND);
-                    return ImageUtils.getScaledBitmapAtLongestSide(thumb, maxWidth);
+                    return ImageUtils.getScaledBitmapAtLongestSide(thumb, maxWidthForEditor);
                 }
 
-                return ImageUtils.getVideoFrameFromVideo(url, maxWidth);
+                return ImageUtils.getVideoFrameFromVideo(url, maxWidthForEditor);
             }
             protected void onPostExecute(Bitmap thumb) {
                 if (thumb == null) {
