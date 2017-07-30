7bb7d754eebe77c9a81106dae92e37b3cec6cae0
aerych <aerychj@gmail.com>
2017-07-29 21:28:56 -0500

    Login: Interface for origin provider.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginHelpshiftOriginProvider.java
@@ -0,0 +1,7 @@
+package org.wordpress.android.ui.accounts.login;
+
+import org.wordpress.android.util.HelpshiftHelper;
+
+public interface LoginHelpshiftOriginProvider {
+    HelpshiftHelper.Tag helpshiftOriginTag();
+}
