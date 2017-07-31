41d54acb69028e17ebd1de24d5a939a2edb22a3e
Oguz Kocer <oguz.kocer@automattic.com>
2017-07-31 18:48:17 +0300

    Fetching plugins implemented

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/PluginAction.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/PluginAction.java
@@ -4,10 +4,15 @@ import org.wordpress.android.fluxc.annotations.Action;
 import org.wordpress.android.fluxc.annotations.ActionEnum;
 import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.model.SiteModel;
+import org.wordpress.android.fluxc.store.PluginStore.FetchedPluginsPayload;
 
 @ActionEnum
 public enum PluginAction implements IAction {
     // Remote actions
     @Action(payloadType = SiteModel.class)
     FETCH_PLUGINS,
+
+    // Remote responses
+    @Action(payloadType = FetchedPluginsPayload.class)
+    FETCHED_PLUGINS,
 }
--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/plugin/PluginRestClient.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/plugin/PluginRestClient.java
@@ -4,12 +4,24 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.Response.Listener;
 
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.generated.PluginActionBuilder;
+import org.wordpress.android.fluxc.generated.endpoint.WPCOMREST;
+import org.wordpress.android.fluxc.model.PluginModel;
 import org.wordpress.android.fluxc.model.SiteModel;
+import org.wordpress.android.fluxc.network.BaseRequest.BaseErrorListener;
+import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError;
 import org.wordpress.android.fluxc.network.UserAgent;
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient;
+import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequest;
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken;
+import org.wordpress.android.fluxc.network.rest.wpcom.plugin.PluginWPComRestResponse.FetchPluginsResponse;
+import org.wordpress.android.fluxc.store.PluginStore.FetchedPluginsPayload;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -23,5 +35,44 @@ public class PluginRestClient extends BaseWPComRestClient {
     }
 
     public void fetchPlugins(@NonNull final SiteModel site) {
+        String url = WPCOMREST.sites.site(site.getSiteId()).plugins.getUrlV1_1();
+        final WPComGsonRequest<FetchPluginsResponse> request = WPComGsonRequest.buildGetRequest(url, null,
+                FetchPluginsResponse.class,
+                new Listener<FetchPluginsResponse>() {
+                    @Override
+                    public void onResponse(FetchPluginsResponse response) {
+                        List<PluginModel> plugins = new ArrayList<>();
+                        if (response.plugins != null) {
+                            for (PluginWPComRestResponse pluginResponse : response.plugins) {
+                                plugins.add(pluginModelFromResponse(site, pluginResponse));
+                            }
+                        }
+                        mDispatcher.dispatch(PluginActionBuilder.newFetchedPluginsAction(new
+                                FetchedPluginsPayload(site, plugins)));
+                    }
+                },
+                new BaseErrorListener() {
+                    @Override
+                    public void onErrorResponse(@NonNull BaseNetworkError error) {
+                    }
+                }
+        );
+        add(request);
+    }
+
+    private PluginModel pluginModelFromResponse(SiteModel siteModel, PluginWPComRestResponse response) {
+        PluginModel pluginModel = new PluginModel();
+        pluginModel.setLocalSiteId(siteModel.getId());
+        pluginModel.setName(response.id);
+        pluginModel.setDisplayName(response.name);
+        pluginModel.setAuthorName(response.author);
+        pluginModel.setAuthorUrl(response.author_url);
+        pluginModel.setDescription(response.description);
+        pluginModel.setIsActive(response.active);
+        pluginModel.setIsAutoUpdateEnabled(response.autoupdate);
+        pluginModel.setPluginUrl(response.plugin_url);
+        pluginModel.setSlug(response.slug);
+        pluginModel.setVersion(response.version);
+        return pluginModel;
     }
 }
