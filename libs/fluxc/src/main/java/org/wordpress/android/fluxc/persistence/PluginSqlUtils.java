d7754abc320239f3deac7ffe109a8e818aa1cab1
Oguz Kocer <oguz.kocer@automattic.com>
2017-08-01 15:44:14 +0300

    PluginSqlUtils introduced and fetched plugins are inserted to DB

---
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/PluginSqlUtils.java
@@ -0,0 +1,26 @@
+package org.wordpress.android.fluxc.persistence;
+
+import android.support.annotation.NonNull;
+
+import com.wellsql.generated.PluginModelTable;
+import com.yarolegovich.wellsql.WellSql;
+
+import org.wordpress.android.fluxc.model.PluginModel;
+import org.wordpress.android.fluxc.model.SiteModel;
+
+import java.util.List;
+
+public class PluginSqlUtils {
+    public static void insertOrReplacePlugins(@NonNull SiteModel site, @NonNull List<PluginModel> plugins) {
+        // Remove previous post formats for this site
+        WellSql.delete(PluginModel.class)
+                .where()
+                .equals(PluginModelTable.LOCAL_SITE_ID, site.getId())
+                .endWhere().execute();
+        // Insert new post formats for this site
+        for (PluginModel pluginModel : plugins) {
+            pluginModel.setLocalSiteId(site.getId());
+        }
+        WellSql.insert(plugins).execute();
+    }
+}
