d5c717f3d158d43687965d64991d70688cedbc39
Danilo Ercoli <ercoli@gmail.com>
2017-08-02 10:33:04 +0200

    Fix an issue where `original width` resize setting were not respected, instead 1x1 picture were produced

--- WordPress/src/main/java/org/wordpress/android/util/WPMediaUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/WPMediaUtils.java
@@ -28,7 +28,7 @@ public class WPMediaUtils {
         if (isVideo) {
             return null;
         }
-        int resizeWidth = AppPrefs.getImageOptimizeWidth() > 0 ? AppPrefs.getImageOptimizeWidth() : Integer.MAX_VALUE;
+        int resizeWidth = AppPrefs.getImageOptimizeWidth() > 1 ? AppPrefs.getImageOptimizeWidth() : Integer.MAX_VALUE;
         int quality = AppPrefs.getImageOptimizeQuality();
         // do not optimize if original-size and 100% quality are set.
         if (resizeWidth == Integer.MAX_VALUE && quality == 100) {
