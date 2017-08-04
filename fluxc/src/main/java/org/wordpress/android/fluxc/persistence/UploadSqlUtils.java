a5d131d1bb383de7007c8eecf4ddbb6808898fc0
Alex Forcier <alex.forcier@automattic.com>
2017-08-04 12:01:33 -0400

    Support registering posts with the UploadStore

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/UploadSqlUtils.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/UploadSqlUtils.java
@@ -3,9 +3,11 @@ package org.wordpress.android.fluxc.persistence;
 import android.support.annotation.Nullable;
 
 import com.wellsql.generated.MediaUploadModelTable;
+import com.wellsql.generated.PostUploadModelTable;
 import com.yarolegovich.wellsql.WellSql;
 
 import org.wordpress.android.fluxc.model.MediaUploadModel;
+import org.wordpress.android.fluxc.model.PostUploadModel;
 
 import java.util.List;
 
@@ -34,6 +36,39 @@ public class UploadSqlUtils {
     public static @Nullable MediaUploadModel getMediaUploadModelForLocalId(int localMediaId) {
         List<MediaUploadModel> result = WellSql.select(MediaUploadModel.class).where()
                 .equals(MediaUploadModelTable.ID, localMediaId)
+                .endWhere()
+                .getAsModel();
+        if (result.isEmpty()) {
+            return null;
+        } else {
+            return result.get(0);
+        }
+    }
+
+    public static int insertOrUpdatePost(PostUploadModel post) {
+        if (post == null) return 0;
+
+        List<PostUploadModel> existingPosts;
+        existingPosts = WellSql.select(PostUploadModel.class)
+                .where()
+                .equals(PostUploadModelTable.ID, post.getId())
+                .endWhere().getAsModel();
+
+        if (existingPosts.isEmpty()) {
+            // Insert, post does not exist
+            WellSql.insert(post).asSingleTransaction(true).execute();
+            return 1;
+        } else {
+            // Update, post already exists
+            int oldId = existingPosts.get(0).getId();
+            return WellSql.update(PostUploadModel.class).whereId(oldId)
+                    .put(post, new UpdateAllExceptId<>(PostUploadModel.class)).execute();
+        }
+    }
+
+    public static @Nullable PostUploadModel getPostUploadModelForLocalId(int localPostId) {
+        List<PostUploadModel> result = WellSql.select(PostUploadModel.class).where()
+                .equals(PostUploadModelTable.ID, localPostId)
                 .endWhere()
                 .getAsModel();
         if (result.isEmpty()) {
