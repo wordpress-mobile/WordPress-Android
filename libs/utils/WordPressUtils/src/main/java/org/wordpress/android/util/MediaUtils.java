d4b3077ebad4a8da510b6264095da24846a05a95
Will Kwon <will@kwon.co>
2017-08-08 12:01:18 +0700

    Change all references of image width to size

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/MediaUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/MediaUtils.java
@@ -133,22 +133,22 @@ public class MediaUtils {
     }
 
     /**
-     * Get image width setting from the image width site setting string. This string can be an int, in this case it's
+     * Get image max size setting from the image max size setting string. This string can be an int, in this case it's
      * the maximum image width defined by the site.
      * Examples:
      *   "1000" will return 1000
      *   "Original Size" will return Integer.MAX_VALUE
      *   "Largeur originale" will return Integer.MAX_VALUE
      *   null will return Integer.MAX_VALUE
-     * @param imageWidthSiteSettingString Image width site setting string
+     * @param imageMaxSizeSiteSettingString Image max size site setting string
      * @return Integer.MAX_VALUE if image width is not defined or invalid, maximum image width in other cases.
      */
-    public static int getImageWidthSettingFromString(String imageWidthSiteSettingString) {
-        if (imageWidthSiteSettingString == null) {
+    public static int getImageMaxSizeSettingFromString(String imageMaxSizeSiteSettingString) {
+        if (imageMaxSizeSiteSettingString == null) {
             return Integer.MAX_VALUE;
         }
         try {
-            return Integer.valueOf(imageWidthSiteSettingString);
+            return Integer.valueOf(imageMaxSizeSiteSettingString);
         } catch (NumberFormatException e) {
             return Integer.MAX_VALUE;
         }
@@ -157,25 +157,25 @@ public class MediaUtils {
     /**
      * Calculate and return the maximum allowed image width by comparing the width of the image at its full size with
      * the maximum upload width set in the blog settings
-     * @param imageWidth the image's natural (full) width
-     * @param imageWidthSiteSettingString the maximum upload width set in the site settings
+     * @param imageSize the image's natural (full) width
+     * @param imageMaxSizeSiteSettingString the maximum upload width set in the site settings
      * @return maximum allowed image width
      */
-    public static int getMaximumImageWidth(int imageWidth, String imageWidthSiteSettingString) {
-        int imageWidthBlogSetting = getImageWidthSettingFromString(imageWidthSiteSettingString);
-        int imageWidthPictureSetting = imageWidth == 0 ? Integer.MAX_VALUE : imageWidth;
+    public static int getMaximumImageSize(int imageSize, String imageMaxSizeSiteSettingString) {
+        int imageMaxSizeBlogSetting = getImageMaxSizeSettingFromString(imageMaxSizeSiteSettingString);
+        int imageWidthPictureSetting = imageSize == 0 ? Integer.MAX_VALUE : imageSize;
 
-        if (Math.min(imageWidthPictureSetting, imageWidthBlogSetting) == Integer.MAX_VALUE) {
+        if (Math.min(imageWidthPictureSetting, imageMaxSizeBlogSetting) == Integer.MAX_VALUE) {
             // Default value in case of errors reading the picture size or the blog settings is set to Original size
             return DEFAULT_MAX_IMAGE_WIDTH;
         } else {
-            return Math.min(imageWidthPictureSetting, imageWidthBlogSetting);
+            return Math.min(imageWidthPictureSetting, imageMaxSizeBlogSetting);
         }
     }
 
-    public static int getMaximumImageWidth(Context context, Uri curStream, String imageWidthBlogSettingString) {
+    public static int getMaximumImageSize(Context context, Uri curStream, String imageMaxSizeBlogSettingString) {
         int[] dimensions = ImageUtils.getImageSize(curStream, context);
-        return getMaximumImageWidth(dimensions[0], imageWidthBlogSettingString);
+        return getMaximumImageSize(dimensions[0], imageMaxSizeBlogSettingString);
     }
 
     public static boolean isInMediaStore(Uri mediaUri) {
