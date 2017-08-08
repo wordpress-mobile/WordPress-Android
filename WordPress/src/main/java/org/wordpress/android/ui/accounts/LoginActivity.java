46132177628971af7de60d148d295c721ebf7747
theck13 <theck13@hotmail.com>
2017-08-08 09:21:02 -0600

    Add forgot password logic to fragments

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
@@ -41,7 +41,7 @@ public class LoginActivity extends AppCompatActivity implements ConnectionCallba
         Callback, LoginListener {
     private static final String KEY_SMARTLOCK_COMPLETED = "KEY_SMARTLOCK_COMPLETED";
 
-    private static final String FORGOT_PASSWORD_URL = "https://wordpress.com/wp-login.php?action=lostpassword";
+    private static final String FORGOT_PASSWORD_URL_SUFFIX = "wp-login.php?action=lostpassword";
 
     private SmartLockHelper mSmartLockHelper;
     private boolean mSmartLockCompleted;
@@ -289,9 +289,9 @@ public class LoginActivity extends AppCompatActivity implements ConnectionCallba
     }
 
     @Override
-    public void forgotPassword() {
+    public void forgotPassword(String url) {
         AnalyticsTracker.track(AnalyticsTracker.Stat.LOGIN_FORGOT_PASSWORD_CLICKED);
-        ActivityLauncher.openUrlExternal(this, FORGOT_PASSWORD_URL);
+        ActivityLauncher.openUrlExternal(this, url + FORGOT_PASSWORD_URL_SUFFIX);
     }
 
     @Override
