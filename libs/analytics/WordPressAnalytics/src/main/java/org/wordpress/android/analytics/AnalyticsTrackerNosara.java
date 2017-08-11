e2ff70d5d8e42619aa2406e89c07bf99af49b72e
theck13 <theck13@hotmail.com>
2017-08-11 12:16:32 -0700

    Add analytics to track Google login

--- libs/analytics/WordPressAnalytics/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
+++ libs/analytics/WordPressAnalytics/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
@@ -165,6 +165,9 @@ public class AnalyticsTrackerNosara extends Tracker {
             case EDITOR_AZTEC_PROMO_POSITIVE:
                 predefinedEventProperties.put("source", "promo_positive");
                 break;
+            case LOGIN_GOOGLE_TAPPED:
+                predefinedEventProperties.put("source", "android");
+                break;
             case OPENED_POSTS:
                 predefinedEventProperties.put("menu_item", "posts");
                 break;
@@ -692,6 +695,8 @@ public class AnalyticsTrackerNosara extends Tracker {
                 return "login_prologue_viewed";
             case LOGIN_EMAIL_FORM_VIEWED:
                 return "login_email_form_viewed";
+            case LOGIN_GOOGLE_TAPPED:
+                return "login_google_tapped";
             case LOGIN_MAGIC_LINK_OPEN_EMAIL_CLIENT_VIEWED:
                 return "login_magic_link_open_email_client_viewed";
             case LOGIN_MAGIC_LINK_OPEN_EMAIL_CLIENT_CLICKED:
