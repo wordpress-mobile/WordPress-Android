f0b70dd695f8cb3a0fcf25f7ce62b0afcfb06457
Alex Forcier <alex.forcier@automattic.com>
2017-08-14 15:05:13 +0200

    Add CANCEL_POST

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/UploadAction.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/UploadAction.java
@@ -3,11 +3,14 @@ package org.wordpress.android.fluxc.action;
 import org.wordpress.android.fluxc.annotations.Action;
 import org.wordpress.android.fluxc.annotations.ActionEnum;
 import org.wordpress.android.fluxc.annotations.action.IAction;
+import org.wordpress.android.fluxc.model.PostModel;
 import org.wordpress.android.fluxc.store.UploadStore.ClearMediaPayload;
 
 @ActionEnum
 public enum UploadAction implements IAction {
     // Local actions
+    @Action(payloadType = PostModel.class)
+    CANCEL_POST,
     @Action(payloadType = ClearMediaPayload.class)
     CLEAR_MEDIA
 }
