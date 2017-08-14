6f011950e7e994d6c183cc86793274cd437a88f8
Alex Forcier <alex.forcier@automattic.com>
2017-08-14 09:38:18 +0200

    Add UploadStore methods returning PostModels based on upload state

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/UploadSqlUtils.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/UploadSqlUtils.java
@@ -1,14 +1,19 @@
 package org.wordpress.android.fluxc.persistence;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.wellsql.generated.MediaUploadModelTable;
+import com.wellsql.generated.PostModelTable;
 import com.wellsql.generated.PostUploadModelTable;
 import com.yarolegovich.wellsql.WellSql;
 
 import org.wordpress.android.fluxc.model.MediaUploadModel;
+import org.wordpress.android.fluxc.model.PostModel;
 import org.wordpress.android.fluxc.model.PostUploadModel;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -46,6 +51,25 @@ public class UploadSqlUtils {
         }
     }
 
+    public static int deleteMediaUploadModelWithLocalId(int localMediaId) {
+        return WellSql.delete(MediaUploadModel.class)
+                .where()
+                .equals(MediaUploadModelTable.ID, localMediaId)
+                .endWhere()
+                .execute();
+    }
+
+    public static int deleteMediaUploadModelsWithLocalIds(Set<Integer> localMediaIds) {
+        if (localMediaIds.size() > 0) {
+            return WellSql.delete(MediaUploadModel.class)
+                    .where()
+                    .isIn(MediaUploadModelTable.ID, localMediaIds)
+                    .endWhere()
+                    .execute();
+        }
+        return 0;
+    }
+
     public static int insertOrUpdatePost(PostUploadModel post) {
         if (post == null) return 0;
 
@@ -79,23 +103,24 @@ public class UploadSqlUtils {
         }
     }
 
-    public static int deleteMediaUploadModelWithLocalId(int localMediaId) {
-        return WellSql.delete(MediaUploadModel.class)
-                .where()
-                .equals(MediaUploadModelTable.ID, localMediaId)
+    public static @NonNull List<PostUploadModel> getPostUploadModelsWithState(@PostUploadModel.UploadState int state) {
+        return WellSql.select(PostUploadModel.class).where()
+                .equals(PostUploadModelTable.UPLOAD_STATE, state)
                 .endWhere()
-                .execute();
+                .getAsModel();
     }
 
-    public static int deleteMediaUploadModelsWithLocalIds(Set<Integer> localMediaIds) {
-        if (localMediaIds.size() > 0) {
-            return WellSql.delete(MediaUploadModel.class)
-                    .where()
-                    .isIn(MediaUploadModelTable.ID, localMediaIds)
-                    .endWhere()
-                    .execute();
+    public static @NonNull List<PostModel> getPostModelsForPostUploadModels(List<PostUploadModel> postUploadModels) {
+        if (postUploadModels.size() > 0) {
+            Set<Integer> postIdSet = new HashSet<>();
+            for (PostUploadModel postUploadModel : postUploadModels) {
+                postIdSet.add(postUploadModel.getId());
+            }
+            return WellSql.select(PostModel.class).where()
+                    .isIn(PostModelTable.ID, postIdSet)
+                    .endWhere().getAsModel();
         }
-        return 0;
+        return Collections.emptyList();
     }
 
     public static int deletePostUploadModelWithLocalId(int localPostId) {
