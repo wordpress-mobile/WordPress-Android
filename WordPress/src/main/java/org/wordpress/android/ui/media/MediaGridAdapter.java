11a1f90b234042d15b26964264352b293e5c744c
Mario Zorz <mariozorz@gmail.com>
2017-08-15 08:35:29 -0300

    removed artificially setting the upload state view to QUEUED and only leaving that to reflect what the media item UploadState really has

--- WordPress/src/main/java/org/wordpress/android/ui/media/MediaGridAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/MediaGridAdapter.java
@@ -365,8 +365,6 @@ public class MediaGridAdapter extends RecyclerView.Adapter<MediaGridAdapter.Grid
                         MediaModel media = mMediaList.get(position);
                         MediaUploadState state = MediaUploadState.fromString(media.getUploadState());
                         if (state == MediaUploadState.FAILED) {
-                            stateTextView.setText(R.string.media_upload_state_queued);
-                            stateTextView.setCompoundDrawables(null, null, null, null);
                             if (mCallback != null) {
                                 mCallback.onAdapterRetryUpload(media.getId());
                             }
