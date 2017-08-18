6c7063f43860af3c1028058092e8c5c8e4dbe230
theck13 <theck13@hotmail.com>
2017-08-17 18:16:48 -0600

    Add Google client connection and callbacks for login

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginThirdPartyAuthenticationFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginThirdPartyAuthenticationFragment.java
@@ -1,26 +1,68 @@
 package org.wordpress.android.ui.accounts.login;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.wordpress.android.R;
 
-public class LoginThirdPartyAuthenticationFragment extends Fragment {
+import static android.app.Activity.RESULT_OK;
+
+public class LoginThirdPartyAuthenticationFragment extends Fragment
+        implements ActivityCompat.OnRequestPermissionsResultCallback,
+                   GoogleApiClient.ConnectionCallbacks,
+                   GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
     private LoginListener mLoginListener;
+    private boolean isResolvingError;
+    private boolean shouldResolveError;
+
+    private static final String STATE_RESOLVING_ERROR = "STATE_RESOLVING_ERROR";
+    private static final int REQUEST_CONNECT = 1000;
+    private static final int REQUEST_PERMISSIONS_GET_ACCOUNTS = 9000;
 
     public static final String TAG = "login_third_party_authentication_fragment_tag";
+
+    @Override
+    public void onActivityResult(int request, int result, Intent data) {
+        super.onActivityResult(request, result, data);
+
+        switch (request) {
+            case REQUEST_CONNECT:
+                if (result != RESULT_OK) {
+                    shouldResolveError = false;
+                }
+
+                if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+
+                isResolvingError = false;
+                break;
+        }
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -34,9 +76,66 @@ public class LoginThirdPartyAuthenticationFragment extends Fragment {
     }
 
     @Override
+    public void onConnected(Bundle bundle) {
+        // Indicates account was selected, that account has granted any required permissions, and a
+        // connection to Google Play services has been established.
+        if (shouldResolveError) {
+            shouldResolveError = false;
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account, grant
+        // permissions or resolve an error in order to sign in.  Refer to the documentation for
+        // ConnectionResult to see possible error codes.
+        if (!isResolvingError && shouldResolveError) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    isResolvingError = true;
+                    connectionResult.startResolutionForResult(getActivity(), REQUEST_CONNECT);
+                } catch (IntentSender.SendIntentException exception) {
+                    isResolvingError = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                isResolvingError = false;
+                // TODO: Show error screen.
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Connection to Google Play services was lost.  GoogleApiClient will automatically attempt
+        // to re-connect.  Any UI elements depending on connection to Google APIs should be hidden
+        // or disabled until onConnected is called again.
+        Log.w(LoginThirdPartyAuthenticationFragment.class.getSimpleName(), "onConnectionSuspended: " + i);
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        // Restore state of error resolving.
+        isResolvingError = savedInstanceState != null && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
+
+        // Configure sign-in to request user's ID, basic profile, email address, and ID token.
+        // ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build Google API client with access to sign-in API and options specified above.
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        // TODO: Start login process.
     }
 
     @Override
@@ -49,9 +148,12 @@ public class LoginThirdPartyAuthenticationFragment extends Fragment {
         View layout = inflater.inflate(R.layout.login_third_party_authentication, container, false);
 
         layout.findViewById(R.id.login_google_try_again).setOnClickListener(new View.OnClickListener() {
+            @SuppressWarnings("PrivateMemberAccessBetweenOuterAndInnerClass")
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "Try again not implemented", Toast.LENGTH_SHORT).show();
+                if (!isResolvingError) {
+                    // TODO: Start login process.
+                }
             }
         });
 
@@ -62,6 +164,7 @@ public class LoginThirdPartyAuthenticationFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mLoginListener = null;
+        disconnectGoogleClient();
     }
 
     @Override
@@ -80,6 +183,26 @@ public class LoginThirdPartyAuthenticationFragment extends Fragment {
     }
 
     @Override
+    public void onRequestPermissionsResult(int request, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(request, permissions, grantResults);
+
+        switch (request) {
+            case REQUEST_PERMISSIONS_GET_ACCOUNTS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    connectGoogleClient();
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_RESOLVING_ERROR, isResolvingError);
+    }
+
+    @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -91,6 +214,20 @@ public class LoginThirdPartyAuthenticationFragment extends Fragment {
         if (actionBar != null) {
             actionBar.setDisplayShowTitleEnabled(false);
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public void connectGoogleClient() {
+        if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+            shouldResolveError = true;
+            mGoogleApiClient.connect();
+        }
+    }
+
+    private void disconnectGoogleClient() {
+        if (mGoogleApiClient.isConnected()) {
+            Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+            mGoogleApiClient.disconnect();
         }
     }
 }
