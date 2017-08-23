e2c96f6d808c90f8e96508ce87b6535ad1bcb00a
Nick Bradbury <nick.bradbury@gmail.com>
2017-08-23 07:39:45 -0400

    Moved pref key methods to enum

--- WordPress/src/main/java/org/wordpress/android/util/SmartToast.java
+++ WordPress/src/main/java/org/wordpress/android/util/SmartToast.java
@@ -20,33 +20,53 @@ public class SmartToast {
     public enum SmartToastType {
         WP_MEDIA_LONG_PRESS,
         PHOTO_PICKER_LONG_PRESS,
-        COMMENTS_LONG_PRESS
+        COMMENTS_LONG_PRESS;
+
+        /*
+         * returns the preference key which stores the number of times the feature associated with the smart toast
+         * type has been used
+        */
+        private UndeletablePrefKey getUsageKey() {
+            switch (this) {
+                case COMMENTS_LONG_PRESS:
+                    return UndeletablePrefKey.SMART_TOAST_COMMENTS_LONG_PRESS_USAGE_COUNTER;
+                case PHOTO_PICKER_LONG_PRESS:
+                    return UndeletablePrefKey.SMART_TOAST_PHOTO_PICKER_LONG_PRESS_USAGE_COUNTER;
+                default: // WP_MEDIA_LONG_PRESS
+                    return UndeletablePrefKey.SMART_TOAST_WP_MEDIA_LONG_PRESS_USAGE_COUNTER;
+            }
+        }
+
+        /*
+         * returns the preference key which stores the number of times the toast associated with the smart toast type
+         * has been shown
+         */
+        private UndeletablePrefKey getShownKey() {
+            switch (this) {
+                case COMMENTS_LONG_PRESS:
+                    return UndeletablePrefKey.SMART_TOAST_COMMENTS_LONG_PRESS_TOAST_COUNTER;
+                case PHOTO_PICKER_LONG_PRESS:
+                    return UndeletablePrefKey.SMART_TOAST_PHOTO_PICKER_LONG_PRESS_TOAST_COUNTER;
+                default: // WP_MEDIA_LONG_PRESS
+                    return UndeletablePrefKey.SMART_TOAST_WP_MEDIA_LONG_PRESS_TOAST_COUNTER;
+            }
+        }
     }
 
     private static final int MIN_TIMES_TO_USE_FEATURE = 3;
     private static final int MAX_TIMES_TO_SHOW_TOAST = 2;
 
     public static boolean show(@NonNull Context context, @NonNull SmartToastType type) {
-        UndeletablePrefKey keyNumTimesUsed;
-        UndeletablePrefKey keyNumTimesShown;
-        try {
-            keyNumTimesUsed = getNumTimesFeatureUsedKey(type);
-            keyNumTimesShown = getNumTimesToastShownKey(type);
-        } catch (IllegalArgumentException e) {
-            AppLog.e(AppLog.T.MEDIA, e);
-            return false;
-        }
-
         // limit the number of times to show the toast
-        int numTimesShown = AppPrefs.getInt(keyNumTimesShown);
+        int numTimesShown = AppPrefs.getInt(type.getShownKey());
         if (numTimesShown >= MAX_TIMES_TO_SHOW_TOAST) {
             return false;
         }
 
         // don't show the toast until the user has used this feature a few times
-        int numTypesFeatureUsed = AppPrefs.getInt(keyNumTimesUsed);
+        int numTypesFeatureUsed = AppPrefs.getInt(type.getUsageKey());
         numTypesFeatureUsed++;
-        AppPrefs.setInt(keyNumTimesUsed, numTypesFeatureUsed);
+        AppPrefs.setInt(type.getUsageKey(), numTypesFeatureUsed);
         if (numTypesFeatureUsed <= MIN_TIMES_TO_USE_FEATURE) {
             return false;
         }
@@ -78,55 +98,16 @@ public class SmartToast {
         toast.show();
 
         numTimesShown++;
-        AppPrefs.setInt(keyNumTimesShown, numTimesShown);
+        AppPrefs.setInt(type.getShownKey(), numTimesShown);
         return true;
     }
 
     /*
      * prevent the passed smart toast type from being shown by setting its counter to the max - this should be
-     * used to disable long press toasts when the user long presses to multiselect since they alraady know
+     * used to disable long press toasts when the user long presses to multiselect since they already know
      * they can do it
      */
     public static void disableSmartToast(@NonNull SmartToastType type) {
-        try {
-            UndeletablePrefKey key = getNumTimesToastShownKey(type);
-            AppPrefs.setInt(key, MAX_TIMES_TO_SHOW_TOAST);
-        } catch (IllegalArgumentException e) {
-            AppLog.e(AppLog.T.MEDIA, e);
-        }
-    }
-
-    /*
-     * get the preference key which stores the number of times the feature associated with the smart toast type has
-     * been used
-     */
-    private static UndeletablePrefKey getNumTimesFeatureUsedKey(@NonNull SmartToastType type) {
-        switch (type) {
-            case WP_MEDIA_LONG_PRESS:
-                return UndeletablePrefKey.SMART_TOAST_WP_MEDIA_LONG_PRESS_USAGE_COUNTER;
-            case PHOTO_PICKER_LONG_PRESS:
-                return UndeletablePrefKey.SMART_TOAST_PHOTO_PICKER_LONG_PRESS_USAGE_COUNTER;
-            case COMMENTS_LONG_PRESS:
-                return UndeletablePrefKey.SMART_TOAST_COMMENTS_LONG_PRESS_USAGE_COUNTER;
-            default:
-                throw new IllegalArgumentException("Unknown smart toast type");
-        }
-    }
-
-    /*
-     * get the preference key which stores the number of times the toast associated with the smart toast type has
-     * been shown
-     */
-    private static UndeletablePrefKey getNumTimesToastShownKey(@NonNull SmartToastType type) {
-        switch (type) {
-            case WP_MEDIA_LONG_PRESS:
-                return UndeletablePrefKey.SMART_TOAST_WP_MEDIA_LONG_PRESS_TOAST_COUNTER;
-            case PHOTO_PICKER_LONG_PRESS:
-                return UndeletablePrefKey.SMART_TOAST_PHOTO_PICKER_LONG_PRESS_TOAST_COUNTER;
-            case COMMENTS_LONG_PRESS:
-                return UndeletablePrefKey.SMART_TOAST_COMMENTS_LONG_PRESS_TOAST_COUNTER;
-            default:
-                throw new IllegalArgumentException("Unknown smart toast type");
-        }
+        AppPrefs.setInt(type.getShownKey(), MAX_TIMES_TO_SHOW_TOAST);
     }
 }
