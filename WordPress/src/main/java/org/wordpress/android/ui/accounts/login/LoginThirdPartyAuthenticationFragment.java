22de7f58e1078fa79a1f7fca797e0e1660c5a015
theck13 <theck13@hotmail.com>
2017-08-23 17:03:21 -0600

    Add server auth code to Google login request

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginThirdPartyAuthenticationFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginThirdPartyAuthenticationFragment.java
@@ -151,6 +151,7 @@ public class LoginThirdPartyAuthenticationFragment extends Fragment
         // Configure sign-in to request user's ID, basic profile, email address, and ID token.
         // ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestServerAuthCode(getString(R.string.default_web_client_id), false)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
