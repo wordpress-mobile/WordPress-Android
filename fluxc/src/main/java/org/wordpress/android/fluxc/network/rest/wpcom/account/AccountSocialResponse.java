fe30fd9dea188316d534f961f653721db0a970b4
theck13 <theck13@hotmail.com>
2017-08-25 09:38:25 -0600

    Add social login response class

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/account/AccountSocialResponse.java
@@ -0,0 +1,8 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.account;
+
+import org.wordpress.android.fluxc.network.Response;
+
+public class AccountSocialResponse implements Response {
+    public String bearer_token;
+    public String username;
+}
