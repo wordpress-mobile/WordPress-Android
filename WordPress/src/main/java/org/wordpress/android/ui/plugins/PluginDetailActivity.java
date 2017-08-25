721d425466dffcf819f3f6bcdf32b0dc9778d98a
Oguz Kocer <oguz.kocer@automattic.com>
2017-08-25 15:06:44 +0300

    Setup plugin detail activity to handle passed site and plugin

--- WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginDetailActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginDetailActivity.java
@@ -9,12 +9,19 @@ import android.view.MenuItem;
 import org.wordpress.android.R;
 import org.wordpress.android.WordPress;
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.model.PluginModel;
+import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.store.PluginStore;
+import org.wordpress.android.util.ToastUtils;
+import org.wordpress.android.util.ToastUtils.Duration;
 
 import javax.inject.Inject;
 
 public class PluginDetailActivity extends AppCompatActivity {
     public static final String KEY_PLUGIN_NAME = "KEY_PLUGIN_NAME";
+
+    private SiteModel mSite;
+    private PluginModel mPlugin;
 
     @Inject PluginStore mPluginStore;
     @Inject Dispatcher mDispatcher;
@@ -25,6 +32,30 @@ public class PluginDetailActivity extends AppCompatActivity {
         ((WordPress) getApplication()).component().inject(this);
 
         setContentView(R.layout.plugin_detail_activity);
+
+        String pluginName;
+
+        if (savedInstanceState == null) {
+            mSite = (SiteModel) getIntent().getSerializableExtra(WordPress.SITE);
+            pluginName = getIntent().getStringExtra(KEY_PLUGIN_NAME);
+        } else {
+            mSite = (SiteModel) savedInstanceState.getSerializable(WordPress.SITE);
+            pluginName = savedInstanceState.getString(KEY_PLUGIN_NAME);
+        }
+
+        mPlugin = mPluginStore.getPluginByName(mSite, pluginName);
+
+        if (mSite == null) {
+            ToastUtils.showToast(this, R.string.blog_not_found, Duration.SHORT);
+            finish();
+            return;
+        }
+
+        if (mPlugin == null) {
+            ToastUtils.showToast(this, R.string.plugin_not_found, Duration.SHORT);
+            finish();
+            return;
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,5 +74,12 @@ public class PluginDetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(WordPress.SITE, mSite);
+        outState.putString(KEY_PLUGIN_NAME, mPlugin.getName());
     }
 }
