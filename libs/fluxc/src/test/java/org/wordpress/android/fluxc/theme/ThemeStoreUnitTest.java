eb2274b81e3c426e5d15be926c8f95c3492cd7c1
Tony Rankin <tonyr@tonyrankin.com>
2017-08-28 16:52:08 -0700

    Add unit test class for ThemeStore

---
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/theme/ThemeStoreUnitTest.java
@@ -0,0 +1,12 @@
+package org.wordpress.android.fluxc.theme;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ThemeStoreUnitTest {
+    @Before
+    public void setUp() {
+    }
+}
