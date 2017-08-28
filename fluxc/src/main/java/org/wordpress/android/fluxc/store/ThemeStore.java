c8136ed4eef572fa5499215ccc12edc4f206cef3
Tony Rankin <tonyr@tonyrankin.com>
2017-08-28 16:51:27 -0700

    Add payload, error, and OnChanged definitions

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/ThemeStore.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/ThemeStore.java
@@ -1,15 +1,76 @@
 package org.wordpress.android.fluxc.store;
 
+import android.support.annotation.NonNull;
+
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.Payload;
 import org.wordpress.android.fluxc.annotations.action.Action;
+import org.wordpress.android.fluxc.annotations.action.IAction;
+import org.wordpress.android.fluxc.model.SiteModel;
+import org.wordpress.android.fluxc.model.ThemeModel;
 import org.wordpress.android.util.AppLog;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
-
 public class ThemeStore extends Store {
+    // Payloads
+    public static class FetchedCurrentThemePayload extends Payload {
+        public SiteModel site;
+        public ThemeModel theme;
+        public FetchThemesError error;
+
+        public FetchedCurrentThemePayload(FetchThemesError error) {
+            this.error = error;
+        }
+
+        public FetchedCurrentThemePayload(@NonNull SiteModel site, @NonNull ThemeModel theme) {
+            this.site = site;
+            this.theme = theme;
+        }
+    }
+
+    public static class FetchedThemesPayload extends Payload {
+        public SiteModel site;
+        public List<ThemeModel> themes;
+        public FetchThemesError error;
+
+        public FetchedThemesPayload(FetchThemesError error) {
+            this.error = error;
+        }
+
+        public FetchedThemesPayload(@NonNull SiteModel site, @NonNull List<ThemeModel> themes) {
+            this.site = site;
+            this.themes = themes;
+        }
+    }
+
+    public enum ThemeErrorType {
+        GENERIC_ERROR,
+        UNAUTHORIZED,
+        NOT_AVAILABLE
+    }
+
+    public static class FetchThemesError implements OnChangedError {
+        ThemeErrorType type;
+        public String message;
+        public FetchThemesError(ThemeErrorType type, String message) {
+            this.type = type;
+            this.message = message;
+        }
+    }
+
+    public static class OnThemesChanged extends OnChanged<FetchThemesError> {
+        public SiteModel site;
+
+        public OnThemesChanged(SiteModel site) {
+            this.site = site;
+        }
+    }
+
     @Inject
     public ThemeStore(Dispatcher dispatcher) {
         super(dispatcher);
