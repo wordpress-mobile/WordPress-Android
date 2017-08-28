e931f833171da32c2b3cb89c9c4a55741ca454ad
Will Kwon <will@kwon.co>
2017-08-28 22:42:15 +0900

    Adding test to verify that if maxSize is 0 getScaleForResizing returns 1

--- libs/utils/WordPressUtils/src/androidTest/java/org/wordpress/android/util/ImageUtilsTest.java
+++ libs/utils/WordPressUtils/src/androidTest/java/org/wordpress/android/util/ImageUtilsTest.java
@@ -1,7 +1,13 @@
 package org.wordpress.android.util;
 
+import android.graphics.BitmapFactory;
 import android.test.InstrumentationTestCase;
 
 public class ImageUtilsTest extends InstrumentationTestCase {
-    
+    public void testGetScaleForResizingReturnsOneWhenMaxSizeIsZero() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        int scale = ImageUtils.getScaleForResizing(0, options);
+
+        assertEquals(1, scale);
+    }
 }
