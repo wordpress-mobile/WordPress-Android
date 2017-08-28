9243be0fe30e608887eff1b525ff2f55eb6feb34
Will Kwon <will@kwon.co>
2017-08-28 22:50:26 +0900

    Adding tests to make sure scaling works correctly

--- libs/utils/WordPressUtils/src/androidTest/java/org/wordpress/android/util/ImageUtilsTest.java
+++ libs/utils/WordPressUtils/src/androidTest/java/org/wordpress/android/util/ImageUtilsTest.java
@@ -21,4 +21,58 @@ public class ImageUtilsTest extends InstrumentationTestCase {
 
         assertEquals(1, scale);
     }
+
+    public void testGetScaleForResizingPortraitMaxHeightSameAsMaxSizeReturnsOne() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.outHeight = 100;
+        options.outWidth = 1;
+        int maxSize = 100;
+
+        int scale = ImageUtils.getScaleForResizing(maxSize, options);
+
+        assertEquals(1, scale);
+    }
+
+    public void testGetScaleForResizingLandscapeMaxWidthSameAsMaxSizeReturnsOne() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.outHeight = 1;
+        options.outWidth = 100;
+        int maxSize = 100;
+
+        int scale = ImageUtils.getScaleForResizing(maxSize, options);
+
+        assertEquals(1, scale);
+    }
+
+    public void testGetScaleForResizingDoubleSizeReturnsTwo() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.outHeight = 100;
+        options.outWidth = 200;
+        int maxSize = 100;
+
+        int scale = ImageUtils.getScaleForResizing(maxSize, options);
+
+        assertEquals(2, scale);
+    }
+    public void testGetScaleForResizingThreeTimesSizeReturnsTwo() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.outHeight = 100;
+        options.outWidth = 300;
+        int maxSize = 100;
+
+        int scale = ImageUtils.getScaleForResizing(maxSize, options);
+
+        assertEquals(2, scale);
+    }
+
+    public void testGetScaleForResizingEightTimesSizeReturnsEight() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.outHeight = 100;
+        options.outWidth = 800;
+        int maxSize = 100;
+
+        int scale = ImageUtils.getScaleForResizing(maxSize, options);
+
+        assertEquals(8, scale);
+    }
 }
