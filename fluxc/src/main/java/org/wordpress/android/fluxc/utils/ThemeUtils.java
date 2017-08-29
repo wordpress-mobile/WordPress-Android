5342cc8963c0e2e2d5d341c69a39aa570b18268a
Tony Rankin <tonyr@tonyrankin.com>
2017-08-28 17:43:30 -0700

    Update all available fields when reading theme REST responses

--- fluxc/src/main/java/org/wordpress/android/fluxc/utils/ThemeUtils.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/utils/ThemeUtils.java
@@ -3,6 +3,7 @@ package org.wordpress.android.fluxc.utils;
 import android.text.TextUtils;
 
 import org.wordpress.android.fluxc.model.ThemeModel;
+import org.wordpress.android.fluxc.network.rest.wpcom.theme.ThemeJetpackResponse.MultipleJetpackThemesResponse;
 import org.wordpress.android.fluxc.network.rest.wpcom.theme.ThemeJetpackResponse;
 import org.wordpress.android.fluxc.network.rest.wpcom.theme.ThemeWPComResponse;
 import org.wordpress.android.fluxc.network.rest.wpcom.theme.ThemeWPComResponse.MultipleWPComThemesResponse;
@@ -14,9 +15,40 @@ public class ThemeUtils {
     public static ThemeModel createThemeFromWPComResponse(ThemeWPComResponse response) {
         ThemeModel theme = new ThemeModel();
         theme.setThemeId(response.id);
+        theme.setSlug(response.slug);
+        theme.setStylesheet(response.stylesheet);
         theme.setName(response.name);
         theme.setAuthorName(response.author);
+        theme.setAuthorUrl(response.author_uri);
+        theme.setThemeUrl(response.theme_uri);
+        theme.setDemoUrl(response.demo_uri);
+        theme.setVersion(response.version);
         theme.setScreenshotUrl(response.screenshot);
+        theme.setDescription(response.description);
+        theme.setDownloadUrl(response.download_uri);
+        return theme;
+    }
+
+    public static ThemeModel createThemeFromJetpackResponse(ThemeJetpackResponse response) {
+        ThemeModel theme = new ThemeModel();
+        theme.setThemeId(response.id);
+        theme.setName(response.name);
+        theme.setThemeUrl(response.theme_uri);
+        theme.setDescription(response.description);
+        theme.setAuthorName(response.author);
+        theme.setAuthorUrl(response.author_uri);
+        theme.setVersion(response.version);
+        theme.setActive(response.active);
+        theme.setAutoUpdate(response.autoupdate);
+        theme.setAutoUpdateTranslation(response.autoupdate_translation);
+
+        // the screenshot field in Jetpack responses does not contain a protocol so we'll prepend 'https'
+        String screenshotUrl = response.screenshot;
+        if (!TextUtils.isEmpty(screenshotUrl) && screenshotUrl.startsWith("//")) {
+            screenshotUrl = screenshotUrl + "https";
+        }
+        theme.setScreenshotUrl(screenshotUrl);
+
         return theme;
     }
 
@@ -30,22 +62,10 @@ public class ThemeUtils {
     }
 
     /** Creates a list of ThemeModels from the Jetpack /v1/sites/$siteId/themes REST response. */
-    public static List<ThemeModel> createThemeListFromJetpackResponse(ThemeJetpackResponse.MultipleJetpackThemesResponse response) {
+    public static List<ThemeModel> createThemeListFromJetpackResponse(MultipleJetpackThemesResponse response) {
         List<ThemeModel> themeList = new ArrayList<>();
         for (ThemeJetpackResponse item : response.themes) {
-            ThemeModel theme = new ThemeModel();
-            theme.setThemeId(item.id);
-            theme.setName(item.name);
-            theme.setAuthorName(item.author);
-
-            // the screenshot field in Jetpack responses does not contain a protocol so we'll prepend 'https'
-            String screenshotUrl = item.screenshot;
-            if (!TextUtils.isEmpty(screenshotUrl) && screenshotUrl.startsWith("//")) {
-                screenshotUrl = screenshotUrl + "https";
-            }
-            theme.setScreenshotUrl(screenshotUrl);
-
-            themeList.add(theme);
+            themeList.add(createThemeFromJetpackResponse(item));
         }
         return themeList;
     }
