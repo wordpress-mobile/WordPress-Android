fc341de09246fd07ea87a2a4730dcc02682a2fd3
Tony Rankin <tonyr@tonyrankin.com>
2017-08-28 17:19:51 -0700

    Handle fetch/fetched installed themes for Jetpack sites

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/theme/ThemeRestClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/theme/ThemeRestClient.java
@@ -9,15 +9,22 @@ import com.android.volley.Response;
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.generated.ThemeActionBuilder;
 import org.wordpress.android.fluxc.generated.endpoint.WPCOMREST;
+import org.wordpress.android.fluxc.model.SiteModel;
+import org.wordpress.android.fluxc.model.ThemeModel;
 import org.wordpress.android.fluxc.network.BaseRequest;
 import org.wordpress.android.fluxc.network.UserAgent;
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequest;
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken;
 import org.wordpress.android.fluxc.network.rest.wpcom.theme.ThemeWPComResponse.MultipleWPComThemesResponse;
+import org.wordpress.android.fluxc.network.rest.wpcom.theme.ThemeJetpackResponse.MultipleJetpackThemesResponse;
 import org.wordpress.android.fluxc.store.ThemeStore.ThemeErrorType;
+import org.wordpress.android.fluxc.store.ThemeStore.FetchThemesError;
 import org.wordpress.android.fluxc.store.ThemeStore.FetchedThemesPayload;
+import org.wordpress.android.fluxc.utils.ThemeUtils;
 import org.wordpress.android.util.AppLog;
+
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -50,6 +57,30 @@ public class ThemeRestClient extends BaseWPComRestClient {
                                 new FetchThemesError(ThemeErrorType.GENERIC_ERROR, "Error fetching WP.com themes");
                         FetchedThemesPayload payload = new FetchedThemesPayload(themeError);
                         mDispatcher.dispatch(ThemeActionBuilder.newFetchedWpThemesAction(payload));
+                    }
+                }));
+    }
+
+    /** Endpoint: v1/site/$siteId/themes */
+    public void fetchJetpackInstalledThemes(@NonNull final SiteModel site) {
+        String url = WPCOMREST.sites.site(site.getSiteId()).themes.getUrlV1();
+        add(WPComGsonRequest.buildGetRequest(url, null, MultipleJetpackThemesResponse.class,
+                new Response.Listener<MultipleJetpackThemesResponse>() {
+                    @Override
+                    public void onResponse(MultipleJetpackThemesResponse response) {
+                        AppLog.d(AppLog.T.API, "Received response to Jetpack installed themes fetch request.");
+                        List<ThemeModel> themes = ThemeUtils.createThemeListFromJetpackResponse(response);
+                        FetchedThemesPayload payload = new FetchedThemesPayload(site, themes);
+                        mDispatcher.dispatch(ThemeActionBuilder.newFetchedInstalledThemesAction(payload));
+                    }
+                }, new BaseRequest.BaseErrorListener() {
+                    @Override
+                    public void onErrorResponse(@NonNull BaseRequest.BaseNetworkError error) {
+                        FetchThemesError themeError = new FetchThemesError(ThemeErrorType.GENERIC_ERROR,
+                                "Error fetching installed themes for Jetpack site.");
+                        FetchedThemesPayload payload = new FetchedThemesPayload(themeError);
+                        mDispatcher.dispatch(ThemeActionBuilder.newFetchedInstalledThemesAction(payload));
+                        AppLog.d(AppLog.T.API, "Received error response to Jetpack installed themes fetch request.");
                     }
                 }));
     }
