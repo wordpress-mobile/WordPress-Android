5d9d9be5c97d0fee09a526ac4fcd2c9fce0fa96d
Tony Rankin <tonyr@tonyrankin.com>
2017-08-28 17:01:34 -0700

    Handle fetch/fetched actions in ThemeStore

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/ThemeStore.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/ThemeStore.java
@@ -6,6 +6,7 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.Payload;
+import org.wordpress.android.fluxc.action.ThemeAction;
 import org.wordpress.android.fluxc.annotations.action.Action;
 import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.model.SiteModel;
@@ -89,10 +90,65 @@ public class ThemeStore extends Store {
     @Subscribe(threadMode = ThreadMode.ASYNC)
     @Override
     public void onAction(Action action) {
+        IAction actionType = action.getType();
+        if (!(actionType instanceof ThemeAction)) {
+            return;
+        }
+        switch ((ThemeAction) actionType) {
+            case FETCH_WP_THEMES:
+                fetchWpThemes();
+                break;
+            case FETCHED_WP_THEMES:
+                handleWpThemesFetched((FetchedThemesPayload) action.getPayload());
+                break;
+            case FETCH_INSTALLED_THEMES:
+                fetchInstalledThemes((SiteModel) action.getPayload());
+                break;
+            case FETCHED_INSTALLED_THEMES:
+                handleInstalledThemesFetched((FetchedThemesPayload) action.getPayload());
+                break;
+            case FETCH_CURRENT_THEME:
+                fetchCurrentTheme((SiteModel) action.getPayload());
+                break;
+            case FETCHED_CURRENT_THEME:
+                handleCurrentThemeFetched((FetchedCurrentThemePayload) action.getPayload());
+                break;
+        }
     }
 
     @Override
     public void onRegister() {
         AppLog.d(AppLog.T.API, "ThemeStore onRegister");
+    }
+
+    private void fetchWpThemes() {
+    }
+
+    private void handleWpThemesFetched(FetchedThemesPayload payload) {
+        OnThemesChanged event = new OnThemesChanged(payload.site);
+        if (payload.isError()) {
+            event.error = payload.error;
+    }
+
+    private void fetchInstalledThemes(@NonNull SiteModel site) {
+    }
+
+    private void handleInstalledThemesFetched(FetchedThemesPayload payload) {
+        OnThemesChanged event = new OnThemesChanged(payload.site);
+        if (payload.isError()) {
+            event.error = payload.error;
+        }
+        emitChange(event);
+    }
+
+    private void fetchCurrentTheme(@NonNull SiteModel site) {
+    }
+
+    private void handleCurrentThemeFetched(FetchedCurrentThemePayload payload) {
+        OnCurrentThemeFetched event = new OnCurrentThemeFetched(payload.site, payload.theme);
+        if (payload.isError()) {
+            event.error = payload.error;
+        }
+        emitChange(event);
     }
 }
