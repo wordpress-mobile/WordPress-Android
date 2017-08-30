f42133cb0b468ce6e38adde2b13ce4ac15491ce5
Tony Rankin <tonyr@tonyrankin.com>
2017-08-29 18:34:24 -0700

    Remove unused inner class

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/theme/ThemeWPComResponse.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/theme/ThemeWPComResponse.java
@@ -8,12 +8,6 @@ public class ThemeWPComResponse {
         public int count;
     }
 
-    public class Price {
-        public float value;
-        public String currency;
-        public String display;
-    }
-
     public String id;
     public String slug;
     public String stylesheet;
