023882b0b45c9319d58619140d63b68cbfc41366
theck13 <theck13@hotmail.com>
2017-08-31 14:31:44 -0600

    Remove generic error methods from login listener interface

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
@@ -358,11 +358,6 @@ public class LoginActivity extends AppCompatActivity implements ConnectionCallba
     }
 
     @Override
-    public void helpErrorGeneric(String email) {
-        launchHelpshift(null, email, true, Tag.ORIGIN_LOGIN_ERROR_GENERIC);
-    }
-
-    @Override
     public void helpMagicLinkRequest(String email) {
         launchHelpshift(null, email, true, Tag.ORIGIN_LOGIN_MAGIC_LINK);
     }
