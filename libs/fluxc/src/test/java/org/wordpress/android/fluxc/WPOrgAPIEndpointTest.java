25d8b40f4da026e50609d62359202a6e05bdae9e
Oguz Kocer <oguz.kocer@automattic.com>
2017-09-06 17:35:38 +0300

    Add general plugins info endpoint to wporg api endpoints

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/WPOrgAPIEndpointTest.java
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/WPOrgAPIEndpointTest.java
@@ -13,11 +13,14 @@ public class WPOrgAPIEndpointTest {
     public void testAllEndpoints() {
         // Plugins info
         assertEquals("/plugins/info/1.0/akismet/", WPORGAPI.plugins.info.version("1.0").slug("akismet").getEndpoint());
+        assertEquals("/plugins/info/1.1/", WPORGAPI.plugins.info.version("1.1").getEndpoint());
     }
 
     @Test
     public void testUrls() {
         assertEquals("https://api.wordpress.org/plugins/info/1.0/akismet.json",
                 WPORGAPI.plugins.info.version("1.0").slug("akismet").getUrl());
+        assertEquals("https://api.wordpress.org/plugins/info/1.1/",
+                WPORGAPI.plugins.info.version("1.1").getUrl());
     }
 }
