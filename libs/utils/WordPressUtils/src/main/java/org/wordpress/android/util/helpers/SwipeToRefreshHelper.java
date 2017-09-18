02020c593d1184c75a7abdaa83fe85613a70d3b3
theck13 <theck13@hotmail.com>
2017-09-18 16:48:02 -0700

    Add comments to constructor and initialization methods for swipe refresh layout

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/helpers/SwipeToRefreshHelper.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/helpers/SwipeToRefreshHelper.java
@@ -17,15 +17,52 @@ public class SwipeToRefreshHelper implements OnRefreshListener {
         public void onRefreshStarted();
     }
 
+    /**
+     * Helps {@link org.wordpress.android.util.widgets.CustomSwipeRefreshLayout} by passing the
+     * {@link android.support.v4.widget.SwipeRefreshLayout}, {@link RefreshListener}, and color.
+     *
+     * @param context               {@link Context} in which this layout is used.
+     * @param swipeRefreshLayout    {@link CustomSwipeRefreshLayout} for refreshing the contents
+     *                              of a view via a vertical swipe gesture.
+     * @param listener              {@link RefreshListener} notified when a refresh is triggered
+     *                              via the swipe gesture.
+     *
+     * @deprecated Use {@link #SwipeToRefreshHelper(CustomSwipeRefreshLayout, RefreshListener, int...)} instead.
+     */
+    @Deprecated
     public SwipeToRefreshHelper(Context context, CustomSwipeRefreshLayout swipeRefreshLayout, RefreshListener listener) {
         init(swipeRefreshLayout, listener, android.R.color.holo_blue_dark);
     }
 
+    /**
+     * Helps {@link org.wordpress.android.util.widgets.CustomSwipeRefreshLayout} by passing the
+     * {@link android.support.v4.widget.SwipeRefreshLayout}, {@link RefreshListener}, and color(s).
+     *
+     * @param swipeRefreshLayout    {@link CustomSwipeRefreshLayout} for refreshing the contents
+     *                              of a view via a vertical swipe gesture.
+     * @param listener              {@link RefreshListener} notified when a refresh is triggered
+     *                              via the swipe gesture.
+     * @param colorResIds           Comma-separated color resource integers used in the progress
+     *                              animation. The first color will also be the color of the bar
+     *                              that grows in response to a user swipe gesture.
+     */
     public SwipeToRefreshHelper(CustomSwipeRefreshLayout swipeRefreshLayout, RefreshListener listener,
                                 @ColorRes int... colorResIds) {
         init(swipeRefreshLayout, listener, colorResIds);
     }
 
+    /**
+     * Initializes {@link org.wordpress.android.util.widgets.CustomSwipeRefreshLayout} by assigning
+     * {@link android.support.v4.widget.SwipeRefreshLayout}, {@link RefreshListener}, and color(s).
+     *
+     * @param swipeRefreshLayout    {@link CustomSwipeRefreshLayout} for refreshing the contents
+     *                              of a view via a vertical swipe gesture.
+     * @param listener              {@link RefreshListener} notified when a refresh is triggered
+     *                              via the swipe gesture.
+     * @param colorResIds           Comma-separated color resource integers used in the progress
+     *                              animation. The first color will also be the color of the bar
+     *                              that grows in response to a user swipe gesture.
+     */
     public void init(CustomSwipeRefreshLayout swipeRefreshLayout, RefreshListener listener,
                      @ColorRes int... colorResIds) {
         mRefreshListener = listener;
