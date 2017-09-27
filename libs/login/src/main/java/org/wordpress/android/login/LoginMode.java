8c68b27a728360dc79eb012ff68e6549cf0ad0c9
Alex Forcier <alex.forcier@automattic.com>
2017-09-27 17:25:22 +0300

    Move LoginMode to library

---
+++ libs/login/src/main/java/org/wordpress/android/login/LoginMode.java
@@ -0,0 +1,25 @@
+package org.wordpress.android.login;
+
+import android.content.Intent;
+
+public enum LoginMode {
+    FULL,
+    SELFHOSTED_ONLY,
+    JETPACK_STATS,
+    WPCOM_LOGIN_DEEPLINK,
+    WPCOM_REAUTHENTICATE;
+
+    private static final String ARG_LOGIN_MODE = "ARG_LOGIN_MODE";
+
+    public static LoginMode fromIntent(Intent intent) {
+        if (intent.hasExtra(ARG_LOGIN_MODE)) {
+            return LoginMode.valueOf(intent.getStringExtra(ARG_LOGIN_MODE));
+        } else {
+            return FULL;
+        }
+    }
+
+    public void putInto(Intent intent) {
+        intent.putExtra(ARG_LOGIN_MODE, this.name());
+    }
+}
