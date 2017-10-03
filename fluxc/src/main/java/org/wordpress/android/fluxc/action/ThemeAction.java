1e433df4001592355886a35654807847056a6c30
Tony Rankin <tonyr@tonyrankin.com>
2017-10-03 13:08:37 -0700

    Update payloads to remove site requirement from theme search

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/ThemeAction.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/ThemeAction.java
@@ -5,6 +5,7 @@ import org.wordpress.android.fluxc.annotations.ActionEnum;
 import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.store.ThemeStore.SearchThemesPayload;
+import org.wordpress.android.fluxc.store.ThemeStore.SearchedThemesPayload;
 import org.wordpress.android.fluxc.store.ThemeStore.FetchedCurrentThemePayload;
 import org.wordpress.android.fluxc.store.ThemeStore.FetchedThemesPayload;
 import org.wordpress.android.fluxc.store.ThemeStore.ActivateThemePayload;
@@ -36,7 +37,7 @@ public enum ThemeAction implements IAction {
     FETCHED_PURCHASED_THEMES,
     @Action(payloadType = FetchedCurrentThemePayload.class)
     FETCHED_CURRENT_THEME,
-    @Action(payloadType = FetchedThemesPayload.class)
+    @Action(payloadType = SearchedThemesPayload.class)
     SEARCHED_THEMES,
     @Action(payloadType = ActivateThemePayload.class)
     ACTIVATED_THEME,
