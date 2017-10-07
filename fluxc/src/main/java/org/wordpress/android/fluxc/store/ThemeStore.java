2e5a37704940ded41a4299a2c41aba68b49b8704
Tony Rankin <tonyr@tonyrankin.com>
2017-10-06 18:58:40 -0700

    Add origin action to OnThemesChanged event

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/ThemeStore.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/ThemeStore.java
@@ -137,6 +137,7 @@ public class ThemeStore extends Store {
 
     public static class OnThemesChanged extends OnChanged<FetchThemesError> {
         public SiteModel site;
+        public ThemeAction origin;
 
         public OnThemesChanged(SiteModel site) {
             this.site = site;
@@ -267,6 +268,7 @@ public class ThemeStore extends Store {
 
     private void handleWpThemesFetched(@NonNull FetchedThemesPayload payload) {
         OnThemesChanged event = new OnThemesChanged(payload.site);
+        event.origin = ThemeAction.FETCH_WP_COM_THEMES;
         if (payload.isError()) {
             event.error = payload.error;
         } else {
@@ -287,6 +289,7 @@ public class ThemeStore extends Store {
 
     private void handleInstalledThemesFetched(@NonNull FetchedThemesPayload payload) {
         OnThemesChanged event = new OnThemesChanged(payload.site);
+        event.origin = ThemeAction.FETCH_INSTALLED_THEMES;
         if (payload.isError()) {
             event.error = payload.error;
         } else {
