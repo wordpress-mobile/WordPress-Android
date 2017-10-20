e3a30ad70c8f1ebc840523de48f523dbd6a45025
Mario Zorz <mariozorz@gmail.com>
2017-10-20 12:07:45 -0300

    removed unused import

--- WordPress/src/main/java/org/wordpress/android/ui/uploads/PostUploadNotifier.java
+++ WordPress/src/main/java/org/wordpress/android/ui/uploads/PostUploadNotifier.java
@@ -5,7 +5,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
