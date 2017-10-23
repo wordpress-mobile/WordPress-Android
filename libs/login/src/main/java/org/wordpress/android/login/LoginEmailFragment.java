102a1e0f502a445dc4b5e77a091c2922644bef08
Alex Forcier <alex.forcier@automattic.com>
2017-10-23 09:16:17 -0400

    Merge branch 'develop' into feature/extract-login-flow
    # Conflicts:
    #	WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
    #	WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginEpilogueFragment.java
    #	libs/login/WordPressLoginFlow/src/main/java/org/wordpress/android/login/LoginSiteAddressFragment.java

--- libs/login/src/main/java/org/wordpress/android/login/LoginEmailFragment.java
+++ libs/login/src/main/java/org/wordpress/android/login/LoginEmailFragment.java
@@ -58,6 +58,9 @@ public class LoginEmailFragment extends LoginBaseFormFragment<LoginListener> imp
             case WPCOM_LOGIN_DEEPLINK:
                 label.setText(R.string.login_log_in_for_deeplink);
                 break;
+            case SHARE_INTENT:
+                label.setText(R.string.login_log_in_for_share_intent);
+                break;
             case FULL:
                 label.setText(R.string.enter_email_wordpress_com);
                 break;
@@ -97,6 +100,7 @@ public class LoginEmailFragment extends LoginBaseFormFragment<LoginListener> imp
 
         switch (mLoginListener.getLoginMode()) {
             case FULL:
+            case SHARE_INTENT:
                 // all features enabled and with typical values
                 secondaryButton.setText(R.string.enter_site_address_instead);
                 break;
