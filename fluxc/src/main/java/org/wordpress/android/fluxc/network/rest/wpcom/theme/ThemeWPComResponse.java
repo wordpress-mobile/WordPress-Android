ee19307df7197481e4c6019b5b49f1e97a8ba034
Tony Rankin <tonyr@tonyrankin.com>
2017-11-17 13:01:33 -0800

    Remove unused fields from response models, suppress access warnings

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/theme/ThemeWPComResponse.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/theme/ThemeWPComResponse.java
@@ -3,6 +3,7 @@ package org.wordpress.android.fluxc.network.rest.wpcom.theme;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("WeakerAccess")
 public class ThemeWPComResponse {
     public class MultipleWPComThemesResponse {
         public Map<String, ThemeWPComResponse> themes;
@@ -22,14 +23,8 @@ public class ThemeWPComResponse {
     public String theme_uri;
     public String demo_uri;
     public String version;
-    public String template;
     public String screenshot;
     public String description;
-    public String date_launched;
-    public String date_updated;
-    public String language;
     public String download_uri;
     public String price;
-    public int rank_popularity;
-    public int rank_trending;
 }
