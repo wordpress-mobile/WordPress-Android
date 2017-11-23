00a7b7aa692940431d0d819778d439c0f18314d3
Alex Forcier <alex.forcier@automattic.com>
2017-11-23 09:06:42 +0900

    Eliminate Class-based GET request constructor

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/jetpacktunnel/WPComJPTunnelGsonRequest.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/jetpacktunnel/WPComJPTunnelGsonRequest.kt
@@ -6,7 +6,6 @@ import org.wordpress.android.fluxc.generated.endpoint.WPCOMREST
 import org.wordpress.android.fluxc.network.BaseRequest.BaseErrorListener
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequest
 import java.lang.reflect.Type
-import kotlin.reflect.KClass
 
 /**
  * A request making a WP-API call on a Jetpack site via the WordPress.com <code>/jetpack-blogs/$site/rest-api</code>
@@ -30,31 +29,6 @@ object WPComJPTunnelGsonRequest {
     ): WPComGsonRequest<JPTunnelWPComRestResponse<T>>? {
         val tunnelRequestUrl = getTunnelApiUrl(siteId)
         val wrappedType = TypeToken.getParameterized(JPTunnelWPComRestResponse::class.java, type).type
-        val wrappedListener = Response.Listener<JPTunnelWPComRestResponse<T>> { listener(it.data) }
-
-        val wrappedParams = createTunnelParams(params, wpApiEndpoint)
-
-        return WPComGsonRequest.buildGetRequest(tunnelRequestUrl, wrappedParams, wrappedType,
-                wrappedListener, errorListener)
-    }
-
-    /**
-     * Creates a new GET request to the given WP-API endpoint, calling it via the WP.com Jetpack WP-API tunnel.
-     *
-     * @param wpApiEndpoint the WP-API request endpoint (e.g. /wp/v2/posts/)
-     * @param siteId the WordPress.com site ID
-     * @param params the parameters to append to the request URL
-     * @param clazz the class defining the expected response
-     * @param listener the success listener
-     * @param errorListener the error listener
-     *
-     * @param T the expected response object from the WP-API endpoint
-     */
-    fun <T : Any> buildGetRequest(wpApiEndpoint: String, siteId: Long, params: Map<String, String>,
-                                  clazz: KClass<T>, listener: (T?) -> Unit, errorListener: BaseErrorListener
-    ): WPComGsonRequest<JPTunnelWPComRestResponse<T>>? {
-        val tunnelRequestUrl = getTunnelApiUrl(siteId)
-        val wrappedType = TypeToken.getParameterized(JPTunnelWPComRestResponse::class.java, clazz.java).type
         val wrappedListener = Response.Listener<JPTunnelWPComRestResponse<T>> { listener(it.data) }
 
         val wrappedParams = createTunnelParams(params, wpApiEndpoint)
