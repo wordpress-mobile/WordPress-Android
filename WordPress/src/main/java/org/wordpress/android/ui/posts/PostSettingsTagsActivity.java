7a337b783a9e0ef3ec9303a13da1d4067b7341f0
Alex Forcier <alex.forcier@automattic.com>
2017-11-30 12:49:28 +1100

    Move forced hideKeyboard method to utils library

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostSettingsTagsActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostSettingsTagsActivity.java
@@ -30,8 +30,8 @@ import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.model.TermModel;
 import org.wordpress.android.fluxc.store.TaxonomyStore;
+import org.wordpress.android.util.ActivityUtils;
 import org.wordpress.android.util.ToastUtils;
-import org.wordpress.android.util.WPActivityUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -131,7 +131,7 @@ public class PostSettingsTagsActivity extends AppCompatActivity implements TextW
     }
 
     private void saveAndFinish() {
-        WPActivityUtils.hideKeyboard(mTagsEditText);
+        ActivityUtils.hideKeyboardForced(mTagsEditText);
 
         Bundle bundle = new Bundle();
         bundle.putString(KEY_SELECTED_TAGS, mTagsEditText.getText().toString());
--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/ActivityUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/ActivityUtils.java
@@ -2,6 +2,8 @@ package org.wordpress.android.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 public class ActivityUtils {
@@ -12,5 +14,12 @@ public class ActivityUtils {
             inputManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(),
                     InputMethodManager.HIDE_NOT_ALWAYS);
         }
+    }
+
+    public static void hideKeyboardForced(@Nullable final View view) {
+        if (view == null) return;
+        InputMethodManager inputMethodManager = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
