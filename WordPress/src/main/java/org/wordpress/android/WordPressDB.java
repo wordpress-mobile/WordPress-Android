cb4e1fe10deb5e81380fa50c752276478d95e234
Maxime Biais <maxime.biais@gmail.com>
2017-12-21 14:54:55 +0100

    Enable Aztec for all users when they upgrade to the latest version

--- WordPress/src/main/java/org/wordpress/android/WordPressDB.java
+++ WordPress/src/main/java/org/wordpress/android/WordPressDB.java
@@ -20,7 +20,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class WordPressDB {
-    private static final int DATABASE_VERSION = 59;
+    private static final int DATABASE_VERSION = 61;
 
     // Warning if you rename DATABASE_NAME, that could break previous App backups (see: xml/backup_scheme.xml)
     private static final String DATABASE_NAME = "wordpress";
@@ -194,6 +194,15 @@ public class WordPressDB {
             case 58:
                 // ThemeStore merged, remove deprecated themes tables
                 db.execSQL(DROP_TABLE_PREFIX + THEMES_TABLE);
+                currentVersion++;
+            case 59:
+                // add Start of Week site setting as part of #betterjetpackxp
+                db.execSQL(SiteSettingsModel.ADD_START_OF_WEEK);
+                currentVersion++;
+            case 60:
+                // Enable Aztec for all users
+                AppPrefs.setVisualEditorEnabled(false);
+                AppPrefs.setAztecEditorEnabled(true);
                 currentVersion++;
         }
         db.setVersion(DATABASE_VERSION);
