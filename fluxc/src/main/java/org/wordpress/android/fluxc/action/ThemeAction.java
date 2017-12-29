ad89ab54f43676da09d232625c0abccc6fb06947
Oguz Kocer <oguz.kocer@automattic.com>
2017-12-29 15:01:58 -0500

    ActivateThemePayload renamed as SiteThemePayload

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/ThemeAction.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/ThemeAction.java
@@ -5,7 +5,7 @@ import org.wordpress.android.fluxc.annotations.ActionEnum;
 import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.model.ThemeModel;
-import org.wordpress.android.fluxc.store.ThemeStore.ActivateThemePayload;
+import org.wordpress.android.fluxc.store.ThemeStore.SiteThemePayload;
 import org.wordpress.android.fluxc.store.ThemeStore.FetchedCurrentThemePayload;
 import org.wordpress.android.fluxc.store.ThemeStore.FetchedSiteThemesPayload;
 import org.wordpress.android.fluxc.store.ThemeStore.FetchedWpComThemesPayload;
@@ -21,11 +21,11 @@ public enum ThemeAction implements IAction {
     FETCH_PURCHASED_THEMES,
     @Action(payloadType = SiteModel.class)
     FETCH_CURRENT_THEME,
-    @Action(payloadType = ActivateThemePayload.class)
+    @Action(payloadType = SiteThemePayload.class)
     ACTIVATE_THEME,
-    @Action(payloadType = ActivateThemePayload.class)
+    @Action(payloadType = SiteThemePayload.class)
     INSTALL_THEME,
-    @Action(payloadType = ActivateThemePayload.class)
+    @Action(payloadType = SiteThemePayload.class)
     DELETE_THEME,
 
     // Remote responses
@@ -37,11 +37,11 @@ public enum ThemeAction implements IAction {
     FETCHED_PURCHASED_THEMES,
     @Action(payloadType = FetchedCurrentThemePayload.class)
     FETCHED_CURRENT_THEME,
-    @Action(payloadType = ActivateThemePayload.class)
+    @Action(payloadType = SiteThemePayload.class)
     ACTIVATED_THEME,
-    @Action(payloadType = ActivateThemePayload.class)
+    @Action(payloadType = SiteThemePayload.class)
     INSTALLED_THEME,
-    @Action(payloadType = ActivateThemePayload.class)
+    @Action(payloadType = SiteThemePayload.class)
     DELETED_THEME,
 
     // Local actions
