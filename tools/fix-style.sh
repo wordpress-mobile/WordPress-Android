f4b13b5de4939b52817010284e96d9b4aa564791
Alex Forcier <alex.forcier@automattic.com>
2018-01-03 11:49:00 +1100

    Add style correction functions

--- tools/fix-style.sh
+++ tools/fix-style.sh
@@ -37,6 +37,23 @@ function remove_empty_lines_before_brace() {
     perl -i -0pe 's/\r?\n(\r?\n\s*})/$1/g' $1
 }
 
+function add_space_between_bracket_and_brace() {
+    # perl -i -pe 's/\){/\) {/g' $1
+    perl -i -pe 's/\){\r?\n/\) {\n/g' $1
+}
+
+function add_space_around_loop_equals() {
+    perl -i -pe 's/int i\=0;/int i \= 0;/g' $1
+}
+
+function add_space_around_equality_operator() {
+    perl -i -pe 's/(?<=[a-z0-9)])\=\=(?=[a-z0-9])/ == /g' $1
+}
+
+function add_space_after_switch() {
+    perl -i -pe 's/switch\(/switch \(/g' $1
+}
+
 for filename in $(find WordPress/src -type f -iname "*.java"); do
     fix_line_comments $filename
     move_binary_operators $filename
@@ -46,5 +63,9 @@ for filename in $(find WordPress/src -type f -iname "*.java"); do
     add_white_space_after_if $filename
     remove_empty_lines_after_brace $filename
     remove_empty_lines_before_brace $filename
+    add_space_between_bracket_and_brace $filename
+    add_space_around_loop_equals $filename
+    add_space_around_equality_operator $filename
+    add_space_after_switch $filename
 done
 
