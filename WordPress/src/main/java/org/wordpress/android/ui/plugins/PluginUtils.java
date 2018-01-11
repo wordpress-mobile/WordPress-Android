eba9646b0f84a1101c5109adf4c1e4c39b9a8ca8
Nick Bradbury <nick.bradbury@gmail.com>
2018-01-11 15:54:10 -0500

    Strip "-alpha" or "-beta" from the version

--- WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginUtils.java
+++ WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginUtils.java
@@ -15,11 +15,18 @@ public class PluginUtils {
         String jetpackVersion = site.getJetpackVersion();
         if (site.isUsingWpComRestApi() && site.isJetpackConnected() && !TextUtils.isEmpty(jetpackVersion)) {
             try {
+                jetpackVersion = "5.6-alpha";
+                // strip any trailing "-beta" or "-alpha" from the version
+                int index = jetpackVersion.lastIndexOf("-");
+                if (index > 0) {
+                    jetpackVersion = jetpackVersion.substring(0, index);
+                }
                 Version siteJetpackVersion = new Version(jetpackVersion);
                 Version minVersion = new Version("5.6");
                 return siteJetpackVersion.compareTo(minVersion) >= 0; // if the site has Jetpack 5.6 or newer installed
             } catch (IllegalArgumentException e) {
                 AppLog.e(AppLog.T.UTILS, "Invalid site jetpack version " + jetpackVersion, e);
+                return true;
             }
         }
         return false;
