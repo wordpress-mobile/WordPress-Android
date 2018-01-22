0027bc45aee1258655ed3f7e2c93c9fbe4e783f1
Maxime Biais <maxime.biais@gmail.com>
2018-01-22 08:31:40 +0100

    Let the user specify the branches

--- tools/update-release-names.sh
+++ tools/update-release-names.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 if [ x"$4" == x ]; then
-  echo "Usage:   $0 version-code current-version beta-version alpha-version"
+  echo "Usage:   $0 version-code current-version beta-version alpha-version [current-branch] [beta-branch] [alpha-branch]"
   echo "Example: $0 514 9.1 9.2-rc-2 alpha-93"
+  echo "Example: $0 517 9.1.1 9.2-rc-3 alpha-94 hotfix/9.1.1 release/9.2 develop"
   exit 1
 fi
 
@@ -11,11 +12,30 @@ CURRENT_VERSION=$2
 BETA_VERSION=$3
 ALPHA_VERSION=$4
 
+if [ x"$5" == x ]; then
+    CURRENT_BRANCH=release/$CURRENT_VERSION
+else
+    CURRENT_BRANCH=$5
+fi
+
+if [ x"$6" == x ]; then
+    BETA_BRANCH=release/`echo $BETA_VERSION | cut -d- -f1`
+else
+    BETA_BRANCH=$6
+fi
+
+if [ x"$7" == x ]; then
+    ALPHA_BRANCH=develop
+else
+    ALPHA_BRANCH=$7
+fi
+
 BETA_VERSION_CODE=$((CURRENT_VERSION_CODE + 1))
 ALPHA_VERSION_CODE=$((CURRENT_VERSION_CODE + 2))
 BUILD_FILE=WordPress/build.gradle
 LOGFILE=/tmp/update-release-names.log
 echo > $LOGFILE
+
 
 function edit_build_file_commit() {
     echo "Editing $BUILD_FILE - name: $2 code: $1"
@@ -32,11 +52,11 @@ function switch_branch_pull() {
     git pull origin $1 >> $LOGFILE 2>> $LOGFILE
 }
 
-switch_branch_pull release/$CURRENT_VERSION
+switch_branch_pull $CURRENT_BRANCH
 edit_build_file_commit $CURRENT_VERSION_CODE $CURRENT_VERSION
 
-switch_branch_pull release/`echo $BETA_VERSION | cut -d- -f1`
+switch_branch_pull $BETA_BRANCH
 edit_build_file_commit $BETA_VERSION_CODE $BETA_VERSION
 
-switch_branch_pull develop
+switch_branch_pull $ALPHA_BRANCH
 edit_build_file_commit $ALPHA_VERSION_CODE $ALPHA_VERSION
