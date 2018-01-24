02ea5a7b898b6c6ca1ac77a093cb7a51b5727649
Nick Bradbury <nick.bradbury@gmail.com>
2018-01-24 12:06:03 -0500

    Rewrote PluginList.isSameList() to do more accurate comparison of items

--- WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginList.java
+++ WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginList.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 
 import org.wordpress.android.fluxc.model.plugin.SitePluginModel;
 import org.wordpress.android.fluxc.model.plugin.WPOrgPluginModel;
+import org.wordpress.android.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,19 +46,40 @@ class PluginList extends ArrayList<Object> {
         return -1;
     }
 
+    /*
+     * note this is not the same as an "equals" comparison as it only checks a few fields, which
+     * is sufficient for our purposes here
+     */
+    private boolean isSameItem(Object item1, Object item2) {
+        if (item1 instanceof SitePluginModel && item2 instanceof SitePluginModel) {
+            SitePluginModel plugin1 = (SitePluginModel) item1;
+            SitePluginModel plugin2 = (SitePluginModel) item2;
+            return StringUtils.equals(plugin1.getSlug(), plugin2.getSlug())
+                    && plugin1.getLocalSiteId() == plugin2.getLocalSiteId()
+                    && plugin1.isActive() == plugin2.isActive()
+                    && plugin1.isAutoUpdateEnabled() == plugin2.isAutoUpdateEnabled()
+                    && StringUtils.equals(plugin1.getVersion(), plugin2.getVersion());
+        } else if (item1 instanceof WPOrgPluginModel && item2 instanceof WPOrgPluginModel) {
+            WPOrgPluginModel plugin1 = (WPOrgPluginModel) item1;
+            WPOrgPluginModel plugin2 = (WPOrgPluginModel) item2;
+            return StringUtils.equals(plugin1.getSlug(), plugin2.getSlug())
+                    && plugin1.getDownloadCount() == plugin2.getDownloadCount()
+                    && plugin1.getNumberOfRatings() == plugin2.getNumberOfRatings()
+                    && StringUtils.equals(plugin1.getRating(), plugin2.getRating())
+                    && StringUtils.equals(plugin1.getVersion(), plugin2.getVersion());
+
+        } else {
+            return false;
+        }
+    }
+
     boolean isSameList(@NonNull List<?> items) {
         if (this.size() != items.size()) {
             return false;
         }
-        for (Object item : items) {
-            if (item instanceof WPOrgPluginModel) {
-                if (indexOfPluginWithSlug(((WPOrgPluginModel) item).getSlug()) == -1) {
-                    return false;
-                }
-            } else if (item instanceof SitePluginModel) {
-                if (indexOfPluginWithSlug(((SitePluginModel) item).getSlug()) == -1) {
-                    return false;
-                }
+        for (int i = 0; i < items.size(); i++) {
+            if (!isSameItem(items.get(i), this.get(i))) {
+                return false;
             }
         }
         return true;
