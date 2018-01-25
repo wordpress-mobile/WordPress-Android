1ebe713ce178f5a95d62666d21bd0fc93da46006
Maxime Biais <maxime.biais@gmail.com>
2018-01-25 14:04:13 +0100

    Check current version code is greater than pervious alpha

--- tools/update-release-names.sh
+++ tools/update-release-names.sh
@@ -36,6 +36,17 @@ BUILD_FILE=WordPress/build.gradle
 LOGFILE=/tmp/update-release-names.log
 echo > $LOGFILE
 
+function check_version_code() {
+    git checkout develop >> $LOGFILE 2>> $LOGFILE
+    previous_alpha_version_code=`grep -E 'versionCode' $BUILD_FILE | grep -Eo "[0-9]+"`
+    if [ $CURRENT_VERSION_CODE -gt $previous_alpha_version_code ]; then
+        echo "Current version code ($CURRENT_VERSION_CODE) should be greater than previous alpha version code ($previous_alpha_version_code)"
+        exit 2
+    else
+        echo "Current version code: $CURRENT_VERSION_CODE - previous alpha version code: $previous_alpha_version_code"
+    fi
+    git checkout - >> $LOGFILE 2>> $LOGFILE
+}
 
 function edit_build_file_commit() {
     echo "Editing $BUILD_FILE - name: $2 code: $1"
@@ -51,6 +62,8 @@ function switch_branch_pull() {
     git checkout $1 >> $LOGFILE 2>> $LOGFILE
     git pull origin $1 >> $LOGFILE 2>> $LOGFILE
 }
+
+check_version_code
 
 switch_branch_pull $CURRENT_BRANCH
 edit_build_file_commit $CURRENT_VERSION_CODE $CURRENT_VERSION
