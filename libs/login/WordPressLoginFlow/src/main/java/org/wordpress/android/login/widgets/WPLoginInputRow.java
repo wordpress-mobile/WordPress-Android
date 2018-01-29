6a0a37288e25706a903723d319d5e8af6eadfaff
malinajirka <malinajirka@gmail.com>
2018-01-29 10:33:08 +0100

    Remove unused import in WpLoginInputRow and Fix email textAlignment on login screen

--- libs/login/WordPressLoginFlow/src/main/java/org/wordpress/android/login/widgets/WPLoginInputRow.java
+++ libs/login/WordPressLoginFlow/src/main/java/org/wordpress/android/login/widgets/WPLoginInputRow.java
@@ -6,7 +6,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.text.TextWatcher;
