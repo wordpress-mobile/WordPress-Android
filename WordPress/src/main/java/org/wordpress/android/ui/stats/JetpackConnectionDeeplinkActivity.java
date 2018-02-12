bc34ea445416387d606049f5c99eaa68e6d756ee
vojtasmrcek <planarvoid@gmail.com>
2018-02-12 15:08:10 +0100

    Add Jetpack connection to stats without WP.com account

--- WordPress/src/main/java/org/wordpress/android/ui/stats/JetpackConnectionDeeplinkActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/JetpackConnectionDeeplinkActivity.java
@@ -20,6 +20,7 @@ import org.wordpress.android.fluxc.store.AccountStore;
 import org.wordpress.android.fluxc.store.SiteStore;
 import org.wordpress.android.login.LoginMode;
 import org.wordpress.android.ui.ActivityLauncher;
+import org.wordpress.android.ui.JetpackConnectionWebViewActivity.Source;
 import org.wordpress.android.ui.RequestCodes;
 import org.wordpress.android.ui.accounts.LoginActivity;
 import org.wordpress.android.util.AnalyticsUtils;
@@ -37,8 +38,9 @@ import static org.wordpress.android.ui.RequestCodes.JETPACK_LOGIN;
  * <p>
  * Redirects users to the stats activity if the jetpack connection was succesful
  */
-public class StatsDeeplinkActivity extends AppCompatActivity {
+public class JetpackConnectionDeeplinkActivity extends AppCompatActivity {
     private String reason;
+    private Source source;
 
     @Inject
     AccountStore mAccountStore;
@@ -73,11 +75,16 @@ public class StatsDeeplinkActivity extends AppCompatActivity {
         if (Intent.ACTION_VIEW.equals(action) && uri != null) {
 
             reason = uri.getQueryParameter("reason");
+            source = Source.fromString(uri.getQueryParameter("source"));
 
             // if user is signed in wpcom show the post right away - otherwise show welcome activity
             // and then show the post once the user has signed in
             if (mAccountStore.hasAccessToken()) {
-                showStats();
+                if (source == Source.STATS) {
+                    showStats();
+                } else {
+                    finish();
+                }
             } else {
                 Intent loginIntent = new Intent(this, LoginActivity.class);
                 LoginMode.JETPACK_STATS.putInto(loginIntent);
@@ -103,7 +110,7 @@ public class StatsDeeplinkActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RequestCodes.JETPACK_LOGIN && resultCode == RESULT_OK) {
+        if (requestCode == RequestCodes.JETPACK_LOGIN && resultCode == RESULT_OK && source == Source.STATS) {
             showStats();
         } else {
             finish();
