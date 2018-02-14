2557353e4bfe9dd132315a3e0821187e4e726cee
malinajirka <malinajirka@gmail.com>
2018-02-14 08:08:12 +0100

    Remove unused imports in RtlUtils

--- WordPress/src/main/java/org/wordpress/android/util/RtlUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/RtlUtils.java
@@ -5,15 +5,12 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.support.v4.view.ViewCompat;
-import android.view.View;
-
-import org.wordpress.android.*;
 
 public class RtlUtils {
 
     public static boolean isRtl(Context ctx) {
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && ctx.getResources().getBoolean(org.wordpress.android.R.bool.rtl_supported)) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && ctx.getResources().getBoolean(
+                org.wordpress.android.R.bool.rtl_supported)) {
             Configuration configuration = ctx.getResources().getConfiguration();
             if (configuration.getLayoutDirection() == ViewCompat.LAYOUT_DIRECTION_RTL) {
                 return true;
