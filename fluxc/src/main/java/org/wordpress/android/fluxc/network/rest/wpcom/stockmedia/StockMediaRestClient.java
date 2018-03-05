fc8c682b7afb40d694064cca584522219947ae0e
Nick Bradbury <nick.bradbury@gmail.com>
2018-03-05 14:04:15 -0500

    Added custom deserializer

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/stockmedia/StockMediaRestClient.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/stockmedia/StockMediaRestClient.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 
-import org.apache.commons.text.StringEscapeUtils;
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.generated.StockMediaActionBuilder;
 import org.wordpress.android.fluxc.generated.endpoint.WPCOMREST;
@@ -21,7 +20,6 @@ import org.wordpress.android.fluxc.store.StockMediaStore;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.UrlUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Singleton;
@@ -47,10 +45,8 @@ public class StockMediaRestClient extends BaseWPComRestClient {
                 new Response.Listener<SearchStockMediaResponse>() {
                     @Override
                     public void onResponse(SearchStockMediaResponse response) {
-                        List<StockMediaModel> mediaList = getStockMediaListFromRestResponse(response);
-                        AppLog.v(AppLog.T.MEDIA, "Fetched stock media list with size: " + mediaList.size());
-                        boolean canLoadMore = mediaList.size() == number;
-                        notifyStockMediaListFetched(mediaList, searchTerm, response.getNextPage(), canLoadMore);
+                        boolean canLoadMore = response.nextPage > page;
+                        notifyStockMediaListFetched(response.media, searchTerm, response.nextPage, canLoadMore);
                     }
                 }, new BaseRequest.BaseErrorListener() {
                     @Override
@@ -79,47 +75,5 @@ public class StockMediaRestClient extends BaseWPComRestClient {
     private void notifyStockMediaListFetched(MediaStore.MediaError error) {
         StockMediaStore.FetchedStockMediaListPayload payload = new StockMediaStore.FetchedStockMediaListPayload(error);
         mDispatcher.dispatch(StockMediaActionBuilder.newFetchedStockMediaAction(payload));
-    }
-
-    /**
-     * Creates a {@link StockMediaModel} list from a WP.com REST response to a request for all media.
-     */
-    private List<StockMediaModel> getStockMediaListFromRestResponse(final SearchStockMediaResponse from) {
-        if (from == null || from.media == null) return null;
-
-        final List<StockMediaModel> mediaList = new ArrayList<>();
-        for (StockMediaResponse mediaItem : from.media) {
-            StockMediaModel mediaModel = getStockMediaFromRestResponse(mediaItem);
-            mediaList.add(mediaModel);
-        }
-        return mediaList;
-    }
-
-    private StockMediaModel getStockMediaFromRestResponse(final StockMediaResponse from) {
-        if (from == null) return null;
-
-        final StockMediaModel media = new StockMediaModel();
-
-        media.setId(from.ID);
-        media.setDate(from.date);
-        media.setExtension(from.extension);
-        media.setUrl(from.URL);
-        media.setGuid(from.guid);
-        media.setFile(from.file);
-        media.setTitle(StringEscapeUtils.unescapeHtml4(from.title));
-        media.setName(StringEscapeUtils.unescapeHtml4(from.name));
-        media.setType(from.type);
-
-        media.setHeight(from.height);
-        media.setWidth(from.width);
-
-        if (from.thumbnails != null) {
-            media.setThumbnail(from.thumbnails.thumbnail);
-            media.setLargeThumbnail(from.thumbnails.large);
-            media.setMediumThumbnail(from.thumbnails.medium);
-            media.setPostThumbnail(from.thumbnails.post_thumbnail);
-        }
-
-        return media;
     }
 }
