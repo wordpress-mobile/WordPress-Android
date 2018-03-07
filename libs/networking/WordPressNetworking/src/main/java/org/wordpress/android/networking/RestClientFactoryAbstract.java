41bf1ee228545bc59e22ec4c58f2a20a681671d4
Masked Pony <maskedpony@bia.is>
2018-03-07 13:43:38 +0100

    Fix style violations in the networking subtree

--- libs/networking/WordPressNetworking/src/main/java/org/wordpress/android/networking/RestClientFactoryAbstract.java
+++ libs/networking/WordPressNetworking/src/main/java/org/wordpress/android/networking/RestClientFactoryAbstract.java
@@ -4,6 +4,6 @@ import com.android.volley.RequestQueue;
 import com.wordpress.rest.RestClient;
 
 public interface RestClientFactoryAbstract {
-    public RestClient make(RequestQueue queue);
-    public RestClient make(RequestQueue queue, RestClient.REST_CLIENT_VERSIONS version);
+    RestClient make(RequestQueue queue);
+    RestClient make(RequestQueue queue, RestClient.REST_CLIENT_VERSIONS version);
 }
