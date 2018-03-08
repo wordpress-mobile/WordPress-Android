aee694e156bd03d15fa2717452164b58c6117cbc
Oguz Kocer <oguz.kocer@automattic.com>
2018-03-08 13:38:38 -0500

    Attempt to fix checkstyle issues in plugins

--- WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginUtils.java
+++ WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginUtils.java
@@ -17,8 +17,8 @@ public class PluginUtils {
 
     static boolean isUpdateAvailable(@Nullable ImmutablePluginModel immutablePlugin) {
         if (immutablePlugin == null
-                || TextUtils.isEmpty(immutablePlugin.getInstalledVersion())
-                || TextUtils.isEmpty(immutablePlugin.getWPOrgPluginVersion())) {
+            || TextUtils.isEmpty(immutablePlugin.getInstalledVersion())
+            || TextUtils.isEmpty(immutablePlugin.getWPOrgPluginVersion())) {
             return false;
         }
         String installedVersionStr = immutablePlugin.getInstalledVersion();
@@ -28,8 +28,9 @@ public class PluginUtils {
             Version availableVersion = new Version(availableVersionStr);
             return currentVersion.compareTo(availableVersion) == -1;
         } catch (IllegalArgumentException e) {
-            String errorStr = String.format("An IllegalArgumentException occurred while trying to compare site" +
-                    " plugin version: %s with wporg plugin version: %s", installedVersionStr, availableVersionStr);
+            String errorStr =
+                    String.format("An IllegalArgumentException occurred while trying to compare site plugin version: %s"
+                                  + " with wporg plugin version: %s", installedVersionStr, availableVersionStr);
             AppLog.e(AppLog.T.PLUGINS, errorStr, e);
             CrashlyticsUtils.logException(e, AppLog.T.PLUGINS, errorStr);
             // If the versions are not in the expected format, we can assume that an update is available if the version
