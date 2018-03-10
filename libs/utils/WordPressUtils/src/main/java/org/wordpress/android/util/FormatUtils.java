3309e3c8e1e5bb964cb3cf9096d94219ce24a210
Sergio Estevao <sergioestevao@gmail.com>
2018-03-10 23:39:52 +0000

    Fix style warnings.

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/FormatUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/FormatUtils.java
@@ -40,10 +40,10 @@ public class FormatUtils {
         if (size <= 0) {
             return "0";
         }
-        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
-        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+        final String[] units = new String[] {"B", "kB", "MB", "GB", "TB" };
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
 
-        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
 
     /*
