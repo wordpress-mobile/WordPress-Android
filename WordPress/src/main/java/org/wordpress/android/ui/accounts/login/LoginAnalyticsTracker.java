68fe71c3da90a3deb5b08093192a69a547357e29
theck13 <theck13@users.noreply.github.com>
2018-03-11 20:55:53 -0600

    Add signup Google tracking to login analytics listener interface

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginAnalyticsTracker.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginAnalyticsTracker.java
@@ -61,6 +61,22 @@ public class LoginAnalyticsTracker implements LoginAnalyticsListener {
         AnalyticsTracker.track(AnalyticsTracker.Stat.SIGNUP_EMAIL_TO_LOGIN);
     }
 
+    @Override public void trackSignupSocial2faNeeded() {
+        AnalyticsTracker.track(AnalyticsTracker.Stat.SIGNUP_SOCIAL_2FA_NEEDED);
+    }
+
+    @Override public void trackSignupSocialAccountsNeedConnecting() {
+        AnalyticsTracker.track(AnalyticsTracker.Stat.SIGNUP_SOCIAL_ACCOUNTS_NEED_CONNECTING);
+    }
+
+    @Override public void trackSignupSocialButtonFailure() {
+        AnalyticsTracker.track(AnalyticsTracker.Stat.SIGNUP_SOCIAL_BUTTON_FAILURE);
+    }
+
+    @Override public void trackSignupSocialToLogin() {
+        AnalyticsTracker.track(AnalyticsTracker.Stat.SIGNUP_SOCIAL_TO_LOGIN);
+    }
+
     @Override
     public void trackSocialAccountsNeedConnecting() {
         AnalyticsTracker.track(AnalyticsTracker.Stat.LOGIN_SOCIAL_ACCOUNTS_NEED_CONNECTING);
