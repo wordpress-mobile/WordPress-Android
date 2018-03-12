b66701c87ce6d892ab51504b8d6649ee1bc71cb7
Mario Zorz <mariozorz@gmail.com>
2018-03-12 10:09:26 -0300

    using DECIMAL_INSTANCE instead of new Decimal()

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/FormatUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/FormatUtils.java
@@ -43,7 +43,7 @@ public class FormatUtils {
         final String[] units = new String[] {"B", "kB", "MB", "GB", "TB" };
         int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
 
-        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+        return DECIMAL_INSTANCE.get().format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
 
     /*
