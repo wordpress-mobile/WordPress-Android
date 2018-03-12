a7fa66e840bf7b91a96aaa8ff4e1fbce6bc29087
theck13 <theck13@users.noreply.github.com>
2018-03-11 18:35:41 -0600

    Add sign email fragment to login fragment module

--- libs/login/src/main/java/org/wordpress/android/login/di/LoginFragmentModule.java
+++ libs/login/src/main/java/org/wordpress/android/login/di/LoginFragmentModule.java
@@ -9,6 +9,7 @@ import org.wordpress.android.login.LoginMagicLinkSentFragment;
 import org.wordpress.android.login.LoginSiteAddressFragment;
 import org.wordpress.android.login.LoginSiteAddressHelpDialogFragment;
 import org.wordpress.android.login.LoginUsernamePasswordFragment;
+import org.wordpress.android.login.SignupEmailFragment;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
@@ -41,4 +42,7 @@ public abstract class LoginFragmentModule {
 
     @ContributesAndroidInjector
     abstract LoginUsernamePasswordFragment loginUsernamePasswordFragment();
+
+    @ContributesAndroidInjector
+    abstract SignupEmailFragment signupEmailFragment();
 }
