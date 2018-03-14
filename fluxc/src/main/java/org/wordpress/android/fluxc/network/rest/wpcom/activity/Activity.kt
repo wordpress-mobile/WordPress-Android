3994214ddc3a577525ed5feb4baf4d70da61bd25
vojtasmrcek <planarvoid@gmail.com>
2018-03-14 16:08:05 +0100

    RewindStatus model + model cleanup

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/activity/Activity.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/activity/Activity.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.activity
 
+import org.wordpress.android.fluxc.store.Store
 import java.util.Date
 
 data class Activity(val activityID: String,
@@ -26,4 +27,16 @@ data class Activity(val activityID: String,
                              val role: String?) {
         val isJetpack = { type == "Application" && displayName == "Jetpack" }
     }
+
+    enum class ActivityErrorType {
+        GENERIC_ERROR,
+        AUTHORIZATION_REQUIRED,
+        INVALID_RESPONSE,
+        MISSING_ACTIVITY_ID,
+        MISSING_SUMMARY,
+        MISSING_CONTENT_TEXT,
+        MISSING_PUBLISHED_DATE
+    }
+
+    data class ActivityError(var type: ActivityErrorType, var message: String? = null) : Store.OnChangedError
 }
