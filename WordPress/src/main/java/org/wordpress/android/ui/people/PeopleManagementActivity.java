a89da83f8437167281aed87a35d43ebbb81d4b3d
malinajirka <malinajirka@gmail.com>
2018-03-14 16:37:00 +0100

    Fix accessibility issue - people screen titles

--- WordPress/src/main/java/org/wordpress/android/ui/people/PeopleManagementActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/people/PeopleManagementActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.accessibility.AccessibilityEvent;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
