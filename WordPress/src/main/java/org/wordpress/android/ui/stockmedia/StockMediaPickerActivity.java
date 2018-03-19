c469369a16dc2eca35f8abf1fada7c3cc47533fb
Nick Bradbury <nick.bradbury@gmail.com>
2018-03-19 13:09:26 -0400

    Use `android.support.v4.app.Fragment`

--- WordPress/src/main/java/org/wordpress/android/ui/stockmedia/StockMediaPickerActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/stockmedia/StockMediaPickerActivity.java
@@ -1,10 +1,10 @@
 package org.wordpress.android.ui.stockmedia;
 
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -92,7 +92,7 @@ public class StockMediaPickerActivity extends AppCompatActivity implements Searc
             return;
         }
 
-        FragmentManager fm = getFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
         mRetainedFragment = (StockMediaRetainedFragment) fm.findFragmentByTag(TAG_RETAINED_FRAGMENT);
         if (mRetainedFragment == null) {
             mRetainedFragment = StockMediaRetainedFragment.newInstance();
@@ -188,7 +188,7 @@ public class StockMediaPickerActivity extends AppCompatActivity implements Searc
     @Override
     public void onPause() {
         if (isFinishing() && mRetainedFragment != null) {
-            getFragmentManager().beginTransaction().remove(mRetainedFragment).commit();
+            getSupportFragmentManager().beginTransaction().remove(mRetainedFragment).commit();
         }
         super.onPause();
     }
--- WordPress/src/main/java/org/wordpress/android/ui/stockmedia/StockMediaRetainedFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/stockmedia/StockMediaRetainedFragment.java
@@ -1,9 +1,9 @@
 package org.wordpress.android.ui.stockmedia;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 
 import org.wordpress.android.fluxc.model.StockMediaModel;
 
