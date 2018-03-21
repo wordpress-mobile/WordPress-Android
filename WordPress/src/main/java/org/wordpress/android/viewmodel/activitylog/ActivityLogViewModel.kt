95758c8616249fa56342da0c02a1fbf7f8febe15
Ondrej Ruttkay <ondrej@automattic.com>
2018-03-21 23:42:06 +0100

    Move Activity Log view model to a correct package

--- WordPress/src/main/java/org/wordpress/android/viewmodel/activitylog/ActivityLogViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/activitylog/ActivityLogViewModel.kt
@@ -1,5 +1,6 @@
-package org.wordpress.android.ui.activitylog
+package org.wordpress.android.viewmodel.activitylog
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.model.activity.ActivityLogModel
@@ -10,7 +11,7 @@ class ActivityLogViewModel() : ViewModel() {
     private lateinit var dispatcher: Dispatcher
     private lateinit var activityLogStore: ActivityLogStore
 
-    private lateinit var activityLogModel: ActivityLogModel
+    private val activities: MutableLiveData<List<ActivityLogModel>> = MutableLiveData()
 
     @Inject
     constructor(dispatcher: Dispatcher, activityLogStore: ActivityLogStore) : this() {
