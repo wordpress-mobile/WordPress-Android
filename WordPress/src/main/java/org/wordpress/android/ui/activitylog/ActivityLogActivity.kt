df74c3dfe0143864e832412e73ff367409edda8b
Ondrej Ruttkay <ondrej@automattic.com>
2018-03-23 11:32:30 +0100

    Add AL activity and fragment

---
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogActivity.kt
@@ -0,0 +1,49 @@
+package org.wordpress.android.ui.activitylog
+
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import org.wordpress.android.R
+import org.wordpress.android.WordPress
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.ui.plugins.PluginListFragment
+import org.wordpress.android.viewmodel.activitylog.ActivityLogViewModel
+import javax.inject.Inject
+
+class ActivityLogActivity : AppCompatActivity() {
+    @Inject
+    private lateinit var viewModelFactory: ViewModelProvider.Factory
+    private lateinit var viewModel: ActivityLogViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_log_activity)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(ActivityLogViewModel::class.java)
+
+        viewModel.site = if (savedInstanceState == null) {
+            intent.getSerializableExtra(WordPress.SITE) as SiteModel
+        } else {
+            savedInstanceState.getSerializable(WordPress.SITE) as SiteModel
+        }
+
+        showListFragment()
+    }
+
+    private fun showListFragment() {
+        val listFragment = ActivityLogListFragment.newInstance(viewModel.site!!)
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, listFragment, ActivityLogListFragment.TAG)
+                .addToBackStack(null)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit()
+    }
+}
---
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogListFragment.kt
@@ -0,0 +1,25 @@
+package org.wordpress.android.ui.activitylog
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.wordpress.android.WordPress
+import org.wordpress.android.fluxc.model.SiteModel
+
+class ActivityLogListFragment : Fragment() {
+    val TAG = ActivityLogListFragment::class.java.name
+
+    fun newInstance(site: SiteModel): ActivityLogListFragment {
+        val fragment = ActivityLogListFragment()
+        val bundle = Bundle()
+        bundle.putSerializable(WordPress.SITE, site)
+        fragment.arguments = bundle
+        return fragment
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
