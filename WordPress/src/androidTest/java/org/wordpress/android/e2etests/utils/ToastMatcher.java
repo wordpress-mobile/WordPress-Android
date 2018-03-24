5223d55b8803ccf58e855637c1166c9e95fa91fb
Sofia Vistas <sofia.vistas@gmail.com>
2018-03-24 23:50:29 +0100

    Use ToastMatcher for asserting toaster when uploading post

---
+++ WordPress/src/androidTest/java/org/wordpress/android/e2etests/utils/ToastMatcher.java
@@ -0,0 +1,30 @@
+package org.wordpress.android.e2etests.utils;
+
+
+import android.os.IBinder;
+import android.support.test.espresso.Root;
+import android.view.WindowManager;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+
+public class ToastMatcher extends TypeSafeMatcher<Root> {
+
+        @Override public void describeTo(Description description) {
+            description.appendText("is toast");
+        }
+
+        @Override public boolean matchesSafely(Root root) {
+            int type = root.getWindowLayoutParams().get().type;
+            if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
+                IBinder windowToken = root.getDecorView().getWindowToken();
+                IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                if (windowToken == appToken) {
+                    return true;
+                }
+            }
+            return false;
+        }
+}
+
