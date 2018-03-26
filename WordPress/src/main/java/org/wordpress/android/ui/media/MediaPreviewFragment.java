9a9bd0478f3076048034168ad14a927f0ae9298f
Nick Bradbury <nick.bradbury@gmail.com>
2018-03-26 05:54:51 -0400

    Use support library fragment

--- WordPress/src/main/java/org/wordpress/android/ui/media/MediaPreviewFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/MediaPreviewFragment.java
@@ -1,6 +1,5 @@
 package org.wordpress.android.ui.media;
 
-import android.app.Fragment;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
@@ -10,6 +9,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -141,7 +141,7 @@ public class MediaPreviewFragment extends Fragment implements MediaController.Me
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         View view = inflater.inflate(R.layout.media_preview_fragment, container, false);
@@ -228,7 +228,7 @@ public class MediaPreviewFragment extends Fragment implements MediaController.Me
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
         if (mIsVideo) {
