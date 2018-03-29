5693e0bb3bf8951ed723cc3f26df8231a7594f52
Ondrej Ruttkay <ondrej@automattic.com>
2018-03-29 16:12:24 +0200

    Use a base data source that invalidates data and test models

--- WordPress/src/main/java/org/wordpress/android/viewmodel/activitylog/ActivityLogDao.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/activitylog/ActivityLogDao.kt
@@ -1,27 +1,36 @@
 package org.wordpress.android.viewmodel.activitylog
 
-import android.arch.paging.PositionalDataSource
+import android.arch.paging.DataSource
+import android.util.Log
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.ActivityLogActionBuilder
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.activity.ActivityLogModel
 import org.wordpress.android.fluxc.store.ActivityLogStore
 import java.util.Date
+import java.util.UUID
 
-class ActivityLogDao (val site: SiteModel, val store: ActivityLogStore, val dispatcher: Dispatcher) : PositionalDataSource<ActivityLogModel>() {
-    override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<ActivityLogModel>) {
-        val payload = ActivityLogStore.FetchActivityLogPayload(site, params.startPosition, params.loadSize)
-        val action = ActivityLogActionBuilder.newFetchActivitiesAction(payload)
-//        dispatcher.dispatch(action)
-
-        callback.onResult(listOf(ActivityLogModel("1", "Summary", "Text", "Name", "Type", "x", "Status", true, "1", Date(), false, ActivityLogModel.ActivityActor("ActorName", "ActorType", 123, null, "Admin"))))
+class ActivityLogDao (val site: SiteModel, val store: ActivityLogStore, val dispatcher: Dispatcher) : BaseDataSource<ActivityLogModel>() {
+    override fun countItems(): Int {
+        return 100
     }
 
-    override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<ActivityLogModel>) {
-        val payload = ActivityLogStore.FetchActivityLogPayload(site, params.requestedStartPosition, params.requestedLoadSize)
+    override fun loadRangeAtPosition(position: Int, size: Int): List<ActivityLogModel>? {
+        val payload = ActivityLogStore.FetchActivityLogPayload(site, position, size)
         val action = ActivityLogActionBuilder.newFetchActivitiesAction(payload)
 //        dispatcher.dispatch(action)
 
-        callback.onResult(listOf(ActivityLogModel("1", "Summary", "Text", "Name", "Type", "x", "Status", true, "1", Date(), false, ActivityLogModel.ActivityActor("ActorName", "ActorType", 123, null, "Admin"))), 0, 1)
+        Log.d("Onko", "position $position, size $size")
+        val arrayList = ArrayList<ActivityLogModel>()
+        for (a in 1..size) {
+            arrayList.add(ActivityLogModel(UUID.randomUUID().toString(), "Summary", "Text", "Name $a", "Type", "x", "Status", true, "1", Date(), false, ActivityLogModel.ActivityActor("ActorName", "ActorType", 123, null, "Admin")))
+        }
+        return arrayList
+     }
+
+    class Factory(val site: SiteModel, val store: ActivityLogStore, val dispatcher: Dispatcher) : DataSource.Factory<Int, ActivityLogModel>() {
+        override fun create(): DataSource<Int, ActivityLogModel> {
+            return ActivityLogDao(site, store, dispatcher)
+        }
     }
 }
