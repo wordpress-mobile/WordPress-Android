87bd8872f55836b46236334121a4dce2f2421074
Alex Forcier <alex.forcier@automattic.com>
2018-04-04 12:51:25 -0400

    Add unit tests for WCOrderModel

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/UnitTestUtils.java
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/UnitTestUtils.java
@@ -3,6 +3,11 @@ package org.wordpress.android.fluxc;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.AppLog.T;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class UnitTestUtils {
     public static final int DEFAULT_TIMEOUT_MS = 30000;
 
@@ -16,6 +21,26 @@ public class UnitTestUtils {
 
     public static void waitForNetworkCall() {
         waitFor(DEFAULT_TIMEOUT_MS);
+    }
+
+    public static String getStringFromResourceFile(Class clazz, String filename) {
+        try {
+            InputStream is = clazz.getClassLoader().getResourceAsStream(filename);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            StringBuilder buffer = new StringBuilder();
+            String lineString;
+
+            while ((lineString = bufferedReader.readLine()) != null) {
+                buffer.append(lineString);
+            }
+
+            bufferedReader.close();
+            return buffer.toString();
+        } catch (IOException e) {
+            AppLog.e(T.TESTS, "Could not load response JSON file.");
+            return null;
+        }
     }
 }
 
