0007079721f3b5118d44148cf198018654072d6c
Ondrej Ruttkay <ondrej@automattic.com>
2018-04-05 13:42:53 +0200

    Setup the toolbar and initialize the ViewModel

--- WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogActivity.kt
@@ -5,15 +5,15 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import kotlinx.android.synthetic.main.activity_log_activity.*
+import kotlinx.android.synthetic.main.toolbar.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.util.ToastUtils
 import org.wordpress.android.viewmodel.activitylog.ActivityLogViewModel
 import javax.inject.Inject
 
-class ActivityLogActivity : AppCompatActivity() {
+class   ActivityLogActivity : AppCompatActivity() {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: ActivityLogViewModel
 
@@ -23,7 +23,11 @@ class ActivityLogActivity : AppCompatActivity() {
         (application as WordPress).component().inject(this)
         setContentView(R.layout.activity_log_activity)
 
-        setSupportActionBar(toolbar as Toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
 
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(ActivityLogViewModel::class.java)
 
@@ -33,6 +37,14 @@ class ActivityLogActivity : AppCompatActivity() {
             savedInstanceState.getSerializable(WordPress.SITE) as SiteModel
         }
 
+        viewModel.start()
+
+        if (viewModel.site == null) {
+            ToastUtils.showToast(this, R.string.blog_not_found)
+            finish()
+            return
+        }
+
         showListFragment()
     }
 
@@ -40,7 +52,6 @@ class ActivityLogActivity : AppCompatActivity() {
         val listFragment = ActivityLogListFragment.newInstance(viewModel.site!!)
         supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_container, listFragment, ActivityLogListFragment.TAG)
-                .addToBackStack(null)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                 .commit()
     }
