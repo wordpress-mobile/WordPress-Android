0214f4163fd28db872e80c6e5714459e75bbc121
Ondrej Ruttkay <ondrej@automattic.com>
2018-04-05 13:52:19 +0200

    Update the AL list item layout

--- WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogViewHolder.kt
@@ -5,9 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
-import com.bumptech.glide.RequestManager
 import org.wordpress.android.R
 import org.wordpress.android.fluxc.model.activity.ActivityLogModel
 import java.text.DateFormat
@@ -15,16 +15,16 @@ import java.util.Locale
 
 class ActivityLogViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context)
         .inflate(R.layout.activity_log_list_item, parent, false)) {
-    private val subject: TextView = itemView.findViewById(R.id.action_subject)
-    private val detail: TextView = itemView.findViewById(R.id.action_detail)
-    private val header: TextView = itemView.findViewById(R.id.activity_header_text)
-    private val thumbnail : TextView = itemView.findViewById(R.id.action_icon)
+    private val summary: TextView = itemView.findViewById(R.id.action_summary)
+    private val text: TextView = itemView.findViewById(R.id.action_text)
+    val header: TextView = itemView.findViewById(R.id.activity_header_text)
+    private val thumbnail : ImageView = itemView.findViewById(R.id.action_icon)
     private val progressBar : ProgressBar = itemView.findViewById(R.id.rewind_progress_bar)
     private val button : ImageButton = itemView.findViewById(R.id.rewind_button)
 
     private var activity : ActivityLogModel? = null
 
-    private val timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, Locale.getDefault())
+    private val timeFormatter = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault())
 
     init {
         button.setOnClickListener {
@@ -34,9 +34,15 @@ class ActivityLogViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutI
     fun bind(activity: ActivityLogModel?) {
         this.activity = activity
 
-        subject.text = activity?.name
-        detail.text = activity?.summary
-        thumbnail.text = activity?.gridicon
+        summary.text = activity?.summary
+        text.text = activity?.text
+
+        val thumb = when (activity?.gridicon) {
+            "comment" -> R.drawable.ic_comment_white_24dp
+            else -> R.drawable.ic_checkmark_white_24dp
+        }
+        
+        thumbnail.setImageResource(thumb)
         header.text = timeFormatter.format(activity?.published)
     }
 
