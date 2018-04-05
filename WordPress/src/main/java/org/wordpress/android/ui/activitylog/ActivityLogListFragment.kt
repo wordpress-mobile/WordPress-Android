2caa2d33a7d3093e088b80ca14cf408dbda5cb36
Ondrej Ruttkay <ondrej@automattic.com>
2018-04-05 13:50:35 +0200

    Move AL list adapter to fragment

--- WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogListFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogListFragment.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.ui.activitylog
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import kotlinx.android.synthetic.main.activity_log_list_fragment.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.activity.ActivityLogModel
 import org.wordpress.android.viewmodel.activitylog.ActivityLogViewModel
 import javax.inject.Inject
 
@@ -54,5 +56,49 @@ class ActivityLogListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_log_list_fragment, container, false)
+    }
+
+    inner class ActivityLogAdapter(context: Context) : RecyclerView.Adapter<ActivityLogViewHolder>() {
+        private val list = ArrayList<ActivityLogModel>()
+        private var layoutInflater: LayoutInflater = LayoutInflater.from(context)
+
+        override fun onBindViewHolder(holder: ActivityLogViewHolder, position: Int) {
+            holder.bind(getItem(position))
+
+            if (position == itemCount - 1) {
+                viewModel.loadMore()
+            }
+
+            if (position % 4 == 0) {
+                holder.header.visibility = View.VISIBLE
+            }
+        }
+
+        init {
+            setHasStableIds(true)
+        }
+
+        internal fun setEvents(items: List<ActivityLogModel>) {
+            list.clear()
+            list.addAll(items.toTypedArray())
+            notifyDataSetChanged()
+        }
+
+        protected fun getItem(position: Int): ActivityLogModel {
+            return list[position]
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun getItemId(position: Int): Long {
+            return list[position].activityID.hashCode().toLong()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityLogViewHolder {
+            val view = layoutInflater.inflate(R.layout.activity_log_list_item, parent, false) as ViewGroup
+            return ActivityLogViewHolder(view)
+        }
     }
 }
