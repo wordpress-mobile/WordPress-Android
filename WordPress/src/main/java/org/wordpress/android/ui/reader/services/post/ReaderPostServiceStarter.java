ba7ac92c22d9f6d1983897da1e1777ad05f8f8a7
Mario Zorz <mariozorz@gmail.com>
2018-04-06 17:11:21 -0300

    fixed typo in comment

--- WordPress/src/main/java/org/wordpress/android/ui/reader/services/post/ReaderPostServiceStarter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/services/post/ReaderPostServiceStarter.java
@@ -88,7 +88,7 @@ public class ReaderPostServiceStarter {
     @TargetApi(21)
     private static void doScheduleJobWithBundle(Context context, PersistableBundle extras, int jobId) {
         // schedule the JobService here for API >= 26. The JobScheduler is available since API 21, but
-        // it's preferrable to use it only since enforcement in API 26 to not break any old behavior
+        // it's preferable to use it only since enforcement in API 26 to not break any old behavior
         ComponentName componentName = new ComponentName(context, ReaderPostJobService.class);
 
         JobInfo jobInfo = new JobInfo.Builder(jobId, componentName)
--- WordPress/src/main/java/org/wordpress/android/ui/reader/services/update/ReaderUpdateServiceStarter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/services/update/ReaderUpdateServiceStarter.java
@@ -29,7 +29,7 @@ public class ReaderUpdateServiceStarter {
             context.startService(intent);
         } else {
             // schedule the JobService here for API >= 26. The JobScheduler is available since API 21, but
-            // it's preferrable to use it only since enforcement in API 26 to not break any old behavior
+            // it's preferable to use it only since enforcement in API 26 to not break any old behavior
             ComponentName componentName = new ComponentName(context, ReaderUpdateJobService.class);
 
             PersistableBundle extras = new PersistableBundle();
