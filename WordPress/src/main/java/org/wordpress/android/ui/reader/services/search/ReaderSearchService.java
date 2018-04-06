4682c0f2c4b294eff5481815f46c03343da2849c
Mario Zorz <mariozorz@gmail.com>
2018-04-06 14:42:59 -0300

    removed unused import

--- WordPress/src/main/java/org/wordpress/android/ui/reader/services/search/ReaderSearchService.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/services/search/ReaderSearchService.java
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.reader.services.search;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
