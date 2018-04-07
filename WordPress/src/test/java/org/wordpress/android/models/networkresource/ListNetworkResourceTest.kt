7308f8ea05e3202dece7891e6179908a284376cb
Oguz Kocer <oguz.kocer@automattic.com>
2018-04-07 00:24:18 -0400

    Adds load first page state tests and converts every test to assertThat

--- WordPress/src/test/java/org/wordpress/android/models/networkresource/ListNetworkResourceTest.kt
+++ WordPress/src/test/java/org/wordpress/android/models/networkresource/ListNetworkResourceTest.kt
@@ -1,75 +1,116 @@
 package org.wordpress.android.models.networkresource
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertNull
-import org.junit.Assert.assertTrue
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.nullValue
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class ListNetworkResourceTest {
     @Test
     fun testInitState() {
-        val initState = ListNetworkResource.Init<String>()
+        val initState: ListNetworkResource<String> = ListNetworkResource.Init()
 
         // Verify the state
 
-        assertNull(initState.previous)
-        assertTrue(initState.data.isEmpty())
+        assertThat(initState.previous, `is`(nullValue()))
+        assertThat(initState.data, `is`(emptyList()))
 
         // We are not loading anything
-        assertFalse(initState.isFetchingFirstPage())
-        assertFalse(initState.isLoadingMore())
+        assertThat(initState.isFetchingFirstPage(), `is`(false))
+        assertThat(initState.isLoadingMore(), `is`(false))
 
         // We shouldn't be able to fetch anything
-        assertFalse(initState.shouldFetch(true))
-        assertFalse(initState.shouldFetch(false))
+        assertThat(initState.shouldFetch(true), `is`(false))
+        assertThat(initState.shouldFetch(false), `is`(false))
 
         // State transitions
 
         val readyState = initState.ready(ArrayList())
-        assertEquals(initState, readyState.previous)
+        assertThat(readyState.previous, `is`(equalTo(initState)))
 
         val successState = initState.success(ArrayList())
-        assertEquals(initState, successState.previous)
+        assertThat(successState.previous, `is`(equalTo(initState)))
 
         val loadingState = initState.loading(false)
-        assertEquals(initState, loadingState.previous)
+        assertThat(loadingState.previous, `is`(equalTo(initState)))
 
         val errorState = initState.error("error")
-        assertEquals(initState, errorState.previous)
+        assertThat(errorState.previous, `is`(equalTo(initState)))
     }
 
     @Test
     fun testReadyState() {
-        val initState = ListNetworkResource.Init<String>()
+        val initState: ListNetworkResource<String> = ListNetworkResource.Init()
         val testData = listOf("item1", "item2")
-        val readyState = ListNetworkResource.Ready(initState, testData)
+        val readyState: ListNetworkResource<String> = ListNetworkResource.Ready(initState, testData)
 
         // Verify the state
 
-        assertEquals(testData, readyState.data)
-        assertEquals(initState, readyState.previous)
+        assertThat(readyState.data, `is`(equalTo(testData)))
+        assertThat(readyState.previous, `is`(equalTo(initState)))
 
         // We are not loading anything
-        assertFalse(readyState.isFetchingFirstPage())
-        assertFalse(readyState.isLoadingMore())
+        assertThat(readyState.isFetchingFirstPage(), `is`(false))
+        assertThat(readyState.isLoadingMore(), `is`(false))
 
         // We can refresh the first page but we can't load more
-        assertFalse(readyState.shouldFetch(true))
-        assertTrue(readyState.shouldFetch(false))
+        assertThat(readyState.shouldFetch(true), `is`(false))
+        assertThat(readyState.shouldFetch(false), `is`(true))
 
         // State transitions
 
         val successState = readyState.success(ArrayList())
-        assertEquals(readyState, successState.previous)
-        assertEquals(testData, successState.previous?.data)
+        assertThat(successState.previous, `is`(equalTo(readyState)))
+        assertThat(successState.previous?.data, `is`(equalTo(testData)))
 
         val loadingState = readyState.loading(false)
-        assertEquals(readyState, loadingState.previous)
-        assertEquals(testData, loadingState.previous?.data)
+        assertThat(loadingState.previous, `is`(equalTo(readyState)))
+        assertThat(loadingState.previous?.data, `is`(equalTo(testData)))
 
         val errorState = readyState.error("error")
-        assertEquals(readyState, errorState.previous)
-        assertEquals(testData, errorState.previous?.data)
+        assertThat(errorState.previous, `is`(equalTo(readyState)))
+        assertThat(errorState.previous?.data, `is`(equalTo(testData)))
+    }
+
+    @Test
+    fun testLoadingFirstPageState() {
+        val testDataReady = listOf("item3", "item4")
+        val readyState: ListNetworkResource<String> = ListNetworkResource.Ready(ListNetworkResource.Init(),
+                testDataReady)
+        val loadingState: ListNetworkResource<String> = ListNetworkResource.Loading(readyState)
+
+        // Verify the state
+
+        assertThat(loadingState.data, `is`(equalTo(testDataReady)))
+        assertThat(loadingState.previous, `is`(equalTo(readyState)))
+
+        // We are not first page
+        assertThat(loadingState.isFetchingFirstPage(), `is`(true))
+        assertThat(loadingState.isLoadingMore(), `is`(false))
+
+        // We are already fetching
+        assertThat(loadingState.shouldFetch(true), `is`(false))
+        assertThat(loadingState.shouldFetch(false), `is`(false))
+
+        // State transitions
+
+        val testDataSuccess = listOf("item 5")
+
+        val successState = loadingState.success(testDataSuccess)
+        assertThat(successState.previous, `is`(equalTo(loadingState)))
+        assertThat(successState.previous?.data, `is`(equalTo(testDataReady)))
+        assertThat(successState.data, `is`(equalTo(testDataSuccess)))
+
+        assertThat(successState.previous?.isLoadingMore(), `is`(false))
+        assertThat(successState.previous?.isFetchingFirstPage(), `is`(true))
+
+        val errorState = loadingState.error("error")
+        assertThat(errorState.previous, `is`(equalTo(loadingState)))
+        assertThat(errorState.previous?.data, `is`(equalTo(testDataReady)))
+        assertThat(errorState.data, `is`(equalTo(testDataReady)))
+
+        assertThat(errorState.previous?.isLoadingMore(), `is`(false))
+        assertThat(errorState.previous?.isFetchingFirstPage(), `is`(true))
     }
 }
