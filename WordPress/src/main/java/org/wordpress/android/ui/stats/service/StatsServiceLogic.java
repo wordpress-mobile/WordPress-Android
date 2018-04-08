c77360ae88282357991156e6dc1ff86da214f090
Mario Zorz <mariozorz@gmail.com>
2018-04-08 19:25:51 -0300

    brought loop back as it was

--- WordPress/src/main/java/org/wordpress/android/ui/stats/service/StatsServiceLogic.java
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/service/StatsServiceLogic.java
@@ -285,7 +285,7 @@ public class StatsServiceLogic {
         final int maxResultsRequested = extras.getInt(StatsService.ARG_MAX_RESULTS, DEFAULT_NUMBER_OF_RESULTS);
         final int pageRequested = extras.getInt(StatsService.ARG_PAGE_REQUESTED, -1);
 
-        for (int i : sectionFromIntent) {
+        for (int i = 0; i < sectionFromIntent.length; i++) {
             final StatsServiceLogic.StatsEndpointsEnum currentSectionsToUpdate =
                     StatsServiceLogic.StatsEndpointsEnum.values()[sectionFromIntent[i]];
             mSingleThreadNetworkHandler.submit(new Thread() {
