6230c29332b1b0972b5f7f0edfe23be7e13792d6
Mario Zorz <mariozorz@gmail.com>
2018-04-09 16:02:40 -0300

    added method to check for identical jobservice parameters, useful to not repeat the same job scheduling

--- WordPress/src/main/java/org/wordpress/android/JobServiceId.java
+++ WordPress/src/main/java/org/wordpress/android/JobServiceId.java
@@ -1,5 +1,14 @@
 package org.wordpress.android;
 
+import android.annotation.TargetApi;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.PersistableBundle;
+
+import org.wordpress.android.util.AppLog;
+
 public class JobServiceId {
     public static final int JOB_STATS_SERVICE_ID = 8000;
     public static final int JOB_NOTIFICATIONS_UPDATE_SERVICE_ID = 7000;
@@ -7,4 +16,38 @@ public class JobServiceId {
     public static final int JOB_PUBLICIZE_UPDATE_SERVICE_ID = 3000;
     public static final int JOB_READER_UPDATE_SERVICE_ID = 2000;
     public static final int JOB_GCM_REG_SERVICE_ID = 1000;
+
+    @TargetApi(21)
+    public static boolean isJobServiceWithSameParamsPending(Context context, ComponentName componentName, PersistableBundle bundleCompare) {
+        JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        boolean jobAlreadyScheduled = false;
+
+        for (JobInfo jobInfo : scheduler.getAllPendingJobs()) {
+            // check this is the same Service we are looking for
+            if (jobInfo.getService().getClassName().compareTo(componentName.getClassName()) == 0) {
+                PersistableBundle extras = jobInfo.getExtras();
+                if (extras != null) {
+                    if (extras.keySet().containsAll(bundleCompare.keySet())) {
+                        // compare all parameters
+                        jobAlreadyScheduled = true;
+                        for (String key : bundleCompare.keySet()) {
+                            // this is contained, check the value is the same now
+                            Object one = extras.get(key);
+                            Object two = bundleCompare.get(key);
+                            if (!one.equals(two)) {
+                                jobAlreadyScheduled = false;
+                                break;
+                            }
+                        }
+                        // if all is good, we found it
+                        if (jobAlreadyScheduled) {
+                            AppLog.i(AppLog.T.STATS, "Job was already scheduled");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return jobAlreadyScheduled;
+    }
 }
