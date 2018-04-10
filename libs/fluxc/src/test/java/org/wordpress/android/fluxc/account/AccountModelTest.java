26b0ed2d722a1bfa342852e5bbf6bf487466a185
Danilo Ercoli <ercoli@gmail.com>
2018-04-10 19:12:21 +0200

    Update AccountModelTests

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/account/AccountModelTest.java
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/account/AccountModelTest.java
@@ -37,10 +37,12 @@ public class AccountModelTest {
         copyAccount.setVisibleSiteCount(testAccount.getVisibleSiteCount() + 1);
         copyAccount.setEmail("copyEmail");
         copyAccount.setPendingEmailChange(!testAccount.getPendingEmailChange());
+        copyAccount.setTracksOptOut(!testAccount.getTracksOptOut());
         Assert.assertFalse(copyAccount.equals(testAccount));
         testAccount.copyAccountAttributes(copyAccount);
         Assert.assertFalse(copyAccount.equals(testAccount));
         copyAccount.setPendingEmailChange(testAccount.getPendingEmailChange());
+        copyAccount.setTracksOptOut(testAccount.getTracksOptOut());
         Assert.assertTrue(copyAccount.equals(testAccount));
     }
 
@@ -56,6 +58,7 @@ public class AccountModelTest {
         copyAccount.setDate("copyDate");
         copyAccount.setNewEmail("copyNewEmail");
         copyAccount.setPendingEmailChange(!testAccount.getPendingEmailChange());
+        copyAccount.setTracksOptOut(!testAccount.getTracksOptOut());
         copyAccount.setWebAddress("copyWebAddress");
         copyAccount.setUserId(testAccount.getUserId() + 1);
         Assert.assertFalse(copyAccount.equals(testAccount));
