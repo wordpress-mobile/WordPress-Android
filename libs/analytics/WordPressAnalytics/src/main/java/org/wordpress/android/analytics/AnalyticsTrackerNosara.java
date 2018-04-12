01df91f8ea77701efe97819ab9072420149d9681
theck13 <theck13@users.noreply.github.com>
2018-04-12 14:38:51 -0400

    Add signup magic link open email client button clicked analytic

--- libs/analytics/WordPressAnalytics/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
+++ libs/analytics/WordPressAnalytics/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
@@ -765,6 +765,8 @@ public class AnalyticsTrackerNosara extends Tracker {
                 return "signup_email_to_login";
             case SIGNUP_MAGIC_LINK_FAILED:
                 return "signup_magic_link_failed";
+            case SIGNUP_MAGIC_LINK_OPEN_EMAIL_CLIENT_CLICKED:
+                return "signup_magic_link_open_email_client_clicked";
             case SIGNUP_MAGIC_LINK_SENT:
                 return "signup_magic_link_sent";
             case SIGNUP_MAGIC_LINK_SUCCEEDED:
