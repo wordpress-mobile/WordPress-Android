0bdb23847652f31a5d80c76df75c501ff0afeaec
Danilo Ercoli <ercoli@gmail.com>
2018-04-13 09:47:49 +0200

    Clear all queues (already submitted events) when clear all data.

--- libs/analytics/WordPressAnalytics/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
+++ libs/analytics/WordPressAnalytics/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
@@ -320,6 +320,7 @@ public class AnalyticsTrackerNosara extends Tracker {
             return;
         }
         mNosaraClient.clearUserProperties();
+        mNosaraClient.clearQueues();
     }
 
     @Override
