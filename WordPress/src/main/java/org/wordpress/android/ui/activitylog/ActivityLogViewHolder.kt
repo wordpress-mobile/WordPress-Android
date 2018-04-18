9db32ae181fde513fd366f50a1fc3da9dccb6505
Ondrej Ruttkay <ondrej@automattic.com>
2018-04-18 09:40:56 -0400

    Expose the activity item button

--- WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/ActivityLogViewHolder.kt
@@ -15,12 +15,14 @@ import java.util.Locale
 
 class ActivityLogViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context)
         .inflate(R.layout.activity_log_list_item, parent, false)) {
+
     private val summary: TextView = itemView.findViewById(R.id.action_summary)
     private val text: TextView = itemView.findViewById(R.id.action_text)
-    val header: TextView = itemView.findViewById(R.id.activity_header_text)
     private val thumbnail : ImageView = itemView.findViewById(R.id.action_icon)
     private val progressBar : ProgressBar = itemView.findViewById(R.id.rewind_progress_bar)
-    private val button : ImageButton = itemView.findViewById(R.id.rewind_button)
+
+    val header: TextView = itemView.findViewById(R.id.activity_header_text)
+    val button : ImageButton = itemView.findViewById(R.id.rewind_button)
 
     private var activity : ActivityLogModel? = null
 
