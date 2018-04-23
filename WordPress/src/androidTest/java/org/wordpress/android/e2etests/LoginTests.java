c4ed5509cc4921c7b6236599df527f70d263cdcb
Sofia Vistas <sofia.vistas@gmail.com>
2018-04-23 10:07:06 +0200

    Refactor the Login tests with the Robot Pattern

--- WordPress/src/androidTest/java/org/wordpress/android/e2etests/LoginTests.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2etests/LoginTests.java
@@ -9,18 +9,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wordpress.android.BuildConfig;
-import org.wordpress.android.R;
 import org.wordpress.android.TestUtils;
+import org.wordpress.android.e2etests.robots.LoginRobot;
+import org.wordpress.android.e2etests.robots.LoginRobot.ResultRobot;
 import org.wordpress.android.ui.WPLaunchActivity;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -38,38 +31,15 @@ public class LoginTests {
 
     @Test
     public void testLoginSuccess() {
-        onView(withText("Log In"))
-                .perform(click());
+        new LoginRobot()
+                .selectLoginOption()
+                .typeUsername(mUsername)
+                .tapNextButton()
+                .tapToEnterPasswordInstead()
+                .typePassword(mPassword)
+                .tapNextButton();
 
-        onView(withId(R.id.input))
-                .perform(replaceText(mUsername), closeSoftKeyboard());
-
-        onView(withText("Next"))
-                .perform(click());
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        onView(withId(R.id.login_enter_password))
-                .perform(click());
-
-        onView(withId(R.id.input))
-                .perform(replaceText(mPassword), closeSoftKeyboard());
-
-        onView(withText("Next"))
-                .perform(click());
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        onView(withId(R.id.logged_in_as_heading))
-                .check(matches(isDisplayed()));
-        }
+        new ResultRobot().isSucessfullyLoggedIn();
+    }
 }
 
---
+++ WordPress/src/androidTest/java/org/wordpress/android/e2etests/robots/LoginRobot.java
@@ -0,0 +1,75 @@
+package org.wordpress.android.e2etests.robots;
+
+
+import org.wordpress.android.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+public class LoginRobot {
+    public LoginRobot selectLoginOption() {
+        onView(withText("Log In"))
+                .perform(click());
+        return this;
+    }
+
+    public LoginRobot typeUsername(String username) {
+        onView(withId(R.id.input))
+                .perform(click())
+                .perform(typeText(username), closeSoftKeyboard());
+        return this;
+    }
+
+    public LoginRobot typePassword(String password) {
+        onView(withId(R.id.input))
+                .perform(click())
+                .perform(typeText(password), closeSoftKeyboard());
+        return this;
+    }
+
+    public LoginRobot tapNextButton() {
+        onView(withText("Next"))
+                .perform(click());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return this;
+    }
+
+    public LoginRobot tapToEnterPasswordInstead() {
+        onView(withId(R.id.login_enter_password))
+                .perform(click());
+        return this;
+    }
+
+    public LoginRobot tapToContinueOnSiteSelection() {
+        onView(withText("Continue"))
+                .perform(click());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public static class ResultRobot {
+        public void isSucessfullyLoggedIn(){
+            onView(withId(R.id.logged_in_as_heading))
+                    .check(matches(isDisplayed()));
+        }
+    }
+
+}
