f85dbffb80fd040ed6541b9f5effab607e47139b
Sofia Vistas <sofia.vistas@gmail.com>
2018-04-23 10:53:34 +0200

    Refactor Navigation tests to the Robot pattern

--- WordPress/src/androidTest/java/org/wordpress/android/e2etests/NavigationTests.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2etests/NavigationTests.java
@@ -1,25 +1,17 @@
 package org.wordpress.android.e2etests;
 
 
-import android.support.test.espresso.contrib.ViewPagerActions;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.wordpress.android.BuildConfig;
-import org.wordpress.android.R;
 import org.wordpress.android.TestUtils;
+import org.wordpress.android.e2etests.robots.LoginRobot;
+import org.wordpress.android.e2etests.robots.NavigationRobot;
 import org.wordpress.android.ui.WPLaunchActivity;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class NavigationTests {
     @Rule
@@ -35,70 +27,28 @@ public class NavigationTests {
 
     @Test
     public void testNavigation() {
-        onView(withText("Log In"))
-                .perform(click());
+        new LoginRobot()
+                .selectLoginOption()
+                .typeUsername(mUsername)
+                .tapNextButton()
+                .tapToEnterPasswordInstead()
+                .typePassword(mPassword)
+                .tapNextButton()
+                .tapToContinueOnSiteSelection();
 
-        onView(withId(R.id.input))
-                .perform(replaceText(mUsername), closeSoftKeyboard());
+        NavigationRobot navigationRobot = new NavigationRobot();
+        navigationRobot.selectMyWordpressSites();
 
-        onView(withText("Next"))
-                .perform(click());
+        NavigationRobot.ResultRobot resultRobot = new NavigationRobot.ResultRobot();
+        resultRobot.displaysSiteMenuPageSucessfully();
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        navigationRobot.selectMyPosts();
+        resultRobot.displaysPostsPageSucessfully();
 
-        onView(withId(R.id.login_enter_password))
-                .perform(click());
+        navigationRobot.selectProfile();
+        resultRobot.displaysProfilePageSucessfully();
 
-        onView(withId(R.id.input))
-                .perform(replaceText(mPassword), closeSoftKeyboard());
-
-        onView(withText("Next"))
-                .perform(click());
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        onView(withText("Continue"))
-                .perform(click());
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        onView(withId(R.id.viewpager_main))
-                .perform(ViewPagerActions.scrollToFirst());
-
-        onView(withId(R.id.row_stats))
-                .check(matches(isDisplayed()));
-
-        onView(withId(R.id.my_site_plan_text_view))
-                .check(matches(isDisplayed()));
-
-        onView(withId(R.id.viewpager_main))
-                .perform(ViewPagerActions.scrollToPage(1));
-
-        onView(withId(R.id.title_empty))
-                .check(matches(isDisplayed()));
-
-        onView(withId(R.id.viewpager_main))
-                .perform(ViewPagerActions.scrollToPage(2));
-
-        onView(withId(R.id.row_my_profile))
-                .check(matches(isDisplayed()));
-
-        onView(withId(R.id.viewpager_main))
-                .perform(ViewPagerActions.scrollToLast());
-
-        onView(withId(R.id.notifications_filter))
-                .check(matches(isDisplayed()));
+        navigationRobot.selectNotifications();
+        resultRobot.displaysNotificationsPageSucessfully();
     }
 }
---
+++ WordPress/src/androidTest/java/org/wordpress/android/e2etests/robots/NavigationRobot.java
@@ -0,0 +1,63 @@
+package org.wordpress.android.e2etests.robots;
+
+
+import android.support.test.espresso.contrib.ViewPagerActions;
+
+import org.wordpress.android.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class NavigationRobot {
+    public NavigationRobot selectMyWordpressSites() {
+        onView(withId(R.id.viewpager_main))
+                .perform(ViewPagerActions.scrollToFirst());
+        return this;
+    }
+
+    public NavigationRobot selectMyPosts() {
+        onView(withId(R.id.viewpager_main))
+                .perform(ViewPagerActions.scrollToPage(1));
+        return this;
+    }
+
+    public NavigationRobot selectProfile() {
+        onView(withId(R.id.viewpager_main))
+                .perform(ViewPagerActions.scrollToPage(2));
+        return this;
+    }
+
+    public NavigationRobot selectNotifications() {
+        onView(withId(R.id.viewpager_main))
+                .perform(ViewPagerActions.scrollToLast());
+        return this;
+    }
+
+    public static class ResultRobot {
+        public void displaysPostsPageSucessfully(){
+            onView(withId(R.id.title_empty))
+                    .check(matches(isDisplayed()));
+        }
+
+        public void displaysSiteMenuPageSucessfully(){
+            onView(withId(R.id.row_stats))
+                    .check(matches(isDisplayed()));
+
+            onView(withId(R.id.my_site_plan_text_view))
+                    .check(matches(isDisplayed()));
+        }
+
+        public void displaysProfilePageSucessfully(){
+            onView(withId(R.id.row_my_profile))
+                    .check(matches(isDisplayed()));
+        }
+
+        public void displaysNotificationsPageSucessfully(){
+            onView(withId(R.id.notifications_filter))
+                    .check(matches(isDisplayed()));
+        }
+    }
+}
+
