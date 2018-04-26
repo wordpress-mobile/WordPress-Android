803f1f2fa6e3329809aaa64e8ffe6979d9ec3307
Mario Zorz <mariozorz@gmail.com>
2018-04-26 08:15:27 -0300

    added comment describing where contentContainsGutenbergBlocks logic comes from

--- libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/AztecEditorFragment.java
+++ libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/AztecEditorFragment.java
@@ -542,6 +542,25 @@ public class AztecEditorFragment extends EditorFragmentAbstract implements
         mAztecReady = true;
     }
 
+    /*
+        Note the way we detect we're in presence of Gutenberg blocks logic is taken from
+        https://github.com/WordPress/gutenberg/blob/5a6693589285363341bebad15bd56d9371cf8ecc/lib/register.php#L331-L345
+
+        * Determine whether a content string contains blocks. This test optimizes for
+        * performance rather than strict accuracy, detecting the pattern of a block
+        * but not validating its structure. For strict accuracy, you should use the
+        * block parser on post content.
+        *
+        * @since 1.6.0
+        * @see gutenberg_parse_blocks()
+        *
+        * @param string $content Content to test.
+        * @return bool Whether the content contains blocks.
+
+        function gutenberg_content_has_blocks( $content ) {
+            return false !== strpos( $content, '<!-- wp:' );
+        }
+     */
     private boolean contentContainsGutenbergBlocks(String postContent) {
         return (postContent != null && postContent.contains(GUTENBERG_BLOCK_START));
     }
