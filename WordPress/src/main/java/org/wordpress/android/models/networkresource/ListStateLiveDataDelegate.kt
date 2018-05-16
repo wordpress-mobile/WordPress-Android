48a9cf3d27153e12c7595e5a925bffda413aa817
Ondrej Ruttkay <ondrej@automattic.com>
2018-05-16 21:38:12 +0200

    Store the assigned value locally so that it can be used immediately

--- WordPress/src/main/java/org/wordpress/android/models/networkresource/ListStateLiveDataDelegate.kt
+++ WordPress/src/main/java/org/wordpress/android/models/networkresource/ListStateLiveDataDelegate.kt
@@ -6,21 +6,21 @@ import android.arch.lifecycle.Observer
 import kotlin.reflect.KProperty
 
 class ListStateLiveDataDelegate<T: Any>(
-    initialState: ListState<T> = ListState.Init(),
-    private val liveData: MutableLiveData< ListState<T>> =
+    private var listState: ListState<T> = ListState.Init(),
+    private val liveData: MutableLiveData<ListState<T>> =
             MutableLiveData()) {
-
     init {
-        liveData.value = initialState
+        liveData.value = listState
     }
 
     fun observe(owner: LifecycleOwner, observer: Observer<ListState<T>>) =
             liveData.observe(owner, observer)
 
     operator fun setValue(ref: Any, p: KProperty<*>, value: ListState<T>) {
-        liveData.postValue(value)
+        listState = value
+        liveData.postValue(listState)
     }
 
     operator fun getValue(ref: Any, p: KProperty<*>): ListState<T> =
-            liveData.value!!
+            listState
 }
