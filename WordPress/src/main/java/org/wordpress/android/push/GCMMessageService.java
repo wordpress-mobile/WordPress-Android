b9806676ca2084322ff399b283c550e2b49d860c
Mario Zorz <mariozorz@gmail.com>
2018-05-21 10:57:02 -0300

    fix remaining references to ACTIVE_NOTIFICATIONS_MAP in rebuildAndUpdateNotificationsOnSystemBar()

--- WordPress/src/main/java/org/wordpress/android/push/GCMMessageService.java
+++ WordPress/src/main/java/org/wordpress/android/push/GCMMessageService.java
@@ -767,10 +767,10 @@ public class GCMMessageService extends GcmListenerService {
             Bundle authPNBundle = tmpMap.remove(AUTH_PUSH_NOTIFICATION_ID);
             if (authPNBundle != null) {
                 handlePushAuth(context, authPNBundle);
-                if (ACTIVE_NOTIFICATIONS_MAP.size() > 0 && noteType.equals(PUSH_TYPE_PUSH_AUTH)) {
+                if (tmpMap.size() > 0 && noteType.equals(PUSH_TYPE_PUSH_AUTH)) {
                     // get the data for the next notification in map for re-build
                     // because otherwise we would be keeping the PUSH_AUTH type note in `data`
-                    data = ACTIVE_NOTIFICATIONS_MAP.values().iterator().next();
+                    data = tmpMap.values().iterator().next();
                 } else if (noteType.equals(PUSH_TYPE_PUSH_AUTH)) {
                     // only note is the 2fa note, just return
                     return;
