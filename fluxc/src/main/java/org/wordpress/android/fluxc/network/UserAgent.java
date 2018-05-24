2f4c2711c37c7ac7f7dcb074c248b352087aa78b
Alex Forcier <alex.forcier@automattic.com>
2018-05-24 12:43:44 -0400

    Specify UserAgent app name string from host app

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.java
@@ -6,11 +6,10 @@ import android.content.pm.PackageManager;
 import android.webkit.WebView;
 
 public class UserAgent {
-    private static final String USER_AGENT_APPNAME = "wp-android";
     private String mUserAgent;
     private String mDefaultUserAgent;
 
-    public UserAgent(Context appContext) {
+    public UserAgent(Context appContext, String appName) {
         // Device's default User-Agent string.
         // E.g.:
         //   "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
@@ -27,8 +26,7 @@ public class UserAgent {
         //    "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
         //    AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36
         //    wp-android/4.7"
-        // Note that app versions prior to 4.7 simply used "wp-android" as the user agent
-        mUserAgent = mDefaultUserAgent + " " + USER_AGENT_APPNAME + "/" + getVersionName(appContext);
+        mUserAgent = mDefaultUserAgent + " " + appName + "/" + PackageUtils.getVersionName(appContext);
     }
 
     public String getUserAgent() {
