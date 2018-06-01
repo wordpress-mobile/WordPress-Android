0e547d826970d20b3513c41cd61f6308f0e24a22
Christos Antonopoulos <chris.antonopoulos96@gmail.com>
2018-06-01 19:11:11 +0300

    PostUtils.java and CommentUtils.java documentation and proposed solution to #6832 issue (multi-line comment at the and of CommentUtils.java class)

--- WordPress/src/main/java/org/wordpress/android/ui/comments/CommentUtils.java
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/CommentUtils.java
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.comments;
 
+
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
@@ -9,6 +10,7 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.LeadingMarginSpan;
 import android.text.util.Linkify;
+
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
@@ -18,10 +20,16 @@ import org.wordpress.android.util.EmoticonsUtils;
 import org.wordpress.android.util.HtmlUtils;
 import org.wordpress.android.util.helpers.WPImageGetter;
 
+
+
 public class CommentUtils {
+
     /*
+     *
      * displays comment text as html, including retrieving images
+     *
      */
+
     public static void displayHtmlComment(TextView textView, String content, int maxImageSize,
                                           ImageLoader imageLoader) {
         if (textView == null) {
@@ -33,33 +41,59 @@ public class CommentUtils {
             return;
         }
 
-        // skip performance hit of html conversion if content doesn't contain html
+        /*
+         *
+         * skip performance hit of html conversion if content doesn't contain html
+         *
+         */
+
         if (!content.contains("<") && !content.contains("&")) {
             content = content.trim();
             textView.setText(content);
-            // make sure unnamed links are clickable
+
+            /*
+             *
+             * make sure unnamed links are clickable
+             *
+             */
+
             if (content.contains("://")) {
                 Linkify.addLinks(textView, Linkify.WEB_URLS);
             }
             return;
         }
 
-        // convert emoticons first (otherwise they'll be downloaded)
+        /*
+         *
+         * convert emoticons first (otherwise they'll be downloaded)
+         *
+         */
+
         content = EmoticonsUtils.replaceEmoticonsWithEmoji(content);
 
-        // now convert to HTML with an image getter that enforces a max image size
+        /*
+         *
+         * now convert to HTML with an image getter that enforces a max image size
+         *
+         */
+
         final Spanned html;
         if (maxImageSize > 0 && content.contains("<img")) {
             Drawable loading = ContextCompat.getDrawable(textView.getContext(),
-                    R.drawable.legacy_dashicon_format_image_big_grey);
+                                                         R.drawable.legacy_dashicon_format_image_big_grey);
             Drawable failed = ContextCompat.getDrawable(textView.getContext(),
-                    R.drawable.ic_notice_grey_500_48dp);
+                                                        R.drawable.ic_notice_grey_500_48dp);
             html = HtmlUtils.fromHtml(content, new WPImageGetter(textView, maxImageSize, imageLoader, loading, failed));
         } else {
             html = HtmlUtils.fromHtml(content);
         }
 
-        // remove extra \n\n added by Html.convert()
+        /*
+         *
+         * remove extra \n\n added by Html.convert()
+         *
+         */
+
         int start = 0;
         int end = html.length();
         while (start < end && Character.isWhitespace(html.charAt(start))) {
@@ -72,37 +106,76 @@ public class CommentUtils {
         textView.setText(html.subSequence(start, end));
     }
 
-    // Assumes all lines after first line will not be indented
+    /*
+     * Assumes all lines after first line will not be indented
+     */
+
     public static void indentTextViewFirstLine(TextView textView, int textOffsetX) {
-        if (textView == null || textOffsetX < 0) return;
+        if (textView == null || textOffsetX < 0) {
+            return;
+        }
+    /*
+     * Text should fit the dimensions provided (do not overpass margin)xz
+     */
 
         SpannableString text = new SpannableString(textView.getText());
-        text.setSpan(new TextWrappingLeadingMarginSpan(textOffsetX), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        text.setSpan(new TextWrappingLeadingMarginSpan(textOffsetX), 0, text.length(),
+                     Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         textView.setText(text);
     }
 
     private static class TextWrappingLeadingMarginSpan implements LeadingMarginSpan.LeadingMarginSpan2 {
-        private final int margin;
-        private final int lines;
+        private final int mMargin;
+        private final int mLines;
 
         TextWrappingLeadingMarginSpan(int margin) {
-            this.margin = margin;
-            this.lines = 1;
+            this.mMargin = margin;
+            this.mLines = 1;
         }
+
+    /**
+     *
+     * @param first true if the request is for the first line of a paragraph,
+     * false for subsequent lines.
+     *
+     * There is an override of getLeadingMargin method so as to define whether
+     * the first line of the message is appropriate and returns true, otherwise
+     * returns false.
+     *
+     */
 
         @Override
         public int getLeadingMargin(boolean first) {
-            return first ? margin : 0;
+            return first ? mMargin : 0;
         }
 
         @Override
-        public void drawLeadingMargin(Canvas c, Paint p, int x, int dir, int top, int baseline, int bottom, CharSequence text, int start, int end, boolean first, Layout layout) {
-
+        public void drawLeadingMargin(Canvas c, Paint p, int x, int dir, int top, int baseline, int bottom,
+                                      CharSequence text, int start, int end, boolean first, Layout layout) {
         }
 
         @Override
         public int getLeadingMarginLineCount() {
-            return lines;
+
+            return mLines;
         }
+
+        /**
+         *
+         * Providing a solution to issue #6832 about Add Word Counts to Comment Editor.
+         *
+         * import based problem doesn't let us complete this solution.
+         * import android.annotation.SuppressLint;
+         * import android.content.Context;
+         * import android.content.res.TypedArray;
+         * import android.util.AttributeSet;
+         * import static org.wordpress.android.WordPress.getContext;
+         *
+         * Context context = getContext();
+         AttributeSet attrs;
+         @SuppressLint("Recycle") TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.SummaryEditTextPreference);
+         mLines = array.getIndexCount();
+         */
+
     }
 }
