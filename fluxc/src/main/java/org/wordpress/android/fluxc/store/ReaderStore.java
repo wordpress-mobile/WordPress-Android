c5d9c65db7229ff08cbbdd3a1a3507a94bded831
Nick Bradbury <nick.bradbury@gmail.com>
2018-06-04 09:33:07 -0400

    Added ReaderAction enum

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/ReaderStore.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/ReaderStore.java
@@ -1,8 +1,16 @@
 package org.wordpress.android.fluxc.store;
 
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.Payload;
+import org.wordpress.android.fluxc.action.ReaderAction;
 import org.wordpress.android.fluxc.annotations.action.Action;
+import org.wordpress.android.fluxc.annotations.action.IAction;
+import org.wordpress.android.fluxc.model.SitesModel;
+import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError;
 import org.wordpress.android.fluxc.network.rest.wpcom.reader.ReaderRestClient;
+import org.wordpress.android.fluxc.store.PostStore.PostError;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.AppLog.T;
 
@@ -24,8 +32,52 @@ public class ReaderStore extends Store {
         AppLog.d(T.API, "ReaderStore onRegister");
     }
 
+    @Subscribe(threadMode = ThreadMode.ASYNC)
     @Override
     public void onAction(Action action) {
-        // todo
+        IAction actionType = action.getType();
+        if (!(actionType instanceof ReaderAction)) {
+            return;
+        }
+        switch ((ReaderAction) actionType) {
+            case READER_SEARCH_SITES:
+                break;
+            case READER_SEARCHED_SITES:
+                break;
+        }
+    }
+
+    public static class ReaderSearchSitesPayload extends Payload<BaseNetworkError> {
+        public String searchTerm;
+        public int offset;
+
+        public ReaderSearchSitesPayload(String searchTerm) {
+            this.searchTerm = searchTerm;
+        }
+
+        public ReaderSearchSitesPayload(String searchTerm, int offset) {
+            this(searchTerm);
+            this.offset = offset;
+        }
+    }
+
+    public static class ReaderSearchSitesResponsePayload extends Payload<PostError> {
+        public SitesModel sites;
+        public String searchTerm;
+        public boolean loadedMore;
+        public boolean canLoadMore;
+
+        public ReaderSearchSitesResponsePayload(SitesModel sites, String searchTerm,
+                                                boolean loadedMore, boolean canLoadMore) {
+            this.sites = sites;
+            this.searchTerm = searchTerm;
+            this.loadedMore = loadedMore;
+            this.canLoadMore = canLoadMore;
+        }
+
+        public ReaderSearchSitesResponsePayload(String searchTerm, PostError error) {
+            this.searchTerm = searchTerm;
+            this.error = error;
+        }
     }
 }
