6ad6b4daf49f10fd9a63eeb81411db9227021829
Nick Bradbury <nick.bradbury@gmail.com>
2018-06-04 13:02:46 -0400

    Added ReaderSearchSitesDeserializer

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/ReaderFeedModel.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/ReaderFeedModel.java
@@ -4,34 +4,8 @@ import org.wordpress.android.fluxc.Payload;
 import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError;
 
 public class ReaderFeedModel extends Payload<BaseNetworkError> {
-    /*
-    {
-    "algorithm": "reader/manage/search:0",
-    "feeds": [
-        {
-            "feed_ID": "46238560",
-            "meta": {
-                "links": {
-                    "feed": "https://public-api.wordpress.com/rest/v1.1/read/feed/46238560"
-                }
-            },
-            "railcar": {
-                "fetch_algo": "reader/manage/search:0",
-                "fetch_lang": "en",
-                "fetch_position": 0,
-                "fetch_query": "yoga",
-                "railcar": "8&6VDLsINQLj",
-                "rec_feed_id": 46238560
-            },
-            "subscribe_URL": "http://theyogalunchbox.co.nz/feed",
-            "subscribers_count": 167,
-            "title": "The Yoga Lunchbox",
-            "URL": "https://theyogalunchbox.co.nz/"
-        },
-     */
-
     private long mFeedId;
-    public int SubscriberCount;
+    public int mSubscriberCount;
     private String mSubscribeUrl;
     private String mTitle;
     private String mUrl;
@@ -45,11 +19,11 @@ public class ReaderFeedModel extends Payload<BaseNetworkError> {
     }
 
     public int getSubscriberCount() {
-        return SubscriberCount;
+        return mSubscriberCount;
     }
 
     public void setSubscriberCount(int subscriberCount) {
-        SubscriberCount = subscriberCount;
+        mSubscriberCount = subscriberCount;
     }
 
     public String getSubscribeUrl() {
--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/reader/ReaderSearchSitesResponse.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/reader/ReaderSearchSitesResponse.java
@@ -2,10 +2,15 @@ package org.wordpress.android.fluxc.network.rest.wpcom.reader;
 
 import android.support.annotation.NonNull;
 
-import org.wordpress.android.fluxc.model.SitesModel;
+import com.google.gson.annotations.JsonAdapter;
 
+import org.wordpress.android.fluxc.model.ReaderFeedModel;
+
+import java.util.List;
+
+@JsonAdapter(ReaderSearchSitesDeserializer.class)
 public class ReaderSearchSitesResponse {
     public boolean canLoadMore;
     public int offset;
-    public @NonNull SitesModel sites;
+    public @NonNull List<ReaderFeedModel> feeds;
 }
