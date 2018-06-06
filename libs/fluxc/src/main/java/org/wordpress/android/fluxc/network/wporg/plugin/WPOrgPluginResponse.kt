fecc687919eb122faa6e07e0791854d381801b79
Oguz Kocer <oguz.kocer@automattic.com>
2018-06-06 16:09:14 -0400

    WPOrgPluginResponse is now immutable

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/wporg/plugin/WPOrgPluginResponse.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/wporg/plugin/WPOrgPluginResponse.kt
@@ -11,36 +11,34 @@ import org.wordpress.android.fluxc.network.utils.getString
 import java.lang.reflect.Type
 
 @JsonAdapter(WPOrgPluginDeserializer::class)
-class WPOrgPluginResponse {
-    var authorAsHtml: String? = null
-    var banner: String? = null
-    var homepageUrl: String? = null
-    var icon: String? = null
-    var lastUpdated: String? = null
-    var name: String? = null
-    var rating: String? = null
-    var requiredWordPressVersion: String? = null
-    var slug: String? = null
-    var version: String? = null
-    var downloadCount: Int = 0
+class WPOrgPluginResponse (
+    val authorAsHtml: String? = null,
+    val banner: String? = null,
+    val homepageUrl: String? = null,
+    val icon: String? = null,
+    val lastUpdated: String? = null,
+    val name: String? = null,
+    val rating: String? = null,
+    val requiredWordPressVersion: String? = null,
+    val slug: String? = null,
+    val version: String? = null,
+    val downloadCount: Int = 0,
 
-    // Sections
-    var descriptionAsHtml: String? = null
-    var faqAsHtml: String? = null
-    var installationInstructionsAsHtml: String? = null
-    var whatsNewAsHtml: String? = null
+    // Sections,
+    val descriptionAsHtml: String? = null,
+    val faqAsHtml: String? = null,
+    val installationInstructionsAsHtml: String? = null,
+    val whatsNewAsHtml: String? = null,
 
-    // Ratings
-    var numberOfRatings: Int = 0
-    var numberOfRatingsOfOne: Int = 0
-    var numberOfRatingsOfTwo: Int = 0
-    var numberOfRatingsOfThree: Int = 0
-    var numberOfRatingsOfFour: Int = 0
-    var numberOfRatingsOfFive: Int = 0
-
-    // Errors are returned with success code
-    var errorMessage: String? = null
-}
+    // Ratings,
+    val numberOfRatings: Int = 0,
+    val numberOfRatingsOfOne: Int = 0,
+    val numberOfRatingsOfTwo: Int = 0,
+    val numberOfRatingsOfThree: Int = 0,
+    val numberOfRatingsOfFour: Int = 0,
+    val numberOfRatingsOfFive: Int = 0,
+    val errorMessage: String? = null
+)
 
 class WPOrgPluginDeserializer : JsonDeserializer<WPOrgPluginResponse> {
     override fun deserialize(
@@ -49,59 +47,57 @@ class WPOrgPluginDeserializer : JsonDeserializer<WPOrgPluginResponse> {
         context: JsonDeserializationContext
     ): WPOrgPluginResponse {
         val jsonObject = json.asJsonObject
-        val response = WPOrgPluginResponse()
-        response.errorMessage = jsonObject.getString("error")
+
+        val errorMessage = jsonObject.getString("error")
         // Response has an error instead of the plugin information
-        if (!response.errorMessage.isNullOrEmpty()) {
-            return response
+        if (!errorMessage.isNullOrEmpty()) {
+            return WPOrgPluginResponse(errorMessage = errorMessage)
         }
-        response.authorAsHtml = jsonObject.getString("author")
-        response.banner = getBannerFromJson(jsonObject)
-        response.downloadCount = jsonObject.getInt("downloaded")
-        response.icon = getIconFromJson(jsonObject)
-        response.homepageUrl = jsonObject.getString("homepage")
-        response.lastUpdated = jsonObject.getString("last_updated")
-        response.name = jsonObject.getString("name")
-        response.rating = jsonObject.getString("rating")
-        response.requiredWordPressVersion = jsonObject.getString("requires")
-        response.slug = jsonObject.getString("slug")
-        response.version = jsonObject.getString("version")
+        val sections = jsonObject.getJsonObject("sections")
+        val ratings = jsonObject.getJsonObject("ratings")
+        return WPOrgPluginResponse(
+                authorAsHtml = jsonObject.getString("author"),
+                banner = getBannerFromJson(jsonObject),
+                downloadCount = jsonObject.getInt("downloaded"),
+                icon = getIconFromJson(jsonObject),
+                homepageUrl = jsonObject.getString("homepage"),
+                lastUpdated = jsonObject.getString("last_updated"),
+                name = jsonObject.getString("name"),
+                rating = jsonObject.getString("rating"),
+                requiredWordPressVersion = jsonObject.getString("requires"),
+                slug = jsonObject.getString("slug"),
+                version = jsonObject.getString("version"),
 
-        // Sections
-        jsonObject.getJsonObject("sections")?.let { sections ->
-            response.descriptionAsHtml = sections.getString("description")
-            response.faqAsHtml = sections.getString("faq")
-            response.installationInstructionsAsHtml = sections.getString("installation")
-            response.whatsNewAsHtml = sections.getString("changelog")
-        }
+                // sections
+                descriptionAsHtml = sections.getString("description"),
+                faqAsHtml = sections.getString("faq"),
+                installationInstructionsAsHtml = sections.getString("installation"),
+                whatsNewAsHtml = sections.getString("changelog"),
 
-        // Ratings
-        response.numberOfRatings = jsonObject.getInt("num_ratings")
-        jsonObject.getJsonObject("ratings")?.let { ratings ->
-            response.numberOfRatingsOfOne = ratings.getInt("1")
-            response.numberOfRatingsOfTwo = ratings.getInt("2")
-            response.numberOfRatingsOfThree = ratings.getInt("3")
-            response.numberOfRatingsOfFour = ratings.getInt("4")
-            response.numberOfRatingsOfFive = ratings.getInt("5")
-        }
-
-        return response
+                // Ratings
+                numberOfRatings = jsonObject.getInt("num_ratings"),
+                numberOfRatingsOfOne = ratings.getInt("1"),
+                numberOfRatingsOfTwo = ratings.getInt("2"),
+                numberOfRatingsOfThree = ratings.getInt("3"),
+                numberOfRatingsOfFour = ratings.getInt("4"),
+                numberOfRatingsOfFive = ratings.getInt("5")
+        )
     }
 
     private fun getBannerFromJson(jsonObject: JsonObject): String? {
         val banners = jsonObject.getJsonObject("banners")
-        banners?.getString("high")?.let { bannerUrlHigh ->
+        banners.getString("high")?.let { bannerUrlHigh ->
             // When high version is not available API returns `false` instead of `null`
             if (!bannerUrlHigh.equals("false", ignoreCase = true)) {
                 return bannerUrlHigh
             }
         }
         // High version wasn't available
-        return banners?.getString("low")
+        return banners.getString("low")
     }
 
     private fun getIconFromJson(jsonObject: JsonObject): String? {
         val icons = jsonObject.getJsonObject("icons")
-        return icons?.getString("2x") ?: icons?.getString("1x")
+        return icons.getString("2x") ?: icons.getString("1x")
     }
 }
