0aab4b6192e643bcdb6d710b7db74badee0777c8
Oguz Kocer <oguz.kocer@automattic.com>
2018-06-06 16:09:14 -0400

    WPOrgPluginResponse is now immutable

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/utils/JsonObjectExtensions.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/utils/JsonObjectExtensions.kt
@@ -3,13 +3,13 @@ package org.wordpress.android.fluxc.network.utils
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 
-fun JsonObject.getString(property: String): String? = checkAndGet(property)?.asString
-fun JsonObject.getInt(property: String, defaultValue: Int = 0): Int = checkAndGet(property)?.asInt ?: defaultValue
-fun JsonObject.getJsonObject(property: String): JsonObject? {
+fun JsonObject?.getString(property: String): String? = checkAndGet(property)?.asString
+fun JsonObject?.getInt(property: String, defaultValue: Int = 0): Int = checkAndGet(property)?.asInt ?: defaultValue
+fun JsonObject?.getJsonObject(property: String): JsonObject? {
     val obj = checkAndGet(property)
     return if (obj?.isJsonObject == true) obj.asJsonObject else null
 }
 
-private fun JsonObject.checkAndGet(property: String): JsonElement? = if (has(property)) {
+private fun JsonObject?.checkAndGet(property: String): JsonElement? = if (this?.has(property) == true) {
     this.get(property)
 } else null
