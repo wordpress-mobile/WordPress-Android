ed39dc73885e6adf1e412f939dee113187c874dc
Oguz Kocer <oguz.kocer@automattic.com>
2018-06-06 15:37:10 -0400

    JsonObjectExstensions introduced

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/utils/JsonObjectExtensions.kt
@@ -0,0 +1,15 @@
+package org.wordpress.android.fluxc.network.utils
+
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+
+fun JsonObject.getString(property: String): String? = checkAndGet(property)?.asString
+fun JsonObject.getInt(property: String, defaultValue: Int = 0): Int = checkAndGet(property)?.asInt ?: defaultValue
+fun JsonObject.getJsonObject(property: String): JsonObject? {
+    val obj = checkAndGet(property)
+    return if (obj?.isJsonObject == true) obj.asJsonObject else null
+}
+
+private fun JsonObject.checkAndGet(property: String): JsonElement? = if (has(property)) {
+    this.get(property)
+} else null
