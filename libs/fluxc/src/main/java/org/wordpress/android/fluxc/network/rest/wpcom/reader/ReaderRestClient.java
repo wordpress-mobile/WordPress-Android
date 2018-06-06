2761f419a8fe7f6b3fcbbc55cae33dc888f7cc2c
Nick Bradbury <nick.bradbury@gmail.com>
2018-06-06 14:37:01 -0400

    Fixed indented ");"

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/reader/ReaderRestClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/reader/ReaderRestClient.java
@@ -73,7 +73,7 @@ public class ReaderRestClient extends BaseWPComRestClient {
                         mDispatcher.dispatch(ReaderActionBuilder.newReaderSearchedSitesAction(payload));
                     }
                 }
-                                                                   );
+        );
         add(request);
     }
 }
