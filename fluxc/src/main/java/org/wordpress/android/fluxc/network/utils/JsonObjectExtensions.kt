915f3298515e2fd2a9ccfbfbab1283617fa3abee
Oguz Kocer <oguz.kocer@automattic.com>
2018-06-06 16:47:41 -0400

    Add unescapeHtml4 option to json string deserializer

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/utils/JsonObjectExtensions.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/utils/JsonObjectExtensions.kt
@@ -2,9 +2,17 @@ package org.wordpress.android.fluxc.network.utils
 
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import org.apache.commons.text.StringEscapeUtils
 
-fun JsonObject?.getString(property: String): String? = checkAndGet(property)?.asString
+fun JsonObject?.getString(property: String, unescapeHtml4: Boolean = false): String? {
+    val str = checkAndGet(property)?.asString
+    return if (unescapeHtml4) {
+        StringEscapeUtils.unescapeHtml4(str)
+    } else str
+}
+
 fun JsonObject?.getInt(property: String, defaultValue: Int = 0): Int = checkAndGet(property)?.asInt ?: defaultValue
+
 fun JsonObject?.getJsonObject(property: String): JsonObject? {
     val obj = checkAndGet(property)
     return if (obj?.isJsonObject == true) obj.asJsonObject else null
