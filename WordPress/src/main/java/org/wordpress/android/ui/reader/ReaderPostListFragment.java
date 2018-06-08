646254974387395af04e5502726794a92f1d5177
Nick Bradbury <nick.bradbury@gmail.com>
2018-06-08 10:06:54 -0400

    Don't update sites when site tab clicked if empty view is showing

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListFragment.java
@@ -876,7 +876,10 @@ private static class HistoryStack extends Stack<String> {
                         if (mSiteSearchAdapterPos > 0) {
                             mRecyclerView.scrollRecycleViewToPosition(mSiteSearchAdapterPos);
                         }
-                        if (getSiteSearchAdapter().isEmpty() && !TextUtils.isEmpty(mCurrentSearchQuery)) {
+                        // perform a site search if the user switched to the site tab and results aren't already showing
+                        if (getSiteSearchAdapter().isEmpty()
+                            && !TextUtils.isEmpty(mCurrentSearchQuery)
+                            && !isEmptyViewShowing()) {
                             updateSitesInCurrentSearch(0);
                         }
                     }
@@ -1221,6 +1224,10 @@ private static class HistoryStack extends Stack<String> {
         if (isAdded()) {
             mEmptyView.setVisibility(View.GONE);
         }
+    }
+
+    private boolean isEmptyViewShowing() {
+        return isAdded() && mEmptyView.getVisibility() == View.VISIBLE;
     }
 
     /*
