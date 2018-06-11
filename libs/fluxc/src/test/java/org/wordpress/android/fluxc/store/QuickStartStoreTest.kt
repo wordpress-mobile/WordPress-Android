00dce343d1d887157edd4b10066cfac93614febe
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2018-06-12 00:39:02 +0300

    Minor refactoring - naming, imported inner class.

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/store/QuickStartStoreTest.kt
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/store/QuickStartStoreTest.kt
@@ -11,12 +11,13 @@ import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.SingleStoreWellSqlConfigForTests
 import org.wordpress.android.fluxc.model.QuickStartModel
 import org.wordpress.android.fluxc.persistence.QuickStartSqlUtils
+import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartTask
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 @RunWith(RobolectricTestRunner::class)
 class QuickStartStoreTest {
-    private val TEST_LOCAL_SITE_ID: Long = 72
+    private val testLocalSiteId: Long = 72
     private lateinit var quickStartStore: QuickStartStore
 
     @Before
@@ -31,45 +32,45 @@ class QuickStartStoreTest {
 
     @Test
     fun testDoneCount() {
-        assertEquals(0, quickStartStore.getDoneCount(TEST_LOCAL_SITE_ID))
+        assertEquals(0, quickStartStore.getDoneCount(testLocalSiteId))
 
-        quickStartStore.setDoneTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.FOLLOW_SITE, true)
-        assertEquals(1, quickStartStore.getDoneCount(TEST_LOCAL_SITE_ID))
+        quickStartStore.setDoneTask(testLocalSiteId, QuickStartTask.FOLLOW_SITE, true)
+        assertEquals(1, quickStartStore.getDoneCount(testLocalSiteId))
 
-        quickStartStore.setDoneTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.FOLLOW_SITE, false)
-        assertEquals(0, quickStartStore.getDoneCount(TEST_LOCAL_SITE_ID))
+        quickStartStore.setDoneTask(testLocalSiteId, QuickStartTask.FOLLOW_SITE, false)
+        assertEquals(0, quickStartStore.getDoneCount(testLocalSiteId))
     }
 
     @Test
     fun testShownCount() {
-        assertEquals(0, quickStartStore.getShownCount(TEST_LOCAL_SITE_ID))
+        assertEquals(0, quickStartStore.getShownCount(testLocalSiteId))
 
-        quickStartStore.setShownTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CHOOSE_THEME, true)
-        assertEquals(1, quickStartStore.getShownCount(TEST_LOCAL_SITE_ID))
+        quickStartStore.setShownTask(testLocalSiteId, QuickStartTask.CHOOSE_THEME, true)
+        assertEquals(1, quickStartStore.getShownCount(testLocalSiteId))
 
-        quickStartStore.setShownTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CHOOSE_THEME, false)
-        assertEquals(0, quickStartStore.getShownCount(TEST_LOCAL_SITE_ID))
+        quickStartStore.setShownTask(testLocalSiteId, QuickStartTask.CHOOSE_THEME, false)
+        assertEquals(0, quickStartStore.getShownCount(testLocalSiteId))
     }
 
     @Test
     fun testTaskDoneStatus() {
-        assertFalse(quickStartStore.hasDoneTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CUSTOMIZE_SITE))
+        assertFalse(quickStartStore.hasDoneTask(testLocalSiteId, QuickStartTask.CUSTOMIZE_SITE))
 
-        quickStartStore.setDoneTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CUSTOMIZE_SITE, true)
-        assertTrue(quickStartStore.hasDoneTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CUSTOMIZE_SITE))
+        quickStartStore.setDoneTask(testLocalSiteId, QuickStartTask.CUSTOMIZE_SITE, true)
+        assertTrue(quickStartStore.hasDoneTask(testLocalSiteId, QuickStartTask.CUSTOMIZE_SITE))
 
-        quickStartStore.setDoneTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CUSTOMIZE_SITE, false)
-        assertFalse(quickStartStore.hasDoneTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CUSTOMIZE_SITE))
+        quickStartStore.setDoneTask(testLocalSiteId, QuickStartTask.CUSTOMIZE_SITE, false)
+        assertFalse(quickStartStore.hasDoneTask(testLocalSiteId, QuickStartTask.CUSTOMIZE_SITE))
     }
 
     @Test
     fun testTaskShownStatus() {
-        assertFalse(quickStartStore.hasShownTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CREATE_SITE))
+        assertFalse(quickStartStore.hasShownTask(testLocalSiteId, QuickStartTask.CREATE_SITE))
 
-        quickStartStore.setShownTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CREATE_SITE, true)
-        assertTrue(quickStartStore.hasShownTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CREATE_SITE))
+        quickStartStore.setShownTask(testLocalSiteId, QuickStartTask.CREATE_SITE, true)
+        assertTrue(quickStartStore.hasShownTask(testLocalSiteId, QuickStartTask.CREATE_SITE))
 
-        quickStartStore.setShownTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CREATE_SITE, false)
-        assertFalse(quickStartStore.hasShownTask(TEST_LOCAL_SITE_ID, QuickStartStore.QuickStartTask.CREATE_SITE))
+        quickStartStore.setShownTask(testLocalSiteId, QuickStartTask.CREATE_SITE, false)
+        assertFalse(quickStartStore.hasShownTask(testLocalSiteId, QuickStartTask.CREATE_SITE))
     }
 }
