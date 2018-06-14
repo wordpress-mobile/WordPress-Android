7f84f1bdc4aec0f151dddcce1090d1b095420373
Mario Zorz <mariozorz@gmail.com>
2018-06-14 08:17:18 -0300

    moved method contentContainsGutenbergBlocks() to PostUtils

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostUtils.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostUtils.java
@@ -38,6 +38,8 @@ public class PostUtils {
 
     private static final HashSet<String> SHORTCODE_TABLE = new HashSet<>();
 
+    private static final String GUTENBERG_BLOCK_START = "<!-- wp:";
+
     /*
      * collapses shortcodes in the passed post content, stripping anything between the
      * shortcode name and the closing brace
@@ -353,5 +355,28 @@ public class PostUtils {
         }
 
         return postsThatContainListedMedia;
+    }
+
+    /*
+    Note the way we detect we're in presence of Gutenberg blocks logic is taken from
+    https://github.com/WordPress/gutenberg/blob/5a6693589285363341bebad15bd56d9371cf8ecc/lib/register.php#L331-L345
+
+    * Determine whether a content string contains blocks. This test optimizes for
+    * performance rather than strict accuracy, detecting the pattern of a block
+    * but not validating its structure. For strict accuracy, you should use the
+    * block parser on post content.
+    *
+    * @since 1.6.0
+    * @see gutenberg_parse_blocks()
+    *
+    * @param string $content Content to test.
+    * @return bool Whether the content contains blocks.
+
+    function gutenberg_content_has_blocks( $content ) {
+        return false !== strpos( $content, '<!-- wp:' );
+    }
+    */
+    public static boolean contentContainsGutenbergBlocks(String postContent) {
+        return (postContent != null && postContent.contains(GUTENBERG_BLOCK_START));
     }
 }
