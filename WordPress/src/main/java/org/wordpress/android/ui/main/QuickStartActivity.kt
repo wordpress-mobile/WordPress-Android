dc7b2f3d2a39b17d6dd7fa9976b2998e47abe86a
Klymentiy Haykov <forsver@gmail.com>
2018-06-15 18:09:54 +0300

    Slimed down activity a bit.

--- WordPress/src/main/java/org/wordpress/android/ui/main/QuickStartActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/QuickStartActivity.kt
@@ -37,15 +37,17 @@ class QuickStartActivity : AppCompatActivity(), BasicFragmentDialog.BasicDialogP
         (application as WordPress).component()?.inject(this)
         setContentView(R.layout.quick_start_activity)
 
-        checkTasksCompleted()
+        checkCompletedTasks()
         setTasksClickListeners()
     }
 
+    // TODO to be used in future branches
     override fun onBackPressed() {
         setResult(Activity.RESULT_OK)
         finish()
     }
 
+    // TODO to be used in future branches
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
             setResult(Activity.RESULT_OK)
@@ -55,172 +57,40 @@ class QuickStartActivity : AppCompatActivity(), BasicFragmentDialog.BasicDialogP
         return super.onOptionsItemSelected(item)
     }
 
-    private fun checkTasksCompleted() {
-        if (areAllTasksCompleted()) {
-            setTasksCompleted()
-        } else {
-            setTasksDone()
-        }
-    }
-
-    private fun areAllTasksCompleted(): Boolean {
-        QuickStartTask.values().forEach {
-            if (it != QuickStartTask.CREATE_SITE) { // CREATE_SITE is completed by default, regardless of DB flag
-                if (!quickStartStore.hasDoneTask(site.toLong(), it)) {
-                    return@areAllTasksCompleted false
-                }
-            }
-        }
-        return true
-    }
-
     private fun setTasksClickListeners() {
         findViewById<RelativeLayout>(R.id.layout_view_site).setOnClickListener {
             quickStartStore.setDoneTask(site.toLong(), QuickStartTask.VIEW_SITE, true)
-            checkTasksCompleted()
+            checkCompletedTasks()
         }
 
         findViewById<RelativeLayout>(R.id.layout_browse_themes).setOnClickListener {
             quickStartStore.setDoneTask(site.toLong(), QuickStartTask.CHOOSE_THEME, true)
-            checkTasksCompleted()
+            checkCompletedTasks()
         }
 
         findViewById<RelativeLayout>(R.id.layout_customize_site).setOnClickListener {
             quickStartStore.setDoneTask(site.toLong(), QuickStartTask.CUSTOMIZE_SITE, true)
-            checkTasksCompleted()
+            checkCompletedTasks()
         }
 
         findViewById<RelativeLayout>(R.id.layout_share_site).setOnClickListener {
             quickStartStore.setDoneTask(site.toLong(), QuickStartTask.SHARE_SITE, true)
-            checkTasksCompleted()
+            checkCompletedTasks()
         }
 
         findViewById<RelativeLayout>(R.id.layout_publish_post).setOnClickListener {
             quickStartStore.setDoneTask(site.toLong(), QuickStartTask.PUBLISH_POST, true)
-            checkTasksCompleted()
+            checkCompletedTasks()
         }
 
         findViewById<RelativeLayout>(R.id.layout_follow_site).setOnClickListener {
             quickStartStore.setDoneTask(site.toLong(), QuickStartTask.FOLLOW_SITE, true)
-            checkTasksCompleted()
+            checkCompletedTasks()
         }
 
         findViewById<AppCompatButton>(R.id.button_skip_all).setOnClickListener {
             showSkipDialog()
         }
-    }
-
-    private fun setTasksCompleted() {
-        val titleCreateSite = findViewById<TextView>(R.id.title_create_site)
-        titleCreateSite.paintFlags = titleCreateSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        val doneCreateSite = findViewById<ImageView>(R.id.done_create_site)
-        doneCreateSite.visibility = View.VISIBLE
-
-        val titleViewSite = findViewById<TextView>(R.id.title_view_site)
-        titleViewSite.paintFlags = titleViewSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_view_site).visibility = View.VISIBLE
-
-        val titleBrowseThemes = findViewById<TextView>(R.id.title_browse_themes)
-        titleBrowseThemes.paintFlags = titleBrowseThemes.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_browse_themes).visibility = View.VISIBLE
-
-        val titleCustomizeSite = findViewById<TextView>(R.id.title_customize_site)
-        titleCustomizeSite.paintFlags = titleCustomizeSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_customize_site).visibility = View.VISIBLE
-
-        val titleAddSocial = findViewById<TextView>(R.id.title_share_site)
-        titleAddSocial.paintFlags = titleAddSocial.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_share_site).visibility = View.VISIBLE
-
-        val titlePublishPost = findViewById<TextView>(R.id.title_publish_post)
-        titlePublishPost.paintFlags = titlePublishPost.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_publish_post).visibility = View.VISIBLE
-
-        val titleFollowSite = findViewById<TextView>(R.id.title_follow_site)
-        titleFollowSite.paintFlags = titleFollowSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_follow_site).visibility = View.VISIBLE
-
-        val layoutListComplete = findViewById<LinearLayout>(R.id.layout_list_complete)
-        layoutListComplete.visibility = View.VISIBLE
-        findViewById<RelativeLayout>(R.id.layout_skip_all).visibility = View.GONE
-    }
-
-    private fun setTasksDone() {
-        val titleCreateSite = findViewById<TextView>(R.id.title_create_site)
-        titleCreateSite.paintFlags = titleCreateSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_create_site).visibility = View.VISIBLE
-
-        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.VIEW_SITE)) {
-            val titleViewSite = findViewById<TextView>(R.id.title_view_site)
-            titleViewSite.paintFlags = titleViewSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-            findViewById<ImageView>(R.id.done_view_site).visibility = View.VISIBLE
-        }
-
-        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.CHOOSE_THEME)) {
-            val titleBrowseThemes = findViewById<TextView>(R.id.title_browse_themes)
-            titleBrowseThemes.paintFlags = titleBrowseThemes.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-            findViewById<ImageView>(R.id.done_browse_themes).visibility = View.VISIBLE
-        }
-
-        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.CUSTOMIZE_SITE)) {
-            val titleCustomizeSite = findViewById<TextView>(R.id.title_customize_site)
-            titleCustomizeSite.paintFlags = titleCustomizeSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-            findViewById<ImageView>(R.id.done_customize_site).visibility = View.VISIBLE
-        }
-
-        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.SHARE_SITE)) {
-            val titleAddSocial = findViewById<TextView>(R.id.title_share_site)
-            titleAddSocial.paintFlags = titleAddSocial.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-            findViewById<ImageView>(R.id.done_share_site).visibility = View.VISIBLE
-        }
-
-        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.PUBLISH_POST)) {
-            val titlePublishPost = findViewById<TextView>(R.id.title_publish_post)
-            titlePublishPost.paintFlags = titlePublishPost.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-            findViewById<ImageView>(R.id.done_publish_post).visibility = View.VISIBLE
-        }
-
-        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.FOLLOW_SITE)) {
-            val titleFollowSite = findViewById<TextView>(R.id.title_follow_site)
-            titleFollowSite.paintFlags = titleFollowSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-            findViewById<ImageView>(R.id.done_follow_site).visibility = View.VISIBLE
-        }
-    }
-
-    private fun setTasksSkip() {
-        QuickStartTask.values().forEach { quickStartStore.setDoneTask(site.toLong(), it, true) }
-
-        val titleCreateSite = findViewById<TextView>(R.id.title_create_site)
-        titleCreateSite.paintFlags = titleCreateSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_create_site).visibility = View.VISIBLE
-
-        val titleViewSite = findViewById<TextView>(R.id.title_view_site)
-        titleViewSite.paintFlags = titleViewSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_view_site).visibility = View.VISIBLE
-
-        val titleBrowseThemes = findViewById<TextView>(R.id.title_browse_themes)
-        titleBrowseThemes.paintFlags = titleBrowseThemes.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_browse_themes).visibility = View.VISIBLE
-
-        val titleCustomizeSite = findViewById<TextView>(R.id.title_customize_site)
-        titleCustomizeSite.paintFlags = titleCustomizeSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_customize_site).visibility = View.VISIBLE
-
-        val titleAddSocial = findViewById<TextView>(R.id.title_share_site)
-        titleAddSocial.paintFlags = titleAddSocial.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_share_site).visibility = View.VISIBLE
-
-        val titlePublishPost = findViewById<TextView>(R.id.title_publish_post)
-        titlePublishPost.paintFlags = titlePublishPost.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_publish_post).visibility = View.VISIBLE
-
-        val titleFollowSite = findViewById<TextView>(R.id.title_follow_site)
-        titleFollowSite.paintFlags = titleFollowSite.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        findViewById<ImageView>(R.id.done_follow_site).visibility = View.VISIBLE
-
-        val layoutListComplete = findViewById<LinearLayout>(R.id.layout_list_complete)
-        layoutListComplete.visibility = View.VISIBLE
-        findViewById<RelativeLayout>(R.id.layout_skip_all).visibility = View.GONE
     }
 
     private fun showSkipDialog() {
@@ -235,11 +105,72 @@ class QuickStartActivity : AppCompatActivity(), BasicFragmentDialog.BasicDialogP
     }
 
     override fun onPositiveClicked(instanceTag: String) {
-        findViewById<ScrollView>(R.id.checklist_scrollview).let {
-            it.post {
-                findViewById<ScrollView>(R.id.checklist_scrollview).smoothScrollTo(0, 0)
-                setTasksSkip()
+        findViewById<ScrollView>(R.id.checklist_scrollview).smoothScrollTo(0, 0)
+        skipAllTasks()
+    }
+
+    private fun skipAllTasks() {
+        QuickStartTask.values().forEach { quickStartStore.setDoneTask(site.toLong(), it, true) }
+        checkCompletedTasks()
+    }
+
+    private fun checkCompletedTasks() {
+        crossOutCompletedTasks()
+
+        if (areAllTasksCompleted()) {
+            findViewById<LinearLayout>(R.id.layout_list_complete).visibility = View.VISIBLE
+            findViewById<RelativeLayout>(R.id.layout_skip_all).visibility = View.GONE
+        }
+    }
+
+    private fun areAllTasksCompleted(): Boolean {
+        QuickStartTask.values().forEach {
+            if (it != QuickStartTask.CREATE_SITE) { // CREATE_SITE is completed by default, regardless of DB flag
+                if (!quickStartStore.hasDoneTask(site.toLong(), it)) {
+                    return@areAllTasksCompleted false
+                }
             }
         }
+        return true
+    }
+
+    private fun crossOutCompletedTasks() {
+        // Create Site task is completed by default
+        strikeThroughTextView(findViewById(R.id.title_create_site))
+        findViewById<ImageView>(R.id.done_create_site).visibility = View.VISIBLE
+
+        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.VIEW_SITE)) {
+            strikeThroughTextView(findViewById(R.id.title_view_site))
+            findViewById<ImageView>(R.id.done_view_site).visibility = View.VISIBLE
+        }
+
+        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.CHOOSE_THEME)) {
+            strikeThroughTextView(findViewById(R.id.title_browse_themes))
+            findViewById<ImageView>(R.id.done_browse_themes).visibility = View.VISIBLE
+        }
+
+        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.CUSTOMIZE_SITE)) {
+            strikeThroughTextView(findViewById(R.id.title_customize_site))
+            findViewById<ImageView>(R.id.done_customize_site).visibility = View.VISIBLE
+        }
+
+        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.SHARE_SITE)) {
+            strikeThroughTextView(findViewById(R.id.title_share_site))
+            findViewById<ImageView>(R.id.done_share_site).visibility = View.VISIBLE
+        }
+
+        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.PUBLISH_POST)) {
+            strikeThroughTextView(findViewById(R.id.title_publish_post))
+            findViewById<ImageView>(R.id.done_publish_post).visibility = View.VISIBLE
+        }
+
+        if (quickStartStore.hasDoneTask(site.toLong(), QuickStartTask.FOLLOW_SITE)) {
+            strikeThroughTextView(findViewById(R.id.title_follow_site))
+            findViewById<ImageView>(R.id.done_follow_site).visibility = View.VISIBLE
+        }
+    }
+
+    private fun strikeThroughTextView(textView: TextView) {
+        textView.let { it.paintFlags = it.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG }
     }
 }
