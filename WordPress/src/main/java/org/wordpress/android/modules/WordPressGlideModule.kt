e71c40a95a9e6bf642be20246e7f488b1a5e0983
malinajirka <malinajirka@gmail.com>
2018-06-20 11:25:02 +0200

    Fix glide auth headers

--- WordPress/src/main/java/org/wordpress/android/modules/WordPressGlideModule.kt
+++ WordPress/src/main/java/org/wordpress/android/modules/WordPressGlideModule.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.modules
 
 import android.content.Context
-
 import com.android.volley.RequestQueue
 import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
@@ -10,15 +9,9 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.volley.VolleyUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
-
 import org.wordpress.android.WordPress
-import org.wordpress.android.fluxc.network.HTTPAuthManager
-import org.wordpress.android.fluxc.network.UserAgent
-import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
-import org.wordpress.android.networking.GlideHeaderLoaderFactory
-
+import org.wordpress.android.networking.GlideRequestFactory
 import java.io.InputStream
-
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -29,9 +22,7 @@ import javax.inject.Named
 @GlideModule
 class WordPressGlideModule : AppGlideModule() {
     @Inject @field:Named("custom-ssl") lateinit var requestQueue: RequestQueue
-    @Inject lateinit var accessToken: AccessToken
-    @Inject lateinit var httpAuthManager: HTTPAuthManager
-    @Inject lateinit var userAgent: UserAgent
+    @Inject lateinit var glideRequestFactory: GlideRequestFactory
 
     override fun applyOptions(context: Context, builder: GlideBuilder) {}
 
@@ -41,8 +32,7 @@ class WordPressGlideModule : AppGlideModule() {
 
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
         (context as WordPress).component().inject(this)
-        registry.replace(GlideUrl::class.java, InputStream::class.java, VolleyUrlLoader.Factory(requestQueue))
-        registry.append(GlideUrl::class.java, InputStream::class.java,
-                GlideHeaderLoaderFactory(accessToken, httpAuthManager, userAgent))
+        registry.replace(GlideUrl::class.java, InputStream::class.java,
+                VolleyUrlLoader.Factory(requestQueue, glideRequestFactory))
     }
 }
