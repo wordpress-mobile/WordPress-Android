0781bdf8acc3598e2b49423fa36e725c96598684
malinajirka <malinajirka@gmail.com>
2018-07-03 07:58:01 +0200

    Use private extension functions in ImageManager

--- WordPress/src/main/java/org/wordpress/android/util/image/ImageManager.kt
+++ WordPress/src/main/java/org/wordpress/android/util/image/ImageManager.kt
@@ -23,69 +23,69 @@ class ImageManager @Inject constructor(val placeholderManager: ImagePlaceholderM
         imgUrl: String,
         scaleType: ImageView.ScaleType = CENTER
     ) {
-        var request = GlideApp.with(imageView.context)
+        GlideApp.with(imageView.context)
                 .load(imgUrl)
-        request = addFallback(request, imageType)
-        request = addPlaceholder(request, imageType)
-        request = applyScaleType(request, scaleType)
-        request.into(imageView)
+                .addFallback(imageType)
+                .addPlaceholder(imageType)
+                .applyScaleType(scaleType)
+                .into(imageView)
     }
 
     @JvmOverloads
     fun load(imageView: ImageView, bitmap: Bitmap, scaleType: ImageView.ScaleType = CENTER) {
-        var request = GlideApp.with(imageView.context)
+        GlideApp.with(imageView.context)
                 .load(bitmap)
-        request = applyScaleType(request, scaleType)
-        request.into(imageView)
+                .applyScaleType(scaleType)
+                .into(imageView)
     }
 
     @JvmOverloads
     fun load(imageView: ImageView, drawable: Drawable, scaleType: ImageView.ScaleType = CENTER) {
-        var request = GlideApp.with(imageView.context)
+        GlideApp.with(imageView.context)
                 .load(drawable)
-        request = applyScaleType(request, scaleType)
-        request.into(imageView)
+                .applyScaleType(scaleType)
+                .into(imageView)
     }
 
     fun loadIntoCircle(imageView: ImageView, imageType: ImageType, imgUrl: String) {
-        var request = GlideApp.with(imageView.context)
+        GlideApp.with(imageView.context)
                 .load(imgUrl)
-        request = addFallback(request, imageType)
-        request = addPlaceholder(request, imageType)
-        request.circleCrop().into(imageView)
+                .addFallback(imageType)
+                .addPlaceholder(imageType)
+                .circleCrop()
+                .into(imageView)
     }
 
     fun cancelRequestAndClearImageView(imageView: ImageView) {
         GlideApp.with(imageView.context).clear(imageView)
     }
 
-    private fun applyScaleType(
-        request: GlideRequest<Drawable>,
+    private fun GlideRequest<Drawable>.applyScaleType(
         scaleType: ScaleType
     ): GlideRequest<Drawable> {
         return when (scaleType) {
-            ImageView.ScaleType.CENTER_CROP -> request.centerCrop()
-            ImageView.ScaleType.CENTER_INSIDE -> request.centerInside()
-            ImageView.ScaleType.FIT_CENTER -> request.fitCenter()
-            ImageView.ScaleType.CENTER -> request
+            ImageView.ScaleType.CENTER_CROP -> this.centerCrop()
+            ImageView.ScaleType.CENTER_INSIDE -> this.centerInside()
+            ImageView.ScaleType.FIT_CENTER -> this.fitCenter()
+            ImageView.ScaleType.CENTER -> this
             ImageView.ScaleType.FIT_END,
             ImageView.ScaleType.FIT_START,
             ImageView.ScaleType.FIT_XY,
             ImageView.ScaleType.MATRIX -> {
                 AppLog.e(AppLog.T.UTILS, String.format("ScaleType %s is not supported.", scaleType.toString()))
-                request
+                this
             }
         }
     }
 
-    private fun addPlaceholder(request: GlideRequest<Drawable>, imageType: ImageType): GlideRequest<Drawable> {
+    private fun GlideRequest<Drawable>.addPlaceholder(imageType: ImageType): GlideRequest<Drawable> {
         val placeholderImageRes = placeholderManager.getPlaceholderImage(imageType)
-        return if (placeholderImageRes == null) request else request.placeholder(placeholderImageRes)
+        return if (placeholderImageRes == null) this else this.placeholder(placeholderImageRes)
     }
 
-    private fun addFallback(request: GlideRequest<Drawable>, imageType: ImageType): GlideRequest<Drawable> {
+    private fun GlideRequest<Drawable>.addFallback(imageType: ImageType): GlideRequest<Drawable> {
         val errorImageRes = placeholderManager.getErrorImage(imageType)
-        return if (errorImageRes == null) request else request.error(errorImageRes)
+        return if (errorImageRes == null) this else this.error(errorImageRes)
     }
 
     @Deprecated("Object for backward compatibility with code which doesn't support DI")
