1d1ed1087e57c3e0f5dd730755646f1bca7884d9
Klymentiy Haykov <forsver@gmail.com>
2018-07-11 00:59:55 +0300

    Added additional data to initial tutorial prompts, added quick start focus point addition logic to quick start utils.

--- WordPress/src/main/java/org/wordpress/android/util/QuickStartUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/util/QuickStartUtils.kt
@@ -7,6 +7,10 @@ import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import android.text.style.ImageSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
 import org.wordpress.android.R
 
 class QuickStartUtils {
@@ -45,6 +49,63 @@ class QuickStartUtils {
             }
 
             return spannedString
+        }
+
+        /**
+         * Adds animated quick start focus point targetedView to the top level parent,
+         * and places it in the top-right cornere of the specified targetedView.
+         *
+         * @param topLevelParent Parent where quick start focus targetedView will be added.
+         * Usually Relative or Frame layout
+         * @param targetedView View in top-right corner of which the quick start focus view will be placed. Child of
+         * topLevelParent
+         * @param rightOffset specifies in px how much should we move view to the left from the right
+         * @param topOffset specifies in px how much should we move view to the bottom from the top
+         */
+        @JvmStatic
+        fun addQuickStartFocusPointAboveTheView(
+            topLevelParent: ViewGroup,
+            targetedView: View,
+            rightOffset: Int,
+            topOffset: Int
+        ) {
+            topLevelParent.post {
+                val quickStartFocusPointView = LayoutInflater.from(topLevelParent.context)
+                        .inflate(R.layout.quick_start_focus_point_view, topLevelParent, false)
+                val focusPointSize =
+                        topLevelParent.context.resources.getDimensionPixelOffset(R.dimen.quick_start_focus_point_size)
+
+                val topLevelParentViewLocation = IntArray(2)
+                topLevelParent.getLocationInWindow(topLevelParentViewLocation)
+
+                val topLevelParentsVerticalOffset = topLevelParentViewLocation[1]
+
+                val focusPointTargetViewLocation = IntArray(2)
+                targetedView.getLocationOnScreen(focusPointTargetViewLocation)
+
+                val x = focusPointTargetViewLocation[0] + targetedView.width - focusPointSize - rightOffset
+                val y = focusPointTargetViewLocation[1] - topLevelParentsVerticalOffset + topOffset
+
+                val params = quickStartFocusPointView.layoutParams as MarginLayoutParams
+                params.leftMargin = x
+                params.topMargin = y
+                topLevelParent.addView(quickStartFocusPointView)
+
+                quickStartFocusPointView.post {
+                    quickStartFocusPointView.layoutParams = params
+                }
+            }
+        }
+
+        @JvmStatic
+        fun removeQuickStartFocusPoint(topLevelParent: ViewGroup) {
+            val focusPointView = topLevelParent.findViewById<View>(R.id.quick_start_focus_point)
+            if (focusPointView != null) {
+                val directParent = focusPointView.parent
+                if (directParent is ViewGroup) {
+                    directParent.removeView(focusPointView)
+                }
+            }
         }
     }
 }
