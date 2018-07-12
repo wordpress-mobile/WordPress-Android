6192a289c0ba48f57eb6315a6e29af0d97ee7d4f
Klymentiy Haykov <forsver@gmail.com>
2018-07-13 02:55:25 +0300

    Updated quick start prompt message styling logic.

--- WordPress/src/main/java/org/wordpress/android/util/QuickStartUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/util/QuickStartUtils.kt
@@ -1,8 +1,11 @@
 package org.wordpress.android.util
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
+import android.text.Html
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
@@ -21,30 +24,48 @@ class QuickStartUtils {
          */
         @JvmStatic
         fun stylizeQuickStartPrompt(context: Context, messageId: Int, iconId: Int): Spannable {
-            val message = context.resources.getString(messageId)
-            val icon = ContextCompat.getDrawable(context, iconId)
+            val spanTagOpen = context.resources.getString(R.string.quick_start_span_start)
+            val spanTagEnd = context.resources.getString(R.string.quick_start_span_end)
+            val formattedMessage = context.resources.getString(messageId, spanTagOpen, spanTagEnd)
+
+            val spannedMessage = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                Html.fromHtml(formattedMessage, Html.FROM_HTML_MODE_LEGACY)
+            } else {
+                Html.fromHtml(formattedMessage)
+            }
 
             val highlightColor = ContextCompat.getColor(context, R.color.blue_light)
 
-            val startOfHighlight = message.indexOf("%1${'$'}s")
-            var clearString = message.replace("%1${'$'}s", if (icon != null) " " else "")
-            val endOfHighlight = clearString.indexOf("%2${'$'}s")
-            clearString = clearString.replace("%2${'$'}s", "")
+            val mutableSpannedMessage = SpannableStringBuilder(spannedMessage)
+            val foregroundColorSpan = mutableSpannedMessage
+                    .getSpans(0, spannedMessage.length, ForegroundColorSpan::class.java).firstOrNull()
 
-            val spannedString = SpannableStringBuilder(clearString)
-            spannedString.setSpan(ForegroundColorSpan(highlightColor),
-                    startOfHighlight, endOfHighlight, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            // nothing to highlight
+            if (foregroundColorSpan != null) {
+                val startOfHighlight = mutableSpannedMessage.getSpanStart(foregroundColorSpan)
+                val endOfHighlight = mutableSpannedMessage.getSpanEnd(foregroundColorSpan)
 
-            if (icon != null) {
-                val iconSize = context.resources.getDimensionPixelOffset(R.dimen.dialog_snackbar_max_icons_size)
-                icon.setBounds(0, 0, iconSize, iconSize)
+                mutableSpannedMessage.removeSpan(foregroundColorSpan)
+                mutableSpannedMessage.setSpan(ForegroundColorSpan(highlightColor),
+                        startOfHighlight, endOfHighlight, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-                DrawableCompat.setTint(icon, highlightColor)
-                spannedString.setSpan(ImageSpan(icon), startOfHighlight, startOfHighlight + 1,
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                val icon : Drawable? = try{
+                    ContextCompat.getDrawable(context, iconId)
+                }catch (e: Resources.NotFoundException){
+                    null
+                }
+
+                if (icon != null) {
+                    val iconSize = context.resources.getDimensionPixelOffset(R.dimen.dialog_snackbar_max_icons_size)
+                    icon.setBounds(0, 0, iconSize, iconSize)
+
+                    DrawableCompat.setTint(icon, highlightColor)
+                    mutableSpannedMessage.setSpan(ImageSpan(icon), startOfHighlight - 1, startOfHighlight,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                }
             }
 
-            return spannedString
+            return mutableSpannedMessage
         }
     }
 }
