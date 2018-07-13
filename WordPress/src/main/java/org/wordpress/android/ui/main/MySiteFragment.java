92d735e74c23e8c7f2898897bfab58aa29f49155
Klymentiy Haykov <forsver@gmail.com>
2018-07-13 04:32:16 +0300

    Fixed references.

--- WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
@@ -151,7 +151,7 @@ public class MySiteFragment extends Fragment implements
 
         if (savedInstanceState != null) {
             mActiveTutorialPrompt =
-                    (InitialTutorialPrompts) savedInstanceState.getSerializable(InitialTutorialPrompts.KEY);
+                    (QuickStartMySitePrompts) savedInstanceState.getSerializable(QuickStartMySitePrompts.KEY);
         }
     }
 
@@ -182,7 +182,7 @@ public class MySiteFragment extends Fragment implements
 
     @Override public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putSerializable(InitialTutorialPrompts.KEY, mActiveTutorialPrompt);
+        outState.putSerializable(QuickStartMySitePrompts.KEY, mActiveTutorialPrompt);
     }
 
     private void initSiteSettings() {
@@ -892,7 +892,7 @@ public class MySiteFragment extends Fragment implements
             int horizontalOffset;
             int verticalOffset;
 
-            if (InitialTutorialPrompts.isTargetingBottomNavBar(mActiveTutorialPrompt.getTask())) {
+            if (QuickStartMySitePrompts.isTargetingBottomNavBar(mActiveTutorialPrompt.getTask())) {
                 horizontalOffset = (quickStartTarget.getWidth() / 2) - focusPointSize + getResources()
                         .getDimensionPixelOffset(R.dimen.quick_start_focus_point_bottom_nav_offset);
                 verticalOffset = 0;
@@ -906,7 +906,7 @@ public class MySiteFragment extends Fragment implements
                     verticalOffset);
 
             // highlighting MySite row and scrolling to it
-            if (!InitialTutorialPrompts.isTargetingBottomNavBar(mActiveTutorialPrompt.getTask())) {
+            if (!QuickStartMySitePrompts.isTargetingBottomNavBar(mActiveTutorialPrompt.getTask())) {
                 mScrollView.post(new Runnable() {
                     @Override public void run() {
                         mScrollView.smoothScrollTo(0, quickStartTarget.getBottom());
