986c9f19f505a226ce83ecbfa95ddc0e0f6eeb1a
Oguz Kocer <oguz.kocer@automattic.com>
2018-07-19 14:55:25 -0400

    Introduces ListModel, ListSqlUtils and ListSqlUtilsTest

---
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/ListModel.kt
@@ -0,0 +1,26 @@
+package org.wordpress.android.fluxc.model
+
+import com.yarolegovich.wellsql.core.Identifiable
+import com.yarolegovich.wellsql.core.annotation.Column
+import com.yarolegovich.wellsql.core.annotation.PrimaryKey
+import com.yarolegovich.wellsql.core.annotation.RawConstraints
+import com.yarolegovich.wellsql.core.annotation.Table
+
+@Table
+@RawConstraints("UNIQUE (type)")
+class ListModel(@PrimaryKey @Column private var id: Int = 0) : Identifiable {
+    enum class ListType(val value: String) {
+        POSTS_ALL("posts_all");
+    }
+
+    @Column var dateCreated: String? = null // ISO 8601-formatted date in UTC, e.g. 1955-11-05T14:15:00Z
+    @Column var lastModified: String? = null // ISO 8601-formatted date in UTC, e.g. 1955-11-05T14:15:00Z
+    @Column var localSiteId: Int = 0
+    @Column var type: String? = null
+
+    override fun getId(): Int = id
+
+    override fun setId(id: Int) {
+        this.id = id
+    }
+}
