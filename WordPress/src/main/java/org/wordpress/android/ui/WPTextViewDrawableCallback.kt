fea963957dd40739a43b5789d0783a04b1166bce
malinajirka <malinajirka@gmail.com>
2018-07-19 13:07:41 +0200

    Fix checkstyle

--- WordPress/src/main/java/org/wordpress/android/ui/WPTextViewDrawableCallback.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/WPTextViewDrawableCallback.kt
@@ -8,8 +8,8 @@ import java.lang.ref.WeakReference
  * Class extends Drawable.Callback, so we can support animated drawables (gifs).
  * Upon retrieving a drawable, call Drawable.setCallback(...).
  */
-class WPTextViewDrawableCallback(textView: TextView): Drawable.Callback {
-    private val weakView : WeakReference<TextView> = WeakReference(textView)
+class WPTextViewDrawableCallback(textView: TextView) : Drawable.Callback {
+    private val weakView: WeakReference<TextView> = WeakReference(textView)
 
     override fun invalidateDrawable(who: Drawable) {
         weakView.get()?.invalidate()
