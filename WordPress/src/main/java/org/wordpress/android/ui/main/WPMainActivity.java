22ece0bd48c8dc88023ae5cb47b19292517a932c
Mario Zorz <mariozorz@gmail.com>
2018-07-20 09:14:46 -0300

    removed unused import

--- WordPress/src/main/java/org/wordpress/android/ui/main/WPMainActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/WPMainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
