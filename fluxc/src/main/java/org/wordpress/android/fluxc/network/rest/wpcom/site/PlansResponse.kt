9decbc4eb27a7324383eab42567f6b42aac8aa11
usamahamid <usamahamid007@gmail.com>
2018-07-24 04:34:45 +0530

    Refactored PlansDeserializer
    Refactored PlansDeserializer, to utilize JsonObjectExtensions.

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/PlansResponse.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/PlansResponse.kt
@@ -7,6 +7,8 @@ import com.google.gson.JsonObject
 import com.google.gson.JsonParseException
 import com.google.gson.annotations.JsonAdapter
 import org.wordpress.android.fluxc.model.PlanModel
+import org.wordpress.android.fluxc.network.utils.getBoolean
+import org.wordpress.android.fluxc.network.utils.getString
 import java.lang.reflect.Type
 
 @JsonAdapter(PlansDeserializer::class)
@@ -18,23 +20,19 @@ class PlansDeserializer : JsonDeserializer<PlansResponse> {
     override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): PlansResponse {
         val jsonObject = json.asJsonObject
         val planModels = ArrayList<PlanModel>()
-        val entries = jsonObject.entrySet()
-        for ((_, value) in entries) {
+        jsonObject.entrySet().forEach { (_, value) ->
             val planJsonObj = value.asJsonObject
-            val productSlug = planJsonObj.get("product_slug").asString
-            val productName = planJsonObj.get("product_name").asString
+            val productSlug = planJsonObj.getString("product_slug") ?: String()
+            val productName = planJsonObj.getString("product_name") ?: String()
 
             // 'id' attribute is sometimes null, even though the it should be a long value
             // Check if it is JsonObject (i.e. not null and not JsonNull), before casting it to long
-            val idJsonObj = planJsonObj.get("id")
-            val productId = (idJsonObj as? JsonObject)?.asLong ?: 0
+            val productId = (planJsonObj.get("id") as? JsonObject)?.asLong ?: 0
 
             // 'current_plan' and 'has_domain_credit' attributes might be missing,
             // consider them as false, if they are missing
-            val isCurrentPlan =
-                    planJsonObj.has("current_plan") && planJsonObj.get("current_plan").asBoolean
-            val hasDomainCredit =
-                    planJsonObj.has("has_domain_credit") && planJsonObj.get("has_domain_credit").asBoolean
+            val isCurrentPlan = planJsonObj.getBoolean("current_plan")
+            val hasDomainCredit = planJsonObj.getBoolean("has_domain_credit")
             planModels.add(PlanModel(productId, productSlug, productName, isCurrentPlan, hasDomainCredit))
         }
         return PlansResponse(planModels)
