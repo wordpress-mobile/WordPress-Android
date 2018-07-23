7e7b85f8790c420787aa14a1bc36ba5d2c43ff78
usamahamid <usamahamid007@gmail.com>
2018-07-24 03:54:35 +0530

    Refactored to utilize primary constructor instead of secondary constructor
    Refactored to utilize primary constructor instead of secondary constructor

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/PlanModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/PlanModel.kt
@@ -1,23 +1,9 @@
 package org.wordpress.android.fluxc.model
 
-class PlanModel {
-    val productId: Long
-    val productSlug: String
-    val productName: String
-    val isCurrentPlan: Boolean
+class PlanModel (
+    val productId: Long,
+    val productSlug: String,
+    val productName: String,
+    val isCurrentPlan: Boolean,
     val hasDomainCredit: Boolean
-
-    constructor(
-        productId: Long,
-        productSlug: String,
-        productName: String,
-        isCurrentPlan: Boolean,
-        hasDomainCredit: Boolean
-    ) {
-        this.productId = productId
-        this.productSlug = productSlug
-        this.productName = productName
-        this.isCurrentPlan = isCurrentPlan
-        this.hasDomainCredit = hasDomainCredit
-    }
-}
+)
