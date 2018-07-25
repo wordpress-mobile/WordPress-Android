18c7590759b127173cf0ebb4ab4efc4226cb583e
Oguz Kocer <oguz.kocer@automattic.com>
2018-07-25 16:56:45 -0400

    Update the PostListModel migration for the model changes

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.java
@@ -326,13 +326,12 @@ public class WellSqlConfig extends DefaultWellConfig {
                 oldVersion++;
             case 39:
                 AppLog.d(T.DB, "Migrating to version " + (oldVersion + 1));
-                db.execSQL("CREATE TABLE PostListModel (LIST_ID INTEGER,LOCAL_SITE_ID INTEGER,POST_ID INTEGER,"
-                           + "DATE TEXT,_id INTEGER PRIMARY KEY AUTOINCREMENT,FOREIGN KEY(LIST_ID) "
-                           + "REFERENCES ListModel(_id) ON DELETE CASCADE,FOREIGN KEY(LOCAL_SITE_ID) "
-                           + "REFERENCES SiteModel(_id) ON DELETE CASCADE,UNIQUE(LIST_ID, LOCAL_SITE_ID, POST_ID))");
                 db.execSQL("CREATE TABLE ListModel (DATE_CREATED TEXT,LAST_MODIFIED TEXT,LOCAL_SITE_ID INTEGER,"
                            + "TYPE TEXT,_id INTEGER PRIMARY KEY AUTOINCREMENT,FOREIGN KEY(LOCAL_SITE_ID) "
                            + "REFERENCES SiteModel(_id) ON DELETE CASCADE,UNIQUE(LOCAL_SITE_ID, TYPE))");
+                db.execSQL("CREATE TABLE PostListModel (LIST_ID INTEGER,POST_ID INTEGER,_id INTEGER "
+                           + "PRIMARY KEY AUTOINCREMENT,FOREIGN KEY(LIST_ID) REFERENCES ListModel(_id) "
+                           + "ON DELETE CASCADE,UNIQUE(LIST_ID, POST_ID))");
                 oldVersion++;
         }
         db.setTransactionSuccessful();
