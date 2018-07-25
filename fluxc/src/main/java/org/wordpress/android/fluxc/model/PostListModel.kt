e5f0f5416ed8ead1bf09b61f740ae9862a7476c4
Oguz Kocer <oguz.kocer@automattic.com>
2018-07-25 16:51:32 -0400

    Remove localSiteId from PostListModel

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/PostListModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/PostListModel.kt
@@ -9,12 +9,10 @@ import com.yarolegovich.wellsql.core.annotation.Table
 @Table
 @RawConstraints(
         "FOREIGN KEY(LIST_ID) REFERENCES ListModel(_id) ON DELETE CASCADE",
-        "FOREIGN KEY(LOCAL_SITE_ID) REFERENCES SiteModel(_id) ON DELETE CASCADE",
-        "UNIQUE(LIST_ID, LOCAL_SITE_ID, POST_ID)"
+        "UNIQUE(LIST_ID, POST_ID)"
 )
 class PostListModel(@PrimaryKey @Column private var id: Int = 0) : Identifiable {
     @Column var listId: Int = 0
-    @Column var localSiteId: Int = 0
     @Column var postId: Int = 0
 
     override fun getId(): Int = id
