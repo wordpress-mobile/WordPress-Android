d66f6e1f018edef9564feec29acbde5bc1b44fc0
Ondrej Ruttkay <ondrej@automattic.com>
2018-07-25 23:48:10 +0200

    Add a page upload function to page store

--- WordPress/src/main/java/org/wordpress/android/networking/PageStore.kt
+++ WordPress/src/main/java/org/wordpress/android/networking/PageStore.kt
@@ -1,6 +1,8 @@
 package org.wordpress.android.networking
 
+import android.content.Context
 import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.withContext
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
@@ -9,19 +11,60 @@ import org.wordpress.android.models.pages.PageModel
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.PostActionBuilder
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.post.PostStatus
 import org.wordpress.android.fluxc.store.PostStore.FetchPostsPayload
 import org.wordpress.android.fluxc.store.PostStore.OnPostChanged
+import org.wordpress.android.networking.PageStore.UploadRequestResult.ERROR_EMPTY_PAGE
+import org.wordpress.android.networking.PageStore.UploadRequestResult.ERROR_NO_NETWORK
+import org.wordpress.android.networking.PageStore.UploadRequestResult.SUCCESS
+import org.wordpress.android.ui.posts.PostUtils
+import org.wordpress.android.ui.uploads.UploadService
+import org.wordpress.android.util.NetworkUtils
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.coroutines.experimental.Continuation
 import kotlin.coroutines.experimental.suspendCoroutine
 
 @Singleton
-class PageStore @Inject constructor(private val postStore: PostStore, private val dispatcher: Dispatcher) {
+class PageStore @Inject constructor(
+    private val postStore: PostStore,
+    private val dispatcher: Dispatcher,
+    private val context: Context
+) {
     private var postLoadContinuation: Continuation<OnPostChanged>? = null
 
     init {
         dispatcher.register(this)
+    }
+
+    suspend fun uploadPage(page: PageModel): UploadRequestResult = withContext(CommonPool) {
+        val post = postStore.getPostByLocalPostId(page.pageId)
+
+        if (!NetworkUtils.isNetworkAvailable(context)) {
+            return@withContext ERROR_NO_NETWORK
+        }
+
+        if (!PostUtils.isPublishable(post)) {
+            return@withContext ERROR_EMPTY_PAGE
+        }
+
+        PostUtils.updatePublishDateIfShouldBePublishedImmediately(post)
+        val isFirstTimePublish = PostStatus.fromPost(post) == PostStatus.DRAFT ||
+                PostStatus.fromPost(post) == PostStatus.PUBLISHED && post.isLocalDraft
+        post.status = PostStatus.PUBLISHED.toString()
+
+        // save the post in the DB so the UploadService will get the latest change
+        dispatcher.dispatch(PostActionBuilder.newUpdatePostAction(post))
+
+        if (isFirstTimePublish) {
+            UploadService.uploadPostAndTrackAnalytics(context, post)
+        } else {
+            UploadService.uploadPost(context, post)
+        }
+
+        PostUtils.trackSavePostAnalytics(post, page.site)
+
+        return@withContext SUCCESS
     }
 
     suspend fun getPageByLocalId(pageId: Int, site: SiteModel): PageModel? = withContext(CommonPool) {
@@ -69,5 +112,11 @@ class PageStore @Inject constructor(private val postStore: PostStore, private va
     fun onPostChanged(event: OnPostChanged) {
         postLoadContinuation?.resume(event)
         postLoadContinuation = null
+    }
+
+    enum class UploadRequestResult {
+        SUCCESS,
+        ERROR_NO_NETWORK,
+        ERROR_EMPTY_PAGE
     }
 }
