8f4d4c19540ce06478805dc2a44b068797261450
Oguz Kocer <oguz.kocer@automattic.com>
2018-07-25 16:51:32 -0400

    Remove localSiteId from PostListModel

--- fluxc/src/test/java/org/wordpress/android/fluxc/utils/PostListSqlUtilsTest.kt
+++ fluxc/src/test/java/org/wordpress/android/fluxc/utils/PostListSqlUtilsTest.kt
@@ -14,13 +14,9 @@ import org.wordpress.android.fluxc.persistence.ListSqlUtils
 import org.wordpress.android.fluxc.persistence.PostListSqlUtils
 import org.wordpress.android.fluxc.persistence.SiteSqlUtils
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
-import org.wordpress.android.fluxc.site.SiteUtils.generateJetpackSiteOverXMLRPC
 import org.wordpress.android.fluxc.site.SiteUtils.generateSelfHostedNonJPSite
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
-import kotlin.test.assertNull
-import kotlin.test.assertTrue
 
 @RunWith(RobolectricTestRunner::class)
 class PostListSqlUtilsTest {
@@ -51,13 +47,13 @@ class PostListSqlUtilsTest {
          * 3. Verify that the [PostListModel] instances are inserted correctly
          */
         val testList = insertTestList(testSite.id, listType)
-        val postList = generatePostList(testList, testSite.id, postCount)
+        val postList = generatePostList(testList, postCount)
         postListSqlUtils.insertPostList(postList)
         assertEquals(postCount, postListSqlUtils.getPostList(testList.id).size)
     }
 
     @Test
-    fun testDeletePostList() {
+    fun testListIdForeignKeyCascadeDelete() {
         val testSite = generateAndInsertSelfHostedNonJPTestSite()
         val postCount = 20 // value doesn't matter
         val listType = ListType.POSTS_ALL
@@ -69,7 +65,7 @@ class PostListSqlUtilsTest {
          * 3. Verify that the [PostListModel] instances are inserted correctly
          */
         val testList = insertTestList(testSite.id, listType)
-        val postList = generatePostList(testList, testSite.id, postCount)
+        val postList = generatePostList(testList, postCount)
         postListSqlUtils.insertPostList(postList)
         assertEquals(postCount, postListSqlUtils.getPostList(testList.id).size)
 
@@ -92,7 +88,7 @@ class PostListSqlUtilsTest {
          * 3. Verify that the [PostListModel] was inserted correctly
          */
         val testLists = ListType.values().map { insertTestList(testSite.id, it) }
-        val postList = testLists.map { generatePostListModel(it.id, testSite.id, testPostId) }
+        val postList = testLists.map { generatePostListModel(it.id, testPostId) }
         postListSqlUtils.insertPostList(postList)
         testLists.forEach { list ->
             assertEquals(1, postListSqlUtils.getPostList(list.id).size)
@@ -102,52 +98,10 @@ class PostListSqlUtilsTest {
          * 1. Delete [PostListModel]s for which [PostListModel.postId] == `testPostId`
          * 2. Verify that [PostListModel]s from every list is deleted
          */
-        postListSqlUtils.deletePost(testSite.id, testPostId)
+        postListSqlUtils.deletePost(testLists.map { it.id }, testPostId)
         testLists.forEach {
             assertEquals(0, postListSqlUtils.getPostList(it.id).size)
         }
-    }
-
-    /**
-     * This is a test to ensure that if there are multiple [PostListModel]s with the same `postId`, only the one with
-     * the correct `siteId` will be deleted.
-     */
-    @Test
-    fun testDeletePostOnlyForTheCorrectSite() {
-        /**
-         * 1. Generate and insert 2 different test sites
-         * 2. Insert a [ListModel] for each test site
-         * 3. Insert a [PostListModel] for each test list with the same postId
-         */
-        val testSite1 = generateAndInsertSelfHostedNonJPTestSite()
-        val testSite2 = generateAndInsertJetpackSiteOverXMLRPCTestSite()
-        val testPostId = 1245 // value doesn't matter
-        val listType = ListType.POSTS_ALL
-
-        val testLists = listOf(testSite1, testSite2).map { insertTestList(it.id, listType) }
-        val postList = testLists.map {
-            generatePostListModel(it.id, it.localSiteId, testPostId)
-        }
-        postListSqlUtils.insertPostList(postList)
-
-        /**
-         * 1. Verify that for each list, a single post is inserted
-         * 2. Verify that inserted post has the expected id
-         */
-        testLists.forEach {
-            val insertedPostList = postListSqlUtils.getPostList(it.id)
-            assertEquals(1, insertedPostList.size)
-            assertEquals(testPostId, insertedPostList[0].postId)
-        }
-
-        /**
-         * 1. Delete the post for the first site
-         * 2. Verify that it's deleted from the first list which corresponds to the first list
-         * 3. Verify that it's NOT deleted from the second list
-         */
-        postListSqlUtils.deletePost(testSite1.id, testPostId)
-        assertEquals(0, postListSqlUtils.getPostList(testLists[0].id).size)
-        assertEquals(1, postListSqlUtils.getPostList(testLists[1].id).size)
     }
 
     @Test
@@ -163,8 +117,8 @@ class PostListSqlUtilsTest {
          * 3. Verify that first [PostListModel] is inserted correctly
          */
         val testList = insertTestList(testSite.id, listType)
-        val postListModel = generatePostListModel(testList.id, testSite.id, testPostId)
-        val postListModel2 = generatePostListModel(testList.id, testSite.id, testPostId)
+        val postListModel = generatePostListModel(testList.id, testPostId)
+        val postListModel2 = generatePostListModel(testList.id, testPostId)
         postListSqlUtils.insertPostList(listOf(postListModel))
         val insertedPostList = postListSqlUtils.getPostList(testList.id)
         assertEquals(1, insertedPostList.size)
@@ -180,64 +134,6 @@ class PostListSqlUtilsTest {
         assertEquals(insertedPostList[0].id, updatedPostList[0].id)
     }
 
-    @Test
-    fun testListIdForeignKeyCascadeDelete() {
-        val postCount = 20 // value doesn't matter
-        val listType = ListType.POSTS_ALL
-
-        /**
-         * 1. Generate and insert a self-hosted test site
-         * 2. Verify that the site is inserted
-         * 3. Generate and insert a [ListModel] for the test site
-         * 4. Generate a list of [PostListModel]s for the test list
-         * 5. Verify that the [PostListModel] was inserted correctly
-         */
-        val testSite = generateAndInsertSelfHostedNonJPTestSite()
-        assertFalse(SiteSqlUtils.getSitesAccessedViaXMLRPC().asModel.isEmpty())
-        val testList = insertTestList(testSite.id, listType)
-        val postList = generatePostList(testList, testSite.id, postCount)
-        postListSqlUtils.insertPostList(postList)
-        assertEquals(postCount, postListSqlUtils.getPostList(testList.id).size)
-
-        /**
-         * 1. Delete the test list
-         * 2. Verify that test list is deleted
-         * 3. Verify that [PostListModel]s for the test list are deleted
-         */
-        listSqlUtils.deleteList(testSite.id, listType)
-        assertNull(listSqlUtils.getList(testSite.id, listType))
-        assertTrue(postListSqlUtils.getPostList(testList.id).isEmpty())
-    }
-
-    @Test
-    fun testLocalSiteIdForeignKeyCascadeDelete() {
-        val postCount = 20 // value doesn't matter
-        val listType = ListType.POSTS_ALL
-
-        /**
-         * 1. Generate and insert a self-hosted test site
-         * 2. Verify that the site is inserted
-         * 3. Generate and insert a [ListModel] for the test site
-         * 4. Generate a list of [PostListModel]s for the test list
-         * 5. Verify that the [PostListModel] was inserted correctly
-         */
-        val testSite = generateAndInsertSelfHostedNonJPTestSite()
-        assertFalse(SiteSqlUtils.getSitesAccessedViaXMLRPC().asModel.isEmpty())
-        val testList = insertTestList(testSite.id, listType)
-        val postList = generatePostList(testList, testSite.id, postCount)
-        postListSqlUtils.insertPostList(postList)
-        assertEquals(postCount, postListSqlUtils.getPostList(testList.id).size)
-
-        /**
-         * 1. Delete the test site
-         * 2. Verify that test site is deleted
-         * 3. Verify that [PostListModel]s for the test site are deleted
-         */
-        SiteSqlUtils.deleteSite(testSite)
-        assertTrue(SiteSqlUtils.getSitesAccessedViaXMLRPC().asModel.isEmpty())
-        assertTrue(postListSqlUtils.getPostList(testList.id).isEmpty())
-    }
-
     /**
      * Creates and inserts a [ListModel] for the given site. It also verifies that the list is inserted correctly.
      */
@@ -251,40 +147,28 @@ class PostListSqlUtilsTest {
     /**
      * Helper function that creates a list of [PostListModel] to be used in tests.
      */
-    private fun generatePostList(listModel: ListModel, localSiteId: Int, count: Int): List<PostListModel> =
-            (1..count).map { generatePostListModel(listModel.id, localSiteId, it) }
+    private fun generatePostList(listModel: ListModel, count: Int): List<PostListModel> =
+            (1..count).map { generatePostListModel(listModel.id, it) }
 
     /**
      * Helper function that generates a [PostListModel] instance.
      */
     private fun generatePostListModel(
         listId: Int,
-        localSiteId: Int,
         postId: Int
     ): PostListModel {
         val postListModel = PostListModel()
         postListModel.listId = listId
-        postListModel.localSiteId = localSiteId
         postListModel.postId = postId
         return postListModel
     }
 
     /**
      * Helper function that generates a self-hosted test site and inserts it into the DB. Since we have a FK restriction
-     * for [PostListModel.localSiteId] we need to do this before we can insert [PostListModel] instances.
+     * for [ListModel.localSiteId] we need to do this before we can insert [ListModel] instances.
      */
     private fun generateAndInsertSelfHostedNonJPTestSite(): SiteModel {
         val site = generateSelfHostedNonJPSite()
-        SiteSqlUtils.insertOrUpdateSite(site)
-        return site
-    }
-
-    /**
-     * Helper function that generates a self-hosted test site with Jetpack and inserts it into the DB. Since we have a
-     * FK restriction for [PostListModel.localSiteId] we need to do this before we can insert [PostListModel] instances.
-     */
-    private fun generateAndInsertJetpackSiteOverXMLRPCTestSite(): SiteModel {
-        val site = generateJetpackSiteOverXMLRPC()
         SiteSqlUtils.insertOrUpdateSite(site)
         return site
     }
