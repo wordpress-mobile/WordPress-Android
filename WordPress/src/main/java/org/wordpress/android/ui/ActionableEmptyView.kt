c88b063d623ebbf82cda9fbd5b27b02920b2d423
Tyler Heck <theck13@users.noreply.github.com>
2018-07-26 16:55:55 -0700

    Update runtime exception message in actionable empty view class

--- WordPress/src/main/java/org/wordpress/android/ui/ActionableEmptyView.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/ActionableEmptyView.kt
@@ -58,7 +58,7 @@ class ActionableEmptyView : LinearLayout {
             if (!titleAttribute.isNullOrEmpty()) {
                 title.text = titleAttribute
             } else {
-                throw RuntimeException(context.toString() + ": ActionableEmptyView must have a title (wpTitle)")
+                throw RuntimeException(context.toString() + ": ActionableEmptyView must have a title (aevTitle)")
             }
 
             if (!subtitleAttribute.isNullOrEmpty()) {
