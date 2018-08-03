11d888dc1f1154758c7c6e518f2ea695212ab50e
Alex Forcier <alex.forcier@automattic.com>
2018-08-03 14:20:48 -0400

    Support unregistering devices for PN with WordPress.com

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/NotificationAction.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/NotificationAction.java
@@ -5,16 +5,21 @@ import org.wordpress.android.fluxc.annotations.ActionEnum;
 import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.store.NotificationStore.RegisterDevicePayload;
 import org.wordpress.android.fluxc.store.NotificationStore.RegisterDeviceResponsePayload;
+import org.wordpress.android.fluxc.store.NotificationStore.UnregisterDeviceResponsePayload;
 
 @ActionEnum
 public enum NotificationAction implements IAction {
     // Remote actions
     @Action(payloadType = RegisterDevicePayload.class)
     REGISTER_DEVICE, // Register device for push notifications with WordPress.com
+    @Action
+    UNREGISTER_DEVICE, // Unregister device for push notifications with WordPress.com
 
     // Remote responses
     @Action(payloadType = RegisterDeviceResponsePayload.class)
     REGISTERED_DEVICE, // Response to device registration received
+    @Action(payloadType = UnregisterDeviceResponsePayload.class)
+    UNREGISTERED_DEVICE // Response to device unregistration
 
     // Local actions
 }
