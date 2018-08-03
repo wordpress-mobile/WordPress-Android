6c2776291cd37c3ea3f2d3ed909223f8852546a6
Alex Forcier <alex.forcier@automattic.com>
2018-08-03 11:42:23 -0400

    Handle storing PN server ID in FluxC

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/NotificationStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/NotificationStore.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.fluxc.store
 
+import android.content.Context
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
@@ -8,6 +9,7 @@ import org.wordpress.android.fluxc.action.NotificationAction
 import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError
 import org.wordpress.android.fluxc.network.rest.wpcom.notifications.NotificationRestClient
+import org.wordpress.android.fluxc.utils.PreferenceUtils
 import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.AppLog.T
 import java.util.Locale
@@ -16,7 +18,15 @@ import javax.inject.Singleton
 
 @Singleton
 class NotificationStore @Inject
-constructor(dispatcher: Dispatcher, private val notificationRestClient: NotificationRestClient) : Store(dispatcher) {
+constructor(
+    dispatcher: Dispatcher,
+    private val context: Context,
+    private val notificationRestClient: NotificationRestClient
+) : Store(dispatcher) {
+    companion object {
+        const val WPCOM_PUSH_DEVICE_SERVER_ID = "NOTIFICATIONS_SERVER_ID_PREF_KEY"
+    }
+
     class RegisterDevicePayload(
         val params: Map<String, String>
     ) : Payload<BaseNetworkError>()
@@ -66,8 +76,20 @@ constructor(dispatcher: Dispatcher, private val notificationRestClient: Notifica
     private fun handleRegisteredDevice(payload: RegisterDeviceResponsePayload) {
         val onDeviceRegistered = OnDeviceRegistered(payload.deviceId)
 
-        if (payload.isError) {
-            onDeviceRegistered.error = payload.error
+        with(payload) {
+            if (isError || deviceId.isNullOrEmpty()) {
+                when (error.type) {
+                    DeviceRegistrationErrorType.MISSING_DEVICE_ID ->
+                        AppLog.e(T.NOTIFS, "Server response missing device_id - registration skipped!")
+                    DeviceRegistrationErrorType.GENERIC_ERROR ->
+                        AppLog.e(T.NOTIFS, "Error trying to register device: ${error.type} - ${error.message}")
+                }
+                onDeviceRegistered.error = payload.error
+            } else {
+                PreferenceUtils.getFluxCPreferences(context)
+                        .edit().putString(WPCOM_PUSH_DEVICE_SERVER_ID, deviceId).apply()
+                AppLog.i(T.NOTIFS, "Server response OK. Device ID: $deviceId")
+            }
         }
 
         emitChange(onDeviceRegistered)
