8b25046cbaad8b8af1e126273e783c5883461ed4
usamahamid <usamahamid007@gmail.com>
2018-08-03 06:16:45 +0530

    Switched to Enum to hold Domain Availability and Mappability.
    Refactored such that Domain name, availability and mappability are non null, since the API always seem to respond with those values.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/DomainAvailabilityModel.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/DomainAvailabilityModel.kt
@@ -1,27 +1,27 @@
 package org.wordpress.android.fluxc.model
 
-import android.support.annotation.StringDef
-
-const val BLACKLISTED_DOMAIN = "blacklisted_domain"
-const val INVALID_TLD = "invalid_tld"
-const val INVALID_DOMAIN = "invalid_domain"
-const val TLD_NOT_SUPPORTED = "tld_not_supported"
-const val TRANSFERRABLE_DOMAIN = "transferrable"
-const val AVAILABLE = "available"
-const val MAPPABLE_DOMAIN = "mappable"
-
-@StringDef(BLACKLISTED_DOMAIN, INVALID_TLD, INVALID_DOMAIN, TLD_NOT_SUPPORTED, TRANSFERRABLE_DOMAIN, AVAILABLE)
-annotation class AvailabilityStatus
-
-@StringDef(BLACKLISTED_DOMAIN, INVALID_TLD, INVALID_DOMAIN, MAPPABLE_DOMAIN)
-annotation class Mappability
-
 class DomainAvailabilityModel(
     val productId: Int?,
     val productSlug: String?,
-    val domainName: String?,
-    @AvailabilityStatus val status: String?,
-    @Mappability val mappable: String?,
+    val domainName: String,
+    val status: AvailabilityStatus,
+    val mappable: Mappability,
     val cost: String?,
     val supportsPrivacy: Boolean = false
-)
+) {
+    enum class AvailabilityStatus {
+        BLACKLISTED_DOMAIN,
+        INVALID_TLD,
+        INVALID_DOMAIN,
+        TLD_NOT_SUPPORTED,
+        TRANSFERRABLE_DOMAIN,
+        AVAILABLE
+    }
+
+    enum class Mappability {
+        BLACKLISTED_DOMAIN,
+        INVALID_TLD,
+        INVALID_DOMAIN,
+        MAPPABLE_DOMAIN
+    }
+}
