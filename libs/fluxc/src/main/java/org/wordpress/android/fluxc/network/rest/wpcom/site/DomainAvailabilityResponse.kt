9e7a3b1a0c968b7a566ee58d4cc4ee7ebdb54937
usamahamid <usamahamid007@gmail.com>
2018-08-04 11:13:03 +0530

    Updated Response attributes to be nullable.
    Introduced `UNKNOWN_STATUS` as a placeholder in Availability status and Mappability, incase status is null/unknown.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/DomainAvailabilityResponse.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/DomainAvailabilityResponse.kt
@@ -3,9 +3,9 @@ package org.wordpress.android.fluxc.network.rest.wpcom.site
 class DomainAvailabilityResponse(
     val product_id: Int?,
     val product_slug: String?,
-    val domain_name: String,
-    val status: String,
-    val mappable: String,
+    val domain_name: String?,
+    val status: String?,
+    val mappable: String?,
     val cost: String?,
     val supports_privacy: Boolean = false
 )
