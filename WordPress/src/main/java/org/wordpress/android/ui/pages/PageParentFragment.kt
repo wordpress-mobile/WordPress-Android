ce82ba242941ab26264667c5de1f48ddd8225d50
Ondrej Ruttkay <ondrej@automattic.com>
2018-08-07 15:54:04 +0200

    Optimize imports

--- WordPress/src/main/java/org/wordpress/android/ui/pages/PageParentFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/pages/PageParentFragment.kt
@@ -1,6 +1,5 @@
 package org.wordpress.android.ui.pages
 
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
