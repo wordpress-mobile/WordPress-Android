cc939f5d9607db2c4ec548a3e28bbc08546ebae7
Ondrej Ruttkay <ondrej@automattic.com>
2018-08-08 18:29:03 +0200

    Add methods for page loading

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/page/PageModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/page/PageModel.kt
@@ -1,7 +1,23 @@
 package org.wordpress.android.fluxc.model.page
 
 import org.wordpress.android.fluxc.model.PostModel
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.util.DateTimeUtils
+import java.util.Date
 
-data class PageModel(val pageId: Int, val title: String, var status: PageStatus, var parentId: Long) {
-    constructor(post: PostModel) : this(post.id, post.title, PageStatus.fromPost(post), post.parentId)
+data class PageModel(
+    val site: SiteModel,
+    val pageId: Int,
+    val title: String,
+    var status: PageStatus,
+    var date: Date,
+    var hasLocalChanges: Boolean,
+    val parentId: Long,
+    val remoteId: Long
+) {
+    var parent: PageModel? = null
+
+    constructor(post: PostModel, site: SiteModel) : this(site, post.id, post.title, PageStatus.fromPost(post),
+            Date(DateTimeUtils.timestampFromIso8601Millis(post.dateCreated)), post.isLocallyChanged, post.parentId,
+            post.remotePostId)
 }
