d0c11f0299b697fabffec003db36a006d9f96829
malinajirka <malinajirka@gmail.com>
2018-08-13 16:06:39 +0200

    Add ReaderPostList ViewModel for NewsCard

--- WordPress/src/main/java/org/wordpress/android/modules/ViewModelModule.java
+++ WordPress/src/main/java/org/wordpress/android/modules/ViewModelModule.java
@@ -3,6 +3,7 @@ package org.wordpress.android.modules;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 
+import org.wordpress.android.ui.reader.viewmodels.ReaderPostListViewModel;
 import org.wordpress.android.viewmodel.PluginBrowserViewModel;
 import org.wordpress.android.viewmodel.ViewModelFactory;
 import org.wordpress.android.viewmodel.ViewModelKey;
@@ -29,6 +30,11 @@ abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(ActivityLogDetailViewModel.class)
     abstract ViewModel activityLogDetailViewModel(ActivityLogDetailViewModel viewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ReaderPostListViewModel.class)
+    abstract ViewModel readerPostListViewModel(ReaderPostListViewModel viewModel);
 
     @Binds
     abstract ViewModelProvider.Factory provideViewModelFactory(ViewModelFactory viewModelFactory);
---
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/viewmodels/ReaderPostListViewModel.kt
@@ -0,0 +1,50 @@
+package org.wordpress.android.ui.reader.viewmodels
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
+import android.arch.lifecycle.ViewModel
+import org.wordpress.android.models.ReaderTag
+import org.wordpress.android.models.news.NewsItem
+import org.wordpress.android.ui.news.NewsManager
+import javax.inject.Inject
+
+class ReaderPostListViewModel @Inject constructor(
+    private val newsManager: NewsManager
+) : ViewModel() {
+    private val newsItemSource = newsManager.newsItemSource()
+    private val _newsItemSourceMediator = MediatorLiveData<NewsItem?>()
+    val newsItem: LiveData<NewsItem?>
+        get() = _newsItemSourceMediator
+
+    private lateinit var initialTag: ReaderTag
+    private var isStarted = false
+
+    fun start(tag: ReaderTag) {
+        if (isStarted) {
+            return
+        }
+        initialTag = tag
+        onTagChanged(tag)
+        newsManager.pull()
+        isStarted = true
+    }
+
+    fun onTagChanged(tag: ReaderTag) {
+        // show the card only when the initial tag is selected in the filter
+        if (tag == initialTag) {
+            _newsItemSourceMediator.addSource(newsItemSource) { _newsItemSourceMediator.value = it }
+        } else {
+            _newsItemSourceMediator.removeSource(newsItemSource)
+            _newsItemSourceMediator.value = null
+        }
+    }
+
+    fun onDismissClicked(item: NewsItem) {
+        newsManager.dismiss(item)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        newsManager.stop()
+    }
+}
