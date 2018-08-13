c47cf8a67abdd791e8360e623855f263f9ded24e
Usama Hamid <usamahamid.amjadali@impigertech.com>
2018-08-13 20:00:25 +0530

    Fixed ktlint.

--- WordPress/src/main/java/org/wordpress/android/ui/domainregister/suggestionslist/DomainSuggestionsFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domainregister/suggestionslist/DomainSuggestionsFragment.kt
@@ -52,7 +52,6 @@ class DomainSuggestionsFragment : Fragment() {
             ToastUtils.showToast(activity, "Still under development.")
         }
         domainSearchEditText.addTextChangedListener(object : TextWatcher {
-
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
