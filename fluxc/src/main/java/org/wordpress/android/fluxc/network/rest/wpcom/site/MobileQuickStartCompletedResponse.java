953789ee83b41d3afb690587a4d8e90a391d6d7e
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2018-08-14 18:01:36 +0200

    Quick start action and response.

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/MobileQuickStartCompletedResponse.java
@@ -0,0 +1,8 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.site;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MobileQuickStartCompletedResponse {
+    @SerializedName("status")
+    public boolean status;
+}
