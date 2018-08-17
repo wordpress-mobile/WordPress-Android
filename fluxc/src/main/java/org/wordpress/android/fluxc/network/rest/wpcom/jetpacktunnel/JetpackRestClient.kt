5624a61e6dc035f5518f867a4084ec3368f881d2
vojtasmrcek <planarvoid@gmail.com>
2018-08-17 17:39:44 +0200

    Cleanup rest client to remove delays from test and make tests predictable

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/jetpacktunnel/JetpackRestClient.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/jetpacktunnel/JetpackRestClient.kt
@@ -9,6 +9,7 @@ import org.wordpress.android.fluxc.network.BaseRequest
 import org.wordpress.android.fluxc.network.UserAgent
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder
+import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Success
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
 import org.wordpress.android.fluxc.store.JetpackStore.JetpackInstallError
 import org.wordpress.android.fluxc.store.JetpackStore.JetpackInstallErrorType.AUTHORIZATION_REQUIRED
@@ -19,7 +20,6 @@ import org.wordpress.android.fluxc.store.JetpackStore.JetpackInstallErrorType.US
 import org.wordpress.android.fluxc.store.JetpackStore.JetpackInstalledPayload
 import java.net.URLEncoder
 import javax.inject.Singleton
-import kotlin.coroutines.experimental.suspendCoroutine
 
 @Singleton
 class JetpackRestClient
@@ -31,26 +31,28 @@ constructor(
     accessToken: AccessToken,
     userAgent: UserAgent
 ) : BaseWPComRestClient(appContext, dispatcher, requestQueue, accessToken, userAgent) {
-    suspend fun installJetpack(site: SiteModel) = suspendCoroutine<JetpackInstalledPayload> { cont ->
+    suspend fun installJetpack(site: SiteModel): JetpackInstalledPayload {
         val url = WPCOMREST.jetpack_install.site(URLEncoder.encode(site.url, "UTF-8")).urlV1
         val params = mapOf("user" to site.username, "password" to site.password)
-        val request = wpComGsonRequestBuilder.buildPostRequest(
-                url, params, JetpackInstallResponse::class.java,
-                { response ->
-                    cont.resume(JetpackInstalledPayload(site, response.status))
-                },
-                { networkError ->
-                    val error = when {
-                        networkError.apiError == "SITE_IS_JETPACK" -> SITE_IS_JETPACK
-                        networkError.isGeneric &&
-                                networkError.type == BaseRequest.GenericErrorType.INVALID_RESPONSE -> INVALID_RESPONSE
-                        networkError.apiError == "unauthorized" -> AUTHORIZATION_REQUIRED
-                        networkError.apiError == "INVALID_INPUT" -> USERNAME_OR_PASSWORD_MISSING
-                        else -> GENERIC_ERROR
-                    }
-                    cont.resume(JetpackInstalledPayload(JetpackInstallError(error, networkError.message), site))
-                })
-        add(request)
+        val response = wpComGsonRequestBuilder.syncPostRequest(
+                this,
+                url,
+                params,
+                JetpackInstallResponse::class.java)
+        return when(response) {
+            is Success -> JetpackInstalledPayload(site, response.data.status)
+            is WPComGsonRequestBuilder.Response.Error -> {
+                val error = when {
+                    response.error.apiError == "SITE_IS_JETPACK" -> SITE_IS_JETPACK
+                    response.error.isGeneric &&
+                            response.error.type == BaseRequest.GenericErrorType.INVALID_RESPONSE -> INVALID_RESPONSE
+                    response.error.apiError == "unauthorized" -> AUTHORIZATION_REQUIRED
+                    response.error.apiError == "INVALID_INPUT" -> USERNAME_OR_PASSWORD_MISSING
+                    else -> GENERIC_ERROR
+                }
+                JetpackInstalledPayload(JetpackInstallError(error, response.error.message), site)
+            }
+        }
     }
 
     data class JetpackInstallResponse(val status: Boolean)
