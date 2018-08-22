dfff40396b8b405ff40a511edf8a0baf477f7be4
Jeremy Massel <jeremy.massel@gmail.com>
2018-08-21 23:41:35 -0600

    Generate Emulators From Scratch
    Instead of trying to use the right magic strings, use our own devices, then build emulators powered by them!
    
    Dead code cleanup + input validation

--- fastlane/helpers/fixup-android-emulator.sh
+++ fastlane/helpers/fixup-android-emulator.sh
@@ -1,16 +1,5 @@
 #/bin/bash
 
-function replace_key_with (){
-    key=$1
-    value=$2
-    path=$3
-
-    awk "!/${key}/" $path > tmp
-    mv tmp $path
-
-    echo "${key}=${value}" >> $path
-}
-
 device_key=$1
 
 if [ -z "$device_key" ]
@@ -18,33 +7,6 @@ then
     echo "ERROR: You must provide the device name"
     exit 1
 fi
-
-path="${HOME}/.android/avd/${device_key}.avd/config.ini"
-
-if [ ! -f "$path" ]; then
-    echo "ERROR: Unable to find the emulator at ${path} – are you sure your device name is '${device_key}'?"
-    exit 2
-fi
-
-if [ -z "${ANDROID_HOME}" ]
-then
-    echo 'ERROR: $ANDROID_HOME is not set as an environment variable.'
-    exit 3
-fi
-
-## Set up a disk partition size
-replace_key_with 'disk.dataPartition.size' '800M' $path
-
-## Don't allow fast boot
-replace_key_with 'fastboot.forceColdBoot' 'yes' $path
-
-# Set up the Hardware Profile
-replace_key_with 'hw.gpu.enabled' 'yes' $path
-replace_key_with 'hw.gpu.mode' 'auto' $path
-
-# Ensure the Emulator works in Android Studio
-replace_key_with 'skin.path' '_no_skin' $path
-
 
 ### Set up the emulator without animations, per
 ### https://developer.android.com/training/testing/espresso/setup#set-up-environment
