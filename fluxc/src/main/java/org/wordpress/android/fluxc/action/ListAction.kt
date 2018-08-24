475081d40d229b2a5ff5aa5b85e00926b79c51fd
Oguz Kocer <oguz.kocer@automattic.com>
2018-08-24 12:47:22 -0400

    Fetch list action implemented in ListStore

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/ListAction.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/ListAction.kt
@@ -3,11 +3,13 @@ package org.wordpress.android.fluxc.action
 import org.wordpress.android.fluxc.annotations.Action
 import org.wordpress.android.fluxc.annotations.ActionEnum
 import org.wordpress.android.fluxc.annotations.action.IAction
+import org.wordpress.android.fluxc.store.ListStore.FetchListPayload
 import org.wordpress.android.fluxc.store.ListStore.UpdateListPayload
 
 @ActionEnum
 enum class ListAction : IAction {
-    // Remote actions
+    @Action(payloadType = FetchListPayload::class)
+    FETCH_LIST,
     @Action(payloadType = UpdateListPayload::class)
     UPDATE_LIST
 }
