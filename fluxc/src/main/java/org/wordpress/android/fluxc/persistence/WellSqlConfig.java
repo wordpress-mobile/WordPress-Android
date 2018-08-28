fc8077a7d70ee39d5649126919c30d9dedf69f7e
Klymentiy haykov <forsver@gmail.com>
2018-08-28 20:17:40 +0200

    Addressed PR feedback.

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.java
@@ -309,8 +309,11 @@ public class WellSqlConfig extends DefaultWellConfig {
                 oldVersion++;
             case 37:
                 AppLog.d(T.DB, "Migrating to version " + (oldVersion + 1));
-                db.execSQL("CREATE TABLE QuickStartModel (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                db.execSQL("DROP TABLE IF EXISTS QuickStartModel");
+                db.execSQL("CREATE TABLE QuickStartTaskModel (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                            + "SITE_ID INTEGER,TASK_NAME TEXT,IS_DONE INTEGER,IS_SHOWN INTEGER)");
+                db.execSQL("CREATE TABLE QuickStartStatusModel (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                           + "SITE_ID INTEGER,IS_COMPLETED INTEGER,IS_NOTIFICATION_RECEIVED INTEGER)");
                 oldVersion++;
             case 38:
                 AppLog.d(T.DB, "Migrating to version " + (oldVersion + 1));
@@ -327,9 +330,9 @@ public class WellSqlConfig extends DefaultWellConfig {
             case 39:
                 AppLog.d(T.DB, "Migrating to version " + (oldVersion + 1));
                 db.execSQL("DROP TABLE IF EXISTS QuickStartModel");
-                db.execSQL("CREATE TABLE QuickStartTaskModel (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                db.execSQL("CREATE TABLE IF NOT EXISTS QuickStartTaskModel (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                            + "SITE_ID INTEGER,TASK_NAME TEXT,IS_DONE INTEGER,IS_SHOWN INTEGER)");
-                db.execSQL("CREATE TABLE QuickStartStatusModel (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                db.execSQL("CREATE TABLE IF NOT EXISTS QuickStartStatusModel (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                            + "SITE_ID INTEGER,IS_COMPLETED INTEGER,IS_NOTIFICATION_RECEIVED INTEGER)");
                 oldVersion++;
         }
