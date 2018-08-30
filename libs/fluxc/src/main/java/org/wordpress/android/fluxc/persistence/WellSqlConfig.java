ebc0ea54d8b577b08f8649c1da0a282aa4f00a45
Oguz Kocer <oguz.kocer@automattic.com>
2018-08-30 12:09:27 -0400

    Remove lastModified property from ListItemModel

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.java
@@ -329,10 +329,9 @@ public class WellSqlConfig extends DefaultWellConfig {
                 db.execSQL("CREATE TABLE ListModel (DATE_CREATED TEXT,LAST_MODIFIED TEXT,LOCAL_SITE_ID INTEGER,"
                            + "TYPE TEXT,_id INTEGER PRIMARY KEY AUTOINCREMENT,FOREIGN KEY(LOCAL_SITE_ID) "
                            + "REFERENCES SiteModel(_id) ON DELETE CASCADE,UNIQUE(LOCAL_SITE_ID, TYPE))");
-                db.execSQL("CREATE TABLE ListItemModel (LIST_ID INTEGER,REMOTE_ITEM_ID INTEGER,LAST_MODIFIED TEXT,"
-                           + "_id INTEGER PRIMARY KEY AUTOINCREMENT,FOREIGN KEY(LIST_ID) REFERENCES ListModel(_id) "
-                           + "ON DELETE CASCADE,UNIQUE(LIST_ID, REMOTE_ITEM_ID))");
-                db.execSQL("ALTER TABLE PostModel ADD LAST_MODIFIED TEXT");
+                db.execSQL("CREATE TABLE ListItemModel (LIST_ID INTEGER,REMOTE_ITEM_ID INTEGER,_id INTEGER PRIMARY KEY "
+                           + "AUTOINCREMENT,FOREIGN KEY(LIST_ID) REFERENCES ListModel(_id) ON DELETE CASCADE,"
+                           + "UNIQUE(LIST_ID, REMOTE_ITEM_ID))");
                 oldVersion++;
         }
         db.setTransactionSuccessful();
