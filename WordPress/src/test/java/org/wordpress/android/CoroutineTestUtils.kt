ad29bd3839ee80218638ecdc280a5297cc6a8ad5
vojtasmrcek <planarvoid@gmail.com>
2018-08-31 15:04:32 +0200

    Remove unused coroutine utils

--- WordPress/src/test/java/org/wordpress/android/CoroutineTestUtils.kt
+++
@@ -1,9 +0,0 @@
-package org.wordpress.android
-
-import kotlinx.coroutines.experimental.delay
-import org.mockito.stubbing.OngoingStubbing
-
-suspend fun <T> OngoingStubbing<T>.thenReturnSuspended(value: T) {
-    delay(1000)
-    thenReturn(value)
-}
