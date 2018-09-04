d5695135b820089846362ffa37e9861a43546323
Oguz Kocer <oguz.kocer@automattic.com>
2018-09-04 16:14:09 -0400

    ListFilter, ListOrder and ListType separated. fromValue now works with types

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListDescriptor.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListDescriptor.kt
@@ -1,30 +1,5 @@
 package org.wordpress.android.fluxc.model.list
 
-interface ListFilter {
-    val value: String
-    companion object {
-        fun fromValue(value: String?): ListFilter? =
-                ListFilter::class.java.enumConstants?.firstOrNull { it.value == value }
-    }
-}
-
-interface ListOrder {
-    val value: String
-    companion object {
-        fun fromValue(value: String?): ListOrder? =
-                ListOrder::class.java.enumConstants?.firstOrNull { it.value == value }
-    }
-}
-
-enum class ListType(val value: Int) {
-    POST(0),
-    WOO_ORDER(1);
-    companion object {
-        // If the type is missing we want the app to crash so we can fix it immediately
-        fun fromValue(value: Int?): ListType = ListType.values().firstOrNull { it.value == value }!!
-    }
-}
-
 data class ListDescriptor(
     val type: ListType,
     val localSiteId: Int? = null,
@@ -32,5 +7,8 @@ data class ListDescriptor(
     val order: ListOrder? = null
 ) {
     constructor(type: Int?, localSiteId: Int?, filter: String?, order: String?) :
-            this(ListType.fromValue(type), localSiteId, ListFilter.fromValue(filter), ListOrder.fromValue(order))
+            this(ListType.fromValue(type), localSiteId, filter, order)
+
+    constructor(type: ListType, localSiteId: Int?, filter: String?, order: String?) :
+            this(type, localSiteId, ListFilter.fromValue(type, filter), ListOrder.fromValue(type, order))
 }
