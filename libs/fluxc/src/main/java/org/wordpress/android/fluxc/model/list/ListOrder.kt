776df958d3a4c0c5d26ad6ca0c8805f889254fa1
Oguz Kocer <oguz.kocer@automattic.com>
2018-09-04 18:16:40 -0400

    Adds documentation to list models

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListOrder.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListOrder.kt
@@ -1,5 +1,15 @@
 package org.wordpress.android.fluxc.model.list
 
+/**
+ * This is an interface used by `ListModel` to indicate the order of a list. The only requirement is
+ * to have a [value] which will be saved into the DB.
+ *
+ * [fromValue] function will be used to convert the DB value back to a [ListOrder] instance.
+ * Once the enum is added, the [fromValue] function should be updated to associate the `ListType` with that enum.
+ *
+ * A [BasicListOrder] is provided, but if a different order is required per type, a similar approach to `ListFilter`
+ * could be utilized. [fromValue] function requires the list type for this reason.
+ */
 interface ListOrder {
     val value: String
 
