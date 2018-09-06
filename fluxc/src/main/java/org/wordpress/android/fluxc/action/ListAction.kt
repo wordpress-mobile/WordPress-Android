3c108c31cff1d68aa5aabe3b41a2a4651a25d60c
Oguz Kocer <oguz.kocer@automattic.com>
2018-09-06 13:00:04 -0400

    ListStore small improvements and documentation

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/ListAction.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/ListAction.kt
@@ -5,14 +5,14 @@ import org.wordpress.android.fluxc.annotations.ActionEnum
 import org.wordpress.android.fluxc.annotations.action.IAction
 import org.wordpress.android.fluxc.store.ListStore.DeleteListItemsPayload
 import org.wordpress.android.fluxc.store.ListStore.FetchListPayload
-import org.wordpress.android.fluxc.store.ListStore.UpdateListPayload
+import org.wordpress.android.fluxc.store.ListStore.FetchedListItemsPayload
 
 @ActionEnum
 enum class ListAction : IAction {
     @Action(payloadType = FetchListPayload::class)
     FETCH_LIST,
-    @Action(payloadType = UpdateListPayload::class)
-    UPDATE_LIST,
+    @Action(payloadType = FetchedListItemsPayload::class)
+    FETCHED_LIST_ITEMS,
     @Action(payloadType = DeleteListItemsPayload::class)
     DELETE_LIST_ITEMS
 }
