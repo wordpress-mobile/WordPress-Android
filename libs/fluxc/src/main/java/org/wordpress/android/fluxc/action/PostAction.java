29b7362f673f81d64c3c7700f96245e421d1c76d
Oguz Kocer <oguz.kocer@automattic.com>
2018-09-06 15:59:33 -0400

    Removes unused search post and page actions from PostStore

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/PostAction.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/PostAction.java
@@ -6,8 +6,6 @@ import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.model.PostModel;
 import org.wordpress.android.fluxc.store.PostStore.FetchPostResponsePayload;
 import org.wordpress.android.fluxc.store.PostStore.FetchPostsPayload;
-import org.wordpress.android.fluxc.store.PostStore.SearchPostsPayload;
-import org.wordpress.android.fluxc.store.PostStore.SearchPostsResponsePayload;
 import org.wordpress.android.fluxc.store.PostStore.FetchPostsResponsePayload;
 import org.wordpress.android.fluxc.store.PostStore.RemotePostPayload;
 
@@ -24,10 +22,6 @@ public enum PostAction implements IAction {
     PUSH_POST,
     @Action(payloadType = RemotePostPayload.class)
     DELETE_POST,
-    @Action(payloadType = SearchPostsPayload.class)
-    SEARCH_POSTS,
-    @Action(payloadType = SearchPostsPayload.class)
-    SEARCH_PAGES,
 
     // Remote responses
     @Action(payloadType = FetchPostsResponsePayload.class)
@@ -38,8 +32,6 @@ public enum PostAction implements IAction {
     PUSHED_POST,
     @Action(payloadType = RemotePostPayload.class)
     DELETED_POST,
-    @Action(payloadType = SearchPostsResponsePayload.class)
-    SEARCHED_POSTS,
 
     // Local actions
     @Action(payloadType = PostModel.class)
