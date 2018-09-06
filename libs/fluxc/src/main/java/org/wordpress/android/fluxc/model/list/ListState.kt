838ca0d8f3ffd4c6bc35e431d73e0017d13987b8
Oguz Kocer <oguz.kocer@automattic.com>
2018-09-06 15:01:37 -0400

    Move ListState logic back to ListStore

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListState.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListState.kt
@@ -1,5 +1,11 @@
 package org.wordpress.android.fluxc.model.list
 
+/**
+ * This is an enum used by `ListStore` to manage the state of a [ListModel]. It'll be saved to `ListModelTable`.
+ *
+ * IMPORTANT: Because the values are stored in the DB, in case of a change to the enum values a migration needs to
+ * be added!
+ */
 enum class ListState(val value: Int) {
     NEEDS_REFRESH(0),
     CAN_LOAD_MORE(1),
@@ -7,6 +13,12 @@ enum class ListState(val value: Int) {
     FETCHING_FIRST_PAGE(3),
     LOADING_MORE(4),
     ERROR(5);
+
+    fun canLoadMore() = this == CAN_LOAD_MORE
+
+    fun isFetchingFirstPage() = this == FETCHING_FIRST_PAGE
+
+    fun isLoadingMore() = this == LOADING_MORE
 
     companion object {
         val defaultState = ListState.NEEDS_REFRESH
