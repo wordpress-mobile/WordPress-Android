751085ca132645e766428073ce67b4e8110c3297
Ondrej Ruttkay <ondrej@automattic.com>
2018-09-08 00:47:59 +0200

    Add an interface with common functionality for all page lists

---
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/pages/IListViewModel.kt
@@ -0,0 +1,19 @@
+package org.wordpress.android.viewmodel.pages
+
+import android.arch.lifecycle.LiveData
+import org.wordpress.android.ui.pages.PageItem
+import org.wordpress.android.ui.pages.PageItem.Action
+import org.wordpress.android.ui.pages.PageItem.Page
+
+interface IListViewModel {
+    val pages: LiveData<List<PageItem>>
+    val listType: ListType
+
+    fun onMenuAction(action: Action, pageItem: Page): Boolean
+    fun onItemTapped(pageItem: Page)
+
+    enum class ListType {
+        PAGES,
+        SEARCH
+    }
+}
