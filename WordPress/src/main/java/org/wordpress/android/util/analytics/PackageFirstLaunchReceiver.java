ca7ad8f5b98305a3485cf4526fe0be3033da3e9f
Mario Zorz <mariozorz@gmail.com>
2018-09-10 18:41:40 -0300

    implemented broadcaste receiver to listen to ACTION_PACKAGE_FIRST_LAUNCH first time app is run

---
+++ WordPress/src/main/java/org/wordpress/android/util/analytics/PackageFirstLaunchReceiver.java
@@ -0,0 +1,12 @@
+package org.wordpress.android.util.analytics;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class PackageFirstLaunchReceiver extends BroadcastReceiver {
+    public void onReceive(Context context, Intent receivedIntent) {
+        // ToDo implement install referrer library https://developer.android.com/google/play/installreferrer/library
+    }
+}
