9546b9824943365cd01ad7dfbde3a863248d97a2
Klymentiy haykov <forsver@gmail.com>
2018-09-10 12:28:59 +0300

    Revisions response.

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/revisions/RevisionsResponse.kt
@@ -0,0 +1,55 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.revisions
+
+import com.google.gson.annotations.SerializedName
+
+class RevisionsResponse(
+    val diffs: List<DiffDetails>,
+    val revisions: Map<String, Revision>) {
+
+    inner class DiffDetails(
+        val from: Int,
+        val to: Int,
+        val diff: Diff)
+
+    inner class Diff(
+        val post_title: PostTitle,
+        val post_content: PostContent,
+        val total: Totals) {
+
+        inner class PostTitle(
+            val op: DiffAction,
+            val value: String)
+
+        inner class PostContent(
+            val op: DiffAction,
+            val value: String
+        )
+
+        inner class Totals(
+            val del: Int,
+            val add: Int)
+    }
+
+    enum class DiffAction {
+        @SerializedName("copy")
+        COPY,
+        @SerializedName("add")
+        ADD,
+        @SerializedName("del")
+        DELETE
+    }
+
+    inner class Revision(
+        val post_date_gmt: String,
+        val post_modified_gmt: String,
+        val post_author: String,
+        val id: Int,
+        val post_content: String,
+        val post_excerpt: String,
+        val post_title: String
+    )
+
+}
+
+
+
