2279fd44a120d67bfcd27ca3886ccfa877aac143
vojtasmrcek <planarvoid@gmail.com>
2018-09-12 10:09:23 +0200

    Remove unused live data utils and fix tests afterwards

--- WordPress/src/test/java/org/wordpress/android/viewmodel/pages/PagesViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/pages/PagesViewModelTest.kt
@@ -15,14 +15,16 @@ import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.page.PageModel
+import org.wordpress.android.fluxc.model.page.PageStatus
 import org.wordpress.android.fluxc.model.page.PageStatus.DRAFT
 import org.wordpress.android.fluxc.store.PageStore
 import org.wordpress.android.fluxc.store.PostStore.OnPostChanged
+import org.wordpress.android.viewmodel.pages.PageListViewModel.PageListState
 import org.wordpress.android.viewmodel.pages.PageListViewModel.PageListState.DONE
 import org.wordpress.android.viewmodel.pages.PageListViewModel.PageListState.FETCHING
 import org.wordpress.android.viewmodel.pages.PageListViewModel.PageListState.REFRESHING
-import org.wordpress.android.viewmodel.test
 import java.util.Date
+import java.util.SortedMap
 
 @RunWith(MockitoJUnitRunner::class)
 class PagesViewModelTest {
@@ -34,50 +36,51 @@ class PagesViewModelTest {
     @Mock lateinit var dispatcher: Dispatcher
     @Mock lateinit var actionPerformer: ActionPerformer
     private lateinit var viewModel: PagesViewModel
+    private lateinit var listStates: MutableList<PageListState>
+    private lateinit var refreshPages: MutableList<List<PageModel>>
+    private lateinit var searchPages: MutableList<SortedMap<PageStatus, List<PageModel>>>
 
     @Before
     fun setUp() {
         viewModel = PagesViewModel(pageStore, dispatcher, actionPerformer, Unconfined, Unconfined)
+        listStates = mutableListOf()
+        refreshPages = mutableListOf()
+        viewModel.listState.observeForever { if (it != null) listStates.add(it) }
+        viewModel.pages.observeForever { if (it != null) refreshPages.add(it) }
+        viewModel.searchPages.observeForever { if (it != null) searchPages.add(it) }
     }
 
     @Test
     fun clearsResultAndLoadsDataOnStart() = runBlocking<Unit> {
-        whenever(pageStore.getPagesFromDb(site)).thenReturn(listOf(
-                PageModel(site, 1, "title", DRAFT, Date(), false, 1, null))
+        whenever(pageStore.getPagesFromDb(site)).thenReturn(
+                listOf(
+                        PageModel(site, 1, "title", DRAFT, Date(), false, 1, null)
+                )
         )
         whenever(pageStore.requestPagesFromServer(any())).thenReturn(OnPostChanged(1, false))
-        val listStateObserver = viewModel.listState.test()
-        val refreshPagesObserver = viewModel.pages.test()
-        val searchPagesObserver = viewModel.searchPages.test()
 
         viewModel.start(site)
 
-        val listStates = listStateObserver.awaitValues(2)
-
         assertThat(listStates).containsExactly(REFRESHING, DONE)
-        refreshPagesObserver.awaitNullableValues(2)
+        assertThat(refreshPages).hasSize(3)
     }
 
     @Test
     fun onSiteWithoutPages() = runBlocking<Unit> {
         whenever(pageStore.getPagesFromDb(site)).thenReturn(emptyList())
         whenever(pageStore.requestPagesFromServer(any())).thenReturn(OnPostChanged(0, false))
-        val listStateObserver = viewModel.listState.test()
-        val refreshPagesObserver = viewModel.pages.test()
 
         viewModel.start(site)
 
-        val listStates = listStateObserver.awaitValues(2)
-
         assertThat(listStates).containsExactly(FETCHING, DONE)
-        refreshPagesObserver.awaitNullableValues(2)
+        assertThat(refreshPages).hasSize(3)
     }
 
     @Test
     fun onSearchReturnsResultsFromStore() = runBlocking<Unit> {
         initSearch()
         val query = "query"
-        val drafts = listOf( PageModel(site, 1, "title", DRAFT, Date(), false, 1, null))
+        val drafts = listOf(PageModel(site, 1, "title", DRAFT, Date(), false, 1, null))
         val expectedResult = sortedMapOf(DRAFT to drafts)
         whenever(pageStore.groupedSearch(site, query)).thenReturn(expectedResult)
 
