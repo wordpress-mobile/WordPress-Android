ac2eefb4c088bb0af134e144bb23a4e8b49cb247
Oguz Kocer <oguz.kocer@automattic.com>
2018-09-13 17:46:28 -0400

    Fix lint issues related to PostCauseOfChange

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/PostCauseOfChange.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/PostCauseOfChange.kt
@@ -1,10 +1,10 @@
 package org.wordpress.android.fluxc.model
 
 sealed class PostCauseOfChange {
-    class DeletePost(val localPostId: Int, val remotePostId: Long): PostCauseOfChange()
+    class DeletePost(val localPostId: Int, val remotePostId: Long) : PostCauseOfChange()
     object FetchPages : PostCauseOfChange()
-    object FetchPosts: PostCauseOfChange()
-    object RemoveAllPosts: PostCauseOfChange()
-    class RemovePost(val localPostId: Int, val remotePostId: Long): PostCauseOfChange()
-    class UpdatePost(val localPostId: Int, val remotePostId: Long): PostCauseOfChange()
+    object FetchPosts : PostCauseOfChange()
+    object RemoveAllPosts : PostCauseOfChange()
+    class RemovePost(val localPostId: Int, val remotePostId: Long) : PostCauseOfChange()
+    class UpdatePost(val localPostId: Int, val remotePostId: Long) : PostCauseOfChange()
 }
