88ec9e8f9c60c77e99f21298152c21b23c669136
Klymentiy haykov <forsver@gmail.com>
2018-09-17 17:17:19 +0300

    Generating sample revision model for testing convenience.

--- fluxc/src/test/java/org/wordpress/android/fluxc/post/PostTestUtils.java
+++ fluxc/src/test/java/org/wordpress/android/fluxc/post/PostTestUtils.java
@@ -3,7 +3,11 @@ package org.wordpress.android.fluxc.post;
 import com.yarolegovich.wellsql.WellSql;
 
 import org.wordpress.android.fluxc.model.PostModel;
+import org.wordpress.android.fluxc.model.revisions.Diff;
+import org.wordpress.android.fluxc.model.revisions.DiffOperations;
+import org.wordpress.android.fluxc.model.revisions.RevisionModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PostTestUtils {
@@ -49,5 +53,32 @@ public class PostTestUtils {
 
     public static int getPostsCount() {
         return getPosts().size();
+    }
+
+    public static RevisionModel generateSamplePostRevision() {
+        ArrayList<Diff> testTitleDiffs = new ArrayList<>();
+        testTitleDiffs.add(new Diff(DiffOperations.COPY, "copy title"));
+        testTitleDiffs.add(new Diff(DiffOperations.ADD, "add title"));
+        testTitleDiffs.add(new Diff(DiffOperations.DELETE, "del title"));
+
+        ArrayList<Diff> testContentDiff = new ArrayList<>();
+        testContentDiff.add(new Diff(DiffOperations.COPY, "copy content"));
+        testContentDiff.add(new Diff(DiffOperations.ADD, "add content"));
+        testContentDiff.add(new Diff(DiffOperations.DELETE, "del content"));
+
+        return new RevisionModel(
+                1,
+                2,
+                5,
+                6,
+                "post content",
+                "post excerpt",
+                "post title",
+                "2018-09-04 12:19:34Z",
+                "2018-09-05 13:19:34Z",
+                "111111111",
+                testTitleDiffs,
+                testContentDiff
+        );
     }
 }
