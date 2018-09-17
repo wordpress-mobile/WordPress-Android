9fedba8cf9a2d1daad7eadee97847eaa00846cd2
Oguz Kocer <oguz.kocer@automattic.com>
2018-09-17 19:28:35 -0400

    Dispatch list items updated action on post delete

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListDescriptor.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListDescriptor.kt
@@ -16,10 +16,4 @@ data class ListDescriptor(
     val localSiteId: Int? = null,
     val filter: ListFilter? = null,
     val order: ListOrder? = null
-) {
-    constructor(type: Int?, localSiteId: Int?, filter: String?, order: String?) :
-            this(ListType.fromValue(type), localSiteId, filter, order)
-
-    constructor(type: ListType, localSiteId: Int?, filter: String?, order: String?) :
-            this(type, localSiteId, ListFilter.fromValue(type, filter), ListOrder.fromValue(type, order))
-}
+)
