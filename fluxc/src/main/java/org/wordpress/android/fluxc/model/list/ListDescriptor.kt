171a3c3404146f8cf144b4c8ee35991bb3d20bc6
Oguz Kocer <oguz.kocer@automattic.com>
2018-09-20 00:13:33 -0400

    Add search to new ListDescriptor

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListDescriptor.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/list/ListDescriptor.kt
@@ -23,12 +23,27 @@ sealed class ListDescriptor {
     class PostListDescriptor @JvmOverloads constructor(
         val localSiteId: Int,
         val filter: PostListFilter = PostListFilter.ANY,
-        val order: BasicListOrder = BasicListOrder.ASC,
+        val order: ListOrder = ListOrder.DESC,
+        val orderBy: PostOrderBy = PostOrderBy.DATE,
         val searchQuery: String? = null
     ) : ListDescriptor() {
         override fun calculateHash(): Int {
             // TODO: do something better!
-            return "$localSiteId-f${filter.value}-o${order.value}-s$searchQuery".hashCode()
+            return "$localSiteId-f${filter.value}-o${order.value}-ob-${orderBy.value}-sq$searchQuery".hashCode()
         }
     }
 }
+
+enum class ListOrder(val value: String) {
+    ASC("asc"),
+    DESC("desc");
+}
+
+enum class PostOrderBy(val value: String) {
+    DATE("date"),
+    LAST_MODIFIED("modified"),
+    TITLE("title"),
+    COMMENT_COUNT("comment_count"),
+    ID("ID");
+}
+
