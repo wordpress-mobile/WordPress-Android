97efd2da4936d142ef10663cf63b4f1455628207
Ondrej Ruttkay <ondrej@automattic.com>
2018-10-09 14:27:52 +0200

    Update Kotlin version and make necessary changes

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/InsightsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/InsightsViewModel.kt
@@ -3,9 +3,9 @@ package org.wordpress.android.ui.stats.refresh
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
-import kotlinx.coroutines.experimental.CoroutineDispatcher
+import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.launch
-import org.wordpress.android.modules.UI_CONTEXT
+import org.wordpress.android.modules.UI_SCOPE
 import org.wordpress.android.ui.stats.refresh.InsightsItem.Type.NOT_IMPLEMENTED
 import org.wordpress.android.ui.stats.refresh.InsightsUiState.ListStatus.DONE
 import org.wordpress.android.ui.stats.refresh.InsightsUiState.ListStatus.FETCHING
@@ -15,7 +15,7 @@ import javax.inject.Named
 class InsightsViewModel
 @Inject constructor(
     private val insightsDomain: InsightsDomain,
-    @Named(UI_CONTEXT) private val uiContext: CoroutineDispatcher
+    @Named(UI_SCOPE) private val uiScope: CoroutineScope
 ) : ViewModel() {
     private val mutableData: MutableLiveData<InsightsUiState> = MutableLiveData()
     val data: LiveData<InsightsUiState> = mutableData
@@ -23,7 +23,7 @@ class InsightsViewModel
         if (mutableData.value == null) {
             mutableData.value = InsightsUiState(status = FETCHING)
         }
-        launch(uiContext) {
+        uiScope.launch {
             val loadedData = insightsDomain.loadInsightItems()
             mutableData.value = InsightsUiState(loadedData, DONE)
         }
