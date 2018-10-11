c5cbc096bb46ebbdccef8d5db4cabf43df46c04b
Tyler Heck <theck13@users.noreply.github.com>
2018-10-11 10:59:36 -0400

    Update formatted time of activity log list item to use extension function

--- WordPress/src/main/java/org/wordpress/android/ui/activitylog/list/ActivityLogListItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/list/ActivityLogListItem.kt
@@ -10,9 +10,8 @@ import org.wordpress.android.ui.activitylog.list.ActivityLogListItem.ViewType.HE
 import org.wordpress.android.ui.activitylog.list.ActivityLogListItem.ViewType.LOADING
 import org.wordpress.android.ui.activitylog.list.ActivityLogListItem.ViewType.PROGRESS
 import org.wordpress.android.util.toFormattedDateString
-import java.text.DateFormat
+import org.wordpress.android.util.toFormattedTimeString
 import java.util.Date
-import java.util.Locale
 
 sealed class ActivityLogListItem(val type: ViewType) {
     interface IActionableItem {
@@ -35,7 +34,7 @@ sealed class ActivityLogListItem(val type: ViewType) {
         val isProgressBarVisible: Boolean = false
     ) : ActivityLogListItem(EVENT), IActionableItem {
         val formattedDate: String = date.toFormattedDateString()
-        val formattedTime: String = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault()).format(date)
+        val formattedTime: String = date.toFormattedTimeString()
         val icon = Icon.fromValue(gridIcon)
         val status = Status.fromValue(eventStatus)
 
