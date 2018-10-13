54f5c98ef0631552d3830bf99ad9eea1d8e85e9f
Ondrej Ruttkay <ondrej@automattic.com>
2018-10-13 11:22:14 +0200

    Refactor Inisghts fragment and VM to a general stats list fragment

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsListFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsListFragment.kt
@@ -12,11 +12,29 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_log_list_fragment.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
+import org.wordpress.android.ui.stats.refresh.StatsListViewModel.StatsListType
 import javax.inject.Inject
 
-class InsightsFragment : Fragment() {
+class StatsListFragment : Fragment() {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
-    private lateinit var mViewModel: InsightsViewModel
+    private lateinit var mViewModel: StatsListViewModel
+
+    private lateinit var viewModel: StatsListViewModel
+    private var linearLayoutManager: LinearLayoutManager? = null
+
+    private val listStateKey = "list_state"
+
+    companion object {
+        private const val typeKey = "type_key"
+
+        fun newInstance(listType: StatsListType): StatsListFragment {
+            val fragment = StatsListFragment()
+            val bundle = Bundle()
+            bundle.putSerializable(typeKey, listType)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_log_list_fragment, container, false)
@@ -30,7 +48,7 @@ class InsightsFragment : Fragment() {
         (activity?.application as WordPress).component()?.inject(this)
 
         val viewModelProvider = ViewModelProviders.of(this, viewModelFactory)
-        mViewModel = viewModelProvider.get(InsightsViewModel::class.java)
+        mViewModel = viewModelProvider.get(StatsListViewModel::class.java)
 
         val adapter = InsightsAdapter()
 
--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsListViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsListViewModel.kt
@@ -3,8 +3,10 @@ package org.wordpress.android.ui.stats.refresh
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.support.annotation.StringRes
 import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.launch
+import org.wordpress.android.R
 import org.wordpress.android.modules.UI_SCOPE
 import org.wordpress.android.ui.stats.refresh.InsightsItem.Type.NOT_IMPLEMENTED
 import org.wordpress.android.ui.stats.refresh.InsightsUiState.ListStatus.DONE
@@ -12,11 +14,18 @@ import org.wordpress.android.ui.stats.refresh.InsightsUiState.ListStatus.FETCHIN
 import javax.inject.Inject
 import javax.inject.Named
 
-class InsightsViewModel
+class StatsListViewModel
 @Inject constructor(
     private val insightsDomain: InsightsDomain,
     @Named(UI_SCOPE) private val uiScope: CoroutineScope
 ) : ViewModel() {
+    enum class StatsListType(@StringRes val titleRes: Int) {
+        INSIGHTS(R.string.stats_insights),
+        DAYS(R.string.stats_timeframe_days),
+        WEEKS(R.string.stats_timeframe_weeks),
+        MONTHS(R.string.stats_timeframe_months);
+    }
+
     private val mutableData: MutableLiveData<InsightsUiState> = MutableLiveData()
     val data: LiveData<InsightsUiState> = mutableData
     fun start() {
