cadca038c61dea6c7a9fe272fe5d980a45122c93
Ondrej Ruttkay <ondrej@automattic.com>
2018-10-19 23:42:36 +0200

    Let StatsViewModel inherit from ScopedViewModel

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewModel.kt
@@ -2,19 +2,17 @@ package org.wordpress.android.ui.stats.refresh
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
 import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.withContext
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.modules.DEFAULT_SCOPE
-import org.wordpress.android.modules.UI_SCOPE
 import org.wordpress.android.ui.pages.PageItem.Action
 import org.wordpress.android.ui.pages.PageItem.Page
 import org.wordpress.android.ui.pages.SnackbarMessageHolder
 import org.wordpress.android.ui.stats.refresh.InsightsUiState.StatsListState
 import org.wordpress.android.ui.stats.refresh.InsightsUiState.StatsListState.FETCHING
 import org.wordpress.android.ui.stats.refresh.StatsListViewModel.StatsListType
+import org.wordpress.android.viewmodel.ScopedViewModel
 import org.wordpress.android.viewmodel.SingleLiveEvent
 import javax.inject.Inject
 import javax.inject.Named
@@ -22,9 +20,8 @@ import javax.inject.Named
 class StatsViewModel
 @Inject constructor(
     private val insightsViewModel: InsightsViewModel,
-    @Named(UI_SCOPE) private val uiScope: CoroutineScope,
     @Named(DEFAULT_SCOPE) private val defaultScope: CoroutineScope
-) : ViewModel() {
+) : ScopedViewModel() {
     private val _listState = MutableLiveData<StatsListState>()
     val listState: LiveData<StatsListState> = _listState
 
@@ -73,19 +70,8 @@ class StatsViewModel
     }
 
     fun onPullToRefresh() {
-        uiScope.launch {
+        launch {
             reloadStats()
-        }
-    }
-
-    private suspend fun <T> MutableLiveData<T>.setOnUi(value: T) = withContext(uiScope.coroutineContext) {
-        setValue(value)
-    }
-
-    private fun <T> MutableLiveData<T>.postOnUi(value: T) {
-        val liveData = this
-        uiScope.launch {
-            liveData.value = value
         }
     }
 }
