cd81485c1b14214fb9ebf50a6db3bb93fe6a055c
Ondrej Ruttkay <ondrej@automattic.com>
2018-10-19 23:35:01 +0200

    Add scoped ViewModel class that will be the base for all ViewModels

---
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/ScopedViewModel.kt
@@ -0,0 +1,33 @@
+package org.wordpress.android.viewmodel
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.Main
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
+import kotlin.coroutines.experimental.CoroutineContext
+
+abstract class ScopedViewModel : ViewModel(), CoroutineScope {
+    protected var job: Job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Default + job
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
+    protected suspend fun <T> MutableLiveData<T>.setOnUi(value: T) = withContext(coroutineContext) {
+        setValue(value)
+    }
+
+    private fun <T> MutableLiveData<T>.postOnUi(value: T) {
+        val liveData = this
+        launch {
+            liveData.value = value
+        }
+    }
+}
