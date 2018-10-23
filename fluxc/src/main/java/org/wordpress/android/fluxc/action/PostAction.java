397f2054f1f7473aa496593d5ca6eff729c442c6
Ondrej Ruttkay <ondrej@automattic.com>
2018-10-23 10:54:33 +0200

    Optimize imports

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/PostAction.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/PostAction.java
@@ -6,9 +6,9 @@ import org.wordpress.android.fluxc.annotations.action.IAction;
 import org.wordpress.android.fluxc.model.PostModel;
 import org.wordpress.android.fluxc.store.PostStore.FetchPostResponsePayload;
 import org.wordpress.android.fluxc.store.PostStore.FetchPostsPayload;
+import org.wordpress.android.fluxc.store.PostStore.FetchPostsResponsePayload;
 import org.wordpress.android.fluxc.store.PostStore.FetchRevisionsPayload;
 import org.wordpress.android.fluxc.store.PostStore.FetchRevisionsResponsePayload;
-import org.wordpress.android.fluxc.store.PostStore.FetchPostsResponsePayload;
 import org.wordpress.android.fluxc.store.PostStore.RemotePostPayload;
 
 @ActionEnum
