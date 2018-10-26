904aa20da4364d467c62e9f4cabab1a5e11b4968
Tyler Heck <theck13@users.noreply.github.com>
2018-10-26 13:27:22 -0400

    Add space quota bar to media browser activity

--- WordPress/src/main/java/org/wordpress/android/ui/media/MediaBrowserActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/MediaBrowserActivity.java
@@ -37,6 +37,7 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -59,13 +60,14 @@ import org.wordpress.android.ui.RequestCodes;
 import org.wordpress.android.ui.media.MediaGridFragment.MediaFilter;
 import org.wordpress.android.ui.media.MediaGridFragment.MediaGridListener;
 import org.wordpress.android.ui.media.services.MediaDeleteService;
+import org.wordpress.android.ui.plans.PlansConstants;
 import org.wordpress.android.ui.uploads.UploadService;
 import org.wordpress.android.ui.uploads.UploadUtils;
 import org.wordpress.android.util.ActivityUtils;
-import org.wordpress.android.util.analytics.AnalyticsUtils;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.DisplayUtils;
 import org.wordpress.android.util.FluxCUtils;
+import org.wordpress.android.util.FormatUtils;
 import org.wordpress.android.util.ListUtils;
 import org.wordpress.android.util.LocaleManager;
 import org.wordpress.android.util.MediaUtils;
@@ -74,6 +76,7 @@ import org.wordpress.android.util.PermissionUtils;
 import org.wordpress.android.util.ToastUtils;
 import org.wordpress.android.util.WPMediaUtils;
 import org.wordpress.android.util.WPPermissionUtils;
+import org.wordpress.android.util.analytics.AnalyticsUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -107,6 +110,7 @@ public class MediaBrowserActivity extends AppCompatActivity implements MediaGrid
     private MenuItem mSearchMenuItem;
     private Menu mMenu;
     private TabLayout mTabLayout;
+    private TextView mQuotaText;
 
     private MediaDeleteService.MediaDeleteBinder mDeleteService;
     private boolean mDeleteServiceBound;
@@ -195,6 +199,36 @@ public class MediaBrowserActivity extends AppCompatActivity implements MediaGrid
         } else {
             setFilter(filter);
         }
+
+        mQuotaText = findViewById(R.id.quota_text);
+
+        if (mSite != null && mSite.hasDiskSpaceQuotaInformation()) {
+            formatQuotaDiskSpace();
+        }
+    }
+
+    private void formatQuotaDiskSpace() {
+        String percentage = FormatUtils.formatPercentage(mSite.getSpacePercentUsed() / 100);
+
+        final String[] units = new String[] {
+                getString(R.string.file_size_in_bytes),
+                getString(R.string.file_size_in_kilobytes),
+                getString(R.string.file_size_in_megabytes),
+                getString(R.string.file_size_in_gigabytes),
+                getString(R.string.file_size_in_terabytes)
+        };
+
+        String quota;
+
+        if (mSite.getPlanId() == PlansConstants.BUSINESS_PLAN_ID) {
+            String space = FormatUtils.formatFileSize(mSite.getSpaceUsed(), units);
+            quota = String.format(getString(R.string.site_settings_quota_space_unlimited), space);
+        } else {
+            String space = FormatUtils.formatFileSize(mSite.getSpaceAllowed(), units);
+            quota = String.format(getString(R.string.site_settings_quota_space_value), percentage, space);
+        }
+
+        mQuotaText.setText(getString(R.string.media_space_used, quota));
     }
 
     public MediaDeleteService getMediaDeleteService() {
