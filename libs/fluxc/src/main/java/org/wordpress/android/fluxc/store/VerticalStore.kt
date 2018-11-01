0bc3d2999f32fdab50938f7c0fedd179afcec557
Oguz Kocer <oguz.kocer@automattic.com>
2018-11-01 13:30:10 -0400

    Remove VerticalSqlUtils and return segments from onchanged event

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/VerticalStore.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/VerticalStore.kt
@@ -2,7 +2,6 @@ package org.wordpress.android.fluxc.store
 
 import kotlinx.coroutines.experimental.GlobalScope
 import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.withContext
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
@@ -13,7 +12,6 @@ import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.model.vertical.VerticalModel
 import org.wordpress.android.fluxc.model.vertical.VerticalSegmentModel
 import org.wordpress.android.fluxc.network.rest.wpcom.vertical.VerticalRestClient
-import org.wordpress.android.fluxc.persistence.VerticalSqlUtils
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -21,7 +19,6 @@ import kotlin.coroutines.experimental.CoroutineContext
 
 @Singleton
 class VerticalStore @Inject constructor(
-    private val verticalSqlUtils: VerticalSqlUtils,
     private val verticalRestClient: VerticalRestClient,
     private val coroutineContext: CoroutineContext,
     dispatcher: Dispatcher
@@ -43,26 +40,13 @@ class VerticalStore @Inject constructor(
         AppLog.d(AppLog.T.API, ListStore::class.java.simpleName + " onRegister")
     }
 
-    suspend fun getSegments() = withContext(coroutineContext) {
-        verticalSqlUtils.getSegments()
-    }
-
-    // TODO: Do we have pagination and if so, how complicated is it? Do we need all the data at once or should we
-    // TODO: use the ListStore to paginate?
     private suspend fun fetchSegments(): OnSegmentsFetched {
         val fetchedSegmentsPayload = verticalRestClient.fetchSegments()
-        val onSegmentsFetched = OnSegmentsFetched(fetchedSegmentsPayload.error)
-        if (!fetchedSegmentsPayload.isError) {
-            // TODO: If there is pagination, we need to delete the current segments before we insert the new list
-            verticalSqlUtils.insertSegments(fetchedSegmentsPayload.segmentList)
-        }
-        return onSegmentsFetched
+        return OnSegmentsFetched(fetchedSegmentsPayload.segmentList, fetchedSegmentsPayload.error)
     }
 
     private suspend fun fetchVerticals(payload: FetchVerticalsPayload): OnVerticalsFetched {
         val fetchedVerticalsPayload = verticalRestClient.fetchVerticals(payload.searchQuery)
-        // TODO: Don't use the OnChanged event to return the data, save it in the DB. However this depends on a few
-        // TODO: decisions we have yet to made, especially on the API side, so temporarily we use the event.
         return OnVerticalsFetched(
                 payload.searchQuery,
                 fetchedVerticalsPayload.verticalList,
@@ -70,7 +54,10 @@ class VerticalStore @Inject constructor(
         )
     }
 
-    class OnSegmentsFetched(error: FetchSegmentsError? = null) : Store.OnChanged<FetchSegmentsError>() {
+    class OnSegmentsFetched(
+        val segmentList: List<VerticalSegmentModel>,
+        error: FetchSegmentsError? = null
+    ) : Store.OnChanged<FetchSegmentsError>() {
         init {
             this.error = error
         }
