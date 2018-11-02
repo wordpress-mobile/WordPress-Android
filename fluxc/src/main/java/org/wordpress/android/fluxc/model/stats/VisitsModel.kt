86f848154743fad16610663a8cd9b280da289849
vojtasmrcek <planarvoid@gmail.com>
2018-11-02 13:23:40 +0100

    Fix ktlint issues

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/VisitsModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/VisitsModel.kt
@@ -1,11 +1,11 @@
 package org.wordpress.android.fluxc.model.stats
 
 data class VisitsModel(
-        val period: String,
-        val views: Int,
-        val visitors: Int,
-        val likes: Int,
-        val reblogs: Int,
-        val comments: Int,
-        val posts: Int
+    val period: String,
+    val views: Int,
+    val visitors: Int,
+    val likes: Int,
+    val reblogs: Int,
+    val comments: Int,
+    val posts: Int
 )
