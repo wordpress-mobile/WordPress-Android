87ef24cb4ee2fe10121097f75edf081bc8ceffc7
Oguz Kocer <oguz.kocer@automattic.com>
2018-11-02 19:22:19 -0400

    Simplify VerticalModel and VerticalSegmentModel

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/vertical/VerticalSegmentModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/vertical/VerticalSegmentModel.kt
@@ -1,28 +1,3 @@
 package org.wordpress.android.fluxc.model.vertical
 
-import com.yarolegovich.wellsql.core.Identifiable
-import com.yarolegovich.wellsql.core.annotation.Column
-import com.yarolegovich.wellsql.core.annotation.PrimaryKey
-import com.yarolegovich.wellsql.core.annotation.Table
-
-@Table
-// TODO: Do we need any constraints?
-class VerticalSegmentModel(@PrimaryKey @Column private var id: Int = 0) : Identifiable {
-    // TODO: Delete this constructor if its not used after implementing `VerticalRestClient`
-    constructor(title: String, subtitle: String, iconUrl: String, segmentId: Long): this() {
-        this.title = title
-        this.subtitle = subtitle
-        this.iconUrl = iconUrl
-        this.segmentId = segmentId
-    }
-    @Column var title: String? = null
-    @Column var subtitle: String? = null
-    @Column var iconUrl: String? = null
-    @Column var segmentId: Long? = null
-
-    override fun getId(): Int = id
-
-    override fun setId(id: Int) {
-        this.id = id
-    }
-}
+data class VerticalSegmentModel(val title: String, val subtitle: String, val iconUrl: String, val segmentId: Long)
