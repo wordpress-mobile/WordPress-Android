1f56cb06e3c6f35f7c4d09fc7224c9172096dd65
Ondrej Ruttkay <ondrej@automattic.com>
2018-11-04 19:14:59 +0100

    Add a default dispatcher to ScopedViewModel

--- WordPress/src/main/java/org/wordpress/android/viewmodel/ScopedViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/ScopedViewModel.kt
@@ -1,33 +1,24 @@
 package org.wordpress.android.viewmodel
 
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.Dispatchers
 import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.HandlerDispatcher
 import kotlinx.coroutines.experimental.android.Main
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.withContext
 import kotlin.coroutines.experimental.CoroutineContext
 
-abstract class ScopedViewModel : ViewModel(), CoroutineScope {
+abstract class ScopedViewModel(
+    private val defaultDispatcher: HandlerDispatcher = Dispatchers.Main
+) : ViewModel(), CoroutineScope {
     protected var job: Job = Job()
+
     override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job
+        get() = defaultDispatcher + job
 
     override fun onCleared() {
         super.onCleared()
+
         job.cancel()
-    }
-
-    protected suspend fun <T> MutableLiveData<T>.setOnUi(value: T) = withContext(coroutineContext) {
-        setValue(value)
-    }
-
-    private fun <T> MutableLiveData<T>.postOnUi(value: T) {
-        val liveData = this
-        launch {
-            liveData.value = value
-        }
     }
 }
