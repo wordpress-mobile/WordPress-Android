abc2ad56101113e191baba850cb14921e747ee13
Oguz Kocer <oguz.kocer@automattic.com>
2018-11-08 16:33:41 -0500

    Adds the ability to hide items in ListStore

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/list/PagedListFactory.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/list/PagedListFactory.kt
@@ -11,12 +11,19 @@ class PagedListFactory<T, R>(
     private val dataStore: ListDataStoreInterface<T>,
     private val listDescriptor: ListDescriptor,
     private val getList: (ListDescriptor) -> List<Long>,
-    private val transform: (T) -> R
+    private val transform: (T) -> R,
+    private val getRemoteItemIdsToHide: (ListDescriptor) -> List<Long>
 ) : DataSource.Factory<Int, PagedListItemType<R>>() {
     private var currentSource: PagedListPositionalDataSource<T, R>? = null
 
     override fun create(): DataSource<Int, PagedListItemType<R>> {
-        val source = PagedListPositionalDataSource(dataStore, listDescriptor, getList, transform)
+        val source = PagedListPositionalDataSource(
+                dataStore = dataStore,
+                listDescriptor = listDescriptor,
+                getList = getList,
+                transform = transform,
+                getRemoteItemIdsToHide = getRemoteItemIdsToHide
+        )
         currentSource = source
         return source
     }
@@ -30,10 +37,14 @@ private class PagedListPositionalDataSource<T, R>(
     private val dataStore: ListDataStoreInterface<T>,
     private val listDescriptor: ListDescriptor,
     getList: (ListDescriptor) -> List<Long>,
-    private val transform: (T) -> R
+    private val transform: (T) -> R,
+    getRemoteItemIdsToHide: (ListDescriptor) -> List<Long>
 ) : PositionalDataSource<PagedListItemType<R>>(), LifecycleObserver {
     private val localItems = dataStore.localItems(listDescriptor)
-    private val remoteItemIds: List<Long> = getList(listDescriptor)
+    private val remoteItemIdsToHide = getRemoteItemIdsToHide(listDescriptor)
+    private val remoteItemIds: List<Long> = getList(listDescriptor).filter {
+        !remoteItemIdsToHide.contains(it)
+    }
     private val totalSize: Int = localItems.size + remoteItemIds.size
 
     override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<PagedListItemType<R>>) {
