2e996031c1ef3b6a6aaaaa9519a272891cce62c4
vojtasmrcek <planarvoid@gmail.com>
2018-11-12 10:09:58 +0100

    Simplify domain model

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/TagsModel.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/TagsModel.kt
@@ -1,14 +1,7 @@
 package org.wordpress.android.fluxc.model.stats
 
 data class TagsModel(val tags: List<TagModel>) {
-    sealed class TagModel {
-        data class SingleTag(
-            val name: String,
-            val type: String,
-            val link: String,
-            val views: Long?
-        ) : TagModel()
-
-        data class Category(val tags: List<SingleTag>, val views: Long) : TagModel()
+    class TagModel(val items: List<Item>, val views: Long) {
+        data class Item(val name: String, val type: String, val link: String)
     }
 }
