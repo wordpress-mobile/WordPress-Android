f6ed8833d3827bae3a6fbcaad2e6efa6eb9b181e
Ondrej Ruttkay <ondrej@automattic.com>
2018-11-15 18:32:14 +0100

    Display data from the model

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/InsightsMostPopularUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/InsightsMostPopularUseCase.kt
@@ -7,7 +7,11 @@ import org.wordpress.android.fluxc.store.InsightsStore
 import org.wordpress.android.ui.stats.refresh.BlockListItem.Empty
 import org.wordpress.android.ui.stats.refresh.BlockListItem.ListItem
 import org.wordpress.android.ui.stats.refresh.BlockListItem.Title
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 class InsightsMostPopularUseCase
 @Inject constructor(private val insightsStore: InsightsStore) {
@@ -34,15 +38,48 @@ class InsightsMostPopularUseCase
         if (model == null) {
             items.add(Empty)
         } else {
-//            items.add(
-//                    ListItem(
-//                    )
-//            )
-//            items.add(
-//                    ListItem(
-//                    )
-//            )
+            items.add(
+                    ListItem(
+                            getDay(model.highestDayOfWeek),
+                            "${model.highestDayPercent.roundToInt()}%",
+                            true
+                    )
+            )
+            items.add(
+                    ListItem(
+                            getHour(model.highestHour),
+                            "${model.highestHourPercent.roundToInt()}%",
+                            true
+                    )
+            )
         }
         return ListInsightItem(items)
+    }
+
+    private fun getDay(dayOfTheWeek: Int): String {
+        val c = Calendar.getInstance()
+        c.firstDayOfWeek = Calendar.MONDAY
+        c.timeInMillis = System.currentTimeMillis()
+        when (dayOfTheWeek) {
+            0 -> c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+            1 -> c.set(Calendar.DAY_OF_WEEK, Calendar.TUESDAY)
+            2 -> c.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY)
+            3 -> c.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY)
+            4 -> c.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY)
+            5 -> c.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY)
+            6 -> c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY)
+        }
+
+        val formatter = SimpleDateFormat("EEEE", Locale.getDefault())
+        return formatter.format(c.time)
+    }
+
+    private fun getHour(hour: Int): String {
+        val c = Calendar.getInstance()
+        c.set(Calendar.HOUR_OF_DAY, hour)
+        c.set(Calendar.MINUTE, 0)
+
+        val formatter = SimpleDateFormat("HH", Locale.getDefault())
+        return formatter.format(c.time)
     }
 }
