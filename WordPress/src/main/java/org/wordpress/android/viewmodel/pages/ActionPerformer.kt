5439fec6fb082a4f19db4fe5e206b9ccc6c66b6a
Ondrej Ruttkay <ondrej@automattic.com>
2018-11-15 15:25:46 +0100

    Optimize imports

--- WordPress/src/main/java/org/wordpress/android/viewmodel/pages/ActionPerformer.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/pages/ActionPerformer.kt
@@ -4,15 +4,15 @@ import kotlinx.coroutines.experimental.launch
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.store.PostStore.OnPostUploaded
-import javax.inject.Inject
 import org.wordpress.android.fluxc.model.CauseOfOnPostChanged
 import org.wordpress.android.fluxc.store.PostStore.OnPostChanged
+import org.wordpress.android.fluxc.store.PostStore.OnPostUploaded
 import org.wordpress.android.util.coroutines.suspendCoroutineWithTimeout
 import org.wordpress.android.viewmodel.pages.ActionPerformer.PageAction.EventType
 import org.wordpress.android.viewmodel.pages.ActionPerformer.PageAction.EventType.DELETE
 import org.wordpress.android.viewmodel.pages.ActionPerformer.PageAction.EventType.UPDATE
 import org.wordpress.android.viewmodel.pages.ActionPerformer.PageAction.EventType.UPLOAD
+import javax.inject.Inject
 import kotlin.coroutines.experimental.Continuation
 
 class ActionPerformer
