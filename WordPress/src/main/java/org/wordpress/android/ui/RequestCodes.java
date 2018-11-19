7f4f0f991fcdf47c57250ff4f28ccd6ad6b31209
Ondrej Ruttkay <ondrej@automattic.com>
2018-11-19 11:10:20 +0100

    Change duplicate request number

--- WordPress/src/main/java/org/wordpress/android/ui/RequestCodes.java
+++ WordPress/src/main/java/org/wordpress/android/ui/RequestCodes.java
@@ -24,8 +24,8 @@ public class RequestCodes {
     public static final int NOTIFICATION_SETTINGS = 1600;
     public static final int ACTIVITY_LOG_DETAIL = 1700;
     public static final int PAGE_PARENT = 1800;
-    public static final int QUICK_START = 1800;
     public static final int HISTORY_DETAIL = 1900;
+    public static final int QUICK_START = 2000;
 
     // Media
     public static final int PICTURE_LIBRARY = 2000;
