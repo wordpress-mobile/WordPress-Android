78ab27c0238e17cb0464bd1761d1a65ce9cdd334
Oguz Kocer <oguz.kocer@automattic.com>
2018-11-21 17:25:45 -0500

    Fetch segment prompt action added

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/VerticalAction.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/VerticalAction.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.fluxc.action
 import org.wordpress.android.fluxc.annotations.Action
 import org.wordpress.android.fluxc.annotations.ActionEnum
 import org.wordpress.android.fluxc.annotations.action.IAction
+import org.wordpress.android.fluxc.store.VerticalStore.FetchSegmentPromptPayload
 import org.wordpress.android.fluxc.store.VerticalStore.FetchVerticalsPayload
 
 @ActionEnum
@@ -10,5 +11,7 @@ enum class VerticalAction : IAction {
     @Action
     FETCH_SEGMENTS,
     @Action(payloadType = FetchVerticalsPayload::class)
-    FETCH_VERTICALS
+    FETCH_VERTICALS,
+    @Action(payloadType = FetchSegmentPromptPayload::class)
+    FETCH_SEGMENT_PROMPT
 }
