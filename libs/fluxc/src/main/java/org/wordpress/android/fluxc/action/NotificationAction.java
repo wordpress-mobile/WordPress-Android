0562a20ceabe9f22e787e7919d9b1586d5a496e5
Amanda Riu <amanda.riu@automattic.com>
2018-11-21 20:38:26 -0500

    Add notification actions for fetching notifications

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/NotificationAction.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/NotificationAction.java
@@ -3,6 +3,8 @@ package org.wordpress.android.fluxc.action;
 import org.wordpress.android.fluxc.annotations.Action;
 import org.wordpress.android.fluxc.annotations.ActionEnum;
 import org.wordpress.android.fluxc.annotations.action.IAction;
+import org.wordpress.android.fluxc.store.NotificationStore.FetchNotificationsPayload;
+import org.wordpress.android.fluxc.store.NotificationStore.FetchNotificationsResponsePayload;
 import org.wordpress.android.fluxc.store.NotificationStore.RegisterDevicePayload;
 import org.wordpress.android.fluxc.store.NotificationStore.RegisterDeviceResponsePayload;
 import org.wordpress.android.fluxc.store.NotificationStore.UnregisterDeviceResponsePayload;
@@ -14,12 +16,16 @@ public enum NotificationAction implements IAction {
     REGISTER_DEVICE, // Register device for push notifications with WordPress.com
     @Action
     UNREGISTER_DEVICE, // Unregister device for push notifications with WordPress.com
+    @Action(payloadType = FetchNotificationsPayload.class)
+    FETCH_NOTES, // Fetch notifications
 
     // Remote responses
     @Action(payloadType = RegisterDeviceResponsePayload.class)
     REGISTERED_DEVICE, // Response to device registration received
     @Action(payloadType = UnregisterDeviceResponsePayload.class)
-    UNREGISTERED_DEVICE // Response to device unregistration
+    UNREGISTERED_DEVICE, // Response to device unregistration
+    @Action(payloadType = FetchNotificationsResponsePayload.class)
+    FETCHED_NOTES // Response to fetching notifications
 
     // Local actions
 }
