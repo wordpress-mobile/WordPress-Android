62b47c804398d8b0dfa6003c7d0177706b60cbac
Amanda Riu <amanda.riu@automattic.com>
2018-11-21 20:40:36 -0500

    Update notification store to handling new fetch notification actions

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/NotificationStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/NotificationStore.kt
@@ -1,18 +1,23 @@
 package org.wordpress.android.fluxc.store
 
 import android.content.Context
+import com.yarolegovich.wellsql.SelectQuery
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.Payload
 import org.wordpress.android.fluxc.action.NotificationAction
+import org.wordpress.android.fluxc.action.NotificationAction.FETCH_NOTES
 import org.wordpress.android.fluxc.annotations.action.Action
+import org.wordpress.android.fluxc.model.NotificationModel
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError
 import org.wordpress.android.fluxc.network.rest.wpcom.notifications.NotificationRestClient
+import org.wordpress.android.fluxc.persistence.NotificationSqlUtils
 import org.wordpress.android.fluxc.utils.PreferenceUtils
 import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.AppLog.T
+import java.util.Date
 import java.util.Locale
 import java.util.UUID
 import javax.inject.Inject
@@ -23,7 +28,8 @@ class NotificationStore @Inject
 constructor(
     dispatcher: Dispatcher,
     private val context: Context,
-    private val notificationRestClient: NotificationRestClient
+    private val notificationRestClient: NotificationRestClient,
+    private val notificationSqlUtils: NotificationSqlUtils
 ) : Store(dispatcher) {
     companion object {
         const val WPCOM_PUSH_DEVICE_UUID = "NOTIFICATIONS_UUID_PREF_KEY"
@@ -75,27 +81,80 @@ constructor(
 
     enum class DeviceUnregistrationErrorType { GENERIC_ERROR; }
 
+    class FetchNotificationsPayload(
+        val site: SiteModel
+    ) : Payload<BaseNetworkError>()
+
+    class FetchNotificationsResponsePayload(
+        val site: SiteModel,
+        val notifs: List<NotificationModel> = emptyList(),
+        val lastSeen: Date? = null
+    ) : Payload<NotificationError>() {
+        constructor(error: NotificationError, site: SiteModel) : this(site) { this.error = error }
+    }
+
+    class NotificationError(val type: NotificationErrorType, val message: String = "") : OnChangedError
+
+    enum class NotificationErrorType {
+        BAD_REQUEST,
+        NOT_FOUND,
+        AUTHORIZATION_REQUIRED,
+        GENERIC_ERROR;
+
+        companion object {
+            private val reverseMap = NotificationErrorType.values().associateBy(NotificationErrorType::name)
+            fun fromString(type: String) = reverseMap[type.toUpperCase(Locale.US)] ?: GENERIC_ERROR
+        }
+    }
+
     // OnChanged events
     class OnDeviceRegistered(val deviceId: String?) : OnChanged<DeviceRegistrationError>()
 
     class OnDeviceUnregistered : OnChanged<DeviceUnregistrationError>()
 
+    class OnNotificationChanged(var rowsAffected: Int): OnChanged<NotificationError>() {
+        var causeOfChange: NotificationAction? = null
+    }
+
     @Subscribe(threadMode = ThreadMode.ASYNC)
     override fun onAction(action: Action<*>) {
         val actionType = action.type as? NotificationAction ?: return
         when (actionType) {
+            // remote actions
             NotificationAction.REGISTER_DEVICE -> registerDevice(action.payload as RegisterDevicePayload)
             NotificationAction.UNREGISTER_DEVICE -> unregisterDevice()
+            NotificationAction.FETCH_NOTES -> fetchNotifications(action.payload as FetchNotificationsPayload)
+
+            // remote responses
             NotificationAction.REGISTERED_DEVICE ->
                 handleRegisteredDevice(action.payload as RegisterDeviceResponsePayload)
             NotificationAction.UNREGISTERED_DEVICE ->
                 handleUnregisteredDevice(action.payload as UnregisterDeviceResponsePayload)
+            NotificationAction.FETCHED_NOTES ->
+                handleFetchNotificationsCompleted(action.payload as FetchNotificationsResponsePayload)
         }
     }
 
     override fun onRegister() {
         AppLog.d(T.API, NotificationStore::class.java.simpleName + " onRegister")
     }
+
+    /**
+     * Fetch all notifications for the given site.
+     *
+     * Filtering. Filtering is done by fetching all records that match the strings in [filterByType] OR
+     * [filterBySubtype].
+     *
+     * @param site The [SiteModel] to fetch notifications for
+     * @param filterByType Optional. A list of notification type strings to filter by
+     * @param filterBySubtype Optional. A list of notification subtype strings to filter by
+     */
+    fun getNotificationsForSite(
+        site: SiteModel,
+        filterByType: List<String>,
+        filterBySubtype: List<String>
+    ): List<NotificationModel> =
+            notificationSqlUtils.getNotificationsForSite(site, SelectQuery.ORDER_DESCENDING, filterByType, filterBySubtype)
 
     private fun registerDevice(payload: RegisterDevicePayload) {
         val uuid = preferences.getString(WPCOM_PUSH_DEVICE_UUID, null) ?: generateAndStoreUUID()
@@ -156,5 +215,28 @@ constructor(
         return UUID.randomUUID().toString().also {
             preferences.edit().putString(WPCOM_PUSH_DEVICE_UUID, it).apply()
         }
+    }
+
+    private fun fetchNotifications(payload: FetchNotificationsPayload) {
+        notificationRestClient.fetchNotifications(payload.site)
+    }
+
+    private fun handleFetchNotificationsCompleted(payload: FetchNotificationsResponsePayload) {
+        val onNotificationChanged = if (payload.isError) {
+            // Notification error
+            OnNotificationChanged(0).also { it.error = payload.error }
+        } else {
+            // Clear out existing notifications on fetch.
+            notificationSqlUtils.deleteNotificationsForSite(payload.site)
+
+            // Save notifications to the database
+            val rowsAffected = notificationSqlUtils.insertOrUpdateNotifications(payload.site, payload.notifs)
+
+            OnNotificationChanged(rowsAffected)
+        }.apply {
+            causeOfChange = FETCH_NOTES
+        }
+
+        emitChange(onNotificationChanged)
     }
 }
