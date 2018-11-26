aa17347f6554d4230965880d2674e34c9d84389e
Ondrej Ruttkay <ondrej@automattic.com>
2018-11-26 15:54:09 +0100

    Add a hasMore parameter to PostAndPageViewsModel

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/time/PostAndPageViewsModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/time/PostAndPageViewsModel.kt
@@ -1,6 +1,6 @@
 package org.wordpress.android.fluxc.model.stats.time
 
-data class PostAndPageViewsModel(val views: List<ViewsModel>) {
+data class PostAndPageViewsModel(val views: List<ViewsModel>, val hasMore: Boolean) {
     data class ViewsModel(
         val title: String,
         val views: Int,
