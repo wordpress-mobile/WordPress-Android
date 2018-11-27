ad64a930bc6a3f480e8dae5f9b5d73ab6a4c73c7
Oguz Kocer <oguz.kocer@automattic.com>
2018-11-27 18:53:30 -0500

    Site info page connected to its view model

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/NewSiteCreationSiteInfoFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/NewSiteCreationSiteInfoFragment.kt
@@ -1,10 +1,16 @@
 package org.wordpress.android.ui.sitecreation
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.annotation.LayoutRes
+import android.support.design.widget.TextInputEditText
 import android.support.v4.app.FragmentActivity
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.AppCompatButton
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.ViewGroup
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
@@ -16,6 +22,10 @@ class NewSiteCreationSiteInfoFragment : NewSiteCreationBaseFormFragment<NewSiteC
 
     private lateinit var nonNullActivity: FragmentActivity
     private lateinit var viewModel: NewSiteCreationSiteInfoViewModel
+
+    private lateinit var skipNextButton: AppCompatButton
+    private lateinit var businessNameEditText: TextInputEditText
+    private lateinit var tagLineEditText: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +40,49 @@ class NewSiteCreationSiteInfoFragment : NewSiteCreationBaseFormFragment<NewSiteC
 
     override fun setupContent(rootView: ViewGroup) {
         // TODO: Get the title from the main VM
+        skipNextButton = rootView.findViewById(R.id.site_info_skip_or_next_button)
+        businessNameEditText = rootView.findViewById(R.id.site_info_business_name)
+        tagLineEditText = rootView.findViewById(R.id.site_info_tag_line)
         initViewModel()
+        initTextWatchers()
+    }
+
+    private fun initTextWatchers() {
+        val addTextWatcher = { editText: TextInputEditText, onTextChanged: (String) -> Unit ->
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    onTextChanged(s?.toString() ?: "")
+                }
+            })
+        }
+        addTextWatcher(businessNameEditText) { viewModel.updateBusinessName(it) }
+        addTextWatcher(tagLineEditText) { viewModel.updateTagLine(it) }
     }
 
     private fun initViewModel() {
         viewModel = ViewModelProviders.of(nonNullActivity, viewModelFactory)
                 .get(NewSiteCreationSiteInfoViewModel::class.java)
+        viewModel.uiState.observe(this, Observer {
+            it?.let { state ->
+                val updateEditTextIfDifferent = { editText: TextInputEditText, value: String ->
+                    // This let's us avoid triggering the TextWatcher unnecessarily
+                    if (editText.text.toString() != value) {
+                        editText.setText(value)
+                    }
+                }
+                updateEditTextIfDifferent(businessNameEditText, state.businessName)
+                updateEditTextIfDifferent(tagLineEditText, state.tagLine)
+                state.skipButtonState.let { buttonState ->
+                    skipNextButton.apply {
+                        setText(buttonState.text)
+                        setTextColor(ContextCompat.getColor(nonNullActivity, buttonState.textColor))
+                        setBackgroundColor(ContextCompat.getColor(nonNullActivity, buttonState.backgroundColor))
+                    }
+                }
+            }
+        })
     }
 
     override fun onHelp() {
--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/NewSiteCreationSiteInfoViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/NewSiteCreationSiteInfoViewModel.kt
@@ -3,8 +3,11 @@ package org.wordpress.android.ui.sitecreation.verticals
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.support.annotation.ColorRes
 import android.support.annotation.StringRes
 import org.wordpress.android.R
+import org.wordpress.android.ui.sitecreation.verticals.NewSiteCreationSiteInfoViewModel.SiteInfoUiState.SkipNextButtonState.NEXT
+import org.wordpress.android.ui.sitecreation.verticals.NewSiteCreationSiteInfoViewModel.SiteInfoUiState.SkipNextButtonState.SKIP
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
@@ -22,19 +25,38 @@ class NewSiteCreationSiteInfoViewModel @Inject constructor() : ViewModel() {
     val uiState: LiveData<SiteInfoUiState> = _uiState
 
     fun updateBusinessName(businessName: String) {
-        currentUiState = currentUiState.copy(businessName = businessName)
+        if (currentUiState.businessName != businessName) {
+            currentUiState = currentUiState.copy(businessName = businessName)
+        }
     }
 
     fun updateTagLine(tagLine: String) {
-        currentUiState = currentUiState.copy(tagLine = tagLine)
+        if (currentUiState.tagLine != tagLine) {
+            currentUiState = currentUiState.copy(tagLine = tagLine)
+        }
     }
 
     data class SiteInfoUiState(
         val businessName: String,
         val tagLine: String
     ) {
-        @StringRes val skipNextButtonTitleRes: Int = if (businessName.isEmpty() && tagLine.isEmpty()) {
-            R.string.new_site_creation_button_skip
-        } else R.string.new_site_creation_button_next
+        enum class SkipNextButtonState(
+            @StringRes val text: Int,
+            @ColorRes val textColor: Int,
+            @ColorRes val backgroundColor: Int
+        ) {
+            SKIP(
+                    text = R.string.new_site_creation_button_skip,
+                    textColor = R.color.wp_grey_dark,
+                    backgroundColor = R.color.white
+            ),
+            NEXT(
+                    text = R.string.new_site_creation_button_next,
+                    textColor = R.color.white,
+                    backgroundColor = R.color.wp_blue_medium
+            )
+        }
+
+        val skipButtonState = if (businessName.isEmpty() && tagLine.isEmpty()) SKIP else NEXT
     }
 }
