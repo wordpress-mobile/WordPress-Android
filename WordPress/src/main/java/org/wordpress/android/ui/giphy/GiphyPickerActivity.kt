dbd5b7b1b066aa79d081c92064462502b9f325be
Shiki <jayson@basanes.net>
2018-12-02 06:48:07 -0700

    Giphy Picker: Preview selected images

--- WordPress/src/main/java/org/wordpress/android/ui/giphy/GiphyPickerActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/giphy/GiphyPickerActivity.kt
@@ -14,9 +14,11 @@ import kotlinx.android.synthetic.main.media_picker_activity.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.ui.giphy.GiphyMediaViewHolder.ThumbnailViewDimensions
+import org.wordpress.android.ui.media.MediaPreviewActivity
 import org.wordpress.android.util.AniUtils
 import org.wordpress.android.util.DisplayUtils
 import org.wordpress.android.util.image.ImageManager
+import org.wordpress.android.viewmodel.giphy.GiphyMediaViewModel
 import org.wordpress.android.viewmodel.giphy.GiphyPickerViewModel
 import javax.inject.Inject
 
@@ -54,6 +56,7 @@ class GiphyPickerActivity : AppCompatActivity() {
         initializeRecyclerView()
         initializeSearchView()
         initializeSelectionBar()
+        initializePreviewHandlers()
     }
 
     /**
@@ -146,6 +149,30 @@ class GiphyPickerActivity : AppCompatActivity() {
                 text_add.text = getString(R.string.add_count, selectedCount)
             }
         })
+    }
+
+    /**
+     * Set up listener for the Preview button
+     */
+    private fun initializePreviewHandlers() {
+        text_preview.setOnClickListener {
+            val mediaViewModels = viewModel.selectedMediaViewModelList.value?.values?.toList()
+            if (mediaViewModels != null && mediaViewModels.isNotEmpty()) {
+                showPreview(mediaViewModels)
+            }
+        }
+    }
+
+    /**
+     * Show the images of the given [mediaViewModels] in [MediaPreviewActivity]
+     *
+     * @param mediaViewModels A non-empty list
+     */
+    private fun showPreview(mediaViewModels: List<GiphyMediaViewModel>) {
+        check(mediaViewModels.isNotEmpty())
+
+        val uris = mediaViewModels.map { it.previewImageUri.toString() }
+        MediaPreviewActivity.showPreview(this, null, ArrayList(uris), uris.first())
     }
 
     /**
