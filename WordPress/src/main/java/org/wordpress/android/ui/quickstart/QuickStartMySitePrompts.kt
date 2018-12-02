e1b9add8fbc8d449b6bd949fa047e2c05d4310f6
Klymentiy haykov <forsver@gmail.com>
2018-12-02 08:02:31 +0200

    Updated prompts.

--- WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartMySitePrompts.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartMySitePrompts.kt
@@ -2,6 +2,8 @@ package org.wordpress.android.ui.quickstart
 
 import org.wordpress.android.R
 import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartTask
+import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartTask.FOLLOW_SITE
+import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartTask.PUBLISH_POST
 
 /**
  * Static data about initial tutorial prompts you see when selecting one from Quick Start task list
@@ -22,7 +24,8 @@ enum class QuickStartMySitePrompts constructor(
             R.string.quick_start_dialog_view_site_message_short,
             R.drawable.ic_globe_grey_24dp,
             R.string.quick_start_dialog_view_site_title,
-            R.string.quick_start_dialog_view_site_message),
+            R.string.quick_start_dialog_view_site_message
+    ),
     CHOSE_THEME_TUTORIAL(
             QuickStartTask.CHOOSE_THEME,
             R.id.my_site_scroll_view_root,
@@ -30,7 +33,8 @@ enum class QuickStartMySitePrompts constructor(
             R.string.quick_start_dialog_choose_theme_message_short,
             R.drawable.ic_themes_grey_24dp,
             R.string.quick_start_dialog_choose_theme_title,
-            R.string.quick_start_dialog_choose_theme_message),
+            R.string.quick_start_dialog_choose_theme_message
+    ),
     CUSTOMIZE_SITE_TUTORIAL(
             QuickStartTask.CUSTOMIZE_SITE,
             R.id.my_site_scroll_view_root,
@@ -38,15 +42,17 @@ enum class QuickStartMySitePrompts constructor(
             R.string.quick_start_dialog_customize_site_message_short_themes,
             R.drawable.ic_themes_grey_24dp,
             R.string.quick_start_dialog_customize_site_title,
-            R.string.quick_start_dialog_customize_site_message),
+            R.string.quick_start_dialog_customize_site_message
+    ),
     SHARE_SITE_TUTORIAL(
-            QuickStartTask.SHARE_SITE,
+            QuickStartTask.ENABLE_POST_SHARING,
             R.id.my_site_scroll_view_root,
             R.id.row_sharing,
             R.string.quick_start_dialog_share_site_message_short_sharing,
             R.drawable.ic_share_white_24dp,
             R.string.quick_start_dialog_share_site_title,
-            R.string.quick_start_dialog_share_site_message),
+            R.string.quick_start_dialog_share_site_message
+    ),
     PUBLISH_POST_TUTORIAL(
             QuickStartTask.PUBLISH_POST,
             R.id.root_view_main,
@@ -54,7 +60,8 @@ enum class QuickStartMySitePrompts constructor(
             R.string.quick_start_dialog_publish_post_message_short,
             R.drawable.ic_create_white_24dp,
             R.string.quick_start_dialog_publish_post_title,
-            R.string.quick_start_dialog_publish_post_message),
+            R.string.quick_start_dialog_publish_post_message
+    ),
     FOLLOW_SITES_TUTORIAL(
             QuickStartTask.FOLLOW_SITE,
             R.id.root_view_main,
@@ -62,7 +69,44 @@ enum class QuickStartMySitePrompts constructor(
             R.string.quick_start_dialog_follow_sites_message_short_reader,
             R.drawable.ic_reader_white_32dp,
             R.string.quick_start_dialog_follow_sites_title,
-            R.string.quick_start_dialog_follow_sites_message);
+            R.string.quick_start_dialog_follow_sites_message
+    ),
+    UPLOAD_SITE_ICON(
+            QuickStartTask.UPLOAD_SITE_ICON,
+            R.id.my_site_scroll_view_root,
+            R.id.my_site_blavatar,
+            R.string.quick_start_dialog_upload_site_icon_message_short,
+            -1,
+            -1,
+            -1
+    ),
+    CREATE_NEW_PAGE(
+            QuickStartTask.CREATE_NEW_PAGE,
+            R.id.my_site_scroll_view_root,
+            R.id.row_pages,
+            R.string.quick_start_dialog_create_new_page_message_short,
+            -1,
+            -1,
+            -1
+    ),
+    CHECK_STATS(
+            QuickStartTask.CHECK_STATS,
+            R.id.my_site_scroll_view_root,
+            R.id.row_stats,
+            R.string.quick_start_dialog_check_stats_message_short,
+            R.drawable.ic_stats_alt_grey_24dp,
+            -1,
+            -1
+    ),
+    EXPLORE_PLANS(
+            QuickStartTask.EXPLORE_PLANS,
+            R.id.my_site_scroll_view_root,
+            R.id.row_plan,
+            R.string.quick_start_dialog_explore_plans_message_short,
+            R.drawable.ic_plans_grey_24dp,
+            -1,
+            -1
+    );
 
     companion object {
         const val KEY = "my_site_tutorial_prompts"
@@ -77,7 +121,7 @@ enum class QuickStartMySitePrompts constructor(
 
         @JvmStatic
         fun isTargetingBottomNavBar(task: QuickStartTask): Boolean {
-            return task == QuickStartTask.FOLLOW_SITE || task == QuickStartTask.PUBLISH_POST
+            return task == FOLLOW_SITE || task == PUBLISH_POST
         }
     }
 }
