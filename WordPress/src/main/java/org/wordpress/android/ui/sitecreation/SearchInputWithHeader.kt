2f865e2ffcc881634d0ae743149e7609a207749b
Oguz Kocer <oguz.kocer@automattic.com>
2018-12-07 20:34:44 -0500

    Verticals fragment now uses the SearchInputWithHeader

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/SearchInputWithHeader.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/SearchInputWithHeader.kt
@@ -2,19 +2,21 @@ package org.wordpress.android.ui.sitecreation
 
 import android.support.v4.content.ContextCompat
 import android.support.v7.content.res.AppCompatResources
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
 import org.wordpress.android.R
 
-class SearchInputWithHeader(rootView: View, onClear: () -> Unit) {
+class SearchInputWithHeader(rootView: View, onClear: () -> Unit, onTextChanged: (String) -> Unit) {
     private val headerLayout = rootView.findViewById<ViewGroup>(R.id.header_layout)
     private val headerTitle = rootView.findViewById<TextView>(R.id.title)
     private val headerSubtitle = rootView.findViewById<TextView>(R.id.subtitle)
     private val searchInput = rootView.findViewById<EditText>(R.id.input)
-    private val progressBar = rootView.findViewById<TextView>(R.id.progress_bar)
-    private val clearAllButton = rootView.findViewById<TextView>(R.id.clear_all_btn)
+    private val progressBar = rootView.findViewById<View>(R.id.progress_bar)
+    private val clearAllButton = rootView.findViewById<View>(R.id.clear_all_btn)
 
     init {
         val context = rootView.context
@@ -28,9 +30,24 @@ class SearchInputWithHeader(rootView: View, onClear: () -> Unit) {
         clearAllLayout.setOnClickListener {
             onClear()
         }
+
+        searchInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                onTextChanged(s?.toString() ?: "")
+            }
+        })
     }
 
-    private fun updateHeader(uiState: SiteCreationHeaderUiState?) {
+    fun setInputText(text: String) {
+        // If the text hasn't changed avoid triggering the text watcher
+        if (searchInput.text.toString() != text) {
+            searchInput.setText(text)
+        }
+    }
+
+    fun updateHeader(uiState: SiteCreationHeaderUiState?) {
         uiState?.let {
             if (headerLayout.visibility == View.VISIBLE) {
                 headerLayout.animate().translationY(-headerLayout.height.toFloat())
@@ -43,7 +60,7 @@ class SearchInputWithHeader(rootView: View, onClear: () -> Unit) {
         } ?: updateVisibility(headerLayout, false)
     }
 
-    private fun updateSearchInput(uiState: SiteCreationSearchInputUiState) {
+    fun updateSearchInput(uiState: SiteCreationSearchInputUiState) {
         searchInput.hint = uiState.hint
         updateVisibility(progressBar, uiState.showProgress)
         updateVisibility(clearAllButton, uiState.showClearButton)
