2df3707bbd337d6d66e4787cad23ec5fcab2f431
Oguz Kocer <oguz.kocer@automattic.com>
2018-12-12 18:34:31 -0500

    Fetch verticals implemented

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/vertical/VerticalModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/vertical/VerticalModel.kt
@@ -1,3 +1,10 @@
 package org.wordpress.android.fluxc.model.vertical
 
-data class VerticalModel(val name: String, val verticalId: String, val isNewUserVertical: Boolean)
+import com.google.gson.annotations.SerializedName
+
+data class VerticalModel(
+    @SerializedName("vertical_name") val name: String,
+    @SerializedName("vertical_id") val verticalId: String,
+    // TODO: What should this be?
+    @SerializedName("TODO") val isNewUserVertical: Boolean
+)
