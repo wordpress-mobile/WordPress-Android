7e532f18d11f9f7edc3574266798760a7b52309b
Oguz Kocer <oguz.kocer@automattic.com>
2018-12-12 18:01:05 -0500

    Implements actual fetch vertical segments rest call

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/vertical/VerticalRestClient.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/vertical/VerticalRestClient.kt
@@ -4,17 +4,24 @@ import android.content.Context
 import com.android.volley.RequestQueue
 import kotlinx.coroutines.experimental.delay
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.endpoint.WPCOMV2
 import org.wordpress.android.fluxc.model.vertical.SegmentPromptModel
 import org.wordpress.android.fluxc.model.vertical.VerticalModel
 import org.wordpress.android.fluxc.model.vertical.VerticalSegmentModel
 import org.wordpress.android.fluxc.network.UserAgent
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder
+import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Error
+import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Success
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
+import org.wordpress.android.fluxc.store.VerticalStore.FetchSegmentsError
 import org.wordpress.android.fluxc.store.VerticalStore.FetchedSegmentPromptPayload
 import org.wordpress.android.fluxc.store.VerticalStore.FetchedSegmentsPayload
 import org.wordpress.android.fluxc.store.VerticalStore.FetchedVerticalsPayload
+import org.wordpress.android.fluxc.store.VerticalStore.VerticalErrorType.GENERIC_ERROR
 import javax.inject.Singleton
+
+class FetchSegmentsResponse: ArrayList<VerticalSegmentModel>()
 
 @Singleton
 class VerticalRestClient
@@ -27,18 +34,12 @@ constructor(
     userAgent: UserAgent
 ) : BaseWPComRestClient(appContext, dispatcher, requestQueue, accessToken, userAgent) {
     suspend fun fetchSegments(): FetchedSegmentsPayload {
-        // TODO: Implement the actual call
-        delay(1000)
-        val segmentList = (1..100L).map {
-            VerticalSegmentModel(
-                    title = "title-$it",
-                    subtitle = "subtitle-$it",
-                    iconUrl = "https://picsum.photos/50",
-                    iconColor = "#0000FF",
-                    segmentId = it
-            )
+        val url = WPCOMV2.segments.url
+        val response = wpComGsonRequestBuilder.syncGetRequest(this, url, emptyMap(), FetchSegmentsResponse::class.java)
+        return when(response) {
+            is Success -> FetchedSegmentsPayload(response.data)
+            is Error -> FetchedSegmentsPayload(FetchSegmentsError(GENERIC_ERROR))
         }
-        return FetchedSegmentsPayload(segmentList)
     }
 
     suspend fun fetchSegmentPrompt(segmentId: Long): FetchedSegmentPromptPayload {
