851f5ad6ae7568affd61d0c01bd04ab183d989ef
vojtasmrcek <planarvoid@gmail.com>
2018-12-14 11:22:03 +0100

    Rename list to search terms

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/time/SearchTermsModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/time/SearchTermsModel.kt
@@ -3,7 +3,7 @@ package org.wordpress.android.fluxc.model.stats.time
 data class SearchTermsModel(
     val otherSearchTerms: Int,
     val totalSearchTerms: Int,
-    val list: List<SearchTerm>,
+    val searchTerms: List<SearchTerm>,
     val hasMore: Boolean
 ) {
     data class SearchTerm(val text: String, val views: Int)
