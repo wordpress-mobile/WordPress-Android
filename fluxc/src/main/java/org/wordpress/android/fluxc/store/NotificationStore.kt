5fac04dc925e3f97fcf2f4b1083fa86d9ce9fef7
AmandaRiu <amanda.riu@automattic.com>
2018-12-15 01:00:04 -0500

    Revert "Woo Notification: Fetch and save a single notification"
    This reverts commit 6259f21f42181237654688145aa647b06227f975.

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/NotificationStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/NotificationStore.kt
@@ -10,7 +10,6 @@ import org.wordpress.android.fluxc.Payload
 import org.wordpress.android.fluxc.action.NotificationAction
 import org.wordpress.android.fluxc.action.NotificationAction.FETCH_NOTIFICATIONS
 import org.wordpress.android.fluxc.action.NotificationAction.MARK_NOTIFICATIONS_SEEN
-import org.wordpress.android.fluxc.action.NotificationAction.UPDATE_NOTIFICATION
 import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.model.notification.NotificationModel
 import org.wordpress.android.fluxc.model.SiteModel
@@ -139,7 +138,6 @@ constructor(
         var causeOfChange: NotificationAction? = null
         var lastSeenTime: Long? = null
         var success: Boolean = true
-        val changedNotificationLocalIds = mutableListOf<Int>()
     }
 
     @Subscribe(threadMode = ThreadMode.ASYNC)
