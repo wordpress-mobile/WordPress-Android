66d15c2f8cebd20947ce6c0790a8dcf988d1bec3
Oguz Kocer <oguz.kocer@automattic.com>
2018-12-15 00:57:16 -0500

    Enable new site creation flow for DEBUG builds

--- WordPress/src/main/java/org/wordpress/android/ui/ActivityLauncher.java
+++ WordPress/src/main/java/org/wordpress/android/ui/ActivityLauncher.java
@@ -535,7 +535,7 @@ public class ActivityLauncher {
 
     public static void newBlogForResult(Activity activity) {
         Intent intent = new Intent(activity,
-                BuildConfig.NEW_SITE_CREATION_ENABLED ? NewSiteCreationActivity.class : SiteCreationActivity.class);
+                BuildConfig.DEBUG ? NewSiteCreationActivity.class : SiteCreationActivity.class);
         activity.startActivityForResult(intent, RequestCodes.CREATE_SITE);
     }
 
