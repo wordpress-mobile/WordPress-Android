de75eb3576eb6d44a0cab295c29e631306c1f920
AmandaRiu <amanda.riu@automattic.com>
2018-12-17 12:35:54 -0500

    Remove unused class

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/notifications/NotificationReadApiResponse.kt
+++
@@ -1,5 +0,0 @@
-package org.wordpress.android.fluxc.network.rest.wpcom.notifications
-
-import org.wordpress.android.fluxc.network.Response
-
-class NotificationReadApiResponse(val success: Boolean) : Response
