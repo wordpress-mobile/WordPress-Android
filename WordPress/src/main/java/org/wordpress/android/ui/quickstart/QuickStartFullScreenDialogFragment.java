972b5625601a0a09d79b1bc7a8804feffd3e82b1
Tyler Heck <theck13@users.noreply.github.com>
2018-12-22 09:10:35 -0500

    Add disabling default item animator for quick start dialog layout list view

--- WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartFullScreenDialogFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartFullScreenDialogFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -95,6 +96,8 @@ public class QuickStartFullScreenDialogFragment extends Fragment implements Full
         mQuickStartAdapter.setOnTaskTappedListener(QuickStartFullScreenDialogFragment.this);
         list.setLayoutManager(new LinearLayoutManager(requireContext()));
         list.setAdapter(mQuickStartAdapter);
+        // Disable default change animations to avoid blinking effect when adapter data is changed.
+        ((DefaultItemAnimator) list.getItemAnimator()).setSupportsChangeAnimations(false);
 
         return rootView;
     }
