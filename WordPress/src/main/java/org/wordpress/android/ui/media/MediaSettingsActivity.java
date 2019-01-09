60cf284b888d6a1272933bc3197ff212109b29ff
Danilo Ercoli <ercoli@gmail.com>
2019-01-09 11:01:59 +0100

    Make sure to properly check the Status of the request on completion

--- WordPress/src/main/java/org/wordpress/android/ui/media/MediaSettingsActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/MediaSettingsActivity.java
@@ -852,7 +852,8 @@ public class MediaSettingsActivity extends AppCompatActivity
                 if (cursor != null && cursor.moveToFirst()) {
                     // meaning of `reason` depends on the value of COLUMN_STATUS
                     int reason = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_REASON));
-                    if (reason == DownloadManager.STATUS_FAILED) {
+                    int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                    if (status == DownloadManager.STATUS_FAILED) {
                         ToastUtils.showToast(MediaSettingsActivity.this, R.string.error_media_save);
                         // If an HTTP error occurred, this will hold the HTTP status code as defined in RFC 2616.
                         // Otherwise, it will hold one of the ERROR_* constants
