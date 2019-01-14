2f8de93d9dda734685078bbc3d4e487754d7d489
vojtasmrcek <planarvoid@gmail.com>
2019-01-14 16:15:14 +0100

    Add tests for insights management

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/stats/InsightTypesSqlUtils.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/stats/InsightTypesSqlUtils.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.fluxc.persistence.stats
 
+import android.content.ContentValues
 import com.wellsql.generated.InsightTypesTable
 import com.yarolegovich.wellsql.SelectQuery.ORDER_ASCENDING
 import com.yarolegovich.wellsql.WellSql
@@ -8,19 +9,26 @@ import com.yarolegovich.wellsql.core.annotation.Column
 import com.yarolegovich.wellsql.core.annotation.PrimaryKey
 import com.yarolegovich.wellsql.core.annotation.Table
 import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.model.stats.InsightTypeModel
-import org.wordpress.android.fluxc.model.stats.InsightTypeModel.Status
-import org.wordpress.android.fluxc.persistence.UpdateAllExceptId
+import org.wordpress.android.fluxc.model.stats.InsightTypeDataModel
+import org.wordpress.android.fluxc.model.stats.InsightTypeDataModel.Status
+import org.wordpress.android.fluxc.model.stats.InsightTypeDataModel.Status.ADDED
+import org.wordpress.android.fluxc.model.stats.InsightTypeDataModel.Status.REMOVED
 import org.wordpress.android.fluxc.store.stats.StatsStore.InsightsTypes
-import org.wordpress.android.fluxc.tools.FormattableContentMapper
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class InsightTypesSqlUtils
-@Inject constructor(private val formattableContentMapper: FormattableContentMapper) {
+@Inject constructor() {
+    fun selectAddedItemsOrderedByStatus(site: SiteModel): List<InsightsTypes> {
+        return selectItemsOrderedByStatus(site, ADDED)
+    }
 
-    fun getOrderedInsightTypesByStatus(site: SiteModel, status: InsightTypeModel.Status): List<InsightTypeModel> {
+    fun selectRemovedItemsOrderedByStatus(site: SiteModel): List<InsightsTypes> {
+        return selectItemsOrderedByStatus(site, REMOVED)
+    }
+
+    private fun selectItemsOrderedByStatus(site: SiteModel, status: InsightTypeDataModel.Status): List<InsightsTypes> {
         return WellSql.select(InsightTypesBuilder::class.java)
                 .where()
                 .equals(InsightTypesTable.LOCAL_SITE_ID, site.id)
@@ -28,10 +36,10 @@ class InsightTypesSqlUtils
                 .endWhere()
                 .orderBy(InsightTypesTable.POSITION, ORDER_ASCENDING)
                 .asModel
-                .map { it.build() }
+                .map { InsightsTypes.valueOf(it.insightType) }
     }
 
-    fun getInsightTypes(site: SiteModel): List<InsightTypeModel> {
+    fun selectAll(site: SiteModel): List<InsightTypeDataModel> {
         return WellSql.select(InsightTypesBuilder::class.java)
                 .where()
                 .equals(InsightTypesTable.LOCAL_SITE_ID, site.id)
@@ -40,21 +48,67 @@ class InsightTypesSqlUtils
                 .map { it.build() }
     }
 
-    fun insertInsightTypes(site: SiteModel, insightsTypes: List<InsightTypeModel>) {
-        WellSql.giveMeWritableDb().beginTransaction()
-        WellSql.delete(InsightTypesBuilder::class.java).execute()
-        val insertQuery = WellSql.insert(insightsTypes.map { it.toBuilder(site) })
-        insertQuery.execute()
-        WellSql.giveMeWritableDb().endTransaction()
+    fun insertOrReplaceAddedItems(site: SiteModel, insightsTypes: List<InsightsTypes>) {
+        insertOrReplaceList(site, insightsTypes, ADDED)
     }
 
-    private fun InsightTypeModel.toBuilder(site: SiteModel): InsightTypesBuilder {
+    fun insertOrReplaceRemovedItems(site: SiteModel, insightsTypes: List<InsightsTypes>) {
+        insertOrReplaceList(site, insightsTypes, REMOVED)
+    }
+
+    private fun insertOrReplaceList(site: SiteModel, insightsTypes: List<InsightsTypes>, status: Status) {
+        WellSql.delete(InsightTypesBuilder::class.java)
+                .where()
+                .equals(InsightTypesTable.LOCAL_SITE_ID, site.id)
+                .equals(InsightTypesTable.STATUS, status.name)
+                .endWhere().execute()
+        WellSql.insert(insightsTypes.mapIndexed { index, type ->
+            type.toBuilder(
+                    site,
+                    status,
+                    index
+            )
+        }).execute()
+    }
+
+    fun updateStatus(site: SiteModel, type: InsightsTypes, status: Status = REMOVED, position: Int = -1) {
+        WellSql.update(InsightTypesBuilder::class.java)
+                .where()
+                .equals(InsightTypesTable.LOCAL_SITE_ID, site.id)
+                .equals(InsightTypesTable.INSIGHT_TYPE, type)
+                .endWhere()
+                .put(status.name) {
+                    val cv = ContentValues()
+                    cv.put(InsightTypesTable.STATUS, it)
+                    cv.put(InsightTypesTable.POSITION, position)
+                    cv
+                }
+    }
+
+    fun updatePositions(site: SiteModel, types: List<InsightsTypes>) {
+        types.forEachIndexed { index, type -> updatePosition(site, type, index) }
+    }
+
+    private fun updatePosition(site: SiteModel, type: InsightsTypes, position: Int = -1) {
+        WellSql.update(InsightTypesBuilder::class.java)
+                .where()
+                .equals(InsightTypesTable.LOCAL_SITE_ID, site.id)
+                .equals(InsightTypesTable.INSIGHT_TYPE, type)
+                .endWhere()
+                .put(position) {
+                    val cv = ContentValues()
+                    cv.put(InsightTypesTable.POSITION, it)
+                    cv
+                }
+    }
+
+    private fun InsightsTypes.toBuilder(site: SiteModel, status: Status, position: Int): InsightTypesBuilder {
         return InsightTypesBuilder(
                 localSiteId = site.id,
                 remoteSiteId = site.siteId,
-                insightType = this.status.name,
-                position = this.position ?: -1,
-                status = this.status.name
+                insightType = this.name,
+                position = if (status == ADDED) position else -1,
+                status = status.name
         )
     }
 
@@ -75,8 +129,8 @@ class InsightTypesSqlUtils
 
         override fun getId() = mId
 
-        fun build(): InsightTypeModel {
-            return InsightTypeModel(
+        fun build(): InsightTypeDataModel {
+            return InsightTypeDataModel(
                     InsightsTypes.valueOf(insightType),
                     Status.valueOf(status),
                     if (position >= 0) position else null
