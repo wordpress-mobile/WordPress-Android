2b246f2f464dee6b6266e8df69b031a0148cbf52
malinajirka <malinajirka@gmail.com>
2019-01-18 09:37:21 +0100

    Use injected tracker in NewSiteCreationTracker

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/NewSiteCreationTracker.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/NewSiteCreationTracker.kt
@@ -1,23 +1,24 @@
 package org.wordpress.android.ui.sitecreation
 
 import org.wordpress.android.analytics.AnalyticsTracker
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 import javax.inject.Inject
 import javax.inject.Singleton
 
 const val INTERNET_UNAVAILABLE_ERROR = "internet_unavailable"
 
 @Singleton
-class NewSiteCreationTracker @Inject constructor() {
+class NewSiteCreationTracker @Inject constructor(val tracker: AnalyticsTrackerWrapper) {
     fun trackSiteCreationAccessed() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_ACCESSED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_ACCESSED)
     }
 
     fun trackSegmentsViewed() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SEGMENTS_VIEWED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SEGMENTS_VIEWED)
     }
 
     fun trackSegmentSelected(segmentName: String, segmentId: Long) {
-        AnalyticsTracker.track(
+        tracker.track(
                 AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SEGMENTS_SELECTED,
                 mapOf(
                         "segment_name" to segmentName,
@@ -27,11 +28,11 @@ class NewSiteCreationTracker @Inject constructor() {
     }
 
     fun trackVerticalsViewed() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_VERTICALS_VIEWED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_VERTICALS_VIEWED)
     }
 
     fun trackVerticalSelected(verticalName: String, verticalId: String, isUserVertical: Boolean) {
-        AnalyticsTracker.track(
+        tracker.track(
                 AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_VERTICALS_SELECTED,
                 mapOf(
                         "vertical_name" to verticalName,
@@ -42,15 +43,15 @@ class NewSiteCreationTracker @Inject constructor() {
     }
 
     fun trackVerticalsSkipped() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_VERTICALS_SKIPPED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_VERTICALS_SKIPPED)
     }
 
     fun trackBasicInformationViewed() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_BASIC_INFORMATION_VIEWED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_BASIC_INFORMATION_VIEWED)
     }
 
     fun trackBasicInformationCompleted(siteTitle: String, siteTagLine: String) {
-        AnalyticsTracker.track(
+        tracker.track(
                 AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_BASIC_INFORMATION_COMPLETED,
                 mapOf(
                         "site_title" to siteTitle,
@@ -60,15 +61,15 @@ class NewSiteCreationTracker @Inject constructor() {
     }
 
     fun trackBasicInformationSkipped() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_BASIC_INFORMATION_SKIPPED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_BASIC_INFORMATION_SKIPPED)
     }
 
     fun trackDomainsAccessed() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_DOMAINS_ACCESSED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_DOMAINS_ACCESSED)
     }
 
     fun trackDomainSelected(chosenDomain: String, searchTerm: String) {
-        AnalyticsTracker.track(
+        tracker.track(
                 AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_DOMAINS_SELECTED,
                 mapOf(
                         "chosen_domain" to chosenDomain,
@@ -78,31 +79,31 @@ class NewSiteCreationTracker @Inject constructor() {
     }
 
     fun trackPreviewLoading() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SUCCESS_LOADING)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SUCCESS_LOADING)
     }
 
     fun trackPreviewWebviewShown() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SUCCESS_PREVIEW_VIEWED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SUCCESS_PREVIEW_VIEWED)
     }
 
     fun trackPreviewWebviewFullyLoaded() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SUCCESS_PREVIEW_LOADED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_SUCCESS_PREVIEW_LOADED)
     }
 
     fun trackCreationCompleted() {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_COMPLETED)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_COMPLETED)
     }
 
     fun trackErrorShown(errorContext: String, errorType: String, errorDescription: String? = null) {
-        AnalyticsTracker.track(
+        tracker.track(
                 AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_ERROR_SHOWN,
                 errorContext,
                 errorType,
-                errorDescription
+                errorDescription ?: ""
         )
     }
 
     fun trackSiteCreationServiceStateUpdated(props: Map<String, *>) {
-        AnalyticsTracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_BACKGROUND_SERVICE_UPDATED, props)
+        tracker.track(AnalyticsTracker.Stat.ENHANCED_SITE_CREATION_BACKGROUND_SERVICE_UPDATED, props)
     }
 }
