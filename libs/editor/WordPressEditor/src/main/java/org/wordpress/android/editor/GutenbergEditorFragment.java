6d2783e65667f40a52d373c4915da154b7d39eda
Danilo Ercoli <ercoli@gmail.com>
2019-01-21 12:28:16 +0100

    Remove unused import

--- libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/GutenbergEditorFragment.java
+++ libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/GutenbergEditorFragment.java
@@ -17,7 +17,6 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
 import com.android.volley.toolbox.ImageLoader;
