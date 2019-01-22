4583d7dca9d553ca7d25933a46a8d4a2de3ae2f4
vojtasmrcek <planarvoid@gmail.com>
2019-01-22 16:53:57 +0100

    Revert package changes

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/InsightTypeDataModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/InsightTypeDataModel.kt
@@ -1,6 +1,6 @@
 package org.wordpress.android.fluxc.model.stats
 
-import org.wordpress.android.fluxc.store.stats.StatsStore.InsightsTypes
+import org.wordpress.android.fluxc.store.StatsStore.InsightsTypes
 
 data class InsightTypeDataModel(val type: InsightsTypes, val status: Status, val position: Int?) {
     enum class Status {
