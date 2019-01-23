f03f9f6964e1299de882dd151bed9d84c9ffa3cb
vojtasmrcek <planarvoid@gmail.com>
2019-01-23 14:23:28 +0100

    Ignore timezone in stats params

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/stats/time/StatsUtils.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/stats/time/StatsUtils.kt
@@ -1,16 +1,17 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.stats.time
 
-import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.utils.CurrentDateUtils
-import org.wordpress.android.fluxc.utils.SiteUtils
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 import javax.inject.Inject
 
 const val DATE_FORMAT_DAY = "yyyy-MM-dd"
 
 class StatsUtils
 @Inject constructor(private val currentDateUtils: CurrentDateUtils) {
-    fun getFormattedDate(site: SiteModel, date: Date? = null): String {
-        return SiteUtils.getDateTimeForSite(site, DATE_FORMAT_DAY, date ?: currentDateUtils.getCurrentDate())
+    fun getFormattedDate(date: Date? = null): String {
+        val dateFormat = SimpleDateFormat(DATE_FORMAT_DAY, Locale.ROOT)
+        return dateFormat.format(date ?: currentDateUtils.getCurrentDate())
     }
 }
