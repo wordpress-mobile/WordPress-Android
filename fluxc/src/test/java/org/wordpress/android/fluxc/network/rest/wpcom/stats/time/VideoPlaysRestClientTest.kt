fea97f4659e2e54babbe1dba829af1abf08121e9
vojtasmrcek <planarvoid@gmail.com>
2019-01-23 14:23:28 +0100

    Ignore timezone in stats params

--- fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/stats/time/VideoPlaysRestClientTest.kt
+++ fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/stats/time/VideoPlaysRestClientTest.kt
@@ -68,7 +68,7 @@ class VideoPlaysRestClientTest {
                 gson,
                 statsUtils
         )
-        whenever(statsUtils.getFormattedDate(eq(site), eq(currentDate))).thenReturn(currentDateValue)
+        whenever(statsUtils.getFormattedDate(eq(currentDate))).thenReturn(currentDateValue)
     }
 
     @Test
