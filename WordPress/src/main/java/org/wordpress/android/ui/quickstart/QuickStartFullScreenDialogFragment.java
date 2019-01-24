9674577ec07049e3a3dc5b8094a42a66d2c003b8
Tyler Heck <theck13@users.noreply.github.com>
2019-01-23 18:35:49 -0700

    Update site brush illustration vector drawable

--- WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartFullScreenDialogFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartFullScreenDialogFragment.java
@@ -79,7 +79,7 @@ public class QuickStartFullScreenDialogFragment extends Fragment implements Full
             case CUSTOMIZE:
                 tasksUncompleted.addAll(mQuickStartStore.getUncompletedTasksByType(site, CUSTOMIZE));
                 tasksCompleted.addAll(mQuickStartStore.getCompletedTasksByType(site, CUSTOMIZE));
-                setCompleteViewImage(R.drawable.img_illustration_completed_quick_start_customize_191dp);
+                setCompleteViewImage(R.drawable.img_illustration_site_brush_191dp);
                 break;
             case GROW:
                 tasksUncompleted.addAll(mQuickStartStore.getUncompletedTasksByType(site, GROW));
@@ -89,10 +89,9 @@ public class QuickStartFullScreenDialogFragment extends Fragment implements Full
             case UNKNOWN:
                 tasksUncompleted.addAll(mQuickStartStore.getUncompletedTasksByType(site, CUSTOMIZE));
                 tasksCompleted.addAll(mQuickStartStore.getCompletedTasksByType(site, CUSTOMIZE));
-                setCompleteViewImage(R.drawable.img_illustration_completed_quick_start_customize_191dp);
+                setCompleteViewImage(R.drawable.img_illustration_site_brush_191dp);
                 break;
         }
-
 
         boolean isCompletedTasksListExpanded = savedInstanceState != null
                                                && savedInstanceState.getBoolean(KEY_COMPLETED_TASKS_LIST_EXPANDED);
