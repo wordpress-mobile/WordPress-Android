0acdef838c462034268f1bad57cc697f8ec7b779
Klymentiy haykov <forsver@gmail.com>
2019-01-29 01:15:47 +0200

    Plans actions, model, rest client and updated sql utils.

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/plans/PlanModel.kt
@@ -0,0 +1,64 @@
+package org.wordpress.android.fluxc.model.plans
+
+data class PlanModel(
+    var planIds: List<Int>?,
+    var features: List<Feature>?,
+    var name: String?,
+    var shortName: String?,
+    var tagline: String?,
+    var description: String?,
+    var iconUrl: String?
+) {
+    data class Feature(
+        var id: String?,
+        var name: String?,
+        var description: String?
+    ) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) {
+                return true
+            }
+
+            if (other == null || other !is Feature) {
+                return false
+            }
+
+            return id == other.id && name == other.name &&
+                    description == other.description
+        }
+
+        override fun hashCode(): Int {
+            var result = id?.hashCode() ?: 0
+            result = 31 * result + (name?.hashCode() ?: 0)
+            result = 31 * result + (description?.hashCode() ?: 0)
+            return result
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+
+        if (other == null || other !is PlanModel) {
+            return false
+        }
+
+        return name == other.name && shortName == other.shortName &&
+                tagline == other.tagline && description == other.description &&
+                iconUrl == other.iconUrl && planIds == other.planIds && features == other.features
+    }
+
+    override fun hashCode(): Int {
+        var result = planIds?.hashCode() ?: 0
+        result = 31 * result + (features?.hashCode() ?: 0)
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (shortName?.hashCode() ?: 0)
+        result = 31 * result + (tagline?.hashCode() ?: 0)
+        result = 31 * result + (description?.hashCode() ?: 0)
+        result = 31 * result + (iconUrl?.hashCode() ?: 0)
+        result = 31 * result + (planIds?.hashCode() ?: 0)
+        result = 31 * result + (features?.hashCode() ?: 0)
+        return result
+    }
+}
--- fluxc/src/main/java/org/wordpress/android/fluxc/module/ReleaseNetworkModule.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/module/ReleaseNetworkModule.java
@@ -31,6 +31,7 @@ import org.wordpress.android.fluxc.network.rest.wpcom.comment.CommentRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.media.MediaRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.notifications.NotificationRestClient;
+import org.wordpress.android.fluxc.network.rest.wpcom.plans.PlansRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.plugin.PluginRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.post.PostRestClient;
 import org.wordpress.android.fluxc.network.rest.wpcom.reader.ReaderRestClient;
@@ -321,6 +322,16 @@ public class ReleaseNetworkModule {
                                                         AccessToken token, UserAgent userAgent,
                                                         WPComGsonRequestBuilder wpComGsonRequestBuilder) {
         return new VerticalRestClient(dispatcher, wpComGsonRequestBuilder, appContext, requestQueue, token,
+                userAgent);
+    }
+
+    @Singleton
+    @Provides
+    public PlansRestClient providePlansRestClient(Context appContext, Dispatcher dispatcher,
+                                                        @Named("regular") RequestQueue requestQueue,
+                                                        AccessToken token, UserAgent userAgent,
+                                                        WPComGsonRequestBuilder wpComGsonRequestBuilder) {
+        return new PlansRestClient(dispatcher, wpComGsonRequestBuilder, appContext, requestQueue, token,
                 userAgent);
     }
 
