0d05e120b521a1fa112a5517e54abac88ec03ea7
Klymentiy haykov <forsver@gmail.com>
2019-01-29 01:11:48 +0200

    Sql utils test.

---
+++ fluxc/src/test/java/org/wordpress/android/fluxc/utils/PlansSqlUtilsTest.kt
@@ -0,0 +1,44 @@
+package org.wordpress.android.fluxc.utils
+
+import com.yarolegovich.wellsql.WellSql
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.wordpress.android.fluxc.SingleStoreWellSqlConfigForTests
+import org.wordpress.android.fluxc.network.rest.wpcom.plans.PLANS_MODEL
+import org.wordpress.android.fluxc.persistence.PlansSqlUtils
+import org.wordpress.android.fluxc.persistence.PlansSqlUtils.PlanBuilder
+import org.wordpress.android.fluxc.persistence.PlansSqlUtils.PlanFeatureBuilder
+import org.wordpress.android.fluxc.persistence.PlansSqlUtils.PlanIdBuilder
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+@RunWith(RobolectricTestRunner::class)
+class PlansSqlUtilsTest {
+    private lateinit var plansSqlUtils: PlansSqlUtils
+
+    @Before
+    fun setUp() {
+        plansSqlUtils = PlansSqlUtils()
+
+        val appContext = RuntimeEnvironment.application.applicationContext
+        val config = SingleStoreWellSqlConfigForTests(
+                appContext,
+                listOf(PlanBuilder::class.java, PlanIdBuilder::class.java, PlanFeatureBuilder::class.java), ""
+        )
+        WellSql.init(config)
+        config.reset()
+    }
+
+    @Test
+    fun testStoreAndRetrievePlans() {
+        plansSqlUtils.storePlans(PLANS_MODEL)
+
+        val cachedPlans = plansSqlUtils.getPlans()
+
+        assertNotNull(cachedPlans)
+        assertEquals(PLANS_MODEL, cachedPlans)
+    }
+}
