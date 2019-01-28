232440d35a09432cd10b10f445a4e7210b79106d
Klymentiy haykov <forsver@gmail.com>
2019-01-29 01:14:02 +0200

    Adding plans endpoint and registering connected tests.

---
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/plans/PlansRestClientTest.kt
@@ -0,0 +1,10 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.plans
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class PostRestClient {
+
+
+}
