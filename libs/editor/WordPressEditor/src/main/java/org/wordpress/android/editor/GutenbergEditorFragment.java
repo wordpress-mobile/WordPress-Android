0fc2aa4e0c46228791b707449bdc19f74a780451
Mario Zorz <mariozorz@gmail.com>
2019-01-29 16:32:31 -0300

    removed contentContainsGutenbergBlocks method from GutenbergEditorFragment as it was already moved to PostUtils before

--- libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/GutenbergEditorFragment.java
+++ libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/GutenbergEditorFragment.java
@@ -46,7 +46,6 @@ public class GutenbergEditorFragment extends EditorFragmentAbstract implements
         EditorMediaUploadListener,
         IHistoryListener {
     private static final String KEY_HTML_MODE_ENABLED = "KEY_HTML_MODE_ENABLED";
-    private static final String GUTENBERG_BLOCK_START = "<!-- wp:";
     private static final String ARG_IS_NEW_POST = "param_is_new_post";
 
     private static boolean mIsToolbarExpanded = false;
@@ -344,29 +343,6 @@ public class GutenbergEditorFragment extends EditorFragmentAbstract implements
         }
 
         mWPAndroidGlueCode.toggleEditorMode();
-    }
-
-    /*
-        Note the way we detect we're in presence of Gutenberg blocks logic is taken from
-        https://github.com/WordPress/gutenberg/blob/5a6693589285363341bebad15bd56d9371cf8ecc/lib/register.php#L331-L345
-
-        * Determine whether a content string contains blocks. This test optimizes for
-        * performance rather than strict accuracy, detecting the pattern of a block
-        * but not validating its structure. For strict accuracy, you should use the
-        * block parser on post content.
-        *
-        * @since 1.6.0
-        * @see gutenberg_parse_blocks()
-        *
-        * @param string $content Content to test.
-        * @return bool Whether the content contains blocks.
-
-        function gutenberg_content_has_blocks( $content ) {
-            return false !== strpos( $content, '<!-- wp:' );
-        }
-     */
-    public static boolean contentContainsGutenbergBlocks(String postContent) {
-        return (postContent != null && postContent.contains(GUTENBERG_BLOCK_START));
     }
 
     /*
