95968fdbee22bc4ed78b5bb08ad640fea884a532
Ondrej Ruttkay <ondrej@automattic.com>
2019-01-31 12:45:05 +0100

    Refactor the StatsViewAllViewModel

---
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllFollowersViewModel.kt
@@ -0,0 +1,14 @@
+package org.wordpress.android.ui.stats.refresh
+
+import kotlinx.coroutines.CoroutineDispatcher
+import org.wordpress.android.modules.UI_THREAD
+import org.wordpress.android.ui.stats.refresh.lists.BaseListUseCase
+import org.wordpress.android.ui.stats.refresh.lists.StatsListViewModel
+import javax.inject.Inject
+import javax.inject.Named
+
+class ViewAllFollowersViewModel
+@Inject constructor(
+    @Named(UI_THREAD) mainDispatcher: CoroutineDispatcher,
+    @Named(INSIGHTS_USE_CASE) private val insightsUseCase: BaseListUseCase
+) : StatsListViewModel(mainDispatcher, insightsUseCase)
--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllViewModel.kt
+++
@@ -1,3 +0,0 @@
-package org.wordpress.android.ui.stats.refresh
-
-class StatsViewAllViewModel
