a8cd9b45e52840024d70073a1e6fe9c3d398b54f
Ondrej Ruttkay <ondrej@automattic.com>
2019-01-31 12:48:10 +0100

    Add the use case loading logic to the View all ViewModel

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllFollowersViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllFollowersViewModel.kt
@@ -1,14 +1,27 @@
 package org.wordpress.android.ui.stats.refresh
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.modules.UI_THREAD
 import org.wordpress.android.ui.stats.refresh.lists.BaseListUseCase
 import org.wordpress.android.ui.stats.refresh.lists.StatsListViewModel
 import javax.inject.Inject
 import javax.inject.Named
 
-class ViewAllFollowersViewModel
+class StatsViewAllFollowersViewModel
 @Inject constructor(
     @Named(UI_THREAD) mainDispatcher: CoroutineDispatcher,
-    @Named(INSIGHTS_USE_CASE) private val insightsUseCase: BaseListUseCase
-) : StatsListViewModel(mainDispatcher, insightsUseCase)
+    @Named(VIEW_ALL_FOLLOWERS_USE_CASE) private val useCase: BaseListUseCase
+) : StatsListViewModel(mainDispatcher, useCase) {
+    fun start(site: SiteModel) {
+        loadData {
+            useCase.loadData(site)
+        }
+    }
+
+    private fun CoroutineScope.loadData(executeLoading: suspend () -> Unit) = launch {
+        executeLoading()
+    }
+}
