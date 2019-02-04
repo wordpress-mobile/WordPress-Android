94a894b7cec636522764266f923f812f2f30f219
malinajirka <malinajirka@gmail.com>
2019-02-04 12:24:54 +0100

    Add WizardManager to Dagger graph - fix tests

--- WordPress/src/test/java/org/wordpress/android/ui/sitecreation/NewSiteCreationMainVMTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/sitecreation/NewSiteCreationMainVMTest.kt
@@ -5,7 +5,7 @@ import android.arch.lifecycle.Observer
 import android.os.Bundle
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.argThat
-import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.clearInvocations
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
@@ -14,29 +14,26 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentCaptor
-import org.mockito.Captor
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.ui.sitecreation.NewSiteCreationMainVM.NewSiteCreationScreenTitle.ScreenTitleEmpty
 import org.wordpress.android.ui.sitecreation.NewSiteCreationMainVM.NewSiteCreationScreenTitle.ScreenTitleGeneral
 import org.wordpress.android.ui.sitecreation.NewSiteCreationMainVM.NewSiteCreationScreenTitle.ScreenTitleStepCount
-import org.wordpress.android.ui.sitecreation.SiteCreationStep.DOMAINS
-import org.wordpress.android.ui.sitecreation.SiteCreationStep.SEGMENTS
-import org.wordpress.android.ui.sitecreation.SiteCreationStep.SITE_INFO
-import org.wordpress.android.ui.sitecreation.SiteCreationStep.SITE_PREVIEW
-import org.wordpress.android.ui.sitecreation.SiteCreationStep.VERTICALS
 import org.wordpress.android.ui.sitecreation.misc.NewSiteCreationTracker
 import org.wordpress.android.ui.sitecreation.previews.NewSitePreviewViewModel.CreateSiteState
 import org.wordpress.android.ui.sitecreation.previews.NewSitePreviewViewModel.CreateSiteState.SiteCreationCompleted
+import org.wordpress.android.util.wizard.WizardManager
+import org.wordpress.android.viewmodel.SingleLiveEvent
 
 private const val LOCAL_SITE_ID = 1
 private const val SEGMENT_ID = 1L
 private const val VERTICAL_ID = "m1v1"
 private const val SITE_TITLE = "test title"
-private const val SITE_TAGLINE = "test tagline"
+private const val SITE_TAG_LINE = "test tagLine"
 private const val DOMAIN = "test.domain.com"
-
-private val SITE_CREATION_STEPS = listOf(SITE_INFO, VERTICALS, SEGMENTS, DOMAINS, SITE_PREVIEW)
+private const val STEP_COUNT = 20
+private const val FIRST_STEP_INDEX = 1
+private const val LAST_STEP_INDEX = STEP_COUNT
 
 @RunWith(MockitoJUnitRunner::class)
 class NewSiteCreationMainVMTest {
@@ -44,81 +41,87 @@ class NewSiteCreationMainVMTest {
     @JvmField val rule = InstantTaskExecutorRule()
 
     @Mock lateinit var tracker: NewSiteCreationTracker
-    @Mock lateinit var stepsProvider: NewSiteCreationStepsProvider
     @Mock lateinit var navigationTargetObserver: Observer<NavigationTarget>
     @Mock lateinit var wizardFinishedObserver: Observer<CreateSiteState>
     @Mock lateinit var savedInstanceState: Bundle
-
-    @Captor
-    private val captorNavigationTarget: ArgumentCaptor<NavigationTarget>? = null
+    @Mock lateinit var wizardManager: WizardManager<SiteCreationStep>
+    @Mock lateinit var siteCreationStep: SiteCreationStep
+    private val wizardManagerNavigatorLiveData = SingleLiveEvent<SiteCreationStep>()
 
     private lateinit var viewModel: NewSiteCreationMainVM
 
     @Before
     fun setUp() {
-        whenever(stepsProvider.getSteps()).thenReturn(SITE_CREATION_STEPS)
-        viewModel = NewSiteCreationMainVM(stepsProvider, tracker)
+        whenever(wizardManager.navigatorLiveData).thenReturn(wizardManagerNavigatorLiveData)
+        whenever(wizardManager.showNextStep()).then {
+            wizardManagerNavigatorLiveData.value = siteCreationStep
+            Unit
+        }
+        viewModel = NewSiteCreationMainVM(tracker, wizardManager)
         viewModel.start(null)
         viewModel.navigationTargetObservable.observeForever(navigationTargetObserver)
         viewModel.wizardFinishedObservable.observeForever(wizardFinishedObserver)
+        whenever(wizardManager.stepsCount).thenReturn(STEP_COUNT)
+        // clear invocations since viewModel.start() calls wizardManager.showNextStep
+        clearInvocations(wizardManager)
     }
 
     @Test
     fun skipClickedResultsInNextStep() {
         viewModel.onSkipClicked()
-        verifyNavigatedToNextStep()
+        verify(wizardManager).showNextStep()
     }
 
     @Test
     fun segmentSelectedResultsInNextStep() {
         viewModel.onSegmentSelected(SEGMENT_ID)
-        verifyNavigatedToNextStep()
+        verify(wizardManager).showNextStep()
     }
 
     @Test
     fun verticalSelectedResultsInNextStep() {
         viewModel.onVerticalsScreenFinished(VERTICAL_ID)
-        verifyNavigatedToNextStep()
+        verify(wizardManager).showNextStep()
     }
 
     @Test
     fun siteInfoFinishedResultsInNextStep() {
         viewModel.onInfoScreenFinished(SITE_TITLE, null)
-        verifyNavigatedToNextStep()
+        verify(wizardManager).showNextStep()
     }
 
     @Test
     fun domainSelectedResultsInNextStep() {
         viewModel.onDomainsScreenFinished(DOMAIN)
-        verifyNavigatedToNextStep()
+        verify(wizardManager).showNextStep()
     }
 
     @Test
     fun siteCreationStateUpdatedWithSelectedSegment() {
         viewModel.onSegmentSelected(SEGMENT_ID)
-        assertThat(viewModel.navigationTargetObservable.lastEvent!!.wizardState.segmentId).isEqualTo(SEGMENT_ID)
+        assertThat(currentWizardState(viewModel).segmentId).isEqualTo(SEGMENT_ID)
     }
 
     @Test
     fun siteCreationStateUpdatedWithSelectedVertical() {
         viewModel.onVerticalsScreenFinished(VERTICAL_ID)
-        assertThat(viewModel.navigationTargetObservable.lastEvent!!.wizardState.verticalId).isEqualTo(VERTICAL_ID)
+        assertThat(currentWizardState(viewModel).verticalId).isEqualTo(VERTICAL_ID)
     }
 
     @Test
     fun siteCreationStateUpdatedWithSiteInfo() {
         viewModel.onInfoScreenFinished(
                 SITE_TITLE,
-                SITE_TAGLINE
+                SITE_TAG_LINE
         )
-        assertThat(viewModel.navigationTargetObservable.lastEvent!!.wizardState.siteTitle).isEqualTo(SITE_TITLE)
-        assertThat(viewModel.navigationTargetObservable.lastEvent!!.wizardState.siteTagLine).isEqualTo(SITE_TAGLINE)
+        assertThat(currentWizardState(viewModel).siteTitle).isEqualTo(SITE_TITLE)
+        assertThat(currentWizardState(viewModel).siteTagLine).isEqualTo(SITE_TAG_LINE)
     }
 
     @Test
     fun siteCreationStateUpdatedWithSelectedDomain() {
         viewModel.onDomainsScreenFinished(DOMAIN)
-        assertThat(viewModel.navigationTargetObservable.lastEvent!!.wizardState.domain).isEqualTo(DOMAIN)
+        assertThat(currentWizardState(viewModel).domain).isEqualTo(DOMAIN)
     }
 
     @Test
@@ -133,43 +136,44 @@ class NewSiteCreationMainVMTest {
     }
 
     @Test
-    fun nextStepAfterOnBackPressedIsCurrentStep() {
-        val startingStep = currentStep()
-        viewModel.onBackPressed() // navigate to previous step
-        viewModel.onSkipClicked() // navigate to next step
-        assertThat(currentStep()).isEqualTo(startingStep)
+    fun onBackPressedPropagatedToWizardManager() {
+        viewModel.onBackPressed()
+        verify(wizardManager).onBackPressed()
     }
 
     @Test
-    fun backSuppressedOnlyForLastStep() {
-        while (!isAtLastStep()) {
-            assertThat(viewModel.shouldSuppressBackPress()).isFalse()
-            viewModel.onSkipClicked() // navigate to a next step
-        }
+    fun backNotSuppressedWhenNotLastStep() {
+        whenever(wizardManager.isLastStep()).thenReturn(false)
+        assertThat(viewModel.shouldSuppressBackPress()).isFalse()
+    }
+
+    @Test
+    fun backSuppressedForLastStep() {
+        whenever(wizardManager.isLastStep()).thenReturn(true)
         assertThat(viewModel.shouldSuppressBackPress()).isTrue()
     }
 
     @Test
     fun titleForFirstStepIsGeneralSiteCreation() {
-        assertThat(viewModel.screenTitleForWizardStep(SITE_CREATION_STEPS.first()))
+        whenever(wizardManager.stepPosition(siteCreationStep)).thenReturn(FIRST_STEP_INDEX)
+        assertThat(viewModel.screenTitleForWizardStep(siteCreationStep))
                 .isInstanceOf(ScreenTitleGeneral::class.java)
     }
 
     @Test
     fun titleForLastStepIsEmptyTitle() {
-        assertThat(viewModel.screenTitleForWizardStep(SITE_CREATION_STEPS.last()))
+        whenever(wizardManager.stepPosition(siteCreationStep)).thenReturn(LAST_STEP_INDEX)
+        assertThat(viewModel.screenTitleForWizardStep(siteCreationStep))
                 .isInstanceOf(ScreenTitleEmpty::class.java)
     }
 
     @Test
     fun titlesForOtherThanFirstAndLastStepIsStepCount() {
-        val isFirstStep = { index: Int -> index == 0 }
-        val isLastStep = { index: Int -> index == SITE_CREATION_STEPS.size - 1 }
+        (FIRST_STEP_INDEX + 1 until STEP_COUNT).forEach { stepIndex ->
+            whenever(wizardManager.stepPosition(siteCreationStep)).thenReturn(stepIndex)
 
-        SITE_CREATION_STEPS.filterIndexed { index, _ ->
-            !isFirstStep(index) && !isLastStep(index)
-        }.forEach { step ->
-            assertThat(viewModel.screenTitleForWizardStep(step)).isInstanceOf(ScreenTitleStepCount::class.java)
+            assertThat(viewModel.screenTitleForWizardStep(siteCreationStep))
+                    .isInstanceOf(ScreenTitleStepCount::class.java)
         }
     }
 
@@ -182,27 +186,37 @@ class NewSiteCreationMainVMTest {
     @Test
     fun siteCreationStateRestored() {
         val expectedState = SiteCreationState()
-        whenever(savedInstanceState.getParcelable<SiteCreationState>(any()))
+        whenever(savedInstanceState.getParcelable<SiteCreationState>("key_site_creation_state"))
                 .thenReturn(expectedState)
 
         // we need to create a new instance of the VM as the `viewModel` has already been started in setUp()
-        val newViewModel = NewSiteCreationMainVM(stepsProvider, tracker)
+        val newViewModel = NewSiteCreationMainVM(tracker, wizardManager)
         newViewModel.start(savedInstanceState)
 
-        /* we need to navigate to the next step as the value of navigationTargetObservable isn't changed when the VM
-        is restored from a savedInstanceState. */
-        newViewModel.onSkipClicked()
+        /* we need simulate navigation to the next step as wizardManager.showNextStep() isn't invoked
+        when the VM is restored from a savedInstanceState. */
+        wizardManagerNavigatorLiveData.value = siteCreationStep
 
         newViewModel.navigationTargetObservable.observeForever(navigationTargetObserver)
-        assertThat(newViewModel.navigationTargetObservable.lastEvent!!.wizardState).isSameAs(expectedState)
+        assertThat(currentWizardState(newViewModel)).isSameAs(expectedState)
     }
 
-    private fun verifyNavigatedToNextStep() {
-        // onChange invoked after viewModel.start() and after viewModel.onSkipClicked()
-        verify(navigationTargetObserver, times(2)).onChanged(captorNavigationTarget!!.capture())
+    @Test
+    fun siteCreationStepIndexRestored() {
+        val index = 17
+        whenever(savedInstanceState.getInt("key_current_step")).thenReturn(index)
+
+        // siteCreationState is not nullable - we need to set it
+        whenever(savedInstanceState.getParcelable<SiteCreationState>("key_site_creation_state"))
+                .thenReturn(SiteCreationState())
+
+        // we need to create a new instance of the VM as the `viewModel` has already been started in setUp()
+        val newViewModel = NewSiteCreationMainVM(tracker, wizardManager)
+        newViewModel.start(savedInstanceState)
+
+        verify(wizardManager).setCurrentStepIndex(index)
     }
 
-    private fun currentStep() = viewModel.navigationTargetObservable.lastEvent!!.wizardStep
-
-    private fun isAtLastStep() = SITE_CREATION_STEPS.last() == currentStep()
+    private fun currentWizardState(vm: NewSiteCreationMainVM) =
+            vm.navigationTargetObservable.lastEvent!!.wizardState
 }
