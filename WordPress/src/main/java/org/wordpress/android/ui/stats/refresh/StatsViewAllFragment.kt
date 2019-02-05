35e4c147cd02c85de1da27575d24255782f443fe
Ondrej Ruttkay <ondrej@automattic.com>
2019-02-05 15:19:24 +0100

    Use the common navigator class in the navigation target observer

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllFragment.kt
@@ -3,7 +3,6 @@ package org.wordpress.android.ui.stats.refresh
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
-import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
 import android.support.v4.app.FragmentActivity
@@ -24,35 +23,13 @@ import org.wordpress.android.fluxc.network.utils.StatsGranularity.DAYS
 import org.wordpress.android.fluxc.network.utils.StatsGranularity.MONTHS
 import org.wordpress.android.fluxc.network.utils.StatsGranularity.WEEKS
 import org.wordpress.android.fluxc.network.utils.StatsGranularity.YEARS
-import org.wordpress.android.ui.ActivityLauncher
-import org.wordpress.android.ui.WPWebViewActivity
 import org.wordpress.android.ui.stats.StatsAbstractFragment
-import org.wordpress.android.ui.stats.StatsTimeframe
-import org.wordpress.android.ui.stats.StatsUtils
 import org.wordpress.android.ui.stats.StatsViewType
-import org.wordpress.android.ui.stats.models.StatsPostModel
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.AddNewPost
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.SharePost
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewAuthors
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewClicks
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewCommentsStats
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewCountries
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewFollowersStats
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewPost
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewPostDetailStats
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewPostsAndPages
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewPublicizeStats
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewReferrers
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewSearchTerms
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewTag
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewTagsAndCategoriesStats
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewUrl
-import org.wordpress.android.ui.stats.refresh.NavigationTarget.ViewVideoPlays
 import org.wordpress.android.ui.stats.refresh.lists.StatsBlock
 import org.wordpress.android.ui.stats.refresh.lists.StatsBlockAdapter
 import org.wordpress.android.ui.stats.refresh.lists.StatsListViewModel
 import org.wordpress.android.ui.stats.refresh.utils.StatsDateFormatter
-import org.wordpress.android.util.ToastUtils
+import org.wordpress.android.ui.stats.refresh.utils.StatsNavigator
 import org.wordpress.android.util.image.ImageManager
 import org.wordpress.android.util.observeEvent
 import javax.inject.Inject
@@ -62,6 +39,7 @@ class StatsViewAllFragment : DaggerFragment() {
     @Inject lateinit var imageManager: ImageManager
     @Inject lateinit var statsDateFormatter: StatsDateFormatter
     private lateinit var viewModel: StatsListViewModel
+    private lateinit var navigator: StatsNavigator
 
     private var layoutManager: LayoutManager? = null
 
@@ -155,127 +133,23 @@ class StatsViewAllFragment : DaggerFragment() {
 
         viewModel = ViewModelProviders.of(activity, viewModelFactory).get(clazz)
 
-        setupObservers(activity, site)
+        this.navigator = StatsNavigator(site, activity, statsDateFormatter)
+
+        setupObservers()
 
         (viewModel as StatsViewAllFollowersViewModel).start(site)
     }
 
-    private fun setupObservers(activity: FragmentActivity, site: SiteModel) {
+    private fun setupObservers() {
         viewModel.data.observe(this, Observer {
             if (it != null) {
                 updateInsights(it)
             }
         })
 
-        viewModel.navigationTarget.observeEvent(this) {
-            when (it) {
-                is AddNewPost -> ActivityLauncher.addNewPostForResult(activity, site, false)
-                is ViewPost -> {
-                    StatsUtils.openPostInReaderOrInAppWebview(
-                            activity,
-                            site.siteId,
-                            it.postId.toString(),
-                            it.postType,
-                            it.postUrl
-                    )
-                }
-                is SharePost -> {
-                    val intent = Intent(Intent.ACTION_SEND)
-                    intent.type = "text/plain"
-                    intent.putExtra(Intent.EXTRA_TEXT, it.url)
-                    intent.putExtra(Intent.EXTRA_SUBJECT, it.title)
-                    try {
-                        startActivity(Intent.createChooser(intent, getString(R.string.share_link)))
-                    } catch (ex: android.content.ActivityNotFoundException) {
-                        ToastUtils.showToast(activity, R.string.reader_toast_err_share_intent)
-                    }
-                }
-                is ViewPostDetailStats -> {
-                    val postModel = StatsPostModel(
-                            site.siteId,
-                            it.postId.toString(),
-                            it.postTitle,
-                            it.postUrl,
-                            it.postType
-                    )
-                    ActivityLauncher.viewStatsSinglePostDetails(activity, postModel)
-                }
-                is ViewFollowersStats -> {
-                    ActivityLauncher.viewFollowersStats(activity, site)
-                }
-                is ViewCommentsStats -> {
-                    ActivityLauncher.viewCommentsStats(activity, site)
-                }
-                is ViewTagsAndCategoriesStats -> {
-                    ActivityLauncher.viewTagsAndCategoriesStats(activity, site)
-                }
-                is ViewTag -> {
-                    ActivityLauncher.openStatsUrl(activity, it.link)
-                }
-                is ViewPublicizeStats -> {
-                    ActivityLauncher.viewPublicizeStats(activity, site)
-                }
-                is ViewPostsAndPages -> {
-                    ActivityLauncher.viewPostsAndPagesStats(
-                            activity,
-                            site,
-                            it.statsGranularity.toStatsTimeFrame(),
-                            statsDateFormatter.printStatsDate(it.selectedDate)
-                    )
-                }
-                is ViewReferrers -> {
-                    ActivityLauncher.viewReferrersStats(
-                            activity,
-                            site,
-                            it.statsGranularity.toStatsTimeFrame(),
-                            statsDateFormatter.printStatsDate(it.selectedDate)
-                    )
-                }
-                is ViewClicks -> {
-                    ActivityLauncher.viewClicksStats(
-                            activity,
-                            site,
-                            it.statsGranularity.toStatsTimeFrame(),
-                            statsDateFormatter.printStatsDate(it.selectedDate)
-                    )
-                }
-                is ViewCountries -> {
-                    ActivityLauncher.viewCountriesStats(
-                            activity,
-                            site,
-                            it.statsGranularity.toStatsTimeFrame(),
-                            statsDateFormatter.printStatsDate(it.selectedDate)
-                    )
-                }
-                is ViewVideoPlays -> {
-                    ActivityLauncher.viewVideoPlays(
-                            activity,
-                            site,
-                            it.statsGranularity.toStatsTimeFrame(),
-                            statsDateFormatter.printStatsDate(it.selectedDate)
-                    )
-                }
-                is ViewSearchTerms -> {
-                    ActivityLauncher.viewSearchTerms(
-                            activity,
-                            site,
-                            it.statsGranularity.toStatsTimeFrame(),
-                            statsDateFormatter.printStatsDate(it.selectedDate)
-                    )
-                }
-                is ViewAuthors -> {
-                    ActivityLauncher.viewAuthorsStats(
-                            activity,
-                            site,
-                            it.statsGranularity.toStatsTimeFrame(),
-                            statsDateFormatter.printStatsDate(it.selectedDate)
-                    )
-                }
-                is ViewUrl -> {
-                    WPWebViewActivity.openURL(activity, it.url)
-                }
-            }
-            true
+        viewModel.navigationTarget.observeEvent(this) { target ->
+            navigator.navigate(target)
+            return@observeEvent true
         }
     }
 
