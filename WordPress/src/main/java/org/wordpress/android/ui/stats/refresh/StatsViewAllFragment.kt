2a7e817d9febe263ab754a4112e6d74f54ea7334
Ondrej Ruttkay <ondrej@automattic.com>
2019-02-05 14:51:25 +0100

    Remove unused class

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllFragment.kt
@@ -27,7 +27,6 @@ import org.wordpress.android.fluxc.network.utils.StatsGranularity.YEARS
 import org.wordpress.android.ui.ActivityLauncher
 import org.wordpress.android.ui.WPWebViewActivity
 import org.wordpress.android.ui.stats.StatsAbstractFragment
-import org.wordpress.android.ui.stats.StatsConstants
 import org.wordpress.android.ui.stats.StatsTimeframe
 import org.wordpress.android.ui.stats.StatsUtils
 import org.wordpress.android.ui.stats.StatsViewType
@@ -53,11 +52,9 @@ import org.wordpress.android.ui.stats.refresh.lists.StatsBlock
 import org.wordpress.android.ui.stats.refresh.lists.StatsBlockAdapter
 import org.wordpress.android.ui.stats.refresh.lists.StatsListViewModel
 import org.wordpress.android.ui.stats.refresh.utils.StatsDateFormatter
-import org.wordpress.android.util.Event
 import org.wordpress.android.util.ToastUtils
 import org.wordpress.android.util.image.ImageManager
 import org.wordpress.android.util.observeEvent
-import java.util.Date
 import javax.inject.Inject
 
 class StatsViewAllFragment : DaggerFragment() {
@@ -295,34 +292,6 @@ class StatsViewAllFragment : DaggerFragment() {
         adapter.update(statsState)
         layoutManager?.onRestoreInstanceState(recyclerViewState)
     }
-}
-
-sealed class NavigationTarget : Event() {
-    class AddNewPost : NavigationTarget()
-    data class ViewPost(val postId: Long, val postUrl: String, val postType: String = StatsConstants.ITEM_TYPE_POST) :
-            NavigationTarget()
-
-    data class SharePost(val url: String, val title: String) : NavigationTarget()
-    data class ViewPostDetailStats(
-        val postId: Long,
-        val postTitle: String,
-        val postUrl: String,
-        val postType: String = StatsConstants.ITEM_TYPE_POST
-    ) : NavigationTarget()
-
-    class ViewFollowersStats : NavigationTarget()
-    class ViewCommentsStats : NavigationTarget()
-    class ViewTagsAndCategoriesStats : NavigationTarget()
-    class ViewPublicizeStats : NavigationTarget()
-    data class ViewTag(val link: String) : NavigationTarget()
-    data class ViewPostsAndPages(val statsGranularity: StatsGranularity, val selectedDate: Date) : NavigationTarget()
-    data class ViewReferrers(val statsGranularity: StatsGranularity, val selectedDate: Date) : NavigationTarget()
-    data class ViewClicks(val statsGranularity: StatsGranularity, val selectedDate: Date) : NavigationTarget()
-    data class ViewCountries(val statsGranularity: StatsGranularity, val selectedDate: Date) : NavigationTarget()
-    data class ViewVideoPlays(val statsGranularity: StatsGranularity, val selectedDate: Date) : NavigationTarget()
-    data class ViewSearchTerms(val statsGranularity: StatsGranularity, val selectedDate: Date) : NavigationTarget()
-    data class ViewAuthors(val statsGranularity: StatsGranularity, val selectedDate: Date) : NavigationTarget()
-    data class ViewUrl(val url: String) : NavigationTarget()
 }
 
 fun StatsGranularity.toStatsTimeFrame(): StatsTimeframe {
