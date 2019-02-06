ffb93468c9a0ef4c37066666b12db12c6e76713f
Ondrej Ruttkay <ondrej@automattic.com>
2019-02-06 19:23:14 +0100

    Make LoadMode.Paged a data class

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/LoadMode.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/LoadMode.kt
@@ -3,6 +3,6 @@ package org.wordpress.android.fluxc.model.stats
 const val ALL_PAGE_SIZE = 100
 
 sealed class LoadMode(open val pageSize: Int) {
-    class Paged(pageSize: Int, val loadMore: Boolean = false) : LoadMode(pageSize)
+    data class Paged(override val pageSize: Int, val loadMore: Boolean = false) : LoadMode(pageSize)
     object All : LoadMode(ALL_PAGE_SIZE)
 }
