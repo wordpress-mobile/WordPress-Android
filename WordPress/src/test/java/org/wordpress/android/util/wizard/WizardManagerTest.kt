092e2e9b128ce700421f623fe4b8dde0ca635b28
malinajirka <malinajirka@gmail.com>
2019-02-08 09:44:50 +0100

    Add unit tests for WizardManager

--- WordPress/src/test/java/org/wordpress/android/util/wizard/WizardManagerTest.kt
+++ WordPress/src/test/java/org/wordpress/android/util/wizard/WizardManagerTest.kt
@@ -1,7 +1,14 @@
 package org.wordpress.android.util.wizard
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.ui.sitecreation.SiteCreationStep
 import org.wordpress.android.ui.sitecreation.SiteCreationStep.DOMAINS
@@ -9,14 +16,69 @@ import org.wordpress.android.ui.sitecreation.SiteCreationStep.SEGMENTS
 import org.wordpress.android.ui.sitecreation.SiteCreationStep.SITE_PREVIEW
 
 private val STEPS = listOf(SEGMENTS, DOMAINS, SITE_PREVIEW)
+private val LAST_STEP_INDEX = STEPS.size - 1
 
 @RunWith(MockitoJUnitRunner::class)
 class WizardManagerTest {
+    @Rule
+    @JvmField val rule = InstantTaskExecutorRule()
+
     private lateinit var manager: WizardManager<SiteCreationStep>
+    @Mock private lateinit var navigatorLiveDataObserver: Observer<SiteCreationStep>
 
     @Before
     fun setUp() {
         manager = WizardManager(STEPS)
+        manager.navigatorLiveData.observeForever(navigatorLiveDataObserver)
     }
+
+    @Test
+    fun `showNextStep is propagated`() {
+        manager.showNextStep()
+        verify(navigatorLiveDataObserver).onChanged(STEPS[0])
+    }
+
+    @Test
+    fun `showNextStep increments currentStepIndex`() {
+        val initialStepIndex = manager.currentStep
+        manager.showNextStep()
+        assertThat(manager.currentStep).isEqualTo(initialStepIndex + 1)
+    }
+
+    @Test
+    fun `step position is correctly calculated`() {
+        STEPS.forEach { step ->
+            assertThat(manager.stepPosition(step)).isEqualTo(STEPS.indexOf(step) + 1)
+        }
+    }
+
+    @Test
+    fun `isLastStep returns true only for last step`() {
+        STEPS.forEachIndexed { currentStepIndex, _ ->
+            manager.showNextStep()
+            assertThat(manager.isLastStep()).isEqualTo(currentStepIndex == LAST_STEP_INDEX)
+        }
+    }
+
+    @Test
+    fun `manager is initialized with provided step index`() {
+        manager = createWizardManager(initialStepIndex = LAST_STEP_INDEX)
+        assertThat(manager.currentStep).isEqualTo(LAST_STEP_INDEX)
+    }
+
+    @Test
+    fun `onBackPressed decrements currentStepIndex`() {
+        manager = createWizardManager(initialStepIndex = LAST_STEP_INDEX)
+        manager.onBackPressed()
+        assertThat(manager.currentStep).isEqualTo(LAST_STEP_INDEX - 1)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `exception thrown on navigation to invalid index`() {
+        manager = createWizardManager(initialStepIndex = LAST_STEP_INDEX)
+        manager.showNextStep()
+    }
+
+    private fun createWizardManager(initialStepIndex: Int) = WizardManager(STEPS, initialStepIndex)
 }
 
