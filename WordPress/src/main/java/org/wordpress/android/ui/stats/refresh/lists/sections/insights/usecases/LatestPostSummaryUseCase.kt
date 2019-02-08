7fb6a99d4ca4e863a309a168e2f210834080a2ba
Tyler Heck <theck13@users.noreply.github.com>
2019-02-08 08:49:00 -0700

    Update icon create blue medium drawable references to white

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/insights/usecases/LatestPostSummaryUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/insights/usecases/LatestPostSummaryUseCase.kt
@@ -95,7 +95,7 @@ class LatestPostSummaryUseCase
     private fun buildLink(model: InsightsLatestPostModel?): Link {
         return when {
             model == null -> Link(
-                    R.drawable.ic_create_blue_medium_24dp,
+                    R.drawable.ic_create_white_24dp,
                     R.string.stats_insights_create_post,
                     navigateAction = NavigationAction.create(this::onAddNewPostClick)
             )
