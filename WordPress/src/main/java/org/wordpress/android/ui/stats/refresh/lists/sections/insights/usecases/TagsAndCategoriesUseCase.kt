4d1c25a645e68f09ffb38ac4a07ba42f420ed9ec
Tyler Heck <theck13@users.noreply.github.com>
2019-02-08 18:06:45 -0700

    Update icon folder grey dark drawable references to white

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/insights/usecases/TagsAndCategoriesUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/insights/usecases/TagsAndCategoriesUseCase.kt
@@ -149,7 +149,7 @@ class TagsAndCategoriesUseCase
     }
 
     private fun getIcon(type: String) =
-            if (type == "tag") drawable.ic_tag_white_24dp else drawable.ic_folder_grey_dark_24dp
+            if (type == "tag") drawable.ic_tag_white_24dp else drawable.ic_folder_white_24dp
 
     private fun onLinkClick() {
         analyticsTracker.track(AnalyticsTracker.Stat.STATS_TAGS_AND_CATEGORIES_VIEW_MORE_TAPPED)
