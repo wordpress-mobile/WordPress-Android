630f454051c69d91e3653f63705bf2c74ddc37d1
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2019-02-11 14:50:55 +0200

    Introduce "use Gutenberg" for new posts App Setting
    And hardcode the `is...Enabled` App settings to only have Aztec and
    Gutenberg enabled. This way, the code that depends on those flags can
    still run fine while we're transitioning away from supporting the 2
    oldest editors.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostUtils.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostUtils.java
@@ -381,8 +381,8 @@ public class PostUtils {
         // Default to Gutenberg
 
         if (isNewPost || TextUtils.isEmpty(post.getContent())) {
-            // for a new post, use Gutenberg if the switch is set
-            return AppPrefs.isGutenbergEditorEnabled();
+            // for a new post, use Gutenberg if the "use for new posts" switch is set
+            return AppPrefs.isGutenbergDefaultForNewPosts();
         } else {
             // for already existing (and non-empty) posts, open Gutenberg only if the post contains blocks
             return contentContainsGutenbergBlocks(post.getContent());
