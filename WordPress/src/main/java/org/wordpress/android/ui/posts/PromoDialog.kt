898ff6a71fb20d8d7126b61d5534db16759235a5
Danilo Ercoli <ercoli@gmail.com>
2019-02-18 14:18:37 +0100

    Introduce UNDEFINED_RES_ID and use it instead of the "-1" hard coded value

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PromoDialog.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PromoDialog.kt
@@ -27,10 +27,11 @@ class PromoDialog : AppCompatDialogFragment() {
         private const val STATE_KEY_POSITIVE_BUTTON_LABEL = "state_key_positive_button_label"
         private const val STATE_KEY_NEGATIVE_BUTTON_LABEL = "state_key_negative_button_label"
         private const val STATE_KEY_NEUTRAL_BUTTON_LABEL = "state_key_neutral_button_label"
+        private const val UNDEFINED_RES_ID = -1
     }
 
     @DrawableRes
-    private var drawableResId: Int = -1
+    private var drawableResId: Int = UNDEFINED_RES_ID
     private lateinit var fragmentTag: String
     private lateinit var linkLabel: String
     private lateinit var message: String
@@ -56,7 +57,7 @@ class PromoDialog : AppCompatDialogFragment() {
         title: String,
         message: String,
         positiveButtonLabel: String,
-        @DrawableRes drawableResId: Int = -1,
+        @DrawableRes drawableResId: Int = UNDEFINED_RES_ID,
         negativeButtonLabel: String = "",
         linkLabel: String = "",
         neutralButtonLabel: String = ""
@@ -114,7 +115,7 @@ class PromoDialog : AppCompatDialogFragment() {
 
     private fun initializeView(view: View) {
         val imageContainer = view.findViewById<LinearLayout>(R.id.promo_dialog_image_container)
-        if (drawableResId == -1) {
+        if (drawableResId == UNDEFINED_RES_ID) {
             imageContainer.visibility = View.GONE
         } else {
             val image = view.findViewById<ImageView>(R.id.promo_dialog_image)
