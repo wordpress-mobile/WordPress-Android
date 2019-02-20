027f993897aab20e14b81749e8207b11312d3cf0
aerych <aerychj@gmail.com>
2019-02-20 16:24:59 -0600

    Publicize: Reorder switch statement that determines actions and listeners.
    Let OK and MUST_DISCONNECT statuses go to the disconnect flow.
    Let BROKEN and any other statuses go to the reconnect flow.

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/adapters/PublicizeConnectionAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/adapters/PublicizeConnectionAdapter.java
@@ -110,25 +110,25 @@ public class PublicizeConnectionAdapter extends RecyclerView.Adapter<PublicizeCo
     private void bindButton(ConnectButton btnConnect, final PublicizeConnection connection) {
         ConnectStatus status = connection.getStatusEnum();
         switch (status) {
-            case BROKEN:
-                btnConnect.setAction(ConnectAction.RECONNECT);
-                btnConnect.setOnClickListener(new View.OnClickListener() {
-                    @Override public void onClick(View view) {
-                        if (mActionListener != null) {
-                            mActionListener.onRequestReconnect(mService, connection);
-                        }
-                    }
-                });
-                break;
             case OK:
             case MUST_DISCONNECT:
-            default:
                 btnConnect.setAction(PublicizeConstants.ConnectAction.DISCONNECT);
                 btnConnect.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         if (mActionListener != null) {
                             mActionListener.onRequestDisconnect(connection);
+                        }
+                    }
+                });
+                break;
+            case BROKEN:
+            default:
+                btnConnect.setAction(ConnectAction.RECONNECT);
+                btnConnect.setOnClickListener(new View.OnClickListener() {
+                    @Override public void onClick(View view) {
+                        if (mActionListener != null) {
+                            mActionListener.onRequestReconnect(mService, connection);
                         }
                     }
                 });
