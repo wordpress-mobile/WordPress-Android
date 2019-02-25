c328a6fc82fd65f8f61d05264d23e324a111a020
Tyler Heck <theck13@users.noreply.github.com>
2019-02-25 13:09:55 -0700

    Update color utilities apply tint to drawable method to use context compat

--- WordPress/src/main/java/org/wordpress/android/util/ColorUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/util/ColorUtils.kt
@@ -4,23 +4,16 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
 import android.widget.ImageView
-
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 
 object ColorUtils {
     fun applyTintToDrawable(context: Context, @DrawableRes drawableResId: Int, @ColorRes colorResId: Int): Drawable {
         val drawable = context.resources.getDrawable(drawableResId, context.theme)
-        val color = if (Build.VERSION.SDK_INT >= 23) {
-            context.resources.getColor(colorResId, context.theme)
-        } else {
-            context.resources.getColor(colorResId)
-        }
-
+        val color = ContextCompat.getColor(context, colorResId)
         DrawableCompat.setTint(drawable, color)
         DrawableCompat.setTintMode(drawable, PorterDuff.Mode.SRC_IN)
         return drawable
