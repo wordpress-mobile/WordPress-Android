1e0cef5b60fdbb3cfc6842ccfe9a0ab5a9d7f6d7
vojtasmrcek <planarvoid@gmail.com>
2019-02-25 15:07:17 +0100

    Cleanup layouts and add label

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/viewholders/ActivityViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/viewholders/ActivityViewHolder.kt
@@ -5,6 +5,8 @@ import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.stats_block_single_activity.view.*
 import org.wordpress.android.R
 import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem.ActivityItem
 import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem.ActivityItem.Box
@@ -18,26 +20,29 @@ class ActivityViewHolder(val parent: ViewGroup) : BlockListItemViewHolder(
         parent,
         R.layout.stats_block_activity_item
 ) {
-    private val firstBlock = itemView.findViewById<RecyclerView>(R.id.first_block)
-    private val secondBlock = itemView.findViewById<RecyclerView>(R.id.second_block)
-    private val thirdBlock = itemView.findViewById<RecyclerView>(R.id.third_block)
+    private val firstBlock = itemView.findViewById<LinearLayout>(R.id.first_activity)
+    private val secondBlock = itemView.findViewById<LinearLayout>(R.id.second_activity)
+    private val thirdBlock = itemView.findViewById<LinearLayout>(R.id.third_activity)
 
     fun bind(
         item: ActivityItem
     ) {
         val widthInDp = parent.width / parent.context.resources.displayMetrics.density
-        drawBlock(firstBlock, item.blocks[0].boxes)
+        drawBlock(firstBlock.activity, item.blocks[0].boxes)
+        firstBlock.label.text = item.blocks[0].label
         val canFitTwoBlocks = widthInDp > 2 * BLOCK_WIDTH + GAP + SIZE_PADDING
         if (canFitTwoBlocks && item.blocks.size > 1) {
             secondBlock.visibility = View.VISIBLE
-            drawBlock(secondBlock, item.blocks[1].boxes)
+            drawBlock(secondBlock.activity, item.blocks[1].boxes)
+            secondBlock.label.text = item.blocks[1].label
         } else {
             secondBlock.visibility = View.GONE
         }
         val canFitThreeBlocks = widthInDp > 3 * BLOCK_WIDTH + 2 * GAP + SIZE_PADDING
         if (canFitThreeBlocks && item.blocks.size > 2) {
             thirdBlock.visibility = View.VISIBLE
-            drawBlock(thirdBlock, item.blocks[2].boxes)
+            drawBlock(thirdBlock.activity, item.blocks[2].boxes)
+            thirdBlock.label.text = item.blocks[2].label
         } else {
             thirdBlock.visibility = View.GONE
         }
