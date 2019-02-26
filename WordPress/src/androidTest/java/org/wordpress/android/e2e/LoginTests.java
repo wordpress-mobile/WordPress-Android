3dcacd483761e7c9cb55eec660756db1e31cfa0e
Javon <javondavis@users.noreply.github.com>
2019-02-26 02:05:10 -0500

    changed wp_com to wp_com_user

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/LoginTests.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/LoginTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.wordpress.android.e2e.pages.MePage;
 import org.wordpress.android.support.BaseTest;
 
-import static org.wordpress.android.BuildConfig.E2E_WP_COM_USERNAME;
+import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_USERNAME;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginTests extends BaseTest {
@@ -16,7 +16,7 @@ public class LoginTests extends BaseTest {
         wpLogin();
         sleep();
 
-        new MePage().go().verifyUsername(E2E_WP_COM_USERNAME);
+        new MePage().go().verifyUsername(E2E_WP_COM_USER_USERNAME);
         wpLogout();
     }
 }
