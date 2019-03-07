277a781ffe6e9dddb60e8283af93be10f7d30d45
vojtasmrcek <planarvoid@gmail.com>
2019-03-07 08:55:44 +0100

    Inject view model into detail activity

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/detail/StatsDetailActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/detail/StatsDetailActivity.kt
@@ -1,22 +1,31 @@
 package org.wordpress.android.ui.stats.refresh.lists.detail
 
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.toolbar.*
 import org.wordpress.android.R
+import org.wordpress.android.ui.stats.refresh.lists.StatsListViewModel.StatsSection
+import javax.inject.Inject
 
 class StatsDetailActivity : AppCompatActivity() {
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidInjection.inject(this)
 
         setContentView(R.layout.stats_detail_activity)
-
         setSupportActionBar(toolbar)
         supportActionBar?.let {
             it.setHomeButtonEnabled(true)
             it.setDisplayHomeAsUpEnabled(true)
         }
+        val viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(StatsSection.DETAIL.name, DetailListViewModel::class.java)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
