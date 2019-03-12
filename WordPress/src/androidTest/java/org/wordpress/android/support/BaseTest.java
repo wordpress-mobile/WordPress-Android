dc0320fc5011e6c22e2e91490b43e0a65349062e
James Treanor <jtreanor3@gmail.com>
2019-03-12 14:26:37 +0000

    Mock Login Tests networking using Wiremock

--- WordPress/src/androidTest/java/org/wordpress/android/support/BaseTest.java
+++ WordPress/src/androidTest/java/org/wordpress/android/support/BaseTest.java
@@ -1,6 +1,9 @@
 package org.wordpress.android.support;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import org.junit.Rule;
 import org.wordpress.android.R;
@@ -8,12 +11,21 @@ import org.wordpress.android.e2e.flows.LoginFlow;
 import org.wordpress.android.e2e.pages.MePage;
 import org.wordpress.android.e2e.pages.MySitesPage;
 import org.wordpress.android.ui.WPLaunchActivity;
+import org.wordpress.android.util.AssetFileSource;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_SITE_ADDRESS;
 import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_USERNAME;
 import static org.wordpress.android.support.WPSupportUtils.isElementDisplayed;
 
 public class BaseTest {
+    private static final String WIREMOCK_PATH = "wiremock";
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(
+            wireMockConfig().port(8080).fileSource(
+                    new AssetFileSource(InstrumentationRegistry.getContext().getAssets(), WIREMOCK_PATH)));
     @Rule
     public ActivityTestRule<WPLaunchActivity> mActivityTestRule = new ActivityTestRule<>(WPLaunchActivity.class);
 
