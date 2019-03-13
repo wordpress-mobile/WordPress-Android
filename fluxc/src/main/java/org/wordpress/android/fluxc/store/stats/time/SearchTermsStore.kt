913c38f191fc5cb777a31fe8d4db785d566cdf11
Ondrej Ruttkay <ondrej@automattic.com>
2019-03-13 10:45:10 +0100

    Add LimitMode to SearchTermsStore

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/stats/time/SearchTermsStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/stats/time/SearchTermsStore.kt
@@ -2,6 +2,8 @@ package org.wordpress.android.fluxc.store.stats.time
 
 import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.stats.LimitMode
+import org.wordpress.android.fluxc.model.stats.LimitMode.Top
 import org.wordpress.android.fluxc.model.stats.time.SearchTermsModel
 import org.wordpress.android.fluxc.model.stats.time.TimeStatsMapper
 import org.wordpress.android.fluxc.network.rest.wpcom.stats.time.SearchTermsRestClient
@@ -25,23 +27,23 @@ class SearchTermsStore
 ) {
     suspend fun fetchSearchTerms(
         site: SiteModel,
-        pageSize: Int,
         granularity: StatsGranularity,
+        limitMode: LimitMode.Top,
         date: Date,
         forced: Boolean = false
     ) = withContext(coroutineContext) {
-        val payload = restClient.fetchSearchTerms(site, granularity, date, pageSize + 1, forced)
+        val payload = restClient.fetchSearchTerms(site, granularity, date, limitMode.limit + 1, forced)
         return@withContext when {
             payload.isError -> OnStatsFetched(payload.error)
             payload.response != null -> {
                 sqlUtils.insert(site, payload.response, granularity, date)
-                OnStatsFetched(timeStatsMapper.map(payload.response, pageSize))
+                OnStatsFetched(timeStatsMapper.map(payload.response, limitMode))
             }
             else -> OnStatsFetched(StatsError(INVALID_RESPONSE))
         }
     }
 
-    fun getSearchTerms(site: SiteModel, period: StatsGranularity, pageSize: Int, date: Date): SearchTermsModel? {
-        return sqlUtils.selectSearchTerms(site, period, date)?.let { timeStatsMapper.map(it, pageSize) }
+    fun getSearchTerms(site: SiteModel, period: StatsGranularity, limitMode: LimitMode, date: Date): SearchTermsModel? {
+        return sqlUtils.selectSearchTerms(site, period, date)?.let { timeStatsMapper.map(it, limitMode) }
     }
 }
