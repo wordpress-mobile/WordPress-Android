8bfa67cf680e018b1f0d72a6ba89d4b8cd2fd403
Alex Forcier <alex.forcier@automattic.com>
2019-03-13 15:54:50 -0400

    Handle Dagger injection in BaseTest

--- WordPress/src/androidTest/java/org/wordpress/android/support/BaseTest.java
+++ WordPress/src/androidTest/java/org/wordpress/android/support/BaseTest.java
@@ -5,22 +5,37 @@ import android.support.test.rule.ActivityTestRule;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.wordpress.android.R;
+import org.wordpress.android.WordPress;
 import org.wordpress.android.e2e.flows.LoginFlow;
 import org.wordpress.android.e2e.pages.MePage;
 import org.wordpress.android.e2e.pages.MySitesPage;
+import org.wordpress.android.modules.AppComponentTest;
+import org.wordpress.android.modules.DaggerAppComponentTest;
 import org.wordpress.android.ui.WPLaunchActivity;
 import org.wordpress.android.util.AssetFileSource;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-
 import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_SITE_ADDRESS;
 import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_USERNAME;
 import static org.wordpress.android.support.WPSupportUtils.isElementDisplayed;
 
 public class BaseTest {
+    protected WordPress mAppContext;
+    protected AppComponentTest mMockedAppComponent;
+
     private static final String WIREMOCK_PATH = "wiremock";
+
+    @Before
+    public void setup() {
+        mAppContext =
+                (WordPress) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
+        mMockedAppComponent = DaggerAppComponentTest.builder()
+                                                    .application(mAppContext)
+                                                    .build();
+    }
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(
