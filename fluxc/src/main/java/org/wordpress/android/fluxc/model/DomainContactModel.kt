de1da1ea7774dc2ccf7eb9a56520c34af4712b7f
Klymentiy haykov <forsver@gmail.com>
2019-03-14 04:27:31 +0200

    Added serialized names to domain contact model.

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/DomainContactModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/DomainContactModel.kt
@@ -1,14 +1,22 @@
 package org.wordpress.android.fluxc.model
 
+import com.google.gson.annotations.SerializedName
+
 class DomainContactModel(
+    @SerializedName("first_name")
     val firstName: String?,
+    @SerializedName("last_name")
     val lastName: String?,
     val organization: String?,
+    @SerializedName("address_1")
     val addressLine1: String?,
+    @SerializedName("address_2")
     val addressLine2: String?,
+    @SerializedName("postal_code")
     val postalCode: String?,
     val city: String?,
     val state: String?,
+    @SerializedName("country_code")
     val countryCode: String?,
     val email: String?,
     val phone: String?,
