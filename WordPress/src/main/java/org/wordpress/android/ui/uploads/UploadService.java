wordpress-mobile/WordPress-Android

commit 757cbb05b76a7d3a71da9522bbad85265221f6e9

fixed bug that would count current editing Post into the upload queue when adding media only, plus only upload posts that are publishable

WordPress/src/main/java/org/wordpress/android/ui/uploads/UploadService.java
@@ -684,14 +684,17 @@ public class UploadService extends Service {
             return false;
         }
 
-        SiteModel site = mSiteStore.getSiteByLocalId(postToCancel.getLocalSiteId());
-        mPostUploadNotifier.incrementUploadedPostCountFromForegroundNotification(postToCancel);
+        if (PostUploadHandler.isPostUploadingOrQueued(postToCancel) && !isPostCurrentlyBeingEdited(postToCancel)) {
+            // post is not being edited and is currently queued, update the count on the foreground notification
+            mPostUploadNotifier.incrementUploadedPostCountFromForegroundNotification(postToCancel);
+        }
 
         if (showError || mUploadStore.isFailedPost(postToCancel)) {
             // Only show the media upload error notification if the post is NOT registered in the UploadStore
             // - otherwise if it IS registered in the UploadStore and we get a `cancelled` signal it means
             // the user actively cancelled it. No need to show an error then.
             String message = UploadUtils.getErrorMessage(this, postToCancel, errorMessage, true);
+            SiteModel site = mSiteStore.getSiteByLocalId(postToCancel.getLocalSiteId());
             mPostUploadNotifier.updateNotificationErrorForPost(postToCancel, site, message,
                                                                mUploadStore.getFailedMediaForPost(postToCancel).size());
         }
@@ -920,8 +923,10 @@ public class UploadService extends Service {
                         }
                         // TODO Should do some extra validation here
                         // e.g. what if the post has local media URLs but no pending media uploads?
-                        mPostUploadHandler.upload(updatedPost);
-                        return true;
+                        if (PostUtils.isPublishable(updatedPost)) {
+                            mPostUploadHandler.upload(updatedPost);
+                            return true;
+                        }
                     }
                 }
             }

commit 51be57b584543f87c0ab4618ff3cedf46ce38204

removed wrong check introduced in commit 757cbb05b76a7d3a71da9522bbad85265221f6e9 that would prevent untitled drafts from being uploaded in certain situations

WordPress/src/main/java/org/wordpress/android/ui/uploads/UploadService.java
@@ -923,10 +923,8 @@ public class UploadService extends Service {
                         }
                         // TODO Should do some extra validation here
                         // e.g. what if the post has local media URLs but no pending media uploads?
-                        if (PostUtils.isPublishable(updatedPost)) {
-                            mPostUploadHandler.upload(updatedPost);
-                            return true;
-                        }
+                        mPostUploadHandler.upload(updatedPost);
+                        return true;
                     }
                 }
             }