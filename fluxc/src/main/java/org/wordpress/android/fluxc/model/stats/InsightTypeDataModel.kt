7b54ba0732c67023866d22e905c35eba94ee5a92
Ondrej Ruttkay <0nko@users.noreply.github.com>
2019-03-21 10:54:30 +0100

    Revert "Manage insights in stats"

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/stats/InsightTypeDataModel.kt
+++
@@ -1,9 +0,0 @@
-package org.wordpress.android.fluxc.model.stats
-
-import org.wordpress.android.fluxc.store.StatsStore.InsightsTypes
-
-data class InsightTypeDataModel(val type: InsightsTypes, val status: Status, val position: Int?) {
-    enum class Status {
-        ADDED, REMOVED, NEW
-    }
-}
