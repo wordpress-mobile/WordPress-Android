65b06719c79fedeaa4049ad91a602fbba984313f
Klymentiy haykov <forsver@gmail.com>
2019-03-24 01:47:42 +0200

    Minor refactoring.

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/transactions/Country.kt
+++
@@ -1,3 +0,0 @@
-package org.wordpress.android.fluxc.network.rest.wpcom.transactions
-
-data class Country(val code: String, val name: String)
---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/transactions/SupportedDomainCountry.kt
@@ -0,0 +1,3 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.transactions
+
+data class SupportedDomainCountry(val code: String, val name: String)
