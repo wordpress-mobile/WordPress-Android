aa0263c069fd0f56a6971a75d4c6830972fa6ee9
malinajirka <malinajirka@gmail.com>
2019-03-25 11:46:27 +0100

    Introduce methods to show/hide textView and ImageView in UIHelpers

--- WordPress/src/main/java/org/wordpress/android/ui/utils/UiHelpers.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/utils/UiHelpers.kt
@@ -1,7 +1,11 @@
 package org.wordpress.android.ui.utils
 
 import android.content.Context
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.ui.utils.UiString.UiStringText
 import javax.inject.Inject
@@ -15,5 +19,28 @@ class UiHelpers @Inject constructor() {
 
     fun updateVisibility(view: View, visible: Boolean) {
         view.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+
+    fun setTextOrHide(view: TextView, uiString: UiString?) {
+        view.visibility = if (uiString == null) View.GONE else View.VISIBLE
+        uiString?.let {
+            view.text = getTextOfUiString(view.context, uiString)
+        }
+    }
+
+    fun setTextOrHide(view: TextView, @StringRes resId: Int?) {
+        val uiString = if (resId != null) {
+            UiStringRes(resId)
+        } else {
+            null
+        }
+        setTextOrHide(view, uiString)
+    }
+
+    fun setImageOrHide(imageView: ImageView, @DrawableRes resId: Int?) {
+        imageView.visibility = if (resId == null) View.GONE else View.VISIBLE
+        resId?.let {
+            imageView.setImageResource(resId)
+        }
     }
 }
