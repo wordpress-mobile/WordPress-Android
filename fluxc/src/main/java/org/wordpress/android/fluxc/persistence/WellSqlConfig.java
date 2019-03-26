03ced27bb4fff79076042a5d63fd26a0f1c271b7
vojtasmrcek <planarvoid@gmail.com>
2019-03-26 08:49:23 +0100

    Fix crash in stats migration

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.java
@@ -44,7 +44,7 @@ public class WellSqlConfig extends DefaultWellConfig {
 
     @Override
     public int getDbVersion() {
-        return 61;
+        return 62;
     }
 
     @Override
@@ -478,6 +478,12 @@ public class WellSqlConfig extends DefaultWellConfig {
                         + "BLOCK_TYPE TEXT NOT NULL,STATS_TYPE TEXT NOT NULL,DATE TEXT,TIME_STAMP INTEGER,"
                         + "REQUESTED_ITEMS INTEGER)");
                 oldVersion++;
+            case 61:
+                db.execSQL("DROP TABLE StatsRequest");
+                db.execSQL(
+                        "CREATE TABLE StatsRequest (_id INTEGER PRIMARY KEY AUTOINCREMENT,LOCAL_SITE_ID INTEGER,"
+                        + "BLOCK_TYPE TEXT NOT NULL,STATS_TYPE TEXT NOT NULL,DATE TEXT,POST_ID INTEGER,TIME_STAMP "
+                        + "INTEGER,REQUESTED_ITEMS INTEGER)");
         }
         db.setTransactionSuccessful();
         db.endTransaction();
