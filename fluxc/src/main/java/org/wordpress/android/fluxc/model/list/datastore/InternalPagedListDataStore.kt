936ffe16143e951dea60258ea116070079a0ab4a
Oguz Kocer <oguz.kocer@automattic.com>
2019-03-27 19:25:48 -0400

    Updates InternalPagedListDataStore for the ListItemDataStoreInterface changes

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/list/datastore/InternalPagedListDataStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/list/datastore/InternalPagedListDataStore.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.fluxc.model.list.datastore
 
 import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
+import org.wordpress.android.fluxc.model.list.ListDescriptor
 
 /**
  * An internal class that plays the middle man between `PagedListFactory` and [ListItemDataStoreInterface] by extracting
@@ -8,18 +9,23 @@ import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
  *
  * It takes a snapshot of the identifiers for the list when it's created and propagates the calls from the PagedList
  * data source to [ListItemDataStoreInterface] using these identifiers.
+ *
+ * [LD] represents the ListDescriptor
+ * [ID] represents the identifier for the type [T]
+ * [T] represents the end result type that'll be used by `PagedList`.
  */
-class InternalPagedListDataStore<T>(
+class InternalPagedListDataStore<LD: ListDescriptor, ID, T>(
+    private val listDescriptor: LD,
     remoteItemIds: List<RemoteId>,
     isListFullyFetched: Boolean,
-    private val itemDataStore: ListItemDataStoreInterface<T>
+    private val itemDataStore: ListItemDataStoreInterface<LD, ID, T>
 ) {
     /*
-     * PagedList library takes a snapshot of the data. It does the heavy lifting by caching the items provided to it,
+     * PagedList library needs a snapshot of the data. It does the heavy lifting by caching the items provided to it,
      * but it still needs a consistent list of identifiers to work with. In order to do that, we take a snapshot of the
      * current identifiers and work with those until a new instance is created by PagedList.
      */
-    private val itemIdentifiers = itemDataStore.getItemIdentifiers(remoteItemIds, isListFullyFetched)
+    private val itemIdentifiers = itemDataStore.getItemIdentifiers(listDescriptor, remoteItemIds, isListFullyFetched)
 
     /**
      * Number of items the list contains.
@@ -33,12 +39,13 @@ class InternalPagedListDataStore<T>(
      * Returns the list of items [T] by propagating the call to [ListItemDataStoreInterface]
      */
     fun getItemsInRange(startPosition: Int, endPosition: Int): List<T> =
-            itemDataStore.getItemsAndFetchIfNecessary(getItemIds(startPosition, endPosition))
+            itemDataStore.getItemsAndFetchIfNecessary(listDescriptor, getItemIds(startPosition, endPosition))
 
     /**
-     * Helper function that returns the list of [ListItemIdentifier]s from [itemIdentifiers].
+     * Helper function that returns the list of identifiers [ID] for the given start and end positions using the
+     * internal [itemIdentifiers].
      */
-    private fun getItemIds(startPosition: Int, endPosition: Int): List<ListItemIdentifier> {
+    private fun getItemIds(startPosition: Int, endPosition: Int): List<ID> {
         if (startPosition < 0 || endPosition < 0 || startPosition > endPosition || endPosition >= totalSize) {
             throw IllegalArgumentException("Illegal start or end position")
         }
