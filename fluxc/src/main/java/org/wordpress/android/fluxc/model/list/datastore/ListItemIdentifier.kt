30cf0231120dee5795143da219c165d3026b651c
Oguz Kocer <oguz.kocer@automattic.com>
2019-03-27 19:28:57 -0400

    Removes unnecessary ListItemIdentifier

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/list/datastore/ListItemIdentifier.kt
+++
@@ -1,7 +0,0 @@
-package org.wordpress.android.fluxc.model.list.datastore
-
-/**
- * An empty interface used by [InternalPagedListDataStore] to give a structured but flexible way to identify list
- * items.
- */
-interface ListItemIdentifier
