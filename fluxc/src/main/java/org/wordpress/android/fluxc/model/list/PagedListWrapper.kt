931567bdb931cbdc1439b31f0cda03b2bd8ebd2b
malinajirka <malinajirka@gmail.com>
2019-04-05 14:20:43 +0200

    Minor changes to coroutineContext in ListStore and PagedListWrapper

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/list/PagedListWrapper.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/list/PagedListWrapper.kt
@@ -6,7 +6,8 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.OnLifecycleEvent
 import android.arch.paging.PagedList
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
@@ -36,8 +37,13 @@ class PagedListWrapper<T>(
     private val refresh: () -> Unit,
     private val invalidate: () -> Unit,
     private val isListEmpty: () -> Boolean,
-    private val coroutineContext: CoroutineContext
-) : LifecycleObserver {
+    private val parentCoroutineContext: CoroutineContext
+) : LifecycleObserver, CoroutineScope {
+    private var job: Job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = parentCoroutineContext + job
+
     private val _isFetchingFirstPage = MutableLiveData<Boolean>()
     val isFetchingFirstPage: LiveData<Boolean> = _isFetchingFirstPage
 
@@ -70,13 +76,14 @@ class PagedListWrapper<T>(
     private fun onDestroy() {
         lifecycle.removeObserver(this)
         dispatcher.unregister(this)
+        job.cancel()
     }
 
     /**
      * A method to be used by clients to refresh the first page of a list from network.
      */
     fun fetchFirstPage() {
-        GlobalScope.launch(coroutineContext) {
+        launch {
             refresh()
         }
     }
@@ -138,7 +145,7 @@ class PagedListWrapper<T>(
      * A helper function that checks and post if a list is empty.
      */
     private fun updateIsEmpty() {
-        GlobalScope.launch(coroutineContext) {
+        launch {
             _isEmpty.postValue(isListEmpty())
         }
     }
