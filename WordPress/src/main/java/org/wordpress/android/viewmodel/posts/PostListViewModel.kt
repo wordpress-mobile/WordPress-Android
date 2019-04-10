a2c53597808fc034deb8e475b4a8edaf11de21d8
Oguz Kocer <oguz.kocer@automattic.com>
2019-04-10 16:29:33 -0400

    Revert "Improve PostListVM.showDeletePostConfirmationDialog"
    This reverts commit 6365cc1fb77d55f77bc656024b1ef966bfffcc3a.

--- WordPress/src/main/java/org/wordpress/android/viewmodel/posts/PostListViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/posts/PostListViewModel.kt
@@ -414,10 +414,20 @@ class PostListViewModel @Inject constructor(
     }
 
     private fun showDeletePostConfirmationDialog(post: PostModel) {
+        // We need network connection to delete a remote post, but not a local draft
+        if (!post.isLocalDraft && !checkNetworkConnection()) {
+            return
+        }
+        val messageRes = if (!UploadService.isPostUploadingOrQueued(post)) {
+            R.string.dialog_confirm_delete_permanently_post
+        } else {
+            // TODO Can this ever happen? - delete action is disabled when an upload is in progress
+            R.string.dialog_confirm_cancel_post_media_uploading
+        }
         val dialogHolder = DialogHolder(
                 tag = CONFIRM_DELETE_POST_DIALOG_TAG,
                 title = UiStringRes(R.string.delete_post),
-                message = UiStringRes(R.string.dialog_confirm_delete_permanently_post),
+                message = UiStringRes(messageRes),
                 positiveButton = UiStringRes(R.string.delete),
                 negativeButton = UiStringRes(R.string.cancel)
         )
