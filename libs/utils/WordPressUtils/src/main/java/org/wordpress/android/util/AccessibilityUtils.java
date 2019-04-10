93b294f77818c6b81b7f47835b4081dbc9b7203a
Tyler Heck <theck13@users.noreply.github.com>
2019-04-09 18:27:38 -0700

    Update get snackbar duration method in accessibility utils for indefinite constant

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/AccessibilityUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/AccessibilityUtils.java
@@ -22,12 +22,14 @@ public class AccessibilityUtils {
     }
 
     /**
+     * If the default duration is LENGTH_INDEFINITE, ignore accessibility duration and return LENGTH_INDEFINITE.
      * If the accessibility is enabled, returns increased snackbar duration, otherwise returns defaultDuration.
      *
      * @param defaultDuration Either be one of the predefined lengths: LENGTH_SHORT, LENGTH_LONG, or a custom duration
      *                        in milliseconds.
      */
     public static int getSnackbarDuration(Context ctx, int defaultDuration) {
-        return isAccessibilityEnabled(ctx) ? SNACKBAR_WITH_ACTION_DURATION_IN_MILLIS : defaultDuration;
+        return defaultDuration == Snackbar.LENGTH_INDEFINITE ? Snackbar.LENGTH_INDEFINITE
+                : isAccessibilityEnabled(ctx) ? SNACKBAR_WITH_ACTION_DURATION_IN_MILLIS : defaultDuration;
     }
 }
