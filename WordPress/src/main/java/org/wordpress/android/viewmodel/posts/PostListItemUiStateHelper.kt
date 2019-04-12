050e3ad7a1adde6082b84d9f3e4cd69ac16cd3dd
Oguz Kocer <oguz.kocer@automattic.com>
2019-04-12 17:32:42 -0400

    Implements move to draft for post list

--- WordPress/src/main/java/org/wordpress/android/viewmodel/posts/PostListItemUiStateHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/posts/PostListItemUiStateHelper.kt
@@ -260,7 +260,7 @@ class PostListItemUiStateHelper @Inject constructor(private val appPrefsWrapper:
             isLocalDraft -> buttonTypes.add(PostListButtonType.BUTTON_DELETE)
             postStatus == PostStatus.TRASHED -> {
                 buttonTypes.add(PostListButtonType.BUTTON_DELETE)
-                buttonTypes.add(PostListButtonType.BUTTON_RESTORE)
+                buttonTypes.add(PostListButtonType.BUTTON_MOVE_TO_DRAFT)
             }
             postStatus != PostStatus.TRASHED -> buttonTypes.add(PostListButtonType.BUTTON_TRASH)
         }
