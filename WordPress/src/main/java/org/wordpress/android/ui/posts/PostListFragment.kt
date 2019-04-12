c69f0f780e5aed21461f5b533797f0b66f1c5303
malinajirka <malinajirka@gmail.com>
2019-04-12 18:58:56 +0200

    Remove unused imports

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostListFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostListFragment.kt
@@ -10,13 +10,11 @@ import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ProgressBar
-import android.widget.Toast
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.fluxc.model.SiteModel
