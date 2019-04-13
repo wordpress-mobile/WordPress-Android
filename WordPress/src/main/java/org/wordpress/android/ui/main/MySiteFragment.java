5618b8cde93405b96208180df3f9481bbae36519
Tyler Heck <theck13@users.noreply.github.com>
2019-04-12 20:43:03 -0500

    Remove unused import statement from my site fragment class

--- WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.PopupMenu;
