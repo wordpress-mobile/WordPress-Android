f817bdd770aab4e8a7a210c31b180b8f07e73988
Oguz Kocer <oguz.kocer@automattic.com>
2019-04-15 19:39:52 -0400

    Fixes site icon not updating correctly after switch site

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/SiteSettingsInterface.java
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/SiteSettingsInterface.java
@@ -193,6 +193,8 @@ public abstract class SiteSettingsInterface {
         SiteSettingsTable.saveSettings(mSettings);
     }
 
+    public int getLocalSiteId() { return mSite.getId(); }
+
     public @NonNull String getTitle() {
         return mSettings.title == null ? "" : mSettings.title;
     }
