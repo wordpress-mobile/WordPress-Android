1c9e5d6b924d34d681bf4cf3ba9d0b773838a7b5
Oguz Kocer <oguz.kocer@automattic.com>
2019-04-15 18:20:31 -0400

    Don't send the external user id for new twitter publicize connections

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeActions.java
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeActions.java
@@ -161,7 +161,8 @@ public class PublicizeActions {
 
         Map<String, String> params = new HashMap<>();
         params.put("keyring_connection_ID", Long.toString(keyringConnectionId));
-        if (!externalUserId.isEmpty()) {
+        // Sending the external id for Twitter connections result in an error
+        if (!externalUserId.isEmpty() && !serviceId.equals(PublicizeConstants.TWITTER_ID)) {
             params.put("external_user_ID", externalUserId);
         }
         String path = String.format(Locale.ROOT, "/sites/%d/publicize-connections/new", siteId);
