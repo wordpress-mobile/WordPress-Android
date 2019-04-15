b4f3bbcef4d52269d947e3d5892cdfc16e42d3b9
Ondrej Ruttkay <ondrej@automattic.com>
2019-04-16 00:59:16 +0200

    Fix the unit tests

--- WordPress/src/test/java/org/wordpress/android/ui/stats/refresh/lists/UiModelMapperTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/stats/refresh/lists/UiModelMapperTest.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.stats.refresh.lists
 
+import android.arch.lifecycle.MutableLiveData
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
@@ -8,9 +9,12 @@ import org.mockito.Mock
 import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.fluxc.store.StatsStore.InsightsTypes
 import org.wordpress.android.fluxc.store.StatsStore.InsightsTypes.FOLLOWER_TOTALS
+import org.wordpress.android.ui.stats.refresh.NavigationTarget
+import org.wordpress.android.ui.stats.refresh.lists.StatsBlock.Success
 import org.wordpress.android.ui.stats.refresh.lists.StatsListViewModel.UiModel
 import org.wordpress.android.ui.stats.refresh.lists.sections.BaseStatsUseCase.UseCaseModel
 import org.wordpress.android.ui.stats.refresh.lists.sections.BaseStatsUseCase.UseCaseModel.UseCaseState.SUCCESS
+import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem.LinkButton
 import org.wordpress.android.util.NetworkUtilsWrapper
 
 class UiModelMapperTest : BaseUnitTest() {
@@ -25,15 +29,19 @@ class UiModelMapperTest : BaseUnitTest() {
     @Test
     fun `mapInsights returns success ui model when all the inputs are successful`() {
         var error: Int? = null
-        val uiModel = mapper.mapInsights(listOf(UseCaseModel(FOLLOWER_TOTALS, data = listOf(), state = SUCCESS))) {
+        val navigationTarget = MutableLiveData<NavigationTarget>()
+        val uiModel = mapper.mapInsights(listOf(
+                UseCaseModel(FOLLOWER_TOTALS, data = listOf(), state = SUCCESS)), navigationTarget) {
             error = it
         }
 
         val model = uiModel as UiModel.Success
-        assertThat(model.data).hasSize(1)
-        assertThat(model.data[0].statsTypes).isEqualTo(InsightsTypes.FOLLOWER_TOTALS)
+        assertThat(model.data).hasSize(2)
+        assertThat((model.data[0] as Success).statsTypes).isEqualTo(FOLLOWER_TOTALS)
         assertThat(model.data[0].type).isEqualTo(StatsBlock.Type.SUCCESS)
         assertThat(model.data[0].data).isEmpty()
+        assertThat(model.data[1].type).isEqualTo(StatsBlock.Type.CONTROL)
+        assertThat(model.data[1].data.first() is LinkButton).isTrue()
         assertThat(error).isNull()
     }
 }
