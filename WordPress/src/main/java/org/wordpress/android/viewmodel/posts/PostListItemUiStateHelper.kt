5efd390b10c138f6f129ab5a043a81480e7437ef
John Kartupelis <john.kartupelis@gmail.com>
2019-04-26 16:31:06 +0100

    PostListItemUiStateHelper: fully qualify more item
    Do not mix and match the fully qualified version and the unqualified
    version. Just use PostListItemAction.MoreItem throughout.

--- WordPress/src/main/java/org/wordpress/android/viewmodel/posts/PostListItemUiStateHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/posts/PostListItemUiStateHelper.kt
@@ -24,7 +24,6 @@ import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.ui.utils.UiString.UiStringText
 import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.AppLog.T.POSTS
-import org.wordpress.android.viewmodel.posts.PostListItemAction.MoreItem
 import org.wordpress.android.viewmodel.posts.PostListItemIdentifier.LocalPostId
 import org.wordpress.android.viewmodel.posts.PostListItemIdentifier.RemotePostId
 import org.wordpress.android.viewmodel.posts.PostListItemType.PostListItemUiState
@@ -314,7 +313,7 @@ class PostListItemUiStateHelper @Inject constructor(private val appPrefsWrapper:
     private fun createCompactViewActions(
         buttonTypes: List<PostListButtonType>,
         onButtonClicked: (PostListButtonType) -> Unit
-    ): MoreItem {
+    ): PostListItemAction.MoreItem {
         val allItems = buttonTypes.map { buttonType: PostListButtonType ->
             PostListItemAction.SingleItem(buttonType, onButtonClicked)
         }
