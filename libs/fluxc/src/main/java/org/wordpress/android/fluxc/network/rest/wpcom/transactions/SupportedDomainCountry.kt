e6649a23c752ec97703bec1762b02762d11556d7
Klymentiy haykov <forsver@gmail.com>
2019-05-02 13:26:05 -0700

    Rarcelized some models.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/transactions/SupportedDomainCountry.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/transactions/SupportedDomainCountry.kt
@@ -1,3 +1,10 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.transactions
 
-data class SupportedDomainCountry(val code: String, val name: String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class SupportedDomainCountry(
+    val code: String,
+    val name: String
+) : Parcelable
