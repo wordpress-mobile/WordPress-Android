1acfc96056dd9558f7e93d914455dd684e519455
James Treanor <jtreanor3@gmail.com>
2019-05-02 15:30:56 +0100

    Extract a universal .apk as well as the app bundle

--- tools/build-app-bundle-core.sh
+++ tools/build-app-bundle-core.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Check for bundletool
+command -v bundletool > /dev/null || { echo "bundletool is required to build the APKs. Install it with 'brew install bundletool'" >&2; exit 1; }
+
 # Exit if any command fails
 set -eu
 # Print the logs on failure
@@ -9,6 +12,12 @@ function cleanup {
 trap cleanup ERR
 
 # This script defines some shared functions that are used by the build-app-bundle* set.
+
+function get_gradle_property {
+    PROP_KEY=$1
+    PROP_VALUE=`cat "gradle.properties" | grep "$PROP_KEY" | cut -d'=' -f2`
+    echo $PROP_VALUE
+}
 
 function gradle_version_name {
   grep -E 'versionName' $BUILDFILE | sed s/versionName// | grep -Eo "[a-zA-Z0-9.-]+"
@@ -25,6 +34,7 @@ function build_app_bundle {
   version_code=`gradle_version_code`
   version_name=`gradle_version_name`
   name="wpandroid-$version_name.aab"
+  apk_name="wpandroid-$version_name-universal.apk"
   aab="WordPress.aab"
 
   echo "Cleaning in branch: $branch" | tee -a $LOGFILE
@@ -35,7 +45,26 @@ function build_app_bundle {
   ./gradlew bundle"$flavor"Release >> $LOGFILE 2>&1
   cp -v $OUTDIR/"$flavor"Release/$aab $BUILDDIR/$name | tee -a $LOGFILE
   echo "Bundle ready: $name" | tee -a $LOGFILE
+  extract_universal_apk $BUILDDIR/$name $BUILDDIR/$apk_name
   BUILD_APP_BUNDLE_RET_VALUE=$version_code
+}
+
+function extract_universal_apk {
+  app_bundle="$1"
+  apk_output="$2"
+  tmp_dir=$(mktemp -d)
+
+  echo "Extracting universal APK..." | tee -a $LOGFILE
+  bundletool build-apks --bundle="$app_bundle" \
+                        --output="$tmp_dir/universal.apks" \
+                        --mode=universal \
+                        --ks="$(get_gradle_property storeFile)" \
+                        --ks-pass="pass:$(get_gradle_property storePassword)" \
+                        --ks-key-alias="$(get_gradle_property keyAlias)" \
+                        --key-pass="pass:$(get_gradle_property keyPassword)" >> $LOGFILE 2>&1
+  
+  unzip "$tmp_dir/universal.apks" -d "$tmp_dir"  >> $LOGFILE 2>&1
+  cp "$tmp_dir/universal.apk" "$apk_output" | tee -a $LOGFILE
 }
 
 function check_clean_working_dir {
--- tools/install-app-bundle.sh
+++ tools/install-app-bundle.sh
@@ -8,21 +8,11 @@ command -v bundletool > /dev/null || { echo "bundletool is required to build the
 # Exit if any command fails
 set -eu
 
-function get_gradle_property {
-    PROP_KEY=$1
-    PROP_VALUE=`cat "gradle.properties" | grep "$PROP_KEY" | cut -d'=' -f2`
-    echo $PROP_VALUE
-}
-
 APP_BUNDLE="$1"
 TMP_DIR=$(mktemp -d)
 
 echo "Generating APKs..."
 bundletool build-apks --bundle="$APP_BUNDLE" \
-                      --output="$TMP_DIR/output.apks" \
-                      --ks="$(get_gradle_property storeFile)" \
-                      --ks-pass="pass:$(get_gradle_property storePassword)" \
-                      --ks-key-alias="$(get_gradle_property keyAlias)" \
-                      --key-pass="pass:$(get_gradle_property keyPassword)"
+                      --output="$TMP_DIR/output.apks"
 echo "Installing..."
 bundletool install-apks --apks="$TMP_DIR/output.apks"
