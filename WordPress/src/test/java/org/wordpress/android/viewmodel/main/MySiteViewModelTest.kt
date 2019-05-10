ff62d5f0a26805bd4d2d6467e2d7a0c88f78cce8
Tyler Marien <tyler.marien@gmail.com>
2019-05-09 22:52:01 -0600

    Added tests to make sure setSite only fires observers when new site id is different from the old site id

--- WordPress/src/test/java/org/wordpress/android/viewmodel/main/MySiteViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/main/MySiteViewModelTest.kt
@@ -1,8 +1,11 @@
 package org.wordpress.android.viewmodel.main
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
 import org.junit.Before
@@ -11,6 +14,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
+import org.mockito.Mockito.never
+import org.mockito.Mockito.times
 import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.SiteActionBuilder
@@ -34,7 +39,9 @@ class MySiteViewModelTest {
     @Mock private lateinit var store: SiteStore
     private lateinit var viewModel: MySiteViewModel
 
-    private val site = SiteModel()
+    @Mock private lateinit var siteObserver: Observer<SiteModel>
+
+    private val site = SiteModel().apply { id = 1 }
 
     @Before
     fun setUp() {
@@ -104,5 +111,28 @@ class MySiteViewModelTest {
         }
 
         viewModel.loadPlans(site)
+    }
+
+    @Test
+    fun setSiteWillFireObserversIfNewSiteIsDifferentThanOldSite() {
+        viewModel.site.value = site
+        viewModel.site.observeForever(siteObserver)
+
+        val site = SiteModel().apply { id = 2 }
+        viewModel.setSite(site)
+
+        verify(siteObserver, times(1)).onChanged(site)
+        assertEquals(2, viewModel.site.value?.id)
+    }
+
+    @Test
+    fun setSiteWillNotFireObserversIfNewSiteHasSameIdAsOldSite() {
+        viewModel.site.value = site
+        viewModel.site.observeForever(siteObserver)
+
+        val site = SiteModel().apply { id = 1 }
+        viewModel.setSite(site)
+
+        verify(siteObserver, never()).onChanged(site)
     }
 }
