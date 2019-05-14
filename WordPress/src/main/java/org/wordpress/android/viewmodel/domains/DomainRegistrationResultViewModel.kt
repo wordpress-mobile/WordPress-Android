8bb6abbb1258a0da24aa602bcdcd0e414fa633af
Tyler Marien <tyler.marien@gmail.com>
2019-05-13 19:33:29 -0600

    Fixed formatting issues

--- WordPress/src/main/java/org/wordpress/android/viewmodel/domains/DomainRegistrationResultViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/domains/DomainRegistrationResultViewModel.kt
@@ -2,7 +2,7 @@ package org.wordpress.android.viewmodel.domains
 
 import android.arch.lifecycle.ViewModel
 
-class DomainRegistrationResultViewModel: ViewModel() {
+class DomainRegistrationResultViewModel : ViewModel() {
     var domainName: String? = null
     var email: String? = null
 }
