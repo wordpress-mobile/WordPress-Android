72a9ab31cc896514676ca8cb4082596141037ab3
Javon <javondavis@users.noreply.github.com>
2019-05-15 12:08:11 -0500

    resolve import issue for isDescendant

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/EditorPage.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/EditorPage.java
@@ -13,6 +13,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
