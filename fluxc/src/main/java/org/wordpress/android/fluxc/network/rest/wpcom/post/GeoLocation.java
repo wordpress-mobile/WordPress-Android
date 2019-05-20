04875e4be918c4ba1211d139ea854c4bee400f56
malinajirka <malinajirka@gmail.com>
2019-05-20 13:45:17 +0200

    Revert: Convert GeoLocation and PostParent to kotlin

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/post/GeoLocation.java
@@ -0,0 +1,9 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.post;
+
+import org.wordpress.android.fluxc.network.rest.JsonObjectOrFalse;
+
+public class GeoLocation extends JsonObjectOrFalse {
+    public double latitude;
+    public double longitude;
+    public String address;
+}
