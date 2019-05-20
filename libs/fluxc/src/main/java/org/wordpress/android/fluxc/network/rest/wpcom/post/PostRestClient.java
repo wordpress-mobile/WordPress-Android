df3805f6f04d5464e2489323347b83f7d63cce73
malinajirka <malinajirka@gmail.com>
2019-05-20 13:45:17 +0200

    Revert: Convert GeoLocation and PostParent to kotlin

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/post/PostRestClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/post/PostRestClient.java
@@ -346,8 +346,8 @@ public class PostRestClient extends BaseWPComRestClient {
         }
         post.setPostFormat(from.getFormat());
         if (from.getGeo() != null) {
-            post.setLatitude(from.getGeo().getLatitude());
-            post.setLongitude(from.getGeo().getLongitude());
+            post.setLatitude(from.getGeo().latitude);
+            post.setLongitude(from.getGeo().longitude);
         }
 
         if (from.getCategories() != null) {
@@ -373,8 +373,8 @@ public class PostRestClient extends BaseWPComRestClient {
         }
 
         if (from.getParent() != null) {
-            post.setParentId(from.getParent().getId());
-            post.setParentTitle(from.getParent().getTitle());
+            post.setParentId(from.getParent().ID);
+            post.setParentTitle(from.getParent().title);
         }
 
         return post;
