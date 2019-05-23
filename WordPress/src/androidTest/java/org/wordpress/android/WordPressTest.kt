a915b144a4e31615aee3d3cb763128ddaeb19951
Maxime Biais <maxime.biais@gmail.com>
2019-05-23 10:45:18 +0200

    Add WorkerManager debug logs in WordPressTest (it uses the debug AndroidManifest)

--- WordPress/src/androidTest/java/org/wordpress/android/WordPressTest.kt
+++ WordPress/src/androidTest/java/org/wordpress/android/WordPressTest.kt
@@ -1,9 +1,22 @@
 package org.wordpress.android
 
+import android.util.Log
+import androidx.work.Configuration
+import androidx.work.WorkManager
 import org.wordpress.android.modules.AppComponent
 import org.wordpress.android.modules.DaggerAppComponentTest
 
 class WordPressTest : WordPress() {
+    override fun onCreate() {
+        // Setup WorkManager debug logging level
+        val config = Configuration.Builder()
+                .setMinimumLoggingLevel(Log.DEBUG)
+                .build()
+        WorkManager.initialize(this, config)
+
+        super.onCreate()
+    }
+
     override fun initDaggerComponent() {
         mAppComponent = DaggerAppComponentTest.builder()
                 .application(this)
