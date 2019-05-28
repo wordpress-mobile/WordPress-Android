062cd3be87c736fff6d141658c2bb338604dc50c
Maxime Biais <maxime.biais@gmail.com>
2019-05-28 10:58:50 +0200

    Remove unnecessary _open_

--- WordPress/src/main/java/org/wordpress/android/util/NetworkUtilsWrapper.kt
+++ WordPress/src/main/java/org/wordpress/android/util/NetworkUtilsWrapper.kt
@@ -5,7 +5,7 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-open class NetworkUtilsWrapper @Inject constructor() {
+class NetworkUtilsWrapper @Inject constructor() {
     /**
      * Returns true if a network connection is available.
      */
