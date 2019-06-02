c9048e62786903e21d31a656be91ea9e8febf59c
Tyler Marien <tyler.marien@gmail.com>
2019-06-02 10:30:54 -0600

    Removed unused import

--- WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationResultFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationResultFragment.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.domains
 
 import android.app.Activity.RESULT_OK
-import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.text.HtmlCompat
