a61ea0dfbc9856083e0ee4ab5f34bd15bac2769c
vojtasmrcek <planarvoid@gmail.com>
2019-06-18 13:19:29 +0200

    Add minified updater and layouts

---
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/widget/minified/MinifiedWidgetUpdater.kt
@@ -0,0 +1,119 @@
+package org.wordpress.android.ui.stats.refresh.lists.widget.minified
+
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Context
+import android.widget.RemoteViews
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.wordpress.android.R
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.fluxc.store.stats.insights.TodayInsightsStore
+import org.wordpress.android.ui.prefs.AppPrefsWrapper
+import org.wordpress.android.ui.stats.StatsTimeframe
+import org.wordpress.android.ui.stats.refresh.lists.widget.WidgetUpdater
+import org.wordpress.android.ui.stats.refresh.lists.widget.configuration.StatsColorSelectionViewModel.Color.DARK
+import org.wordpress.android.ui.stats.refresh.lists.widget.configuration.StatsColorSelectionViewModel.Color.LIGHT
+import org.wordpress.android.ui.stats.refresh.lists.widget.configuration.StatsDataTypeSelectionViewModel.DataType
+import org.wordpress.android.ui.stats.refresh.lists.widget.configuration.StatsDataTypeSelectionViewModel.DataType.COMMENTS
+import org.wordpress.android.ui.stats.refresh.lists.widget.configuration.StatsDataTypeSelectionViewModel.DataType.LIKES
+import org.wordpress.android.ui.stats.refresh.lists.widget.configuration.StatsDataTypeSelectionViewModel.DataType.VIEWS
+import org.wordpress.android.ui.stats.refresh.lists.widget.configuration.StatsDataTypeSelectionViewModel.DataType.VISITORS
+import org.wordpress.android.ui.stats.refresh.lists.widget.utils.WidgetUtils
+import org.wordpress.android.ui.stats.refresh.utils.MILLION
+import org.wordpress.android.ui.stats.refresh.utils.ONE_THOUSAND
+import org.wordpress.android.ui.stats.refresh.utils.toFormattedString
+import org.wordpress.android.util.NetworkUtilsWrapper
+import org.wordpress.android.viewmodel.ResourceProvider
+import javax.inject.Inject
+
+class MinifiedWidgetUpdater
+@Inject constructor(
+    private val appPrefsWrapper: AppPrefsWrapper,
+    private val siteStore: SiteStore,
+    private val networkUtilsWrapper: NetworkUtilsWrapper,
+    private val resourceProvider: ResourceProvider,
+    private val todayInsightsStore: TodayInsightsStore,
+    private val widgetUtils: WidgetUtils
+) : WidgetUpdater {
+    override fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        val wideView = widgetUtils.isWidgetWiderThanLimit(appWidgetManager, appWidgetId)
+        val colorMode = appPrefsWrapper.getAppWidgetColor(appWidgetId) ?: LIGHT
+        val siteId = appPrefsWrapper.getAppWidgetSiteId(appWidgetId)
+        val dataType = appPrefsWrapper.getAppWidgetDataType(appWidgetId)
+        val siteModel = siteStore.getSiteBySiteId(siteId)
+        val networkAvailable = networkUtilsWrapper.isNetworkAvailable()
+        val layout = when (colorMode) {
+            LIGHT -> R.layout.stats_widget_minified_light
+            DARK -> R.layout.stats_widget_minified_dark
+        }
+        val views = RemoteViews(context.packageName, layout)
+        widgetUtils.setSiteIcon(siteModel, context, views, appWidgetId)
+        if (networkAvailable && siteModel != null && dataType != null) {
+            views.setOnClickPendingIntent(
+                    R.id.widget_content,
+                    widgetUtils.getPendingSelfIntent(context, siteModel.id, StatsTimeframe.INSIGHTS)
+            )
+            showValue(appWidgetManager, appWidgetId, views, siteModel, dataType, wideView)
+        } else {
+            widgetUtils.showError(appWidgetManager, views, appWidgetId, networkAvailable, resourceProvider, context)
+        }
+    }
+
+    override fun updateAllWidgets(context: Context) {
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        val viewsWidget = ComponentName(context, StatsMinifiedWidget::class.java)
+        val allWidgetIds = appWidgetManager.getAppWidgetIds(viewsWidget)
+        for (appWidgetId in allWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    private fun showValue(
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        views: RemoteViews,
+        site: SiteModel,
+        dataType: DataType,
+        wideView: Boolean
+    ) {
+        GlobalScope.launch {
+            runBlocking {
+                todayInsightsStore.fetchTodayInsights(site)
+            }
+            loadValue(appWidgetManager, appWidgetId, site, views, dataType, wideView)
+        }
+    }
+
+    private fun loadValue(
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        site: SiteModel,
+        views: RemoteViews,
+        dataType: DataType,
+        wideView: Boolean
+    ) {
+        val todayInsights = todayInsightsStore.getTodayInsights(site)
+        val (key, value) = when (dataType) {
+            VIEWS -> R.string.stats_views to todayInsights?.views
+            VISITORS -> R.string.stats_visitors to todayInsights?.visitors
+            COMMENTS -> R.string.stats_comments to todayInsights?.comments
+            LIKES -> R.string.stats_likes to todayInsights?.likes
+        }
+        views.setTextViewText(R.id.name, resourceProvider.getString(key))
+        val startValue = if (wideView) MILLION else ONE_THOUSAND
+        views.setTextViewText(R.id.value, value?.toFormattedString(startValue) ?: "-")
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    override fun delete(appWidgetId: Int) {
+        appPrefsWrapper.removeAppWidgetColorModeId(appWidgetId)
+        appPrefsWrapper.removeAppWidgetSiteId(appWidgetId)
+    }
+}
