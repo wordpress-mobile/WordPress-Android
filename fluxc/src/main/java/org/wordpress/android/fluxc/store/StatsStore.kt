1910e1e2c039e18710f8c5c93c794a1d371fa8e4
vojtasmrcek <planarvoid@gmail.com>
2019-06-24 09:27:53 +0200

    First version of file downloads endpoint

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/StatsStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/StatsStore.kt
@@ -23,22 +23,9 @@ import org.wordpress.android.fluxc.store.StatsStore.InsightType.COMMENTS
 import org.wordpress.android.fluxc.store.StatsStore.InsightType.MOST_POPULAR_DAY_AND_HOUR
 import org.wordpress.android.fluxc.store.StatsStore.InsightType.POSTING_ACTIVITY
 import org.wordpress.android.fluxc.store.StatsStore.InsightType.TODAY_STATS
-import org.wordpress.android.fluxc.store.StatsStore.PostDetailType.AVERAGE_VIEWS_PER_DAY
-import org.wordpress.android.fluxc.store.StatsStore.PostDetailType.CLICKS_BY_WEEKS
-import org.wordpress.android.fluxc.store.StatsStore.PostDetailType.MONTHS_AND_YEARS
-import org.wordpress.android.fluxc.store.StatsStore.PostDetailType.POST_HEADER
-import org.wordpress.android.fluxc.store.StatsStore.PostDetailType.POST_OVERVIEW
 import org.wordpress.android.fluxc.store.StatsStore.StatsError
 import org.wordpress.android.fluxc.store.StatsStore.StatsErrorType
 import org.wordpress.android.fluxc.store.StatsStore.StatsErrorType.GENERIC_ERROR
-import org.wordpress.android.fluxc.store.StatsStore.TimeStatsType.AUTHORS
-import org.wordpress.android.fluxc.store.StatsStore.TimeStatsType.CLICKS
-import org.wordpress.android.fluxc.store.StatsStore.TimeStatsType.COUNTRIES
-import org.wordpress.android.fluxc.store.StatsStore.TimeStatsType.OVERVIEW
-import org.wordpress.android.fluxc.store.StatsStore.TimeStatsType.POSTS_AND_PAGES
-import org.wordpress.android.fluxc.store.StatsStore.TimeStatsType.REFERRERS
-import org.wordpress.android.fluxc.store.StatsStore.TimeStatsType.SEARCH_TERMS
-import org.wordpress.android.fluxc.store.StatsStore.TimeStatsType.VIDEOS
 import org.wordpress.android.fluxc.store.Store.OnChangedError
 import org.wordpress.android.fluxc.utils.PreferenceUtils.PreferenceUtilsWrapper
 import java.util.Collections
@@ -132,26 +119,11 @@ class StatsStore
     }
 
     suspend fun getTimeStatsTypes(): List<TimeStatsType> = withContext(coroutineContext) {
-        return@withContext listOf(
-                OVERVIEW,
-                POSTS_AND_PAGES,
-                REFERRERS,
-                CLICKS,
-                AUTHORS,
-                COUNTRIES,
-                SEARCH_TERMS,
-                VIDEOS
-        )
+        return@withContext TimeStatsType.values().toList()
     }
 
     suspend fun getPostDetailTypes(): List<PostDetailType> = withContext(coroutineContext) {
-        return@withContext listOf(
-                POST_HEADER,
-                POST_OVERVIEW,
-                MONTHS_AND_YEARS,
-                AVERAGE_VIEWS_PER_DAY,
-                CLICKS_BY_WEEKS
-        )
+        return@withContext PostDetailType.values().toList()
     }
 
     interface StatsType
@@ -184,7 +156,8 @@ class StatsStore
         COUNTRIES,
         SEARCH_TERMS,
         PUBLISHED,
-        VIDEOS
+        VIDEOS,
+        FILE_DOWNLOADS
     }
 
     enum class PostDetailType : StatsType {
