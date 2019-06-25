7c32ea46b32052c243daee9ca55417444b8c005c
malinajirka <malinajirka@gmail.com>
2019-06-25 11:00:13 +0200

    Revert: Start uploadService as foreground service (656e7268)

--- WordPress/src/main/java/org/wordpress/android/ui/uploads/UploadServiceFacade.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/uploads/UploadServiceFacade.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.uploads
 
 import android.content.Context
-import androidx.core.content.ContextCompat
 import org.wordpress.android.fluxc.model.PostModel
 import javax.inject.Inject
 
@@ -14,7 +13,7 @@ import javax.inject.Inject
 class UploadServiceFacade @Inject constructor() {
     fun uploadPost(context: Context, post: PostModel, trackAnalytics: Boolean, publish: Boolean, isRetry: Boolean) {
         val intent = UploadService.getUploadPostServiceIntent(context, post, trackAnalytics, publish, isRetry)
-        ContextCompat.startForegroundService(context, intent)
+        context.startService(intent)
     }
 
     fun isPostUploadingOrQueued(post: PostModel) = UploadService.isPostUploadingOrQueued(post)
