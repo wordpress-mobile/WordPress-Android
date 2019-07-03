43aef9534664821893f4a65ac7fb52b2e8c01eee
AmandaRiu <amanda.riu@automattic.com>
2019-07-03 15:01:40 -0600

    Squashed 'libs/login/' changes from 78c5307f7a..979ddf4aec
    979ddf4aec Merge commit '827a47189094a6bc7800eaccc3ed069841251373' into sync-login-lib
    df3c11ed09 added textAlignment property to secondary button in login
    da783ea3c7 Login screens: added textAlignment property to labels
    6d486bbf6b Merge pull request #21 from wordpress-mobile/merge-wpa-develop
    57a2d00b14 Update login lib standalone FluxC hash
    0ea2405def Merge commit 'ad485b27b26ffb38b8718940b0b7556b902dc28c' into sync-login-lib
    6df15ab86b Update 2FA unavailable API error message
    eefd72f317 Show error and continue login on OAuth client 2FA error
    ca3b68e817 Merge branch 'amanda/signin-has-woo' into amanda/signin-tracks-events
    4755048046 Remove part of login site error msg and add help button
    3cc4f31b3b Refresh branch with AndroidX changes in develop
    76c40f3034 Fixed incorrect ordering of imports in login library
    08a9f0c65a First pass at AndroidX migration. Used the migration wizard then did far too much cleanup.
    a46ceeb96b Track connected site info requests, success, and failures
    47528a1c33 Special handling for atomic sites
    578dca7f29 Update androidx dependencies
    1d09d1536d Fix AndroidX import order
    48df84638f Fix import ordering for androidx
    d3dc35035a Migrate to AndroidX
    e9e219db2f Add logic to get connected site info for woo login mode.
    873db9b385 Add new logic to change the label to show the site logging into
    9f442e9a2f Add new WOO_LOGIN_MODE
    309eeda212 Add new string to handle non-WordPress site error
    dfc6991275 Revert "Add new loginMode for woo and update related error strings"
    9b44d78d3e Add debug property to populate site address during login
    45ae481c03 Add new loginMode for woo and update related error strings
    91731b6196 Add a way to hide the option to login by site address
    4d6c83f111 Merge pull request #9890 from wordpress-mobile/update-dagger-version-to-v2.22.1
    9cbf3f17ff Merge pull request #9885 from wordpress-mobile/update-login-library
    0ddf4dcdbd Update Glide version to 4.9.0
    f7860b9861 Updated Dagger in the login library
    1c311d430c Updated Dagger in the login library
    79dd984392 Updated login library to Glide 4.9.0
    e995e12f02 Revert sdk change for login library
    954e7ddebc Updated targetSdkVersion to 28
    7ee1e1c003 Added another catch block to handle NoActivityFoundException
    804225d94f Merge commit 'a9cf59e44590548a123efeb521f9ff99b3277072' into update-login-lib
    
    git-subtree-dir: libs/login
    git-subtree-split: 979ddf4aec664cf9afafa1cdf7d99599488f1eee

--- WordPressLoginFlow/src/main/java/org/wordpress/android/login/LoginListener.java
+++ WordPressLoginFlow/src/main/java/org/wordpress/android/login/LoginListener.java
@@ -1,8 +1,9 @@
 package org.wordpress.android.login;
 
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import org.wordpress.android.fluxc.network.MemorizingTrustManager;
 import org.wordpress.android.fluxc.store.SiteStore;
@@ -25,6 +26,7 @@ public interface LoginListener {
     void helpEmailScreen(String email);
     void helpSocialEmailScreen(String email);
     void addGoogleLoginFragment();
+    void showHelpFindingConnectedEmail();
 
     // Login Request Magic Link callbacks
     void showMagicLinkSentScreen(String email);
@@ -46,6 +48,7 @@ public interface LoginListener {
     // Login Site Address input callbacks
     void alreadyLoggedInWpcom(ArrayList<Integer> oldSitesIds);
     void gotWpcomSiteInfo(String siteAddress, String siteName, String siteIconUrl);
+    void gotConnectedSiteInfo(String siteAddress, boolean hasJetpack);
     void gotXmlRpcEndpoint(String inputSiteAddress, String endpointAddress);
     void handleSslCertificateError(MemorizingTrustManager memorizingTrustManager, SelfSignedSSLCallback callback);
     void helpSiteAddress(String url);
