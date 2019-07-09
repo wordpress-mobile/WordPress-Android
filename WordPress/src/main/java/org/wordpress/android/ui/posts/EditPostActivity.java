8483d93bf5d8c56285b3ad723f2e2c58a11a351a
vojtasmrcek <planarvoid@gmail.com>
2019-07-09 14:00:24 +0200

    Remove unused import

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -177,7 +177,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
