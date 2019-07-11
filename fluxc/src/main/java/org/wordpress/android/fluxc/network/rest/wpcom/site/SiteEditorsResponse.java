abf91d3aa87e577e05b389162588c6f9f86445da
Danilo Ercoli <ercoli@gmail.com>
2019-07-11 14:45:51 +0200

    Add missing file from the prev commit

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteEditorsResponse.java
@@ -0,0 +1,10 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.site;
+
+import org.wordpress.android.fluxc.network.Response;
+
+public class SiteEditorsResponse implements Response {
+    public String editor_mobile;
+    public String editor_web;
+    public String opt_in;
+    public String opt_out;
+}
