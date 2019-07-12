f4109a8246cd9ec400d89073ab761c2d06135961
Danilo Ercoli <ercoli@gmail.com>
2019-07-12 08:18:08 +0200

    Remove unused fields returned from the site editors endpoint not used here

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteEditorsResponse.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteEditorsResponse.java
@@ -5,6 +5,4 @@ import org.wordpress.android.fluxc.network.Response;
 public class SiteEditorsResponse implements Response {
     public String editor_mobile;
     public String editor_web;
-    public String opt_in;
-    public String opt_out;
 }
