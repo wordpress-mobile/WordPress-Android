0514508fd6c152dd15d3669bc6070c6246e7ccae
vojtasmrcek <planarvoid@gmail.com>
2019-07-15 11:46:34 +0200

    SQL improvements

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/PostSchedulingNotificationStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/PostSchedulingNotificationStore.kt
@@ -7,22 +7,24 @@ import javax.inject.Singleton
 @Singleton
 class PostSchedulingNotificationStore
 @Inject constructor(private val sqlUtils: PostSchedulingNotificationSqlUtils) {
-    fun schedule(postId: Long, scheduledTime: ScheduledTime): Int? {
+    fun schedule(postId: Int, scheduledTime: ScheduledTime): Int? {
         return sqlUtils.insert(postId, scheduledTime)
     }
-    fun deletePostSchedulingNotifications(postId: Long){
+    fun deletePostSchedulingNotifications(postId: Int){
         sqlUtils.deletePostSchedulingNotifications(postId)
     }
 
-    fun isScheduled(notificationId: Int): Boolean {
-        return sqlUtils.isScheduled(notificationId)
+    fun getNotification(notificationId: Int): NotificationModel? {
+        return sqlUtils.getNotification(notificationId)
     }
 
-    fun getScheduledTime(postId: Long): ScheduledTime {
+    fun getScheduledTime(postId: Int): ScheduledTime {
         return sqlUtils.getScheduledTime(postId)
     }
 
     enum class ScheduledTime {
         ONE_HOUR, TEN_MINUTES, WHEN_PUBLISHED, OFF
     }
+
+    data class NotificationModel(val notificationId: Int, val postId: Int, val scheduledTime: ScheduledTime)
 }
