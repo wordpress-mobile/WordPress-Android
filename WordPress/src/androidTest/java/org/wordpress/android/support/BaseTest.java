a2bd1617edf94d38810146c7655669a92361566a
Jeremy Massel <jeremy.massel@gmail.com>
2019-07-16 08:55:38 -0600

    Add screenshot mocks
    Adds a set of mocks specifically for screenshots, and implements a workaround to enable non-localized dates for use as JSON keys.
    
    Add missing mocks
    
    Remove deleted media library items from mocks
    
    Combine mocks
    
    Fix mocks
    
    Fix missing image
    
    Anonymize mocks
    
    More mocks adjustments

--- WordPress/src/androidTest/java/org/wordpress/android/support/BaseTest.java
+++ WordPress/src/androidTest/java/org/wordpress/android/support/BaseTest.java
@@ -3,9 +3,15 @@ package org.wordpress.android.support;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.fasterxml.jackson.databind.util.ISO8601Utils;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.DateOffset;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.HandlebarsHelper;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
+import org.apache.commons.lang3.LocaleUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.wordpress.android.R;
@@ -17,6 +23,13 @@ import org.wordpress.android.mocks.AndroidNotifier;
 import org.wordpress.android.mocks.AssetFileSource;
 import org.wordpress.android.modules.AppComponentTest;
 import org.wordpress.android.modules.DaggerAppComponentTest;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_SITE_ADDRESS;
@@ -37,12 +50,22 @@ public class BaseTest {
     }
 
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(
-            options().port(WIREMOCK_PORT)
-                     .fileSource(new AssetFileSource(
-                             InstrumentationRegistry.getInstrumentation().getContext().getAssets()))
-                     .extensions(new ResponseTemplateTransformer(true))
-                     .notifier(new AndroidNotifier()));
+    public WireMockRule wireMockRule;
+
+    {
+        wireMockRule = new WireMockRule(
+                options().port(WIREMOCK_PORT)
+                         .fileSource(new AssetFileSource(InstrumentationRegistry.getContext().getAssets()))
+                         .extensions(new ResponseTemplateTransformer(true, new HashMap<String, Helper>() {
+                             {
+                                 put("fnow", new UnlocalizedDateHelper());
+                             }
+                         }))
+                         .notifier(new AndroidNotifier()));
+    }
+
+    @Rule
+    public ActivityTestRule<WPLaunchActivity> mActivityTestRule = new ActivityTestRule<>(WPLaunchActivity.class);
 
     private void logout() {
         MePage mePage = new MePage();
@@ -71,5 +94,71 @@ public class BaseTest {
 
     private void wpLogout() {
         new MePage().go().logout();
+    }
+}
+
+class UnlocalizedDateHelper extends HandlebarsHelper<Date> {
+    @Override public Object apply(Date context, Options options) throws IOException {
+        String format = options.hash("format", null);
+        String offset = options.hash("offset", null);
+        String timezone = options.hash("timezone", null);
+        String localeCode = options.hash("locale", "US");
+
+        Date date = context != null ? context : new Date();
+        if (offset != null) {
+            date = new DateOffset(offset).shift(date);
+        }
+
+        Locale locale = Locale.getDefault();
+
+        if (localeCode != null) {
+            locale = LocaleUtils.toLocale(localeCode);
+        }
+
+        return new LocaleAwareRenderableDate(date, format, timezone, locale);
+    }
+}
+
+class LocaleAwareRenderableDate {
+    private static final long DIVIDE_MILLISECONDS_TO_SECONDS = 1000L;
+
+    private final Date mDate;
+    private final String mFormat;
+    private final String mTimezoneName;
+    private final Locale mLocale;
+
+    LocaleAwareRenderableDate(Date date, String format, String timezone, Locale locale) {
+        this.mDate = date;
+        this.mFormat = format;
+        this.mTimezoneName = timezone;
+        this.mLocale = locale;
+    }
+
+    @Override
+    public String toString() {
+        if (mFormat != null) {
+            if (mFormat.equals("epoch")) {
+                return String.valueOf(mDate.getTime());
+            }
+
+            if (mFormat.equals("unix")) {
+                return String.valueOf(mDate.getTime() / DIVIDE_MILLISECONDS_TO_SECONDS);
+            }
+
+            return formatCustom();
+        }
+
+        return mTimezoneName != null
+                ? ISO8601Utils.format(mDate, false, TimeZone.getTimeZone(mTimezoneName))
+                : ISO8601Utils.format(mDate, false);
+    }
+
+    private String formatCustom() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(mFormat, mLocale);
+        if (mTimezoneName != null) {
+            TimeZone zone = TimeZone.getTimeZone(mTimezoneName);
+            dateFormat.setTimeZone(zone);
+        }
+        return dateFormat.format(mDate);
     }
 }
