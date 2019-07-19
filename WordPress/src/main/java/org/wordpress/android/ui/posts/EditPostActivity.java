c8e93196602bd30ae73839cd9db6c81c1f25e73c
Danilo Ercoli <ercoli@gmail.com>
2019-07-19 17:10:49 +0200

    Make sure to read the editor preference set for the current blog from the new site settings location

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -80,6 +80,7 @@ import org.wordpress.android.fluxc.action.AccountAction;
 import org.wordpress.android.fluxc.generated.AccountActionBuilder;
 import org.wordpress.android.fluxc.generated.MediaActionBuilder;
 import org.wordpress.android.fluxc.generated.PostActionBuilder;
+import org.wordpress.android.fluxc.generated.SiteActionBuilder;
 import org.wordpress.android.fluxc.generated.UploadActionBuilder;
 import org.wordpress.android.fluxc.model.AccountModel;
 import org.wordpress.android.fluxc.model.CauseOfOnPostChanged;
@@ -105,6 +106,7 @@ import org.wordpress.android.fluxc.store.PostStore.RemotePostPayload;
 import org.wordpress.android.fluxc.store.QuickStartStore;
 import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartTask;
 import org.wordpress.android.fluxc.store.SiteStore;
+import org.wordpress.android.fluxc.store.SiteStore.DesignateMobileEditorPayload;
 import org.wordpress.android.fluxc.store.UploadStore;
 import org.wordpress.android.fluxc.store.UploadStore.ClearMediaPayload;
 import org.wordpress.android.fluxc.tools.FluxCImageLoader;
@@ -1542,8 +1544,10 @@ public class EditPostActivity extends AppCompatActivity implements
     private void setGutenbergEnabledIfNeeded() {
         if (AppPrefs.isGutenbergAutoEnabledForTheNewPosts()
             && !mIsNewPost
-            && !AppPrefs.isGutenbergDefaultForNewPosts()) {
+            && !SiteUtils.isBlockEditorDefaultForNewPost(mSite, mAccountStore)) {
             AppPrefs.setGutenbergDefaultForNewPosts(true);
+            mDispatcher.dispatch(SiteActionBuilder.newDesignateMobileEditorAction(
+                    new DesignateMobileEditorPayload(mSite, "gutenberg")));
             AppPrefs.setGutenbergAutoEnabledForTheNewPosts(false);
             showGutenbergInformativeDialog();
         }
