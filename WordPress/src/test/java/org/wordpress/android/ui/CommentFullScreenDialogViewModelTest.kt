c38fab8db71f647499f2427f71b5f40d71266776
vojtasmrcek <planarvoid@gmail.com>
2019-07-26 13:46:34 +0200

    Add a simple test

---
+++ WordPress/src/test/java/org/wordpress/android/ui/CommentFullScreenDialogViewModelTest.kt
@@ -0,0 +1,26 @@
+package org.wordpress.android.ui
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.wordpress.android.BaseUnitTest
+import org.wordpress.android.viewmodel.Event
+
+class CommentFullScreenDialogViewModelTest : BaseUnitTest() {
+    private lateinit var viewModel: CommentFullScreenDialogViewModel
+    @Before
+    fun setUp() {
+        viewModel = CommentFullScreenDialogViewModel()
+    }
+
+    @Test
+    fun `on init opens keyboard`() {
+        var openKeyboardEvent: Event<Unit>? = null
+        viewModel.onKeyboardOpened.observeForever { openKeyboardEvent = it }
+
+        viewModel.init()
+
+        assertThat(openKeyboardEvent).isNotNull()
+        assertThat(openKeyboardEvent!!.getContentIfNotHandled()).isNotNull()
+    }
+}
