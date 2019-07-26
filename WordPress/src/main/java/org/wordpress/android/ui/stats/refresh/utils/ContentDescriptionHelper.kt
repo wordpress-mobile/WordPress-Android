1aa0fddc866baf112637740996712f8d7e2b2ca2
vojtasmrcek <planarvoid@gmail.com>
2019-07-26 09:21:17 +0200

    Remove string resource that was not translatable

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/ContentDescriptionHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/ContentDescriptionHelper.kt
@@ -1,16 +1,15 @@
 package org.wordpress.android.ui.stats.refresh.utils
 
-import org.wordpress.android.R
+import org.wordpress.android.util.RtlUtils
 import org.wordpress.android.viewmodel.ResourceProvider
 import javax.inject.Inject
 
 class ContentDescriptionHelper
-@Inject constructor(private val resourceProvider: ResourceProvider) {
+@Inject constructor(private val resourceProvider: ResourceProvider, private val rtlUtils: RtlUtils) {
     fun buildContentDescription(keyLabel: Int, key: Long): String {
-        return resourceProvider.getString(
-                R.string.stats_list_item_short_description,
-                resourceProvider.getString(keyLabel),
-                key
-        )
+        return when (rtlUtils.isRtl) {
+            true -> "$key :${resourceProvider.getString(keyLabel)}"
+            false -> "${resourceProvider.getString(keyLabel)}: $key"
+        }
     }
 }
