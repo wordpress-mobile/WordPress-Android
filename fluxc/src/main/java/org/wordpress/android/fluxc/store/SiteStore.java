1940d37afacbe6ddad4311d069e4071ae7bd6f8b
Danilo Ercoli <ercoli@gmail.com>
2019-07-29 13:09:44 +0200

    Write the new editor value locally before pushing it to the remote (on wpcom sites).

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/SiteStore.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/SiteStore.java
@@ -1687,20 +1687,21 @@ public class SiteStore extends Store {
     }
 
     private void designateMobileEditor(DesignateMobileEditorPayload payload) {
+        // wpcom sites sync the new value with the backend
         if (payload.site.isUsingWpComRestApi()) {
             mSiteRestClient.designateMobileEditor(payload.site, payload.editor);
-        } else {
-            // .ORG sites: Just update the editor pref on the DB and emit the change
-            SiteModel site = payload.site;
-            site.setMobileEditor(payload.editor);
-            OnSiteEditorsChanged event = new OnSiteEditorsChanged(site);
-            try {
-                event.rowsAffected = SiteSqlUtils.insertOrUpdateSite(site);
-            } catch (Exception e) {
-                event.error = new SiteEditorsError(SiteEditorsErrorType.GENERIC_ERROR);
-            }
-            emitChange(event);
         }
+
+        // Update the editor pref on the DB, and emit the change immediately
+        SiteModel site = payload.site;
+        site.setMobileEditor(payload.editor);
+        OnSiteEditorsChanged event = new OnSiteEditorsChanged(site);
+        try {
+            event.rowsAffected = SiteSqlUtils.insertOrUpdateSite(site);
+        } catch (Exception e) {
+            event.error = new SiteEditorsError(SiteEditorsErrorType.GENERIC_ERROR);
+        }
+        emitChange(event);
     }
 
     private void updateSiteEditors(FetchedEditorsPayload payload) {
