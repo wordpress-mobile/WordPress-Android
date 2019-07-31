28e46050b23e8a6abcfc844d0334860c2841d143
James Treanor <jtreanor3@gmail.com>
2019-07-31 10:55:41 +0100

    Remove unused import

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/EditorTests.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/EditorTests.java
@@ -22,7 +22,6 @@ import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.TestCase.assertTrue;
-import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_SITE_ADDRESS;
 import static org.wordpress.android.support.WPSupportUtils.checkViewHasText;
 import static org.wordpress.android.support.WPSupportUtils.sleep;
 import static org.wordpress.android.support.WPSupportUtils.waitForElementToNotBeDisplayed;
