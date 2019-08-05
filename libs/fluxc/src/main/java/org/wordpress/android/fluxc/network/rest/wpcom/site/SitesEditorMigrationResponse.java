288ea98782755ceb1908796f1f1f57936c5ef6ef
Danilo Ercoli <ercoli@gmail.com>
2019-08-05 18:16:28 +0200

    Add missing file from prev commit

---
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SitesEditorMigrationResponse.java
@@ -0,0 +1,7 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.site;
+
+import org.wordpress.android.fluxc.network.Response;
+
+public class SitesEditorMigrationResponse implements Response {
+    public boolean success;
+}
