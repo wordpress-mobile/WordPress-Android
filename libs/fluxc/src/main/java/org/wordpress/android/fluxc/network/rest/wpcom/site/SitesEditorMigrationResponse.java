48e57b839f998700021f1116eb06c4cc437f2bee
Danilo Ercoli <ercoli@gmail.com>
2019-08-06 12:39:14 +0200

    Parse the network response and make sure the correct values are stored in the local DB

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SitesEditorMigrationResponse.java
+++
@@ -1,9 +0,0 @@
-package org.wordpress.android.fluxc.network.rest.wpcom.site;
-
-import org.wordpress.android.fluxc.network.Response;
-
-import java.util.ArrayList;
-
-
-public class SitesEditorMigrationResponse extends ArrayList<String> implements Response {
-}
