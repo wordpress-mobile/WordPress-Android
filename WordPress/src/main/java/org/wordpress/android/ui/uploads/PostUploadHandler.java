dea9a62ad81dbb1a589e228df7efb7b0a9bbc2a3
malinajirka <malinajirka@gmail.com>
2019-09-24 13:32:29 +0200

    Hide remote auto save completed notification

--- WordPress/src/main/java/org/wordpress/android/ui/uploads/PostUploadHandler.java
+++ WordPress/src/main/java/org/wordpress/android/ui/uploads/PostUploadHandler.java
@@ -654,7 +654,6 @@ public class PostUploadHandler implements UploadHandler<PostModel> {
             PostModel post = mPostStore.getPostByLocalPostId(postLocalId);
             SiteModel site = mSiteStore.getSiteByLocalId(post.getLocalSiteId());
             mPostUploadNotifier.incrementUploadedPostCountFromForegroundNotification(post);
-            mPostUploadNotifier.updateNotificationSuccessForPost(post, site, false);
             finishUpload();
         }
     }
