b2ab4c9f94035113c388651610795999e8e73395
Oguz Kocer <oguz.kocer@automattic.com>
2019-10-18 12:53:33 -0400

    Add signup_flow_name parameter to auth/sendSignupEmail call

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/AccountStore.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/AccountStore.java
@@ -80,6 +80,7 @@ public class AccountStore extends Store {
         public AuthEmailPayloadFlow flow;
         public AuthEmailPayloadSource source;
         public String emailOrUsername;
+        public String signupFlowName;
         public boolean isSignup;
 
         public AuthEmailPayload(String emailOrUsername, boolean isSignup, AuthEmailPayloadFlow flow,
