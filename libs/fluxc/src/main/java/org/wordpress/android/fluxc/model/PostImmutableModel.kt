f6420e7d5e871048fb354aba9f9cce2696ee8518
vojtasmrcek <planarvoid@gmail.com>
2019-11-04 09:10:11 +0100

    Remove empty lines

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/PostImmutableModel.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/PostImmutableModel.kt
@@ -6,91 +6,48 @@ import org.wordpress.android.fluxc.model.post.PostLocation
 
 internal interface PostImmutableModel {
     val id: Int
-
     val localSiteId: Int
-
     val remoteSiteId: Long
-
     val remotePostId: Long
-
     val title: String
-
     val content: String
-
     val dateCreated: String
-
     val lastModified: String
-
     val remoteLastModified: String
-
     val categoryIds: String
-
     val categoryIdList: List<Long>
-
     val customFields: String
-
     val link: String
-
     val excerpt: String
-
     val tagNames: String
-
     val tagNameList: List<String>
-
     val status: String
-
     val password: String
-
     val featuredImageId: Long
-
     val postFormat: String
-
     val slug: String
-
     val longitude: Double
-
     val latitude: Double
-
     val location: PostLocation
-
     val authorId: Long
-
     val authorDisplayName: String?
-
     val changesConfirmedContentHashcode: Int
-
     val isPage: Boolean
-
     val parentId: Long
-
     val parentTitle: String
-
     val isLocalDraft: Boolean
-
     val isLocallyChanged: Boolean
-
     val autoSaveRevisionId: Long
-
     val autoSaveModified: String?
-
     val remoteAutoSaveModified: String?
-
     val autoSavePreviewUrl: String?
-
     val autoSaveTitle: String?
-
     val autoSaveContent: String?
-
     val autoSaveExcerpt: String?
-
     val hasCapabilityPublishPost: Boolean
-
     val hasCapabilityEditPost: Boolean
-
     val hasCapabilityDeletePost: Boolean
-
     val dateLocallyChanged: String
-
     val jsonCustomFields: JSONArray?
 
     fun hasFeaturedImage(): Boolean
