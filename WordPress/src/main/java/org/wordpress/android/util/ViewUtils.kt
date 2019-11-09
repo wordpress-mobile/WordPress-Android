b24a715f5d905c878d27e72262787df7fe3b2119
Joel Dean <jdeanjj1000@gmail.com>
2019-11-08 18:50:15 -0800

    Added TouchDelegate extension method so that the touch area of views can be extended.

--- WordPress/src/main/java/org/wordpress/android/util/ViewUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/util/ViewUtils.kt
@@ -1,7 +1,9 @@
 package org.wordpress.android.util
 
+import android.graphics.Rect
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
+import android.view.TouchDelegate
 import android.view.View
 
 fun View.setVisible(visible: Boolean) {
@@ -13,3 +15,24 @@ fun View.redirectContextClickToLongPressListener() {
         this.setOnContextClickListener { it.performLongClick() }
     }
 }
+
+
+fun View.expandTouchTargetArea(dps: Int, heightOnly: Boolean = false) {
+    val pixels = DisplayUtils.dpToPx(context, dps)
+    val parent = this.parent as View
+
+    parent.post {
+        val touchTargetRect = Rect()
+        getHitRect(touchTargetRect)
+        touchTargetRect.top -= pixels
+        touchTargetRect.bottom += pixels
+
+        if (!heightOnly) {
+            touchTargetRect.right += pixels
+            touchTargetRect.left -= pixels
+        }
+
+        parent.touchDelegate = TouchDelegate(touchTargetRect, this)
+    }
+}
+
