98a6ed3637659e28920e4fb608a8f796f0820d8c
Joel Dean <jdeanjj1000@gmail.com>
2019-11-08 20:05:15 -0800

    Added touch target sizing to more menu button to meet accessibility requirements.

--- WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderPostAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderPostAdapter.java
@@ -125,6 +125,7 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
     private static final int LAYOUT_DISCOVER_EXTRA_DPS = 16;
     private static final int LAYOUT_VISIT_EXTRA_DPS = 12;
+    private static final int IMAGE_MORE_EXTRA_DPS = 12;
 
     @Inject AccountStore mAccountStore;
     @Inject SiteStore mSiteStore;
@@ -243,6 +244,7 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
             ViewUtilsKt.expandTouchTargetArea(mLayoutDiscover, LAYOUT_DISCOVER_EXTRA_DPS, true);
             ViewUtilsKt.expandTouchTargetArea(mVisit, LAYOUT_VISIT_EXTRA_DPS, false);
+            ViewUtilsKt.expandTouchTargetArea(mImgMore, IMAGE_MORE_EXTRA_DPS, false);
 
             // show post in internal browser when "visit" is clicked
             View.OnClickListener visitListener = new View.OnClickListener() {
