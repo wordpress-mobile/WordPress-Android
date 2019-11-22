55a1f507229bac5552b7540e2c4716061749cfc5
Joel Dean <jdeanjj1000@gmail.com>
2019-11-21 22:57:52 -0500

    made imgUrl nullable so it can be checked before it's utilized.

--- WordPress/src/main/java/org/wordpress/android/util/image/ImageManager.kt
+++ WordPress/src/main/java/org/wordpress/android/util/image/ImageManager.kt
@@ -142,22 +142,24 @@ class ImageManager @Inject constructor(private val placeholderManager: ImagePlac
     fun loadWithResultListener(
         imageView: ImageView,
         imageType: ImageType,
-        imgUrl: String,
+        imgUrl: String?,
         scaleType: ScaleType = CENTER,
         thumbnailUrl: String? = null,
         requestListener: RequestListener<Drawable>
     ) {
         val context = imageView.context
         if (!context.isAvailable()) return
-        GlideApp.with(context)
-                .load(imgUrl)
-                .addFallback(imageType)
-                .addPlaceholder(imageType)
-                .addThumbnail(context, thumbnailUrl, requestListener)
-                .applyScaleType(scaleType)
-                .attachRequestListener(requestListener)
-                .into(imageView)
-                .clearOnDetach()
+        imgUrl?.let {
+            GlideApp.with(context)
+                    .load(it)
+                    .addFallback(imageType)
+                    .addPlaceholder(imageType)
+                    .addThumbnail(context, thumbnailUrl, requestListener)
+                    .applyScaleType(scaleType)
+                    .attachRequestListener(requestListener)
+                    .into(imageView)
+                    .clearOnDetach()
+        }
     }
 
     /**
