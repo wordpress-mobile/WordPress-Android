1a3fe6fcf4fd459bf67e2db0b38a8845704ed4f2
Joel Dean <jdeanjj1000@gmail.com>
2019-11-28 21:42:05 -0500

    Created a domain sanitizer and respective tests to verify it's behavior

---
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainSanitizer.kt
@@ -0,0 +1,24 @@
+package org.wordpress.android.ui.sitecreation.domains
+
+import javax.inject.Inject
+
+class SiteCreationDomainSanitizer
+@Inject constructor() {
+    /**
+     * Sanitizes a query by taking the value before it's first period if it's present,
+     * removes it's scheme and finally, removes any characters that aren't alphanumeric.
+     */
+    fun sanitizeDomainQuery(query: String) =
+            query.run {
+                val periodIndex = indexOf(".")
+                if (periodIndex > 0) {
+                    substring(0, periodIndex)
+                } else {
+                    this
+                }
+            }.replace("http://".toRegex(), "")
+                    .replace("https://".toRegex(), "")
+                    .replace("[^a-zA-Z0-9]".toRegex(), "")
+
+}
+
