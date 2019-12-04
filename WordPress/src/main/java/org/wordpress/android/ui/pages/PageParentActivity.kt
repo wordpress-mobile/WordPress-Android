b04d1bfb71060cd4982b95a50b4860a393486fb1
Klymentiy haykov <forsver@gmail.com>
2019-12-03 16:09:45 -0800

    Fixed toolbar reference and removed manual coloring of radio buttons.

--- WordPress/src/main/java/org/wordpress/android/ui/pages/PageParentActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/pages/PageParentActivity.kt
@@ -2,7 +2,7 @@ package org.wordpress.android.ui.pages
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.toolbar.*
+import kotlinx.android.synthetic.main.toolbar_main.*
 import org.wordpress.android.R
 
 class PageParentActivity : AppCompatActivity() {
@@ -11,7 +11,7 @@ class PageParentActivity : AppCompatActivity() {
 
         setContentView(R.layout.pages_parent_activity)
 
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar_main)
         supportActionBar?.let {
             it.setHomeButtonEnabled(true)
             it.setDisplayHomeAsUpEnabled(true)
