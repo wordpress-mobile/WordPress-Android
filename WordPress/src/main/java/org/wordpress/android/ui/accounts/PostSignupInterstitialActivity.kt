3ae56f586f655bcb4def0e07fd929744590eb53a
Renan Ferrari <renan.fb@gmail.com>
2019-12-09 20:26:58 -0300

    Creating post-signup interstitial layout

---
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/PostSignupInterstitialActivity.kt
@@ -0,0 +1,12 @@
+package org.wordpress.android.ui.accounts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.wordpress.android.R
+
+class PostSignupInterstitialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.post_signup_interstitial_activity)
+    }
+}
