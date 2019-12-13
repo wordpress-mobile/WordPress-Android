4412abf0d2a34a72de1029d158ae0b4c2c3f5b28
develric <develric@gmail.com>
2019-12-13 12:29:22 +0100

    Unit testing

---
+++ WordPress/src/test/java/org/wordpress/android/ui/reader/ReaderTrackerTest.kt
@@ -0,0 +1,70 @@
+package org.wordpress.android.ui.reader
+
+import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.ui.reader.utils.DateProvider
+import org.wordpress.android.ui.reader.utils.ReaderTrackersProvider
+import java.util.Calendar
+import java.util.Date
+
+@RunWith(MockitoJUnitRunner::class)
+class ReaderTrackerTest {
+    @Mock lateinit var dateProvider: DateProvider
+    lateinit var readerTrackersProvider: ReaderTrackersProvider
+
+    private lateinit var tracker: ReaderTracker
+
+    @Before
+    fun setup() {
+        readerTrackersProvider = ReaderTrackersProvider(dateProvider)
+        tracker = ReaderTracker(readerTrackersProvider)
+    }
+
+    @Test
+    fun `trackers are initialized on initTrackers`() {
+        tracker.initTrackers()
+        val expected = ReaderTrackersProvider(dateProvider).getTrackers().associateBy( {it.key}, { it.accumulatedTime })
+        assertThat(tracker.getAnalyticsData()).isEqualTo(expected)
+    }
+
+    @Test
+    fun `trackers accumulates as expected`() {
+        tracker.initTrackers()
+        val startPoint = Date()
+
+        whenever(dateProvider.getCurrentTime()).thenReturn(startPoint)
+
+        tracker.start(ReaderTrackerInfo.ReaderTopLevelList::class.java)
+        tracker.start(ReaderTrackerInfo.ReaderFilteredList::class.java)
+        tracker.start(ReaderTrackerInfo.ReaderPagedPosts::class.java)
+
+        whenever(dateProvider.getCurrentTime()).thenReturn(addToDate(startPoint, Int.MAX_VALUE - 1))
+        tracker.stop(ReaderTrackerInfo.ReaderTopLevelList::class.java)
+
+        whenever(dateProvider.getCurrentTime()).thenReturn(addToDate(startPoint, Int.MAX_VALUE - 2))
+        tracker.stop(ReaderTrackerInfo.ReaderFilteredList::class.java)
+
+        whenever(dateProvider.getCurrentTime()).thenReturn(addToDate(startPoint, Int.MAX_VALUE - 3))
+        tracker.stop(ReaderTrackerInfo.ReaderPagedPosts::class.java)
+
+
+        val expected = mapOf(
+                "time_in_main_reader" to Int.MAX_VALUE - 1,
+                "time_in_reader_filtered_list" to Int.MAX_VALUE - 2,
+                "time_in_reader_paged_post" to Int.MAX_VALUE - 3
+        )
+        assertThat(tracker.getAnalyticsData()).isEqualTo(expected)
+    }
+
+    private fun addToDate(date: Date, seconds: Int) : Date {
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        calendar.add(Calendar.SECOND, seconds)
+        return calendar.time
+    }
+}
