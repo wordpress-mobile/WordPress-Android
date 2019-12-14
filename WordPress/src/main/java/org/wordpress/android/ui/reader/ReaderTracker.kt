7850e2bce383b1a252c02aa97a4b81c26e021746
develric <develric@gmail.com>
2019-12-14 18:38:12 +0100

    Addede @MainThread annotation, updated comment, renamed method.

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderTracker.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderTracker.kt
@@ -1,19 +1,18 @@
 package org.wordpress.android.ui.reader
 
+import androidx.annotation.MainThread
 import org.wordpress.android.ui.reader.utils.ReaderTrackersProvider
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
+@MainThread
 class ReaderTracker @Inject constructor(trackersProvider: ReaderTrackersProvider) {
-    // we did not protect the concurrent access to the trackers
-    // intended usage is to call the ReaderTracker fun from the UI thread
-    // in events like onResume/onPause
     // TODO: evaluate during IA extensions to use something like Dispatchers.Main.Immediate in the fun(s)
-    // to make the usage of this class more robust to its users
+    // to make the usage of this class more robust/transparent to its users
     private val trackers = trackersProvider.getTrackers()
 
-    fun initTrackers() {
+    fun setupTrackers() {
         for (tracker in trackers) {
             tracker.init()
         }
