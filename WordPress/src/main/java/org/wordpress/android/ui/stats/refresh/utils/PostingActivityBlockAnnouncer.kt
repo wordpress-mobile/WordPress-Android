faf761093018e3e217c77e9d3f2af0c37991791f
Joel Dean <jdeanjj1000@gmail.com>
2019-12-17 14:48:39 -0500

    Created skeleton for the announcer that will traverse each block

---
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/PostingActivityBlockAnnouncer.kt
@@ -0,0 +1,23 @@
+package org.wordpress.android.ui.stats.refresh.utils
+
+import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem.ActivityItem
+import javax.inject.Inject
+
+class PostingActivityBlockAnnouncer
+@Inject constructor() {
+    private var activityItem: ActivityItem? = null
+    private var currentBoxIndex: Int? = null
+    private var currentBlockIndex: Int? = null
+
+    fun onBlockClicked() {
+
+    }
+
+    fun onBlockFocused() {
+
+    }
+
+    fun onBlockBinded(activityItem: ActivityItem) {
+
+    }
+}
