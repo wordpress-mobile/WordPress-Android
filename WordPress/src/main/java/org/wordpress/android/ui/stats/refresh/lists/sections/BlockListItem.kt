843ed0e41e80e19d1ed1db534711488a85b7b0cf
Joel Dean <jdeanjj1000@gmail.com>
2019-12-17 23:33:46 -0500

    Made content description nullable as invisible items wont need to set it

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/BlockListItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/BlockListItem.kt
@@ -234,7 +234,7 @@ sealed class BlockListItem(val type: Type) {
 
     data class ActivityItem(val blocks: List<Block>) : BlockListItem(ACTIVITY_ITEM) {
         data class Block(val label: String, val boxes: List<BoxItem>)
-        data class BoxItem(val box: Box, val contentDescription: String)
+        data class BoxItem(val box: Box, val contentDescription: String? = null)
         enum class Box {
             INVISIBLE, VERY_LOW, LOW, MEDIUM, HIGH, VERY_HIGH
         }
