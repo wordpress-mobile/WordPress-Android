421a05ad7e04d1d6ad752f6089f32d0d5f492127
Renan Ferrari <renan.fb@gmail.com>
2019-12-18 15:33:22 -0300

    Tracking post-signup interstitial screen events

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/PostSignupInterstitialActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/PostSignupInterstitialActivity.kt
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.post_signup_interstitial_default.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.POST_SIGNUP_INTERSTITIAL_ADD_SELF_HOSTED_SITE_TAPPED
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.POST_SIGNUP_INTERSTITIAL_CREATE_NEW_SITE_TAPPED
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.POST_SIGNUP_INTERSTITIAL_DISMISSED
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.POST_SIGNUP_INTERSTITIAL_SHOWN
 import org.wordpress.android.ui.ActivityLauncher
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 import javax.inject.Inject
 
 class PostSignupInterstitialActivity : AppCompatActivity() {
     @Inject lateinit var appPrefsWrapper: AppPrefsWrapper
+    @Inject lateinit var analyticsTrackerWrapper: AnalyticsTrackerWrapper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +24,7 @@ class PostSignupInterstitialActivity : AppCompatActivity() {
 
         setContentView(R.layout.post_signup_interstitial_activity)
 
-        appPrefsWrapper.shouldShowPostSignupInterstitial = false
+        trackInterstitialShown()
 
         create_new_site_button.setOnClickListener { startSiteCreationFlow() }
         add_self_hosted_site_button.setOnClickListener { startSiteConnectionFlow() }
@@ -30,17 +36,28 @@ class PostSignupInterstitialActivity : AppCompatActivity() {
         dismiss()
     }
 
+    private fun trackInterstitialShown() {
+        analyticsTrackerWrapper.track(POST_SIGNUP_INTERSTITIAL_SHOWN)
+        appPrefsWrapper.shouldShowPostSignupInterstitial = false
+    }
+
     private fun startSiteCreationFlow() {
+        analyticsTrackerWrapper.track(POST_SIGNUP_INTERSTITIAL_CREATE_NEW_SITE_TAPPED)
+
         ActivityLauncher.newBlogForResult(this)
         finish()
     }
 
     private fun startSiteConnectionFlow() {
+        analyticsTrackerWrapper.track(POST_SIGNUP_INTERSTITIAL_ADD_SELF_HOSTED_SITE_TAPPED)
+
         ActivityLauncher.addSelfHostedSiteForResult(this)
         finish()
     }
 
     private fun dismiss() {
+        analyticsTrackerWrapper.track(POST_SIGNUP_INTERSTITIAL_DISMISSED)
+
         ActivityLauncher.viewReader(this)
         finish()
     }
