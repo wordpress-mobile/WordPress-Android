d786bb8187082ab4759eb9cc8b87ca7357d1d47e
Matthew Kevins <mmkevins@yahoo.com>
2019-12-19 14:22:35 +1000

    Add some tests for PostUtils upload processing

---
+++ WordPress/src/test/java/org/wordpress/android/ui/posts/PostUtilsUploadProcessingTest.kt
@@ -0,0 +1,72 @@
+package org.wordpress.android.ui.posts
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.util.helpers.MediaFile
+
+@RunWith(MockitoJUnitRunner::class)
+class PostUtilsUploadProcessingTest {
+
+    private val remoteUrl = "https://wordpress.org/gutenberg/files/2018/07/Screenshot-1-1.png"
+    private val localId = "112"
+    private val remoteId = "97629"
+
+    private val oldImageBlock = """<!-- wp:image {"id":112,"align":"full"} -->
+<figure class="wp-block-image alignfull"><img src="file://Screenshot-1-1.png" alt="" class="wp-image-112"/><figcaption><em>Gutenberg</em> on web</figcaption></figure>
+<!-- /wp:image -->
+"""
+    private val newImageBlock = """<!-- wp:image {"id":97629,"align":"full"} -->
+<figure class="wp-block-image alignfull"><img src="https://wordpress.org/gutenberg/files/2018/07/Screenshot-1-1.png" alt="" class="wp-image-97629"/><figcaption><em>Gutenberg</em> on web</figcaption></figure>
+<!-- /wp:image -->
+"""
+    private val imageBlockWithPrefixCollision = """<!-- wp:image {"id":11242,"align":"full"} -->
+<figure class="wp-block-image alignfull"><img src="https://wordpress.org/gutenberg/files/2019/07/Screenshot-4-2.png" alt="" class="wp-image-11242"/><figcaption><em>Gutenberg</em> on web</figcaption></figure>
+<!-- /wp:image -->
+"""
+    private val oldMediaTextBlock = """<!-- wp:media-text {"mediaId":112,"mediaType":"image"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="file://img_20191202_094944-18.jpg" alt="" class="wp-image-112"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
+<p class="has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+"""
+    private val newMediaTextBlock = """<!-- wp:media-text {"mediaId":97629,"mediaType":"image"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="https://wordpress.org/gutenberg/files/2018/07/Screenshot-1-1.png" alt="" class="wp-image-97629"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
+<p class="has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+"""
+
+    val mediaFile: MediaFile = mock()
+
+    @Before
+    fun before() {
+        whenever(mediaFile.mediaId).thenReturn(remoteId)
+        whenever(mediaFile.fileURL).thenReturn(remoteUrl)
+    }
+
+
+    @Test
+    fun `replaceMediaFileWithUrlInGutenbergPost replaces temporary local id and url for image block`() {
+        val processedContent = PostUtils.replaceMediaFileWithUrlInGutenbergPost(oldImageBlock, localId, mediaFile)
+        Assertions.assertThat(processedContent).isEqualTo(newImageBlock)
+    }
+
+    @Test
+    fun `replaceMediaFileWithUrlInGutenbergPost replaces temporary local id and url for image block with colliding prefixes`() {
+        val oldContent = oldImageBlock + imageBlockWithPrefixCollision
+        val newContent = newImageBlock + imageBlockWithPrefixCollision
+        val processedContent = PostUtils.replaceMediaFileWithUrlInGutenbergPost(oldContent, localId, mediaFile)
+        Assertions.assertThat(processedContent).isEqualTo(newContent)
+    }
+
+    @Test
+    fun `replaceMediaFileWithUrlInGutenbergPost replaces temporary local id and url for media-text block`() {
+        val processedContent = PostUtils.replaceMediaFileWithUrlInGutenbergPost(oldMediaTextBlock, localId, mediaFile)
+        Assertions.assertThat(processedContent).isEqualTo(newMediaTextBlock)
+    }
+}
