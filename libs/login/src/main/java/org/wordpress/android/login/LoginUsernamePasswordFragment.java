a98adab6bf688c0b5e05e236e2b1e70dd4543707
AmandaRiu <amanda.riu@automattic.com>
2019-12-30 19:02:04 -0700

    Revert erroneously deleted code during merge

--- libs/login/src/main/java/org/wordpress/android/login/LoginUsernamePasswordFragment.java
+++ libs/login/src/main/java/org/wordpress/android/login/LoginUsernamePasswordFragment.java
@@ -154,9 +154,8 @@ public class LoginUsernamePasswordFragment extends LoginBaseDiscoveryFragment im
         siteAddressView.setText(UrlUtils.removeScheme(UrlUtils.removeXmlrpcSuffix(mInputSiteAddress)));
         siteAddressView.setVisibility(mInputSiteAddress != null ? View.VISIBLE : View.GONE);
 
-        String inputSiteAddressWithoutProtocol = UrlUtils.removeScheme(mInputSiteAddress);
         mInputSiteAddressWithoutSuffix = (mEndpointAddress == null || mEndpointAddress.isEmpty())
-                ? inputSiteAddressWithoutProtocol : UrlUtils.removeXmlrpcSuffix(mEndpointAddress);
+                ? mInputSiteAddress : UrlUtils.removeXmlrpcSuffix(mEndpointAddress);
 
         mUsernameInput = rootView.findViewById(R.id.login_username_row);
         mUsernameInput.setText(mInputUsername);
