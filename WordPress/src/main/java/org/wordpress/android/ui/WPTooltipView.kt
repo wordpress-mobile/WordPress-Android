106da3741bd778d9788a09e754276d89ee0b5a02
develric <develric@gmail.com>
2020-01-09 18:46:59 +0100

    Fixing lint.

--- WordPress/src/main/java/org/wordpress/android/ui/WPTooltipView.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/WPTooltipView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.LayoutRes
