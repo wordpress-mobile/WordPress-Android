cc35a6b6eef014126ffed84ae0e7b4c8d9be21f2
ashiagr <agr.ash@gmail.com>
2020-01-13 09:38:25 +0530

    Convert BottomSheetDialog to BottomSheetDialogFragment and update styles

---
+++ libs/login/src/main/java/org/wordpress/android/login/SignupBottomSheetDialogFragment.java
@@ -0,0 +1,100 @@
+package org.wordpress.android.login;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnShowListener;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import org.wordpress.android.login.widgets.WPBottomSheetDialogFragment;
+
+public class SignupBottomSheetDialogFragment extends WPBottomSheetDialogFragment {
+    public static final String TAG = SignupBottomSheetDialogFragment.class.getSimpleName();
+    private SignupSheetListener mSignupSheetListener;
+
+    public SignupBottomSheetDialogFragment(@NonNull final SignupSheetListener signupSheetListener) {
+        mSignupSheetListener = signupSheetListener;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        final View layout = inflater.inflate(R.layout.signup_bottom_sheet_dialog, container, false);
+
+        Button termsOfServiceText = layout.findViewById(R.id.signup_tos);
+        termsOfServiceText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSignupSheetListener.onSignupSheetTermsOfServiceClicked();
+            }
+        });
+        termsOfServiceText.setText(Html.fromHtml(String.format(
+                getDialog()
+                    .getContext()
+                    .getResources()
+                    .getString(R.string.signup_terms_of_service_text), "<u>", "</u>")));
+
+        Button signupWithEmailButton = layout.findViewById(R.id.signup_email);
+        signupWithEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSignupSheetListener.onSignupSheetEmailClicked();
+            }
+        });
+
+        Button signupWithGoogleButton = layout.findViewById(R.id.signup_google);
+        signupWithGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSignupSheetListener.onSignupSheetGoogleClicked();
+            }
+        });
+
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    mSignupSheetListener.onSignupSheetCanceled();
+                }
+            });
+
+            dialog.setContentView(layout);
+
+            // Set peek height to full height of view to avoid signup buttons being off screen when
+            // bottom sheet is shown with small screen height (e.g. landscape orientation).
+            final BottomSheetBehavior behavior = BottomSheetBehavior.from((View) layout.getParent());
+            dialog.setOnShowListener(new OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialog) {
+                    behavior.setPeekHeight(layout.getHeight());
+                }
+            });
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        mSignupSheetListener.onSignupSheetCanceled();
+    }
+
+    public interface SignupSheetListener {
+        void onSignupSheetCanceled();
+        void onSignupSheetEmailClicked();
+        void onSignupSheetGoogleClicked();
+        void onSignupSheetTermsOfServiceClicked();
+    }
+}
