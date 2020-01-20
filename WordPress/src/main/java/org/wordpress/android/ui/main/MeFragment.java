593e347a5c40d151f2b3dc4651060891639ae650
Klymentiy haykov <forsver@gmail.com>
2020-01-20 15:29:30 -0800

    Removed unused import.

--- WordPress/src/main/java/org/wordpress/android/ui/main/MeFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MeFragment.java
@@ -3,7 +3,6 @@ package org.wordpress.android.ui.main;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
