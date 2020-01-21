520e7b48b995292a27b5bb6a6ed54ff9b2edf6ad
Oguz Kocer <oguz.kocer@automattic.com>
2020-01-21 18:53:46 -0500

    Fetching an empty list of posts should mark canLoadMore posts as false

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/post/PostRestClient.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/post/PostRestClient.java
@@ -133,7 +133,10 @@ public class PostRestClient extends BaseWPComRestClient {
                                     .add(new PostListItem(postResponse.getRemotePostId(), postResponse.getModified(),
                                             postResponse.getStatus(), autoSaveModified, postResponse.getDate()));
                         }
-                        boolean canLoadMore = response.getFound() > offset + postListItems.size();
+                        // The API sometimes return wrong number of posts "found", so we also check if we get an empty
+                        // list in which case there would be no more posts to be fetched.
+                        boolean canLoadMore = postListItems.size() > 0
+                                              && response.getFound() > offset + postListItems.size();
                         FetchPostListResponsePayload responsePayload =
                                 new FetchPostListResponsePayload(listDescriptor, postListItems, loadedMore,
                                         canLoadMore, null);
