f07755533d2e9955b6b04b44cc4b16629f6ea23b
Joel Dean <jdeanjj1000@gmail.com>
2020-01-21 16:52:19 -0500

    wrote tests for content description behavior that was added to verify it

--- WordPress/src/test/java/org/wordpress/android/ui/stats/refresh/lists/sections/insights/usecases/PostingActivityMapperTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/stats/refresh/lists/sections/insights/usecases/PostingActivityMapperTest.kt
@@ -4,8 +4,12 @@ import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mock
 import org.wordpress.android.BaseUnitTest
+import org.wordpress.android.R
 import org.wordpress.android.fluxc.model.stats.insights.PostingActivityModel.Month
 import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem.ActivityItem.Box
 import org.wordpress.android.util.LocaleManagerWrapper
@@ -21,6 +25,50 @@ class PostingActivityMapperTest : BaseUnitTest() {
     fun setUp() {
         mapper = PostingActivityMapper(localeManagerWrapper, resourceProvider)
         whenever(localeManagerWrapper.getLocale()).thenReturn(Locale.US)
+        whenever(resourceProvider.getString(eq(R.string.stats_posting_activity_label_content_description),
+                anyString())).then {
+            val month = it.getArgument<String>(1)
+            "Posting Activity for $month"
+        }
+        whenever(
+                resourceProvider.getString(
+                        eq(R.string.stats_posting_activity_content_description),
+                        anyString(), anyString(), anyString()
+                )
+        ).then {
+            val readableBoxType = it.getArgument<String>(1)
+            val month = it.getArgument<String>(2)
+            val days = it.getArgument<String>(3)
+            "The days with $readableBoxType views for $month are : $month $days.Tap for more."
+        }
+        whenever(resourceProvider.getString(anyInt())).then {
+            val resourceId = it.getArgument<Int>(0)
+            when (resourceId) {
+                R.string.stats_box_type_medium -> "medium"
+                R.string.stats_box_type_high -> "high"
+                R.string.stats_box_type_very_high -> "very high"
+                else -> "low"
+            }
+        }
+    }
+
+    @Test
+    fun `ensure content descriptions are accurate`() {
+        val result = mapper.buildActivityItem(
+                listOf(
+                        Month(
+                                2019,
+                                Calendar.JANUARY,
+                                mapOf(Pair(1, 60), Pair(2, 10))
+                        )
+                ), 100
+        )
+
+        assertThat(result.blocks[0].contentDescription).isEqualTo("Posting Activity for January")
+        assertThat(result.blocks[0].activityContentDescriptions[0])
+                .isEqualTo("The days with high views for January are : January 1.Tap for more.")
+        assertThat(result.blocks[0].activityContentDescriptions[1])
+                .isEqualTo("The days with low views for January are : January 2.Tap for more.")
     }
 
     @Test
