d9eeb6fc68b1442ff94eaebba1c531f7ca6a8981
ashiagr <agr.ash@gmail.com>
2020-01-24 18:09:36 +0530

    Remove unused import

--- ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/HomeFragment.kt
+++ ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 
 import androidx.fragment.app.Fragment
-import org.wordpress.android.imageeditor.R
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
