73a67fb0ab6233e8c88f534783a3cbc7e7952a82
ashiagr <agr.ash@gmail.com>
2020-01-29 18:30:16 +0530

    Add basic unit test class for the VM

---
+++ ImageEditor/src/test/kotlin/org/wordpress/android/imageeditor/home/HomeViewModelTest.kt
@@ -0,0 +1,17 @@
+package org.wordpress.android.imageeditor.home
+
+import org.junit.Before
+import org.junit.Test
+
+class HomeViewModelTest {
+    // Class under test
+    private lateinit var viewModel: HomeViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = HomeViewModel()
+    }
+
+    @Test
+    fun `sample test`() {} // TODO:
+}
