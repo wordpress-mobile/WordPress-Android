2ab3525c57022e9889ed15870d8fa786794d3dca
Joel Dean <jdeanjj1000@gmail.com>
2020-02-02 20:45:54 -0500

    add override to change payload so that animation doesnt blink.

--- WordPress/src/main/java/org/wordpress/android/ui/pages/PageItemDiffUtil.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/pages/PageItemDiffUtil.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.ui.pages
 import androidx.recyclerview.widget.DiffUtil.Callback
 import org.wordpress.android.ui.pages.PageItem.Page
 import org.wordpress.android.ui.pages.PageItem.ParentPage
+import org.wordpress.android.viewmodel.posts.PostListItemProgressBar.Determinate
 
 class PageItemDiffUtil(val items: List<PageItem>, val result: List<PageItem>) : Callback() {
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
@@ -21,5 +22,25 @@ class PageItemDiffUtil(val items: List<PageItem>, val result: List<PageItem>) : 
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         return items[oldItemPosition] == result[newItemPosition]
+    }
+
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+        val oldItem = items[oldItemPosition]
+        val newItem = result[newItemPosition]
+
+        if(oldItem is Page && newItem is Page) {
+            /**
+             * Suppresses the default animation if the progress has changed to prevent blinking as the upload progresses
+             *
+             * We don't need to use the payload in onBindViewHolder unless we want to. Passing a non-null value
+             * suppresses the default ItemAnimator, which is all we need in this case.
+             */
+            if (oldItem.progressBarState is Determinate &&
+                    newItem.progressBarState is Determinate &&
+                    (oldItem.progressBarState as Determinate).progress != (newItem.progressBarState as Determinate).progress) {
+                return true
+            }
+        }
+        return super.getChangePayload(oldItemPosition, newItemPosition)
     }
 }
