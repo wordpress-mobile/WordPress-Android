e67752deb599445f7b4e119680b3034b4ec3bd6c
Klymentiy haykov <forsver@gmail.com>
2020-02-04 23:25:21 -0800

    Do not style our icons with colors retrieved from server.

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/segments/SiteCreationSegmentViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/segments/SiteCreationSegmentViewHolder.kt
@@ -1,7 +1,5 @@
 package org.wordpress.android.ui.sitecreation.segments
 
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
@@ -14,13 +12,18 @@ import androidx.recyclerview.widget.RecyclerView
 import org.wordpress.android.R
 import org.wordpress.android.ui.sitecreation.segments.SegmentsItemUiState.HeaderUiState
 import org.wordpress.android.ui.sitecreation.segments.SegmentsItemUiState.SegmentUiState
-import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.image.ImageManager
 import org.wordpress.android.util.image.ImageManager.RequestListener
 import org.wordpress.android.util.image.ImageType.ICON
 
 sealed class SiteCreationSegmentViewHolder(internal val parent: ViewGroup, @LayoutRes layout: Int) :
-        RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(layout, parent, false)) {
+        RecyclerView.ViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                        layout,
+                        parent,
+                        false
+                )
+        ) {
     abstract fun onBind(uiState: SegmentsItemUiState)
 
     class SegmentsItemViewHolder(
@@ -48,14 +51,17 @@ sealed class SiteCreationSegmentViewHolder(internal val parent: ViewGroup, @Layo
                         }
 
                         override fun onResourceReady(resource: Drawable) {
-                            try {
-                                icon.setColorFilter(Color.parseColor(uiState.iconColor), PorterDuff.Mode.SRC_IN)
-                            } catch (e: IllegalArgumentException) {
-                                AppLog.e(
-                                        AppLog.T.SITE_CREATION,
-                                        "Error parsing segment icon color: ${uiState.iconColor}"
-                                )
-                            }
+                            // At the moment, we should not use colors that come from server to style our icons,
+                            // since they interfere with themes. We should uncomment this when ability to request icon
+                            // colors based on current app theme will be implemented.
+                            // try {
+                            //    icon.setColorFilter(Color.parseColor(uiState.iconColor), PorterDuff.Mode.SRC_IN)
+                            // } catch (e: IllegalArgumentException) {
+                            //    AppLog.e(
+                            //            AppLog.T.SITE_CREATION,
+                            //            "Error parsing segment icon color: ${uiState.iconColor}"
+                            //    )
+                            // }
                         }
                     }
             )
