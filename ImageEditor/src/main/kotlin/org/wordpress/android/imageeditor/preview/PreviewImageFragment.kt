f22898b10bbd563502ea2c04846019faf0ddc7ea
ashiagr <agr.ash@gmail.com>
2020-02-05 12:18:27 +0530

    Show low res preview image using reduced size image url

--- ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/preview/PreviewImageFragment.kt
+++ ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/preview/PreviewImageFragment.kt
@@ -14,9 +14,14 @@ import org.wordpress.android.imageeditor.R.layout
 
 class PreviewImageFragment : Fragment() {
     private lateinit var viewModel: PreviewImageViewModel
+    private val imageEditor = ImageEditor.instance
+
+    private var wasPaused: Boolean = false
+    private var lowResImageUrl: String? = null
 
     companion object {
-        const val ARG_IMAGE_URL = "arg_image_url"
+        const val ARG_LOW_RES_IMAGE_URL = "arg_low_res_image_url"
+        const val ARG_HIGH_RES_IMAGE_URL = "arg_high_res_image_url"
     }
 
     override fun onCreateView(
@@ -28,10 +33,16 @@ class PreviewImageFragment : Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         val intent = activity?.intent
         if (intent != null) {
-            if (intent.hasExtra(ARG_IMAGE_URL)) {
+            if (intent.hasExtra(ARG_LOW_RES_IMAGE_URL)) {
                 outState.putString(
-                        ARG_IMAGE_URL,
-                        intent.getStringExtra(ARG_IMAGE_URL)
+                        ARG_LOW_RES_IMAGE_URL,
+                        intent.getStringExtra(ARG_LOW_RES_IMAGE_URL)
+                )
+            }
+            if (intent.hasExtra(ARG_HIGH_RES_IMAGE_URL)) {
+                outState.putString(
+                        ARG_HIGH_RES_IMAGE_URL,
+                        intent.getStringExtra(ARG_HIGH_RES_IMAGE_URL)
                 )
             }
         }
@@ -41,30 +52,39 @@ class PreviewImageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initializeViews(savedInstanceState)
-        initializeViewModels()
-    }
-
-    private fun initializeViews(savedInstanceState: Bundle?) {
         val nonNullIntent = checkNotNull(requireActivity().intent)
 
-        // TODO: replace ARG_IMAGE_URL with thumb/ low res image url
-        val previewImageUrl = if (savedInstanceState == null) {
-            nonNullIntent.getStringExtra(ARG_IMAGE_URL)
+        lowResImageUrl = if (savedInstanceState == null) {
+            nonNullIntent.getStringExtra(ARG_LOW_RES_IMAGE_URL)
         } else {
-            savedInstanceState.getString(ARG_IMAGE_URL)
+            savedInstanceState.getString(ARG_LOW_RES_IMAGE_URL)
         }
-        previewImageUrl?.let {
-            previewImageFromUrl(it)
-        }
+
+        initializeViewModels()
     }
 
     private fun initializeViewModels() {
         viewModel = ViewModelProviders.of(this).get(PreviewImageViewModel::class.java)
     }
 
-    private fun previewImageFromUrl(@NonNull imageUrl: String) {
-        val imageEditor = ImageEditor.instance
+    private fun loadImage(@NonNull imageUrl: String) {
         imageEditor.loadUrlIntoImageView(imageUrl, previewImageView, CENTER)
+    }
+
+    override fun onPause() {
+        wasPaused = true
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (wasPaused) {
+            wasPaused = false
+        } else {
+            lowResImageUrl?.let {
+                loadImage(it)
+            }
+        }
     }
 }
