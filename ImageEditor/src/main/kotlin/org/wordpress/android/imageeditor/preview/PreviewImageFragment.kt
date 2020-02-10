b8346ea30a19e1124895ed84701397a7b54c34fc
ashiagr <agr.ash@gmail.com>
2020-02-10 15:28:31 +0530

    Pass the URL into the VM using the vm.start(..) approach
    Also load image by observing VM's live data

--- ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/preview/PreviewImageFragment.kt
+++ ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/preview/PreviewImageFragment.kt
@@ -1,12 +1,13 @@
 package org.wordpress.android.imageeditor.preview
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView.ScaleType.CENTER
-import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_preview_image.*
 import org.wordpress.android.imageeditor.ImageEditor
@@ -14,7 +15,6 @@ import org.wordpress.android.imageeditor.R.layout
 
 class PreviewImageFragment : Fragment() {
     private lateinit var viewModel: PreviewImageViewModel
-    private val imageEditor = ImageEditor.instance
 
     companion object {
         const val ARG_LOW_RES_IMAGE_URL = "arg_low_res_image_url"
@@ -32,17 +32,24 @@ class PreviewImageFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val nonNullIntent = checkNotNull(requireActivity().intent)
-        val lowResImageUrl = nonNullIntent.getStringExtra(ARG_LOW_RES_IMAGE_URL) as String
-
-        initializeViewModels()
-        loadImage(lowResImageUrl)
+        initializeViewModels(nonNullIntent)
     }
 
-    private fun initializeViewModels() {
+    private fun initializeViewModels(nonNullIntent: Intent) {
+        val lowResImageUrl = nonNullIntent.getStringExtra(ARG_LOW_RES_IMAGE_URL)
+
         viewModel = ViewModelProvider(this).get(PreviewImageViewModel::class.java)
+        setupObservers()
+        viewModel.start(lowResImageUrl)
     }
 
-    private fun loadImage(@NonNull imageUrl: String) {
-        imageEditor.loadUrlIntoImageView(imageUrl, previewImageView, CENTER)
+    private fun setupObservers() {
+        viewModel.loadImageFromUrl.observe(this, Observer {
+            loadImage(it)
+        })
+    }
+
+    private fun loadImage(it: String) {
+        ImageEditor.instance.loadUrlIntoImageView(it, previewImageView, CENTER)
     }
 }
--- ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/preview/PreviewImageViewModel.kt
+++ ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/preview/PreviewImageViewModel.kt
@@ -1,5 +1,24 @@
 package org.wordpress.android.imageeditor.preview
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class PreviewImageViewModel : ViewModel()
+class PreviewImageViewModel : ViewModel() {
+    private lateinit var lowResImageUrl: String
+
+    private val _loadImageFromUrl = MutableLiveData<String>()
+    val loadImageFromUrl: LiveData<String> = _loadImageFromUrl
+
+    private var isStarted = false
+
+    fun start(loResImageUrl: String) {
+        if (isStarted) {
+            return
+        }
+        isStarted = true
+
+        lowResImageUrl = loResImageUrl
+        _loadImageFromUrl.value = lowResImageUrl
+    }
+}
