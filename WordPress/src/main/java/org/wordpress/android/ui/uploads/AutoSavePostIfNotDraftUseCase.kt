6767c91dc1638619cdf49cd701ecd06f5f589fdd
Oguz Kocer <oguz.kocer@automattic.com>
2020-02-10 14:16:23 -0500

    Refactor AutoSavePostIfNotDraftUseCase

--- WordPress/src/main/java/org/wordpress/android/ui/uploads/AutoSavePostIfNotDraftUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/uploads/AutoSavePostIfNotDraftUseCase.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.uploads
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -13,15 +14,16 @@ import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.fluxc.store.PostStore
 import org.wordpress.android.fluxc.store.PostStore.OnPostChanged
 import org.wordpress.android.fluxc.store.PostStore.OnPostStatusFetched
+import org.wordpress.android.fluxc.store.PostStore.PostError
 import org.wordpress.android.fluxc.store.PostStore.RemotePostPayload
+import org.wordpress.android.modules.BG_THREAD
 import org.wordpress.android.ui.uploads.AutoSavePostIfNotDraftResult.PostAutoSaved
 import org.wordpress.android.ui.uploads.AutoSavePostIfNotDraftResult.PostIsDraftInRemote
 import java.lang.IllegalArgumentException
 import javax.inject.Inject
+import javax.inject.Named
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
-
-// TODO: Not be so verbose in naming
 
 private const val DRAFT_POST_STATUS = "draft"
 
@@ -30,16 +32,22 @@ interface OnAutoSavePostIfNotDraftCallback {
 }
 
 sealed class AutoSavePostIfNotDraftResult {
+    // Initial fetch post status request failed
+    data class PostStatusCheckFailed(val post:PostModel, val error: PostError) : AutoSavePostIfNotDraftResult()
+    // Post status is `DRAFT` in remote which means we'll want to update the draft directly
     data class PostIsDraftInRemote(val post: PostModel) : AutoSavePostIfNotDraftResult()
+    // Post status is not `DRAFT` in remote and the post was auto-saved successfully
     data class PostAutoSaved(val post: PostModel) : AutoSavePostIfNotDraftResult()
-    // TODO: Add error result types
+    // Post status is not `DRAFT` in remote but the post auto-save failed
+    data class PostAutoSaveFailed(val post:PostModel, val error: PostError) : AutoSavePostIfNotDraftResult()
 }
 
-// TODO: Add documentation
+// TODO: Add documentation (add shortcode for the p2 discussion)
 // TODO: Add unit tests
 class AutoSavePostIfNotDraftUseCase @Inject constructor(
     private val dispatcher: Dispatcher,
-    private val postStore: PostStore
+    private val postStore: PostStore,
+    @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher
 ) {
     private val postStatusContinuations = HashMap<LocalId, Continuation<String>>()
     private val autoSaveContinuations = HashMap<LocalId, Continuation<PostModel>>()
@@ -53,31 +61,38 @@ class AutoSavePostIfNotDraftUseCase @Inject constructor(
         callback: OnAutoSavePostIfNotDraftCallback
     ) {
         val localPostId = LocalId(remotePostPayload.post.id)
+        if (remotePostPayload.post.isLocalDraft) {
+            throw IllegalArgumentException("Local drafts should not be auto-saved")
+        }
         if (postStatusContinuations.containsKey(localPostId) ||
                 autoSaveContinuations.containsKey(localPostId)) {
             throw IllegalArgumentException("This post is already being processed. Make sure not to start an autoSave " +
                     "or update draft action while another one is going on.")
         }
-        // TODO: What scope should we use for this?
-        GlobalScope.launch {
-            val remotePostStatus: String = suspendCancellableCoroutine { cont ->
-                postStatusContinuations[localPostId] = cont
-                dispatcher.dispatch(PostActionBuilder.newFetchPostStatusAction(remotePostPayload))
-            }
-
+        GlobalScope.launch(bgDispatcher) {
+            val remotePostStatus = fetchRemotePostStatus(remotePostPayload)
             if (remotePostStatus == DRAFT_POST_STATUS) {
                 callback.handleAutoSavePostIfNotDraftResult(PostIsDraftInRemote(remotePostPayload.post))
             } else {
-                val updatedPost: PostModel = suspendCancellableCoroutine { cont ->
-                    autoSaveContinuations[localPostId] = cont
-                    dispatcher.dispatch(
-                            PostActionBuilder.newRemoteAutoSavePostAction(
-                                    remotePostPayload
-                            )
-                    )
-                }
+                val updatedPost = autoSavePost(remotePostPayload)
                 callback.handleAutoSavePostIfNotDraftResult(PostAutoSaved(updatedPost))
             }
+        }
+    }
+
+    private suspend fun fetchRemotePostStatus(remotePostPayload: RemotePostPayload): String {
+        val localPostId = LocalId(remotePostPayload.post.id)
+        return suspendCancellableCoroutine { cont ->
+            postStatusContinuations[localPostId] = cont
+            dispatcher.dispatch(PostActionBuilder.newFetchPostStatusAction(remotePostPayload))
+        }
+    }
+
+    private suspend fun autoSavePost(remotePostPayload: RemotePostPayload): PostModel {
+        val localPostId = LocalId(remotePostPayload.post.id)
+        return suspendCancellableCoroutine { cont ->
+            autoSaveContinuations[localPostId] = cont
+            dispatcher.dispatch(PostActionBuilder.newRemoteAutoSavePostAction(remotePostPayload))
         }
     }
 
