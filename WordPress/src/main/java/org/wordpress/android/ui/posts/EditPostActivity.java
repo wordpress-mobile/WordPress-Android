251b4056effd89f2c2c07793b5e340dcbf0ad953
malinajirka <malinajirka@gmail.com>
2020-02-10 09:01:39 +0100

    Rename OnPostUpdatedWithUIListener

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -1525,7 +1525,7 @@ public class EditPostActivity extends AppCompatActivity implements
         mViewModel.updatePostObjectWithUIAsync(mEditPostRepository, this::updateFromEditor, null);
     }
 
-    private void updateAndSavePostAsync(final OnPostUpdatedWithUIListener listener) {
+    private void updateAndSavePostAsync(final OnPostUpdatedFromUIListener listener) {
         if (mEditorFragment == null) {
             AppLog.e(AppLog.T.POSTS, "Fragment not initialized");
             return;
@@ -1535,7 +1535,7 @@ public class EditPostActivity extends AppCompatActivity implements
                 (post, result) -> {
                     // Ignore the result as we want to invoke the listener even when the PostModel was up-to-date
                     if (listener != null) {
-                        listener.onPostUpdatedWithUI();
+                        listener.onPostUpdatedFromUI();
                     }
                     return null;
                 });
@@ -1695,8 +1695,8 @@ public class EditPostActivity extends AppCompatActivity implements
         }
     }
 
-    public interface OnPostUpdatedWithUIListener {
-        void onPostUpdatedWithUI();
+    public interface OnPostUpdatedFromUIListener {
+        void onPostUpdatedFromUI();
     }
 
     @Override
@@ -2895,7 +2895,7 @@ public class EditPostActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void syncPostObjectWithUiAndSaveIt(@Nullable OnPostUpdatedWithUIListener listener) {
+    public void syncPostObjectWithUiAndSaveIt(@Nullable OnPostUpdatedFromUIListener listener) {
         updateAndSavePostAsync(listener);
     }
 
--- WordPress/src/main/java/org/wordpress/android/ui/posts/editor/media/EditorMedia.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/editor/media/EditorMedia.kt
@@ -23,7 +23,7 @@ import org.wordpress.android.fluxc.store.MediaStore.CancelMediaPayload
 import org.wordpress.android.fluxc.store.MediaStore.FetchMediaListPayload
 import org.wordpress.android.modules.UI_THREAD
 import org.wordpress.android.ui.pages.SnackbarMessageHolder
-import org.wordpress.android.ui.posts.EditPostActivity.OnPostUpdatedWithUIListener
+import org.wordpress.android.ui.posts.EditPostActivity.OnPostUpdatedFromUIListener
 import org.wordpress.android.ui.posts.EditPostRepository
 import org.wordpress.android.ui.posts.ProgressDialogUiState
 import org.wordpress.android.ui.posts.ProgressDialogUiState.HiddenProgressDialog
@@ -48,7 +48,7 @@ import kotlin.coroutines.CoroutineContext
 
 interface EditorMediaListener {
     fun appendMediaFiles(mediaFiles: Map<String, MediaFile>)
-    fun syncPostObjectWithUiAndSaveIt(listener: OnPostUpdatedWithUIListener? = null)
+    fun syncPostObjectWithUiAndSaveIt(listener: OnPostUpdatedFromUIListener? = null)
     fun advertiseImageOptimization(listener: () -> Unit)
     fun getImmutablePost(): PostImmutableModel
 }
