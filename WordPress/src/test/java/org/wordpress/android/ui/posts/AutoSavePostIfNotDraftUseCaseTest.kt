19fad9d7893be52ccb7014345a6843671e443bd7
Oguz Kocer <oguz.kocer@automattic.com>
2020-02-13 16:28:49 -0500

    Adds unit test for PostAutoSaveFailed

--- WordPress/src/test/java/org/wordpress/android/ui/posts/AutoSavePostIfNotDraftUseCaseTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/posts/AutoSavePostIfNotDraftUseCaseTest.kt
@@ -1,17 +1,25 @@
 package org.wordpress.android.ui.posts
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argWhere
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.InternalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.action.PostAction
+import org.wordpress.android.fluxc.annotations.action.Action
+import org.wordpress.android.fluxc.model.CauseOfOnPostChanged
 import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.store.PostStore
+import org.wordpress.android.fluxc.store.PostStore.OnPostChanged
 import org.wordpress.android.fluxc.store.PostStore.OnPostStatusFetched
 import org.wordpress.android.fluxc.store.PostStore.PostError
 import org.wordpress.android.fluxc.store.PostStore.PostErrorType.UNKNOWN_POST
@@ -20,14 +28,13 @@ import org.wordpress.android.ui.uploads.AutoSavePostIfNotDraftResult
 import org.wordpress.android.ui.uploads.AutoSavePostIfNotDraftUseCase
 import org.wordpress.android.ui.uploads.OnAutoSavePostIfNotDraftCallback
 import java.lang.reflect.Constructor
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
-import org.wordpress.android.TEST_DISPATCHER
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 private const val FETCH_POST_STATUS_ERROR_MESSAGE = "FETCH_POST_STATUS_ERROR_MESSAGE"
+private const val AUTO_SAVE_POST_ERROR_MESSAGE = "AUTO_SAVE_POST_ERROR_MESSAGE"
 private const val DRAFT_STATUS = "DRAFT"
+private const val PUBLISH_STATUS = "PUBLISH"
 
 @InternalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
@@ -35,33 +42,59 @@ class AutoSavePostIfNotDraftUseCaseTest {
     @Rule
     @JvmField val rule = InstantTaskExecutorRule()
 
-    @Mock lateinit var dispatcher: Dispatcher
     @Mock lateinit var postStore: PostStore
 
     @Test
     fun `error while fetching post status will result in FetchPostStatusFailed`() {
-        val useCase = createUseCase()
-        val error = PostError(UNKNOWN_POST, FETCH_POST_STATUS_ERROR_MESSAGE)
         val remotePostPayload = createRemotePostPayload()
-        whenever(dispatcher.dispatch(any())).then {
-            useCase.onPostStatusFetched(
-                    createOnPostStatusFetchedEvent(
-                            post = remotePostPayload.post,
-                            status = DRAFT_STATUS,
-                            error = error
-                    )
-            )
-        }
+        val onPostStatusFetched = createOnPostStatusFetchedEvent(
+                post = remotePostPayload.post,
+                status = DRAFT_STATUS,
+                error = PostError(UNKNOWN_POST, FETCH_POST_STATUS_ERROR_MESSAGE)
+        )
+        val useCase = createUseCase(onPostStatusFetched)
         useCase.autoSavePostOrUpdateDraftAndAssertResult(remotePostPayload) { result ->
             assertThat(result).isInstanceOf(AutoSavePostIfNotDraftResult.FetchPostStatusFailed::class.java)
         }
     }
 
-    private fun createUseCase() = AutoSavePostIfNotDraftUseCase(
-            dispatcher = dispatcher,
-            postStore = postStore,
-            bgDispatcher = TEST_DISPATCHER
-    )
+    @Test
+    fun `error while auto-saving will result in PostAutoSaveFailed`() {
+        val remotePostPayload = createRemotePostPayload()
+        val onPostStatusFetched = createOnPostStatusFetchedEvent(
+                post = remotePostPayload.post,
+                status = PUBLISH_STATUS
+        )
+        val onPostChanged = createOnPostChangedEvent(
+                remotePostPayload.post,
+                error = PostError(UNKNOWN_POST, AUTO_SAVE_POST_ERROR_MESSAGE)
+        )
+        val useCase = createUseCase(onPostStatusFetched, onPostChanged)
+        useCase.autoSavePostOrUpdateDraftAndAssertResult(remotePostPayload) { result ->
+            assertThat(result).isInstanceOf(AutoSavePostIfNotDraftResult.PostAutoSaveFailed::class.java)
+        }
+    }
+
+    private fun createUseCase(
+        onPostStatusFetched: OnPostStatusFetched,
+        onPostChanged: OnPostChanged? = null
+    ): AutoSavePostIfNotDraftUseCase {
+        val dispatcher = mock<Dispatcher>()
+        val useCase = AutoSavePostIfNotDraftUseCase(
+                dispatcher = dispatcher,
+                postStore = postStore,
+                bgDispatcher = TEST_DISPATCHER
+        )
+        whenever(dispatcher.dispatch(argWhere<Action<Void>> { it.type == PostAction.FETCH_POST_STATUS })).then {
+            useCase.onPostStatusFetched(onPostStatusFetched)
+        }
+        onPostChanged?.let { onPostChangedEvent ->
+            whenever(dispatcher.dispatch(argWhere<Action<Void>> { it.type == PostAction.REMOTE_AUTO_SAVE_POST })).then {
+                useCase.onPostChanged(onPostChangedEvent)
+            }
+        }
+        return useCase
+    }
 
     private fun createRemotePostPayload() = RemotePostPayload(PostModel(), SiteModel())
 
@@ -81,6 +114,12 @@ class AutoSavePostIfNotDraftUseCaseTest {
         )
         constructor.isAccessible = true
         return constructor.newInstance(post, status, error)
+    }
+
+    private fun createOnPostChangedEvent(post: PostModel, error: PostError? = null): OnPostChanged {
+        val event = OnPostChanged(CauseOfOnPostChanged.RemoteAutoSavePost(post.id), 0)
+        error?.let { event.error = it }
+        return event
     }
 }
 
