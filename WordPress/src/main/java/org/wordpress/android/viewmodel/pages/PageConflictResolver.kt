487f06b784d014a7ff6539d150871aec084dfff0
Joel Dean <jdeanjj1000@gmail.com>
2020-02-15 13:18:35 -0500

    added logic for determining if there is a conflict with a post.

--- WordPress/src/main/java/org/wordpress/android/viewmodel/pages/PageConflictResolver.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/pages/PageConflictResolver.kt
@@ -1,11 +1,22 @@
 package org.wordpress.android.viewmodel.pages
 
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
 import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.ui.posts.PostUtils
 import javax.inject.Inject
 
 class PageConflictResolver @Inject constructor() {
+    private var postIdForFetchingRemoteVersionOfConflictedPost: LocalId? = null
+    private var postIdForConflictResolutionDialog: LocalId? = null
+
     fun hasUnhandledAutoSave(post: PostModel): Boolean {
         return PostUtils.hasAutoSave(post)
+    }
+
+    fun doesPostHaveUnhandledConflict(post: PostModel): Boolean {
+        // If we are fetching the remote version of a conflicted post, it means it's already being handled
+        val isFetchingConflictedPost = postIdForFetchingRemoteVersionOfConflictedPost != null &&
+                postIdForFetchingRemoteVersionOfConflictedPost == LocalId(post.id)
+        return !isFetchingConflictedPost && PostUtils.isPostInConflictWithRemote(post)
     }
 }
