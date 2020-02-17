4cef88787d0b744118fa625471d86a960268eef5
ashiagr <agr.ash@gmail.com>
2020-02-17 14:42:22 +0530

    Remove unused import

--- WordPress/src/main/java/org/wordpress/android/ui/media/MediaPreviewFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/media/MediaPreviewFragment.java
@@ -19,7 +19,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import org.jetbrains.annotations.NotNull;
 import org.wordpress.android.R;
 import org.wordpress.android.WordPress;
 import org.wordpress.android.fluxc.model.MediaModel;
