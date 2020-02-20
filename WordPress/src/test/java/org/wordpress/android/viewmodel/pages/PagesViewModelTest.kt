3c91442aacbc7c8e23e6d8a98d4027ca6fa4106c
Joel Dean <jdeanjj1000@gmail.com>
2020-02-20 18:56:33 -0500

    added a test to verify scroll takes place when page has been edited.

--- WordPress/src/test/java/org/wordpress/android/viewmodel/pages/PagesViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/pages/PagesViewModelTest.kt
@@ -233,6 +233,21 @@ class PagesViewModelTest {
         assertThat(viewModel.postUploadAction.value).isEqualTo(Triple(pageModel.post, pageModel.site, intent))
     }
 
+    @Test
+    fun `scrollToPage is invoked on edit post activity result`() = test {
+        // Given
+        val intent = mock<Intent>()
+        val pageModel = setUpPageStoreWithASinglePage()
+        whenever(pageStore.getPageByLocalId(eq(pageModel.pageId), anyOrNull()))
+                .thenReturn(pageModel)
+
+        viewModel.start(site)
+        // When
+        viewModel.onPageEditFinished(pageModel.pageId, intent)
+        // Then
+        assertThat(viewModel.scrollToPage.value).isEqualTo(pageModel)
+    }
+
     private suspend fun setUpPageStoreWithEmptyPages() {
         whenever(pageStore.getPagesFromDb(site)).thenReturn(listOf())
         whenever(pageStore.requestPagesFromServer(any())).thenReturn(
