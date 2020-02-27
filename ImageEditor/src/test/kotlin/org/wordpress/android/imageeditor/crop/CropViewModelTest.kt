b85baf9879567cfd00c0497749cc44bd2cf00c58
ashiagr <agr.ash@gmail.com>
2020-02-27 17:44:57 +0530

    Add test for writeToBundle (TODO)

--- ImageEditor/src/test/kotlin/org/wordpress/android/imageeditor/crop/CropViewModelTest.kt
+++ ImageEditor/src/test/kotlin/org/wordpress/android/imageeditor/crop/CropViewModelTest.kt
@@ -5,10 +5,13 @@ import org.junit.Before
 import org.junit.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
+import java.io.File
 
+private const val TEST_INPUT_IMAGE_PATH = "/input/file/path"
 class CropViewModelTest {
     @Rule
     @JvmField val rule = InstantTaskExecutorRule()
+    private val cacheDir = File("/cache/dir")
 
     // Class under test
     private lateinit var viewModel: CropViewModel
@@ -23,4 +26,14 @@ class CropViewModelTest {
         viewModel.onDoneMenuClicked()
         assertThat(viewModel.shouldCropAndSaveImage.value).isEqualTo(true)
     }
+
+    /*@Test
+    fun `file paths written to bundle `() {
+        initViewModel()
+        viewModel.writeToBundle(arguments)
+//        TODO: Mock
+//        verify(arguments).putParcelable(any(), argThat {  })
+    }*/
+
+    private fun initViewModel() = viewModel.start(TEST_INPUT_IMAGE_PATH, cacheDir)
 }
