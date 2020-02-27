26532e18f5619b8261ace48acef0775116f5d72b
ashiagr <agr.ash@gmail.com>
2020-02-27 14:15:05 +0530

    Grab nav args in crop frag and pass it to VM

--- ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/crop/CropFragment.kt
+++ ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/crop/CropFragment.kt
@@ -1,31 +1,37 @@
 package org.wordpress.android.imageeditor.crop
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.yalantis.ucrop.UCropFragment
 import org.wordpress.android.imageeditor.R
+import androidx.navigation.fragment.navArgs
 
 class CropFragment : UCropFragment() {
     private lateinit var viewModel: CropViewModel
+    private val navArgs: CropFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         initializeViewModels()
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     private fun initializeViewModels() {
         viewModel = ViewModelProvider(this).get(CropViewModel::class.java)
         setupObservers()
+        viewModel.start(navArgs.inputFilePath, requireContext().cacheDir)
+        viewModel.writeToBundle(arguments ?: Bundle())
     }
 
     private fun setupObservers() {
--- ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/crop/CropViewModel.kt
+++ ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/crop/CropViewModel.kt
@@ -1,14 +1,59 @@
 package org.wordpress.android.imageeditor.crop
 
+import android.net.Uri
+import android.os.Bundle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.yalantis.ucrop.UCrop
+import com.yalantis.ucrop.UCrop.Options
+import com.yalantis.ucrop.model.AspectRatio
+import com.yalantis.ucrop.view.CropImageView
+import java.io.File
 
 class CropViewModel : ViewModel() {
     private val _shouldCropAndSaveImage = MutableLiveData<Boolean>(false)
     val shouldCropAndSaveImage: LiveData<Boolean> = _shouldCropAndSaveImage
 
+    private lateinit var cacheDir: File
+    private lateinit var inputFilePath: String
+
+    private val cropOptions
+        get() = Options().also {
+            with(it) {
+                setShowCropGrid(true)
+                setFreeStyleCropEnabled(true)
+                setShowCropFrame(true)
+                setHideBottomControls(false)
+                setAspectRatioOptions(
+                    0,
+                    AspectRatio("1:2", 1f, 2f),
+                    AspectRatio("3:4", 3f, 4f),
+                    AspectRatio("Original", CropImageView.DEFAULT_ASPECT_RATIO, CropImageView.DEFAULT_ASPECT_RATIO),
+                    AspectRatio("16:9", 16f, 9f),
+                    AspectRatio("1:1", 1f, 1f)
+                )
+            }
+        }
+
+    fun start(inputFilePath: String, cacheDir: File) {
+        this.cacheDir = cacheDir
+        this.inputFilePath = inputFilePath
+    }
+
+    fun writeToBundle(bundle: Bundle) {
+        with(bundle) {
+            putParcelable(UCrop.EXTRA_INPUT_URI, Uri.fromFile(File(inputFilePath)))
+            putParcelable(UCrop.EXTRA_OUTPUT_URI, Uri.fromFile(File(cacheDir, IMAGE_EDITOR_OUTPUT_IMAGE_FILE_NAME)))
+            putAll(cropOptions.optionBundle)
+        }
+    }
+
     fun onDoneMenuClicked() {
         _shouldCropAndSaveImage.value = true
+    }
+
+    companion object {
+        const val IMAGE_EDITOR_OUTPUT_IMAGE_FILE_NAME = "image_editor_output_image.jpg"
     }
 }
