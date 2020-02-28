978c4dec95e11e7e5a19d8dc2ceb6e181ae134e5
Klymentiy haykov <forsver@gmail.com>
2020-02-28 15:41:59 -0800

    Added cookie response.

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteCookieResponse.kt
@@ -0,0 +1,14 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.site
+
+class SiteCookieResponse(
+    val url: String,
+    val cookies: List<SiteCookie>
+)
+
+class SiteCookie(
+    val expires: String,
+    val path: String?,
+    val domain: String?,
+    val name: Boolean,
+    val value: Boolean
+)
