d8b7e5d1332d3eabc55fafabdb23fd1ea6d51dec
Joel Dean <jdeanjj1000@gmail.com>
2020-02-28 10:11:25 -0500

    Added tests for PostPageListLabelColorUseCase

--- WordPress/src/test/java/org/wordpress/android/viewmodel/pages/PostPageListLabelColorUseCaseTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/pages/PostPageListLabelColorUseCaseTest.kt
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.viewmodel.pages.PostModelUploadUiStateUseCase.PostUploadUiState
+import org.wordpress.android.viewmodel.pages.PostModelUploadUiStateUseCase.PostUploadUiState.UploadQueued
+import org.wordpress.android.viewmodel.pages.PostModelUploadUiStateUseCase.PostUploadUiState.UploadingPost
 
 @RunWith(MockitoJUnitRunner::class)
 class PostPageListLabelColorUseCaseTest {
@@ -48,13 +50,103 @@ class PostPageListLabelColorUseCaseTest {
         assertThat(labelsColor).isEqualTo(PROGRESS_INFO_COLOR)
     }
 
+    @Test
+    fun `label has progress color when post queued`() {
+        // Arrange
+        val uploadState = UploadQueued
+        // Act
+        val labelsColor = useCase.getLabelsColor(
+                dummyPostModel(),
+                uploadState,
+                hasUnhandledConflicts = false,
+                hasUnhandledAutoSave = false
+        )
+        // Assert
+        assertThat(labelsColor).isEqualTo(PROGRESS_INFO_COLOR)
+    }
+
+    @Test
+    fun `label has progress color when uploading post`() {
+        // Arrange
+        val uploadState = UploadingPost(false)
+        // Act
+        val labelsColor = useCase.getLabelsColor(
+                dummyPostModel(),
+                uploadState,
+                hasUnhandledConflicts = false,
+                hasUnhandledAutoSave = false
+        )
+        // Assert
+        assertThat(labelsColor).isEqualTo(PROGRESS_INFO_COLOR)
+    }
+
+    @Test
+    fun `label has state info color after failed upload but eligible for auto upload`() {
+        // Arrange
+        val uploadState = failedUpload(isEligibleForAutoUpload = true)
+        // Act
+        val labelsColor = useCase.getLabelsColor(
+                dummyPostModel(),
+                uploadState,
+                hasUnhandledConflicts = false,
+                hasUnhandledAutoSave = false
+        )
+        // Assert
+        assertThat(labelsColor).isEqualTo(STATE_INFO_COLOR)
+    }
+
+    @Test
+    fun `label has error color after failed upload when not eligible for auto upload`() {
+        // Arrange
+        val uploadState = failedUpload(isEligibleForAutoUpload = false)
+        // Act
+        val labelsColor = useCase.getLabelsColor(
+                dummyPostModel(),
+                uploadState,
+                hasUnhandledConflicts = false,
+                hasUnhandledAutoSave = false
+        )
+        // Assert
+        assertThat(labelsColor).isEqualTo(ERROR_COLOR)
+    }
+
+    @Test
+    fun `label has state info color on auto-save conflict`() {
+        // Arrange
+        val hasUnhandledAutoSave = true
+        // Act
+        val labelsColor = useCase.getLabelsColor(
+                dummyPostModel(),
+                mock(),
+                hasUnhandledConflicts = false,
+                hasUnhandledAutoSave = hasUnhandledAutoSave
+        )
+        // Assert
+        assertThat(labelsColor).isEqualTo(STATE_INFO_COLOR)
+    }
+
+    @Test
+    fun `label has error color on version conflict`() {
+        // Arrange
+        val hasUnhandledConflicts = true
+        // Act
+        val labelsColor = useCase.getLabelsColor(
+                dummyPostModel(),
+                mock(),
+                hasUnhandledConflicts = hasUnhandledConflicts,
+                hasUnhandledAutoSave = false
+        )
+        // Assert
+        assertThat(labelsColor).isEqualTo(ERROR_COLOR)
+    }
+
     private fun dummyPostModel(): PostModel {
         val post = PostModel()
         post.setDateCreated("1970-01-01'T'00:00:01Z")
         return post
     }
 
-    private fun failedUpload() = PostUploadUiState.UploadFailed(mock(), false, false)
+    private fun failedUpload(isEligibleForAutoUpload: Boolean = false) = PostUploadUiState.UploadFailed(mock(), isEligibleForAutoUpload, false)
 
     private fun mediaUploadInProgress() = PostUploadUiState.UploadingMedia(0)
 }
