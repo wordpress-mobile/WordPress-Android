fa373c09a687659c75f543a9db786c2a5f34704e
ashiagr <agr.ash@gmail.com>
2020-03-03 11:52:17 +0530

    Attempt to reduce inheritance coupling

--- ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/crop/CropFragment.kt
+++ ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/crop/CropFragment.kt
@@ -1,26 +1,40 @@
 package org.wordpress.android.imageeditor.crop
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.yalantis.ucrop.UCropFragment
 import org.wordpress.android.imageeditor.R
+import com.yalantis.ucrop.UCropFragment
 
-class CropFragment : UCropFragment() {
+/**
+ * Container fragment for displaying third party crop fragment.
+ */
+class CropFragment : Fragment() {
     private lateinit var viewModel: CropViewModel
+    private lateinit var thirdPartyCropFragment: UCropFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_crop_image, container, false)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initializeViewModels()
+        showThirdPartyCropFragment()
     }
 
     private fun initializeViewModels() {
@@ -31,7 +45,7 @@ class CropFragment : UCropFragment() {
     private fun setupObservers() {
         viewModel.shouldCropAndSaveImage.observe(this, Observer { shouldCropAndSaveImage ->
             if (shouldCropAndSaveImage) {
-                cropAndSaveImage()
+                thirdPartyCropFragment.cropAndSaveImage()
             }
         })
     }
@@ -46,5 +60,13 @@ class CropFragment : UCropFragment() {
         true
     } else {
         super.onOptionsItemSelected(item)
+    }
+
+    private fun showThirdPartyCropFragment() {
+        thirdPartyCropFragment = UCropFragment.newInstance(arguments)
+        childFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, thirdPartyCropFragment, UCropFragment.TAG)
+            .disallowAddToBackStack()
+            .commit()
     }
 }
