2ce96630d2c09faaf062ec1828b1dbacb6c2bb42
Joel Dean <jdeanjj1000@gmail.com>
2020-03-12 00:20:06 -0500

    trim comment to ensure it doesn't submit when it only has whitespaces.

--- WordPress/src/main/java/org/wordpress/android/ui/CommentFullScreenDialogFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/CommentFullScreenDialogFragment.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.ui
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
@@ -45,7 +46,7 @@ class CommentFullScreenDialogFragment : Fragment(), CollapseFullScreenDialogCont
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
 
             override fun afterTextChanged(s: Editable) {
-                dialogController.setConfirmEnabled(s.isNotEmpty())
+                dialogController.setConfirmEnabled(!TextUtils.isEmpty(s.toString().trim()))
             }
         })
 
