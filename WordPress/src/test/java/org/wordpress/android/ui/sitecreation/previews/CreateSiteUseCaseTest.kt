612e9d170c0e0fafc402325b5c8e672e6aa1262c
ashiagr <agr.ash@gmail.com>
2020-03-13 16:05:21 +0530

    Update tests to not include site title and tagline

--- WordPress/src/test/java/org/wordpress/android/ui/sitecreation/previews/CreateSiteUseCaseTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/sitecreation/previews/CreateSiteUseCaseTest.kt
@@ -26,8 +26,6 @@ import org.wordpress.android.util.UrlUtilsWrapper
 
 private val DUMMY_SITE_DATA: SiteCreationServiceData = SiteCreationServiceData(
         123,
-        "title",
-        "tagLine",
         "slug"
 )
 private const val LANGUAGE_ID = "lang_id"
@@ -70,9 +68,7 @@ class CreateSiteUseCaseTest {
         assertThat(captor.value.payload).isInstanceOf(NewSitePayload::class.java)
         val payload = captor.value.payload as NewSitePayload
         assertThat(payload.siteName).isEqualTo(DUMMY_SITE_DATA.domain)
-        assertThat(payload.siteTitle).isEqualTo(DUMMY_SITE_DATA.siteTitle)
         assertThat(payload.segmentId).isEqualTo(DUMMY_SITE_DATA.segmentId)
-        assertThat(payload.tagLine).isEqualTo(DUMMY_SITE_DATA.siteTagLine)
     }
 
     @Test
