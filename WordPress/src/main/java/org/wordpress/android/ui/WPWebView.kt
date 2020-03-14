8cd0b46980ac9a6fe6705265529198cd051180ec
Klymentiy haykov <forsver@gmail.com>
2020-03-14 14:37:19 -0700

    Using themed context in updated webview.

--- WordPress/src/main/java/org/wordpress/android/ui/WPWebView.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/WPWebView.kt
@@ -5,6 +5,8 @@ import android.content.res.Configuration
 import android.os.Build
 import android.util.AttributeSet
 import android.webkit.WebView
+import androidx.appcompat.view.ContextThemeWrapper
+import org.wordpress.android.R
 
 // https://stackoverflow.com/questions/58028821/webview-crash-on-android-5-5-1-api-21-22-resourcesnotfoundexception-string-r
 open class WPWebView @JvmOverloads constructor(
@@ -18,6 +20,6 @@ open class WPWebView @JvmOverloads constructor(
 
 private fun Context.getModifiedContextForOlderAPIs(): Context {
     return if (Build.VERSION.SDK_INT in 21..22) {
-        createConfigurationContext(Configuration())
+        ContextThemeWrapper(createConfigurationContext(Configuration()), R.style.WordPress)
     } else this
 }
