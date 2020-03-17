c27d094b01712fd66b3a272bc6c2b5fd2e6d1851
vojtasmrcek <planarvoid@gmail.com>
2020-03-17 14:54:06 +0100

    Use composition instead of inheritance

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/tools/CoroutineEngine.kt
@@ -0,0 +1,27 @@
+package org.wordpress.android.fluxc.tools
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.withContext
+import org.wordpress.android.fluxc.utils.AppLogWrapper
+import org.wordpress.android.util.AppLog
+import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
+
+class CoroutineEngine
+@Inject constructor(private val coroutineContext: CoroutineContext, private val appLog: AppLogWrapper) {
+    suspend fun <RESULT_TYPE> runOnBackground(
+        tag: AppLog.T,
+        caller: Any,
+        loggedMessage: String,
+        block: suspend CoroutineScope.() -> RESULT_TYPE
+    ): RESULT_TYPE {
+        appLog.d(tag, "${caller.javaClass.simpleName}: $loggedMessage")
+        return withContext(coroutineContext, block)
+    }
+
+    fun <RESULT_TYPE> run(tag: AppLog.T, caller: Any, loggedMessage: String, block: () -> RESULT_TYPE): RESULT_TYPE {
+        appLog.d(tag, "${caller.javaClass.simpleName}: $loggedMessage")
+        return block()
+    }
+}
+
