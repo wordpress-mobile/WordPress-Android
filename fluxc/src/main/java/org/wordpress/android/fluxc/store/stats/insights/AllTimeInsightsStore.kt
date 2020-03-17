54fc67f380db4f77b3f5ddb6dfa5337628ea920a
vojtasmrcek <planarvoid@gmail.com>
2020-03-17 14:54:06 +0100

    Use composition instead of inheritance

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/stats/insights/AllTimeInsightsStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/stats/insights/AllTimeInsightsStore.kt
@@ -5,30 +5,28 @@ import org.wordpress.android.fluxc.model.stats.InsightsAllTimeModel
 import org.wordpress.android.fluxc.model.stats.InsightsMapper
 import org.wordpress.android.fluxc.network.rest.wpcom.stats.insights.AllTimeInsightsRestClient
 import org.wordpress.android.fluxc.persistence.InsightsSqlUtils.AllTimeSqlUtils
-import org.wordpress.android.fluxc.store.BaseStore
 import org.wordpress.android.fluxc.store.StatsStore.OnStatsFetched
 import org.wordpress.android.fluxc.store.StatsStore.StatsError
 import org.wordpress.android.fluxc.store.StatsStore.StatsErrorType.INVALID_RESPONSE
+import org.wordpress.android.fluxc.tools.CoroutineEngine
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class AllTimeInsightsStore @Inject constructor(
     private val restClient: AllTimeInsightsRestClient,
     private val sqlUtils: AllTimeSqlUtils,
     private val insightsMapper: InsightsMapper,
-    private val coroutineContext: CoroutineContext,
-    loggerFactory: LoggerFactory
-) : BaseStore(loggerFactory.build(AppLog.T.STATS)) {
+    private val coroutineEngine: CoroutineEngine
+) {
     suspend fun fetchAllTimeInsights(site: SiteModel, forced: Boolean = false) =
-            withContext(coroutineContext, "fetchAllTimeInsights") {
+            coroutineEngine.runOnBackground(AppLog.T.STATS, this, "fetchAllTimeInsights") {
                 if (!forced && sqlUtils.hasFreshRequest(site)) {
-                    return@withContext OnStatsFetched(getAllTimeInsights(site), cached = true)
+                    return@runOnBackground OnStatsFetched(getAllTimeInsights(site), cached = true)
                 }
                 val payload = restClient.fetchAllTimeInsights(site, forced)
-                return@withContext when {
+                return@runOnBackground when {
                     payload.isError -> OnStatsFetched(payload.error)
                     payload.response != null -> {
                         sqlUtils.insert(site, payload.response)
@@ -38,7 +36,8 @@ class AllTimeInsightsStore @Inject constructor(
                 }
             }
 
-    fun getAllTimeInsights(site: SiteModel): InsightsAllTimeModel? = withLog("getAllTimeInsights") {
-        return@withLog sqlUtils.select(site)?.let { insightsMapper.map(it, site) }
-    }
+    fun getAllTimeInsights(site: SiteModel): InsightsAllTimeModel? =
+            coroutineEngine.run(AppLog.T.STATS, this, "getAllTimeInsights") {
+                sqlUtils.select(site)?.let { insightsMapper.map(it, site) }
+            }
 }
