26bba8be748e3c473f1a92d3adf76befa314e9f4
Jeremy Massel <jeremy.massel@automattic.com>
2020-03-24 21:44:17 -0600

    Add a test host for connected tests

---
+++ libs/test-host/src/main/java/org/wordpress/testhost/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.wordpress.testhost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
