18f0c2eeebd11c60147b97471039187e82d92150
Thomaz Cortez <thomazfcortez@gmail.com>
2020-03-26 04:41:50 -0300

    Added tests for GIF Picker DataSource

---
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/giphy/GiphyPickerDataSourceFixtures.kt
@@ -0,0 +1,12 @@
+package org.wordpress.android.viewmodel.giphy
+
+import com.nhaarman.mockitokotlin2.mock
+
+object GiphyPickerDataSourceFixtures {
+    internal val expectedGifMediaViewModelCollection: List<GiphyMediaViewModel> = listOf(
+            mock(),
+            mock(),
+            mock(),
+            mock()
+    )
+}
