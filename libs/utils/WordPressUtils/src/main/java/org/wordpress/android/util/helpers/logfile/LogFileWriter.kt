b1ce39c150b782227c757dc60319c00c49313162
Oguz Kocer <oguz.kocer@automattic.com>
2020-03-30 16:45:23 -0400

    Minor changes in LogFileWriter to make it more idiomatic
    * Removes `this`
    * Makes the `File` `@TestOnly`

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/helpers/logfile/LogFileWriter.kt
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/helpers/logfile/LogFileWriter.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.util.helpers.logfile
 
 import android.content.Context
+import org.jetbrains.annotations.TestOnly
 import java.io.File
 import java.io.FileWriter
 import java.util.Date
@@ -12,31 +13,29 @@ import org.wordpress.android.util.DateTimeUtils
  * This class creates and writes to a log file, and will typically persist for the entire lifecycle
  * of its host application.
  */
-class LogFileWriter {
-    private val fileWriter: FileWriter
+class LogFileWriter
+/**
+ * General-purpose constructor – given a log file directory, creates and prepares a log file for writing.
+ * @param context: The Android [context] object
+ */ @JvmOverloads constructor(
+    context: Context,
+    id: String = DateTimeUtils.iso8601FromDate(Date())
+) {
+    private val file = File(LogFileHelpers.logFileDirectory(context), "$id.log")
+    private val fileWriter: FileWriter = FileWriter(file)
 
     /**
      * A reference to the underlying {@link Java.IO.File} file.
      * Should only be used for testing.
      */
-    internal val file: File
-
-    /**
-     * General-purpose constructor – given a log file directory, creates and prepares a log file for writing.
-     * @param context: The Android [context] object
-     */
-    @JvmOverloads
-    constructor(context: Context, id: String = DateTimeUtils.iso8601FromDate(Date())) {
-        val logFileDirectory = LogFileHelpers.logFileDirectory(context)
-        this.file = File(logFileDirectory, "$id.log")
-        this.fileWriter = FileWriter(this.file)
-    }
+    @TestOnly
+    fun getFile(): File = file
 
     /**
      * Writes the provided string to the log file synchronously
      */
     fun write(data: String) {
-        this.fileWriter.write(data)
-        this.fileWriter.flush()
+        fileWriter.write(data)
+        fileWriter.flush()
     }
 }
