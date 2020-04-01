f13e77dfc3c970bffc762b07f8991c21e2620ccc
Antonis Lilis <a.lilis@afse.eu>
2020-04-01 22:15:14 +0300

    11532: Fixes snackbar statuses and editor switch when reblogging from the filtered post list

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListActivity.java
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.reader;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
@@ -17,15 +18,33 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.appbar.AppBarLayout;
 
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 import org.wordpress.android.R;
+import org.wordpress.android.WordPress;
 import org.wordpress.android.analytics.AnalyticsTracker;
 import org.wordpress.android.analytics.AnalyticsTracker.Stat;
 import org.wordpress.android.datasets.ReaderBlogTable;
+import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.model.PostModel;
+import org.wordpress.android.fluxc.model.SiteModel;
+import org.wordpress.android.fluxc.store.PostStore;
+import org.wordpress.android.fluxc.store.PostStore.OnPostUploaded;
+import org.wordpress.android.fluxc.store.SiteStore;
 import org.wordpress.android.models.ReaderBlog;
 import org.wordpress.android.models.ReaderTag;
+import org.wordpress.android.ui.ActivityLauncher;
+import org.wordpress.android.ui.RequestCodes;
+import org.wordpress.android.ui.posts.EditPostActivity;
+import org.wordpress.android.ui.prefs.AppPrefs;
 import org.wordpress.android.ui.reader.ReaderTypes.ReaderPostListType;
+import org.wordpress.android.ui.uploads.UploadActionUseCase;
+import org.wordpress.android.ui.uploads.UploadUtils;
+import org.wordpress.android.ui.uploads.UploadUtilsWrapper;
 import org.wordpress.android.util.LocaleManager;
 import org.wordpress.android.util.ToastUtils;
+
+import javax.inject.Inject;
 
 /*
  * serves as the host for ReaderPostListFragment when showing blog preview & tag preview
@@ -33,6 +52,12 @@ import org.wordpress.android.util.ToastUtils;
 public class ReaderPostListActivity extends AppCompatActivity {
     private ReaderPostListType mPostListType;
     private long mSiteId;
+
+    @Inject SiteStore mSiteStore;
+    @Inject PostStore mPostStore;
+    @Inject Dispatcher mDispatcher;
+    @Inject UploadActionUseCase mUploadActionUseCase;
+    @Inject UploadUtilsWrapper mUploadUtilsWrapper;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -42,6 +67,8 @@ public class ReaderPostListActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((WordPress) getApplication()).component().inject(this);
+
         setContentView(R.layout.reader_activity_post_list);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -105,6 +132,19 @@ public class ReaderPostListActivity extends AppCompatActivity {
         super.onResumeFragments();
         // this particular Activity doesn't show filtering, so we'll disable the FilteredRecyclerView toolbar here
         disableFilteredRecyclerViewToolbar();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // We register the dispatcher in order to receive the OnPostUploaded event and show the snackbar
+        mDispatcher.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDispatcher.unregister(this);
     }
 
     /*
@@ -263,5 +303,56 @@ public class ReaderPostListActivity extends AppCompatActivity {
             return null;
         }
         return ((ReaderPostListFragment) fragment);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RequestCodes.EDIT_POST && resultCode == Activity.RESULT_OK
+            && data != null && !isFinishing()) {
+            int localId = data.getIntExtra(EditPostActivity.EXTRA_POST_LOCAL_ID, 0);
+            final SiteModel site = (SiteModel) data.getSerializableExtra(WordPress.SITE);
+            final PostModel post = mPostStore.getPostByLocalPostId(localId);
+
+            if (EditPostActivity.checkToRestart(data)) {
+                ActivityLauncher.editPostOrPageForResult(data, ReaderPostListActivity.this, site,
+                        data.getIntExtra(EditPostActivity.EXTRA_POST_LOCAL_ID, 0));
+                // a restart will happen so, no need to continue here
+                return;
+            }
+
+            if (site != null && post != null) {
+                mUploadUtilsWrapper.handleEditPostResultSnackbars(
+                        this,
+                        findViewById(R.id.coordinator),
+                        data,
+                        post,
+                        site,
+                        mUploadActionUseCase.getUploadAction(post),
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                UploadUtils.publishPost(ReaderPostListActivity.this, post, site, mDispatcher);
+                            }
+                        });
+            }
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onPostUploaded(OnPostUploaded event) {
+        int siteLocalId = AppPrefs.getSelectedSite();
+        SiteModel site = mSiteStore.getSiteByLocalId(siteLocalId);
+        if (site != null && event.post != null) {
+            mUploadUtilsWrapper.onPostUploadedSnackbarHandler(
+                    this,
+                    findViewById(R.id.coordinator),
+                    event.isError(),
+                    event.post,
+                    null,
+                    site);
+        }
     }
 }
