37e8c8739bf3a11f66e4fb9f1dbf2a76c8ef19da
Klymentiy haykov <forsver@gmail.com>
2020-04-05 20:29:16 -0700

    Added missing temp progress dialog.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/PrivateAtCookieRefreshProgressDialog.kt
@@ -0,0 +1,18 @@
+package org.wordpress.android.ui
+
+import android.app.Dialog
+import android.app.ProgressDialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class PrivateAtCookieRefreshProgressDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+       return ProgressDialog.show(
+                activity, "", "Establishing access to media files on private site.", true, true
+        )
+    }
+
+    companion object {
+        const val TAG = "private_at_cookie_progress_dialog"
+    }
+}
