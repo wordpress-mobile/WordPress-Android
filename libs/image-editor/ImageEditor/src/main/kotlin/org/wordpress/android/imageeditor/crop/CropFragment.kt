162f9fa2484b94487d01120f47c8a8cbab29c4f3
ashiagr <agr.ash@gmail.com>
2020-04-06 09:38:55 +0530

    Add material, viewpager2 dependencies

--- libs/image-editor/ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/crop/CropFragment.kt
+++ libs/image-editor/ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/crop/CropFragment.kt
@@ -97,10 +97,10 @@ class CropFragment : Fragment(), UCropFragmentCallback {
         })
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.menu_crop_fragment, menu)
-        doneMenu = menu?.findItem(R.id.menu_done)
+        inflater.inflate(R.menu.menu_crop_fragment, menu)
+        doneMenu = menu.findItem(R.id.menu_done)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = if (item.itemId == R.id.menu_done) {
