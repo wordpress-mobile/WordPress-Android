5b2555720b42ea6ae47588f5b838c2b04dce9364
vojtasmrcek <planarvoid@gmail.com>
2020-04-09 14:30:40 +0200

    Use CoroutineEngine in Transactions store

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/TransactionsStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/TransactionsStore.kt
@@ -1,8 +1,6 @@
 package org.wordpress.android.fluxc.store
 
 import android.text.TextUtils
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
@@ -19,32 +17,32 @@ import org.wordpress.android.fluxc.network.rest.wpcom.transactions.SupportedDoma
 import org.wordpress.android.fluxc.network.rest.wpcom.transactions.TransactionsRestClient
 import org.wordpress.android.fluxc.network.rest.wpcom.transactions.TransactionsRestClient.CreateShoppingCartResponse
 import org.wordpress.android.fluxc.store.TransactionsStore.FetchSupportedCountriesErrorType.GENERIC_ERROR
+import org.wordpress.android.fluxc.tools.CoroutineEngine
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class TransactionsStore @Inject constructor(
     private val transactionsRestClient: TransactionsRestClient,
-    private val coroutineContext: CoroutineContext,
+    private val coroutineEngine: CoroutineEngine,
     dispatcher: Dispatcher
 ) : Store(dispatcher) {
     @Subscribe(threadMode = ThreadMode.ASYNC)
     override fun onAction(action: Action<*>) {
         when (action.type as? TransactionAction ?: return) {
             FETCH_SUPPORTED_COUNTRIES -> {
-                GlobalScope.launch(coroutineContext) {
+                coroutineEngine.launch(AppLog.T.API, this, "FETCH_SUPPORTED_COUNTRIES") {
                     emitChange(fetchSupportedCountries())
                 }
             }
             CREATE_SHOPPING_CART -> {
-                GlobalScope.launch(coroutineContext) {
+                coroutineEngine.launch(AppLog.T.API, this, "CREATE_SHOPPING_CART") {
                     emitChange(createShoppingCart(action.payload as CreateShoppingCartPayload))
                 }
             }
             REDEEM_CART_WITH_CREDITS -> {
-                GlobalScope.launch(coroutineContext) {
+                coroutineEngine.launch(AppLog.T.API, this, "REDEEM_CART_WITH_CREDITS") {
                     emitChange(redeemCartUsingCredits(action.payload as RedeemShoppingCartPayload))
                 }
             }
