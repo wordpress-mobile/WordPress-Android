538b50763e78106fcb3aaa98d3e02c18f616e4f3
vojtasmrcek <planarvoid@gmail.com>
2020-04-09 16:42:29 +0200

    Use composition instead of interface CoroutineScope

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/tools/CoroutineEngine.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/tools/CoroutineEngine.kt
@@ -13,10 +13,8 @@ class CoroutineEngine
 @Inject constructor(
     private val context: CoroutineContext,
     private val appLog: AppLogWrapper
-) : CoroutineScope {
-    private val job = Job()
-    override val coroutineContext: CoroutineContext
-        get() = job + context
+) {
+    private val coroutineScope = CoroutineScope(context)
 
     suspend fun <RESULT_TYPE> withDefaultContext(
         tag: AppLog.T,
@@ -25,7 +23,7 @@ class CoroutineEngine
         block: suspend CoroutineScope.() -> RESULT_TYPE
     ): RESULT_TYPE {
         appLog.d(tag, "${caller.javaClass.simpleName}: $loggedMessage")
-        return withContext(coroutineContext, block)
+        return withContext(context, block)
     }
 
     fun <RESULT_TYPE> run(tag: AppLog.T, caller: Any, loggedMessage: String, block: () -> RESULT_TYPE): RESULT_TYPE {
@@ -40,7 +38,7 @@ class CoroutineEngine
         block: suspend CoroutineScope.() -> RESULT_TYPE
     ): Job {
         appLog.d(tag, "${caller.javaClass.simpleName}: $loggedMessage")
-        return launch(coroutineContext) {
+        return coroutineScope.launch {
             block(this)
         }
     }
