f99862a479b54d31d1d34fff1c6a514b91ecd8f6
Joel Dean <jdeanjj1000@gmail.com>
2020-04-09 14:38:22 -0500

    actions viewmodel tests.

---
+++ WordPress/src/test/java/org/wordpress/android/ui/posts/PrepublishingActionsViewModelTest.kt
@@ -0,0 +1,82 @@
+package org.wordpress.android.ui.posts
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.ActionType
+import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.ActionType.PUBLISH
+import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.ActionType.VISIBILITY
+import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.PrepublishingActionUiState
+
+@RunWith(MockitoJUnitRunner::class)
+class PrepublishingActionsViewModelTest {
+    @Rule
+    @JvmField val rule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: PrepublishingActionsViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = PrepublishingActionsViewModel()
+        viewModel.start()
+    }
+
+    @Test
+    fun `verify that actions are propagated to prepublishingActionsUiState once the viewModel is started`() {
+        // arrange
+        val expectedActionsAmount = 3
+
+        // act - viewModel is already started
+
+        // assert
+        assertThat(viewModel.prepublishingActionsUiState.value?.size).isEqualTo(expectedActionsAmount)
+    }
+
+    @Test
+    fun `verify that when publish action is clicked the right action type is propagated to prepublishingActionType`() {
+        // arrange
+        val expectedActionType = PUBLISH
+
+        // act
+        val publishAction = getActionUiState(expectedActionType)
+        publishAction.onActionClicked.invoke(expectedActionType)
+
+        // assert
+        assertThat(viewModel.prepublishingActionType.value).isEqualTo(expectedActionType)
+    }
+
+    @Test
+    fun `verify that when visibility action is clicked the right action type is propagated to prepublishingActionType`() {
+        // arrange
+        val expectedActionType = VISIBILITY
+
+        // act
+        val visibilityAction = getActionUiState(expectedActionType)
+        visibilityAction.onActionClicked.invoke(expectedActionType)
+
+        // assert
+        assertThat(viewModel.prepublishingActionType.value).isEqualTo(expectedActionType)
+    }
+
+    @Test
+    fun `verify that when tags action is clicked the right action type is propagated to prepublishingActionType`() {
+        // arrange
+        val expectedActionType = VISIBILITY
+
+        // act
+        val tagsAction = getActionUiState(expectedActionType)
+        tagsAction.onActionClicked.invoke(expectedActionType)
+
+        // assert
+        assertThat(viewModel.prepublishingActionType.value).isEqualTo(expectedActionType)
+    }
+
+    private fun getActionUiState(actionType: ActionType): PrepublishingActionUiState {
+        val actions = viewModel.prepublishingActionsUiState.value?.filterIsInstance(PrepublishingActionUiState::class.java)
+        return actions?.find { it.actionType == actionType }!!
+    }
+}
