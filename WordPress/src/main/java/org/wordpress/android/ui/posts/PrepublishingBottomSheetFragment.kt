ede4cf6279193ce007bae7c2c266d042dbe63fb3
Joel Dean <jdeanjj1000@gmail.com>
2020-04-12 21:54:54 -0500

    Started setting up navigation structure within the parent viewmodel

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingBottomSheetFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingBottomSheetFragment.kt
@@ -6,19 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import org.apache.commons.lang3.NotImplementedException
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.login.widgets.WPBottomSheetDialogFragment
+import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.ActionType
+import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.ActionType.PUBLISH
+import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.ActionType.TAGS
+import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.ActionType.VISIBILITY
 import javax.inject.Inject
 
 class PrepublishingBottomSheetFragment : WPBottomSheetDialogFragment() {
     @Inject internal lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    private lateinit var viewModel: PrepublishingViewModel
+    private lateinit var prepublishingViewModel: PrepublishingViewModel
+    private lateinit var prepublishingActionsViewModel: PrepublishingActionsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +42,7 @@ class PrepublishingBottomSheetFragment : WPBottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViewModel()
+        initViewModels()
         dialog?.setOnShowListener { dialogInterface ->
             val sheetDialog = dialogInterface as? BottomSheetDialog
 
@@ -50,12 +57,43 @@ class PrepublishingBottomSheetFragment : WPBottomSheetDialogFragment() {
         }
     }
 
-    private fun initViewModel() {
-        viewModel = ViewModelProviders.of(this, viewModelFactory)
+    private fun initViewModels() {
+        prepublishingViewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(PrepublishingViewModel::class.java)
+
+        prepublishingActionsViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(PrepublishingActionsViewModel::class.java)
+
+        prepublishingActionsViewModel.prepublishingActionType.observe(this, Observer { event ->
+            event.getContentIfNotHandled()?.let { actionType ->
+                navigateToAction(actionType)
+            }
+        })
     }
 
-    override fun onDismiss(dialog: DialogInterface) {
+    // Create a sealed class to hold the state for all these actions.
+    private fun navigateToAction(actionType: ActionType) {
+        val fragment = when (actionType) {
+            TAGS -> PostSettingsTagsFragment.newInstance(null, null)
+            else -> throw NotImplementedError()
+        }
+        }
+        slideInFragment(fragment)
+    }
+
+    private fun slideInFragment(fragment: Fragment, tag: String) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        if (supportFragmentManager.findFragmentById(R.id.fragment_container) != null) {
+            // add to back stack and animate all screen except of the first one
+            fragmentTransaction.addToBackStack(null).setCustomAnimations(
+                    R.anim.activity_slide_in_from_right, R.anim.activity_slide_out_to_left,
+                    R.anim.activity_slide_in_from_left, R.anim.activity_slide_out_to_right
+            )
+        }
+        fragmentTransaction.replace(R.id.fragment_container, fragment, tag)
+
+
+        override fun onDismiss(dialog: DialogInterface) {
         super.onDismiss(dialog)
         removeContentFragmentBeforeDismissal()
     }
