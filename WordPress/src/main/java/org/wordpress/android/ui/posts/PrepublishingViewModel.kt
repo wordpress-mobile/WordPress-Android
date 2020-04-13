5dfc319f1f07aff5138773d347c20d65820f7765
Joel Dean <jdeanjj1000@gmail.com>
2020-04-13 14:49:23 -0500

    updated viewmodel state to allow the navigation.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.android.parcel.Parcelize
 import org.wordpress.android.ui.posts.ActionState.TagsActionState
+import org.wordpress.android.ui.posts.CurrentActionTypeState.ActionTypeState
 import org.wordpress.android.ui.posts.PrepublishingActionItemUiState.ActionType
 import org.wordpress.android.viewmodel.Event
 import javax.inject.Inject
@@ -19,7 +20,7 @@ sealed class CurrentActionTypeState() : Parcelable {
     object HomeActionTypeState : CurrentActionTypeState()
 
     @Parcelize
-    data class ActionTypeTypeState(val actionType: ActionType) : CurrentActionTypeState()
+    data class ActionTypeState(val actionType: ActionType) : CurrentActionTypeState()
 }
 
 @Parcelize
@@ -33,21 +34,31 @@ sealed class ActionState() : Parcelable {
 class PrepublishingViewModel @Inject constructor() : ViewModel() {
     private var isStarted = false
 
-    private lateinit var tagsActionState: TagsActionState
-    private lateinit var mCurrentActionTypeState: CurrentActionTypeState
+    private lateinit var actionsState: ActionsState
+    private lateinit var currentActionTypeState: CurrentActionTypeState
 
-    private val _currentActionType = MutableLiveData<Event<Pair<CurrentActionTypeState, TagsActionState>>>()
-    val mCurrentActionType: LiveData<Event<Pair<CurrentActionTypeState, TagsActionState>>> = _currentActionType
+    private val _currentActionType = MutableLiveData<Event<Pair<CurrentActionTypeState, ActionsState>>>()
+    val currentActionType: LiveData<Event<Pair<CurrentActionTypeState, ActionsState>>> = _currentActionType
 
     fun start(currentActionTypeState: CurrentActionTypeState, actionsState: ActionsState) {
         if (isStarted) return
         isStarted = true
+        this.actionsState = actionsState
+        this.currentActionTypeState = currentActionTypeState
+        updateState()
+    }
 
-        _currentActionType.postValue(Event(Pair(currentActionTypeState, tagsActionState)))
+    private fun updateState() {
+        _currentActionType.postValue(Event(Pair(currentActionTypeState, actionsState)))
+    }
+
+    fun updateCurrentActionTypeState(actionType: ActionType) {
+        currentActionTypeState = ActionTypeState(actionType)
+        updateState()
     }
 
     fun writeToBundle(outState: Bundle) {
-        outState.putParcelable(KEY_CURRENT_ACTION_TYPE_STATE, mCurrentActionTypeState)
-        outState.putParcelable(KEY_TAGS_ACTION_STATE, tagsActionState)
+        outState.putParcelable(KEY_CURRENT_ACTION_TYPE_STATE, currentActionTypeState)
+        outState.putParcelable(KEY_TAGS_ACTION_STATE, actionsState)
     }
 }
