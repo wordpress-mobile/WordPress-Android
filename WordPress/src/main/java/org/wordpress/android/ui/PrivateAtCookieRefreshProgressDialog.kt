0de78bccb6ac47d175e3ca06ecbed838f9dbfd45
Klymentiy haykov <forsver@gmail.com>
2020-04-12 23:02:14 -0700

    Added couple of additional utility methods to progress dialog.

--- WordPress/src/main/java/org/wordpress/android/ui/PrivateAtCookieRefreshProgressDialog.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/PrivateAtCookieRefreshProgressDialog.kt
@@ -2,10 +2,13 @@ package org.wordpress.android.ui
 
 import android.app.Dialog
 import android.app.ProgressDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import org.wordpress.android.R
+import javax.annotation.Nullable
 
 class PrivateAtCookieRefreshProgressDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -15,19 +18,38 @@ class PrivateAtCookieRefreshProgressDialog : DialogFragment() {
         )
     }
 
+    interface PrivateAtCookieProgressDialogOnDismissListener {
+        fun onCookieProgressDialogCancelled()
+    }
+
     fun isDialogVisible(): Boolean {
         return dialog != null && dialog!!.isShowing
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        super.onCancel(dialog)
+        if (targetFragment is PrivateAtCookieProgressDialogOnDismissListener) {
+            (targetFragment as PrivateAtCookieProgressDialogOnDismissListener).onCookieProgressDialogCancelled()
+        } else if (activity is PrivateAtCookieProgressDialogOnDismissListener) {
+            (activity as PrivateAtCookieProgressDialogOnDismissListener).onCookieProgressDialogCancelled()
+        }
     }
 
     companion object {
         const val TAG = "private_at_cookie_progress_dialog"
 
         fun showIfNecessary(fragmentManager: FragmentManager?) {
+            showIfNecessary(fragmentManager, null)
+        }
+
+        fun showIfNecessary(fragmentManager: FragmentManager?, @Nullable targetFragment: Fragment?) {
             fragmentManager.let {
                 val thisFragment = fragmentManager!!.findFragmentByTag(TAG)
                 if (thisFragment == null ||
                         (thisFragment is PrivateAtCookieRefreshProgressDialog && !thisFragment.isDialogVisible())) {
-                    PrivateAtCookieRefreshProgressDialog().show(fragmentManager, TAG)
+                    val progressFragment = PrivateAtCookieRefreshProgressDialog()
+                    progressFragment.setTargetFragment(targetFragment, 0)
+                    progressFragment.show(fragmentManager, TAG)
                 }
             }
         }
@@ -38,6 +60,13 @@ class PrivateAtCookieRefreshProgressDialog : DialogFragment() {
                 if (thisFragment is PrivateAtCookieRefreshProgressDialog) {
                     thisFragment.dismiss()
                 }
+            }
+        }
+
+        fun isShowing(fragmentManager: FragmentManager?): Boolean {
+            fragmentManager.let {
+                val thisFragment = fragmentManager!!.findFragmentByTag(TAG)
+                return thisFragment is PrivateAtCookieRefreshProgressDialog && thisFragment.isDialogVisible()
             }
         }
     }
