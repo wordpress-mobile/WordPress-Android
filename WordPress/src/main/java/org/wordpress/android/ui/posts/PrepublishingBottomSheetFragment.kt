a4051039cdc730a357f36ad4ef3430d6d3b7988d
Joel Dean <jdeanjj1000@gmail.com>
2020-04-13 17:50:39 -0500

    bottom sheet readjusts for keyboard.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingBottomSheetFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingBottomSheetFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.annotation.NonNull
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -31,6 +32,7 @@ class PrepublishingBottomSheetFragment : WPBottomSheetDialogFragment(), TagsSele
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.WordPress_BottomSheetDialogTheme)
         (requireNotNull(activity).application as WordPress).component().inject(this)
         site = arguments?.getSerializable(SITE) as SiteModel
     }
