c8913c03fc124c043ec909b93585fdc728f2ed0c
malinajirka <malinajirka@gmail.com>
2020-04-14 14:33:14 +0530

    Hide "take photo" on media custom picker in gutenberg
    (cherry picked from commit ed6b137438685663619261e43d91c498a6cf1cc6)

--- WordPress/src/main/java/org/wordpress/android/ui/photopicker/PhotoPickerFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/photopicker/PhotoPickerFragment.java
@@ -167,21 +167,26 @@ public class PhotoPickerFragment extends Fragment {
         if (!canShowMediaSourceBottomBar()) {
             mMediaSourceBottomBar.setVisibility(View.GONE);
         } else {
-            mMediaSourceBottomBar.findViewById(R.id.icon_camera).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mBrowserType.isImagePicker() && mBrowserType.isVideoPicker()) {
-                        showCameraPopupMenu(v);
-                    } else if (mBrowserType.isImagePicker()) {
-                        doIconClicked(PhotoPickerIcon.ANDROID_CAPTURE_PHOTO);
-                    } else if (mBrowserType.isVideoPicker()) {
-                        doIconClicked(PhotoPickerIcon.ANDROID_CAPTURE_VIDEO);
-                    } else {
-                        AppLog.e(T.MEDIA, "This code should be unreachable. If you see this message one of "
-                                          + "the MediaBrowserTypes isn't setup correctly.");
+            View camera = mMediaSourceBottomBar.findViewById(R.id.icon_camera);
+            if (mBrowserType.isGutenbergPicker()) {
+                camera.setVisibility(View.GONE);
+            } else {
+                camera.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mBrowserType.isImagePicker() && mBrowserType.isVideoPicker()) {
+                            showCameraPopupMenu(v);
+                        } else if (mBrowserType.isImagePicker()) {
+                            doIconClicked(PhotoPickerIcon.ANDROID_CAPTURE_PHOTO);
+                        } else if (mBrowserType.isVideoPicker()) {
+                            doIconClicked(PhotoPickerIcon.ANDROID_CAPTURE_VIDEO);
+                        } else {
+                            AppLog.e(T.MEDIA, "This code should be unreachable. If you see this message one of "
+                                              + "the MediaBrowserTypes isn't setup correctly.");
+                        }
                     }
-                }
-            });
+                });
+            }
             mMediaSourceBottomBar.findViewById(R.id.icon_picker).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
