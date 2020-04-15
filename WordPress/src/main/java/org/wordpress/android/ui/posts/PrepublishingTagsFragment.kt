68acec4ed61eda22c79f1008ea2ef118255d8c16
Joel Dean <jdeanjj1000@gmail.com>
2020-04-14 23:58:23 -0500

    added ripple effect to ImageView buttons.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingTagsFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingTagsFragment.kt
@@ -2,6 +2,8 @@ package org.wordpress.android.ui.posts
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.fluxc.model.SiteModel
@@ -23,5 +25,15 @@ class PrepublishingTagsFragment : TagsFragment() {
             }
             return PrepublishingTagsFragment().apply { arguments = bundle }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val closeButton = view.findViewById<ImageView>(R.id.close_button)
+        val backButton = view.findViewById<ImageView>(R.id.back_button)
+
+        closeButton.setOnClickListener {}
+        backButton.setOnClickListener {}
     }
 }
