90f0918e011b8e05558a9761e833e1e3eef2c74b
rachelmcr <rachel@automattic.com>
2020-04-15 11:35:58 +0100

    Fix linting

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/LoginTests.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/LoginTests.java
@@ -1,15 +1,15 @@
 package org.wordpress.android.e2e;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
+//import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wordpress.android.e2e.flows.LoginFlow;
 import org.wordpress.android.support.BaseTest;
-import org.wordpress.android.ui.accounts.LoginMagicLinkInterceptActivity;
+//import org.junit.Rule;
+//import org.wordpress.android.ui.accounts.LoginMagicLinkInterceptActivity;
 
 import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_PASSWORD;
 import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_SITE_ADDRESS;
