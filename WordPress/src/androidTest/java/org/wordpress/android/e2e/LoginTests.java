aba5f5a97dd14947ce34e29c35e8c4f27416f3ea
rachelmcr <rachel@automattic.com>
2020-04-15 10:57:20 +0100

    Re-enable and refactor login tests that don't use magic links

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/LoginTests.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/LoginTests.java
@@ -1,63 +1,69 @@
 package org.wordpress.android.e2e;
-// Disabled temporarily
-//import androidx.test.ext.junit.runners.AndroidJUnit4;
-//import androidx.test.rule.ActivityTestRule;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.wordpress.android.e2e.flows.LoginFlow;
-//import org.wordpress.android.support.BaseTest;
-//import org.wordpress.android.ui.accounts.LoginMagicLinkInterceptActivity;
-//
-//import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_PASSWORD;
-//import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_SITE_ADDRESS;
-//import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_USERNAME;
-//import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_PASSWORD;
-//import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_SITE_ADDRESS;
-//import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_USERNAME;
-//
-//@RunWith(AndroidJUnit4.class)
-//public class LoginTests extends BaseTest {
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wordpress.android.e2e.flows.LoginFlow;
+import org.wordpress.android.support.BaseTest;
+import org.wordpress.android.ui.accounts.LoginMagicLinkInterceptActivity;
+
+import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_PASSWORD;
+import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_SITE_ADDRESS;
+import static org.wordpress.android.BuildConfig.E2E_SELF_HOSTED_USER_USERNAME;
+import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_PASSWORD;
+import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_SITE_ADDRESS;
+import static org.wordpress.android.BuildConfig.E2E_WP_COM_USER_USERNAME;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginTests extends BaseTest {
 //    @Rule
 //    public ActivityTestRule<LoginMagicLinkInterceptActivity> mMagicLinkActivityTestRule =
 //            new ActivityTestRule<>(LoginMagicLinkInterceptActivity.class);
-//
-//    @Before
-//    public void setUp() {
-//        logoutIfNecessary();
-//    }
-//
-//    @Test
-//    public void loginWithEmailPassword() {
-//        wpLogin();
-//    }
-//
-//    @Test
-//    public void loginWithSiteAddress() {
-//        new LoginFlow().loginSiteAddress(
-//                E2E_WP_COM_USER_SITE_ADDRESS,
-//                E2E_WP_COM_USER_USERNAME,
-//                E2E_WP_COM_USER_PASSWORD);
-//    }
-//
+
+    @Before
+    public void setUp() {
+        logoutIfNecessary();
+    }
+
+    @Test
+    public void loginWithEmailPassword() {
+        new LoginFlow().chooseLogin()
+                 .enterEmailAddress()
+                 .enterPassword()
+                 .confirmLogin();
+    }
+
+    @Test
+    public void loginWithSiteAddress() {
+        new LoginFlow().chooseLogin()
+                 .chooseAndEnterSiteAddress(E2E_WP_COM_USER_SITE_ADDRESS)
+                 .enterUsernameAndPassword(E2E_WP_COM_USER_USERNAME, E2E_WP_COM_USER_PASSWORD)
+                 .confirmLogin();
+    }
+
+//    Disabled temporarily, see: https://github.com/wordpress-mobile/WordPress-Android/pull/11283
 //    @Test
 //    public void loginWithMagicLink() {
-//        new LoginFlow().loginMagicLink(mMagicLinkActivityTestRule);
+//        new LoginFlow().chooseLogin()
+//                 .enterEmailAddress()
+//                 .chooseMagicLink(mMagicLinkActivityTestRule)
+//                 .confirmLogin();
 //    }
-//
-//    @Test
-//    public void loginWithSelfHostedAccount() {
-//        new LoginFlow().loginSiteAddress(
-//                E2E_SELF_HOSTED_USER_SITE_ADDRESS,
-//                E2E_SELF_HOSTED_USER_USERNAME,
-//                E2E_SELF_HOSTED_USER_PASSWORD);
-//    }
-//
-//    @After
-//    public void tearDown() {
-//        logoutIfNecessary();
-//    }
-//}
+
+    @Test
+    public void loginWithSelfHostedAccount() {
+        new LoginFlow().chooseLogin()
+                .chooseAndEnterSiteAddress(E2E_SELF_HOSTED_USER_SITE_ADDRESS)
+                .enterUsernameAndPassword(E2E_SELF_HOSTED_USER_USERNAME, E2E_SELF_HOSTED_USER_PASSWORD)
+                .confirmLogin();
+    }
+
+    @After
+    public void tearDown() {
+        logoutIfNecessary();
+    }
+}
