9d71d65b29c140758a3e23551ac89c9688005d6e
Joel Dean <jdeanjj1000@gmail.com>
2020-04-19 15:43:29 -0500

    added delay for tags updating.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingTagsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingTagsViewModel.kt
@@ -2,17 +2,25 @@ package org.wordpress.android.ui.posts
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.wordpress.android.R
+import org.wordpress.android.modules.BG_THREAD
 import org.wordpress.android.ui.utils.UiString
 import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.viewmodel.Event
+import org.wordpress.android.viewmodel.ScopedViewModel
 import javax.inject.Inject
+import javax.inject.Named
+
+private const val THROTTLE_DELAY = 2000L
 
 class PrepublishingTagsViewModel @Inject constructor(
     private val getPostTagsUseCase: GetPostTagsUseCase,
-    private val updatePostTagsUseCase: UpdatePostTagsUseCase
-) : ViewModel() {
+    private val updatePostTagsUseCase: UpdatePostTagsUseCase,
+    @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher
+) : ScopedViewModel(bgDispatcher) {
     private var isStarted = false
     private lateinit var editPostRepository: EditPostRepository
 
@@ -38,7 +46,10 @@ class PrepublishingTagsViewModel @Inject constructor(
     }
 
     fun onTagsSelected(selectedTags: String) {
-        updatePostTagsUseCase.updateTags(selectedTags, editPostRepository)
+        launch(bgDispatcher) {
+            delay(THROTTLE_DELAY)
+            updatePostTagsUseCase.updateTags(selectedTags, editPostRepository)
+        }
     }
 
     fun onCloseButtonClicked() = _dismissBottomSheet.postValue(Event(Unit))
