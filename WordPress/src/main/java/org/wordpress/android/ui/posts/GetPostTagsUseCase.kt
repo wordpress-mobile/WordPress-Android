a63135a07763c99f9ec7adada19b874be71992d5
Joel Dean <jdeanjj1000@gmail.com>
2020-04-19 11:45:15 -0500

    created GetPostTagsUseCase so functionality can be reusable.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/GetPostTagsUseCase.kt
@@ -0,0 +1,19 @@
+package org.wordpress.android.ui.posts
+
+import android.text.TextUtils
+import dagger.Reusable
+import org.apache.commons.text.StringEscapeUtils
+import javax.inject.Inject
+
+@Reusable
+class GetPostTagsUseCase @Inject constructor() {
+    fun getTags(editPostRepository: EditPostRepository): String? {
+        val tags = editPostRepository.getPost()?.tagNameList
+        return tags?.let { formatTags(it) }
+    }
+
+    private fun formatTags(tags: List<String>): String {
+        val formattedTags = TextUtils.join(",", tags)
+        return StringEscapeUtils.unescapeHtml4(formattedTags)
+    }
+}
