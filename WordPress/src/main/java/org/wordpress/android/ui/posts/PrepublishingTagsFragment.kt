0166dbe02b69d5b234fc884baf491d45857170fe
Joel Dean <jdeanjj1000@gmail.com>
2020-04-19 12:59:48 -0500

    wired PrepublishingTagsFragment to ViewModel

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingTagsFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingTagsFragment.kt
@@ -5,19 +5,26 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.posts.EditPostSettingsFragment.EditPostActivityHook
+import org.wordpress.android.ui.utils.UiHelpers
 import org.wordpress.android.util.ActivityUtils
 import javax.inject.Inject
 
 class PrepublishingTagsFragment : TagsFragment(), TagsSelectedListener {
+    private lateinit var toolbarTitle: TextView
     private var closeListener: PrepublishingScreenClosedListener? = null
 
-    @Inject lateinit var getPostTagsUseCase: GetPostTagsUseCase
-    @Inject lateinit var updatePostTagsUseCase: UpdatePostTagsUseCase
+    @Inject internal lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject lateinit var uiHelpers: UiHelpers
+
     private lateinit var editPostRepository: EditPostRepository
+    private lateinit var viewModel: PrepublishingTagsViewModel
 
     override fun getContentLayout() = R.layout.fragment_prepublishing_tags
 
@@ -43,7 +50,7 @@ class PrepublishingTagsFragment : TagsFragment(), TagsSelectedListener {
         closeListener = null
     }
 
-    override fun getTagsFromEditPostRepositoryOrArguments() = getPostTagsUseCase.getTags(editPostRepository)
+    override fun getTagsFromEditPostRepositoryOrArguments() = viewModel.getPostTags()
 
     companion object {
         const val TAG = "prepublishing_tags_fragment_tag"
@@ -56,22 +63,45 @@ class PrepublishingTagsFragment : TagsFragment(), TagsSelectedListener {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        initViewModel()
 
         val closeButton = view.findViewById<ImageView>(R.id.close_button)
         val backButton = view.findViewById<ImageView>(R.id.back_button)
-        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_title)
+        toolbarTitle = view.findViewById(R.id.toolbar_title)
 
-        toolbarTitle.text = context?.getString(R.string.prepublishing_nudges_toolbar_title_tags)
-
-        closeButton.setOnClickListener { closeListener?.onCloseClicked() }
+        closeButton.setOnClickListener { viewModel.onCloseButtonClicked() }
         backButton.setOnClickListener {
-            ActivityUtils.hideKeyboard(requireActivity())
-            closeListener?.onBackClicked()
+            viewModel.onBackButtonClicked()
         }
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(PrepublishingTagsViewModel::class.java)
+
+        viewModel.dismissBottomSheet.observe(this, Observer { event ->
+            event?.applyIfNotHandled {
+                closeListener?.onCloseClicked()
+            }
+        })
+
+        viewModel.navigateToHomeScreen.observe(this, Observer { event ->
+            event?.applyIfNotHandled {
+                ActivityUtils.hideKeyboard(requireActivity())
+                closeListener?.onBackClicked()
+            }
+        })
+
+        viewModel.updateToolbarTitle.observe(this, Observer { uiString ->
+            toolbarTitle.text = uiHelpers.getTextOfUiString(requireContext(), uiString)
+        })
+
+        viewModel.start(editPostRepository)
     }
 
     override fun onTagsSelected(selectedTags: String) {
-        updatePostTagsUseCase.updateTags(selectedTags, editPostRepository)
+        viewModel.onTagsSelected(selectedTags)
     }
 }
