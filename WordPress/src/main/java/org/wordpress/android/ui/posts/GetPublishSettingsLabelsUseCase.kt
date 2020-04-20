24387d07dd889d39a401f63eafe4757b95c8dc9b
Joel Dean <jdeanjj1000@gmail.com>
2020-04-19 21:55:47 -0500

    add scaffolding for use case.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/GetPublishSettingsLabelsUseCase.kt
@@ -0,0 +1,24 @@
+package org.wordpress.android.ui.posts
+
+import dagger.Reusable
+import javax.inject.Inject
+
+@Reusable
+class GetPublishSettingsLabelsUseCase @Inject constructor() {
+
+    fun buildUiModel() {
+
+    }
+
+    fun getPublishLabel() {
+
+    }
+
+    fun getPublishTimeLabel() {
+
+    }
+
+    fun getPublishButtonLabel() {
+
+    }
+}
