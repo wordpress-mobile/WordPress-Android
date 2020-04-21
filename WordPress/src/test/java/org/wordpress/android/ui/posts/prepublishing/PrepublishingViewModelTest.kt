0b47c038d8ac1655f7d008decea4542e07c09108
Joel Dean <jdeanjj1000@gmail.com>
2020-04-21 15:50:52 -0500

    created prepublishingviewmodel test class and first test.

---
+++ WordPress/src/test/java/org/wordpress/android/ui/posts/prepublishing/PrepublishingViewModelTest.kt
@@ -0,0 +1,34 @@
+package org.wordpress.android.ui.posts.prepublishing
+
+import com.nhaarman.mockitokotlin2.mock
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.wordpress.android.BaseUnitTest
+import org.wordpress.android.ui.posts.PrepublishingNavigationTarget
+import org.wordpress.android.ui.posts.PrepublishingScreen
+import org.wordpress.android.ui.posts.PrepublishingViewModel
+import org.wordpress.android.viewmodel.Event
+
+class PrepublishingViewModelTest : BaseUnitTest() {
+    private lateinit var viewModel: PrepublishingViewModel
+
+    @Before
+    fun setup() {
+        viewModel = PrepublishingViewModel()
+    }
+
+    @Test
+    fun `when viewModel start is called with a null currentScreen navigateToScreen should be invoked with HOME`() {
+        val expectedScreen = PrepublishingScreen.HOME
+
+        var event: Event<PrepublishingNavigationTarget>? = null
+        viewModel.navigationTarget.observeForever {
+            event = it
+        }
+
+        viewModel.start(mock(), null)
+
+        assertThat(event?.peekContent()?.targetScreen).isEqualTo(expectedScreen)
+    }
+}
