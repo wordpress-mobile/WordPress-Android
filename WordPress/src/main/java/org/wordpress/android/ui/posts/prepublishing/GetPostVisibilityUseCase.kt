3506101ddda781f6f7319502843dea3aef18439b
Joel Dean <jdeanjj1000@gmail.com>
2020-04-24 11:51:55 -0500

    created scaffolds for the adapter and use case.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/GetPostVisibilityUseCase.kt
@@ -0,0 +1,7 @@
+package org.wordpress.android.ui.posts.prepublishing
+
+import javax.inject.Inject
+
+class GetPostVisibilityUseCase @Inject constructor() {
+
+}
---
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/visibility/PrepublishingVisibilityAdapter.kt
@@ -0,0 +1,5 @@
+package org.wordpress.android.ui.posts.prepublishing.visibility
+
+class PrepublishingVisibilityAdapter {
+
+}
