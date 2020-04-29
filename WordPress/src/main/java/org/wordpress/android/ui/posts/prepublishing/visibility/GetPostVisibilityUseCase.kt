d17fe1dc43da5fea9b09a3353d0d15f7a84fbd1a
Joel Dean <jdeanjj1000@gmail.com>
2020-04-28 19:04:56 -0500

    added use case to get the current post visibility

---
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/visibility/GetPostVisibilityUseCase.kt
@@ -0,0 +1,16 @@
+package org.wordpress.android.ui.posts.prepublishing.visibility
+
+import org.wordpress.android.fluxc.model.post.PostStatus
+import org.wordpress.android.ui.posts.EditPostRepository
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PASSWORD_PROTECTED
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PRIVATE
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PUBLIC
+import javax.inject.Inject
+
+class GetPostVisibilityUseCase @Inject constructor() {
+    fun getVisibility(editPostRepository: EditPostRepository) = when {
+        editPostRepository.password.isNotEmpty() -> PASSWORD_PROTECTED
+        editPostRepository.status == PostStatus.PRIVATE -> PRIVATE
+        else -> PUBLIC
+    }
+}
