90c9dfa3baea01af90b04f75c765100b09af3c72
Klymentiy haykov <forsver@gmail.com>
2020-04-28 21:37:18 -0700

    Added resources and basic announcement item model.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/whatsnew/FeatureAnnouncementItem.kt
@@ -0,0 +1,7 @@
+package org.wordpress.android.ui.whatsnew
+
+data class FeatureAnnouncementItem(
+    val announcementTitle: String,
+    val announcementSubtitle: String,
+    val announcementIconUrl: String
+)
