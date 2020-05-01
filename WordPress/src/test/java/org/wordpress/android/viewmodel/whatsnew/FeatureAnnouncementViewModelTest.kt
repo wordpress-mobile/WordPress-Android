06492c9fc2823ccacf6da7c79997a5cc499a8c64
Klymentiy haykov <forsver@gmail.com>
2020-04-30 19:15:53 -0700

    Added version code to feature announcement.

--- WordPress/src/test/java/org/wordpress/android/viewmodel/whatsnew/FeatureAnnouncementViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/whatsnew/FeatureAnnouncementViewModelTest.kt
@@ -43,7 +43,7 @@ class FeatureAnnouncementViewModelTest : BaseUnitTest() {
             )
     )
 
-    private val featureAnnouncement = FeatureAnnouncement("14.7", "https://wordpress.org/", testFeatures)
+    private val featureAnnouncement = FeatureAnnouncement("14.7", 857, "https://wordpress.org/", testFeatures)
 
     @Before
     fun setUp() {
