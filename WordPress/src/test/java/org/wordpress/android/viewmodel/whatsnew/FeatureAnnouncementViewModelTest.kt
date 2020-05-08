d1419bdf899df67c63cf7da3b90f569b3ccf7696
Klymentiy haykov <forsver@gmail.com>
2020-05-08 16:03:41 -0700

    Made find out more button optional.

--- WordPress/src/test/java/org/wordpress/android/viewmodel/whatsnew/FeatureAnnouncementViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/whatsnew/FeatureAnnouncementViewModelTest.kt
@@ -61,18 +61,18 @@ class FeatureAnnouncementViewModelTest : BaseUnitTest() {
         viewModel.onDialogClosed.observeForever(onDialogClosedObserver)
         viewModel.onAnnouncementDetailsRequested.observeForever(onAnnouncementDetailsRequestedObserver)
         viewModel.featureItems.observeForever(featuresObserver)
-
-        viewModel.start()
     }
 
     @Test
-    fun `progress is visible after start`() {
+    fun `progress and find out more is visible after start`() {
+        viewModel.start()
         assertThat(uiModelResults.isNotEmpty()).isEqualTo(true)
         assertThat(uiModelResults[0].isProgressVisible).isEqualTo(true)
     }
 
     @Test
     fun `announcement details are loaded after start`() {
+        viewModel.start()
         assertThat(uiModelResults.isNotEmpty()).isEqualTo(true)
         assertThat(uiModelResults[1].isProgressVisible).isEqualTo(false)
         assertThat(uiModelResults[1].appVersion).isEqualTo("14.7")
@@ -81,13 +81,31 @@ class FeatureAnnouncementViewModelTest : BaseUnitTest() {
 
     @Test
     fun `pressing close button closes the dialog`() {
+        viewModel.start()
         viewModel.onCloseDialogButtonPressed()
         verify(onDialogClosedObserver).onChanged(anyOrNull())
     }
 
     @Test
     fun `pressing Find Out More triggers request for announcement details with specific URL`() {
+        viewModel.start()
         viewModel.onFindMoreButtonPressed()
         verify(onAnnouncementDetailsRequestedObserver).onChanged("https://wordpress.org/")
+    }
+
+    @Test
+    fun `find Out More is  visible when detailsUrl is present`() {
+        viewModel.start()
+        assertThat(uiModelResults[1].isFindOutMoreVisible).isEqualTo(true)
+    }
+
+    @Test
+    fun `find Out More is not visible when detailsUrl is missing`() {
+        whenever(featureAnnouncementProvider.getLatestFeatureAnnouncement()).thenReturn(
+                featureAnnouncement.copy(detailsUrl = "")
+        )
+
+        viewModel.start()
+        assertThat(uiModelResults[1].isFindOutMoreVisible).isEqualTo(false)
     }
 }
