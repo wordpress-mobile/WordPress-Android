6ec0e24fd361a83ece3b24aa8ba50c3b3229fc9c
Klymentiy haykov <forsver@gmail.com>
2020-05-13 12:25:20 -0700

    Trying out suspended what's new.

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/WhatsNewStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/WhatsNewStore.kt
@@ -1,54 +1,42 @@
 package org.wordpress.android.fluxc.store
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
-import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.Payload
-import org.wordpress.android.fluxc.action.WhatsNewAction
-import org.wordpress.android.fluxc.action.WhatsNewAction.FETCH_WHATS_NEW
-import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.model.whatsnew.WhatsNewAnnounceModel
 import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError
 import org.wordpress.android.fluxc.network.rest.wpcom.whatsnew.WhatsNewRestClient
+import org.wordpress.android.fluxc.store.Store.OnChangedError
 import org.wordpress.android.fluxc.store.WhatsNewStore.WhatsNewErrorType.GENERIC_ERROR
-import org.wordpress.android.util.AppLog
+import org.wordpress.android.fluxc.tools.CoroutineEngine
+import org.wordpress.android.util.AppLog.T
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class WhatsNewStore @Inject constructor(
     private val whatsNewRestClient: WhatsNewRestClient,
-    private val coroutineContext: CoroutineContext,
-    dispatcher: Dispatcher
-) : Store(dispatcher) {
-    @Subscribe(threadMode = ThreadMode.ASYNC)
-    override fun onAction(action: Action<*>) {
-        val actionType = action.type as? WhatsNewAction ?: return
-        when (actionType) {
-            FETCH_WHATS_NEW -> {
-                val versionCode = (action.payload as WhatsNewFetchPayload).versionCode
-                GlobalScope.launch(coroutineContext) { emitChange(fetchWhatsNew(versionCode)) }
-            }
-        }
-    }
+    private val coroutineEngine: CoroutineEngine
+) {
+//    @Subscribe(threadMode = ThreadMode.ASYNC)
+//    override fun onAction(action: Action<*>) {
+//        val actionType = action.type as? WhatsNewAction ?: return
+//        when (actionType) {
+//            FETCH_WHATS_NEW -> {
+//                val versionCode = (action.payload as WhatsNewFetchPayload).versionCode
+//                GlobalScope.launch(coroutineContext) { emitChange(fetchWhatsNew(versionCode)) }
+//            }
+//        }
+//    }
 
-    private suspend fun fetchWhatsNew(versionCode: String): OnWhatsNewFetched {
+    suspend fun fetchWhatsNew(versionCode: String) = coroutineEngine.withDefaultContext(T.API, this, "fetchWhatsNew") {
         val fetchedWhatsNewPayload = whatsNewRestClient.fetchWhatsNew(versionCode)
 
-        return if (!fetchedWhatsNewPayload.isError) {
+        return@withDefaultContext if (!fetchedWhatsNewPayload.isError) {
             OnWhatsNewFetched(fetchedWhatsNewPayload.whatsNewItems)
         } else {
             OnWhatsNewFetched(
                     fetchError = WhatsNewFetchError(GENERIC_ERROR, fetchedWhatsNewPayload.error.message)
             )
         }
-    }
-
-    override fun onRegister() {
-        AppLog.d(AppLog.T.API, WhatsNewStore::class.java.simpleName + " onRegister")
     }
 
     class WhatsNewFetchPayload(
