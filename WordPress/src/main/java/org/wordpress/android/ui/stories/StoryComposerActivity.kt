a2b134705f86765a2a0fc722d9454c2863f28902
Mario Zorz <mariozorz@gmail.com>
2020-05-14 15:35:10 -0300

    added snackbar handling in wpandroid for the base activity to be able to use the host-provided snackbar

--- WordPress/src/main/java/org/wordpress/android/ui/stories/StoryComposerActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stories/StoryComposerActivity.kt
@@ -1,10 +1,13 @@
 package org.wordpress.android.ui.stories
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import com.wordpress.stories.compose.ComposeLoopFrameActivity
 import com.wordpress.stories.compose.MediaPickerProvider
 import com.wordpress.stories.compose.SnackbarProvider
+import org.wordpress.android.widgets.WPSnackbar
 
 class StoryComposerActivity : ComposeLoopFrameActivity(), SnackbarProvider, MediaPickerProvider {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +17,10 @@ class StoryComposerActivity : ComposeLoopFrameActivity(), SnackbarProvider, Medi
     }
 
     override fun showProvidedSnackbar(message: String, actionLabel: String?, callback: () -> Unit) {
-        // TODO implement snackbar
+        val view = findViewById<View>(android.R.id.content)
+        val snackbar = WPSnackbar.make(view, message, Snackbar.LENGTH_LONG)
+        snackbar.setAction(actionLabel) { callback }
+        snackbar.show()
     }
 
     override fun setupRequestCodes(requestCodes: ExternalMediaPickerRequestCodesAndExtraKeys) {
