0cf599dde5a5b4756d106a0979ae90fc34e5f037
Mario Zorz <mariozorz@gmail.com>
2020-05-18 10:38:52 -0300

    fixed checkstyle violation: line length

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostNotificationScheduleTimeDialogFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostNotificationScheduleTimeDialogFragment.kt
@@ -25,7 +25,8 @@ class PostNotificationScheduleTimeDialogFragment : DialogFragment() {
         viewModel = ViewModelProviders.of(requireActivity(), viewModelFactory)
                 .get(EditPostPublishSettingsViewModel::class.java)
         val alertDialogBuilder = MaterialAlertDialogBuilder(activity)
-        val view = requireActivity().layoutInflater.inflate(R.layout.post_notification_type_selector, null) as RadioGroup
+        val view = requireActivity().layoutInflater.inflate(R.layout.post_notification_type_selector, null)
+                as RadioGroup
         alertDialogBuilder.setView(view)
         val notificationTime = arguments?.getString(ARG_NOTIFICATION_SCHEDULE_TIME)?.let {
             SchedulingReminderModel.Period.valueOf(
