58916ecf7671d1ac5ed2c308b55bede9a881ce22
Mario Zorz <mariozorz@gmail.com>
2020-05-18 13:26:04 -0300

    removed unused import

--- WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.java
@@ -12,7 +12,6 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
