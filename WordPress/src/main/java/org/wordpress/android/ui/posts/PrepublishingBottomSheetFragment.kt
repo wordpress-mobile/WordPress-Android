b15775b8954db7d28676489bc13e0edda3fd356e
Joel Dean <jdeanjj1000@gmail.com>
2020-05-18 17:19:09 -0500

    moved publish button clicked logic to the viewmodel.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingBottomSheetFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingBottomSheetFragment.kt
@@ -113,6 +113,10 @@ class PrepublishingBottomSheetFragment : WPBottomSheetDialogFragment(),
             event.applyIfNotHandled { dismiss() }
         })
 
+        viewModel.triggerOnPublishButtonClickedListener.observe(this, Observer { event ->
+            event.applyIfNotHandled { prepublishingBottomSheetListener?.onPublishButtonClicked() }
+        })
+
         val prepublishingScreenState = savedInstanceState?.getParcelable<PrepublishingScreen>(KEY_SCREEN_STATE)
         val site = arguments?.getSerializable(SITE) as SiteModel
 
@@ -185,7 +189,6 @@ class PrepublishingBottomSheetFragment : WPBottomSheetDialogFragment(),
     }
 
     override fun onPublishButtonClicked() {
-        viewModel.onCloseClicked()
-        prepublishingBottomSheetListener?.onPublishButtonClicked()
+        viewModel.onPublishButtonClicked()
     }
 }
