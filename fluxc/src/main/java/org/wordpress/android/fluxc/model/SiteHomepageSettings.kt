25fa74848f0aed1dd99870aa8471573d6fc88b7b
vojtasmrcek <planarvoid@gmail.com>
2020-05-18 16:39:24 +0200

    Add methods to site options store

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/SiteHomepageSettings.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/SiteHomepageSettings.kt
@@ -4,7 +4,7 @@ import org.wordpress.android.fluxc.model.SiteHomepageSettings.ShowOnFront.PAGE
 import org.wordpress.android.fluxc.model.SiteHomepageSettings.ShowOnFront.POSTS
 
 sealed class SiteHomepageSettings(val showOnFront: ShowOnFront) {
-    data class Page(val pageForPostsId: Long, val pageOnFrontId: Long) : SiteHomepageSettings(PAGE)
+    data class StaticPage(val pageForPostsId: Long, val pageOnFrontId: Long) : SiteHomepageSettings(PAGE)
     object Posts : SiteHomepageSettings(POSTS)
 
     enum class ShowOnFront(val value: String) {
--- fluxc/src/main/java/org/wordpress/android/fluxc/model/SiteHomepageSettingsMapper.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/SiteHomepageSettingsMapper.kt
@@ -1,11 +1,10 @@
 package org.wordpress.android.fluxc.model
 
-import org.wordpress.android.fluxc.model.SiteHomepageSettings.Page
+import org.wordpress.android.fluxc.model.SiteHomepageSettings.StaticPage
 import org.wordpress.android.fluxc.model.SiteHomepageSettings.Posts
 import org.wordpress.android.fluxc.model.SiteHomepageSettings.ShowOnFront.PAGE
 import org.wordpress.android.fluxc.model.SiteHomepageSettings.ShowOnFront.POSTS
 import org.wordpress.android.fluxc.network.rest.wpcom.site.SiteHomepageRestClient.UpdateHomepageResponse
-import java.lang.IllegalStateException
 import javax.inject.Inject
 
 class SiteHomepageSettingsMapper
@@ -18,7 +17,7 @@ class SiteHomepageSettingsMapper
         }
         return showOnFront?.let {
             when (showOnFront) {
-                PAGE -> Page(site.pageForPosts, site.pageOnFront)
+                PAGE -> StaticPage(site.pageForPosts, site.pageOnFront)
                 POSTS -> Posts
             }
         }
@@ -28,7 +27,7 @@ class SiteHomepageSettingsMapper
         return if (data.isPageOnFront) {
             val pageForPostsId = data.pageForPostsId ?: -1
             val pageOnFrontId = data.pageOnFrontId ?: -1
-            Page(pageForPostsId, pageOnFrontId)
+            StaticPage(pageForPostsId, pageOnFrontId)
         } else {
             Posts
         }
