7cae5a6976dd494fcbe2a0e58b02daacd80877d7
Chip Snyder <chip.snyder3@gmail.com>
2020-05-19 17:22:43 -0400

    Setup callbacks for fetching events from on change handler

--- fluxc/src/main/java/org/wordpress/android/fluxc/action/EditorThemeAction.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/EditorThemeAction.kt
@@ -1,9 +1,12 @@
 package org.wordpress.android.fluxc.action
 
+import org.wordpress.android.fluxc.annotations.Action
 import org.wordpress.android.fluxc.annotations.ActionEnum
 import org.wordpress.android.fluxc.annotations.action.IAction
+import org.wordpress.android.fluxc.store.EditorThemeStore.FetchEditorThemePayload
 
 @ActionEnum
 enum class EditorThemeAction : IAction {
+    @Action(payloadType = FetchEditorThemePayload::class)
     FETCH_EDITOR_THEME
 }
