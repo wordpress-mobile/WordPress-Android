ee059397723a1df25f1ccfd2715bcdb98dde35b2
Chip Snyder <chip.snyder3@gmail.com>
2020-05-19 15:58:21 -0400

    Adding EditorTheme Fragment and Store (With mocked data)

---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/action/EditorThemeAction.kt
@@ -0,0 +1,9 @@
+package org.wordpress.android.fluxc.action
+
+import org.wordpress.android.fluxc.annotations.ActionEnum
+import org.wordpress.android.fluxc.annotations.action.IAction
+
+@ActionEnum
+enum class EditorThemeAction : IAction {
+    FETCH_EDITOR_THEME
+}
