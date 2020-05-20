20950859c0e31931fe53f3b6b197d193a230a208
Klymentiy haykov <forsver@gmail.com>
2020-05-20 13:53:15 -0700

    Updated store.

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/WhatsNewStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/WhatsNewStore.kt
@@ -1,54 +1,82 @@
 package org.wordpress.android.fluxc.store
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.Payload
-import org.wordpress.android.fluxc.model.whatsnew.WhatsNewAnnounceModel
+import org.wordpress.android.fluxc.action.WhatsNewAction
+import org.wordpress.android.fluxc.action.WhatsNewAction.FETCH_WHATS_NEW
+import org.wordpress.android.fluxc.annotations.action.Action
+import org.wordpress.android.fluxc.model.whatsnew.WhatsNewAnnouncementModel
 import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError
 import org.wordpress.android.fluxc.network.rest.wpcom.whatsnew.WhatsNewRestClient
-import org.wordpress.android.fluxc.store.Store.OnChangedError
+import org.wordpress.android.fluxc.persistence.WhatsNewSqlUtils
 import org.wordpress.android.fluxc.store.WhatsNewStore.WhatsNewErrorType.GENERIC_ERROR
 import org.wordpress.android.fluxc.tools.CoroutineEngine
+import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.AppLog.T
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class WhatsNewStore @Inject constructor(
     private val whatsNewRestClient: WhatsNewRestClient,
-    private val coroutineEngine: CoroutineEngine
-) {
-//    @Subscribe(threadMode = ThreadMode.ASYNC)
-//    override fun onAction(action: Action<*>) {
-//        val actionType = action.type as? WhatsNewAction ?: return
-//        when (actionType) {
-//            FETCH_WHATS_NEW -> {
-//                val versionCode = (action.payload as WhatsNewFetchPayload).versionCode
-//                GlobalScope.launch(coroutineContext) { emitChange(fetchWhatsNew(versionCode)) }
-//            }
-//        }
-//    }
-
-    suspend fun fetchWhatsNew(versionCode: String) = coroutineEngine.withDefaultContext(T.API, this, "fetchWhatsNew") {
-        val fetchedWhatsNewPayload = whatsNewRestClient.fetchWhatsNew(versionCode)
-
-        return@withDefaultContext if (!fetchedWhatsNewPayload.isError) {
-            OnWhatsNewFetched(fetchedWhatsNewPayload.whatsNewItems)
-        } else {
-            OnWhatsNewFetched(
-                    fetchError = WhatsNewFetchError(GENERIC_ERROR, fetchedWhatsNewPayload.error.message)
-            )
+    private val whatsNewSqlUtils: WhatsNewSqlUtils,
+    private val coroutineContext: CoroutineContext,
+    private val coroutineEngine: CoroutineEngine,
+    dispatcher: Dispatcher
+) : Store(dispatcher) {
+    @Subscribe(threadMode = ThreadMode.ASYNC)
+    override fun onAction(action: Action<*>) {
+        val actionType = action.type as? WhatsNewAction ?: return
+        when (actionType) {
+            FETCH_WHATS_NEW -> {
+                val versionCode = (action.payload as WhatsNewFetchPayload).versionCode
+                val isForced = (action.payload as WhatsNewFetchPayload).forced
+                GlobalScope.launch(coroutineContext) { emitChange(fetchWhatsNew(versionCode, isForced)) }
+            }
         }
     }
 
+    suspend fun fetchWhatsNew(versionCode: String, forced: Boolean) =
+            coroutineEngine.withDefaultContext(T.API, this, "fetchWhatsNew") {
+
+                if (!forced && whatsNewSqlUtils.hasCachedAnnouncements()) {
+                    return@withDefaultContext OnWhatsNewFetched(whatsNewSqlUtils.getAnnouncements())
+                }
+
+                val fetchedWhatsNewPayload = whatsNewRestClient.fetchWhatsNew(versionCode)
+
+                return@withDefaultContext if (!fetchedWhatsNewPayload.isError) {
+
+                    val fetchedAnnouncements = fetchedWhatsNewPayload.whatsNewItems
+                    whatsNewSqlUtils.updateAnnouncementCache(fetchedAnnouncements)
+                    OnWhatsNewFetched(fetchedAnnouncements)
+                } else {
+                    OnWhatsNewFetched(
+                            fetchError = WhatsNewFetchError(GENERIC_ERROR, fetchedWhatsNewPayload.error.message)
+                    )
+                }
+            }
+
+    override fun onRegister() {
+        AppLog.d(AppLog.T.API, WhatsNewStore::class.java.simpleName + " onRegister")
+    }
+
     class WhatsNewFetchPayload(
-        val versionCode: String
+        val versionCode: String,
+        val forced: Boolean
     ) : Payload<BaseNetworkError>()
 
     class WhatsNewFetchedPayload(
-        val whatsNewItems: List<WhatsNewAnnounceModel>? = null
+        val whatsNewItems: List<WhatsNewAnnouncementModel>? = null
     ) : Payload<BaseNetworkError>()
 
     data class OnWhatsNewFetched(
-        val whatsNewItems: List<WhatsNewAnnounceModel>? = null,
+        val whatsNewItems: List<WhatsNewAnnouncementModel>? = null,
         val fetchError: WhatsNewFetchError? = null
     ) : Store.OnChanged<WhatsNewFetchError>() {
         init {
