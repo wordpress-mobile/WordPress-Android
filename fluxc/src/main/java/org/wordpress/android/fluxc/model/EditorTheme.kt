29e7a31f197c9ff3635adbd7ef50f1523578a0ce
Chip Snyder <chip.snyder3@gmail.com>
2020-05-21 15:29:05 -0400

    Adjust the return types from the editor theme store.

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/EditorTheme.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/EditorTheme.kt
@@ -1,6 +1,6 @@
 package org.wordpress.android.fluxc.model
 
-import com.google.gson.JsonArray
+import android.os.Bundle
 import com.google.gson.annotations.SerializedName
 
 data class EditorTheme(
@@ -10,6 +10,39 @@ data class EditorTheme(
 )
 
 data class EditorThemeSupport(
-    @SerializedName("editor-color-palette") val colors: JsonArray?,
-    @SerializedName("editor-gradient-presets") val gradients: JsonArray?
-)
+    @SerializedName("editor-color-palette") val colors: ArrayList<EditorThemeElement>?,
+    @SerializedName("editor-gradient-presets") val gradients: ArrayList<EditorThemeElement>?
+) {
+    fun toBundle(): Bundle {
+        val bundle = Bundle()
+        colors?.map { it.toBundle() }.let {
+            bundle.putParcelableArrayList("colors", ArrayList<Bundle>(it))
+        }
+
+        gradients?.map { it.toBundle() }.let {
+            bundle.putParcelableArrayList("gradients", ArrayList<Bundle>(it))
+        }
+
+        return bundle
+    }
+}
+
+data class EditorThemeElement(
+    val name: String?,
+    val slug: String?,
+    val color: String?,
+    val gradient: String?
+) {
+    fun toBundle(): Bundle {
+        val bundle = Bundle()
+        bundle.putString("name", name)
+        bundle.putString("slug", slug)
+        if (color != null) {
+            bundle.putString("color", color)
+        }
+        if (gradient != null) {
+            bundle.putString("gradient", gradient)
+        }
+        return bundle
+    }
+}
