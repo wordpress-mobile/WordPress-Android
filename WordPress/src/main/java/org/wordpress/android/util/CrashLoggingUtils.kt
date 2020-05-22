73d0d6b29aedc845018e64574e666e0fdc424be1
Oguz Kocer <oguz.kocer@automattic.com>
2020-05-21 21:57:14 -0400

    Updates Sentry to 2.1.3.
    The migration guide at https://docs.sentry.io/platforms/android/migrate/ was used to make this a 1-to-1 update. There is a very minor refactor, but otherwise the functionality should be the same.
    
    This will currently crash because `Sentry` wants `cacheDirPath` to be passed which will be handled in a subsequent commit.

--- WordPress/src/main/java/org/wordpress/android/util/CrashLoggingUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/util/CrashLoggingUtils.kt
@@ -1,39 +1,32 @@
 package org.wordpress.android.util
 
 import android.preference.PreferenceManager
-import io.sentry.Sentry
-import io.sentry.android.AndroidSentryClientFactory
-import io.sentry.event.BreadcrumbBuilder
+import io.sentry.core.Sentry
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.R
 
 class CrashLoggingUtils {
-    fun shouldEnableCrashLogging(context: android.content.Context): Boolean {
-        if (PackageUtils.isDebugBuild()) {
-            return false
+    companion object {
+        @JvmStatic fun shouldEnableCrashLogging(context: android.content.Context): Boolean {
+            if (PackageUtils.isDebugBuild()) {
+                return false
+            }
+
+            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+            val hasUserOptedOut = !prefs.getBoolean(context.getString(R.string.pref_key_send_crash), true)
+            return !hasUserOptedOut
         }
 
-        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-        val hasUserOptedOut = !prefs.getBoolean(context.getString(R.string.pref_key_send_crash), true)
-        return !hasUserOptedOut
-    }
-
-    fun enableCrashLogging(context: android.content.Context) {
-        Sentry.init(BuildConfig.SENTRY_DSN, AndroidSentryClientFactory(context))
-        Sentry.getContext().addTag("version", BuildConfig.VERSION_NAME)
-    }
-
-    companion object {
-        @JvmStatic fun startCrashLogging(context: android.content.Context) {
-            val obj = CrashLoggingUtils()
-
-            if (obj.shouldEnableCrashLogging(context)) {
-                obj.enableCrashLogging(context)
+        // TODO: Update Sentry DSN in mobile-secrets
+        @JvmStatic fun startCrashLogging() {
+            Sentry.init { options ->
+                options.dsn = BuildConfig.SENTRY_DSN
             }
+            Sentry.setTag("version", BuildConfig.VERSION_NAME)
         }
 
         @JvmStatic fun stopCrashLogging() {
-            Sentry.clearContext()
+            Sentry.clearBreadcrumbs()
             Sentry.close()
         }
 
@@ -42,13 +35,11 @@ class CrashLoggingUtils {
                 return
             }
 
-            Sentry.getContext().recordBreadcrumb(
-                    BreadcrumbBuilder().setMessage(message).build()
-            )
+            Sentry.addBreadcrumb(message)
         }
 
         @JvmStatic fun log(exception: Throwable) {
-            Sentry.capture(exception)
+            Sentry.captureException(exception)
         }
 
         @JvmStatic fun logException(tr: Throwable, tag: AppLog.T) {
