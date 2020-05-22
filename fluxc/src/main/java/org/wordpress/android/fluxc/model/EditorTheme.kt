7d26adbc18d78ed288ded5940bc4518458c88548
Chip Snyder <chip.snyder3@gmail.com>
2020-05-22 14:59:26 -0400

    Cache results of Editor Theme fetch

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/EditorTheme.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/EditorTheme.kt
@@ -2,16 +2,36 @@ package org.wordpress.android.fluxc.model
 
 import android.os.Bundle
 import com.google.gson.annotations.SerializedName
+import org.wordpress.android.fluxc.persistence.EditorThemeSqlUtils.EditorThemeBuilder
+import org.wordpress.android.fluxc.persistence.EditorThemeSqlUtils.EditorThemeElementBuilder
 
 data class EditorTheme(
     @SerializedName("theme_supports") val themeSupport: EditorThemeSupport,
     val stylesheet: String?,
     val version: String?
-)
+) {
+    fun toBuilder(siteId: Int): EditorThemeBuilder {
+        val element = EditorThemeBuilder()
+        element.localSiteId = siteId
+        element.stylesheet = stylesheet
+        element.version = version
+
+        return element
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null ||
+                other !is EditorTheme ||
+                stylesheet != other.stylesheet ||
+                version != other.version) return false
+
+        return true
+    }
+}
 
 data class EditorThemeSupport(
-    @SerializedName("editor-color-palette") val colors: ArrayList<EditorThemeElement>?,
-    @SerializedName("editor-gradient-presets") val gradients: ArrayList<EditorThemeElement>?
+    @SerializedName("editor-color-palette") val colors: List<EditorThemeElement>?,
+    @SerializedName("editor-gradient-presets") val gradients: List<EditorThemeElement>?
 ) {
     fun toBundle(): Bundle {
         val bundle = Bundle()
@@ -44,5 +64,16 @@ data class EditorThemeElement(
             bundle.putString("gradient", gradient)
         }
         return bundle
+    }
+
+    fun toBuilder(themeId: Int): EditorThemeElementBuilder {
+        val element = EditorThemeElementBuilder()
+        element.isColor = color != null
+        element.name = name
+        element.slug = slug
+        element.value = if (element.isColor) color else gradient
+        element.themeId = themeId
+
+        return element
     }
 }
---
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/EditorThemeSqlUtils.kt
@@ -0,0 +1,121 @@
+package org.wordpress.android.fluxc.persistence
+
+import com.wellsql.generated.ActivityLogTable
+import com.wellsql.generated.EditorThemeElementTable
+import com.wellsql.generated.EditorThemeTable
+import com.yarolegovich.wellsql.WellSql
+import com.yarolegovich.wellsql.core.Identifiable
+import com.yarolegovich.wellsql.core.annotation.Column
+import com.yarolegovich.wellsql.core.annotation.PrimaryKey
+import com.yarolegovich.wellsql.core.annotation.Table
+import org.wordpress.android.fluxc.model.EditorTheme
+import org.wordpress.android.fluxc.model.EditorThemeElement
+import org.wordpress.android.fluxc.model.EditorThemeSupport
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.persistence.ActivityLogSqlUtils.ActivityLogBuilder
+
+class EditorThemeSqlUtils {
+    fun replaceEditorThemeForSite(site: SiteModel, editorTheme: EditorTheme) {
+        deleteEditorThemeForSite(site)
+        makeEditorTheme(site, editorTheme)
+    }
+
+    fun getEditorThemeForSite(site: SiteModel): EditorTheme? {
+        val editorTheme: EditorThemeBuilder? = WellSql.select(EditorThemeBuilder::class.java)
+                .limit(1)
+                .where()
+                .equals(EditorThemeTable.LOCAL_SITE_ID, site.id)
+                .endWhere()
+                .asModel
+                .firstOrNull()
+
+        if (editorTheme == null ) return null
+
+        val colors: List<EditorThemeElementBuilder>? = WellSql.select(EditorThemeElementBuilder::class.java)
+                .where()
+                .equals(EditorThemeElementTable.THEME_ID, editorTheme.id)
+                .equals(EditorThemeElementTable.IS_COLOR, true)
+                .endWhere()
+                .asModel
+
+        val gradients: List<EditorThemeElementBuilder>? = WellSql.select(EditorThemeElementBuilder::class.java)
+                .where()
+                .equals(EditorThemeElementTable.THEME_ID, editorTheme.id)
+                .equals(EditorThemeElementTable.IS_COLOR, false)
+                .endWhere()
+                .asModel
+
+        return editorTheme.toEditorTheme(colors, gradients)
+    }
+
+    private fun deleteEditorThemeForSite(site: SiteModel) {
+        WellSql.delete(EditorThemeBuilder::class.java)
+                .where()
+                .equals(EditorThemeTable.LOCAL_SITE_ID, site.id)
+                .endWhere()
+                .execute()
+    }
+
+    private fun makeEditorTheme(site: SiteModel, editorTheme: EditorTheme) {
+        val editorThemeBuilder = editorTheme.toBuilder(site.id)
+        val items = (editorTheme.themeSupport.colors ?: emptyList()) +
+                (editorTheme.themeSupport.gradients ?: emptyList())
+
+        val elements = items.map {
+            it.toBuilder(editorThemeBuilder.id)
+        }
+
+        WellSql.insert(editorThemeBuilder).execute()
+        WellSql.insert(elements).asSingleTransaction(true).execute()
+    }
+
+    @Table(name = "EditorTheme")
+    data class EditorThemeBuilder(@PrimaryKey @Column private var mId: Int = -1) : Identifiable {
+        @Column var localSiteId: Int = -1
+            @JvmName("getLocalSiteId")
+            get
+            @JvmName("setLocalSiteId")
+            set
+        @Column var stylesheet: String? = null
+        @Column var version: String? = null
+
+        override fun setId(id: Int) {
+            this.mId = id
+        }
+
+        override fun getId() = mId
+
+        fun toEditorTheme(storedColors: List<EditorThemeElementBuilder>?, storedGradients: List<EditorThemeElementBuilder>?): EditorTheme {
+            val colors = storedColors?.map { it.toEditorThemeElement() }
+            val gradients = storedGradients?.map { it.toEditorThemeElement() }
+            val editorThemeSupport = EditorThemeSupport(colors, gradients)
+
+            return EditorTheme(editorThemeSupport, stylesheet, version)
+        }
+    }
+
+    @Table(name = "EditorThemeElement")
+    data class EditorThemeElementBuilder(@PrimaryKey @Column private var mId: Int = -1) : Identifiable {
+        @Column var themeId: Int = -1
+        @Column var isColor: Boolean = false
+            @JvmName("setIsColor")
+            set
+        @Column var name: String? = null
+        @Column var slug: String? = null
+        @Column var value: String? = null
+
+        override fun setId(id: Int) {
+            this.mId = id
+        }
+
+        override fun getId() = mId
+
+        fun toEditorThemeElement(): EditorThemeElement {
+            if (isColor) {
+                return EditorThemeElement(name, slug, value, null)
+            } else {
+                return EditorThemeElement(name, slug, null, value)
+            }
+        }
+    }
+}
