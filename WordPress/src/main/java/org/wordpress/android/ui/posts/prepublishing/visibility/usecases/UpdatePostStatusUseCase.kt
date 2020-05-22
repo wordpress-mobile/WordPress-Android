f1dbc5c6c8ff0eaf737c5514e17283be664f208b
Joel Dean <jdeanjj1000@gmail.com>
2020-05-22 17:11:36 -0500

    added missing throw to the next use case

--- WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/visibility/usecases/UpdatePostStatusUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/visibility/usecases/UpdatePostStatusUseCase.kt
@@ -24,7 +24,7 @@ class UpdatePostStatusUseCase @Inject constructor() {
             DRAFT -> PostStatus.DRAFT
             PENDING_REVIEW -> PostStatus.PENDING
             PRIVATE -> PostStatus.PRIVATE
-            PASSWORD_PROTECTED -> IllegalStateException("$visibility shouldn't be persisted because does map to a PostStatus.")
+            PASSWORD_PROTECTED -> throw IllegalStateException("$visibility shouldn't be persisted because does map to a PostStatus.")
         }
 
         editPostRepository.updateAsync({ postModel: PostModel ->
