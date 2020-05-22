c936efc920522046eae19f302bfe7be1b8dacc3b
Joel Dean <jdeanjj1000@gmail.com>
2020-05-22 17:47:58 -0500

    updated PostVisibilityUseCase tests

--- WordPress/src/test/java/org/wordpress/android/ui/posts/prepublishing/visibility/usecases/GetPostVisibilityUseCaseTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/posts/prepublishing/visibility/usecases/GetPostVisibilityUseCaseTest.kt
@@ -10,9 +10,11 @@ import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.fluxc.model.post.PostStatus
 import org.wordpress.android.ui.posts.EditPostRepository
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.DRAFT
 import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PASSWORD_PROTECTED
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PENDING_REVIEW
 import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PRIVATE
-import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PUBLIC
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PUBLISH
 
 class GetPostVisibilityUseCaseTest : BaseUnitTest() {
     private lateinit var editPostRepository: EditPostRepository
@@ -42,6 +44,52 @@ class GetPostVisibilityUseCaseTest : BaseUnitTest() {
     }
 
     @Test
+    fun `If PostStatus is PUBLISHED then PUBLISH should be the visibility`() {
+        // arrange
+        val post = PostModel().apply {
+            setStatus(PostStatus.PUBLISHED.toString())
+        }
+        editPostRepository.set { post }
+
+        // act
+        val visibility = getPostVisibilityUseCase.getVisibility(editPostRepository)
+
+        // assert
+        assertThat(visibility).isEqualTo(PUBLISH)
+    }
+
+    @Test
+    fun `If PostStatus is DRAFT then DRAFT should be the visibility`() {
+        // arrange
+        val post = PostModel().apply {
+            setStatus(PostStatus.DRAFT.toString())
+        }
+        editPostRepository.set { post }
+
+        // act
+        val visibility = getPostVisibilityUseCase.getVisibility(editPostRepository)
+
+        // assert
+        assertThat(visibility).isEqualTo(DRAFT)
+    }
+
+    @Test
+    fun `If PostStatus is PENDING then PENDING_REVIEW should be the visibility`() {
+        // arrange
+        val post = PostModel().apply {
+            setStatus(PostStatus.PENDING.toString())
+        }
+        editPostRepository.set { post }
+
+        // act
+        val visibility = getPostVisibilityUseCase.getVisibility(editPostRepository)
+
+        // assert
+        assertThat(visibility).isEqualTo(PENDING_REVIEW)
+    }
+
+
+    @Test
     fun `If PostStatus is PRIVATE then PRIVATE should be the visibility`() {
         // arrange
         val post = PostModel().apply {
@@ -54,20 +102,5 @@ class GetPostVisibilityUseCaseTest : BaseUnitTest() {
 
         // assert
         assertThat(visibility).isEqualTo(PRIVATE)
-    }
-
-    @Test
-    fun `If PostStatus is anything other than PRIVATE then PUBLIC should be the visibility`() {
-        // arrange
-        val post = PostModel().apply {
-            setStatus(PostStatus.PUBLISHED.toString())
-        }
-        editPostRepository.set { post }
-
-        // act
-        val visibility = getPostVisibilityUseCase.getVisibility(editPostRepository)
-
-        // assert
-        assertThat(visibility).isEqualTo(PUBLIC)
     }
 }
