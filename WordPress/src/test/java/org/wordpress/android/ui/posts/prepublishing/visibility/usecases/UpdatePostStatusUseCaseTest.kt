26530ea135334da0477fe8011d67e0eaf0b51a11
Joel Dean <jdeanjj1000@gmail.com>
2020-05-22 17:41:29 -0500

    updated tests for PostStatusUseCase

--- WordPress/src/test/java/org/wordpress/android/ui/posts/prepublishing/visibility/usecases/UpdatePostStatusUseCaseTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/posts/prepublishing/visibility/usecases/UpdatePostStatusUseCaseTest.kt
@@ -10,8 +10,12 @@ import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.fluxc.model.post.PostStatus
 import org.wordpress.android.ui.posts.EditPostRepository
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.DRAFT
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PASSWORD_PROTECTED
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PENDING_REVIEW
 import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PRIVATE
-import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PUBLIC
+import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PUBLISH
+import java.lang.IllegalStateException
 
 class UpdatePostStatusUseCaseTest : BaseUnitTest() {
     private lateinit var editPostRepository: EditPostRepository
@@ -26,6 +30,42 @@ class UpdatePostStatusUseCaseTest : BaseUnitTest() {
     }
 
     @Test
+    fun `verify that when updatePostStatus is called with PUBLISH Visibility the PostStatus is PUBLISHED`() {
+        // arrange
+        val expectedPostStatus = PostStatus.PUBLISHED
+
+        // act
+        updatePostStatusUseCase.updatePostStatus(PUBLISH, editPostRepository) {}
+
+        // assert
+        assertThat(editPostRepository.getPost()?.status).isEqualTo(expectedPostStatus.toString())
+    }
+
+    @Test
+    fun `verify that when updatePostStatus is called with DRAFT Visibility the PostStatus is DRAFT`() {
+        // arrange
+        val expectedPostStatus = PostStatus.DRAFT
+
+        // act
+        updatePostStatusUseCase.updatePostStatus(DRAFT, editPostRepository) {}
+
+        // assert
+        assertThat(editPostRepository.getPost()?.status).isEqualTo(expectedPostStatus.toString())
+    }
+
+    @Test
+    fun `verify that when updatePostStatus is called with PENDING_REVIEW Visibility the PostStatus is PENDING`() {
+        // arrange
+        val expectedPostStatus = PostStatus.PENDING
+
+        // act
+        updatePostStatusUseCase.updatePostStatus(PENDING_REVIEW, editPostRepository) {}
+
+        // assert
+        assertThat(editPostRepository.getPost()?.status).isEqualTo(expectedPostStatus.toString())
+    }
+
+    @Test
     fun `verify that when updatePostStatus is called with PRIVATE Visibility the PostStatus is PRIVATE`() {
         // arrange
         val expectedPostStatus = PostStatus.PRIVATE
@@ -37,15 +77,9 @@ class UpdatePostStatusUseCaseTest : BaseUnitTest() {
         assertThat(editPostRepository.getPost()?.status).isEqualTo(expectedPostStatus.toString())
     }
 
-    @Test
-    fun `verify that when updatePostStatus is called with PUBLIC Visibility the PostStatus is DRAFT`() {
-        // arrange
-        val expectedPostStatus = PostStatus.DRAFT
-
+    @Test(expected = IllegalStateException::class)
+    fun `verify that when updatePostStatus is called with PASSWORD_PROTECTED Visibility an exception is thrown`() {
         // act
-        updatePostStatusUseCase.updatePostStatus(PUBLIC, editPostRepository) {}
-
-        // assert
-        assertThat(editPostRepository.getPost()?.status).isEqualTo(expectedPostStatus.toString())
+        updatePostStatusUseCase.updatePostStatus(PASSWORD_PROTECTED, editPostRepository) {}
     }
 }
