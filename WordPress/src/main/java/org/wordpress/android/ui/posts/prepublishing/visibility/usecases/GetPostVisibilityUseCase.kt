eb91df76f0721730adbcb7cafa033db96b451926
Joel Dean <jdeanjj1000@gmail.com>
2020-05-22 19:42:39 -0500

    added SCHEDULED to visibility use case

--- WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/visibility/usecases/GetPostVisibilityUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/visibility/usecases/GetPostVisibilityUseCase.kt
@@ -4,6 +4,7 @@ import org.wordpress.android.fluxc.model.post.PostStatus
 import org.wordpress.android.fluxc.model.post.PostStatus.DRAFT
 import org.wordpress.android.fluxc.model.post.PostStatus.PENDING
 import org.wordpress.android.fluxc.model.post.PostStatus.PUBLISHED
+import org.wordpress.android.fluxc.model.post.PostStatus.SCHEDULED
 import org.wordpress.android.ui.posts.EditPostRepository
 import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility
 import org.wordpress.android.ui.posts.prepublishing.visibility.PrepublishingVisibilityItemUiState.Visibility.PASSWORD_PROTECTED
@@ -15,6 +16,7 @@ class GetPostVisibilityUseCase @Inject constructor() {
     fun getVisibility(editPostRepository: EditPostRepository) = when {
         editPostRepository.password.isNotEmpty() -> PASSWORD_PROTECTED
         editPostRepository.status == PUBLISHED -> PUBLISH
+        editPostRepository.status == SCHEDULED -> PUBLISH
         editPostRepository.status == PENDING -> PENDING_REVIEW
         editPostRepository.status == DRAFT -> Visibility.DRAFT
         editPostRepository.status == PostStatus.PRIVATE -> Visibility.PRIVATE
