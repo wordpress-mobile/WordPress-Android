85e732cbedf5fb3ec262484e3b6fbcff5b8898b7
Joel Dean <jdeanjj1000@gmail.com>
2020-05-29 00:24:43 -0500

    extracted logic to share functionality.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/home/usecases/PublishPostImmediatelyUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/home/usecases/PublishPostImmediatelyUseCase.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.ui.posts.prepublishing.home.usecases
 
 import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.fluxc.model.post.PostStatus
+import org.wordpress.android.fluxc.model.post.PostStatus.SCHEDULED
 import org.wordpress.android.ui.posts.EditPostRepository
 import org.wordpress.android.util.DateTimeUtilsWrapper
 import javax.inject.Inject
@@ -9,17 +10,18 @@ import javax.inject.Inject
 class PublishPostImmediatelyUseCase @Inject constructor(private val dateTimeUtilsWrapper: DateTimeUtilsWrapper) {
     fun updatePostToPublishImmediately(
         editPostRepository: EditPostRepository,
-        isNewPost: Boolean
+        publishPost: Boolean
     ) {
         editPostRepository.updateAsync({ postModel: PostModel ->
-            postModel.setDateCreated(dateTimeUtilsWrapper.currentTimeInIso8601())
-
+            if (postModel.status == SCHEDULED.toString()) {
+                postModel.setDateCreated(dateTimeUtilsWrapper.currentTimeInIso8601())
+            }
             // when the post is a Draft, Publish Now is shown as the Primary Action but if it's already Published then
             // Update Now is shown.
-            if (isNewPost) {
-                postModel.setStatus(PostStatus.DRAFT.toString())
-            } else {
+            if (publishPost) {
                 postModel.setStatus(PostStatus.PUBLISHED.toString())
+            } else {
+                postModel.setStatus(PostStatus.DRAFT.toString())
             }
             true
         })
