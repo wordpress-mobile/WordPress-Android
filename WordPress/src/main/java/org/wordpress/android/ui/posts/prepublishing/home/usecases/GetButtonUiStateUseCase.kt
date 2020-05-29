14d31a6ad24027fbb91bc037392e66dec50a3c4e
Joel Dean <jdeanjj1000@gmail.com>
2020-05-28 23:06:57 -0500

    made use case primary action only.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/home/usecases/GetButtonUiStateUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/home/usecases/GetButtonUiStateUseCase.kt
@@ -11,11 +11,7 @@ import org.wordpress.android.ui.posts.editor.EditorActionsProvider
 import org.wordpress.android.ui.posts.editor.PrimaryEditorAction
 import org.wordpress.android.ui.posts.editor.PrimaryEditorAction.SAVE
 import org.wordpress.android.ui.posts.editor.PrimaryEditorAction.SUBMIT_FOR_REVIEW
-import org.wordpress.android.ui.posts.editor.SecondaryEditorAction
-import org.wordpress.android.ui.posts.editor.SecondaryEditorAction.NONE
-import org.wordpress.android.ui.posts.editor.SecondaryEditorAction.SAVE_AS_DRAFT
 import org.wordpress.android.ui.uploads.UploadUtilsWrapper
-import java.lang.IllegalStateException
 import javax.inject.Inject
 
 class GetButtonUiStateUseCase @Inject constructor(
@@ -25,34 +21,19 @@ class GetButtonUiStateUseCase @Inject constructor(
     fun getUiState(
         editPostRepository: EditPostRepository,
         site: SiteModel,
-        isPrimaryEditorAction: Boolean,
         onButtonClicked: (PublishPost) -> Unit
     ): SubmitButtonUiState {
-        if (isPrimaryEditorAction) {
-            val editorAction = editorActionsProvider.getPrimaryAction(
-                    editPostRepository.status,
-                    uploadUtilsWrapper.userCanPublish(site)
-            )
+        val editorAction = editorActionsProvider.getPrimaryAction(
+                editPostRepository.status,
+                uploadUtilsWrapper.userCanPublish(site)
+        )
 
-            return when (editorAction) {
-                PrimaryEditorAction.PUBLISH_NOW -> PublishButtonUiState(onButtonClicked)
-                PrimaryEditorAction.SCHEDULE -> ScheduleButtonUiState(onButtonClicked)
-                PrimaryEditorAction.UPDATE -> UpdateButtonUiState(onButtonClicked)
-                SUBMIT_FOR_REVIEW, SAVE -> {
-                    throw IllegalStateException("The $editorAction action shouldn't be available in this bottom sheet.")
-                }
-            }
-        } else {
-            val editorAction = editorActionsProvider.getSecondaryAction(
-                    editPostRepository.status,
-                    uploadUtilsWrapper.userCanPublish(site)
-            )
-
-            return when (editorAction) {
-                SecondaryEditorAction.PUBLISH_NOW -> PublishButtonUiState(onButtonClicked)
-                SAVE_AS_DRAFT, SecondaryEditorAction.SAVE, NONE -> {
-                    throw IllegalStateException("The $editorAction action shouldn't be available in this bottom sheet.")
-                }
+        return when (editorAction) {
+            PrimaryEditorAction.PUBLISH_NOW -> PublishButtonUiState(onButtonClicked)
+            PrimaryEditorAction.SCHEDULE -> ScheduleButtonUiState(onButtonClicked)
+            PrimaryEditorAction.UPDATE -> UpdateButtonUiState(onButtonClicked)
+            SUBMIT_FOR_REVIEW, SAVE -> {
+                throw IllegalStateException("The $editorAction action shouldn't be available in this bottom sheet.")
             }
         }
     }
