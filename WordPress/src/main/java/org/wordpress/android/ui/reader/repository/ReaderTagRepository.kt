34b0db33e4d15a87f58291f0d894f184eb7ff586
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2020-05-29 10:27:29 -0400

    Update to use ReaderTagList

--- WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderTagRepository.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderTagRepository.kt
@@ -6,9 +6,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.wordpress.android.models.ReaderTag
+import org.wordpress.android.models.ReaderTagList
 import org.wordpress.android.models.ReaderTagType
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.SECONDS
 
 /**
  * ReaderTagRepository is middleware that encapsulates data related business related data logic
@@ -18,28 +21,27 @@ class ReaderTagRepository() {
     private val mutableTopics = MutableLiveData<List<ReaderTag>>()
     val topics: LiveData<List<ReaderTag>> = mutableTopics
 
-    suspend fun getInterests() {
-        // todo: full implementation needed
-        CoroutineScope(Dispatchers.IO).launch {
-            delay(TimeUnit.SECONDS.toMillis(5))
-            mutableTopics.postValue(getMockInterests())
-        }
+    suspend fun getInterests(): ReaderTagList =
+        withContext(Dispatchers.IO) {
+            delay(SECONDS.toMillis(5))
+            getMockInterests()
     }
 
     // todo: remove method post implementation
-    private fun getMockInterests(): List<ReaderTag> {
-        return listOf<ReaderTag>().apply {
-            for (c in 'A'..'Z')
-                (ReaderTag(c.toString(), c.toString(), c.toString(),
-                        "https://public-api.wordpress.com/rest/v1.2/read/tags/$c/posts",
-                        ReaderTagType.DEFAULT))
-        }
-    }
+    private fun getMockInterests() =
+            ReaderTagList().apply {
+                for (c in 'A'..'Z')
+                    (add(ReaderTag(
+                            c.toString(), c.toString(), c.toString(),
+                            "https://public-api.wordpress.com/rest/v1.2/read/tags/$c/posts",
+                            ReaderTagType.DEFAULT
+                    )))
+            }
 
     // todo: full implementation needed
     suspend fun saveInterests(tags: List<ReaderTag>) {
         CoroutineScope(Dispatchers.IO).launch {
-            delay(TimeUnit.SECONDS.toMillis(3))
+            delay(TimeUnit.SECONDS.toMillis(5))
         }
     }
 }
