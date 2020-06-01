baae2ea0a4b183e8735ea054a698afea6d81470b
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2020-06-01 16:06:35 -0400

    Add stub functions to support reader improvement dev

--- WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderPostRepository.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderPostRepository.kt
@@ -1,3 +1,38 @@
 package org.wordpress.android.ui.reader.repository
 
-class ReaderPostRepository
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import org.wordpress.android.modules.BG_THREAD
+import org.wordpress.android.modules.DEFAULT_SCOPE
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Named
+
+class ReaderPostRepository @Inject constructor(
+    @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
+    @Named(DEFAULT_SCOPE) private val defaultDispatcher: CoroutineDispatcher
+) {
+    private val mutableDiscoveryFeed = MutableLiveData<List<String>>()
+    val discoveryFeed: LiveData<List<String>> = mutableDiscoveryFeed
+
+    suspend fun getDiscoveryFeed(): LiveData<List<String>> =
+            withContext(bgDispatcher) {
+                delay(TimeUnit.SECONDS.toMillis(5))
+                getMockDiscoverFeed()
+            }
+
+    private suspend fun getMockDiscoverFeed(): LiveData<List<String>> {
+        return withContext(defaultDispatcher) {
+            mutableDiscoveryFeed.postValue(getListOfString())
+            discoveryFeed
+        }
+    }
+
+    private fun getListOfString() = ArrayList<String>().apply {
+        for (c in 'A'..'Z')
+            (add(c.toString()))
+    }.toList()
+}
--- WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderTagRepository.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderTagRepository.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.ui.reader.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -10,33 +11,28 @@ import kotlinx.coroutines.withContext
 import org.wordpress.android.models.ReaderTag
 import org.wordpress.android.models.ReaderTagList
 import org.wordpress.android.models.ReaderTagType
+import org.wordpress.android.modules.BG_THREAD
+import org.wordpress.android.modules.DEFAULT_SCOPE
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.SECONDS
 import javax.inject.Inject
+import javax.inject.Named
 
 /**
  * ReaderTagRepository is middleware that encapsulates data related business related data logic
  * Handle communicate with ReaderServices and Actions
  */
-class ReaderTagRepository @Inject constructor() {
-    private val mutableTopics = MutableLiveData<List<ReaderTag>>()
-    val topics: LiveData<List<ReaderTag>> = mutableTopics
+class ReaderTagRepository @Inject constructor(
+    @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
+    @Named(DEFAULT_SCOPE) private val defaultDispatcher: CoroutineDispatcher
+) {
+    private val mutableRecommendedInterests = MutableLiveData<ReaderTagList>()
+    val recommendedInterests: LiveData<ReaderTagList> = mutableRecommendedInterests
 
     suspend fun getInterests(): ReaderTagList =
-        withContext(Dispatchers.IO) {
-            delay(SECONDS.toMillis(5))
-            getMockInterests()
-    }
-
-    // todo: remove method post implementation
-    private fun getMockInterests() =
-            ReaderTagList().apply {
-                for (c in 'A'..'Z')
-                    (add(ReaderTag(
-                            c.toString(), c.toString(), c.toString(),
-                            "https://public-api.wordpress.com/rest/v1.2/read/tags/$c/posts",
-                            ReaderTagType.DEFAULT
-                    )))
+            withContext(Dispatchers.IO) {
+                delay(SECONDS.toMillis(5))
+                getMockInterests()
             }
 
     // todo: full implementation needed
@@ -45,4 +41,30 @@ class ReaderTagRepository @Inject constructor() {
             delay(TimeUnit.SECONDS.toMillis(5))
         }
     }
+
+    suspend fun getRecommendedInterests(): LiveData<ReaderTagList> =
+            withContext(bgDispatcher) {
+                delay(TimeUnit.SECONDS.toMillis(5))
+                getMockRecommendedInterests()
+            }
+
+    private suspend fun getMockRecommendedInterests(): LiveData<ReaderTagList> {
+        return withContext(defaultDispatcher) {
+            mutableRecommendedInterests.postValue(getMockInterests())
+            recommendedInterests
+        }
+    }
+
+    // todo: remove method post implementation
+    private fun getMockInterests() =
+            ReaderTagList().apply {
+                for (c in 'A'..'Z')
+                    (add(
+                            ReaderTag(
+                                    c.toString(), c.toString(), c.toString(),
+                                    "https://public-api.wordpress.com/rest/v1.2/read/tags/$c/posts",
+                                    ReaderTagType.DEFAULT
+                            )
+                    ))
+            }
 }
