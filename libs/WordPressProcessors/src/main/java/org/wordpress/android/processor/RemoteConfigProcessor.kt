ceabd3b161f8d93e96c9d7795865786a886ac7b4
vojtasmrcek <planarvoid@gmail.com>
2020-06-01 11:53:00 +0200

    Get remote config defaults file name from annotation

--- libs/WordPressProcessors/src/main/java/org/wordpress/android/processor/RemoteConfigProcessor.kt
+++ libs/WordPressProcessors/src/main/java/org/wordpress/android/processor/RemoteConfigProcessor.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.processor
 import com.google.auto.service.AutoService
 import org.wordpress.android.annotation.Experiment
 import org.wordpress.android.annotation.Feature
+import org.wordpress.android.annotation.RemoteConfig
 import java.io.File
 import java.net.URI
 import javax.annotation.processing.AbstractProcessor
@@ -15,9 +16,15 @@ import javax.lang.model.element.TypeElement
 
 @AutoService(Processor::class) // For registering the service
 @SupportedSourceVersion(SourceVersion.RELEASE_8) // to support Java 8
-@SupportedAnnotationTypes("org.wordpress.android.annotation.Experiment", "org.wordpress.android.annotation.Feature")
+@SupportedAnnotationTypes(
+        "org.wordpress.android.annotation.RemoteConfig",
+        "org.wordpress.android.annotation.Experiment",
+        "org.wordpress.android.annotation.Feature"
+)
 class RemoteConfigProcessor : AbstractProcessor() {
     override fun process(p0: MutableSet<out TypeElement>?, roundEnvironment: RoundEnvironment?): Boolean {
+        val remoteConfigFile: String? = roundEnvironment?.getElementsAnnotatedWith(RemoteConfig::class.java)
+                ?.firstOrNull()?.getAnnotation(RemoteConfig::class.java)?.location
         val experiments = roundEnvironment?.getElementsAnnotatedWith(Experiment::class.java)?.map { element ->
             val annotation = element.getAnnotation(Experiment::class.java)
             annotation.remoteField to annotation.defaultVariant
@@ -26,23 +33,26 @@ class RemoteConfigProcessor : AbstractProcessor() {
             val annotation = element.getAnnotation(Feature::class.java)
             annotation.remoteField to annotation.defaultValue.toString()
         } ?: listOf()
-        return if (experiments.isNotEmpty() || features.isNotEmpty()) {
-            generateRemoteConfigDefaults((experiments + features).toMap())
+        return if (remoteConfigFile != null && (experiments.isNotEmpty() || features.isNotEmpty())) {
+            generateRemoteConfigDefaults(remoteConfigFile, (experiments + features).toMap())
             true
         } else {
             false
         }
     }
 
-    private fun generateRemoteConfigDefaults(remoteConfigDefaults: Map<String, String>) {
+    private fun generateRemoteConfigDefaults(
+        remoteConfigFile: String,
+        remoteConfigDefaults: Map<String, String>
+    ) {
         val fileContent = RemoteConfigDefaultsBuilder(remoteConfigDefaults).getContent()
         val resFile = getResPath()
 
-        val file = File(resFile, "remote_config_defaults.xml")
+        val file = File(resFile, "$remoteConfigFile.xml")
         if (file.exists()) {
             file.delete()
         }
-        val newFile = File(resFile, "remote_config_defaults.xml")
+        val newFile = File(resFile, "$remoteConfigFile.xml")
         newFile.writeText(fileContent)
     }
 
