ee014fccbdd1cda0446862f0cd81be023ab9a4b1
ashiagr <agr.ash@gmail.com>
2020-06-02 13:51:06 +0530

    Add tests for show interests

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/interests/ReaderInterestsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/interests/ReaderInterestsViewModel.kt
@@ -2,16 +2,19 @@ package org.wordpress.android.ui.reader.discover.interests
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import org.wordpress.android.models.ReaderTagList
+import org.wordpress.android.modules.UI_THREAD
 import org.wordpress.android.ui.reader.repository.ReaderTagRepository
+import org.wordpress.android.viewmodel.ScopedViewModel
 import javax.inject.Inject
+import javax.inject.Named
 
 class ReaderInterestsViewModel @Inject constructor(
+    @Named(UI_THREAD) private val mainDispatcher: CoroutineDispatcher,
     private val readerTagRepository: ReaderTagRepository
-) : ViewModel() {
+) : ScopedViewModel(mainDispatcher) {
     var initialized: Boolean = false
 
     private val _uiState = MutableLiveData<UiState>()
@@ -23,7 +26,7 @@ class ReaderInterestsViewModel @Inject constructor(
     }
 
     private fun loadInterests() {
-        viewModelScope.launch { // TODO: Might want to use ScopedViewModel with mainDispatcher for consistency
+        launch {
             val tagList = readerTagRepository.getInterests()
             if (tagList.isNotEmpty()) {
                 updateUiState(UiState(transformToInterestsUiState(tagList), tagList))
--- WordPress/src/test/java/org/wordpress/android/ui/reader/discover/interests/ReaderInterestsViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/reader/discover/interests/ReaderInterestsViewModelTest.kt
@@ -1,12 +1,21 @@
 package org.wordpress.android.ui.reader.discover.interests
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.TEST_DISPATCHER
+import org.wordpress.android.models.ReaderTag
+import org.wordpress.android.models.ReaderTagList
+import org.wordpress.android.models.ReaderTagType
+import org.wordpress.android.test
+import org.wordpress.android.ui.reader.discover.interests.ReaderInterestsViewModel.InterestUiState
 import org.wordpress.android.ui.reader.repository.ReaderTagRepository
 
 @RunWith(MockitoJUnitRunner::class)
@@ -18,11 +27,52 @@ class ReaderInterestsViewModelTest {
 
     @Mock lateinit var readerTagRepository: ReaderTagRepository
 
+    @InternalCoroutinesApi
     @Before
     fun setUp() {
-        viewModel = ReaderInterestsViewModel(readerTagRepository)
+        viewModel = ReaderInterestsViewModel(TEST_DISPATCHER, readerTagRepository)
     }
 
     @Test
-    fun `sample test`() {} // TODO:
+    fun `interests ui correctly loaded if non empty interests received from repo on start`() = test {
+        // Given
+        val mockInterests = getMockInterests()
+        whenever(readerTagRepository.getInterests()).thenReturn(mockInterests)
+
+        // When
+        initViewModel()
+
+        // Then
+        assertThat(requireNotNull(viewModel.uiState.value).interests).isEqualTo(mockInterests)
+        assertThat(requireNotNull(viewModel.uiState.value).interestsUiState[0])
+                .isInstanceOf(InterestUiState::class.java)
+        assertThat(requireNotNull(viewModel.uiState.value).interestsUiState[0].title)
+                .isEqualTo(mockInterests[0].tagTitle)
+    }
+
+    @Test
+    fun `interests ui not updated if no tags received from repo on start`() = test {
+        // Given
+        whenever(readerTagRepository.getInterests()).thenReturn(ReaderTagList())
+
+        // When
+        initViewModel()
+
+        // Then
+        assertThat(viewModel.uiState.value).isNull()
+    }
+
+    private fun initViewModel() = viewModel.start()
+
+    private fun getMockInterests() =
+        ReaderTagList().apply {
+            for (c in 'A'..'Z')
+                (add(
+                    ReaderTag(
+                        c.toString(), c.toString(), c.toString(),
+                        "https://public-api.wordpress.com/rest/v1.2/read/tags/$c/posts",
+                        ReaderTagType.DEFAULT
+                    )
+                ))
+        }
 }
