b454bf65a797d8e8b4f81a1862cb82b02d801806
vojtasmrcek <planarvoid@gmail.com>
2020-06-03 12:38:14 +0200

    Disable crash on missing flow

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/UnifiedLoginTracker.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/UnifiedLoginTracker.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.accounts
 
+import org.wordpress.android.BuildConfig
 import org.wordpress.android.analytics.AnalyticsTracker.Stat.UNIFIED_LOGIN_STEP
 import org.wordpress.android.ui.accounts.UnifiedLoginTracker.Source.DEFAULT
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
@@ -12,32 +13,23 @@ class UnifiedLoginTracker
     private var currentSource: Source = DEFAULT
     private var currentFlow: Flow? = null
     fun track(step: Step) {
-        track(requireNotNull(currentFlow), step)
+        track(step = step)
     }
 
-    fun track(flow: Flow, step: Step) {
+    fun track(flow: Flow? = null, step: Step) {
         currentFlow = flow
-        analyticsTracker.track(
-                UNIFIED_LOGIN_STEP,
-                mapOf("source" to currentSource.value, "flow" to flow.value, "step" to step.value)
-        )
+        if (BuildConfig.UNIFIED_LOGIN_AVAILABLE) {
+            currentFlow?.let {
+                analyticsTracker.track(
+                        UNIFIED_LOGIN_STEP,
+                        mapOf("source" to currentSource.value, "flow" to it.value, "step" to step.value)
+                )
+            }
+        }
     }
 
     fun setSource(source: Source) {
         currentSource = source
-    }
-
-    fun clear() {
-        currentFlow = null
-    }
-
-    enum class Flow(val value: String) {
-        GET_STARTED("get_started"),
-        LOGIN_SOCIAL("social_login"),
-        LOGIN_MAGIC_LINK("login_magic_link"),
-        LOGIN_PASSWORD("login_password"),
-        LOGIN_SITE_ADDRESS("login_site_address"),
-        SIGNUP("signup")
     }
 
     enum class Source(val value: String) {
@@ -47,6 +39,15 @@ class UnifiedLoginTracker
         REAUTHENTICATION("reauthentication"),
         SELF_HOSTED("self_hosted"),
         DEFAULT("default")
+    }
+
+    enum class Flow(val value: String) {
+        GET_STARTED("get_started"),
+        LOGIN_SOCIAL("social_login"),
+        LOGIN_MAGIC_LINK("login_magic_link"),
+        LOGIN_PASSWORD("login_password"),
+        LOGIN_SITE_ADDRESS("login_site_address"),
+        SIGNUP("signup")
     }
 
     enum class Step(val value: String) {
