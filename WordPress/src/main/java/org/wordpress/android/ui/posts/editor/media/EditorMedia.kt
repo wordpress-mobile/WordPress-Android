3bd350e02018fd9ea0a7fda342bfbd240570ed52
malinajirka <malinajirka@gmail.com>
2020-06-12 12:19:34 +0200

    Inline the TrackMediaUploadErrorUseCase

--- WordPress/src/main/java/org/wordpress/android/ui/posts/editor/media/EditorMedia.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/editor/media/EditorMedia.kt
@@ -8,9 +8,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.wordpress.android.R
 import org.wordpress.android.analytics.AnalyticsTracker
 import org.wordpress.android.analytics.AnalyticsTracker.Stat
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.EDITOR_UPLOAD_MEDIA_FAILED
 import org.wordpress.android.editor.EditorMediaUploadListener
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.MediaActionBuilder
@@ -22,6 +24,7 @@ import org.wordpress.android.fluxc.store.MediaStore
 import org.wordpress.android.fluxc.store.MediaStore.CancelMediaPayload
 import org.wordpress.android.fluxc.store.MediaStore.FetchMediaListPayload
 import org.wordpress.android.fluxc.store.MediaStore.MediaError
+import org.wordpress.android.modules.BG_THREAD
 import org.wordpress.android.modules.UI_THREAD
 import org.wordpress.android.ui.pages.SnackbarMessageHolder
 import org.wordpress.android.ui.posts.EditPostActivity.OnPostUpdatedFromUIListener
@@ -38,6 +41,8 @@ import org.wordpress.android.util.MediaUtilsWrapper
 import org.wordpress.android.util.NetworkUtilsWrapper
 import org.wordpress.android.util.StringUtils
 import org.wordpress.android.util.ToastUtils.Duration
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
+import org.wordpress.android.util.analytics.AnalyticsUtilsWrapper
 import org.wordpress.android.util.helpers.MediaFile
 import org.wordpress.android.viewmodel.Event
 import org.wordpress.android.viewmodel.SingleLiveEvent
@@ -66,8 +71,10 @@ class EditorMedia @Inject constructor(
     private val cleanUpMediaToPostAssociationUseCase: CleanUpMediaToPostAssociationUseCase,
     private val removeMediaUseCase: RemoveMediaUseCase,
     private val reattachUploadingMediaUseCase: ReattachUploadingMediaUseCase,
-    private val trackMediaUploadErrorUseCase: TrackMediaUploadErrorUseCase,
-    @Named(UI_THREAD) private val mainDispatcher: CoroutineDispatcher
+    private val analyticsUtilsWrapper: AnalyticsUtilsWrapper,
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper,
+    @Named(UI_THREAD) private val mainDispatcher: CoroutineDispatcher,
+    @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher
 ) : CoroutineScope {
     // region Fields
     private var job: Job = Job()
@@ -297,7 +304,14 @@ class EditorMedia @Inject constructor(
     }
 
     fun onMediaUploadError(listener: EditorMediaUploadListener, media: MediaModel, error: MediaError) = launch {
-        trackMediaUploadErrorUseCase.trackMediaUploadError(media, error)
+        val properties: Map<String, Any?> = withContext(bgDispatcher) {
+            analyticsUtilsWrapper
+                    .getMediaProperties(media.isVideo, null, media.filePath)
+                    .also {
+                        it["error_type"] = error.type.name
+                    }
+        }
+        analyticsTrackerWrapper.track(EDITOR_UPLOAD_MEDIA_FAILED, properties)
         listener.onMediaUploadFailed(media.id.toString())
     }
 
