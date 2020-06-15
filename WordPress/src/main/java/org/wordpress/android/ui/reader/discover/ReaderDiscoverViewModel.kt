124dbcaf96f9adb28623105923bc78456518a609
malinajirka <malinajirka@gmail.com>
2020-06-15 15:50:20 +0200

    Start loading posts in discover VM

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderDiscoverViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderDiscoverViewModel.kt
@@ -1,19 +1,57 @@
 package org.wordpress.android.ui.reader.discover
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MediatorLiveData
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import org.wordpress.android.models.ReaderPostList
+import org.wordpress.android.modules.BG_THREAD
+import org.wordpress.android.modules.UI_THREAD
+import org.wordpress.android.ui.reader.discover.ReaderDiscoverViewModel.DiscoverUiState.ContentUiState
 import org.wordpress.android.ui.reader.discover.ReaderDiscoverViewModel.DiscoverUiState.LoadingUiState
 import org.wordpress.android.ui.reader.repository.ReaderPostRepository
+import org.wordpress.android.viewmodel.ScopedViewModel
 import javax.inject.Inject
+import javax.inject.Named
 
-class ReaderDiscoverViewModel @Inject constructor(private val readerPostRepository: ReaderPostRepository) : ViewModel() {
-    private val _uiState = MutableLiveData<DiscoverUiState>(LoadingUiState)
-    private val uiState: LiveData<DiscoverUiState> = _uiState
+class ReaderDiscoverViewModel @Inject constructor(
+    private val readerPostRepository: ReaderPostRepository,
+    @Named(UI_THREAD) private val mainDispatcher: CoroutineDispatcher,
+    @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher
+) : ScopedViewModel(mainDispatcher) {
+    private var isStarted = false
 
-    sealed class DiscoverUiState() {
-        data class ContentUiState() : DiscoverUiState()
+    private val _uiState = MediatorLiveData<DiscoverUiState>()
+    val uiState: LiveData<DiscoverUiState> = _uiState
+
+    fun start() {
+        if (isStarted) return
+        isStarted = true
+
+        init()
+        loadPosts()
+    }
+
+    private fun init() {
+        // Start with loading state
+        _uiState.value = LoadingUiState
+
+        // Listen to changes to the discover feed
+        _uiState.addSource(readerPostRepository.discoveryFeed) { posts ->
+            _uiState.value = ContentUiState(posts)
+        }
+    }
+
+    private fun loadPosts() {
+        // TODO we'll remove this method when the repositories start managing the requests automatically
+        launch(bgDispatcher) {
+            readerPostRepository.getDiscoveryFeed()
+        }
+    }
+
+    sealed class DiscoverUiState {
+        data class ContentUiState(val posts: ReaderPostList) : DiscoverUiState()
         object LoadingUiState : DiscoverUiState()
-        data class ErrorUiState() : DiscoverUiState()
+        object ErrorUiState : DiscoverUiState()
     }
 }
