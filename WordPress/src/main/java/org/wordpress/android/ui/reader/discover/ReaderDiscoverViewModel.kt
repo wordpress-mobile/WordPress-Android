146e47679937d97f2cbcfb0cbad5b4e5796c843b
malinajirka <malinajirka@gmail.com>
2020-06-15 14:59:56 +0200

    Init Discover UIState

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderDiscoverViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderDiscoverViewModel.kt
@@ -1,6 +1,19 @@
 package org.wordpress.android.ui.reader.discover
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.wordpress.android.ui.reader.discover.ReaderDiscoverViewModel.DiscoverUiState.LoadingUiState
+import org.wordpress.android.ui.reader.repository.ReaderPostRepository
 import javax.inject.Inject
 
-class ReaderDiscoverViewModel @Inject constructor() : ViewModel()
+class ReaderDiscoverViewModel @Inject constructor(private val readerPostRepository: ReaderPostRepository) : ViewModel() {
+    private val _uiState = MutableLiveData<DiscoverUiState>(LoadingUiState)
+    private val uiState: LiveData<DiscoverUiState> = _uiState
+
+    sealed class DiscoverUiState() {
+        data class ContentUiState() : DiscoverUiState()
+        object LoadingUiState : DiscoverUiState()
+        data class ErrorUiState() : DiscoverUiState()
+    }
+}
