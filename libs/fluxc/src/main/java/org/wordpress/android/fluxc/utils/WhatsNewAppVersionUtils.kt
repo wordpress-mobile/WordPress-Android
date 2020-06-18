4f38ba8bb1020aeaea3c352c4826f267964bbb8e
Klymentiy haykov <forsver@gmail.com>
2020-06-17 19:07:31 -0700

    Remove normalze version functionality.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/utils/WhatsNewAppVersionUtils.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/utils/WhatsNewAppVersionUtils.kt
@@ -4,10 +4,6 @@ class WhatsNewAppVersionUtils {
     companion object {
         val versionRegex = Regex("(\\d+\\.)(\\d+)")
 
-        fun normalizeVersion(appVersion: String): String? {
-            return versionRegex.find(appVersion, 0)?.value
-        }
-
         fun versionNameToInt(appVersion: String): Int {
             val majorMinor = Regex("(\\d+\\.)(\\d+)").find(appVersion, 0)?.value
 
--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/utils/WhatsNewAppVersionUtilsTest.kt
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/utils/WhatsNewAppVersionUtilsTest.kt
@@ -5,26 +5,6 @@ import kotlin.test.assertEquals
 
 class WhatsNewAppVersionUtilsTest {
     @Test
-    fun `normalizeVersion converts major dot minor dot patch into major dot minor`() {
-        val inputVersion = "14.1.2"
-        val expectedOutputVersion = "14.1"
-
-        val result = WhatsNewAppVersionUtils.normalizeVersion(inputVersion)
-
-        assertEquals(expectedOutputVersion, result)
-    }
-
-    @Test
-    fun `normalizeVersion returns null from malformed version name`() {
-        val inputVersion = "alpha-222"
-        val expectedOutputVersion = null
-
-        val result = WhatsNewAppVersionUtils.normalizeVersion(inputVersion)
-
-        assertEquals(expectedOutputVersion, result)
-    }
-
-    @Test
     fun `versionNameToInt converts major dot minor dot patch into major minor int`() {
         val inputVersion = "14.1.2"
         val expectedOutputVersion = 141
