4843bca1d942d3cdfecba71e4ace20334d9a2c01
Klymentiy haykov <forsver@gmail.com>
2020-06-25 22:54:24 -0700

    Removed unused import.

--- WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.kt
@@ -13,7 +13,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.PopupMenu
 import androidx.appcompat.widget.TooltipCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.yalantis.ucrop.UCrop
 import com.yalantis.ucrop.UCrop.Options
