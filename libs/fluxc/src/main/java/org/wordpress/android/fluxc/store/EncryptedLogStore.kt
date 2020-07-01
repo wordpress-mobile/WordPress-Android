dc7cf4aa779add90e342e86b5b15d4fa8118f78d
Oguz Kocer <oguz.kocer@automattic.com>
2020-07-01 19:26:27 -0400

    Simplify how encrypted logs are queued for upload

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/EncryptedLogStore.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/EncryptedLogStore.kt
@@ -1,20 +1,17 @@
 package org.wordpress.android.fluxc.store
 
 import org.wordpress.android.fluxc.model.EncryptedLog
-import org.wordpress.android.fluxc.model.EncryptedLogUploadState
-import org.wordpress.android.fluxc.model.EncryptedLogUploadState.NEEDS_UPLOAD
-import org.wordpress.android.fluxc.model.EncryptedLogUploadState.UPLOAD_FAILED
+import org.wordpress.android.fluxc.model.EncryptedLogUploadState.FAILED
+import org.wordpress.android.fluxc.model.EncryptedLogUploadState.QUEUED
 import org.wordpress.android.fluxc.network.rest.wpcom.encryptedlog.EncryptedLogRestClient
 import org.wordpress.android.fluxc.persistence.EncryptedLogSqlUtils
 import java.io.File
-import java.util.Date
-import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Singleton
 
 private const val MAX_FAIL_COUNT = 3
 
-//TODO: Add EncryptedLogModel DB migration
+// TODO: Add EncryptedLogModel DB migration
 
 @Singleton
 class EncryptedLogStore @Inject constructor(
@@ -31,21 +28,14 @@ class EncryptedLogStore @Inject constructor(
      * 4. At application start
      * 5. After a timer - maybe due to [handleFailedUpload]
      */
-    fun newLogFile(): EncryptedLog {
-        val encryptedLog = EncryptedLog(
-                uuid = UUID.randomUUID().toString(),
-                file = File("") // TODO: Give proper path
-        )
-        encryptedLogSqlUtils.insertOrUpdateEncryptedLog(encryptedLog)
-        return encryptedLog
-    }
-
-    private suspend fun queueLogForUpload(uuid: String) {
-        // TODO: Delete the file if we can't find a record in the DB
-        encryptedLogSqlUtils.getEncryptedLog(uuid)?.copy(uploadState = NEEDS_UPLOAD)?.let { encryptedLog ->
-            encryptedLogSqlUtils.insertOrUpdateEncryptedLog(encryptedLog)
-            uploadNext()
+    private suspend fun queueLogForUpload(uuid: String, file: File) {
+        // If the log file doesn't exist, there is nothing we can do
+        if (!file.exists()) {
+            return
         }
+        val encryptedLog = EncryptedLog(uuid = uuid, file = file)
+        encryptedLogSqlUtils.insertOrUpdateEncryptedLog(encryptedLog)
+        uploadNext()
     }
 
     private suspend fun uploadNextWithBackOffTiming() {
@@ -55,10 +45,18 @@ class EncryptedLogStore @Inject constructor(
 
     private suspend fun uploadNext() {
         // TODO: Don't upload if we are already uploading something
-        val list = encryptedLogSqlUtils.getEncryptedLogsForUploadState(uploadState = NEEDS_UPLOAD)
-        list.forEach { encryptedLog ->
-            encryptedLogRestClient.uploadLog(encryptedLog.uuid, encryptedLog.file)
+        val (logsToUpload, logsToDelete) = encryptedLogSqlUtils.getEncryptedLogsForUpload()
+                .partition { it.file.exists() }
+        // Delete any queued encrypted log records if the log file no longer exists
+        encryptedLogSqlUtils.deleteEncryptedLogs(logsToDelete)
+        // We want to upload a single file at a time
+        logsToUpload.firstOrNull()?.let {
+            uploadEncryptedLog(it)
         }
+    }
+
+    private suspend fun uploadEncryptedLog(encryptedLog: EncryptedLog) {
+        encryptedLogRestClient.uploadLog(encryptedLog.uuid, encryptedLog.file)
     }
 
     private suspend fun handleSuccessfulUpload(encryptedLog: EncryptedLog) {
@@ -71,7 +69,7 @@ class EncryptedLogStore @Inject constructor(
         // TODO: and just queue it again with a back off timer
         val updatedEncryptedLog = encryptedLog.copy(
                 failedCount = encryptedLog.failedCount + 1,
-                uploadState = UPLOAD_FAILED
+                uploadState = FAILED
         )
         if (updatedEncryptedLog.failedCount >= MAX_FAIL_COUNT) {
             // If a log failed to upload too many times, assume we can't upload it
