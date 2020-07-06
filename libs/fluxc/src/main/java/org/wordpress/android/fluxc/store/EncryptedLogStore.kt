65ee66d6fe440b419759fadd0f5904f22a709a13
Oguz Kocer <oguz.kocer@automattic.com>
2020-07-06 12:32:00 +0300

    Convert EncryptedLogStore to a proper FluxC Store

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/EncryptedLogStore.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/EncryptedLogStore.kt
@@ -1,15 +1,25 @@
 package org.wordpress.android.fluxc.store
 
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.Payload
+import org.wordpress.android.fluxc.action.EncryptedLogAction
+import org.wordpress.android.fluxc.action.EncryptedLogAction.UPLOAD_LOG
+import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.model.encryptedlogging.EncryptedLog
 import org.wordpress.android.fluxc.model.encryptedlogging.EncryptedLogUploadState.FAILED
 import org.wordpress.android.fluxc.model.encryptedlogging.EncryptedLogUploadState.UPLOADING
 import org.wordpress.android.fluxc.model.encryptedlogging.EncryptionUtils
 import org.wordpress.android.fluxc.model.encryptedlogging.LogEncrypter
+import org.wordpress.android.fluxc.network.BaseRequest.BaseNetworkError
 import org.wordpress.android.fluxc.network.rest.wpcom.encryptedlog.EncryptedLogRestClient
-import org.wordpress.android.fluxc.network.rest.wpcom.encryptedlog.LogUploadErrorType
-import org.wordpress.android.fluxc.network.rest.wpcom.encryptedlog.LogUploadResult.LogUploadFailed
-import org.wordpress.android.fluxc.network.rest.wpcom.encryptedlog.LogUploadResult.LogUploaded
+import org.wordpress.android.fluxc.network.rest.wpcom.encryptedlog.UploadEncryptedLogResult.LogUploadFailed
+import org.wordpress.android.fluxc.network.rest.wpcom.encryptedlog.UploadEncryptedLogResult.LogUploaded
 import org.wordpress.android.fluxc.persistence.EncryptedLogSqlUtils
+import org.wordpress.android.fluxc.tools.CoroutineEngine
+import org.wordpress.android.util.AppLog
+import org.wordpress.android.util.AppLog.T.API
 import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -21,9 +31,27 @@ private const val MAX_FAIL_COUNT = 3
 @Singleton
 class EncryptedLogStore @Inject constructor(
     private val encryptedLogRestClient: EncryptedLogRestClient,
-    private val encryptedLogSqlUtils: EncryptedLogSqlUtils
-) {
+    private val encryptedLogSqlUtils: EncryptedLogSqlUtils,
+    private val coroutineEngine: CoroutineEngine,
+    dispatcher: Dispatcher
+) : Store(dispatcher) {
     private val keyPair = EncryptionUtils.sodium.cryptoBoxKeypair()
+
+    override fun onRegister() {
+        AppLog.d(API, this.javaClass.name + ": onRegister")
+    }
+
+    @Subscribe(threadMode = ThreadMode.ASYNC)
+    override fun onAction(action: Action<*>) {
+        val actionType = action.type as? EncryptedLogAction ?: return
+        when (actionType) {
+            UPLOAD_LOG -> {
+                coroutineEngine.launch(API, this, "EncryptedLogStore: On UPLOAD_LOG") {
+                    queueLogForUpload(action.payload as UploadEncryptedLogPayload)
+                }
+            }
+        }
+    }
 
     /**
      * Document the logic for when uploads will happen:
@@ -35,15 +63,14 @@ class EncryptedLogStore @Inject constructor(
      * 4. At application start
      * 5. After a timer - maybe due to [handleFailedUpload]
      */
-    // TODO: Remove `suspend` and move the logic to bg thread and return asap
-    suspend fun queueLogForUpload(uuid: String, file: File) {
+    suspend fun queueLogForUpload(payload: UploadEncryptedLogPayload) {
         // If the log file doesn't exist, there is nothing we can do
-        if (!file.exists()) {
+        if (!payload.file.exists()) {
             return
         }
         val encryptedLog = EncryptedLog(
-                uuid = uuid,
-                file = file
+                uuid = payload.uuid,
+                file = payload.file
         )
         encryptedLogSqlUtils.insertOrUpdateEncryptedLog(encryptedLog)
         uploadNext()
@@ -81,16 +108,17 @@ class EncryptedLogStore @Inject constructor(
         ).encrypt()
         when (val result = encryptedLogRestClient.uploadLog(encryptedLog.uuid, contents)) {
             is LogUploaded -> handleSuccessfulUpload(encryptedLog)
-            is LogUploadFailed -> handleFailedUpload(encryptedLog, result.errorType)
+            is LogUploadFailed -> handleFailedUpload(encryptedLog, result.error)
         }
     }
 
     private suspend fun handleSuccessfulUpload(encryptedLog: EncryptedLog) {
         deleteEncryptedLog(encryptedLog)
+        emitChange(OnEncryptedLogUploaded(uuid = encryptedLog.uuid, file = encryptedLog.file))
         uploadNext()
     }
 
-    private suspend fun handleFailedUpload(encryptedLog: EncryptedLog, errorType: LogUploadErrorType) {
+    private suspend fun handleFailedUpload(encryptedLog: EncryptedLog, error: UploadEncryptedLogError) {
         // TODO: Handle known server errors. If the upload failed for a temporary reason, don't increase failed count
         // TODO: and just queue it again with a back off timer
         val updatedEncryptedLog = encryptedLog.copy(
@@ -100,6 +128,9 @@ class EncryptedLogStore @Inject constructor(
         if (updatedEncryptedLog.failedCount >= MAX_FAIL_COUNT) {
             // If a log failed to upload too many times, assume we can't upload it
             deleteEncryptedLog(updatedEncryptedLog)
+
+            // Since we have a retry mechanism we should only notify that we failed to upload when we give up
+            emitChange(OnEncryptedLogUploaded(uuid = encryptedLog.uuid, file = encryptedLog.file, error = error))
         } else {
             encryptedLogSqlUtils.insertOrUpdateEncryptedLog(updatedEncryptedLog)
         }
@@ -109,5 +140,25 @@ class EncryptedLogStore @Inject constructor(
     private fun deleteEncryptedLog(encryptedLog: EncryptedLog) {
         // TODO: Do we want to delete the unencrypted log file?
         encryptedLogSqlUtils.deleteEncryptedLogs(listOf(encryptedLog))
+    }
+
+    class UploadEncryptedLogPayload(
+        val uuid: String,
+        val file: File
+    ) : Payload<BaseNetworkError>()
+
+    class OnEncryptedLogUploaded(
+        val uuid: String,
+        val file: File,
+        error: UploadEncryptedLogError? = null
+    ) : Store.OnChanged<UploadEncryptedLogError>() {
+        init {
+            this.error = error
+        }
+    }
+
+    sealed class UploadEncryptedLogError : OnChangedError {
+        object Unknown : UploadEncryptedLogError()
+        class InvalidUuid(val message: String) : UploadEncryptedLogError()
     }
 }
