adf5ddcd9d29bc11a3ae660e0541d4a212b637cd
Marko Savic <savicmarko1985@gmail.com>
2020-07-10 13:36:54 +0200

    Disable unsupported block editor when web editor is set to classic

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -2018,13 +2018,15 @@ public class EditPostActivity extends LocaleAwareActivity implements
                         EditorTheme editorTheme = mEditorThemeStore.getEditorThemeForSite(mSite);
                         Bundle themeBundle = (editorTheme != null) ? editorTheme.getThemeSupport().toBundle() : null;
 
-                        // The Unsupported Block Editor is disabled for self-hosted sites
-                        // that are connected via Jetpack to a WP.com account.
-                        // This is because we don't have the self-hosted site's credentials
+                        // The Unsupported Block Editor is disabled for all self-hosted sites
+                        // even the one that are connected via Jetpack to a WP.com account.
+                        // The option is disabled on Self-hosted sites because they can have their web editor
+                        // to be set to classic and then the fallback will not work.
+                        // We disable in Jetpack site because we don't have the self-hosted site's credentials
                         // which are required for us to be able to fetch the site's authentication cookie.
-                        // This cookie is needed to authenticate the network request
-                        // that fetches the unsupported block editor web page.
-                        boolean isUnsupportedBlockEditorEnabled = mSite.isWPComAtomic() || !mSite.isJetpackConnected();
+                        boolean isUnsupportedBlockEditorEnabled =
+                                (mSite.isWPComAtomic() || !mSite.isJetpackConnected())
+                                && mSite.getWebEditor().equals("gutenberg");
 
                         return GutenbergEditorFragment.newInstance(
                                 "",
