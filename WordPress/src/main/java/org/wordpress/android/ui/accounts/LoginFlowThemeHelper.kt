c14b15dfbc8d4f0cc0be7d71b83df7fefc0eb5fb
Renan Ferrari <renan.fb@gmail.com>
2020-07-13 16:57:27 -0300

    Fix comment on LoginFlowThemeHelper

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginFlowThemeHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginFlowThemeHelper.kt
@@ -8,7 +8,7 @@ object LoginFlowThemeHelper {
      * This function should be used by activities that use the LoginFlow theme.
      * These activities often use components that refer to custom theme attributes defined by the WordPress theme,
      * but that are missing from the LoginFlow theme. Some examples: wpColorText, wpColorError, wpColorSuccess, etc.
-     * Instead of extending the LoginTheme only to include these attributes and having to maintain them in multiple
+     * Instead of extending the LoginFlow theme only to include these attributes and having to maintain them in multiple
      * places, we use this function to "inject" them directly.
      */
     @JvmStatic fun injectMissingCustomAttributes(theme: Resources.Theme) {
