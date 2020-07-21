8a4bd240d273631a5f869c45d0bccecd9387c2d1
Joel Dean <jdeanjj1000@gmail.com>
2020-07-21 16:57:21 -0500

    fixed lint error.

--- WordPress/src/main/java/org/wordpress/android/ui/stories/SaveInitialPostUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stories/SaveInitialPostUseCase.kt
@@ -12,7 +12,7 @@ class SaveInitialPostUseCase @Inject constructor(
     val postStore: PostStore,
     val savePostToDbUseCase: SavePostToDbUseCase
 ) {
-     fun saveInitialPost(editPostRepository: EditPostRepository, site: SiteModel?) {
+    fun saveInitialPost(editPostRepository: EditPostRepository, site: SiteModel?) {
         editPostRepository.set {
             val post: PostModel = postStore.instantiatePostModel(site, false, null, null)
             post.setStatus(PostStatus.DRAFT.toString())
