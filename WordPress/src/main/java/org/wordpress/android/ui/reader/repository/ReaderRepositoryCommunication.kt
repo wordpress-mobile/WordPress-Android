3e49fc3fd98aacdcaf09877f6188e9a29e3eedc7
ashiagr <agr.ash@gmail.com>
2020-07-21 17:57:22 +0530

    Allow data to be passed in ReaderRepositoryCommunication.SuccessWithData

--- WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderRepositoryCommunication.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderRepositoryCommunication.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.ui.reader.repository
 
 sealed class ReaderRepositoryCommunication {
     object Success : ReaderRepositoryCommunication()
+    data class SuccessWithData<out T>(val data: T) : ReaderRepositoryCommunication()
     sealed class Error : ReaderRepositoryCommunication() {
         object NetworkUnavailable : Error()
         object RemoteRequestFailure : Error()
