4f3c3df22a7dba579169e2cc682daa7ffab38932
Joel Dean <jdeanjj1000@gmail.com>
2020-07-24 13:40:24 -0500

    added the story title layout as a seperate view from the RecyclerView

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingHomeFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PrepublishingHomeFragment.kt
@@ -15,9 +15,14 @@ import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.posts.EditPostSettingsFragment.EditPostActivityHook
+import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.util.image.ImageManager
 import javax.inject.Inject
 
 class PrepublishingHomeFragment : Fragment() {
+    @Inject lateinit var uiHelpers: UiHelpers
+    @Inject lateinit var imageManager: ImageManager
+
     @Inject internal lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: PrepublishingHomeViewModel
 
@@ -58,6 +63,11 @@ class PrepublishingHomeFragment : Fragment() {
     private fun initViewModel() {
         viewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(PrepublishingHomeViewModel::class.java)
+
+        viewModel.storyTitleUiState.observe(viewLifecycleOwner, Observer { storyTitleUiState ->
+            uiHelpers.updateVisibility(story_title_header_view, true)
+            story_title_header_view.init(uiHelpers, imageManager, storyTitleUiState)
+        })
 
         viewModel.uiState.observe(viewLifecycleOwner, Observer { uiState ->
             (actions_recycler_view.adapter as PrepublishingHomeAdapter).update(uiState)
