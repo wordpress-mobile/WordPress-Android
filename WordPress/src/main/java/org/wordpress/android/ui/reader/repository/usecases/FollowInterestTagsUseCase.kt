9ed4e5e45c82095ee68c1b827f1c93d60cf1aa61
ashiagr <agr.ash@gmail.com>
2020-07-25 18:12:13 +0530

    Add NetworkUnavailable check to follow tags use case

--- WordPress/src/main/java/org/wordpress/android/ui/reader/repository/usecases/FollowInterestTagsUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/repository/usecases/FollowInterestTagsUseCase.kt
@@ -6,22 +6,28 @@ import org.wordpress.android.models.ReaderTag
 import org.wordpress.android.ui.reader.ReaderEvents.FollowedTagsChanged
 import org.wordpress.android.ui.reader.actions.ReaderTagActions
 import org.wordpress.android.ui.reader.repository.ReaderRepositoryCommunication
+import org.wordpress.android.ui.reader.repository.ReaderRepositoryCommunication.Error.NetworkUnavailable
 import org.wordpress.android.ui.reader.repository.ReaderRepositoryCommunication.Error.RemoteRequestFailure
 import org.wordpress.android.ui.reader.repository.ReaderRepositoryCommunication.Success
 import org.wordpress.android.util.EventBusWrapper
+import org.wordpress.android.util.NetworkUtilsWrapper
 import javax.inject.Inject
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class FollowInterestTagsUseCase @Inject constructor(
-    private val eventBusWrapper: EventBusWrapper
+    private val eventBusWrapper: EventBusWrapper,
+    private val networkUtilsWrapper: NetworkUtilsWrapper
 ) {
     private var continuation: Continuation<ReaderRepositoryCommunication>? = null
 
     suspend fun followInterestTags(tags: List<ReaderTag>): ReaderRepositoryCommunication {
         if (continuation != null) {
             throw IllegalStateException("Follow interest tags already in progress.")
+        }
+        if (!networkUtilsWrapper.isNetworkAvailable()) {
+            return NetworkUnavailable
         }
         return suspendCoroutine { cont ->
             continuation = cont
