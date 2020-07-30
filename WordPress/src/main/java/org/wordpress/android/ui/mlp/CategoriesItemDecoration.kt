eef5ad93bafad7a81538c11d4c257ab5fe0a4131
Antonis Lilis <antonis.lilis@automattic.com>
2020-07-30 13:24:46 +0300

    Renamed function parameter

--- WordPress/src/main/java/org/wordpress/android/ui/mlp/CategoriesItemDecoration.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mlp/CategoriesItemDecoration.kt
@@ -23,16 +23,16 @@ class CategoriesItemDecoration : RecyclerView.ItemDecoration() {
         }
     }
 
-    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        super.onDrawOver(c, parent, state)
+    override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(canvas, parent, state)
         if (scrollingView.y > 0 || !::scrollingView.isInitialized) return
         if (!::stickyView.isInitialized) {
             stickyView = LayoutInflater.from(parent.context)
                     .inflate(R.layout.modal_layout_picker_categories_row, parent, false)
         }
-        c.save()
-        c.translate(0f, max(0f, stickyView.top.toFloat() - stickyView.height.toFloat()))
-        scrollingView.draw(c)
-        c.restore()
+        canvas.save()
+        canvas.translate(0f, max(0f, stickyView.top.toFloat() - stickyView.height.toFloat()))
+        scrollingView.draw(canvas)
+        canvas.restore()
     }
 }
