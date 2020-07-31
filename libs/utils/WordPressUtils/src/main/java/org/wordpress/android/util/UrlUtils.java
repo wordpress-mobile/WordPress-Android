c3481d5494aeca6f121d9d205d5464088936d161
develric <develric@gmail.com>
2020-07-31 17:17:56 +0200

    Adding utils method for getPageJumpOrNull

--- libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/UrlUtils.java
+++ libs/utils/WordPressUtils/src/main/java/org/wordpress/android/util/UrlUtils.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import android.webkit.MimeTypeMap;
 import android.webkit.URLUtil;
 
+import androidx.annotation.Nullable;
+
 import org.wordpress.android.util.AppLog.T;
 
 import java.io.UnsupportedEncodingException;
@@ -17,6 +19,8 @@ import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.wordpress.android.util.PhotonUtils.ATOMIC_MEDIA_PROXY_URL_PREFIX;
 import static org.wordpress.android.util.PhotonUtils.ATOMIC_MEDIA_PROXY_URL_SUFFIX;
@@ -255,6 +259,20 @@ public class UrlUtils {
 
         return cleanedUrl.endsWith("jpg") || cleanedUrl.endsWith("jpeg")
                || cleanedUrl.endsWith("gif") || cleanedUrl.endsWith("png");
+    }
+
+    public static @Nullable String getPageJumpOrNull(String url) {
+        if (TextUtils.isEmpty(url)) {
+            return null;
+        }
+
+        if (url.contains("#")
+            && url.indexOf("#") < url.length() - 1
+            && url.split("#").length == 2) {
+            return url.substring(url.indexOf('#') + 1);
+        }
+
+        return null;
     }
 
     private static boolean isAtomicImageProxyUrl(String urlString) {
