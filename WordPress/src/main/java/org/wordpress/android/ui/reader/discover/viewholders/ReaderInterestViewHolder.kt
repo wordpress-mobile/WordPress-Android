8dd9695bd94d4251407351dbf6271d2656d9157c
Joel Dean <jdeanjj1000@gmail.com>
2020-08-03 08:41:58 -0500

    resolved ktx issue with LayoutContainer implementation

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/viewholders/ReaderInterestViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/viewholders/ReaderInterestViewHolder.kt
@@ -3,28 +3,22 @@ package org.wordpress.android.ui.reader.discover.viewholders
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.reader_interest_item.*
 import org.wordpress.android.R
 import org.wordpress.android.ui.reader.discover.ReaderCardUiState.ReaderInterestCardUiState.ReaderInterestUiState
 import org.wordpress.android.ui.utils.UiHelpers
 
 class ReaderInterestViewHolder(
     private val uiHelpers: UiHelpers,
-    internal val parent: ViewGroup
-) : RecyclerView.ViewHolder(
-        LayoutInflater.from(parent.context).inflate(
-                R.layout.reader_interest_item,
-                parent,
-                false
-        )
-) {
+    internal val parent: ViewGroup,
+    override val containerView: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.reader_interest_item, parent, false)
+) : RecyclerView.ViewHolder(containerView), LayoutContainer {
     fun onBind(uiState: ReaderInterestUiState) {
-        val interestTextView = this.itemView.findViewById<TextView>(R.id.interest)
-        val dividerView = this.itemView.findViewById<View>(R.id.divider)
-        val interestContainer = this.itemView.findViewById<View>(R.id.reader_interest_container)
-        uiHelpers.setTextOrHide(interestTextView, uiState.interest)
-        uiHelpers.updateVisibility(dividerView, uiState.isDividerVisible)
-        interestContainer.setOnClickListener { uiState.onClicked.invoke(uiState.interest) }
+        uiHelpers.setTextOrHide(interest, uiState.interest)
+        uiHelpers.updateVisibility(divider, uiState.isDividerVisible)
+        reader_interest_container.setOnClickListener { uiState.onClicked.invoke(uiState.interest) }
     }
 }
