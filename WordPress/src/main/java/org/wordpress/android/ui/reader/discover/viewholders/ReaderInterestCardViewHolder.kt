5e059ea96e9e5414fb71494c512bb6a2dd901ddb
Joel Dean <jdeanjj1000@gmail.com>
2020-08-03 07:33:21 -0500

    updated reader interest card ui and behavior

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/viewholders/ReaderInterestCardViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/viewholders/ReaderInterestCardViewHolder.kt
@@ -1,14 +1,13 @@
 package org.wordpress.android.ui.reader.discover.viewholders
 
-import android.content.Context
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.reader_interest_card.*
 import org.wordpress.android.R
 import org.wordpress.android.ui.reader.discover.ReaderCardUiState
 import org.wordpress.android.ui.reader.discover.ReaderCardUiState.ReaderInterestCardUiState
 import org.wordpress.android.ui.reader.discover.ReaderInterestAdapter
-import org.wordpress.android.ui.stats.refresh.utils.WrappingLinearLayoutManager
 import org.wordpress.android.ui.utils.UiHelpers
 
 class ReaderInterestCardViewHolder(
@@ -18,18 +17,11 @@ class ReaderInterestCardViewHolder(
     override fun onBind(uiState: ReaderCardUiState) {
         uiState as ReaderInterestCardUiState
 
-        interests_list.isNestedScrollingEnabled = false
         if (interests_list.adapter == null) {
+            interests_list.layoutManager = LinearLayoutManager(interests_list.context, RecyclerView.VERTICAL, false)
             val readerInterestAdapter = ReaderInterestAdapter(uiHelpers)
-            val layoutManager = WrappingLinearLayoutManager(
-                    interests_list.context,
-                    LinearLayoutManager.VERTICAL,
-                    false
-            )
             interests_list.adapter = readerInterestAdapter
-            interests_list.layoutManager = layoutManager
         }
-        (interests_list.layoutManager as WrappingLinearLayoutManager).init()
         (interests_list.adapter as ReaderInterestAdapter).update(uiState.interest)
     }
 }
