b98a3fac9e2295259ec67ba01c45ba7bb084b841
Antonis Lilis <antonis.lilis@automattic.com>
2020-08-04 13:44:09 +0300

    Fixes broken tests

--- WordPress/src/test/java/org/wordpress/android/viewmodel/mlp/ModalLayoutPickerViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/mlp/ModalLayoutPickerViewModelTest.kt
@@ -11,6 +11,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.ui.mlp.ModalLayoutPickerListItem.ViewType.CATEGORIES
 import org.wordpress.android.ui.mlp.ModalLayoutPickerListItem.ViewType.SUBTITLE
 import org.wordpress.android.ui.mlp.ModalLayoutPickerListItem.ViewType.TITLE
 import org.wordpress.android.util.NoDelayCoroutineDispatcher
@@ -36,60 +37,66 @@ class ModalLayoutPickerViewModelTest {
 
     @Test
     fun `the modal layout picker list is populated when initialized`() {
-        viewModel.init()
+        viewModel.init(false)
         assertThat(viewModel.listItems.value!!.size).isGreaterThan(0)
     }
 
     @Test
-    fun `the modal layout picker contains the title as a first item when initialized`() {
-        viewModel.init()
+    fun `the modal layout picker contains the title as a first item when initialized in portrait mode`() {
+        viewModel.init(false)
         assertThat(viewModel.listItems.value!!.first().type).isEqualTo(TITLE)
     }
 
     @Test
-    fun `the modal layout picker contains the subtitle as a second item when initialized`() {
-        viewModel.init()
+    fun `the modal layout picker contains the subtitle as a second item when initialized in portrait mode`() {
+        viewModel.init(false)
         assertThat(viewModel.listItems.value!!.get(1).type).isEqualTo(SUBTITLE)
     }
 
     @Test
+    fun `the modal layout picker contains the categories bar as a first item when initialized in landscape mode`() {
+        viewModel.init(true)
+        assertThat(viewModel.listItems.value!!.first().type).isEqualTo(CATEGORIES)
+    }
+
+    @Test
     fun `modal layout picker is shown when triggered`() {
-        viewModel.init()
+        viewModel.init(false)
         viewModel.show()
         assertThat(viewModel.isModalLayoutPickerShowing.value!!.peekContent()).isEqualTo(true)
     }
 
     @Test
     fun `modal layout picker is dismissed when the user hits the back button`() {
-        viewModel.init()
+        viewModel.init(false)
         viewModel.dismiss()
         assertThat(viewModel.isModalLayoutPickerShowing.value!!.peekContent()).isEqualTo(false)
     }
 
     @Test
-    fun `modal layout picker header is visible when the user scrolls up`() {
-        viewModel.init()
+    fun `modal layout picker header is visible when the user scrolls up in portrait mode`() {
+        viewModel.init(false)
         viewModel.setHeaderTitleVisibility(true)
         assertThat(viewModel.isHeaderVisible.value!!.peekContent()).isEqualTo(true)
     }
 
     @Test
-    fun `modal layout picker header is gone when the user scrolls down`() {
-        viewModel.init()
+    fun `modal layout picker header is gone when the user scrolls down in portrait mode`() {
+        viewModel.init(false)
         viewModel.setHeaderTitleVisibility(false)
         assertThat(viewModel.isHeaderVisible.value!!.peekContent()).isEqualTo(false)
     }
 
     @Test
     fun `when the create page is triggered the modal layout picker is dismissed`() {
-        viewModel.init()
+        viewModel.init(false)
         viewModel.createPage()
         assertThat(viewModel.isModalLayoutPickerShowing.value!!.peekContent()).isEqualTo(false)
     }
 
     @Test
     fun `when the create page is triggered the page creation flow starts`() {
-        viewModel.init()
+        viewModel.init(false)
         viewModel.createPage()
         verify(onCreateNewPageRequestedObserver).onChanged(anyOrNull())
     }
