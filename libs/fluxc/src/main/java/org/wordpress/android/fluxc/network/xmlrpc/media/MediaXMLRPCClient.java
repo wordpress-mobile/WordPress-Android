858cbca78fe57518ac87e39b003a094a842d75dd
develric <develric@gmail.com>
2020-08-08 00:35:31 +0200

    Map 401 and 500 xmlrpc error codes to MediaErrorType.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/media/MediaXMLRPCClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/media/MediaXMLRPCClient.java
@@ -562,11 +562,17 @@ public class MediaXMLRPCClient extends BaseXMLRPCClient implements ProgressListe
         mediaError.message = exception.getLocalizedMessage();
         if (exception instanceof XMLRPCFault) {
             switch (((XMLRPCFault) exception).getFaultCode()) {
-                case 404:
-                    mediaError.type = MediaErrorType.NOT_FOUND;
+                case 401:
+                    mediaError.type = MediaErrorType.XMLRPC_UNAUTHORIZED;
                     break;
                 case 403:
                     mediaError.type = MediaErrorType.NOT_AUTHENTICATED;
+                    break;
+                case 404:
+                    mediaError.type = MediaErrorType.NOT_FOUND;
+                    break;
+                case 500:
+                    mediaError.type = MediaErrorType.XMLRPC_INTERNAL_SERVER_ERROR;
                     break;
             }
         }
