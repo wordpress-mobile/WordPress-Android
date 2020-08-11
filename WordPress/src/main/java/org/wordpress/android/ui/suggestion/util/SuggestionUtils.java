cbee9117a3fc81f63e7d79ba3e68bba6215f1581
Matt Chowning <matt.chowning@automattic.com>
2020-08-11 16:06:49 -0400

    Only refresh suggestions after at least 60 seconds

--- WordPress/src/main/java/org/wordpress/android/ui/suggestion/util/SuggestionUtils.java
+++ WordPress/src/main/java/org/wordpress/android/ui/suggestion/util/SuggestionUtils.java
@@ -11,9 +11,13 @@ import org.wordpress.android.models.Suggestion;
 import org.wordpress.android.ui.suggestion.adapters.SuggestionAdapter;
 import org.wordpress.android.util.SiteUtils;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class SuggestionUtils {
+    private static HashMap<Long, Long> lastCallMap = new HashMap<>();
+    private static final long MIN_MS_BEFORE_REFRESH = 60 * 1000;
+
     @Nullable
     public static SuggestionAdapter setupSuggestions(
             SiteModel site,
@@ -36,11 +40,25 @@ public class SuggestionUtils {
         }
 
         List<Suggestion> suggestions = SuggestionTable.getSuggestionsForSite(siteId);
-        serviceConnectionManager.bindToService();
+        if (shouldRefresh(siteId)) {
+            serviceConnectionManager.bindToService();
+        }
 
         SuggestionAdapter suggestionAdapter = new SuggestionAdapter(context);
         suggestionAdapter.setSuggestionList(suggestions);
 
         return suggestionAdapter;
+    }
+
+    private static boolean shouldRefresh(final long siteId) {
+        Long lastCallMs = lastCallMap.get(siteId);
+        long currentMs = System.currentTimeMillis();
+        boolean shouldMakeCall = lastCallMs == null || currentMs - lastCallMs > MIN_MS_BEFORE_REFRESH;
+
+        if (shouldMakeCall) {
+            lastCallMap.put(siteId, currentMs);
+        }
+
+        return shouldMakeCall;
     }
 }
