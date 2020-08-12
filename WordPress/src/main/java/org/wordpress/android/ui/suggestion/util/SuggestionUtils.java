fd6ae50c5891c5df105a3d7258446b90a1d83c70
Matt Chowning <matt.chowning@automattic.com>
2020-08-12 10:47:11 -0400

    Move throttle to SuggestionService

--- WordPress/src/main/java/org/wordpress/android/ui/suggestion/util/SuggestionUtils.java
+++ WordPress/src/main/java/org/wordpress/android/ui/suggestion/util/SuggestionUtils.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.VisibleForTesting;
 
 import org.wordpress.android.datasets.SuggestionTable;
 import org.wordpress.android.fluxc.model.SiteModel;
@@ -12,14 +11,9 @@ import org.wordpress.android.models.Suggestion;
 import org.wordpress.android.ui.suggestion.adapters.SuggestionAdapter;
 import org.wordpress.android.util.SiteUtils;
 
-import java.util.HashMap;
 import java.util.List;
 
 public class SuggestionUtils {
-    @VisibleForTesting
-    protected static HashMap<Long, Long> lastCallMap = new HashMap<>();
-    private static final long MIN_MS_BEFORE_REFRESH = 60 * 1000;
-
     @Nullable
     public static SuggestionAdapter setupSuggestions(
             SiteModel site,
@@ -41,26 +35,13 @@ public class SuggestionUtils {
             return null;
         }
 
-        List<Suggestion> suggestions = SuggestionTable.getSuggestionsForSite(siteId);
-        if (shouldRefresh(siteId, System.currentTimeMillis())) {
-            serviceConnectionManager.bindToService();
-        }
 
+        serviceConnectionManager.bindToService();
+
+        List<Suggestion> suggestions = SuggestionTable.getSuggestionsForSite(siteId);
         SuggestionAdapter suggestionAdapter = new SuggestionAdapter(context);
         suggestionAdapter.setSuggestionList(suggestions);
 
         return suggestionAdapter;
-    }
-
-    @VisibleForTesting
-    protected static boolean shouldRefresh(final long siteId, final long currentMs) {
-        Long lastCallMs = lastCallMap.get(siteId);
-        boolean shouldMakeCall = lastCallMs == null || currentMs - lastCallMs >= MIN_MS_BEFORE_REFRESH;
-
-        if (shouldMakeCall) {
-            lastCallMap.put(siteId, currentMs);
-        }
-
-        return shouldMakeCall;
     }
 }
