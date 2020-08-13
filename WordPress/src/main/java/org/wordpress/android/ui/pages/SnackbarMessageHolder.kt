0c1cc1f3611949fe9e80aeef0efe0a1c3d5c6443
ashiagr <agr.ash@gmail.com>
2020-08-13 12:42:05 +0530

    Update SnackbarMessageHolder to accept a string

--- WordPress/src/main/java/org/wordpress/android/ui/pages/SnackbarMessageHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/pages/SnackbarMessageHolder.kt
@@ -2,9 +2,12 @@ package org.wordpress.android.ui.pages
 
 import androidx.annotation.StringRes
 
+const val INVALID_MESSAGE_RES = -1
+
 data class SnackbarMessageHolder(
     @StringRes val messageRes: Int,
     @StringRes val buttonTitleRes: Int? = null,
     val buttonAction: () -> Unit = {},
-    val onDismissAction: () -> Unit = {}
+    val onDismissAction: () -> Unit = {},
+    val message: CharSequence? = null
 )
