a5bcbbcab4b1e579bbf7da2fe27f37e3ed2dbfb4
Alex Forcier <alex.forcier@automattic.com>
2020-08-20 12:25:10 +0900

    Remove gallery fallback logic for stories
    With the story block now added to Jetpack, we can
    use the story block on any site meeting the requirements
    (WP.com or Jetpack above a minimum version).

--- WordPress/src/main/java/org/wordpress/android/ui/stories/SaveStoryGutenbergBlockUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stories/SaveStoryGutenbergBlockUseCase.kt
@@ -3,28 +3,12 @@ package org.wordpress.android.ui.stories
 import com.google.gson.Gson
 import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.ui.posts.EditPostRepository
-import org.wordpress.android.ui.posts.PostUtils
 import org.wordpress.android.util.ShortcodeUtils
 import org.wordpress.android.util.StringUtils
 import org.wordpress.android.util.helpers.MediaFile
 import javax.inject.Inject
 
 class SaveStoryGutenbergBlockUseCase @Inject constructor() {
-    // TODO will be removed shortly, but keeping for sites that don't yet have support for the jetpack stories block.
-    fun buildWPGalleryInPost(
-        editPostRepository: EditPostRepository,
-        mediaFiles: Map<String, MediaFile>
-    ) {
-        // Create a gallery shortcode and placeholders for Media Ids
-        val idsString = mediaFiles.map {
-            PostUtils.WP_STORIES_POST_MEDIA_LOCAL_ID_PLACEHOLDER + it.value.id.toString()
-        }.joinToString(separator = ",")
-        editPostRepository.update { postModel: PostModel ->
-            postModel.setContent("[gallery type=\"slideshow\" ids=\"$idsString\"]")
-            true
-        }
-    }
-
     fun buildJetpackStoryBlockInPost(
         editPostRepository: EditPostRepository,
         mediaFiles: Map<String, MediaFile>
