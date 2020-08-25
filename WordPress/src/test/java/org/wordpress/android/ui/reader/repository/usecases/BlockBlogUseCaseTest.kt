d67910a3b44b19998cd851ae611871852171ef30
malinajirka <malinajirka@gmail.com>
2020-08-25 14:57:50 +0200

    Fix test

--- WordPress/src/test/java/org/wordpress/android/ui/reader/repository/usecases/BlockBlogUseCaseTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/reader/repository/usecases/BlockBlogUseCaseTest.kt
@@ -25,7 +25,6 @@ class BlockBlogUseCaseTest {
     @Before
     fun setUp() {
         mBlockBlogUseCase = BlockBlogUseCase(networkUtilsWrapper, analyticsUtilsWrapper, readerBlogActionsWrapper)
-        whenever(networkUtilsWrapper.isNetworkAvailable()).thenReturn(true)
     }
 
     @Test
