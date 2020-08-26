798a9b5507f16dad02281c8f8ac8c5626f595294
ashiagr <agr.ash@gmail.com>
2020-08-26 16:00:54 +0530

    Remove unused method

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListFragment.java
@@ -1619,35 +1619,6 @@ public class ReaderPostListFragment extends Fragment
     }
 
     /*
-     * called when user taps follow item in popup menu for a post
-     */
-    private void toggleFollowStatusForPost(final ReaderPost post) {
-        if (post == null
-            || !hasPostAdapter()
-            || !NetworkUtils.checkConnection(getActivity())) {
-            return;
-        }
-
-        final boolean isAskingToFollow = !ReaderPostTable.isPostFollowed(post);
-
-        ReaderActions.ActionListener actionListener = new ReaderActions.ActionListener() {
-            @Override
-            public void onActionResult(boolean succeeded) {
-                if (isAdded() && !succeeded) {
-                    int resId = (isAskingToFollow ? R.string.reader_toast_err_follow_blog
-                            : R.string.reader_toast_err_unfollow_blog);
-                    ToastUtils.showToast(getActivity(), resId);
-                    getPostAdapter().setFollowStatusForBlog(post.blogId, !isAskingToFollow);
-                }
-            }
-        };
-
-        if (ReaderBlogActions.followBlogForPost(post, isAskingToFollow, actionListener)) {
-            getPostAdapter().setFollowStatusForBlog(post.blogId, isAskingToFollow);
-        }
-    }
-
-    /*
      * blocks the blog associated with the passed post and removes all posts in that blog
      * from the adapter
      */
