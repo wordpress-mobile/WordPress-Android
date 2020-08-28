12002f4cf2d684c9ded6fb39a9e19c7d754a7d5c
malinajirka <malinajirka@gmail.com>
2020-08-28 13:02:38 +0200

    Add new methods for tracking active tab into ReaderTracker

--- WordPress/src/main/java/org/wordpress/android/ui/reader/tracker/ReaderTracker.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/tracker/ReaderTracker.kt
@@ -1,17 +1,34 @@
 package org.wordpress.android.ui.reader.tracker
 
 import androidx.annotation.MainThread
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.READER_CUSTOM_TAB_SHOWN
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.READER_DISCOVER_SHOWN
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.READER_FOLLOWING_SHOWN
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.READER_LIKED_SHOWN
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.READER_SAVED_LIST_SHOWN
+import org.wordpress.android.models.ReaderTag
+import org.wordpress.android.ui.prefs.AppPrefsWrapper
+import org.wordpress.android.ui.reader.tracker.ReaderTab.CUSTOM
+import org.wordpress.android.ui.reader.tracker.ReaderTab.DISCOVER
+import org.wordpress.android.ui.reader.tracker.ReaderTab.FOLLOWING
+import org.wordpress.android.ui.reader.tracker.ReaderTab.LIKED
+import org.wordpress.android.ui.reader.tracker.ReaderTab.SAVED
 import org.wordpress.android.ui.reader.utils.DateProvider
 import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.AppLog.T
 import org.wordpress.android.util.AppLog.T.MAIN
 import org.wordpress.android.util.DateTimeUtils
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 @MainThread
-class ReaderTracker @Inject constructor(private val dateProvider: DateProvider) {
+class ReaderTracker @Inject constructor(
+    private val dateProvider: DateProvider,
+    private val appPrefsWrapper: AppPrefsWrapper,
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
+) {
     // TODO: evaluate to use something like Dispatchers.Main.Immediate in the fun(s)
     // to sync the access to trackers; so to remove the @MainThread and make the
     // usage of this class more transparent to its users
@@ -58,5 +75,53 @@ class ReaderTracker @Inject constructor(private val dateProvider: DateProvider) 
 
     fun getAnalyticsData(): Map<String, Any> {
         return trackers.entries.associate { it.key.propertyName to it.value.accumulatedTime }
+    }
+
+    fun trackReaderTabIfNecessary(readerTab: ReaderTab) {
+        if (appPrefsWrapper.isReaderImprovementsPhase2Enabled() && readerTab != appPrefsWrapper.getReaderActiveTab()) {
+            when (readerTab) {
+                FOLLOWING -> analyticsTrackerWrapper.track(READER_FOLLOWING_SHOWN)
+                DISCOVER -> analyticsTrackerWrapper.track(READER_DISCOVER_SHOWN)
+                LIKED -> analyticsTrackerWrapper.track(READER_LIKED_SHOWN)
+                SAVED -> analyticsTrackerWrapper.track(READER_SAVED_LIST_SHOWN, mapOf("source" to "reader_filter"))
+                CUSTOM -> analyticsTrackerWrapper.track(READER_CUSTOM_TAB_SHOWN)
+            }
+            appPrefsWrapper.setReaderActiveTab(readerTab)
+        }
+    }
+
+    fun onAppGoesToBackground() {
+        appPrefsWrapper.setReaderActiveTab(null)
+    }
+
+    fun onBottomNavigationTabChanged() {
+        appPrefsWrapper.setReaderActiveTab(null)
+    }
+}
+
+enum class ReaderTab(val id: Int) {
+    FOLLOWING(1), DISCOVER(2), LIKED(3), SAVED(4), CUSTOM(5);
+
+    companion object {
+        fun fromId(id: Int): ReaderTab {
+            return when (id) {
+                FOLLOWING.id -> FOLLOWING
+                DISCOVER.id -> DISCOVER
+                LIKED.id -> LIKED
+                SAVED.id -> SAVED
+                CUSTOM.id -> CUSTOM
+                else -> throw RuntimeException("Unexpected ReaderTab id")
+            }
+        }
+
+        fun transformTagToTab(readerTag: ReaderTag): ReaderTab {
+            return when {
+                readerTag.isFollowedSites -> FOLLOWING
+                readerTag.isPostsILike -> LIKED
+                readerTag.isBookmarked -> SAVED
+                readerTag.isDiscover -> DISCOVER
+                else -> CUSTOM
+            }
+        }
     }
 }
