72836a1b695da23d9ffa56959bff596ef0d41254
Klymentiy haykov <forsver@gmail.com>
2020-09-02 21:18:27 -0700

    Fixed site title glitch.

--- WordPress/src/main/java/org/wordpress/android/widgets/MySiteTitleAndSubtitleLabelView.kt
+++ WordPress/src/main/java/org/wordpress/android/widgets/MySiteTitleAndSubtitleLabelView.kt
@@ -2,7 +2,6 @@ package org.wordpress.android.widgets
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.TypedValue
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
@@ -12,18 +11,14 @@ import org.wordpress.android.R
 
 /**
  * Text View used for a site name and url label on My Site fragment.
- * This view accomplishes two main tasks:
- * - When the title is long, and wraps to the next line text size is reduced.
- * - Both title and subtitle are centered in the view, unless site title wraps to the next line.
+ * This view works in tandem with autosizing behavior of title textiview so
+ * when the title is long, and wraps to the next line text size is reduced and title and subtitle stop being centered.
  */
 class MySiteTitleAndSubtitleLabelView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
-    private val singleLineTextSize = resources.getDimensionPixelSize(R.dimen.my_site_name_label_single_line_text_size)
-    private val doubleLineTextSize = resources.getDimensionPixelSize(R.dimen.my_site_name_label_double_line_text_size)
-
     var title: MaterialTextView
     var subtitle: MaterialButton
 
@@ -36,20 +31,16 @@ class MySiteTitleAndSubtitleLabelView @JvmOverloads constructor(
         val guideline = findViewById<View>(R.id.guideline)
 
         title.viewTreeObserver.addOnGlobalLayoutListener {
-            if (title.lineCount == 1 && title.textSize.toInt() != singleLineTextSize) {
+            if (title.lineCount == 1) {
                 val constraintSet = ConstraintSet()
-                constraintSet.clone(this)
+                constraintSet.clone(this@MySiteTitleAndSubtitleLabelView)
                 constraintSet.connect(title.id, ConstraintSet.BOTTOM, guideline.id, ConstraintSet.TOP, 0)
-                constraintSet.applyTo(this)
-
-                title.setTextSize(TypedValue.COMPLEX_UNIT_PX, singleLineTextSize.toFloat())
-            } else if (title.lineCount == 2 && title.textSize.toInt() != doubleLineTextSize) {
+                constraintSet.applyTo(this@MySiteTitleAndSubtitleLabelView)
+            } else if (title.lineCount == 2) {
                 val constraintSet = ConstraintSet()
-                constraintSet.clone(this)
+                constraintSet.clone(this@MySiteTitleAndSubtitleLabelView)
                 constraintSet.clear(title.id, ConstraintSet.BOTTOM)
-                constraintSet.applyTo(this)
-
-                title.setTextSize(TypedValue.COMPLEX_UNIT_PX, doubleLineTextSize.toFloat())
+                constraintSet.applyTo(this@MySiteTitleAndSubtitleLabelView)
             }
         }
     }
