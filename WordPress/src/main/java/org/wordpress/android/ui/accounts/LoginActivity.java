f207448b1e00416399c16921a604dae07903fc59
Klymentiy haykov <forsver@gmail.com>
2020-09-02 19:48:16 -0700

    Removed unused import.

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
@@ -9,7 +9,6 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.auth.api.credentials.Credential;
