0f17dd4646688c74e8a44a3c8f99a5e57989c80d
vojtasmrcek <planarvoid@gmail.com>
2020-09-09 11:29:56 +0200

    Add loader item to adapter for next page loading

---
+++ WordPress/src/main/java/org/wordpress/android/ui/mediapicker/LoaderViewHolder.kt
@@ -0,0 +1,30 @@
+package org.wordpress.android.ui.mediapicker
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager.LayoutParams
+import org.wordpress.android.R
+import org.wordpress.android.widgets.WPTextView
+
+class LoaderViewHolder(parent: ViewGroup) :
+        ThumbnailViewHolder(parent, R.layout.media_picker_loader_item) {
+    private val progress: View = itemView.findViewById(R.id.progress)
+    private val error: WPTextView = itemView.findViewById(R.id.error)
+    fun bind(item: MediaPickerUiItem.NextPageLoader) {
+        setFullWidth()
+        if (item.isLoading) {
+            progress.visibility = View.VISIBLE
+            error.visibility = View.GONE
+        } else if (item.error != null) {
+            progress.visibility = View.GONE
+            error.visibility = View.VISIBLE
+            error.text = item.error
+        }
+    }
+
+    private fun setFullWidth() {
+        val layoutParams = itemView.layoutParams as? LayoutParams
+        layoutParams?.isFullSpan = true
+    }
+}
