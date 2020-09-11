1de6b2a5a930a8e3d6578e4e2cfbe3c573bf0345
develric <develric@gmail.com>
2020-09-11 11:17:18 +0200

    Removing Unneeded method.

--- WordPress/src/main/java/org/wordpress/android/ui/mediapicker/MediaPickerSetup.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mediapicker/MediaPickerSetup.kt
@@ -28,8 +28,6 @@ data class MediaPickerSetup(
         intent.putExtra(KEY_CAMERA_ENABLED, cameraEnabled)
     }
 
-    fun isBrowser() = dataSource == DEVICE && allowedTypes.containsAll(MediaType.values().toList())
-
     companion object {
         private const val KEY_DATA_SOURCE = "key_data_source"
         private const val KEY_CAN_MULTISELECT = "key_can_multiselect"
