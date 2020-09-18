ad0fe27f8909546fe0c22fd8be9f625fdf8feea3
Joel Den <jdeanjj1000@gmail.com>
2020-09-18 10:18:16 -0500

    test Java ViewModel.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/TesterViewModel.java
@@ -0,0 +1,11 @@
+package org.wordpress.android.ui.posts;
+
+import android.annotation.SuppressLint;
+import android.widget.TextView;
+
+import androidx.lifecycle.ViewModel;
+
+public class TesterViewModel extends ViewModel {
+    @SuppressLint("StaticFieldLeak") TextView mTextView;
+
+}
