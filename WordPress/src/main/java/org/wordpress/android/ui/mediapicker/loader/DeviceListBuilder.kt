623b7c10379c1ade2358f1a83d2b9c8103051c14
vojtasmrcek <planarvoid@gmail.com>
2020-10-02 10:42:20 +0200

    Add documentation

--- WordPress/src/main/java/org/wordpress/android/ui/mediapicker/loader/DeviceListBuilder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mediapicker/loader/DeviceListBuilder.kt
@@ -51,6 +51,14 @@ class DeviceListBuilder(
                 }
             }
             val results = deferredJobs.map { it.await() }
+            // This is necessary because of the following use case:
+            // - Page size is 5, we have 10 new images, 10 audio files that are all older than all the images
+            // - We load first 5 images and first five audio files
+            // - The 5 other images are newer than the currently loaded audio files
+            // - We shouldn't show any audio files until we show all the images
+            // The following solution goes through all the results and finds the last item in each list.
+            // From these items it picks the newest one (the last one we definitely want to show)
+            // This item sets the threshold for the visible items in all the list
             val lastShownTimestamp = results.fold(0L) { timestamp, (_, result) ->
                 val nextTimestamp = result?.nextTimestamp
                 if (nextTimestamp != null && nextTimestamp > timestamp) {
@@ -59,9 +67,10 @@ class DeviceListBuilder(
                     timestamp
                 }
             }
+            // Here we filter out all the items older than the selected last visible item
             results.forEach { (mediaType, result) ->
                 if (result != null) {
-                    val visibleItems = result.items.takeWhile { it.dataModified > lastShownTimestamp }
+                    val visibleItems = result.items.takeWhile { it.dataModified >= lastShownTimestamp }
                     cache[mediaType] = result.copy(visibleItems = visibleItems.size)
                     mediaItems.addAll(visibleItems)
                 }
@@ -123,6 +132,7 @@ class DeviceListBuilder(
 
     data class Result(val items: List<MediaItem>, val nextTimestamp: Long? = null, val visibleItems: Int = 0)
 
+    // We only want to show more data if there isn't already a page loaded that wasn't shown before
     private fun Result?.shouldLoadMoreData(): Boolean {
         return this == null || (nextTimestamp != null && this.items.size <= (visibleItems + pageSize))
     }
