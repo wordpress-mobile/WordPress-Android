4bfb9f095fe51cc991a1747b239a01d83ceca3b8
develric <develric@gmail.com>
2020-10-14 03:01:48 +0200

    Extracted classes to make them mockable.

---
+++ WordPress/src/main/java/org/wordpress/android/util/MimeTypeMapUtilsWrapper.kt
@@ -0,0 +1,12 @@
+package org.wordpress.android.util
+
+import android.net.Uri
+import android.webkit.MimeTypeMap
+import dagger.Reusable
+import javax.inject.Inject
+
+@Reusable
+class MimeTypeMapUtilsWrapper @Inject constructor() {
+    fun getFileExtensionFromUrl(url: String?) = MimeTypeMap.getFileExtensionFromUrl(url)
+    fun getSingleton() = MimeTypeMap.getSingleton()
+}
