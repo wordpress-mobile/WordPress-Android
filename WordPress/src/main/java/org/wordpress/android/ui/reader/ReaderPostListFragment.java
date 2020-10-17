bf03c7e9dcecfcbe36f3ea7b326c1e09a0c7a58f
ashiagr <agr.ash@gmail.com>
2020-10-17 16:46:37 +0530

    Remove settings menu from following tab filter bar

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderPostListFragment.java
@@ -172,7 +172,6 @@ public class ReaderPostListFragment extends ViewPagerFragment
     private MenuItem mSearchMenuItem;
 
     private View mSubFilterComponent;
-    private ImageView mSettingsButton;
     private View mSubFiltersListButton;
     private TextView mSubFilterTitle;
     private View mRemoveFilterButton;
@@ -605,10 +604,6 @@ public class ReaderPostListFragment extends ViewPagerFragment
     private void initSubFilterViews(ViewGroup rootView, LayoutInflater inflater) {
         mSubFilterComponent = inflater.inflate(R.layout.subfilter_component, rootView, false);
         ((ViewGroup) rootView.findViewById(R.id.sub_filter_component_container)).addView(mSubFilterComponent);
-        mSettingsButton = mSubFilterComponent.findViewById(R.id.filter_settings_button);
-        mSettingsButton.setOnClickListener(v -> {
-            showSettings();
-        });
 
         mSubFiltersListButton = mSubFilterComponent.findViewById(R.id.filter_selection);
         mSubFiltersListButton.setOnClickListener(v -> {
@@ -622,7 +617,6 @@ public class ReaderPostListFragment extends ViewPagerFragment
             mSubFilterViewModel.setDefaultSubfilter();
         });
         mSubFilterComponent.setVisibility(isFollowingScreen() ? View.VISIBLE : View.GONE);
-        mSettingsButton.setVisibility(isFollowingScreen() && mAccountStore.hasAccessToken() ? View.VISIBLE : View.GONE);
 
         ElevationOverlayProvider elevationOverlayProvider = new ElevationOverlayProvider(mRecyclerView.getContext());
         float cardElevation = getResources().getDimension(R.dimen.card_elevation);
@@ -1119,12 +1113,8 @@ public class ReaderPostListFragment extends ViewPagerFragment
         return rootView;
     }
 
-    private void showSettings() {
-        ReaderActivityLauncher.showReaderSubs(getActivity());
-    }
-
     /*
-     * adds a menu to the recycler's toolbar containing settings & search items - only called
+     * adds a menu to the recycler's toolbar containing search items - only called
      * for followed tags
      */
     private void setupRecyclerToolbar() {
