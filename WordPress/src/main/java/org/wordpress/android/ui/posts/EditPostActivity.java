423068ac7f19989a4cbd970ca9c1df4249021d71
Mario Zorz <mariozorz@gmail.com>
2020-10-20 08:27:37 -0300

    removed old commented dialog and related string resourced

--- WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/EditPostActivity.java
@@ -3253,15 +3253,6 @@ public class EditPostActivity extends LocaleAwareActivity implements
 
     @Override public void onStoryComposerLoadRequested(ArrayList<Object> mediaFiles, String blockId) {
         if (mediaFiles.isEmpty()) {
-            // TODO remove this code and related string resources after beta
-//            AlertDialog.Builder builder = new MaterialAlertDialogBuilder(this);
-//            builder.setTitle(getString(R.string.dialog_edit_story_no_new_blocks_title));
-//            builder.setMessage(getString(R.string.dialog_edit_story_no_new_blocks_message));
-//            builder.setPositiveButton(R.string.dialog_button_ok, (dialog, id) -> {
-//                dialog.dismiss();
-//            });
-//            AlertDialog dialog = builder.create();
-//            dialog.show();
             ActivityLauncher.editEmptyStoryForResult(
                     this,
                     mSite,
