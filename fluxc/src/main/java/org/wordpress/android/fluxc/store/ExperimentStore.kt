5f071f3272f119c183997f13dba5568bc3f8f850
Renan Ferrari <renan.fb@gmail.com>
2020-11-05 17:43:45 -0300

    Implement logic to fetch assignments in ExperimentStore

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/ExperimentStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/ExperimentStore.kt
@@ -4,7 +4,11 @@ import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.Payload
+import org.wordpress.android.fluxc.action.ExperimentAction
+import org.wordpress.android.fluxc.action.ExperimentAction.FETCH_ASSIGNMENTS
 import org.wordpress.android.fluxc.annotations.action.Action
+import org.wordpress.android.fluxc.network.rest.wpcom.experiments.ExperimentRestClient
+import org.wordpress.android.fluxc.tools.CoroutineEngine
 import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.AppLog.T.API
 import javax.inject.Inject
@@ -12,15 +16,32 @@ import javax.inject.Singleton
 
 @Singleton
 class ExperimentStore @Inject constructor(
+    private val experimentRestClient: ExperimentRestClient,
+    private val coroutineEngine: CoroutineEngine,
     dispatcher: Dispatcher
 ) : Store(dispatcher) {
     @Subscribe(threadMode = ThreadMode.ASYNC)
-    override fun onAction(action: Action<*>?) {
-        TODO("Not yet implemented")
+    override fun onAction(action: Action<*>) {
+        val actionType = action.type as? ExperimentAction ?: return
+        when (actionType) {
+            FETCH_ASSIGNMENTS -> coroutineEngine.launch(API, this, "FETCH_ASSIGNMENTS") {
+                emitChange(fetchAssignments(action.payload as FetchAssignmentsPayload))
+            }
+        }
     }
 
     override fun onRegister() {
         AppLog.d(API, "${this.javaClass.simpleName}: onRegister")
+    }
+
+    private suspend fun fetchAssignments(fetchPayload: FetchAssignmentsPayload): OnAssignmentsFetched {
+        val fetchedPayload = experimentRestClient.fetchAssignments(fetchPayload.platform, fetchPayload.anonId)
+        return if (!fetchedPayload.isError) {
+            // TODO: Persist locally
+            OnAssignmentsFetched(variations = fetchedPayload.variations)
+        } else {
+            OnAssignmentsFetched(error = fetchedPayload.error)
+        }
     }
 
     data class FetchAssignmentsPayload(
