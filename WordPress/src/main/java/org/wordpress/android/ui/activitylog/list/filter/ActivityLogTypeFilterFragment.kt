e6f96e7edc9053deee57bf2495d060935e67e0a7
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2020-11-10 16:25:28 +0200

    Add dagger to activity log type filter fragment

--- WordPress/src/main/java/org/wordpress/android/ui/activitylog/list/filter/ActivityLogTypeFilterFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/list/filter/ActivityLogTypeFilterFragment.kt
@@ -1,13 +1,20 @@
 package org.wordpress.android.ui.activitylog.list.filter
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import org.wordpress.android.R.layout
+import org.wordpress.android.WordPress
 
 class ActivityLogTypeFilterFragment : DialogFragment() {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().applicationContext as WordPress).component().inject(this)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(layout.activity_log_type_filter_fragment, container, false)
     }
