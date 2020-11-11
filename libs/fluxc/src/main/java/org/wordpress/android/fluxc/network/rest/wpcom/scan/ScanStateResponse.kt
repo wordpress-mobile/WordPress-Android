4032a926e098cefeabfb13780dca39561c67fd2f
ashiagr <agr.ash@gmail.com>
2020-11-11 17:07:25 +0530

    Introduce method to fetch scan state in ScanRestClient

---
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/scan/ScanStateResponse.kt
@@ -0,0 +1,49 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.scan
+
+import com.google.gson.annotations.SerializedName
+import org.wordpress.android.fluxc.network.Response
+
+data class ScanStateResponse(
+    val state: String,
+    val threats: List<Threat>?,
+    val credentials: List<Credentials>?,
+    @SerializedName("has_cloud") val hasCloud: Boolean,
+    @SerializedName("most_recent") val mostRecent: MostRecent?
+) : Response {
+    data class Threat(
+        val id: Int,
+        val signature: String,
+        val description: String,
+        val status: String,
+        val fixable: Fixable,
+        val extension: Extension,
+        @SerializedName("first_detected") val firstDetected: String
+    ) {
+        data class Fixable(
+            val fixer: String,
+            val target: String
+        )
+
+        data class Extension(
+            val type: String,
+            val slug: String,
+            val name: String,
+            val version: String,
+            val isPremium: Boolean
+        )
+    }
+
+    data class Credentials(
+        val type: String,
+        val role: String,
+        @SerializedName("still_valid") val stillValid: Boolean
+    )
+
+    data class MostRecent(
+        val timestamp: String,
+        val duration: Int,
+        val progress: Int,
+        val error: Boolean,
+        @SerializedName("is_initial") val isInitial: Boolean
+    )
+}
