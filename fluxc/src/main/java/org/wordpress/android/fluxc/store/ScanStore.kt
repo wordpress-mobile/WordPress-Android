09a1a226f8e3da99f8e4abc582a2af11f4117f8a
ashiagr <agr.ash@gmail.com>
2020-11-11 17:13:56 +0530

    Introduce FETCH_SCAN_STATE action

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/ScanStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/ScanStore.kt
@@ -1,9 +1,12 @@
 package org.wordpress.android.fluxc.store
 
+import android.annotation.SuppressLint
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.Payload
+import org.wordpress.android.fluxc.action.ScanAction
+import org.wordpress.android.fluxc.action.ScanAction.FETCH_SCAN_STATE
 import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.scan.ScanStateModel
@@ -24,10 +27,42 @@ class ScanStore @Inject constructor(
 ) : Store(dispatcher) {
     @Subscribe(threadMode = ThreadMode.ASYNC)
     override fun onAction(action: Action<*>) {
+        val actionType = action.type as? ScanAction ?: return
+        when (actionType) {
+            FETCH_SCAN_STATE -> {
+                coroutineEngine.launch(AppLog.T.API, this, "Scan: On FETCH_SCAN_STATE") {
+                    emitChange(fetchScanState(action.payload as FetchScanStatePayload))
+                }
+            }
+        }
     }
 
     override fun onRegister() {
         AppLog.d(AppLog.T.API, this.javaClass.name + ": onRegister")
+    }
+
+    @SuppressLint("WrongConstant")
+    suspend fun fetchScanState(fetchScanStatePayload: FetchScanStatePayload): OnScanStateFetched {
+        val payload = scanRestClient.fetchScanState(fetchScanStatePayload.site)
+        return storeScanState(payload)
+    }
+
+    private fun storeScanState(payload: FetchedScanStatePayload): OnScanStateFetched {
+        return if (payload.error != null) {
+            OnScanStateFetched(payload.error, FETCH_SCAN_STATE)
+        } else {
+            if (payload.scanStateModel != null) {
+                // TODO: ashiagr save data to DB
+            }
+            OnScanStateFetched(FETCH_SCAN_STATE)
+        }
+    }
+
+    // Actions
+    data class OnScanStateFetched(var causeOfChange: ScanAction) : Store.OnChanged<ScanStateError>() {
+        constructor(error: ScanStateError, causeOfChange: ScanAction) : this(causeOfChange = causeOfChange) {
+            this.error = error
+        }
     }
 
     // Payloads
