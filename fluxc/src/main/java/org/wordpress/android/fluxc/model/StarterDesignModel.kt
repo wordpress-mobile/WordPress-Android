70ecb03c1606795d52b56ce6d7fc69d38df1d5fb
Antonis Lilis <antonis.lilis@automattic.com>
2020-11-12 14:24:24 +0200

    Adds "segment_id" in FETCH_STARTER_DESIGNS return model (#1754)
    * Adds segment_id in starter design return model

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/StarterDesignModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/StarterDesignModel.kt
@@ -6,11 +6,12 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class StarterDesignModel(
-    @SerializedName("blog_id") val blogId: Int?,
+    @SerializedName("blog_id") val blogId: Long?,
     val slug: String?,
     val title: String?,
     @SerializedName("site_url") val siteUrl: String?,
     @SerializedName("demo_url") val demoUrl: String?,
     val theme: String?,
+    @SerializedName("segment_id") val segmentId: Long?,
     val screenshot: String?
 ) : Parcelable
