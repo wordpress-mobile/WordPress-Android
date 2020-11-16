a0634e58307eea02d7434b9a4806037cbe53c78b
Antonis Lilis <antonis.lilis@automattic.com>
2020-11-16 08:02:11 +0200

    Fixes lint issue

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/theme/HomePagePickerFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/theme/HomePagePickerFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
@@ -96,7 +95,7 @@ class HomePagePickerFragment : Fragment() {
         })
 
         viewModel.onPreviewButtonPressed.observe(viewLifecycleOwner, { preview ->
-            WPWebViewActivity.openURL(requireContext(), preview.demoUrl)// TODO
+            WPWebViewActivity.openURL(requireContext(), preview.demoUrl) // TODO
         })
 
         viewModel.start()
