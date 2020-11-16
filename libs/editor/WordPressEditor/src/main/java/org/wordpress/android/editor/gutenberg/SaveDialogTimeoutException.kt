b48a8260cdf01b4b6de37a3ffda1e34abcc0aa8d
Matt Chowning <matt.chowning@automattic.com>
2020-11-15 21:56:36 -0500

    Fix lint error

--- libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/gutenberg/SaveDialogTimeoutException.kt
+++ libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/gutenberg/SaveDialogTimeoutException.kt
@@ -2,7 +2,7 @@ package org.wordpress.android.editor.gutenberg
 
 class SaveDialogTimeoutException(timeoutDuration: Int) : Exception(timeoutMessage(timeoutDuration)) {
     companion object {
-        private fun timeoutMessage(timeoutDuration: Int) : String =
+        private fun timeoutMessage(timeoutDuration: Int): String =
                 "The Gutenberg editor's save dialog timeout fired after $timeoutDuration " +
                         "and automatically dismissed the dialog to ensure the user was not " +
                         "left with a blocking dialog. This indicates either that the local save " +
