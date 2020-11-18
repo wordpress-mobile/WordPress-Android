cde8160a27dc4643f4fbd28adacc8b974803352f
Antonis Lilis <antonis.lilis@automattic.com>
2020-11-18 23:21:36 +0200

    Revert "Fixes tests"
    This reverts commit eee29b8312d7e2b9effa38b926f25b4f3de8d6f6.

--- WordPress/src/test/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainsViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainsViewModelTest.kt
@@ -124,7 +124,7 @@ class SiteCreationDomainsViewModelTest {
         viewModel.start(SEGMENT_ID)
         viewModel.updateQuery(MULTI_RESULT_DOMAIN_FETCH_QUERY.first)
         val captor = ArgumentCaptor.forClass(DomainsUiState::class.java)
-        verify(uiStateObserver, times(2)).onChanged(captor.capture())
+        verify(uiStateObserver, times(3)).onChanged(captor.capture())
         verifyInitialContentUiState(captor.secondValue, showProgress = true, showClearButton = true)
     }
 
@@ -153,8 +153,8 @@ class SiteCreationDomainsViewModelTest {
                 viewModel.start(SEGMENT_ID)
                 viewModel.updateQuery(MULTI_RESULT_DOMAIN_FETCH_QUERY.first)
                 val captor = ArgumentCaptor.forClass(DomainsUiState::class.java)
-                verify(uiStateObserver, times(2)).onChanged(captor.capture())
-                verifyVisibleItemsContentUiState(captor.secondValue, showClearButton = true)
+                verify(uiStateObserver, times(3)).onChanged(captor.capture())
+                verifyVisibleItemsContentUiState(captor.thirdValue, showClearButton = true)
             }
 
     /**
@@ -167,8 +167,10 @@ class SiteCreationDomainsViewModelTest {
                 viewModel.start(SEGMENT_ID)
                 viewModel.updateQuery(MULTI_RESULT_DOMAIN_FETCH_QUERY.first)
                 val captor = ArgumentCaptor.forClass(DomainsUiState::class.java)
-                verify(uiStateObserver, times(2)).onChanged(captor.capture())
-                verifyContentAndDomainValidityUiStatesAreVisible(captor.secondValue)
+                verify(uiStateObserver, times(3)).onChanged(captor.capture())
+                verifyContentAndDomainValidityUiStatesAreVisible(
+                        captor.thirdValue
+                )
             }
 
     /**
@@ -201,7 +203,7 @@ class SiteCreationDomainsViewModelTest {
         viewModel.updateQuery(MULTI_RESULT_DOMAIN_FETCH_QUERY.first)
         viewModel.updateQuery("")
         val captor = ArgumentCaptor.forClass(DomainsUiState::class.java)
-        verify(uiStateObserver, times(3)).onChanged(captor.capture())
+        verify(uiStateObserver, times(4)).onChanged(captor.capture())
         verifyInitialContentUiState(captor.lastValue)
     }
 
