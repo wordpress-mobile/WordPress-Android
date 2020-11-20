a793bfa13b4a08fe8f98b96cf919b0ba6c66ac17
malinajirka <malinajirka@gmail.com>
2020-11-20 12:12:06 +0100

    Fix tests

--- WordPress/src/test/java/org/wordpress/android/ui/activitylog/list/filter/ActivityLogTypeFilterViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/activitylog/list/filter/ActivityLogTypeFilterViewModelTest.kt
@@ -1,49 +1,57 @@
 package org.wordpress.android.ui.activitylog.list.filter
 
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.InternalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
 import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.test
 import org.wordpress.android.ui.activitylog.list.filter.ActivityLogTypeFilterViewModel.ListItemUiState
 import org.wordpress.android.ui.activitylog.list.filter.ActivityLogTypeFilterViewModel.UiState
 import org.wordpress.android.ui.activitylog.list.filter.ActivityLogTypeFilterViewModel.UiState.Content
+import org.wordpress.android.ui.activitylog.list.filter.DummyActivityTypesProvider.DummyActivityType
+import org.wordpress.android.ui.activitylog.list.filter.DummyActivityTypesProvider.DummyAvailableActivityTypesResponse
 
 @InternalCoroutinesApi
 class ActivityLogTypeFilterViewModelTest : BaseUnitTest() {
     private lateinit var viewModel: ActivityLogTypeFilterViewModel
+    @Mock private lateinit var dummyActivityTypesProvider: DummyActivityTypesProvider
 
     @Before
     fun setUp() {
-        viewModel = ActivityLogTypeFilterViewModel(TEST_DISPATCHER, TEST_DISPATCHER)
+        viewModel = ActivityLogTypeFilterViewModel(dummyActivityTypesProvider, TEST_DISPATCHER, TEST_DISPATCHER)
     }
 
     @Test
     fun `fullscreen loading shown, when screen initialized`() = test {
-        val uiStates = initObservers().uiStates
+        val uiStates = init().uiStates
 
-        viewModel.start()
+        viewModel.start(0L)
 
         assertThat(uiStates[0]).isInstanceOf(UiState.FullscreenLoading::class.java)
     }
 
     @Test
-    fun `section header gets added as first item in the list`() {
-        val uiStates = initObservers().uiStates
+    fun `section header gets added as first item in the list`() = test {
+        val uiStates = init().uiStates
 
-        viewModel.start()
+        viewModel.start(0L)
 
         assertThat((viewModel.uiState.value as Content).items[0])
                 .isInstanceOf(ListItemUiState.SectionHeader::class.java)
     }
 
-    private fun initObservers(): Observers {
+    private suspend fun init(): Observers {
         val uiStates = mutableListOf<UiState>()
         viewModel.uiState.observeForever {
             uiStates.add(it)
         }
+        whenever(dummyActivityTypesProvider.fetchAvailableActivityTypes(anyOrNull()))
+                .thenReturn(DummyAvailableActivityTypesResponse(false, listOf(DummyActivityType("Test 1"))))
         return Observers((uiStates))
     }
 
