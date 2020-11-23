2b878e48ab8b76a5e40e13b1bd7dabd31e19d77a
ashiagr <agr.ash@gmail.com>
2020-11-23 12:00:19 +0530

    Add test to check appropriate rest client method is called for FETCH_SCAN_STATE action

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/ScanAction.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/action/ScanAction.kt
@@ -3,9 +3,10 @@ package org.wordpress.android.fluxc.action
 import org.wordpress.android.fluxc.annotations.Action
 import org.wordpress.android.fluxc.annotations.ActionEnum
 import org.wordpress.android.fluxc.annotations.action.IAction
+import org.wordpress.android.fluxc.store.ScanStore
 
 @ActionEnum
 enum class ScanAction : IAction {
-    @Action
-    FETCH_SCAN_STATE,
+    @Action(payloadType = ScanStore.FetchScanStatePayload::class)
+    FETCH_SCAN_STATE
 }
