b3da3ae19c97cc0af251ce518158c60421e98721
vojtasmrcek <planarvoid@gmail.com>
2020-11-26 10:17:40 +0100

    Implement title changes in site info block

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteViewModel.kt
@@ -143,12 +143,18 @@ class MySiteViewModel
         _onNavigation.value = Event(OpenSitePicker(site))
     }
 
-    fun onSiteNameChosen(input: String?) {
-        TODO("Not yet implemented")
+    fun onSiteNameChosen(input: String) {
+        if (!networkUtilsWrapper.isNetworkAvailable()) {
+            _onSnackbarMessage.postValue(
+                    Event(SnackbarMessageHolder(UiStringRes(R.string.error_update_site_title_network)))
+            )
+        } else {
+            selectedSiteRepository.updateTitle(input)
+        }
     }
 
     fun onSiteNameChooserDismissed() {
-        TODO("Not yet implemented")
+        // do nothing
     }
 
     fun onDialogInteraction(interaction: DialogInteraction) {
