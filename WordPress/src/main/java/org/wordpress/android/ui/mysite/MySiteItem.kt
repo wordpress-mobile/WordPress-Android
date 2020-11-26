a2517686572567f4c35e8abd8639940f55acee5d
Renan Ferrari <renan.fb@gmail.com>
2020-11-26 16:13:45 -0300

    Add quick actions block

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteItem.kt
@@ -1,11 +1,15 @@
 package org.wordpress.android.ui.mysite
 
+import org.wordpress.android.ui.mysite.MySiteItem.Type.QUICK_ACTIONS_BLOCK
 import org.wordpress.android.ui.mysite.MySiteItem.Type.SITE_INFO_BLOCK
 import org.wordpress.android.ui.utils.ListItemInteraction
 
 sealed class MySiteItem(val type: Type) {
     enum class Type {
-        SITE_INFO_BLOCK, HEADER, LIST_ITEM
+        SITE_INFO_BLOCK,
+        QUICK_ACTIONS_BLOCK,
+        HEADER,
+        LIST_ITEM
     }
 
     data class SiteInfoBlock(
@@ -22,4 +26,12 @@ sealed class MySiteItem(val type: Type) {
             data class Visible(val url: String? = null) : IconState()
         }
     }
+
+    data class QuickActionsBlock(
+        val onStatsClick: ListItemInteraction,
+        val onPagesClick: ListItemInteraction,
+        val onPostsClick: ListItemInteraction,
+        val onMediaClick: ListItemInteraction,
+        val showPages: Boolean = true,
+    ) : MySiteItem(QUICK_ACTIONS_BLOCK)
 }
