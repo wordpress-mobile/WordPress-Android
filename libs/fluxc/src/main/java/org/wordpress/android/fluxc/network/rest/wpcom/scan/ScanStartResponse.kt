4874c57d8a60b167bb78664507199f7b3d4e413d
ashiagr <agr.ash@gmail.com>
2020-11-27 12:00:28 +0530

    Add SerializedName annotation

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/scan/ScanStartResponse.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/scan/ScanStartResponse.kt
@@ -1,15 +1,17 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.scan
 
+import com.google.gson.annotations.SerializedName
+
 data class ScanStartResponse(
-    val error: Error? = null,
-    val success: Boolean
+    @SerializedName("error") val error: Error? = null,
+    @SerializedName("success") val success: Boolean?
 ) {
     data class Error(
-        val error_data: List<Any>,
-        val errors: Errors
+        @SerializedName("error_data") val errorData: List<Any>?,
+        @SerializedName("errors") val errors: Errors?
     )
 
     data class Errors(
-        val vp_api_error: List<String>
+        @SerializedName("vp_api_error") val vpApiError: List<String>?
     )
 }
