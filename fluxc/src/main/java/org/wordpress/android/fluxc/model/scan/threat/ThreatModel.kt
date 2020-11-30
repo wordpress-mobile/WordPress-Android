3ca16922cca158264acf95a3eb4b74d8b1ca8fcb
ashiagr <agr.ash@gmail.com>
2020-11-30 13:01:00 +0530

    Introduce new fields: context, fileName, rows, diff, Fixable.file. Also convert all Threat fields to nullable.

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/scan/threat/ThreatModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/scan/threat/ThreatModel.kt
@@ -1,24 +1,40 @@
 package org.wordpress.android.fluxc.model.scan.threat
 
+import com.google.gson.JsonArray
 import java.util.Date
 
 data class ThreatModel(
-    val id: Int,
+    val id: Long,
     val signature: String,
     val description: String,
-    val status: String?,
-    val fixable: Fixable?,
-    val extension: Extension?,
+    val status: String?, // TODO: ashiagr - possible values: fixed / ignored / current
+    val fixable: Fixable? = null,
+    val extension: Extension? = null,
     val firstDetected: Date,
-    var fixedOn: Date?
+    val fixedOn: Date? = null,
+    var context: ThreatContext? = null,
+    val fileName: String? = null,
+    val diff: String? = null,
+    val rows: String? = null
 ) {
+    data class ThreatContext(
+        val lines: List<ContextLine>
+    ) {
+        data class ContextLine(
+            val lineNumber: Int,
+            val contents: String,
+            val highlights: JsonArray?
+        )
+    }
+
     data class Fixable(
-        val fixer: String?,
+        val file: String?,
+        val fixer: String?, // TODO: ashiagr - possible values: replace, delete, update, edit
         val target: String?
     )
 
     data class Extension(
-        val type: String?,
+        val type: String?, // TODO: ashiagr - possible values: plugin, theme
         val slug: String?,
         val name: String?,
         val version: String?,
--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/scan/threat/Threat.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/scan/threat/Threat.kt
@@ -1,19 +1,25 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.scan.threat
 
 import com.google.gson.annotations.SerializedName
+import org.wordpress.android.fluxc.model.scan.threat.ThreatModel.ThreatContext
 import java.util.Date
 
 data class Threat(
-    @SerializedName("id") val id: Int,
-    @SerializedName("signature") val signature: String,
-    @SerializedName("description") val description: String,
-    @SerializedName("status") val status: String,
+    @SerializedName("id") val id: Long?,
+    @SerializedName("signature") val signature: String?,
+    @SerializedName("description") val description: String?,
+    @SerializedName("status") val status: String?,
     @SerializedName("fixable") val fixable: Fixable?,
     @SerializedName("extension") val extension: Extension?,
-    @SerializedName("first_detected") val firstDetected: Date,
-    @SerializedName("fixed_on") val fixedOn: Date?
+    @SerializedName("first_detected") val firstDetected: Date?,
+    @SerializedName("fixed_on") val fixedOn: Date?,
+    @SerializedName("context") var context: ThreatContext?,
+    @SerializedName("filename") val fileName: String?,
+    @SerializedName("diff") val diff: String?,
+    @SerializedName("rows") val rows: String?
 ) {
     data class Fixable(
+        @SerializedName("file") val file: String?,
         @SerializedName("fixer") val fixer: String?,
         @SerializedName("target") val target: String?
     )
