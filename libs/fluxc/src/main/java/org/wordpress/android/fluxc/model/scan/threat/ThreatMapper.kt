5fbac57312ad1ab67f0961d29cc9046d1409e128
ashiagr <agr.ash@gmail.com>
2020-11-30 10:35:16 +0530

    Introduce ThreatMapper
    Separate threat mapper will ease mapping threats based on different types (database, core, file etc) later.

---
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/scan/threat/ThreatMapper.kt
@@ -0,0 +1,31 @@
+package org.wordpress.android.fluxc.model.scan.threat
+
+import org.wordpress.android.fluxc.model.scan.ThreatModel
+import org.wordpress.android.fluxc.model.scan.ThreatModel.Extension
+import org.wordpress.android.fluxc.model.scan.ThreatModel.Fixable
+import org.wordpress.android.fluxc.network.rest.wpcom.scan.threat.Threat
+import javax.inject.Inject
+
+class ThreatMapper @Inject constructor() {
+    fun map(response: Threat): ThreatModel {
+        return ThreatModel(
+            id = response.id,
+            signature = response.signature,
+            description = response.description,
+            status = response.status,
+            fixable = Fixable(
+                fixer = response.fixable?.fixer,
+                target = response.fixable?.target
+            ),
+            extension = Extension(
+                type = response.extension?.type,
+                slug = response.extension?.slug,
+                name = response.extension?.name,
+                version = response.extension?.version,
+                isPremium = response.extension?.isPremium ?: false
+            ),
+            firstDetected = response.firstDetected,
+            fixedOn = response.fixedOn
+        )
+    }
+}
