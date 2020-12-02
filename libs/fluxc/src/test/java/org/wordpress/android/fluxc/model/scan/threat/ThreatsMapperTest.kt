ffb6f2dfd23bf3c4c1f8c5f9ef0a1f26ee3bd871
ashiagr <agr.ash@gmail.com>
2020-12-02 10:44:16 +0530

    Fix package directive

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/model/scan/threat/ThreatsMapperTest.kt
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/model/scan/threat/ThreatsMapperTest.kt
@@ -1,4 +1,4 @@
-package org.wordpress.android.fluxc.scan.threat
+package org.wordpress.android.fluxc.model.scan.threat
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -7,7 +7,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.fluxc.UnitTestUtils
-import org.wordpress.android.fluxc.model.scan.threat.ThreatMapper
 import org.wordpress.android.fluxc.model.scan.threat.ThreatModel.CoreFileModificationThreatModel
 import org.wordpress.android.fluxc.model.scan.threat.ThreatModel.DatabaseThreatModel
 import org.wordpress.android.fluxc.model.scan.threat.ThreatModel.Extension.ExtensionType
