4e3cc6d019e342dbe8c6bb57fa673b11cf913421
vojtasmrcek <planarvoid@gmail.com>
2020-12-02 14:56:08 +0100

    Simplify response building in SiteItemsBuilder.kt

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/SiteItemsBuilder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/SiteItemsBuilder.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.ui.mysite
 
-import org.wordpress.android.R
+import org.wordpress.android.R.drawable
+import org.wordpress.android.R.string
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.mysite.MySiteItem.CategoryHeader
 import org.wordpress.android.ui.mysite.MySiteItem.ListItem
@@ -14,65 +15,49 @@ class SiteItemsBuilder
     private val siteListItemBuilder: SiteListItemBuilder
 ) {
     fun buildSiteItems(site: SiteModel, onClick: ListItemInteraction): List<MySiteItem> {
-        val siteItems = mutableListOf<MySiteItem>()
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildPlanItemIfAvailable(site, onClick))
-        siteItems.addItemIfNotNull(siteCategoryItemBuilder.buildJetpackCategoryIfAvailable(site))
-        siteItems.add(
+        return listOfNotNull(
+                siteListItemBuilder.buildPlanItemIfAvailable(site, onClick),
+                siteCategoryItemBuilder.buildJetpackCategoryIfAvailable(site),
                 ListItem(
-                        R.drawable.ic_stats_alt_white_24dp,
-                        UiStringRes(R.string.stats),
+                        drawable.ic_stats_alt_white_24dp,
+                        UiStringRes(string.stats),
                         onClick = onClick
-                )
-        )
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildActivityLogItemIfAvailable(site, onClick))
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildScanItemIfAvailable(onClick))
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildJetpackItemIfAvailable(site, onClick))
-        siteItems.add(CategoryHeader(UiStringRes(R.string.my_site_header_publish)))
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildPagesItemIfAvailable(site, onClick))
-        siteItems.add(
+                ),
+                siteListItemBuilder.buildActivityLogItemIfAvailable(site, onClick),
+                siteListItemBuilder.buildScanItemIfAvailable(onClick),
+                siteListItemBuilder.buildJetpackItemIfAvailable(site, onClick),
+                CategoryHeader(UiStringRes(string.my_site_header_publish)),
+                siteListItemBuilder.buildPagesItemIfAvailable(site, onClick),
                 ListItem(
-                        R.drawable.ic_posts_white_24dp,
-                        UiStringRes(R.string.my_site_btn_blog_posts),
+                        drawable.ic_posts_white_24dp,
+                        UiStringRes(string.my_site_btn_blog_posts),
                         onClick = onClick
-                )
-        )
-        siteItems.add(
+                ),
                 ListItem(
-                        R.drawable.ic_media_white_24dp,
-                        UiStringRes(R.string.media),
+                        drawable.ic_media_white_24dp,
+                        UiStringRes(string.media),
                         onClick = onClick
-                )
-        )
-        siteItems.add(
+                ),
                 ListItem(
-                        R.drawable.ic_comment_white_24dp,
-                        UiStringRes(R.string.my_site_btn_comments),
+                        drawable.ic_comment_white_24dp,
+                        UiStringRes(string.my_site_btn_comments),
                         onClick = onClick
-                )
-        )
-        siteItems.addItemIfNotNull(siteCategoryItemBuilder.buildLookAndFeelHeaderIfAvailable(site))
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildThemesItemIfAvailable(site, onClick))
-        siteItems.addItemIfNotNull(siteCategoryItemBuilder.buildConfigurationHeaderIfAvailable(site))
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildPeopleItemIfAvailable(site, onClick))
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildPluginItemIfAvailable(site, onClick))
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildShareItemIfAvailable(site, onClick))
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildSiteSettingsItemIfAvailable(site, onClick))
-        siteItems.add(CategoryHeader(UiStringRes(R.string.my_site_header_external)))
-        siteItems.add(
+                ),
+                siteCategoryItemBuilder.buildLookAndFeelHeaderIfAvailable(site),
+                siteListItemBuilder.buildThemesItemIfAvailable(site, onClick),
+                siteCategoryItemBuilder.buildConfigurationHeaderIfAvailable(site),
+                siteListItemBuilder.buildPeopleItemIfAvailable(site, onClick),
+                siteListItemBuilder.buildPluginItemIfAvailable(site, onClick),
+                siteListItemBuilder.buildShareItemIfAvailable(site, onClick),
+                siteListItemBuilder.buildSiteSettingsItemIfAvailable(site, onClick),
+                CategoryHeader(UiStringRes(string.my_site_header_external)),
                 ListItem(
-                        R.drawable.ic_globe_white_24dp,
-                        UiStringRes(R.string.my_site_btn_view_site),
-                        secondaryIcon = R.drawable.ic_external_white_24dp,
+                        drawable.ic_globe_white_24dp,
+                        UiStringRes(string.my_site_btn_view_site),
+                        secondaryIcon = drawable.ic_external_white_24dp,
                         onClick = onClick
-                )
+                ),
+                siteListItemBuilder.buildAdminItemIfAvailable(site, onClick),
         )
-        siteItems.addItemIfNotNull(siteListItemBuilder.buildAdminItemIfAvailable(site, onClick))
-        return siteItems
-    }
-
-    private fun MutableList<MySiteItem>.addItemIfNotNull(item: MySiteItem?) {
-        if (item != null) {
-            this.add(item)
-        }
     }
 }
