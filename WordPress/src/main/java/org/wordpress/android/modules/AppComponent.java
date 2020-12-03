7e5423c98fb26d6f84bcabcff1105cce94d17dbd
ashiagr <agr.ash@gmail.com>
2020-12-03 11:29:38 +0530

    Introduce skeleton ScanActivity & ScanFragment

--- WordPress/src/main/java/org/wordpress/android/modules/AppComponent.java
+++ WordPress/src/main/java/org/wordpress/android/modules/AppComponent.java
@@ -164,6 +164,8 @@ import org.wordpress.android.ui.reader.views.ReaderPostDetailHeaderView;
 import org.wordpress.android.ui.reader.views.ReaderSiteHeaderView;
 import org.wordpress.android.ui.reader.views.ReaderTagHeaderView;
 import org.wordpress.android.ui.reader.views.ReaderWebView;
+import org.wordpress.android.ui.scan.ScanActivity;
+import org.wordpress.android.ui.scan.ScanFragment;
 import org.wordpress.android.ui.sitecreation.SiteCreationActivity;
 import org.wordpress.android.ui.sitecreation.domains.SiteCreationDomainsFragment;
 import org.wordpress.android.ui.sitecreation.previews.SiteCreationPreviewFragment;
@@ -459,6 +461,10 @@ public interface AppComponent extends AndroidInjector<WordPress> {
     void inject(ActivityLogListFragment object);
 
     void inject(ActivityLogDetailFragment object);
+
+    void inject(ScanActivity object);
+
+    void inject(ScanFragment object);
 
     void inject(PluginListFragment object);
 
