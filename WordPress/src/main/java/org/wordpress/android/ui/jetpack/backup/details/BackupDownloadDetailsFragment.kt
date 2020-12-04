f029b32e6baf3e34739a48413728d0751aa7b28a
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2020-12-04 08:32:14 -0500

    Add base uiState, hook up adapter

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/backup/details/BackupDownloadDetailsFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/backup/details/BackupDownloadDetailsFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.backup_download_details_fragment.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.ui.jetpack.backup.BackupDownloadViewModel
+import org.wordpress.android.ui.jetpack.backup.details.BackupDownloadDetailsViewModel.UiState.Content
+import org.wordpress.android.ui.jetpack.backup.details.BackupDownloadDetailsViewModel.UiState.Loading
+import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.util.ToastUtils
 import javax.inject.Inject
 
 class BackupDownloadDetailsFragment : Fragment() {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject lateinit var uiHelpers: UiHelpers
     private lateinit var parentViewModel: BackupDownloadViewModel
     private lateinit var viewModel: BackupDownloadDetailsViewModel
     override fun onCreateView(
@@ -30,24 +39,39 @@ class BackupDownloadDetailsFragment : Fragment() {
         val nonNullActivity = requireActivity()
         (nonNullActivity.application as WordPress).component().inject(this)
 
+        initRecyclerView()
+        initViewModel()
+    }
+
+    private fun initRecyclerView() {
+        recycler_view.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        recycler_view.adapter = BackupDownloadDetailsAdapter(uiHelpers)
+    }
+
+    private fun initViewModel() {
         parentViewModel = ViewModelProviders.of(requireActivity(), viewModelFactory)
                 .get(BackupDownloadViewModel::class.java)
 
         viewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(BackupDownloadDetailsViewModel::class.java)
 
-        setupViews()
-        setupObservers()
-        // todo: annmarie - if something needs to be passed to VM, do it on start
+        viewModel.uiState.observe(viewLifecycleOwner, Observer { uiState ->
+            when (uiState) {
+                is Loading -> ToastUtils.showToast(requireContext(), "Implement loading")
+                is Content -> showContent(uiState)
+                is Error -> ToastUtils.showToast(requireContext(), "Implement Error")
+            }
+        })
+
         viewModel.start()
     }
 
-    private fun setupViews() {
-        // TODO: annmarie implement setupViews
-    }
-
-    private fun setupObservers() {
-        // TODO: annmarie implement setupObservers
+    private fun showContent(content: Content) {
+        ((recycler_view.adapter) as BackupDownloadDetailsAdapter).update(content.items)
     }
 
     companion object {
