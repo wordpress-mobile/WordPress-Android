8eb8344cc5d2399eab5ac0b2a1ecedfc7bfe6745
Antonis Lilis <antonis.lilis@automattic.com>
2020-12-04 12:05:10 +0200

    Lint issue: unused imports

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostListMainViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostListMainViewModel.kt
@@ -34,7 +34,6 @@ import org.wordpress.android.fluxc.store.UploadStore
 import org.wordpress.android.modules.BG_THREAD
 import org.wordpress.android.modules.UI_THREAD
 import org.wordpress.android.ui.pages.SnackbarMessageHolder
-import org.wordpress.android.ui.posts.EditPostRepository.UpdatePostResult
 import org.wordpress.android.ui.posts.PostListType.DRAFTS
 import org.wordpress.android.ui.posts.PostListType.PUBLISHED
 import org.wordpress.android.ui.posts.PostListType.SCHEDULED
@@ -48,7 +47,6 @@ import org.wordpress.android.ui.uploads.UploadActionUseCase
 import org.wordpress.android.ui.uploads.UploadStarter
 import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.util.AppLog
-import org.wordpress.android.util.DateTimeUtils
 import org.wordpress.android.util.NetworkUtilsWrapper
 import org.wordpress.android.util.SiteUtils
 import org.wordpress.android.util.ToastUtils.Duration
