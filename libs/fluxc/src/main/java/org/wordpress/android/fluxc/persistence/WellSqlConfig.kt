4d74354f93e2ef7285d9bde2a18408dbc15afabc
anitaa <murthyanitaa@gmail.com>
2020-12-05 16:05:03 +0530

    Added product_ids field to WCShippingLabelModel and updated migration script

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
@@ -28,7 +28,7 @@ open class WellSqlConfig : DefaultWellConfig {
     annotation class AddOn
 
     override fun getDbVersion(): Int {
-        return 125
+        return 127
     }
 
     override fun getDbName(): String {
@@ -1354,6 +1354,9 @@ open class WellSqlConfig : DefaultWellConfig {
                 }
                 125 -> migrateAddOn(ADDON_WOOCOMMERCE, version) {
                     db.execSQL("ALTER TABLE WCOrderModel ADD FEE_LINES TEXT")
+                }
+                126 -> migrateAddOn(ADDON_WOOCOMMERCE, version) {
+                    db.execSQL("ALTER TABLE WCShippingLabelModel ADD PRODUCT_IDS TEXT")
                 }
             }
         }
