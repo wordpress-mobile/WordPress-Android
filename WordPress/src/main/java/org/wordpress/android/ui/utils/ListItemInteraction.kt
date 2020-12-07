bcf6230c94c955d23fc7369d06a3852ecd3b08f1
vojtasmrcek <planarvoid@gmail.com>
2020-12-07 11:17:49 +0100

    Use function pointers in ListItemInteraction calls

--- WordPress/src/main/java/org/wordpress/android/ui/utils/ListItemInteraction.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/utils/ListItemInteraction.kt
@@ -4,10 +4,19 @@ interface ListItemInteraction {
     fun click()
 
     companion object {
+        /**
+         * Use this creator only with function pointer. If you use it with a lambda, the created `ListItemInteraction`
+         * equals method will always fail because two lambdas are never equal. If you need a parametrized function call,
+         * use the other creator method.
+         */
         fun create(action: () -> Unit): ListItemInteraction {
             return NoParams(action)
         }
 
+        /**
+         * Use this creator only with function pointer. If you use it with a lambda, the created `ListItemInteraction`
+         * equals method will always fail because two lambdas are never equal.
+         */
         fun <T> create(data: T, action: (T) -> Unit): ListItemInteraction {
             return OneParam(data, action)
         }
