d6ef5b78382eba854fb533988aeeb62734a3c4af
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2020-12-08 15:48:40 -0500

    Add popup adapter and render

--- WordPress/src/main/java/org/wordpress/android/ui/activitylog/list/ActivityLogListItemMenuAdapter.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/activitylog/list/ActivityLogListItemMenuAdapter.kt
@@ -8,82 +8,71 @@ import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
-import org.wordpress.android.R.id
-import org.wordpress.android.R.layout
+import org.wordpress.android.R
 import org.wordpress.android.ui.activitylog.list.ActivityLogListItem.SecondaryAction
-import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.ui.activitylog.list.ActivityLogListItem.SecondaryAction.DOWNLOAD_BACKUP
+import org.wordpress.android.ui.activitylog.list.ActivityLogListItem.SecondaryAction.RESTORE
 import org.wordpress.android.util.ColorUtils.setImageResourceWithTint
 import org.wordpress.android.util.getColorResIdFromAttribute
-import java.util.ArrayList
 
 class ActivityLogListItemMenuAdapter(
-    context: Context?,
-    uiHelpers: UiHelpers,
-    menuItems: List<SecondaryAction?>
+    context: Context
 ) : BaseAdapter() {
     private val inflater: LayoutInflater = LayoutInflater.from(context)
-    private val menuItems: MutableList<SecondaryAction> = ArrayList()
-    private val uiHelpers: UiHelpers
+    private val items: List<SecondaryAction> = SecondaryAction.values().toList()
     override fun getCount(): Int {
-        return menuItems.size
+        return items.size
     }
 
     override fun getItem(position: Int): Any {
-        return menuItems[position]
+        return items[position]
     }
 
     override fun getItemId(position: Int): Long {
-        return menuItems[position].type.ordinal.toLong()
+        return items[position].itemId
     }
 
-    override fun getView(
-        position: Int,
-        convertView: View,
-        parent: ViewGroup
-    ): View {
-        var convertView = convertView
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
         val holder: ActivityListItemMenuHolder
-        if (convertView == null) {
-            convertView = inflater.inflate(layout.activity_log_list_item_menu_item, parent, false)
-            holder = ActivityListItemMenuHolder(convertView)
-            convertView.tag = holder
+        if (view == null) {
+            view = inflater.inflate(R.layout.activity_log_list_item_menu_item, parent, false)
+            holder = ActivityListItemMenuHolder(view!!)
+            view.tag = holder
         } else {
-            holder = convertView.tag as ActivityListItemMenuHolder
+            holder = view.tag as ActivityListItemMenuHolder
         }
-        val (label, labelColor, iconRes, iconColor) = menuItems[position]
-        val textRes = uiHelpers.getTextOfUiString(
-                convertView.context,
-                label
-        )
-        val textColorRes = convertView.context.getColorResIdFromAttribute(labelColor)
-        val iconColorRes = convertView.context.getColorResIdFromAttribute(iconColor)
-        holder.mText.text = textRes
-        holder.mText.setTextColor(
+
+        val textRes: Int
+        val iconRes: Int
+        val colorRes = view.context.getColorResIdFromAttribute(R.attr.wpColorOnSurfaceMedium)
+        when (items[position]) {
+            RESTORE -> {
+                textRes = R.string.activity_log_item_menu_restore_label
+                iconRes = R.drawable.ic_history_white_24dp
+            }
+            DOWNLOAD_BACKUP -> {
+                textRes = R.string.activity_log_item_menu_download_backup_label
+                iconRes = R.drawable.ic_download_white_24dp
+            }
+        }
+        holder.text.setText(textRes)
+        holder.text.setTextColor(
                 AppCompatResources.getColorStateList(
-                        convertView.context,
-                        textColorRes
+                        view.context,
+                        colorRes
                 )
         )
         setImageResourceWithTint(
                 holder.icon,
                 iconRes,
-                iconColorRes
+                colorRes
         )
-        return convertView
+        return view
     }
 
     internal inner class ActivityListItemMenuHolder(view: View) {
-        private val label: TextView
-        private val icon: ImageView
-
-        init {
-            label = view.findViewById(id.text)
-            icon = view.findViewById(id.image)
-        }
-    }
-
-    init {
-        this.menuItems.addAll(menuItems)
-        this.uiHelpers = uiHelpers
+        val text: TextView = view.findViewById(R.id.text)
+        val icon: ImageView = view.findViewById(R.id.image)
     }
 }
