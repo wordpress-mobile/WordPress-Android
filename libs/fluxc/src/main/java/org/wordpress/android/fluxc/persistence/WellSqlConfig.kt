6342d2da373327bc6ac3ec4310f1fe56d133506d
Ondrej Ruttkay <ondrej@automattic.com>
2020-12-08 17:48:56 +0100

    Add the missing comma

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
@@ -1362,7 +1362,7 @@ open class WellSqlConfig : DefaultWellConfig {
                     db.execSQL("DROP TABLE IF EXISTS WCLocations")
                     db.execSQL("CREATE TABLE WCLocations (" +
                             "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                            "PARENT_CODE TEXT NOT NULL" +
+                            "PARENT_CODE TEXT NOT NULL," +
                             "CODE TEXT NOT NULL," +
                             "NAME TEXT NOT NULL)"
                     )
