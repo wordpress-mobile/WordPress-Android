9b235c1d128e790663e5f2c4e056780e64c82314
ashiagr <agr.ash@gmail.com>
2020-12-10 12:38:33 +0530

    Pass site to VM

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/ScanFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/ScanFragment.kt
@@ -6,23 +6,36 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
+import org.wordpress.android.fluxc.model.SiteModel
 import javax.inject.Inject
 
 class ScanFragment : Fragment(R.layout.scan_fragment) {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: ScanViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        (requireActivity().application as WordPress).component().inject(this)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViewModel()
+
+        val nonNullActivity = requireActivity()
+        (nonNullActivity.application as WordPress).component()?.inject(this)
+
+        val site = if (savedInstanceState == null) {
+            val nonNullIntent = checkNotNull(nonNullActivity.intent)
+            nonNullIntent.getSerializableExtra(WordPress.SITE) as SiteModel
+        } else {
+            savedInstanceState.getSerializable(WordPress.SITE) as SiteModel
+        }
+
+        initViewModel(site)
     }
 
-    private fun initViewModel() {
+    private fun initViewModel(site: SiteModel) {
         viewModel = ViewModelProvider(this, viewModelFactory).get(ScanViewModel::class.java)
+        viewModel.start(site)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putSerializable(WordPress.SITE, viewModel.site)
+        super.onSaveInstanceState(outState)
     }
 }
--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/ScanViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/ScanViewModel.kt
@@ -1,15 +1,19 @@
 package org.wordpress.android.ui.jetpack.scan
 
 import androidx.lifecycle.ViewModel
+import org.wordpress.android.fluxc.model.SiteModel
 import javax.inject.Inject
 
 class ScanViewModel @Inject constructor() : ViewModel() {
     private var isStarted = false
 
-    fun start() {
+    lateinit var site: SiteModel
+
+    fun start(site: SiteModel) {
         if (isStarted) {
             return
         }
+        this.site = site
         isStarted = true
     }
 }
