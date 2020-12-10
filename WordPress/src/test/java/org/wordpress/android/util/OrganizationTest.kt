2a648189f95d1e5b2ca8b2072c564de8dbf00b96
khaykov <forsver@gmail.com>
2020-12-09 21:06:13 -0800

    Added Organization

---
+++ WordPress/src/test/java/org/wordpress/android/util/OrganizationTest.kt
@@ -0,0 +1,15 @@
+package org.wordpress.android.util
+
+import org.junit.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.wordpress.android.ui.Organization
+
+class OrganizationTest {
+    @Test
+    fun `fromOrgId returns correct Organization`() {
+        assertThat(Organization.fromOrgId(0)).isEqualTo(Organization.NO_ORGANIZATION)
+        assertThat(Organization.fromOrgId(1)).isEqualTo(Organization.A8C)
+        assertThat(Organization.fromOrgId(2)).isEqualTo(Organization.P2)
+        assertThat(Organization.fromOrgId(3)).isEqualTo(Organization.UNKNOWN)
+    }
+}
