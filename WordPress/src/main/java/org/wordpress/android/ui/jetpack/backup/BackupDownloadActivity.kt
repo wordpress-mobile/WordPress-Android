ef12c38a181fd7c15c0a8d5ba1b3076349d1d9df
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2020-12-15 09:04:18 -0500

    Replace SingleLiveEvent

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/backup/BackupDownloadActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/backup/BackupDownloadActivity.kt
@@ -61,10 +61,10 @@ class BackupDownloadActivity : LocaleAwareActivity() {
                 .get(BackupDownloadViewModel::class.java)
 
         viewModel.navigationTargetObservable.observe(this, { target ->
-                    target?.let {
-                        showStep(target)
-                    }
-                })
+            target?.let {
+                showStep(target)
+            }
+        })
 
         viewModel.screenTitleObservable.observe(this, { title ->
             supportActionBar?.title = getString(title)
@@ -72,16 +72,15 @@ class BackupDownloadActivity : LocaleAwareActivity() {
             this.title = getString(title)
         })
 
-        // todo: annmarie - may there are more states?
         // Canceled, Running, Complete -> (Running = kick off status)
-        viewModel.wizardFinishedObservable.observe(this, { backupDownloadWizardState ->
-            backupDownloadWizardState?.let {
+        viewModel.wizardFinishedObservable.observe(this, {
+            it.applyIfNotHandled {
                 val intent = Intent()
-                val (backupDownloadCreated, _) = when (backupDownloadWizardState) {
+                val (backupDownloadCreated, _) = when (this) {
                     // teh request was canceled
                     is BackupDownloadCanceled -> Pair(false, null)
-                    is BackupDownloadInProgress -> Pair(true, backupDownloadWizardState.activityId)
-                    is BackupDownloadCompleted -> Pair(true, backupDownloadWizardState.activityId)
+                    is BackupDownloadInProgress -> Pair(true, activityId)
+                    is BackupDownloadCompleted -> Pair(true, activityId)
                 }
                 // todo: annmarie what information do I need to send back - just to kick off status
                 // intent.putExtra(SOME_KEY_THAT_DESCRIBES_THE_ID, activityId )
