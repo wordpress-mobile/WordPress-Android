2b5440d11180fc4259e84dd19ae033be578c0a63
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2020-12-15 11:06:00 -0500

    Reorder quick actions and publish items

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/QuickActionsViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/QuickActionsViewHolder.kt
@@ -9,9 +9,9 @@ import org.wordpress.android.ui.mysite.MySiteItem.QuickActionsBlock
 class QuickActionsViewHolder(parent: ViewGroup) : MySiteItemViewHolder(parent, R.layout.quick_actions_block) {
     fun bind(item: QuickActionsBlock) = itemView.apply {
         quick_action_stats_button.setOnClickListener { item.onStatsClick.click() }
-        quick_action_pages_button.setOnClickListener { item.onPagesClick.click() }
         quick_action_posts_button.setOnClickListener { item.onPostsClick.click() }
         quick_action_media_button.setOnClickListener { item.onMediaClick.click() }
+        quick_action_pages_button.setOnClickListener { item.onPagesClick.click() }
 
         val pagesVisibility = if (item.showPages) View.VISIBLE else View.GONE
         quick_action_pages_container.visibility = pagesVisibility
