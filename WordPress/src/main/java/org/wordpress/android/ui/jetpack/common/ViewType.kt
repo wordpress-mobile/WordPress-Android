3c8188bf732059c0ca84a88d4c252140958e7ebb
ashiagr <agr.ash@gmail.com>
2020-12-18 17:53:38 +0530

    Create reusable common list item type states in JetpackListItemState

---
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/common/ViewType.kt
@@ -0,0 +1,10 @@
+package org.wordpress.android.ui.jetpack.common
+
+enum class ViewType(val id: Int) {
+    IMAGE(0),
+    HEADER(1),
+    DESCRIPTION(2),
+    ACTION_BUTTON(3),
+    THREATS_HEADER(4),
+    THREAT_ITEM(5)
+}
