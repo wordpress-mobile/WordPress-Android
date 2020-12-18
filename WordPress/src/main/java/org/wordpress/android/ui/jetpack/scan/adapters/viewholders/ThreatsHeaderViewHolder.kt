4f03c50e7b90e78ea820456633359f68649dd977
ashiagr <agr.ash@gmail.com>
2020-12-18 17:55:15 +0530

    Split ScanStateViewHolder into reusable view holders

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/adapters/viewholders/ThreatsHeaderViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/adapters/viewholders/ThreatsHeaderViewHolder.kt
@@ -3,15 +3,16 @@ package org.wordpress.android.ui.jetpack.scan.adapters.viewholders
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.scan_list_threats_header_item.*
 import org.wordpress.android.R
-import org.wordpress.android.ui.jetpack.scan.ScanListItemState
+import org.wordpress.android.ui.jetpack.common.JetpackListItemState
+import org.wordpress.android.ui.jetpack.common.viewholders.JetpackViewHolder
 import org.wordpress.android.ui.jetpack.scan.ScanListItemState.ThreatsHeaderItemState
 import org.wordpress.android.ui.utils.UiHelpers
 
 class ThreatsHeaderViewHolder(
     private val uiHelpers: UiHelpers,
     parent: ViewGroup
-) : ScanViewHolder(R.layout.scan_list_threats_header_item, parent) {
-    override fun onBind(itemUiState: ScanListItemState) {
+) : JetpackViewHolder(R.layout.scan_list_threats_header_item, parent) {
+    override fun onBind(itemUiState: JetpackListItemState) {
         val headerItemState = itemUiState as ThreatsHeaderItemState
         header_text.text = uiHelpers.getTextOfUiString(itemView.context, headerItemState.text)
     }
