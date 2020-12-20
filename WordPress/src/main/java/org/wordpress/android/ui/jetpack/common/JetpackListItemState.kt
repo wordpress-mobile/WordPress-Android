00a56f3662d4fe8b7656261f3f3a151e07e25956
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2020-12-20 15:49:53 -0500

    Add colorResId

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/common/JetpackListItemState.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/common/JetpackListItemState.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.jetpack.common
 
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import org.wordpress.android.ui.jetpack.common.providers.JetpackAvailableItemsProvider.JetpackAvailableItemType
 import org.wordpress.android.ui.utils.UiString
@@ -9,7 +10,8 @@ abstract class JetpackListItemState(open val type: ViewType) {
 
     data class IconState(
         @DrawableRes val icon: Int,
-        val contentDescription: UiString
+        val contentDescription: UiString,
+        @ColorRes val colorResId: Int? = null
     ) : JetpackListItemState(ViewType.ICON)
 
     data class HeaderState(val text: UiString) : JetpackListItemState(ViewType.HEADER)
--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/common/viewholders/JetpackIconViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/common/viewholders/JetpackIconViewHolder.kt
@@ -1,10 +1,12 @@
 package org.wordpress.android.ui.jetpack.common.viewholders
 
 import android.view.ViewGroup
+import android.widget.ImageView.ScaleType.FIT_END
 import kotlinx.android.synthetic.main.jetpack_list_icon_item.*
 import org.wordpress.android.R
 import org.wordpress.android.ui.jetpack.common.JetpackListItemState
 import org.wordpress.android.ui.jetpack.common.JetpackListItemState.IconState
+import org.wordpress.android.util.ColorUtils
 import org.wordpress.android.util.image.ImageManager
 
 class JetpackIconViewHolder(
@@ -13,6 +15,14 @@ class JetpackIconViewHolder(
 ) : JetpackViewHolder(R.layout.jetpack_list_icon_item, parent) {
     override fun onBind(itemUiState: JetpackListItemState) {
         val iconState = itemUiState as IconState
-        imageManager.load(icon, iconState.icon)
+        if (iconState.colorResId == null) {
+            imageManager.load(icon, iconState.icon)
+        } else {
+            ColorUtils.setImageResourceWithTint(
+                    icon,
+                    iconState.icon,
+                    iconState.colorResId
+            )
+        }
     }
 }
