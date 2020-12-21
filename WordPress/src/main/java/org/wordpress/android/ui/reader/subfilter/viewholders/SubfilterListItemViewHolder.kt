1a17c53d03b69327268f0b82bee2d94527deb58d
develric <develric@gmail.com>
2020-12-21 23:11:13 +0100

    Fixing lint.

--- WordPress/src/main/java/org/wordpress/android/ui/reader/subfilter/viewholders/SubfilterListItemViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/subfilter/viewholders/SubfilterListItemViewHolder.kt
@@ -1,9 +1,7 @@
 package org.wordpress.android.ui.reader.subfilter.viewholders
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
