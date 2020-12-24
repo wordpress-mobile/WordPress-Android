5ae1c6556e46f6614716655a8bd2eb7af36ae321
ashiagr <agr.ash@gmail.com>
2020-12-24 11:32:45 +0530

    Introduce ThreatDetails RecyclerView and Adapter

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/details/ThreatDetailsFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/details/ThreatDetailsFragment.kt
@@ -4,23 +4,34 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.threat_details_fragment.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.ui.jetpack.scan.ScanConstants
+import org.wordpress.android.ui.jetpack.scan.details.adapters.ThreatDetailsAdapter
+import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.util.image.ImageManager
 import javax.inject.Inject
 
 class ThreatDetailsFragment : Fragment(R.layout.threat_details_fragment) {
+    @Inject lateinit var imageManager: ImageManager
+    @Inject lateinit var uiHelpers: UiHelpers
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: ThreatDetailsViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initDagger()
+        initAdapter()
         initViewModel()
     }
 
     private fun initDagger() {
         (requireActivity().application as WordPress).component()?.inject(this)
+    }
+
+    private fun initAdapter() {
+        recycler_view.adapter = ThreatDetailsAdapter(imageManager, uiHelpers)
     }
 
     private fun initViewModel() {
---
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/details/adapters/ThreatDetailsAdapter.kt
@@ -0,0 +1,71 @@
+package org.wordpress.android.ui.jetpack.scan.details.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import org.wordpress.android.ui.jetpack.common.JetpackListItemState
+import org.wordpress.android.ui.jetpack.common.ViewType
+import org.wordpress.android.ui.jetpack.common.viewholders.JetpackDescriptionViewHolder
+import org.wordpress.android.ui.jetpack.common.viewholders.JetpackIconViewHolder
+import org.wordpress.android.ui.jetpack.common.viewholders.JetpackViewHolder
+import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.util.image.ImageManager
+
+class ThreatDetailsAdapter(
+    private val imageManager: ImageManager,
+    private val uiHelpers: UiHelpers
+) : Adapter<JetpackViewHolder>() {
+    private val items = mutableListOf<JetpackListItemState>()
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JetpackViewHolder {
+        return when (viewType) {
+            ViewType.ICON.id -> JetpackIconViewHolder(imageManager, parent)
+            ViewType.DESCRIPTION.id -> JetpackDescriptionViewHolder(uiHelpers, parent)
+            else -> throw IllegalArgumentException("Unexpected view type in ${this::class.java.simpleName}")
+        }
+    }
+
+    override fun onBindViewHolder(holder: JetpackViewHolder, position: Int) {
+        holder.onBind(items[position])
+    }
+
+    override fun getItemViewType(position: Int) = items[position].type.id
+
+    override fun getItemId(position: Int): Long = items[position].longId()
+
+    override fun getItemCount() = items.size
+
+    fun update(newItems: List<JetpackListItemState>) {
+        val diffResult = DiffUtil.calculateDiff(ScanDiffCallback(this.items.toList(), items))
+        items.clear()
+        items.addAll(newItems)
+
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    class ScanDiffCallback(
+        private val oldList: List<JetpackListItemState>,
+        private val newList: List<JetpackListItemState>
+    ) : DiffUtil.Callback() {
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldList[oldItemPosition]
+            val newItem = newList[newItemPosition]
+            if (oldItem::class != newItem::class) {
+                return false
+            }
+            return oldItem.longId() == newItem.longId()
+        }
+
+        override fun getOldListSize() = oldList.size
+
+        override fun getNewListSize() = newList.size
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+    }
+}
