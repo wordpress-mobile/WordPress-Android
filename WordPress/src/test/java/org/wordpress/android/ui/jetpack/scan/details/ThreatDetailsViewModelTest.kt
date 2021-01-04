65aa194d0e5b42badda10836a1c3139c6ce0d5f9
ashiagr <agr.ash@gmail.com>
2021-01-04 08:31:16 +0530

    Rename use case

--- WordPress/src/test/java/org/wordpress/android/ui/jetpack/scan/details/ThreatDetailsViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/jetpack/scan/details/ThreatDetailsViewModelTest.kt
@@ -5,17 +5,17 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 import org.wordpress.android.BaseUnitTest
-import org.wordpress.android.ui.jetpack.usecases.GetThreatDetailsUseCase
+import org.wordpress.android.ui.jetpack.usecases.GetThreatModelUseCase
 
 @InternalCoroutinesApi
 class ThreatDetailsViewModelTest : BaseUnitTest() {
-    @Mock private lateinit var getThreatDetailsUseCase: GetThreatDetailsUseCase
+    @Mock private lateinit var getThreatModelUseCase: GetThreatModelUseCase
     @Mock private lateinit var builder: ThreatDetailsListItemsBuilder
     private lateinit var viewModel: ThreatDetailsViewModel
 
     @Before
     fun setUp() {
-        viewModel = ThreatDetailsViewModel(getThreatDetailsUseCase, builder)
+        viewModel = ThreatDetailsViewModel(getThreatModelUseCase, builder)
     }
 
     @Test
