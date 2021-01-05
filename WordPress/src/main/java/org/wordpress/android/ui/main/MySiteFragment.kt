e02b4d2a0705c4ebccbb998ceb7c2f0db0fcfef8
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2021-01-05 16:59:45 +0200

    Refactor: Optimize android R related imports for my site fragment
    This fixes lots of import related warning on this class, thus making it
    easier to spot a new warning.

--- WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MySiteFragment.kt
@@ -35,8 +35,6 @@ import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.R
-import org.wordpress.android.R.attr
-import org.wordpress.android.R.string
 import org.wordpress.android.WordPress
 import org.wordpress.android.analytics.AnalyticsTracker
 import org.wordpress.android.analytics.AnalyticsTracker.Stat.DOMAIN_CREDIT_PROMPT_SHOWN
@@ -961,7 +959,7 @@ class MySiteFragment : Fragment(),
         options.setShowCropGrid(false)
         options.setStatusBarColor(context.getColorFromAttribute(android.R.attr.statusBarColor))
         options.setToolbarColor(context.getColorFromAttribute(R.attr.wpColorAppBar))
-        options.setToolbarWidgetColor(context.getColorFromAttribute(attr.colorOnSurface))
+        options.setToolbarWidgetColor(context.getColorFromAttribute(R.attr.colorOnSurface))
         options.setAllowedGestures(UCropActivity.SCALE, UCropActivity.NONE, UCropActivity.NONE)
         options.setHideBottomControls(true)
         UCrop.of(uri, Uri.fromFile(File(context.cacheDir, "cropped_for_site_icon.jpg")))
@@ -1191,7 +1189,7 @@ class MySiteFragment : Fragment(),
         if (!event.isSuccess()) {
             // note: no tracking added here as we'll perform tracking in StoryMediaSaveUploadBridge
             val errorText = String.format(
-                    getString(string.story_saving_snackbar_finished_with_error),
+                    getString(R.string.story_saving_snackbar_finished_with_error),
                     getStoryAtIndex(event.storyIndex).title
             )
             val snackbarMessage = buildSnackbarErrorMessage(
@@ -1202,7 +1200,7 @@ class MySiteFragment : Fragment(),
             uploadUtilsWrapper.showSnackbarError(
                     requireActivity().findViewById<View>(R.id.coordinator),
                     snackbarMessage,
-                    string.story_saving_failed_quick_action_manage
+                    R.string.story_saving_failed_quick_action_manage
             ) {
                 // TODO WPSTORIES add TRACKS: the putExtra described here below for NOTIFICATION_TYPE
                 // is meant to be used for tracking purposes. Use it!
@@ -1224,7 +1222,7 @@ class MySiteFragment : Fragment(),
     fun onStorySaveStart(event: StorySaveProcessStart) {
         EventBus.getDefault().removeStickyEvent(event)
         val snackbarMessage = String.format(
-                getString(string.story_saving_snackbar_started),
+                getString(R.string.story_saving_snackbar_started),
                 getStoryAtIndex(event.storyIndex).title
         )
         uploadUtilsWrapper.showSnackbar(
