b3b0f9acef885262bdf9abbeadf888caf3111f00
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-01-05 11:17:11 -0500

    Update test

--- WordPress/src/test/java/org/wordpress/android/ui/jetpack/backup/download/BackupDownloadProgressViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/jetpack/backup/download/BackupDownloadProgressViewModelTest.kt
@@ -13,6 +13,7 @@ import org.wordpress.android.R.string
 import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.test
+import org.wordpress.android.ui.jetpack.backup.download.progress.BackupDownloadProgressStateListItemBuilder
 import org.wordpress.android.ui.jetpack.backup.download.progress.BackupDownloadProgressViewModel.UiState.Content
 import org.wordpress.android.ui.jetpack.backup.download.progress.BackupDownloadProgressViewModel
 import org.wordpress.android.ui.jetpack.backup.download.progress.BackupDownloadProgressViewModel.UiState
@@ -20,6 +21,7 @@ import org.wordpress.android.ui.jetpack.backup.download.usecases.GetBackupDownlo
 import org.wordpress.android.ui.pages.SnackbarMessageHolder
 import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.viewmodel.Event
+import org.wordpress.android.ui.jetpack.backup.download.BackupDownloadListItemState.ProgressState
 import java.util.Date
 
 @InternalCoroutinesApi
@@ -28,6 +30,7 @@ class BackupDownloadProgressViewModelTest : BaseUnitTest() {
     @Mock private lateinit var parentViewModel: BackupDownloadViewModel
     @Mock private lateinit var site: SiteModel
     @Mock private lateinit var backupDownloadStatusUseCase: GetBackupDownloadStatusUseCase
+    private lateinit var stateListItemBuilder: BackupDownloadProgressStateListItemBuilder
 
     private val backupDownloadState = BackupDownloadState(
             activityId = "activityId",
@@ -40,8 +43,10 @@ class BackupDownloadProgressViewModelTest : BaseUnitTest() {
 
     @Before
     fun setUp() = test {
+        stateListItemBuilder = BackupDownloadProgressStateListItemBuilder()
         viewModel = BackupDownloadProgressViewModel(
                 backupDownloadStatusUseCase,
+                stateListItemBuilder,
                 TEST_DISPATCHER,
                 TEST_DISPATCHER
         )
@@ -59,11 +64,23 @@ class BackupDownloadProgressViewModelTest : BaseUnitTest() {
     }
 
     @Test
+    fun `when started, the progress is set to zero `() = test {
+        val uiStates = initObservers().uiStates
+        whenever(backupDownloadStatusUseCase.getBackupDownloadStatus(anyOrNull(), anyOrNull()))
+                .thenReturn(flow { emit(getStatusProgress) })
+
+        viewModel.start(site, backupDownloadState, parentViewModel)
+
+        assertThat((((uiStates[0] as Content).items).first { it is ProgressState } as ProgressState).progress)
+                .isEqualTo(0)
+    }
+
+    @Test
     fun `snackbar message is shown when request encounters a network connection issue`() = test {
         whenever(backupDownloadStatusUseCase.getBackupDownloadStatus(anyOrNull(), anyOrNull()))
                 .thenReturn(flow { emit(getStatusNetworkError) })
 
-        val msgs = initObservers().snackbarMsgs
+        val msgs = initObservers().snackbarMessages
 
         viewModel.start(site, backupDownloadState, parentViewModel)
 
@@ -75,7 +92,7 @@ class BackupDownloadProgressViewModelTest : BaseUnitTest() {
         whenever(backupDownloadStatusUseCase.getBackupDownloadStatus(anyOrNull(), anyOrNull()))
                 .thenReturn(flow { emit(getStatusRemoteRequestError) })
 
-        val msgs = initObservers().snackbarMsgs
+        val msgs = initObservers().snackbarMessages
 
         viewModel.start(site, backupDownloadState, parentViewModel)
 
@@ -87,19 +104,19 @@ class BackupDownloadProgressViewModelTest : BaseUnitTest() {
         viewModel.uiState.observeForever {
             uiStates.add(it)
         }
-        val snackbarMsgs = mutableListOf<Event<SnackbarMessageHolder>>()
+        val snackbarMessages = mutableListOf<Event<SnackbarMessageHolder>>()
         viewModel.snackbarEvents.observeForever {
-            snackbarMsgs.add(it)
+            snackbarMessages.add(it)
         }
-        return Observers(uiStates, snackbarMsgs)
+        return Observers(uiStates, snackbarMessages)
     }
 
     private data class Observers(
         val uiStates: List<UiState>,
-        val snackbarMsgs: List<Event<SnackbarMessageHolder>>
+        val snackbarMessages: List<Event<SnackbarMessageHolder>>
     )
 
     private val getStatusNetworkError = BackupDownloadRequestState.Failure.NetworkUnavailable
     private val getStatusRemoteRequestError = BackupDownloadRequestState.Failure.RemoteRequestFailure
-    private val getStatusProgress = BackupDownloadRequestState.Progress(rewindId = "rewindId", progress = 10)
+    private val getStatusProgress = BackupDownloadRequestState.Progress(rewindId = "rewindId", progress = 0)
 }
