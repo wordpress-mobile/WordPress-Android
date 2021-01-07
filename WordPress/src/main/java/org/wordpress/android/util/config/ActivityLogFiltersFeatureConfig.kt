e024f5d784a13a8e37e857160d7bf9aadec203d4
malinajirka <malinajirka@gmail.com>
2021-01-07 15:37:36 +0100

    Add remote field to ActivityLogFiltersFeatureConfig

--- WordPress/src/main/java/org/wordpress/android/util/config/ActivityLogFiltersFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/ActivityLogFiltersFeatureConfig.kt
@@ -1,15 +1,21 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.annotation.Feature
+import org.wordpress.android.util.config.ActivityLogFiltersFeatureConfig.Companion.ACTIVITY_LOG_FILTERS
 import javax.inject.Inject
 
 /**
  * Configuration of the Activity Log Filters feature.
  */
-@FeatureInDevelopment
+@Feature(remoteField = ACTIVITY_LOG_FILTERS)
 class ActivityLogFiltersFeatureConfig
 @Inject constructor(appConfig: AppConfig) : FeatureConfig(
         appConfig,
-        BuildConfig.ACTIVITY_LOG_FILTERS
-)
+        BuildConfig.ACTIVITY_LOG_FILTERS,
+        ACTIVITY_LOG_FILTERS
+) {
+    companion object {
+        const val ACTIVITY_LOG_FILTERS = "activity_log_filters_enabled"
+    }
+}
