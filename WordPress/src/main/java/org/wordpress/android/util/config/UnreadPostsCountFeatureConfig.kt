7d3000cd53548329ab0bba416ee20ee3e421a73c
khaykov <forsver@gmail.com>
2021-01-08 11:15:54 -0800

    Removed unread post count feature flag.

--- WordPress/src/main/java/org/wordpress/android/util/config/UnreadPostsCountFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/UnreadPostsCountFeatureConfig.kt
@@ -1,15 +1,21 @@
 package org.wordpress.android.util.config
 
-import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.BuildConfig.UNREAD_POSTS_COUNT
+import org.wordpress.android.annotation.Feature
+import org.wordpress.android.util.config.UnreadPostsCountFeatureConfig.Companion.UNREAD_POSTS_COUNT_REMOTE_FIELD
 import javax.inject.Inject
 
 /**
  * Configuration of the Unread Posts Count
  */
-@FeatureInDevelopment
+@Feature(UNREAD_POSTS_COUNT_REMOTE_FIELD, true)
 class UnreadPostsCountFeatureConfig
 @Inject constructor(appConfig: AppConfig) : FeatureConfig(
         appConfig,
-        BuildConfig.UNREAD_POSTS_COUNT
-)
+        UNREAD_POSTS_COUNT,
+        UNREAD_POSTS_COUNT_REMOTE_FIELD
+) {
+    companion object {
+        const val UNREAD_POSTS_COUNT_REMOTE_FIELD = "unread_post_count"
+    }
+}
