83ee8b2c6542b7ca9995cf532057700fc33bb9e8
ashiagr <agr.ash@gmail.com>
2021-01-11 12:24:07 +0530

    Add use case with tests for fetch scan state (with support for polling)

---
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/usecases/FetchScanStateUseCase.kt
@@ -0,0 +1,62 @@
+package org.wordpress.android.ui.jetpack.scan.usecases
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.scan.ScanStateModel
+import org.wordpress.android.fluxc.store.ScanStore
+import org.wordpress.android.fluxc.store.ScanStore.FetchScanStatePayload
+import org.wordpress.android.ui.jetpack.scan.usecases.FetchScanStateUseCase.FetchScanState.Failure
+import org.wordpress.android.ui.jetpack.scan.usecases.FetchScanStateUseCase.FetchScanState.Success
+import org.wordpress.android.util.NetworkUtilsWrapper
+import javax.inject.Inject
+
+const val FETCH_SCAN_STATE_DELAY_MILLIS = 5000L
+
+class FetchScanStateUseCase @Inject constructor(
+    private val networkUtilsWrapper: NetworkUtilsWrapper,
+    private val scanStore: ScanStore
+) {
+    suspend fun fetchScanState(
+        site: SiteModel,
+        polling: Boolean = true,
+        delayInMs: Long = FETCH_SCAN_STATE_DELAY_MILLIS
+    ): Flow<FetchScanState> = flow {
+        if (!networkUtilsWrapper.isNetworkAvailable()) {
+            emit(Failure.NetworkUnavailable)
+            return@flow
+        }
+
+        val result = scanStore.fetchScanState(FetchScanStatePayload(site))
+        if (result.isError) {
+            emit(Failure.RemoteRequestFailure)
+            return@flow
+        } else {
+            val scanStateModel = scanStore.getScanStateForSite(site)
+            if (scanStateModel != null) {
+                emit(Success(scanStateModel))
+                if (polling && scanStateModel.state == ScanStateModel.State.SCANNING) {
+                    fetchScanState(site = site, polling = true).collect {
+                        emit(it)
+                        delay(delayInMs)
+                    }
+                } else {
+                    return@flow
+                }
+            } else {
+                emit(Failure.RemoteRequestFailure)
+                return@flow
+            }
+        }
+    }
+
+    sealed class FetchScanState {
+        data class Success(val scanStateModel: ScanStateModel) : FetchScanState()
+        sealed class Failure : FetchScanState() {
+            object NetworkUnavailable : Failure()
+            object RemoteRequestFailure : Failure()
+        }
+    }
+}
---
+++ WordPress/src/test/java/org/wordpress/android/ui/jetpack/scan/usecases/FetchScanStateUseCaseTest.kt
@@ -0,0 +1,113 @@
+package org.wordpress.android.ui.jetpack.scan.usecases
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.wordpress.android.BaseUnitTest
+import org.wordpress.android.fluxc.action.ScanAction.FETCH_SCAN_STATE
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.scan.ScanStateModel
+import org.wordpress.android.fluxc.store.ScanStore
+import org.wordpress.android.fluxc.store.ScanStore.OnScanStateFetched
+import org.wordpress.android.fluxc.store.ScanStore.ScanStateError
+import org.wordpress.android.fluxc.store.ScanStore.ScanStateErrorType
+import org.wordpress.android.test
+import org.wordpress.android.ui.jetpack.scan.usecases.FetchScanStateUseCase.FetchScanState
+import org.wordpress.android.util.NetworkUtilsWrapper
+
+@InternalCoroutinesApi
+class FetchScanStateUseCaseTest : BaseUnitTest() {
+    private lateinit var useCase: FetchScanStateUseCase
+    @Mock private lateinit var site: SiteModel
+    @Mock private lateinit var scanStateModel: ScanStateModel
+    @Mock lateinit var networkUtilsWrapper: NetworkUtilsWrapper
+    @Mock lateinit var scanStore: ScanStore
+
+    @Before
+    fun setup() {
+        useCase = FetchScanStateUseCase(networkUtilsWrapper, scanStore)
+        whenever(networkUtilsWrapper.isNetworkAvailable()).thenReturn(true)
+        whenever(scanStore.getScanStateForSite(site)).thenReturn(scanStateModel)
+    }
+
+    @Test
+    fun `given site, when scan state is fetched, then NetworkUnavailable is returned on no network`() = test {
+        whenever(networkUtilsWrapper.isNetworkAvailable()).thenReturn(false)
+
+        val result = useCase.fetchScanState(site).toList(mutableListOf())
+
+        assertThat(result).contains(FetchScanState.Failure.NetworkUnavailable)
+    }
+
+    @Test
+    fun `given site, when scan state is fetched, then Success is returned on success`() = testWithSuccessResponse {
+        val result = useCase.fetchScanState(site).toList(mutableListOf())
+
+        assertThat(result).contains(FetchScanState.Success(scanStateModel))
+    }
+
+    @Test
+    fun `given site, when scan state is fetched, then RemoteRequestFailure is returned on failure`() = test {
+        whenever(scanStore.fetchScanState(any())).thenReturn(
+            OnScanStateFetched(ScanStateError(ScanStateErrorType.GENERIC_ERROR), FETCH_SCAN_STATE)
+        )
+        val result = useCase.fetchScanState(site).toList(mutableListOf())
+
+        assertThat(result).contains(FetchScanState.Failure.RemoteRequestFailure)
+    }
+
+    @Test
+    fun `when SCANNING scan state is fetched, then polling occurs until IDLE state is returned on success`() =
+        testWithSuccessResponse {
+            val scanStateScanningModel = ScanStateModel(state = ScanStateModel.State.SCANNING)
+            val scanStateModels = listOf(
+                scanStateScanningModel,
+                scanStateScanningModel,
+                ScanStateModel(state = ScanStateModel.State.IDLE)
+            )
+            whenever(scanStore.getScanStateForSite(any()))
+                .thenReturn(scanStateModels[0])
+                .thenReturn(scanStateModels[1])
+                .thenReturn(scanStateModels[2])
+
+            val result = useCase.fetchScanState(site = site, delayInMs = 0).toList(mutableListOf())
+
+            verify(scanStore, times(scanStateModels.size)).fetchScanState(any())
+            assertThat(result).containsSequence(scanStateModels.map { FetchScanState.Success(it) })
+        }
+
+    @Test
+    fun `when SCANNING scan state is fetched, then polling occurs until error is returned on failure`() =
+        test {
+            val scanStateScanningModel = ScanStateModel(state = ScanStateModel.State.SCANNING)
+            val scanStateError = ScanStateError(ScanStateErrorType.GENERIC_ERROR)
+            whenever(scanStore.getScanStateForSite(any())).thenReturn(scanStateScanningModel)
+            whenever(scanStore.fetchScanState(any())).thenReturn(
+                OnScanStateFetched(FETCH_SCAN_STATE),
+                OnScanStateFetched(scanStateError, FETCH_SCAN_STATE)
+            )
+
+            val result = useCase.fetchScanState(site = site, delayInMs = 0).toList(mutableListOf())
+
+            verify(scanStore, times(2)).fetchScanState(any())
+            assertThat(result).containsSequence(
+                FetchScanState.Success(scanStateScanningModel),
+                FetchScanState.Failure.RemoteRequestFailure
+            )
+        }
+
+    private fun <T> testWithSuccessResponse(block: suspend CoroutineScope.() -> T) {
+        test {
+            whenever(scanStore.fetchScanState(any())).thenReturn(OnScanStateFetched(FETCH_SCAN_STATE))
+            block()
+        }
+    }
+}
