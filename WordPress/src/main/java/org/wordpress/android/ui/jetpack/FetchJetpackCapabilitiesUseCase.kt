9306c6238e3cc8de5ee0e6f9219f6fdf309d8c02
malinajirka <malinajirka@gmail.com>
2021-01-11 16:23:20 +0100

    Add caching support to FetchJetpackCapabilitiesUseCase

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/FetchJetpackCapabilitiesUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/FetchJetpackCapabilitiesUseCase.kt
@@ -6,36 +6,78 @@ import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.SiteActionBuilder
+import org.wordpress.android.fluxc.model.JetpackCapability
 import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.fluxc.store.SiteStore.FetchJetpackCapabilitiesPayload
 import org.wordpress.android.fluxc.store.SiteStore.OnJetpackCapabilitiesFetched
+import org.wordpress.android.fluxc.utils.CurrentTimeProvider
 import org.wordpress.android.modules.BG_THREAD
+import org.wordpress.android.ui.prefs.AppPrefsWrapper
 import javax.inject.Inject
 import javax.inject.Named
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+private const val MAX_CACHE_VALIDITY = 1000 * 60 * 15 // 15 minutes
+
 class FetchJetpackCapabilitiesUseCase @Inject constructor(
     @Suppress("unused") private val siteStore: SiteStore,
     private val dispatcher: Dispatcher,
+    private val appPrefsWrapper: AppPrefsWrapper,
+    private val currentDateProvider: CurrentTimeProvider,
     @param:Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher
 ) {
     private var continuation: Continuation<OnJetpackCapabilitiesFetched>? = null
 
-    suspend fun fetchJetpackCapabilities(remoteSiteId: Long): OnJetpackCapabilitiesFetched {
+    suspend fun getOrFetchJetpackCapabilities(remoteSiteId: Long): List<JetpackCapability> {
+        return if(hasValidCache(remoteSiteId)) {
+            getCachedJetpackCapabilities(remoteSiteId)
+        } else {
+            fetchJetpackCapabilities(remoteSiteId)
+        }
+    }
+
+    private suspend fun hasValidCache(remoteSiteId: Long): Boolean {
+        return withContext(bgDispatcher) {
+            val lastUpdated = appPrefsWrapper.getSiteJetpackCapabilitiesLastUpdated(remoteSiteId)
+            lastUpdated > currentDateProvider.currentDate.time - MAX_CACHE_VALIDITY
+        }
+    }
+
+    private suspend fun getCachedJetpackCapabilities(remoteSiteId: Long): List<JetpackCapability> {
+        return withContext(bgDispatcher) { appPrefsWrapper.getSiteJetpackCapabilities(remoteSiteId) }
+    }
+
+    private suspend fun fetchJetpackCapabilities(remoteSiteId: Long): List<JetpackCapability> {
         return withContext(bgDispatcher) {
             if (continuation != null) {
                 throw IllegalStateException("Request already in progress.")
             }
 
             dispatcher.register(this@FetchJetpackCapabilitiesUseCase)
-            suspendCoroutine { cont ->
+            val response = suspendCoroutine<OnJetpackCapabilitiesFetched> { cont ->
                 val payload = FetchJetpackCapabilitiesPayload(remoteSiteId)
                 continuation = cont
                 dispatcher.dispatch(SiteActionBuilder.newFetchJetpackCapabilitiesAction(payload))
             }
+
+            val capabilities: List<JetpackCapability> = response.capabilities ?: listOf()
+            if (!response.isError) {
+                updateCache(remoteSiteId, capabilities)
+            }
+            return@withContext capabilities
         }
+    }
+
+    private fun updateCache(
+        remoteSiteId: Long,
+        capabilities: List<JetpackCapability>
+    ) {
+        appPrefsWrapper.setSiteJetpackCapabilities(
+                remoteSiteId,
+                capabilities
+        )
     }
 
     @Subscribe(threadMode = ThreadMode.BACKGROUND)
