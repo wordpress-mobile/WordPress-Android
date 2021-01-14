0fa3b9d766867daef7dbf1910b6a97053f4ba882
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-01-14 14:13:44 -0500

    Main restore progress logic

---
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/restore/usecases/GetRestoreStatusUseCase.kt
@@ -0,0 +1,61 @@
+package org.wordpress.android.ui.jetpack.restore.usecases
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.activity.RewindStatusModel.Rewind.Status.FAILED
+import org.wordpress.android.fluxc.model.activity.RewindStatusModel.Rewind.Status.FINISHED
+import org.wordpress.android.fluxc.model.activity.RewindStatusModel.Rewind.Status.RUNNING
+import org.wordpress.android.fluxc.store.ActivityLogStore
+import org.wordpress.android.fluxc.store.ActivityLogStore.FetchRewindStatePayload
+import org.wordpress.android.ui.jetpack.restore.RestoreRequestState.Complete
+import org.wordpress.android.ui.jetpack.restore.RestoreRequestState.Failure.NetworkUnavailable
+import org.wordpress.android.ui.jetpack.restore.RestoreRequestState.Failure.RemoteRequestFailure
+import org.wordpress.android.ui.jetpack.restore.RestoreRequestState.Progress
+import org.wordpress.android.util.NetworkUtilsWrapper
+import javax.inject.Inject
+
+const val DELAY_MILLIS = 5000L
+
+class GetRestoreStatusUseCase @Inject constructor(
+    private val networkUtilsWrapper: NetworkUtilsWrapper,
+    private val activityLogStore: ActivityLogStore
+) {
+    suspend fun getRestoreStatus(site: SiteModel, restoreId: Long) = flow {
+        while (true) {
+            if (!networkUtilsWrapper.isNetworkAvailable()) {
+                emit(NetworkUnavailable)
+                return@flow
+            }
+
+            val statusForSite = activityLogStore.getRewindStatusForSite(site)
+            val rewind = statusForSite?.rewind
+            if (rewind != null && rewind.restoreId == restoreId) {
+                when (rewind.status) {
+                    FINISHED -> {
+                        if (rewind.rewindId != null) {
+                            emit(Complete(rewind.rewindId as String, rewind.restoreId))
+                        } else {
+                            emit(RemoteRequestFailure)
+                        }
+                        return@flow
+                    }
+                    FAILED -> {
+                        emit(RemoteRequestFailure)
+                        return@flow
+                    }
+                    RUNNING -> {
+                        emit(Progress(rewind.rewindId as String, rewind.progress))
+                    }
+                }
+            }
+
+            val result = activityLogStore.fetchActivitiesRewind(FetchRewindStatePayload(site))
+            if (result.isError) {
+                emit(RemoteRequestFailure)
+                return@flow
+            }
+            delay(DELAY_MILLIS)
+        }
+    }
+}
