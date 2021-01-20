e53268fc6233b7b65061767e0c53908603e9eb9e
malinajirka <malinajirka@gmail.com>
2021-01-20 14:24:55 +0100

    Provide tab ui state from ScanHistoryViewModel

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/history/ScanHistoryViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/history/ScanHistoryViewModel.kt
@@ -4,11 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
+import org.wordpress.android.R
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.scan.threat.ThreatModel
 import org.wordpress.android.fluxc.store.ScanStore
 import org.wordpress.android.fluxc.store.ScanStore.FetchScanHistoryPayload
 import org.wordpress.android.modules.UI_THREAD
+import org.wordpress.android.ui.jetpack.scan.history.ScanHistoryViewModel.ScanHistoryTabType.ALL
+import org.wordpress.android.ui.jetpack.scan.history.ScanHistoryViewModel.ScanHistoryTabType.FIXED
+import org.wordpress.android.ui.jetpack.scan.history.ScanHistoryViewModel.ScanHistoryTabType.IGNORED
+import org.wordpress.android.ui.utils.UiString
+import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.viewmodel.ScopedViewModel
 import javax.inject.Inject
 import javax.inject.Named
@@ -21,6 +27,14 @@ class ScanHistoryViewModel @Inject constructor(
 
     private val _threats = MutableLiveData<List<ThreatModel>>()
     val threats: LiveData<List<ThreatModel>> = _threats
+
+    val tabs: LiveData<List<TabUiState>> = MutableLiveData(
+                    listOf(
+                            TabUiState(UiStringRes(R.string.scan_history_all_threats_tab), ALL),
+                            TabUiState(UiStringRes(R.string.scan_history_fixed_threats_tab), FIXED),
+                            TabUiState(UiStringRes(R.string.scan_history_ignored_threats_tab), IGNORED)
+                    )
+    )
 
     lateinit var site: SiteModel
 
@@ -40,5 +54,10 @@ class ScanHistoryViewModel @Inject constructor(
                 _threats.value = scanStore.getScanHistoryForSite(site)
             }
         }
+    }
+
+    data class TabUiState(val label: UiString, val type: ScanHistoryTabType)
+    enum class ScanHistoryTabType {
+        ALL, FIXED, IGNORED
     }
 }
