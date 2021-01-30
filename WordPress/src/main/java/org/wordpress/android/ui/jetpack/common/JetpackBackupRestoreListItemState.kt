316410135614afaa351eaae3fb06e29454816842
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-01-30 13:23:42 -0500

    Add bullet state

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/common/JetpackBackupRestoreListItemState.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/common/JetpackBackupRestoreListItemState.kt
@@ -1,5 +1,9 @@
 package org.wordpress.android.ui.jetpack.common
 
+import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
+import androidx.annotation.DrawableRes
+import org.wordpress.android.R
 import org.wordpress.android.ui.utils.UiString
 
 sealed class JetpackBackupRestoreListItemState(override val type: ViewType) : JetpackListItemState(type) {
@@ -8,4 +12,13 @@ sealed class JetpackBackupRestoreListItemState(override val type: ViewType) : Je
 
     data class FootnoteState(val text: UiString) :
             JetpackListItemState(ViewType.BACKUP_RESTORE_FOOTNOTE)
+
+    data class BulletState(
+        @DrawableRes val icon: Int,
+        @ColorRes val colorResId: Int? = null,
+        @DimenRes val sizeResId: Int = R.dimen.jetpack_backup_restore_bullet_icon_size,
+        @DimenRes val marginResId: Int = R.dimen.jetpack_icon_margin,
+        val contentDescription: UiString,
+        val label: UiString
+    ) : JetpackListItemState(ViewType.BACKUP_RESTORE_BULLET)
 }
