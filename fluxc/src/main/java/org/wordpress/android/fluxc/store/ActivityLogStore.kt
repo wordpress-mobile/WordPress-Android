5ea716280a86415ac0f2f0130049e3c1162cfe29
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-02-02 13:59:41 -0500

    Add support for deleting rows when payload is null

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/ActivityLogStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/ActivityLogStore.kt
@@ -183,6 +183,8 @@ class ActivityLogStore
         } else {
             if (payload.rewindStatusModelResponse != null) {
                 activityLogSqlUtils.replaceRewindStatus(payload.site, payload.rewindStatusModelResponse)
+            } else {
+                activityLogSqlUtils.deleteRewindStatus(payload.site)
             }
             OnRewindStatusFetched(action)
         }
@@ -195,6 +197,8 @@ class ActivityLogStore
         } else {
             if (payload.backupDownloadStatusModelResponse != null) {
                 activityLogSqlUtils.replaceBackupDownloadStatus(payload.site, payload.backupDownloadStatusModelResponse)
+            } else {
+                activityLogSqlUtils.deleteBackupDownloadStatus(payload.site)
             }
             OnBackupDownloadStatusFetched(action)
         }
