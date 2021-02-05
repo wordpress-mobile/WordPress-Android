afe0a79ab7bc6d863c578add00be3a089860ddef
khaykov <forsver@gmail.com>
2021-02-05 14:27:55 -0800

    Fixed lint issue.

--- WordPress/src/main/java/org/wordpress/android/util/config/SeenUnseenWithCounterFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/SeenUnseenWithCounterFeatureConfig.kt
@@ -14,7 +14,7 @@ class SeenUnseenWithCounterFeatureConfig
         appConfig,
         SEEN_UNSEEN_WITH_COUNTER,
         SEEN_UNSEEN_WITH_COUNTER_REMOTE_FIELD
-){
+) {
     companion object {
         const val SEEN_UNSEEN_WITH_COUNTER_REMOTE_FIELD = "seen_unseen_with_counter"
     }
