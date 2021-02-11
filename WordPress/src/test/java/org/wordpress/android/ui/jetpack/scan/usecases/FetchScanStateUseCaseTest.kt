436c07724ef093853c6d71813c366a014c713f51
malinajirka <malinajirka@gmail.com>
2021-02-11 13:59:52 +0100

    Remove unnecessary advanceTimeBy calls

--- WordPress/src/test/java/org/wordpress/android/ui/jetpack/scan/usecases/FetchScanStateUseCaseTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/jetpack/scan/usecases/FetchScanStateUseCaseTest.kt
@@ -7,14 +7,11 @@ import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.wordpress.android.BaseUnitTest
-import org.wordpress.android.MainCoroutineScopeRule
 import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.fluxc.action.ScanAction.FETCH_SCAN_STATE
 import org.wordpress.android.fluxc.model.SiteModel
@@ -30,9 +27,6 @@ import org.wordpress.android.util.NetworkUtilsWrapper
 @ExperimentalCoroutinesApi
 @InternalCoroutinesApi
 class FetchScanStateUseCaseTest : BaseUnitTest() {
-    @Rule
-    @JvmField val coroutineScope = MainCoroutineScopeRule()
-
     private lateinit var useCase: FetchScanStateUseCase
     @Mock private lateinit var site: SiteModel
     @Mock private lateinit var scanStateModel: ScanStateModel
@@ -67,7 +61,7 @@ class FetchScanStateUseCaseTest : BaseUnitTest() {
     @Test
     fun `given site, when scan state is fetched, then RemoteRequestFailure is returned on failure`() = test {
         whenever(scanStore.fetchScanState(any())).thenReturn(
-            OnScanStateFetched(ScanStateError(ScanStateErrorType.GENERIC_ERROR), FETCH_SCAN_STATE)
+                OnScanStateFetched(ScanStateError(ScanStateErrorType.GENERIC_ERROR), FETCH_SCAN_STATE)
         )
         val result = useCase.fetchScanState(site).toList(mutableListOf())
 
@@ -75,45 +69,41 @@ class FetchScanStateUseCaseTest : BaseUnitTest() {
     }
 
     @Test
-    fun `when SCANNING scan state is fetched, then polling occurs until IDLE state is returned on success`() =
-        coroutineScope.runBlockingTest {
-            whenever(scanStore.fetchScanState(any())).thenReturn(OnScanStateFetched(FETCH_SCAN_STATE))
-            val scanStateScanningModel = ScanStateModel(state = ScanStateModel.State.SCANNING)
-            val scanStateModels = listOf(
+    fun `when SCANNING scan state is fetched, then polling occurs until IDLE state is returned on success`() = test {
+        whenever(scanStore.fetchScanState(any())).thenReturn(OnScanStateFetched(FETCH_SCAN_STATE))
+        val scanStateScanningModel = ScanStateModel(state = ScanStateModel.State.SCANNING)
+        val scanStateModels = listOf(
                 scanStateScanningModel,
                 scanStateScanningModel,
                 ScanStateModel(state = ScanStateModel.State.IDLE)
-            )
-            whenever(scanStore.getScanStateForSite(any()))
+        )
+        whenever(scanStore.getScanStateForSite(any()))
                 .thenReturn(scanStateModels[0])
                 .thenReturn(scanStateModels[1])
                 .thenReturn(scanStateModels[2])
 
-            val result = useCase.fetchScanState(site = site).toList(mutableListOf())
-            advanceTimeBy(FETCH_SCAN_STATE_DELAY_MILLIS)
+        val result = useCase.fetchScanState(site = site).toList(mutableListOf())
 
-            verify(scanStore, times(scanStateModels.size)).fetchScanState(any())
-            assertThat(result).containsSequence(scanStateModels.map { FetchScanState.Success(it) })
-        }
+        verify(scanStore, times(scanStateModels.size)).fetchScanState(any())
+        assertThat(result).containsSequence(scanStateModels.map { FetchScanState.Success(it) })
+    }
 
     @Test
-    fun `when SCANNING scan state is fetched, then polling occurs until error is returned on failure`() =
-        coroutineScope.runBlockingTest {
-            val scanStateScanningModel = ScanStateModel(state = ScanStateModel.State.SCANNING)
-            val scanStateError = ScanStateError(ScanStateErrorType.GENERIC_ERROR)
-            whenever(scanStore.getScanStateForSite(any())).thenReturn(scanStateScanningModel)
-            whenever(scanStore.fetchScanState(any())).thenReturn(
+    fun `when SCANNING scan state is fetched, then polling occurs until error is returned on failure`() = test {
+        val scanStateScanningModel = ScanStateModel(state = ScanStateModel.State.SCANNING)
+        val scanStateError = ScanStateError(ScanStateErrorType.GENERIC_ERROR)
+        whenever(scanStore.getScanStateForSite(any())).thenReturn(scanStateScanningModel)
+        whenever(scanStore.fetchScanState(any())).thenReturn(
                 OnScanStateFetched(FETCH_SCAN_STATE),
                 OnScanStateFetched(scanStateError, FETCH_SCAN_STATE)
-            )
+        )
 
-            val result = useCase.fetchScanState(site = site).toList(mutableListOf())
-            advanceTimeBy(FETCH_SCAN_STATE_DELAY_MILLIS)
+        val result = useCase.fetchScanState(site = site).toList(mutableListOf())
 
-            verify(scanStore, times(2)).fetchScanState(any())
-            assertThat(result).containsSequence(
+        verify(scanStore, times(2)).fetchScanState(any())
+        assertThat(result).containsSequence(
                 FetchScanState.Success(scanStateScanningModel),
                 FetchScanState.Failure.RemoteRequestFailure
-            )
-        }
+        )
+    }
 }
