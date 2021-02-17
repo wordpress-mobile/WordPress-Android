b2ce188037ee2dbd5f202e0c98be04691533a6d6
malinajirka <malinajirka@gmail.com>
2021-02-17 17:42:57 +0100

    Fix imports

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/AppPrefs.java
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/AppPrefs.java
@@ -29,7 +29,6 @@ import org.wordpress.android.util.WPMediaUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
