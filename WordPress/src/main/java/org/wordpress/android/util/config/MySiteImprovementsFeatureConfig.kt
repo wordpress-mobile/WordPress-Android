82d46d6759c06f3c1d44cb6f097f3f04f02dd5c6
Renan Ferrari <renan.fb@gmail.com>
2021-02-19 14:04:53 -0300

    Add remote field for My Site feature

--- WordPress/src/main/java/org/wordpress/android/util/config/MySiteImprovementsFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/MySiteImprovementsFeatureConfig.kt
@@ -1,15 +1,21 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.annotation.Feature
+import org.wordpress.android.util.config.MySiteImprovementsFeatureConfig.Companion.MY_SITE_IMPROVEMENTS_REMOTE_FIELD
 import javax.inject.Inject
 
 /**
  * Configuration of the my site infrastructure improvements
  */
-@FeatureInDevelopment
+@Feature(remoteField = MY_SITE_IMPROVEMENTS_REMOTE_FIELD)
 class MySiteImprovementsFeatureConfig
 @Inject constructor(appConfig: AppConfig) : FeatureConfig(
         appConfig,
-        BuildConfig.MY_SITE_IMPROVEMENTS
-)
+        BuildConfig.MY_SITE_IMPROVEMENTS,
+        MY_SITE_IMPROVEMENTS_REMOTE_FIELD
+) {
+    companion object {
+        const val MY_SITE_IMPROVEMENTS_REMOTE_FIELD = "my_site_improvements_enabled"
+    }
+}
