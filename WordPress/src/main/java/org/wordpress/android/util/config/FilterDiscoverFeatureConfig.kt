98134652066a8cb7c61343b8096c58c4f412598e
kaungmyatmin <kaungmyatmin727@gmail.com>
2021-02-24 21:42:44 +0630

    change Config for Filter in Discover to Development type

--- WordPress/src/main/java/org/wordpress/android/util/config/FilterDiscoverFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/FilterDiscoverFeatureConfig.kt
@@ -1,14 +1,13 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.Feature
-import org.wordpress.android.util.config.FilterDiscoverFeatureConfig.Companion.FILTER_DISCOVER
+import org.wordpress.android.annotation.FeatureInDevelopment
 import javax.inject.Inject
 
 /**
  * Configuration of the filter for discover or topic feature.
  */
-@Feature(remoteField = FILTER_DISCOVER, defaultValue = true)
+@FeatureInDevelopment
 class FilterDiscoverFeatureConfig
 @Inject constructor(appConfig: AppConfig) : FeatureConfig(
         appConfig,
