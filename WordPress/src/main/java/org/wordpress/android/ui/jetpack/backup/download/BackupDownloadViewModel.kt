ed675fb3ed131aa5c55f10794089b5820edbdf8d
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-02-25 10:23:11 -0500

    Add JetpackBackupDownloadActionState to support return to calling activity

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/backup/download/BackupDownloadViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/backup/download/BackupDownloadViewModel.kt
@@ -151,7 +151,7 @@ class BackupDownloadViewModel @Inject constructor(
         when (wizardManager.currentStep) {
             DETAILS.id -> _wizardFinishedObservable.value = Event(BackupDownloadCanceled)
             PROGRESS.id -> constructProgressEvent()
-            COMPLETE.id -> _wizardFinishedObservable.value = Event(BackupDownloadCompleted)
+            COMPLETE.id -> constructCompleteEvent()
             ERROR.id -> _wizardFinishedObservable.value = Event(BackupDownloadCanceled)
         }
     }
@@ -160,6 +160,19 @@ class BackupDownloadViewModel @Inject constructor(
         _wizardFinishedObservable.value = if (backupDownloadState.downloadId != null) {
             Event(
                     BackupDownloadInProgress(
+                            backupDownloadState.rewindId as String,
+                            backupDownloadState.downloadId as Long
+                    )
+            )
+        } else {
+            Event(BackupDownloadCanceled)
+        }
+    }
+
+    private fun constructCompleteEvent() {
+        _wizardFinishedObservable.value = if (backupDownloadState.downloadId != null) {
+            Event(
+                    BackupDownloadCompleted(
                             backupDownloadState.rewindId as String,
                             backupDownloadState.downloadId as Long
                     )
@@ -445,6 +458,9 @@ class BackupDownloadViewModel @Inject constructor(
         ) : BackupDownloadWizardState()
 
         @Parcelize
-        object BackupDownloadCompleted : BackupDownloadWizardState()
+        data class BackupDownloadCompleted(
+            val rewindId: String,
+            val downloadId: Long
+        ) : BackupDownloadWizardState()
     }
 }
