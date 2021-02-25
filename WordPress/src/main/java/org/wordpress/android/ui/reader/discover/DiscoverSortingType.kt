e0bbc707d6336ce6aef23624d24d39628ec81196
kaungmyatmin <kaungmyatmin727@gmail.com>
2021-02-25 14:33:39 +0630

    add new sorting type "NONE"

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/DiscoverSortingType.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/DiscoverSortingType.kt
@@ -4,9 +4,14 @@ package org.wordpress.android.ui.reader.discover
  * Use for filtering posts in Discover tab
  *
  * Example : https://public-api.wordpress.com/wpcom/v2/read/tags/cards?tags=dogs&sort=date
- * @property [sortedBy] : value used in api call
+ *
+ * If [NONE] is used, the parameter value will not be added to api call.
+ *
+ *  @property [sortedBy] : value used in api call
+ *
  */
 enum class DiscoverSortingType(val sortedBy: String) {
+    NONE("none"),
     POPULARITY("popularity"),
     TIME("date")
 }
