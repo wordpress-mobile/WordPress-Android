38cc19e7e1a29e49c09f98d92d2e6fbc19454a65
ashiagr <agr.ash@gmail.com>
2021-02-26 11:47:21 +0530

    Refactor: Remove explicit return and assign single data object directly

--- WordPress/src/main/java/org/wordpress/android/ui/reader/viewmodels/ReaderPostDetailUiStateBuilder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/viewmodels/ReaderPostDetailUiStateBuilder.kt
@@ -2,13 +2,11 @@ package org.wordpress.android.ui.reader.viewmodels
 
 import dagger.Reusable
 import org.wordpress.android.models.ReaderPost
-import org.wordpress.android.ui.reader.discover.ReaderPostActions
 import org.wordpress.android.ui.reader.discover.ReaderPostCardAction.SecondaryAction
 import org.wordpress.android.ui.reader.discover.ReaderPostCardActionType
 import org.wordpress.android.ui.reader.discover.ReaderPostUiStateBuilder
 import org.wordpress.android.ui.reader.viewmodels.ReaderPostDetailViewModel.ReaderPostDetailsUiState
 import org.wordpress.android.ui.reader.views.ReaderPostDetailsHeaderViewUiStateBuilder
-import org.wordpress.android.ui.reader.views.uistates.ReaderPostDetailsHeaderViewUiState.ReaderPostDetailsHeaderUiState
 import javax.inject.Inject
 
 @Reusable
@@ -23,39 +21,33 @@ class ReaderPostDetailUiStateBuilder @Inject constructor(
         onBlogSectionClicked: (Long, Long) -> Unit,
         onFollowClicked: () -> Unit,
         onTagItemClicked: (String) -> Unit
-    ): ReaderPostDetailsUiState {
-            return ReaderPostDetailsUiState(
-                postId = post.postId,
-                blogId = post.blogId,
-                headerUiState = buildPostDetailsHeaderUiState(
-                        post,
-                        onBlogSectionClicked,
-                        onFollowClicked,
-                        onTagItemClicked
-                ),
-                moreMenuItems = moreMenuItems,
-                actions = buildPostActions(post, onButtonClicked)
-        )
-    }
+    ) = ReaderPostDetailsUiState(
+            postId = post.postId,
+            blogId = post.blogId,
+            headerUiState = buildPostDetailsHeaderUiState(
+                    post,
+                    onBlogSectionClicked,
+                    onFollowClicked,
+                    onTagItemClicked
+            ),
+            moreMenuItems = moreMenuItems,
+            actions = buildPostActions(post, onButtonClicked)
+    )
 
     private fun buildPostDetailsHeaderUiState(
         post: ReaderPost,
         onBlogSectionClicked: (Long, Long) -> Unit,
         onFollowClicked: () -> Unit,
         onTagItemClicked: (String) -> Unit
-    ): ReaderPostDetailsHeaderUiState {
-        return postDetailsHeaderViewUiStateBuilder.mapPostToUiState(
-                post,
-                onBlogSectionClicked,
-                onFollowClicked,
-                onTagItemClicked
-        )
-    }
+    ) = postDetailsHeaderViewUiStateBuilder.mapPostToUiState(
+            post,
+            onBlogSectionClicked,
+            onFollowClicked,
+            onTagItemClicked
+    )
 
     fun buildPostActions(
         post: ReaderPost,
         onButtonClicked: (Long, Long, ReaderPostCardActionType) -> Unit
-    ): ReaderPostActions {
-        return postUiStateBuilder.mapPostToActions(post, onButtonClicked)
-    }
+    ) = postUiStateBuilder.mapPostToActions(post, onButtonClicked)
 }
