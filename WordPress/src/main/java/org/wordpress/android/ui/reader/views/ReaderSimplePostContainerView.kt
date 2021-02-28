38b9a4c4958639ec21a3ba67a36313f9e2a6cde7
ashiagr <agr.ash@gmail.com>
2021-02-28 07:24:15 +0530

    Feature: Add recyclerview to show related posts

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderSimplePostContainerView.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderSimplePostContainerView.kt
@@ -3,14 +3,20 @@ package org.wordpress.android.ui.reader.views
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.reader_simple_posts_container_view.view.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.ui.reader.ReaderInterfaces.OnFollowListener
+import org.wordpress.android.ui.reader.adapters.ReaderRelatedPostsAdapter
 import org.wordpress.android.ui.reader.models.ReaderSimplePostList
 import org.wordpress.android.ui.reader.viewmodels.ReaderPostDetailViewModel.ReaderPostDetailsUiState.RelatedPostsUiState
 import org.wordpress.android.ui.reader.views.ReaderSimplePostView.OnSimplePostClickListener
+import org.wordpress.android.ui.utils.UiHelpers
 import org.wordpress.android.util.analytics.AnalyticsUtils
+import org.wordpress.android.util.image.ImageManager
+import javax.inject.Inject
 
 /**
  * used by the detail view to display related posts, which can be either local (related posts
@@ -24,6 +30,9 @@ class ReaderSimplePostContainerView @JvmOverloads constructor(
     private var followListener: OnFollowListener? = null
     private val simplePostList = ReaderSimplePostList()
 
+    @Inject lateinit var uiHelpers: UiHelpers
+    @Inject lateinit var imageManager: ImageManager
+
     init {
         (context.applicationContext as WordPress).component().inject(this)
         initView(context)
@@ -31,26 +40,24 @@ class ReaderSimplePostContainerView @JvmOverloads constructor(
 
     private fun initView(context: Context) {
         inflate(context, R.layout.reader_simple_posts_container_view, this)
+        initRecyclerView(context)
+    }
+
+    private fun initRecyclerView(context: Context) {
+        recycler_view.layoutManager = object : LinearLayoutManager(context, RecyclerView.HORIZONTAL, false) {
+            override
+            fun checkLayoutParams(lp: RecyclerView.LayoutParams): Boolean {
+                lp.width = width / 2
+                return true
+            }
+        }
+        recycler_view.adapter = ReaderRelatedPostsAdapter(uiHelpers, imageManager)
     }
 
     fun showPosts(state: RelatedPostsUiState, listener: OnSimplePostClickListener?) {
-        // TODO: ashiagr show posts from RelatedPostsUiState
-        /*simplePostList.clear()
-        simplePostList.addAll(posts)
-        container_related_posts.removeAllViews()
+        if (state.cards?.size == 0) return
 
-        // nothing more to do if passed list is empty
-        if (simplePostList.size == 0) {
-            return
-        }
-
-        // add a view for each post
-        for (index in simplePostList.indices) {
-            val relatedPost = simplePostList[index]
-            val postView = ReaderSimplePostView(context)
-            postView.setOnFollowListener(followListener)
-            postView.showPost(relatedPost, container_related_posts, isGlobal, listener)
-        }*/
+        state.cards?.let { (recycler_view.adapter as ReaderRelatedPostsAdapter).update(it) }
 
         // make sure the label for these posts has the correct caption
         if (state.isGlobal) {
