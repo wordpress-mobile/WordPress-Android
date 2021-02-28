b8527f76edd12914346b147fa127727550f02257
ashiagr <agr.ash@gmail.com>
2021-02-28 17:32:56 +0530

    Feature: Add related posts adapter and view holder

---
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/viewholders/ReaderRelatedPostViewHolder.kt
@@ -0,0 +1,44 @@
+package org.wordpress.android.ui.reader.discover.viewholders
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.reader_cardview_related_post.*
+import org.wordpress.android.R
+import org.wordpress.android.WordPress
+import org.wordpress.android.ui.reader.viewmodels.ReaderPostDetailViewModel.ReaderPostDetailsUiState.RelatedPostsUiState.ReaderRelatedPostUiState
+
+import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.util.image.ImageManager
+import org.wordpress.android.util.image.ImageType.PHOTO
+
+class ReaderRelatedPostViewHolder(
+    private val uiHelpers: UiHelpers,
+    private val imageManager: ImageManager,
+    private val parent: ViewGroup,
+    override val containerView: View = LayoutInflater.from(parent.context).inflate(
+            R.layout.reader_cardview_related_post,
+            parent,
+            false
+    )
+) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    fun onBind(uiState: ReaderRelatedPostUiState) {
+        updateFeaturedImage(uiState)
+        uiHelpers.setTextOrHide(text_title, uiState.title)
+    }
+
+    private fun updateFeaturedImage(state: ReaderRelatedPostUiState) {
+        if (state.featuredImageUrl == null) {
+            imageManager.cancelRequestAndClearImageView(image_featured)
+        } else {
+            imageManager.loadImageWithCorners(
+                    image_featured,
+                    PHOTO,
+                    state.featuredImageUrl,
+                    uiHelpers.getPxOfUiDimen(WordPress.getContext(), state.featuredImageCornerRadius)
+            )
+        }
+    }
+}
