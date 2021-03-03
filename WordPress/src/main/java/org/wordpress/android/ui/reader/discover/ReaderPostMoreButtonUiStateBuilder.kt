8ebed1253445cf30d10872af3781ee5b898420c7
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-03-03 14:34:59 -0500

    Break up fun based on detekt feedback

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderPostMoreButtonUiStateBuilder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderPostMoreButtonUiStateBuilder.kt
@@ -47,138 +47,138 @@ class ReaderPostMoreButtonUiStateBuilder @Inject constructor(
         val menuItems = mutableListOf<ReaderPostCardAction>()
         val isPostFollowed = readerPostTableWrapper.isPostFollowed(post)
 
-        menuItems.add(
-                SecondaryAction(
-                        type = VISIT_SITE,
-                        label = UiStringRes(R.string.reader_label_visit),
-                        labelColor = R.attr.colorOnSurface,
-                        iconRes = R.drawable.ic_globe_white_24dp,
-                        iconColor = R.attr.wpColorOnSurfaceMedium,
-                        onClicked = onButtonClicked
-                )
-        )
+        menuItems.add(buildVisitSite(onButtonClicked))
 
         if (isPostFollowed) {
             // When post not from external feed then show notifications option.
             if (!readerUtilsWrapper.isExternalFeed(post.blogId, post.feedId)) {
-                if (readerBlogTableWrapper.isNotificationsEnabled(post.blogId)) {
-                    menuItems.add(
-                            SecondaryAction(
-                                    type = SITE_NOTIFICATIONS,
-                                    label = UiStringRes(R.string.reader_btn_notifications_off),
-                                    labelColor = R.attr.wpColorOnSurfaceMedium,
-                                    iconRes = R.drawable.ic_bell_white_24dp,
-                                    isSelected = true,
-                                    onClicked = onButtonClicked
-                            )
-                    )
-                } else {
-                    menuItems.add(
-                            SecondaryAction(
-                                    type = SITE_NOTIFICATIONS,
-                                    label = UiStringRes(R.string.reader_btn_notifications_on),
-                                    labelColor = R.attr.colorOnSurface,
-                                    iconRes = R.drawable.ic_bell_white_24dp,
-                                    iconColor = R.attr.wpColorOnSurfaceMedium,
-                                    isSelected = false,
-                                    onClicked = onButtonClicked
-                            )
-                    )
-                }
+                menuItems.add(buildSiteNotifications(
+                        readerBlogTableWrapper.isNotificationsEnabled(post.blogId), onButtonClicked))
             }
         }
 
         if (seenUnseenWithCounterFeatureConfig.isEnabled()) {
-            if (post.isSeenSupported) {
-                if (readerPostTableWrapper.isPostSeen(post)) {
-                    menuItems.add(
-                            SecondaryAction(
-                                    type = TOGGLE_SEEN_STATUS,
-                                    label = UiStringRes(R.string.reader_menu_mark_as_unseen),
-                                    labelColor = R.attr.colorOnSurface,
-                                    iconRes = R.drawable.ic_not_visible_white_24dp,
-                                    iconColor = R.attr.wpColorOnSurfaceMedium,
-                                    isSelected = false,
-                                    onClicked = onButtonClicked
-                            )
-                    )
-                } else {
-                    menuItems.add(
-                            SecondaryAction(
-                                    type = TOGGLE_SEEN_STATUS,
-                                    label = UiStringRes(R.string.reader_menu_mark_as_seen),
-                                    labelColor = R.attr.colorOnSurface,
-                                    iconRes = R.drawable.ic_visible_white_24dp,
-                                    iconColor = R.attr.wpColorOnSurfaceMedium,
-                                    isSelected = false,
-                                    onClicked = onButtonClicked
-                            )
-                    )
-                }
-            }
+            if (post.isSeenSupported) buildPostSeenUnseen(readerPostTableWrapper.isPostSeen(post), onButtonClicked)
         }
 
-        menuItems.add(
-                SecondaryAction(
-                        type = SHARE,
-                        label = UiStringRes(R.string.reader_btn_share),
-                        labelColor = R.attr.colorOnSurface,
-                        iconRes = R.drawable.ic_share_white_24dp,
-                        iconColor = R.attr.wpColorOnSurfaceMedium,
-                        onClicked = onButtonClicked
-                )
-        )
+        menuItems.add(buildShare(onButtonClicked))
+        menuItems.add(buildFollow(isPostFollowed, onButtonClicked))
+        menuItems.add(SpacerNoAction())
+        if (!isPostFollowed) menuItems.add(buildBlockSite(onButtonClicked))
 
-        if (isPostFollowed) {
-            menuItems.add(
-                    SecondaryAction(
-                            type = FOLLOW,
-                            label = UiStringRes(R.string.reader_btn_unfollow),
-                            labelColor = R.attr.wpColorOnSurfaceMedium,
-                            iconRes = R.drawable.ic_reader_following_white_24dp,
-                            isSelected = true,
-                            onClicked = onButtonClicked
-                    )
-            )
-        } else {
-            menuItems.add(
-                    SecondaryAction(
-                            type = FOLLOW,
-                            label = UiStringRes(R.string.reader_btn_follow),
-                            labelColor = R.attr.colorSecondary,
-                            iconRes = R.drawable.ic_reader_follow_white_24dp,
-                            isSelected = false,
-                            onClicked = onButtonClicked
-                    )
-            )
-        }
-
-        if (!isPostFollowed) {
-            menuItems.add(SpacerNoAction())
-            menuItems.add(
-                    SecondaryAction(
-                            type = BLOCK_SITE,
-                            label = UiStringRes(R.string.reader_menu_block_blog),
-                            labelColor = R.attr.wpColorError,
-                            iconRes = R.drawable.ic_block_white_24dp,
-                            iconColor = R.attr.wpColorError,
-                            onClicked = onButtonClicked
-                    )
-            )
-        } else {
-            menuItems.add(SpacerNoAction())
-        }
-
-        menuItems.add(
-                SecondaryAction(
-                        type = REPORT_POST,
-                        label = UiStringRes(R.string.reader_menu_report_post),
-                        labelColor = R.attr.wpColorError,
-                        iconRes = R.drawable.ic_block_white_24dp,
-                        iconColor = R.attr.wpColorError,
-                        onClicked = onButtonClicked,
-                )
-        )
+        menuItems.add(buildReportPost(onButtonClicked))
         return menuItems
     }
+
+    private fun buildVisitSite(onButtonClicked: (Long, Long, ReaderPostCardActionType) -> Unit) =
+            SecondaryAction(
+                    type = VISIT_SITE,
+                    label = UiStringRes(R.string.reader_label_visit),
+                    labelColor = R.attr.colorOnSurface,
+                    iconRes = R.drawable.ic_globe_white_24dp,
+                    iconColor = R.attr.wpColorOnSurfaceMedium,
+                    onClicked = onButtonClicked
+            )
+
+    private fun buildSiteNotifications(
+        isNotificationsEnabled: Boolean,
+        onButtonClicked: (Long, Long, ReaderPostCardActionType) -> Unit
+    ) =
+            if (isNotificationsEnabled) {
+                SecondaryAction(
+                        type = SITE_NOTIFICATIONS,
+                        label = UiStringRes(R.string.reader_btn_notifications_off),
+                        labelColor = R.attr.wpColorOnSurfaceMedium,
+                        iconRes = R.drawable.ic_bell_white_24dp,
+                        isSelected = true,
+                        onClicked = onButtonClicked
+                )
+            } else {
+                SecondaryAction(
+                        type = SITE_NOTIFICATIONS,
+                        label = UiStringRes(R.string.reader_btn_notifications_on),
+                        labelColor = R.attr.colorOnSurface,
+                        iconRes = R.drawable.ic_bell_white_24dp,
+                        iconColor = R.attr.wpColorOnSurfaceMedium,
+                        isSelected = false,
+                        onClicked = onButtonClicked
+                )
+            }
+
+    private fun buildPostSeenUnseen(
+        isPostSeen: Boolean,
+        onButtonClicked: (Long, Long, ReaderPostCardActionType) -> Unit
+    ) =
+        if (isPostSeen) {
+            SecondaryAction(
+                    type = TOGGLE_SEEN_STATUS,
+                    label = UiStringRes(R.string.reader_menu_mark_as_unseen),
+                    labelColor = R.attr.colorOnSurface,
+                    iconRes = R.drawable.ic_not_visible_white_24dp,
+                    iconColor = R.attr.wpColorOnSurfaceMedium,
+                    isSelected = false,
+                    onClicked = onButtonClicked
+            )
+        } else {
+            SecondaryAction(
+                    type = TOGGLE_SEEN_STATUS,
+                    label = UiStringRes(R.string.reader_menu_mark_as_seen),
+                    labelColor = R.attr.colorOnSurface,
+                    iconRes = R.drawable.ic_visible_white_24dp,
+                    iconColor = R.attr.wpColorOnSurfaceMedium,
+                    isSelected = false,
+                    onClicked = onButtonClicked
+            )
+        }
+
+    private fun buildShare(onButtonClicked: (Long, Long, ReaderPostCardActionType) -> Unit) =
+        SecondaryAction(
+        type = SHARE,
+        label = UiStringRes(R.string.reader_btn_share),
+        labelColor = R.attr.colorOnSurface,
+        iconRes = R.drawable.ic_share_white_24dp,
+        iconColor = R.attr.wpColorOnSurfaceMedium,
+        onClicked = onButtonClicked
+    )
+
+    private fun buildFollow(isPostFollowed: Boolean, onButtonClicked: (Long, Long, ReaderPostCardActionType) -> Unit) =
+        if (isPostFollowed) {
+            SecondaryAction(
+                    type = FOLLOW,
+                    label = UiStringRes(R.string.reader_btn_unfollow),
+                    labelColor = R.attr.wpColorOnSurfaceMedium,
+                    iconRes = R.drawable.ic_reader_following_white_24dp,
+                    isSelected = true,
+                    onClicked = onButtonClicked
+            )
+        } else {
+            SecondaryAction(
+                    type = FOLLOW,
+                    label = UiStringRes(R.string.reader_btn_follow),
+                    labelColor = R.attr.colorSecondary,
+                    iconRes = R.drawable.ic_reader_follow_white_24dp,
+                    isSelected = false,
+                    onClicked = onButtonClicked
+            )
+        }
+
+    private fun buildBlockSite(onButtonClicked: (Long, Long, ReaderPostCardActionType) -> Unit) =
+            SecondaryAction(
+                    type = BLOCK_SITE,
+                    label = UiStringRes(R.string.reader_menu_block_blog),
+                    labelColor = R.attr.wpColorError,
+                    iconRes = R.drawable.ic_block_white_24dp,
+                    iconColor = R.attr.wpColorError,
+                    onClicked = onButtonClicked
+            )
+
+    private fun buildReportPost(onButtonClicked: (Long, Long, ReaderPostCardActionType) -> Unit) =
+            SecondaryAction(
+                    type = REPORT_POST,
+                    label = UiStringRes(R.string.reader_menu_report_post),
+                    labelColor = R.attr.wpColorError,
+                    iconRes = R.drawable.ic_block_white_24dp,
+                    iconColor = R.attr.wpColorError,
+                    onClicked = onButtonClicked,
+            )
 }
