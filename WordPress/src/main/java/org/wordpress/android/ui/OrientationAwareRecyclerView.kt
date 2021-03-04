cb22010328fc0f388b8ea014cb55bed92e973f1a
vojtasmrcek <planarvoid@gmail.com>
2021-03-04 16:34:18 +0100

    Add missing licence

--- WordPress/src/main/java/org/wordpress/android/ui/OrientationAwareRecyclerView.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/OrientationAwareRecyclerView.kt
@@ -1,19 +1,35 @@
 package org.wordpress.android.ui
 
+/*
+ * Copyright 2018 Rúben Sousa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.abs
 
-class OrientationAwareRecyclerView : RecyclerView{
+class OrientationAwareRecyclerView : RecyclerView {
     private var lastX = 0.0f
     private var lastY = 0.0f
     private var scrolling = false
 
     constructor(context: Context) : this(context, null)
 
-    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
     constructor(
         context: Context, attrs: AttributeSet?, defStyleAttr: Int
