61ebbbc880fdd73db0b67f7e9ddae4b7794e3904
develric <develric@gmail.com>
2021-03-15 02:56:21 +0100

    Creating Optimizer hierarchy for m4m and mp4composer libs

---
+++ WordPress/src/main/java/org/wordpress/android/ui/uploads/VideoOptimizerProvider.kt
@@ -0,0 +1,8 @@
+package org.wordpress.android.ui.uploads
+
+// TODO: move here the VideoOptimizationListener interface and ProgressEvent. Leaving them in their original
+// position for now to not change the files as they are
+
+interface VideoOptimizerProvider {
+    fun start()
+}
