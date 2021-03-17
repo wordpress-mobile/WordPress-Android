ca277d4c38881292b4046fddcca19ead1415446d
Renan Ferrari <renan.fb@gmail.com>
2021-03-17 13:52:42 -0300

    Rename bottom buttons container buttons ids

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginPrologueFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginPrologueFragment.kt
@@ -54,12 +54,12 @@ class LoginPrologueFragment : Fragment(R.layout.login_signup_screen) {
         val binding = LoginSignupScreenBinding.bind(view)
 
         with(binding.bottomButtonsContainer) {
-            firstButton.setOnClickListener {
+            continueWithWpcomButton.setOnClickListener {
                 unifiedLoginTracker.trackClick(Click.CONTINUE_WITH_WORDPRESS_COM)
                 loginPrologueListener.showEmailLoginScreen()
             }
 
-            secondButton.setOnClickListener {
+            enterYourSiteAddressButton.setOnClickListener {
                 unifiedLoginTracker.trackClick(Click.LOGIN_WITH_SITE_ADDRESS)
                 loginPrologueListener.loginViaSiteAddress()
             }
