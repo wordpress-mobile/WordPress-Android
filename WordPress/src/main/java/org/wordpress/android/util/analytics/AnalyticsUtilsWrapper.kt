977024202915c6defd877a8d8b51397a68da4834
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2021-03-18 16:01:50 +0200

    Refactor: Reformat analytics utils wrapper

--- WordPress/src/main/java/org/wordpress/android/util/analytics/AnalyticsUtilsWrapper.kt
+++ WordPress/src/main/java/org/wordpress/android/util/analytics/AnalyticsUtilsWrapper.kt
@@ -16,20 +16,34 @@ import javax.inject.Inject
  *
  * AnalyticsUtils interface is consisted of static methods, which make the client code difficult to test/mock.
  * Main purpose of this wrapper is to make testing easier.
- *
  */
 @Reusable
-class AnalyticsUtilsWrapper @Inject constructor(private val appContext: Context) {
-    fun getMediaProperties(isVideo: Boolean, mediaURI: UriWrapper?, path: String?): MutableMap<String, Any?> =
-            AnalyticsUtils.getMediaProperties(appContext, isVideo, mediaURI?.uri, path)
+class AnalyticsUtilsWrapper @Inject constructor(
+    private val appContext: Context
+) {
+    fun getMediaProperties(
+        isVideo: Boolean,
+        mediaURI: UriWrapper?,
+        path: String?
+    ): MutableMap<String, Any?> = AnalyticsUtils.getMediaProperties(appContext, isVideo, mediaURI?.uri, path)
 
-    fun trackEditorCreatedPost(action: String?, intent: Intent, site: SiteModel, post: PostImmutableModel?) =
-            AnalyticsUtils.trackEditorCreatedPost(action, intent, site, post)
+    fun trackEditorCreatedPost(
+        action: String?,
+        intent: Intent,
+        site: SiteModel,
+        post: PostImmutableModel?
+    ) = AnalyticsUtils.trackEditorCreatedPost(action, intent, site, post)
 
-    fun trackWithSiteId(stat: AnalyticsTracker.Stat, blogId: Long) = AnalyticsUtils.trackWithSiteId(stat, blogId)
+    fun trackWithSiteId(
+        stat: AnalyticsTracker.Stat,
+        blogId: Long
+    ) = AnalyticsUtils.trackWithSiteId(stat, blogId)
 
-    fun trackWithReaderPostDetails(stat: AnalyticsTracker.Stat, post: ReaderPost?, properties: Map<String, Any?>?) =
-            AnalyticsUtils.trackWithReaderPostDetails(stat, post, properties)
+    fun trackWithReaderPostDetails(
+        stat: AnalyticsTracker.Stat,
+        post: ReaderPost?,
+        properties: Map<String, Any?>?
+    ) = AnalyticsUtils.trackWithReaderPostDetails(stat, post, properties)
 
     fun trackFollowCommentsWithReaderPostDetails(
         stat: AnalyticsTracker.Stat,
