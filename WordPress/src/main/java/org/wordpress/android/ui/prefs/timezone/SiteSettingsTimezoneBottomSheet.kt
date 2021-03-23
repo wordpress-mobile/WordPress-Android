b8dbfc657d6e9322ccfc55403ca87c51c44bfd1a
Ravi <ravi.program@gmail.com>
2021-03-23 15:16:44 +1100

    UI updates to address review comments
    - fix keyboard covering search results by dismissing it
    - fix ripple effect not extending full width
    - Remove bottom margin on list expanded
    - expand bottom sheet on landscape mode
    - Align suggested timezone  to start with the Suggestion label
    - show scrollbar

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/timezone/SiteSettingsTimezoneBottomSheet.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/timezone/SiteSettingsTimezoneBottomSheet.kt
@@ -1,16 +1,20 @@
 package org.wordpress.android.ui.prefs.timezone
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.FrameLayout
 import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.button.MaterialButton
 import kotlinx.coroutines.Job
@@ -43,6 +47,8 @@ class SiteSettingsTimezoneBottomSheet : BottomSheetDialogFragment() {
     private var _binding: SiteSettingsTimezoneBottomSheetListBinding? = null
     private val binding get() = _binding
 
+    private var bottomSheet: FrameLayout? = null
+
     private var callback: TimezoneSelectionCallback? = null
 
     fun setTimezoneSettingCallback(callback: TimezoneSelectionCallback) {
@@ -70,19 +76,46 @@ class SiteSettingsTimezoneBottomSheet : BottomSheetDialogFragment() {
         timezoneViewModel.getTimezones(requireActivity())
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) expandBottomSheet()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        callback = null
+        super.onDestroyView()
+    }
+
     private fun setupUI() {
         binding?.apply {
+            dialog?.setOnShowListener { dialogInterface ->
+                val sheetDialog = dialogInterface as? BottomSheetDialog
+
+                bottomSheet = sheetDialog?.findViewById<View>(
+                        com.google.android.material.R.id.design_bottom_sheet
+                ) as? FrameLayout
+            }
+
             list.adapter = timezoneAdapter
-            list.addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+            list.run {
+                addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+
+                addOnScrollListener(object: RecyclerView.OnScrollListener() {
+                    override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                        super.onScrollStateChanged(recyclerView, newState)
+                        if (newState == RecyclerView.SCROLL_STATE_DRAGGING) hideSearchKeyboard()
+                    }
+                })
+            }
 
             searchInputLayout.editText?.doOnTextChanged { inputText, _, _, _ ->
                 search(inputText)
             }
 
             searchInputLayout.editText?.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
-                if (!hasFocus) {
-                    hideSearchKeyboard()
-                }
+                if (hasFocus) expandBottomSheet()
             }
 
             searchInputLayout.editText?.setOnKeyListener { _, keyCode, event ->
@@ -143,18 +176,19 @@ class SiteSettingsTimezoneBottomSheet : BottomSheetDialogFragment() {
         })
     }
 
+    private fun expandBottomSheet() {
+        bottomSheet?.let {
+            val behavior = BottomSheetBehavior.from(it)
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
+
     private fun updateTimezonesFromInput() {
         binding?.searchInputLayout?.editText?.text?.trim().let {
             if (it?.isNotEmpty() == true) {
                 search(it)
             }
         }
-    }
-
-    override fun onDestroyView() {
-        _binding = null
-        callback = null
-        super.onDestroyView()
     }
 
     private fun setSelectedTimezone(timezone: TimezoneItem?) {
