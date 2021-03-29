d367e8852787f26d9ba84665288d55e17123cb1f
khaykov <forsver@gmail.com>
2021-03-29 11:03:23 -0500

    Explicitly marking missing parent.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/comment/CommentRestClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/comment/CommentRestClient.java
@@ -307,6 +307,8 @@ public class CommentRestClient extends BaseWPComRestClient {
         if (response.parent != null) {
             comment.setHasParent(true);
             comment.setParentId(response.parent.ID);
+        } else {
+            comment.setHasParent(false);
         }
 
         return comment;
