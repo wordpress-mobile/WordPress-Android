541696cd6840d3a6109cc4c680c43780d9253792
vojtasmrcek <planarvoid@gmail.com>
2021-03-31 18:00:14 +0200

    Add more fields to UriWrapper.kt

--- WordPress/src/main/java/org/wordpress/android/util/UriWrapper.kt
+++ WordPress/src/main/java/org/wordpress/android/util/UriWrapper.kt
@@ -6,5 +6,10 @@ import android.net.Uri
  * This class is necessary because standard Uri doesn't work in unit tests (it's always null)
  */
 data class UriWrapper(val uri: Uri) {
+    constructor(uriString: String) : this(Uri.parse(uriString))
+    val pathSegments: List<String> = uri.pathSegments
+    val host: String? = uri.host
+
     override fun toString() = uri.toString()
+    fun getQueryParameter(key: String): String? = uri.getQueryParameter(key)
 }
