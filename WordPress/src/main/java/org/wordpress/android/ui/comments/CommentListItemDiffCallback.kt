e85d59711d87c4b03e3cee5cf4f1ee5dadfbfc8c
khaykov <forsver@gmail.com>
2021-04-07 11:04:07 -0500

    Added Diff logic for comments in the list.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/CommentListItemDiffCallback.kt
@@ -0,0 +1,49 @@
+package org.wordpress.android.ui.comments
+
+import androidx.recyclerview.widget.DiffUtil
+import org.wordpress.android.ui.comments.CommentListItem.Comment
+import org.wordpress.android.ui.comments.CommentListItem.SubHeader
+
+class CommentListItemDiffCallback(
+    private val oldList: List<CommentListItem>,
+    private val newList: List<CommentListItem>
+) : DiffUtil.Callback() {
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+
+        return when {
+            oldItem is SubHeader && newItem is SubHeader -> oldItem.longId() == newItem.longId()
+            oldItem is Comment && newItem is Comment -> {
+                oldItem.comment.remoteCommentId == newItem.comment.remoteCommentId
+            }
+            else -> false
+        }
+    }
+
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+
+        if (oldItem is SubHeader && newItem is SubHeader) {
+            return oldItem.label == newItem.label
+        }
+
+        if (oldItem is Comment && newItem is Comment) {
+            return oldItem.comment.status == newItem.comment.status &&
+                    oldItem.comment.content == newItem.comment.content &&
+                    oldItem.comment.datePublished == newItem.comment.datePublished &&
+                    oldItem.comment.authorName == newItem.comment.authorName
+        }
+
+        return false
+    }
+}
