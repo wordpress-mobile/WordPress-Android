9582590c1160baae266e0898966f139190b7937d
develric <develric@gmail.com>
2021-04-12 01:00:42 +0200

    Adding preferred blog and bio fields to endpoint response parsing.

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/common/LikeWPComRestResponse.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/common/LikeWPComRestResponse.kt
@@ -5,10 +5,25 @@ class LikeWPComRestResponse {
         var likes: List<LikeWPComRestResponse>? = null
     }
 
+    inner class PreferredBlogResponse {
+        var id: Long = 0
+        var name: String? = null
+        var url: String? = null
+        var icon: PreferredBlogIcon? = null
+    }
+
+    inner class PreferredBlogIcon {
+        var ico: String? = null
+        var img: String? = null
+    }
+
     var ID: Long = 0
     var login: String? = null
     var name: String? = null
     var avatar_URL: String? = null
+    var bio: String? = null
     var site_ID: Long = 0
     var primary_blog: String? = null
+
+    var preferred_blog: PreferredBlogResponse? = null
 }
