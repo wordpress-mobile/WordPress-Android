3d7a976c7375a6431c4fc861e98a83b7da82583e
Mario Zorz <mariozorz@gmail.com>
2021-04-14 17:22:45 +0200

    add flag to abort loop

--- WordPress/src/main/java/org/wordpress/android/ui/stories/SaveStoryGutenbergBlockUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stories/SaveStoryGutenbergBlockUseCase.kt
@@ -105,12 +105,14 @@ class SaveStoryGutenbergBlockUseCase @Inject constructor(
         // --> remove mediaFiles entirely
         // set start index and go up.
         var storyBlockStartIndex = 0
-        while (storyBlockStartIndex > -1 && storyBlockStartIndex < content.length) {
+        var hasMediaFiles = true
+        while (storyBlockStartIndex > -1 && storyBlockStartIndex < content.length && hasMediaFiles) {
             storyBlockStartIndex = content.indexOf(HEADING_START, storyBlockStartIndex)
             if (storyBlockStartIndex > -1) {
                 val storyBlockEndIndex = content.indexOf(HEADING_END, storyBlockStartIndex)
                 val mediaFilesStartIndex = storyBlockStartIndex + HEADING_START.length
-                if (mediaFilesStartIndex < storyBlockEndIndex) {
+                hasMediaFiles = mediaFilesStartIndex < storyBlockEndIndex
+                if (hasMediaFiles) {
                     try {
                         val jsonString: String = content.substring(
                                 mediaFilesStartIndex,
