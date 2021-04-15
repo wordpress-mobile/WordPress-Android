3a02499b4a467c8e6743e26514cef7ae5a677c4b
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-04-15 15:11:29 +0200

    Share single instance of okhttp client

--- fluxc/src/main/java/org/wordpress/android/fluxc/module/DebugOkHttpClientModule.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/module/DebugOkHttpClientModule.java
@@ -5,7 +5,6 @@ import org.wordpress.android.fluxc.network.MemorizingTrustManager;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.AppLog.T;
 
-import java.net.CookieManager;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -23,8 +22,8 @@ import dagger.Provides;
 import dagger.multibindings.Multibinds;
 import okhttp3.CookieJar;
 import okhttp3.Interceptor;
-import okhttp3.JavaNetCookieJar;
 import okhttp3.OkHttpClient;
+import okhttp3.OkHttpClient.Builder;
 import okhttp3.internal.tls.OkHostnameVerifier;
 
 @Module
@@ -34,82 +33,60 @@ public abstract class DebugOkHttpClientModule {
 
     @Multibinds abstract @Named("network-interceptors") Set<Interceptor> networkInterceptorSet();
 
-    @Provides
-    @Named("regular")
-    public static OkHttpClient.Builder provideOkHttpClientBuilder(
-            @Named("interceptors") Set<Interceptor> interceptors,
-            @Named("network-interceptors") Set<Interceptor> networkInterceptors,
-            final CookieJar cookieJar) {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        builder.cookieJar(cookieJar);
-        for (Interceptor interceptor : interceptors) {
-            builder.addInterceptor(interceptor);
-        }
-        for (Interceptor networkInterceptor : networkInterceptors) {
-            builder.addNetworkInterceptor(networkInterceptor);
-        }
-        return builder;
-    }
 
     @Provides
     @Named("no-redirects")
-    public static OkHttpClient.Builder provideNoRedirectsOkHttpClientBuilder(
-            @Named("interceptors") Set<Interceptor> interceptors,
-            @Named("network-interceptors") Set<Interceptor> networkInterceptors,
-            final CookieJar cookieJar) {
-        OkHttpClient.Builder builder = provideOkHttpClientBuilder(interceptors, networkInterceptors, cookieJar);
-        builder.followRedirects(false);
-        return builder;
-    }
-
-    @Provides
-    @Named("custom-ssl")
-    public static OkHttpClient.Builder provideOkHttpClientBuilderCustomSSL(
-            MemorizingTrustManager memorizingTrustManager,
-            @Named("interceptors") Set<Interceptor> interceptors,
-            @Named("network-interceptors") Set<Interceptor> networkInterceptors,
-            final CookieJar cookieJar) {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        builder.cookieJar(cookieJar);
-        try {
-            final SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, new TrustManager[]{memorizingTrustManager}, new SecureRandom());
-            builder.hostnameVerifier(memorizingTrustManager.wrapHostnameVerifier(OkHostnameVerifier.INSTANCE));
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-            builder.sslSocketFactory(sslSocketFactory, memorizingTrustManager);
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-            AppLog.e(T.API, e);
-        }
-        for (Interceptor interceptor : interceptors) {
-            builder.addInterceptor(interceptor);
-        }
-        for (Interceptor networkInterceptor : networkInterceptors) {
-            builder.addNetworkInterceptor(networkInterceptor);
-        }
-        return builder;
+    public static OkHttpClient provideNoRedirectsOkHttpClientBuilder(
+            @Named("regular") final OkHttpClient okHttpRegularClient) {
+        return okHttpRegularClient.newBuilder()
+                                  .connectTimeout(10000, TimeUnit.MILLISECONDS)
+                                  .readTimeout(10000, TimeUnit.MILLISECONDS)
+                                  .writeTimeout(10000, TimeUnit.MILLISECONDS)
+                                  .followRedirects(false)
+                                  .build();
     }
 
     @Singleton
     @Provides
     @Named("custom-ssl")
     public static OkHttpClient provideMediaOkHttpClientInstanceCustomSSL(
-            @Named("custom-ssl") OkHttpClient.Builder builder) {
-        return builder
-                .connectTimeout(BaseRequest.DEFAULT_REQUEST_TIMEOUT, TimeUnit.MILLISECONDS)
-                .readTimeout(BaseRequest.UPLOAD_REQUEST_READ_TIMEOUT, TimeUnit.MILLISECONDS)
-                .writeTimeout(BaseRequest.DEFAULT_REQUEST_TIMEOUT, TimeUnit.MILLISECONDS)
-                .build();
+            @Named("regular") final OkHttpClient okHttpClient,
+            final MemorizingTrustManager memorizingTrustManager) {
+        final OkHttpClient.Builder builder = okHttpClient.newBuilder();
+        try {
+            final SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{memorizingTrustManager}, new SecureRandom());
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+            builder.hostnameVerifier(memorizingTrustManager.wrapHostnameVerifier(OkHostnameVerifier.INSTANCE));
+            builder.sslSocketFactory(sslSocketFactory, memorizingTrustManager);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            AppLog.e(T.API, e);
+        }
+        return builder.build();
     }
 
     @Singleton
     @Provides
     @Named("regular")
     public static OkHttpClient provideMediaOkHttpClientInstance(
-            @Named("regular") OkHttpClient.Builder builder) {
-        return builder
-                .connectTimeout(BaseRequest.DEFAULT_REQUEST_TIMEOUT, TimeUnit.MILLISECONDS)
-                .readTimeout(BaseRequest.UPLOAD_REQUEST_READ_TIMEOUT, TimeUnit.MILLISECONDS)
-                .writeTimeout(BaseRequest.DEFAULT_REQUEST_TIMEOUT, TimeUnit.MILLISECONDS)
-                .build();
+            final CookieJar cookieJar,
+            @Named("interceptors") Set<Interceptor> interceptors,
+            @Named("network-interceptors") Set<Interceptor> networkInterceptors) {
+        final OkHttpClient.Builder builder = new Builder().cookieJar(cookieJar)
+                                                          .connectTimeout(BaseRequest.DEFAULT_REQUEST_TIMEOUT,
+                                                                  TimeUnit.MILLISECONDS)
+                                                          .readTimeout(BaseRequest.UPLOAD_REQUEST_READ_TIMEOUT,
+                                                                  TimeUnit.MILLISECONDS)
+                                                          .writeTimeout(BaseRequest.DEFAULT_REQUEST_TIMEOUT,
+                                                                  TimeUnit.MILLISECONDS);
+        for (final Interceptor interceptor : interceptors) {
+            builder.addInterceptor(interceptor);
+        }
+
+        for (final Interceptor networkInterceptor : networkInterceptors) {
+            builder.addNetworkInterceptor(networkInterceptor);
+        }
+
+        return builder.build();
     }
 }
