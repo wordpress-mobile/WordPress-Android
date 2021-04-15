fcfade1c2b8755ccf504a7a292eb22d0a7220de2
Mario Zorz <mariozorz@gmail.com>
2021-04-15 14:05:19 +0200

    make sure to only relax the definiton of block start tag - end marker for search only

--- WordPress/src/main/java/org/wordpress/android/ui/stories/SaveStoryGutenbergBlockUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stories/SaveStoryGutenbergBlockUseCase.kt
@@ -108,7 +108,7 @@ class SaveStoryGutenbergBlockUseCase @Inject constructor(
         while (storyBlockStartIndex > -1 && storyBlockStartIndex < content.length) {
             storyBlockStartIndex = content.indexOf(HEADING_START, storyBlockStartIndex)
             if (storyBlockStartIndex > -1) {
-                val storyBlockEndIndex = content.indexOf(HEADING_END, storyBlockStartIndex)
+                val storyBlockEndIndex = content.indexOf(HEADING_END_NO_NEW_LINE, storyBlockStartIndex)
                 try {
                     val jsonString: String = content.substring(
                             storyBlockStartIndex + HEADING_START.length,
@@ -247,7 +247,8 @@ class SaveStoryGutenbergBlockUseCase @Inject constructor(
     companion object {
         const val TEMPORARY_ID_PREFIX = "tempid-"
         const val HEADING_START = "<!-- wp:jetpack/story "
-        const val HEADING_END = " -->"
+        const val HEADING_END = " -->\n"
+        const val HEADING_END_NO_NEW_LINE = " -->"
         const val DIV_PART = "<div class=\"wp-story wp-block-jetpack-story\"></div>\n"
         const val CLOSING_TAG = "<!-- /wp:jetpack/story -->"
     }
