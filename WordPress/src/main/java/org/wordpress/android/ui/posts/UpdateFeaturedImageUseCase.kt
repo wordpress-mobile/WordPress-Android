0f54607b6073d96694e96b24ccd86a5880a4f678
Joel Dean <jdeanjj1000@gmail.com>
2021-04-23 13:15:28 -0500

    added an update featured image use case.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/UpdateFeaturedImageUseCase.kt
@@ -0,0 +1,23 @@
+package org.wordpress.android.ui.posts
+
+import org.wordpress.android.fluxc.model.PostImmutableModel
+import org.wordpress.android.fluxc.model.PostModel
+import org.wordpress.android.ui.posts.EditPostRepository.UpdatePostResult
+import org.wordpress.android.ui.posts.EditPostRepository.UpdatePostResult.Updated
+
+class UpdateFeaturedImageUseCase {
+    fun updateFeaturedImage(
+        featuredImageId: Long,
+        editPostRepository: EditPostRepository,
+        onPostFeaturedImageUpdated: (PostImmutableModel) -> Unit
+    ) {
+          editPostRepository.updateAsync({ postModel: PostModel ->
+            postModel.setFeaturedImageId(featuredImageId)
+            true
+        }) { postModel: PostImmutableModel, result: UpdatePostResult ->
+            if (result === Updated) {
+                onPostFeaturedImageUpdated.invoke(postModel)
+            }
+        }
+    }
+}
