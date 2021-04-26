2b3d3d223c757ef5f5ee0a183604573c88f895a1
develric <develric@gmail.com>
2021-04-27 01:10:52 +0200

    Adding unit testing for ListScenarioUtils.

---
+++ WordPress/src/test/java/org/wordpress/android/ui/engagement/ListScenarioUtilsTest.kt
@@ -0,0 +1,92 @@
+package org.wordpress.android.ui.engagement
+
+import android.content.Context
+import android.text.SpannableStringBuilder
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions.assertThat
+import org.json.JSONArray
+import org.json.JSONObject
+import org.junit.Assert.assertThrows
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.fluxc.tools.FormattableContent
+import org.wordpress.android.models.Note
+import org.wordpress.android.ui.engagement.ListScenarioType.LOAD_POST_LIKES
+import org.wordpress.android.ui.notifications.blocks.NoteBlockClickableSpan
+import org.wordpress.android.ui.notifications.utils.NotificationsUtilsWrapper
+import org.wordpress.android.util.image.ImageManager
+
+@RunWith(MockitoJUnitRunner::class)
+class ListScenarioUtilsTest {
+    @Mock lateinit var imageManager: ImageManager
+    @Mock lateinit var notificationsUtilsWrapper: NotificationsUtilsWrapper
+    @Mock lateinit var note: Note
+    @Mock lateinit var context: Context
+    @Mock lateinit var spannableBuilder: SpannableStringBuilder
+
+    private lateinit var listScenarioUtils: ListScenarioUtils
+
+    private val siteId = 100L
+    private val postId = 1000L
+
+    @Before
+    fun setup() {
+        whenever(note.isLikeType).thenReturn(true)
+        whenever(note.isPostLikeType).thenReturn(true)
+        whenever(note.siteId).thenReturn(siteId.toInt())
+        whenever(note.postId).thenReturn(postId.toInt())
+        whenever(note.siteId).thenReturn(siteId.toInt())
+        whenever(
+                spannableBuilder.getSpans(anyInt(), anyInt(), eq(NoteBlockClickableSpan::class.java))
+        ).thenReturn(listOf<NoteBlockClickableSpan>().toTypedArray())
+        whenever(spannableBuilder.length).thenReturn(0)
+        whenever(
+                notificationsUtilsWrapper.getSpannableContentForRanges(anyOrNull<FormattableContent>())
+        ).thenReturn(spannableBuilder)
+
+        listScenarioUtils = ListScenarioUtils(imageManager, notificationsUtilsWrapper)
+    }
+
+    @Test
+    fun `exception is thrown if note is not like type`() {
+        whenever(note.isLikeType).thenReturn(false)
+
+        assertThrows(IllegalArgumentException::class.java) {
+            listScenarioUtils.mapLikeNoteToListScenario(note, context)
+        }
+    }
+
+    @Test
+    fun `note is mapped to list scenario`() {
+        val listScenario = listScenarioUtils.mapLikeNoteToListScenario(note, context)
+
+        with(listScenario) {
+            assertThat(type).isEqualTo(LOAD_POST_LIKES)
+            assertThat(source).isEqualTo(EngagementNavigationSource.LIKE_NOTIFICATION_LIST)
+            assertThat(siteId).isEqualTo(this@ListScenarioUtilsTest.siteId)
+            assertThat(postId).isEqualTo(this@ListScenarioUtilsTest.postId)
+            assertThat(commentPostId).isEqualTo(0L)
+            assertThat(commentSiteUrl).isEqualTo("")
+        }
+    }
+
+    @Test
+    fun `json array is mapped to list of formattable content`() {
+        val headerArray = mock<JSONArray>()
+        val content = mock<FormattableContent>()
+        val jsonObject = mock<JSONObject>()
+        whenever(headerArray.length()).thenReturn(1)
+        whenever(headerArray.getJSONObject(anyInt())).thenReturn(jsonObject)
+        whenever(notificationsUtilsWrapper.mapJsonToFormattableContent(anyOrNull())).thenReturn(content)
+        val formattableList = listScenarioUtils.transformToFormattableContentList(headerArray)
+
+        assertThat(formattableList).isEqualTo(listOf(content))
+    }
+}
