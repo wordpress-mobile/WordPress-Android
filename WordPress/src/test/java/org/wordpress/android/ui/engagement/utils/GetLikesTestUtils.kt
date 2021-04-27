6140afde1e533ca0abf20666e1aef20aa7fd1bd2
develric <develric@gmail.com>
2021-04-28 00:39:06 +0200

    Adding utility methods.

--- WordPress/src/test/java/org/wordpress/android/ui/engagement/utils/GetLikesTestUtils.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/engagement/utils/GetLikesTestUtils.kt
@@ -2,8 +2,27 @@ package org.wordpress.android.ui.engagement.utils
 
 import org.wordpress.android.fluxc.model.LikeModel
 import org.wordpress.android.fluxc.model.LikeModel.LikeType
+import org.wordpress.android.fluxc.model.LikeModel.LikeType.COMMENT_LIKE
+import org.wordpress.android.fluxc.model.LikeModel.LikeType.POST_LIKE
 import org.wordpress.android.ui.engagement.EngageItem
+import org.wordpress.android.ui.engagement.EngageItem.LikedItem
 import org.wordpress.android.ui.engagement.EngageItem.Liker
+import org.wordpress.android.ui.engagement.EngagementNavigationSource.LIKE_NOTIFICATION_LIST
+import org.wordpress.android.ui.engagement.EngagementNavigationSource.LIKE_READER_LIST
+import org.wordpress.android.ui.engagement.GetLikesUseCase.FailureType.GENERIC
+import org.wordpress.android.ui.engagement.GetLikesUseCase.GetLikesState
+import org.wordpress.android.ui.engagement.GetLikesUseCase.GetLikesState.Failure
+import org.wordpress.android.ui.engagement.GetLikesUseCase.GetLikesState.Failure.EmptyStateData
+import org.wordpress.android.ui.engagement.GetLikesUseCase.GetLikesState.LikesData
+import org.wordpress.android.ui.engagement.ListScenario
+import org.wordpress.android.ui.engagement.utils.GetLikesTestConfig.TEST_CONFIG_1
+import org.wordpress.android.ui.engagement.utils.GetLikesTestConfig.TEST_CONFIG_2
+import org.wordpress.android.ui.engagement.utils.GetLikesTestConfig.TEST_CONFIG_3
+import org.wordpress.android.ui.engagement.utils.GetLikesTestConfig.TEST_CONFIG_4
+import org.wordpress.android.ui.engagement.utils.GetLikesTestConfig.TEST_CONFIG_5
+import org.wordpress.android.ui.reader.tracker.ReaderTracker
+import org.wordpress.android.ui.utils.UiString.UiStringRes
+import org.wordpress.android.ui.utils.UiString.UiStringText
 
 fun getDefaultLikers(numLikers: Int, likedItemType: LikeType, siteId: Long, itemId: Long): List<LikeModel> {
     val likersList = mutableListOf<LikeModel>()
@@ -30,6 +49,61 @@ fun getDefaultLikers(numLikers: Int, likedItemType: LikeType, siteId: Long, item
     return likersList
 }
 
+enum class GetLikesTestConfig {
+    TEST_CONFIG_1,
+    TEST_CONFIG_2,
+    TEST_CONFIG_3,
+    TEST_CONFIG_4,
+    TEST_CONFIG_5
+}
+
+fun getGetLikesState(testConfig: GetLikesTestConfig): GetLikesState {
+    return when(testConfig) {
+        TEST_CONFIG_1 -> {
+            LikesData(
+                    getDefaultLikers(10, POST_LIKE, 10, 100),
+                    10,
+                    false
+            )
+        }
+        TEST_CONFIG_2 -> {
+            LikesData(
+                    getDefaultLikers(10, COMMENT_LIKE, 10, 100),
+                    10,
+                    true
+            )
+        }
+        TEST_CONFIG_3 -> {
+            LikesData(
+                    getDefaultLikers(10, COMMENT_LIKE, 10, 100),
+                    10,
+                    false
+            )
+        }
+        TEST_CONFIG_4 -> {
+            LikesData(
+                    getDefaultLikers(10, COMMENT_LIKE, 10, 100),
+                    10,
+                    true
+            )
+        }
+        TEST_CONFIG_5 -> {
+            val cachedData = listOf<LikeModel>()
+            Failure(
+                    GENERIC,
+                    UiStringText("There was an error"),
+                    listOf(),
+                    EmptyStateData(
+                            cachedData.isEmpty(),
+                            UiStringRes(10)
+                    ),
+                    10,
+                    false
+            )
+        }
+    }
+}
+
 fun List<LikeModel>.isEqualTo(engageItemList: List<EngageItem>): Boolean {
     val sameSize = this.size == engageItemList.size
     val likersList = engageItemList as? List<Liker>
@@ -45,7 +119,7 @@ fun List<LikeModel>.isEqualTo(engageItemList: List<EngageItem>): Boolean {
     return sameSize && allEquals
 }
 
-private fun LikeModel.isEqualTo(liker: Liker): Boolean {
+fun LikeModel.isEqualTo(liker: Liker): Boolean {
     return liker.name == this.likerName &&
             liker.login == this.likerLogin &&
             liker.userSiteId == this.likerSiteId &&
@@ -57,4 +131,22 @@ private fun LikeModel.isEqualTo(liker: Liker): Boolean {
             liker.preferredBlogName == this.preferredBlogName &&
             liker.preferredBlogUrl == this.preferredBlogUrl &&
             liker.preferredBlogBlavatar == this.preferredBlogBlavatarUrl
+}
+
+fun ListScenario.generatesEquivalentLikedItem(likedItem: LikedItem): Boolean {
+    val headerData = this.headerData
+    return likedItem.author == headerData.authorName &&
+            likedItem.postOrCommentText == headerData.snippetText &&
+            likedItem.authorAvatarUrl == headerData.authorAvatarUrl &&
+            likedItem.likedItemId == this.postOrCommentId &&
+            likedItem.likedItemSiteId == this.siteId &&
+            likedItem.likedItemSiteUrl == this.commentSiteUrl &&
+            likedItem.likedItemPostId == this.commentPostId &&
+            likedItem.authorUserId == headerData.authorUserId &&
+            likedItem.authorPreferredSiteId == headerData.authorPreferredSiteId &&
+            likedItem.authorPreferredSiteUrl == headerData.authorPreferredSiteUrl &&
+            likedItem.blogPreviewSource == when (this.source) {
+                LIKE_NOTIFICATION_LIST -> ReaderTracker.SOURCE_NOTIFICATION
+                LIKE_READER_LIST -> ReaderTracker.SOURCE_READER_LIKE_LIST
+            }
 }
