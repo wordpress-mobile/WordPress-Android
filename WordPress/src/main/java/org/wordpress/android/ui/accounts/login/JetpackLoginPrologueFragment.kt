cfda7008dd9ea95b62e9231c7d53df8b58c9ec96
ashiagr <agr.ash@gmail.com>
2021-05-03 18:41:14 +0530

    Update jetpack prologue title, on click action using vm

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/JetpackLoginPrologueFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/JetpackLoginPrologueFragment.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.button.MaterialButton
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
+import org.wordpress.android.databinding.JetpackLoginPrologueScreenBinding
+import org.wordpress.android.ui.accounts.LoginNavigationEvents.ShowEmailLoginScreen
+import org.wordpress.android.ui.accounts.LoginNavigationEvents.ShowLoginViaSiteAddressScreen
 import org.wordpress.android.util.WPActivityUtils
 import javax.inject.Inject
 
@@ -27,18 +31,54 @@ class JetpackLoginPrologueFragment : Fragment(R.layout.jetpack_login_prologue_sc
         super.onViewCreated(view, savedInstanceState)
         initDagger()
         initSystemBars(view)
-        initViewModel()
+        with(JetpackLoginPrologueScreenBinding.bind(view)) { initViewModel() }
     }
 
     private fun initDagger() {
         (requireActivity().application as WordPress).component().inject(this)
     }
 
-    private fun initViewModel() {
+    private fun JetpackLoginPrologueScreenBinding.initViewModel() {
         viewModel = ViewModelProvider(this@JetpackLoginPrologueFragment, viewModelFactory).get(
                 JetpackLoginPrologueViewModel::class.java
         )
+        initObservers()
         viewModel.start()
+    }
+
+    private fun JetpackLoginPrologueScreenBinding.initObservers() {
+        viewModel.navigationEvents.observe(viewLifecycleOwner, { events ->
+            events.getContentIfNotHandled()?.let {
+                when (it) {
+                    is ShowEmailLoginScreen -> loginPrologueListener.showEmailLoginScreen()
+                    is ShowLoginViaSiteAddressScreen -> loginPrologueListener.loginViaSiteAddress()
+                    else -> Unit // Do nothing
+                }
+            }
+        })
+
+        viewModel.uiState.observe(viewLifecycleOwner, { uiState ->
+            updateButtonUiState(
+                    bottomButtonsContainer.continueWithWpcomButton,
+                    uiState.continueWithWpcomButtonState.title,
+                    uiState.continueWithWpcomButtonState.onClick
+            )
+            updateButtonUiState(
+                    bottomButtonsContainer.enterYourSiteAddressButton,
+                    uiState.enterYourSiteAddressButtonState.title,
+                    uiState.enterYourSiteAddressButtonState.onClick
+            )
+        })
+    }
+
+    private fun updateButtonUiState(button: MaterialButton, title: Int, onClick: () -> Unit) {
+        button.setText(title)
+        button.setOnClickListener { onClick.invoke() }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.onFragmentResume()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
