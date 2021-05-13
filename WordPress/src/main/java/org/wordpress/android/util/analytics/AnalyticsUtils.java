44ad89c4a81f532f2e7ad83bbf478eead63ca43c
develric <develric@gmail.com>
2021-05-14 01:28:30 +0200

    Fixing typo.

--- WordPress/src/main/java/org/wordpress/android/util/analytics/AnalyticsUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/analytics/AnalyticsUtils.java
@@ -72,7 +72,7 @@ public class AnalyticsUtils {
     private static final String COMMENT_ACTION_SOURCE = "source";
     private static final String SOURCE_KEY = "source";
     private static final String LIST_TYPE_KEY = "list_type";
-    private static final String IS_STORAGE_SETTINGS_RESOLVED_KET = "is_storage_settings_resolved";
+    private static final String IS_STORAGE_SETTINGS_RESOLVED_KEY = "is_storage_settings_resolved";
 
     public static final String HAS_GUTENBERG_BLOCKS_KEY = "has_gutenberg_blocks";
     public static final String HAS_WP_STORIES_BLOCKS_KEY = "has_wp_stories_blocks";
@@ -725,7 +725,7 @@ public class AnalyticsUtils {
     public static void trackStorageWarningDialogEvent(Stat stat, String source, Boolean isStorageSettingsResolved) {
         Map<String, Object> properties = new HashMap<>();
         properties.put(SOURCE_KEY, source);
-        properties.put(IS_STORAGE_SETTINGS_RESOLVED_KET, isStorageSettingsResolved ? "true" : "false");
+        properties.put(IS_STORAGE_SETTINGS_RESOLVED_KEY, isStorageSettingsResolved ? "true" : "false");
 
         AnalyticsTracker.track(stat, properties);
     }
