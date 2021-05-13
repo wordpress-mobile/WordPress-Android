a97f3afe7231b85d803f9f31b1184506dd8f5560
develric <develric@gmail.com>
2021-05-14 00:32:27 +0200

    Adding tracking for storage warning dialog events.

--- WordPress/src/main/java/org/wordpress/android/util/analytics/AnalyticsUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/analytics/AnalyticsUtils.java
@@ -72,6 +72,7 @@ public class AnalyticsUtils {
     private static final String COMMENT_ACTION_SOURCE = "source";
     private static final String SOURCE_KEY = "source";
     private static final String LIST_TYPE_KEY = "list_type";
+    private static final String IS_STORAGE_SETTINGS_RESOLVED_KET = "is_storage_settings_resolved";
 
     public static final String HAS_GUTENBERG_BLOCKS_KEY = "has_gutenberg_blocks";
     public static final String HAS_WP_STORIES_BLOCKS_KEY = "has_wp_stories_blocks";
@@ -719,5 +720,13 @@ public class AnalyticsUtils {
         properties.put(LIST_TYPE_KEY, listType);
 
         AnalyticsTracker.track(Stat.LIKE_LIST_OPENED, properties);
+    }
+
+    public static void trackStorageWarningDialogEvent(Stat stat, String source, Boolean isStorageSettingsResolved) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(SOURCE_KEY, source);
+        properties.put(IS_STORAGE_SETTINGS_RESOLVED_KET, isStorageSettingsResolved ? "true" : "false");
+
+        AnalyticsTracker.track(stat, properties);
     }
 }
