fa76582c219efe8ba20c14cfc1e96380f57ac953
Siobhan <siobhan@automattic.com>
2021-05-20 08:16:40 +0100

    Create GutenbergDialogFragment.kt using BasicDialogFragment.kt as a template
    The code for this DialogFragment is copied from the BasicDialogFragment file, with the neutral button omitted: https://github.com/wordpress-mobile/WordPress-Android/blob/develop/WordPress/src/main/java/org/wordpress/android/ui/posts/BasicFragmentDialog.kt
    
    The code will continue to be edited for use with dialogs in GutenbergEditorFragment.

---
+++ libs/editor/WordPressEditor/src/main/java/org/wordpress/android/editor/gutenberg/GutenbergDialogFragment.kt
@@ -0,0 +1,121 @@
+package org.wordpress.android.editor.gutenberg
+
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class GutenbergDialogFragment() : AppCompatDialogFragment() {
+    private lateinit var mTag: String
+    private lateinit var mMessage: CharSequence
+    private var mPositiveButtonLabel: CharSequence? = null
+    private var mTitle: CharSequence? = null
+    private var mNegativeButtonLabel: CharSequence? = null
+    private var dismissedByPositiveButton = false
+    private var dismissedByNegativeButton = false
+
+    interface BasicDialogPositiveClickInterface {
+        fun onPositiveClicked(instanceTag: String, mediaId: Int)
+    }
+
+    interface BasicDialogNegativeClickInterface {
+        fun onNegativeClicked(instanceTag: String)
+    }
+
+    interface BasicDialogOnDismissByOutsideTouchInterface {
+        fun onDismissByOutsideTouch(instanceTag: String)
+    }
+
+    fun initialize(
+        tag: String,
+        title: CharSequence?,
+        message: CharSequence,
+        positiveButtonLabel: CharSequence,
+        negativeButtonLabel: CharSequence? = null,
+    ) {
+        mTag = tag
+        mTitle = title
+        mMessage = message
+        mPositiveButtonLabel = positiveButtonLabel
+        mNegativeButtonLabel = negativeButtonLabel
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.isCancelable = true
+        val theme = 0
+        setStyle(STYLE_NORMAL, theme)
+
+        if (savedInstanceState != null) {
+            mTag = requireNotNull(savedInstanceState.getString(STATE_KEY_TAG))
+            mTitle = savedInstanceState.getCharSequence(STATE_KEY_TITLE)
+            mMessage = requireNotNull(savedInstanceState.getCharSequence(STATE_KEY_MESSAGE))
+            mPositiveButtonLabel = savedInstanceState.getCharSequence(STATE_KEY_POSITIVE_BUTTON_LABEL)
+            mNegativeButtonLabel = savedInstanceState.getCharSequence(STATE_KEY_NEGATIVE_BUTTON_LABEL)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(STATE_KEY_TAG, mTag)
+        outState.putCharSequence(STATE_KEY_TITLE, mTitle)
+        outState.putCharSequence(STATE_KEY_MESSAGE, mMessage)
+        outState.putCharSequence(STATE_KEY_POSITIVE_BUTTON_LABEL, mPositiveButtonLabel)
+        outState.putCharSequence(STATE_KEY_NEGATIVE_BUTTON_LABEL, mNegativeButtonLabel)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = MaterialAlertDialogBuilder(requireActivity())
+        builder.setMessage(mMessage)
+
+        mTitle?.let {
+            builder.setTitle(mTitle)
+        }
+
+        mPositiveButtonLabel?.let {
+            builder.setPositiveButton(mPositiveButtonLabel) { _, _ ->
+                dismissedByPositiveButton = true
+                val activity = activity
+                if (activity != null) {
+                    (activity as BasicDialogPositiveClickInterface).onPositiveClicked(mTag)
+                }
+        }
+
+        mNegativeButtonLabel?.let {
+            builder.setNegativeButton(mNegativeButtonLabel) { _, _ ->
+                dismissedByNegativeButton = true
+                if (activity != null) {
+                    (activity as BasicDialogNegativeClickInterface).onNegativeClicked(mTag)
+                }
+            }
+        }
+
+        return builder.create()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        val activity = activity
+        if (activity != null && activity is BasicDialogOnDismissByOutsideTouchInterface) {
+            // Only handle the event if it wasn't triggered by a button
+            if (!dismissedByPositiveButton && !dismissedByNegativeButton) {
+                (activity as BasicDialogOnDismissByOutsideTouchInterface).onDismissByOutsideTouch(mTag)
+            }
+        }
+        super.onDismiss(dialog)
+    }
+
+    companion object {
+        private const val STATE_KEY_TAG = "state_key_tag"
+        private const val STATE_KEY_TITLE = "state_key_title"
+        private const val STATE_KEY_MESSAGE = "state_key_message"
+        private const val STATE_KEY_POSITIVE_BUTTON_LABEL = "state_key_positive_button_label"
+        private const val STATE_KEY_NEGATIVE_BUTTON_LABEL = "state_key_negative_button_label"
+    }
+}
