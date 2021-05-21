5b12402407f264f2db7bd3827b06c48a6de04d2d
khaykov <forsver@gmail.com>
2021-05-21 17:51:47 -0500

    Inject comment fragment and activity.

--- WordPress/src/main/java/org/wordpress/android/modules/AppComponent.java
+++ WordPress/src/main/java/org/wordpress/android/modules/AppComponent.java
@@ -39,6 +39,8 @@ import org.wordpress.android.ui.comments.CommentsActivity;
 import org.wordpress.android.ui.comments.CommentsDetailActivity;
 import org.wordpress.android.ui.comments.CommentsListFragment;
 import org.wordpress.android.ui.comments.EditCommentActivity;
+import org.wordpress.android.ui.comments.unified.UnifiedCommentListFragment;
+import org.wordpress.android.ui.comments.unified.UnifiedCommentsActivity;
 import org.wordpress.android.ui.deeplinks.DeepLinkingIntentReceiverActivity;
 import org.wordpress.android.ui.domains.DomainRegistrationActivity;
 import org.wordpress.android.ui.domains.DomainRegistrationDetailsFragment;
@@ -674,6 +676,10 @@ public interface AppComponent extends AndroidInjector<WordPress> {
     void inject(UserProfileBottomSheetFragment object);
 
     void inject(EngagedPeopleListActivity object);
+
+    void inject(UnifiedCommentsActivity object);
+
+    void inject(UnifiedCommentListFragment object);
 
     // Allows us to inject the application without having to instantiate any modules, and provides the Application
     // in the app graph
