bac4cf6ab2fa4474349112964b5eded18ecd97d2
develric <develric@gmail.com>
2021-05-24 18:06:24 +0200

    Refactoring avoiding reflection.

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/media/MediaErrorSubType.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/media/MediaErrorSubType.kt
@@ -1,35 +1,56 @@
 package org.wordpress.android.fluxc.store.media
 
-import org.wordpress.android.fluxc.store.media.MediaErrorSubType.UndefinedSubType.Type.UNDEFINED
-import kotlin.reflect.full.companionObject
-import kotlin.reflect.full.companionObjectInstance
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.DIRECTORY_PATH_SUPPLIED_FILE_NEEDED
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.MEDIA_FILE_NOT_FOUND_LOCALLY
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.MEDIA_WAS_NULL
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.NOT_VALID_LOCAL_FILE_PATH
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.NO_ERROR
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.UNSUPPORTED_MIME_TYPE
 
-interface GetByString {
-    fun fromString(name: String?): MediaErrorSubType?
-}
+sealed class MediaErrorSubType(val categoryName: String, val subTypeName: String) {
+    // This enum describes the categories of MediaErrorType that are mapped into one or multiple
+    // MediaErrorSubType. UNDEFINED category collects all MediaErrorType(s) that are not mapped yet.
+    // To map another MediaErrorType, add an item to the enum and define a MediaErrorSubType element
+    enum class MediaErrorSubtypeCategory(val errors: List<MediaErrorSubType>) {
+        UNDEFINED_SUBTYPE(listOf(
+                UndefinedSubType
+        )),
+        MALFORMED_MEDIA_ARG_SUBTYPE(listOf(
+                MalformedMediaArgSubType(MEDIA_WAS_NULL),
+                MalformedMediaArgSubType(UNSUPPORTED_MIME_TYPE),
+                MalformedMediaArgSubType(NOT_VALID_LOCAL_FILE_PATH),
+                MalformedMediaArgSubType(MEDIA_FILE_NOT_FOUND_LOCALLY),
+                MalformedMediaArgSubType(DIRECTORY_PATH_SUPPLIED_FILE_NEEDED),
+                MalformedMediaArgSubType(NO_ERROR)
+        ));
+    }
 
-sealed class MediaErrorSubType {
-    data class UndefinedSubType constructor(val type: Type = UNDEFINED) : MediaErrorSubType() {
-        enum class Type {
-            UNDEFINED
-        }
+    fun serialize(): String {
+        return "$categoryName:$subTypeName"
+    }
 
-        override fun toString(): String {
-            return "${UndefinedSubType::class.java.simpleName}@$type"
-        }
+    companion object {
+        @JvmStatic
+        fun deserialize(name: String?): MediaErrorSubType {
+            if (name == null) return UndefinedSubType
 
-        companion object : GetByString {
-            override fun fromString(name: String?): MediaErrorSubType? {
-                return if (name?.startsWith(UndefinedSubType::class.java.simpleName) == true) {
-                    UndefinedSubType(UNDEFINED)
-                } else {
-                    null
+            MediaErrorSubtypeCategory.values().forEach { category ->
+                category.errors.forEach { subType ->
+                    if (subType.serialize() == name) {
+                        return subType
+                    }
                 }
             }
+
+            return UndefinedSubType
         }
     }
 
-    data class MalformedMediaArgSubType(val type: Type) : MediaErrorSubType() {
+    object UndefinedSubType : MediaErrorSubType(UndefinedSubType::class.java.simpleName, "")
+
+    data class MalformedMediaArgSubType(
+        val type: Type
+    ) : MediaErrorSubType(MalformedMediaArgSubType::class.java.simpleName, type.name) {
         enum class Type(val errorLogDescription: String?) {
             MEDIA_WAS_NULL("media cannot be null"),
             UNSUPPORTED_MIME_TYPE("media must define a valid MIME type"),
@@ -37,53 +58,6 @@ sealed class MediaErrorSubType {
             MEDIA_FILE_NOT_FOUND_LOCALLY("local file path for media does not exist"),
             DIRECTORY_PATH_SUPPLIED_FILE_NEEDED("supplied file path is a directory, a file is required"),
             NO_ERROR(null)
-        }
-
-        override fun toString(): String {
-            return "${MalformedMediaArgSubType::class.java.simpleName}@$type"
-        }
-
-        companion object : GetByString {
-            override fun fromString(name: String?): MediaErrorSubType? {
-                return name?.takeIf { it.startsWith(MalformedMediaArgSubType::class.java.simpleName) }?.let {
-                    val targetSubType: String? = it.split("@").lastOrNull()
-
-                    targetSubType?.let { subTypeName ->
-                        Type.values().firstOrNull { type ->
-                            subTypeName.equals(type.name, true)
-                        }?.let { matchedType ->
-                            MalformedMediaArgSubType(matchedType)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    companion object {
-        @JvmStatic
-        fun getSubTypeFromString(name: String?): MediaErrorSubType {
-            if (name == null) return UndefinedSubType()
-
-            for (errorSubType in MediaErrorSubType::class.sealedSubclasses) {
-                if (errorSubType.companionObject?.isCompanion != true) {
-                    throw IllegalStateException("${errorSubType.simpleName} does not implement a companion object.")
-                }
-
-                val companion = errorSubType.companionObjectInstance
-
-                if (companion !is GetByString) {
-                    throw IllegalStateException(
-                            "${errorSubType.simpleName} does not implement GetByString interface."
-                    )
-                }
-
-                companion.fromString(name)?.also {
-                    return it
-                }
-            }
-
-            return UndefinedSubType()
         }
     }
 }
