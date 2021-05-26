edea971ffb4e2002ef72acbb8021c8b3302cf1e9
develric <develric@gmail.com>
2021-05-26 17:41:11 +0200

    Refactoring the MediaErrorSubType as per review feedback.

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/media/MediaErrorSubType.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/media/MediaErrorSubType.kt
@@ -1,44 +1,38 @@
 package org.wordpress.android.fluxc.store.media
 
-import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.DIRECTORY_PATH_SUPPLIED_FILE_NEEDED
-import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.MEDIA_FILE_NOT_FOUND_LOCALLY
-import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.MEDIA_WAS_NULL
-import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.NOT_VALID_LOCAL_FILE_PATH
-import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.NO_ERROR
-import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type.UNSUPPORTED_MIME_TYPE
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MalformedMediaArgSubType.Type
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MediaErrorSubtypeCategory.MALFORMED_MEDIA_ARG_SUBTYPE
+import org.wordpress.android.fluxc.store.media.MediaErrorSubType.MediaErrorSubtypeCategory.UNDEFINED_SUBTYPE
 
-sealed class MediaErrorSubType(val categoryName: String, val subTypeName: String) {
-    // This enum describes the categories of MediaErrorType that are mapped into one or multiple
-    // MediaErrorSubType. UNDEFINED category collects all MediaErrorType(s) that are not mapped yet.
+sealed class MediaErrorSubType(val category: MediaErrorSubtypeCategory, val subTypeName: String) {
+    // This enum lists the categories of MediaErrorType that are mapped into one or multiple
+    // MediaErrorSubType in the companion object categories field.
+    // UNDEFINED_SUBTYPE category collects all MediaErrorType(s) that are not mapped yet.
     // To map another MediaErrorType, add an item to the enum and define a MediaErrorSubType element
-    enum class MediaErrorSubtypeCategory(val errors: List<MediaErrorSubType>) {
-        UNDEFINED_SUBTYPE(listOf(
-                UndefinedSubType
-        )),
-        MALFORMED_MEDIA_ARG_SUBTYPE(listOf(
-                MalformedMediaArgSubType(MEDIA_WAS_NULL),
-                MalformedMediaArgSubType(UNSUPPORTED_MIME_TYPE),
-                MalformedMediaArgSubType(NOT_VALID_LOCAL_FILE_PATH),
-                MalformedMediaArgSubType(MEDIA_FILE_NOT_FOUND_LOCALLY),
-                MalformedMediaArgSubType(DIRECTORY_PATH_SUPPLIED_FILE_NEEDED),
-                MalformedMediaArgSubType(NO_ERROR)
-        ));
+    enum class MediaErrorSubtypeCategory {
+        UNDEFINED_SUBTYPE,
+        MALFORMED_MEDIA_ARG_SUBTYPE
     }
 
     fun serialize(): String {
-        return "$categoryName:$subTypeName"
+        return "${category.name}:$subTypeName"
     }
 
     companion object {
+        private val categories = MediaErrorSubtypeCategory.values().flatMap { value ->
+            when(value) {
+                UNDEFINED_SUBTYPE -> listOf(UndefinedSubType)
+                MALFORMED_MEDIA_ARG_SUBTYPE -> Type.values().map { MalformedMediaArgSubType(it) }
+            }
+        }
+
         @JvmStatic
         fun deserialize(name: String?): MediaErrorSubType {
             if (name == null) return UndefinedSubType
 
-            MediaErrorSubtypeCategory.values().forEach { category ->
-                category.errors.forEach { subType ->
-                    if (subType.serialize() == name) {
-                        return subType
-                    }
+            categories.forEach { subType ->
+                if (subType.serialize() == name) {
+                    return subType
                 }
             }
 
@@ -46,11 +40,11 @@ sealed class MediaErrorSubType(val categoryName: String, val subTypeName: String
         }
     }
 
-    object UndefinedSubType : MediaErrorSubType(UndefinedSubType::class.java.simpleName, "")
+    object UndefinedSubType : MediaErrorSubType(UNDEFINED_SUBTYPE, "")
 
     data class MalformedMediaArgSubType(
         val type: Type
-    ) : MediaErrorSubType(MalformedMediaArgSubType::class.java.simpleName, type.name) {
+    ) : MediaErrorSubType(MALFORMED_MEDIA_ARG_SUBTYPE, type.name) {
         enum class Type(val errorLogDescription: String?) {
             MEDIA_WAS_NULL("media cannot be null"),
             UNSUPPORTED_MIME_TYPE("media must define a valid MIME type"),
