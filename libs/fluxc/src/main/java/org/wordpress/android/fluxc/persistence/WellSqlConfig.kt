b611d972ca0a4558e119cc2526c1208695157b6b
Antonis Lilis <antonis.lilis@automattic.com>
2021-05-27 14:34:14 +0300

    Removes rawFeatures (__experimentalFeatures) from props passed to the editor

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
@@ -1782,7 +1782,6 @@ open class WellSqlConfig : DefaultWellConfig {
                                     "STYLESHEET TEXT," +
                                     "VERSION TEXT," +
                                     "RAW_STYLES TEXT," +
-                                    "RAW_FEATURES TEXT," +
                                     "FOREIGN KEY(LOCAL_SITE_ID) REFERENCES SiteModel(_id) ON DELETE CASCADE)"
                     )
                 }
