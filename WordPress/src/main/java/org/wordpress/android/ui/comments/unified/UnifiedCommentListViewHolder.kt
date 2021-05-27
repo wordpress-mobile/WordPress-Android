44f326ce3fb83d4dab00112ff70ed286111dbc9f
khaykov <forsver@gmail.com>
2021-05-27 12:55:02 -0500

    Added parent view holder that works with view bindings.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/unified/UnifiedCommentListViewHolder.kt
@@ -0,0 +1,6 @@
+package org.wordpress.android.ui.comments.unified
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+abstract class UnifiedCommentListViewHolder<T : ViewBinding>(protected val binding: T) : RecyclerView.ViewHolder(binding.root)
