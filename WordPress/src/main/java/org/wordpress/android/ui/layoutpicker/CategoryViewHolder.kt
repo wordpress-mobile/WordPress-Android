c654b23936a21183d54e9b1e9bc46a0d520315c3
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-06-16 08:07:16 -0400

    Refactor: Use companion object and remove field initialization

--- WordPress/src/main/java/org/wordpress/android/ui/layoutpicker/CategoryViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/layoutpicker/CategoryViewHolder.kt
@@ -1,43 +1,48 @@
 package org.wordpress.android.ui.layoutpicker
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.wordpress.android.databinding.ModalLayoutPickerCategoryBinding
 import org.wordpress.android.login.util.getColorStateListFromAttribute
 import org.wordpress.android.util.getColorFromAttribute
 import org.wordpress.android.util.setVisible
-import org.wordpress.android.util.viewBinding
 
 /**
  * Renders the Layout Category header buttons
  */
 class CategoryViewHolder(
-    internal val parent: ViewGroup,
-    internal val binding: ModalLayoutPickerCategoryBinding =
-            parent.viewBinding(ModalLayoutPickerCategoryBinding::inflate)
+    private val parent: ViewGroup,
+    private val binding: ModalLayoutPickerCategoryBinding
 ) : RecyclerView.ViewHolder(binding.root) {
-    val container: View = binding.categoryContainer
-    val category: TextView = binding.category
-    val emoji: TextView = binding.emoji
-    val check: ImageView = binding.check
-
     fun onBind(uiState: CategoryListItemUiState) {
-        category.text = uiState.title
-        emoji.text = uiState.emoji
-        container.contentDescription = parent.context.getString(uiState.contentDescriptionResId, uiState.title)
-        container.setOnClickListener {
+        binding.category.text = uiState.title
+        binding.emoji.text = uiState.emoji
+        binding.categoryContainer.contentDescription = parent.context.getString(
+                uiState.contentDescriptionResId,
+                uiState.title
+        )
+        binding.categoryContainer.setOnClickListener {
             uiState.onItemTapped.invoke()
         }
         setSelectedStateUI(uiState)
     }
 
     private fun setSelectedStateUI(uiState: CategoryListItemUiState) {
-        check.setVisible(uiState.checkIconVisible)
-        emoji.setVisible(uiState.emojiIconVisible)
-        container.backgroundTintList = parent.context.getColorStateListFromAttribute(uiState.background)
-        category.setTextColor(parent.context.getColorFromAttribute(uiState.textColor))
+        binding.check.setVisible(uiState.checkIconVisible)
+        binding.emoji.setVisible(uiState.emojiIconVisible)
+        binding.categoryContainer.backgroundTintList = parent.context.getColorStateListFromAttribute(uiState.background)
+        binding.category.setTextColor(parent.context.getColorFromAttribute(uiState.textColor))
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): CategoryViewHolder {
+            val binding = ModalLayoutPickerCategoryBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+            )
+            return CategoryViewHolder(parent, binding)
+        }
     }
 }
--- WordPress/src/main/java/org/wordpress/android/ui/layoutpicker/LayoutViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/layoutpicker/LayoutViewHolder.kt
@@ -1,35 +1,28 @@
 package org.wordpress.android.ui.layoutpicker
 
 import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.card.MaterialCardView
 import org.wordpress.android.R
 import org.wordpress.android.databinding.ModalLayoutPickerLayoutsCardBinding
 import org.wordpress.android.networking.MShot
 import org.wordpress.android.util.image.ImageManager
 import org.wordpress.android.util.image.ImageManager.RequestListener
 import org.wordpress.android.util.setVisible
-import org.wordpress.android.util.viewBinding
 
 /**
  * Renders the Layout card
  */
 class LayoutViewHolder(
-    internal val parent: ViewGroup,
-    internal val binding: ModalLayoutPickerLayoutsCardBinding =
-            parent.viewBinding(ModalLayoutPickerLayoutsCardBinding::inflate)
+    private val parent: ViewGroup,
+    private val binding: ModalLayoutPickerLayoutsCardBinding
 ) : RecyclerView.ViewHolder(binding.root) {
-    private val container: MaterialCardView = binding.layoutContainer
-    private val preview: ImageView = binding.preview
-    private val selected: ImageView = binding.selectedOverlay
-
     fun onBind(
         uiState: LayoutListItemUiState,
         imageManager: ImageManager
     ) {
-        imageManager.loadWithResultListener(preview, MShot(uiState.preview),
+        imageManager.loadWithResultListener(binding.preview, MShot(uiState.preview),
                 object : RequestListener<Drawable> {
                     override fun onLoadFailed(e: Exception?, model: Any?) {
                     }
@@ -39,17 +32,28 @@ class LayoutViewHolder(
                     }
                 })
 
-        selected.setVisible(uiState.selectedOverlayVisible)
-        preview.contentDescription = parent.context.getString(uiState.contentDescriptionResId, uiState.title)
-        preview.context?.let { ctx ->
-            container.strokeWidth = if (uiState.selectedOverlayVisible) {
+        binding.selectedOverlay.setVisible(uiState.selectedOverlayVisible)
+        binding.preview.contentDescription = parent.context.getString(uiState.contentDescriptionResId, uiState.title)
+        binding.preview.context?.let { ctx ->
+            binding.layoutContainer.strokeWidth = if (uiState.selectedOverlayVisible) {
                 ctx.resources.getDimensionPixelSize(R.dimen.picker_header_selection_overlay_width)
             } else {
                 0
             }
         }
-        container.setOnClickListener {
+        binding.layoutContainer.setOnClickListener {
             uiState.onItemTapped.invoke()
+        }
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): LayoutViewHolder {
+            val binding = ModalLayoutPickerLayoutsCardBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+            )
+            return LayoutViewHolder(parent, binding)
         }
     }
 }
