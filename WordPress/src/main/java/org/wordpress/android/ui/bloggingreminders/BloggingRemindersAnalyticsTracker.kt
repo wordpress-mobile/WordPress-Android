b9e2b7fb8d21691584cba0a82bb68e0c9fed0602
Renan Ferrari <renan.fb@gmail.com>
2021-06-17 19:15:56 -0300

    Add BloggingRemindersAnalyticsTracker

---
+++ WordPress/src/main/java/org/wordpress/android/ui/bloggingreminders/BloggingRemindersAnalyticsTracker.kt
@@ -0,0 +1,8 @@
+package org.wordpress.android.ui.bloggingreminders
+
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
+import javax.inject.Inject
+
+class BloggingRemindersAnalyticsTracker @Inject constructor(
+    private val analyticsTracker: AnalyticsTrackerWrapper
+)
