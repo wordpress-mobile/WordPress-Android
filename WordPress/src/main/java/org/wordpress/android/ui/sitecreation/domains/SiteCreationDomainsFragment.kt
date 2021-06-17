7fed6246489cc438553225af3bf98eb2d6955775
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-06-17 15:12:31 -0400

    Refactor: remove synthetic accessors

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainsFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainsFragment.kt
@@ -3,17 +3,14 @@ package org.wordpress.android.ui.sitecreation.domains
 import android.content.Context
 import android.os.Bundle
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.site_creation_domains_screen.*
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
+import org.wordpress.android.databinding.SiteCreationDomainsScreenBinding
+import org.wordpress.android.databinding.SiteCreationFormScreenBinding
 import org.wordpress.android.ui.accounts.HelpActivity
 import org.wordpress.android.ui.sitecreation.SiteCreationBaseFormFragment
 import org.wordpress.android.ui.sitecreation.domains.SiteCreationDomainsViewModel.DomainsUiState.DomainsUiContentState
@@ -23,8 +20,8 @@ import org.wordpress.android.ui.utils.UiHelpers
 import org.wordpress.android.util.DisplayUtilsWrapper
 import javax.inject.Inject
 
+@Suppress("TooManyFunctions")
 class SiteCreationDomainsFragment : SiteCreationBaseFormFragment() {
-    private lateinit var nonNullActivity: FragmentActivity
     private var searchInputWithHeader: SearchInputWithHeader? = null
     private lateinit var viewModel: SiteCreationDomainsViewModel
 
@@ -32,110 +29,7 @@ class SiteCreationDomainsFragment : SiteCreationBaseFormFragment() {
     @Inject internal lateinit var uiHelpers: UiHelpers
     @Inject internal lateinit var displayUtils: DisplayUtilsWrapper
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context !is DomainsScreenListener) {
-            throw IllegalStateException("Parent activity must implement DomainsScreenListener.")
-        }
-        if (context !is OnHelpClickedListener) {
-            throw IllegalStateException("Parent activity must implement OnHelpClickedListener.")
-        }
-    }
-
-    @LayoutRes
-    override fun getContentLayout(): Int {
-        return R.layout.site_creation_domains_screen
-    }
-
-    override fun setupContent(rootView: ViewGroup) {
-        searchInputWithHeader = SearchInputWithHeader(
-                uiHelpers = uiHelpers,
-                rootView = rootView,
-                onClear = { viewModel.onClearTextBtnClicked() }
-        )
-        rootView.findViewById<AppCompatButton>(R.id.create_site_button).setOnClickListener {
-            viewModel.createSiteBtnClicked()
-        }
-        initRecyclerView()
-        initViewModel()
-    }
-
-    override fun getScreenTitle(): String {
-        return arguments?.getString(EXTRA_SCREEN_TITLE)
-                ?: throw IllegalStateException("Required argument screen title is missing.")
-    }
-
-    override fun onHelp() {
-        viewModel.onHelpClicked()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        nonNullActivity = requireActivity()
-        (nonNullActivity.application as WordPress).component().inject(this)
-    }
-
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        // we need to set the `onTextChanged` after the viewState has been restored otherwise the viewModel.updateQuery
-        // is called when the system sets the restored value to the EditText which results in an unnecessary request
-        searchInputWithHeader?.onTextChanged = { viewModel.updateQuery(it) }
-    }
-
-    private fun initRecyclerView() {
-        recycler_view.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
-        initAdapter()
-    }
-
-    private fun initAdapter() {
-        val adapter = SiteCreationDomainsAdapter(uiHelpers)
-        recycler_view.adapter = adapter
-    }
-
-    private fun initViewModel() {
-        viewModel = ViewModelProvider(this, viewModelFactory)
-                .get(SiteCreationDomainsViewModel::class.java)
-
-        viewModel.uiState.observe(this, Observer { uiState ->
-            uiState?.let {
-                searchInputWithHeader?.updateHeader(nonNullActivity, uiState.headerUiState)
-                searchInputWithHeader?.updateSearchInput(nonNullActivity, uiState.searchInputUiState)
-                updateContentUiState(uiState.contentState)
-                uiHelpers.updateVisibility(create_site_button_container, uiState.createSiteButtonContainerVisibility)
-                uiHelpers.updateVisibility(create_site_button_shaddow, uiState.createSiteButtonContainerVisibility)
-                updateTitleVisibility(uiState.headerUiState == null)
-            }
-        })
-        viewModel.clearBtnClicked.observe(this, Observer {
-            searchInputWithHeader?.setInputText("")
-        })
-        viewModel.createSiteBtnClicked.observe(this, Observer { domain ->
-            domain?.let { (requireActivity() as DomainsScreenListener).onDomainSelected(domain) }
-        })
-        viewModel.onHelpClicked.observe(this, Observer {
-            (requireActivity() as OnHelpClickedListener).onHelpClicked(HelpActivity.Origin.SITE_CREATION_DOMAINS)
-        })
-        viewModel.start()
-    }
-
-    private fun updateContentUiState(contentState: DomainsUiContentState) {
-        uiHelpers.updateVisibility(domain_list_empty_view, contentState.emptyViewVisibility)
-        uiHelpers.updateVisibility(domain_list_example, contentState.exampleViewVisibility)
-        if (contentState.items.isNotEmpty()) {
-            view?.announceForAccessibility(getString(R.string.suggestions_updated_content_description))
-        }
-        (recycler_view.adapter as SiteCreationDomainsAdapter).update(contentState.items)
-    }
-
-    private fun updateTitleVisibility(visible: Boolean) {
-        val actionBar = (requireActivity() as? AppCompatActivity)?.supportActionBar
-        actionBar?.setDisplayShowTitleEnabled(displayUtils.isLandscapeBySize() || visible)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        searchInputWithHeader = null
-    }
+    private var binding: SiteCreationDomainsScreenBinding? = null
 
     companion object {
         const val TAG = "site_creation_domains_fragment_tag"
@@ -147,5 +41,118 @@ class SiteCreationDomainsFragment : SiteCreationBaseFormFragment() {
             fragment.arguments = bundle
             return fragment
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context !is DomainsScreenListener) {
+            throw IllegalStateException("Parent activity must implement DomainsScreenListener.")
+        }
+        if (context !is OnHelpClickedListener) {
+            throw IllegalStateException("Parent activity must implement OnHelpClickedListener.")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (requireActivity().application as WordPress).component().inject(this)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        // we need to set the `onTextChanged` after the viewState has been restored otherwise the viewModel.updateQuery
+        // is called when the system sets the restored value to the EditText which results in an unnecessary request
+        searchInputWithHeader?.onTextChanged = { viewModel.updateQuery(it) }
+    }
+
+    override fun getContentLayout(): Int {
+        return R.layout.site_creation_domains_screen
+    }
+
+    override val screenTitle: String
+        get() = arguments?.getString(EXTRA_SCREEN_TITLE)
+                ?: throw IllegalStateException("Required argument screen title is missing.")
+
+    override fun setBindingViewStubListener(parentBinding: SiteCreationFormScreenBinding) {
+        parentBinding.siteCreationFormContentStub.setOnInflateListener { _, inflated ->
+            binding = SiteCreationDomainsScreenBinding.bind(inflated)
+        }
+    }
+
+    override fun setupContent() {
+        with (checkNotNull(binding)) {
+                searchInputWithHeader = SearchInputWithHeader(
+                        uiHelpers = uiHelpers,
+                        rootView = root as ViewGroup,
+                        onClear = { viewModel.onClearTextBtnClicked() }
+                )
+                createSiteButton.setOnClickListener { viewModel.createSiteBtnClicked() }
+                initRecyclerView()
+                initViewModel()
+        }
+    }
+
+    private fun SiteCreationDomainsScreenBinding.initRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        initAdapter()
+    }
+
+    private fun SiteCreationDomainsScreenBinding.initAdapter() {
+        val adapter = SiteCreationDomainsAdapter(uiHelpers)
+        recyclerView.adapter = adapter
+    }
+
+    private fun SiteCreationDomainsScreenBinding.initViewModel() {
+        viewModel = ViewModelProvider(this@SiteCreationDomainsFragment, viewModelFactory)
+                .get(SiteCreationDomainsViewModel::class.java)
+
+        viewModel.uiState.observe(this@SiteCreationDomainsFragment, { uiState ->
+            uiState?.let {
+                searchInputWithHeader?.updateHeader(requireActivity(), uiState.headerUiState)
+                searchInputWithHeader?.updateSearchInput(requireActivity(), uiState.searchInputUiState)
+                updateContentUiState(uiState.contentState)
+                uiHelpers.updateVisibility(createSiteButtonContainer, uiState.createSiteButtonContainerVisibility)
+                uiHelpers.updateVisibility(createSiteButtonShaddow, uiState.createSiteButtonContainerVisibility)
+                updateTitleVisibility(uiState.headerUiState == null)
+            }
+        })
+        viewModel.clearBtnClicked.observe(this@SiteCreationDomainsFragment, {
+            searchInputWithHeader?.setInputText("")
+        })
+        viewModel.createSiteBtnClicked.observe(this@SiteCreationDomainsFragment, { domain ->
+            domain?.let { (requireActivity() as DomainsScreenListener).onDomainSelected(domain) }
+        })
+        viewModel.onHelpClicked.observe(this@SiteCreationDomainsFragment, {
+            (requireActivity() as OnHelpClickedListener).onHelpClicked(HelpActivity.Origin.SITE_CREATION_DOMAINS)
+        })
+        viewModel.start()
+    }
+
+    private fun updateContentUiState(contentState: DomainsUiContentState) {
+        binding?.let {
+            uiHelpers.updateVisibility(it.domainListEmptyView, contentState.emptyViewVisibility)
+            uiHelpers.updateVisibility(
+                    it.siteCreationDomainsScreenExample.root, contentState.exampleViewVisibility
+            )
+            (it.recyclerView.adapter as SiteCreationDomainsAdapter).update(contentState.items)
+        }
+        if (contentState.items.isNotEmpty()) {
+            view?.announceForAccessibility(getString(R.string.suggestions_updated_content_description))
+        }
+    }
+
+    private fun updateTitleVisibility(visible: Boolean) {
+        val actionBar = (requireActivity() as? AppCompatActivity)?.supportActionBar
+        actionBar?.setDisplayShowTitleEnabled(displayUtils.isLandscapeBySize() || visible)
+    }
+
+    override fun onHelp() {
+        viewModel.onHelpClicked()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        searchInputWithHeader = null
+        binding = null
     }
 }
