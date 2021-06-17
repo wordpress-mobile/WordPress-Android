11b2a459c30a60be8e7a7f1f2ba65044fe0ae0c6
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-06-17 15:11:25 -0400

    Update to inflate ViewStub viewBinding

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/SiteCreationBaseFormFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/SiteCreationBaseFormFragment.kt
@@ -1,59 +1,48 @@
 package org.wordpress.android.ui.sitecreation
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
-import android.view.ViewStub
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import org.wordpress.android.R
-import org.wordpress.android.R.layout
+import org.wordpress.android.databinding.SiteCreationFormScreenBinding
 
-abstract class SiteCreationBaseFormFragment : Fragment() {
-    @get:LayoutRes protected abstract val contentLayout: Int
-    protected abstract fun setupContent(rootView: ViewGroup?)
+abstract class SiteCreationBaseFormFragment : Fragment(R.layout.site_creation_form_screen) {
+    @LayoutRes protected abstract fun getContentLayout(): Int
+    protected abstract val screenTitle: String
+    protected abstract fun setupContent()
     protected abstract fun onHelp()
+    protected abstract fun setBindingViewStubListener(parentBinding: SiteCreationFormScreenBinding)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
 
-    protected fun createMainView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): ViewGroup {
-        val rootView = inflater.inflate(layout.site_creation_form_screen, container, false) as ViewGroup
-        val formContainer = rootView.findViewById<ViewStub>(R.id.site_creation_form_content_stub)
-        formContainer.layoutResource = contentLayout
-        formContainer.inflate()
-        return rootView
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return createMainView(inflater, container, savedInstanceState)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupContent(getView()!!.rootView as ViewGroup)
-        val toolbar: Toolbar = view.findViewById(R.id.toolbar_main)
-        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
-        val actionBar = (activity as AppCompatActivity?)!!.supportActionBar
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setTitle(screenTitle)
-            // important for accessibility
-            activity!!.title = screenTitle
+
+        with (SiteCreationFormScreenBinding.bind(view)) {
+            siteCreationFormContentStub.layoutResource = getContentLayout()
+            setBindingViewStubListener(this)
+            siteCreationFormContentStub.inflate()
+
+            setupContent()
+
+            (requireActivity() as AppCompatActivity).setSupportActionBar(toolbarMain)
+            val actionBar = (activity as AppCompatActivity?)!!.supportActionBar
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true)
+                actionBar.title = screenTitle
+                // important for accessibility
+                requireActivity().title = screenTitle
+            }
         }
     }
 
-    protected abstract val screenTitle: String?
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_site_creation, menu)
     }
