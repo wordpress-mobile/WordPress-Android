89d2792b51d4350051b8242aaa3e27e3197b000c
khaykov <forsver@gmail.com>
2021-06-16 21:16:38 -0500

    Updated diff calback to prevent recycler view blinking.

--- WordPress/src/main/java/org/wordpress/android/ui/comments/unified/UnifiedCommentsListDiffCallback.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/unified/UnifiedCommentsListDiffCallback.kt
@@ -1,6 +1,8 @@
 package org.wordpress.android.ui.comments.unified
 
 import androidx.recyclerview.widget.DiffUtil
+import org.wordpress.android.ui.comments.CommentListItem
+import org.wordpress.android.ui.comments.CommentListItemDiffCallback.Payload
 import org.wordpress.android.ui.comments.unified.UnifiedCommentListItem.Comment
 import org.wordpress.android.ui.comments.unified.UnifiedCommentListItem.SubHeader
 
@@ -16,4 +18,14 @@ class UnifiedCommentsListDiffCallback : DiffUtil.ItemCallback<UnifiedCommentList
     override fun areContentsTheSame(oldItem: UnifiedCommentListItem, newItem: UnifiedCommentListItem): Boolean {
         return oldItem == newItem
     }
+
+    /**
+     * Since we don't currently have a logic for updating only a part of the view, this method sends empty Payload
+     * to avoid view blinking in RecyclerView.
+     */
+    override fun getChangePayload(oldItem: UnifiedCommentListItem, newItem: UnifiedCommentListItem): Any {
+        return Payload
+    }
+
+    object Payload
 }
