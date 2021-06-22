26791d95887a3188a558e2b4b79310284307ef2d
vojtasmrcek <planarvoid@gmail.com>
2021-06-22 11:52:22 +0200

    Show shadow only when not scrolled to the bottom

--- WordPress/src/main/java/org/wordpress/android/ui/bloggingreminders/BloggingReminderBottomSheetFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/bloggingreminders/BloggingReminderBottomSheetFragment.kt
@@ -7,8 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.wordpress.android.R
@@ -37,7 +38,18 @@ class BloggingReminderBottomSheetFragment : BottomSheetDialogFragment() {
         with(RecyclerViewPrimaryButtonBottomSheetBinding.bind(view)) {
             contentRecyclerView.layoutManager = LinearLayoutManager(requireActivity())
             contentRecyclerView.adapter = adapter
-
+            contentRecyclerView.addOnScrollListener(object: OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    if (recyclerView.canScrollVertically(1)) {
+                        // Show shadow when not at the bottom
+                        bottomShadow.animate().alpha(1.0f).start()
+                    } else {
+                        // Hide shadow at the bottom
+                        bottomShadow.animate().alpha(0.0f).start()
+                    }
+                }
+            })
             viewModel =
                     ViewModelProvider(requireActivity(), viewModelFactory).get(BloggingRemindersViewModel::class.java)
             viewModel.uiState.observe(this@BloggingReminderBottomSheetFragment) { uiState ->
