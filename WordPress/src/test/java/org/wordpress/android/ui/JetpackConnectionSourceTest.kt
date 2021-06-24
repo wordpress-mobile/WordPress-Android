11837b46cf4c36ceb1f50d42990ca300a3797f3d
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-06-24 07:55:25 -0400

    Ad blank lines and move Assertions to import

--- WordPress/src/test/java/org/wordpress/android/ui/JetpackConnectionSourceTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/JetpackConnectionSourceTest.kt
@@ -1,6 +1,6 @@
 package org.wordpress.android.ui
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.wordpress.android.BaseUnitTest
 
@@ -8,35 +8,45 @@ class JetpackConnectionSourceTest : BaseUnitTest() {
     @Test
     fun `given valid notifications source, when fromString requested, then results match enum`() {
         val value = "notifications"
+
         val result = JetpackConnectionSource.fromString(value)
-        Assertions.assertThat(result).isEqualTo(JetpackConnectionSource.NOTIFICATIONS)
+
+        assertThat(result).isEqualTo(JetpackConnectionSource.NOTIFICATIONS)
     }
 
     @Test
     fun `given valid stats source, when fromString requested, then results match enum`() {
         val value = "stats"
+
         val result = JetpackConnectionSource.fromString(value)
-        Assertions.assertThat(result).isEqualTo(JetpackConnectionSource.STATS)
+
+        assertThat(result).isEqualTo(JetpackConnectionSource.STATS)
     }
 
     @Test
     fun `given invalid source, when fromString requested, then results are null`() {
         val value = "abcdefg"
+
         val result = JetpackConnectionSource.fromString(value)
-        Assertions.assertThat(result).isNull()
+
+        assertThat(result).isNull()
     }
 
     @Test
     fun `given invalid notifications source, when fromString requested, then results are null`() {
         val value = "NoTiFiCaTiOnS"
+
         val result = JetpackConnectionSource.fromString(value)
-        Assertions.assertThat(result).isNull()
+
+        assertThat(result).isNull()
     }
 
     @Test
     fun `given invalid stats source, when fromString requested, then results are null`() {
         val value = "StAtS"
+
         val result = JetpackConnectionSource.fromString(value)
-        Assertions.assertThat(result).isNull()
+
+        assertThat(result).isNull()
     }
 }
