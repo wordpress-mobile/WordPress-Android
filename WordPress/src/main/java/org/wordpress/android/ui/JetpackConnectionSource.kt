df8bebebe5938a67363aa680f9c743c3568123dd
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-06-24 07:46:47 -0400

    Simplify methods

--- WordPress/src/main/java/org/wordpress/android/ui/JetpackConnectionSource.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/JetpackConnectionSource.kt
@@ -3,23 +3,13 @@ package org.wordpress.android.ui
 enum class JetpackConnectionSource(private val value: String) {
     NOTIFICATIONS("notifications"), STATS("stats");
 
-    override fun toString(): String {
-        return value
-    }
+    override fun toString() = value
 
     companion object {
-        @JvmStatic fun fromString(value: String): JetpackConnectionSource? {
-            return when {
-                NOTIFICATIONS.value == value -> {
-                    NOTIFICATIONS
-                }
-                STATS.value == value -> {
-                    STATS
-                }
-                else -> {
-                    null
-                }
-            }
+        @JvmStatic fun fromString(value: String) = when {
+            NOTIFICATIONS.value == value -> { NOTIFICATIONS }
+            STATS.value == value -> { STATS }
+            else -> { null }
         }
     }
 }
