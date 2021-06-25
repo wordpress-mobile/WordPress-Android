fdb1ee072800781154851734dbfe40a1b4e0ee35
vojtasmrcek <planarvoid@gmail.com>
2021-06-25 14:54:12 +0200

    Remove unused import

--- WordPress/src/main/java/org/wordpress/android/util/config/BloggingRemindersFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/BloggingRemindersFeatureConfig.kt
@@ -2,7 +2,6 @@ package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.Feature
-import org.wordpress.android.annotation.FeatureInDevelopment
 import org.wordpress.android.util.config.BloggingRemindersFeatureConfig.Companion.BLOGGING_REMINDERS_REMOTE_FIELD
 import javax.inject.Inject
 
