12848495424f1a788feea6038283b8ad9aefd056
ashiagr <agr.ash@gmail.com>
2021-07-01 11:36:38 +0530

    Add active_modules, publicize_permanently_disabled fields in SiteWPComRestResponse, SiteModel

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
@@ -30,7 +30,7 @@ open class WellSqlConfig : DefaultWellConfig {
     annotation class AddOn
 
     override fun getDbVersion(): Int {
-        return 155
+        return 156
     }
 
     override fun getDbName(): String {
@@ -1788,6 +1788,10 @@ open class WellSqlConfig : DefaultWellConfig {
                                     "RAW_FEATURES TEXT," +
                                     "FOREIGN KEY(LOCAL_SITE_ID) REFERENCES SiteModel(_id) ON DELETE CASCADE)"
                     )
+                }
+                155 -> migrate(version) {
+                    db.execSQL("ALTER TABLE SiteModel ADD ACTIVE_MODULES TEXT")
+                    db.execSQL("ALTER TABLE SiteModel ADD IS_PUBLICIZE_PERMANENTLY_DISABLED BOOLEAN")
                 }
             }
         }
