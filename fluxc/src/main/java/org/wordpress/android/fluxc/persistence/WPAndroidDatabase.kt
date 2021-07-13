016823f777b453e63bb298fe00d9e1d8ad38cca6
Renan Ferrari <renan.fb@gmail.com>
2021-07-13 17:21:27 -0300

    Add Room database version 2 migration

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WPAndroidDatabase.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WPAndroidDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import org.wordpress.android.fluxc.persistence.BloggingRemindersDao.BloggingReminders
 import org.wordpress.android.fluxc.persistence.PlanOffersDao.PlanOffer
 import org.wordpress.android.fluxc.persistence.PlanOffersDao.PlanOfferFeature
@@ -28,6 +30,51 @@ abstract class WPAndroidDatabase : RoomDatabase() {
                 applicationContext,
                 WPAndroidDatabase::class.java,
                 "wp-android-database"
-        ).fallbackToDestructiveMigration().build()
+        )
+                .fallbackToDestructiveMigration()
+                .addMigrations(MIGRATION_1_2)
+                .build()
+
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.apply {
+                    execSQL(
+                            "CREATE TABLE IF NOT EXISTS `PlanOffers` (" +
+                                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                                    "`internalPlanId` INTEGER NOT NULL, " +
+                                    "`name` TEXT, " +
+                                    "`shortName` TEXT, " +
+                                    "`tagline` TEXT, " +
+                                    "`description` TEXT, " +
+                                    "`icon` TEXT" +
+                                    ")"
+                    )
+                    execSQL(
+                            "CREATE UNIQUE INDEX IF NOT EXISTS `index_PlanOffers_internalPlanId` " +
+                                    "ON `PlanOffers` (`internalPlanId`)"
+                    )
+                    execSQL(
+                            "CREATE TABLE IF NOT EXISTS `PlanOfferIds` (" +
+                                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                                    "`productId` INTEGER NOT NULL, " +
+                                    "`internalPlanId` INTEGER NOT NULL, " +
+                                    "FOREIGN KEY(`internalPlanId`) REFERENCES `PlanOffers`(`internalPlanId`) " +
+                                    "ON UPDATE NO ACTION ON DELETE CASCADE" +
+                                    ")"
+                    )
+                    execSQL(
+                            "CREATE TABLE IF NOT EXISTS `PlanOfferFeatures` (" +
+                                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                                    "`internalPlanId` INTEGER NOT NULL, " +
+                                    "`stringId` TEXT, " +
+                                    "`name` TEXT, " +
+                                    "`description` TEXT, " +
+                                    "FOREIGN KEY(`internalPlanId`) REFERENCES `PlanOffers`(`internalPlanId`) " +
+                                    "ON UPDATE NO ACTION ON DELETE CASCADE" +
+                                    ")"
+                    )
+                }
+            }
+        }
     }
 }
