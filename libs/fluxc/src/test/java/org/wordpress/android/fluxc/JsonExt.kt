a118272cfe637f2aaf53bfdfb21b993058f776ac
ThomazFB <thomazfcortez@gmail.com>
2021-07-22 13:20:48 -0300

    Fix redundant JsonLoader object structure

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/JsonExt.kt
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/JsonExt.kt
@@ -2,12 +2,10 @@ package org.wordpress.android.fluxc
 
 import com.google.gson.Gson
 
-class JsonLoader {
-    companion object {
-        fun <T> String.jsonFileAs(clazz: Class<T>) =
-                UnitTestUtils.getStringFromResourceFile(
-                        this@Companion::class.java,
-                        this
-                )?.let { Gson().fromJson(it, clazz) }
-    }
+object JsonLoader {
+    fun <T> String.jsonFileAs(clazz: Class<T>) =
+            UnitTestUtils.getStringFromResourceFile(
+                    this@JsonLoader::class.java,
+                    this
+            )?.let { Gson().fromJson(it, clazz) }
 }
