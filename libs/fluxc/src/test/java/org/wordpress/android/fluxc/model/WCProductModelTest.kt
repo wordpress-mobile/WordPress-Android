6dd92d21ea0dbbdafa4f20f80df762c883651b9e
ThomazFB <thomazfcortez@gmail.com>
2021-07-21 23:10:55 -0300

    Add Product metadata serialization test

---
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/model/WCProductModelTest.kt
@@ -0,0 +1,23 @@
+package org.wordpress.android.fluxc.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.wordpress.android.fluxc.JsonLoader.Companion.jsonFileAs
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.ProductApiResponse
+
+class WCProductModelTest {
+
+
+    @Test
+    fun `Product addons should be serialized correctly`() {
+        val productModelUnderTest =
+                "wc/product-with-addons.json"
+                .jsonFileAs(ProductApiResponse::class.java)
+                ?.asProductModel()
+
+        assertThat(productModelUnderTest).isNotNull
+        assertThat(productModelUnderTest?.addons).isNotEmpty
+    }
+
+}
+
