b4e9cd92b25f5c23226def5cff6532a02c4592be
ashiagr <agr.ash@gmail.com>
2021-07-23 09:44:22 +0530

    Handle multisite not supported in usecase

--- WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/usecases/FetchScanStateUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpack/scan/usecases/FetchScanStateUseCase.kt
@@ -59,6 +59,10 @@ class FetchScanStateUseCase @Inject constructor(
                 val retryAttemptsExceeded = handleError(retryAttempts++, Failure.RemoteRequestFailure)
                 if (retryAttemptsExceeded) break else continue
             }
+            if (scanStateModel.reason == ScanStateModel.Reason.MULTISITE_NOT_SUPPORTED) {
+                emit(Failure.MultisiteNotSupported)
+                return@flow
+            }
             emit(Success(scanStateModel))
 
             if (scanStateModel.state != ScanStateModel.State.SCANNING) {
