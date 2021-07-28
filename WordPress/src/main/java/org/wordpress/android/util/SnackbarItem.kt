6fc11926b89dbfe4d4d0fb6fb44325b96e6a2325
khaykov <forsver@gmail.com>
2021-07-28 11:43:42 -0500

    Added optional onShow callback to snackbar.

--- WordPress/src/main/java/org/wordpress/android/util/SnackbarItem.kt
+++ WordPress/src/main/java/org/wordpress/android/util/SnackbarItem.kt
@@ -14,12 +14,15 @@ private const val LONG_DURATION_MS = 2750L
 
 const val INDEFINITE_SNACKBAR_NOT_ALLOWED = "Snackbar.LENGTH_INDEFINITE not allowed in getSnackbarDurationMs."
 
+
 class SnackbarItem(
     val info: Info,
     val action: Action? = null,
-    dismissCallback: ((transientBottomBar: Snackbar?, event: Int) -> Unit)? = null
+    dismissCallback: ((transientBottomBar: Snackbar?, event: Int) -> Unit)? = null,
+    showCallback: ((transientBottomBar: Snackbar?) -> Unit)? = null
 ) {
     val dismissCallback = SoftReference(dismissCallback)
+    val showCallback = SoftReference(showCallback)
 
     fun getSnackbarDurationMs(): Long {
         return when (info.duration) {
@@ -47,6 +50,11 @@ class SnackbarItem(
     }
 
     val snackbarCallback = object : Snackbar.Callback() {
+        override fun onShown(transientBottomBar: Snackbar?) {
+            this@SnackbarItem.showCallback.get()?.invoke(transientBottomBar)
+            super.onShown(transientBottomBar)
+        }
+
         override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
             this@SnackbarItem.dismissCallback.get()?.invoke(transientBottomBar, event)
             super.onDismissed(transientBottomBar, event)
