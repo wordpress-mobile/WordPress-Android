4d69e99985cb92d79b42a098eddc37dc51837cef
khaykov <forsver@gmail.com>
2021-07-30 18:15:59 -0500

    Make wrapper argument nullable.

--- WordPress/src/main/java/org/wordpress/android/ui/utils/AnimationUtilsWrapper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/utils/AnimationUtilsWrapper.kt
@@ -5,7 +5,7 @@ import org.wordpress.android.util.AniUtils
 import javax.inject.Inject
 
 class AnimationUtilsWrapper @Inject constructor() {
-    fun startAnimation(targetView: View, animationResId: Int) {
+    fun startAnimation(targetView: View?, animationResId: Int) {
         AniUtils.startAnimation(targetView, animationResId)
     }
 }
