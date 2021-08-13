c0cadffd8e381ec5874a789f2cee6ebfce6a6c6f
Renan Ferrari <renan.fb@gmail.com>
2021-08-13 12:29:51 -0300

    Add initial logic to build notification

--- WordPress/src/main/java/org/wordpress/android/workers/weeklyroundup/WeeklyRoundupNotifier.kt
+++ WordPress/src/main/java/org/wordpress/android/workers/weeklyroundup/WeeklyRoundupNotifier.kt
@@ -1,18 +1,45 @@
 package org.wordpress.android.workers.weeklyroundup
 
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_CANCEL_CURRENT
+import android.app.PendingIntent.FLAG_IMMUTABLE
 import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.push.NotificationPushIds.WEEKLY_ROUNDUP_NOTIFICATION_ID
+import org.wordpress.android.ui.stats.StatsTimeframe.WEEK
+import org.wordpress.android.ui.stats.refresh.StatsActivity
+import org.wordpress.android.util.SiteUtils
+import org.wordpress.android.viewmodel.ContextProvider
 import javax.inject.Inject
 
 class WeeklyRoundupNotifier @Inject constructor(
     private val accountStore: AccountStore,
     private val siteStore: SiteStore,
+    private val contextProvider: ContextProvider
 ) {
     fun shouldShowNotifications() = accountStore.hasAccessToken() && siteStore.hasSitesAccessedViaWPComRest()
 
     fun buildNotifications(): List<WeeklyRoundupNotification> {
-        return emptyList()
-    }
+        val context = contextProvider.getContext()
+        val site = siteStore.sitesAccessedViaWPComRest[0]
+        val siteId = site.id
+        val siteName = SiteUtils.getSiteNameOrHomeURL(site)
+        val notificationId = WEEKLY_ROUNDUP_NOTIFICATION_ID + siteId
 
-    fun onNotificationsShown(notifications: List<WeeklyRoundupNotification>) {}
+        val weeklyRoundupNotification = WeeklyRoundupNotification(
+                id = notificationId,
+                contentIntentBuilder = {
+                    PendingIntent.getActivity(
+                            context,
+                            notificationId,
+                            StatsActivity.buildIntent(context, site, WEEK),
+                            FLAG_CANCEL_CURRENT or FLAG_IMMUTABLE
+                    )
+                },
+                contentTitle = "Weekly Roundup: $siteName",
+                contentText = "Your site got X views, Y likes, Z comments.",
+        )
+
+        return listOf(weeklyRoundupNotification)
+    }
 }
