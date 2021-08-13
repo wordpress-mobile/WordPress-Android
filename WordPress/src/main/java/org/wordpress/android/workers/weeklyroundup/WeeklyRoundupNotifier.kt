7ebf299e6fbb9a74202bb37f8afd61144718ee3c
Renan Ferrari <renan.fb@gmail.com>
2021-08-13 16:38:31 -0300

    Extract build notification logic into a method

--- WordPress/src/main/java/org/wordpress/android/workers/weeklyroundup/WeeklyRoundupNotifier.kt
+++ WordPress/src/main/java/org/wordpress/android/workers/weeklyroundup/WeeklyRoundupNotifier.kt
@@ -4,6 +4,7 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_CANCEL_CURRENT
 import android.app.PendingIntent.FLAG_IMMUTABLE
 import org.wordpress.android.R
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.push.NotificationPushIds.WEEKLY_ROUNDUP_NOTIFICATION_ID
@@ -30,13 +31,23 @@ class WeeklyRoundupNotifier @Inject constructor(
     fun shouldShowNotifications() = accountStore.hasAccessToken() && siteStore.hasSitesAccessedViaWPComRest()
 
     fun buildNotifications(): List<WeeklyRoundupNotification> {
-        val context = contextProvider.getContext()
         val site = selectedSiteRepository.getSelectedSite() ?: siteStore.sitesAccessedViaWPComRest[0]
-        val siteId = site.id
-        val siteName = siteUtils.getSiteNameOrHomeURL(site)
-        val notificationId = WEEKLY_ROUNDUP_NOTIFICATION_ID + siteId
 
-        val weeklyRoundupNotification = WeeklyRoundupNotification(
+        val notification = buildNotification(site)
+
+        return listOf(notification)
+    }
+
+    fun onNotificationsShown(notifications: List<WeeklyRoundupNotification>) {
+        notificationsTracker.trackShownNotification(WEEKLY_ROUNDUP)
+
+        weeklyRoundupScheduler.schedule()
+    }
+
+    private fun buildNotification(site: SiteModel): WeeklyRoundupNotification {
+        val context = contextProvider.getContext()
+        val notificationId = WEEKLY_ROUNDUP_NOTIFICATION_ID + site.id
+        return WeeklyRoundupNotification(
                 id = notificationId,
                 contentIntentBuilder = {
                     PendingIntent.getActivity(
@@ -46,16 +57,16 @@ class WeeklyRoundupNotifier @Inject constructor(
                             FLAG_CANCEL_CURRENT or FLAG_IMMUTABLE
                     )
                 },
-                contentTitle = resourceProvider.getString(R.string.weekly_roundup_notification_title, siteName),
-                contentText = resourceProvider.getString(R.string.weekly_roundup_notification_text, 0, 0, 0),
+                contentTitle = resourceProvider.getString(
+                        R.string.weekly_roundup_notification_title,
+                        siteUtils.getSiteNameOrHomeURL(site)
+                ),
+                contentText = resourceProvider.getString(
+                        R.string.weekly_roundup_notification_text,
+                        0,
+                        0,
+                        0
+                ),
         )
-
-        return listOf(weeklyRoundupNotification)
-    }
-
-    fun onNotificationsShown(notifications: List<WeeklyRoundupNotification>) {
-        notificationsTracker.trackShownNotification(WEEKLY_ROUNDUP)
-
-        weeklyRoundupScheduler.schedule()
     }
 }
