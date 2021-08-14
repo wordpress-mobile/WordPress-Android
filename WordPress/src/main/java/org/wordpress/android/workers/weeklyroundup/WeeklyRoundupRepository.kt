8dbfc77198c5cbd3e11838569d34bae108e5767e
Renan Ferrari <renan.fb@gmail.com>
2021-08-13 21:20:20 -0300

    Log fetch error

--- WordPress/src/main/java/org/wordpress/android/workers/weeklyroundup/WeeklyRoundupRepository.kt
+++ WordPress/src/main/java/org/wordpress/android/workers/weeklyroundup/WeeklyRoundupRepository.kt
@@ -6,6 +6,8 @@ import org.wordpress.android.fluxc.model.stats.time.VisitsAndViewsModel
 import org.wordpress.android.fluxc.model.stats.time.VisitsAndViewsModel.PeriodData
 import org.wordpress.android.fluxc.network.utils.StatsGranularity.WEEKS
 import org.wordpress.android.fluxc.store.stats.time.VisitsAndViewsStore
+import org.wordpress.android.util.AppLog
+import org.wordpress.android.util.AppLog.T
 import org.wordpress.android.workers.weeklyroundup.WeeklyRoundupUtils.parseStandardDate
 import org.wordpress.android.workers.weeklyroundup.WeeklyRoundupUtils.parseWeekPeriodDate
 import java.time.DayOfWeek.MONDAY
@@ -18,15 +20,15 @@ class WeeklyRoundupRepository @Inject constructor(
     suspend fun fetchWeeklyRoundupData(site: SiteModel): WeeklyRoundupData? {
         val response = visitsAndViewsStore.fetchVisits(site, WEEKS, LimitMode.Top(2), true)
 
-        val visitsAndViewsModel = response.model
+        val model = response.model
 
-        if (visitsAndViewsModel == null) {
-            val visitsAndViewsError = response.error
-            // TODO Handle error
+        if (model == null) {
+            val message = response.error?.let { "Error fetching weekly roundup data for ${site.url}: [${it.type.name}: ${it.message}]" }
+            AppLog.e(T.NOTIFS, message)
             return null
         }
 
-        return getLastWeekPeriodData(visitsAndViewsModel)?.let { WeeklyRoundupData.create(site, it) }
+        return getLastWeekPeriodData(model)?.let { WeeklyRoundupData.create(site, it) }
     }
 
     private fun getLastWeekPeriodData(visitsAndViewsModel: VisitsAndViewsModel): PeriodData? {
