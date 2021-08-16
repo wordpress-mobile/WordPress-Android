ca83b700cb055f733d632517f37d180f0b08e010
ashiagr <agr.ash@gmail.com>
2021-08-16 10:37:54 +0530

    Update diff calculation in keyboard resize view util
    Diff is used for adding padding to the pre-publishing bottom sheet content view when the keyboard is visible so that the publish visible is not hidden behind the keyboard. It is approximately equal to the keyboard height.
    
    Based on the runtime values, it seems windowVisibleDisplayFrame height includes the top inset when the screen is in full screen immersive mode and subtracting it from the display height will give a shorter keyboard height. Hence it is subtracted from the real screen size, and also the bottom inset is subtracted to get the actual keyboard height.

--- WordPress/src/main/java/org/wordpress/android/util/KeyboardResizeViewUtil.java
+++ WordPress/src/main/java/org/wordpress/android/util/KeyboardResizeViewUtil.java
@@ -1,9 +1,15 @@
 package org.wordpress.android.util;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 /**
  * Created by mikepenz on 14.03.15.
@@ -41,8 +47,7 @@ public class KeyboardResizeViewUtil {
                 mDecorView.getWindowVisibleDisplayFrame(r);
 
                 // get screen height and calculate the difference with the useable area from the r
-                int height = mDecorView.getContext().getResources().getDisplayMetrics().heightPixels;
-                int diff = height - r.bottom;
+                int diff = getRealScreenHeight() - (r.bottom + getInsetBottom());
 
                 // if it could be a keyboard add the padding to the view
                 if (diff != 0) {
@@ -63,4 +68,16 @@ public class KeyboardResizeViewUtil {
             });
         }
     };
+
+    private int getInsetBottom() {
+        WindowInsetsCompat insets = ViewCompat.getRootWindowInsets(mContentView);
+        return insets != null ? insets.getSystemWindowInsetBottom() : 0;
+    }
+
+    private int getRealScreenHeight() {
+        WindowManager windowManager = (WindowManager) mDecorView.getContext().getSystemService(Context.WINDOW_SERVICE);
+        Point realSize = new Point();
+        if (windowManager != null) windowManager.getDefaultDisplay().getRealSize(realSize);
+        return realSize.y;
+    }
 }
