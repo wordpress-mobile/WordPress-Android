50e60754223cae3660ac810095711acd77317e3e
Ravi <ravi.program@gmail.com>
2021-08-20 16:10:49 +1000

    Update DomainPurchaseFeatureConfig.kt
    fix lint checks

--- WordPress/src/main/java/org/wordpress/android/util/config/DomainPurchaseFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/DomainPurchaseFeatureConfig.kt
@@ -1,12 +1,11 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.Feature
 import org.wordpress.android.annotation.FeatureInDevelopment
 import javax.inject.Inject
 
 // TODO: Uncomment the lines 9 and 13 when remote field is configured and remove line 10 and this to-do
-//@Feature(DomainPurchaseFeatureConfig.DOMAIN_PURCHASE_REMOTE_FIELD, false)
+// @Feature(DomainPurchaseFeatureConfig.DOMAIN_PURCHASE_REMOTE_FIELD, false)
 @FeatureInDevelopment
 class DomainPurchaseFeatureConfig
 @Inject constructor(appConfig: AppConfig) : FeatureConfig(
