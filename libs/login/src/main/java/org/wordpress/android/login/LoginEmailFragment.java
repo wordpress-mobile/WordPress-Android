f31385c5cc87313294354cb7a14af48697df58e6
ashiagr <agr.ash@gmail.com>
2021-08-31 11:45:12 +0530

    Revert "temp commit"
    This reverts commit 7c3bdfc8

--- libs/login/src/main/java/org/wordpress/android/login/LoginEmailFragment.java
+++ libs/login/src/main/java/org/wordpress/android/login/LoginEmailFragment.java
@@ -222,7 +222,6 @@ public class LoginEmailFragment extends LoginBaseFormFragment<LoginListener> imp
                 if (hasFocus && !mIsDisplayingEmailHints && !mHasDismissedEmailHints) {
                     mAnalyticsListener.trackSelectEmailField();
                     showHintPickerDialogIfNeeded();
-                    ActivityUtils.showKeyboard(mEmailInput);
                 } else {
                     ActivityUtils.hideKeyboardForced(mEmailInput);
                 }
