d22e0609bdf4dc51fb540a2acb9706434a3f9382
Ravi <ravi.program@gmail.com>
2021-09-01 15:26:56 +1000

    Layouts picker scroll position
    Fixes layout picker scroll position issue

--- WordPress/src/main/java/org/wordpress/android/ui/layoutpicker/LayoutCategoryAdapter.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/layoutpicker/LayoutCategoryAdapter.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.layoutpicker
 
+import android.os.Bundle
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView.Adapter
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
  */
 class LayoutCategoryAdapter : Adapter<LayoutsItemViewHolder>() {
     private var items: List<LayoutCategoryUiState> = listOf()
+    private var nestedScrollStates = Bundle()
 
     fun update(newItems: List<LayoutCategoryUiState>) {
         val diffResult = DiffUtil.calculateDiff(
@@ -27,5 +29,14 @@ class LayoutCategoryAdapter : Adapter<LayoutsItemViewHolder>() {
         holder.bind(items[position])
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = LayoutsItemViewHolder(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            LayoutsItemViewHolder(parent = parent, nestedScrollStates = nestedScrollStates)
+
+    fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        nestedScrollStates = savedInstanceState
+    }
+
+    fun onSaveInstanceState(): Bundle {
+        return nestedScrollStates
+    }
 }
