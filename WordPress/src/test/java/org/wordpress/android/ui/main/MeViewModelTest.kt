a2b128cee5e084c3868fe4f55304217abea2d441
develric <develric@gmail.com>
2021-09-02 15:46:19 +0200

    Fixing unit testing.

--- WordPress/src/test/java/org/wordpress/android/ui/main/MeViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/main/MeViewModelTest.kt
@@ -9,18 +9,31 @@ import org.mockito.Mock
 import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.WordPress
+import org.wordpress.android.models.recommend.RecommendApiCallsProvider
 import org.wordpress.android.ui.mysite.SelectedSiteRepository
+import org.wordpress.android.util.NetworkUtilsWrapper
+import org.wordpress.android.viewmodel.ContextProvider
 import org.wordpress.android.viewmodel.Event
 
 @InternalCoroutinesApi
 class MeViewModelTest : BaseUnitTest() {
     @Mock lateinit var wordPress: WordPress
     @Mock lateinit var selectedSiteRepository: SelectedSiteRepository
+    @Mock lateinit var recommendApiCallsProvider: RecommendApiCallsProvider
+    @Mock lateinit var networkUtilsWrapper: NetworkUtilsWrapper
+    @Mock lateinit var contextProvider: ContextProvider
     private lateinit var viewModel: MeViewModel
 
     @Before
     fun setUp() {
-        viewModel = MeViewModel(TEST_DISPATCHER, TEST_DISPATCHER, selectedSiteRepository)
+        viewModel = MeViewModel(
+                TEST_DISPATCHER,
+                TEST_DISPATCHER,
+                selectedSiteRepository,
+                recommendApiCallsProvider,
+                networkUtilsWrapper,
+                contextProvider
+        )
     }
 
     @Test
