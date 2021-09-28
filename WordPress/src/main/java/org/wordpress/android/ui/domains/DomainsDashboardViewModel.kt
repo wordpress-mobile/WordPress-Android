9aa6de5e964056fbd258820318beacc0883ac527
Ravi <ravi.program@gmail.com>
2021-09-28 17:21:08 +1000

    Update DomainsDashboardViewModel.kt
    Fetch and display list of domains for a site on domains dashboard

--- WordPress/src/main/java/org/wordpress/android/ui/domains/DomainsDashboardViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainsDashboardViewModel.kt
@@ -4,10 +4,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.viewModelScope
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.Constants
 import org.wordpress.android.R
 import org.wordpress.android.R.string
 import org.wordpress.android.analytics.AnalyticsTracker.Stat.DOMAIN_CREDIT_REDEMPTION_TAPPED
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.SiteActionBuilder
+import org.wordpress.android.fluxc.network.rest.wpcom.site.Domain
+import org.wordpress.android.fluxc.store.SiteStore.OnSiteDomainsFetched
 import org.wordpress.android.ui.domains.DomainsListItem.Action
 import org.wordpress.android.ui.domains.DomainsListItem.Action.CHANGE_SITE_ADDRESS
 import org.wordpress.android.ui.domains.DomainsListItem.AddDomain
@@ -25,6 +31,8 @@ import org.wordpress.android.ui.utils.ListItemInteraction
 import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.ui.utils.UiString.UiStringResWithParams
 import org.wordpress.android.ui.utils.UiString.UiStringText
+import org.wordpress.android.util.AppLog
+import org.wordpress.android.util.AppLog.T
 import org.wordpress.android.util.SiteUtils
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 import org.wordpress.android.viewmodel.Event
@@ -32,6 +40,7 @@ import javax.inject.Inject
 
 @Suppress("TooManyFunctions")
 class DomainsDashboardViewModel @Inject constructor(
+    private val dispatcher: Dispatcher,
     private val analyticsTrackerWrapper: AnalyticsTrackerWrapper,
     selectedSiteRepository: SelectedSiteRepository,
     domainRegistrationHandler: DomainRegistrationHandler,
@@ -52,6 +61,16 @@ class DomainsDashboardViewModel @Inject constructor(
     private val hasCustomDomain = SiteUtils.hasCustomDomain(selectedSite)
 
     private var isStarted: Boolean = false
+
+    init {
+        dispatcher.register(this)
+    }
+
+    override fun onCleared() {
+        dispatcher.unregister(this)
+        super.onCleared()
+    }
+
     fun start() {
         if (isStarted) {
             return
@@ -101,11 +120,37 @@ class DomainsDashboardViewModel @Inject constructor(
 
     // if site has a registered domain then show Site Domains, Add Domain and Manage Domains
     private fun manageDomainsItems(): List<DomainsListItem> {
+        dispatcher.dispatch(SiteActionBuilder.newFetchDomainsAction(selectedSite))
+        return manageDomainsListItems(null)
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onSiteDomainsFetched(event: OnSiteDomainsFetched) {
+        if (event.isError) {
+            AppLog.e(T.DOMAIN_REGISTRATION, "An error occurred while fetching site domains")
+        } else {
+            AppLog.d(T.DOMAIN_REGISTRATION, event.domains.toString())
+            _uiModel.value = manageDomainsListItems(event.domains)
+        }
+    }
+
+    private fun manageDomainsListItems(domains: List<Domain>?): List<DomainsListItem> {
         val listItems = mutableListOf<DomainsListItem>()
 
         listItems += PrimaryDomain(UiStringText(siteUrl), this::onChangeSiteClick)
 
         listItems += SiteDomainsHeader(UiStringRes(string.domains_site_domains))
+
+        domains?.forEach {
+            if (!it.wpcomDomain && !it.isWpcomStagingDomain) {
+                listItems += SiteDomains(
+                        UiStringText(it.domain.toString()),
+                        UiStringResWithParams(
+                                string.domains_site_domain_expires, listOf(UiStringText(it.expiry.toString()))
+                        )
+                )
+            }
+        }
 
         // if site has redirected domain then show this blurb
         if (!hasCustomDomain) {
@@ -117,11 +162,6 @@ class DomainsDashboardViewModel @Inject constructor(
                     )
             )
         }
-
-        // TODO: Loop through and add all domains, replace hard coded date.  Not sure where to find this info yet!
-        listItems += SiteDomains(
-                UiStringText(siteUrl),
-                UiStringResWithParams(string.domains_site_domain_expires, listOf(UiStringText("03/09/2024"))))
 
         listItems += AddDomain(ListItemInteraction.create(this::onAddDomainClick))
 
