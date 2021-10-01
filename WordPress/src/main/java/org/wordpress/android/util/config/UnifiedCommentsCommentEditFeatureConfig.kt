4aab6a8d8b7041a6b8f2677c12ea7d28daebdf23
develric <develric@gmail.com>
2021-10-01 15:51:19 +0200

    Enabling remote feature flag.

--- WordPress/src/main/java/org/wordpress/android/util/config/UnifiedCommentsCommentEditFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/UnifiedCommentsCommentEditFeatureConfig.kt
@@ -1,11 +1,17 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.annotation.Feature
+import org.wordpress.android.util.config.UnifiedCommentsCommentEditFeatureConfig.Companion.UNIFIED_COMMENTS_COMMENT_EDIT_FIELD
 import javax.inject.Inject
 
-@FeatureInDevelopment
+@Feature(UNIFIED_COMMENTS_COMMENT_EDIT_FIELD, true)
 class UnifiedCommentsCommentEditFeatureConfig @Inject constructor(appConfig: AppConfig) : FeatureConfig(
         appConfig,
-        BuildConfig.UNIFIED_COMMENTS_COMMENT_EDIT
-)
+        BuildConfig.UNIFIED_COMMENTS_COMMENT_EDIT,
+        UNIFIED_COMMENTS_COMMENT_EDIT_FIELD
+) {
+    companion object {
+        const val UNIFIED_COMMENTS_COMMENT_EDIT_FIELD = "unified_comments_comment_edit_enabled"
+    }
+}
