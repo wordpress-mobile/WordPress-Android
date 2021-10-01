da10c704adae92a382fab9d3d7e146e92d670341
develric <develric@gmail.com>
2021-10-01 17:11:43 +0200

    Renaming remote field for clarity.

--- WordPress/src/main/java/org/wordpress/android/util/config/UnifiedCommentsCommentEditFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/UnifiedCommentsCommentEditFeatureConfig.kt
@@ -2,16 +2,16 @@ package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.Feature
-import org.wordpress.android.util.config.UnifiedCommentsCommentEditFeatureConfig.Companion.UNIFIED_COMMENTS_COMMENT_EDIT_FIELD
+import org.wordpress.android.util.config.UnifiedCommentsCommentEditFeatureConfig.Companion.UNIFIED_COMMENTS_EDIT_REMOTE_FIELD
 import javax.inject.Inject
 
-@Feature(UNIFIED_COMMENTS_COMMENT_EDIT_FIELD, true)
+@Feature(UNIFIED_COMMENTS_EDIT_REMOTE_FIELD, true)
 class UnifiedCommentsCommentEditFeatureConfig @Inject constructor(appConfig: AppConfig) : FeatureConfig(
         appConfig,
         BuildConfig.UNIFIED_COMMENTS_COMMENT_EDIT,
-        UNIFIED_COMMENTS_COMMENT_EDIT_FIELD
+        UNIFIED_COMMENTS_EDIT_REMOTE_FIELD
 ) {
     companion object {
-        const val UNIFIED_COMMENTS_COMMENT_EDIT_FIELD = "unified_comments_comment_edit_enabled"
+        const val UNIFIED_COMMENTS_EDIT_REMOTE_FIELD = "unified_comments_edit_remote_field"
     }
 }
