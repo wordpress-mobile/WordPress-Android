258f726cd9dc19eeef8cebd72a744338e71a6140
ashiagr <agr.ash@gmail.com>
2021-10-04 17:28:13 +0530

    Get file size for file with content URI protocol

--- WordPress/src/main/java/org/wordpress/android/ui/media/VideoLoader.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/media/VideoLoader.kt
@@ -1,5 +1,8 @@
 package org.wordpress.android.ui.media
 
+import android.content.Context
+import android.net.Uri
+import android.provider.OpenableColumns
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -9,6 +12,7 @@ import org.wordpress.android.modules.BG_THREAD
 import org.wordpress.android.modules.UI_THREAD
 import org.wordpress.android.ui.utils.AuthenticationUtils
 import org.wordpress.android.util.AppLog.T
+import org.wordpress.android.util.MediaUtilsWrapper
 import java.io.IOException
 import java.net.URL
 import javax.inject.Inject
@@ -19,7 +23,9 @@ class VideoLoader
     @param:Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
     @param:Named(UI_THREAD) private val mainDispatcher: CoroutineDispatcher,
     private val authenticationUtils: AuthenticationUtils,
-    private val appLogWrapper: AppLogWrapper
+    private val appLogWrapper: AppLogWrapper,
+    private val mediaUtilsWrapper: MediaUtilsWrapper,
+    private val appContext: Context
 ) {
     fun runIfMediaNotTooBig(
         scope: CoroutineScope,
@@ -31,7 +37,12 @@ class VideoLoader
             var length = MIN_SIZE
             withContext(bgDispatcher) {
                 try {
-                    length = getSizeFromURL(URL(filePath))
+                    val uri = Uri.parse(filePath)
+                    length = if (mediaUtilsWrapper.isInMediaStore(uri)) {
+                        getSizeFromContentUri(uri)
+                    } else {
+                        getSizeFromURL(URL(filePath))
+                    }
                 } catch (ioe: IOException) {
                     appLogWrapper.e(T.MEDIA, "Failed to load video thumbnail: ${ioe.stackTrace}")
                 }
@@ -45,6 +56,14 @@ class VideoLoader
             }
         }
     }
+
+    private fun getSizeFromContentUri(contentUri: Uri) =
+            appContext.contentResolver.query(contentUri, null, null, null, null, null).use { cursor ->
+                cursor?.moveToFirst()?.takeIf { true }?.let {
+                    val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+                    if (!cursor.isNull(sizeIndex)) cursor.getInt(sizeIndex) else null
+                } ?: 0
+            }
 
     private fun getSizeFromURL(url: URL): Int {
         val urlConnection = url.openConnection()
