43be5a15c2622ee4ea2d3af5e1e468b047573be1
ashiagr <agr.ash@gmail.com>
2021-10-06 14:23:19 +0530

    Apply review comments

--- WordPress/src/main/java/org/wordpress/android/util/config/MySiteDashboardPhase2FeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/MySiteDashboardPhase2FeatureConfig.kt
@@ -4,9 +4,14 @@ import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.FeatureInDevelopment
 import javax.inject.Inject
 
+/**
+ * Configuration of the 'My Site Dashboard - Phase 2', which amongst others includes the new 'Dashboard Post Cards'
+ * that will be added on the 'My Site' screen.
+ */
 @FeatureInDevelopment
-class MySiteDashboardPhase2FeatureConfig
-@Inject constructor(appConfig: AppConfig) : FeatureConfig(
+class MySiteDashboardPhase2FeatureConfig @Inject constructor(
+    appConfig: AppConfig
+) : FeatureConfig(
         appConfig,
         BuildConfig.MY_SITE_DASHBOARD_PHASE_2
 )
