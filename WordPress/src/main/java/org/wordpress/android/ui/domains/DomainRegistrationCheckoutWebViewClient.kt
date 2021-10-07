b99fa81a8854a5e1ce6f7a9f8c68f6c9643c1049
Renan Ferrari <renan.fb@gmail.com>
2021-10-06 21:33:43 -0300

    Add DomainRegistrationCheckoutWebViewClient

---
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationCheckoutWebViewClient.kt
@@ -0,0 +1,7 @@
+package org.wordpress.android.ui.domains
+
+import org.wordpress.android.util.ErrorManagedWebViewClient
+
+class DomainRegistrationCheckoutWebViewClient(
+    baseListener: ErrorManagedWebViewClientListener
+) : ErrorManagedWebViewClient(baseListener)
