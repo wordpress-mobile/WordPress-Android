d47ca3349caa25475ea3076bc94128a5201738b0
Renan Ferrari <renan.fb@gmail.com>
2021-10-08 14:34:50 -0300

    Rename domains dashboard fragment layout for consistency

--- WordPress/src/main/java/org/wordpress/android/ui/domains/DomainsDashboardFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainsDashboardFragment.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
-import org.wordpress.android.databinding.FragmentDomainsDashboardBinding
+import org.wordpress.android.databinding.DomainsDashboardFragmentBinding
 import org.wordpress.android.ui.ActivityLauncher
 import org.wordpress.android.ui.domains.DomainRegistrationActivity.DomainRegistrationPurpose.CTA_DOMAIN_CREDIT_REDEMPTION
 import org.wordpress.android.ui.domains.DomainsNavigationEvents.GetDomain
@@ -15,7 +15,7 @@ import org.wordpress.android.ui.utils.UiHelpers
 import org.wordpress.android.viewmodel.observeEvent
 import javax.inject.Inject
 
-class DomainsDashboardFragment : Fragment(R.layout.fragment_domains_dashboard) {
+class DomainsDashboardFragment : Fragment(R.layout.domains_dashboard_fragment) {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     @Inject lateinit var uiHelpers: UiHelpers
     private lateinit var viewModel: DomainsDashboardViewModel
@@ -24,14 +24,14 @@ class DomainsDashboardFragment : Fragment(R.layout.fragment_domains_dashboard) {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity().application as WordPress).component().inject(this)
 
-        with(FragmentDomainsDashboardBinding.bind(view)) {
+        with(DomainsDashboardFragmentBinding.bind(view)) {
             setupViews()
             setupViewModel()
             setupObservers()
         }
     }
 
-    private fun FragmentDomainsDashboardBinding.setupViews() {
+    private fun DomainsDashboardFragmentBinding.setupViews() {
         contentRecyclerView.adapter = DomainsDashboardAdapter(uiHelpers)
     }
 
@@ -40,7 +40,7 @@ class DomainsDashboardFragment : Fragment(R.layout.fragment_domains_dashboard) {
         viewModel.start()
     }
 
-    private fun FragmentDomainsDashboardBinding.setupObservers() {
+    private fun DomainsDashboardFragmentBinding.setupObservers() {
         viewModel.uiModel.observe(viewLifecycleOwner) { uiState ->
             (contentRecyclerView.adapter as? DomainsDashboardAdapter)?.submitList(uiState ?: listOf())
         }
