47008fb40a8bc2aea2ec355a4e17e098b9b5aee6
Renan Ferrari <renan.fb@gmail.com>
2021-10-08 16:45:49 -0300

    Add navigation action to claim domain

--- WordPress/src/main/java/org/wordpress/android/ui/domains/DomainsDashboardNavigationAction.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainsDashboardNavigationAction.kt
@@ -4,5 +4,6 @@ import org.wordpress.android.fluxc.model.SiteModel
 
 sealed class DomainsDashboardNavigationAction {
     data class GetDomain(val site: SiteModel) : DomainsDashboardNavigationAction()
+    data class ClaimDomain(val site: SiteModel) : DomainsDashboardNavigationAction()
     data class OpenManageDomains(val url: String) : DomainsDashboardNavigationAction()
 }
