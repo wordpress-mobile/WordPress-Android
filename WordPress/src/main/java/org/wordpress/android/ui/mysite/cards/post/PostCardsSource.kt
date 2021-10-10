1a20c484a6f938fc621c9f7dc6f3ebc8a8085ca7
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-10-10 12:59:51 -0400

    New source for posts, uses mock data

---
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/post/PostCardsSource.kt
@@ -0,0 +1,29 @@
+package org.wordpress.android.ui.mysite.cards.post
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.wordpress.android.R
+import org.wordpress.android.ui.mysite.MySiteSource
+import org.wordpress.android.ui.mysite.MySiteUiState.PartialState.PostsUpdate
+import org.wordpress.android.ui.mysite.cards.post.mockdata.MockedDataJsonUtils
+import org.wordpress.android.ui.mysite.cards.post.mockdata.MockedPostsData
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class PostCardsSource @Inject constructor(
+    private val mockedDataJsonUtils: MockedDataJsonUtils
+) : MySiteSource<PostsUpdate> {
+    override fun buildSource(coroutineScope: CoroutineScope, siteLocalId: Int): LiveData<PostsUpdate> {
+        val result = MutableLiveData<PostsUpdate>()
+        result.value = PostsUpdate(MockedPostsData())
+        coroutineScope.launch {
+            val jsonString = mockedDataJsonUtils.getJsonStringFromRawResource(R.raw.dashboard_cards)
+            val mockedPostsData = mockedDataJsonUtils.getMockedPostsDataFromJsonString(jsonString!!)
+            result.postValue(PostsUpdate(mockedPostsData = mockedPostsData))
+        }
+        return result
+    }
+}
