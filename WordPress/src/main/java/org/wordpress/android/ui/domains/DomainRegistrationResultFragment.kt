520badaf0502fd3308193d5401788b871071fd42
Renan Ferrari <renan.fb@gmail.com>
2021-10-12 22:15:06 -0300

    DomainRegistrationResultFragment cleanup

--- WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationResultFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationResultFragment.kt
@@ -5,8 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.HtmlCompat
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
+import androidx.core.text.parseAsHtml
 import androidx.fragment.app.Fragment
 import org.wordpress.android.R
 import org.wordpress.android.databinding.DomainRegistrationResultFragmentBinding
@@ -15,50 +15,41 @@ import org.wordpress.android.util.setLightNavigationBar
 import org.wordpress.android.util.setLightStatusBar
 
 class DomainRegistrationResultFragment : Fragment(R.layout.domain_registration_result_fragment) {
-    private var domainName: String? = null
-    private var email: String? = null
-
     companion object {
         private const val EXTRA_REGISTERED_DOMAIN_NAME = "extra_registered_domain_name"
         private const val EXTRA_REGISTERED_DOMAIN_EMAIL = "extra_registered_domain_email"
         const val RESULT_REGISTERED_DOMAIN_EMAIL = "RESULT_REGISTERED_DOMAIN_EMAIL"
         const val TAG = "DOMAIN_REGISTRATION_RESULT_FRAGMENT"
 
-        fun newInstance(domainName: String, email: String?): DomainRegistrationResultFragment {
-            val fragment = DomainRegistrationResultFragment()
-            val bundle = Bundle()
-            bundle.putString(EXTRA_REGISTERED_DOMAIN_NAME, domainName)
-            bundle.putString(EXTRA_REGISTERED_DOMAIN_EMAIL, email)
-            fragment.arguments = bundle
-            return fragment
+        fun newInstance(domainName: String, email: String?) = DomainRegistrationResultFragment().apply {
+            arguments = Bundle().apply {
+                putString(EXTRA_REGISTERED_DOMAIN_NAME, domainName)
+                putString(EXTRA_REGISTERED_DOMAIN_EMAIL, email)
+            }
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        domainName = arguments?.getString(EXTRA_REGISTERED_DOMAIN_NAME, "")
-        email = arguments?.getString(EXTRA_REGISTERED_DOMAIN_EMAIL, "")
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val domainName = requireArguments().getString(EXTRA_REGISTERED_DOMAIN_NAME).orEmpty()
+        val email = requireArguments().getString(EXTRA_REGISTERED_DOMAIN_EMAIL).orEmpty()
+
         setupWindow()
         setupToolbar()
+
         with(DomainRegistrationResultFragmentBinding.bind(view)) {
             continueButton.setOnClickListener {
-                val intent = Intent()
-                intent.putExtra(RESULT_REGISTERED_DOMAIN_EMAIL, email)
-                val nonNullActivity = requireActivity()
-                nonNullActivity.setResult(RESULT_OK, intent)
-                nonNullActivity.finish()
+                with(requireActivity()) {
+                    setResult(RESULT_OK, Intent().putExtra(RESULT_REGISTERED_DOMAIN_EMAIL, email))
+                    finish()
+                }
             }
 
-            domainRegistrationResultMessage.text = HtmlCompat.fromHtml(
-                    getString(
-                            R.string.domain_registration_result_description,
-                            domainName
-                    ), FROM_HTML_MODE_COMPACT
-            )
+            domainRegistrationResultMessage.text = getString(
+                    R.string.domain_registration_result_description,
+                    domainName
+            ).parseAsHtml(FROM_HTML_MODE_COMPACT)
         }
     }
 
