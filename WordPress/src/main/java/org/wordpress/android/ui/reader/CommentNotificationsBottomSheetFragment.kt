a04bbeb7ae82d7aa43535e994b72068a29514c5f
develric <develric@gmail.com>
2021-10-14 02:30:38 +0200

    Creating bottom sheet for follow conversation management.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/CommentNotificationsBottomSheetFragment.kt
@@ -0,0 +1,94 @@
+package org.wordpress.android.ui.reader
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.SwitchCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
+import org.wordpress.android.R
+import org.wordpress.android.WordPress
+import org.wordpress.android.databinding.CommentNotificationsBottomSheetBinding
+import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.viewmodel.ContextProvider
+import org.wordpress.android.viewmodel.observeEvent
+import org.wordpress.android.widgets.WPSnackbar
+import javax.inject.Inject
+
+class CommentNotificationsBottomSheetFragment : BottomSheetDialogFragment() {
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject lateinit var contextProvider: ContextProvider
+    @Inject lateinit var uiHelpers: UiHelpers
+    private lateinit var viewModel: ReaderCommentListViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.comment_notifications_bottom_sheet, container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val isReceivingPushNotifications = requireArguments().getBoolean(ARG_IS_RECEIVING_PUSH_NOTIFICATIONS)
+
+        with(CommentNotificationsBottomSheetBinding.bind(view)) {
+            viewModel = ViewModelProvider(
+                    requireActivity(),
+                    viewModelFactory
+            ).get(ReaderCommentListViewModel::class.java)
+
+            if (savedInstanceState == null) {
+                enablePushNotifications.isChecked = isReceivingPushNotifications
+            }
+
+            unfollowConversation.setOnClickListener {
+                viewModel.onUnfollowTapped()
+            }
+
+            enablePushNotifications.setOnClickListener {
+                viewModel.onChangePushNotificationsRequest((it as SwitchCompat).isChecked)
+            }
+
+            viewModel.snackbarEvents.observeEvent(viewLifecycleOwner, { messageHolder ->
+                if (!isAdded) return@observeEvent
+
+                WPSnackbar.make(
+                        coordinator,
+                        uiHelpers.getTextOfUiString(contextProvider.getContext(), messageHolder.message),
+                        Snackbar.LENGTH_LONG
+                ).show()
+            })
+
+            viewModel.pushNotificationsStatusUpdate.observeEvent(viewLifecycleOwner, { isReceivingPushNotifications ->
+                enablePushNotifications.isChecked = isReceivingPushNotifications
+            })
+        }
+
+        (dialog as? BottomSheetDialog)?.apply {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            behavior.skipCollapsed = true
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().applicationContext as WordPress).component().inject(this)
+    }
+
+    companion object {
+        private const val ARG_IS_RECEIVING_PUSH_NOTIFICATIONS = "ARG_IS_RECEIVING_PUSH_NOTIFICATIONS"
+
+        @JvmStatic
+        fun newInstance(isReceivingPushNotifications: Boolean): CommentNotificationsBottomSheetFragment {
+            val fragment = CommentNotificationsBottomSheetFragment()
+            val bundle = Bundle()
+            bundle.putBoolean(ARG_IS_RECEIVING_PUSH_NOTIFICATIONS, isReceivingPushNotifications)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+}
