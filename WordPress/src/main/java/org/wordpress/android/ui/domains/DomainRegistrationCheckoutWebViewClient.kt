890355c99e4c3531a331180578dfeb2e33635b43
Renan Ferrari <renan.fb@gmail.com>
2021-10-15 16:38:34 -0300

    Restrict navigation to certain URLs

--- WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationCheckoutWebViewClient.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationCheckoutWebViewClient.kt
@@ -1,13 +1,16 @@
 package org.wordpress.android.ui.domains
 
+import android.net.Uri
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
+import org.wordpress.android.ui.domains.DomainRegistrationCheckoutWebViewNavigationDelegate.toUrl
 import org.wordpress.android.util.ErrorManagedWebViewClient
 
 class DomainRegistrationCheckoutWebViewClient(
     private val listener: DomainRegistrationCheckoutWebViewClientListener
 ) : ErrorManagedWebViewClient(listener) {
+    private val navigationDelegate = DomainRegistrationCheckoutWebViewNavigationDelegate
     private var hasRegistered = false
 
     interface DomainRegistrationCheckoutWebViewClientListener : ErrorManagedWebViewClientListener {
@@ -29,6 +32,10 @@ class DomainRegistrationCheckoutWebViewClient(
 
         return super.shouldInterceptRequest(view, request)
     }
+
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest) = !canNavigateTo(request.url)
+
+    private fun canNavigateTo(uri: Uri) = navigationDelegate.canNavigateTo(uri.toUrl())
 
     companion object {
         private const val WPCOM_API_HOST = "public-api.wordpress.com"
