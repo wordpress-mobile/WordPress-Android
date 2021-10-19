79cb80cfaa416db17d768c737452ed14d1f004d8
Renan Ferrari <renan.fb@gmail.com>
2021-10-18 23:16:55 -0300

    Update logic to intercept checkout success page

--- WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationCheckoutWebViewClient.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainRegistrationCheckoutWebViewClient.kt
@@ -2,8 +2,8 @@ package org.wordpress.android.ui.domains
 
 import android.net.Uri
 import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
 import android.webkit.WebView
+import androidx.core.net.toUri
 import org.wordpress.android.ui.domains.DomainRegistrationCheckoutWebViewNavigationDelegate.toUrl
 import org.wordpress.android.util.ErrorManagedWebViewClient
 
@@ -11,33 +11,26 @@ class DomainRegistrationCheckoutWebViewClient(
     private val listener: DomainRegistrationCheckoutWebViewClientListener
 ) : ErrorManagedWebViewClient(listener) {
     private val navigationDelegate = DomainRegistrationCheckoutWebViewNavigationDelegate
-    private var hasRegistered = false
 
     interface DomainRegistrationCheckoutWebViewClientListener : ErrorManagedWebViewClientListener {
         fun onCheckoutSuccess()
-    }
-
-    override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? {
-        val host = request.url.host.orEmpty()
-        val path = request.url.path.orEmpty()
-
-        // TODO Revisit this logic
-        if (host == WPCOM_API_HOST) {
-            if (hasRegistered) {
-                listener.onCheckoutSuccess()
-            } else if (path == "/rest/v1.1/me/transactions") {
-                hasRegistered = true
-            }
-        }
-
-        return super.shouldInterceptRequest(view, request)
     }
 
     override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest) = !canNavigateTo(request.url)
 
     private fun canNavigateTo(uri: Uri) = navigationDelegate.canNavigateTo(uri.toUrl())
 
+    override fun doUpdateVisitedHistory(view: WebView, url: String, isReload: Boolean) {
+        if (isCheckoutSuccessPage(url.toUri())) {
+            listener.onCheckoutSuccess()
+        } else {
+            super.doUpdateVisitedHistory(view, url, isReload)
+        }
+    }
+
+    private fun isCheckoutSuccessPage(uri: Uri) = uri.path.orEmpty().startsWith(CHECKOUT_SUCCESS_PATH_PREFIX)
+
     companion object {
-        private const val WPCOM_API_HOST = "public-api.wordpress.com"
+        private const val CHECKOUT_SUCCESS_PATH_PREFIX = "/checkout/thank-you/"
     }
 }
