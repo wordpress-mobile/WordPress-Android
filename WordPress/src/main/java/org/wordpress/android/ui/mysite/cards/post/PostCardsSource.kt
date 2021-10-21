6eb29bd55f02cca96b38dbe2023dd75e71f9c6bc
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-10-21 16:02:02 -0400

    Add refresh functionality including new liveData indicating the refresh is finished

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/post/PostCardsSource.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/post/PostCardsSource.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.ui.mysite.cards.post
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -9,6 +10,7 @@ import org.wordpress.android.ui.mysite.MySiteSource
 import org.wordpress.android.ui.mysite.MySiteUiState.PartialState.PostsUpdate
 import org.wordpress.android.ui.mysite.cards.post.mockdata.MockedDataJsonUtils
 import org.wordpress.android.ui.mysite.cards.post.mockdata.MockedPostsData
+import org.wordpress.android.viewmodel.Event
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,14 +18,32 @@ import javax.inject.Singleton
 class PostCardsSource @Inject constructor(
     private val mockedDataJsonUtils: MockedDataJsonUtils
 ) : MySiteSource<PostsUpdate> {
+    private val data = MutableLiveData<PostsUpdate>()
+    private val _refreshFinished = MutableLiveData<Event<Unit>>()
+    val refreshFinished = _refreshFinished as LiveData<Event<Unit>>
+
     override fun buildSource(coroutineScope: CoroutineScope, siteLocalId: Int): LiveData<PostsUpdate> {
-        val result = MutableLiveData<PostsUpdate>()
+        val result = MediatorLiveData<PostsUpdate>()
         result.value = PostsUpdate(MockedPostsData())
         coroutineScope.launch {
             val jsonString = mockedDataJsonUtils.getJsonStringFromRawResource(R.raw.mocked_posts_data)
             val mockedPostsData = mockedDataJsonUtils.getMockedPostsDataFromJsonString(jsonString!!)
-            result.postValue(PostsUpdate(mockedPostsData = mockedPostsData))
+            result.value = PostsUpdate(mockedPostsData = mockedPostsData)
+            result.addSource(data) {
+                result.value = it
+            }
         }
         return result
+    }
+
+    fun refresh(coroutineScope: CoroutineScope) {
+        coroutineScope.launch {
+            val jsonString = mockedDataJsonUtils.getJsonStringFromRawResource(R.raw.mocked_refresh_posts_data)
+            val mockedPostsData = mockedDataJsonUtils.getMockedPostsDataFromJsonString(jsonString!!)
+            _refreshFinished.postValue(Event(Unit))
+            if (mockedPostsData != data.value?.mockedPostsData) {
+                data.postValue(PostsUpdate(mockedPostsData = mockedPostsData))
+            }
+        }
     }
 }
