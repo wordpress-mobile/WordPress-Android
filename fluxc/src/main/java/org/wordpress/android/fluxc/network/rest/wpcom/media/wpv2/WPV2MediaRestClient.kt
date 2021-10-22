f6fe5a865d6703bddaea3c4c5f0cbcea85020d1a
hicham <hicham.boushaba@gmail.com>
2021-10-22 15:35:11 +0100

    Add support for upload cancellation

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/wpv2/WPV2MediaRestClient.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/wpv2/WPV2MediaRestClient.kt
@@ -4,18 +4,17 @@ import android.content.Context
 import com.android.volley.RequestQueue
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
-import kotlinx.coroutines.CancellableContinuation
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.Channel.Factory
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.sendBlocking
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
@@ -24,6 +23,7 @@ import okhttp3.Response
 import org.json.JSONException
 import org.json.JSONObject
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.MediaActionBuilder
 import org.wordpress.android.fluxc.generated.UploadActionBuilder
 import org.wordpress.android.fluxc.generated.endpoint.WPAPI
 import org.wordpress.android.fluxc.model.MediaModel
@@ -40,13 +40,12 @@ import org.wordpress.android.fluxc.store.MediaStore.MediaErrorType.REQUEST_TOO_L
 import org.wordpress.android.fluxc.store.MediaStore.ProgressPayload
 import org.wordpress.android.fluxc.tools.CoroutineEngine
 import org.wordpress.android.util.AppLog
-import org.wordpress.android.util.AppLog.T
 import org.wordpress.android.util.AppLog.T.MEDIA
 import java.io.IOException
+import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
-import kotlin.coroutines.resume
 
 @Singleton
 class WPV2MediaRestClient @Inject constructor(
@@ -60,22 +59,42 @@ class WPV2MediaRestClient @Inject constructor(
 ) : BaseWPComRestClient(appContext, dispatcher, requestQueue, accessToken, userAgent) {
     private val gson: Gson by lazy { Gson() }
 
+    private val currentUploads = ConcurrentHashMap<Int, CoroutineScope>()
+
     fun uploadMedia(site: SiteModel, media: MediaModel) {
-        coroutineEngine.launch(MEDIA, this, "Upload Media using WPCom's /wp/v2 API") {
+        coroutineEngine.launch(MEDIA, this, "Upload Media using WPCom's v2 API") {
             syncUploadMedia(site, media)
+                    .onStart {
+                        currentUploads[media.id] = this@launch
+                    }
+                    .onCompletion {
+                        currentUploads.remove(media.id)
+                    }
                     .collect { payload ->
                         mDispatcher.dispatch(UploadActionBuilder.newUploadedMediaAction(payload))
                     }
         }
     }
 
-    fun syncUploadMedia(site: SiteModel, media: MediaModel): Flow<ProgressPayload> {
+    fun cancelUpload(media: MediaModel) {
+        currentUploads[media.id]?.let { scope ->
+            scope.cancel()
+            val payload = ProgressPayload(media, 0f, false, true)
+            mDispatcher.dispatch(MediaActionBuilder.newCanceledMediaUploadAction(payload))
+        }
+    }
+
+    private fun syncUploadMedia(site: SiteModel, media: MediaModel): Flow<ProgressPayload> {
         return callbackFlow {
             val url = WPAPI.media.getWPComUrl(site.siteId)
             val body = WPRestUploadRequestBody(media) { media, progress ->
                 if (!isClosedForSend) {
                     val payload = ProgressPayload(media, progress, false, null)
-                    offer(payload)
+                    try {
+                        offer(payload)
+                    } catch (e: CancellationException) {
+                        // the flow has been cancelled
+                    }
                 }
             }
 
@@ -89,7 +108,7 @@ class WPV2MediaRestClient @Inject constructor(
 
             call.enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
-                    // If the continuation has been canceled, then ignore errors
+                    // If the upload has been canceled, then ignore errors
                     if (!isClosedForSend) {
                         val message = "media upload failed: $e"
                         AppLog.w(MEDIA, message)
@@ -106,8 +125,12 @@ class WPV2MediaRestClient @Inject constructor(
                             val res = gson.fromJson(response.body!!.string(), MediaWPRESTResponse::class.java)
                             val uploadedMedia = res.toMediaModel()
                             val payload = ProgressPayload(uploadedMedia, 1f, true, false)
-                            sendBlocking(payload)
-                            close()
+                            try {
+                                sendBlocking(payload)
+                                close()
+                            } catch (e: CancellationException) {
+                                // the flow has been cancelled
+                            }
                         } catch (e: JsonSyntaxException) {
                             AppLog.e(MEDIA, e)
                             val error = MediaError(PARSE_ERROR)
@@ -133,8 +156,12 @@ class WPV2MediaRestClient @Inject constructor(
     private fun ProducerScope<ProgressPayload>.handleFailure(media: MediaModel, error: MediaError) {
         media.setUploadState(FAILED)
         val payload = ProgressPayload(media, 1f, false, error)
-        sendBlocking(payload)
-        close()
+        try {
+            sendBlocking(payload)
+            close()
+        } catch (e: CancellationException) {
+            // the flow has been cancelled
+        }
     }
 
     private fun Response.parseError(): MediaError {
