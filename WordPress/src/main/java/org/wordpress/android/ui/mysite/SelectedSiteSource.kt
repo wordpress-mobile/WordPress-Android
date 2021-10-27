2911b3de17187c4c86071a07be923db76e042195
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-10-27 15:16:58 -0400

    Refactor: extract class from MySiteProvider

---
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/SelectedSiteSource.kt
@@ -0,0 +1,18 @@
+package org.wordpress.android.ui.mysite
+
+import kotlinx.coroutines.CoroutineScope
+import org.wordpress.android.ui.mysite.MySiteUiState.PartialState.SelectedSite
+import org.wordpress.android.util.filter
+import org.wordpress.android.util.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SelectedSiteSource @Inject constructor(
+    private val selectedSiteRepository: SelectedSiteRepository
+) : MySiteSource<SelectedSite> {
+    override fun buildSource(coroutineScope: CoroutineScope, siteLocalId: Int) =
+            selectedSiteRepository.selectedSiteChange
+                    .filter { it == null || it.id == siteLocalId }
+                    .map { SelectedSite(it) }
+}
