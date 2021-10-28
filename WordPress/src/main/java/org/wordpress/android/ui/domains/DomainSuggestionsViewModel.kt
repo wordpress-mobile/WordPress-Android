8e59876bd9acaef51b3deef55f51b2e2f1124de1
Ravi <ravi.program@gmail.com>
2021-10-28 11:58:45 +1100

    Update DomainSuggestionsViewModel.kt
    With tracks

--- WordPress/src/main/java/org/wordpress/android/ui/domains/DomainSuggestionsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/DomainSuggestionsViewModel.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.CoroutineDispatcher
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.analytics.AnalyticsTracker.Stat
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.DOMAINS_DASHBOARD_ADD_DOMAIN_TAPPED
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.DOMAINS_PURCHASE_WEBVIEW_VIEWED
+import org.wordpress.android.analytics.AnalyticsTracker.Stat.DOMAINS_SEARCH_SELECT_DOMAIN_TAPPED
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.SiteActionBuilder
 import org.wordpress.android.fluxc.model.SiteModel
@@ -193,6 +196,8 @@ class DomainSuggestionsViewModel @Inject constructor(
             DOMAIN_PURCHASE -> createCart(selectedSuggestion)
             else -> selectDomain(selectedSuggestion)
         }
+
+        analyticsTracker.track(DOMAINS_SEARCH_SELECT_DOMAIN_TAPPED, site)
     }
 
     fun updateSearchQuery(query: String) {
@@ -233,6 +238,7 @@ class DomainSuggestionsViewModel @Inject constructor(
     private fun selectDomain(selectedSuggestion: DomainSuggestionItem) {
         val domainProductDetails = DomainProductDetails(selectedSuggestion.productId, selectedSuggestion.domainName)
         _onDomainSelected.postValue(Event(domainProductDetails))
+        analyticsTracker.track(DOMAINS_PURCHASE_WEBVIEW_VIEWED, site)
     }
 
     private fun showLoadingButton(isLoading: Boolean) {
