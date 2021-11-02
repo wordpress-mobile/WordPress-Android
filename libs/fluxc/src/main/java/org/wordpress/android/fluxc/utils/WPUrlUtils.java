093b61cbd595fa0f95efa494fac89c70232c9eb4
Joel Dean <jdeanjj1000@gmail.com>
2021-11-01 22:08:55 -0500

    Updated comment for urlPath util function.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/utils/WPUrlUtils.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/utils/WPUrlUtils.java
@@ -79,9 +79,9 @@ public class WPUrlUtils {
 
     /**
      * Disallows delim characters as per RFC 3986
-     * https://datatracker.ietf.org/doc/html/rfc3986
+     * https://datatracker.ietf.org/doc/html/rfc3986#section-2.2
      *
-     * Similar fix in iOS
+     * Similar fix in iOS:
      * https://github.com/wordpress-mobile/WordPressKit-iOS/pull/457
      */
     public static String escapeUrlPathWithRFC3986(String urlPath) {
