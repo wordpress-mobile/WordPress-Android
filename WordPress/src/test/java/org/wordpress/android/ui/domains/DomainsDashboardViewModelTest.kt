c6414b45b71944417068a3ecd6a7b967880242f0
Renan Ferrari <renan.fb@gmail.com>
2021-11-01 22:08:20 -0300

    Update tests

--- WordPress/src/test/java/org/wordpress/android/ui/domains/DomainsDashboardViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/domains/DomainsDashboardViewModelTest.kt
@@ -1,22 +1,24 @@
 package org.wordpress.android.ui.domains
 
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.InternalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
 import org.wordpress.android.BaseUnitTest
-import org.wordpress.android.R.string
+import org.wordpress.android.R
 import org.wordpress.android.TEST_DISPATCHER
-import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.model.PlanModel
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.rest.wpcom.site.Domain
 import org.wordpress.android.fluxc.network.rest.wpcom.site.GoogleAppsSubscription
 import org.wordpress.android.fluxc.network.rest.wpcom.site.TitanMailSubscription
 import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.fluxc.store.SiteStore.FetchedDomainsPayload
+import org.wordpress.android.fluxc.store.SiteStore.OnPlansFetched
 import org.wordpress.android.test
 import org.wordpress.android.ui.domains.DomainsDashboardItem.AddDomain
 import org.wordpress.android.ui.domains.DomainsDashboardItem.DomainBlurb
@@ -24,144 +26,107 @@ import org.wordpress.android.ui.domains.DomainsDashboardItem.FreeDomain
 import org.wordpress.android.ui.domains.DomainsDashboardItem.PurchaseDomain
 import org.wordpress.android.ui.domains.DomainsDashboardItem.SiteDomains
 import org.wordpress.android.ui.domains.DomainsDashboardItem.SiteDomainsHeader
+import org.wordpress.android.ui.domains.usecases.FetchPlansUseCase
+import org.wordpress.android.ui.plans.PlansConstants.FREE_PLAN_ID
+import org.wordpress.android.ui.plans.PlansConstants.PREMIUM_PLAN_ID
 import org.wordpress.android.ui.utils.HtmlMessageUtils
 import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 
 class DomainsDashboardViewModelTest : BaseUnitTest() {
-    @Mock private lateinit var siteStore: SiteStore
-    @Mock private lateinit var analyticsTracker: AnalyticsTrackerWrapper
-    @Mock private lateinit var dispatcher: Dispatcher
-    @Mock private lateinit var htmlMessageUtils: HtmlMessageUtils
+    private val siteStore: SiteStore = mock()
+    private val analyticsTracker: AnalyticsTrackerWrapper = mock()
+    private val htmlMessageUtils: HtmlMessageUtils = mock {
+        on { getHtmlMessageFromStringFormatResId(any(), any()) } doReturn ""
+    }
+    private val fetchPlansUseCase: FetchPlansUseCase = mock()
 
     private lateinit var viewModel: DomainsDashboardViewModel
-    private var site: SiteModel = SiteModel()
-    private var domains: List<Domain> = listOf()
 
-    private val siteId = 1234L
-    private val testDomainName = "testdomain.blog"
-    private val navigationActions = mutableListOf<DomainsDashboardNavigationAction>()
     private val uiModel = mutableListOf<DomainsDashboardItem>()
 
     @InternalCoroutinesApi
     @Before
     fun setUp() {
         viewModel = DomainsDashboardViewModel(
-                dispatcher,
                 siteStore,
                 analyticsTracker,
                 htmlMessageUtils,
+                fetchPlansUseCase,
                 TEST_DISPATCHER
         )
 
-        site.siteId = siteId
-        site.url = testDomainName
-        site.unmappedUrl = testDomainName
-        site.planId = 1L
-
-        viewModel.onNavigation.observeForever { it.applyIfNotHandled { navigationActions.add(this) } }
-
-        uiModel.clear()
         viewModel.uiModel.observeForever { if (it != null) uiModel += it }
     }
 
     @Test
-    fun `free plan with custom domain shows manage your domains and get your domain`() = test {
-        addCustomDomain()
-        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
-        whenever(htmlMessageUtils.getHtmlMessageFromStringFormatResId(any(), any())).thenReturn("")
-
-        viewModel.start(site)
+    fun `free plan with custom domains`() = test {
+        setupWith(
+                hasPaidPlan = false,
+                hasCustomDomains = true,
+                hasDomainCredits = false
+        )
 
         val dashboardItems = uiModel
+
+        assertThat(dashboardItems).hasSize(5)
 
         assertThat(dashboardItems[0]).isInstanceOf(FreeDomain::class.java)
         assertThat(dashboardItems[1]).isInstanceOf(SiteDomainsHeader::class.java)
         assertThat(dashboardItems[2]).isInstanceOf(SiteDomains::class.java)
         assertThat(dashboardItems[3]).isInstanceOf(AddDomain::class.java)
         assertThat(dashboardItems[4]).isInstanceOf(DomainBlurb::class.java)
-        assertThat(dashboardItems[5]).isInstanceOf(PurchaseDomain::class.java)
-        assertThat((dashboardItems[5] as PurchaseDomain).title)
-                .isEqualTo(UiStringRes(string.domains_free_plan_get_your_domain_title))
     }
 
     @Test
-    fun `free plan with no custom domain shows get your domain card`() = test {
-        clearCustomDomain()
-        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
-        whenever(htmlMessageUtils.getHtmlMessageFromStringFormatResId(any(), any())).thenReturn("")
-
-        viewModel.start(site)
+    fun `free plan with no custom domains`() = test {
+        setupWith(
+                hasPaidPlan = false,
+                hasCustomDomains = false,
+                hasDomainCredits = false
+        )
 
         val dashboardItems = uiModel
+
+        assertThat(dashboardItems).hasSize(2)
 
         assertThat(dashboardItems[0]).isInstanceOf(FreeDomain::class.java)
         assertThat(dashboardItems[1]).isInstanceOf(PurchaseDomain::class.java)
         assertThat((dashboardItems[1] as PurchaseDomain).title)
-                .isEqualTo(UiStringRes(string.domains_free_plan_get_your_domain_title))
+                .isEqualTo(UiStringRes(R.string.domains_free_plan_get_your_domain_title))
     }
 
     @Test
-    fun `paid plan with primary custom domain and credits shows manage your domains`() = test {
-        addCustomDomain()
-        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
-        whenever(htmlMessageUtils.getHtmlMessageFromStringFormatResId(any(), any())).thenReturn("")
-
-        viewModel.start(site)
+    fun `paid plan with custom domains and credits`() = test {
+        setupWith(
+                hasPaidPlan = true,
+                hasCustomDomains = true,
+                hasDomainCredits = true
+        )
 
         val dashboardItems = uiModel
 
-        assertThat(dashboardItems).isNotEmpty
+        assertThat(dashboardItems).hasSize(4)
+
         assertThat(dashboardItems[0]).isInstanceOf(FreeDomain::class.java)
         assertThat(dashboardItems[1]).isInstanceOf(SiteDomainsHeader::class.java)
         assertThat(dashboardItems[2]).isInstanceOf(SiteDomains::class.java)
-        assertThat(dashboardItems[3]).isInstanceOf(AddDomain::class.java)
+        assertThat(dashboardItems[3]).isInstanceOf(PurchaseDomain::class.java)
+        assertThat((dashboardItems[3] as PurchaseDomain).title)
+                .isEqualTo(UiStringRes(R.string.domains_paid_plan_claim_your_domain_title))
     }
 
     @Test
-    fun `paid plan with primary custom domain and no credits shows manage domains`() = test {
-        addCustomDomain()
-        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
-        whenever(htmlMessageUtils.getHtmlMessageFromStringFormatResId(any(), any())).thenReturn("")
-
-        viewModel.start(site)
-
-        val dashboardItems = uiModel
-
-        assertThat(dashboardItems).isNotEmpty
-        assertThat(dashboardItems[0]).isInstanceOf(FreeDomain::class.java)
-        assertThat(dashboardItems[1]).isInstanceOf(SiteDomainsHeader::class.java)
-        assertThat(dashboardItems[2]).isInstanceOf(SiteDomains::class.java)
-        assertThat(dashboardItems[3]).isInstanceOf(AddDomain::class.java)
-    }
-
-    @Test
-    fun `paid plan with secondary custom domain and credits shows manage your domains`() = test {
-        addCustomDomain()
-        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
-        whenever(htmlMessageUtils.getHtmlMessageFromStringFormatResId(any(), any())).thenReturn("")
-
-        viewModel.start(site)
+    fun `paid plan with custom domains and no credits`() = test {
+        setupWith(
+                hasPaidPlan = true,
+                hasCustomDomains = true,
+                hasDomainCredits = false
+        )
 
         val dashboardItems = uiModel
 
-        assertThat(dashboardItems).isNotEmpty
-        assertThat(dashboardItems[0]).isInstanceOf(FreeDomain::class.java)
-        assertThat(dashboardItems[1]).isInstanceOf(SiteDomainsHeader::class.java)
-        assertThat(dashboardItems[2]).isInstanceOf(SiteDomains::class.java)
-        assertThat(dashboardItems[3]).isInstanceOf(AddDomain::class.java)
-    }
-
-    // Ignored as manage domains is de-scoped for v1
-    @Test
-    fun `paid plan with secondary custom domain and no credits shows manage domain items`() = test {
-        addCustomDomain()
-        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
-        whenever(htmlMessageUtils.getHtmlMessageFromStringFormatResId(any(), any())).thenReturn("")
-
-        viewModel.start(site)
-
-        val dashboardItems = uiModel
+        assertThat(dashboardItems).hasSize(4)
 
         assertThat(dashboardItems[0]).isInstanceOf(FreeDomain::class.java)
         assertThat(dashboardItems[1]).isInstanceOf(SiteDomainsHeader::class.java)
@@ -170,106 +135,90 @@ class DomainsDashboardViewModelTest : BaseUnitTest() {
     }
 
     @Test
-    fun `paid plan with no custom domain and credits shows claim your domain card`() = test {
-        clearCustomDomain()
-        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
-        whenever(htmlMessageUtils.getHtmlMessageFromStringFormatResId(any(), any())).thenReturn("")
-
-        viewModel.start(site)
+    fun `paid plan with no custom domains and credits`() = test {
+        setupWith(
+                hasPaidPlan = true,
+                hasCustomDomains = false,
+                hasDomainCredits = true
+        )
 
         val dashboardItems = uiModel
+
+        assertThat(dashboardItems).hasSize(2)
 
         assertThat(dashboardItems[0]).isInstanceOf(FreeDomain::class.java)
         assertThat(dashboardItems[1]).isInstanceOf(PurchaseDomain::class.java)
     }
 
     @Test
-    fun `paid plan with no custom domain and no credits shows get your domain card`() = test {
-        clearCustomDomain()
-        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
-        whenever(htmlMessageUtils.getHtmlMessageFromStringFormatResId(any(), any())).thenReturn("")
-
-        viewModel.start(site)
+    fun `paid plan with no custom domains and no credits`() = test {
+        setupWith(
+                hasPaidPlan = true,
+                hasCustomDomains = false,
+                hasDomainCredits = false
+        )
 
         val dashboardItems = uiModel
+
+        assertThat(dashboardItems).hasSize(2)
 
         assertThat(dashboardItems[0]).isInstanceOf(FreeDomain::class.java)
         assertThat(dashboardItems[1]).isInstanceOf(PurchaseDomain::class.java)
         assertThat((dashboardItems[1] as PurchaseDomain).title)
-                .isEqualTo(UiStringRes(string.domains_free_plan_get_your_domain_title))
+                .isEqualTo(UiStringRes(R.string.domains_paid_plan_add_your_domain_title))
     }
 
-    private fun clearCustomDomain() {
-        domains = emptyList()
+    private suspend fun setupWith(hasPaidPlan: Boolean, hasCustomDomains: Boolean, hasDomainCredits: Boolean) {
+        val site = if (hasPaidPlan) siteWithPaidPlan else siteWithFreePlan
+        val domains = if (hasCustomDomains) listOf(customDomain) else emptyList()
+        whenever(siteStore.fetchSiteDomains(site)).thenReturn(FetchedDomainsPayload(site, domains))
+
+        val plan = if (hasDomainCredits) planWithCredits else planWithNoCredits
+        whenever(fetchPlansUseCase.execute(site)).thenReturn(OnPlansFetched(site, listOf(plan)))
+
+        viewModel.start(site)
     }
 
-    private fun addCustomDomain() {
-        clearCustomDomain()
-        domains = listOf(Domain(
-                aRecordsRequiredForMapping = listOf("192.0.78.138, 192.0.78.237"),
-                autoRenewalDate = "May 9, 2022",
-                autoRenewing = true,
-                blogId = 189860817,
-                bundledPlanSubscriptionId = "17182973",
-                canSetAsPrimary = true,
-                connectionMode = null,
-                contactInfoDisclosed = false,
-                contactInfoDisclosureAvailable = false,
-                currentUserCanAddEmail = true,
-                currentUserCanCreateSiteFromDomainOnly = false,
-                currentUserCanManage = true,
-                currentUserCannotAddEmailReason = null,
+    companion object {
+        private const val TEST_SITE_ID = 1234L
+        private const val TEST_DOMAIN_NAME = "testdomain.blog"
+
+        private val customDomain = Domain(
                 domain = "henna.tattoo",
-                domainLockingAvailable = false,
-                domainRegistrationAgreementUrl = "",
-                emailForwardsCount = 0,
                 expired = false,
                 expiry = "June 8, 2022",
                 expirySoon = false,
-                googleAppsSubscription = GoogleAppsSubscription(status = "no_subscription"),
-                hasPrivateRegistration = false,
-                hasRegistration = false,
-                hasWpcomNameservers = false,
-                hasZone = true,
-                isEligibleForInboundTransfer = false,
-                isLocked = false,
-                isPendingIcannVerification = false,
-                isPremium = false,
-                isRedeemable = false,
-                isRenewable = false,
-                isSubdomain = false,
-                isWhoisEditable = false,
-                isWpcomStagingDomain = false,
-                manualTransferRequired = false,
-                newRegistration = false,
-                owner = "Test (tester)",
-                partnerDomain = false,
-                pendingRegistration = false,
-                pendingRegistrationTime = "",
-                pendingTransfer = false,
-                pendingWhoisUpdate = false,
-                pointsToWpcom = false,
                 primaryDomain = false,
-                privacyAvailable = false,
-                privateDomain = false,
-                productSlug = "domain_map",
-                redeemableUntil = null,
-                registrar = null,
-                registrationDate = "July 27, 2021",
-                renewableUntil = null,
-                sslStatus = "pending",
-                subdomainPart = null,
-                subscriptionId = "17447242",
-                supportsDomainConnect = false,
-                supportsGdprConsentManagement = false,
-                supportsTransferApproval = false,
-                titanMailSubscription = TitanMailSubscription(
-                        isEligibleForIntroductoryOffer = true, status = "no_subscription"),
-                tldMaintenanceEndTime = 0,
-                transferAwayEligibleAt = null,
-                transferLockOnWhoisUpdateOptional = false,
-                type = "mapping",
-                whoisUpdateUnmodifiableFields = null,
-                wpcomDomain = false))
+                wpcomDomain = false
+        )
+
+        private val siteWithFreePlan = SiteModel().apply {
+            siteId = TEST_SITE_ID
+            url = TEST_DOMAIN_NAME
+            unmappedUrl = TEST_DOMAIN_NAME
+            planId = FREE_PLAN_ID
+        }
+
+        private val siteWithPaidPlan = SiteModel().apply {
+            siteId = TEST_SITE_ID
+            url = TEST_DOMAIN_NAME
+            unmappedUrl = TEST_DOMAIN_NAME
+            planId = PREMIUM_PLAN_ID
+        }
+
+        private val planWithNoCredits = PlanModel(
+                productId = 1,
+                productSlug = "plan-1",
+                productName = "Plan 1",
+                isCurrentPlan = true,
+                hasDomainCredit = false
+        )
+        private val planWithCredits = PlanModel(
+                productId = 2,
+                productSlug = "plan-2",
+                productName = "Plan 2",
+                isCurrentPlan = true,
+                hasDomainCredit = true
+        )
     }
 }
