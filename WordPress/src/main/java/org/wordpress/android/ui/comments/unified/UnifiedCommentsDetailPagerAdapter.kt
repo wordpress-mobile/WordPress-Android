65a41d5c09d14ec3882485bb88365bc2104f70cd
khaykov <forsver@gmail.com>
2021-11-03 17:51:13 +0200

    Added missing pager adaptper for commments details activity.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/unified/UnifiedCommentsDetailPagerAdapter.kt
@@ -0,0 +1,24 @@
+package org.wordpress.android.ui.comments.unified
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import java.lang.ref.WeakReference
+
+class UnifiedCommentsDetailPagerAdapter(
+    fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+    private val listFragments = mutableMapOf<Int, WeakReference<UnifiedCommentDetailsFragment>>()
+
+    fun getItemAtPosition(position: Int): UnifiedCommentDetailsFragment? {
+        return listFragments[position]?.get()
+    }
+
+    override fun getItemCount(): Int = 3 // TODO return size of the actual collection of comments
+
+    override fun createFragment(position: Int): Fragment {
+        val fragment = UnifiedCommentDetailsFragment.newInstance()
+        listFragments[position] = WeakReference(fragment)
+        return fragment
+    }
+}
