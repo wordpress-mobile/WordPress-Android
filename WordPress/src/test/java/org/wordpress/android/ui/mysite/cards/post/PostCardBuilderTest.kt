56dc4c8b684af73f9595dd921edf80c13ba7d355
ashiagr <agr.ash@gmail.com>
2021-11-09 11:53:32 +0530

    (Tests) Add tests for draft/ scheduled post cards

--- WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/post/PostCardBuilderTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/post/PostCardBuilderTest.kt
@@ -5,40 +5,170 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.wordpress.android.BaseUnitTest
+import org.wordpress.android.R
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.PostCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.PostCard.PostCardDraftOrScheduled
 import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.PostCardBuilderParams
-import org.wordpress.android.ui.mysite.cards.post.PostCardBuilder.Companion.DRAFT_TITLE
-import org.wordpress.android.ui.mysite.cards.post.PostCardBuilder.Companion.SCHEDULED_TITLE
+import org.wordpress.android.ui.mysite.cards.post.PostCardType.DRAFT
+import org.wordpress.android.ui.mysite.cards.post.PostCardType.SCHEDULED
 import org.wordpress.android.ui.mysite.cards.post.mockdata.MockedPostsData
 import org.wordpress.android.ui.mysite.cards.post.mockdata.MockedPostsData.Post
 import org.wordpress.android.ui.mysite.cards.post.mockdata.MockedPostsData.Posts
+import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.ui.utils.UiString.UiStringText
+
+private const val POST_ID = "1"
+private const val POST_TITLE = "title"
+private const val POST_EXCERPT = "excerpt"
+private const val FEATURED_IMAGE_URL = "/image/url"
 
 // This class contains placeholder tests until mock data is removed
 @InternalCoroutinesApi
 class PostCardBuilderTest : BaseUnitTest() {
     private lateinit var builder: PostCardBuilder
-
-    private val mockedPostsData: MockedPostsData
-        get() = MockedPostsData(
-                posts = Posts(
-                        hasPublishedPosts = true,
-                        draft = listOf(Post(id = "1", title = DRAFT_TITLE)),
-                        scheduled = listOf(Post(id = "1", title = SCHEDULED_TITLE))
-                )
-        )
+    private val post = Post(id = POST_ID)
 
     @Before
     fun setUp() {
         builder = PostCardBuilder()
     }
 
-    private fun buildPostCards() = builder.build(PostCardBuilderParams(mockedPostsData))
+    /* DRAFT POST CARD */
 
     @Test
-    fun `when toolbar is built, then card title exists`() {
-        val postCards = buildPostCards()
+    fun `given draft post, when post cards are built, then draft post card exists`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = listOf(post))
 
-        assertThat(postCards[0].title).isEqualTo(UiStringText(DRAFT_TITLE))
-        assertThat(postCards[1].title).isEqualTo(UiStringText(SCHEDULED_TITLE))
+        val postCards = buildPostCards(mockedPostsData)
+
+        assertThat(postCards.filterDraftPostCard()).isNotNull
     }
+
+    @Test
+    fun `given no draft post, when post cards are built, then draft post card not exists`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = emptyList())
+
+        val postCards = buildPostCards(mockedPostsData)
+
+        assertThat(postCards.filterDraftPostCard()).isNull()
+    }
+
+    /* SCHEDULED POST CARD */
+
+    @Test
+    fun `given scheduled post, when post cards are built, then scheduled post card exists`() {
+        val mockedPostsData = getMockedPostsData(scheduledPosts = listOf(post))
+
+        val postCards = buildPostCards(mockedPostsData)
+
+        assertThat(postCards.filterScheduledPostCard()).isNotNull
+    }
+
+    @Test
+    fun `given no scheduled post, when post cards are built, then scheduled post card not exists`() {
+        val mockedPostsData = getMockedPostsData(scheduledPosts = emptyList())
+
+        val postCards = buildPostCards(mockedPostsData)
+
+        assertThat(postCards.filterScheduledPostCard()).isNull()
+    }
+
+    /* DRAFT OR SCHEDULED POST ITEM - TITLE */
+
+    @Test
+    fun `given post with title, when draft post card is built, then post title exists`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = listOf(post.copy(title = POST_TITLE)))
+
+        val postItem = buildPostCards(mockedPostsData).filterDraftPostCard()?.postItems?.first()
+
+        assertThat(postItem?.title).isEqualTo(UiStringText(POST_TITLE))
+    }
+
+    @Test
+    fun `given post without title, when draft post card is built, then untitled title exists`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = listOf(post.copy(title = null)))
+
+        val postItem = buildPostCards(mockedPostsData).filterDraftPostCard()?.postItems?.first()
+
+        assertThat(postItem?.title).isEqualTo(UiStringRes(R.string.my_site_untitled_post))
+    }
+
+    /* DRAFT OR SCHEDULED POST ITEM - EXCERPT */
+
+    @Test
+    fun `given post with excerpt, when post item is built, then excerpt exists`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = listOf(post.copy(excerpt = POST_EXCERPT)))
+
+        val postItem = buildPostCards(mockedPostsData).filterDraftPostCard()?.postItems?.first()
+
+        assertThat(postItem?.excerpt).isEqualTo(UiStringText(POST_EXCERPT))
+    }
+
+    @Test
+    fun `given post without excerpt, when post item is built, then excerpt not exists`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = listOf(post.copy(excerpt = null)))
+
+        val postItem = buildPostCards(mockedPostsData).filterDraftPostCard()?.postItems?.first()
+
+        assertThat(postItem?.excerpt).isNull()
+    }
+
+    /* DRAFT OR SCHEDULED POST ITEM - FEATURED IMAGE */
+
+    @Test
+    fun `given post with featured image, when post item is built, then featured image visible`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = listOf(post.copy(featuredImageUrl = FEATURED_IMAGE_URL)))
+
+        val postItem = buildPostCards(mockedPostsData).filterDraftPostCard()?.postItems?.first()
+
+        assertThat(postItem?.isFeaturedImageVisible).isTrue
+    }
+
+    @Test
+    fun `given post without featured image, when post item is built, then featured image not visible`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = listOf(post.copy(featuredImageUrl = null)))
+
+        val postItem = buildPostCards(mockedPostsData).filterDraftPostCard()?.postItems?.first()
+
+        assertThat(postItem?.isFeaturedImageVisible).isFalse
+    }
+
+    /* DRAFT OR SCHEDULED POST ITEM - TIME ICON */
+
+    @Test
+    fun `given draft post, when post item is built, then time icon is not visible`() {
+        val mockedPostsData = getMockedPostsData(draftPosts = listOf(post))
+
+        val postCards = buildPostCards(mockedPostsData)
+
+        assertThat((postCards.filterDraftPostCard())?.postItems?.first()?.isTimeIconVisible).isFalse
+    }
+
+    @Test
+    fun `given scheduled post, when post item is built, then time icon is visible`() {
+        val mockedPostsData = getMockedPostsData(scheduledPosts = listOf(post))
+
+        val postCards = buildPostCards(mockedPostsData)
+
+        assertThat((postCards.filterScheduledPostCard())?.postItems?.first()?.isTimeIconVisible).isTrue
+    }
+
+    private fun List<PostCard>.filterDraftPostCard() =
+            firstOrNull { it.postCardType == DRAFT } as? PostCardDraftOrScheduled
+
+    private fun List<PostCard>.filterScheduledPostCard() =
+            firstOrNull { it.postCardType == SCHEDULED } as? PostCardDraftOrScheduled
+
+    private fun buildPostCards(mockedData: MockedPostsData) = builder.build(PostCardBuilderParams(mockedData))
+
+    private fun getMockedPostsData(
+        draftPosts: List<Post>? = null,
+        scheduledPosts: List<Post>? = null
+    ) = MockedPostsData(
+            posts = Posts(
+                    hasPublishedPosts = true,
+                    draft = draftPosts,
+                    scheduled = scheduledPosts
+            )
+    )
 }
