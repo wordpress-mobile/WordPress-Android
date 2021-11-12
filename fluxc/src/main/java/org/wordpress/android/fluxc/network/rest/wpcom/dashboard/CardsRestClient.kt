686086857f704d3b5ef80c81183b6d71a4948f1a
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2021-11-12 11:42:31 +0200

    Test: Create skeleton test for dashboard cards rest client test

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/dashboard/CardsRestClient.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/dashboard/CardsRestClient.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import com.android.volley.RequestQueue
 import com.google.gson.annotations.SerializedName
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.UserAgent
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
+import org.wordpress.android.fluxc.store.dashboard.CardsStore.FetchedCardsPayload
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
@@ -14,11 +16,14 @@ import javax.inject.Singleton
 @Singleton
 class CardsRestClient @Inject constructor(
     dispatcher: Dispatcher,
-    appContext: Context,
+    appContext: Context?,
     @Named("regular") requestQueue: RequestQueue,
     accessToken: AccessToken,
     userAgent: UserAgent
 ) : BaseWPComRestClient(appContext, dispatcher, requestQueue, accessToken, userAgent) {
+    @Suppress("RedundantSuspendModifier", "UNUSED_PARAMETER")
+    suspend fun fetchCards(site: SiteModel) = FetchedCardsPayload(CardsResponse())
+
     data class CardsResponse(
         @SerializedName("todo") val todo: Int = 0
     )
