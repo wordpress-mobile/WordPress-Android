10e70750c8b69540a7fe510f7f0ce6a1260ce6ac
develric <develric@gmail.com>
2021-11-16 01:00:15 +0100

    Todos from original activity.

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderCommentListActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderCommentListActivity.java
@@ -97,6 +97,7 @@ import static org.wordpress.android.util.WPSwipeToRefreshHelper.buildSwipeToRefr
 
 import kotlin.Unit;
 
+// TODOD: declare this and relevant other classes as deprecated
 public class ReaderCommentListActivity extends LocaleAwareActivity implements OnConfirmListener,
         OnCollapseListener {
     private static final String KEY_REPLY_TO_COMMENT_ID = "reply_to_comment_id";
@@ -377,7 +378,6 @@ public class ReaderCommentListActivity extends LocaleAwareActivity implements On
         }
         mViewModel.start(mBlogId, mPostId);
     }
-
 
     @Override
     public void onCollapse(@Nullable Bundle result) {
@@ -942,6 +942,7 @@ public class ReaderCommentListActivity extends LocaleAwareActivity implements On
         mSwipeToRefreshHelper.setRefreshing(refreshing);
     }
 
+    // TODOD
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
