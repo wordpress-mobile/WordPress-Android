4e0826b02a53b0a929a345c6ea204200e7343c79
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-11-17 13:45:48 -0500

    Refactor: swap in MySiteSourceRefresh interface, remove override refresh and override refresh property

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/SelectedSiteSource.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/SelectedSiteSource.kt
@@ -7,6 +7,7 @@ import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.SiteActionBuilder
 import org.wordpress.android.fluxc.store.SiteStore.OnSiteChanged
+import org.wordpress.android.ui.mysite.MySiteSource.MySiteRefreshSource
 import org.wordpress.android.ui.mysite.MySiteUiState.PartialState.SelectedSite
 import org.wordpress.android.util.filter
 import org.wordpress.android.util.map
@@ -17,8 +18,8 @@ import javax.inject.Singleton
 class SelectedSiteSource @Inject constructor(
     private val selectedSiteRepository: SelectedSiteRepository,
     private val dispatcher: Dispatcher
-) : MySiteSource<SelectedSite> {
-    val refresh: MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
+) : MySiteRefreshSource<SelectedSite> {
+    override val refresh: MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
 
     override fun build(
         coroutineScope: CoroutineScope,
@@ -27,15 +28,13 @@ class SelectedSiteSource @Inject constructor(
             .filter { it == null || it.id == siteLocalId }
             .map { SelectedSite(it) }
 
-    fun refresh() {
+    override fun refresh() {
         selectedSiteRepository.updateSiteSettingsIfNecessary()
         selectedSiteRepository.getSelectedSite()?.let {
             refresh.postValue(true)
             dispatcher.dispatch(SiteActionBuilder.newFetchSiteAction(it))
         }
     }
-
-    fun isRefreshing() = refresh.value as Boolean
 
     init {
         dispatcher.register(this)
