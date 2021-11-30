6f10329eb95017b46ac94d35366a968788f065f1
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2021-11-30 12:57:28 +0200

    Feature: Implement initial cards store logic for cards source
    This commit makes the previous failing test pass (TDD).

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsSource.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsSource.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.wordpress.android.fluxc.store.dashboard.CardsStore
-import org.wordpress.android.fluxc.store.dashboard.CardsStore.CardsResult
 import org.wordpress.android.ui.mysite.MySiteSource.MySiteRefreshSource
 import org.wordpress.android.ui.mysite.MySiteUiState.PartialState.CardsUpdate
 import org.wordpress.android.ui.mysite.SelectedSiteRepository
@@ -22,26 +21,31 @@ class CardsSource @Inject constructor(
 
     override fun build(coroutineScope: CoroutineScope, siteLocalId: Int): LiveData<CardsUpdate> {
         val result = MediatorLiveData<CardsUpdate>()
-        result.refreshData(coroutineScope)
-        result.addSource(refresh) { result.refreshData(coroutineScope, refresh.value) }
+        result.refreshData(coroutineScope, siteLocalId)
+        result.addSource(refresh) { result.refreshData(coroutineScope, siteLocalId, refresh.value) }
         return result
     }
 
     private fun MediatorLiveData<CardsUpdate>.refreshData(
         coroutineScope: CoroutineScope,
+        siteLocalId: Int,
         isRefresh: Boolean? = null
     ) {
         when (isRefresh) {
-            null, true -> refreshData(coroutineScope)
+            null, true -> refreshData(coroutineScope, siteLocalId)
             else -> Unit // Do nothing
         }
     }
 
     private fun MediatorLiveData<CardsUpdate>.refreshData(
-        coroutineScope: CoroutineScope
+        coroutineScope: CoroutineScope,
+        siteLocalId: Int
     ) {
         coroutineScope.launch {
-            postState(CardsUpdate(CardsResult()))
+            val selectedSite = selectedSiteRepository.getSelectedSite()
+            if (selectedSite != null && selectedSite.id == siteLocalId) {
+                postState(CardsUpdate(cardsStore.fetchCards(selectedSite)))
+            }
         }
     }
 }
