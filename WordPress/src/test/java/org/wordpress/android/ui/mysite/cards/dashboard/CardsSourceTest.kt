440ddcb10758ea5c657147e7944ad817457d2829
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2021-11-30 15:42:42 +0200

    Test: Add and update test for cards source based on get data and error
    These tests will fail as the implementation is not ready yet. The next
    commit will make the tests pass (TDD).

--- WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsSourceTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsSourceTest.kt
@@ -1,7 +1,9 @@
 package org.wordpress.android.ui.mysite.cards.dashboard
 
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -9,7 +11,13 @@ import org.mockito.Mock
 import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.dashboard.CardModel
+import org.wordpress.android.fluxc.model.dashboard.CardModel.PostsCardModel
+import org.wordpress.android.fluxc.model.dashboard.CardModel.PostsCardModel.PostCardModel
+import org.wordpress.android.fluxc.network.rest.wpcom.dashboard.CardsUtils
 import org.wordpress.android.fluxc.store.dashboard.CardsStore
+import org.wordpress.android.fluxc.store.dashboard.CardsStore.CardsError
+import org.wordpress.android.fluxc.store.dashboard.CardsStore.CardsErrorType
 import org.wordpress.android.fluxc.store.dashboard.CardsStore.CardsResult
 import org.wordpress.android.test
 import org.wordpress.android.testScope
@@ -20,13 +28,48 @@ import org.wordpress.android.ui.mysite.SelectedSiteRepository
 
 const val SITE_LOCAL_ID = 1
 
+/* POST */
+
+const val POST_ID = 1
+const val POST_TITLE = "title"
+const val POST_CONTENT = "content"
+const val POST_FEATURED_IMAGE = "featuredImage"
+const val POST_DATE = "2021-12-27 11:33:55"
+
+/* MODEL */
+
+private val POST_MODEL = PostCardModel(
+        id = POST_ID,
+        title = POST_TITLE,
+        content = POST_CONTENT,
+        featuredImage = POST_FEATURED_IMAGE,
+        date = CardsUtils.fromDate(POST_DATE)
+)
+
+private val POSTS_MODEL = PostsCardModel(
+        hasPublished = false,
+        draft = listOf(POST_MODEL),
+        scheduled = listOf(POST_MODEL)
+)
+
+private val CARDS_MODEL: List<CardModel> = listOf(
+        POSTS_MODEL
+)
+
 @InternalCoroutinesApi
 class CardsSourceTest : BaseUnitTest() {
     @Mock private lateinit var selectedSiteRepository: SelectedSiteRepository
     @Mock private lateinit var cardsStore: CardsStore
     @Mock private lateinit var siteModel: SiteModel
     private lateinit var cardSource: CardsSource
-    private lateinit var isRefreshing: MutableList<Boolean>
+
+    private val data = CardsResult(
+            model = CARDS_MODEL
+    )
+    private val success = CardsResult<List<CardModel>>()
+    private val error = CardsResult<List<CardModel>>(
+            error = CardsError(CardsErrorType.API_ERROR)
+    )
 
     @Before
     fun setUp() {
@@ -35,7 +78,6 @@ class CardsSourceTest : BaseUnitTest() {
                 cardsStore,
                 TEST_DISPATCHER
         )
-        isRefreshing = mutableListOf()
         setUpMocks()
     }
 
@@ -44,61 +86,149 @@ class CardsSourceTest : BaseUnitTest() {
         whenever(selectedSiteRepository.getSelectedSite()).thenReturn(siteModel)
     }
 
-    /* DATA */
+    /* GET DATA */
 
     @Test
-    fun `when source is requested upon start, then data is loaded`() = test {
-        var result: CardsUpdate? = null
-        whenever(cardsStore.fetchCards(siteModel)).thenReturn(CardsResult())
+    fun `when build is invoked, then start collecting cards from store (database)`() = test {
+        cardSource.refresh.observeForever { }
 
-        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { it?.let { result = it } }
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { }
 
-        assertThat(result?.cards).isNotNull
+        verify(cardsStore).getCards(siteModel)
     }
 
     @Test
-    fun `when refresh is invoked, then data is refreshed`() = test {
-        val result: MutableList<CardsUpdate?> = mutableListOf()
-        whenever(cardsStore.fetchCards(siteModel)).thenReturn(CardsResult())
+    fun `given build is invoked, when cards are collected, then data is loaded (database)`() = test {
+        val result = mutableListOf<CardsUpdate>()
+        whenever(cardsStore.getCards(siteModel)).thenReturn(flowOf(data))
+        cardSource.refresh.observeForever { }
+
         cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { it?.let { result.add(it) } }
-        cardSource.refresh.observeForever { isRefreshing.add(it) }
+
+        assertThat(result.size).isEqualTo(1)
+        assertThat(result.first()).isEqualTo(CardsUpdate(data))
+    }
+
+    /* REFRESH DATA */
+
+    @Test
+    fun `when build is invoked, then cards are fetched from store (network)`() = test {
+        whenever(cardsStore.getCards(siteModel)).thenReturn(flowOf(CardsResult(CARDS_MODEL)))
+        cardSource.refresh.observeForever { }
+
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { }
+
+        verify(cardsStore).fetchCards(siteModel)
+    }
+
+    @Test
+    fun `given no error, when build is invoked, then data is only loaded from get cards (database)`() = test {
+        val result = mutableListOf<CardsUpdate>()
+        whenever(cardsStore.getCards(siteModel)).thenReturn(flowOf(data))
+        whenever(cardsStore.fetchCards(siteModel)).thenReturn(CardsResult())
+        cardSource.refresh.observeForever { }
+
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { it?.let { result.add(it) } }
+
+        assertThat(result.size).isEqualTo(1)
+        assertThat(result.first()).isEqualTo(CardsUpdate(data))
+    }
+
+    @Test
+    fun `given error, when build is invoked, then error data is also loaded (network)`() = test {
+        val result = mutableListOf<CardsUpdate>()
+        whenever(cardsStore.getCards(siteModel)).thenReturn(flowOf(data))
+        whenever(cardsStore.fetchCards(siteModel)).thenReturn(error)
+        cardSource.refresh.observeForever { }
+
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { it?.let { result.add(it) } }
+
+        assertThat(result.size).isEqualTo(1)
+        assertThat(result.first()).isEqualTo(CardsUpdate(error))
+    }
+
+    @Test
+    fun `given no error, when refresh is invoked, then data is only loaded from get cards (database)`() = test {
+        val result = mutableListOf<CardsUpdate>()
+        whenever(cardsStore.getCards(siteModel)).thenReturn(flowOf(data))
+        whenever(cardsStore.fetchCards(siteModel)).thenReturn(success).thenReturn(success)
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { it?.let { result.add(it) } }
+        cardSource.refresh.observeForever { }
 
         cardSource.refresh()
 
-        assertThat(result.first()?.cards).isNotNull
-        assertThat(result.last()?.cards).isNotNull
+        assertThat(result.size).isEqualTo(1)
+        assertThat(result.first()).isEqualTo(CardsUpdate(data))
+    }
+
+    @Test
+    fun `given error, when refresh is invoked, then error data is also loaded (network)`() = test {
+        val result = mutableListOf<CardsUpdate>()
+        whenever(cardsStore.getCards(siteModel)).thenReturn(flowOf(data))
+        whenever(cardsStore.fetchCards(siteModel)).thenReturn(success).thenReturn(error)
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { it?.let { result.add(it) } }
+        cardSource.refresh.observeForever { }
+
+        cardSource.refresh()
+
         assertThat(result.size).isEqualTo(2)
+        assertThat(result.first()).isEqualTo(CardsUpdate(data))
+        assertThat(result.last()).isEqualTo(CardsUpdate(error))
     }
 
     /* IS REFRESHING */
 
     @Test
-    fun `when source is invoked, then refresh is false`() = test {
-        cardSource.refresh.observeForever { isRefreshing.add(it) }
+    fun `when build is invoked, then refresh is set to false`() = test {
+        val result = mutableListOf<Boolean>()
+        cardSource.refresh.observeForever { result.add(it) }
 
-        cardSource.build(testScope(), SITE_LOCAL_ID)
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { }
 
-        assertThat(isRefreshing.last()).isFalse
+        assertThat(result.size).isEqualTo(1)
+        assertThat(result.last()).isFalse
     }
 
     @Test
-    fun `when refresh is invoked, then refresh is true`() = test {
-        cardSource.refresh.observeForever { isRefreshing.add(it) }
+    fun `when refresh is invoked, then refresh is set to true`() = test {
+        val result = mutableListOf<Boolean>()
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { }
+        cardSource.refresh.observeForever { result.add(it) }
 
         cardSource.refresh()
 
-        assertThat(isRefreshing.last()).isTrue
+        assertThat(result.size).isEqualTo(2)
+        assertThat(result.first()).isFalse
+        assertThat(result.last()).isTrue
     }
 
     @Test
-    fun `when data has been refreshed, then refresh is set to false`() = test {
-        val result: MutableList<CardsUpdate?> = mutableListOf()
-        whenever(cardsStore.fetchCards(siteModel)).thenReturn(CardsResult())
-        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { it?.let { result.add(it) } }
-        cardSource.refresh.observeForever { isRefreshing.add(it) }
+    fun `given no error, when data has been refreshed, then refresh is set to true`() = test {
+        val result = mutableListOf<Boolean>()
+        whenever(cardsStore.getCards(siteModel)).thenReturn(flowOf(data))
+        whenever(cardsStore.fetchCards(siteModel)).thenReturn(success)
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { }
+        cardSource.refresh.observeForever { result.add(it) }
 
         cardSource.refresh()
 
-        assertThat(isRefreshing.last()).isFalse
+        assertThat(result.size).isEqualTo(2)
+        assertThat(result.first()).isFalse
+        assertThat(result.last()).isTrue
+    }
+
+    @Test
+    fun `given error, when data has been refreshed, then refresh is set to false`() = test {
+        val result = mutableListOf<Boolean>()
+        whenever(cardsStore.getCards(siteModel)).thenReturn(flowOf(data))
+        whenever(cardsStore.fetchCards(siteModel)).thenReturn(error)
+        cardSource.build(testScope(), SITE_LOCAL_ID).observeForever { }
+        cardSource.refresh.observeForever { result.add(it) }
+
+        cardSource.refresh()
+
+        assertThat(result.size).isEqualTo(2)
+        assertThat(result.first()).isFalse
+        assertThat(result.last()).isFalse
     }
 }
