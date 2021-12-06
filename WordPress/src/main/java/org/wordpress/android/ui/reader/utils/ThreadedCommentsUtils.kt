adc1b743ad11c63b819e6e546ec6b007849a063c
develric <develric@gmail.com>
2021-12-06 23:50:16 +0100

    Extracting a few utilities function for threaded comments.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/utils/ThreadedCommentsUtils.kt
@@ -0,0 +1,37 @@
+package org.wordpress.android.ui.reader.utils
+
+import android.view.View
+import android.widget.TextView
+import com.facebook.shimmer.ShimmerFrameLayout
+import dagger.Reusable
+import org.wordpress.android.R
+import org.wordpress.android.models.ReaderPost
+import org.wordpress.android.util.DisplayUtils
+import org.wordpress.android.viewmodel.ContextProvider
+import javax.inject.Inject
+
+@Reusable
+class ThreadedCommentsUtils @Inject constructor(
+    val contextProvider: ContextProvider
+) {
+    fun getMaxWidthForContent(): Int {
+        val context = contextProvider.getContext()
+
+        // calculate the max width of comment content
+        val displayWidth = DisplayUtils.getDisplayPixelWidth(context)
+        val cardMargin: Int = context.resources.getDimensionPixelSize(R.dimen.reader_card_margin)
+        val contentPadding: Int = context.resources.getDimensionPixelSize(R.dimen.reader_card_content_padding)
+        val mediumMargin: Int = context.resources.getDimensionPixelSize(R.dimen.margin_medium)
+
+        return displayWidth - cardMargin * 2 - contentPadding * 2 - mediumMargin * 2
+    }
+
+    fun isPrivatePost(post: ReaderPost?) = post != null && post.isPrivate
+
+    fun setLinksClickable(textView: TextView?, isPrivatePost: Boolean) {
+        textView?.let {
+            it.linksClickable = true
+            it.movementMethod = ReaderLinkMovementMethod.getInstance(isPrivatePost)
+        }
+    }
+}
