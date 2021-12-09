a07aac99448ef22a97d9be886965b919c1813e01
develric <develric@gmail.com>
2021-12-09 02:21:01 +0100

    Adding strings for tracking.

---
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/comments/ThreadedCommentsActionSource.kt
@@ -0,0 +1,32 @@
+package org.wordpress.android.ui.reader.comments
+
+import org.wordpress.android.ui.reader.tracker.ReaderTracker.Companion.SOURCE_DISCOVER
+import org.wordpress.android.ui.reader.tracker.ReaderTracker.Companion.SOURCE_POST_DETAIL
+import org.wordpress.android.ui.reader.tracker.ReaderTracker.Companion.SOURCE_POST_DETAIL_COMMENT_SNIPPET
+
+enum class ThreadedCommentsActionSource(val sourceDescription: String) {
+    READER_POST_CARD("reader_post_card"),
+    READER_POST_DETAILS("reader_post_details"),
+    READER_POST_DETAILS_COMMENTS("reader_post_details_comments"),
+    READER_THREADED_COMMENTS("reader_threaded_comments"),
+    COMMENT_NOTIFICATION("comment_notification"),
+    MY_SITE_COMMENT("my_site_comment"),
+    COMMENT_LIKE_NOTIFICATION("comment_like_notification"),
+    ACTIVITY_LOG_DETAIL("activity_log_detail"),
+    DIRECT_OPERATION("direct_operation"),
+    UNKNOWN("unknown");
+
+    companion object {
+        fun mapReaderPostSource(source: String): ThreadedCommentsActionSource {
+            return if (source.equals(SOURCE_POST_DETAIL)) {
+                READER_POST_DETAILS
+            } else if (source.equals(SOURCE_POST_DETAIL_COMMENT_SNIPPET)) {
+                READER_POST_DETAILS_COMMENTS
+            } else if (source.equals(SOURCE_DISCOVER)) {
+                READER_POST_CARD
+            } else {
+                UNKNOWN
+            }
+        }
+    }
+}
