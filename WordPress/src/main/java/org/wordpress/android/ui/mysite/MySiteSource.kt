43bd5537a69bc3160791762dd6f6e1833d83350c
ashiagr <agr.ash@gmail.com>
2021-12-17 15:56:35 +0530

    Remove unused import

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteSource.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteSource.kt
@@ -1,6 +1,5 @@
 package org.wordpress.android.ui.mysite
 
-import android.os.Looper
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
