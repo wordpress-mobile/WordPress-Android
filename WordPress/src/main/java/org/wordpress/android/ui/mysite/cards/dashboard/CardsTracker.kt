a615fcf89a09dd0e38ab6fe0181fa53bfc611826
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-12-20 08:42:00 -0500

    Refactor: Use fully qualified names for enums

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsTracker.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsTracker.kt
@@ -1,12 +1,7 @@
 package org.wordpress.android.ui.mysite.cards.dashboard
 
 import org.wordpress.android.analytics.AnalyticsTracker.Stat
-import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker.Type.POST
 import org.wordpress.android.ui.mysite.cards.dashboard.posts.PostCardType
-import org.wordpress.android.ui.mysite.cards.dashboard.posts.PostCardType.CREATE_FIRST
-import org.wordpress.android.ui.mysite.cards.dashboard.posts.PostCardType.CREATE_NEXT
-import org.wordpress.android.ui.mysite.cards.dashboard.posts.PostCardType.DRAFT
-import org.wordpress.android.ui.mysite.cards.dashboard.posts.PostCardType.SCHEDULED
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 import javax.inject.Inject
 
@@ -25,7 +20,7 @@ class CardsTracker @Inject constructor(
     }
 
     fun trackDashboardPostCardFooterLinkClicked(postCardType: PostCardType) {
-        trackDashboardCardFooterLinkClicked(POST, postCardType.toSubtypeValue())
+        trackDashboardCardFooterLinkClicked(Type.POST, postCardType.toSubtypeValue())
     }
 
     private fun trackDashboardCardFooterLinkClicked(
@@ -43,10 +38,10 @@ class CardsTracker @Inject constructor(
 
     private fun PostCardType.toSubtypeValue(): Subtype {
         return when (this) {
-            CREATE_FIRST -> Subtype.CREATE_FIRST
-            CREATE_NEXT -> Subtype.CREATE_NEXT
-            DRAFT -> Subtype.DRAFT
-            SCHEDULED -> Subtype.SCHEDULED
+            PostCardType.CREATE_FIRST -> Subtype.CREATE_FIRST
+            PostCardType.CREATE_NEXT -> Subtype.CREATE_NEXT
+            PostCardType.DRAFT -> Subtype.DRAFT
+            PostCardType.SCHEDULED -> Subtype.SCHEDULED
         }
     }
 
