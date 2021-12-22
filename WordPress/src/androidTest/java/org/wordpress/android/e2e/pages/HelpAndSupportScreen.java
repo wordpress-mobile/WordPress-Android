d4e294bc5597f428a70e184704300b3a1e3a859c
pachlava <pachlava@gmail.com>
2021-12-22 13:10:58 +0200

    Variables renaming to solve stylecheck warnings

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/HelpAndSupportScreen.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/HelpAndSupportScreen.java
@@ -17,8 +17,8 @@ import static org.hamcrest.Matchers.allOf;
 
 public class HelpAndSupportScreen {
     static ViewInteraction contactUsButton = onView(withId(R.id.contact_us_button));
-    static ViewInteraction FAQButton = onView(withId(R.id.faq_button));
-    static ViewInteraction myTicketsbutton = onView(withId(R.id.my_tickets_button));
+    static ViewInteraction faqButton = onView(withId(R.id.faq_button));
+    static ViewInteraction myTicketsButton = onView(withId(R.id.my_tickets_button));
     static ViewInteraction applicationLogButton = onView(withId(R.id.application_log_button));
     static ViewInteraction applicationVersionText = onView(withId(R.id.applicationVersion));
     static ViewInteraction emailAddressText = onView(withId(R.id.contactEmailAddress));
@@ -26,8 +26,8 @@ public class HelpAndSupportScreen {
 
     public HelpAndSupportScreen assertHelpAndSupportScreenLoaded() {
         contactUsButton.check(matches(isCompletelyDisplayed()));
-        FAQButton.check(matches(isCompletelyDisplayed()));
-        myTicketsbutton.check(matches(isCompletelyDisplayed()));
+        faqButton.check(matches(isCompletelyDisplayed()));
+        myTicketsButton.check(matches(isCompletelyDisplayed()));
         applicationLogButton.check(matches(isCompletelyDisplayed()));
         applicationVersionText.check(matches(isCompletelyDisplayed()));
         emailAddressText.check(matches(isCompletelyDisplayed()));
