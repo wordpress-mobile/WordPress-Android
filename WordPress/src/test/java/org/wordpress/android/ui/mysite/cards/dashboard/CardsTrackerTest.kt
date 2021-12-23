ec0d4ff777d7c49d80a239eb46cceadeb812b7c2
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2021-12-23 10:19:16 -0500

    Refactor: Removed cards shown tests to new class (next commit)

--- WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsTrackerTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsTrackerTest.kt
@@ -1,6 +1,5 @@
 package org.wordpress.android.ui.mysite.cards.dashboard
 
-import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import org.junit.Before
 import org.junit.Test
@@ -8,16 +7,9 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.analytics.AnalyticsTracker.Stat
-import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards
-import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard
-import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.ErrorCard
-import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard.FooterLink
-import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard.PostCardWithPostItems
-import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard.PostCardWithoutPostItems
 import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker.PostSubtype
 import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker.Type
 import org.wordpress.android.ui.mysite.cards.dashboard.posts.PostCardType
-import org.wordpress.android.ui.utils.UiString.UiStringText
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 
 private const val TYPE = "type"
@@ -26,11 +18,12 @@ private const val SUBTYPE = "subtype"
 @RunWith(MockitoJUnitRunner::class)
 class CardsTrackerTest {
     @Mock lateinit var analyticsTracker: AnalyticsTrackerWrapper
+    @Mock lateinit var cardsShownTracker: CardsShownTracker
     private lateinit var cardsTracker: CardsTracker
 
     @Before
     fun setUp() {
-        cardsTracker = CardsTracker(analyticsTracker)
+        cardsTracker = CardsTracker(cardsShownTracker, analyticsTracker)
     }
 
     @Test
@@ -61,44 +54,6 @@ class CardsTrackerTest {
         verifyFooterLinkClickedTracked(Type.POST, PostSubtype.SCHEDULED)
     }
 
-    @Test
-    fun `when post card create first card is shown, then create first shown event is tracked`() {
-        cardsTracker.trackCardsShown(buildDashboardCards(PostCardType.CREATE_FIRST))
-
-        verifyCardShowTracked(Type.POST.label, PostSubtype.CREATE_FIRST.label)
-    }
-
-    @Test
-    fun `when post card create next card is shown, then create next shown event is tracked`() {
-        cardsTracker.trackCardsShown(buildDashboardCards(PostCardType.CREATE_NEXT))
-
-        verifyCardShowTracked(Type.POST.label, PostSubtype.CREATE_NEXT.label)
-    }
-
-    @Test
-    fun `when post card scheduled card is shown, then scheduled shown event is tracked`() {
-        cardsTracker.trackCardsShown(buildDashboardCards(PostCardType.SCHEDULED))
-
-        verifyCardShowTracked(Type.POST.label, PostSubtype.SCHEDULED.label)
-    }
-
-    @Test
-    fun `when post card drafts card is shown, then drafts shown event is tracked`() {
-        cardsTracker.trackCardsShown(buildDashboardCards(PostCardType.DRAFT))
-
-        verifyCardShowTracked(Type.POST.label, PostSubtype.DRAFT.label)
-    }
-
-    @Test
-    fun `when error card is shown, then error card shown event is tracked`() {
-        cardsTracker.trackCardsShown(buildErrorCard())
-
-        verify(analyticsTracker).track(
-                Stat.MY_SITE_DASHBOARD_CARD_SHOWN,
-                mapOf(TYPE to Type.ERROR.label)
-        )
-    }
-
     private fun verifyFooterLinkClickedTracked(
         typeValue: Type,
         subtypeValue: PostSubtype
@@ -107,51 +62,5 @@ class CardsTrackerTest {
                 Stat.MY_SITE_DASHBOARD_CARD_FOOTER_ACTION_TAPPED,
                 mapOf(TYPE to typeValue.label, SUBTYPE to subtypeValue.label)
         )
-    }
-
-    private fun verifyCardShowTracked(type: String, subtype: String) {
-        verify(analyticsTracker).track(
-                Stat.MY_SITE_DASHBOARD_CARD_SHOWN,
-                mapOf(TYPE to type, SUBTYPE to subtype)
-        )
-    }
-
-    private fun buildDashboardCards(postCardType: PostCardType) = DashboardCards(
-            cards = mutableListOf<DashboardCard>().apply {
-                when (postCardType) {
-                    PostCardType.SCHEDULED, PostCardType.DRAFT -> addAll(buildPostCardsWithItems(postCardType))
-                    PostCardType.CREATE_FIRST, PostCardType.CREATE_NEXT -> addAll(
-                            buildPostCardsWithoutItems(
-                                    postCardType
-                            )
-                    )
-                }
-            }
-    )
-
-    private fun buildPostCardsWithoutItems(postCardType: PostCardType) =
-        listOf(PostCardWithoutPostItems(
-                postCardType = postCardType,
-                title = UiStringText(""),
-                footerLink = FooterLink(UiStringText(""), onClick = mock()),
-                excerpt = UiStringText(""),
-                imageRes = 0
-            )
-    )
-
-    private fun buildPostCardsWithItems(postCardType: PostCardType) = listOf(
-            PostCardWithPostItems(
-                    postCardType = postCardType,
-                    title = UiStringText(""),
-                    postItems = emptyList(),
-                    footerLink = FooterLink(UiStringText(""), onClick = mock())
-            )
-    )
-
-    private fun buildErrorCard(): DashboardCards {
-        val cards = listOf(ErrorCard())
-        val dashboardCard = mutableListOf<DashboardCard>()
-        dashboardCard.addAll(cards)
-        return DashboardCards(cards = cards)
     }
 }
