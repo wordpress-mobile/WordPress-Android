c217b72414a247db14ef5e977b245d93b9049036
Siobhan <siobhan@automattic.com>
2022-01-07 15:49:51 +0000

    Remove redundant comment

--- WordPress/src/main/java/org/wordpress/android/util/SiteUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/SiteUtils.java
@@ -350,7 +350,6 @@ public class SiteUtils {
         return site != null && (site.isWPCom() || site.isWPComAtomic());
     }
 
-    // TODO: Double check minimal Jetpack version
     public static boolean supportsTiledGalleryFeature(SiteModel site) {
         return site != null && site.isWPCom();
     }
