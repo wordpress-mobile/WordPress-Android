6cb429a17e227c2021a0be34fc22ceacb6ee7f66
Irfan Ömür <omur.irfan@gmail.com>
2022-01-10 00:39:43 +0300

    Add DummyInterceptorModule for UI tests

--- WordPress/src/androidTest/java/org/wordpress/android/modules/InterceptorModuleTest.kt
+++ WordPress/src/androidTest/java/org/wordpress/android/modules/InterceptorModuleTest.kt
@@ -2,14 +2,14 @@ package org.wordpress.android.modules
 
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dagger.hilt.testing.TestInstallIn
 import dagger.multibindings.IntoSet
 import okhttp3.Interceptor
 import org.wordpress.android.support.MockingInterceptor
 import javax.inject.Named
 
-@TestInstallIn(components = [SingletonComponent::class], replaces = [InterceptorModule::class])
+@InstallIn(SingletonComponent::class)
 @Module
 class InterceptorModuleTest {
     @Provides @IntoSet @Named("interceptors")
