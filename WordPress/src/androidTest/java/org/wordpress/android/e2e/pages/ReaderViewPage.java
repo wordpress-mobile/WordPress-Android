0c1dedc56d3b2efb8824f22ec5f9bbeedd4932e8
Tiago Marques <tiago.marques@automattic.com>
2022-01-11 19:34:30 -0300

    wait for swipe for more message to be gone before tapping like

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/ReaderViewPage.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/ReaderViewPage.java
@@ -14,14 +14,16 @@ import static org.wordpress.android.support.WPSupportUtils.isTextDisplayed;
 public class ReaderViewPage {
     UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
 
-    UiObject mLikeButton =
-            mDevice.findObject(new UiSelector().resourceId("org.wordpress.android.prealpha:id/count_likes"));
-    UiObject mLikerContainer =
-            mDevice.findObject(new UiSelector().resourceId("org.wordpress.android.prealpha:id/liker_faces_container"));
-    UiObject mLikeCountSelector =
-            mDevice.findObject(new UiSelector().resourceId("org.wordpress.android.prealpha:id/text_count"));
-    UiObject mRelatedPostsContainer =
-            mDevice.findObject(new UiSelector().resourceId("org.wordpress.android.prealpha:id/container_related_posts"));
+    String mLikeButtonId = "org.wordpress.android.prealpha:id/count_likes";
+    String mLikerContainerId = "org.wordpress.android.prealpha:id/liker_faces_container";
+    String mLikeCountId = "org.wordpress.android.prealpha:id/text_count";
+    String mRelatedPostsId = "org.wordpress.android.prealpha:id/container_related_posts";
+
+    UiObject mLikeButton = mDevice.findObject(new UiSelector().resourceId(mLikeButtonId));
+    UiObject mLikerContainer = mDevice.findObject(new UiSelector().resourceId(mLikerContainerId));
+    UiObject mLikeCount = mDevice.findObject(new UiSelector().resourceId(mLikeCountId));
+    UiObject mRelatedPostsContainer = mDevice.findObject(new UiSelector().resourceId(mRelatedPostsId));
+    UiObject mSwipeForMore = mDevice.findObject(new UiSelector().textContains("Swipe for more"));
 
     public ReaderViewPage waitUntilLoaded() {
         mRelatedPostsContainer.waitForExists(DEFAULT_TIMEOUT);
@@ -45,6 +47,7 @@ public class ReaderViewPage {
 
     private void tapLikeButton() {
         try {
+            mSwipeForMore.waitUntilGone(DEFAULT_TIMEOUT);
             mLikeButton.click();
         } catch (Exception e) {
             // Ignore
@@ -66,7 +69,7 @@ public class ReaderViewPage {
 
     public ReaderViewPage verifyPostLiked() throws UiObjectNotFoundException {
         boolean likerDisplayed = mLikerContainer.exists();
-        boolean postHasOneLike = mLikeCountSelector.getText().equals("1");
+        boolean postHasOneLike = mLikeCount.getText().equals("1");
 
         assertTrue("Liker was not displayed.", likerDisplayed);
         assertTrue("Like count was different from '1'.", postHasOneLike);
