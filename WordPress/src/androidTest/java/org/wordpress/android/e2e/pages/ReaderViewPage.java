b9383f04e563cca38004254eb0a828032ef1b76e
Tiago Marques <tiago.marques@automattic.com>
2022-01-13 10:11:35 -0300

    update focus reset and post liked assertion

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/ReaderViewPage.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/ReaderViewPage.java
@@ -1,12 +1,10 @@
 package org.wordpress.android.e2e.pages;
 
-import android.graphics.Rect;
 import android.view.KeyEvent;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
-import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
 import static junit.framework.TestCase.assertFalse;
@@ -61,16 +59,16 @@ public class ReaderViewPage {
             mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_RIGHT);
             mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_CENTER);
             // Click somewhere to remove focus
-            removeFooterButtonFocus();
+            resetFocus();
         } catch (Exception e) {
             // Ignore
         }
     }
 
-    private void removeFooterButtonFocus() throws UiObjectNotFoundException {
-        Rect textTitleContainerBounds = mTextTitleContainer.getBounds();
-        mDevice.drag(textTitleContainerBounds.centerX(), textTitleContainerBounds.top,
-                textTitleContainerBounds.centerX(), textTitleContainerBounds.centerY(), 10);
+    private void resetFocus() {
+        mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_LEFT);
+        mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_LEFT);
+        mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_LEFT);
     }
 
     public ReaderPage goBackToReader() {
@@ -87,9 +85,10 @@ public class ReaderViewPage {
     }
 
     public ReaderViewPage verifyPostLiked() {
-        boolean likerDisplayed = mLikerContainer.exists();
+        boolean isLiked = mDevice
+                .findObject(new UiSelector().textContains("You like this.")).waitForExists(DEFAULT_TIMEOUT);
 
-        assertTrue("Liker was not displayed.", likerDisplayed);
+        assertTrue("Liker was not displayed.", isLiked);
 
         return this;
     }
