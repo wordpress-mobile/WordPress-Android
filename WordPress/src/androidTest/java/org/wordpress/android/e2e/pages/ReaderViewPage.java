6e11c3748b3ab7f9653b2058f146d073eb5b992f
Tiago Marques <tiago.marques@automattic.com>
2022-01-13 07:49:34 -0300

    use arrow navigation to tap like button

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/ReaderViewPage.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/ReaderViewPage.java
@@ -19,12 +19,10 @@ public class ReaderViewPage {
     String mLikeButtonId = "org.wordpress.android.prealpha:id/count_likes";
     String mLikerContainerId = "org.wordpress.android.prealpha:id/liker_faces_container";
     String mRelatedPostsId = "org.wordpress.android.prealpha:id/container_related_posts";
-    String mLayoutBlogSectionId = "org.wordpress.android.prealpha:id/layout_blog_section";
 
     UiObject mLikeButton = mDevice.findObject(new UiSelector().resourceId(mLikeButtonId));
     UiObject mLikerContainer = mDevice.findObject(new UiSelector().resourceId(mLikerContainerId));
     UiObject mRelatedPostsContainer = mDevice.findObject(new UiSelector().resourceId(mRelatedPostsId));
-    UiObject mLayoutBlogSection = mDevice.findObject(new UiSelector().resourceId(mLayoutBlogSectionId));
     UiObject mSwipeForMore = mDevice.findObject(new UiSelector().textContains("Swipe for more"));
 
     public ReaderViewPage waitUntilLoaded() {
@@ -50,13 +48,16 @@ public class ReaderViewPage {
     private void tapLikeButton() {
         try {
             mSwipeForMore.waitUntilGone(DEFAULT_TIMEOUT);
+            // Even though it was working locally in simulator, tapping the footer buttons like 'mLikeButton.click()'
+            // was not working in CI with same settings Pixel 2 API 28 Android 9.0.
+            // The current workaround is to use arrows navigation.
             mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_DOWN);
             mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_RIGHT);
             mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_RIGHT);
             mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_RIGHT);
             mDevice.pressKeyCode(KeyEvent.KEYCODE_DPAD_CENTER);
-            // Click outside the footer to remove focus
-            mLayoutBlogSection.click();
+            // Click somewhere remove focus
+            mDevice.click(10, 10);
         } catch (Exception e) {
             // Ignore
         }
