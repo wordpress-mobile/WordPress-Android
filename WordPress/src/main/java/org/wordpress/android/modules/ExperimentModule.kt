11a57ca991dc5ea6d6aeee00622edb9a61fa0849
Antonis Lilis <antonis.lilis@automattic.com>
2022-01-20 12:54:37 +0200

    Adds land on the editor A/B experiment

--- WordPress/src/main/java/org/wordpress/android/modules/ExperimentModule.kt
+++ WordPress/src/main/java/org/wordpress/android/modules/ExperimentModule.kt
@@ -1,8 +1,11 @@
 package org.wordpress.android.modules
 
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoSet
 import dagger.multibindings.Multibinds
 import org.wordpress.android.util.experiments.Experiment
+import org.wordpress.android.util.experiments.LandOnTheEditorABExperiment
 
 @Module
 interface ExperimentModule {
@@ -10,4 +13,6 @@ interface ExperimentModule {
 
     // Copy and paste the line below to add a new experiment.
     // @Binds @IntoSet fun exampleExperiment(experiment: ExampleExperiment): Experiment
+
+    @Binds @IntoSet fun landOnTheEditorABExperiment(experiment: LandOnTheEditorABExperiment): Experiment
 }
