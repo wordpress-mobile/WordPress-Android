616864064b2b25a41acf76abda55dd47bdcd92f1
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-01-25 17:13:31 -0300

    Add CommentIdentifier class (as implemented in @khaykov branch)

---
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/unified/CommentIdentifier.kt
@@ -0,0 +1,18 @@
+package org.wordpress.android.ui.comments.unified
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+sealed class CommentIdentifier : Parcelable {
+    @Parcelize
+    data class SiteCommentIdentifier(
+        val localCommentId: Int
+    ) : CommentIdentifier()
+
+    @Parcelize
+    data class ReaderCommentIdentifier(
+        val blogId: Long,
+        val postId: Long,
+        val remoteCommentId: Long
+    ) : CommentIdentifier()
+}
