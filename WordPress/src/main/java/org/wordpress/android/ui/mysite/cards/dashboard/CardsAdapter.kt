098b009815a125f7412482aa6c1fc3ecae0e7094
ashiagr <agr.ash@gmail.com>
2022-02-02 18:28:29 +0530

    Feature: Add error within card vh

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsAdapter.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsAdapter.kt
@@ -7,12 +7,14 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import org.apache.commons.lang3.NotImplementedException
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.ErrorCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.ErrorWithinCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard.PostCardWithPostItems
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard.PostCardWithoutPostItems
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.TodaysStatsCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.DashboardCardType
 import org.wordpress.android.ui.mysite.cards.dashboard.error.ErrorCardViewHolder
+import org.wordpress.android.ui.mysite.cards.dashboard.error.ErrorWithinCardViewHolder
 import org.wordpress.android.ui.mysite.cards.dashboard.posts.PostCardViewHolder
 import org.wordpress.android.ui.mysite.cards.dashboard.todaysstats.TodaysStatsCardViewHolder
 
@@ -28,6 +30,7 @@ class CardsAdapter(
         return when (viewType) {
             DashboardCardType.ERROR_CARD.ordinal -> ErrorCardViewHolder(parent)
             DashboardCardType.TODAYS_STATS_CARD.ordinal -> TodaysStatsCardViewHolder(parent, uiHelpers)
+            DashboardCardType.POST_CARD_ERROR.ordinal -> ErrorWithinCardViewHolder(parent, uiHelpers)
             DashboardCardType.POST_CARD_WITHOUT_POST_ITEMS.ordinal ->
                 PostCardViewHolder.PostCardWithoutPostItemsViewHolder(parent, imageManager, uiHelpers)
             DashboardCardType.POST_CARD_WITH_POST_ITEMS.ordinal ->
@@ -41,6 +44,7 @@ class CardsAdapter(
     override fun onBindViewHolder(holder: CardViewHolder<*>, position: Int) {
         when (holder) {
             is ErrorCardViewHolder -> holder.bind(items[position] as ErrorCard)
+            is ErrorWithinCardViewHolder -> holder.bind(items[position] as ErrorWithinCard)
             is TodaysStatsCardViewHolder -> holder.bind(items[position] as TodaysStatsCard)
             is PostCardViewHolder<*> -> holder.bind(items[position] as PostCard)
         }
@@ -65,6 +69,7 @@ class CardsAdapter(
 
             return oldItem.dashboardCardType == newItem.dashboardCardType && when {
                 oldItem is ErrorCard && newItem is ErrorCard -> true
+                oldItem is ErrorWithinCard && newItem is ErrorWithinCard -> true
                 oldItem is TodaysStatsCard && newItem is TodaysStatsCard -> true
                 oldItem is PostCardWithPostItems && newItem is PostCardWithPostItems -> true
                 oldItem is PostCardWithoutPostItems && newItem is PostCardWithoutPostItems -> true
