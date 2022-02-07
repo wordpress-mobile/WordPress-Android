f262d6827f2b79427fdb34899ecc374f9fc80cee
ashiagr <agr.ash@gmail.com>
2022-02-07 12:57:38 +0530

    Feature: Track error cards

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsShownTracker.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/CardsShownTracker.kt
@@ -4,8 +4,10 @@ import org.wordpress.android.analytics.AnalyticsTracker.Stat
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.ErrorCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard.PostCardWithPostItems
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PostCard.PostCardWithoutPostItems
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.TodaysStatsCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.TodaysStatsCard.TodaysStatsCardWithData
 import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker.Type
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
@@ -33,10 +35,22 @@ class CardsShownTracker @Inject constructor(
                         Type.ERROR.label
                 )
         )
+        is TodaysStatsCard.Error -> trackCardShown(
+                Pair(
+                        card.dashboardCardType.toTypeValue().label,
+                        Type.TODAYS_STATS.label
+                )
+        )
         is TodaysStatsCardWithData -> trackCardShown(
                 Pair(
                         card.dashboardCardType.toTypeValue().label,
                         Type.TODAYS_STATS.label
+                )
+        )
+        is PostCard.Error -> trackCardShown(
+                Pair(
+                        card.dashboardCardType.toTypeValue().label,
+                        Type.POST.label
                 )
         )
         is PostCardWithoutPostItems -> trackCardShown(
@@ -51,7 +65,6 @@ class CardsShownTracker @Inject constructor(
                         card.postCardType.toSubtypeValue().label
                 )
         )
-        else -> Unit // TODO: ashiagr add tracking for post card error
     }
 
     private fun trackCardShown(pair: Pair<String, String>) {
