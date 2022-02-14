c7df40f42e8a9ea97196fbd17197183f8a5377fc
khaykov <forsver@gmail.com>
2022-02-14 16:41:13 -0600

    Removed READER from the comment source.

--- WordPress/src/main/java/org/wordpress/android/ui/comments/unified/CommentSource.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/unified/CommentSource.kt
@@ -3,12 +3,11 @@ package org.wordpress.android.ui.comments.unified
 import org.wordpress.android.util.analytics.AnalyticsUtils.AnalyticsCommentActionSource
 
 enum class CommentSource {
-    NOTIFICATION, SITE_COMMENTS, READER;
+    NOTIFICATION, SITE_COMMENTS;
 
     fun toAnalyticsCommentActionSource(): AnalyticsCommentActionSource =
             when (this) {
                 NOTIFICATION -> AnalyticsCommentActionSource.NOTIFICATIONS
                 SITE_COMMENTS -> AnalyticsCommentActionSource.SITE_COMMENTS
-                READER -> AnalyticsCommentActionSource.READER
             }
 }
