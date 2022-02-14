2f9311ccea1f5241dfea8b6589794a2c718628cc
khaykov <forsver@gmail.com>
2022-02-14 16:07:17 -0600

    Added reader comment source.

--- WordPress/src/main/java/org/wordpress/android/ui/comments/unified/CommentSource.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/unified/CommentSource.kt
@@ -3,11 +3,12 @@ package org.wordpress.android.ui.comments.unified
 import org.wordpress.android.util.analytics.AnalyticsUtils.AnalyticsCommentActionSource
 
 enum class CommentSource {
-    NOTIFICATION, SITE_COMMENTS;
+    NOTIFICATION, SITE_COMMENTS, READER;
 
     fun toAnalyticsCommentActionSource(): AnalyticsCommentActionSource =
             when (this) {
                 NOTIFICATION -> AnalyticsCommentActionSource.NOTIFICATIONS
                 SITE_COMMENTS -> AnalyticsCommentActionSource.SITE_COMMENTS
+                READER -> AnalyticsCommentActionSource.READER
             }
 }
