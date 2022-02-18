eb9bf168e92ea3bc6b0b7e467b0ef9ef8f04958e
ashiagr <agr.ash@gmail.com>
2022-02-18 18:19:04 +0530

    Enable my site dashboard - today's stats feature
    Feature will be controlled using Firebase Remote Config condition targeting a percentage of users.

--- WordPress/src/main/java/org/wordpress/android/util/config/MySiteDashboardTodaysStatsCardFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/MySiteDashboardTodaysStatsCardFeatureConfig.kt
@@ -1,16 +1,25 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.annotation.Feature
+import org.wordpress.android.util.config.MySiteDashboardTodaysStatsCardFeatureConfig.Companion.MY_SITE_DASHBOARD_TODAYS_STATS_CARD
 import javax.inject.Inject
 
 /**
- * Configuration of the 'My Site Dashboard - Stats Card' that will add Stats Card on the 'My Site' screen.
+ * Configuration of the 'My Site Dashboard - Today's Stats Card' that will add the card on the 'My Site' screen.
  */
-@FeatureInDevelopment
+@Feature(
+        remoteField = MY_SITE_DASHBOARD_TODAYS_STATS_CARD,
+        defaultValue = true
+)
 class MySiteDashboardTodaysStatsCardFeatureConfig @Inject constructor(
     appConfig: AppConfig
 ) : FeatureConfig(
         appConfig,
-        BuildConfig.MY_SITE_DASHBOARD_TODAYS_STATS_CARD
-)
+        BuildConfig.MY_SITE_DASHBOARD_TODAYS_STATS_CARD,
+        MY_SITE_DASHBOARD_TODAYS_STATS_CARD
+) {
+    companion object {
+        const val MY_SITE_DASHBOARD_TODAYS_STATS_CARD = "my_site_dashboard_todays_stats_card"
+    }
+}
