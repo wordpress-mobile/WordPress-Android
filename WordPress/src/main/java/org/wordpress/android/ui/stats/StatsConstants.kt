c927d6d6bd8fb5656e36dce804fe90566d594cf7
Renan Ferrari <renan.fb@gmail.com>
2022-02-22 17:17:29 -0300

    Remove unused Stats constant

--- WordPress/src/main/java/org/wordpress/android/ui/stats/StatsConstants.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/StatsConstants.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.stats
 
 object StatsConstants {
-    const val STATS_INPUT_DATE_FORMAT = "yyyy-MM-dd"
     const val ITEM_TYPE_POST = "post"
     const val ITEM_TYPE_HOME_PAGE = "homepage"
     const val ITEM_TYPE_ATTACHMENT = "attachment"
