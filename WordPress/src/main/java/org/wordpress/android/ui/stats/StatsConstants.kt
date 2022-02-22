a17c56221b42ff0aea98ec288bc2495d795b7783
Renan Ferrari <renan.fb@gmail.com>
2022-02-22 16:59:41 -0300

    Convert StatsConstants to Kotlin

--- WordPress/src/main/java/org/wordpress/android/ui/stats/StatsConstants.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/StatsConstants.kt
@@ -1,8 +1,8 @@
-package org.wordpress.android.ui.stats;
+package org.wordpress.android.ui.stats
 
-public class StatsConstants {
-    public static final String STATS_INPUT_DATE_FORMAT = "yyyy-MM-dd";
-    public static final String ITEM_TYPE_POST = "post";
-    public static final String ITEM_TYPE_HOME_PAGE = "homepage";
-    public static final String ITEM_TYPE_ATTACHMENT = "attachment";
+object StatsConstants {
+    const val STATS_INPUT_DATE_FORMAT = "yyyy-MM-dd"
+    const val ITEM_TYPE_POST = "post"
+    const val ITEM_TYPE_HOME_PAGE = "homepage"
+    const val ITEM_TYPE_ATTACHMENT = "attachment"
 }
