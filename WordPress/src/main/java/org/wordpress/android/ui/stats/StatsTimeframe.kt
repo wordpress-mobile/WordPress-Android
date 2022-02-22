17bf1847a348b9cbe0798e901bdd41b06aa9ea19
Renan Ferrari <renan.fb@gmail.com>
2022-02-22 17:10:45 -0300

    Remove StatsTimeframe unused field

--- WordPress/src/main/java/org/wordpress/android/ui/stats/StatsTimeframe.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/StatsTimeframe.kt
@@ -1,18 +1,12 @@
 package org.wordpress.android.ui.stats
 
-import org.wordpress.android.R
-import org.wordpress.android.WordPress
-
 /**
  * Timeframes for the stats pages.
  */
-enum class StatsTimeframe(private val labelResId: Int) {
-    INSIGHTS(R.string.stats_insights),
-    DAY(R.string.stats_timeframe_days),
-    WEEK(R.string.stats_timeframe_weeks),
-    MONTH(R.string.stats_timeframe_months),
-    YEAR(R.string.stats_timeframe_years);
-
-    val label: String
-        get() = WordPress.getContext().getString(labelResId)
+enum class StatsTimeframe {
+    INSIGHTS,
+    DAY,
+    WEEK,
+    MONTH,
+    YEAR
 }
