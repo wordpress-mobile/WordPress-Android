6e073fb08bd2c87d9539f9b0bb0ba8da20287221
Renan Ferrari <renan.fb@gmail.com>
2022-02-23 19:18:18 -0300

    Simplify StatsSinceLabelFormatter and improve documentation

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/StatsSinceLabelFormatter.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/StatsSinceLabelFormatter.kt
@@ -1,107 +1,100 @@
 package org.wordpress.android.ui.stats.refresh.utils
 
+import androidx.annotation.StringRes
 import org.wordpress.android.R
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.ONE_DAY_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.ONE_HOUR_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.ONE_MINUTE_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.ONE_MONTH_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.ONE_YEAR_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.SECONDS_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.X_DAYS_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.X_HOURS_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.X_MINUTES_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.X_MONTHS_AGO
+import org.wordpress.android.ui.stats.refresh.utils.StatsSinceLabelFormatter.Interval.X_YEARS_AGO
 import org.wordpress.android.viewmodel.ResourceProvider
+import java.time.Duration
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit.DAYS
+import java.time.temporal.ChronoUnit.FOREVER
+import java.time.temporal.ChronoUnit.HOURS
+import java.time.temporal.ChronoUnit.MINUTES
+import java.time.temporal.ChronoUnit.MONTHS
+import java.time.temporal.ChronoUnit.YEARS
 import java.util.Date
 import java.util.Locale
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeUnit.MILLISECONDS
-import java.util.concurrent.TimeUnit.SECONDS
 import javax.inject.Inject
+import kotlin.math.roundToLong
 
+/**
+ * ┌──────────────────────────────┬───────────────────────────┐
+ * │ Interval                     │ Label                     │
+ * ├──────────────────────────────┼───────────────────────────┤
+ * │ 0 to 44 seconds              │ seconds ago               │
+ * │ 45 to 89 seconds             │ a minute ago              │
+ * │ 90 seconds to 44 minutes     │ 2 minutes ... 45 minutes  │
+ * │ 45 to 90 minutes             │ an hour ago               │
+ * │ 90 minutes to 21 hours       │ 2 hours ... 22 hours      │
+ * │ 22 to 35 hours               │ a day                     │
+ * │ 36 hours to 25 days          │ 2 days ... 25 days        │
+ * │ 26 to 44 days                │ a month                   │
+ * │ 45 days to 319 days          │ 2 months ... 11 months    │
+ * │ 320 to 547 days (1.5 years)  │ a year                    │
+ * │ 548 days+                    │ 2 years ... 20 years      │
+ * └──────────────────────────────┴───────────────────────────┘
+ *
+ * Based on http://momentjs.com/docs/#/displaying/fromnow/
+ */
 class StatsSinceLabelFormatter @Inject constructor(
     val resourceProvider: ResourceProvider
 ) {
-    fun getSinceLabelLowerCase(date: Date): String {
-        return getSinceLabel(resourceProvider, date).toLowerCase(Locale.getDefault())
+    fun getSinceLabelLowerCase(date: Date) = getSinceLabel(date).lowercase(Locale.getDefault())
+
+    fun getSinceLabel(date: Date) = getSinceLabel(date.toInstant().atZone(ZoneId.systemDefault()))
+
+    fun getSinceLabel(
+        date: ZonedDateTime,
+        now: ZonedDateTime = ZonedDateTime.now()
+    ) = when (Interval.between(date, now)) {
+        SECONDS_AGO -> getLabelString(R.string.stats_followers_seconds_ago)
+        ONE_MINUTE_AGO -> getLabelString(R.string.stats_followers_a_minute_ago)
+        X_MINUTES_AGO -> getLabelString(R.string.stats_followers_minutes, MINUTES.between(date, now))
+        ONE_HOUR_AGO -> getLabelString(R.string.stats_followers_an_hour_ago)
+        X_HOURS_AGO -> getLabelString(R.string.stats_followers_hours, HOURS.between(date, now))
+        ONE_DAY_AGO -> getLabelString(R.string.stats_followers_a_day)
+        X_DAYS_AGO -> getLabelString(R.string.stats_followers_days, DAYS.between(date, now))
+        ONE_MONTH_AGO -> getLabelString(R.string.stats_followers_a_month)
+        X_MONTHS_AGO -> getLabelString(R.string.stats_followers_months, MONTHS.between(date, now))
+        ONE_YEAR_AGO -> getLabelString(R.string.stats_followers_a_year)
+        X_YEARS_AGO -> getLabelString(R.string.stats_followers_years, YEARS.between(date, now))
     }
 
-    /**
-     * Get a diff between two dates
-     *
-     * @param date1    the oldest date in Ms
-     * @param date2    the newest date in Ms
-     * @param timeUnit the unit in which you want the diff
-     * @return the diff value, in the provided unit
-     */
-    private fun getDateDiff(date1: Date, date2: Date, timeUnit: TimeUnit): Long {
-        val diffInMillies = date2.time - date1.time
-        return timeUnit.convert(diffInMillies, MILLISECONDS)
-    }
+    private fun getLabelString(@StringRes labelId: Int, labelTime: Long? = null) = labelTime
+            ?.let { resourceProvider.getString(labelId, labelTime.roundUp()) }
+            ?: resourceProvider.getString(labelId)
 
-    private fun roundUp(num: Double, divisor: Double): Int {
-        val unrounded = num / divisor
-        // return (int) Math.ceil(unrounded);
-        return (unrounded + 0.5).toInt()
-    }
+    private fun Long.roundUp(increment: Double = 0.5) = plus(increment).roundToLong()
 
-    fun getSinceLabel(ctx: ResourceProvider, date: Date): String {
-        val currentDateTime = Date()
+    private enum class Interval(val threshold: Duration) {
+        SECONDS_AGO(Duration.ofSeconds(45)),
+        ONE_MINUTE_AGO(Duration.ofSeconds(90)),
+        X_MINUTES_AGO(Duration.ofMinutes(45)),
+        ONE_HOUR_AGO(Duration.ofMinutes(90)),
+        X_HOURS_AGO(Duration.ofHours(22)),
+        ONE_DAY_AGO(Duration.ofHours(36)),
+        X_DAYS_AGO(Duration.ofDays(25)),
+        ONE_MONTH_AGO(Duration.ofDays(45)),
+        X_MONTHS_AGO(Duration.ofDays(320)),
+        ONE_YEAR_AGO(Duration.ofDays(548)),
+        X_YEARS_AGO(FOREVER.duration);
 
-        // See http://momentjs.com/docs/#/displaying/fromnow/
-        val currentDifference = Math.abs(getDateDiff(date, currentDateTime, SECONDS))
-        if (currentDifference <= 45) {
-            return ctx.getString(R.string.stats_followers_seconds_ago)
+        companion object {
+            fun between(
+                startDate: ZonedDateTime,
+                endDate: ZonedDateTime
+            ) = values().first { Duration.between(startDate, endDate) < it.threshold }
         }
-        if (currentDifference < 90) {
-            return ctx.getString(R.string.stats_followers_a_minute_ago)
-        }
-
-        // 90 seconds to 45 minutes
-        if (currentDifference <= 2700) {
-            val minutes = roundUp(currentDifference.toDouble(), 60.0).toLong()
-            val followersMinutes = ctx.getString(R.string.stats_followers_minutes)
-            return String.format(followersMinutes, minutes)
-        }
-
-        // 45 to 90 minutes
-        if (currentDifference <= 5400) {
-            return ctx.getString(R.string.stats_followers_an_hour_ago)
-        }
-
-        // 90 minutes to 22 hours
-        if (currentDifference <= 79200) {
-            val hours = roundUp(currentDifference.toDouble(), (60 * 60).toDouble()).toLong()
-            val followersHours = ctx.getString(R.string.stats_followers_hours)
-            return String.format(followersHours, hours)
-        }
-
-        // 22 to 36 hours
-        if (currentDifference <= 129600) {
-            return ctx.getString(R.string.stats_followers_a_day)
-        }
-
-        // 36 hours to 25 days
-        // 86400 secs in a day - 2160000 secs in 25 days
-        if (currentDifference <= 2160000) {
-            val days = roundUp(currentDifference.toDouble(), 86400.0).toLong()
-            val followersDays = ctx.getString(R.string.stats_followers_days)
-            return String.format(followersDays, days)
-        }
-
-        // 25 to 45 days
-        // 3888000 secs in 45 days
-        if (currentDifference <= 3888000) {
-            return ctx.getString(R.string.stats_followers_a_month)
-        }
-
-        // 45 to 345 days
-        // 2678400 secs in a month - 29808000 secs in 345 days
-        if (currentDifference <= 29808000) {
-            val months = roundUp(currentDifference.toDouble(), 2678400.0).toLong()
-            val followersMonths = ctx.getString(R.string.stats_followers_months)
-            return String.format(followersMonths, months)
-        }
-
-        // 345 to 547 days (1.5 years)
-        if (currentDifference <= 47260800) {
-            return ctx.getString(R.string.stats_followers_a_year)
-        }
-
-        // 548 days+
-        // 31536000 secs in a year
-        val years = roundUp(currentDifference.toDouble(), 31536000.0).toLong()
-        val followersYears = ctx.getString(R.string.stats_followers_years)
-        return String.format(followersYears, years)
     }
 }
