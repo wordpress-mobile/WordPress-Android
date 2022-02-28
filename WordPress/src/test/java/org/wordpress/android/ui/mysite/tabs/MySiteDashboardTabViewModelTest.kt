b65da1c4f4b222e05215483dcb630b794392aa3a
ashiagr <agr.ash@gmail.com>
2022-02-28 15:58:13 +0530

    Feature: Add blank tab fragments, VM, VM test classes

---
+++ WordPress/src/test/java/org/wordpress/android/ui/mysite/tabs/MySiteDashboardTabViewModelTest.kt
@@ -0,0 +1,8 @@
+package org.wordpress.android.ui.mysite.tabs
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.BaseUnitTest
+
+@RunWith(MockitoJUnitRunner::class)
+class MySiteDashboardTabViewModelTest : BaseUnitTest()
