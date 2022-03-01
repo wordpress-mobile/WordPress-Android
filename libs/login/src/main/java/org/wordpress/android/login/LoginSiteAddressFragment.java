f81b3565f3dc50bae6ca619e79f6f3df9f8a45e6
ashiagr <agr.ash@gmail.com>
2022-03-01 15:58:55 +0530

    Allow WordPress.com site address login for Jetpack app
    Since we now allow sign up and site creation using WordPress.com (which creates wpcom simple sites) in the Jetpack app, when a site address during login is a WordPress.com site (irrespective of simple or atomic site), we now direct to the WordPress.com email login.

--- libs/login/src/main/java/org/wordpress/android/login/LoginSiteAddressFragment.java
+++ libs/login/src/main/java/org/wordpress/android/login/LoginSiteAddressFragment.java
@@ -414,7 +414,7 @@ public class LoginSiteAddressFragment extends LoginBaseDiscoveryFragment impleme
 
             if (mLoginListener.getLoginMode() == LoginMode.WOO_LOGIN_MODE) {
                 handleConnectSiteInfoForWoo(event.info);
-            } else if (mLoginListener.getLoginMode() == LoginMode.JETPACK_LOGIN_ONLY) {
+            } else if (!event.info.isWPCom && mLoginListener.getLoginMode() == LoginMode.JETPACK_LOGIN_ONLY) {
                 handleConnectSiteInfoForJetpack(event.info);
             } else {
                 handleConnectSiteInfoForWordPress(event.info);
