91d0b717224430b61b8e50dfbc54e348cab83bc3
Sowmia <sowmiasundar@gmail.com>
2022-03-08 12:12:44 +0530

    Creates extension function showSignIn

---
+++ WordPress/src/main/java/org/wordpress/android/extension/AndroidExtensions.kt
@@ -0,0 +1,13 @@
+package org.wordpress.android.extension
+
+import androidx.fragment.app.FragmentActivity
+import org.wordpress.android.BuildConfig
+import org.wordpress.android.ui.ActivityLauncher
+
+fun FragmentActivity.showSignInForResult(){
+    if (BuildConfig.IS_JETPACK_APP) {
+        ActivityLauncher.showSignInForResultJetpackOnly(this)
+    } else {
+        ActivityLauncher.showSignInForResultWpComOnly(this)
+    }
+}
