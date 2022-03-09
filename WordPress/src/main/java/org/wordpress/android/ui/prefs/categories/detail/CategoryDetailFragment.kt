bffec0e9a3e7ca3fcea9f9dd08a518810682b6f7
Ajesh R Pai <ajeshr2k1@gmail.com>
2022-03-09 15:40:27 +0530

    Adds: UI logic and implementation logic in Viewmodel

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/categories/detail/CategoryDetailFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/categories/detail/CategoryDetailFragment.kt
@@ -1,24 +1,44 @@
 package org.wordpress.android.ui.prefs.categories.detail
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import org.wordpress.android.R
+import org.wordpress.android.R.layout
 import org.wordpress.android.WordPress
 import org.wordpress.android.databinding.CategoryDetailFragmentBinding
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.models.CategoryNode
+import org.wordpress.android.ui.pages.SnackbarMessageHolder
+import org.wordpress.android.ui.posts.ParentCategorySpinnerAdapter
+import org.wordpress.android.ui.prefs.categories.detail.CategoryDetailViewModel.SubmitButtonUiState
+import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.util.ActivityUtils
+import org.wordpress.android.util.ToastUtils
+import org.wordpress.android.util.ToastUtils.Duration.SHORT
+import org.wordpress.android.viewmodel.observeEvent
 import javax.inject.Inject
 
 class CategoryDetailFragment : Fragment(R.layout.category_detail_fragment) {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: CategoryDetailViewModel
+    @Inject lateinit var uiHelpers: UiHelpers
+
+    var spinnerTouched: Boolean = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initDagger()
 
         with(CategoryDetailFragmentBinding.bind(view)) {
+            initSubmitButton()
+            initAdapter()
+            initSpinner()
+            initInputText()
             initViewModel(getSite(savedInstanceState))
         }
     }
@@ -27,9 +47,10 @@ class CategoryDetailFragment : Fragment(R.layout.category_detail_fragment) {
         (requireActivity().application as WordPress).component()?.inject(this)
     }
 
-    private fun initViewModel(site: SiteModel) {
+    private fun CategoryDetailFragmentBinding.initViewModel(site: SiteModel) {
         viewModel = ViewModelProvider(this@CategoryDetailFragment, viewModelFactory)
                 .get(CategoryDetailViewModel::class.java)
+        startObserving()
         viewModel.start(site)
     }
 
@@ -44,5 +65,96 @@ class CategoryDetailFragment : Fragment(R.layout.category_detail_fragment) {
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putSerializable(WordPress.SITE, viewModel.siteModel)
         super.onSaveInstanceState(outState)
+    }
+
+    private fun CategoryDetailFragmentBinding.initSubmitButton() {
+        submitButton.setOnClickListener {
+            viewModel.onSubmitButtonClick()
+        }
+    }
+
+    private fun CategoryDetailFragmentBinding.initInputText() {
+        categoryName.requestFocus()
+        categoryName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                viewModel.categoryNameUpdated(s.toString())
+            }
+        })
+        ActivityUtils.showKeyboard(categoryName)
+    }
+
+    private fun CategoryDetailFragmentBinding.initAdapter() {
+        val categoryAdapter = ParentCategorySpinnerAdapter(
+                activity,
+                layout.categories_row_parent,
+                arrayListOf<CategoryNode>()
+        )
+        parentCategory.adapter = categoryAdapter
+    }
+
+    private fun CategoryDetailFragmentBinding.initSpinner() {
+        parentCategory.setOnTouchListener { v, _ ->
+            spinnerTouched = true
+            v.performClick()
+            false
+        }
+
+        parentCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (spinnerTouched) {
+                    viewModel.parentCategorySelected(position)
+                    spinnerTouched = false
+                }
+            }
+        }
+    }
+
+    private fun CategoryDetailFragmentBinding.startObserving() {
+        viewModel.dismissKeyboard.observeEvent(viewLifecycleOwner) {
+            ActivityUtils.hideKeyboardForced(categoryName)
+        }
+
+        viewModel.uiState.observe(viewLifecycleOwner) { uiState ->
+            loadCategories(uiState.categories)
+            if (uiState.selectedParentCategoryPosition != parentCategory.selectedItemPosition) {
+                parentCategory.setSelection(uiState.selectedParentCategoryPosition)
+            }
+            updateSubmitButton(uiState.submitButtonUiState)
+        }
+    }
+
+    private fun CategoryDetailFragmentBinding.updateSubmitButton(submitButtonUiState: SubmitButtonUiState) {
+        with(submitButton) {
+            isEnabled = submitButtonUiState.enabled
+        }
+        with(uiHelpers) {
+            updateVisibility(submitButton, submitButtonUiState.visibility)
+        }
+    }
+
+    private fun CategoryDetailFragmentBinding.loadCategories(categoryLevels: ArrayList<CategoryNode>) {
+        (parentCategory.adapter as? ParentCategorySpinnerAdapter)?.replaceItems(categoryLevels)
+    }
+
+    private fun SnackbarMessageHolder.showToast() {
+        val message = uiHelpers.getTextOfUiString(requireContext(), this.message).toString()
+        ToastUtils.showToast(
+                requireContext(), message,
+                SHORT
+        )
     }
 }
--- WordPress/src/main/java/org/wordpress/android/ui/prefs/categories/detail/CategoryDetailViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/categories/detail/CategoryDetailViewModel.kt
@@ -1,19 +1,55 @@
 package org.wordpress.android.ui.prefs.categories.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.CoroutineDispatcher
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode.MAIN
+import org.wordpress.android.R
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.store.TaxonomyStore.OnTaxonomyChanged
+import org.wordpress.android.models.CategoryNode
 import org.wordpress.android.modules.BG_THREAD
+import org.wordpress.android.ui.pages.SnackbarMessageHolder
+import org.wordpress.android.ui.posts.AddCategoryUseCase
+import org.wordpress.android.ui.posts.GetCategoriesUseCase
+import org.wordpress.android.ui.prefs.categories.detail.CategoryDetailViewModel.SubmitButtonUiState.SubmitButtonDisabledUiState
+import org.wordpress.android.ui.prefs.categories.detail.CategoryDetailViewModel.SubmitButtonUiState.SubmitButtonEnabledUiState
+import org.wordpress.android.ui.utils.UiString
+import org.wordpress.android.ui.utils.UiString.UiStringRes
+import org.wordpress.android.util.AppLog
+import org.wordpress.android.util.AppLog.T
+import org.wordpress.android.util.NetworkUtilsWrapper
+import org.wordpress.android.viewmodel.Event
+import org.wordpress.android.viewmodel.ResourceProvider
 import org.wordpress.android.viewmodel.ScopedViewModel
 import javax.inject.Inject
 import javax.inject.Named
 
 class CategoryDetailViewModel @Inject constructor(
     @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
+    private val networkUtilsWrapper: NetworkUtilsWrapper,
+    private val getCategoriesUseCase: GetCategoriesUseCase,
+    private val addCategoryUseCase: AddCategoryUseCase,
+    resourceProvider: ResourceProvider,
     private val dispatcher: Dispatcher
 ) : ScopedViewModel(bgDispatcher) {
     private var isStarted = false
     lateinit var siteModel: SiteModel
+
+    private val _dismissKeyboard = MutableLiveData<Event<Unit>>()
+    val dismissKeyboard: LiveData<Event<Unit>> = _dismissKeyboard
+
+    private val _toolbarTitleUiState = MutableLiveData<UiString>()
+    val toolbarTitleUiState: LiveData<UiString> = _toolbarTitleUiState
+
+    private val _snackbarEvents = MediatorLiveData<Event<SnackbarMessageHolder>>()
+    val snackbarEvents: LiveData<Event<SnackbarMessageHolder>> = _snackbarEvents
+
+    private val _uiState: MutableLiveData<UiState> = MutableLiveData()
+    val uiState: LiveData<UiState> = _uiState
 
     init {
         dispatcher.register(this)
@@ -29,5 +65,93 @@ class CategoryDetailViewModel @Inject constructor(
         isStarted = true
 
         this.siteModel = siteModel
+        initCategories()
+    }
+
+    private fun initCategories() {
+        launch {
+            val siteCategories = getCategoriesUseCase.getSiteCategories(siteModel)
+            siteCategories.add(topLevelCategory)
+            _uiState.postValue(
+                    UiState(
+                            toolbarTitle = UiStringRes(R.string.add_new_category),
+                            categories = siteCategories,
+                            selectedParentCategoryPosition = 0,
+                            categoryName = ""
+                    )
+            )
+        }
+    }
+
+    private val topLevelCategory = CategoryNode(0, 0, resourceProvider.getString(R.string.top_level_category_name))
+
+    fun onSubmitButtonClick() {
+        _uiState.value?.let { state ->
+            addCategory(
+                    state.categoryName,
+                    state.categories[state.selectedParentCategoryPosition]
+            )
+        }
+    }
+
+    private fun addCategory(categoryText: String, parentCategory: CategoryNode) {
+        if (!networkUtilsWrapper.isNetworkAvailable()) {
+            _dismissKeyboard.postValue(Event(Unit))
+            _snackbarEvents.postValue(
+                    Event(SnackbarMessageHolder(UiStringRes(R.string.no_network_message)))
+            )
+            return
+        }
+        addCategoryUseCase.addCategory(categoryText, parentCategory.categoryId, siteModel)
+    }
+
+    fun categoryNameUpdated(inputValue: String) {
+        uiState.value?.let { state ->
+            val submitButtonUiState = if (inputValue.isNotEmpty()) {
+                SubmitButtonEnabledUiState
+            } else {
+                SubmitButtonDisabledUiState
+            }
+            _uiState.value = state.copy(
+                    categoryName = inputValue,
+                    submitButtonUiState = submitButtonUiState
+            )
+        }
+    }
+
+    fun parentCategorySelected(position: Int) {
+        _uiState.value?.let { state ->
+            _uiState.value = state.copy(selectedParentCategoryPosition = position)
+        }
+    }
+
+    data class UiState(
+        val toolbarTitle: UiString,
+        val categories: ArrayList<CategoryNode>,
+        val selectedParentCategoryPosition: Int,
+        val categoryName: String,
+        val submitButtonUiState: SubmitButtonUiState = SubmitButtonDisabledUiState
+    )
+
+    sealed class SubmitButtonUiState(
+        val visibility: Boolean = true,
+        val enabled: Boolean = false
+    ) {
+        object SubmitButtonEnabledUiState : SubmitButtonUiState(
+                enabled = true
+        )
+
+        object SubmitButtonDisabledUiState : SubmitButtonUiState(
+                enabled = false
+        )
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe(threadMode = MAIN)
+    fun onTaxonomyChanged(event: OnTaxonomyChanged) {
+        if (event.isError) AppLog.e(
+                T.SETTINGS,
+                "An error occurred while updating taxonomy with type: " + event.error.type
+        )
     }
 }
