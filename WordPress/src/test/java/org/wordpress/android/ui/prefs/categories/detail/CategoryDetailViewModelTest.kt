a7d530e8305f4cb3de37c2d51f4cc205e4c39868
Ajesh R Pai <ajeshr2k1@gmail.com>
2022-03-10 19:18:24 +0530

    Fixes: Adds missing dependencies in CategoryDetailViewModelTest

--- WordPress/src/test/java/org/wordpress/android/ui/prefs/categories/detail/CategoryDetailViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/prefs/categories/detail/CategoryDetailViewModelTest.kt
@@ -7,16 +7,34 @@ import org.junit.Test
 import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.TEST_DISPATCHER
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.ui.mysite.SelectedSiteRepository
+import org.wordpress.android.ui.posts.AddCategoryUseCase
+import org.wordpress.android.ui.posts.GetCategoriesUseCase
+import org.wordpress.android.util.NetworkUtilsWrapper
+import org.wordpress.android.viewmodel.ResourceProvider
 
 @InternalCoroutinesApi
 class CategoryDetailViewModelTest : BaseUnitTest() {
-    private val dispatcher: Dispatcher = mock()
-
     private lateinit var viewModel: CategoryDetailViewModel
+
+    private val dispatcher: Dispatcher = mock()
+    private val networkUtilsWrapper: NetworkUtilsWrapper = mock()
+    private val getCategoriesUseCase: GetCategoriesUseCase = mock()
+    private val addCategoryUseCase: AddCategoryUseCase = mock()
+    private val resourceProvider: ResourceProvider = mock()
+    private val selectedSiteRepository: SelectedSiteRepository = mock()
 
     @Before
     fun setUp() {
-        viewModel = CategoryDetailViewModel(TEST_DISPATCHER, dispatcher)
+        viewModel = CategoryDetailViewModel(
+                TEST_DISPATCHER,
+                networkUtilsWrapper,
+                getCategoriesUseCase,
+                addCategoryUseCase,
+                resourceProvider,
+                dispatcher,
+                selectedSiteRepository
+        )
     }
 
     @Test
