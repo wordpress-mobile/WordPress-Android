c8de28a5ef741263c64cb4cafa31f03e97b174dd
Ovi Trif <octrif@gmail.com>
2022-03-14 18:59:36 +0100

    Handle animating header & toolbar title visibility #16051

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/SiteCreationIntentsFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/SiteCreationIntentsFragment.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.appbar.AppBarLayout
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.databinding.SiteCreationIntentsFragmentBinding
@@ -60,23 +60,44 @@ class SiteCreationIntentsFragment : Fragment() {
     }
 
     private fun SiteCreationIntentsFragmentBinding.setupUi() {
-        siteCreationIntentsTitlebar.title.isInvisible = !isPhoneLandscape()
+        siteCreationIntentsTitlebar.title.visibility = if (isPhoneLandscape()) View.VISIBLE else View.INVISIBLE
         siteCreationHeaderItem.title.setText(R.string.new_site_creation_intents_header_title)
         siteCreationHeaderItem.subtitle.setText(R.string.new_site_creation_intents_header_subtitle)
     }
 
     private fun SiteCreationIntentsFragmentBinding.setupViewModel() {
+        viewModel.uiState.observe(viewLifecycleOwner) { uiState ->
+            setHeaderVisibility(uiState.isHeaderVisible)
+        }
+
         viewModel.start()
+    }
+
+    private fun SiteCreationIntentsFragmentBinding.setHeaderVisibility(visible: Boolean) {
+        uiHelper.fadeInfadeOutViews(
+                siteCreationIntentsTitlebar.title,
+                siteCreationHeaderItem.title,
+                visible
+        )
     }
 
     private fun SiteCreationIntentsFragmentBinding.setupActionListeners() {
         siteCreationIntentsTitlebar.skipButton.setOnClickListener { viewModel.onSkipPressed() }
         siteCreationIntentsTitlebar.backButton.setOnClickListener { viewModel.onBackPressed() }
+        setScrollListener()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    private fun SiteCreationIntentsFragmentBinding.setScrollListener() {
+        val scrollThreshold = resources.getDimension(R.dimen.picker_header_scroll_snap_threshold).toInt()
+        appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+            viewModel.onAppBarOffsetChanged(verticalOffset, scrollThreshold)
+        })
+        viewModel.onAppBarOffsetChanged(0, scrollThreshold)
     }
 
     private fun isPhoneLandscape() = displayUtils.isLandscapeBySize() && !displayUtils.isTablet()
--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/SiteCreationIntentsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/SiteCreationIntentsViewModel.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.ui.sitecreation.verticals
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
@@ -28,12 +29,26 @@ class SiteCreationIntentsViewModel @Inject constructor(
     private val _onBackButtonPressed = SingleLiveEvent<Unit>()
     val onBackButtonPressed: LiveData<Unit> = _onBackButtonPressed
 
+    private val _uiState = MutableLiveData<IntentsUiState>()
+    val uiState: LiveData<IntentsUiState> = _uiState
+
     fun start() {
         if (isStarted) {
             return
         }
         isStarted = true
         // tracker.trackSiteIntentQuestionViewed()
+        resetUiState()
+    }
+
+    private fun resetUiState() {
+        updateUiState(
+                IntentsUiState()
+        )
+    }
+
+    private fun updateUiState(uiState: IntentsUiState) {
+        _uiState.value = uiState
     }
 
     fun onSkipPressed() {
@@ -45,4 +60,16 @@ class SiteCreationIntentsViewModel @Inject constructor(
         // tracker.trackSiteIntentQuestionCanceled()
         _onBackButtonPressed.call()
     }
+
+    fun onAppBarOffsetChanged(verticalOffset: Int, scrollThreshold: Int) {
+        val headerShouldBeVisible = verticalOffset < scrollThreshold
+        uiState.value?.let { state ->
+            if (state.isHeaderVisible == headerShouldBeVisible) return // No change
+            updateUiState(state.copy(isHeaderVisible = headerShouldBeVisible))
+        }
+    }
+
+    data class IntentsUiState(
+        val isHeaderVisible: Boolean = false,
+    )
 }
