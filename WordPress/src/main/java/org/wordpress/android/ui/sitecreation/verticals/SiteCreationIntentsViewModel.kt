5b051070813d4cd45141dc8258f3d426b1f29c43
Ovi Trif <octrif@gmail.com>
2022-03-15 12:44:00 +0100

    Remove handling of scroll title visibility to keep PR smaller

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/SiteCreationIntentsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/SiteCreationIntentsViewModel.kt
@@ -1,20 +1,17 @@
 package org.wordpress.android.ui.sitecreation.verticals
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import org.wordpress.android.modules.BG_THREAD
-import org.wordpress.android.ui.sitecreation.misc.SiteCreationTracker
 import org.wordpress.android.viewmodel.SingleLiveEvent
 import javax.inject.Inject
 import javax.inject.Named
 import kotlin.coroutines.CoroutineContext
 
 class SiteCreationIntentsViewModel @Inject constructor(
-    private val tracker: SiteCreationTracker,
     @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher
 ) : ViewModel(), CoroutineScope {
     private val job = Job()
@@ -29,26 +26,10 @@ class SiteCreationIntentsViewModel @Inject constructor(
     private val _onBackButtonPressed = SingleLiveEvent<Unit>()
     val onBackButtonPressed: LiveData<Unit> = _onBackButtonPressed
 
-    private val _uiState = MutableLiveData<IntentsUiState>()
-    val uiState: LiveData<IntentsUiState> = _uiState
-
     fun start() {
-        if (isStarted) {
-            return
-        }
+        if (isStarted) return
         isStarted = true
         // tracker.trackSiteIntentQuestionViewed()
-        resetUiState()
-    }
-
-    private fun resetUiState() {
-        updateUiState(
-                IntentsUiState()
-        )
-    }
-
-    private fun updateUiState(uiState: IntentsUiState) {
-        _uiState.value = uiState
     }
 
     fun onSkipPressed() {
@@ -60,16 +41,4 @@ class SiteCreationIntentsViewModel @Inject constructor(
         // tracker.trackSiteIntentQuestionCanceled()
         _onBackButtonPressed.call()
     }
-
-    fun onAppBarOffsetChanged(verticalOffset: Int, scrollThreshold: Int) {
-        val headerShouldBeVisible = verticalOffset < scrollThreshold
-        uiState.value?.let { state ->
-            if (state.isHeaderVisible == headerShouldBeVisible) return // No change
-            updateUiState(state.copy(isHeaderVisible = headerShouldBeVisible))
-        }
-    }
-
-    data class IntentsUiState(
-        val isHeaderVisible: Boolean = false
-    )
 }
