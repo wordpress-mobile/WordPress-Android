85240315410995dd8237c06cb32b84742c28d98b
khaykov <forsver@gmail.com>
2022-03-22 18:17:36 -0500

    card builder and VH

---
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloggingprompts/BloggingPromptCardBuilder.kt
@@ -0,0 +1,17 @@
+package org.wordpress.android.ui.mysite.cards.dashboard.bloggingprompts
+
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BloggingPromptCard.BloggingPromptCardWithData
+import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.BloggingPromptCardBuilderParams
+import org.wordpress.android.ui.utils.UiString.UiStringText
+import javax.inject.Inject
+
+class BloggingPromptCardBuilder @Inject constructor() {
+    fun build(params: BloggingPromptCardBuilderParams) = params.bloggingPrompt.let {
+        BloggingPromptCardWithData(
+                prompt = UiStringText(it.text),
+                answeredUsers = emptyList(),
+                numberOfAnswers = it.numberOfAnswers,
+                isAnswered = false
+        )
+    }
+}
