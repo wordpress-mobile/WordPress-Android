aae908fe7a6f964e83f6f9964a5aa4e7f7677695
Antonis Lilis <antonis.lilis@automattic.com>
2022-03-29 16:24:37 +0300

    Replaces fuzzy search with simple string search

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/VerticalsSearchResultsProvider.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/verticals/VerticalsSearchResultsProvider.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.sitecreation.verticals
 
 import dagger.Reusable
-import org.apache.commons.text.similarity.FuzzyScore
 import org.wordpress.android.ui.sitecreation.verticals.SiteCreationIntentsViewModel.IntentListItemUiState
 import org.wordpress.android.util.LocaleManagerWrapper
 import javax.inject.Inject
@@ -9,15 +8,10 @@ import javax.inject.Inject
 @Reusable
 class VerticalsSearchResultsProvider @Inject constructor(localeManagerWrapper: LocaleManagerWrapper) {
     private val locale = localeManagerWrapper.getLocale()
-    private val fuzzyScore = FuzzyScore(locale)
 
     fun search(items: List<IntentListItemUiState>, query: String) = if (query.isEmpty()) items else items.filter {
         val text = it.verticalText.lowercase(locale)
         val query = query.lowercase(locale)
-        fuzzyScore.fuzzyScore(query, text) > fuzzyScoreThreshold
-    }
-
-    companion object {
-        private const val fuzzyScoreThreshold = 6
+        text.contains(query)
     }
 }
