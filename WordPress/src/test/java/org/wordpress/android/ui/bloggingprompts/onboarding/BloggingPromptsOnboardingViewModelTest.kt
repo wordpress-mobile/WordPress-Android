5de5988701ae2b10c006a6d78ce0d5d0b13f3632
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-03-29 03:24:57 -0300

    Update BloggingPromptsOnboardingViewModel tests

--- WordPress/src/test/java/org/wordpress/android/ui/bloggingprompts/onboarding/BloggingPromptsOnboardingViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/bloggingprompts/onboarding/BloggingPromptsOnboardingViewModelTest.kt
@@ -3,19 +3,18 @@ package org.wordpress.android.ui.bloggingprompts.onboarding
 import androidx.lifecycle.Observer
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
-import kotlinx.coroutines.flow.flow
 import org.junit.Before
 import org.junit.Test
 import org.wordpress.android.BaseUnitTest
+import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.models.bloggingprompts.BloggingPrompt
-import org.wordpress.android.models.usecases.GetBloggingPromptUseCase
 import org.wordpress.android.test
 import org.wordpress.android.ui.bloggingprompts.onboarding.BloggingPromptsOnboardingAction.OpenEditor
 
 class BloggingPromptsOnboardingViewModelTest : BaseUnitTest() {
-    private val getBloggingPromptUseCase: GetBloggingPromptUseCase = mock()
-    private val bloggingPromptsOnboardingViewModel = BloggingPromptsOnboardingViewModel(getBloggingPromptUseCase)
+    private val uiStateMapper: BloggingPromptsOnboardingUiStateMapper = mock()
+    private val siteStore: SiteStore = mock()
+    private val bloggingPromptsOnboardingViewModel = BloggingPromptsOnboardingViewModel(siteStore, uiStateMapper)
     private val actionObserver: Observer<BloggingPromptsOnboardingAction> = mock()
 
     @Before
@@ -26,13 +25,12 @@ class BloggingPromptsOnboardingViewModelTest : BaseUnitTest() {
     @Test
     fun `Should execute GetBloggingPromptUseCase when start is called`() = test {
         bloggingPromptsOnboardingViewModel.start()
-        verify(getBloggingPromptUseCase).execute()
+        verify(uiStateMapper).mapReady()
     }
 
     @Test
     fun `Should trigger OpenEditor action when onTryNow is called`() = test {
         val bloggingPrompt: BloggingPrompt = mock()
-        whenever(getBloggingPromptUseCase.execute()).thenReturn(flow { emit(bloggingPrompt) })
         bloggingPromptsOnboardingViewModel.start()
         bloggingPromptsOnboardingViewModel.onTryNowClick()
         verify(actionObserver).onChanged(OpenEditor(bloggingPrompt))
