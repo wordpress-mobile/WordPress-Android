03ac60d6811a6759009c5c27a52142e8160a07b2
khaykov <forsver@gmail.com>
2022-03-31 19:50:09 -0500

    Playing with centering of recyclerview.

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloggingprompts/BloggingPromptCardViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloggingprompts/BloggingPromptCardViewHolder.kt
@@ -3,27 +3,38 @@ package org.wordpress.android.ui.mysite.cards.dashboard.bloggingprompts
 import android.view.ViewGroup
 import androidx.appcompat.widget.PopupMenu
 import androidx.core.view.MenuCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import org.wordpress.android.R
 import org.wordpress.android.databinding.MySiteBloggingPrompCardBinding
+import org.wordpress.android.ui.avatars.FACE_ITEM_LEFT_OFFSET_DIMEN
+import org.wordpress.android.ui.avatars.PostLikerItemDecorator
+import org.wordpress.android.ui.avatars.ReaderPostLikersAdapter
+import org.wordpress.android.ui.avatars.TrainOfFacesItem.BloggersLikingTextItem
+import org.wordpress.android.ui.avatars.TrainOfFacesItem.FaceItem
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BloggingPromptCard.BloggingPromptCardWithData
 import org.wordpress.android.ui.mysite.cards.dashboard.CardViewHolder
 import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.ui.utils.UiString.UiStringText
+import org.wordpress.android.util.RtlUtils
 import org.wordpress.android.util.extensions.viewBinding
+import org.wordpress.android.util.image.ImageManager
 
 class BloggingPromptCardViewHolder(
     parent: ViewGroup,
-    private val uiHelpers: UiHelpers
+    private val uiHelpers: UiHelpers,
+    private val imageManager: ImageManager
 ) : CardViewHolder<MySiteBloggingPrompCardBinding>(
         parent.viewBinding(MySiteBloggingPrompCardBinding::inflate)
 ) {
     fun bind(card: BloggingPromptCardWithData) = with(binding) {
         uiHelpers.setTextOrHide(promptContent, card.prompt)
 
-        val numberOfAnswersLabel = numberOfAnswers.context.getString(
+        val numberOfAnswersLabel = promptContent.context.getString(
                 R.string.my_site_blogging_prompt_card_number_of_answers,
                 card.numberOfAnswers
         )
-        uiHelpers.setTextOrHide(numberOfAnswers, numberOfAnswersLabel)
+//        uiHelpers.setTextOrHide(numberOfAnswers, numberOfAnswersLabel)
         uiHelpers.updateVisibility(answerButton, !card.isAnswered)
 
         bloggingPromptCardMenu.setOnClickListener {
@@ -47,15 +58,46 @@ class BloggingPromptCardViewHolder(
             )
         }
         uiHelpers.updateVisibility(answeredPromptControls, card.isAnswered)
-    }
 
-    private fun MySiteBloggingPrompCardBinding.showCardMenu() {
-        val quickStartPopupMenu = PopupMenu(itemView.context, bloggingPromptCardMenu)
-        quickStartPopupMenu.setOnMenuItemClickListener {
-            return@setOnMenuItemClickListener true
-        }
-        quickStartPopupMenu.inflate(R.menu.blogging_prompt_card_menu)
-        MenuCompat.setGroupDividerEnabled(quickStartPopupMenu.menu, true)
-        quickStartPopupMenu.show()
+        val layoutManager = LinearLayoutManager(answeredUsersRecycler.context, LinearLayoutManager.HORIZONTAL, false)
+        answeredUsersRecycler.addItemDecoration(
+                PostLikerItemDecorator(
+                        RtlUtils.isRtl(answeredUsersRecycler.context),
+                        answeredUsersRecycler.context,
+                        FACE_ITEM_LEFT_OFFSET_DIMEN
+                )
+        )
+
+        answeredUsersRecycler.layoutManager = layoutManager
+
+        val snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(answeredUsersRecycler)
+
+        val adapter = ReaderPostLikersAdapter(
+                imageManager,
+                uiHelpers
+        )
+
+        adapter.loadData(
+                listOf(
+                        FaceItem(
+                                54279365,
+                                "https://0.gravatar.com/avatar/cec64efa352617c35743d8ed233ab410?s=96&d=identicon&r=G"
+                        ), BloggersLikingTextItem(UiStringText(numberOfAnswersLabel))
+                )
+        )
+
+        answeredUsersRecycler.adapter = adapter
     }
 }
+
+private fun MySiteBloggingPrompCardBinding.showCardMenu() {
+    val quickStartPopupMenu = PopupMenu(bloggingPromptCardMenu.context, bloggingPromptCardMenu)
+    quickStartPopupMenu.setOnMenuItemClickListener {
+        return@setOnMenuItemClickListener true
+    }
+    quickStartPopupMenu.inflate(R.menu.blogging_prompt_card_menu)
+    MenuCompat.setGroupDividerEnabled(quickStartPopupMenu.menu, true)
+    quickStartPopupMenu.show()
+}
+
