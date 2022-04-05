8eed6baf462484f931d1ad713c763a66249e8f64
khaykov <forsver@gmail.com>
2022-04-04 22:33:34 -0500

    Added a blogging prompt respondent to the main model.

--- WordPress/src/main/java/org/wordpress/android/models/bloggingprompts/BloggingPrompt.kt
+++ WordPress/src/main/java/org/wordpress/android/models/bloggingprompts/BloggingPrompt.kt
@@ -1,3 +1,8 @@
 package org.wordpress.android.models.bloggingprompts
 
-data class BloggingPrompt(val text: String, val numberOfAnswers: Int, val template: String)
+data class BloggingPrompt(
+    val text: String,
+    val numberOfAnswers: Int,
+    val template: String,
+    val respondents: List<BloggingPromptRespondent>
+)
