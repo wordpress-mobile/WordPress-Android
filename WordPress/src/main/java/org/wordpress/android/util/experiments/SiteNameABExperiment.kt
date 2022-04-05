89e857aaa2b8de17e1a5e838c62d5b0f3be54d8e
Matthew Kevins <mmkevins@yahoo.com>
2022-04-05 21:20:08 +1000

    Add site name ExPlat experiment

---
+++ WordPress/src/main/java/org/wordpress/android/util/experiments/SiteNameABExperiment.kt
@@ -0,0 +1,6 @@
+package org.wordpress.android.util.experiments
+
+import javax.inject.Inject
+
+class SiteNameABExperiment
+@Inject constructor(exPlat: ExPlat) : Experiment(name = "wpandroid_site_name_experiment", exPlat)
