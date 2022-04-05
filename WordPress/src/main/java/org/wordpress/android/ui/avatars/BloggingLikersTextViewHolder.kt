cbc31b57939b2fa29c039e171b68103006250e2a
khaykov <forsver@gmail.com>
2022-04-04 20:48:03 -0500

    Updated BloggingLikersTextViewHolder to use width of parent instead of whole display to calculate it's width.

--- WordPress/src/main/java/org/wordpress/android/ui/avatars/BloggingLikersTextViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/avatars/BloggingLikersTextViewHolder.kt
@@ -1,11 +1,12 @@
 package org.wordpress.android.ui.avatars
 
+import android.view.View
 import android.view.ViewGroup
-import org.wordpress.android.R
+import android.view.ViewParent
+import androidx.recyclerview.widget.RecyclerView
 import org.wordpress.android.databinding.BloggerLikersTextItemBinding
 import org.wordpress.android.ui.avatars.TrainOfFacesItem.BloggersLikingTextItem
 import org.wordpress.android.ui.utils.UiHelpers
-import org.wordpress.android.util.DisplayUtils
 import org.wordpress.android.util.extensions.viewBinding
 
 class BloggingLikersTextViewHolder(
@@ -16,17 +17,37 @@ class BloggingLikersTextViewHolder(
         val position = adapterPosition
 
         if (position >= 0) {
-            val displayWidth = DisplayUtils.getDisplayPixelWidth(itemView.context)
-            val paddingWidth = 2 * itemView.context.resources.getDimensionPixelSize(R.dimen.reader_detail_margin)
-            val avatarSize = itemView.context.resources.getDimensionPixelSize(FACE_ITEM_AVATAR_SIZE_DIMEN)
-            val leftOffest = itemView.context.resources.getDimensionPixelSize(FACE_ITEM_LEFT_OFFSET_DIMEN)
-            val facesWidth = position * avatarSize - (position - 1).coerceAtLeast(0) * leftOffest
+            itemView.post {
+                val parent = findParentRecyclerView(itemView)
 
-            itemView.layoutParams.width = displayWidth - paddingWidth - facesWidth
+                parent?.let {
+                    val parentWidth = it.measuredWidth
+                    val avatarSize = itemView.context.resources.getDimensionPixelSize(FACE_ITEM_AVATAR_SIZE_DIMEN)
+                    val leftOffest = itemView.context.resources.getDimensionPixelSize(FACE_ITEM_LEFT_OFFSET_DIMEN)
+                    val facesWidth = position * avatarSize - (position - 1).coerceAtLeast(0) * leftOffest
+
+                    itemView.layoutParams.width = parentWidth - facesWidth
+                }
+            }
         }
 
         numBloggers.text = with(bloggersTextItem) {
             uiHelpers.getTextOfUiString(itemView.context, text)
+        }
+    }
+
+    // we do assume that the adapter is only attached to one RecyclerView at time
+    private fun findParentRecyclerView(v: View): View? {
+        return when (val parent: ViewParent = v.parent) {
+            is RecyclerView -> {
+                parent
+            }
+            is View -> {
+                findParentRecyclerView(parent as View)
+            }
+            else -> {
+                null
+            }
         }
     }
 }
