8f361d4672f53abcd96c4f02d82c23d2b59c7d28
antonis <antonis.lilis@automattic.com>
2022-04-06 18:59:57 +0300

    Changed experiment name

--- WordPress/src/main/java/org/wordpress/android/util/experiments/SiteNameABExperiment.kt
+++ WordPress/src/main/java/org/wordpress/android/util/experiments/SiteNameABExperiment.kt
@@ -3,4 +3,4 @@ package org.wordpress.android.util.experiments
 import javax.inject.Inject
 
 class SiteNameABExperiment
-@Inject constructor(exPlat: ExPlat) : Experiment(name = "wpandroid_site_name_experiment", exPlat)
+@Inject constructor(exPlat: ExPlat) : Experiment(name = "wpandroid_site_name_v1", exPlat)
