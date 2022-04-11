caed701962222ed2902dc115894b95b1a40e74d2
Sowmia <sowmiasundar@gmail.com>
2022-04-11 13:09:47 +0530

    Adds suspend function to update account settings

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/AccountSettingsRepository.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/AccountSettingsRepository.kt
@@ -1,14 +1,22 @@
 package org.wordpress.android.ui.prefs.accountsettings
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+import org.greenrobot.eventbus.Subscribe
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.AccountActionBuilder
 import org.wordpress.android.fluxc.model.AccountModel
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.store.AccountStore
+import org.wordpress.android.fluxc.store.AccountStore.OnAccountChanged
+import org.wordpress.android.fluxc.store.AccountStore.PushAccountSettingsPayload
 import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.modules.IO_THREAD
 import javax.inject.Inject
 import javax.inject.Named
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
 
 class AccountSettingsRepository @Inject constructor(
     private var dispatcher: Dispatcher,
@@ -20,6 +28,8 @@ class AccountSettingsRepository @Inject constructor(
         dispatcher.register(this)
     }
 
+    private var continuation: Continuation<OnAccountChanged>? = null
+
     val account: AccountModel
         get() = accountStore.account
 
@@ -27,6 +37,22 @@ class AccountSettingsRepository @Inject constructor(
 
     fun getSite(siteRemoteId: Long): SiteModel? {
         return siteStore.getSiteBySiteId(siteRemoteId)
+    }
+
+    private suspend fun updateAccountSettings(addPayload: (PushAccountSettingsPayload) -> Unit): OnAccountChanged = withContext(ioDispatcher) {
+        suspendCancellableCoroutine {
+            continuation = it
+            val payload = PushAccountSettingsPayload()
+            payload.params = HashMap()
+            addPayload(payload)
+            dispatcher.dispatch(AccountActionBuilder.newPushSettingsAction(payload))
+        }
+    }
+
+    @Subscribe
+    fun onAccountChanged(event: OnAccountChanged) {
+        continuation?.resume(event)
+        continuation = null
     }
 
     fun onCleanUp() {
