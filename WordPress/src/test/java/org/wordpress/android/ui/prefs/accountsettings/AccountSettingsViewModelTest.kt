f47fcc6b829c10282d6d43be0cd10022ea7b6822
Sowmia <sowmiasundar@gmail.com>
2022-04-11 21:55:31 +0530

    Creates AccountSettingsViewModelTest

--- WordPress/src/test/java/org/wordpress/android/ui/prefs/accountsettings/AccountSettingsViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/prefs/accountsettings/AccountSettingsViewModelTest.kt
@@ -1,4 +1,19 @@
 package org.wordpress.android.ui.prefs.accountsettings
 
-class AccountSettingsViewModelTest {
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.mockito.Mock
+import org.wordpress.android.BaseUnitTest
+import org.wordpress.android.fluxc.model.AccountModel
+import org.wordpress.android.util.NetworkUtilsWrapper
+import org.wordpress.android.viewmodel.ResourceProvider
+
+@InternalCoroutinesApi
+class AccountSettingsViewModelTest : BaseUnitTest(){
+
+    private lateinit var viewModel: AccountSettingsViewModel
+    @Mock private lateinit var resourceProvider: ResourceProvider
+    @Mock private lateinit var networkUtilsWrapper: NetworkUtilsWrapper
+    @Mock private lateinit var accountsSettingsRepository:AccountSettingsRepository
+    @Mock private lateinit var account: AccountModel
+
 }
