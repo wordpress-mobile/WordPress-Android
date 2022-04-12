623f55c8ebc6c6eb667cbf6c3d2731163f750d4e
antonis <antonis.lilis@automattic.com>
2022-04-12 17:37:04 +0300

    Validates that the provided site title contains latin alphanumerics before passing as a blog_name

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteRestClientTest.kt
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteRestClientTest.kt
@@ -297,6 +297,66 @@ class SiteRestClientTest {
     }
 
     @Test
+    fun `creates new site without a site name and an invalid site title`() = test {
+        val data = NewSiteResponse()
+        val blogDetails = BlogDetails()
+        blogDetails.blogid = siteId.toString()
+        data.blog_details = blogDetails
+        val appId = "app_id"
+        whenever(appSecrets.appId).thenReturn(appId)
+        val appSecret = "app_secret"
+        whenever(appSecrets.appSecret).thenReturn(appSecret)
+
+        initNewSiteResponse(data)
+
+        val dryRun = false
+        val username = "username"
+        val siteName = null
+        val siteTitle = "..."
+        val language = "CZ"
+        val visibility = PUBLIC
+        val segmentId = 123L
+        val siteDesign = "design"
+        val timeZoneId = "Europe/London"
+
+        val result = restClient.newSite(
+                username,
+                siteName,
+                siteTitle,
+                language,
+                timeZoneId,
+                visibility,
+                segmentId,
+                siteDesign,
+                dryRun
+        )
+
+        assertThat(result.newSiteRemoteId).isEqualTo(siteId)
+        assertThat(result.dryRun).isEqualTo(dryRun)
+
+        assertThat(urlCaptor.lastValue)
+                .isEqualTo("https://public-api.wordpress.com/rest/v1.1/sites/new/")
+        assertThat(bodyCaptor.lastValue).isEqualTo(
+                mapOf(
+                        "blog_name" to username,
+                        "blog_title" to siteTitle,
+                        "lang_id" to language,
+                        "public" to "1",
+                        "validate" to "0",
+                        "find_available_url" to "1",
+                        "site_creation_flow" to "with-design-picker",
+                        "client_id" to appId,
+                        "client_secret" to appSecret,
+                        "options" to mapOf<String, Any>(
+                                "site_segment" to segmentId,
+                                "template" to siteDesign,
+                                "timezone_string" to timeZoneId
+                        )
+                )
+        )
+    }
+
+    @Test
     fun `creates new site without a site name and site title`() = test {
         val data = NewSiteResponse()
         val blogDetails = BlogDetails()
