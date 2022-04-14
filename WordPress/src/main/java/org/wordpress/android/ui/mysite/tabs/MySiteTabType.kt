2387f2506d8bb52924325b062eef86a3b499267f
ashiagr <agr.ash@gmail.com>
2022-04-14 15:27:46 +0530

    Add my site tab label
    My site tab labels now match the actual labels: home, menu
    Intermediary tracking label used when creating a new tab fragment can now be replaced with this label.

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/tabs/MySiteTabType.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/tabs/MySiteTabType.kt
@@ -3,19 +3,19 @@ package org.wordpress.android.ui.mysite.tabs
 import androidx.annotation.StringRes
 import org.wordpress.android.R
 
-enum class MySiteTabType(val trackingLabel: String, @StringRes val stringResId: Int) {
-    ALL("all", R.string.my_site_all_tab_title),
-    DASHBOARD("dashboard", R.string.my_site_dashboard_tab_title),
-    SITE_MENU("site_menu", R.string.my_site_menu_tab_title);
+enum class MySiteTabType(val label: String, @StringRes val stringResId: Int, val trackingLabel: String) {
+    ALL("all", R.string.my_site_all_tab_title, "nonexistent"),
+    DASHBOARD("home", R.string.my_site_dashboard_tab_title, "dashboard"),
+    SITE_MENU("menu", R.string.my_site_menu_tab_title, "site_menu");
 
-    override fun toString() = trackingLabel
+    override fun toString() = label
 
     companion object {
         @JvmStatic
         fun fromString(label: String) = when {
-            ALL.trackingLabel == label -> ALL
-            DASHBOARD.trackingLabel == label -> DASHBOARD
-            SITE_MENU.trackingLabel == label -> SITE_MENU
+            ALL.label == label -> ALL
+            DASHBOARD.label == label -> DASHBOARD
+            SITE_MENU.label == label -> SITE_MENU
             else -> SITE_MENU
         }
     }
