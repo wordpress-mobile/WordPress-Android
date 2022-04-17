7a75b9e45447387175facfc3d3190a2f66041132
Irfan Omur <irfan.omur@automattic.com>
2022-04-18 01:50:32 +0300

    Use custom hilt test application for UI tests

--- WordPress/src/androidTest/java/org/wordpress/android/WordPressTest.kt
+++ WordPress/src/androidTest/java/org/wordpress/android/WordPressTest.kt
@@ -1,4 +1,30 @@
 package org.wordpress.android
 
-class WordPressTest : WordPressApp() {
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import dagger.hilt.EntryPoint
+import dagger.hilt.EntryPoints
+import dagger.hilt.InstallIn
+import dagger.hilt.android.testing.CustomTestApplication
+import dagger.hilt.components.SingletonComponent
+
+@CustomTestApplication(BaseWordPressTest::class)
+interface WordPressTest
+
+open class BaseWordPressTest : WordPress(), HasAndroidInjector {
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface AndroidInjectorEntryPoint {
+        fun injector(): DispatchingAndroidInjector<Any>
+    }
+
+    lateinit var initializer: AppInitializer
+
+    override fun androidInjector(): AndroidInjector<Any> = EntryPoints.get(
+            applicationContext,
+            AndroidInjectorEntryPoint::class.java
+    ).injector()
+
+    override fun initializer() = initializer
 }
