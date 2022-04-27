25dad5c025e5cd5d5468be62a0036a14ed7917bd
Ravi <ravi.program@gmail.com>
2022-04-27 22:12:35 +1000

    Add percent views sub title

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/viewholders/QuickScanItemViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/viewholders/QuickScanItemViewHolder.kt
@@ -15,23 +15,27 @@ class QuickScanItemViewHolder(parent: ViewGroup) : BlockListItemViewHolder(
     private val startValueContainer = itemView.findViewById<LinearLayout>(R.id.start_value_container)
     private val startLabel = itemView.findViewById<TextView>(R.id.start_label)
     private val startValue = itemView.findViewById<TextView>(R.id.start_value)
+    private val startText = itemView.findViewById<TextView>(R.id.start_text)
     private val endValueContainer = itemView.findViewById<LinearLayout>(R.id.end_value_container)
     private val endLabel = itemView.findViewById<TextView>(R.id.end_label)
     private val endValue = itemView.findViewById<TextView>(R.id.end_value)
+    private val endText = itemView.findViewById<TextView>(R.id.end_text)
 
     fun bind(item: QuickScanItem) {
-        bindColumn(item.startColumn, startLabel, startValue, startValueContainer)
-        bindColumn(item.endColumn, endLabel, endValue, endValueContainer)
+        bindColumn(item.startColumn, startLabel, startValue, startText, startValueContainer)
+        bindColumn(item.endColumn, endLabel, endValue, endText, endValueContainer)
     }
 
     private fun bindColumn(
         column: Column,
         label: TextView,
         value: TextView,
+        text: TextView,
         container: LinearLayout
     ) {
         label.setText(column.label)
         value.text = column.value
+        text.text = column.tooltip
         column.tooltip?.let {
             TooltipCompat.setTooltipText(container, column.tooltip)
             container.setOnClickListener {
