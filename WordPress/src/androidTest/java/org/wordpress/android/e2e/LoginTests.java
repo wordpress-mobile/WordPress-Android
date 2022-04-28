56b5ff5ebd8dbf28ac3f84b02e20b7c700f33acd
Ovi Trif <octrif@gmail.com>
2022-04-28 15:10:57 +0200

    Unignore the loginWithPasswordlessAccount and loginWithMagicLink UI tests

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/LoginTests.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/LoginTests.java
@@ -2,7 +2,6 @@ package org.wordpress.android.e2e;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.wordpress.android.e2e.flows.LoginFlow;
 import org.wordpress.android.support.BaseTest;
@@ -32,7 +31,6 @@ public class LoginTests extends BaseTest {
                        .confirmLogin(false);
     }
 
-    @Ignore("Ignored temporarily. This sometimes fail on CI while running with whole test suite.")
     @Test
     public void loginWithPasswordlessAccount() {
         new LoginFlow().chooseContinueWithWpCom()
@@ -50,7 +48,6 @@ public class LoginTests extends BaseTest {
                        .confirmLogin(false);
     }
 
-    @Ignore("Ignored temporarily. This sometimes fail on CI while running with whole test suite.")
     @Test
     public void loginWithMagicLink() {
         new LoginFlow().chooseContinueWithWpCom()
