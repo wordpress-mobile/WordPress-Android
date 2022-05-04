25757e707972b917126849c06d8735fd7fbbe46a
Sowmia <sowmiasundar@gmail.com>
2022-05-04 11:38:39 +0530

    Provides GetSitesUseCase and GetAccountsUseCase dependency

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/module/AccountSettingsModule.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/module/AccountSettingsModule.kt
@@ -3,23 +3,18 @@ package org.wordpress.android.ui.prefs.accountsettings.module
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
-import org.wordpress.android.fluxc.network.utils.StatsGranularity.YEARS
+import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.fluxc.store.AccountStore.OnAccountChanged
-import org.wordpress.android.fluxc.store.StatsStore
-import org.wordpress.android.modules.BG_THREAD
+import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.modules.IO_THREAD
-import org.wordpress.android.modules.UI_THREAD
 import org.wordpress.android.ui.prefs.accountsettings.usecase.FetchAccountSettingsInteractor
 import org.wordpress.android.ui.prefs.accountsettings.usecase.FetchAccountSettingsUseCase
+import org.wordpress.android.ui.prefs.accountsettings.usecase.GetAccountInteractor
+import org.wordpress.android.ui.prefs.accountsettings.usecase.GetAccountUseCase
+import org.wordpress.android.ui.prefs.accountsettings.usecase.GetSitesInteractor
+import org.wordpress.android.ui.prefs.accountsettings.usecase.GetSitesUseCase
 import org.wordpress.android.ui.prefs.accountsettings.usecase.PushAccountSettingsInteractor
 import org.wordpress.android.ui.prefs.accountsettings.usecase.PushAccountSettingsUseCase
-import org.wordpress.android.ui.stats.refresh.GRANULAR_USE_CASE_FACTORIES
-import org.wordpress.android.ui.stats.refresh.YEAR_STATS_USE_CASE
-import org.wordpress.android.ui.stats.refresh.lists.BaseListUseCase
-import org.wordpress.android.ui.stats.refresh.lists.UiModelMapper
-import org.wordpress.android.ui.stats.refresh.lists.sections.BaseStatsUseCase.UseCaseMode.BLOCK
-import org.wordpress.android.ui.stats.refresh.lists.sections.granular.GranularUseCaseFactory
-import org.wordpress.android.ui.stats.refresh.utils.StatsSiteProvider
 import org.wordpress.android.ui.utils.ContinuationWrapper
 import org.wordpress.android.ui.utils.ContinuationWrapperWithConcurrency
 import org.wordpress.android.util.EventBusWrapper
@@ -53,6 +48,28 @@ class AccountSettingsModule {
                 eventBusWrapper,
                 continuationWrapper,
                 ioDispatcher
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetSitesUseCase(
+        siteStore: SiteStore,
+        @Named(IO_THREAD) ioDispatcher: CoroutineDispatcher
+    ): GetSitesInteractor {
+        return GetSitesUseCase(
+                ioDispatcher,
+                siteStore
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAccountsUseCase(
+        accountStore: AccountStore
+    ): GetAccountInteractor {
+        return GetAccountUseCase(
+                accountStore
         )
     }
 }
