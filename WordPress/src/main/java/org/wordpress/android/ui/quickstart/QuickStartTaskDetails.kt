54f4799a74abd07185eef795c566ec3f1e9a1a3d
ashiagr <agr.ash@gmail.com>
2022-05-06 13:00:06 +0530

    Convert from java to kt

--- WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartTaskDetails.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartTaskDetails.kt
@@ -1,116 +1,95 @@
-package org.wordpress.android.ui.quickstart;
+package org.wordpress.android.ui.quickstart
 
-import org.wordpress.android.R;
-import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartNewSiteTask;
-import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartTask;
+import org.wordpress.android.R.drawable
+import org.wordpress.android.R.string
+import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartNewSiteTask
+import org.wordpress.android.fluxc.store.QuickStartStore.QuickStartTask
 
 /**
  * Static data that represents user facing description of quick start tasks.
- **/
-public enum QuickStartTaskDetails {
+ */
+enum class QuickStartTaskDetails(
+    val task: QuickStartNewSiteTask,
+    val titleResId: Int,
+    val subtitleResId: Int,
+    val iconResId: Int
+) {
     CREATE_SITE_TUTORIAL(
             QuickStartNewSiteTask.CREATE_SITE,
-            R.string.quick_start_list_create_site_title,
-            R.string.quick_start_list_create_site_subtitle,
-            R.drawable.ic_gridicons_site_white_24dp
+            string.quick_start_list_create_site_title,
+            string.quick_start_list_create_site_subtitle,
+            drawable.ic_gridicons_site_white_24dp
     ),
     UPDATE_SITE_TITLE(
             QuickStartNewSiteTask.UPDATE_SITE_TITLE,
-            R.string.quick_start_list_update_site_title_title,
-            R.string.quick_start_list_update_site_title_subtitle,
-            R.drawable.ic_pencil_white_24dp
+            string.quick_start_list_update_site_title_title,
+            string.quick_start_list_update_site_title_subtitle,
+            drawable.ic_pencil_white_24dp
     ),
     VIEW_SITE_TUTORIAL(
             QuickStartNewSiteTask.VIEW_SITE,
-            R.string.quick_start_list_view_site_title,
-            R.string.quick_start_list_view_site_subtitle,
-            R.drawable.ic_external_white_24dp
+            string.quick_start_list_view_site_title,
+            string.quick_start_list_view_site_subtitle,
+            drawable.ic_external_white_24dp
     ),
     SHARE_SITE_TUTORIAL(
             QuickStartNewSiteTask.ENABLE_POST_SHARING,
-            R.string.quick_start_list_enable_sharing_title,
-            R.string.quick_start_list_enable_sharing_subtitle,
-            R.drawable.ic_share_white_24dp
+            string.quick_start_list_enable_sharing_title,
+            string.quick_start_list_enable_sharing_subtitle,
+            drawable.ic_share_white_24dp
     ),
     PUBLISH_POST_TUTORIAL(
             QuickStartNewSiteTask.PUBLISH_POST,
-            R.string.quick_start_list_publish_post_title,
-            R.string.quick_start_list_publish_post_subtitle,
-            R.drawable.ic_posts_white_24dp
+            string.quick_start_list_publish_post_title,
+            string.quick_start_list_publish_post_subtitle,
+            drawable.ic_posts_white_24dp
     ),
     FOLLOW_SITES_TUTORIAL(
             QuickStartNewSiteTask.FOLLOW_SITE,
-            R.string.quick_start_list_follow_site_title,
-            R.string.quick_start_list_follow_site_subtitle,
-            R.drawable.ic_reader_white_24dp
+            string.quick_start_list_follow_site_title,
+            string.quick_start_list_follow_site_subtitle,
+            drawable.ic_reader_white_24dp
     ),
     UPLOAD_SITE_ICON(
             QuickStartNewSiteTask.UPLOAD_SITE_ICON,
-            R.string.quick_start_list_upload_icon_title,
-            R.string.quick_start_list_upload_icon_subtitle,
-            R.drawable.ic_globe_white_24dp
+            string.quick_start_list_upload_icon_title,
+            string.quick_start_list_upload_icon_subtitle,
+            drawable.ic_globe_white_24dp
     ),
     CHECK_STATS(
             QuickStartNewSiteTask.CHECK_STATS,
-            R.string.quick_start_list_check_stats_title,
-            R.string.quick_start_list_check_stats_subtitle,
-            R.drawable.ic_stats_alt_white_24dp
+            string.quick_start_list_check_stats_title,
+            string.quick_start_list_check_stats_subtitle,
+            drawable.ic_stats_alt_white_24dp
     ),
     EXPLORE_PLANS(
             QuickStartNewSiteTask.EXPLORE_PLANS,
-            R.string.quick_start_list_explore_plans_title,
-            R.string.quick_start_list_explore_plans_subtitle,
-            R.drawable.ic_plans_white_24dp
+            string.quick_start_list_explore_plans_title,
+            string.quick_start_list_explore_plans_subtitle,
+            drawable.ic_plans_white_24dp
     ),
     EDIT_HOMEPAGE(
             QuickStartNewSiteTask.EDIT_HOMEPAGE,
-            R.string.quick_start_list_edit_homepage_title,
-            R.string.quick_start_list_edit_homepage_subtitle,
-            R.drawable.ic_homepage_16dp
+            string.quick_start_list_edit_homepage_title,
+            string.quick_start_list_edit_homepage_subtitle,
+            drawable.ic_homepage_16dp
     ),
     REVIEW_PAGES(
             QuickStartNewSiteTask.REVIEW_PAGES,
-            R.string.quick_start_list_review_pages_title,
-            R.string.quick_start_list_review_pages_subtitle,
-            R.drawable.ic_pages_white_24dp
+            string.quick_start_list_review_pages_title,
+            string.quick_start_list_review_pages_subtitle,
+            drawable.ic_pages_white_24dp
     );
 
-    QuickStartTaskDetails(QuickStartTask task, int titleResId, int subtitleResId, int iconResId) {
-        mTask = task;
-        mTitleResId = titleResId;
-        mSubtitleResId = subtitleResId;
-        mIconResId = iconResId;
-    }
-
-    public static final String KEY = "quick_start_task_details";
-
-    private final QuickStartTask mTask;
-    private int mTitleResId;
-    private int mSubtitleResId;
-    private int mIconResId;
-
-    public QuickStartTask getTask() {
-        return mTask;
-    }
-
-    public int getTitleResId() {
-        return mTitleResId;
-    }
-
-    public int getSubtitleResId() {
-        return mSubtitleResId;
-    }
-
-    public int getIconResId() {
-        return mIconResId;
-    }
-
-    public static QuickStartTaskDetails getDetailsForTask(QuickStartTask task) {
-        for (QuickStartTaskDetails quickStartTaskDetails : QuickStartTaskDetails.values()) {
-            if (quickStartTaskDetails.mTask == task) {
-                return quickStartTaskDetails;
+    companion object {
+        const val KEY = "quick_start_task_details"
+        @JvmStatic fun getDetailsForTask(task: QuickStartTask): QuickStartTaskDetails? {
+            for (quickStartTaskDetails in values()) {
+                if (quickStartTaskDetails.task === task) {
+                    return quickStartTaskDetails
+                }
             }
+            return null
         }
-        return null;
     }
 }
