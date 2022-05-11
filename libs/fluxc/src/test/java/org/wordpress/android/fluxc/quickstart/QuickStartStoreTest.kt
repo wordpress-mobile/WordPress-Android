f5876681b828ba06065ede743fac3424be6e1149
ashiagr <agr.ash@gmail.com>
2022-05-11 16:15:52 +0530

    Cleanup: Remove unused functions related to task shown

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/quickstart/QuickStartStoreTest.kt
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/quickstart/QuickStartStoreTest.kt
@@ -78,39 +78,4 @@ class QuickStartStoreTest {
         assertEquals(PUBLISH_POST, uncompletedGrowTasks[1])
         assertEquals(CHECK_STATS, uncompletedGrowTasks[2])
     }
-
-    @Test
-    fun orderOfShownTasks() = test {
-        // marking tasks as shown in random order
-        quickStartStore.setShownTask(testLocalSiteId, REVIEW_PAGES, true)
-        quickStartStore.setShownTask(testLocalSiteId, UPLOAD_SITE_ICON, true)
-        quickStartStore.setShownTask(testLocalSiteId, CHECK_STATS, true)
-        quickStartStore.setShownTask(testLocalSiteId, ENABLE_POST_SHARING, true)
-        quickStartStore.setShownTask(testLocalSiteId, PUBLISH_POST, true)
-
-        // making sure shown tasks are retrieved in a correct order
-        val shownCustomizeTasks = quickStartStore.getShownTasksByType(testLocalSiteId, CUSTOMIZE)
-        assertEquals(2, shownCustomizeTasks.size)
-        assertEquals(UPLOAD_SITE_ICON, shownCustomizeTasks[0])
-        assertEquals(REVIEW_PAGES, shownCustomizeTasks[1])
-
-        val shownGrowTasks = quickStartStore.getShownTasksByType(testLocalSiteId, GROW)
-        assertEquals(3, shownGrowTasks.size)
-        assertEquals(ENABLE_POST_SHARING, shownGrowTasks[0])
-        assertEquals(PUBLISH_POST, shownGrowTasks[1])
-        assertEquals(CHECK_STATS, shownGrowTasks[2])
-
-        // making sure unshown tasks are retrieved in a correct order
-        val unshownCustomizeTasks = quickStartStore.getUnshownTasksByType(testLocalSiteId, CUSTOMIZE)
-        assertEquals(4, unshownCustomizeTasks.size)
-        assertEquals(CREATE_SITE, unshownCustomizeTasks[0])
-        assertEquals(UPDATE_SITE_TITLE, unshownCustomizeTasks[1])
-        assertEquals(EDIT_HOMEPAGE, unshownCustomizeTasks[2])
-        assertEquals(VIEW_SITE, unshownCustomizeTasks[3])
-
-        val unshownGrowTasks = quickStartStore.getUnshownTasksByType(testLocalSiteId, GROW)
-        assertEquals(2, unshownGrowTasks.size)
-        assertEquals(FOLLOW_SITE, unshownGrowTasks[0])
-        assertEquals(EXPLORE_PLANS, unshownGrowTasks[1])
-    }
 }
