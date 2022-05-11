3a2c22ecee383dd21a15498dfe20b0c72462878d
khaykov <forsver@gmail.com>
2022-05-11 12:01:51 -0500

    Updated tests.

--- WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/bloggingprompt/BloggingPromptCardBuilderTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/bloggingprompt/BloggingPromptCardBuilderTest.kt
@@ -8,8 +8,7 @@ import org.mockito.junit.MockitoJUnitRunner
 import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.R.attr
 import org.wordpress.android.R.plurals
-import org.wordpress.android.models.bloggingprompts.BloggingPrompt
-import org.wordpress.android.models.bloggingprompts.BloggingPromptRespondent
+import org.wordpress.android.fluxc.model.bloggingprompts.BloggingPromptModel
 import org.wordpress.android.ui.avatars.TrainOfAvatarsItem.AvatarItem
 import org.wordpress.android.ui.avatars.TrainOfAvatarsItem.TrailingLabelTextItem
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BloggingPromptCard.BloggingPromptCardWithData
@@ -17,62 +16,47 @@ import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.BloggingPr
 import org.wordpress.android.ui.mysite.cards.dashboard.bloggingprompts.BloggingPromptCardBuilder
 import org.wordpress.android.ui.utils.UiString.UiStringPluralRes
 import org.wordpress.android.ui.utils.UiString.UiStringText
+import java.util.Date
 
 private const val PROMPT_TITLE = "Test Prompt"
 private const val NUMBER_OF_RESPONDENTS = 5
 
 private val RESPONDENTS_IN_CARD = listOf(
-        AvatarItem(
-                1,
-                "http://avatar1.url"
-        ), AvatarItem(
-        2,
-        "http://avatar2.url"
-), AvatarItem(
-        3,
-        "http://avatar3.url"
-), TrailingLabelTextItem(
-        UiStringPluralRes(
-                plurals.my_site_blogging_prompt_card_number_of_answers,
-                NUMBER_OF_RESPONDENTS
-        ), attr.colorPrimary
-)
+        AvatarItem("http://avatar1.url"),
+        AvatarItem("http://avatar2.url"),
+        AvatarItem("http://avatar3.url"),
+        TrailingLabelTextItem(
+                UiStringPluralRes(
+                        plurals.my_site_blogging_prompt_card_number_of_answers,
+                        NUMBER_OF_RESPONDENTS
+                ), attr.colorPrimary
+        )
 )
 
 private val RESPONDENTS = listOf(
-        BloggingPromptRespondent(
-                1,
-                "http://avatar1.url"
-        ),
-        BloggingPromptRespondent(
-                2,
-                "http://avatar2.url"
-        ),
-        BloggingPromptRespondent(
-                3,
-                "http://avatar3.url"
-        ),
-        BloggingPromptRespondent(
-                4,
-                "http://avatar4.url"
-        ),
-        BloggingPromptRespondent(
-                5,
-                "http://avatar5.url"
-        )
+        "http://avatar1.url",
+        "http://avatar2.url",
+        "http://avatar3.url",
+        "http://avatar4.url",
+        "http://avatar5.url"
 )
 
 /* ktlint-disable max-line-length */
 @RunWith(MockitoJUnitRunner::class)
 class BloggingPromptCardBuilderTest : BaseUnitTest() {
     private lateinit var builder: BloggingPromptCardBuilder
-    private val bloggingPrompt = BloggingPrompt(
+    private val bloggingPrompt = BloggingPromptModel(
             id = 1234,
             text = PROMPT_TITLE,
+            title = "",
             content = "<!-- wp:pullquote -->\n" +
                     "<figure class=\"wp-block-pullquote\"><blockquote><p>You have 15 minutes to address the whole world live (on television or radio — choose your format). What would you say?</p><cite>(courtesy of plinky.com)</cite></blockquote></figure>\n" +
                     "<!-- /wp:pullquote -->",
-            respondents = RESPONDENTS
+            date = Date(),
+            isAnswered = false,
+            attribution = "",
+            respondentsCount = 5,
+            respondentsAvatarUrls = RESPONDENTS
     )
 
     @Before
@@ -101,19 +85,20 @@ class BloggingPromptCardBuilderTest : BaseUnitTest() {
         assertThat(statCard).isNull()
     }
 
-    private fun buildBloggingPromptCard(bloggingPrompt: BloggingPrompt?) = builder.build(
+    private fun buildBloggingPromptCard(bloggingPrompt: BloggingPromptModel?) = builder.build(
             BloggingPromptCardBuilderParams(bloggingPrompt, onShareClick, onAnswerClick)
     )
 
     private val onShareClick: (message: String) -> Unit = { }
 
-    private val onAnswerClick: () -> Unit = { }
+    private val onAnswerClick: (promptId: Int) -> Unit = { }
 
     private val bloggingPromptCard = BloggingPromptCardWithData(
             prompt = UiStringText(PROMPT_TITLE),
             respondents = RESPONDENTS_IN_CARD,
             numberOfAnswers = NUMBER_OF_RESPONDENTS,
             false,
+            promptId = 123,
             onShareClick = onShareClick,
             onAnswerClick = onAnswerClick
     )
