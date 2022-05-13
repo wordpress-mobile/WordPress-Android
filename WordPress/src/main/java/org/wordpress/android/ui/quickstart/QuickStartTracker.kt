3843afa66e2179c8b3d33031f5d3a3d5e3419230
ashiagr <agr.ash@gmail.com>
2022-05-13 13:51:03 +0530

    Introduce quick start tracker

---
+++ WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartTracker.kt
@@ -0,0 +1,22 @@
+package org.wordpress.android.ui.quickstart
+
+import org.wordpress.android.analytics.AnalyticsTracker.Stat
+import org.wordpress.android.ui.mysite.cards.quickstart.QuickStartRepository
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
+import javax.inject.Inject
+
+class QuickStartTracker @Inject constructor(
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper,
+    private val quickStartRepository: QuickStartRepository
+) {
+    fun track(stat: Stat, properties: Map<String, Any?>? = null) {
+        val props = HashMap<String, Any?>()
+        properties?.let { props.putAll(it) }
+        props[SITE_TYPE] = quickStartRepository.quickStartType.label
+        analyticsTrackerWrapper.track(stat, props)
+    }
+
+    companion object {
+        private const val SITE_TYPE = "site_type"
+    }
+}
