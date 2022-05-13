4806496f4e83c16f11a1c562dc4f1ccc5973ebd5
Ajesh R Pai <ajeshr2k1@gmail.com>
2022-05-13 10:55:37 +0530

    Adds: Quick start Media Notice Details

--- WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartMySitePrompts.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/quickstart/QuickStartMySitePrompts.kt
@@ -86,6 +86,13 @@ enum class QuickStartMySitePrompts constructor(
             R.id.quick_action_pages_button,
             R.string.quick_start_dialog_edit_homepage_message_short,
             R.drawable.ic_pages_white_24dp
+    ),
+    UPLOAD_MEDIA(
+            QuickStartStore.QUICK_START_UPLOAD_MEDIA_LABEL,
+            -1,
+            R.id.quick_action_media_button,
+            R.string.quick_start_dialog_upload_media_message,
+            R.drawable.ic_media_white_24dp
     );
 
     companion object {
