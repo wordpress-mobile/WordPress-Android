79fb6cf04c45ec89717ea0aa279c574ad924327e
Oguz Kocer <oguz.kocer@automattic.com>
2022-05-24 13:59:44 -0400

    Update navComponentVersion to 2.4.2

--- libs/image-editor/ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/preview/PreviewImageFragment.kt
+++ libs/image-editor/ImageEditor/src/main/kotlin/org/wordpress/android/imageeditor/preview/PreviewImageFragment.kt
@@ -271,14 +271,16 @@ class PreviewImageFragment : Fragment(R.layout.preview_image_fragment) {
         // TODO: Temporarily added if check to fix this occasional crash
         // https://stackoverflow.com/q/51060762/193545
         if (navController.currentDestination?.id == R.id.preview_dest) {
-            navController.navigate(
-                    PreviewImageFragmentDirections.actionPreviewFragmentToCropFragment(
-                            inputFilePath,
-                            outputFileExtension,
-                            shouldReturnToPreviewScreen
-                    ),
-                    navOptions
-            )
+            outputFileExtension?.let {
+                navController.navigate(
+                        PreviewImageFragmentDirections.actionPreviewFragmentToCropFragment(
+                                inputFilePath,
+                                outputFileExtension,
+                                shouldReturnToPreviewScreen
+                        ),
+                        navOptions
+                )
+            }
         }
     }
 
