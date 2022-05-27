868870fb0eced1ac0a413446dc3c0e8fb402ec1e
ashiagr <agr.ash@gmail.com>
2022-05-27 12:20:10 +0530

    Set quick start type on site switch

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteViewModel.kt
@@ -85,6 +85,7 @@ import org.wordpress.android.ui.posts.BasicDialogViewModel.DialogInteraction.Neg
 import org.wordpress.android.ui.posts.BasicDialogViewModel.DialogInteraction.Positive
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
 import org.wordpress.android.ui.quickstart.QuickStartTracker
+import org.wordpress.android.ui.quickstart.QuickStartType.NewSiteQuickStartType
 import org.wordpress.android.ui.sitecreation.misc.SiteCreationSource
 import org.wordpress.android.ui.utils.UiString
 import org.wordpress.android.ui.utils.UiString.UiStringRes
@@ -1063,6 +1064,15 @@ class MySiteViewModel @Inject constructor(
         selectDefaultTabIfNeeded()
     }
 
+    fun onSitePicked() {
+        selectedSiteRepository.getSelectedSite()?.let {
+            val siteLocalId = it.id.toLong()
+            val lastSelectedQuickStartType = appPrefsWrapper.getLastSelectedQuickStartTypeForSite(siteLocalId)
+            quickStartRepository.checkAndSetQuickStartType(lastSelectedQuickStartType == NewSiteQuickStartType)
+        }
+        mySiteSourceManager.refreshQuickStart()
+    }
+
     fun performFirstStepAfterSiteCreation(
         siteLocalId: Int,
         isSiteTitleTaskCompleted: Boolean,
@@ -1093,7 +1103,7 @@ class MySiteViewModel @Inject constructor(
         isSiteTitleTaskCompleted: Boolean,
         isNewSite: Boolean
     ) {
-        quickStartRepository.checkAndSetQuickStartType(isNewSite)
+        quickStartRepository.checkAndSetQuickStartType(isNewSite = isNewSite)
         if (quickStartDynamicCardsFeatureConfig.isEnabled()) {
             startQuickStart(siteLocalId, isSiteTitleTaskCompleted)
         } else {
