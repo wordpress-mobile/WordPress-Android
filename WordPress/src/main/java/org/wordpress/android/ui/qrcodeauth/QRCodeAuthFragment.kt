46e0887142622bc54d69b050f0f46c12a1761eac
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2022-06-03 16:31:51 -0400

    Temporarily show loading, content, and error views

--- WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.wordpress.android.R
 import org.wordpress.android.databinding.QrcodeauthFragmentBinding
 
@@ -19,8 +22,22 @@ class QRCodeAuthFragment : Fragment(R.layout.qrcodeauth_fragment) {
         }
     }
 
+    @Suppress("MagicNumber")
     private fun QrcodeauthFragmentBinding.initView() {
-        qrcodeAuthMessage.text = getString(R.string.qrcode_auth_flow)
         viewModel.start()
+        // Temporarily show each view
+        lifecycleScope.launch {
+            loadingLayout.container.visibility = View.VISIBLE
+            contentLayout.container.visibility = View.GONE
+            errorLayout.container.visibility = View.GONE
+            delay(2000L)
+            loadingLayout.container.visibility = View.GONE
+            contentLayout.container.visibility = View.VISIBLE
+            errorLayout.container.visibility = View.GONE
+            delay(2000L)
+            loadingLayout.container.visibility = View.GONE
+            contentLayout.container.visibility = View.GONE
+            errorLayout.container.visibility = View.VISIBLE
+        }
     }
 }
