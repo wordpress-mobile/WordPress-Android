5794e2a007f1fae5928b20f6f3e1bc39998f31dd
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2022-06-03 15:05:21 -0400

    Remove tool bar - the feature will not use a toolbar

--- WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthActivity.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.qrcodeauth
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import org.wordpress.android.databinding.QrcodeauthActivityBinding
@@ -13,19 +12,6 @@ class QRCodeAuthActivity : AppCompatActivity() {
 
         with(QrcodeauthActivityBinding.inflate(layoutInflater)) {
             setContentView(root)
-            setSupportActionBar(toolbarMain)
         }
-        supportActionBar?.let {
-            it.setHomeButtonEnabled(true)
-            it.setDisplayHomeAsUpEnabled(true)
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            onBackPressed()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
