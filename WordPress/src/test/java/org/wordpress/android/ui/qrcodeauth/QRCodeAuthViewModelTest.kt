59ed77ef2775adea21066c7294524eb6a96447cd
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2022-06-04 06:59:06 -0400

    Fix failing test by adding dependencies

--- WordPress/src/test/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthViewModelTest.kt
@@ -5,18 +5,23 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.wordpress.android.util.NetworkUtilsWrapper
 
 @RunWith(MockitoJUnitRunner::class)
 class QRCodeAuthViewModelTest {
     @Rule
     @JvmField val rule = InstantTaskExecutorRule()
+    @Mock lateinit var networkUtilsWrapper: NetworkUtilsWrapper
+    @Mock lateinit var validator: QRCodeAuthValidator
+    private val uiStateMapper = QRCodeAuthUiStateMapper()
 
     private lateinit var viewModel: QRCodeAuthViewModel
 
     @Before
     fun setUp() {
-        viewModel = QRCodeAuthViewModel()
+        viewModel = QRCodeAuthViewModel(uiStateMapper, networkUtilsWrapper, validator)
     }
 
     @Test
