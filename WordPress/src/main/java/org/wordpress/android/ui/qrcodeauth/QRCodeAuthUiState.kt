13d74b2c34d53e4467bcc7a501386565cbd635ab
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2022-06-03 22:32:46 -0400

    Use default title string if browser is null

--- WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthUiState.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthUiState.kt
@@ -118,7 +118,7 @@ sealed class QRCodeAuthUiState {
         ) : Content() {
             override val type = VALIDATED
             override val title: UiString = if (browser == null) {
-                UiStringResWithParams(R.string.qrcode_auth_flow_validated_title, listOf(UiStringText(location ?: " ")))
+                UiStringResWithParams(R.string.qrcode_auth_flow_validated_default_title, listOf(UiStringText(location ?: " ")))
             } else {
                 UiStringResWithParams(
                         R.string.qrcode_auth_flow_validated_title,
