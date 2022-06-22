f106f3c4f4037a65e7a76db3290f4231fa63e5e0
Ravi <ravi.program@gmail.com>
2022-06-22 18:17:34 +1000

    Update VisitsAndViewsStore.kt
    Add date for granular fetch and get

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/stats/time/VisitsAndViewsStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/stats/time/VisitsAndViewsStore.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.fluxc.store.stats.time
 
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.stats.LimitMode
+import org.wordpress.android.fluxc.model.stats.LimitMode.Top
 import org.wordpress.android.fluxc.model.stats.time.TimeStatsMapper
 import org.wordpress.android.fluxc.model.stats.time.VisitsAndViewsModel
 import org.wordpress.android.fluxc.network.rest.wpcom.stats.time.StatsUtils
@@ -16,6 +17,7 @@ import org.wordpress.android.fluxc.utils.AppLogWrapper
 import org.wordpress.android.fluxc.utils.CurrentTimeProvider
 import org.wordpress.android.fluxc.utils.SiteUtils
 import org.wordpress.android.util.AppLog.T.STATS
+import java.util.Date
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -78,6 +80,52 @@ class VisitsAndViewsStore
         }
     }
 
+    suspend fun fetchVisits(
+        site: SiteModel,
+        granularity: StatsGranularity,
+        limitMode: LimitMode.Top,
+        date: Date,
+        forced: Boolean = false
+    ) = coroutineEngine.withDefaultContext(STATS, this, "fetchVisits") {
+        val dateWithTimeZone = statsUtils.getFormattedDate(date, SiteUtils.getNormalizedTimezone(site.timezone))
+        logProgress(granularity, "Site timezone: ${site.timezone}")
+        try {
+            logProgress(granularity, "Current date: ${currentTimeProvider.currentDate()}")
+        } catch (e: AssertionError) {
+            // Workaround for a bug in Android that can cause crashes on Android 8.0 and 8.1
+            logProgress(granularity, "Cannot print current date because of AssertionError: $e")
+        }
+        logProgress(granularity, "Fetching for date with applied timezone: $dateWithTimeZone")
+        if (!forced && sqlUtils.hasFreshRequest(site, granularity, dateWithTimeZone, limitMode.limit)) {
+            logProgress(granularity, "Loading cached data")
+            return@withDefaultContext OnStatsFetched(
+                    getVisits(site, granularity, limitMode, dateWithTimeZone),
+                    cached = true
+            )
+        }
+        val payload = restClient.fetchVisits(site, granularity, dateWithTimeZone, limitMode.limit, forced)
+        return@withDefaultContext when {
+            payload.isError -> {
+                logProgress(granularity, "Error fetching data: ${payload.error}")
+                OnStatsFetched(payload.error)
+            }
+            payload.response != null -> {
+                logProgress(granularity, "Data fetched correctly")
+                sqlUtils.insert(site, payload.response, granularity, dateWithTimeZone, limitMode.limit)
+                val overviewResponse = timeStatsMapper.map(payload.response, limitMode)
+                if (overviewResponse.period.isBlank() || overviewResponse.dates.isEmpty()) {
+                    logProgress(granularity, "Invalid response")
+                    OnStatsFetched(StatsError(INVALID_RESPONSE, "Overview: Required data 'period' or 'dates' missing"))
+                } else {
+                    logProgress(granularity, "Valid response returned for period: ${overviewResponse.period}")
+                    logProgress(granularity, "Last data item for: ${overviewResponse.dates.lastOrNull()?.period}")
+                    OnStatsFetched(overviewResponse)
+                }
+            }
+            else -> OnStatsFetched(StatsError(INVALID_RESPONSE))
+        }
+    }
+
     private fun logProgress(granularity: StatsGranularity, message: String) {
         appLogWrapper.d(STATS, "fetchVisits for $granularity: $message")
     }
@@ -91,6 +139,16 @@ class VisitsAndViewsStore
                 currentTimeProvider.currentDate(),
                 SiteUtils.getNormalizedTimezone(site.timezone)
         )
+        return getVisits(site, granularity, limitMode, dateWithTimeZone)
+    }
+
+    fun getVisits(
+        site: SiteModel,
+        granularity: StatsGranularity,
+        limitMode: Top,
+        date: Date
+    ): VisitsAndViewsModel? {
+        val dateWithTimeZone = statsUtils.getFormattedDate(date, SiteUtils.getNormalizedTimezone(site.timezone))
         return getVisits(site, granularity, limitMode, dateWithTimeZone)
     }
 
