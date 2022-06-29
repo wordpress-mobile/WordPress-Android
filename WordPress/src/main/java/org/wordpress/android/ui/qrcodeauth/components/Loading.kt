d287a907af94be1766f4d4ac84969df0097f56db
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-06-29 19:10:52 -0300

    Implement Composable loading state for QR code auth

--- WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/components/Loading.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/components/Loading.kt
@@ -1,8 +1,18 @@
 package org.wordpress.android.ui.qrcodeauth.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.wordpress.android.ui.compose.components.IndeterminateCircularProgress
 
 @Composable
 fun Loading() {
-
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        IndeterminateCircularProgress()
+    }
 }
