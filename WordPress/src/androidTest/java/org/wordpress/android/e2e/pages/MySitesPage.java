471172bf40a144a608702f06516966060c2a06ea
Ovi Trif <octrif@gmail.com>
2022-07-01 10:26:52 +0200

    Optimise imports in MySitesPage

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/MySitesPage.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/MySitesPage.java
@@ -22,7 +22,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.wordpress.android.support.WPSupportUtils.clickOn;
 import static org.wordpress.android.support.WPSupportUtils.getTranslatedString;
-import static org.wordpress.android.support.WPSupportUtils.idleFor;
 import static org.wordpress.android.support.WPSupportUtils.isElementDisplayed;
 import static org.wordpress.android.support.WPSupportUtils.longClickOn;
 import static org.wordpress.android.support.WPSupportUtils.selectItemWithTitleInTabLayout;
