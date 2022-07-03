e8935a129b46b304b58ddc98021dbc0fbaee7c06
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-07-03 18:01:33 -0300

    Add documentation to IndeterminateCircularProgress

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/IndeterminateCircularProgress.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/IndeterminateCircularProgress.kt
@@ -4,11 +4,14 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+/**
+ * Indeterminate circular progress UI component. Used for operations that we don't how long are going to take.
+ */
 @Composable
 fun IndeterminateCircularProgress(
     modifier: Modifier = Modifier
 ) {
     CircularProgressIndicator(
-            modifier = modifier
+        modifier = modifier
     )
 }
