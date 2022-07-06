55d0e0ca2efeb453fd77ed654a96b738f9ff377a
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-07-06 01:27:41 -0300

    Refactor ui compose theme packages

--- WordPress/src/main/java/org/wordpress/android/ui/compose/theme/FontSize.kt
+++
@@ -1,8 +0,0 @@
-package org.wordpress.android.ui.compose.theme
-
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
-
-sealed class FontSize(val value: TextUnit) {
-    object DoubleExtraLarge : FontSize(24.sp)
-}
