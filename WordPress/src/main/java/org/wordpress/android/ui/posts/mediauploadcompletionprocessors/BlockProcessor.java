2ae12f5e4025e968a27f84e3e25dc36865b297f3
Gerardo <gerardo.pacheco@automattic.com>
2022-07-19 19:05:32 +0200

    BlockProcessor - Remove unneeded line break

--- WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/BlockProcessor.java
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/BlockProcessor.java
@@ -45,8 +45,7 @@ public abstract class BlockProcessor {
     BlockProcessor(String localId, MediaFile mediaFile) {
         mLocalId = localId;
         mRemoteId = mediaFile.getMediaId();
-        mRemoteUrl = org.wordpress.android.util.StringUtils.notNullStr(Utils
-                .escapeQuotes(mediaFile.getFileURL()));
+        mRemoteUrl = org.wordpress.android.util.StringUtils.notNullStr(Utils.escapeQuotes(mediaFile.getFileURL()));
         mRemoteImageUrl = org.wordpress.android.util.StringUtils.notNullStr(Utils
                 .escapeQuotes(mediaFile.getOptimalFileURL()));
     }
