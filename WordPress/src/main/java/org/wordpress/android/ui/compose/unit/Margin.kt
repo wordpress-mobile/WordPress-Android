6381886702aca33c2b5397966624cfa6b9754892
Ovi Trif <octrif@gmail.com>
2022-07-20 21:34:14 +0300

    Suppress MagicNumber for compose units

--- WordPress/src/main/java/org/wordpress/android/ui/compose/unit/Margin.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/unit/Margin.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.ui.compose.unit
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+@Suppress("MagicNumber")
 sealed class Margin(val value: Dp) {
     object Small : Margin(4.dp)
     object ExtraLarge : Margin(16.dp)
