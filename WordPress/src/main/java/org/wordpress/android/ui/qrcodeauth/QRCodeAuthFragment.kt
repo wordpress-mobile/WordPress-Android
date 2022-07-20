3712c3f7fa53ce6786ce24cd1439ac62db3177c7
Ovi Trif <octrif@gmail.com>
2022-07-20 21:20:27 +0300

    Remove unused code from QRCodeAuthFragment and cleanup formatting

--- WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/QRCodeAuthFragment.kt
@@ -18,14 +18,10 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import org.wordpress.android.analytics.AnalyticsTracker.Stat
-import org.wordpress.android.databinding.QrcodeauthFragmentBinding
 import org.wordpress.android.ui.compose.components.VerticalScrollBox
 import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.ui.posts.BasicDialogViewModel
@@ -69,14 +65,9 @@ class QRCodeAuthFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //TODO @RenanLukas
         initBackPressHandler()
         initViewModel(savedInstanceState)
-//        with(QrcodeauthFragmentBinding.bind(view)) {
-        //            initBackPressHandler()
-//           TODO @RenanLukas observeViewModel()
-        //            initViewModel(savedInstanceState)
-//        }
+        observeDialogViewModel()
     }
 
     @Composable
@@ -101,17 +92,15 @@ class QRCodeAuthFragment : Fragment() {
                         is Content -> ContentState(this)
                         is Error -> ErrorState(this)
                         is Loading -> LoadingState()
-                        is Scanning -> {} // NO OP
+                        is Scanning -> Unit
                     }
                 }
             }
         }
     }
 
-    private fun QrcodeauthFragmentBinding.observeViewModel() {
-        qrCodeAuthViewModel.actionEvents.onEach { handleActionEvents(it) }.launchIn(viewLifecycleOwner.lifecycleScope)
-        dialogViewModel.onInteraction.observeEvent(viewLifecycleOwner) { qrCodeAuthViewModel.onDialogInteraction(it) }
-        qrCodeAuthViewModel.uiState.onEach { renderUi(it) }.launchIn(viewLifecycleOwner.lifecycleScope)
+    private fun observeDialogViewModel() {
+        dialogViewModel.onInteraction.observeEvent(viewLifecycleOwner, qrCodeAuthViewModel::onDialogInteraction)
     }
 
     private fun initViewModel(savedInstanceState: Bundle?) {
@@ -129,86 +118,41 @@ class QRCodeAuthFragment : Fragment() {
             is LaunchDismissDialog -> launchDismissDialog(actionEvent.dialogModel)
             is LaunchScanner -> launchScanner()
             is FinishActivity -> requireActivity().finish()
-        }
-    }
-
-    private fun QrcodeauthFragmentBinding.renderUi(uiState: QRCodeAuthUiState) {
-        uiHelpers.updateVisibility(contentLayout.contentContainer, uiState.contentVisibility)
-        uiHelpers.updateVisibility(errorLayout.errorContainer, uiState.errorVisibility)
-        uiHelpers.updateVisibility(loadingLayout.loadingContainer, uiState.loadingVisibility)
-        when (uiState) {
-            is Content -> { applyContentState(uiState) }
-            is Error -> { applyErrorState(uiState) }
-            is Loading -> { } // NO OP
-            is Scanning -> { } // NO OP
-        }
-    }
-
-    private fun QrcodeauthFragmentBinding.applyContentState(uiState: Content) {
-        uiHelpers.updateVisibility(contentLayout.contentContainer, uiState.contentVisibility)
-        uiHelpers.updateVisibility(contentLayout.progress, uiState.isProgressShowing)
-        uiHelpers.setTextOrHide(contentLayout.contentTitle, uiState.title)
-        uiHelpers.setTextOrHide(contentLayout.contentSubtitle, uiState.subtitle)
-        uiHelpers.setImageOrHide(contentLayout.contentImage, uiState.image)
-        contentLayout.contentContainer.alpha = uiState.alpha
-        uiState.primaryActionButton?.let { action ->
-            uiHelpers.setTextOrHide(contentLayout.contentPrimaryAction, action.label)
-            uiHelpers.updateVisibility(contentLayout.contentPrimaryAction, action.isVisible)
-            contentLayout.contentPrimaryAction.setOnClickListener { action.clickAction?.invoke() }
-            contentLayout.contentPrimaryAction.isEnabled = action.isEnabled
-        }
-        uiState.secondaryActionButton?.let { action ->
-            uiHelpers.setTextOrHide(contentLayout.contentSecondaryAction, action.label)
-            uiHelpers.updateVisibility(contentLayout.contentSecondaryAction, action.isVisible)
-            contentLayout.contentSecondaryAction.setOnClickListener { action.clickAction?.invoke() }
-            contentLayout.contentSecondaryAction.isEnabled = action.isEnabled
-        }
-    }
-
-    private fun QrcodeauthFragmentBinding.applyErrorState(uiState: Error) {
-        uiHelpers.updateVisibility(errorLayout.errorContainer, uiState.errorVisibility)
-        uiHelpers.setImageOrHide(errorLayout.errorImage, uiState.image)
-        uiHelpers.setTextOrHide(errorLayout.errorTitle, uiState.title)
-        uiHelpers.setTextOrHide(errorLayout.errorSubtitle, uiState.subtitle)
-        uiState.primaryActionButton?.let { action ->
-            uiHelpers.setTextOrHide(errorLayout.errorPrimaryAction, action.label)
-            errorLayout.errorPrimaryAction.setOnClickListener { action.clickAction.invoke() }
-        }
-        uiState.secondaryActionButton?.let { action ->
-            uiHelpers.setTextOrHide(errorLayout.errorSecondaryAction, action.label)
-            errorLayout.errorSecondaryAction.setOnClickListener { action.clickAction.invoke() }
+            else -> Unit
         }
     }
 
     private fun launchDismissDialog(model: QRCodeAuthDialogModel) {
-        dialogViewModel.showDialog(requireActivity().supportFragmentManager,
-                BasicDialogModel(model.tag,
-                        getString(model.title),
-                        getString(model.message),
-                        getString(model.positiveButtonLabel),
-                        model.negativeButtonLabel?.let { label -> getString(label) },
-                        model.cancelButtonLabel?.let { label -> getString(label) }
-                ))
+        dialogViewModel.showDialog(
+            requireActivity().supportFragmentManager,
+            BasicDialogModel(
+                model.tag,
+                getString(model.title),
+                getString(model.message),
+                getString(model.positiveButtonLabel),
+                model.negativeButtonLabel?.let { label -> getString(label) },
+                model.cancelButtonLabel?.let { label -> getString(label) }
+            )
+        )
     }
 
     private fun launchScanner() {
         qrCodeAuthViewModel.track(Stat.QRLOGIN_SCANNER_DISPLAYED)
         val scanner = GmsBarcodeScanning.getClient(requireContext())
         scanner.startScan()
-                .addOnSuccessListener { barcode -> qrCodeAuthViewModel.onScanSuccess(barcode.rawValue) }
-                .addOnFailureListener { qrCodeAuthViewModel.onScanFailure() }
+            .addOnSuccessListener { barcode -> qrCodeAuthViewModel.onScanSuccess(barcode.rawValue) }
+            .addOnFailureListener { qrCodeAuthViewModel.onScanFailure() }
     }
 
     private fun initBackPressHandler() {
         requireActivity().onBackPressedDispatcher.addCallback(
-                viewLifecycleOwner,
-                object : OnBackPressedCallback(
-                        true
-                ) {
-                    override fun handleOnBackPressed() {
-                        qrCodeAuthViewModel.onBackPressed()
-                    }
-                })
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    qrCodeAuthViewModel.onBackPressed()
+                }
+            }
+        )
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
