80bd1be928f1bc8a65678fb09d1b608ef297e0a3
Ovi Trif <octrif@gmail.com>
2022-07-20 19:42:26 +0300

    Fix detekt issues in WordPressColors

--- WordPress/src/main/java/org/wordpress/android/ui/compose/theme/color/WordPressColors.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/theme/color/WordPressColors.kt
@@ -8,44 +8,42 @@ import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
 class WordPressColors : ThemeColors {
-    private val blue30 = Color(0xff399ce3)
-    private val blue50 = Color(0xff0675c4)
-    private val blue70 = Color(0xff044b7a)
-    private val red50 = Color(0xffd63638)
-    private val red30 = Color(0xfff86368)
-    private val white = Color(0xffffffff)
-    private val black = Color(0xff000000)
-    private val darkGray = Color(0xff121212)
+    private val blue30 = Color(color = 0xff399ce3)
+    private val blue50 = Color(color = 0xff0675c4)
+    private val blue70 = Color(color = 0xff044b7a)
+    private val red50 = Color(color = 0xffd63638)
+    private val red30 = Color(color = 0xfff86368)
+    private val white = Color(color = 0xffffffff)
+    private val black = Color(color = 0xff000000)
+    private val darkGray = Color(color = 0xff121212)
 
-    override fun light(): Colors =
-            lightColors(
-                    primary = blue50,
-                    primaryVariant = blue70,
-                    secondary = blue50,
-                    secondaryVariant = blue70,
-                    background = white,
-                    surface = white,
-                    error = red50,
-                    onPrimary = white,
-                    onSecondary = white,
-                    onBackground = black,
-                    onSurface = black,
-                    onError = white
-            )
+    override fun light(): Colors = lightColors(
+        primary = blue50,
+        primaryVariant = blue70,
+        secondary = blue50,
+        secondaryVariant = blue70,
+        background = white,
+        surface = white,
+        error = red50,
+        onPrimary = white,
+        onSecondary = white,
+        onBackground = black,
+        onSurface = black,
+        onError = white
+    )
 
-    override fun dark(): Colors =
-            darkColors(
-                    primary = blue30,
-                    primaryVariant = blue50,
-                    secondary = blue30,
-                    secondaryVariant = blue50,
-                    background = darkGray,
-                    surface = darkGray,
-                    error = red30,
-                    onPrimary = black,
-                    onSecondary = white,
-                    onBackground = white,
-                    onSurface = white,
-                    onError = black
-            )
+    override fun dark(): Colors = darkColors(
+        primary = blue30,
+        primaryVariant = blue50,
+        secondary = blue30,
+        secondaryVariant = blue50,
+        background = darkGray,
+        surface = darkGray,
+        error = red30,
+        onPrimary = black,
+        onSecondary = white,
+        onBackground = white,
+        onSurface = white,
+        onError = black
+    )
 }
