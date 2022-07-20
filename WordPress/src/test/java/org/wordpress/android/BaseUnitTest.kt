78bf04e638d00cf1b434c1e585d898c0381db972
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-07-20 16:56:47 +0300

    Analysis: Suppress unnecessary abstract class test warning for wp mod.

--- WordPress/src/test/java/org/wordpress/android/BaseUnitTest.kt
+++ WordPress/src/test/java/org/wordpress/android/BaseUnitTest.kt
@@ -5,6 +5,7 @@ import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 
+@Suppress("UnnecessaryAbstractClass")
 @RunWith(MockitoJUnitRunner::class)
 abstract class BaseUnitTest {
     @Rule @JvmField val rule = InstantTaskExecutorRule()
