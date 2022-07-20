bc462d318cf05c5412733907a573a13523368a6f
Ovi Trif <octrif@gmail.com>
2022-07-20 14:20:43 +0300

    Show background on ErrorState preview

--- WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/compose/state/ErrorState.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/qrcodeauth/compose/state/ErrorState.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.components.ResourceImage
@@ -80,7 +79,7 @@ fun ErrorState(
     }
 }
 
-@Preview(device = Devices.PIXEL_2_XL, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun ErrorStatePreview() {
     AppTheme {
