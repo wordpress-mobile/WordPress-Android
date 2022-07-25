9749581ed680e93f365a89858193bb4aaa5d56d7
Ovi Trif <octrif@gmail.com>
2022-07-25 09:37:57 +0300

    Remove unused compose code to replicate adjustViewBounds from xml

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/ResourceImage.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/ResourceImage.kt
@@ -2,8 +2,6 @@ package org.wordpress.android.ui.compose.components
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,26 +18,15 @@ fun ResourceImage(
     @DrawableRes imageRes: Int,
     contentDescription: String? = null,
     contentScale: ContentScale = ContentScale.Fit,
-    alignment: Alignment = Alignment.Center,
-    adjustViewBounds: Boolean = false
+    alignment: Alignment = Alignment.Center
 ) {
     val painter = painterResource(imageRes)
-    val enrichedModifier = when {
-        adjustViewBounds -> {
-            val aspectRatio = painter.intrinsicSize.width / painter.intrinsicSize.height
-            Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(aspectRatio)
-                    .then(modifier)
-        }
-        else -> modifier
-    }
 
     Image(
             painter,
             contentDescription = contentDescription,
             contentScale = contentScale,
             alignment = alignment,
-            modifier = enrichedModifier
+            modifier = modifier
     )
 }
