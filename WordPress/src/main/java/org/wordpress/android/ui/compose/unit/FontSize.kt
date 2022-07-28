ea378abeded918e699ab11f7fbffb2f4ca7e5a6d
Ovi Trif <octrif@gmail.com>
2022-07-28 12:39:53 +0200

    Remove MagicColors suppression for Compose units

--- WordPress/src/main/java/org/wordpress/android/ui/compose/unit/FontSize.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/unit/FontSize.kt
@@ -3,7 +3,6 @@ package org.wordpress.android.ui.compose.unit
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
-@Suppress("MagicNumber")
 sealed class FontSize(val value: TextUnit) {
     object Large : FontSize(16.sp)
     object DoubleExtraLarge : FontSize(24.sp)
