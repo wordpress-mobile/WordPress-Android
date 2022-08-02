759fe729e7f7a7cd8e5fbc292b15575d83367c10
Ovi Trif <octrif@gmail.com>
2022-08-02 22:22:41 +0200

    Remove comment line about iOS fix

--- fluxc/src/main/java/org/wordpress/android/fluxc/utils/extensions/StringExtensions.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/utils/extensions/StringExtensions.kt
@@ -5,8 +5,6 @@ import java.net.URLEncoder
 /**
  * Encodes delimiting characters as per
  * [RFC 3986](https://datatracker.ietf.org/doc/html/rfc3986#section-2.2)
- *
- * See [Similar fix in iOS](https://github.com/wordpress-mobile/WordPressKit-iOS/pull/457)
  */
 fun String.encodeRfc3986Delimiters(): String {
     val rfc3986Delimiters = "!'()*"
