8598fcb13b37ccb2caf356c593b9521b27e669eb
Ravi <ravi.program@gmail.com>
2022-08-03 13:26:49 +1000

    Remove Stats Feature Intro

--- WordPress/src/main/java/org/wordpress/android/ui/stats/intro/StatsNewFeaturesIntroAction.kt
+++
@@ -1,8 +0,0 @@
-package org.wordpress.android.ui.stats.intro
-
-import org.wordpress.android.fluxc.model.SiteModel
-
-sealed class StatsNewFeaturesIntroAction {
-    data class OpenStats(val site: SiteModel) : StatsNewFeaturesIntroAction()
-    object DismissDialog : StatsNewFeaturesIntroAction()
-}
