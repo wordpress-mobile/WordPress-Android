5d82d25c05218f9d761613e58f9f34665db03573
Ovi Trif <octrif@gmail.com>
2022-08-04 12:09:34 +0200

    Sanitize domain query with the same algorithm as the 'domains/suggestions' API

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainSanitizer.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainSanitizer.kt
@@ -31,9 +31,65 @@ class SiteCreationDomainSanitizer
     fun getDomain(url: String) = url.lastPart
 
     /**
-     * Sanitizes a query by taking the value before it's first period if it's present,
-     * removes it's scheme and finally, removes any characters that aren't alphanumeric.
+     * Sanitizes a domain suggestions query applying the algorithm from the [domains/suggestions] API endpoint.
      */
-    fun sanitizeDomainQuery(query: String) =
-            query.firstPart.removeProtocol.removeNonAlphanumeric.toLowerCase(Locale.ROOT)
+    fun sanitizeDomainQuery(query: String): String {
+        return query
+                .removeDisallowedCharacters
+                .removeDisallowedTerms
+                .trim('.', '-')
+                .trim()
+                .lowercase(Locale.ROOT)
+    }
+
+    /**
+     * Remove or translate characters that aren't permitted in searches.
+     *
+     * @return The sanitized query.
+     */
+    private val String.removeDisallowedCharacters: String
+        get() {
+            val disallowedPattern = """[\s.-]""".toRegex()
+
+            return replace(disallowedPattern) { "" }
+        }
+
+    /**
+     * Remove any terms that aren't permitted in searches.
+     *
+     * @return The sanitized query.
+     */
+    private val String.removeDisallowedTerms: String
+        get() {
+            val blacklistedStrings = arrayOf(
+                    "automattic",
+                    "wordpress",
+                    "wpcomstaging",
+                    "paypal",
+                    "bankofamerica",
+                    "wellsfargo",
+                    "westernunion",
+                    "woocommerce",
+                    "woothemes",
+                    "facebook",
+                    ".blogspot.",
+                    "timboydaustralian.com",
+                    "dssvermoegensverwaltung",
+                    "alertwebmail",
+                    "updaterenewbilling",
+                    "securebilling",
+                    "webmail-postmaster",
+                    "webmail-receipt",
+                    "inboxmail-dataplugs",
+                    "mailerdaemon",
+            )
+
+            var sanitizedQuery = replace(".wordpress.com", "")
+
+            blacklistedStrings.forEach {
+                sanitizedQuery = sanitizedQuery.replace(it, "")
+            }
+
+            return sanitizedQuery
+        }
 }
