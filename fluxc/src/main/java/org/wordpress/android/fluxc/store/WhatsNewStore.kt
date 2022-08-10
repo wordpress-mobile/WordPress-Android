26780f073a5f8fcd0fb5eed11067348530c97fd8
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-08-10 15:44:27 +0300

    Analysis: Resolve global coroutine usage warning for whats new store
    Description: "This rule inspects variables marked as nullable and
    reports which could be declared as non-nullable instead."
    
    For more info see: https://detekt.dev/docs/rules/coroutines
    #globalcoroutineusage
    
    PS: A question was asked of the initial author(s) about whether this
    'GlobalCoroutine' usage within 'WhatsNewStore' was deliberate.
    
    For more info see: https://github.com/wordpress-mobile/
    WordPress-FluxC-Android/pull/1605#issuecomment-1210609730

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/WhatsNewStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/WhatsNewStore.kt
@@ -1,7 +1,5 @@
 package org.wordpress.android.fluxc.store
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
@@ -21,13 +19,11 @@ import org.wordpress.android.util.AppLog.T
 import org.wordpress.android.util.AppLog.T.API
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 @Singleton
 class WhatsNewStore @Inject constructor(
     private val whatsNewRestClient: WhatsNewRestClient,
     private val whatsNewSqlUtils: WhatsNewSqlUtils,
-    private val coroutineContext: CoroutineContext,
     private val coroutineEngine: CoroutineEngine,
     dispatcher: Dispatcher
 ) : Store(dispatcher) {
@@ -38,17 +34,12 @@ class WhatsNewStore @Inject constructor(
             FETCH_REMOTE_ANNOUNCEMENT -> {
                 val versionName = (action.payload as WhatsNewFetchPayload).versionName
                 val appId = (action.payload as WhatsNewFetchPayload).appId
-                GlobalScope.launch(coroutineContext) {
-                    emitChange(
-                            fetchRemoteAnnouncements(
-                                    versionName,
-                                    appId
-                            )
-                    )
+                coroutineEngine.launch(AppLog.T.API, this, "FETCH_REMOTE_ANNOUNCEMENT") {
+                    emitChange(fetchRemoteAnnouncements(versionName, appId))
                 }
             }
             FETCH_CACHED_ANNOUNCEMENT -> {
-                GlobalScope.launch(coroutineContext) {
+                coroutineEngine.launch(AppLog.T.API, this, "FETCH_CACHED_ANNOUNCEMENT") {
                     emitChange(fetchCachedAnnouncements())
                 }
             }
