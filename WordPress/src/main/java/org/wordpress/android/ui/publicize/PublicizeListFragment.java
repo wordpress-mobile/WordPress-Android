bbe5f67f042c4b070dd9f8c66584572414488ea1
Matthew Kevins <matt@A8C-MacBook-Pro-M1.local>
2022-08-16 07:52:41 -0400

    Remove unused import

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeListFragment.java
@@ -6,7 +6,6 @@ import android.text.Spannable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.MarginLayoutParams;
 import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
