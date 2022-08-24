ef064864b00e1c89c39fe3d59bed36c0f5abc544
GitStart <1501599+gitstart@users.noreply.github.com>
2022-08-24 10:39:56 +0000

    added_fix_for_title_problem_on_status_cards

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/viewholders/TitleWithMoreViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/viewholders/TitleWithMoreViewHolder.kt
@@ -1,8 +1,8 @@
 package org.wordpress.android.ui.stats.refresh.lists.sections.viewholders
 
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import com.google.android.material.button.MaterialButton
 import org.wordpress.android.R
@@ -17,12 +17,12 @@ class TitleWithMoreViewHolder(parent: ViewGroup) : BlockListItemViewHolder(
     private val viewMore = itemView.findViewById<MaterialButton>(id.view_more_button)
 
     fun bind(item: TitleWithMore) {
-        title.setTextOrHide(item.textResource, item.text)
+        title.setTextOrHide(item)
         if (item.navigationAction != null) {
             viewMore.isVisible = true
             viewMore.setOnClickListener { item.navigationAction.click() }
         } else {
-            viewMore.isInvisible = true
+            viewMore.visibility = View.GONE
         }
     }
 }
