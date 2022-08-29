7d7097047afadb482944d7cb6566f205afc86d27
Ravi <ravi.program@gmail.com>
2022-08-29 21:23:24 +1000

    Enable widgets on home screen
    - Enable home screen widgets on Jetpack app
    - Add Quick Press Shortcut

---
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/widget/weeks/StatsWeekWidget.kt
@@ -0,0 +1,16 @@
+package org.wordpress.android.ui.stats.refresh.lists.widget.weeks
+
+import org.wordpress.android.modules.AppComponent
+import org.wordpress.android.ui.stats.refresh.lists.widget.StatsWidget
+import org.wordpress.android.ui.stats.refresh.lists.widget.WidgetUpdater
+import javax.inject.Inject
+
+class StatsWeekWidget : StatsWidget() {
+    @Inject lateinit var weekViewsWidgetUpdater: WeekViewsWidgetUpdater
+    override val widgetUpdater: WidgetUpdater
+        get() = weekViewsWidgetUpdater
+
+    override fun inject(appComponent: AppComponent) {
+        appComponent.inject(this)
+    }
+}
---
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/widget/weeks/WeekViewsWidgetListViewModel.kt
@@ -0,0 +1,113 @@
+package org.wordpress.android.ui.stats.refresh.lists.widget.weeks
+
+import androidx.annotation.LayoutRes
+import kotlinx.coroutines.runBlocking
+import org.wordpress.android.R
+import org.wordpress.android.fluxc.model.stats.LimitMode
+import org.wordpress.android.fluxc.model.stats.time.VisitsAndViewsModel
+import org.wordpress.android.fluxc.network.utils.StatsGranularity.WEEKS
+import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.fluxc.store.stats.time.VisitsAndViewsStore
+import org.wordpress.android.ui.prefs.AppPrefsWrapper
+import org.wordpress.android.ui.stats.refresh.lists.widget.configuration.StatsColorSelectionViewModel.Color
+import org.wordpress.android.ui.stats.refresh.lists.widget.utils.WidgetUtils
+import org.wordpress.android.ui.stats.refresh.utils.ONE_THOUSAND
+import org.wordpress.android.ui.stats.refresh.utils.StatsUtils
+import org.wordpress.android.viewmodel.ResourceProvider
+import javax.inject.Inject
+
+class WeekViewsWidgetListViewModel @Inject constructor(
+    private val siteStore: SiteStore,
+    private val visitsAndViewsStore: VisitsAndViewsStore,
+    private val resourceProvider: ResourceProvider,
+    private val appPrefsWrapper: AppPrefsWrapper,
+    private val widgetUtils: WidgetUtils,
+    private val statsUtils: StatsUtils
+) {
+    private var siteId: Int? = null
+    private var colorMode: Color = Color.LIGHT
+    private var appWidgetId: Int? = null
+    private val mutableData = mutableListOf<WeekItemUiModel>()
+    val data: List<WeekItemUiModel> = mutableData
+    fun start(siteId: Int, colorMode: Color, appWidgetId: Int) {
+        this.siteId = siteId
+        this.colorMode = colorMode
+        this.appWidgetId = appWidgetId
+    }
+
+    @Suppress("NestedBlockDepth")
+    fun onDataSetChanged(onError: (appWidgetId: Int) -> Unit) {
+        siteId?.let { nonNullSiteId ->
+            val site = siteStore.getSiteByLocalId(nonNullSiteId)
+            if (site != null) {
+                runBlocking {
+                    visitsAndViewsStore.fetchVisits(site, WEEKS, LimitMode.Top(2))
+                }
+                visitsAndViewsStore.getVisits(site, WEEKS, LimitMode.All)?.let { visitsAndViewsModel ->
+                    val uiModels = buildListItemUiModel(visitsAndViewsModel, nonNullSiteId)
+                    if (uiModels != data) {
+                        mutableData.clear()
+                        mutableData.addAll(uiModels)
+                        appWidgetId?.let {
+                            appPrefsWrapper.setAppWidgetHasData(true, it)
+                        }
+                    }
+                }
+            } else {
+                appWidgetId?.let { nonNullAppWidgetId ->
+                    onError(nonNullAppWidgetId)
+                }
+            }
+        }
+    }
+
+    private fun buildListItemUiModel(
+        domainModel: VisitsAndViewsModel,
+        localSiteId: Int
+    ): List<WeekItemUiModel> {
+        val layout = when (colorMode) {
+            Color.DARK -> R.layout.stats_views_widget_item_dark
+            Color.LIGHT -> R.layout.stats_views_widget_item_light
+        }
+
+        val periodData = widgetUtils.getLastWeekPeriodData(domainModel)
+
+        return listOf(
+                WeekItemUiModel(
+                        layout,
+                        localSiteId,
+                        resourceProvider.getString(R.string.stats_views),
+                        statsUtils.toFormattedString(periodData?.views ?: 0, ONE_THOUSAND)
+                ),
+                WeekItemUiModel(
+                        layout,
+                        localSiteId,
+                        resourceProvider.getString(R.string.stats_visitors),
+                        statsUtils.toFormattedString(periodData?.visitors ?: 0, ONE_THOUSAND)
+                ),
+                WeekItemUiModel(
+                        layout,
+                        localSiteId,
+                        resourceProvider.getString(R.string.likes),
+                        statsUtils.toFormattedString(periodData?.likes ?: 0, ONE_THOUSAND)
+                ),
+                WeekItemUiModel(
+                        layout,
+                        localSiteId,
+                        resourceProvider.getString(R.string.stats_comments),
+                        statsUtils.toFormattedString(periodData?.comments ?: 0, ONE_THOUSAND)
+                )
+        )
+    }
+
+    data class WeekItemUiModel(
+        @LayoutRes val layout: Int,
+        val localSiteId: Int,
+        val key: String,
+        val value: String
+    )
+
+    companion object {
+        const val LIST_ITEM_COUNT = 7
+    }
+}
