0c62f0da83d3730d739886067ed6230b951c3a9f
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-08-30 18:33:08 -0300

    Add debug public hash key to Jetpack public data

--- WordPress/src/main/java/org/wordpress/android/sharedlogin/JetpackPublicData.kt
+++ WordPress/src/main/java/org/wordpress/android/sharedlogin/JetpackPublicData.kt
@@ -12,7 +12,7 @@ class JetpackPublicData @Inject constructor() {
     private sealed class PublicKeyHash(val type: String, val value: String) {
         object Release : PublicKeyHash("release", "f2d7acc12614750009514a0932bf0b0aa9c11829a66e862ce4572bced344e76e")
 
-        object Debug : PublicKeyHash("debug", "")
+        object Debug : PublicKeyHash("debug", "60fca11c59c6933610146f40a1296250abff640dc5da2b85fc8e5aa411dd17d6")
     }
 
     fun currentPublicKeyHash() = when (BuildConfig.BUILD_TYPE) {
