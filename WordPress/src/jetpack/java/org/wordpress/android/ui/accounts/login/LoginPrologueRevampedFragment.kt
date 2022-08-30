9b30b9672af8894f767890d6dc256f96a0393e44
Ovi Trif <octrif@gmail.com>
2022-08-30 13:44:24 +0200

    Scaffold UI of the jetpack splash screen

--- WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/LoginPrologueRevampedFragment.kt
+++ WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/LoginPrologueRevampedFragment.kt
@@ -5,21 +5,34 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.theme.AppTheme
-import org.wordpress.android.ui.compose.unit.Margin
 
 class LoginPrologueRevampedFragment : Fragment() {
     private lateinit var loginPrologueListener: LoginPrologueListener
@@ -32,7 +45,8 @@ class LoginPrologueRevampedFragment : Fragment() {
         setContent {
             AppTheme {
                 LoginScreenRevamped(
-                        onLoginClicked = { loginPrologueListener.showEmailLoginScreen() }
+                        onWpComLoginClicked = loginPrologueListener::showEmailLoginScreen,
+                        onSiteAddressLoginClicked = loginPrologueListener::loginViaSiteAddress,
                 )
             }
         }
@@ -53,26 +67,70 @@ class LoginPrologueRevampedFragment : Fragment() {
 
 @Composable
 private fun LoginScreenRevamped(
-    onLoginClicked: () -> Unit,
+    onWpComLoginClicked: () -> Unit,
+    onSiteAddressLoginClicked: () -> Unit,
 ) {
-    Column(
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(vertical = Margin.ExtraExtraMediumLarge.value)
-    ) {
-        Button(
-                onClick = onLoginClicked,
-        ) {
-            Text(stringResource(R.string.continue_with_wpcom))
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                    painter = painterResource(id = R.drawable.bg_jetpack_login_splash),
+                    contentDescription = stringResource(R.string.login_prologue_revamped_content_description_bg),
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.matchParentSize()
+            )
+            Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                        painter = painterResource(R.drawable.ic_jetpack_logo_green_24dp),
+                        contentDescription = stringResource(
+                                R.string.login_prologue_revamped_content_description_jetpack_logo
+                        ),
+                        modifier = Modifier
+                                .padding(top = 76.dp)
+                                .size(100.dp)
+                )
+                Spacer(modifier = Modifier.weight(1.0f))
+                Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(horizontal = 20.dp)
+                ) {
+                    Button(
+                            onClick = onWpComLoginClicked,
+                            colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = MaterialTheme.colors.primary,
+                                    contentColor = Color.White,
+                            ),
+                            shape = RoundedCornerShape(5.dp),
+                            modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Text(stringResource(R.string.continue_with_wpcom_no_signup))
+                    }
+                    Button(
+                            onClick = onSiteAddressLoginClicked,
+                            colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = Color.White,
+                                    contentColor = Color.Black,
+                            ),
+                            shape = RoundedCornerShape(5.dp),
+                            modifier = Modifier
+                                    .padding(bottom = 60.dp)
+                                    .fillMaxWidth(),
+                    ) {
+                        Text(stringResource(R.string.enter_your_site_address))
+                    }
+                }
+            }
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 400, heightDp = 700)
-@Preview(showBackground = true, widthDp = 400, heightDp = 700, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Preview(showBackground = true, device = Devices.PIXEL_4, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewLoginScreenRevamped() {
     AppTheme {
-        LoginScreenRevamped(onLoginClicked = {})
+        LoginScreenRevamped(onWpComLoginClicked = {}, onSiteAddressLoginClicked = {})
     }
 }
