23600c61eba2f9d381120a8fb9d278ad73542f4e
Ovi Trif <octrif@gmail.com>
2022-09-01 21:40:17 +0200

    Fix large text font style

--- WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LargeTexts.kt
+++ WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LargeTexts.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.unit.Margin
@@ -59,13 +63,16 @@ fun LargeText(
     color: Color,
     modifier: Modifier = Modifier,
 ) {
+    val fontSize by remember { mutableStateOf(43.sp) }
+    val lineHeight by remember { mutableStateOf(fontSize * 0.95) }
+
     Text(
             text = text,
             style = TextStyle(
-                    fontSize = 44.sp,
+                    fontSize = fontSize,
                     fontWeight = FontWeight.SemiBold,
                     letterSpacing = (-0.3).sp,
-                    lineHeight = 36.sp, // 38 * 0,95
+                    lineHeight = lineHeight,
             ),
             color = color,
             modifier = modifier
