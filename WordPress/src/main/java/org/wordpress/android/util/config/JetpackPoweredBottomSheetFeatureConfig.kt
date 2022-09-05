9657786567d503b50f2b88d6ad1b2ff12b72d676
Ovi Trif <octrif@gmail.com>
2022-09-05 15:59:13 +0200

    Fix JetpackPoweredBottomSheetFeatureConfig - Point to build config field: JETPACK_POWERED_BOTTOM_SHEET - Uncomment the @Feature annotation - Remove the @FeatureInDevelopment annotation - Remove comments and cleanup code

--- WordPress/src/main/java/org/wordpress/android/util/config/JetpackPoweredBottomSheetFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/JetpackPoweredBottomSheetFeatureConfig.kt
@@ -1,23 +1,16 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.annotation.Feature
 import javax.inject.Inject
 
-/**
- * Configuration for Jetpack Powered Bottom Sheet
- *
- * TODO: When it is ready to be rolled out uncomment the lines 12 and 19, remove line 13 and this to-do
- */
-// @Feature(JetpackPoweredBottomSheetFeatureConfig.JETPACK_POWERED_BOTTOM_SHEET_REMOTE_FIELD, true)
-@FeatureInDevelopment
-@Suppress("ForbiddenComment")
+@Feature(JetpackPoweredBottomSheetFeatureConfig.JETPACK_POWERED_BOTTOM_SHEET_REMOTE_FIELD, true)
 class JetpackPoweredBottomSheetFeatureConfig @Inject constructor(
     appConfig: AppConfig
 ) : FeatureConfig(
         appConfig,
-        BuildConfig.JETPACK_POWERED,
-//        JETPACK_POWERED_BOTTOM_SHEET_REMOTE_FIELD
+        BuildConfig.JETPACK_POWERED_BOTTOM_SHEET,
+        JETPACK_POWERED_BOTTOM_SHEET_REMOTE_FIELD
 ) {
     companion object {
         const val JETPACK_POWERED_BOTTOM_SHEET_REMOTE_FIELD = "jetpack_powered_bottom_sheet_remote_field"
