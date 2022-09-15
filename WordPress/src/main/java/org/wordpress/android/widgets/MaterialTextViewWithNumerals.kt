6eb6b4b8bdbef713ff6a7934e95922d8814b14f5
Ovi Trif <octrif@gmail.com>
2022-09-15 16:12:08 +0200

    Rename MaterialTextViewWithArabicNumerals into MaterialTextViewWithNumerals

--- WordPress/src/main/java/org/wordpress/android/widgets/MaterialTextViewWithNumerals.kt
+++ WordPress/src/main/java/org/wordpress/android/widgets/MaterialTextViewWithNumerals.kt
@@ -7,7 +7,7 @@ import com.google.android.material.textview.MaterialTextView
 /**
  * MaterialTextView which enforces Western Arabic numerals.
  */
-class MaterialTextViewWithArabicNumerals @JvmOverloads constructor(
+class MaterialTextViewWithNumerals @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
