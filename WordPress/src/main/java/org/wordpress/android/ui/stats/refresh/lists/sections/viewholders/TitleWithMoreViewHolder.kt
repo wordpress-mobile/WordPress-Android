db72da9975c047ed0a15bf2929fcd89ae6fa621e
GitStart <1501599+gitstart@users.noreply.github.com>
2022-09-20 18:22:23 +0530

    Update TitleWithMoreViewHolder.kt

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/viewholders/TitleWithMoreViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/viewholders/TitleWithMoreViewHolder.kt
@@ -7,7 +7,6 @@ import androidx.core.view.isVisible
 import com.google.android.material.button.MaterialButton
 import org.wordpress.android.R
 import org.wordpress.android.R.id
-import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem
 import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem.TitleWithMore
 
 class TitleWithMoreViewHolder(parent: ViewGroup) : BlockListItemViewHolder(
@@ -19,6 +18,7 @@ class TitleWithMoreViewHolder(parent: ViewGroup) : BlockListItemViewHolder(
 
     fun bind(item: TitleWithMore) {
         title.multiLineText(item)
+        title.setTextOrHide(item.textResource,item.text)
         if (item.navigationAction != null) {
             viewMore.isVisible = true
             viewMore.setOnClickListener { item.navigationAction.click() }
@@ -27,12 +27,11 @@ class TitleWithMoreViewHolder(parent: ViewGroup) : BlockListItemViewHolder(
         }
     }
 
-    private fun TextView.multiLineText(item: TitleWithMore) {
+    fun TextView.multiLineText(item: TitleWithMore) {
         this.visibility = View.VISIBLE
-        if (item.navigationAction == null) {
+        if(item.navigationAction == null){
             this.ellipsize = null
             this.maxLines = Int.MAX_VALUE
         }
-        this.setTextOrHide(item.textResource, item.text)
     }
 }
