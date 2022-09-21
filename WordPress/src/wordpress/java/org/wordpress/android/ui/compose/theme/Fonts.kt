5da1473dc2d37258e3e832c0d0a82224b807e176
Ovi Trif <octrif@gmail.com>
2022-09-21 14:04:42 +0200

    Fallback to default Serif font if EBGaramond can't be loaded (to fix Huawei Android 7 issue)

--- WordPress/src/wordpress/java/org/wordpress/android/ui/compose/theme/Fonts.kt
+++ WordPress/src/wordpress/java/org/wordpress/android/ui/compose/theme/Fonts.kt
@@ -1,8 +1,16 @@
 package org.wordpress.android.ui.compose.theme
 
+import android.os.Build
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import org.wordpress.android.R.font
 
+/**
+ * On Huawei devices with Android 7 loading custom fonts fails; so we fallback to the standard Serif font.
+ * see: [GitHub: Problem with fonts on the Android platform](https://github.com/JetBrains/compose-jb/issues/333)
+ */
 val FontFamily.Companion.EBGaramond
-    get() = FontFamily(Font(font.eb_garamond))
+    get() = when (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+        true -> FontFamily(Font(font.eb_garamond))
+        false -> Serif
+    }
