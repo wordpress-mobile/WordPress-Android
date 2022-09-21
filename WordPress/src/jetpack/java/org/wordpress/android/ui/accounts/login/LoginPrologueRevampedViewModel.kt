8a6368d53f119e90d86da1510b9d6452ba8c301e
Ovi Trif <octrif@gmail.com>
2022-09-21 16:12:02 +0200

    Fix code formatting in LoginPrologueRevampedViewModel

--- WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/LoginPrologueRevampedViewModel.kt
+++ WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/LoginPrologueRevampedViewModel.kt
@@ -17,17 +17,20 @@ import kotlin.math.ln
 // This factor is used to convert the raw values emitted from device sensor to an appropriate scale for the consuming
 // composables.
 private const val ACCELERATION_FACTOR = -0.1f
+
 // The maximum velocity (in either direction)
 private const val MAXIMUM_VELOCITY = 0.3f
+
 // The velocity decay factor (i.e. 1/4th velocity after 1 second)
 private val VELOCITY_DECAY = -ln(4f)
+
 // An additional acceleration applied to make the text scroll when the device is flat on a table
 private const val DRIFT = -0.05f
 
 @HiltViewModel
 class LoginPrologueRevampedViewModel @Inject constructor(
     @ApplicationContext appContext: Context,
-): ViewModel() {
+) : ViewModel() {
     private var acceleration = 0f
     private var velocity = 0f
     var position = 0f
@@ -48,28 +51,34 @@ class LoginPrologueRevampedViewModel @Inject constructor(
                 .coerceIn(-MAXIMUM_VELOCITY, MAXIMUM_VELOCITY)
         // Update the position, modulo 1 (ensuring a value greater or equal to 0, and less than 1)
         position = ((position + elapsed * velocity) % 1 + 1) % 1
+
         _positionData.postValue(position)
     }
 
     /** This LiveData responds to accelerometer data from the y-axis of the device and emits updated position data. */
-    private val _positionData = object: MutableLiveData<Float>(), SensorEventListener {
-        private val sensorManager get() = appContext.getSystemService(Context.SENSOR_SERVICE)
-                as SensorManager
+    private val _positionData = object : MutableLiveData<Float>(), SensorEventListener {
+        private val sensorManager
+            get() = appContext.getSystemService(Context.SENSOR_SERVICE)
+                    as SensorManager
+
         override fun onActive() {
             super.onActive()
             val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
             sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME)
         }
+
         override fun onInactive() {
             super.onInactive()
             sensorManager.unregisterListener(this)
         }
+
         override fun onSensorChanged(event: SensorEvent?) {
             event?.let {
                 acceleration = it.values[1] * ACCELERATION_FACTOR + DRIFT
                 postValue(position)
             }
         }
+
         override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) = Unit
     }
     val positionData: LiveData<Float> = _positionData
