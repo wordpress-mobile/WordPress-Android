77e0a36e3675d15b7f208615547ac18bb3d47281
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-09-23 23:06:57 -0300

    Apply PR suggestions: filter user flags that are returned

--- WordPress/src/main/java/org/wordpress/android/userflags/provider/UserFlagsProvider.kt
+++ WordPress/src/main/java/org/wordpress/android/userflags/provider/UserFlagsProvider.kt
@@ -6,8 +6,10 @@ import org.wordpress.android.WordPress
 import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.provider.query.QueryContentProvider
 import org.wordpress.android.provider.query.QueryResult
-import org.wordpress.android.util.publicdata.JetpackPublicData
+import org.wordpress.android.ui.prefs.AppPrefs.DeletablePrefKey
+import org.wordpress.android.ui.prefs.AppPrefs.UndeletablePrefKey
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
+import org.wordpress.android.util.publicdata.JetpackPublicData
 import org.wordpress.android.util.signature.SignatureNotFoundException
 import org.wordpress.android.util.signature.SignatureUtils
 import javax.inject.Inject
@@ -18,6 +20,61 @@ class UserFlagsProvider : QueryContentProvider() {
     @Inject lateinit var signatureUtils: SignatureUtils
     @Inject lateinit var queryResult: QueryResult
     @Inject lateinit var jetpackPublicData: JetpackPublicData
+
+    private val userFlagsKeysSet: Set<String> = setOf(
+            DeletablePrefKey.READER_TAG_NAME.name,
+            DeletablePrefKey.READER_SUBS_PAGE_TITLE.name,
+            DeletablePrefKey.MAIN_PAGE_INDEX.name,
+            DeletablePrefKey.PEOPLE_LIST_FILTER_INDEX.name,
+            DeletablePrefKey.RECENTLY_PICKED_SITE_IDS.name,
+            DeletablePrefKey.IMAGE_OPTIMIZE_ENABLED.name,
+            DeletablePrefKey.IMAGE_OPTIMIZE_WIDTH.name,
+            DeletablePrefKey.IMAGE_OPTIMIZE_QUALITY.name,
+            DeletablePrefKey.VIDEO_OPTIMIZE_ENABLED.name,
+            DeletablePrefKey.VIDEO_OPTIMIZE_WIDTH.name,
+            DeletablePrefKey.VIDEO_OPTIMIZE_QUALITY.name,
+            DeletablePrefKey.STRIP_IMAGE_LOCATION.name,
+            DeletablePrefKey.SUPPORT_EMAIL.name,
+            DeletablePrefKey.SUPPORT_NAME.name,
+            DeletablePrefKey.GUTENBERG_DEFAULT_FOR_NEW_POSTS.name,
+            DeletablePrefKey.USER_IN_GUTENBERG_ROLLOUT_GROUP.name,
+            DeletablePrefKey.SHOULD_AUTO_ENABLE_GUTENBERG_FOR_THE_NEW_POSTS.name,
+            DeletablePrefKey.SHOULD_AUTO_ENABLE_GUTENBERG_FOR_THE_NEW_POSTS_PHASE_2.name,
+            DeletablePrefKey.GUTENBERG_OPT_IN_DIALOG_SHOWN.name,
+            DeletablePrefKey.GUTENBERG_FOCAL_POINT_PICKER_TOOLTIP_SHOWN.name,
+            DeletablePrefKey.IS_QUICK_START_NOTICE_REQUIRED.name,
+            DeletablePrefKey.LAST_SKIPPED_QUICK_START_TASK.name,
+            DeletablePrefKey.POST_LIST_AUTHOR_FILTER.name,
+            DeletablePrefKey.POST_LIST_VIEW_LAYOUT_TYPE.name,
+            DeletablePrefKey.AZTEC_EDITOR_DISABLE_HW_ACC_KEYS.name,
+            DeletablePrefKey.READER_DISCOVER_WELCOME_BANNER_SHOWN.name,
+            DeletablePrefKey.SHOULD_SCHEDULE_CREATE_SITE_NOTIFICATION.name,
+            DeletablePrefKey.SKIPPED_BLOGGING_PROMPT_DAY.name,
+            UndeletablePrefKey.THEME_IMAGE_SIZE_WIDTH.name,
+            UndeletablePrefKey.AZTEC_EDITOR_TOOLBAR_EXPANDED.name,
+            UndeletablePrefKey.BOOKMARKS_SAVED_LOCALLY_DIALOG_SHOWN.name,
+            UndeletablePrefKey.IMAGE_OPTIMIZE_PROMO_REQUIRED.name,
+            UndeletablePrefKey.SWIPE_TO_NAVIGATE_NOTIFICATIONS.name,
+            UndeletablePrefKey.SWIPE_TO_NAVIGATE_READER.name,
+            UndeletablePrefKey.ASKED_PERMISSION_STORAGE_WRITE.name,
+            UndeletablePrefKey.ASKED_PERMISSION_STORAGE_READ.name,
+            UndeletablePrefKey.ASKED_PERMISSION_CAMERA.name,
+            UndeletablePrefKey.SYSTEM_NOTIFICATIONS_ENABLED.name,
+            UndeletablePrefKey.SHOULD_SHOW_POST_SIGNUP_INTERSTITIAL.name,
+            UndeletablePrefKey.IS_MAIN_FAB_TOOLTIP_DISABLED.name,
+            UndeletablePrefKey.SHOULD_SHOW_STORIES_INTRO.name,
+            UndeletablePrefKey.SHOULD_SHOW_STORAGE_WARNING.name,
+            UndeletablePrefKey.GUTENBERG_BLOCK_TYPE_IMPRESSIONS.name
+    )
+
+    private val userFlagsCompositeKeysSet: Set<String> = setOf(
+            DeletablePrefKey.LAST_SELECTED_QUICK_START_TYPE.name,
+            DeletablePrefKey.SHOULD_SHOW_WEEKLY_ROUNDUP_NOTIFICATION.name,
+            DeletablePrefKey.MANUAL_FEATURE_CONFIG.name,
+            DeletablePrefKey.SITE_JETPACK_CAPABILITIES.name,
+            DeletablePrefKey.BLOGGING_REMINDERS_SHOWN.name
+
+    )
 
     override fun onCreate(): Boolean {
         return true
@@ -40,6 +97,12 @@ class UserFlagsProvider : QueryContentProvider() {
                 val callerExpectedSignatureHash = jetpackPublicData.currentPublicKeyHash()
                 if (callerPackageId == callerExpectedPackageId && callerSignatureHash == callerExpectedSignatureHash) {
                     val userFlagsMap = appPrefsWrapper.getAllPrefs()
+                            .filter { entry ->
+                                userFlagsKeysSet.contains(entry.key)
+                                        || userFlagsCompositeKeysSet.firstOrNull { flagKey ->
+                                    entry.key.startsWith(flagKey)
+                                } != null
+                            }
                     queryResult.createCursor(userFlagsMap)
                 } else null
             } catch (signatureNotFoundException: SignatureNotFoundException) {
