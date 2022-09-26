ea825920691bfdd973deb192375d421055280766
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-09-26 16:01:17 +0300

    Analysis: Resolve deprecated coroutines thread module scope warnings
    Warning Messages: "'UI/DEFAULT_SCOPE: String' is deprecated. Implement
    CoroutineScope interface and cancel all child coroutines in
    onCleared/onDestroy/.."ª
    
    It seems that both, the deprecated named 'UI_SCOPE' and 'DEFAULT_SCOPE'
    coroutine scopes, that are being provided by the [ThreadModule] module,
    are no longer being utilized within this codebase and thus, they can be
    now safely removed. There was no need for any migration either.

--- WordPress/src/main/java/org/wordpress/android/modules/ThreadModule.kt
+++ WordPress/src/main/java/org/wordpress/android/modules/ThreadModule.kt
@@ -10,10 +10,6 @@ import kotlinx.coroutines.Dispatchers
 import org.wordpress.android.util.helpers.Debouncer
 import javax.inject.Named
 
-@Deprecated(message = "Implement CoroutineScope interface and cancel all child coroutines in onCleared/onDestroy/..")
-const val UI_SCOPE = "UI_SCOPE"
-@Deprecated(message = "Implement CoroutineScope interface and cancel all child coroutines in onCleared/onDestroy/..")
-const val DEFAULT_SCOPE = "DEFAULT_SCOPE"
 const val APPLICATION_SCOPE = "APPLICATION_SCOPE"
 const val UI_THREAD = "UI_THREAD"
 const val BG_THREAD = "BG_THREAD"
@@ -23,25 +19,9 @@ const val IO_THREAD = "IO_THREAD"
 @Module
 class ThreadModule {
     @Provides
-    @Named(UI_SCOPE)
-    fun provideUiScope(): CoroutineScope {
-        return CoroutineScope(Dispatchers.Main)
-    }
-
-    @Provides
     @Named(UI_THREAD)
     fun provideUiDispatcher(): CoroutineDispatcher {
         return Dispatchers.Main
-    }
-
-    @Provides
-    @Named(DEFAULT_SCOPE)
-    @Deprecated(
-            message = "CoroutineScope should be provided by an object which implements CoroutineScope",
-            replaceWith = ReplaceWith("Inject dispatcher and implement CoroutineScope interface")
-    )
-    fun provideBackgroundScope(): CoroutineScope {
-        return CoroutineScope(Dispatchers.Default)
     }
 
     @Provides
