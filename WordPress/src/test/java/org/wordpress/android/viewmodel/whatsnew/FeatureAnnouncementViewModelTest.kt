e6471d4cf17c5763aa15f606f71270589705c031
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-09-26 16:24:49 +0300

    Analysis: Resolve opt-in experimental coroutines api warnings for tests
    Warning Messages: "This declaration is experimental and its usage
    should be marked with '@kotlinx.coroutines.ExperimentalCoroutinesApi' or
    '@OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)'"

--- WordPress/src/test/java/org/wordpress/android/viewmodel/whatsnew/FeatureAnnouncementViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/whatsnew/FeatureAnnouncementViewModelTest.kt
@@ -6,6 +6,7 @@ import com.nhaarman.mockitokotlin2.anyOrNull
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
@@ -26,6 +27,7 @@ import org.wordpress.android.util.NoDelayCoroutineDispatcher
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 
 @InternalCoroutinesApi
+@ExperimentalCoroutinesApi
 class FeatureAnnouncementViewModelTest : BaseUnitTest() {
     private lateinit var viewModel: FeatureAnnouncementViewModel
     @Mock lateinit var onDialogClosedObserver: Observer<Unit>
