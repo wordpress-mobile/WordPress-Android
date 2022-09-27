0c5dfc92b5281b28b33ed60df9f95cc74b395236
Ovi Trif <octrif@gmail.com>
2022-09-27 13:08:53 +0200

    Extract CharSequence.enforceWesternArabicNumerals

--- WordPress/src/main/java/org/wordpress/android/widgets/MaterialTextViewWithNumerals.kt
+++ WordPress/src/main/java/org/wordpress/android/widgets/MaterialTextViewWithNumerals.kt
@@ -6,6 +6,7 @@ import android.text.Spanned
 import android.text.TextUtils
 import android.util.AttributeSet
 import com.google.android.material.textview.MaterialTextView
+import org.wordpress.android.util.extensions.enforceWesternArabicNumerals
 
 /**
  * MaterialTextView which enforces Western Arabic numerals.
@@ -17,22 +18,5 @@ class MaterialTextViewWithNumerals @JvmOverloads constructor(
 ) : MaterialTextView(context, attrs, defStyleAttr) {
     override fun setText(text: CharSequence?, type: BufferType?) {
         super.setText(text?.enforceWesternArabicNumerals(), type)
-    }
-
-    private fun CharSequence.enforceWesternArabicNumerals(): CharSequence {
-        val textWithArabicNumerals = this
-                // Replace Eastern Arabic numerals
-                .replace(Regex("[٠-٩]")) { match -> (match.value.single() - '٠').toString() }
-                // Replace Persian/Urdu numerals
-                .replace(Regex("[۰-۹]")) { match -> (match.value.single() - '۰').toString() }
-
-        val spannableText = SpannableString(textWithArabicNumerals)
-
-        // Restore spans if text is an instance of Spanned
-        if (this is Spanned) {
-            TextUtils.copySpansFrom(this, 0, this.length, null, spannableText, 0)
-        }
-
-        return spannableText
     }
 }
