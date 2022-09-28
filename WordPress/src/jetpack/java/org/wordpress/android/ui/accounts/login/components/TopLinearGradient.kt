00b915be271f7638bf1cd53a7a5bd8e0cab2e500
Ovi Trif <octrif@gmail.com>
2022-09-28 21:28:02 +0200

    Add preview for top linear gradient

--- WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/TopLinearGradient.kt
+++ WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/TopLinearGradient.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.accounts.login.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,9 +9,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import org.wordpress.android.R.drawable
 import org.wordpress.android.R.string
+import org.wordpress.android.ui.compose.theme.AppTheme
 
 @Composable
 fun TopLinearGradient(modifier: Modifier = Modifier) {
@@ -22,4 +25,13 @@ fun TopLinearGradient(modifier: Modifier = Modifier) {
                     .fillMaxWidth()
                     .fillMaxHeight(0.62f)
     )
+}
+
+@Preview(device = Devices.PIXEL_4_XL)
+@Preview(device = Devices.PIXEL_4_XL, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PreviewTopLinearGradient() {
+    AppTheme {
+        TopLinearGradient()
+    }
 }
