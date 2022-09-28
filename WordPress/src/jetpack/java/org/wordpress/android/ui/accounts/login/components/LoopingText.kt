a54bb92f2f664d4082f45b28760f3fdb907bb518
Ovi Trif <octrif@gmail.com>
2022-09-28 20:30:53 +0200

    Use annotated string to color large texts

--- WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LoopingText.kt
+++ WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LoopingText.kt
@@ -3,11 +3,13 @@ package org.wordpress.android.ui.accounts.login.components
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -17,40 +19,38 @@ import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.util.extensions.isOdd
 
 private val fontSize = 40.sp
-private val lineHeight = fontSize * 0.95
-
-@Composable
-private fun LargeText(
-    text: String,
-    color: Color,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-            text = text,
-            style = TextStyle(
-                    fontSize = fontSize,
-                    fontWeight = FontWeight.Bold,
-                    letterSpacing = (-0.03).sp,
-                    lineHeight = lineHeight,
-            ),
-            color = color,
-            modifier = modifier
-    )
-}
 
 @Composable
 private fun LargeTexts() {
     val texts = stringArrayResource(R.array.login_prologue_revamped_jetpack_feature_texts)
 
-    texts.forEachIndexed { index, text ->
-        LargeText(
-                text = text,
-                color = when (index.isOdd) {
-                    true -> colorResource(R.color.text_color_jetpack_login_feature_odd)
-                    false -> colorResource(R.color.text_color_jetpack_login_feature_even)
-                },
-        )
+    val oddColor = colorResource(R.color.text_color_jetpack_login_feature_odd)
+    val evenColor = colorResource(R.color.text_color_jetpack_login_feature_even)
+
+    val styledText = buildAnnotatedString {
+        texts.forEachIndexed { index, text ->
+            when (index.isOdd) {
+                true -> withStyle(SpanStyle(color = oddColor)) {
+                    append(text)
+                }
+                false -> withStyle(SpanStyle(color = evenColor)) {
+                    append(text)
+                }
+            }
+            if (index != texts.lastIndex) {
+                append("\n")
+            }
+        }
     }
+
+    Text(
+            text = styledText,
+            style = TextStyle(
+                    fontSize = fontSize,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = (-0.03).sp,
+            ),
+    )
 }
 
 @Composable
