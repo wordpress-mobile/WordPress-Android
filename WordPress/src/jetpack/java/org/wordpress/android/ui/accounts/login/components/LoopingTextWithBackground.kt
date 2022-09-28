c7e4dc96020db5dc1ea0cabccd2e161321fac9af
Ovi Trif <octrif@gmail.com>
2022-09-28 20:31:04 +0200

    Add preview for LoopingTextWithBackground

--- WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LoopingTextWithBackground.kt
+++ WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LoopingTextWithBackground.kt
@@ -11,9 +11,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.wordpress.android.R.drawable
 import org.wordpress.android.R.string
+import org.wordpress.android.ui.compose.theme.AppTheme
 
 @Composable
 fun LoopingTextWithBackground(
@@ -33,5 +36,13 @@ fun LoopingTextWithBackground(
                         .padding(horizontal = 20.dp)
                         .then(textModifier)
         )
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4_XL)
+@Composable
+fun PreviewLoopingTextWithBackground() {
+    AppTheme {
+        LoopingTextWithBackground()
     }
 }
