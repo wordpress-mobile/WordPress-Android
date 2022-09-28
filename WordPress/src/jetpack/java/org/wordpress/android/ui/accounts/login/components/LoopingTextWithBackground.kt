a3df92142a5761901939fb320ede11914b1dd78d
Ovi Trif <octrif@gmail.com>
2022-09-28 22:05:46 +0200

    Update login screen bg color

--- WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LoopingTextWithBackground.kt
+++ WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LoopingTextWithBackground.kt
@@ -5,17 +5,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.wordpress.android.R.drawable
-import org.wordpress.android.R.string
+import org.wordpress.android.R
 import org.wordpress.android.ui.compose.theme.AppTheme
 
 @Composable
@@ -23,10 +22,10 @@ fun LoopingTextWithBackground(
     modifier: Modifier = Modifier,
     textModifier: Modifier = Modifier,
 ) {
-    Box(modifier.background(MaterialTheme.colors.background)) {
+    Box(modifier.background(colorResource(R.color.bg_jetpack_login_splash))) {
         Image(
-                painter = painterResource(drawable.bg_jetpack_login_splash),
-                contentDescription = stringResource(string.login_prologue_revamped_content_description_bg),
+                painter = painterResource(R.drawable.bg_jetpack_login_splash),
+                contentDescription = stringResource(R.string.login_prologue_revamped_content_description_bg),
                 contentScale = ContentScale.FillBounds,
                 modifier = Modifier.matchParentSize(),
         )
