24670631bbaefa4a8fc6af5789902e02dfd8133b
Ovi Trif <octrif@gmail.com>
2022-09-28 19:56:24 +0200

    Tweak spacing and size of large texts

--- WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LoopingText.kt
+++ WordPress/src/jetpack/java/org/wordpress/android/ui/accounts/login/components/LoopingText.kt
@@ -1,7 +1,5 @@
 package org.wordpress.android.ui.accounts.login.components
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,14 +10,13 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.wordpress.android.R
 import org.wordpress.android.ui.accounts.login.LocalPosition
 import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.util.extensions.isOdd
 
-private val fontSize = 43.sp
+private val fontSize = 40.sp
 private val lineHeight = fontSize * 0.95
 
 @Composable
@@ -32,8 +29,8 @@ private fun LargeText(
             text = text,
             style = TextStyle(
                     fontSize = fontSize,
-                    fontWeight = FontWeight.SemiBold,
-                    letterSpacing = (-0.3).sp,
+                    fontWeight = FontWeight.Bold,
+                    letterSpacing = (-0.03).sp,
                     lineHeight = lineHeight,
             ),
             color = color,
@@ -51,9 +48,8 @@ private fun LargeTexts() {
                 color = when (index.isOdd) {
                     true -> colorResource(R.color.text_color_jetpack_login_feature_odd)
                     false -> colorResource(R.color.text_color_jetpack_login_feature_even)
-                }
+                },
         )
-        Spacer(modifier = Modifier.height(2.dp))
     }
 }
 
