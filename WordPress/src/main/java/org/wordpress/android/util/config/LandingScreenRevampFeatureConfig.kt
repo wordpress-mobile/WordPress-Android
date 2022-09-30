d1d45046240164f29b28e5a67423eaebcf815ef1
Ovi Trif <octrif@gmail.com>
2022-09-30 15:43:24 +0200

    Convert LandingScreenRevampFeatureConfig to remove config

--- WordPress/src/main/java/org/wordpress/android/util/config/LandingScreenRevampFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/LandingScreenRevampFeatureConfig.kt
@@ -1,12 +1,21 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.annotation.Feature
 import javax.inject.Inject
 
 /**
  * Configuration for the landing screen revamp work
  */
-@FeatureInDevelopment
-class LandingScreenRevampFeatureConfig
-@Inject constructor(appConfig: AppConfig) : FeatureConfig(appConfig, BuildConfig.LANDING_SCREEN_REVAMP)
+@Feature(LandingScreenRevampFeatureConfig.LANDING_SCREEN_REVAMP_REMOTE_FIELD, false)
+class LandingScreenRevampFeatureConfig @Inject constructor(
+    appConfig: AppConfig
+) : FeatureConfig(
+        appConfig,
+        BuildConfig.LANDING_SCREEN_REVAMP,
+        LANDING_SCREEN_REVAMP_REMOTE_FIELD,
+) {
+    companion object {
+        const val LANDING_SCREEN_REVAMP_REMOTE_FIELD = "landing_screen_revamp_remote_field"
+    }
+}
