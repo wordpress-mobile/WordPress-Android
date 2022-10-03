7811dd138074056d0363c5644752d2b883cb6678
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-10-03 18:01:06 +0300

    Analysis: Resolve detekt warnings by manually updating the baseline
    After resolving some of the compile related deprecated warnings, some
    Detekt baseline related warnings needed to be removed and suppressed
    closer to source because the lines on those classes got changed. Also, a
    few new Detekt violations appeared, like the new 'LongMethod' violation
    on the  'MeFragment' class for the 'onActivityResult(...)' function.
    Within this commit, those got suppressed as well so that Detekt
    completes successfully without any new issues.

--- WordPress/src/main/java/org/wordpress/android/ui/main/MeFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MeFragment.kt
@@ -451,7 +451,7 @@ class MeFragment : Fragment(R.layout.me_fragment), OnScrollToTopListener {
         disconnectProgressDialog = null
     }
 
-    @Suppress("DEPRECATION")
+    @Suppress("DEPRECATION", "LongMethod", "NestedBlockDepth")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
--- WordPress/src/main/java/org/wordpress/android/ui/mediapicker/MediaPickerActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mediapicker/MediaPickerActivity.kt
@@ -173,7 +173,7 @@ class MediaPickerActivity : LocaleAwareActivity(), MediaPickerListener {
         return super.onOptionsItemSelected(item)
     }
 
-    @Suppress("DEPRECATION")
+    @Suppress("DEPRECATION", "LongMethod", "NestedBlockDepth")
     override fun onActivityResult(
         requestCode: Int,
         resultCode: Int,
