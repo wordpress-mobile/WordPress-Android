0a31feba5e80b36e20e476327c9a9c0db547c817
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-10-04 13:19:56 -0500

    test: Remove unnecessary mocks

--- WordPress/src/test/java/org/wordpress/android/ui/deeplinks/handlers/HomeLinkHandlerTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/deeplinks/handlers/HomeLinkHandlerTest.kt
@@ -3,16 +3,10 @@ package org.wordpress.android.ui.deeplinks.handlers
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
-import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.deeplinks.DeepLinkNavigator.NavigateAction
 import org.wordpress.android.ui.deeplinks.buildUri
 
-@RunWith(MockitoJUnitRunner::class)
 class HomeLinkHandlerTest {
-    @Mock lateinit var site: SiteModel
     private lateinit var homeLinkHandler: HomeLinkHandler
 
     @Before
