fd4004057ed5293f97cd7a736bb44b37b42f6183
Ravi <ravi.program@gmail.com>
2022-10-10 17:20:31 +1100

    Update WP privacy url

--- WordPress/src/main/java/org/wordpress/android/ui/about/UnifiedAboutViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/about/UnifiedAboutViewModel.kt
@@ -58,7 +58,7 @@ class UnifiedAboutViewModel @Inject constructor(
             ),
             legalConfig = LegalConfig(
                     tosUrl = wpUrlUtils.buildTermsOfServiceUrl(contextProvider.getContext()),
-                    privacyPolicyUrl = Constants.URL_PRIVACY_POLICY,
+                    privacyPolicyUrl = if (buildConfig.isJetpackApp) Constants.URL_PRIVACY_POLICY else WP_PRIVACY_URL,
                     acknowledgementsUrl = LICENSES_FILE_URL
             ),
             automatticConfig = AutomatticConfig(isVisible = buildConfig.isJetpackApp),
@@ -126,6 +126,7 @@ class UnifiedAboutViewModel @Inject constructor(
         private const val WP_APPS_URL = "https://apps.wordpress.com"
         private const val WP_BLOG_URL = "https://wordpress.org/news/"
         private const val WP_CONTRIBUTE_URL = "https://make.wordpress.org/mobile/handbook/"
+        private const val WP_PRIVACY_URL = "https://wordpress.org/about/privacy/"
 
         private const val JP_SOCIAL_HANDLE = "jetpack"
         private const val JP_APPS_URL = "https://jetpack.com/app"
