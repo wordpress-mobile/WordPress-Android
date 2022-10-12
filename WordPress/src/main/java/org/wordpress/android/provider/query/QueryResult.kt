41e471bee6f17020ffdb0c31c3238942b7aefd8e
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2022-10-11 22:25:47 -0300

    Update QueryResult to accept Type parameter

--- WordPress/src/main/java/org/wordpress/android/provider/query/QueryResult.kt
+++ WordPress/src/main/java/org/wordpress/android/provider/query/QueryResult.kt
@@ -3,22 +3,25 @@ package org.wordpress.android.provider.query
 import android.database.Cursor
 import android.database.MatrixCursor
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import org.wordpress.android.fluxc.model.BloggingRemindersModel
+import java.lang.reflect.Type
 import javax.inject.Inject
 
 class QueryResult @Inject constructor() {
     @Suppress("TooGenericExceptionCaught", "SwallowedException")
-    inline fun <reified T> getValue(cursor: Cursor): T? {
+    inline fun <reified T : Any> getValue(cursor: Cursor, type: Type? = null): T? {
         cursor.moveToFirst()
         val value: String = cursor.getString(0)
         return try {
-            Gson().fromJson(value, T::class.java)
+            if (type != null) Gson().fromJson(value, type) else Gson().fromJson(value, T::class.java)
         } catch (exception: Exception) {
             null
         }
     }
 
-    inline fun <reified T> createCursor(value: T): Cursor {
-        val valueJson = Gson().toJson(value)
+    inline fun <reified T : Any> createCursor(value: T): Cursor {
+        val valueJson = Gson().toJson(value, value::class.java)
         val matrixCursor = MatrixCursor(arrayOf(KEY_QUERY_RESULT))
         matrixCursor.newRow().add(KEY_QUERY_RESULT, valueJson)
         return matrixCursor
