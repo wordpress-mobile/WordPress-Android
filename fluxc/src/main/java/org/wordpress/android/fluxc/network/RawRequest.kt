b43a005dc87c57c2d8de426d0e924f0a4d3c034d
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-10-17 17:22:36 +0100

    Add some comments to explain the usage of RawRequest

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/RawRequest.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/RawRequest.kt
@@ -5,18 +5,25 @@ import com.android.volley.Response
 import com.android.volley.Response.Listener
 import com.android.volley.toolbox.HttpHeaderParser
 
+/**
+ * A request that allows returning the network response untouched, this is useful when
+ * we want to read the network response headers.
+ */
 class RawRequest(
     method: Int,
     url: String,
     private val listener: Listener<NetworkResponse>,
     onErrorListener: BaseErrorListener
 ) : BaseRequest<NetworkResponse>(method, url, onErrorListener) {
-    override fun parseNetworkResponse(p0: NetworkResponse): Response<NetworkResponse> {
-        return Response.success(p0, HttpHeaderParser.parseCacheHeaders(p0))
+    override fun parseNetworkResponse(networkResponse: NetworkResponse): Response<NetworkResponse> {
+        return Response.success(
+            networkResponse,
+            HttpHeaderParser.parseCacheHeaders(networkResponse)
+        )
     }
 
-    override fun deliverResponse(p0: NetworkResponse) {
-        listener.onResponse(p0)
+    override fun deliverResponse(networkResponse: NetworkResponse) {
+        listener.onResponse(networkResponse)
     }
 
     override fun deliverBaseNetworkError(error: BaseNetworkError): BaseNetworkError = error
