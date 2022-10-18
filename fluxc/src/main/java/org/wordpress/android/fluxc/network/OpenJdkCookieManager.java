1fdd367b49199a6a321b2921e31df8baa8469318
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-10-18 13:34:26 +0100

    Multiple fixes to OpenJdkCookieManager to be able to support older APIs

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/OpenJdkCookieManager.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/OpenJdkCookieManager.java
@@ -1,7 +1,9 @@
 package org.wordpress.android.fluxc.network;
 
+import android.os.Build;
+
 import java.io.IOException;
-import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.net.CookiePolicy;
 import java.net.CookieStore;
 import java.net.HttpCookie;
@@ -10,6 +12,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class OpenJdkCookieManager extends CookieManager {
     /* ---------------- Fields -------------- */
@@ -55,9 +58,10 @@ public class OpenJdkCookieManager extends CookieManager {
             throw new IllegalArgumentException("Argument is null");
         }
 
+        Map<String, List<String>> cookieMap = new java.util.HashMap<>();
         // if there's no default CookieStore, no way for us to get any cookie
         if (getCookieStore() == null)
-            return Map.of();
+            return Collections.unmodifiableMap(cookieMap);
 
         boolean secureLink = "https".equalsIgnoreCase(uri.getScheme());
         List<HttpCookie> cookies = new java.util.ArrayList<>();
@@ -72,7 +76,7 @@ public class OpenJdkCookieManager extends CookieManager {
             if (pathMatches(path, cookie.getPath()) &&
                     (secureLink || !cookie.getSecure())) {
                 // Enforce httponly attribute
-                if (cookie.isHttpOnly()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && cookie.isHttpOnly()) {
                     String s = uri.getScheme();
                     if (!"http".equalsIgnoreCase(s) && !"https".equalsIgnoreCase(s)) {
                         continue;
@@ -95,15 +99,15 @@ public class OpenJdkCookieManager extends CookieManager {
         }
 
         // apply sort rule (RFC 2965 sec. 3.3.4)
-        List<String> cookieHeader = sortByPath(cookies);
+        List<String> cookieHeader = sortByPathAndAge(cookies);
 
-        return Map.of("Cookie", cookieHeader);
+        cookieMap.put("Cookie", cookieHeader);
+        return Collections.unmodifiableMap(cookieMap);
     }
 
     public void
     put(URI uri, Map<String, List<String>> responseHeaders)
-            throws IOException
-    {
+            throws IOException {
         // pre-condition check
         if (uri == null || responseHeaders == null) {
             throw new IllegalArgumentException("Argument is null");
@@ -114,7 +118,6 @@ public class OpenJdkCookieManager extends CookieManager {
         if (getCookieStore() == null)
             return;
 
-        PlatformLogger logger = PlatformLogger.getLogger("java.net.CookieManager");
         for (String headerKey : responseHeaders.keySet()) {
             // RFC 2965 3.2.2, key must be 'Set-Cookie2'
             // we also accept 'Set-Cookie' here for backward compatibility
@@ -122,8 +125,7 @@ public class OpenJdkCookieManager extends CookieManager {
                     || !(headerKey.equalsIgnoreCase("Set-Cookie2")
                     || headerKey.equalsIgnoreCase("Set-Cookie")
             )
-            )
-            {
+            ) {
                 continue;
             }
 
@@ -135,9 +137,6 @@ public class OpenJdkCookieManager extends CookieManager {
                     } catch (IllegalArgumentException e) {
                         // Bogus header, make an empty list and log the error
                         cookies = java.util.Collections.emptyList();
-                        if (logger.isLoggable(PlatformLogger.Level.SEVERE)) {
-                            logger.severe("Invalid cookie for " + uri + ": " + headerValue);
-                        }
                     }
                     for (HttpCookie cookie : cookies) {
                         if (cookie.getPath() == null) {
@@ -174,7 +173,7 @@ public class OpenJdkCookieManager extends CookieManager {
                             if (ports.isEmpty()) {
                                 // Empty port list means this should be restricted
                                 // to the incoming URI port
-                                cookie.setPortlist("" + port );
+                                cookie.setPortlist("" + port);
                                 if (shouldAcceptInternal(uri, cookie)) {
                                     getCookieStore().add(uri, cookie);
                                 }
@@ -208,7 +207,7 @@ public class OpenJdkCookieManager extends CookieManager {
     private boolean shouldAcceptInternal(URI uri, HttpCookie cookie) {
         try {
             return policyCallback.shouldAccept(uri, cookie);
-        } catch (Exception ignored) { // pretect against malicious callback
+        } catch (Exception ignored) { // protect against malicious callback
             return false;
         }
     }
@@ -219,13 +218,13 @@ public class OpenJdkCookieManager extends CookieManager {
         int val = -1;
         while (i > 0) {
             try {
-                val = Integer.parseInt(lst, 0, i, 10);
+                val = Integer.parseInt(lst.substring(0, i));
                 if (val == port) {
                     return true;
                 }
             } catch (NumberFormatException numberFormatException) {
             }
-            lst = lst.substring(i+1);
+            lst = lst.substring(i + 1);
             i = lst.indexOf(',');
         }
         if (!lst.isEmpty()) {
@@ -244,7 +243,7 @@ public class OpenJdkCookieManager extends CookieManager {
      * path-matches algorithm, as defined by RFC 2965
      */
     private boolean pathMatches(String path, String pathToMatchWith) {
-        if (path == pathToMatchWith)
+        if (Objects.equals(path, pathToMatchWith))
             return true;
         if (path == null || pathToMatchWith == null)
             return false;
@@ -256,11 +255,12 @@ public class OpenJdkCookieManager extends CookieManager {
 
 
     /*
-     * sort cookies with respect to their path: those with more specific Path attributes
-     * precede those with less specific, as defined in RFC 2965 sec. 3.3.4
+     * sort cookies with respect to their path and age: those with more longer Path attributes
+     * precede those with shorter, as defined in RFC 6265. Cookies with the same length
+     * path are distinguished by creation time (older first). Method made PP to enable testing.
      */
-    private List<String> sortByPath(List<HttpCookie> cookies) {
-        Collections.sort(cookies, new CookiePathComparator());
+    static List<String> sortByPathAndAge(List<HttpCookie> cookies) {
+        Collections.sort(cookies, new CookieComparator());
 
         List<String> cookieHeader = new java.util.ArrayList<>();
         for (HttpCookie cookie : cookies) {
@@ -278,22 +278,30 @@ public class OpenJdkCookieManager extends CookieManager {
     }
 
 
-    static class CookiePathComparator implements Comparator<HttpCookie> {
+    // Comparator compares the length of the path. Longer paths should precede shorter ones.
+    // As per rfc6265 cookies with equal path lengths sort on creation time.
+
+    static class CookieComparator implements Comparator<HttpCookie> {
         public int compare(HttpCookie c1, HttpCookie c2) {
             if (c1 == c2) return 0;
             if (c1 == null) return -1;
             if (c2 == null) return 1;
 
-            // path rule only applies to the cookies with same name
-            if (!c1.getName().equals(c2.getName())) return 0;
-
-            // those with more specific Path attributes precede those with less specific
-            if (c1.getPath().startsWith(c2.getPath()))
+            String p1 = c1.getPath();
+            String p2 = c2.getPath();
+            p1 = (p1 == null) ? "" : p1;
+            p2 = (p2 == null) ? "" : p2;
+            int len1 = p1.length();
+            int len2 = p2.length();
+            if (len1 > len2)
                 return -1;
-            else if (c2.getPath().startsWith(c1.getPath()))
+            if (len2 > len1)
                 return 1;
-            else
-                return 0;
+
+            // Check creation time. Sort older first
+            long creation1 = c1.getCreationTime();
+            long creation2 = c2.getCreationTime();
+            return Long.compare(creation1, creation2);
         }
     }
 }
