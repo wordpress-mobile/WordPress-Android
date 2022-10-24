db215d8a60bbc3ebb7bc02c799212b19c0262ca0
Wojtek Zieba <wojtek.zieba@automattic.com>
2022-10-24 13:00:27 +0200

    Revert "fix: check if on main thread before creating webview for user agent get"
    This reverts commit c0a0eaefc66ff9674030fbf212f70fd9db1de977.

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.java
@@ -1,7 +1,6 @@
 package org.wordpress.android.fluxc.network;
 
 import android.content.Context;
-import android.os.Looper;
 import android.webkit.WebView;
 
 import org.wordpress.android.util.PackageUtils;
@@ -10,23 +9,16 @@ public class UserAgent {
     private String mUserAgent;
     private String mDefaultUserAgent;
 
-    private static final String DEFAULT_USER_AGENT =
-            "Mozilla/5.0 (Linux; Android 6.0; default FluxC UA; wv) AppleWebKit/537.36"
-            + "(KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36";
-
     public UserAgent(Context appContext, String appName) {
-        if (isMainThread()) {
-            // Device's default User-Agent string.
-            // E.g.:
-            //   "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
-            //   AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36"
-            try {
-                mDefaultUserAgent = new WebView(appContext).getSettings().getUserAgentString();
-            } catch (RuntimeException re) {
-                mDefaultUserAgent = DEFAULT_USER_AGENT;
-            }
-        } else {
-            mDefaultUserAgent = DEFAULT_USER_AGENT;
+        // Device's default User-Agent string.
+        // E.g.:
+        //   "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
+        //   AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36"
+        try {
+            mDefaultUserAgent = new WebView(appContext).getSettings().getUserAgentString();
+        } catch (RuntimeException re) {
+            mDefaultUserAgent = "Mozilla/5.0 (Linux; Android 6.0; default FluxC UA; wv) AppleWebKit/537.36"
+                    + "(KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36";
         }
         // User-Agent string when making HTTP connections, for both API traffic and WebViews.
         // Appends "wp-android/version" to WebView's default User-Agent string for the webservers
@@ -35,10 +27,6 @@ public class UserAgent {
         //    AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36
         //    wp-android/4.7"
         mUserAgent = mDefaultUserAgent + " " + appName + "/" + PackageUtils.getVersionName(appContext);
-    }
-
-    private boolean isMainThread() {
-        return Looper.myLooper() == Looper.getMainLooper();
     }
 
     public String getUserAgent() {
