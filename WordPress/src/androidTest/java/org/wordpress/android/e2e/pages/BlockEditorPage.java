0e6b3ee02579960be8afb1e2d0438bfced365d69
pachlava <pachlava@gmail.com>
2022-10-25 22:36:31 +0300

    Added a retry to open editor menu if it did not work on first try.

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/BlockEditorPage.java
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/pages/BlockEditorPage.java
@@ -28,6 +28,7 @@ import static org.wordpress.android.support.WPSupportUtils.withIndex;
 
 public class BlockEditorPage {
     private static ViewInteraction titleField = onView(withHint("Add title"));
+    private static ViewInteraction postSettingButton = onView(withText(R.string.post_settings));
 
     private ViewInteraction mEditor;
 
@@ -74,10 +75,25 @@ public class BlockEditorPage {
         return this;
     }
 
-    public void openPostSetting() {
+    /**
+     * Taps the three vertical dots menu button located at the editor screen top right.
+     * Since the tap does not always succeed on FTL, one retry is used
+     */
+    public void openPostKebabMenu() {
         waitForElementToBeDisplayed(R.id.toolbar_main);
+
+        // First attempt to tap the kebab menu (three dots)
         openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
-        clickOn(onView(withText(R.string.post_settings)));
+
+        // Check if the attempt succeeded, retry once if not
+        if (!waitForElementToBeDisplayedWithoutFailure(postSettingButton)) {
+            openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        }
+    }
+
+    public void openPostSetting() {
+        openPostKebabMenu();
+        clickOn(postSettingButton);
     }
 
     public void addCategory(String category) {
