e34a5b6df0bb4daea94dc479236fe8b64c077c90
Ravi <ravi.program@gmail.com>
2022-10-27 13:02:10 +1100

    Remove unused import

--- WordPress/src/main/java/org/wordpress/android/ui/main/WPMainActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/WPMainActivity.java
@@ -3,7 +3,6 @@ package org.wordpress.android.ui.main;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
