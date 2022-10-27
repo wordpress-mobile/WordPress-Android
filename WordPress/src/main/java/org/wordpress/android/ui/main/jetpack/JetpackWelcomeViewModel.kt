a3694baa3a2e180575092d672c91d5e821a21cb2
Ovi Trif <octrif@gmail.com>
2022-10-27 14:46:18 +0200

    Fetch site list from store and map to ui model

--- WordPress/src/main/java/org/wordpress/android/ui/main/jetpack/JetpackWelcomeViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/jetpack/JetpackWelcomeViewModel.kt
@@ -1,20 +1,29 @@
 package org.wordpress.android.ui.main.jetpack
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import org.wordpress.android.R
+import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.ui.main.jetpack.JetpackWelcomeUiState.Content
+import org.wordpress.android.ui.main.jetpack.JetpackWelcomeUiState.Initial
+import org.wordpress.android.ui.main.jetpack.JetpackWelcomeUiState.SiteListItem
 import org.wordpress.android.ui.utils.UiString
 import org.wordpress.android.ui.utils.UiString.UiStringRes
+import org.wordpress.android.util.SiteUtilsWrapper
 import javax.inject.Inject
+
+const val SITE_ICON_SIZE = 60 * 3
 
 @HiltViewModel
 class JetpackWelcomeViewModel @Inject constructor(
+    val siteStore: SiteStore,
+    val siteUtilsWrapper: SiteUtilsWrapper,
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow<JetpackWelcomeUiState>(
-            JetpackWelcomeUiState.Content.SiteList(previewSiteListItems())
-    )
+    private val _uiState = MutableStateFlow<JetpackWelcomeUiState>(Initial)
     val uiState: StateFlow<JetpackWelcomeUiState> = _uiState
 
     fun start() {
@@ -22,19 +31,39 @@ class JetpackWelcomeViewModel @Inject constructor(
     }
 
     private fun initOrRestoreUiState() {
-        // TODO load sites list
+        getSiteList()
+    }
+
+    private fun getSiteList() {
+        val sites = siteStore.sites
+        val siteListItems = sites.map { site ->
+            SiteListItem(
+                    id = site.siteId,
+                    name = siteUtilsWrapper.getSiteNameOrHomeURL(site),
+                    url = siteUtilsWrapper.getHomeURLOrHostName(site),
+                    iconUrl = siteUtilsWrapper.getSiteIconUrl(site, SITE_ICON_SIZE),
+            )
+        }
+
+        postUiState(Content.SiteList(siteListItems))
+    }
+
+    private fun postUiState(state: JetpackWelcomeUiState) {
+        viewModelScope.launch {
+            _uiState.value = state
+        }
     }
 }
 
 sealed class JetpackWelcomeUiState {
     object Initial : JetpackWelcomeUiState()
 
-    sealed class Content: JetpackWelcomeUiState() {
+    sealed class Content : JetpackWelcomeUiState() {
         abstract val title: UiString
         abstract val subtitle: UiString
         abstract val message: UiString
 
-        data class SiteList (
+        data class SiteList(
             val sites: List<SiteListItem>
         ) : Content() {
             override val title = UiStringRes(R.string.jp_welcome_title)
