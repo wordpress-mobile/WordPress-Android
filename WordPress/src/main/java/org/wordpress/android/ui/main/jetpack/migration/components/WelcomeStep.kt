505f2a13310291ba4406995e17df66283b30fa95
Ovi Trif <octrif@gmail.com>
2022-10-29 17:52:14 +0200

    Add buttons to ui of welcome screen

--- WordPress/src/main/java/org/wordpress/android/ui/main/jetpack/migration/components/WelcomeStep.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/jetpack/migration/components/WelcomeStep.kt
@@ -1,5 +1,7 @@
 package org.wordpress.android.ui.main.jetpack.migration.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import org.wordpress.android.ui.compose.utils.uiStringText
 import org.wordpress.android.ui.main.jetpack.migration.JetpackMigrationViewModel.StepUiState
@@ -10,4 +12,26 @@ fun WelcomeStep(uiState: StepUiState.Welcome) = with(uiState) {
     Title(text = uiStringText(title))
     Subtitle(text = uiStringText(subtitle))
     Message(text = uiStringText(message))
+
+    Box {
+        ButtonsPanel {
+            PrimaryButton(
+                    text = uiStringText(primaryActionButton.text),
+                    onClick = primaryActionButton.onClick,
+            )
+            SecondaryButton(
+                    text = uiStringText(secondaryActionButton.text),
+                    onClick = secondaryActionButton.onClick,
+            )
+        }
+    }
+}
+
+@Composable
+private fun ButtonsPanel(
+    content: @Composable () -> Unit,
+) {
+    Column {
+        content()
+    }
 }
