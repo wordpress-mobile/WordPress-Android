dc7ba567078f2f4a470554c72382bf6bd9029276
Matthew Kevins <mmkevins@yahoo.com>
2022-10-31 16:11:26 +1000

    Implement trusted provider to migrate content from WordPress to Jetpack
    This serves as a basic foundation to transmit arbitrary data over the
    same provider, utilizing the URI path of the query as a form of
    multiplexing. This will make it more convenient to tightly enforce types
    in transit for a growing variety of data shapes.

---
+++ WordPress/src/main/java/org/wordpress/android/localcontentmigration/LocalContentMigrationProvider.kt
@@ -0,0 +1,112 @@
+package org.wordpress.android.localcontentmigration
+
+import android.database.Cursor
+import android.net.Uri
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
+import org.wordpress.android.fluxc.model.PostModel
+import org.wordpress.android.fluxc.store.PostStore
+import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.localcontentmigration.LocalContentEntityData.EligibilityStatusData
+import org.wordpress.android.localcontentmigration.LocalContentEntityData.PostData
+import org.wordpress.android.localcontentmigration.LocalContentEntity.EligibilityStatus
+import org.wordpress.android.localcontentmigration.LocalContentEntity.Post
+import org.wordpress.android.localcontentmigration.LocalContentEntity.Site
+import org.wordpress.android.localcontentmigration.LocalContentEntityData.PostsData
+import org.wordpress.android.localcontentmigration.LocalContentEntityData.SitesData
+import org.wordpress.android.provider.query.QueryResult
+import java.lang.Integer.parseInt
+
+class LocalContentMigrationProvider: TrustedQueryContentProvider() {
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface LocalContentMigrationProviderEntryPoint {
+        fun queryResult(): QueryResult
+        fun siteStore(): SiteStore
+        fun postStore(): PostStore
+    }
+
+    override fun query(uri: Uri): Cursor {
+        val path = uri.path!!
+        val (entity, groups) = LocalContentEntity.values().firstNotNullOf { entity ->
+            entity.contentIdCapturePattern.find(path)?.let { match ->
+                return@firstNotNullOf Pair(entity, match.groups)
+            }
+        }
+        val parameters = groups.drop(1).mapNotNull { it?.value?.let { id -> parseInt(id) } }
+        val siteId = parameters.getOrNull(0)
+        val entityId = parameters.getOrNull(1)
+        return query(entity, siteId, entityId)
+    }
+
+    private fun query(entity: LocalContentEntity, siteId: Int?, entityId: Int?): Cursor {
+        with(EntryPointAccessors.fromApplication(requireContext().applicationContext,
+                LocalContentMigrationProviderEntryPoint::class.java)) {
+            val response = when (entity) {
+                EligibilityStatus -> getEligibilityStatus(siteStore(), postStore())
+                Site -> getSiteData(siteStore())
+                Post -> getPostData(siteStore(), postStore(), siteId!!, entityId)
+            }
+            return queryResult().createCursor(response)
+        }
+    }
+
+    private fun getEligibilityStatus(siteStore: SiteStore, postStore: PostStore) : LocalContentEntityData {
+        postStore.toString()
+        // TODO: check for eligibility on-the-fly
+        return EligibilityStatusData(true, siteStore.sitesCount)
+    }
+
+    private fun getSiteData(siteStore: SiteStore) : LocalContentEntityData {
+        return SitesData(localIds = siteStore.sites.map { it.id })
+    }
+    private fun getPostData(siteStore: SiteStore, postStore: PostStore, siteId: Int, postId: Int?)
+            : LocalContentEntityData {
+        postId?.let {
+            val post = postStore.getPostByLocalPostId(it)
+            return PostData(post = post)
+        } ?: run {
+            val site = siteStore.getSiteByLocalId(siteId)
+            return PostsData(localIds = postStore.getPostsForSite(site).mapNotNull { it.id })
+        }
+    }
+}
+
+enum class LocalContentEntity {
+    EligibilityStatus {
+        override val contentIdCapturePattern: Regex
+            get() = Regex(this.name)
+
+        override fun getPathForContent(siteId: Int?, entityId: Int?) : String {
+            return this.name
+        }
+    },
+    Site {
+        override val contentIdCapturePattern: Regex
+            get() = Regex(this.name)
+
+        override fun getPathForContent(siteId: Int?, entityId: Int?): String {
+            return this.name
+        }
+    },
+    Post,
+    ;
+
+    open val contentIdCapturePattern = Regex("site/(\\d+)/${this.name}(?:/(\\d*))?")
+    open fun getPathForContent(siteId: Int?, entityId: Int?) : String {
+        return "site/${siteId}/${this.name}${ entityId?.let { "/${it}" } ?: "" }"
+    }
+}
+
+sealed class LocalContentEntityData {
+    data class EligibilityStatusData(
+        val isEligible: Boolean,
+        val siteCount: Int,
+    ): LocalContentEntityData()
+
+    data class SitesData(val localIds: List<Int>): LocalContentEntityData()
+    data class PostsData(val localIds: List<Int>): LocalContentEntityData()
+    data class PostData(val post: PostModel) : LocalContentEntityData()
+}
---
+++ WordPress/src/main/java/org/wordpress/android/localcontentmigration/TrustedQueryContentProvider.kt
@@ -0,0 +1,40 @@
+package org.wordpress.android.localcontentmigration
+
+import android.database.Cursor
+import android.net.Uri
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
+import org.wordpress.android.provider.query.QueryContentProvider
+import org.wordpress.android.util.config.JetpackProviderSyncFeatureConfig
+import org.wordpress.android.util.publicdata.ClientVerification
+
+abstract class TrustedQueryContentProvider : QueryContentProvider() {
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface TrustedQueryContentProviderEntryPoint {
+        fun jetpackProviderSyncFeatureConfig(): JetpackProviderSyncFeatureConfig
+        fun clientVerification(): ClientVerification
+    }
+
+    override fun onCreate() = true
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        with (EntryPointAccessors.fromApplication(requireContext().applicationContext,
+                TrustedQueryContentProviderEntryPoint::class.java)) {
+            return if (jetpackProviderSyncFeatureConfig().isEnabled() && clientVerification().canTrust(callingPackage))
+                query(uri)
+            else
+                null
+        }
+    }
+
+    abstract fun query(uri: Uri) : Cursor?
+}
