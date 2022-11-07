c1fa8d146667a163c869c332bba4d7b1f400c466
develric <develric@gmail.com>
2022-11-07 23:08:31 +0100

    Adding copyQsDataWithIndexes and refactor to dry.

--- WordPress/src/main/java/org/wordpress/android/resolver/ResolverUtility.kt
+++ WordPress/src/main/java/org/wordpress/android/resolver/ResolverUtility.kt
@@ -1,8 +1,13 @@
 package org.wordpress.android.resolver
 
+import com.wellsql.generated.QuickStartStatusModelMapper
+import com.wellsql.generated.QuickStartTaskModelMapper
 import com.wellsql.generated.SiteModelMapper
+import com.yarolegovich.wellsql.core.Identifiable
 import com.yarolegovich.wellsql.mapper.MapperAdapter
 import dagger.Reusable
+import org.wordpress.android.fluxc.model.QuickStartStatusModel
+import org.wordpress.android.fluxc.model.QuickStartTaskModel
 import org.wordpress.android.fluxc.model.SiteModel
 import javax.inject.Inject
 
@@ -10,32 +15,73 @@ import javax.inject.Inject
 class ResolverUtility @Inject constructor(
     private val dbWrapper: DbWrapper
 ) {
-    fun copySitesWithIndexes(sites: List<SiteModel>) {
+    private fun <T : Identifiable> copyWithIndexes(
+        tableName: String,
+        mapperAdapter: MapperAdapter<T>,
+        items: List<T>
+    ): Boolean {
+        if (items.isEmpty()) return true
+
+        var wasSuccessful: Boolean
+
         val db = dbWrapper.giveMeWritableDb()
         db.beginTransaction()
         try {
-            db.delete("SiteModel", null, null)
-            db.delete("sqlite_sequence", "name='SiteModel'", null)
-            val mapperAdapter = MapperAdapter(SiteModelMapper())
-            val orderedSites = sites.sortedBy { it.id }
+            db.delete(tableName, null, null)
+            db.delete("sqlite_sequence", "name='$tableName'", null)
+            val orderedItems = items.sortedBy { it.id }
 
-            // pre-populate sites; this also has the effect of adding and logging in self-hosted sites if present
-            for ((index, site) in orderedSites.withIndex()) {
+            for ((index, item) in orderedItems.withIndex()) {
                 val sqlStatement = if (index == 0) {
-                    db.compileStatement("INSERT INTO SQLITE_SEQUENCE (name,seq) VALUES ('SiteModel', ?)")
+                    db.compileStatement("INSERT INTO SQLITE_SEQUENCE (name,seq) VALUES ('$tableName', ?)")
                 } else {
-                    db.compileStatement("UPDATE SQLITE_SEQUENCE SET seq=? WHERE name='SiteModel'")
+                    db.compileStatement("UPDATE SQLITE_SEQUENCE SET seq=? WHERE name='$tableName'")
                 }
 
-                sqlStatement.bindLong(1, (site.id - 1).toLong())
+                sqlStatement.bindLong(1, (item.id - 1).toLong())
                 sqlStatement.execute()
 
-                db.insert("SiteModel", null, mapperAdapter.toCv(site))
+                db.insert(tableName, null, mapperAdapter.toCv(item))
             }
 
             db.setTransactionSuccessful()
+            wasSuccessful = true
         } finally {
             db.endTransaction()
         }
+        return wasSuccessful
+    }
+
+    fun copySitesWithIndexes(sites: List<SiteModel>) {
+        copyWithIndexes("SiteModel", MapperAdapter(SiteModelMapper()), sites)
+    }
+
+    fun copyQsDataWithIndexes(statusList: List<QuickStartStatusModel>, taskList: List<QuickStartTaskModel>): Boolean {
+        var wasSuccessful = false
+
+        val db = dbWrapper.giveMeWritableDb()
+        db.beginTransaction()
+
+        try {
+            if (
+                    copyWithIndexes(
+                            "QuickStartStatusModel",
+                            MapperAdapter(QuickStartStatusModelMapper()),
+                            statusList
+                    )
+                    && copyWithIndexes(
+                            "QuickStartTaskModel",
+                            MapperAdapter(QuickStartTaskModelMapper()),
+                            taskList
+                    )
+            ) {
+                db.setTransactionSuccessful()
+                wasSuccessful = true
+            }
+        } finally {
+            db.endTransaction()
+        }
+
+        return wasSuccessful
     }
 }
