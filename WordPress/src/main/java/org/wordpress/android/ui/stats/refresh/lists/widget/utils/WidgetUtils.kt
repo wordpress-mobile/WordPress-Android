d9e8e53a4e3cde1806b4bfc91e4a35fa9ddeadb3
Matthew Kevins <mmkevins@yahoo.com>
2022-11-08 14:17:17 +1000

    Set the pending intent template with FLAG_MUTABLE
    This is necessary in order for widget subview click-handlers to be
    differentiated. This should be ok to mark mutable since this is an
    explicit intent.

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/widget/utils/WidgetUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/widget/utils/WidgetUtils.kt
@@ -195,7 +195,7 @@ class WidgetUtils
                 context,
                 getRandomId(),
                 intent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
         )
     }
 
