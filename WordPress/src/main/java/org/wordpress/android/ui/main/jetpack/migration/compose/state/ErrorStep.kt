d8969ae64ca460121e52edfe958c82055915cd7c
Ovi Trif <octrif@gmail.com>
2022-11-18 16:53:00 +0100

    Implement UI for errors

--- WordPress/src/main/java/org/wordpress/android/ui/main/jetpack/migration/compose/state/ErrorStep.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/jetpack/migration/compose/state/ErrorStep.kt
@@ -1,16 +1,52 @@
 package org.wordpress.android.ui.main.jetpack.migration.compose.state
 
 import android.content.res.Configuration
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import org.wordpress.android.ui.compose.theme.AppTheme
+import org.wordpress.android.ui.compose.utils.uiStringText
 import org.wordpress.android.ui.main.jetpack.migration.JetpackMigrationViewModel.UiState
+import org.wordpress.android.ui.main.jetpack.migration.compose.components.ButtonsColumn
+import org.wordpress.android.ui.main.jetpack.migration.compose.components.Message
+import org.wordpress.android.ui.main.jetpack.migration.compose.components.PrimaryButton
+import org.wordpress.android.ui.main.jetpack.migration.compose.components.ScreenIcon
+import org.wordpress.android.ui.main.jetpack.migration.compose.components.SecondaryButton
+import org.wordpress.android.ui.main.jetpack.migration.compose.components.Subtitle
+import org.wordpress.android.ui.main.jetpack.migration.compose.components.Title
 
 @Composable
 fun ErrorStep(uiState: UiState.Error) = with(uiState) {
-    Text(text = "TODO")
+    Column(
+            modifier = Modifier.fillMaxSize()
+    ) {
+        val scrollState = rememberScrollState()
+        Column(
+                modifier = Modifier
+                        .verticalScroll(scrollState)
+                        .weight(1f)
+        ) {
+            ScreenIcon(iconRes = screenIconRes)
+            Title(text = uiStringText(title))
+            Subtitle(text = uiStringText(subtitle))
+            Message(text = uiStringText(message))
+        }
+        ButtonsColumn {
+            PrimaryButton(
+                    text = uiStringText(primaryActionButton.text),
+                    onClick = primaryActionButton.onClick,
+            )
+            SecondaryButton(
+                    text = uiStringText(secondaryActionButton.text),
+                    onClick = secondaryActionButton.onClick,
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4_XL)
