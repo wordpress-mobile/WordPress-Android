ccb280d5b57fca11dbd9f9f5a519af930b936e13
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2022-11-20 14:51:46 -0500

    Add helper to maanage the rules around showing the open web links with jetpack overlay

---
+++ WordPress/src/main/java/org/wordpress/android/ui/deeplinks/DeepLinkOpenWebLinksWithJetpackHelper.kt
@@ -0,0 +1,59 @@
+package org.wordpress.android.ui.deeplinks
+
+import org.wordpress.android.ui.prefs.AppPrefsWrapper
+import org.wordpress.android.util.DateTimeUtilsWrapper
+import org.wordpress.android.util.FirebaseRemoteConfigWrapper
+import org.wordpress.android.util.PackageManagerWrapper
+import org.wordpress.android.util.config.OpenWebLinksWithJetpackFlowFeatureConfig
+import java.util.Date
+import javax.inject.Inject
+
+class DeepLinkOpenWebLinksWithJetpackHelper @Inject constructor(
+    private val openWebLinksWithJetpackFlowFeatureConfig: OpenWebLinksWithJetpackFlowFeatureConfig,
+    private val appPrefsWrapper: AppPrefsWrapper,
+    private val firebaseRemoteConfigWrapper: FirebaseRemoteConfigWrapper,
+    private val packageManagerWrapper: PackageManagerWrapper,
+    private val dateTimeUtilsWrapper: DateTimeUtilsWrapper
+) {
+    fun shouldShowDeepLinkOpenWebLinksWithJetpackOverlay() = showOverlay()
+
+    private fun showOverlay() : Boolean {
+        return openWebLinksWithJetpackFlowFeatureConfig.isEnabled()
+                && isJetpackInstalled()
+                && isWebDeepLinkHandlerComponentEnabled()
+                && isValidOverlayFrequency()
+    }
+
+    private fun isJetpackInstalled() = packageManagerWrapper.isPackageInstalled(JETPACK_PACKAGE_NAME)
+
+    private fun isWebDeepLinkHandlerComponentEnabled() =
+        packageManagerWrapper.isComponentEnabledSettingEnabled(DeepLinkingIntentReceiverActivity::class.java)
+
+    private fun isValidOverlayFrequency() : Boolean {
+        if (!hasOverlayBeenShown()) return true // short circuit if the overlay has never been shown
+
+        return hasExceededOverlayFrequency()
+    }
+
+    private fun hasExceededOverlayFrequency() : Boolean {
+        val frequency = firebaseRemoteConfigWrapper.getOpenWebLinksWithJetpackFlowFrequency()
+        if (frequency == 0L) return false // Only show the overlay 1X and it's already been shown do not show again
+
+        val lastShownDate = Date(getOpenWebLinksWithJetpackOverlayLastShownTimestamp())
+        val daysPastOverlayShown = dateTimeUtilsWrapper.daysBetween(
+                lastShownDate,
+                getTodaysDate())
+        return daysPastOverlayShown >= frequency
+    }
+
+    private fun hasOverlayBeenShown() = getOpenWebLinksWithJetpackOverlayLastShownTimestamp() > 0L
+
+    private fun getOpenWebLinksWithJetpackOverlayLastShownTimestamp() =
+            appPrefsWrapper.getOpenWebLinksWithJetpackOverlayLastShownTimestamp()
+
+    private fun getTodaysDate() = Date(System.currentTimeMillis())
+
+    companion object {
+        const val JETPACK_PACKAGE_NAME = "com.jetpack.android"
+    }
+}
