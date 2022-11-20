ed1c48fe1b3cbc325073417b811d1551970b256b
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2022-11-20 14:46:54 -0500

    Add feature flag for open web links with jetpack

---
+++ WordPress/src/main/java/org/wordpress/android/util/config/OpenWebLinksWithJetpackFlowFeatureConfig.kt
@@ -0,0 +1,20 @@
+package org.wordpress.android.util.config
+
+import org.wordpress.android.BuildConfig
+import org.wordpress.android.annotation.Feature
+import javax.inject.Inject
+
+@Feature(OpenWebLinksWithJetpackFlowFeatureConfig.OPEN_WEB_LINKS_WITH_JETPACK_FLOW, true)
+class OpenWebLinksWithJetpackFlowFeatureConfig
+@Inject constructor(appConfig: AppConfig) : FeatureConfig(
+        appConfig,
+        BuildConfig.OPEN_WEB_LINKS_WITH_JETPACK_FLOW,
+        OPEN_WEB_LINKS_WITH_JETPACK_FLOW
+) {
+    override fun isEnabled(): Boolean {
+        return BuildConfig.ENABLE_OPEN_WEB_LINKS_WITH_JP_FLOW && super.isEnabled()
+    }
+    companion object {
+        const val OPEN_WEB_LINKS_WITH_JETPACK_FLOW = "open_web_links_with_jetpack_flow"
+    }
+}
