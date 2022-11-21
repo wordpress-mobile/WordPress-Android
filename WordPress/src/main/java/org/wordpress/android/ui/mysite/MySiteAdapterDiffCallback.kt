e950a413313086a4b0b104ec5dfc48d1b62b3352
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-11-21 12:45:25 +0200

    Analysis: Remove unnecessary commons lang not implemented exception
    This 'NotImplementedException' is only of use if a 'code' argument is
    passed to the created exception. Otherwise, it is internally using the
    'UnsupportedOperationException' anyway.

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteAdapterDiffCallback.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteAdapterDiffCallback.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.mysite
 
 import androidx.recyclerview.widget.DiffUtil
-import org.apache.commons.lang3.NotImplementedException
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DomainRegistrationCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.QuickActionsCard
@@ -27,7 +26,7 @@ object MySiteAdapterDiffCallback : DiffUtil.ItemCallback<MySiteCardAndItem>() {
             oldItem is ListItem && updatedItem is ListItem -> oldItem.primaryText == updatedItem.primaryText
             oldItem is DashboardCards && updatedItem is DashboardCards -> true
             oldItem is JetpackBadge && updatedItem is JetpackBadge -> true
-            else -> throw NotImplementedException("Diff not implemented yet")
+            else -> throw UnsupportedOperationException("Diff not implemented yet")
         }
     }
 
