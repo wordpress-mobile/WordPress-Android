f77c5f75a1dca223f51266797776edc8d784f128
Matthew Kevins <mmkevins@yahoo.com>
2022-11-22 15:13:37 +1000

    Fix post provider test

--- WordPress/src/test/java/org/wordpress/android/localcontentmigration/LocalPostProviderHelperTest.kt
+++ WordPress/src/test/java/org/wordpress/android/localcontentmigration/LocalPostProviderHelperTest.kt
@@ -1,8 +1,11 @@
 package org.wordpress.android.localcontentmigration
 
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import org.wordpress.android.fluxc.model.PostModel
@@ -16,11 +19,14 @@ import org.wordpress.android.resolver.DbWrapper
 class LocalPostProviderHelperTest {
     private val siteProviderHelper: LocalSiteProviderHelper = mock()
     private val dbWrapper: DbWrapper = mock()
+    private val mockDb: SQLiteDatabase = mock()
+    private val mockCursor: Cursor = mock()
     private val postStore: PostStore = mock()
     private val mockSiteModel: SiteModel = mock()
     private val mockPostModel: PostModel = mock()
     private val mockPostModel2: PostModel = mock()
 
+    private val mockSiteId = 471
     private val mockPostId = 42
     private val mockPostId2 = 7
     private val mockPostList = listOf(mockPostModel, mockPostModel2)
@@ -40,6 +46,13 @@ class LocalPostProviderHelperTest {
         whenever(mockPostModel2.id).thenReturn(mockPostId2)
         whenever(postStore.getPostsForSite(mockSiteModel)).thenReturn(mockPostList)
         whenever(siteProviderHelper.getData()).thenReturn(SitesData(sites = listOf(mockSiteModel)))
+        whenever(mockSiteModel.id).thenReturn(mockSiteId)
+        whenever(dbWrapper.giveMeReadableDb()).thenReturn(mockDb)
+        whenever(mockDb.query(
+                anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull()
+        )).thenReturn(mockCursor)
+        whenever(mockCursor.moveToNext()).thenReturn(true, true, false)
+        whenever(mockCursor.getInt(0)).thenReturn(mockPostId, mockPostId2)
     }
 
     @Test
