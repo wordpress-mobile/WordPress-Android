8db9c3a8de34c1667781f1ad0839d6cb23e8e7d5
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2022-11-23 14:42:11 -0500

    Add method for setting isOpenWithWebLinks app setting

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/AppPrefsWrapper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/AppPrefsWrapper.kt
@@ -256,6 +256,9 @@ class AppPrefsWrapper @Inject constructor() {
 
     fun getIsOpenWebLinksWithJetpack(): Boolean = AppPrefs.getIsOpenWebLinksWithJetpack()
 
+    fun setIsOpenWebLinksWithJetpack(isOpenWebLinksWithJetpack: Boolean) =
+            AppPrefs.setIsOpenWebLinksWithJetpack(isOpenWebLinksWithJetpack)
+
     fun getAllPrefs(): Map<String, Any?> = AppPrefs.getAllPrefs()
 
     fun setString(prefKey: PrefKey, value: String) {
