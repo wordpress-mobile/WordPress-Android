f218ae0e8d2524d15055019f1db3254a8a64c835
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2022-11-23 11:16:26 -0500

    Refactor: remove unused functions and add relevant one

--- WordPress/src/main/java/org/wordpress/android/util/PackageManagerWrapper.kt
+++ WordPress/src/main/java/org/wordpress/android/util/PackageManagerWrapper.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.util
 
 import android.content.ComponentName
+import android.content.Intent
 import android.content.pm.PackageManager
 import org.wordpress.android.util.AppLog.T
 import org.wordpress.android.viewmodel.ContextProvider
@@ -11,15 +12,6 @@ import javax.inject.Singleton
 class PackageManagerWrapper @Inject constructor(
     private val contextProvider: ContextProvider,
 ) {
-    fun isComponentEnabledSettingEnabled(cls: Class<*>): Boolean {
-        return when (getComponentEnabledSetting(cls)) {
-            null -> false
-            PackageManager.COMPONENT_ENABLED_STATE_DISABLED -> false
-            PackageManager.COMPONENT_ENABLED_STATE_ENABLED -> true
-            else -> isActivityEnabled(cls)
-        }
-    }
-
     fun isPackageInstalled(packageName: String) =
             contextProvider.getContext().packageManager.getLaunchIntentForPackage(packageName) != null
 
@@ -40,25 +32,16 @@ class PackageManagerWrapper @Inject constructor(
     fun disableReaderDeepLinks() = WPActivityUtils.disableReaderDeeplinks(contextProvider.getContext())
     fun enableReaderDeeplinks() = WPActivityUtils.enableReaderDeeplinks(contextProvider.getContext())
 
-    private fun getComponentEnabledSetting(cls: Class<*>) =
+    fun getActivityLabelResFromIntent(intent: Intent) : Int? {
+        intent.component?.let {
             try {
-                contextProvider.getContext().packageManager.getComponentEnabledSetting(
-                        ComponentName(contextProvider.getContext(), cls)
-                )
-            } catch (e: PackageManager.NameNotFoundException) {
-                AppLog.e(T.UTILS, e)
-                null
-            }
-
-    private fun isActivityEnabled(cls: Class<*>) : Boolean {
-        val context = contextProvider.getContext()
-        val packageInfo =
-                context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_ACTIVITIES)
-        for (activity in packageInfo.activities) {
-            if (activity.name == cls.name) {
-                return activity.enabled // This is the default value (set in AndroidManifest.xml)
+                val context = contextProvider.getContext()
+                val activityInfo = context.packageManager.getActivityInfo(it, PackageManager.GET_META_DATA)
+                return activityInfo.labelRes
+            } catch (ex: Exception) {
+                AppLog.e(T.UTILS, "Unable to extract label res from activity info")
             }
         }
-        return false
+        return null
     }
 }
