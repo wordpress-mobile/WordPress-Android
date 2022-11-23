d2ef9b07bc4eb3436426b14538f5016fc5626222
Antonis Lilis <antonis.lilis@automattic.com>
2022-11-23 15:55:13 +0200

    Adds login state pre-flight check

--- WordPress/src/main/java/org/wordpress/android/ui/utils/JetpackAppMigrationFlowUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/utils/JetpackAppMigrationFlowUtils.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.utils
 
+import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.ui.ActivityLauncher
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
 import org.wordpress.android.util.BuildConfigWrapper
@@ -7,21 +8,17 @@ import org.wordpress.android.util.config.JetpackMigrationFlowFeatureConfig
 import org.wordpress.android.viewmodel.ContextProvider
 import javax.inject.Inject
 
-@Suppress("ForbiddenComment")
 class JetpackAppMigrationFlowUtils @Inject constructor(
     private val buildConfigWrapper: BuildConfigWrapper,
     private val jetpackMigrationFlowFeatureConfig: JetpackMigrationFlowFeatureConfig,
     private val contextProvider: ContextProvider,
     private val appPrefsWrapper: AppPrefsWrapper,
+    private val accountStore: AccountStore,
 ) {
-    /** TODO: This should perform some additional pre-flight checks. We should:
-     *   1. Check that the user is not already logged in.
-     *   2. Check that the database is not already populated
-     *     * This also covers the case of an already successful migration.
-     */
     fun shouldShowMigrationFlow() = buildConfigWrapper.isJetpackApp
             && jetpackMigrationFlowFeatureConfig.isEnabled()
             && appPrefsWrapper.getIsFirstTrySharedLoginJetpack()
+            && !accountStore.hasAccessToken()
 
     fun startJetpackMigrationFlow() {
         ActivityLauncher.startJetpackMigrationFlow(contextProvider.getContext())
