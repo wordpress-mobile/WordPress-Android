a532babbfa57ae0df3bfd60cb0858136385c3b80
Antonis Lilis <antonis.lilis@automattic.com>
2022-11-25 12:04:35 +0200

    Fixes checkstyle issue

--- WordPress/src/main/java/org/wordpress/android/ui/utils/JetpackAppMigrationFlowUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/utils/JetpackAppMigrationFlowUtils.kt
@@ -20,7 +20,7 @@ class JetpackAppMigrationFlowUtils @Inject constructor(
     private val appStatus: AppStatus,
     private val wordPressPublicData: WordPressPublicData,
 ) {
-    private val minimumSupportedVersion = "21.3" //non semantic minimum supported version
+    private val minimumSupportedVersion = "21.3" // non semantic minimum supported version
 
     fun shouldShowMigrationFlow() = buildConfigWrapper.isJetpackApp
             && jetpackMigrationFlowFeatureConfig.isEnabled()
