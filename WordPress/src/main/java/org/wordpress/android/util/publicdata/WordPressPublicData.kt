3b8c9a7ec121ddac820a922416039bc1f1b6ed59
Antonis Lilis <antonis.lilis@automattic.com>
2022-11-25 11:42:31 +0200

    Fixes detekt issus with many return statemenents

--- WordPress/src/main/java/org/wordpress/android/util/publicdata/WordPressPublicData.kt
+++ WordPress/src/main/java/org/wordpress/android/util/publicdata/WordPressPublicData.kt
@@ -29,10 +29,11 @@ class WordPressPublicData @Inject constructor(private val packageManagerWrapper:
 
         // Clean app semantic versioning and keep ony major-minor version info. E.g 21.2-rc-3 turns to 21.2
         val majorMinorRegex = "^(\\d*)(\\.(\\d*))".toRegex()
-        val wordPressVersion = majorMinorRegex.find(rawVersion)?.value ?: return null
+        val wordPressVersion = majorMinorRegex.find(rawVersion)?.value
 
         // Verify that the resulting version is supported by org.wordpress.android.util.helpers.Version.Version
-        val versionIsSupportedForComparison = Regex("[0-9]+(\\.[0-9]+)*").matchEntire(wordPressVersion) != null
+        val versionIsSupportedForComparison = wordPressVersion !=null
+                && Regex("[0-9]+(\\.[0-9]+)*").matchEntire(wordPressVersion) != null
 
         return if (versionIsSupportedForComparison) wordPressVersion else null
     }
