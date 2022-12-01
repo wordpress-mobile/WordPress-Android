b052e7aa00e7a6a942ad26bdfac9e791df3a6158
Hicham Boushaba <hicham.boushaba@gmail.com>
2022-12-01 19:40:13 +0100

    Refactor to the solution
    The logic of creation application passwords has been centralized now in a single class, while the different rest clients don't have a lot of logic.

---
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/module/ApplicationName.kt
@@ -0,0 +1,9 @@
+package org.wordpress.android.fluxc.module
+
+import javax.inject.Qualifier
+import kotlin.annotation.AnnotationRetention.RUNTIME
+
+@Qualifier
+@MustBeDocumented
+@Retention(RUNTIME)
+annotation class ApplicationName
