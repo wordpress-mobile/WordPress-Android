18a6aa47368d28497455b327cfbc5d6b40f33d57
develric <develric@gmail.com>
2022-12-11 04:44:11 +0100

    Fixing detekt.

--- WordPress/src/main/java/org/wordpress/android/ui/main/MeFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MeFragment.kt
@@ -198,10 +198,7 @@ class MeFragment : Fragment(R.layout.me_fragment), OnScrollToTopListener {
             }
         }
 
-        if (qrCodeAuthFlowFeatureConfig.isEnabled() &&
-                BuildConfig.ENABLE_QRCODE_AUTH_FLOW &&
-                accountStore.hasAccessToken() &&
-                accountStore.account?.twoStepEnabled != true) {
+        if (shouldShowQrCodeLogin()) {
             rowScanLoginCode.isVisible = true
 
             rowScanLoginCode.setOnClickListener {
@@ -237,6 +234,13 @@ class MeFragment : Fragment(R.layout.me_fragment), OnScrollToTopListener {
                     .newInstance()
                     .show(childFragmentManager, JetpackPoweredBottomSheetFragment.TAG)
         }
+    }
+
+    private fun shouldShowQrCodeLogin(): Boolean {
+        return qrCodeAuthFlowFeatureConfig.isEnabled() &&
+                BuildConfig.ENABLE_QRCODE_AUTH_FLOW &&
+                accountStore.hasAccessToken() &&
+                accountStore.account?.twoStepEnabled != true
     }
 
     private fun MeFragmentBinding.setRecommendLoadingState(startShimmer: Boolean) {
