f5b13e0f53975cfb674cd6d301c9b1e08b795cc5
Ajesh R Pai <ajeshr2k1@gmail.com>
2022-12-13 13:47:27 +0530

    Adds: Persistence logic in RemoteConfigStore

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/mobile/RemoteConfigStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/mobile/RemoteConfigStore.kt
@@ -3,6 +3,9 @@ package org.wordpress.android.fluxc.store.mobile
 import org.wordpress.android.fluxc.network.rest.wpcom.mobile.RemoteConfigErrorType.GENERIC_ERROR
 import org.wordpress.android.fluxc.network.rest.wpcom.mobile.RemoteConfigRestClient
 import org.wordpress.android.fluxc.network.rest.wpcom.mobile.RemoteConfigError
+import org.wordpress.android.fluxc.persistence.RemoteConfigDao
+import org.wordpress.android.fluxc.persistence.RemoteConfigDao.RemoteConfig
+import org.wordpress.android.fluxc.persistence.RemoteConfigDao.RemoteConfigValueSource.BUILD_CONFIG
 import org.wordpress.android.fluxc.store.Store
 import org.wordpress.android.fluxc.tools.CoroutineEngine
 import org.wordpress.android.util.AppLog
@@ -12,14 +15,18 @@ import javax.inject.Singleton
 @Singleton
 class RemoteConfigStore @Inject constructor(
     private val remoteConfigRestClient: RemoteConfigRestClient,
+    private val remoteConfigDao: RemoteConfigDao,
     private val coroutineEngine: CoroutineEngine
 ) {
-    suspend fun fetchRemoteConfig() = coroutineEngine.withDefaultContext(AppLog.T.API, this,
-            "fetch remote-config") {
+    suspend fun fetchRemoteConfig() = coroutineEngine.withDefaultContext(
+            AppLog.T.API, this,
+            "fetch remote-field-config"
+    ) {
         val payload = remoteConfigRestClient.fetchRemoteConfig()
         return@withDefaultContext when {
             payload.isError -> RemoteConfigResult(payload.error)
             payload.remoteConfig != null -> {
+                remoteConfigDao.insert(payload.remoteConfig)
                 RemoteConfigResult(payload.remoteConfig)
             }
             else -> RemoteConfigResult(RemoteConfigError(GENERIC_ERROR))
@@ -32,5 +39,21 @@ class RemoteConfigStore @Inject constructor(
         constructor(error: RemoteConfigError) : this() {
             this.error = error
         }
+    }
+
+    fun getRemoteConfigs(): List<RemoteConfig> {
+        return remoteConfigDao.getRemoteConfigList()
+    }
+
+    fun insertRemoteConfig(key: String, value: String) {
+        remoteConfigDao.insert(
+                RemoteConfig(
+                        key = key,
+                        value = value,
+                        createdAt = System.currentTimeMillis(),
+                        modifiedAt = System.currentTimeMillis(),
+                        source = BUILD_CONFIG
+                )
+        )
     }
 }
