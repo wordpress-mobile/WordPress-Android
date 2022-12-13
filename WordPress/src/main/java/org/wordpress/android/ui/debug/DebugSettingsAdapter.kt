8cb6fb81ee8e40f2d499e667186f5803f3b85023
Ajesh R Pai <ajeshr2k1@gmail.com>
2022-12-13 18:27:30 +0530

    Adds: remote config field to the debug settings view
    Adds: the ui for remote field in debug settings
    Updates: the debug settings view model, viewholder and adapter
    for remote field config
    Updates: the strings file for the remote field config

--- WordPress/src/main/java/org/wordpress/android/ui/debug/DebugSettingsAdapter.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/debug/DebugSettingsAdapter.kt
@@ -5,15 +5,18 @@ import androidx.recyclerview.widget.ListAdapter
 import org.wordpress.android.ui.debug.DebugSettingsItemViewHolder.ButtonViewHolder
 import org.wordpress.android.ui.debug.DebugSettingsItemViewHolder.FeatureViewHolder
 import org.wordpress.android.ui.debug.DebugSettingsItemViewHolder.HeaderViewHolder
+import org.wordpress.android.ui.debug.DebugSettingsItemViewHolder.RemoteFieldConfigViewHolder
 import org.wordpress.android.ui.debug.DebugSettingsItemViewHolder.RowViewHolder
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Button
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Feature
+import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Field
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Header
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Row
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Type
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Type.BUTTON
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Type.FEATURE
+import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Type.FIELD
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Type.HEADER
 import org.wordpress.android.ui.debug.DebugSettingsViewModel.UiItem.Type.ROW
 
@@ -24,6 +27,7 @@ class DebugSettingsAdapter : ListAdapter<UiItem, DebugSettingsItemViewHolder>(De
             FEATURE -> FeatureViewHolder(parent)
             BUTTON -> ButtonViewHolder(parent)
             ROW -> RowViewHolder(parent)
+            FIELD -> RemoteFieldConfigViewHolder(parent)
         }
     }
 
@@ -37,6 +41,7 @@ class DebugSettingsAdapter : ListAdapter<UiItem, DebugSettingsItemViewHolder>(De
             is FeatureViewHolder -> holder.bind(getItem(position) as Feature)
             is ButtonViewHolder -> holder.bind(getItem(position) as Button)
             is RowViewHolder -> holder.bind(getItem(position) as Row)
+            is RemoteFieldConfigViewHolder -> holder.bind(getItem(position) as Field)
         }
     }
 }
