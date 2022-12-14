17ab3f46df3262984a3c8db1b2ee61690e5de351
Ovi Trif <octrif@gmail.com>
2022-12-14 14:24:26 +0100

    Remove redundant check for jetpack app

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackoverlay/JetpackFeatureRemovalBrandingUtil.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackoverlay/JetpackFeatureRemovalBrandingUtil.kt
@@ -4,15 +4,12 @@ import org.wordpress.android.ui.jetpackoverlay.JetpackFeatureRemovalPhase.PhaseF
 import org.wordpress.android.ui.jetpackoverlay.JetpackFeatureRemovalPhase.PhaseOne
 import org.wordpress.android.ui.jetpackoverlay.JetpackFeatureRemovalPhase.PhaseThree
 import org.wordpress.android.ui.jetpackoverlay.JetpackFeatureRemovalPhase.PhaseTwo
-import org.wordpress.android.util.BuildConfigWrapper
 import javax.inject.Inject
 
 class JetpackFeatureRemovalBrandingUtil @Inject constructor(
-    private val buildConfigWrapper: BuildConfigWrapper,
     private val jetpackFeatureRemovalPhaseHelper: JetpackFeatureRemovalPhaseHelper,
 ) {
     fun shouldShowPhaseOneBranding(): Boolean {
-        if (buildConfigWrapper.isJetpackApp) return false
         return when (jetpackFeatureRemovalPhaseHelper.getCurrentPhase()) {
             PhaseOne,
             PhaseTwo,
--- WordPress/src/test/java/org/wordpress/android/ui/jetpackoverlay/JetpackFeatureRemovalBrandingUtilTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/jetpackoverlay/JetpackFeatureRemovalBrandingUtilTest.kt
@@ -11,11 +11,9 @@ import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.whenever
 import org.wordpress.android.ui.jetpackoverlay.JetpackFeatureRemovalPhase.PhaseOne
-import org.wordpress.android.util.BuildConfigWrapper
 
 @RunWith(MockitoJUnitRunner::class)
 class JetpackFeatureRemovalBrandingUtilTest {
-    @Mock private lateinit var buildConfigWrapper: BuildConfigWrapper
     @Mock private lateinit var jetpackFeatureRemovalPhaseHelper: JetpackFeatureRemovalPhaseHelper
 
     @Rule
@@ -26,21 +24,12 @@ class JetpackFeatureRemovalBrandingUtilTest {
     @Before
     fun setup() {
         jetpackFeatureRemovalBrandingUtil = JetpackFeatureRemovalBrandingUtil(
-                buildConfigWrapper,
                 jetpackFeatureRemovalPhaseHelper,
         )
     }
 
     @Test
-    fun `given jetpack app, when phase one branding is checked, should return false`() {
-        whenever(buildConfigWrapper.isJetpackApp).thenReturn(true)
-        val shouldShowBranding = jetpackFeatureRemovalBrandingUtil.shouldShowPhaseOneBranding()
-
-        assertFalse(shouldShowBranding)
-    }
-
-    @Test
-    fun `given phase one, when phase one branding is checked, should return true`() {
+    fun `given phase one started, when phase one branding is checked, should return true`() {
         whenever(jetpackFeatureRemovalPhaseHelper.getCurrentPhase()).thenReturn(PhaseOne)
         val shouldShowBranding = jetpackFeatureRemovalBrandingUtil.shouldShowPhaseOneBranding()
 
