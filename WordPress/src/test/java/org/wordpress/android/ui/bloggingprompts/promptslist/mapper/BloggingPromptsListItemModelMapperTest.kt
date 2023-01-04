f0136d8145e255af88b890168c59bdec8204c3e4
Thomas Horta <thomas.horta@automattic.com>
2023-01-04 18:20:37 -0300

    update Prompts List classes tests

--- WordPress/src/test/java/org/wordpress/android/ui/bloggingprompts/promptslist/mapper/BloggingPromptsListItemModelMapperTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/bloggingprompts/promptslist/mapper/BloggingPromptsListItemModelMapperTest.kt
@@ -7,10 +7,8 @@ import org.junit.Test
 import org.mockito.Mock
 import org.mockito.kotlin.whenever
 import org.wordpress.android.BaseUnitTest
-import org.wordpress.android.fluxc.model.bloggingprompts.BloggingPromptModel
-import org.wordpress.android.ui.bloggingprompts.promptslist.model.BloggingPromptsListItemModel
+import org.wordpress.android.ui.bloggingprompts.promptslist.BloggingPromptsListFixtures
 import org.wordpress.android.util.LocaleManagerWrapper
-import java.util.Date
 import java.util.Locale
 
 @ExperimentalCoroutinesApi
@@ -26,31 +24,8 @@ class BloggingPromptsListItemModelMapperTest : BaseUnitTest() {
 
     @Test
     fun `when toUiModel is called, then maps to UI model correctly`() {
-        val result = mapper.toUiModel(DOMAIN_MODEL)
+        val result = mapper.toUiModel(BloggingPromptsListFixtures.DOMAIN_MODEL)
 
-        assertThat(result).isEqualTo(UI_MODEL)
-    }
-
-    companion object {
-        private val DOMAIN_MODEL = BloggingPromptModel(
-                id = 123,
-                text = "Text",
-                title = "Title",
-                content = "Content",
-                date = Date(1671678000000), // December 22, 2022
-                isAnswered = true,
-                attribution = "Attribution",
-                respondentsCount = 321,
-                respondentsAvatarUrls = emptyList(),
-        )
-
-        private val UI_MODEL = BloggingPromptsListItemModel(
-                id = 123,
-                text = "Text",
-                date = Date(1671678000000), // December 22, 2022
-                formattedDate = "Dec 22",
-                isAnswered = true,
-                answersCount = 321,
-        )
+        assertThat(result).isEqualTo(BloggingPromptsListFixtures.UI_MODEL)
     }
 }
