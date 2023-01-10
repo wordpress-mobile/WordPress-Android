f0837eb246db8ac4724efb1c37b3e00db793cf06
Thomas Horta <thomas.horta@automattic.com>
2023-01-10 13:21:44 -0300

    add remote field to Prompts List feature flag
    Adding the remote field means it will be able to be turned on remotely, now that
    the feature is v1 completed (matching iOS). There were some discussion about
    when the feature should actually show up to users, so its default is still OFF.

--- WordPress/src/main/java/org/wordpress/android/util/config/BloggingPromptsListFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/BloggingPromptsListFeatureConfig.kt
@@ -1,16 +1,21 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.annotation.Feature
 import javax.inject.Inject
 
-@FeatureInDevelopment
+@Feature(BloggingPromptsListFeatureConfig.BLOGGING_PROMPTS_LIST_REMOTE_FIELD, false)
 class BloggingPromptsListFeatureConfig
 @Inject constructor(appConfig: AppConfig) : FeatureConfig(
     appConfig,
-    BuildConfig.BLOGGING_PROMPTS_LIST
+    BuildConfig.BLOGGING_PROMPTS_LIST,
+    BLOGGING_PROMPTS_LIST_REMOTE_FIELD,
 ) {
     override fun isEnabled(): Boolean {
         return super.isEnabled() && BuildConfig.IS_JETPACK_APP
+    }
+
+    companion object {
+        const val BLOGGING_PROMPTS_LIST_REMOTE_FIELD = "blogging_prompts_list_enabled"
     }
 }
