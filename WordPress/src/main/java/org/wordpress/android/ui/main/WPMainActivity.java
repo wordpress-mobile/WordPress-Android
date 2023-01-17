c302d263374241e9b14f96edf3d8c1b495983789
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-01-17 18:53:26 +0530

    Updates: the navigation view being shown when the flag is on or off

--- WordPress/src/main/java/org/wordpress/android/ui/main/WPMainActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/WPMainActivity.java
@@ -309,14 +309,7 @@ public class WPMainActivity extends LocaleAwareActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-
-        if (!mJetpackFeatureRemovalOverlayUtil.shouldHideJetpackFeatures()) {
-            mBottomNav = findViewById(R.id.bottom_navigation);
-            mBottomNav.setVisibility(View.VISIBLE);
-            mBottomNav.init(getSupportFragmentManager(), this);
-        } else {
-            showMySiteFragment();
-        }
+        mBottomNav = findViewById(R.id.bottom_navigation);
 
         mConnectionBar = findViewById(R.id.connection_bar);
         mConnectionBar.setOnClickListener(v -> {
@@ -490,6 +483,26 @@ public class WPMainActivity extends LocaleAwareActivity implements
         }
 
         displayJetpackFeatureCollectionOverlayIfNeeded();
+    }
+
+    private void setUpMainView() {
+        if (!mJetpackFeatureRemovalOverlayUtil.shouldHideJetpackFeatures()) {
+            mBottomNav.setVisibility(View.VISIBLE);
+            clearFragmentBackStack();
+            mBottomNav.init(getSupportFragmentManager(), this);
+        } else {
+            mBottomNav.setVisibility(View.GONE);
+            mBottomNav.clear();
+            showMySiteFragment();
+        }
+    }
+
+    private void clearFragmentBackStack() {
+        FragmentManager fm = getSupportFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            FragmentManager.BackStackEntry first = fm.getBackStackEntryAt(0);
+            fm.popBackStack(first.getId(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
     }
 
     private void showMySiteFragment() {
@@ -972,6 +985,8 @@ public class WPMainActivity extends LocaleAwareActivity implements
     @Override
     protected void onResume() {
         super.onResume();
+
+        setUpMainView();
 
         // Load selected site
         initSelectedSite();
