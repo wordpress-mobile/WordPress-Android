555b22d8dcfd003ee8a19092b0d0b40fd24051ad
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-01-30 16:42:30 -0300

    Apply PR suggestion: make jetpack install full plugin feature flag false by default

--- WordPress/src/main/java/org/wordpress/android/util/config/JetpackInstallFullPluginFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/JetpackInstallFullPluginFeatureConfig.kt
@@ -1,22 +1,16 @@
 package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
-import org.wordpress.android.annotation.Feature
-import org.wordpress.android.util.config.JetpackInstallFullPluginFeatureConfig.Companion.JETPACK_INSTALL_FULL_PLUGIN_REMOTE_FIELD
+import org.wordpress.android.annotation.FeatureInDevelopment
 import javax.inject.Inject
 
-@Feature(JETPACK_INSTALL_FULL_PLUGIN_REMOTE_FIELD, true)
+@FeatureInDevelopment
 class JetpackInstallFullPluginFeatureConfig
 @Inject constructor(appConfig: AppConfig) : FeatureConfig(
     appConfig,
     BuildConfig.JETPACK_INSTALL_FULL_PLUGIN,
-    JETPACK_INSTALL_FULL_PLUGIN_REMOTE_FIELD
 ) {
     override fun isEnabled(): Boolean {
         return super.isEnabled() && BuildConfig.IS_JETPACK_APP
-    }
-
-    companion object {
-        const val JETPACK_INSTALL_FULL_PLUGIN_REMOTE_FIELD = "jetpack_install_full_plugin_remote_field"
     }
 }
