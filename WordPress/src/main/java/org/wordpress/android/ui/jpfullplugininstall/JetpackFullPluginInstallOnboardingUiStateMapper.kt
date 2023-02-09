e87ce030735d63203f22fbb61131556f041d87f1
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-02-09 17:35:24 -0300

    Update JetpackFullPluginInstallOnboardingUiStateMapper

--- WordPress/src/main/java/org/wordpress/android/ui/jpfullplugininstall/JetpackFullPluginInstallOnboardingUiStateMapper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jpfullplugininstall/JetpackFullPluginInstallOnboardingUiStateMapper.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.ui.jpfullplugininstall
 
 import org.wordpress.android.ui.jpfullplugininstall.onboarding.JetpackFullPluginInstallOnboardingViewModel.UiState
 import org.wordpress.android.ui.mysite.SelectedSiteRepository
+import org.wordpress.android.util.extensions.activeJetpackConnectionPluginNames
 import javax.inject.Inject
 
 class JetpackFullPluginInstallOnboardingUiStateMapper @Inject constructor(
@@ -11,12 +12,14 @@ class JetpackFullPluginInstallOnboardingUiStateMapper @Inject constructor(
         onTermsAndConditionsClick: () -> Unit,
         onInstallFullPluginClick: () -> Unit,
         onContactSupportClick: () -> Unit,
-    ): UiState.Loaded =
-        UiState.Loaded(
-            siteName =,
-            pluginName =,
+    ): UiState.Loaded {
+        val selectedSite = selectedSiteRepository.getSelectedSite()
+        return UiState.Loaded(
+            siteName = selectedSite?.name.orEmpty(),
+            pluginNames = selectedSite?.activeJetpackConnectionPluginNames().orEmpty(),
             onTermsAndConditionsClick = onTermsAndConditionsClick,
             onInstallFullPluginClick = onInstallFullPluginClick,
             onContactSupportClick = onContactSupportClick,
         )
+    }
 }
