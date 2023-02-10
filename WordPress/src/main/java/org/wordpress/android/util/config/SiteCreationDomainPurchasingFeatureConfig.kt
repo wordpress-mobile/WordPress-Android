b63712642d53416d264732632013d617b8ed415c
Ovi Trif <octrif@gmail.com>
2023-02-10 13:41:54 +0100

    Integrate experiment with feature config

--- WordPress/src/main/java/org/wordpress/android/util/config/SiteCreationDomainPurchasingFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/SiteCreationDomainPurchasingFeatureConfig.kt
@@ -2,13 +2,20 @@ package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.fluxc.model.experiments.Variation.Control
+import org.wordpress.android.util.experiments.SiteCreationDomainPurchasingExperiment
 import javax.inject.Inject
 
 @FeatureInDevelopment
 class SiteCreationDomainPurchasingFeatureConfig
 @Inject constructor(
     appConfig: AppConfig,
+    private val experiment: SiteCreationDomainPurchasingExperiment,
 ) : FeatureConfig(
     appConfig,
     BuildConfig.ENABLE_SITE_CREATION_DOMAIN_PURCHASING,
-)
+) {
+    override fun isEnabled(): Boolean {
+        return super.isEnabled() && experiment.getVariation() != Control
+    }
+}
