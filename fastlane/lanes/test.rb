fc210781251374d6f6e93a09806166f0abfdecb3
jos <17252150+jostnes@users.noreply.github.com>
2023-02-13 16:53:09 +0800

    create new jetpack instrumented test option

--- fastlane/lanes/test.rb
+++ fastlane/lanes/test.rb
@@ -2,16 +2,16 @@ GOOGLE_FIREBASE_SECRETS_PATH = File.join(Dir.home, '.configure', 'wordpress-andr
 
 platform :android do
   #####################################################################################
-  # build_and_run_instrumented_test
+  # build_and_run_wordpress_instrumented_test
   # -----------------------------------------------------------------------------------
   # Run instrumented tests in Google Firebase Test Lab
   # -----------------------------------------------------------------------------------
   # Usage:
-  # bundle exec fastlane build_and_run_instrumented_test
+  # bundle exec fastlane build_and_run_wordpress_instrumented_test
   #
   #####################################################################################
   desc "Build the application and instrumented tests, then run the tests in Firebase Test Lab"
-  lane :build_and_run_instrumented_test do | options |
+  lane :build_and_run_wordpress_instrumented_test do | options |
     gradle(tasks: ['WordPress:assembleWordPressVanillaDebug', 'WordPress:assembleWordPressVanillaDebugAndroidTest'])
 
     # Run the instrumented tests in Firebase Test Lab
@@ -34,6 +34,50 @@ platform :android do
     )
 
     annotation_ctx = 'firebase-test-wordpress-vanilla-debug'
+    if test_succeeded
+      sh("buildkite-agent annotation remove --context '#{annotation_ctx}' || true") if is_ci?
+    else
+      details_url = lane_context[SharedValues::FIREBASE_TEST_MORE_DETAILS_URL]
+      message = "Firebase Tests failed. Failure details can be seen [here in Firebase Console](#{details_url})"
+      sh('buildkite-agent', 'annotate', message, '--style', 'error', '--context', annotation_ctx) if is_ci?
+      UI.test_failure!(message)
+    end
+  end
+end
+
+  #####################################################################################
+  # build_and_run_jetpack_instrumented_test
+  # -----------------------------------------------------------------------------------
+  # Run instrumented tests in Google Firebase Test Lab
+  # -----------------------------------------------------------------------------------
+  # Usage:
+  # bundle exec fastlane build_and_run_jetpack_instrumented_test
+  #
+  #####################################################################################
+  desc "Build the application and instrumented tests, then run the tests in Firebase Test Lab"
+  lane :build_and_run_jetpack_instrumented_test do | options |
+    gradle(tasks: ['WordPress:assembleJetpackVanillaDebug', 'WordPress:assembleJetpackVanillaDebugAndroidTest'])
+
+    # Run the instrumented tests in Firebase Test Lab
+    firebase_login(
+      key_file: GOOGLE_FIREBASE_SECRETS_PATH
+    )
+
+    apk_dir = File.join(PROJECT_ROOT_FOLDER, 'WordPress', 'build', 'outputs', 'apk')
+
+    test_succeeded = android_firebase_test(
+      project_id: firebase_secret(name: 'project_id'),
+      key_file: GOOGLE_FIREBASE_SECRETS_PATH,
+      model: 'Pixel2.arm',
+      version: 30,
+      test_apk_path: File.join(apk_dir, 'androidTest', 'jetpackVanilla', 'debug', 'org.wordpress.android-jetpack-vanilla-debug-androidTest.apk'),
+      apk_path: File.join(apk_dir, 'wordpressVanilla', 'debug', 'org.wordpress.android-jetpack-vanilla-debug.apk'),
+      test_targets: 'notPackage org.wordpress.android.ui.screenshots',
+      results_output_dir: File.join(PROJECT_ROOT_FOLDER, 'build', 'instrumented-tests'),
+      crash_on_test_failure: false
+    )
+
+    annotation_ctx = 'firebase-test-jetpack-vanilla-debug'
     if test_succeeded
       sh("buildkite-agent annotation remove --context '#{annotation_ctx}' || true") if is_ci?
     else
