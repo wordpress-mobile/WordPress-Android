1e3142ee70cfdf81fb58bc81e7089ac34b134b01
develric <develric@gmail.com>
2023-02-18 01:22:02 +0100

    Updating unit tests.

--- WordPress/src/test/java/org/wordpress/android/ui/jpfullplugininstall/install/JetpackFullPluginInstallViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/jpfullplugininstall/install/JetpackFullPluginInstallViewModelTest.kt
@@ -4,11 +4,20 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.KArgumentCaptor
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.wordpress.android.BaseUnitTest
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.action.PluginAction
+import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.store.PluginStore
 import org.wordpress.android.ui.accounts.HelpActivity
 import org.wordpress.android.ui.mysite.SelectedSiteRepository
 
@@ -16,15 +25,25 @@ import org.wordpress.android.ui.mysite.SelectedSiteRepository
 class JetpackFullPluginInstallViewModelTest : BaseUnitTest() {
     private val uiStateMapper: JetpackFullPluginInstallUiStateMapper = mock()
     private val selectedSiteRepository: SelectedSiteRepository = mock()
+    private val pluginStore: PluginStore = mock()
+    private val dispatcher: Dispatcher = mock()
+    private lateinit var actionCaptor: KArgumentCaptor<Action<Any>>
     private val classToTest = JetpackFullPluginInstallViewModel(
         uiStateMapper = uiStateMapper,
         selectedSiteRepository = selectedSiteRepository,
         bgDispatcher = testDispatcher(),
+        pluginStore,
+        dispatcher
     )
 
     private val selectedSite = SiteModel().apply {
         id = 123
         name = "Site name"
+    }
+
+    @Before
+    fun setUp() {
+        actionCaptor = argumentCaptor()
     }
 
     @Test
@@ -34,22 +53,30 @@ class JetpackFullPluginInstallViewModelTest : BaseUnitTest() {
             uiStateMapper = uiStateMapper,
             selectedSiteRepository = selectedSiteRepository,
             bgDispatcher = testDispatcher(),
+            pluginStore,
+            dispatcher
         )
         assertThat(initialMockedClassToTest.uiState.value).isInstanceOf(UiState.Initial::class.java)
     }
 
     @Test
-    fun `Should post Installing UI state when onContinueClick is called`() {
+    fun `Should post Installing UI state and dispatch install action when onContinueClick is called`() {
+        mockSelectedSite(selectedSite)
         whenever(uiStateMapper.mapInstalling()).thenReturn(UiState.Installing)
         classToTest.onContinueClick()
         assertThat(classToTest.uiState.value).isInstanceOf(UiState.Installing::class.java)
+        verify(dispatcher, times(1)).dispatch(actionCaptor.capture())
+        assertThat(actionCaptor.lastValue.type).isEqualTo(PluginAction.INSTALL_JP_FOR_INDIVIDUAL_PLUGIN_SITE)
     }
 
     @Test
     fun `Should post Installing UI state when onRetryClick is called`() {
+        mockSelectedSite(selectedSite)
         whenever(uiStateMapper.mapInstalling()).thenReturn(UiState.Installing)
         classToTest.onRetryClick()
         assertThat(classToTest.uiState.value).isInstanceOf(UiState.Installing::class.java)
+        verify(dispatcher, times(1)).dispatch(actionCaptor.capture())
+        assertThat(actionCaptor.lastValue.type).isEqualTo(PluginAction.INSTALL_JP_FOR_INDIVIDUAL_PLUGIN_SITE)
     }
 
     @Test
