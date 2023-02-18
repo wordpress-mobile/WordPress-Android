ca3017e36e8dd27b9f1796e5cc57e672e2aaae9c
develric <develric@gmail.com>
2023-02-18 01:21:39 +0100

    Integrating the VM with actuall calls to install endpoint.

--- WordPress/src/main/java/org/wordpress/android/ui/jpfullplugininstall/install/JetpackFullPluginInstallViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jpfullplugininstall/install/JetpackFullPluginInstallViewModel.kt
@@ -7,9 +7,19 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.PluginActionBuilder
+import org.wordpress.android.fluxc.generated.SiteActionBuilder
+import org.wordpress.android.fluxc.store.PluginStore
+import org.wordpress.android.fluxc.store.PluginStore.InstallSitePluginPayload
+import org.wordpress.android.fluxc.store.PluginStore.OnSitePluginInstalled
+import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.modules.BG_THREAD
 import org.wordpress.android.ui.accounts.HelpActivity
 import org.wordpress.android.ui.mysite.SelectedSiteRepository
+import org.wordpress.android.util.AppLog
 import org.wordpress.android.viewmodel.ScopedViewModel
 import javax.inject.Inject
 import javax.inject.Named
@@ -19,12 +29,24 @@ class JetpackFullPluginInstallViewModel @Inject constructor(
     private val uiStateMapper: JetpackFullPluginInstallUiStateMapper,
     private val selectedSiteRepository: SelectedSiteRepository,
     @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
+    // adding pluginStore seems needed to allow the events Subscribe to work
+    private val pluginStore: PluginStore,
+    private val dispatcher: Dispatcher
 ) : ScopedViewModel(bgDispatcher) {
     private val _uiState = MutableStateFlow<UiState>(uiStateMapper.mapInitial())
     val uiState = _uiState.asStateFlow()
 
     private val _actionEvents = MutableSharedFlow<ActionEvent>()
     val actionEvents = _actionEvents
+
+    init {
+        dispatcher.register(this)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        dispatcher.unregister(this)
+    }
 
     fun onContinueClick() {
         postUiState(uiStateMapper.mapInstalling())
@@ -53,17 +75,68 @@ class JetpackFullPluginInstallViewModel @Inject constructor(
         )
     }
 
-    @Suppress("MagicNumber")
+    @Suppress("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onSitePluginInstalled(event: OnSitePluginInstalled) {
+        if (event.isError) {
+            AppLog.d(
+                AppLog.T.PLUGINS,
+                "Error trying to install the full Jetpack plugin: ${event.error.type} - ${event.error.message}"
+            )
+        }
+
+        // Refresh the site regardless any event error if possible
+        event.site?.let {
+            dispatcher.dispatch(SiteActionBuilder.newFetchSiteAction(it))
+        } ?: postUiState(uiStateMapper.mapError())
+    }
+
+    @Suppress("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onSitePluginConfigured(event: PluginStore.OnSitePluginConfigured) {
+        if (event.isError) {
+            AppLog.d(
+                AppLog.T.PLUGINS,
+                "Error trying to configure the full Jetpack plugin: ${event.error.type} - ${event.error.message}"
+            )
+        }
+
+        // Refresh the site regardless any event error if possible
+        event.site?.let {
+            dispatcher.dispatch(SiteActionBuilder.newFetchSiteAction(it))
+        } ?: postUiState(uiStateMapper.mapError())
+    }
+
+    @Suppress("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onSiteChanged(event: SiteStore.OnSiteChanged) {
+        val success = if (event.isError) {
+            AppLog.d(
+                AppLog.T.PLUGINS,
+                "Error trying to update site while installing the full " +
+                        "Jetpack plugin: ${event.error.type} - ${event.error.message}"
+            )
+            false
+        } else {
+            // Check if the final goal (JP installed and active) is matched
+            val selectedSite = selectedSiteRepository.getSelectedSite()
+            selectedSite?.let {
+                it.isJetpackInstalled && it.isJetpackConnected
+            } ?: false
+        }
+
+        if (success) {
+            postUiState(uiStateMapper.mapDone())
+        } else {
+            postUiState(uiStateMapper.mapError())
+        }
+    }
+
     private fun installJetpackPlugin() {
-        // TODO replace mock with endpoint call and update unit tests
-        launch {
-            delay(2000L)
-            val success = true
-            if (success) {
-                postUiState(uiStateMapper.mapDone())
-            } else {
-                postUiState(uiStateMapper.mapError())
-            }
+        val selectedSite = selectedSiteRepository.getSelectedSite()
+        selectedSite?.let {
+            val payload = InstallSitePluginPayload(it, "jetpack")
+            dispatcher.dispatch(PluginActionBuilder.newInstallJpForIndividualPluginSiteAction(payload))
         }
     }
 
