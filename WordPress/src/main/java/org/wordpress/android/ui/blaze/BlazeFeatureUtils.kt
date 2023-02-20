6a310e6c3c0733185185d247904cf2310d92121d
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-02-20 13:42:54 -0500

    Add: entry point source and tracking fun

--- WordPress/src/main/java/org/wordpress/android/ui/blaze/BlazeFeatureUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blaze/BlazeFeatureUtils.kt
@@ -1,15 +1,18 @@
 package org.wordpress.android.ui.blaze
 
+import org.wordpress.android.analytics.AnalyticsTracker
 import org.wordpress.android.fluxc.model.PostModel
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.post.PostStatus
 import org.wordpress.android.util.BuildConfigWrapper
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 import org.wordpress.android.util.config.BlazeFeatureConfig
 import javax.inject.Inject
 
 class BlazeFeatureUtils @Inject constructor(
     private val blazeFeatureConfig: BlazeFeatureConfig,
-    private val buildConfigWrapper: BuildConfigWrapper
+    private val buildConfigWrapper: BuildConfigWrapper,
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper,
 ) {
     fun shouldShowPromoteWithBlaze(
         postStatus: PostStatus,
@@ -22,5 +25,21 @@ class BlazeFeatureUtils @Inject constructor(
                 postStatus == PostStatus.PUBLISHED &&
                 postModel.password.isEmpty() &&
                 siteModel.isAdmin
+    }
+
+    fun track(stat: AnalyticsTracker.Stat, source: BlazeEntryPointSource) {
+        analyticsTrackerWrapper.track(
+            stat,
+            mapOf(SOURCE to source.trackingName)
+        )
+    }
+
+    companion object {
+        const val SOURCE = "source"
+    }
+    enum class BlazeEntryPointSource(val trackingName: String) {
+        DASHBOARD_CARD("dashboard_card"),
+        MENU_ITEM("menu_item"),
+        POSTS_LIST("posts_list")
     }
 }
