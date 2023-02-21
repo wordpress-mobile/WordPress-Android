9c5e3f2fbecf3e92845e6adfd27fc3f3f7fe9872
Ovi Trif <octrif@gmail.com>
2023-02-21 18:51:49 +0100

    Add divider and gray cost text color

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/compose/DomainItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/compose/DomainItem.kt
@@ -2,9 +2,12 @@ package org.wordpress.android.ui.sitecreation.domains.compose
 
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,19 +16,26 @@ import androidx.compose.ui.unit.dp
 import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.ui.sitecreation.domains.SiteCreationDomainsViewModel.ListItemUiState.New.DomainUiState
 
+private val CostColorDefault @Composable get() = MaterialTheme.colors.onSurface.copy(alpha = 0.6f)
+
 @Composable
 fun DomainItem(
     uiState: DomainUiState,
 ) = with(uiState) {
-    Row(
-        modifier = Modifier
-            .clickable { onClick.invoke() }
-            .padding(horizontal = 24.dp, vertical = 16.dp)
-    )
-    {
-        Text(domainName)
-        Spacer(modifier = Modifier.weight(1f))
-        Text(cost)
+    Column {
+        Row(
+            modifier = Modifier
+                .clickable { onClick.invoke() }
+                .padding(horizontal = 24.dp, vertical = 16.dp)
+        ) {
+            Text(domainName)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = cost,
+                color = CostColorDefault,
+            )
+        }
+        Divider(thickness = 0.5.dp)
     }
 }
 
@@ -39,7 +49,11 @@ private fun DomainItemPreview() {
         onClick = {}
     )
     AppTheme {
-        DomainItem(uiState)
+        Column {
+            DomainItem(uiState.copy(cost = "$6/yr"))
+            DomainItem(uiState)
+            DomainItem(uiState.copy(cost = "$24/yr"))
+        }
     }
 }
 
