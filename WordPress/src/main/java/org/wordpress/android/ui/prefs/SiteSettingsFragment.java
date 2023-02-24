24623ff005aad284418b4ada22498f3ae20ce390
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-02-24 18:08:38 +0530

    Fixes: formatting issues

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/SiteSettingsFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/SiteSettingsFragment.java
@@ -38,7 +38,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.collection.SparseArrayCompat;
