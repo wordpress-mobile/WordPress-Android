244e712bd0a084981e9b9f1d0387dccda25cfe26
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-02-26 11:44:54 -0500

    Add states for the webview header and content. Refactor BlazeUiState

--- WordPress/src/main/java/org/wordpress/android/ui/blaze/BlazeUiState.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blaze/BlazeUiState.kt
@@ -1,18 +1,18 @@
 package org.wordpress.android.ui.blaze
 
 import android.os.Parcelable
+import androidx.annotation.StringRes
 import kotlinx.parcelize.Parcelize
+import org.wordpress.android.R
+import org.wordpress.android.WordPress
 
 sealed class BlazeUiState {
     sealed class PromoteScreen : BlazeUiState() {
         data class PromotePost(val postUIModel: PostUIModel) : PromoteScreen()
         object Site : PromoteScreen()
+        object Page : PromoteScreen()
     }
-
-    object PostSelectionScreen : BlazeUiState()
-    object AppearanceScreen : BlazeUiState()
-    object AudienceScreen : BlazeUiState()
-    object PaymentGateway : BlazeUiState()
+    object WebViewScreen : BlazeUiState()
     object Done : BlazeUiState()
 }
 
@@ -31,3 +31,25 @@ data class PostUIModel(
     val imageUrl: Long,
 ) : Parcelable
 
+sealed class BlazeWebViewHeaderUiState {
+    @StringRes val headerTitle = R.string.blaze_activity_title
+    @StringRes val headerActionText = R.string.cancel
+    open val headerActionVisible: Boolean = true
+
+    data class ShowAction(
+        override val headerActionVisible: Boolean = true
+    ): BlazeWebViewHeaderUiState()
+
+    data class HideAction(
+        override val headerActionVisible: Boolean = false
+    ): BlazeWebViewHeaderUiState()
+}
+
+data class BlazeWebViewContentUiState(
+    val enableJavascript: Boolean = true,
+    val enableDomStorage: Boolean = true,
+    val userAgent: String = WordPress.getUserAgent(),
+    val enableChromeClient: Boolean = true,
+    val addressToLoad: String = "",
+    val url: String = ""
+)
