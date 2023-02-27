0ce816758e326f06d5cb6b43885b9b9d067c3124
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-02-27 16:18:05 -0500

    Add new action event for launching external browser

--- WordPress/src/main/java/org/wordpress/android/ui/blaze/BlazeActionEvent.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blaze/BlazeActionEvent.kt
@@ -2,4 +2,5 @@ package org.wordpress.android.ui.blaze
 
 sealed class BlazeActionEvent {
     object FinishActivity : BlazeActionEvent()
+    data class LaunchExternalBrowser(val url: String) : BlazeActionEvent()
 }
