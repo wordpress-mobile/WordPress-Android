b0630f4c6da02c8d0ff6f9d89eb3c5e5992f5c03
Ovi Trif <octrif@gmail.com>
2023-02-27 17:05:26 +0100

    Fix: Replace currency code with symbol in sale price

--- WordPress/src/main/java/org/wordpress/android/util/extensions/ProductExtensions.kt
+++ WordPress/src/main/java/org/wordpress/android/util/extensions/ProductExtensions.kt
@@ -1,6 +1,8 @@
 package org.wordpress.android.util.extensions
 
 import org.wordpress.android.fluxc.model.products.Product
+import java.util.Currency
 
 fun Product?.isSaleDomain(): Boolean = this?.saleCost?.let { it.compareTo(0.0) > 0 } == true
-fun Product?.saleCostForDisplay() = this?.run { currencyCode + "%.2f".format(saleCost) } ?: ""
+fun Product?.saleCostForDisplay() =
+    this?.run { Currency.getInstance(currencyCode).symbol + "%.2f".format(saleCost) } ?: ""
