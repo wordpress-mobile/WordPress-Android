b08a297b831c01b4526693d5a4ad72b553cc7f8a
Ovi Trif <octrif@gmail.com>
2023-02-28 13:15:05 +0100

    Refactor: Remove extra empty line after fetchAndCacheProducts

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/SiteCreationDomainsViewModel.kt
@@ -130,7 +130,6 @@ class SiteCreationDomainsViewModel @Inject constructor(
         }
     }
 
-
     fun onCreateSiteBtnClicked() {
         val domain = requireNotNull(selectedDomain) {
             "Create site button should not be visible if a domain is not selected"
