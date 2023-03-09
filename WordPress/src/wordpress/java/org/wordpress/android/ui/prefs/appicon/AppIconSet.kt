b50f9353877d6611d91b731de54d3d7a33779c14
Thomas Horta <thomas.horta@automattic.com>
2023-03-09 19:30:54 -0300

    use icon set from GH issue for WordPress

--- WordPress/src/wordpress/java/org/wordpress/android/ui/prefs/appicon/AppIconSet.kt
+++ WordPress/src/wordpress/java/org/wordpress/android/ui/prefs/appicon/AppIconSet.kt
@@ -5,15 +5,33 @@ import javax.inject.Inject
 
 class AppIconSet @Inject constructor() : IAppIconSet {
     override val appIcons: List<AppIcon> = listOf(
-        AppIcon.DEFAULT, PURPLE
+        AppIcon.DEFAULT, BLACK, BLUE, CELADON, PINK
     )
 
     companion object {
-        val PURPLE = AppIcon(
-            id = "purple",
-            nameRes = R.string.app_icon_purple_name,
-            iconRes = R.mipmap.app_icon_purple,
-            alias = "org.wordpress.android.launch.WPLaunchActivityPurple"
+        private val BLACK = AppIcon(
+            id = "black",
+            nameRes = R.string.app_icon_black_name,
+            iconRes = R.mipmap.app_icon_black,
+            alias = "org.wordpress.android.launch.WPLaunchActivityBlack"
+        )
+        private val BLUE = AppIcon(
+            id = "blue",
+            nameRes = R.string.app_icon_blue_name,
+            iconRes = R.mipmap.app_icon_blue,
+            alias = "org.wordpress.android.launch.WPLaunchActivityBlue"
+        )
+        private val CELADON = AppIcon(
+            id = "celadon",
+            nameRes = R.string.app_icon_celadon_name,
+            iconRes = R.mipmap.app_icon_celadon,
+            alias = "org.wordpress.android.launch.WPLaunchActivityCeladon"
+        )
+        private val PINK = AppIcon(
+            id = "pink",
+            nameRes = R.string.app_icon_pink_name,
+            iconRes = R.mipmap.app_icon_pink,
+            alias = "org.wordpress.android.launch.WPLaunchActivityPink"
         )
     }
 }
