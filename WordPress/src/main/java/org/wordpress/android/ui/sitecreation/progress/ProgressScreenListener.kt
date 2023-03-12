89553b3998d66732fb7e5167ec5ace246edc6f26
Ovi Trif <octrif@gmail.com>
2023-03-13 00:29:37 +0100

    Refactor: replace progress screen listener with observer in activity vm

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/progress/ProgressScreenListener.kt
+++
@@ -1,8 +0,0 @@
-package org.wordpress.android.ui.sitecreation.progress
-
-import org.wordpress.android.ui.sitecreation.SiteCreationResult
-
-interface ProgressScreenListener {
-    fun onProgressStopped(result: SiteCreationResult)
-    fun onProgressCompleted(result: SiteCreationResult)
-}
