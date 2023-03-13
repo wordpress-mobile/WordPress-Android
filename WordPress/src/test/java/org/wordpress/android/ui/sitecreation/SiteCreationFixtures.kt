b3e7461225a3df9cfebfa0f89b25d23db2e03bfb
Ovi Trif <octrif@gmail.com>
2023-03-13 14:26:54 +0100

    Test: move fetching to the preview step and introduce flow fixtures

---
+++ WordPress/src/test/java/org/wordpress/android/ui/sitecreation/SiteCreationFixtures.kt
@@ -0,0 +1,31 @@
+package org.wordpress.android.ui.sitecreation
+
+import org.wordpress.android.fluxc.store.SiteStore.OnSiteChanged
+import org.wordpress.android.fluxc.store.SiteStore.SiteError
+import org.wordpress.android.fluxc.store.SiteStore.SiteErrorType.GENERIC_ERROR
+import org.wordpress.android.ui.sitecreation.SiteCreationResult.Completed
+import org.wordpress.android.ui.sitecreation.SiteCreationResult.NotInLocalDb
+import org.wordpress.android.ui.sitecreation.domains.DomainModel
+import org.wordpress.android.ui.sitecreation.services.SiteCreationServiceState
+import org.wordpress.android.ui.sitecreation.services.SiteCreationServiceState.SiteCreationStep.CREATE_SITE
+import org.wordpress.android.ui.sitecreation.services.SiteCreationServiceState.SiteCreationStep.FAILURE
+import org.wordpress.android.ui.sitecreation.services.SiteCreationServiceState.SiteCreationStep.SUCCESS
+import org.wordpress.android.ui.sitecreation.theme.defaultTemplateSlug
+
+const val SUB_DOMAIN = "test"
+const val URL = "$SUB_DOMAIN.wordpress.com"
+val DOMAIN = DomainModel(URL, true, "", 1)
+
+val SITE_CREATION_STATE = SiteCreationState(segmentId = 1, siteDesign = defaultTemplateSlug, domain = DOMAIN)
+
+const val SITE_REMOTE_ID = 1L
+private const val SITE_LOCAL_ID = 1
+
+val SUCCESS_RESPONSE = OnSiteChanged(1)
+val ERROR_RESPONSE = OnSiteChanged(0).apply { error = SiteError(GENERIC_ERROR) }
+
+val RESULT_NOT_IN_LOCAL_DB = NotInLocalDb(SITE_REMOTE_ID, false)
+val RESULT_COMPLETED = Completed(SITE_LOCAL_ID, false, URL)
+
+val SERVICE_SUCCESS = SiteCreationServiceState(SUCCESS, Pair(SITE_REMOTE_ID, URL))
+val SERVICE_ERROR = SiteCreationServiceState(FAILURE, SiteCreationServiceState(CREATE_SITE))
