ff3f77a31d4294b3131b3198d09aea5a25a797b5
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-03-13 19:25:58 -0400

    Remove unused imports

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/ImageButton.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/ImageButton.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
