d4dc610d7e709f40ae4020bf8a9b5a74565797b7
Thomas Horta <thomas.horta@automattic.com>
2023-03-16 18:29:11 -0300

    populate jetpackCPSites field from RestClient

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteRestClient.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteRestClient.kt
@@ -135,13 +135,15 @@ class SiteRestClient @Inject constructor(
         return when (response) {
             is Success -> {
                 val siteArray = mutableListOf<SiteModel>()
+                val jetpackCPSiteArray = mutableListOf<SiteModel>()
                 for (siteResponse in response.data.sites) {
                     val siteModel = siteResponseToSiteModel(siteResponse)
                     // see https://github.com/wordpress-mobile/WordPress-Android/issues/15540#issuecomment-993752880
+                    if (siteModel.isJetpackCPConnected) jetpackCPSiteArray.add(siteModel)
                     if (filterJetpackConnectedPackageSite && siteModel.isJetpackCPConnected) continue
                     siteArray.add(siteModel)
                 }
-                SitesModel(siteArray)
+                SitesModel(siteArray, jetpackCPSiteArray)
             }
             is Error -> {
                 val payload = SitesModel(emptyList())
