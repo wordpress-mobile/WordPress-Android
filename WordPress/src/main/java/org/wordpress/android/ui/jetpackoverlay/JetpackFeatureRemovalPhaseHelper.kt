9ccdeec60aaf00070efe7a5fbc2344d73720022c
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-17 15:28:07 +0530

    Updates: the logic to remove template selection ability
    Updates: the logic to remove template selection ability in
    static poster phase 4
    
    This commit is based on this commit
     b2feee0b6b988f213b6907626484daaa5d0cc503 from this PR -
     https://github.com/wordpress-mobile/WordPress-Android/pull/17738
     which disabled the feature on phase 4
    
     Added a helper function to disable the template selection in
     pages and replaced the usages of `shouldRemoveJetpackFeatures` with
     `shouldShowTemplateSelectionInPages`

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackoverlay/JetpackFeatureRemovalPhaseHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackoverlay/JetpackFeatureRemovalPhaseHelper.kt
@@ -92,6 +92,14 @@ class JetpackFeatureRemovalPhaseHelper @Inject constructor(
         }
     }
 
+    fun shouldShowTemplateSelectionInPages(): Boolean {
+        val currentPhase = getCurrentPhase() ?: return true
+        return when (currentPhase) {
+            is PhaseStaticPosters, PhaseFour, PhaseNewUsers, PhaseSelfHostedUsers -> false
+            else -> true
+        }
+    }
+
     @Suppress("Unused")
     fun shouldShowStaticPage(): Boolean {
         val currentPhase = getCurrentPhase() ?: return false
