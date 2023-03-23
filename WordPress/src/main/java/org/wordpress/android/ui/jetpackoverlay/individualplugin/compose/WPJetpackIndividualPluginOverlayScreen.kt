e799f4cfc19d5d91f0c8ff1d46fe30679bcbfaa3
Thomas Horta <thomas.horta@automattic.com>
2023-03-23 17:25:45 -0300

    create the overlay base UI

---
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackoverlay/individualplugin/compose/WPJetpackIndividualPluginOverlayScreen.kt
@@ -0,0 +1,208 @@
+package org.wordpress.android.ui.jetpackoverlay.individualplugin.compose
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.wordpress.android.R
+import org.wordpress.android.ui.compose.components.MainTopAppBar
+import org.wordpress.android.ui.compose.components.NavigationIcons
+import org.wordpress.android.ui.compose.components.buttons.ButtonSize
+import org.wordpress.android.ui.compose.components.buttons.PrimaryButton
+import org.wordpress.android.ui.compose.components.buttons.SecondaryButton
+import org.wordpress.android.ui.compose.theme.AppColor
+import org.wordpress.android.ui.compose.theme.AppTheme
+import org.wordpress.android.ui.compose.theme.JpColorPalette
+import org.wordpress.android.ui.jetpackoverlay.individualplugin.SiteWithIndividualJetpackPlugins
+import org.wordpress.android.ui.jetpackplugininstall.fullplugin.onboarding.compose.component.JPInstallFullPluginAnimation
+
+// TODO hthomas move strings to resources
+
+private val TitleTextStyle
+    @ReadOnlyComposable
+    @Composable
+    get() = TextStyle(
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        fontWeight = FontWeight.Bold,
+    )
+
+private val ContentTextStyle
+    @ReadOnlyComposable
+    @Composable
+    get() = TextStyle(
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+    )
+
+@Composable
+fun WPJetpackIndividualPluginOverlayScreen(
+    sites: List<SiteWithIndividualJetpackPlugins>,
+    onCloseClick: () -> Unit,
+    onPrimaryButtonClick: () -> Unit,
+    onSecondaryButtonClick: () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            MainTopAppBar(
+                title = null,
+                navigationIcon = NavigationIcons.CloseIcon,
+                onNavigationIconClick = onCloseClick
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(20.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(1f)) // Spacer to push the content to the center of the screen
+
+            // Icon
+            JPInstallFullPluginAnimation(
+                modifier = Modifier.align(Alignment.Start)
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Title
+            Text(
+                text = getTitle(siteCount = sites.size),
+                style = TitleTextStyle,
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Content
+            // TODO hthomas add content for each variation:
+            //  1. multiple problem sites
+            //  2. single problem site with 1 individual plugin
+            //  3. single problem site with multiple individual plugins
+            Text(
+                text = "You have ${sites.size} sites with individual Jetpack plugins. Switch to Jetpack!",
+                style = ContentTextStyle,
+            )
+
+            Spacer(modifier = Modifier.weight(1f)) // Spacer to push the content to the center of the screen
+
+            // Buttons
+            PrimaryButton(
+                text = "Switch to Jetpack",
+                onClick = onPrimaryButtonClick,
+                buttonSize = ButtonSize.LARGE,
+                padding = PaddingValues(0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = JpColorPalette().primary,
+                    contentColor = AppColor.White,
+                ),
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            SecondaryButton(
+                text = stringResource(R.string.wp_jetpack_continue_without_jetpack),
+                onClick = onSecondaryButtonClick,
+                buttonSize = ButtonSize.LARGE,
+                padding = PaddingValues(0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Transparent,
+                    contentColor = JpColorPalette().primary,
+                ),
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+        }
+    }
+}
+
+@ReadOnlyComposable
+@Composable
+fun getTitle(siteCount: Int): String = if (siteCount > 1) {
+    "Unable to access some of your sites"
+} else {
+    "Unable to access one of your sites"
+}
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun WPJetpackIndividualPluginOverlayScreenSingleSiteSinglePluginPreview() {
+    AppTheme {
+        WPJetpackIndividualPluginOverlayScreen(
+            sites = listOf(
+                SiteWithIndividualJetpackPlugins(
+                    name = "Site 1",
+                    url = "site1.wordpress.com",
+                    individualPluginNames = listOf("Jetpack Social")
+                ),
+            ),
+            onCloseClick = {},
+            onPrimaryButtonClick = {},
+            onSecondaryButtonClick = {},
+        )
+    }
+}
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun WPJetpackIndividualPluginOverlayScreenSingleSiteMultiplePluginsPreview() {
+    AppTheme {
+        WPJetpackIndividualPluginOverlayScreen(
+            sites = listOf(
+                SiteWithIndividualJetpackPlugins(
+                    name = "Site 1",
+                    url = "site1.wordpress.com",
+                    individualPluginNames = listOf("Jetpack Social", "Jetpack Search")
+                ),
+            ),
+            onCloseClick = {},
+            onPrimaryButtonClick = {},
+            onSecondaryButtonClick = {},
+        )
+    }
+}
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun WPJetpackIndividualPluginOverlayScreenMultipleSitesPreview() {
+    AppTheme {
+        WPJetpackIndividualPluginOverlayScreen(
+            sites = listOf(
+                SiteWithIndividualJetpackPlugins(
+                    name = "Site 1",
+                    url = "site1.wordpress.com",
+                    individualPluginNames = listOf("Jetpack Social", "Jetpack Search")
+                ),
+                SiteWithIndividualJetpackPlugins(
+                    name = "Site 2",
+                    url = "site2.wordpress.com",
+                    individualPluginNames = listOf("Jetpack Boost")
+                ),
+            ),
+            onCloseClick = {},
+            onPrimaryButtonClick = {},
+            onSecondaryButtonClick = {},
+        )
+    }
+}
+
