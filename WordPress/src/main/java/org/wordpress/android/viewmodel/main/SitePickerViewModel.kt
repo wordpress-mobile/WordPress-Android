f961c005a2724e2dfb7d1d7571fd61aa4ecddf62
Thomas Horta <thomas.horta@automattic.com>
2023-03-24 17:01:46 -0300

    refactor site picker to use existing event live data

--- WordPress/src/main/java/org/wordpress/android/viewmodel/main/SitePickerViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/main/SitePickerViewModel.kt
@@ -9,13 +9,14 @@ import kotlinx.coroutines.launch
 import org.wordpress.android.ui.jetpackoverlay.individualplugin.WPJetpackIndividualPluginHelper
 import org.wordpress.android.ui.main.SitePickerAdapter.SiteRecord
 import org.wordpress.android.viewmodel.Event
-import org.wordpress.android.viewmodel.SingleLiveEvent
 import org.wordpress.android.viewmodel.main.SitePickerViewModel.Action.AskForSiteSelection
 import org.wordpress.android.viewmodel.main.SitePickerViewModel.Action.ContinueReblogTo
 import org.wordpress.android.viewmodel.main.SitePickerViewModel.Action.NavigateToState
+import org.wordpress.android.viewmodel.main.SitePickerViewModel.Action.ShowJetpackIndividualPluginOverlay
 import org.wordpress.android.viewmodel.main.SitePickerViewModel.ActionType.ASK_FOR_SITE_SELECTION
 import org.wordpress.android.viewmodel.main.SitePickerViewModel.ActionType.CONTINUE_REBLOG_TO
 import org.wordpress.android.viewmodel.main.SitePickerViewModel.ActionType.NAVIGATE_TO_STATE
+import org.wordpress.android.viewmodel.main.SitePickerViewModel.ActionType.SHOW_JETPACK_INDIVIDUAL_PLUGIN_OVERLAY
 import org.wordpress.android.viewmodel.main.SitePickerViewModel.NavigateState.TO_NO_SITE_SELECTED
 import org.wordpress.android.viewmodel.main.SitePickerViewModel.NavigateState.TO_SITE_SELECTED
 import javax.inject.Inject
@@ -26,21 +27,7 @@ class SitePickerViewModel @Inject constructor(
     private val _onActionTriggered = MutableLiveData<Event<Action>>()
     val onActionTriggered: LiveData<Event<Action>> = _onActionTriggered
 
-    private val _showJetpackIndividualPluginOverlay = SingleLiveEvent<Boolean>()
-    val showJetpackIndividualPluginOverlay: LiveData<Boolean> = _showJetpackIndividualPluginOverlay
-
     private var siteForReblog: SiteRecord? = null
-
-    fun checkJetpackIndividualPluginOverlayNeeded() {
-        // don't show if already shown
-        if (_showJetpackIndividualPluginOverlay.value == true) return
-
-        viewModelScope.launch {
-            delay(DELAY_BEFORE_SHOWING_JETPACK_INDIVIDUAL_PLUGIN_OVERLAY)
-            _showJetpackIndividualPluginOverlay
-                .postValue(wpJetpackIndividualPluginHelper.shouldShowJetpackIndividualPluginOverlay())
-        }
-    }
 
     fun onSiteForReblogSelected(siteRecord: SiteRecord) {
         selectSite(siteRecord)
@@ -71,10 +58,24 @@ class SitePickerViewModel @Inject constructor(
         _onActionTriggered.value = Event(NavigateToState(TO_SITE_SELECTED, siteRecord))
     }
 
+    fun onSiteListLoaded() {
+        // don't check if already shown
+        if (_onActionTriggered.value?.peekContent() == ShowJetpackIndividualPluginOverlay) return
+
+        viewModelScope.launch {
+            val showOverlay = wpJetpackIndividualPluginHelper.shouldShowJetpackIndividualPluginOverlay()
+            if (showOverlay) {
+                delay(DELAY_BEFORE_SHOWING_JETPACK_INDIVIDUAL_PLUGIN_OVERLAY)
+                _onActionTriggered.postValue(Event(ShowJetpackIndividualPluginOverlay))
+            }
+        }
+    }
+
     enum class ActionType {
         NAVIGATE_TO_STATE,
         CONTINUE_REBLOG_TO,
-        ASK_FOR_SITE_SELECTION
+        ASK_FOR_SITE_SELECTION,
+        SHOW_JETPACK_INDIVIDUAL_PLUGIN_OVERLAY
     }
 
     enum class NavigateState {
@@ -92,6 +93,8 @@ class SitePickerViewModel @Inject constructor(
         )
 
         object AskForSiteSelection : Action(ASK_FOR_SITE_SELECTION)
+
+        object ShowJetpackIndividualPluginOverlay : Action(SHOW_JETPACK_INDIVIDUAL_PLUGIN_OVERLAY)
     }
 
     companion object {
