043cdacc0628fbd62968f91427b9db7a10134a3a
Siobhan <siobhan@automattic.com>
2023-03-28 12:44:36 +0100

    feat: Use attributes to define/update settings
    The block's settings may differ from the defaults. We'll need to detect this in order to populate the block's content. As such, we'll define a class and use the information we get from the block's attributes to update the settings.

--- WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/VideoPressBlockProcessor.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/VideoPressBlockProcessor.kt
@@ -5,6 +5,39 @@ import org.jsoup.nodes.Document
 import org.wordpress.android.util.helpers.MediaFile
 
 class VideoPressBlockProcessor(localId: String?, mediaFile: MediaFile?) : BlockProcessor(localId, mediaFile) {
+    class VideoPressBlockSettings(
+        var autoplay: Boolean? = null,
+        var controls: Boolean? = null,
+        var loop: Boolean? = null,
+        var muted: Boolean? = null,
+        var persistVolume: Boolean? = null,
+        var playsinline: Boolean? = null,
+        var poster: String? = null,
+        var preload: String? = null,
+        var seekbarColor: String? = null,
+        var seekbarPlayedColor: String? = null,
+        var seekbarLoadingColor: String? = null,
+        var useAverageColor: Boolean? = null
+    ) {
+        constructor(jsonAttributes: JsonObject) : this() {
+            autoplay = jsonAttributes["autoplay"]?.asBoolean ?: false
+            controls = jsonAttributes["controls"]?.asBoolean ?: true
+            loop = jsonAttributes["loop"]?.asBoolean ?: false
+            jsonAttributes["muted"]?.asBoolean?.let { isMuted ->
+                muted = isMuted
+                persistVolume = !isMuted
+            }
+            poster = jsonAttributes["poster"]?.toString()
+            preload = jsonAttributes["preload"]?.toString() ?: "metadata"
+            seekbarColor = jsonAttributes["seekbarColor"]?.toString()
+            seekbarPlayedColor = jsonAttributes["seekbarPlayedColor"]?.toString()
+            seekbarLoadingColor = jsonAttributes["seekbarLoadingColor"]?.toString()
+            useAverageColor = jsonAttributes["useAverageColor"]?.asBoolean ?: true
+        }
+    }
+
+    private var mBlockSettings = VideoPressBlockSettings()
+
     override fun processBlockContentDocument(document: Document?): Boolean {
         val videoPressElements = document?.select("figure")
 
@@ -23,6 +56,9 @@ class VideoPressBlockProcessor(localId: String?, mediaFile: MediaFile?) : BlockP
                 addProperty(ID_ATTRIBUTE, Integer.parseInt(mRemoteId))
                 addProperty(GUID_ATTRIBUTE, mRemoteGuid)
             }
+
+            mBlockSettings = VideoPressBlockSettings(jsonAttributes)
+
             true
         } else {
             false
