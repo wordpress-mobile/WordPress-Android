f4e1284eba6906463c17b8e73bd35f9b85aeb03d
Hafiz Rahman <hfzrhmn@gmail.com>
2023-04-10 22:14:15 +0700

    Add: Store mechanism to fetch JP connection URL using application passwords.
    In the case of application passwords, we can't register a Jetpack site with it, so we just return the URL result immediately.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/JetpackStore.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/JetpackStore.kt
@@ -200,11 +200,12 @@ class JetpackStore
 
     suspend fun fetchJetpackConnectionUrl(
         site: SiteModel,
-        autoRegisterSiteIfNeeded: Boolean = false
+        autoRegisterSiteIfNeeded: Boolean = false,
+        useApplicationPasswords: Boolean = false
     ): JetpackConnectionUrlResult {
         if (site.isUsingWpComRestApi) error("This function supports only self-hosted site using WPAPI")
         return coroutineEngine.withDefaultContext(T.API, this, "fetchJetpackConnectionUrl") {
-            val result = jetpackWPAPIRestClient.fetchJetpackConnectionUrl(site)
+            val result = jetpackWPAPIRestClient.fetchJetpackConnectionUrl(site, useApplicationPasswords)
 
             when {
                 result.isError -> JetpackConnectionUrlResult(
@@ -221,7 +222,7 @@ class JetpackStore
                 else -> {
                     val url = result.result.trim('"').replace("\\", "")
                     val connectionUri = URI.create(url)
-                    if (!autoRegisterSiteIfNeeded || connectionUri.host == JETPACK_DOMAIN) {
+                    if (!autoRegisterSiteIfNeeded || connectionUri.host == JETPACK_DOMAIN || useApplicationPasswords) {
                         JetpackConnectionUrlResult(url)
                     } else {
                         registerJetpackSite(url).fold(
