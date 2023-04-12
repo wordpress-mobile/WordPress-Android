8c4b64e45b73c1e4dec6f9bd121d03b9ba9082ed
Thomas Horta <thomas.horta@automattic.com>
2023-04-12 17:43:34 -0300

    add TODO to update the FluxC Prompts model

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/bloggingprompts/BloggingPromptModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/bloggingprompts/BloggingPromptModel.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.fluxc.model.bloggingprompts
 
 import java.util.Date
 
+// TODO #2710: add new response field (at least answered_link) and remove title and content
 data class BloggingPromptModel(
     val id: Int,
     val text: String,
