a4f9e437bc32f00776a676431f51328178704507
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-04-16 10:07:32 -0400

    Add the builder to create the dashboard activity card

---
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/activity/ActivityCardBuilder.kt
@@ -0,0 +1,59 @@
+package org.wordpress.android.ui.mysite.cards.dashboard.activity
+
+import org.wordpress.android.R
+import org.wordpress.android.fluxc.model.activity.ActivityLogModel
+import org.wordpress.android.ui.activitylog.list.ActivityLogListItem
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.ActivityCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.ActivityCard.ActivityCardWithItems
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.ActivityCard.ActivityCardWithItems.ActivityItem
+import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.ActivityCardBuilderParams
+import org.wordpress.android.ui.utils.ListItemInteraction
+import org.wordpress.android.ui.utils.UiString
+import org.wordpress.android.ui.utils.UiString.UiStringRes
+import org.wordpress.android.util.DateTimeUtilsWrapper
+import org.wordpress.android.util.config.DashboardCardActivityLogConfig
+import java.util.Date
+import javax.inject.Inject
+
+private const val MAX_ITEMS_IN_CARD: Int = 3
+
+class ActivityCardBuilder @Inject constructor(
+    private val dashboardCardActivityLogConfig: DashboardCardActivityLogConfig,
+    private val dateTimeUtilsWrapper: DateTimeUtilsWrapper
+) {
+    fun build(params: ActivityCardBuilderParams): ActivityCard? {
+        if (!dashboardCardActivityLogConfig.isEnabled() ||
+            params.activityCardModel == null ||
+            params.activityCardModel.activities.isEmpty()) {
+            return null
+        }
+        return convertToActivityItems(params)
+    }
+
+    private fun convertToActivityItems(params: ActivityCardBuilderParams): ActivityCardWithItems {
+        val activities = params.activityCardModel?.activities
+        val content = activities?.take(MAX_ITEMS_IN_CARD)?.mapToActivityItems(params.onActivityItemClick) ?: emptyList()
+        return ActivityCardWithItems(
+            title = UiStringRes(R.string.dashboard_activity_card_title),
+            activityItems = content,
+            footerLink = ActivityCard.FooterLink(
+                label = UiStringRes(R.string.dashboard_activity_card_footer_link),
+                onClick = params.onFooterLinkClick
+            )
+        )
+    }
+
+    private fun List<ActivityLogModel>.mapToActivityItems(onClick: (activityId: String) -> Unit) =
+        map {
+            ActivityItem(
+                title =  UiString.UiStringText(it.summary),
+                content = it.content?.text?:"",
+                displayDate = buildDateLine(it.published),
+                icon = ActivityLogListItem.Icon.fromValue(it.gridicon).drawable,
+                iconBackgroundColor = ActivityLogListItem.Status.fromValue(it.status).color,
+                onClick = ListItemInteraction.create(it.activityID, onClick),
+            )
+        }
+
+    private fun buildDateLine(published: Date) = dateTimeUtilsWrapper.javaDateToTimeSpan(published)
+}
