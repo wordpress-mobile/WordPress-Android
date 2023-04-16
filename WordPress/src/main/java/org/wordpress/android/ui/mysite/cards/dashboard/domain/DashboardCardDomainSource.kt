2743f31bb89bb548ffcbf3278530be81197879e1
Irfan Omur <irfan.omur@automattic.com>
2023-04-16 03:32:55 +0300

    Fix the infinite loading indicator issue on my site
    This fixes the incorrect endpoint for the domains dashboard card and resolves the issue of infinite loading indicator on my site screen.

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/domain/DashboardCardDomainSource.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/domain/DashboardCardDomainSource.kt
@@ -3,18 +3,11 @@ package org.wordpress.android.ui.mysite.cards.dashboard.domain
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
-import kotlinx.coroutines.CancellableContinuation
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
-import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.generated.SiteActionBuilder
 import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.store.SiteStore.FetchedDomainsPayload
+import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.fluxc.utils.AppLogWrapper
 import org.wordpress.android.modules.BG_THREAD
 import org.wordpress.android.ui.mysite.MySiteSource
@@ -24,25 +17,15 @@ import org.wordpress.android.ui.plans.hasSiteDomains
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
 import javax.inject.Named
-import kotlin.coroutines.resume
 
 class DashboardCardDomainSource @Inject constructor(
     @param:Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
-    private val dispatcher: Dispatcher,
     private val selectedSiteRepository: SelectedSiteRepository,
+    private val siteStore: SiteStore,
+    private val domainUtils: DashboardCardDomainUtils,
     private val appLogWrapper: AppLogWrapper
 ) : MySiteSource.MySiteRefreshSource<MySiteUiState.PartialState.DomainsAvailable> {
     override val refresh = MutableLiveData(false)
-
-    private val continuations = mutableMapOf<Int, CancellableContinuation<FetchedDomainsPayload>?>()
-
-    init {
-        dispatcher.register(this)
-    }
-
-    fun clear() {
-        dispatcher.unregister(this)
-    }
 
     override fun build(
         coroutineScope: CoroutineScope,
@@ -52,6 +35,15 @@ class DashboardCardDomainSource @Inject constructor(
         data.addSource(refresh) { data.refreshData(coroutineScope, siteLocalId, refresh.value) }
         refresh()
         return data
+    }
+
+    private fun shouldFetchDomains(selectedSite: SiteModel): Boolean {
+        // By assuming that "isDomainCreditAvailable" and "hasSiteDomain" are true, we are checking other cases for
+        // "shouldShowCard". If "shouldShowCard" still returns false, then we do not need to fetch domains.
+        val isDomainCreditAvailable = true
+        val hasSiteDomain = true
+
+        return domainUtils.shouldShowCard(selectedSite, isDomainCreditAvailable, hasSiteDomain)
     }
 
     private fun MediatorLiveData<MySiteUiState.PartialState.DomainsAvailable>.refreshData(
@@ -71,58 +63,30 @@ class DashboardCardDomainSource @Inject constructor(
         siteLocalId: Int,
         selectedSite: SiteModel?
     ) {
-        if (selectedSite == null || selectedSite.id != siteLocalId) {
+        if (selectedSite == null || selectedSite.id != siteLocalId || !shouldFetchDomains(selectedSite)) {
             postState(MySiteUiState.PartialState.DomainsAvailable(false))
         } else {
-            fetchDomainsAndRefreshData(coroutineScope, siteLocalId, selectedSite)
+            fetchDomainsAndRefreshData(coroutineScope, selectedSite)
         }
     }
 
     private fun MediatorLiveData<MySiteUiState.PartialState.DomainsAvailable>.fetchDomainsAndRefreshData(
         coroutineScope: CoroutineScope,
-        siteLocalId: Int,
         selectedSite: SiteModel
     ) {
-        if (continuations[siteLocalId] == null) {
-            coroutineScope.launch(bgDispatcher) { fetchDomains(siteLocalId, selectedSite) }
-        } else {
-            appLogWrapper.d(AppLog.T.DOMAIN_REGISTRATION, "A request is already running for $siteLocalId")
-        }
-    }
+        coroutineScope.launch(bgDispatcher) {
+            val result = siteStore.fetchSiteDomains(selectedSite)
+            val domains = result.domains
+            val error = result.error
 
-    @Suppress("SwallowedException")
-    private suspend fun MediatorLiveData<MySiteUiState.PartialState.DomainsAvailable>.fetchDomains(
-        siteLocalId: Int,
-        selectedSite: SiteModel
-    ) {
-        try {
-            val event = suspendCancellableCoroutine<FetchedDomainsPayload> { cancellableContinuation ->
-                continuations[siteLocalId] = cancellableContinuation
-                dispatchFetchDomains(selectedSite)
+            if (result.isError) {
+                appLogWrapper.e(
+                    AppLog.T.DOMAIN_REGISTRATION,
+                    "An error occurred while fetching domains: ${error.message}"
+                )
             }
-            when {
-                event.isError -> {
-                    val message = "An error occurred while fetching plans :${event.error.message}"
-                    appLogWrapper.e(AppLog.T.DOMAIN_REGISTRATION, message)
-                    postState(MySiteUiState.PartialState.DomainsAvailable(false))
-                }
-                siteLocalId == event.site.id -> {
-                    postState(MySiteUiState.PartialState.DomainsAvailable(hasSiteDomains(event.domains)))
-                }
-                else -> {
-                    postState(MySiteUiState.PartialState.DomainsAvailable(false))
-                }
-            }
-        } catch (e: CancellationException) {
-            postState(MySiteUiState.PartialState.DomainsAvailable(false))
+
+            postState(MySiteUiState.PartialState.DomainsAvailable(hasSiteDomains(domains)))
         }
-    }
-
-    private fun dispatchFetchDomains(site: SiteModel) = dispatcher.dispatch(SiteActionBuilder.newFetchPlansAction(site))
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onDomainsFetched(event: FetchedDomainsPayload) {
-        continuations[event.site.id]?.resume(event)
-        continuations[event.site.id] = null
     }
 }
