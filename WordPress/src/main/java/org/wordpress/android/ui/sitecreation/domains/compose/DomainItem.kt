29a75d6d21ffefcb2c0557a8c0bb4a8082de97fe
Ovi Trif <octrif@gmail.com>
2023-04-20 17:59:48 +0200

    Update: clicked item ripple color

--- WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/compose/DomainItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitecreation/domains/compose/DomainItem.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.ui.sitecreation.domains.compose
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,7 +13,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Unspecified
@@ -45,7 +48,11 @@ fun DomainItem(uiState: DomainUiState) = with(uiState) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .clickable { onClick.invoke() }
+                .clickable(
+                    interactionSource = remember(::MutableInteractionSource),
+                    indication = rememberRipple(color = HighlightBgColor),
+                    onClick = onClick::invoke,
+                )
                 .padding(vertical = Margin.ExtraLarge.value)
                 .padding(end = Margin.ExtraLarge.value)
         ) {
