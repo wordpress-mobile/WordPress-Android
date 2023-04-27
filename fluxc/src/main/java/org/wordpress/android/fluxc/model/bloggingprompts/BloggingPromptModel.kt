66c9d7626a1c69a7f542c07a5062614a19c83835
Thomas Horta <thomas.horta@automattic.com>
2023-04-27 18:08:24 -0300

    update local data models and room entities for prompts

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/bloggingprompts/BloggingPromptModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/bloggingprompts/BloggingPromptModel.kt
@@ -2,15 +2,13 @@ package org.wordpress.android.fluxc.model.bloggingprompts
 
 import java.util.Date
 
-// TODO #2710: add new response field (at least answered_link) and remove title and content
 data class BloggingPromptModel(
     val id: Int,
     val text: String,
-    val title: String,
-    val content: String,
     val date: Date,
     val isAnswered: Boolean,
     val attribution: String,
     val respondentsCount: Int,
-    val respondentsAvatarUrls: List<String>
+    val respondentsAvatarUrls: List<String>,
+    val answeredLink: String,
 )
