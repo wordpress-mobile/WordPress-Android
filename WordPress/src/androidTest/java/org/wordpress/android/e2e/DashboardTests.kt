2aaffb2394f96790c3b70a55dc1f2b3d61f9dca9
pachlava <pachlava@gmail.com>
2023-05-02 10:58:57 +0300

    Disabled Domains Card test for WP

--- WordPress/src/androidTest/java/org/wordpress/android/e2e/DashboardTests.kt
+++ WordPress/src/androidTest/java/org/wordpress/android/e2e/DashboardTests.kt
@@ -1,15 +1,21 @@
 package org.wordpress.android.e2e
 
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assume.assumeTrue
 import org.junit.Before
 import org.junit.Test
 import org.wordpress.android.e2e.pages.MySitesPage
 import org.wordpress.android.support.BaseTest
+import org.wordpress.android.test.BuildConfig
 
 @HiltAndroidTest
 class DashboardTests : BaseTest() {
     @Before
     fun setUp() {
+        // We run the class for JP only (so far the class contains
+        // only a test for Domains card, which in not valid for WP)
+        assumeTrue(BuildConfig.IS_JETPACK_APP)
+
         logoutIfNecessary()
         wpLogin()
     }
@@ -17,7 +23,6 @@ class DashboardTests : BaseTest() {
     @Test
     fun e2eDomainsCardNavigation() {
         MySitesPage()
-            .go()
             .scrollToDomainsCard()
             .assertDomainsCard()
             .tapDomainsCard()
