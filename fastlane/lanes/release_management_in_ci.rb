b656d9f1549b17614c67b98096d2ad318879b8a7
Oguz Kocer <oguz.kocer@automattic.com>
2023-05-04 23:31:04 -0400

    Adds Buildkite triggers to release_management_in_ci lanes

---
+++ fastlane/lanes/release_management_in_ci.rb
@@ -0,0 +1,64 @@
+BUILDKITE_ORGANIZATION = 'automattic'.freeze
+BUILDKITE_PIPELINE = 'wordpress-android'.freeze
+platform :android do
+  #####################################################################################
+  # Triggers for Buildkite
+  #####################################################################################
+  lane :trigger_code_freeze_in_ci do |options|
+    buildkite_trigger_build(
+      buildkite_organization: BUILDKITE_ORGANIZATION,
+      buildkite_pipeline: BUILDKITE_PIPELINE,
+      branch: 'trunk',
+      pipeline_file: 'code-freeze.yml',
+      message: 'Code Freeze in CI'
+    )
+  end
+
+  lane :trigger_complete_code_freeze_in_ci do |options|
+    release_version = options[:release_version]
+    buildkite_trigger_build(
+      buildkite_organization: BUILDKITE_ORGANIZATION,
+      buildkite_pipeline: BUILDKITE_PIPELINE,
+      branch: "release/#{release_version}",
+      pipeline_file: 'complete-code-freeze.yml',
+      message: 'Complete Code Freeze in CI',
+      environment: { RELEASE_VERSION: release_version }
+    )
+  end
+
+  lane :trigger_finalize_release_in_ci do |options|
+    release_version = options[:release_version]
+    buildkite_trigger_build(
+      buildkite_organization: BUILDKITE_ORGANIZATION,
+      buildkite_pipeline: BUILDKITE_PIPELINE,
+      branch: "release/#{release_version}",
+      pipeline_file: 'finalize-release.yml',
+      message: 'Finalize release',
+      environment: { RELEASE_VERSION: release_version }
+    )
+  end
+
+  lane :trigger_new_beta_release_in_ci do |options|
+    release_version = options[:release_version]
+    buildkite_trigger_build(
+      buildkite_organization: BUILDKITE_ORGANIZATION,
+      buildkite_pipeline: BUILDKITE_PIPELINE,
+      branch: "release/#{release_version}",
+      pipeline_file: 'new-beta-release.yml',
+      message: 'New beta release',
+      environment: { RELEASE_VERSION: release_version }
+    )
+  end
+
+  lane :trigger_update_appstore_strings_in_ci do |options|
+    release_version = options[:release_version]
+    editorial_branch = options[:editorial_branch]
+    buildkite_trigger_build(
+      buildkite_organization: BUILDKITE_ORGANIZATION,
+      buildkite_pipeline: BUILDKITE_PIPELINE,
+      branch: "#{editorial_branch}",
+      pipeline_file: 'update-release-notes.yml',
+      message: 'Update release notes',
+      environment: { RELEASE_VERSION: release_version, EDITORIAL_BRANCH: editorial_branch }
+    )
+end
