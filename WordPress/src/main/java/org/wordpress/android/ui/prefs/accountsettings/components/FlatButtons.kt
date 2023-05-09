f478167260753de9a9d1be7db37c70ff33aec038
Ovi Trif <octrif@gmail.com>
2023-05-09 22:31:30 +0200

    Refactor: remove unused imports

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/components/FlatButtons.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/components/FlatButtons.kt
@@ -5,12 +5,10 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.LocalContentColor
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.wordpress.android.ui.compose.theme.AppColor
 
