e78ca07ca6e7fa3f20cb87711b6af0d9a34a0c5c
Matthew Kevins <mmkevins@yahoo.com>
2023-05-09 14:46:53 +1000

    Add ineligible dialog to the ui

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/components/AccountClosureUi.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/components/AccountClosureUi.kt
@@ -3,16 +3,26 @@ package org.wordpress.android.ui.prefs.accountsettings.components
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import org.wordpress.android.ui.prefs.accountsettings.AccountSettingsViewModel
+import org.wordpress.android.ui.prefs.accountsettings.AccountSettingsViewModel.AccountClosureUiState.Opened
 
 @Composable
 fun AccountClosureUi(viewModel: AccountSettingsViewModel) {
     val uiState = viewModel.accountClosureUiState.collectAsState()
 
     CloseAccountButton(onClick = { viewModel.openAccountClosureDialog() })
-    (uiState.value as? AccountSettingsViewModel.AccountClosureUiState.Opened)?.username?.let { currentUsername ->
-        AccountClosureDialog(
-            onDismissRequest = { viewModel.dismissAccountClosureDialog() },
-            currentUsername,
-        )
+
+    (uiState.value as? Opened)?.let {
+        when(it) {
+           is Opened.Default -> it.username?.let { currentUsername ->
+               AccountClosureDialog(
+                   onDismissRequest = { viewModel.dismissAccountClosureDialog() },
+                   currentUsername,
+               )
+           }
+
+            Opened.Atomic -> IneligibleClosureDialog(
+                onDismissRequest = { viewModel.dismissAccountClosureDialog() },
+            )
+        }
     }
 }
