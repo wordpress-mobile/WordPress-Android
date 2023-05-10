7f19187b940fe731d56b06e21f6d37e28b81311d
Oguz Kocer <oguz.kocer@automattic.com>
2023-05-10 17:36:29 -0400

    Set remote url in configure-git-for-release-management.sh

--- .buildkite/commands/configure-git-for-release-management.sh
+++ .buildkite/commands/configure-git-for-release-management.sh
@@ -5,3 +5,6 @@
 curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts
 git config --global user.email "mobile+wpmobilebot@automattic.com"
 git config --global user.name "Buildkite"
+
+# Buildkite is currently using the https url to checkout. We need to override it to be able to use the deploy key.
+git remote set-url origin git@github.com:wordpress-mobile/WordPress-Android.git
