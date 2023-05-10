08b6070df41ed892d933a23f60cd53047aed2e9f
Oguz Kocer <oguz.kocer@automattic.com>
2023-05-10 17:16:22 -0400

    Move create_release_management_pull_request to a new helper file

--- fastlane/lanes/release_management_in_ci.rb
+++ fastlane/lanes/release_management_in_ci.rb
@@ -62,18 +62,4 @@ platform :android do
       environment: { RELEASE_VERSION: release_version, EDITORIAL_BRANCH: editorial_branch }
     )
   end
-
-  #####################################################################################
-  # Release Management Utils
-  #####################################################################################
-  def create_release_management_pull_request(base_branch, title)
-    create_pull_request(
-        api_token: ENV['GITHUB_TOKEN'],
-        repo: GHHELPER_REPO,
-        title: title,
-        head: Fastlane::Helper::GitHelper.current_git_branch,
-        base: base_branch,
-        labels: 'Releases'
-    )
-  end
 end
