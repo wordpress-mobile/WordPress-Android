0af7b9709715abf9278e0e31142c34a024a4ac95
Oguz Kocer <oguzkocer@users.noreply.github.com>
2023-05-11 23:14:39 -0400

    Change Buildkite git user name to 'Automattic Release Bot'
    Co-authored-by: Gio Lodi <gio.lodi@automattic.com>

--- .buildkite/commands/configure-git-for-release-management.sh
+++ .buildkite/commands/configure-git-for-release-management.sh
@@ -4,7 +4,7 @@
 # Later on, we should be able to configure the agent instead.
 curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >> ~/.ssh/known_hosts
 git config --global user.email "mobile+wpmobilebot@automattic.com"
-git config --global user.name "Buildkite"
+git config --global user.name "Automattic Release Bot"
 
 # Buildkite is currently using the https url to checkout. We need to override it to be able to use the deploy key.
 git remote set-url origin git@github.com:wordpress-mobile/WordPress-Android.git
