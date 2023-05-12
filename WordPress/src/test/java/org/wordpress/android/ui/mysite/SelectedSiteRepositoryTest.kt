5926db0647ab39ede39c65830a052caeb1ca9452
Thomas Horta <thomashorta@gmail.com>
2023-05-12 17:59:58 -0300

    Revert "[Pages List] Detect block-based theme on PageListViewModel"

--- WordPress/src/test/java/org/wordpress/android/ui/mysite/SelectedSiteRepositoryTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/mysite/SelectedSiteRepositoryTest.kt
@@ -15,13 +15,11 @@ import org.mockito.kotlin.verifyNoInteractions
 import org.mockito.kotlin.whenever
 import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.action.EditorThemeAction
 import org.wordpress.android.fluxc.action.SiteAction
 import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
 import org.wordpress.android.ui.prefs.SiteSettingsInterfaceWrapper
-import org.wordpress.android.util.config.GlobalStyleSupportFeatureConfig
 
 @ExperimentalCoroutinesApi
 class SelectedSiteRepositoryTest : BaseUnitTest() {
@@ -36,10 +34,6 @@ class SelectedSiteRepositoryTest : BaseUnitTest() {
 
     @Mock
     lateinit var appPrefsWrapper: AppPrefsWrapper
-
-    @Mock
-    lateinit var globalStyleSupportFeatureConfig: GlobalStyleSupportFeatureConfig
-
     private lateinit var siteModel: SiteModel
     private var siteIconProgressBarVisible: Boolean = false
     private var selectedSite: SiteModel? = null
@@ -52,12 +46,7 @@ class SelectedSiteRepositoryTest : BaseUnitTest() {
 
     @Before
     fun setUp() {
-        selectedSiteRepository = SelectedSiteRepository(
-            dispatcher,
-            siteSettingsInterfaceFactory,
-            appPrefsWrapper,
-            globalStyleSupportFeatureConfig
-        )
+        selectedSiteRepository = SelectedSiteRepository(dispatcher, siteSettingsInterfaceFactory, appPrefsWrapper)
         selectedSiteRepository.showSiteIconProgressBar.observeForever { siteIconProgressBarVisible = it == true }
         selectedSiteRepository.selectedSiteChange.observeForever { selectedSite = it }
         siteModel = SiteModel()
@@ -128,6 +117,7 @@ class SelectedSiteRepositoryTest : BaseUnitTest() {
         selectedSiteRepository.updateTitle(updatedTitle)
 
         assertThat(siteModel.name).isEqualTo(updatedTitle)
+        verify(dispatcher).dispatch(any())
         assertThat(actions.last().payload).isEqualTo(siteModel)
         assertThat(actions.last().type).isEqualTo(SiteAction.UPDATE_SITE)
 
@@ -226,15 +216,6 @@ class SelectedSiteRepositoryTest : BaseUnitTest() {
         selectedSiteRepository.siteSelected.observeForever { emptySiteIdEmitted = true }
 
         assertThat(emptySiteIdEmitted).isTrue
-    }
-
-    @Test
-    fun `Should fetch EditorTheme when updateSiteSettingsIfNecessary is called`() {
-        initializeSiteAndSiteSettings()
-
-        selectedSiteRepository.updateSiteSettingsIfNecessary()
-
-        assertThat(actions.last().type).isEqualTo(EditorThemeAction.FETCH_EDITOR_THEME)
     }
 
     private fun initializeSiteAndSiteSettings() {
