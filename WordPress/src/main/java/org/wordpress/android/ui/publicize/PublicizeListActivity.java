df5afb18e3fc32b149f7c9d60eba265f961d1954
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-05-19 15:42:46 -0300

    Remove PublicizeUpdateServiceV1

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeListActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeListActivity.java
@@ -33,7 +33,6 @@ import org.wordpress.android.ui.LocaleAwareActivity;
 import org.wordpress.android.ui.ScrollableViewInitializedListener;
 import org.wordpress.android.ui.publicize.PublicizeConstants.ConnectAction;
 import org.wordpress.android.ui.publicize.adapters.PublicizeServiceAdapter;
-import org.wordpress.android.ui.publicize.services.PublicizeUpdateServiceV1;
 import org.wordpress.android.util.JetpackBrandingUtils;
 import org.wordpress.android.util.SiteUtils;
 import org.wordpress.android.util.ToastUtils;
@@ -101,7 +100,6 @@ public class PublicizeListActivity extends LocaleAwareActivity
                 finish();
                 return;
             }
-            PublicizeUpdateServiceV1.updateConnectionsForSite(this, mSite);
         } else {
             mSite = (SiteModel) savedInstanceState.getSerializable(WordPress.SITE);
         }
