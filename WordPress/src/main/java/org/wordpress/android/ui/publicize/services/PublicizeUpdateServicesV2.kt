e90141e2ebea908efd7f805ab873500df45e83cb
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-05-19 20:08:13 -0300

    Add unit tests for PublicizeListViewModelTest, simplify PublicizeUpdateServicesV2 logic

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/services/PublicizeUpdateServicesV2.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/services/PublicizeUpdateServicesV2.kt
@@ -2,57 +2,49 @@ package org.wordpress.android.ui.publicize.services
 
 import com.wordpress.rest.RestRequest
 import org.wordpress.android.datasets.PublicizeTable
-import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.models.PublicizeConnectionList
 import org.wordpress.android.models.PublicizeServiceList
-import org.wordpress.android.ui.publicize.PublicizeEvents.ConnectionsChanged
 import org.wordpress.android.util.AppLog
-import org.wordpress.android.util.EventBusWrapper
 import org.wordpress.android.util.RestClientProvider
 import java.util.Locale
 import javax.inject.Inject
 
 class PublicizeUpdateServicesV2 @Inject constructor(
     private val restClientProvider: RestClientProvider,
-    private val eventBusWrapper: EventBusWrapper,
 ) {
-    fun execute(siteModel: SiteModel) {
-        AppLog.d(AppLog.T.SHARING, "${PublicizeUpdateServicesV2::class.simpleName}: updating services and connections")
-        updateServices(siteModel.siteId)
-        updateConnections(siteModel.siteId)
-    }
-
     /*
-     * update the list of publicize services
+     * Updates the list of publicize services
      */
-    private fun updateServices(siteId: Long) {
+    fun updateServices(siteId: Long, success: () -> Unit, failure: (Throwable) -> Unit) {
+        AppLog.d(AppLog.T.SHARING, "${PublicizeUpdateServicesV2::class.simpleName}: updating services")
         val listener = RestRequest.Listener { jsonObject ->
             val serverList = PublicizeServiceList.fromJson(jsonObject)
             val localList = PublicizeTable.getServiceList()
             if (!serverList.isSameAs(localList)) {
                 PublicizeTable.setServiceList(serverList)
-                eventBusWrapper.post(ConnectionsChanged())
+                success()
             }
         }
-        val errorListener = RestRequest.ErrorListener { volleyError -> AppLog.e(AppLog.T.SHARING, volleyError) }
+        val errorListener = RestRequest.ErrorListener { volleyError -> failure(volleyError) }
         val path = "sites/$siteId/external-services?type=publicize"
-        restClientProvider.getRestClientUtilsV2()[path, null, null, listener, errorListener]
+        restClientProvider.getRestClientUtilsV2().get(path, listener, errorListener)
     }
 
     /*
-     * update the connections for the passed blog
+     * Updates the connections for a site
      */
-    private fun updateConnections(siteId: Long) {
+    fun updateConnections(siteId: Long, success: () -> Unit, failure: (Throwable) -> Unit) {
+        AppLog.d(AppLog.T.SHARING, "${PublicizeUpdateServicesV2::class.simpleName}: updating connections")
         val listener = RestRequest.Listener { jsonObject ->
             val serverList = PublicizeConnectionList.fromJson(jsonObject)
             val localList = PublicizeTable.getConnectionsForSite(siteId)
             if (!serverList.isSameAs(localList)) {
                 PublicizeTable.setConnectionsForSite(siteId, serverList)
-                eventBusWrapper.post(ConnectionsChanged())
+                success()
             }
         }
-        val errorListener = RestRequest.ErrorListener { volleyError -> AppLog.e(AppLog.T.SHARING, volleyError) }
+        val errorListener = RestRequest.ErrorListener { volleyError -> failure(volleyError) }
         val path = String.format(Locale.ROOT, "sites/%d/publicize-connections", siteId)
-        restClientProvider.getRestClientUtilsV1_1()[path, null, null, listener, errorListener]
+        restClientProvider.getRestClientUtilsV1_1().get(path, listener, errorListener)
     }
 }
