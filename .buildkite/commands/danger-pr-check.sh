23b6739ae4d211a0cfd4541f59c279d853a51c71
Ian Maia <ian.maia@automattic.com>
2023-06-20 16:34:23 +0200

    Add Danger call script

---
+++ .buildkite/commands/danger-pr-check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eu
+
+export DANGER_GITHUB_API_TOKEN="$GITHUB_TOKEN"
+
+echo "--- :rubygems: Setting up Gems"
+bundle install
+
+echo "--- Running Danger"
+bundle exec danger --dangerfile=.buildkite/danger/Dangerfile-pr-check danger_id=pr-check
