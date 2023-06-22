d15b9fea2a0eb86e3dd39d95a8adae0b33dc9224
Ian Maia <ian.maia@automattic.com>
2023-06-22 12:50:39 +0200

    Use install_gems buildkite plugin

--- .buildkite/commands/danger-pr-check.sh
+++ .buildkite/commands/danger-pr-check.sh
@@ -3,7 +3,7 @@
 export DANGER_GITHUB_API_TOKEN="$GITHUB_TOKEN"
 
 echo "--- :rubygems: Setting up Gems"
-bundle install
+install_gems
 
 echo "--- Running Danger"
 bundle exec danger --fail-on-errors=true --dangerfile=.buildkite/danger/Dangerfile-pr-check --danger_id=pr-check
