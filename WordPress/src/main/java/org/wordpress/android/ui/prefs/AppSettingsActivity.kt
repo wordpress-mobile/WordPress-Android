746c1a3a442a5789738a01cb49e1a6c217135751
Ovi Trif <octrif@gmail.com>
2023-06-27 08:02:56 +0200

    Update: raw convert to kotlin

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/AppSettingsActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/AppSettingsActivity.kt
@@ -1,56 +1,49 @@
-package org.wordpress.android.ui.prefs;
+package org.wordpress.android.ui.prefs
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.widget.Toolbar;
-
-import org.wordpress.android.R;
-import org.wordpress.android.WordPress;
-import org.wordpress.android.ui.LocaleAwareActivity;
-
-import dagger.hilt.android.AndroidEntryPoint;
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import dagger.hilt.android.AndroidEntryPoint
+import org.wordpress.android.R
+import org.wordpress.android.WordPress
+import org.wordpress.android.ui.LocaleAwareActivity
 
 @AndroidEntryPoint
-public class AppSettingsActivity extends LocaleAwareActivity {
-    public static final String EXTRA_SHOW_PRIVACY_SETTINGS = "extra_show_privacy_settings";
-    public static final String EXTRA_REQUESTED_ANALYTICS_VALUE_FROM_ERROR =
-            "extra_requested_analytics_value_from_error";
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ((WordPress) getApplication()).component().inject(this);
-        setContentView(R.layout.app_settings_activity);
-
-        Toolbar toolbar = findViewById(R.id.toolbar_main);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionBar = getSupportActionBar();
+class AppSettingsActivity : LocaleAwareActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (application as WordPress).component().inject(this)
+        setContentView(R.layout.app_settings_activity)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
         if (actionBar != null) {
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setTitle(R.string.me_btn_app_settings);
+            actionBar.setHomeButtonEnabled(true)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setTitle(R.string.me_btn_app_settings)
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            getOnBackPressedDispatcher().onBackPressed();
-            return true;
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressedDispatcher.onBackPressed()
+            return true
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item)
     }
 
-    @Override
-    public void recreate() {
-        Intent intent = new Intent(AppSettingsActivity.this, AppSettingsActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-        finish();
+    override fun recreate() {
+        val intent = Intent(this@AppSettingsActivity, AppSettingsActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        finish()
+    }
+
+    companion object {
+        const val EXTRA_SHOW_PRIVACY_SETTINGS = "extra_show_privacy_settings"
+        const val EXTRA_REQUESTED_ANALYTICS_VALUE_FROM_ERROR =
+            "extra_requested_analytics_value_from_error"
     }
 }
