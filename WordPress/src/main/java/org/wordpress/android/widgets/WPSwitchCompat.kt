dc1265c6fb040d5b68c0074c8cb3ba30276a3a6c
Thomas Horta <thomas.horta@automattic.com>
2023-06-30 12:51:32 -0300

    add some disclaimer to WPSwitchCompat

--- WordPress/src/main/java/org/wordpress/android/widgets/WPSwitchCompat.kt
+++ WordPress/src/main/java/org/wordpress/android/widgets/WPSwitchCompat.kt
@@ -11,6 +11,10 @@ import org.wordpress.android.ui.compose.components.buttons.WPSwitch
  * SwitchCompat with custom track and thumb drawables and width to match closely the [WPSwitch] composable. This lets
  * us incrementally adopt Compose and use [WPSwitch] even inside Android View-based layouts without big visual
  * differences.
+ *
+ * Important: since this view was made to match [WPSwitch], in turn matching [androidx.compose.material.Switch], it is
+ * going to be necessary to double check the appearance of this whenever the Material Compose lib is updated (since that
+ * can lead to visual changes in [androidx.compose.material.Switch]).
  */
 class WPSwitchCompat @JvmOverloads constructor(
     context: Context,
