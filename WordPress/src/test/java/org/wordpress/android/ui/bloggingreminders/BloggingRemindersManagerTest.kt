17af63db857f75e6532adc3814703e76f92d0e00
Ravi <ravi.program@gmail.com>
2023-07-04 20:52:24 +1000

    Update BloggingRemindersManagerTest.kt

--- WordPress/src/test/java/org/wordpress/android/ui/bloggingreminders/BloggingRemindersManagerTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/bloggingreminders/BloggingRemindersManagerTest.kt
@@ -7,50 +7,65 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.whenever
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
-import org.wordpress.android.util.config.BloggingRemindersFeatureConfig
+import org.wordpress.android.util.BuildConfigWrapper
 
 @RunWith(MockitoJUnitRunner::class)
 class BloggingRemindersManagerTest {
     @Mock
-    lateinit var bloggingRemindersFeatureConfig: BloggingRemindersFeatureConfig
+    lateinit var buildConfigWrapper: BuildConfigWrapper
 
     @Mock
     lateinit var appPrefsWrapper: AppPrefsWrapper
+
+    @Mock
+    lateinit var siteModel: SiteModel
+
     private lateinit var bloggingRemindersManager: BloggingRemindersManager
     private val siteId = 123
 
     @Before
     fun setUp() {
-        bloggingRemindersManager = BloggingRemindersManager(bloggingRemindersFeatureConfig, appPrefsWrapper)
+        siteModel.siteId = siteId.toLong()
+        bloggingRemindersManager = BloggingRemindersManager(appPrefsWrapper, buildConfigWrapper)
     }
 
     @Test
-    fun `should not show blogging reminders when flag disabled`() {
-        whenever(bloggingRemindersFeatureConfig.isEnabled()).thenReturn(false)
+    fun `should not show blogging reminders when has no edit post capability`() {
+        whenever(buildConfigWrapper.isJetpackApp).thenReturn(true)
+        whenever(siteModel.hasCapabilityEditPosts).thenReturn(false)
+        val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteModel)
 
-        val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteId)
+        assertThat(result).isFalse
+    }
 
-        assertThat(result).isFalse()
+    @Test
+    fun `should show blogging reminders when has edit post capability`() {
+        whenever(buildConfigWrapper.isJetpackApp).thenReturn(true)
+        whenever(siteModel.hasCapabilityEditPosts).thenReturn(true)
+        val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteModel)
+
+        assertThat(result).isFalse
     }
 
     @Test
     fun `should not show blogging reminders when already shown for a site`() {
-        whenever(bloggingRemindersFeatureConfig.isEnabled()).thenReturn(true)
         whenever(appPrefsWrapper.isBloggingRemindersShown(siteId)).thenReturn(true)
 
-        val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteId)
+        val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteModel)
 
-        assertThat(result).isFalse()
+        assertThat(result).isFalse
     }
 
     @Test
-    fun `should show blogging reminders when already not shown for a site and flag enabled`() {
-        whenever(bloggingRemindersFeatureConfig.isEnabled()).thenReturn(true)
+    fun `should show blogging reminders when already not shown for a site`() {
+        whenever(buildConfigWrapper.isJetpackApp).thenReturn(true)
+        whenever(siteModel.hasCapabilityEditPosts).thenReturn(true)
         whenever(appPrefsWrapper.isBloggingRemindersShown(siteId)).thenReturn(false)
 
-        val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteId)
+        val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteModel)
 
-        assertThat(result).isTrue()
+        assertThat(result).isTrue
     }
 }
