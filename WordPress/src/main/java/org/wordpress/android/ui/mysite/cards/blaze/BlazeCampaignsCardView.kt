f8222fd96543df41fd5f70d912d890a12c00e822
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-06 14:37:55 +0530

    + Adds: Blaze Campaigns card to adapter, Adds VH, XML and Composeview

---
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/blaze/BlazeCampaignsCardView.kt
@@ -0,0 +1,91 @@
+package org.wordpress.android.ui.mysite.cards.blaze
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import org.wordpress.android.R
+import org.wordpress.android.ui.compose.unit.FontSize
+import org.wordpress.android.ui.compose.utils.uiStringText
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.BlazeCampaignsCard
+
+@Composable
+@Suppress("FunctionName")
+fun BlazeCampaignsCardView(
+    modifier: Modifier = Modifier,
+    blazeCampaignCard: BlazeCampaignsCard
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(1.dp),
+        border = BorderStroke(
+            width = dimensionResource(id = R.dimen.unelevated_card_stroke_width),
+            color = colorResource(id = R.color.on_surface_divider)
+        ),
+        elevation = 0.dp,
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Text(
+                text = uiStringText(uiString = blazeCampaignCard.title),
+                fontSize = FontSize.DoubleExtraLarge.value,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                color = colorResource(R.color.material_on_surface_emphasis_medium),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            )
+            Text(
+                text = uiStringText(uiString = blazeCampaignCard.campaign.status),
+                fontSize = FontSize.DoubleExtraLarge.value,
+                fontWeight = FontWeight.Normal,
+                color = colorResource(R.color.material_on_surface_emphasis_medium),
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            )
+            Text(
+                text = uiStringText(uiString = blazeCampaignCard.campaign.title),
+                fontSize = FontSize.DoubleExtraLarge.value,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = colorResource(R.color.material_on_surface_emphasis_medium),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            )
+            OutlinedButton(
+                onClick = { blazeCampaignCard.footer.onClick.click() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            ) {
+                Text(
+                    text = uiStringText(uiString = blazeCampaignCard.footer.label),
+                    color = colorResource(R.color.material_on_surface_emphasis_medium),
+                )
+            }
+        }
+    }
+}
---
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/blaze/BlazeCampaignsCardViewHolder.kt
@@ -0,0 +1,27 @@
+package org.wordpress.android.ui.mysite.cards.blaze
+
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.wordpress.android.databinding.CampaignsCardBinding
+import org.wordpress.android.ui.compose.theme.AppTheme
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.BlazeCampaignsCard
+import org.wordpress.android.ui.mysite.cards.dashboard.CardViewHolder
+import org.wordpress.android.util.extensions.viewBinding
+
+class BlazeCampaignsCardViewHolder(parent: ViewGroup) :
+    CardViewHolder<CampaignsCardBinding>(parent.viewBinding(CampaignsCardBinding::inflate)) {
+    fun bind(card: BlazeCampaignsCard) = with(binding) {
+        blazeCampaignsCard.setContent {
+            AppTheme {
+                BlazeCampaignsCardView(
+                    blazeCampaignCard = card, modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                )
+            }
+        }
+    }
+}
