0e776debd5a04ad84cf66f6b9868fe38486369ec
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-09 07:44:10 +0530

    + Adds: tests for BlazeCardBuilder
    + Adds: tests for logic in BlazeCardBuilder

--- WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/blaze/BlazeCardBuilderTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/blaze/BlazeCardBuilderTest.kt
@@ -1,12 +1,66 @@
 package org.wordpress.android.ui.mysite.cards.dashboard.blaze
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
-import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.PromoteWithBlazeCardBuilderParams
-import org.wordpress.android.ui.mysite.cards.blaze.BlazeCardBuilder
+import org.mockito.kotlin.mock
 import org.wordpress.android.R
+import org.wordpress.android.fluxc.model.blaze.BlazeCampaignModel
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.BlazeCampaignsCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.BlazeCampaignsCard.BlazeCampaignsCardFooter
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.BlazeCampaignsCard.BlazeCampaignsCardItem
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.BlazeCampaignsCard.BlazeCampaignsCardItem.BlazeCampaignStats
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.PromoteWithBlazeCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.BlazeCardBuilderParams.CampaignWithBlazeCardBuilderParams
+import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.BlazeCardBuilderParams.PromoteWithBlazeCardBuilderParams
+import org.wordpress.android.ui.mysite.cards.blaze.BlazeCardBuilder
+import org.wordpress.android.ui.utils.ListItemInteraction
 import org.wordpress.android.ui.utils.UiString
+
+val campaign = BlazeCampaignModel(
+    campaignId = 1,
+    title = "title",
+    imageUrl = "imageUrl",
+    startDate = mock(),
+    endDate = mock(),
+    uiStatus = "uiStatus",
+    budgetCents = 20.0,
+    impressions = 1,
+    clicks = 1,
+)
+
+val onCreateCampaignClick = { }
+val onCampaignClick = { }
+val onCardClick = { }
+
+val campaignWithBlazeBuilderParams = CampaignWithBlazeCardBuilderParams(
+    campaign = campaign,
+    onCardClick = onCardClick,
+    onCampaignClick = onCampaignClick,
+    onCreateCampaignClick = onCreateCampaignClick
+)
+
+val blazeCampaignsCard = BlazeCampaignsCard(
+    title = UiString.UiStringRes(R.string.blaze_campaigns_card_title),
+    campaign = BlazeCampaignsCardItem(
+        id = campaign.campaignId,
+        title = UiString.UiStringText(campaign.title),
+        status = UiString.UiStringText(campaign.uiStatus),
+        featuredImageUrl = campaign.imageUrl,
+        stats = BlazeCampaignStats(
+            impressionCount = UiString.UiStringText(campaign.impressions.toString()),
+            clickCount = UiString.UiStringText(campaign.clicks.toString())
+        ),
+        onClick = onCampaignClick,
+    ),
+    footer = BlazeCampaignsCardFooter(
+        label = UiString.UiStringRes(R.string.blaze_campaigns_card_footer_label),
+        onClick = ListItemInteraction.create(onCreateCampaignClick)
+    ),
+    onClick = ListItemInteraction.create(onCardClick),
+)
+
 
 class BlazeCardBuilderTest {
     private lateinit var builder: BlazeCardBuilder
@@ -15,42 +69,43 @@ class BlazeCardBuilderTest {
     fun setUp() {
         builder = BlazeCardBuilder()
     }
+
     @Test
-    fun `when is eligible for blaze, then return the PromoteWithBlazeCard`() {
+    fun `when promoteBlazeBuilderParams, then return the PromoteWithBlazeCard`() {
         // Arrange
         val params = PromoteWithBlazeCardBuilderParams(
-            isEligible = true,
             onClick = { },
             onHideMenuItemClick = { },
             onMoreMenuClick = { }
         )
 
         // Act
-        val result = builder.build(params)
+        val result = builder.build(params) as PromoteWithBlazeCard
 
         // Assert
         assertNotNull(result)
-        assertEquals(R.string.promote_blaze_card_title, (result!!.title as UiString.UiStringRes).stringRes)
-        assertEquals(R.string.promote_blaze_card_sub_title, (result.subtitle as UiString.UiStringRes).stringRes)
+        assertEquals(
+            R.string.promote_blaze_card_title,
+            (result.title as UiString.UiStringRes).stringRes
+        )
+        assertEquals(
+            R.string.promote_blaze_card_sub_title,
+            (result.subtitle as UiString.UiStringRes).stringRes
+        )
         assertNotNull(result.onClick)
         assertNotNull(result.onHideMenuItemClick)
         assertNotNull(result.onMoreMenuClick)
     }
 
     @Test
-    fun `when is not eligible for blaze, then return null`() {
+    fun `when campaignBuilderParams, then return the CampaignCard`() {
         // Arrange
-        val params = PromoteWithBlazeCardBuilderParams(
-            isEligible = false,
-            onClick = { },
-            onHideMenuItemClick = { },
-            onMoreMenuClick = { }
-        )
+        val params = campaignWithBlazeBuilderParams
 
         // Act
-        val result = builder.build(params)
+        val result = builder.build(params) as BlazeCampaignsCard
 
         // Assert
-        assertNull(result)
+        assertEquals(blazeCampaignsCard,result)
     }
 }
