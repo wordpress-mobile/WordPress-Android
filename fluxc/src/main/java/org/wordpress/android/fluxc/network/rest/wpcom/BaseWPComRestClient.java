3571337d2a220c9f6d9ea5797d0b8733e601783f
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-07-09 17:22:11 -0400

    Add constructor to include AcceptHeaderStrategy

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/BaseWPComRestClient.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/BaseWPComRestClient.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
+import org.wordpress.android.fluxc.network.AcceptHeaderStrategy;
 import org.wordpress.android.fluxc.network.BaseRequest;
 import org.wordpress.android.fluxc.network.BaseRequest.OnAuthFailedListener;
 import org.wordpress.android.fluxc.network.BaseRequest.OnParseErrorListener;
@@ -34,6 +35,7 @@ public abstract class BaseWPComRestClient {
     protected final Context mAppContext;
     protected final Dispatcher mDispatcher;
     protected UserAgent mUserAgent;
+    protected AcceptHeaderStrategy mAcceptHeaderStrategy;
 
     private OnAuthFailedListener mOnAuthFailedListener;
     private OnParseErrorListener mOnParseErrorListener;
@@ -41,11 +43,17 @@ public abstract class BaseWPComRestClient {
 
     public BaseWPComRestClient(Context appContext, Dispatcher dispatcher, RequestQueue requestQueue,
                                AccessToken accessToken, UserAgent userAgent) {
+        this(appContext, dispatcher, requestQueue, accessToken, userAgent, null);
+    }
+    public BaseWPComRestClient(Context appContext, Dispatcher dispatcher, RequestQueue requestQueue,
+                               AccessToken accessToken, UserAgent userAgent,
+                               AcceptHeaderStrategy acceptHeaderStrategy) {
         mRequestQueue = requestQueue;
         mDispatcher = dispatcher;
         mAccessToken = accessToken;
         mUserAgent = userAgent;
         mAppContext = appContext;
+        mAcceptHeaderStrategy = acceptHeaderStrategy;
         mOnAuthFailedListener = new OnAuthFailedListener() {
             @Override
             public void onAuthFailed(AuthenticateErrorPayload authError) {
@@ -121,6 +129,7 @@ public abstract class BaseWPComRestClient {
         if (request.shouldCache() && request.shouldForceUpdate()) {
             mRequestQueue.getCache().invalidate(request.mUri.toString(), true);
         }
+        addAcceptHeaderIfNeeded(request);
         return mRequestQueue.add(request);
     }
 
@@ -133,6 +142,13 @@ public abstract class BaseWPComRestClient {
             request.addQueryParameter(localeParamName, LanguageUtils.getPatchedCurrentDeviceLanguage(mAppContext));
         }
     }
+
+    private void addAcceptHeaderIfNeeded(BaseRequest request) {
+        if (mAcceptHeaderStrategy == null) return;
+
+        request.addHeader(mAcceptHeaderStrategy.getHeader(), mAcceptHeaderStrategy.getValue());
+    }
+
 
     private @NotNull String getLocaleParamName(@NotNull String url) {
         return url.contains(WPCOM_V2_PREFIX) ? LOCALE_PARAM_NAME_FOR_V2 : LOCALE_PARAM_NAME_FOR_V1;
