2d7c31b5548dbff6475678e68dda64dfa95a1838
Ravi <ravi.program@gmail.com>
2023-07-11 16:45:05 +1000

    Update BloggingRemindersManagerTest.kt

--- WordPress/src/test/java/org/wordpress/android/ui/bloggingreminders/BloggingRemindersManagerTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/bloggingreminders/BloggingRemindersManagerTest.kt
@@ -8,35 +8,22 @@ import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.whenever
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
-import org.wordpress.android.util.config.BloggingRemindersFeatureConfig
 
 @RunWith(MockitoJUnitRunner::class)
 class BloggingRemindersManagerTest {
     @Mock
-    lateinit var bloggingRemindersFeatureConfig: BloggingRemindersFeatureConfig
-
-    @Mock
     lateinit var appPrefsWrapper: AppPrefsWrapper
+
     private lateinit var bloggingRemindersManager: BloggingRemindersManager
     private val siteId = 123
 
     @Before
     fun setUp() {
-        bloggingRemindersManager = BloggingRemindersManager(bloggingRemindersFeatureConfig, appPrefsWrapper)
-    }
-
-    @Test
-    fun `should not show blogging reminders when flag disabled`() {
-        whenever(bloggingRemindersFeatureConfig.isEnabled()).thenReturn(false)
-
-        val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteId)
-
-        assertThat(result).isFalse()
+        bloggingRemindersManager = BloggingRemindersManager(appPrefsWrapper)
     }
 
     @Test
     fun `should not show blogging reminders when already shown for a site`() {
-        whenever(bloggingRemindersFeatureConfig.isEnabled()).thenReturn(true)
         whenever(appPrefsWrapper.isBloggingRemindersShown(siteId)).thenReturn(true)
 
         val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteId)
@@ -46,7 +33,6 @@ class BloggingRemindersManagerTest {
 
     @Test
     fun `should show blogging reminders when already not shown for a site and flag enabled`() {
-        whenever(bloggingRemindersFeatureConfig.isEnabled()).thenReturn(true)
         whenever(appPrefsWrapper.isBloggingRemindersShown(siteId)).thenReturn(false)
 
         val result = bloggingRemindersManager.shouldShowBloggingRemindersPrompt(siteId)
