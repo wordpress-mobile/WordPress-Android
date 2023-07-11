d9ee7baeff20c91635f91e9f590158146ad49bf4
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-11 21:27:48 +0530

    * Moves: DashboardCard to compose package

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/card/DashboardCard.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/card/DashboardCard.kt
@@ -1,4 +1,4 @@
-package org.wordpress.android.ui.compose.components
+package org.wordpress.android.ui.compose.components.card
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
