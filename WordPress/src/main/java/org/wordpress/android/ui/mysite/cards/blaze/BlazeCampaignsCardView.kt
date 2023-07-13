f3b24ac50a9f3f81fbb8e3226e1005281592e8e4
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-13 18:27:35 +0530

    +[WIP] Adds: the compose view for campaign stats
    + Adds: the Campaign Stat view and the logic for rendering it

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/blaze/BlazeCampaignsCardView.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/blaze/BlazeCampaignsCardView.kt
@@ -1,16 +1,23 @@
 package org.wordpress.android.ui.mysite.cards.blaze
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -23,11 +30,14 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.components.card.DashboardCard
+import org.wordpress.android.ui.compose.styles.detailText
 import org.wordpress.android.ui.compose.styles.footerCTA
+import org.wordpress.android.ui.compose.styles.largeText
 import org.wordpress.android.ui.compose.styles.smallTitle
 import org.wordpress.android.ui.compose.styles.subTitle
 import org.wordpress.android.ui.compose.utils.uiStringText
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.BlazeCampaignsCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.BlazeCard.BlazeCampaignsCard.BlazeCampaignsCardItem.BlazeCampaignStats
 import org.wordpress.android.ui.utils.UiString
 
 @Composable
@@ -61,6 +71,16 @@ fun BlazeCampaignsCardView(
                 .fillMaxWidth()
                 .wrapContentHeight()
         )
+        if (blazeCampaignCard.campaign.stats != null) {
+            CampaignStats(
+                campaignStats = blazeCampaignCard.campaign.stats,
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .wrapContentHeight()
+                    .background(Color.LightGray)
+                    .padding(start = 16.dp, top = 8.dp, bottom = 8.dp)
+            )
+        }
         Text(
             text = uiStringText(uiString = blazeCampaignCard.footer.label),
             style = footerCTA(),
@@ -68,7 +88,7 @@ fun BlazeCampaignsCardView(
             modifier = modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .padding(start = 16.dp, bottom = 16.dp, end = 8.dp)
+                .padding(start = 16.dp, bottom = 16.dp, end = 16.dp)
         )
     })
 }
@@ -137,4 +157,60 @@ private fun CampaignTitle(title: String, modifier: Modifier = Modifier) {
         overflow = TextOverflow.Ellipsis,
         modifier = modifier.wrapContentHeight()
     )
+}
+
+@Composable
+private fun CampaignStats(
+    campaignStats: BlazeCampaignStats,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .wrapContentHeight()
+            .wrapContentWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        CampaignStat(
+            modifier = modifier
+                .wrapContentHeight()
+                .wrapContentWidth(),
+            title = "Impressions",
+            value = campaignStats.impressions
+        )
+        CampaignStat(
+            modifier = modifier
+                .wrapContentHeight()
+                .wrapContentWidth()
+                .padding(end = 8.dp),
+            title = "Clicks",
+            value = campaignStats.clicks
+        )
+    }
+}
+
+@Composable
+private fun CampaignStat(title: String, value: UiString, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .wrapContentHeight()
+            .wrapContentWidth()
+    ) {
+        Text(
+            text = title,
+            style = detailText(),
+            textAlign = TextAlign.Start,
+            modifier = modifier
+                .wrapContentWidth()
+                .wrapContentHeight()
+        )
+        Text(
+            text = uiStringText(uiString = value),
+            style = largeText(),
+            textAlign = TextAlign.Start,
+            modifier = modifier
+                .wrapContentWidth()
+                .wrapContentHeight()
+        )
+    }
 }
