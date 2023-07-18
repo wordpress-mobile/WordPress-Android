5ae8f4c497f30cb2aa24d98978cd2cb928ba8a5f
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-18 16:28:52 +0530

    + Adds: the campaign details page, VM and tracking logic

---
+++ WordPress/src/main/java/org/wordpress/android/ui/blazeCampaigns/campaigndetail/CampaignDetailViewModel.kt
@@ -0,0 +1,23 @@
+package org.wordpress.android.ui.blazeCampaigns.campaigndetail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.wordpress.android.ui.blaze.BlazeFeatureUtils
+import javax.inject.Inject
+
+@HiltViewModel
+class CampaignDetailViewModel @Inject constructor(
+    private val blazeFeatureUtils: BlazeFeatureUtils,
+) : ViewModel() {
+
+    fun start(campaignDetailPageSource: CampaignDetailPageSource) {
+        blazeFeatureUtils.trackCampaignDetailsOpened(campaignDetailPageSource)
+    }
+}
+
+enum class CampaignDetailPageSource(val trackingName: String){
+    DASHBOARD_CARD("dashboard_card"),
+    CAMPAIGN_LISTING_PAGE("campaign_listing_page"),
+    UNKNOWN("unknown")
+}
+
