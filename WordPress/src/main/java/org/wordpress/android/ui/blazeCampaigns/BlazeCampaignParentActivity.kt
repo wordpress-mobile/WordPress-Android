442da1fb475e091d6da26dc53a49671196b4d0ab
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-18 11:46:33 +0530

    + Adds: Blaze campaign parent activity
    + Adds: Parent activity for showing campaigns and campaign detail

---
+++ WordPress/src/main/java/org/wordpress/android/ui/blazeCampaigns/BlazeCampaignParentActivity.kt
@@ -0,0 +1,12 @@
+package org.wordpress.android.ui.blazeCampaigns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wordpress.android.R
+
+class BlazeCampaignParentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blaze_campaign)
+    }
+}
