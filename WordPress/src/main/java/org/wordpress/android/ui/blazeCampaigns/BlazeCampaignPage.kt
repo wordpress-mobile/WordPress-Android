3bfb3ad4d7fa66bffee43a1b4b80e56cee605a9c
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-18 18:44:53 +0530

    * Updates: the page data class to be parcelable

--- WordPress/src/main/java/org/wordpress/android/ui/blazeCampaigns/BlazeCampaignPage.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blazeCampaigns/BlazeCampaignPage.kt
@@ -1,9 +1,12 @@
 package org.wordpress.android.ui.blazeCampaigns
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import org.wordpress.android.ui.blazeCampaigns.campaigndetail.CampaignDetailPageSource
 import org.wordpress.android.ui.blazeCampaigns.campaignlisting.CampaignListingPageSource
 
-sealed class BlazeCampaignPage {
+@Parcelize
+sealed class BlazeCampaignPage : Parcelable {
     data class CampaignListingPage(val source: CampaignListingPageSource) : BlazeCampaignPage()
     data class CampaignDetailsPage(val source: CampaignDetailPageSource) : BlazeCampaignPage()
     object Done: BlazeCampaignPage()
