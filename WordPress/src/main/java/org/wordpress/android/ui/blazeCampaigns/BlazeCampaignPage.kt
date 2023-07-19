9514143bb17954d1ab66de65679eeb1e37427254
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-19 21:08:34 +0530

    * Fixes: Lint error

--- WordPress/src/main/java/org/wordpress/android/ui/blazeCampaigns/BlazeCampaignPage.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blazeCampaigns/BlazeCampaignPage.kt
@@ -1,11 +1,13 @@
 package org.wordpress.android.ui.blazeCampaigns
 
+import android.annotation.SuppressLint
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import org.wordpress.android.ui.blazeCampaigns.campaigndetail.CampaignDetailPageSource
 import org.wordpress.android.ui.blazeCampaigns.campaignlisting.CampaignListingPageSource
 
 @Parcelize
+@SuppressLint("ParcelCreator")
 sealed class BlazeCampaignPage : Parcelable {
     data class CampaignListingPage(val source: CampaignListingPageSource) : BlazeCampaignPage()
     data class CampaignDetailsPage(val source: CampaignDetailPageSource) : BlazeCampaignPage()
