8f83b5d218ffe891e4673762353c1cde864ab4a7
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-07-21 21:37:27 -0400

    Implement the blaze campaign detail web view

--- WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaigndetail/CampaignDetailFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaigndetail/CampaignDetailFragment.kt
@@ -5,24 +5,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.wordpress.android.R
+import org.wordpress.android.ui.ActivityLauncher
+import org.wordpress.android.ui.WPWebViewActivity
+import org.wordpress.android.ui.blaze.BlazeActionEvent
 import org.wordpress.android.ui.compose.components.MainTopAppBar
 import org.wordpress.android.ui.compose.components.NavigationIcons
 import org.wordpress.android.ui.compose.theme.AppTheme
+import org.wordpress.android.util.ToastUtils
 import org.wordpress.android.util.extensions.getSerializableCompat
 
 private const val CAMPAIGN_DETAIL_PAGE_SOURCE = "campaign_detail_page_source"
 private const val CAMPAIGN_DETAIL_CAMPAIGN_ID = "campaign_detail_campaign_id"
 
 @AndroidEntryPoint
-class CampaignDetailFragment : Fragment() {
+class CampaignDetailFragment : Fragment(), CampaignDetailWebViewClient.CampaignDetailWebViewClientListener {
     companion object {
         fun newInstance(campaignId: Int, source: CampaignDetailPageSource) = CampaignDetailFragment().apply {
             arguments = Bundle().apply {
@@ -49,7 +60,32 @@ class CampaignDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViewModel()
+        observeViewModel()
+    }
+
+    private fun initViewModel() {
         viewModel.start(getCampaignId(), getPageSource())
+    }
+
+    private fun observeViewModel() {
+        viewModel.actionEvents.onEach(this::handleActionEvents).launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+    private fun handleActionEvents(actionEvent: BlazeActionEvent) {
+        when (actionEvent) {
+            is BlazeActionEvent.FinishActivity -> requireActivity().finish()
+            is BlazeActionEvent.LaunchExternalBrowser -> {
+                ActivityLauncher.openUrlExternal(
+                    requireContext(),
+                    actionEvent.url
+                )
+            }
+            is BlazeActionEvent.FinishActivityWithMessage -> {
+                ToastUtils.showToast(requireContext(), actionEvent.id)
+                requireActivity().finish()
+            }
+        }
     }
 
     private fun getPageSource(): CampaignDetailPageSource {
@@ -60,18 +96,48 @@ class CampaignDetailFragment : Fragment() {
     }
 
     private fun getCampaignId() = requireArguments().getInt(CAMPAIGN_DETAIL_CAMPAIGN_ID)
-}
 
-@Composable
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-fun CampaignDetailPage(navigationUp: () -> Unit = { }) {
-    Scaffold(
-        topBar = {
-            MainTopAppBar(
-                title = stringResource(R.string.blaze_campaign_details_page_title),
-                navigationIcon = NavigationIcons.BackIcon,
-                onNavigationIconClick = navigationUp
-            )
-        }
-    ) { }
+    override fun onRedirectToExternalBrowser(url: String) = viewModel.onRedirectToExternalBrowser(url)
+
+    override fun onWebViewPageLoaded() = viewModel.onUrlLoaded()
+
+    override fun onWebViewReceivedError() = viewModel.onWebViewError()
+
+    @Composable
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    fun CampaignDetailPage(
+        navigationUp: () -> Unit = { },
+        viewModel: CampaignDetailViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    ) {
+        val data by viewModel.model.collectAsState()
+        Scaffold(
+            topBar = {
+                MainTopAppBar(
+                    title = stringResource(R.string.blaze_campaign_details_page_title),
+                    navigationIcon = NavigationIcons.BackIcon,
+                    onNavigationIconClick = navigationUp
+                )
+            },
+            content = { CampaignDetailContent(data) }
+        )
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Composable
+    private fun CampaignDetailContent(model: CampaignDetailUIModel) {
+        AndroidView(factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+                settings.userAgentString = model.userAgent
+                settings.javaScriptEnabled = model.enableJavascript
+                settings.domStorageEnabled = model.enableDomStorage
+                webViewClient = CampaignDetailWebViewClient(this@CampaignDetailFragment)
+                postUrl(WPWebViewActivity.WPCOM_LOGIN_URL, model.addressToLoad.toByteArray())
+            }
+        })
+    }
 }
--- WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaigndetail/CampaignDetailViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaigndetail/CampaignDetailViewModel.kt
@@ -1,24 +1,155 @@
 package org.wordpress.android.ui.blaze.blazecampaigns.campaigndetail
 
+import android.text.TextUtils
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import org.wordpress.android.ui.blaze.BlazeActionEvent
 import org.wordpress.android.ui.blaze.BlazeFeatureUtils
+import org.wordpress.android.ui.mysite.SelectedSiteRepository
 import javax.inject.Inject
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import org.wordpress.android.R
+import org.wordpress.android.WordPress
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.store.AccountStore
+import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.ui.WPWebViewActivity
 
 @HiltViewModel
 class CampaignDetailViewModel @Inject constructor(
-    private val blazeFeatureUtils: BlazeFeatureUtils
+    private val accountStore: AccountStore,
+    private val blazeFeatureUtils: BlazeFeatureUtils,
+    private val selectedSiteRepository: SelectedSiteRepository,
+    private val siteStore: SiteStore,
 ) : ViewModel() {
+    private lateinit var pageSource: CampaignDetailPageSource
     private var campaignId: Int = 0
+
+    private val _actionEvents = Channel<BlazeActionEvent>(Channel.BUFFERED)
+    val actionEvents = _actionEvents.receiveAsFlow()
+
+    private val _model = MutableStateFlow(CampaignDetailUIModel())
+    val model: StateFlow<CampaignDetailUIModel> = _model
     fun start(campaignId: Int, campaignDetailPageSource: CampaignDetailPageSource) {
-        blazeFeatureUtils.trackCampaignDetailsOpened(campaignDetailPageSource)
         this.campaignId = campaignId
+        this.pageSource = campaignDetailPageSource
+
+        blazeFeatureUtils.trackCampaignDetailsOpened(campaignDetailPageSource)
+
+        validateAndFinishIfNeeded()
+        assembleAndPostUiState()
+    }
+
+    private fun validateAndFinishIfNeeded() {
+        if (accountStore.account.userName.isNullOrEmpty() || accountStore.accessToken.isNullOrEmpty()) {
+            postActionEvent(BlazeActionEvent.FinishActivityWithMessage(R.string.blaze_campaign_detail_error))
+        }
+    }
+
+    private fun assembleAndPostUiState() {
+        val url = createURL(
+            pathComponents = arrayOf(
+                ADVERTISING_PATH,
+                extractAndSanitizeSiteUrl(),
+                CAMPAIGNS_PATH,
+                campaignId.toString()
+            ),
+            source = pageSource.trackingName
+        )
+        val addressToLoad = prepareAddressToLoad(url)
+        postScreenState(model.value.copy(url = url, addressToLoad = addressToLoad))
+    }
+
+    private fun createURL(vararg pathComponents: String, source: String): String {
+        val basePath = pathComponents.joinToString("/")
+        return "$WP_HOST/$basePath".withQueryParam("source", source)
+    }
+
+    private fun extractAndSanitizeSiteUrl(): String {
+        return selectedSiteRepository.getSelectedSite()?.url?.replace(Regex(HTTP_PATTERN), "") ?: ""
+    }
+
+    private fun prepareAddressToLoad(url: String): String {
+        val username = accountStore.account.userName
+        val accessToken = accountStore.accessToken
+
+        var addressToLoad = url
+
+        // Custom domains are not properly authenticated due to a server side(?) issue, so this gets around that
+        if (!addressToLoad.contains(WPCOM_DOMAIN)) {
+            val wpComSites: List<SiteModel> = siteStore.wPComSites
+            for (siteModel in wpComSites) {
+                // Only replace the url if we know the unmapped url and if it's a custom domain
+                if (!TextUtils.isEmpty(siteModel.unmappedUrl)
+                    && !siteModel.url.contains(WPCOM_DOMAIN)
+                ) {
+                    addressToLoad = addressToLoad.replace(siteModel.url, siteModel.unmappedUrl)
+                }
+            }
+        }
+        // Call the public static method in WPWebViewActivity - no need to recreate functionality with a copy/paste
+        return WPWebViewActivity.getAuthenticationPostData(
+            WPCOM_LOGIN_URL,
+            addressToLoad,
+            username,
+            "",
+            accessToken
+        )
+    }
+
+    private fun postScreenState(state: CampaignDetailUIModel) {
+        viewModelScope.launch {
+            _model.value = state
+        }
+    }
+
+    private fun postActionEvent(actionEvent: BlazeActionEvent) {
+        viewModelScope.launch {
+            _actionEvents.send(actionEvent)
+        }
+    }
+
+    fun onRedirectToExternalBrowser(url: String) {
+        postActionEvent(BlazeActionEvent.LaunchExternalBrowser(url))
+    }
+
+    fun onUrlLoaded() {
+        // no op
+    }
+
+    fun onWebViewError() {
+        postActionEvent(BlazeActionEvent.FinishActivityWithMessage(R.string.blaze_campaign_detail_error))
+    }
+
+    private fun String.withQueryParam(key: String, value: String) = "$this?$key=$value"
+
+    companion object {
+        const val HTTP_PATTERN = "(https?://)"
+        const val WPCOM_LOGIN_URL = "https://wordpress.com/wp-login.php"
+        const val WPCOM_DOMAIN = ".wordpress.com"
+
+        const val WP_HOST = "https://wordpress.com"
+        const val ADVERTISING_PATH = "advertising"
+        const val CAMPAIGNS_PATH = "campaigns"
     }
 }
 
-enum class CampaignDetailPageSource(val trackingName: String){
+enum class CampaignDetailPageSource(val trackingName: String) {
     DASHBOARD_CARD("dashboard_card"),
     CAMPAIGN_LISTING_PAGE("campaign_listing_page"),
     UNKNOWN("unknown")
 }
 
+data class CampaignDetailUIModel(
+    val enableJavascript: Boolean = true,
+    val enableDomStorage: Boolean = true,
+    val userAgent: String = WordPress.getUserAgent(),
+    val enableChromeClient: Boolean = true,
+    val url: String = "",
+    val addressToLoad: String = ""
+)
