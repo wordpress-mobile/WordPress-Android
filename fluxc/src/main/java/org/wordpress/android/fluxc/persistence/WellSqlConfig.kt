8b458acc99322aeda863884163c78a57e73610f1
ThomazFB <thomazfcortez@gmail.com>
2023-07-27 14:48:42 -0300

    Adjust new database migration to account for Woo addon

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WellSqlConfig.kt
@@ -1963,7 +1963,7 @@ open class WellSqlConfig : DefaultWellConfig {
                     db.execSQL("ALTER TABLE PostModel ADD AUTO_SHARE_MESSAGE TEXT")
                     db.execSQL("ALTER TABLE PostModel ADD AUTO_SHARE_ID INTEGER")
                 }
-                191 -> migrate(version) {
+                191 -> migrateAddOn(ADDON_WOOCOMMERCE, version) {
                     db.execSQL("ALTER TABLE WCRevenueStatsModel ADD RANGE_ID INTEGER")
                 }
             }
