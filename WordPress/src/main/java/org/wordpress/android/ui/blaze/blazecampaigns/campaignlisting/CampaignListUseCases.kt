1653f86383a4e0f70ee67d42d7080fa1462ac278
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-29 02:50:08 +0530

    - Removes: unnecessary space

--- WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaignlisting/CampaignListUseCases.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaignlisting/CampaignListUseCases.kt
@@ -37,8 +37,6 @@ sealed class Either<out L, out R> {
 class Left<out L>(val value: L) : Either<L, Nothing>()
 class Right<out R>(val value: R) : Either<Nothing, R>()
 
-
-
 sealed interface NetworkError
 
 object GenericError : NetworkError
