63385052b64b628023edc47dbe64300c35fdcc22
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-07-31 13:10:36 +0530

    * Fixes: the incorrect refresh color and state of refreshing

--- WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaignlisting/CampaignListingFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaignlisting/CampaignListingFragment.kt
@@ -39,7 +39,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -220,10 +219,10 @@ class CampaignListingFragment : Fragment() {
             }
 
             PullRefreshIndicator(
-                refreshing = true,
+                refreshing = refreshState.value ?: false,
                 state = pullRefreshState,
                 modifier = Modifier.align(Alignment.TopCenter),
-                backgroundColor = Color.Red,
+                contentColor = MaterialTheme.colors.primaryVariant,
             )
         }
     }
