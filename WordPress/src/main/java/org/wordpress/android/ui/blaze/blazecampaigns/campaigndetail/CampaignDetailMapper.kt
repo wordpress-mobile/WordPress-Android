463c447d40422af8e89c2160bc9c554f6f58825d
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-07-31 15:40:48 -0400

    Refactor: Use NoNetworkError and GenericError specific classes

--- WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaigndetail/CampaignDetailMapper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/blaze/blazecampaigns/campaigndetail/CampaignDetailMapper.kt
@@ -1,8 +1,6 @@
 package org.wordpress.android.ui.blaze.blazecampaigns.campaigndetail
 
-import org.wordpress.android.R
 import org.wordpress.android.ui.blaze.BlazeFeatureUtils
-import org.wordpress.android.ui.utils.UiString
 import javax.inject.Inject
 
 class CampaignDetailMapper @Inject constructor(
@@ -19,22 +17,8 @@ class CampaignDetailMapper @Inject constructor(
         )
     )
 
-    fun toNoNetworkError(buttonClick: () -> Unit) = CampaignDetailUiState.Error(
-        title = UiString.UiStringRes(R.string.campaign_detail_no_network_error_title),
-        description = UiString.UiStringRes(R.string.campaign_detail_error_description),
-        button = CampaignDetailUiState.Error.ErrorButton(
-            text = UiString.UiStringRes(R.string.campaign_detail_error_button_text),
-            click = buttonClick
-        )
-    )
+    fun toNoNetworkError(buttonClick: () -> Unit) = CampaignDetailUiState.NoNetworkError(buttonClick = buttonClick)
 
-    fun toGenericError(buttonClick: () -> Unit) = CampaignDetailUiState.Error(
-        title = UiString.UiStringRes(R.string.campaign_detail_error_title),
-        description = UiString.UiStringRes(R.string.campaign_detail_error_description),
-        button = CampaignDetailUiState.Error.ErrorButton(
-            text = UiString.UiStringRes(R.string.campaign_detail_error_button_text),
-            click = buttonClick
-        )
-    )
+    fun toGenericError(buttonClick: () -> Unit) = CampaignDetailUiState.GenericError(buttonClick =  buttonClick)
 }
 
