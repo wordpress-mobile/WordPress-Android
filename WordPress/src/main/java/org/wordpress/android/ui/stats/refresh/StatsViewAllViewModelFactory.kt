wordpress-mobile/WordPress-Android

commit 2be916e3fb18beba5ce1f1f6d17cfc9f21136c4e

Fixes UseRequire issues caught by by fixing a false negative detekt issue https://github.com/detekt/detekt/pull/5147

WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllViewModelFactory.kt
@@ -70,19 +70,16 @@ class StatsViewAllViewModelFactory(
     @StringRes private val titleResource: Int
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(StatsViewAllViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return StatsViewAllViewModel(
-                mainDispatcher,
-                bgDispatcher,
-                useCase,
-                statsSiteProvider,
-                dateSelector,
-                titleResource
-            ) as T
-        } else {
-            throw IllegalArgumentException("ViewModel Not Found")
-        }
+        require(!modelClass.isAssignableFrom(StatsViewAllViewModel::class.java)) { "ViewModel Not Found" }
+        @Suppress("UNCHECKED_CAST")
+        return StatsViewAllViewModel(
+            mainDispatcher,
+            bgDispatcher,
+            useCase,
+            statsSiteProvider,
+            dateSelector,
+            titleResource
+        ) as T
     }
 
     class Builder @Inject constructor(

WordPress/src/main/java/org/wordpress/android/ui/LayoutBehaviors.kt
@@ -25,9 +25,9 @@ class WPTooltipViewBehavior : CoordinatorLayout.Behavior<WPTooltipView> {
     }
 
     override fun onDependentViewChanged(parent: CoordinatorLayout, child: WPTooltipView, dependency: View): Boolean {
-        if (child.position != ABOVE) {
+        require(child.position == ABOVE) {
             // Remove this condition if you want to support different TooltipPosition
-            throw IllegalArgumentException("This behavior only supports TooltipPosition.ABOVE")
+            "This behavior only supports TooltipPosition.ABOVE"
         }
 
         if (dependency.measuredWidth == 0 || child.measuredWidth == 0) {

WordPress/src/main/java/org/wordpress/android/ui/uploads/AutoSavePostIfNotDraftUseCase.kt
@@ -80,16 +80,13 @@ class AutoSavePostIfNotDraftUseCase @Inject constructor(
         callback: OnAutoSavePostIfNotDraftCallback
     ) {
         val remotePostId = RemoteId(remotePostPayload.post.remotePostId)
-        if (remotePostPayload.post.isLocalDraft) {
-            throw IllegalArgumentException("Local drafts should not be auto-saved")
-        }
-        if (postStatusContinuations.containsKey(remotePostId) ||
-            autoSaveContinuations.containsKey(remotePostId)
+        require(!remotePostPayload.post.isLocalDraft) { "Local drafts should not be auto-saved" }
+        require(
+            !postStatusContinuations.containsKey(remotePostId) && !autoSaveContinuations.containsKey(remotePostId)
         ) {
-            throw IllegalArgumentException(
-                "This post is already being processed. Make sure not to start an autoSave " +
-                        "or update draft action while another one is going on."
-            )
+            "This post is already being processed. Make sure not to start an autoSave " +
+                    "or update draft action while another one is going on."
+
         }
         coroutineScope.launch {
             val onPostStatusFetched = fetchRemotePostStatus(remotePostPayload)

commit fccf876c789128b413849f728d040e41a54becf6

Fixes require statement logic error introduced in 2be916e3fb18beba5ce1f1f6d17cfc9f21136c4e

WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsViewAllViewModelFactory.kt
@@ -70,7 +70,7 @@ class StatsViewAllViewModelFactory(
     @StringRes private val titleResource: Int
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        require(!modelClass.isAssignableFrom(StatsViewAllViewModel::class.java)) { "ViewModel Not Found" }
+        require(modelClass.isAssignableFrom(StatsViewAllViewModel::class.java)) { "ViewModel Not Found" }
         @Suppress("UNCHECKED_CAST")
         return StatsViewAllViewModel(
             mainDispatcher,