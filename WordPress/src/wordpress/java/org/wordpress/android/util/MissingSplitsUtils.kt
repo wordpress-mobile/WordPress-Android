690feb677bec4aa53d8bf2805cb16ae325bbc0e0
Irfan Omur <irfan.omur@automattic.com>
2023-08-01 18:18:17 +0300

    Fix wrong resource id for isMissingSplits()

--- WordPress/src/wordpress/java/org/wordpress/android/util/MissingSplitsUtils.kt
+++ WordPress/src/wordpress/java/org/wordpress/android/util/MissingSplitsUtils.kt
@@ -13,11 +13,11 @@ object MissingSplitsUtils {
     const val DIALOG_BUTTON = "CLOSE"
 
     /**
-     * R.drawable.splash_icon is the first drawable the app attempts to access. If a NotFoundException occurs while
+     * R.drawable.brush_stroke is the first drawable the app attempts to access. If a NotFoundException occurs while
      * trying to access it, it indicates that there are missing splits, possibly due to sideloading the app.
      */
     fun isMissingSplits(context: Context) = try {
-        context.resources.getValue(R.drawable.splash_icon, TypedValue(), true)
+        context.resources.getValue(R.drawable.brush_stroke, TypedValue(), true)
         false
     } catch (e: Resources.NotFoundException) {
         true
