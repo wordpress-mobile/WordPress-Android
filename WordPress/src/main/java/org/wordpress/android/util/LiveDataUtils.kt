7b268db68b54e122b798fab741c0c9dd51695589
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-08-07 14:17:28 +0530

    + Adds: merge async operator with nullable support

--- WordPress/src/main/java/org/wordpress/android/util/LiveDataUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/util/LiveDataUtils.kt
@@ -91,6 +91,33 @@ fun <T, U, V> mergeAsyncNotNull(
     }
 }
 
+fun <T, U, V> mergeAsync(
+    scope: CoroutineScope,
+    sourceA: LiveData<T>,
+    sourceB: LiveData<U>,
+    distinct: Boolean = true,
+    merger: suspend (T?, U?) -> V
+): LiveData<V> {
+    val mediator = MediatorLiveData<Pair<T?, U?>>()
+    mediator.addSource(sourceA) {
+        if (!distinct || mediator.value?.first != it) {
+            mediator.value = it to mediator.value?.second
+        }
+    }
+    mediator.addSource(sourceB) {
+        if (!distinct || mediator.value?.second != it) {
+            mediator.value = mediator.value?.first to it
+        }
+    }
+    return mediator.mapAsync(scope) { (dataA, dataB) ->
+        if (dataA == null && dataB == null) {
+            null
+        } else {
+            merger(dataA, dataB)
+        }
+    }
+}
+
 /**
  * Merges two LiveData sources using a given function. The function returns an object of a new type.
  * @param sourceA first source
