0f46792e9ab4c2fbf9232cc17f8c21b1344cf6b2
Ravi <ravi.program@gmail.com>
2023-08-10 10:01:30 +1000

    Update HelpActivity.kt

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/HelpActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/HelpActivity.kt
@@ -30,8 +30,7 @@ import org.wordpress.android.fluxc.store.AccountStore.OnAccountChanged
 import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.support.SupportHelper
 import org.wordpress.android.support.SupportWebViewActivity
-import org.wordpress.android.support.SupportWebViewActivity.ChatCompletionEvent
-import org.wordpress.android.support.SupportWebViewActivity.OpenChatWidget.ChatDetails
+import org.wordpress.android.support.SupportWebViewActivity.BotOptions
 import org.wordpress.android.support.ZendeskExtraTags
 import org.wordpress.android.support.ZendeskHelper
 import org.wordpress.android.ui.ActivityId
@@ -96,7 +95,7 @@ class HelpActivity : LocaleAwareActivity() {
 
     private val openChatWidget = registerForActivityResult(SupportWebViewActivity.OpenChatWidget()) {
         it?.let {
-            viewModel.finishSupportChat(it)
+            viewModel.sendChatHistory(it)
         }
     }
 
@@ -178,9 +177,11 @@ class HelpActivity : LocaleAwareActivity() {
 
     private fun launchSupportWidget() {
         openChatWidget.launch(
-            ChatDetails(
-                selectedSiteFromExtras,
-                "https://appassets.androidplatform.net/assets/support_chat_widget.html"
+            BotOptions(
+                id = "TqTdebbGjJeUjrmBIFjh/YbAMwiheXLs2Ue5j7elH",
+                inputPlaceHolder = "Send a message...",
+                firstMessage = "What can we help you with?",
+                getSupport = "Contact support"
             )
         )
     }
@@ -335,15 +336,6 @@ class HelpActivity : LocaleAwareActivity() {
             // Load Main Activity once signed out, which launches the login flow
             ActivityLauncher.showMainActivity(this@HelpActivity, true)
         }
-
-        viewModel.onSupportChatCompleted.observe(this@HelpActivity) {
-            finishSupportChat(it)
-        }
-    }
-
-    private fun finishSupportChat(event: ChatCompletionEvent) {
-        setResult(RESULT_OK, Intent().putExtra(SupportWebViewActivity.OpenChatWidget.CHAT_EMAIL, event.email))
-        finish()
     }
 
     private fun HelpActivityBinding.loadAvatar(avatarUrl: String) {
