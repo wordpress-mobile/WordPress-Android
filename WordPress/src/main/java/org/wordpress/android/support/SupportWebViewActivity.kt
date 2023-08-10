ad8331ab36175cfdac98f5e5ccc43af504ab5c37
Ravi <ravi.program@gmail.com>
2023-08-10 10:01:13 +1000

    Update SupportWebViewActivity.kt

--- WordPress/src/main/java/org/wordpress/android/support/SupportWebViewActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/support/SupportWebViewActivity.kt
@@ -1,16 +1,24 @@
 package org.wordpress.android.support
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewAssetLoader.AssetsPathHandler
 import androidx.webkit.WebViewAssetLoader.DEFAULT_DOMAIN
 import androidx.webkit.WebViewAssetLoader.ResourcesPathHandler
-import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.support.SupportWebViewActivity.OpenChatWidget.ChatDetails
+import org.wordpress.android.R
+import org.wordpress.android.fluxc.network.utils.toMap
+import org.wordpress.android.support.SupportWebViewActivity.OpenChatWidget.Companion.CHAT_HISTORY
 import org.wordpress.android.ui.WPWebViewActivity
 
 class SupportWebViewActivity : WPWebViewActivity(), SupportWebViewClient.SupportWebViewClientListener {
@@ -18,19 +26,16 @@ class SupportWebViewActivity : WPWebViewActivity(), SupportWebViewClient.Support
         super.onCreate(savedInstanceState)
         toggleNavbarVisibility(false)
 
-        val assetLoader = WebViewAssetLoader.Builder()
-            .addPathHandler("/assets/", AssetsPathHandler(this))
-            .addPathHandler("/res/", ResourcesPathHandler(this))
-            .build()
+        // set send message box to appear above system navigation bar
+        val previewContainer = findViewById<View>(R.id.preview_container)
+        val params = previewContainer.layoutParams as ViewGroup.MarginLayoutParams
+        params.bottomMargin = resources.getDimensionPixelSize(R.dimen.margin_64dp)
+        previewContainer.layoutParams = params
 
-        configureWebView()
-        mWebView.webViewClient = SupportWebViewClient(this, assetLoader)
-        mWebView.loadUrl("https://$DEFAULT_DOMAIN/assets/support_chat_widget.html")
-    }
+        supportActionBar?.title = getString(R.string.help)
+        supportActionBar?.subtitle = ""
 
-    override fun configureWebView() {
-        super.configureWebView()
-        supportActionBar?.hide()
+        setupWebView()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -38,38 +43,90 @@ class SupportWebViewActivity : WPWebViewActivity(), SupportWebViewClient.Support
         return true
     }
 
-    override fun onChatSessionClosed() {
+    override fun onChatSessionClosed(chatHistory: String) {
+        intent.putExtra(CHAT_HISTORY, chatHistory)
         setResult(RESULT_OK, intent)
         finish()
     }
 
-    class OpenChatWidget : ActivityResultContract<ChatDetails, ChatCompletionEvent?>() {
-        override fun createIntent(context: Context, input: ChatDetails) =
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        val jsObjName = "jsObject"
+        val allowedOriginRules = setOf("https://$DEFAULT_DOMAIN")
+
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", AssetsPathHandler(this))
+            .addPathHandler("/res/", ResourcesPathHandler(this))
+            .build()
+        mWebView.webViewClient = SupportWebViewClient(this, assetLoader)
+
+        // Setup debugging; See https://developers.google.com/web/tools/chrome-devtools/remote-debugging/webviews
+        if ( 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
+
+        // Create a JS object to be injected into frames; Determines if WebMessageListener
+        // or WebAppInterface should be used
+        createJsObject(
+            mWebView,
+            jsObjName,
+            allowedOriginRules
+        ) { message ->
+            Log.d("Chat history", message)
+            onChatSessionClosed(message)
+        }
+
+        intent.getStringExtra(URL_TO_LOAD)?.let { mWebView.loadUrl(it) }
+    }
+
+
+    class OpenChatWidget : ActivityResultContract<BotOptions, ChatCompletionEvent?>() {
+        override fun createIntent(context: Context, input: BotOptions) =
             Intent(context, SupportWebViewActivity::class.java).apply {
                 putExtra(USE_GLOBAL_WPCOM_USER, true)
                 putExtra(AUTHENTICATION_URL, WPCOM_LOGIN_URL)
-                putExtra(URL_TO_LOAD, input.url)
-//                putExtra(CHAT_TEXT, input.chatText)
-                putExtra(CHAT_EMAIL, input.site?.email)
+                putExtra(URL_TO_LOAD, buildURI(input))
             }
 
         override fun parseResult(resultCode: Int, intent: Intent?): ChatCompletionEvent? {
-            val data = intent?.takeIf { it.hasExtra(CHAT_TEXT) && it.hasExtra(CHAT_EMAIL) }
+            val data = intent?.takeIf { it.hasExtra(CHAT_HISTORY) }
             if (resultCode == RESULT_OK && data != null) {
-                val chatText = data.getStringExtra(CHAT_TEXT).orEmpty()
-                val email = data.getStringExtra(CHAT_EMAIL).orEmpty()
-                return ChatCompletionEvent(chatText, email)
+                val chatHistory = data.getStringExtra(CHAT_HISTORY).orEmpty()
+                return ChatCompletionEvent(chatHistory)
             }
             return null
         }
 
-        data class ChatDetails(val site: SiteModel?, val url: String)
+        private fun buildURI(options: BotOptions): String {
+            // build url with parameters.
+            val botOptions = options.toMap()
+
+            Log.d("BotOptions Map", botOptions.toString())
+
+            val builder = Uri.Builder()
+            builder.scheme("https")
+                .authority(DEFAULT_DOMAIN)
+                .appendPath("assets")
+                .appendPath("support_chat_widget.html")
+
+            botOptions.forEach { (key, value) ->
+                builder.appendQueryParameter(key, value.toString())
+            }
+
+            return builder.build().toString()
+        }
 
         companion object {
-            const val CHAT_TEXT = "CHAT_TEXT"
-            const val CHAT_EMAIL = "CHAT_EMAIL"
+            const val CHAT_HISTORY = "CHAT_HISTORY"
         }
     }
 
-    data class ChatCompletionEvent(val chatText: String, val email: String)
+    data class ChatCompletionEvent(val chatHistory: String)
+
+    data class BotOptions(
+        val id: String,
+        val inputPlaceHolder: String,
+        val firstMessage: String,
+        val getSupport: String
+    )
 }
