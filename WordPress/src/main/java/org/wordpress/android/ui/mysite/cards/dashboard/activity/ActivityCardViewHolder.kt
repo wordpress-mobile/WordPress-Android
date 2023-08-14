7d13943ccbba5c9597518b24198b9400e905c4a7
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-08-14 16:05:48 -0400

    Integrate the activity card menu and menu items into the view holder.

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/activity/ActivityCardViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/activity/ActivityCardViewHolder.kt
@@ -1,6 +1,9 @@
 package org.wordpress.android.ui.mysite.cards.dashboard.activity
 
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
+import org.wordpress.android.R
 import org.wordpress.android.databinding.MySiteActivityCardWithActivityItemsBinding
 import org.wordpress.android.databinding.MySiteCardFooterLinkBinding
 import org.wordpress.android.databinding.MySiteCardToolbarBinding
@@ -8,7 +11,7 @@ import org.wordpress.android.ui.utils.UiHelpers
 import org.wordpress.android.util.extensions.viewBinding
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.ActivityCard
 import org.wordpress.android.ui.mysite.cards.dashboard.CardViewHolder
-import org.wordpress.android.ui.utils.UiString
+import org.wordpress.android.ui.utils.ListItemInteraction
 
 class ActivityCardViewHolder(
     parent: ViewGroup,
@@ -23,13 +26,49 @@ class ActivityCardViewHolder(
     fun bind(card: ActivityCard) = with(binding) {
         val activityCard = card as ActivityCard.ActivityCardWithItems
         (activityItems.adapter as ActivityItemsAdapter).update(activityCard.activityItems)
-        mySiteToolbar.update(activityCard.title)
+        mySiteToolbar.update(activityCard)
         mySiteCardFooterLink.update(activityCard.footerLink)
     }
 
-    private fun MySiteCardToolbarBinding.update(title: UiString?) {
-        uiHelpers.setTextOrHide(mySiteCardToolbarTitle, title)
+    private fun MySiteCardToolbarBinding.update(card: ActivityCard.ActivityCardWithItems) {
+        uiHelpers.setTextOrHide(mySiteCardToolbarTitle, card.title)
+        mySiteCardToolbarMore.visibility = android.view.View.VISIBLE
+        mySiteCardToolbarMore.setOnClickListener {
+            showMoreMenu(
+                card.onHideMenuItemClick,
+                card.onAllActivityItemClick,
+                card.onMoreMenuClick,
+                mySiteCardToolbarMore,
+            )
+        }
     }
+
+    private fun showMoreMenu(
+        onHideMenuItemClick: ListItemInteraction,
+        onAllActivityItemClick: ListItemInteraction,
+        onMoreMenuClick: ListItemInteraction,
+        anchor: View) {
+        onMoreMenuClick.click()
+        val popupMenu = PopupMenu(itemView.context, anchor)
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.activity_card_menu_item_all_activity -> {
+                    onAllActivityItemClick.click()
+                    return@setOnMenuItemClickListener true
+                }
+
+                R.id.activity_card_menu_item_hide_this -> {
+                    onHideMenuItemClick.click()
+                    return@setOnMenuItemClickListener true
+                }
+
+                else -> return@setOnMenuItemClickListener true
+            }
+        }
+        popupMenu.inflate(R.menu.activity_log_card_menu)
+        popupMenu.show()
+    }
+
 
     private fun MySiteCardFooterLinkBinding.update(footerLink: ActivityCard.FooterLink) {
         uiHelpers.setTextOrHide(linkLabel, footerLink.label)
