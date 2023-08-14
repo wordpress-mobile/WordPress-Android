af3f87d53c7366f198f10e354eb51326c6176e00
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-08-14 07:46:16 -0400

    set default remote value to false

--- WordPress/src/main/java/org/wordpress/android/util/config/DashboardPersonalizationFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/DashboardPersonalizationFeatureConfig.kt
@@ -4,7 +4,7 @@ import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.Feature
 import javax.inject.Inject
 
-@Feature(DashboardPersonalizationFeatureConfig.DASHBOARD_PERSONALIZATION_REMOTE_FIELD, true)
+@Feature(DashboardPersonalizationFeatureConfig.DASHBOARD_PERSONALIZATION_REMOTE_FIELD, false)
 class DashboardPersonalizationFeatureConfig @Inject constructor(appConfig: AppConfig) : FeatureConfig(
     appConfig,
     BuildConfig.DASHBOARD_PERSONALIZATION,
