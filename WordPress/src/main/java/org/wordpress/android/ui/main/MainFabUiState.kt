6e610218930b35f4021dfc372a2172ccd4717e8e
Ravi <ravi.program@gmail.com>
2023-08-16 12:27:39 +1000

    Revert "Update MainFabUiState.kt"
    This reverts commit 228c80563bf76f27414567aa58a033ba331684b4.

--- WordPress/src/main/java/org/wordpress/android/ui/main/MainFabUiState.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/MainFabUiState.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 
 data class MainFabUiState(
     val isFabVisible: Boolean,
+    val isFabTooltipVisible: Boolean,
     @StringRes val CreateContentMessageId: Int,
     val isFocusPointVisible: Boolean = false
 )
