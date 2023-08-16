7a3a7189ace92eff137bea9ee166430dfea52b69
Matthew Kevins <mmkevins@yahoo.com>
2023-08-16 17:48:22 +1000

    Restore manual override for domain purchasing feature flag

--- WordPress/src/main/java/org/wordpress/android/util/config/SiteCreationDomainPurchasingFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/SiteCreationDomainPurchasingFeatureConfig.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.util.config
 
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.FeatureInDevelopment
+import org.wordpress.android.util.config.AppConfig.FeatureState.ManuallyOverriden
 import javax.inject.Inject
 
 @FeatureInDevelopment
@@ -11,4 +12,6 @@ class SiteCreationDomainPurchasingFeatureConfig
 ) : FeatureConfig(
     appConfig,
     BuildConfig.ENABLE_SITE_CREATION_DOMAIN_PURCHASING,
-)
+) {
+    fun isEnabledOrManuallyOverridden() = (featureState() as? ManuallyOverriden)?.isEnabled ?: isEnabled()
+}
