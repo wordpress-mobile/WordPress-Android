647ee9628d5e27e3306e3715a012537710540c30
Ravi <ravi.program@gmail.com>
2023-08-17 21:15:07 +1000

    Update feature flag name
    Updated feature flag name to 'contact_support_chatbot' to match remote flag

--- WordPress/src/main/java/org/wordpress/android/util/config/ContactSupportFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/ContactSupportFeatureConfig.kt
@@ -4,13 +4,13 @@ import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.Feature
 import javax.inject.Inject
 
-private const val CONTACT_SUPPORT_REMOTE_FIELD = "contact_support"
+private const val CONTACT_SUPPORT_REMOTE_FIELD = "contact_support_chatbot"
 
 @Feature(CONTACT_SUPPORT_REMOTE_FIELD, false)
 class ContactSupportFeatureConfig @Inject constructor(
     appConfig: AppConfig
 ) : FeatureConfig(
     appConfig,
-    BuildConfig.CONTACT_SUPPORT,
+    BuildConfig.CONTACT_SUPPORT_CHATBOT,
     CONTACT_SUPPORT_REMOTE_FIELD
 )
