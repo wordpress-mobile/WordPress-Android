ace3ec196f1ae9409efe487d0c653eda915aa7a5
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-08-18 12:46:13 +0530

    * Fixes: Tests in PagesCardViewModelSliceTest for PagesCardViewModel

--- WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/pages/PagesCardViewModelSliceTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/mysite/cards/dashboard/pages/PagesCardViewModelSliceTest.kt
@@ -2,24 +2,22 @@ package org.wordpress.android.ui.mysite.cards.dashboard.pages
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions
+import org.junit.Before
 import org.junit.Test
-import org.junit.jupiter.api.Assertions.*
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.wordpress.android.BaseUnitTest
-import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.PagesCard.PagesCardWithData
-import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams
-import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.PagesCardBuilderParams.MoreMenuParams
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.PagesCardBuilderParams.PagesItemClickParams
 import org.wordpress.android.ui.mysite.SelectedSiteRepository
 import org.wordpress.android.ui.mysite.SiteNavigationAction
 import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker
-import org.wordpress.android.ui.utils.ListItemInteraction
-import org.wordpress.android.ui.utils.UiString.UiStringRes
 
-const val pageId = 1L
+const val pageId = 1
 
 @ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
@@ -32,23 +30,31 @@ class PagesCardViewModelSliceTest : BaseUnitTest() {
 
     private lateinit var pagesCardViewModelSlice: PagesCardViewModelSlice
 
-    private var onPageCardFooterLinkClick: (() -> Unit)? = null
-    private var onPageItemClick: ((params: PagesItemClickParams) -> Unit)? = null
+    private lateinit var navigationActions: MutableList<SiteNavigationAction>
 
+    private val site = mock<SiteModel>()
+
+    @Before
     fun setUp() {
         pagesCardViewModelSlice = PagesCardViewModelSlice(
             cardsTracker,
             selectedSiteRepository
         )
+        navigationActions = mutableListOf()
+        pagesCardViewModelSlice.onNavigation.observeForever { event ->
+            event?.getContentIfNotHandled()?.let {
+                navigationActions.add(it)
+            }
+        }
+        whenever(selectedSiteRepository.getSelectedSite()).thenReturn(site)
     }
 
-    /* DASHBOARD PAGES CARD */
     @Test
     fun `when create new pages card is clicked, then trigger create page flow`() =
         test {
-            initSelectedSite()
+            val pagesCardParams = pagesCardViewModelSlice.getPagesCardBuilderParams(mock())
 
-            requireNotNull(onPageCardFooterLinkClick).invoke()
+            pagesCardParams.onFooterLinkClick()
 
             Assertions.assertThat(navigationActions).containsOnly(SiteNavigationAction.TriggerCreatePageFlow(site))
         }
@@ -56,121 +62,37 @@ class PagesCardViewModelSliceTest : BaseUnitTest() {
     @Test
     fun `given draft page card, when page item is clicked, then navigate to page list draft tab`() =
         test {
-            initSelectedSite()
+            val pagesCardParams = pagesCardViewModelSlice.getPagesCardBuilderParams(mock())
+            val pagesParams = PagesItemClickParams(PagesCardContentType.DRAFT, pageId)
 
-            requireNotNull(onPageItemClick).invoke(
-                PagesItemClickParams(
-                    PagesCardContentType.DRAFT,
-                    pageId
-                )
-            )
+            pagesCardParams.onPagesItemClick.invoke(pagesParams)
 
             Assertions.assertThat(navigationActions).containsOnly(SiteNavigationAction.OpenPagesDraftsTab(site, pageId))
+            verify(cardsTracker).trackPagesItemClicked(PagesCardContentType.DRAFT)
         }
 
     @Test
     fun `given scheduled page card, when page item is clicked, then navigate to page list scheduled tab`() =
         test {
-            initSelectedSite()
+            val pagesCardParams = pagesCardViewModelSlice.getPagesCardBuilderParams(mock())
+            val pagesParams = PagesItemClickParams(PagesCardContentType.SCHEDULED, pageId)
 
-            requireNotNull(onPageItemClick).invoke(
-                PagesItemClickParams(
-                    PagesCardContentType.SCHEDULED,
-                    pageId
-                )
-            )
+            pagesCardParams.onPagesItemClick.invoke(pagesParams)
 
             Assertions.assertThat(navigationActions)
                 .containsOnly(SiteNavigationAction.OpenPagesScheduledTab(site, pageId))
+            verify(cardsTracker).trackPagesItemClicked(PagesCardContentType.SCHEDULED)
         }
 
     @Test
     fun `given published page card, when page item is clicked, then navigate to page list published tab`() =
         test {
-            initSelectedSite()
+            val pagesCardParams = pagesCardViewModelSlice.getPagesCardBuilderParams(mock())
+            val pagesParams = PagesItemClickParams(PagesCardContentType.PUBLISH, pageId)
 
-            requireNotNull(onPageItemClick).invoke(
-                MySiteCardAndItemBuilderParams.PagesCardBuilderParams.PagesItemClickParams(
-                    PagesCardContentType.PUBLISH,
-                    pageId
-                )
-            )
+            pagesCardParams.onPagesItemClick.invoke(pagesParams)
 
             Assertions.assertThat(navigationActions).containsOnly(SiteNavigationAction.OpenPages(site))
-        }
-
-    @Test
-    fun `given draft page card, when page item is clicked, then event is tracked`() =
-        test {
-            initSelectedSite()
-
-            requireNotNull(onPageItemClick).invoke(
-                PagesItemClickParams(
-                    PagesCardContentType.DRAFT,
-                    pageId
-                )
-            )
-
-            verify(cardsTracker).trackPagesItemClicked(PagesCardContentType.DRAFT)
-        }
-
-    @Test
-    fun `given scheduled page card, when page item is clicked, then event is tracked`() =
-        test {
-            initSelectedSite()
-
-            requireNotNull(onPageItemClick).invoke(
-                PagesItemClickParams(
-                    PagesCardContentType.SCHEDULED,
-                    pageId
-                )
-            )
-
-            verify(cardsTracker).trackPagesItemClicked(PagesCardContentType.SCHEDULED)
-        }
-
-    @Test
-    fun `given published page card, when page item is clicked, then event is tracked`() =
-        test {
-            initSelectedSite()
-
-            requireNotNull(onPageItemClick).invoke(
-                PagesItemClickParams(
-                    PagesCardContentType.PUBLISH,
-                    pageId
-                )
-            )
-
             verify(cardsTracker).trackPagesItemClicked(PagesCardContentType.PUBLISH)
         }
-
-    private fun initPageCard(mockInvocation: InvocationOnMock): PagesCardWithData {
-        val params = (mockInvocation.arguments.filterIsInstance<MySiteCardAndItemBuilderParams.DashboardCardsBuilderParams>()).first()
-        onPageItemClick = params.pagesCardBuilderParams.onPagesItemClick
-        onPageCardFooterLinkClick = params.pagesCardBuilderParams.onFooterLinkClick
-        return PagesCardWithData(
-            title = UiStringRes(0),
-            pages = listOf(
-                PagesCardWithData.PageContentItem(
-                    title = UiStringRes(0),
-                    status = UiStringRes(0),
-                    statusIcon = 0,
-                    lastEditedOrScheduledTime = UiStringRes(0),
-                    onClick = ListItemInteraction.create {
-                        (onPageItemClick as (PagesItemClickParams) -> Unit).invoke(
-                            PagesItemClickParams(
-                                PagesCardContentType.DRAFT,
-                                pageId
-                            )
-                        )
-                    }
-                )
-            ),
-            footerLink = PagesCardWithData.CreateNewPageItem(
-                label = UiStringRes(0),
-                onClick = onPageCardFooterLinkClick as (() -> Unit)
-            ),
-        )
-    }
-
 }
