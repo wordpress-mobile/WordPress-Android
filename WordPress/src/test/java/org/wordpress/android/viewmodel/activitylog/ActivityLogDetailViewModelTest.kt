30f471997966af99a5d0bd4bb1570e8a2076af0b
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-08-23 16:40:56 +0530

    - Reverts: the tests to fix

--- WordPress/src/test/java/org/wordpress/android/viewmodel/activitylog/ActivityLogDetailViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/viewmodel/activitylog/ActivityLogDetailViewModelTest.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.viewmodel.activitylog
 
 import android.text.SpannableString
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
@@ -21,6 +22,8 @@ import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.activity.ActivityLogModel
+import org.wordpress.android.fluxc.model.dashboard.CardModel
+import org.wordpress.android.fluxc.network.rest.wpcom.dashboard.CardsUtils
 import org.wordpress.android.fluxc.store.ActivityLogStore
 import org.wordpress.android.fluxc.store.dashboard.CardsStore
 import org.wordpress.android.fluxc.tools.FormattableContent
@@ -34,7 +37,57 @@ import java.util.Date
 
 /* ACTIVITY */
 const val ACTIVITY_ID = "activity123"
+const val ACTIVITY_SUMMARY = "activity"
+const val ACTIVITY_NAME = "name"
+const val ACTIVITY_TYPE = "create a blog"
+const val ACTIVITY_IS_REWINDABLE = false
+const val ACTIVITY_REWIND_ID = "10.0"
+const val ACTIVITY_GRID_ICON = "gridicon.jpg"
+const val ACTIVITY_STATUS = "OK"
+const val ACTIVITY_ACTOR_TYPE = "author"
+const val ACTIVITY_ACTOR_NAME = "John Smith"
+const val ACTIVITY_ACTOR_WPCOM_USER_ID = 15L
+const val ACTIVITY_ACTOR_ROLE = "admin"
+const val ACTIVITY_ACTOR_ICON_URL = "dog.jpg"
+const val ACTIVITY_PUBLISHED_DATE = "2021-12-27 11:33:55"
+const val ACTIVITY_CONTENT = "content"
 
+private val ACTIVITY_LOG_MODEL = ActivityLogModel(
+    summary = ACTIVITY_SUMMARY,
+    content = FormattableContent(text = ACTIVITY_CONTENT),
+    name = ACTIVITY_NAME,
+    actor = ActivityLogModel.ActivityActor(
+        displayName = ACTIVITY_ACTOR_NAME,
+        type = ACTIVITY_ACTOR_TYPE,
+        wpcomUserID = ACTIVITY_ACTOR_WPCOM_USER_ID,
+        avatarURL = ACTIVITY_ACTOR_ICON_URL,
+        role = ACTIVITY_ACTOR_ROLE,
+    ),
+    type = ACTIVITY_TYPE,
+    published = CardsUtils.fromDate(ACTIVITY_PUBLISHED_DATE),
+    rewindable = ACTIVITY_IS_REWINDABLE,
+    rewindID = ACTIVITY_REWIND_ID,
+    gridicon = ACTIVITY_GRID_ICON,
+    status = ACTIVITY_STATUS,
+    activityID = ACTIVITY_ID
+)
+
+private val ACTIVITY_CARD_MODEL = CardModel.ActivityCardModel(
+    activities = listOf(ACTIVITY_LOG_MODEL)
+)
+
+private val CARDS_MODEL: List<CardModel> = listOf(
+    ACTIVITY_CARD_MODEL
+)
+
+
+private val data = CardsStore.CardsResult(
+    model = CARDS_MODEL
+)
+
+private val emptyData = CardsStore.CardsResult(
+    model = emptyList<CardModel>()
+)
 @ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
 class ActivityLogDetailViewModelTest : BaseUnitTest() {
@@ -358,6 +411,9 @@ class ActivityLogDetailViewModelTest : BaseUnitTest() {
     @Test
     fun `given card entry, when no results exist, then emits null`() {
         whenever(activityLogStore.getActivityLogForSite(site)).thenReturn(listOf())
+        whenever(cardsStore.getCards(any())).thenReturn(flowOf(emptyData))
+
+        lastEmittedItem = mock()
 
         startViewModel(isDashboardCardEntry = true)
 
@@ -367,8 +423,9 @@ class ActivityLogDetailViewModelTest : BaseUnitTest() {
     @Test
     fun `given card entry, when activityLog is empty, then emits value from dashboard table`() {
         whenever(activityLogStore.getActivityLogForSite(site)).thenReturn(listOf())
+        whenever(cardsStore.getCards(any())).thenReturn(flowOf(data))
 
-        lastEmittedItem = mock()
+        lastEmittedItem = null
 
         startViewModel(activityID = ACTIVITY_ID, isDashboardCardEntry = true)
 
@@ -377,7 +434,9 @@ class ActivityLogDetailViewModelTest : BaseUnitTest() {
 
     @Test
     fun `given card entry, when activityId is not found in activity log, then emits value from dashboard table`() {
-        lastEmittedItem = mock()
+        whenever(cardsStore.getCards(any())).thenReturn(flowOf(data))
+
+        lastEmittedItem = null
 
         startViewModel(activityID = ACTIVITY_ID, isDashboardCardEntry = true)
 
