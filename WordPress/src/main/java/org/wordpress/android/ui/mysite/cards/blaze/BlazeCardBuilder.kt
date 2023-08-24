bec7bb3760a03faa903681d9011ed4b31f6fb926
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-08-24 18:17:57 +0530

    + Adds: more menu options mapping to promote blaze card

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/blaze/BlazeCardBuilder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/blaze/BlazeCardBuilder.kt
@@ -74,8 +74,11 @@ class BlazeCardBuilder @Inject constructor(private val statsUtils: StatsUtils) {
             title = UiString.UiStringRes(R.string.promote_blaze_card_title),
             subtitle = UiString.UiStringRes(R.string.promote_blaze_card_sub_title),
             onClick = ListItemInteraction.create(params.onClick),
-            onHideMenuItemClick = ListItemInteraction.create(params.onHideMenuItemClick),
-            onMoreMenuClick = ListItemInteraction.create(params.onMoreMenuClick)
+            moreMenuOptions = PromoteWithBlazeCard.MoreMenuOptions(
+                onMoreClick = ListItemInteraction.create(params.moreMenuParams.onMoreMenuClick),
+                hideThisMenuItemClick = ListItemInteraction.create(params.moreMenuParams.onHideThisCardItemClick),
+                learnMoreClick = ListItemInteraction.create(params.moreMenuParams.onLearnMoreClick)
+            )
         )
     }
 }
