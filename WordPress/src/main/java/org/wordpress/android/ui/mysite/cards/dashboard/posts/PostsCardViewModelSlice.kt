3a4430cd5d20cb8fde6860946f8277a30288aeae
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-08-28 11:34:23 -0400

    Pass along the click handlers in PostCardBuilderParams

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/posts/PostsCardViewModelSlice.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/posts/PostsCardViewModelSlice.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.mysite.cards.dashboard.posts
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import org.wordpress.android.fluxc.model.dashboard.CardModel.PostsCardModel
 import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.PostCardBuilderParams
@@ -20,8 +21,30 @@ class PostsCardViewModelSlice @Inject constructor(
        return  PostCardBuilderParams(
             posts = postsCardModel,
             onPostItemClick = this::onPostItemClick,
-            onFooterLinkClick = this::onPostCardFooterLinkClick
+            onFooterLinkClick = this::onPostCardFooterLinkClick,
+            moreMenuClickParams = PostCardBuilderParams.MoreMenuParams(
+               onMoreMenuClick = this::onMoreMenuClick,
+               onHideThisMenuItemClick = this::onHideThisMenuItemClick,
+               onViewPostsMenuItemClick = this::onViewPostsMenuItemClick
+           )
         )
+    }
+
+    private fun onMoreMenuClick(postCardType: PostCardType) {
+        // todo: annmarie implement cards tracker
+        Log.i(javaClass.simpleName, "***=> onMoreMenuClick $postCardType")
+    }
+
+    private fun onHideThisMenuItemClick(postCardType: PostCardType) {
+        // todo: annmarie implement logic
+        // todo: annmarie implement cards tracker
+        Log.i(javaClass.simpleName, "***=> onHideThisMenuItemClick $postCardType")
+    }
+
+    private fun onViewPostsMenuItemClick(postCardType: PostCardType) {
+        // todo: annmarie implement logic
+        // todo: annmarie implement cards tracker
+        Log.i(javaClass.simpleName, "***=> onViewPostsMenuItemClick $postCardType")
     }
 
     private fun onPostItemClick(params: PostCardBuilderParams.PostItemClickParams) {
