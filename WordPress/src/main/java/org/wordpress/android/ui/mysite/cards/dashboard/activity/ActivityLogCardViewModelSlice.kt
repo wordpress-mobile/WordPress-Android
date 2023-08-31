93f3821bfe2e2290c7523f1970a21191fdb43c52
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-08-31 11:36:22 +0530

    + [WIP] Adds: ActivityLogCardViewModelSlice class

---
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/activity/ActivityLogCardViewModelSlice.kt
@@ -0,0 +1,12 @@
+package org.wordpress.android.ui.mysite.cards.dashboard.activity
+
+import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ActivityLogCardViewModelSlice @Inject constructor(
+    private val cardsTracker: CardsTracker
+){
+
+}
