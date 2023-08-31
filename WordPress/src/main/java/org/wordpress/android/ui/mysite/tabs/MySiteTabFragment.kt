1ff990b08434dfe14c8584275a6c90bf84a431ba
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-08-31 14:33:47 -0400

    Set and pass the isNewSite value to the quick start dialog fragment

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/tabs/MySiteTabFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/tabs/MySiteTabFragment.kt
@@ -420,7 +420,8 @@ class MySiteTabFragment : Fragment(R.layout.my_site_tab_fragment),
             action.title,
             action.message,
             action.positiveButtonLabel,
-            action.negativeButtonLabel
+            action.negativeButtonLabel,
+            action.isNewSite
         )
         is SiteNavigationAction.OpenQuickStartFullScreenDialog -> openQuickStartFullScreenDialog(action)
         is SiteNavigationAction.OpenDraftsPosts ->
@@ -672,7 +673,8 @@ class MySiteTabFragment : Fragment(R.layout.my_site_tab_fragment),
         @StringRes title: Int,
         @StringRes message: Int,
         @StringRes positiveButtonLabel: Int,
-        @StringRes negativeButtonLabel: Int
+        @StringRes negativeButtonLabel: Int,
+        isNewSite: Boolean
     ) {
         val tag = TAG_QUICK_START_DIALOG
         val quickStartPromptDialogFragment = QuickStartPromptDialogFragment()
@@ -682,7 +684,8 @@ class MySiteTabFragment : Fragment(R.layout.my_site_tab_fragment),
             getString(message),
             getString(positiveButtonLabel),
             R.drawable.img_illustration_site_about_280dp,
-            getString(negativeButtonLabel)
+            getString(negativeButtonLabel),
+            isNewSite
         )
         quickStartPromptDialogFragment.show(parentFragmentManager, tag)
         quickStartTracker.track(AnalyticsTracker.Stat.QUICK_START_REQUEST_VIEWED)
