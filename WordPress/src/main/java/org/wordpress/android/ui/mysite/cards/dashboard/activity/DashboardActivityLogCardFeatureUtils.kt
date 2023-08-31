bc30d088e40d17006b8afc80c6581b608a5d1452
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-08-31 11:32:24 +0530

    - Remove: activity log feature flag

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/activity/DashboardActivityLogCardFeatureUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/activity/DashboardActivityLogCardFeatureUtils.kt
@@ -3,16 +3,14 @@ package org.wordpress.android.ui.mysite.cards.dashboard.activity
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
 import org.wordpress.android.util.SiteUtilsWrapper
-import org.wordpress.android.util.config.DashboardCardActivityLogConfig
 import javax.inject.Inject
 
 class DashboardActivityLogCardFeatureUtils @Inject constructor(
-    private val dashboardCardActivityLogConfig: DashboardCardActivityLogConfig,
     private val siteUtilsWrapper: SiteUtilsWrapper,
     private val appPrefsWrapper: AppPrefsWrapper
 ) {
     fun shouldRequestActivityCard(selectedSite: SiteModel): Boolean {
-        if (!dashboardCardActivityLogConfig.isEnabled() || isActivityCardHiddenByUser(selectedSite.siteId)) return false
+        if (isActivityCardHiddenByUser(selectedSite.siteId)) return false
         val isWpComOrJetpack = siteUtilsWrapper.isAccessedViaWPComRest(selectedSite) ||
                 selectedSite.isJetpackConnected
         return selectedSite.hasCapabilityManageOptions
