672d85b6f8d0eb763be51839cc9f934a37edc53e
Ravi <ravi.program@gmail.com>
2023-09-02 18:45:19 +1000

    Update DomainTransferCardViewHolder.kt
    Add view composition strategy since Compose in a legacy View
    
    https://developer.android.com/jetpack/compose/migrate/interoperability-apis/compose-in-views

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/domaintransfer/DomainTransferCardViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/domaintransfer/DomainTransferCardViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnDetachedFromWindowOrReleasedFromPool
 import org.wordpress.android.databinding.DomainTransferCardBinding
 import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards.DashboardCard.DomainTransferCardModel
@@ -13,12 +14,16 @@ import org.wordpress.android.util.extensions.viewBinding
 class DomainTransferCardViewHolder(parent: ViewGroup) :
     CardViewHolder<DomainTransferCardBinding>(parent.viewBinding(DomainTransferCardBinding::inflate)) {
     fun bind(cardModel: DomainTransferCardModel) = with(binding) {
-        domainTransferCard.setContent {
-            AppTheme {
-                DomainTransferCard(
-                    domainTransferCardModel = cardModel,
-                    modifier = Modifier.fillMaxWidth().wrapContentHeight()
-                )
+        // Dispose of the Composition when the view's LifecycleOwner is destroyed
+        this.domainTransferCard.apply {
+            setViewCompositionStrategy(DisposeOnDetachedFromWindowOrReleasedFromPool)
+            setContent {
+                AppTheme {
+                    DomainTransferCard(
+                        domainTransferCardModel = cardModel,
+                        modifier = Modifier.fillMaxWidth().wrapContentHeight()
+                    )
+                }
             }
         }
     }
