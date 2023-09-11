130951f620f456ee55a1bd882d15a08f06a42536
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-09-11 11:39:25 +0530

    - Removes: resource attribute annotation

--- WordPress/src/main/java/org/wordpress/android/ui/avatars/TrainOfAvatarsItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/avatars/TrainOfAvatarsItem.kt
@@ -18,7 +18,7 @@ const val AVATAR_SIZE_DIMEN = R.dimen.avatar_sz_small
 
 sealed class TrainOfAvatarsItem(val type: TrainOfAvatarsViewType) {
     data class AvatarItem(val userAvatarUrl: String) : TrainOfAvatarsItem(AVATAR)
-    data class TrailingLabelTextItem(val text: UiString, @AttrRes val labelColor: Int) : TrainOfAvatarsItem(
+    data class TrailingLabelTextItem(val text: UiString, val labelColor: Int) : TrainOfAvatarsItem(
         TRAILING_LABEL
     )
 }
