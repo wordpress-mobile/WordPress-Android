05b0238fc8f7cf0e8d4a5c3f7cd77a514c28afde
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-09-13 11:51:26 +0530

    ↑ [WIP] Updates: the activity UI to have list view

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/personalisation/PersonalisationActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/personalisation/PersonalisationActivity.kt
@@ -1,8 +1,10 @@
 package org.wordpress.android.ui.mysite.personalisation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,46 +15,65 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import org.wordpress.android.R
 import org.wordpress.android.ui.compose.components.MainTopAppBar
 import org.wordpress.android.ui.compose.components.NavigationIcons
 import org.wordpress.android.ui.compose.theme.AppTheme
 
 class PersonalisationActivity : ComponentActivity() {
+    private val viewModel:PersonalisationViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                val uiItems =
-                PersonalisationScreen()
+                PersonalisationScreen(viewModel.uiState)
             }
         }
     }
 
     @Composable
-    fun PersonalisationScreen() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    fun PersonalisationScreen(uiState: LiveData<List<DashboardCardState>>) {
         Scaffold(
             topBar = {
                 MainTopAppBar(
                     title = stringResource(id = R.string.personalisation_screen_title),
                     navigationIcon = NavigationIcons.BackIcon,
-                    onNavigationIconClick = onBackClick,
+                    onNavigationIconClick = onBackPressedDispatcher::onBackPressed,
                 )
+            },
+            content = {
+                PersonalisationContent(uiState)
             }
+        )
+    }
+
+    @Composable
+    fun PersonalisationContent(uiState: LiveData<List<DashboardCardState>>) {
+        val cardStateList = uiState.value ?: emptyList()
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp)
         ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    LazyColumn(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentPadding = PaddingValues(bottom = 72.dp),
-                    ) {
-
-
-                    }
+            items(cardStateList.size) { index ->
+                val cardState = cardStateList[index]
+                DashboardCardStateRow(
+                    cardState = cardState,
+                    onToggle = viewModel::onToggle
+                )
             }
         }
     }
+}
+
+@Composable
+fun DashboardCardStateRow(
+    cardState: DashboardCardState,
+    onToggle: (Boolean, CardType) -> Unit,
+){
+    // todo., the logic to show the card state row
 }
 
 
