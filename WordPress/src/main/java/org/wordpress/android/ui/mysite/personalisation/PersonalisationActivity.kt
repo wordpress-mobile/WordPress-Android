4c4f70747bd8988e180ac18ee8751750126e6673
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-09-13 12:52:01 +0530

    + Adds: PersonalisationViewModel for building the list

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/personalisation/PersonalisationActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/personalisation/PersonalisationActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,7 +27,7 @@ import org.wordpress.android.ui.compose.components.NavigationIcons
 import org.wordpress.android.ui.compose.theme.AppTheme
 
 class PersonalisationActivity : ComponentActivity() {
-    private val viewModel:PersonalisationViewModel by viewModels()
+    private val viewModel: PersonalisationViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,25 +50,28 @@ class PersonalisationActivity : ComponentActivity() {
                 )
             },
             content = {
-                PersonalisationContent(uiState)
+                PersonalisationContent(uiState.value ?: emptyList())
             }
         )
     }
 
     @Composable
-    fun PersonalisationContent(uiState: LiveData<List<DashboardCardState>>) {
-        val cardStateList = uiState.value ?: emptyList()
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            items(cardStateList.size) { index ->
-                val cardState = cardStateList[index]
-                DashboardCardStateRow(
-                    cardState = cardState,
-                    onToggle = viewModel::onToggle
-                )
+    fun PersonalisationContent(cardStateList: List<DashboardCardState>) {
+        Row {
+            Text(text = stringResource(id = R.string.personalisation_screen_description))
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(cardStateList.size) { index ->
+                    val cardState = cardStateList[index]
+                    DashboardCardStateRow(
+                        cardState = cardState,
+                        onToggle = viewModel::onToggle
+                    )
+                }
             }
+            Text(text = stringResource(id = R.string.personalisation_screen_footer_cards))
         }
     }
 }
@@ -72,11 +80,25 @@ class PersonalisationActivity : ComponentActivity() {
 fun DashboardCardStateRow(
     cardState: DashboardCardState,
     onToggle: (Boolean, CardType) -> Unit,
-){
-    // todo., the logic to show the card state row
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier.fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Row () {
+                Text(text = stringResource(id = cardState.title))
+                cardState.description?.let {  Text(text = stringResource(id = cardState.description)) }
+            }
+            Switch(checked = cardState.enabled, onCheckedChange = {
+                onToggle(cardState.enabled, cardState.cardType)
+            })
+        }
+        Divider(
+            thickness = 0.5.dp,
+            modifier = Modifier
+                .padding()
+        )
+    }
 }
-
-
-
-
-
