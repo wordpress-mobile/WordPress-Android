4b1e1c6ba733beda151745aaa54a71dda8a4c2ed
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-09-13 15:58:05 -0400

    Track dashboard personalize card tapped

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/personalize/PersonalizeCardViewModelSlice.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/personalize/PersonalizeCardViewModelSlice.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import org.wordpress.android.ui.mysite.SiteNavigationAction.OpenDashboardPersonalization
 import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.PersonalizeCardBuilderParams
 import org.wordpress.android.ui.mysite.SiteNavigationAction
+import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker
 import org.wordpress.android.viewmodel.Event
 import javax.inject.Inject
 
 class PersonalizeCardViewModelSlice @Inject constructor(
+    private val cardsTracker: CardsTracker
 )  {
     private val _onNavigation = MutableLiveData<Event<SiteNavigationAction>>()
     val onNavigation = _onNavigation as LiveData<Event<SiteNavigationAction>>
@@ -16,6 +18,10 @@ class PersonalizeCardViewModelSlice @Inject constructor(
     fun getBuilderParams() = PersonalizeCardBuilderParams(onClick = this::onCardClick)
 
     fun onCardClick() {
+        cardsTracker.trackCardItemClicked(
+            CardsTracker.Type.PERSONALIZE_CARD.label,
+            CardsTracker.Type.PERSONALIZE_CARD.label
+        )
         _onNavigation.value = Event(OpenDashboardPersonalization)
     }
 }
