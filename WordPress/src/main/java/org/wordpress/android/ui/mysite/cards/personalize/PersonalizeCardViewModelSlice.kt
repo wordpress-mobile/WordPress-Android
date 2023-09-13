abef4106579fc0345667d558d505c67a5bf756fd
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-09-13 15:30:16 -0400

    Hook in the navigation event when the personalize card is clicked

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/personalize/PersonalizeCardViewModelSlice.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/personalize/PersonalizeCardViewModelSlice.kt
@@ -1,15 +1,21 @@
 package org.wordpress.android.ui.mysite.cards.personalize
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import org.wordpress.android.ui.mysite.SiteNavigationAction.OpenDashboardPersonalization
 import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.PersonalizeCardBuilderParams
+import org.wordpress.android.ui.mysite.SiteNavigationAction
+import org.wordpress.android.viewmodel.Event
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class PersonalizeCardViewModelSlice@Inject constructor(
+class PersonalizeCardViewModelSlice @Inject constructor(
 )  {
+    private val _onNavigation = MutableLiveData<Event<SiteNavigationAction>>()
+    val onNavigation = _onNavigation as LiveData<Event<SiteNavigationAction>>
+
     fun getBuilderParams() = PersonalizeCardBuilderParams(onClick = this::onCardClick)
 
     fun onCardClick() {
-        // todo: implement
+        _onNavigation.value = Event(OpenDashboardPersonalization)
     }
 }
