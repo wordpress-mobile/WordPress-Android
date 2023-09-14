41cfb6d55bac55015e4b08b37cf7b98575de3b74
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-09-14 12:15:40 +0530

    ↑ Updates: the switch compat from material library with WpSwitch

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/personalisation/PersonalisationActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/personalisation/PersonalisationActivity.kt
@@ -18,23 +18,21 @@ import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.components.MainTopAppBar
 import org.wordpress.android.ui.compose.components.NavigationIcons
+import org.wordpress.android.ui.compose.components.buttons.WPSwitch
 import org.wordpress.android.ui.compose.theme.AppTheme
 
 @AndroidEntryPoint
@@ -143,7 +141,7 @@ fun DashboardCardStateRow(
                 )
             }
             Spacer(Modifier.width(8.dp))
-            Switch(
+            WPSwitch(
                 checked = cardState.enabled,
                 onCheckedChange = {
                     onCardToggled(cardState.cardType, it)
