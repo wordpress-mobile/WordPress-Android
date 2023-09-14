78e2874c6184cffa95d506512d8448bcbcf16de3
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-09-14 15:40:16 +0530

    + Adds: tracking name for each card

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/personalisation/DashboardCardState.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/personalisation/DashboardCardState.kt
@@ -10,13 +10,13 @@ data class DashboardCardState(
 )
 
 
-enum class CardType(val order: Int) {
-    STATS(0),
-    DRAFT_POSTS(1),
-    SCHEDULED_POSTS(2),
-    PAGES(3),
-    ACTIVITY_LOG(4),
-    BLAZE(5),
-    BLOGGING_PROMPTS(6),
-    NEXT_STEPS(7)
+enum class CardType(val order: Int, val trackingName:String) {
+    STATS(0, "todays_stats"),
+    DRAFT_POSTS(1, "draft_posts"),
+    SCHEDULED_POSTS(2,"scheduled_posts"),
+    PAGES(3, "pages"),
+    ACTIVITY_LOG(4, "activity_log"),
+    BLAZE(5, "blaze"),
+    BLOGGING_PROMPTS(6, "blogging_prompts"),
+    NEXT_STEPS(7, "next_steps")
 }
