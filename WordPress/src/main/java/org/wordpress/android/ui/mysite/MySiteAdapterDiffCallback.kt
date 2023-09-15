76884e330f9d2a79b2a282d1f27b235d4675cb89
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-09-16 00:00:24 +0530

    ↑ Updates: the MySite Adapter to create viewholder for all the cards

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteAdapterDiffCallback.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteAdapterDiffCallback.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.mysite
 
 import androidx.recyclerview.widget.DiffUtil
-import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardCards
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DomainRegistrationCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.JetpackFeatureCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.JetpackInstallFullPluginCard
@@ -15,6 +14,17 @@ import org.wordpress.android.ui.mysite.MySiteCardAndItem.Item.InfoItem
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Item.ListItem
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Item.SingleActionCard
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.JetpackBadge
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.ActivityCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DomainTransferCardModel
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.DashboardPlansCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.BlazeCard.PromoteWithBlazeCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.BlazeCard.BlazeCampaignsCardModel
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.BloggingPromptCard.BloggingPromptCardWithData
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.ErrorCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.ErrorWithinCard
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.PostCard.PostCardWithPostItems
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.TodaysStatsCard.TodaysStatsCardWithData
+import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.PagesCard
 
 @Suppress("ComplexMethod")
 object MySiteAdapterDiffCallback : DiffUtil.ItemCallback<MySiteCardAndItem>() {
@@ -27,13 +37,25 @@ object MySiteAdapterDiffCallback : DiffUtil.ItemCallback<MySiteCardAndItem>() {
             oldItem is CategoryHeaderItem && updatedItem is CategoryHeaderItem -> oldItem.title == updatedItem.title
             oldItem is CategoryEmptyHeaderItem
                     && updatedItem is CategoryEmptyHeaderItem -> oldItem.title == updatedItem.title
+
             oldItem is ListItem && updatedItem is ListItem -> oldItem.primaryText == updatedItem.primaryText
-            oldItem is DashboardCards && updatedItem is DashboardCards -> true
+            oldItem is ErrorCard && updatedItem is ErrorCard -> true
+            oldItem is ErrorWithinCard && updatedItem is ErrorWithinCard -> true
+            oldItem is TodaysStatsCardWithData && updatedItem is TodaysStatsCardWithData -> true
+            oldItem is PostCardWithPostItems && updatedItem is PostCardWithPostItems -> true
+            oldItem is BloggingPromptCardWithData && updatedItem is BloggingPromptCardWithData -> true
+            oldItem is PromoteWithBlazeCard && updatedItem is PromoteWithBlazeCard -> true
+            oldItem is BlazeCampaignsCardModel && updatedItem is BlazeCampaignsCardModel -> true
+            oldItem is DomainTransferCardModel && updatedItem is DomainTransferCardModel -> true
+            oldItem is DashboardPlansCard && updatedItem is DashboardPlansCard -> true
+            oldItem is PagesCard && updatedItem is PagesCard -> true
+            oldItem is ActivityCard && updatedItem is ActivityCard -> true
             oldItem is JetpackBadge && updatedItem is JetpackBadge -> true
             oldItem is SingleActionCard && updatedItem is SingleActionCard -> {
                 oldItem.textResource == updatedItem.textResource
                         && oldItem.imageResource == updatedItem.imageResource
             }
+
             oldItem is JetpackFeatureCard && updatedItem is JetpackFeatureCard -> true
             oldItem is JetpackSwitchMenu && updatedItem is JetpackSwitchMenu -> true
             oldItem is JetpackInstallFullPluginCard && updatedItem is JetpackInstallFullPluginCard -> true
@@ -41,6 +63,7 @@ object MySiteAdapterDiffCallback : DiffUtil.ItemCallback<MySiteCardAndItem>() {
             else -> throw UnsupportedOperationException("Diff not implemented yet")
         }
     }
+
 
     override fun areContentsTheSame(oldItem: MySiteCardAndItem, newItem: MySiteCardAndItem): Boolean {
         if (oldItem.activeQuickStartItem || newItem.activeQuickStartItem) return false
