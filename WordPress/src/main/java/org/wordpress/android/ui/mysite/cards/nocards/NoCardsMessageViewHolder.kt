c3bfe16aa61d4156e46f65e1da30a5be1ae7c67b
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-09-18 12:35:13 +0530

    * Fixes: Renames No Cards messaging

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/nocards/NoCardsMessageViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/nocards/NoCardsMessageViewHolder.kt
@@ -4,14 +4,14 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.ui.Modifier
-import org.wordpress.android.databinding.NoCardsMessageCardBinding
+import org.wordpress.android.databinding.NoCardsMessageBinding
 import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.ui.mysite.MySiteCardAndItem
 import org.wordpress.android.ui.mysite.cards.dashboard.CardViewHolder
 import org.wordpress.android.util.extensions.viewBinding
 
 class NoCardsMessageViewHolder(parent: ViewGroup) :
-    CardViewHolder<NoCardsMessageCardBinding>(parent.viewBinding(NoCardsMessageCardBinding::inflate)) {
+    CardViewHolder<NoCardsMessageBinding>(parent.viewBinding(NoCardsMessageBinding::inflate)) {
     fun bind(cardModel: MySiteCardAndItem.Card.NoCardsMessage) = with(binding) {
         noCardsMessage.setContent {
             AppTheme {
