c7b733cb5cf64f053e4ff068d4128f48bcb4e3e6
Thomas Horta <thomas.horta@automattic.com>
2023-09-20 11:52:31 -0300

    Use new header layout when improvement config is enabled

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderPostDetailHeaderView.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/ReaderPostDetailHeaderView.kt
@@ -3,17 +3,22 @@ package org.wordpress.android.ui.reader.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
+import com.google.android.material.textview.MaterialTextView
 import org.wordpress.android.WordPress
-import org.wordpress.android.databinding.ReaderBlogSectionViewBinding
 import org.wordpress.android.databinding.ReaderPostDetailHeaderViewBinding
+import org.wordpress.android.databinding.ReaderPostDetailHeaderViewNewBinding
 import org.wordpress.android.ui.reader.views.uistates.FollowButtonUiState
 import org.wordpress.android.ui.reader.views.uistates.ReaderBlogSectionUiState
 import org.wordpress.android.ui.reader.views.uistates.ReaderPostDetailsHeaderViewUiState.ReaderPostDetailsHeaderUiState
 import org.wordpress.android.ui.utils.UiHelpers
+import org.wordpress.android.util.config.ReaderImprovementsFeatureConfig
 import org.wordpress.android.util.extensions.getDrawableResIdFromAttribute
 import org.wordpress.android.util.extensions.setVisible
 import org.wordpress.android.util.image.ImageManager
+import org.wordpress.android.util.image.ImageType
 import javax.inject.Inject
 
 /**
@@ -24,7 +29,7 @@ class ReaderPostDetailHeaderView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
-    private val binding = ReaderPostDetailHeaderViewBinding.inflate(LayoutInflater.from(context), this, true)
+    private val binding: ReaderPostDetailHeaderBinding
 
     @Inject
     lateinit var imageManager: ImageManager
@@ -32,33 +37,47 @@ class ReaderPostDetailHeaderView @JvmOverloads constructor(
     @Inject
     lateinit var uiHelpers: UiHelpers
 
+    @Inject
+    lateinit var readerImprovementsFeatureConfig: ReaderImprovementsFeatureConfig
+
     init {
         (context.applicationContext as WordPress).component().inject(this)
+        binding = if (readerImprovementsFeatureConfig.isEnabled()) {
+            val viewBinding = ReaderPostDetailHeaderViewNewBinding.inflate(LayoutInflater.from(context), this, true)
+            ReaderPostDetailHeaderBinding.ImprovementsEnabled(
+                viewBinding,
+                uiHelpers
+            )
+        } else {
+            val viewBinding = ReaderPostDetailHeaderViewBinding.inflate(LayoutInflater.from(context), this, true)
+            ReaderPostDetailHeaderBinding.ImprovementsDisabled(
+                viewBinding,
+                uiHelpers
+            )
+        }
     }
 
     fun updatePost(uiState: ReaderPostDetailsHeaderUiState) = with(binding) {
         expandableTagsView.setVisible(uiState.tagItemsVisibility)
         expandableTagsView.updateUi(uiState.tagItems)
 
-        uiHelpers.setTextOrHide(textTitle, uiState.title)
+        uiHelpers.setTextOrHide(titleText, uiState.title)
 
-        layoutBlogSection.updateBlogSection(uiState.blogSectionUiState)
-        updateFollowButton(uiState.followButtonUiState)
+        setAuthorAndDate(uiState.authorName, uiState.dateLine)
 
-        uiHelpers.setTextOrHide(textAuthor, uiState.authorName)
-        textBy.setVisible(uiState.authorName != null)
+        uiHelpers.setTextOrHide(blogNameText, uiState.blogSectionUiState.blogName)
+        blogUrlText?.let { uiHelpers.setTextOrHide(it, uiState.blogSectionUiState.blogUrl) }
 
-        uiHelpers.updateVisibility(postDetailDotSeparator, uiState.dotSeparatorVisibility)
-        uiHelpers.setTextOrHide(postDetailTextDateline, uiState.dateLine)
+        followButton.update(uiState.followButtonUiState)
+
+        updateAvatars(uiState.blogSectionUiState)
+        updateBlogSectionClick(uiState.blogSectionUiState)
     }
 
-    private fun ReaderBlogSectionViewBinding.updateBlogSection(state: ReaderBlogSectionUiState) {
-        updateBlavatar(state)
-
-        uiHelpers.setTextOrHide(textAuthorAndBlogName, state.blogName)
-        uiHelpers.setTextOrHide(textBlogUrl, state.blogUrl)
-
-        root.apply {
+    private fun ReaderPostDetailHeaderBinding.updateBlogSectionClick(
+        state: ReaderBlogSectionUiState
+    ) {
+        blogSectionRoot.apply {
             setBackgroundResource(context.getDrawableResIdFromAttribute(state.blogSectionClickData?.background ?: 0))
             state.blogSectionClickData?.onBlogSectionClicked?.let { onClick ->
                 setOnClickListener { onClick.invoke(state.postId, state.blogId) }
@@ -69,21 +88,99 @@ class ReaderPostDetailHeaderView @JvmOverloads constructor(
         }
     }
 
-    private fun ReaderBlogSectionViewBinding.updateBlavatar(state: ReaderBlogSectionUiState) {
-        uiHelpers.updateVisibility(imageAvatarOrBlavatar, state.avatarOrBlavatarUrl != null)
+    private fun ReaderPostDetailHeaderBinding.updateAvatars(state: ReaderBlogSectionUiState) {
+        uiHelpers.updateVisibility(blogAvatarImage, state.avatarOrBlavatarUrl != null)
         if (state.avatarOrBlavatarUrl == null) {
-            imageManager.cancelRequestAndClearImageView(imageAvatarOrBlavatar)
+            imageManager.cancelRequestAndClearImageView(blogAvatarImage)
         } else {
-            imageManager.loadIntoCircle(imageAvatarOrBlavatar, state.blavatarType, state.avatarOrBlavatarUrl)
+            imageManager.loadIntoCircle(blogAvatarImage, state.blavatarType, state.avatarOrBlavatarUrl)
         }
-        // we don't show the p2 style of header in post details yet
-        uiHelpers.updateVisibility(authorsAvatar, state.isAuthorAvatarVisible)
+
+        val showAuthorsAvatar = state.authorAvatarUrl != null && state.isAuthorAvatarVisible
+        uiHelpers.updateVisibility(authorAvatarImage, showAuthorsAvatar)
+        if (!showAuthorsAvatar) {
+            imageManager.cancelRequestAndClearImageView(authorAvatarImage)
+        } else {
+            imageManager.loadIntoCircle(authorAvatarImage, ImageType.BLAVATAR_CIRCULAR, state.authorAvatarUrl!!)
+        }
     }
 
-    private fun ReaderPostDetailHeaderViewBinding.updateFollowButton(followButtonUiState: FollowButtonUiState) {
-        headerFollowButton.isEnabled = followButtonUiState.isEnabled
-        headerFollowButton.setVisible(followButtonUiState.isVisible)
-        headerFollowButton.setIsFollowed(followButtonUiState.isFollowed)
-        headerFollowButton.setOnClickListener { followButtonUiState.onFollowButtonClicked?.invoke() }
+    private fun ReaderFollowButton.update(followButtonUiState: FollowButtonUiState) {
+        isEnabled = followButtonUiState.isEnabled
+        setVisible(followButtonUiState.isVisible)
+        setIsFollowed(followButtonUiState.isFollowed)
+        setOnClickListener { followButtonUiState.onFollowButtonClicked?.invoke() }
+    }
+
+    private interface ReaderPostDetailHeaderBinding {
+        val titleText: MaterialTextView
+        val blogNameText: MaterialTextView
+        val blogUrlText: MaterialTextView?
+        val expandableTagsView: ReaderExpandableTagsView
+        val blogAvatarImage: ImageView
+        val authorAvatarImage: ImageView
+        val followButton: ReaderFollowButton
+        val blogSectionRoot: ViewGroup
+
+        fun setAuthorAndDate(authorName: String?, dateLine: String)
+
+        class ImprovementsDisabled(
+            private val binding: ReaderPostDetailHeaderViewBinding,
+            private val uiHelpers: UiHelpers,
+        ) : ReaderPostDetailHeaderBinding {
+            override val titleText: MaterialTextView
+                get() = binding.textTitle
+            override val blogNameText: MaterialTextView
+                get() = binding.layoutBlogSection.textAuthorAndBlogName
+            override val blogUrlText: MaterialTextView
+                get() = binding.layoutBlogSection.textBlogUrl
+            override val expandableTagsView: ReaderExpandableTagsView
+                get() = binding.expandableTagsView
+            override val blogAvatarImage: ImageView
+                get() = binding.layoutBlogSection.imageAvatarOrBlavatar
+            override val authorAvatarImage: ImageView
+                get() = binding.layoutBlogSection.authorsAvatar
+            override val followButton: ReaderFollowButton
+                get() = binding.headerFollowButton
+            override val blogSectionRoot: ViewGroup
+                get() = binding.layoutBlogSection.root
+
+            override fun setAuthorAndDate(authorName: String?, dateLine: String) = with(binding) {
+                uiHelpers.setTextOrHide(textAuthor, authorName)
+                uiHelpers.setTextOrHide(postDetailTextDateline, dateLine)
+
+                textBy.setVisible(authorName != null)
+                postDetailDotSeparator.setVisible(authorName != null)
+            }
+        }
+
+        class ImprovementsEnabled(
+            private val binding: ReaderPostDetailHeaderViewNewBinding,
+            private val uiHelpers: UiHelpers,
+        ) : ReaderPostDetailHeaderBinding {
+            override val titleText: MaterialTextView
+                get() = binding.textTitle
+            override val blogNameText: MaterialTextView
+                get() = binding.layoutBlogSection.blogSectionTextBlogName
+            override val blogUrlText: MaterialTextView?
+                get() = null
+            override val expandableTagsView: ReaderExpandableTagsView
+                get() = binding.expandableTagsView
+            override val blogAvatarImage: ImageView
+                get() = binding.layoutBlogSection.blogSectionImageBlogAvatar
+            override val authorAvatarImage: ImageView
+                get() = binding.layoutBlogSection.blogSectionImageAuthorAvatar
+            override val followButton: ReaderFollowButton
+                get() = binding.headerFollowButton
+            override val blogSectionRoot: ViewGroup
+                get() = binding.layoutBlogSection.root
+
+            override fun setAuthorAndDate(authorName: String?, dateLine: String) = with(binding.layoutBlogSection) {
+                uiHelpers.setTextOrHide(blogSectionTextAuthor, authorName)
+                uiHelpers.setTextOrHide(blogSectionTextDateline, dateLine)
+
+                blogSectionDotSeparator.setVisible(authorName != null)
+            }
+        }
     }
 }
