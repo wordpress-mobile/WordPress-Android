1e4b036b450b731a6d45ef837d818b72fac5af98
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-09-25 23:00:45 -0300

    Apply PR suggestion: remove unnecessary class ReaderRecommendedBlogBinding

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/viewholders/ReaderRecommendedBlogNewViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/viewholders/ReaderRecommendedBlogNewViewHolder.kt
@@ -1,13 +1,9 @@
 package org.wordpress.android.ui.reader.discover.viewholders
 
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.wordpress.android.databinding.ReaderRecommendedBlogItemNewBinding
 import org.wordpress.android.ui.reader.discover.ReaderCardUiState.ReaderRecommendedBlogsCardUiState.ReaderRecommendedBlogUiState
-import org.wordpress.android.ui.reader.views.ReaderFollowButton
 import org.wordpress.android.util.extensions.viewBinding
 import org.wordpress.android.util.image.ImageManager
 import org.wordpress.android.util.image.ImageType.BLAVATAR_CIRCULAR
@@ -15,30 +11,25 @@ import org.wordpress.android.util.image.ImageType.BLAVATAR_CIRCULAR
 class ReaderRecommendedBlogNewViewHolder(
     parent: ViewGroup,
     private val imageManager: ImageManager,
-    private val binding: ReaderRecommendedBlogBinding =
-        with(parent.viewBinding(ReaderRecommendedBlogItemNewBinding::inflate)) {
-            ReaderRecommendedBlogBinding(
-                root = root,
-                siteName = siteName,
-                siteUrl = siteUrl,
-                siteIcon = siteIcon,
-                siteFollowIcon = siteFollowIcon,
-            )
-        },
+    private val binding: ReaderRecommendedBlogItemNewBinding =
+        parent.viewBinding(ReaderRecommendedBlogItemNewBinding::inflate)
 ) : RecyclerView.ViewHolder(binding.root) {
     fun onBind(uiState: ReaderRecommendedBlogUiState) =
         with(binding) {
             siteName.text = uiState.name
             siteUrl.text = uiState.url
-            updateSiteFollowIcon(uiState, this)
+            updateSiteFollowButton(uiState, this)
             updateBlogImage(uiState.iconUrl)
             root.setOnClickListener {
                 uiState.onItemClicked(uiState.blogId, uiState.feedId, uiState.isFollowed)
             }
         }
 
-    private fun updateSiteFollowIcon(uiState: ReaderRecommendedBlogUiState, binding: ReaderRecommendedBlogBinding) {
-        with(binding.siteFollowIcon) {
+    private fun updateSiteFollowButton(
+        uiState: ReaderRecommendedBlogUiState,
+        binding: ReaderRecommendedBlogItemNewBinding
+    ) {
+        with(binding.siteFollowButton) {
             setIsFollowed(uiState.isFollowed)
             contentDescription = context.getString(uiState.followContentDescription.stringRes)
             setOnClickListener {
@@ -58,12 +49,4 @@ class ReaderRecommendedBlogNewViewHolder(
             imageManager.cancelRequestAndClearImageView(siteIcon)
         }
     }
-
-    data class ReaderRecommendedBlogBinding(
-        val root: View,
-        val siteName: TextView,
-        val siteUrl: TextView,
-        val siteIcon: ImageView,
-        val siteFollowIcon: ReaderFollowButton,
-    )
 }
