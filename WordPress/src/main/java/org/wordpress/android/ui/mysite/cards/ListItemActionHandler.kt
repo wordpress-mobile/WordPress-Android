214f8f368e2bb3d5b790b2930e69763eb7478fff
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-09-28 18:10:17 +0530

    + Adds: List action handler to handle the site items click

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/ListItemActionHandler.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/ListItemActionHandler.kt
@@ -1,66 +1,48 @@
 package org.wordpress.android.ui.mysite.cards
 
-import androidx.lifecycle.MutableLiveData
-import org.wordpress.android.R
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.ui.blaze.BlazeFeatureUtils
 import org.wordpress.android.ui.blaze.BlazeFlowSource
 import org.wordpress.android.ui.blaze.blazecampaigns.campaignlisting.CampaignListingPageSource
 import org.wordpress.android.ui.jetpackoverlay.JetpackFeatureRemovalPhaseHelper
-import org.wordpress.android.ui.mysite.SelectedSiteRepository
 import org.wordpress.android.ui.mysite.SiteNavigationAction
 import org.wordpress.android.ui.mysite.items.listitem.ListItemAction
-import org.wordpress.android.ui.pages.SnackbarMessageHolder
-import org.wordpress.android.ui.utils.UiString
-import org.wordpress.android.viewmodel.Event
 import javax.inject.Inject
 
 class ListItemActionHandler @Inject constructor(
-    private val selectedSiteRepository: SelectedSiteRepository,
     private val accountStore: AccountStore,
     private val jetpackFeatureRemovalPhaseHelper: JetpackFeatureRemovalPhaseHelper,
     private val blazeFeatureUtils: BlazeFeatureUtils
 ) {
-    private val _onNavigation = MutableLiveData<Event<SiteNavigationAction>>()
-    val onNavigation = _onNavigation
-
-    private val _onSnackbarMessage = MutableLiveData<Event<SnackbarMessageHolder>>()
-    val onSnackbarMessage = _onSnackbarMessage
-
-    fun handleAction(action: ListItemAction) {
-        selectedSiteRepository.getSelectedSite()?.let { selectedSite ->
-            val navigationAction = when (action) {
-                ListItemAction.ACTIVITY_LOG -> SiteNavigationAction.OpenActivityLog(selectedSite)
-                ListItemAction.BACKUP -> SiteNavigationAction.OpenBackup(selectedSite)
-                ListItemAction.SCAN -> SiteNavigationAction.OpenScan(selectedSite)
-                ListItemAction.PLAN -> SiteNavigationAction.OpenPlan(selectedSite)
-                ListItemAction.POSTS -> SiteNavigationAction.OpenPosts(selectedSite)
-                ListItemAction.PAGES -> {
-                    SiteNavigationAction.OpenPages(selectedSite)
-                }
-
-                ListItemAction.ADMIN -> SiteNavigationAction.OpenAdmin(selectedSite)
-                ListItemAction.PEOPLE -> SiteNavigationAction.OpenPeople(selectedSite)
-                ListItemAction.SHARING -> {
-                    SiteNavigationAction.OpenSharing(selectedSite)
-                }
-
-                ListItemAction.DOMAINS -> SiteNavigationAction.OpenDomains(selectedSite)
-                ListItemAction.ME -> SiteNavigationAction.OpenMeScreen
-                ListItemAction.SITE_SETTINGS -> SiteNavigationAction.OpenSiteSettings(selectedSite)
-                ListItemAction.THEMES -> SiteNavigationAction.OpenThemes(selectedSite)
-                ListItemAction.PLUGINS -> SiteNavigationAction.OpenPlugins(selectedSite)
-                ListItemAction.STATS -> getStatsNavigationActionForSite(selectedSite)
-                ListItemAction.MEDIA -> SiteNavigationAction.OpenMedia(selectedSite)
-                ListItemAction.COMMENTS -> SiteNavigationAction.OpenUnifiedComments(selectedSite)
-                ListItemAction.BLAZE -> onBlazeMenuItemClick()
-                ListItemAction.MORE -> SiteNavigationAction.OpenMore(selectedSite)
+    fun handleAction(action: ListItemAction, selectedSite: SiteModel): SiteNavigationAction {
+        return when (action) {
+            ListItemAction.ACTIVITY_LOG -> SiteNavigationAction.OpenActivityLog(selectedSite)
+            ListItemAction.BACKUP -> SiteNavigationAction.OpenBackup(selectedSite)
+            ListItemAction.SCAN -> SiteNavigationAction.OpenScan(selectedSite)
+            ListItemAction.PLAN -> SiteNavigationAction.OpenPlan(selectedSite)
+            ListItemAction.POSTS -> SiteNavigationAction.OpenPosts(selectedSite)
+            ListItemAction.PAGES -> {
+                SiteNavigationAction.OpenPages(selectedSite)
             }
-            _onNavigation.postValue(Event(navigationAction))
-        } ?: _onSnackbarMessage.postValue(
-            Event(SnackbarMessageHolder(UiString.UiStringRes(R.string.site_cannot_be_loaded)))
-        )
+
+            ListItemAction.ADMIN -> SiteNavigationAction.OpenAdmin(selectedSite)
+            ListItemAction.PEOPLE -> SiteNavigationAction.OpenPeople(selectedSite)
+            ListItemAction.SHARING -> {
+                SiteNavigationAction.OpenSharing(selectedSite)
+            }
+
+            ListItemAction.DOMAINS -> SiteNavigationAction.OpenDomains(selectedSite)
+            ListItemAction.ME -> SiteNavigationAction.OpenMeScreen
+            ListItemAction.SITE_SETTINGS -> SiteNavigationAction.OpenSiteSettings(selectedSite)
+            ListItemAction.THEMES -> SiteNavigationAction.OpenThemes(selectedSite)
+            ListItemAction.PLUGINS -> SiteNavigationAction.OpenPlugins(selectedSite)
+            ListItemAction.STATS -> getStatsNavigationActionForSite(selectedSite)
+            ListItemAction.MEDIA -> SiteNavigationAction.OpenMedia(selectedSite)
+            ListItemAction.COMMENTS -> SiteNavigationAction.OpenUnifiedComments(selectedSite)
+            ListItemAction.BLAZE -> onBlazeMenuItemClick()
+            ListItemAction.MORE -> SiteNavigationAction.OpenMore(selectedSite)
+        }
     }
 
     private fun getStatsNavigationActionForSite(site: SiteModel): SiteNavigationAction = when {
