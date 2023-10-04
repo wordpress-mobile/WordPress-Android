5aa4853a053213cef7c9a221899bf9550872425c
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-10-04 17:47:33 +0530

    * Fixes: the base activity in PersonalizationActivity and MenuActivity

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/menu/MenuActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/menu/MenuActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -63,7 +63,7 @@ import org.wordpress.android.util.LocaleManager
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MenuActivity : ComponentActivity() {
+class MenuActivity : AppCompatActivity() {
     @Inject
     lateinit var activityNavigator: ActivityNavigator
     private val viewModel: MenuViewModel by viewModels()
--- WordPress/src/main/java/org/wordpress/android/ui/mysite/personalization/PersonalizationActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/personalization/PersonalizationActivity.kt
@@ -2,9 +2,9 @@ package org.wordpress.android.ui.mysite.personalization
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -57,7 +57,7 @@ import org.wordpress.android.ui.mysite.items.listitem.ListItemAction
 import org.wordpress.android.ui.utils.UiString
 
 @AndroidEntryPoint
-class PersonalizationActivity : ComponentActivity() {
+class PersonalizationActivity : AppCompatActivity() {
     private val viewModel: PersonalizationViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
