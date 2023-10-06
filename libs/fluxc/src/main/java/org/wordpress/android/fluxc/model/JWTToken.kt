2dc764330e8c30f2d0383d47cb7fa29c10833e99
Hicham Boushaba <hicham.boushaba@gmail.com>
2023-10-06 15:32:48 +0100

    Implement logic to validate the token's blog_id property

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/JWTToken.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/JWTToken.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.fluxc.model
 
 import android.util.Base64
-import org.json.JSONException
 import org.json.JSONObject
 
 @JvmInline
@@ -11,21 +10,26 @@ value class JWTToken(
     /**
      * Returns the token if it is still valid, or null if it is expired.
      */
-    @Suppress("SwallowedException", "MagicNumber")
-    fun takeIfValid(): JWTToken? {
-        fun JSONObject.getLongOrNull(name: String) = try {
-            this.getLong(name)
-        } catch (e: JSONException) {
-            null
-        }
+    @Suppress("MagicNumber")
+    fun validateExpiryDate(): JWTToken? {
+        fun JSONObject.getLongOrNull(name: String) = this.optLong(name, Long.MAX_VALUE).takeIf { it != Long.MAX_VALUE }
 
-        val payload = this.value.split(".")[1]
-        val claimsJson = String(Base64.decode(payload, Base64.DEFAULT))
-        val claims = JSONObject(claimsJson)
+        val payloadJson = getPayloadJson()
+        val expiration = payloadJson.getLongOrNull("exp")
+            ?: payloadJson.getLongOrNull("expires")
+            ?: return null
 
-        val expiration = claims.getLongOrNull("exp") ?: claims.getLongOrNull("expires") ?: return null
         val now = System.currentTimeMillis() / 1000
 
         return if (expiration > now) this else null
+    }
+
+    fun getPayloadItem(key: String): String? {
+        return getPayloadJson().optString(key)
+    }
+
+    private fun getPayloadJson(): JSONObject {
+        val payloadEncoded = this.value.split(".")[1]
+        return JSONObject(String(Base64.decode(payloadEncoded, Base64.DEFAULT)))
     }
 }
