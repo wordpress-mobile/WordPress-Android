adcc97b93fc1c274162c5c34d5d991c46e61d0e9
Antonis Lilis <antonis.lilis@automattic.com>
2023-10-12 15:17:21 +0300

    Enhances the existing test and data to cover the domain status in the all-domains response

--- fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteRestClientTest.kt
+++ fluxc/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/site/SiteRestClientTest.kt
@@ -31,6 +31,8 @@ import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AppSecrets
 import org.wordpress.android.fluxc.network.rest.wpcom.site.NewSiteResponse.BlogDetails
 import org.wordpress.android.fluxc.network.rest.wpcom.site.SiteWPComRestResponse.SitesResponse
+import org.wordpress.android.fluxc.network.rest.wpcom.site.StatusType.ERROR
+import org.wordpress.android.fluxc.network.rest.wpcom.site.StatusType.SUCCESS
 import org.wordpress.android.fluxc.store.SiteStore.PostFormatsErrorType
 import org.wordpress.android.fluxc.store.SiteStore.SiteFilter.WPCOM
 import org.wordpress.android.fluxc.store.SiteStore.SiteVisibility
@@ -538,14 +540,26 @@ class SiteRestClientTest {
 
         initAllDomainsResponse(data = response)
 
-        val responseModel = restClient.fetchAllDomains(noWpCom = true)
+        val responseModel = restClient.fetchAllDomains()
         assert(responseModel is Success)
         with((responseModel as Success).data) {
-            assertThat(domains).hasSize(2)
+            assertThat(domains).hasSize(4)
             assertThat(domains[0].domain).isEqualTo("some.test.domain")
             assertThat(domains[0].wpcomDomain).isFalse
-            assertThat(domains[1].domain).isEqualTo("some.test.domain 2")
+            assertThat(domains[0].domainStatus).isNotNull
+            assertThat(domains[0].domainStatus?.status).isEqualTo("Active")
+            assertThat(domains[0].domainStatus?.statusType).isEqualTo(SUCCESS)
+            assertThat(domains[1].domain).isEqualTo("some.test.domain.with.status.weight")
             assertThat(domains[1].wpcomDomain).isTrue
+            assertThat(domains[1].domainStatus).isNotNull
+            assertThat(domains[1].domainStatus?.status).isEqualTo("Expiring soon")
+            assertThat(domains[1].domainStatus?.statusType).isEqualTo(ERROR)
+            assertThat(domains[1].domainStatus?.statusWeight).isEqualTo(1000)
+            assertThat(domains[2].domain).isEqualTo("some.test.domain.with.action.required")
+            assertThat(domains[2].domainStatus).isNotNull
+            assertThat(domains[2].domainStatus?.actionRequired).isTrue
+            assertThat(domains[3].domain).isEqualTo("some.test.domain.no.domain.status")
+            assertThat(domains[3].domainStatus).isNull()
         }
     }
 
@@ -554,7 +568,7 @@ class SiteRestClientTest {
         val error = WPComGsonNetworkError(BaseNetworkError(GenericErrorType.NETWORK_ERROR))
         initAllDomainsResponse(error = error)
 
-        val response = restClient.fetchAllDomains(noWpCom = true)
+        val response = restClient.fetchAllDomains()
         assert(response is Response.Error)
         with((response as Response.Error).error) {
             assertThat(type).isEqualTo(GenericErrorType.NETWORK_ERROR)
@@ -567,7 +581,7 @@ class SiteRestClientTest {
         val error = WPComGsonNetworkError(BaseNetworkError(GenericErrorType.TIMEOUT))
         initAllDomainsResponse(error = error)
 
-        val response = restClient.fetchAllDomains(noWpCom = true)
+        val response = restClient.fetchAllDomains()
         assert(response is Response.Error)
         with((response as Response.Error).error) {
             assertThat(type).isEqualTo(GenericErrorType.TIMEOUT)
@@ -581,7 +595,7 @@ class SiteRestClientTest {
         val error = WPComGsonNetworkError(BaseNetworkError(GenericErrorType.NOT_AUTHENTICATED, tokenErrorMessage))
         initAllDomainsResponse(error = error)
 
-        val response = restClient.fetchAllDomains(noWpCom = true)
+        val response = restClient.fetchAllDomains()
         assert(response is Response.Error)
         with((response as Response.Error).error) {
             assertThat(type).isEqualTo(GenericErrorType.NOT_AUTHENTICATED)
