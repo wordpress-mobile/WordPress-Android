551d249abed3f65d9c783743d74c577c0f1476f2
Matthew Kevins <mmkevins@yahoo.com>
2023-10-16 15:43:25 +1000

    Allow overriding colors and elevation in MainTopAppBar
    This is needed for compatibility with Material 3 theming. In the future,
    a Material 3 MainTopAppBar can be used, but that API is experimental for
    now, and subject to change.

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/MainTopAppBar.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/MainTopAppBar.kt
@@ -13,8 +13,10 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.theme.AppTheme
@@ -70,13 +72,16 @@ fun MainTopAppBar(
     modifier: Modifier = Modifier,
     navigationIcon: NavigationIcon? = null,
     onNavigationIconClick: () -> Unit = {},
-    actions: @Composable RowScope.() -> Unit = {}
+    actions: @Composable RowScope.() -> Unit = {},
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = MaterialTheme.colors.onSurface,
+    elevation: Dp = 0.dp,
 ) {
     TopAppBar(
         modifier = modifier,
-        backgroundColor = MaterialTheme.colors.surface,
-        contentColor = MaterialTheme.colors.onSurface,
-        elevation = 0.dp,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        elevation = elevation,
         title = title?.let {
             withFullContentAlpha {
                 Text(title)
