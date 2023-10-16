ed4b6cc095e549511cce359643066b74f5ae4a3c
Matthew Kevins <mmkevins@yahoo.com>
2023-10-17 06:45:40 +1000

    Add plus icon to domains list top bar

--- WordPress/src/main/java/org/wordpress/android/ui/domains/management/MyDomainsScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/management/MyDomainsScreen.kt
@@ -13,7 +13,10 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -46,6 +49,14 @@ fun MyDomainsScreen(uiState: DomainManagementViewModel.UiState) {
                 title = "My Domains",
                 navigationIcon = NavigationIcons.BackIcon,
                 onNavigationIconClick = {},
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            Icons.Default.Add,
+                            contentDescription = "purchase a domain"
+                        )
+                    }
+                },
                 backgroundColor = MaterialTheme.colorScheme.surface,
                 contentColor = MaterialTheme.colorScheme.onSurface,
             )
