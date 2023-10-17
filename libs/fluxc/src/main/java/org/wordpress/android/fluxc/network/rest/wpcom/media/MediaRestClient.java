3494e1a03822840cecc97820d1b9c12398f2fda9
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2023-10-17 17:42:54 +0300

    Analysis: Add missing final to ok http client field on media clients
    Warning: "Field 'mOkHttpClient' may be 'final'"

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/MediaRestClient.java
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/MediaRestClient.java
@@ -80,7 +80,7 @@ import okhttp3.ResponseBody;
  */
 @Singleton
 public class MediaRestClient extends BaseWPComRestClient implements ProgressListener {
-    private OkHttpClient mOkHttpClient;
+    private final OkHttpClient mOkHttpClient;
     private MediaResponseUtils mMediaResponseUtils;
     // this will hold which media is being uploaded by which call, in order to be able
     // to monitor multiple uploads
