8ddfa019f0af0f7fd7895f83001a8af8d93ca5b6
Matthew Kevins <mmkevins@yahoo.com>
2023-10-18 15:04:38 +1000

    Adjust content padding in domain managment list

--- WordPress/src/main/java/org/wordpress/android/ui/domains/management/MyDomainsScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/management/MyDomainsScreen.kt
@@ -4,9 +4,8 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -113,11 +112,9 @@ fun MyDomainsList(
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier.padding(horizontal = 16.dp),
+        contentPadding = PaddingValues(top = 16.dp),
         state = listState,
     ) {
-        item {
-            Spacer(modifier = Modifier.height(16.dp))
-        }
         when (listUiState) {
             PopulatedList.Initial ->
                 repeat(2) {
