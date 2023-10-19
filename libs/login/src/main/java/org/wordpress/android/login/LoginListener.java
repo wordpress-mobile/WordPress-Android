3c51489519ba6cc3049bcf82b439ab59b521c64e
ThomazFB <thomazfcortez@gmail.com>
2023-10-19 19:17:29 -0300

    Add security key events to LoginListener

--- libs/login/src/main/java/org/wordpress/android/login/LoginListener.java
+++ libs/login/src/main/java/org/wordpress/android/login/LoginListener.java
@@ -50,6 +50,8 @@ public interface LoginListener {
     void needs2fa(String email, String password);
     void needs2faSocial(String email, String userId, String nonceAuthenticator, String nonceBackup, String nonceSms);
     void needs2faSocialConnect(String email, String password, String idToken, String service);
+    void needsSecurityKey(String userId, String nonceInfo);
+    void needsSocialSecurityKey(String userId, String nonceInfo);
     void loggedInViaPassword(ArrayList<Integer> oldSitesIds);
     void helpEmailPasswordScreen(String email);
 
