2a59300ce84b81cc5318319786ecd263a032601c
Artyom Vlasov <artyom.vlasov@automattic.com>
2023-10-24 14:24:38 -0600

    Add transfer domain footer to the new domain search screen

--- WordPress/src/main/java/org/wordpress/android/ui/domains/management/newdomainsearch/composable/NewDomainSearchScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/management/newdomainsearch/composable/NewDomainSearchScreen.kt
@@ -1,12 +1,19 @@
 package org.wordpress.android.ui.domains.management.newdomainsearch.composable
 
+import android.content.res.Configuration
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +21,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
@@ -21,6 +31,7 @@ import org.wordpress.android.R
 import org.wordpress.android.ui.compose.components.MainTopAppBar
 import org.wordpress.android.ui.compose.components.NavigationIcons
 import org.wordpress.android.ui.domains.management.composable.DomainsSearchTextField
+import org.wordpress.android.ui.domains.management.success
 
 @Composable
 fun NewDomainSearchScreen(
@@ -39,18 +50,20 @@ fun NewDomainSearchScreen(
             )
         },
         content = {
-            Column (
-                modifier = Modifier.padding(it),
-            ) {
-                val listState = rememberLazyListState()
+            Column {
+                Column(
+                    modifier = Modifier.padding(it),
+                ) {
+                    val listState = rememberLazyListState()
 
-                val elevation = animateDpAsState(
-                    targetValue = if (listState.canScrollBackward) 4.dp else 0.dp,
-                    label = "Search Input Elevation",
-                )
-                NewDomainSearchInput(
-                    elevation.value
-                )
+                    val elevation = animateDpAsState(
+                        targetValue = if (listState.canScrollBackward) 4.dp else 0.dp,
+                        label = "Search Input Elevation",
+                    )
+                    NewDomainSearchInput(elevation = elevation.value)
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                TransferDomainFooter()
             }
         }
     )
@@ -72,4 +85,53 @@ fun NewDomainSearchInput(
             onValueChange = { queryString = it }
         )
     }
+}
+
+@Composable
+fun TransferDomainFooter(
+    modifier: Modifier = Modifier
+) {
+    Surface (shadowElevation = 4.dp, modifier = Modifier.zIndex(1f)) {
+        Column(
+            modifier = modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.new_domain_search_screen_transfer_domain_description),
+                style = MaterialTheme.typography.bodyMedium,
+            )
+            OutlinedButton(
+                onClick = {},
+                shape = MaterialTheme.shapes.small.copy(CornerSize(36.dp)),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 0.dp,
+                    pressedElevation = 0.dp,
+                    disabledElevation = 0.dp,
+                    hoveredElevation = 0.dp,
+                    focusedElevation = 0.dp
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.new_domain_search_screen_transfer_domain_button),
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        color = MaterialTheme.colorScheme.success,
+                    ),
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Light mode", locale = "en")
+@Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "Small screen", device = Devices.NEXUS_5)
+@Preview(name = "Landscape orientation", device = Devices.AUTOMOTIVE_1024p)
+@Composable
+fun NewDomainSearchScreenPreview() {
+    NewDomainSearchScreen({})
 }
