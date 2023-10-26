34e60ffcb1baff77ed9ee5a55a1437df67738c59
Artyom Vlasov <artyom.vlasov@automattic.com>
2023-10-26 12:08:17 -0600

    Improve DomainsSearchTextField functions - Make it single line - Change a keyboard ime action to Search - Dismiss a keyboard when keyboard search action performed

--- WordPress/src/main/java/org/wordpress/android/ui/domains/management/composable/DomainsSearchTextField.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/management/composable/DomainsSearchTextField.kt
@@ -4,14 +4,18 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import org.wordpress.android.R
 
@@ -23,6 +27,7 @@ fun DomainsSearchTextField(
     modifier: Modifier = Modifier,
     onValueChange: (String) -> Unit
 ) {
+    val focusManager = LocalFocusManager.current
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
@@ -36,6 +41,11 @@ fun DomainsSearchTextField(
                 tint = MaterialTheme.colorScheme.outline,
             )
         },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(onSearch = { focusManager.clearFocus() }),
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
