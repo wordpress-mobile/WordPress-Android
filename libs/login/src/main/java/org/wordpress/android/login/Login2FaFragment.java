d9a1a1b04d8227c9a05689da1d527bf2008d541b
ThomazFB <thomazfcortez@gmail.com>
2023-11-01 19:26:25 -0300

    Fix checkstyle issues

--- libs/login/src/main/java/org/wordpress/android/login/Login2FaFragment.java
+++ libs/login/src/main/java/org/wordpress/android/login/Login2FaFragment.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
