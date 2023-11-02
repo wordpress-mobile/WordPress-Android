4377392f1cf4bb7031d5ce4b37c75d6c253a733b
Antonis Lilis <antonis.lilis@automattic.com>
2023-11-02 09:40:53 +0200

    Adds referrer when a campaign parameter is passed in the play store link function

--- WordPress/src/test/java/org/wordpress/android/ui/ActivityLauncherWrapperTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/ActivityLauncherWrapperTest.kt
@@ -1,8 +1,10 @@
 package org.wordpress.android.ui
 
 import android.app.Activity
+import android.app.Application
 import android.content.pm.PackageManager
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -45,16 +47,39 @@ class ActivityLauncherWrapperTest {
         verify(activity).finishAffinity()
     }
 
+    @Test
+    fun `when a campaign is passed the store url includes a referrer`() {
+        val expectedStoreUrl = "https://play.google.com/store/apps/details?id=com.jetpack.android" +
+                "&referrer=utm_source%3Dorg.wordpress.android%26utm_campaign%3Dtest_campaign"
+        val actualStoreUrl = classToTest.getPlayStoreUrl(SOURCE_PACKAGE, TARGET_PACKAGE, TEST_CAMPAIGN)
+        assertThat(actualStoreUrl).isEqualTo(expectedStoreUrl)
+    }
+
+    @Test
+    fun `when a campaign is not passed the store url does not include a referrer`() {
+        val expectedStoreUrl = "https://play.google.com/store/apps/details?id=com.jetpack.android"
+        val actualStoreUrl = classToTest.getPlayStoreUrl(SOURCE_PACKAGE, TARGET_PACKAGE)
+        assertThat(actualStoreUrl).isEqualTo(expectedStoreUrl)
+    }
 
     private fun mockAppPreinstalled(isInstalled: Boolean): Activity {
         val activity = mock<Activity>()
+        val application = mock<Application>()
         val packageManager = mock<PackageManager>()
         whenever(activity.packageManager).thenReturn(packageManager)
+        whenever(activity.application).thenReturn(application)
+        whenever(application.packageName).thenReturn("org.wordpress.android")
 
         if (isInstalled) {
             whenever(packageManager.getLaunchIntentForPackage(any())).thenReturn(mock())
         }
 
         return activity
+    }
+
+    companion object {
+        const val SOURCE_PACKAGE = "org.wordpress.android"
+        const val TARGET_PACKAGE = "com.jetpack.android"
+        const val TEST_CAMPAIGN = "test_campaign"
     }
 }
