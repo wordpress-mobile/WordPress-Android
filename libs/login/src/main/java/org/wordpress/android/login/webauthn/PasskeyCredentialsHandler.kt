b478eb3d75401b7e18c92a9a8fccfceca15d075f
ThomazFB <thomazfcortez@gmail.com>
2023-11-02 00:45:24 -0300

    Define PasskeyCredentialsHandler.kt

---
+++ libs/login/src/main/java/org/wordpress/android/login/webauthn/PasskeyCredentialsHandler.kt
@@ -0,0 +1,68 @@
+package org.wordpress.android.login.webauthn
+
+import android.content.Context
+import android.util.Base64
+import androidx.activity.result.IntentSenderRequest
+import com.google.android.gms.fido.Fido
+import com.google.android.gms.fido.common.Transport
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredential
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialDescriptor
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialRequestOptions
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialType.PUBLIC_KEY
+import org.wordpress.android.fluxc.network.rest.wpcom.auth.webauthn.WebauthnChallengeInfo
+import org.wordpress.android.fluxc.network.rest.wpcom.auth.webauthn.WebauthnCredentialResponse
+import org.wordpress.android.fluxc.store.AccountStore.FinishWebauthnChallengePayload
+
+class PasskeyCredentialsHandler(
+    private val userId: String,
+    private val challengeInfo: WebauthnChallengeInfo
+) {
+    fun createIntentSender(
+        context: Context,
+        onPasskeyRequestReady: (IntentSenderRequest) -> Unit
+    ) {
+        val options = PublicKeyCredentialRequestOptions.Builder()
+                .setRpId(challengeInfo.rpId)
+                .setAllowList(challengeInfo.allowCredentials.map(::parseToCredentialDescriptor))
+                .setChallenge(challengeInfo.challenge.decodeBase64())
+                .setTimeoutSeconds(challengeInfo.timeout.toDouble())
+                .build()
+
+        Fido.getFido2ApiClient(context)
+                .getSignPendingIntent(options)
+                .addOnSuccessListener {
+                    val intentSender = IntentSenderRequest
+                            .Builder(it.intentSender)
+                            .build()
+
+                    onPasskeyRequestReady(intentSender)
+                }
+    }
+
+    fun onCredentialsAvailable(keyCredential: PublicKeyCredential): FinishWebauthnChallengePayload {
+        return FinishWebauthnChallengePayload().apply {
+            this.mUserId = userId
+            this.mTwoStepNonce = challengeInfo.twoStepNonce
+            this.mClientData = keyCredential.toJson()
+        }
+    }
+
+    private fun parseToCredentialDescriptor(credential: WebauthnCredentialResponse) =
+            PublicKeyCredentialDescriptor(
+                    PUBLIC_KEY.toString(),
+                    credential.id.decodeBase64(),
+                    allTransports
+            )
+
+    private val allTransports = listOf(
+            Transport.USB,
+            Transport.NFC,
+            Transport.BLUETOOTH_LOW_ENERGY,
+            Transport.HYBRID,
+            Transport.INTERNAL
+    )
+
+    private fun String.decodeBase64(): ByteArray {
+        return Base64.decode(this, Base64.NO_PADDING or Base64.NO_WRAP or Base64.URL_SAFE)
+    }
+}
