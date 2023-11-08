01aa44d0c5864fcca6776cb7df655d51069236c4
Antonis Lilis <antonis.lilis@automattic.com>
2023-11-08 16:08:22 +0200

    Simplify code

--- WordPress/src/main/java/org/wordpress/android/ui/domains/management/details/DomainManagementDetailsWebViewClient.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/management/details/DomainManagementDetailsWebViewClient.kt
@@ -1,6 +1,5 @@
 package org.wordpress.android.ui.domains.management.details
 
-import android.net.Uri
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import org.wordpress.android.ui.domains.management.details.DomainManagementDetailsWebViewNavigationDelegate.toUrl
@@ -9,16 +8,15 @@ import org.wordpress.android.util.ErrorManagedWebViewClient
 class DomainManagementDetailsWebViewClient(
     private val listener: DomainManagementWebViewClientListener
 ) : ErrorManagedWebViewClient(listener) {
+    private val navigationDelegate = DomainManagementDetailsWebViewNavigationDelegate
+
     interface DomainManagementWebViewClientListener : ErrorManagedWebViewClientListener {
         fun onRedirectToExternalBrowser(url: String)
     }
 
     override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest) : Boolean {
-        if (canNavigateTo(request.url)) return false
+        if (navigationDelegate.canNavigateTo(request.url.toUrl())) return false
         listener.onRedirectToExternalBrowser(request.url.toString())
         return true
     }
-
-    private fun canNavigateTo(uri: Uri) =
-        DomainManagementDetailsWebViewNavigationDelegate.canNavigateTo(uri.toUrl())
 }
