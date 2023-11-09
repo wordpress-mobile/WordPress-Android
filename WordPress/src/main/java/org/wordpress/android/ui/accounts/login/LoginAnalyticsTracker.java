84ea8f6cacee3affe76187d0b98d3a72d5fe73f6
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-11-09 15:40:46 -0500

    Implement abstract methods for tracking events for passkey 2fa

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginAnalyticsTracker.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LoginAnalyticsTracker.java
@@ -391,4 +391,20 @@ public class LoginAnalyticsTracker implements LoginAnalyticsListener {
     @Override public void usernamePasswordScreenResumed() {
         mUnifiedLoginTracker.setStep(Step.USERNAME_PASSWORD);
     }
+
+    @Override public void trackLoginSecurityKeyFailure() {
+        AnalyticsTracker.track(AnalyticsTracker.Stat.LOGIN_SECURITY_KEY_FAILURE);
+    }
+
+    @Override public void trackLoginSecurityKeyNeeded() {
+        AnalyticsTracker.track(AnalyticsTracker.Stat.LOGIN_SECURITY_KEY_NEEDED);
+    }
+
+    @Override public void trackLoginSecurityKeySuccess() {
+        AnalyticsTracker.track(AnalyticsTracker.Stat.LOGIN_SECURITY_KEY_SUCCESS);
+    }
+
+    @Override public void trackUseSecurityKeyClicked() {
+        AnalyticsTracker.track(AnalyticsTracker.Stat.LOGIN_SECURITY_KEY_CLICKED);
+    }
 }
