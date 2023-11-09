b389d9d7a9d3672516bba72f4c9dca6d84fd0fde
Thomas Horta <thomas.horta@automattic.com>
2023-11-09 18:01:26 -0300

    Add bloganuary attribution in Enum

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloggingprompts/BloggingPromptAttribution.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloggingprompts/BloggingPromptAttribution.kt
@@ -2,12 +2,13 @@ package org.wordpress.android.ui.mysite.cards.dashboard.bloggingprompts
 
 enum class BloggingPromptAttribution {
     NO_ATTRIBUTION,
-    DAY_ONE;
+    DAY_ONE,
+    BLOGANUARY;
 
     companion object {
         fun fromString(value: String): BloggingPromptAttribution = when (value) {
             "dayone" -> DAY_ONE
-
+            "bloganuary" -> BLOGANUARY
             else -> NO_ATTRIBUTION
         }
     }
