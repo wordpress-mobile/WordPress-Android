109af598ffe0a74d31894838a5291cf6fd3e6953
Matthew Kevins <mkevins@users.noreply.github.com>
2023-11-09 10:52:52 +1000

    Remove star import
    Co-authored-by: Antonis Lilis <antonis.lilis@automattic.com>

--- WordPress/src/main/java/org/wordpress/android/ui/domains/management/DomainManagementActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/management/DomainManagementActivity.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.wordpress.android.ui.ActivityLauncher
-import org.wordpress.android.ui.domains.management.DomainManagementViewModel.*
+import org.wordpress.android.ui.domains.management.DomainManagementViewModel.ActionEvent
 import org.wordpress.android.util.extensions.setContent
 
 @AndroidEntryPoint
