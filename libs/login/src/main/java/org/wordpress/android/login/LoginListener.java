c727e3007048028b830bd4a561bb38c8a86e855a
ThomazFB <thomazfcortez@gmail.com>
2023-11-09 18:49:24 -0300

    Fix checkstyle

--- libs/login/src/main/java/org/wordpress/android/login/LoginListener.java
+++ libs/login/src/main/java/org/wordpress/android/login/LoginListener.java
@@ -48,7 +48,8 @@ public interface LoginListener {
     void forgotPassword(String url);
     void useMagicLinkInstead(String email, boolean verifyEmail);
     void needs2fa(String email, String password);
-    void needs2fa(String email, String password, String userId, String webauthnNonce, String nonceAuthenticator, String nonceBackup, String noncePush);
+    void needs2fa(String email, String password, String userId, String webauthnNonce,
+                  String nonceAuthenticator, String nonceBackup, String noncePush);
     void needs2faSocial(String email, String userId, String nonceAuthenticator, String nonceBackup, String nonceSms);
     void needs2faSocialConnect(String email, String password, String idToken, String service);
     void loggedInViaPassword(ArrayList<Integer> oldSitesIds);
