cc1084fddcd78649e0ce3684c7cd1d53697b9269
Antonis Lilis <antonis.lilis@automattic.com>
2023-11-10 20:09:33 +0200

    Moves domain management events together

--- libs/analytics/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
+++ libs/analytics/src/main/java/org/wordpress/android/analytics/AnalyticsTrackerNosara.java
@@ -1927,12 +1927,6 @@ public class AnalyticsTrackerNosara extends Tracker {
                 return "domain_credit_suggestion_queried";
             case DOMAIN_CREDIT_NAME_SELECTED:
                 return "domain_credit_name_selected";
-            case DOMAIN_MANAGEMENT_MY_DOMAINS_SCREEN_SHOWN:
-                return "domain_management_my_domains_screen_shown";
-            case DOMAIN_MANAGEMENT_MY_DOMAINS_SCREEN_DOMAIN_TAPPED:
-                return "domain_management_my_domains_screen_domain_tapped";
-            case DOMAIN_MANAGEMENT_MY_DOMAINS_SCREEN_ADD_DOMAIN_TAPPED:
-                return "domain_management_my_domains_screen_add_domain_tapped";
             case DOMAINS_DASHBOARD_VIEWED:
                 return "domains_dashboard_viewed";
             case DOMAINS_DASHBOARD_GET_DOMAIN_TAPPED:
@@ -2641,6 +2635,12 @@ public class AnalyticsTrackerNosara extends Tracker {
                 return "quick_link_item_tapped";
             case POST_LIST_CREATE_POST_TAPPED:
                 return "post_list_create_post_tapped";
+            case DOMAIN_MANAGEMENT_MY_DOMAINS_SCREEN_SHOWN:
+                return "domain_management_my_domains_screen_shown";
+            case DOMAIN_MANAGEMENT_MY_DOMAINS_SCREEN_DOMAIN_TAPPED:
+                return "domain_management_my_domains_screen_domain_tapped";
+            case DOMAIN_MANAGEMENT_MY_DOMAINS_SCREEN_ADD_DOMAIN_TAPPED:
+                return "domain_management_my_domains_screen_add_domain_tapped";
             case DOMAIN_MANAGEMENT_PURCHASE_DOMAIN_SCREEN_SHOWN:
                 return "domain_management_purchase_domain_screen_shown";
             case DOMAIN_MANAGEMENT_PURCHASE_DOMAIN_SCREEN_NEW_DOMAIN_TAPPED:
