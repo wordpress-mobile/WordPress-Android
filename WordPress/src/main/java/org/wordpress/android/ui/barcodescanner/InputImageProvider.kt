9318c3f0df75e17f74fe62c42223fb13346d11b0
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2023-11-13 13:09:43 -0500

    The image provider for accessed the scanned item

---
+++ WordPress/src/main/java/org/wordpress/android/ui/barcodescanner/InputImageProvider.kt
@@ -0,0 +1,14 @@
+package org.wordpress.android.ui.barcodescanner
+
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import javax.inject.Inject
+interface InputImageProvider {
+    fun provideImage(imageProxy: ImageProxy): InputImage
+}
+class MediaImageProvider @Inject constructor() : InputImageProvider {
+    @androidx.camera.core.ExperimentalGetImage
+    override fun provideImage(imageProxy: ImageProxy): InputImage {
+        return InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+    }
+}
