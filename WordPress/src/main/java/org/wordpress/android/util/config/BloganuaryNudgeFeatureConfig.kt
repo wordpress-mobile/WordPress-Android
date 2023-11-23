2a901c828b17e18522a5c0018541fff94ab75eb7
Thomas Horta <thomas.horta@automattic.com>
2023-11-23 17:22:04 -0300

    Create Bloganuary Nudge FF (default OFF)

---
+++ WordPress/src/main/java/org/wordpress/android/util/config/BloganuaryNudgeFeatureConfig.kt
@@ -0,0 +1,20 @@
+package org.wordpress.android.util.config
+
+import org.wordpress.android.BuildConfig
+import org.wordpress.android.annotation.Feature
+import javax.inject.Inject
+
+private const val BLOGANUARY_NUDGE_REMOTE_FIELD = "bloganuary_dashboard_nudge"
+
+@Feature(BLOGANUARY_NUDGE_REMOTE_FIELD, false)
+class BloganuaryNudgeFeatureConfig @Inject constructor(
+    appConfig: AppConfig
+) : FeatureConfig(
+    appConfig,
+    BuildConfig.BLOGANUARY_DASHBOARD_NUDGE,
+    BLOGANUARY_NUDGE_REMOTE_FIELD
+) {
+    override fun isEnabled(): Boolean {
+        return super.isEnabled() && BuildConfig.IS_JETPACK_APP
+    }
+}
