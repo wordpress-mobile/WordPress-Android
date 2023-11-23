243e85f9beb65184289278ad23036ef09bf8cd5c
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-11-23 19:58:10 -0300

    Fix DropdownMenuItemData parameters

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/DropdownMenuItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/DropdownMenuItem.kt
@@ -11,40 +11,50 @@ fun DropdownMenuItem(itemData: DropdownMenuItemData) {
 sealed class DropdownMenuItemData(
     open val text: String,
     open val isDefault: Boolean,
+    open val id: String,
+    open val onClick: (String) -> Unit,
 ) {
     /**
      * @param onClick callback that returns the defined id
      */
     data class Text(
-        val id: String,
+        override val id: String,
         override val text: String,
         override val isDefault: Boolean = false,
-        val onClick: (String) -> Unit,
+        override val onClick: (String) -> Unit,
     ) : DropdownMenuItemData(
         text = text,
         isDefault = isDefault,
+        id = id,
+        onClick = onClick,
     )
 
     /**
      * @param onClick callback that returns the defined id
      */
     data class TextAndIcon(
-        val id: String,
+        override val id: String,
         override val text: String,
         @DrawableRes val iconRes: Int,
         override val isDefault: Boolean = false,
-        val onClick: (String) -> Unit,
+        override val onClick: (String) -> Unit,
     ) : DropdownMenuItemData(
         text = text,
         isDefault = isDefault,
+        id = id,
+        onClick = onClick,
     )
 
     data class SubMenu(
+        override val id: String,
         override val text: String,
         override val isDefault: Boolean = false,
         val items: List<DropdownMenuItemData>,
+        override val onClick: (String) -> Unit,
     ) : DropdownMenuItemData(
         text = text,
         isDefault = isDefault,
+        id = id,
+        onClick = onClick,
     )
 }
