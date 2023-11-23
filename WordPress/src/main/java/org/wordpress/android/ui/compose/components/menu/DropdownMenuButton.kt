d57b65673d80ea03b7a7435928fd4b9c03f04d7c
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-11-23 17:10:25 -0300

    Add Compose Preview to DropdownMenuButton

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/DropdownMenuButton.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/DropdownMenuButton.kt
@@ -1,7 +1,21 @@
 package org.wordpress.android.ui.compose.components.menu
 
+import android.content.res.Configuration
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.wordpress.android.ui.compose.components.buttons.PrimaryButton
+import org.wordpress.android.ui.compose.theme.AppTheme
 
 @Composable
 fun DropdownMenuButton() {
+}
+
+
+@Preview
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PrimaryButtonPreview() {
+    AppTheme {
+        PrimaryButton(text = "Continue", onClick = {})
+    }
 }
