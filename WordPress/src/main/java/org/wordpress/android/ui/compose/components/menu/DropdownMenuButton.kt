1b7b10100e7b3294bbc544773b033e5b98fc8284
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-11-23 19:18:10 -0300

    Implement DropdownMenuButton

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/DropdownMenuButton.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/DropdownMenuButton.kt
@@ -1,21 +1,131 @@
 package org.wordpress.android.ui.compose.components.menu
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import org.wordpress.android.ui.compose.components.buttons.PrimaryButton
+import androidx.compose.ui.unit.dp
+import org.wordpress.android.R
+import org.wordpress.android.ui.compose.components.menu.DropdownMenuItemData.SubMenu
+import org.wordpress.android.ui.compose.components.menu.DropdownMenuItemData.Text
+import org.wordpress.android.ui.compose.components.menu.DropdownMenuItemData.TextAndIcon
 import org.wordpress.android.ui.compose.theme.AppTheme
+import org.wordpress.android.ui.compose.unit.FontSize
+import org.wordpress.android.ui.compose.unit.Margin
 
 @Composable
-fun DropdownMenuButton() {
+fun DropdownMenuButton(
+    selectedItem: DropdownMenuItemData,
+    onClick: () -> Unit,
+) {
+    if (selectedItem is SubMenu) {
+        throw IllegalArgumentException("DropdownMenuButton selected item cannot be a SubMenu")
+    }
+    Button(
+        modifier = Modifier.defaultMinSize(minHeight = 40.dp),
+        onClick = onClick,
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = 0.dp,
+            pressedElevation = 0.dp,
+        ),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = MaterialTheme.colors.onPrimary,
+            backgroundColor = MaterialTheme.colors.onSurface,
+        ),
+        shape = RoundedCornerShape(50),
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(Margin.Medium.value),
+        ) {
+            if (selectedItem is TextAndIcon) {
+                Icon(
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    painter = painterResource(id = selectedItem.iconRes),
+                    contentDescription = null,
+                )
+            }
+            Text(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .weight(
+                        weight = 1f,
+                        fill = false,
+                    ),
+                text = selectedItem.text,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                fontSize = FontSize.Medium.value,
+            )
+            Icon(
+                modifier = Modifier.align(Alignment.CenterVertically),
+                painter = painterResource(id = R.drawable.ic_chevron_down_white_16dp),
+                contentDescription = null,
+                tint = MaterialTheme.colors.onPrimary,
+            )
+        }
+    }
 }
-
 
 @Preview
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun PrimaryButtonPreview() {
+private fun DropdownMenuButtonText() {
     AppTheme {
-        PrimaryButton(text = "Continue", onClick = {})
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            
+        ) {
+            DropdownMenuButton(
+                selectedItem = Text(
+                    id = "text1",
+                    text = "Text only",
+                    isDefault = true,
+                    onClick = {},
+                ),
+                onClick = {}
+            )
+            DropdownMenuButton(
+                selectedItem = TextAndIcon(
+                    id = "textAndIcon1",
+                    text = "Text and Icon",
+                    iconRes = R.drawable.ic_jetpack_logo_white_24dp,
+                    isDefault = true,
+                    onClick = {},
+                ),
+                onClick = {}
+            )
+            DropdownMenuButton(
+                selectedItem = Text(
+                    id = "textAndIcon1",
+                    text = "Text type with a really long text as the button label",
+                    isDefault = true,
+                    onClick = {},
+                ),
+                onClick = {}
+            )
+            DropdownMenuButton(
+                selectedItem = TextAndIcon(
+                    id = "textAndIcon1",
+                    text = "Text and Icon type with a really long text as the button label",
+                    iconRes = R.drawable.ic_jetpack_logo_white_24dp,
+                    isDefault = true,
+                    onClick = {},
+                ),
+                onClick = {}
+            )
+        }
     }
 }
