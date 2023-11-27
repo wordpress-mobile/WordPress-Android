c46301394905bd6fdc4c20833bdc023f028d353c
Thomas Horta <thomas.horta@automattic.com>
2023-11-27 18:31:34 -0300

    Use correct ViewCompositionStrategy for Bloganuary card

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloganuary/BloganuaryNudgeCardViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloganuary/BloganuaryNudgeCardViewHolder.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.ui.mysite.cards.dashboard.bloganuary
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import org.wordpress.android.databinding.BloganuaryNudgeCardBinding
 import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.BloganuaryNudgeCardModel
@@ -11,8 +12,10 @@ import org.wordpress.android.util.extensions.viewBinding
 
 class BloganuaryNudgeCardViewHolder(parent: ViewGroup) :
     MySiteCardAndItemViewHolder<BloganuaryNudgeCardBinding>(parent.viewBinding(BloganuaryNudgeCardBinding::inflate)) {
-    fun bind(cardModel: BloganuaryNudgeCardModel) = with(binding) {
-        bloganuaryNudgeCard.setContent {
+    fun bind(cardModel: BloganuaryNudgeCardModel) = with(binding.bloganuaryNudgeCard) {
+        // Dispose of the Composition when the view's LifecycleOwner is destroyed
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnDetachedFromWindowOrReleasedFromPool)
+        setContent {
             AppTheme {
                 BloganuaryNudgeCard(
                     model = cardModel,
