8be03dea29e51f582c0fc7a817ca1d4812b059b0
Thomas Horta <thomas.horta@automattic.com>
2023-11-27 16:06:50 -0300

    Create Bloganuary Nudge card UI

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloganuary/BloganuaryNudgeCard.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloganuary/BloganuaryNudgeCard.kt
@@ -1,19 +1,32 @@
 package org.wordpress.android.ui.mysite.cards.dashboard.bloganuary
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.components.card.UnelevatedCard
+import org.wordpress.android.ui.compose.styles.DashboardCardTypography
+import org.wordpress.android.ui.compose.theme.AppTheme
+import org.wordpress.android.ui.compose.utils.uiStringText
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.BloganuaryNudgeCardModel
 import org.wordpress.android.ui.mysite.cards.compose.MySiteCardToolbar
 import org.wordpress.android.ui.mysite.cards.compose.MySiteCardToolbarContextMenuItem
+import org.wordpress.android.ui.utils.ListItemInteraction
+import org.wordpress.android.ui.utils.UiString
 
 @Composable
 fun BloganuaryNudgeCard(
@@ -25,6 +38,36 @@ fun BloganuaryNudgeCard(
     ) {
         Column {
             CardToolbar(model)
+
+            Spacer(Modifier.height(8.dp))
+
+            Text(
+                text = uiStringText(model.title),
+                style = DashboardCardTypography.subTitle,
+                modifier = Modifier.padding(horizontal = 16.dp),
+            )
+
+            Spacer(Modifier.height(4.dp))
+
+            Text(
+                text = uiStringText(model.text),
+                style = DashboardCardTypography.detailText,
+                modifier = Modifier.padding(horizontal = 16.dp),
+            )
+
+            Spacer(Modifier.height(4.dp))
+
+            TextButton(
+                onClick = { model.onLearnMoreClick.click() },
+                modifier = Modifier.padding(horizontal = 4.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.bloganuary_dashboard_nudge_learn_more),
+                    style = DashboardCardTypography.footerCTA,
+                )
+            }
+
+            Spacer(Modifier.height(8.dp))
         }
     }
 }
@@ -48,5 +91,26 @@ private fun CardToolbar(
             modifier = Modifier.size(24.dp),
             tint = MaterialTheme.colors.onSurface
         )
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(name = "Dark Mode", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun BloganuaryNudgeCardPreview() {
+    AppTheme {
+        Surface(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            BloganuaryNudgeCard(
+                model = BloganuaryNudgeCardModel(
+                    UiString.UiStringRes(R.string.bloganuary_dashboard_nudge_title),
+                    UiString.UiStringRes(R.string.bloganuary_dashboard_nudge_text),
+                    onLearnMoreClick = ListItemInteraction.create { },
+                    onMoreMenuClick = ListItemInteraction.create { },
+                    onHideMenuItemClick = ListItemInteraction.create { },
+                )
+            )
+        }
     }
 }
