3807b6c96d12aa0997d9656c64f0989714653940
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-11-28 13:09:33 +0100

    Add slug field to demo page fetched data

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/ThemeCoroutineStore.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/ThemeCoroutineStore.kt
@@ -9,8 +9,30 @@ class ThemeCoroutineStore @Inject constructor(
     private val coroutineEngine: CoroutineEngine,
     private val themeRestClient: ThemeCoroutineRestClient,
 ) {
-    fun fetchDemoThemePages(demoThemeUrl: String) =
-        coroutineEngine.launch(T.PLUGINS, this, "Fetching demo pages for theme $demoThemeUrl") {
-           themeRestClient.fetchThemeDemoPages(demoThemeUrl)
+    suspend fun fetchDemoThemePages(demoThemeUrl: String): List<DemoPage> =
+        coroutineEngine.withDefaultContext(
+            T.API,
+            this,
+            "Fetching demo pages for theme $demoThemeUrl"
+        ) {
+            val response = themeRestClient.fetchThemeDemoPages(demoThemeUrl)
+            when {
+                response.isError || response.result == null -> emptyList()
+                else -> response.result
+                    .toList()
+                    .map {
+                        DemoPage(
+                            link = it.link,
+                            title = it.title.rendered,
+                            slug = it.slug
+                        )
+                    }
+            }
         }
+
+    data class DemoPage(
+        val link: String,
+        val title: String,
+        val slug: String
+    )
 }
