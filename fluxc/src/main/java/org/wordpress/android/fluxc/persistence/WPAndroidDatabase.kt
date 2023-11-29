43e345a647fb712f112ecff8b639a6ce120d12f1
Thomas Horta <thomas.horta@automattic.com>
2023-11-29 17:14:45 -0300

    Remove Migration and fallback to destructive migration
    Since UPSERT is not supported by all Android API versions and the actual data
    inside the Prompts table changed along with the schema, it will be best to avoid
    the effort of maintaing stale data and just rely on Room's fallback to
    destructive migration, which will drop the table (and data) and create a new one
    with the correct schema.
    
    This shouldn't have many downsides as we fetch new data as soon as the app opens
    and the user reaches the Dashboard. The only possible caveat is for users that
    have reminders setup with Prompts enabled, that might see a notification without
    Blogging Prompts if they don't open the app after updating it.

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WPAndroidDatabase.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/WPAndroidDatabase.kt
@@ -105,7 +105,6 @@ abstract class WPAndroidDatabase : RoomDatabase() {
                 .addMigrations(MIGRATION_18_19)
                 .addMigrations(MIGRATION_19_20)
                 .addMigrations(MIGRATION_20_21)
-                .addMigrations(MIGRATION_21_22)
                 .build()
 
         val MIGRATION_1_2 = object : Migration(1, 2) {
@@ -308,43 +307,6 @@ abstract class WPAndroidDatabase : RoomDatabase() {
                     execSQL(
                         "ALTER TABLE `BlazeCampaigns` ADD COLUMN `targetUrn` TEXT"
                     )
-                }
-            }
-        }
-
-        val MIGRATION_21_22 = object : Migration(21, 22) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                // migrate old data to new table schema
-                database.apply {
-                    execSQL(
-                        "CREATE TABLE IF NOT EXISTS `BloggingPrompts_new` (" +
-                            "`id` INTEGER NOT NULL," +
-                            "`siteLocalId` INTEGER NOT NULL," +
-                            "`text` TEXT NOT NULL," +
-                            "`date` TEXT NOT NULL," +
-                            "`isAnswered` INTEGER NOT NULL," +
-                            "`respondentsCount` INTEGER NOT NULL," +
-                            "`attribution` TEXT NOT NULL," +
-                            "`respondentsAvatars` TEXT NOT NULL," +
-                            "`answeredLink` TEXT NOT NULL," +
-                            "PRIMARY KEY(`date`)" +
-                            ")"
-                    )
-
-                    val tagPrefix = "https://wordpress.com/tag/dailyprompt-"
-                    execSQL(
-                        "INSERT INTO BloggingPrompts_new (" +
-                            "id, siteLocalId, text, date, isAnswered, " +
-                            "respondentsCount, attribution, respondentsAvatars, answeredLink) " +
-                            "SELECT id, siteLocalId, text, date, isAnswered, " +
-                            "respondentsCount, attribution, respondentsAvatars, " +
-                            "'$tagPrefix' || id FROM BloggingPrompts " +
-                            "WHERE true " +
-                            "ON CONFLICT DO NOTHING"
-                    )
-
-                    execSQL("DROP TABLE `BloggingPrompts`")
-                    execSQL("ALTER TABLE `BloggingPrompts_new` RENAME TO `BloggingPrompts`")
                 }
             }
         }
