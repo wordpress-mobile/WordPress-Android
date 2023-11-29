1c9976b2699bd20c7648719d53c99796b1e1ddb7
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-11-29 13:08:20 -0300

    Fix detekt

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/DropdownMenuItemList.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/DropdownMenuItemList.kt
@@ -14,7 +14,6 @@ import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.wordpress.android.R
