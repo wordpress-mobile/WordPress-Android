10cfc9580a3291038fa8772a143b851f15ddd3c8
Thomas Horta <thomas.horta@automattic.com>
2023-11-30 11:33:41 -0300

    Make fragment fullscreen

--- WordPress/src/main/java/org/wordpress/android/ui/bloganuary/learnmore/BloganuaryNudgeLearnMoreOverlayFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/bloganuary/learnmore/BloganuaryNudgeLearnMoreOverlayFragment.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.bloganuary.learnmore
 
+import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,9 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.wordpress.android.ui.compose.theme.AppTheme
+import org.wordpress.android.util.extensions.fillScreen
 
 @AndroidEntryPoint
 class BloganuaryNudgeLearnMoreOverlayFragment : BottomSheetDialogFragment() {
@@ -17,6 +20,12 @@ class BloganuaryNudgeLearnMoreOverlayFragment : BottomSheetDialogFragment() {
 
     private val isPromptsEnabled: Boolean by lazy {
         arguments?.getBoolean(ARG_IS_PROMPTS_ENABLED) ?: false
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            (this as? BottomSheetDialog)?.fillScreen(isDraggable = true)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
