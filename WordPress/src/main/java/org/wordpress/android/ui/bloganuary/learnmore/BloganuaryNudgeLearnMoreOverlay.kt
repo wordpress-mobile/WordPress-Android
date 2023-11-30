49c4ea82d84ebde147963292aca521bc3cc8b513
Thomas Horta <thomas.horta@automattic.com>
2023-11-30 15:47:20 -0300

    Update Learn More modal UI to match new specs

--- WordPress/src/main/java/org/wordpress/android/ui/bloganuary/learnmore/BloganuaryNudgeLearnMoreOverlay.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/bloganuary/learnmore/BloganuaryNudgeLearnMoreOverlay.kt
@@ -2,13 +2,22 @@ package org.wordpress.android.ui.bloganuary.learnmore
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -25,11 +34,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.ui.compose.unit.Margin
@@ -55,24 +66,74 @@ fun BloganuaryNudgeLearnMoreOverlay(
             )
         }
 
+        Spacer(
+            Modifier
+                .requiredHeightIn(
+                    min = Margin.Medium.value,
+                    max = Margin.ExtraExtraMediumLarge.value
+                )
+        )
+
         Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .weight(1f)
-                .verticalScroll(rememberScrollState())
                 .padding(horizontal = Margin.ExtraMediumLarge.value)
+                .padding(bottom = Margin.ExtraLarge.value)
         ) {
             Image(
                 painter = painterResource(R.drawable.logo_bloganuary),
+                colorFilter = ColorFilter.tint(Material2Theme.colors.onSurface),
+                modifier = Modifier.width(140.dp),
+                contentScale = ContentScale.Inside,
                 contentDescription = stringResource(
                     R.string.bloganuary_dashboard_nudge_overlay_icon_content_description
-                ),
-                colorFilter = ColorFilter.tint(Material2Theme.colors.onSurface)
+                )
             )
 
-            OverlayContent(
-                modifier = Modifier.weight(1f)
+            Spacer(Modifier.height(Margin.ExtraMediumLarge.value))
+
+            // Title
+            Text(
+                stringResource(R.string.bloganuary_dashboard_nudge_overlay_title),
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
             )
 
+            Spacer(Modifier.height(Margin.ExtraExtraMediumLarge.value))
+
+            // Bullet points
+            Column(
+                verticalArrangement = Arrangement.spacedBy(Margin.ExtraMediumLarge.value),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+                    .padding(horizontal = Margin.ExtraMediumLarge.value),
+            ) {
+                OverlayContentItem(
+                    iconRes = R.drawable.ic_dayone_24dp,
+                    textRes = R.string.bloganuary_dashboard_nudge_overlay_text_one,
+                )
+
+                OverlayContentItem(
+                    iconRes = R.drawable.ic_dayone_24dp,
+                    textRes = R.string.bloganuary_dashboard_nudge_overlay_text_two,
+                )
+
+                OverlayContentItem(
+                    iconRes = R.drawable.ic_dayone_24dp,
+                    textRes = R.string.bloganuary_dashboard_nudge_overlay_text_three,
+                )
+            }
+
+            Spacer(
+                Modifier
+                    .heightIn(min = Margin.ExtraLarge.value)
+                    .weight(1f)
+            )
+
+            // Note
             Text(
                 uiStringText(model.noteText),
                 modifier = Modifier.align(Alignment.CenterHorizontally),
@@ -81,8 +142,6 @@ fun BloganuaryNudgeLearnMoreOverlay(
                 color = LocalContentColor.current.copy(alpha = ContentAlpha.medium),
             )
         }
-
-        Spacer(Modifier.height(Margin.ExtraLarge.value))
 
         Divider()
 
@@ -104,30 +163,42 @@ fun BloganuaryNudgeLearnMoreOverlay(
 }
 
 @Composable
-private fun OverlayContent(
+private fun OverlayContentItem(
+    iconRes: Int,
+    textRes: Int,
     modifier: Modifier = Modifier,
 ) {
-    Column(
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier,
     ) {
-        Spacer(Modifier.weight(0.1f))
+        Box(
+            modifier = Modifier
+                .size(48.dp)
+                .background(
+                    color = Material2Theme.colors.onSurface,
+                    shape = CircleShape,
+                ),
+        ) {
+            Image(
+                painter = painterResource(iconRes),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Material2Theme.colors.surface),
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(Alignment.Center)
+            )
+        }
+
+        Spacer(Modifier.width(Margin.ExtraLarge.value))
 
         Text(
-            stringResource(R.string.bloganuary_dashboard_nudge_overlay_title),
-            style = MaterialTheme.typography.headlineMedium,
-            fontWeight = FontWeight.Bold
+            stringResource(textRes),
+            style = MaterialTheme.typography.titleMedium,
         )
-
-        Spacer(Modifier.height(Margin.ExtraMediumLarge.value))
-
-        Text(
-            stringResource(R.string.bloganuary_dashboard_nudge_overlay_text),
-            style = MaterialTheme.typography.bodyMedium,
-        )
-
-        Spacer(Modifier.weight(0.3f))
     }
 }
+
 
 @Preview(name = "Light Mode")
 @Preview(name = "Dark Mode", uiMode = UI_MODE_NIGHT_YES)
