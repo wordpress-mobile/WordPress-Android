d72e399e25a251d4b3a7f2a29fc32263f8996524
Thomas Horta <thomas.horta@automattic.com>
2023-11-30 18:04:17 -0300

    Add new methods to BloganuaryNudgeAnalyticsTracker

--- WordPress/src/main/java/org/wordpress/android/ui/bloganuary/BloganuaryNudgeAnalyticsTracker.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/bloganuary/BloganuaryNudgeAnalyticsTracker.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.ui.bloganuary
 
 import org.wordpress.android.analytics.AnalyticsTracker.Stat
+import org.wordpress.android.ui.bloganuary.learnmore.BloganuaryNudgeLearnMoreOverlayAction
 import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 import javax.inject.Inject
@@ -11,7 +12,7 @@ class BloganuaryNudgeAnalyticsTracker @Inject constructor(
 ) {
     fun trackMySiteCardLearnMoreTapped(isPromptsEnabled: Boolean) = analyticsTracker.track(
         Stat.BLOGANUARY_NUDGE_MY_SITE_CARD_LEARN_MORE_TAPPED,
-        mapOf("prompts_enabled" to isPromptsEnabled.toString())
+        mapOf(PROPERTY_PROMPTS_ENABLED to isPromptsEnabled.toString())
     )
 
     fun trackMySiteCardMoreMenuTapped() = cardsTracker.trackCardMoreMenuClicked(
@@ -24,7 +25,26 @@ class BloganuaryNudgeAnalyticsTracker @Inject constructor(
             item.label
         )
 
+    fun trackLearnMoreOverlayShown(isPromptsEnabled: Boolean) = analyticsTracker.track(
+        Stat.BLOGANUARY_NUDGE_LEARN_MORE_MODAL_SHOWN,
+        mapOf(PROPERTY_PROMPTS_ENABLED to isPromptsEnabled.toString())
+    )
+
+    fun trackLearnMoreOverlayDismissed() = analyticsTracker.track(
+        Stat.BLOGANUARY_NUDGE_LEARN_MORE_MODAL_DISMISSED
+    )
+
+    fun trackLearnMoreOverlayActionTapped(action: BloganuaryNudgeLearnMoreOverlayAction) = analyticsTracker.track(
+        Stat.BLOGANUARY_NUDGE_LEARN_MORE_MODAL_ACTION_TAPPED,
+        mapOf(PROPERTY_ACTION to action.analyticsLabel)
+    )
+
     enum class BloganuaryNudgeCardMenuItem(val label: String) {
         HIDE_THIS("hide_this"),
+    }
+
+    companion object {
+        private const val PROPERTY_PROMPTS_ENABLED = "prompts_enabled"
+        private const val PROPERTY_ACTION = "action"
     }
 }
