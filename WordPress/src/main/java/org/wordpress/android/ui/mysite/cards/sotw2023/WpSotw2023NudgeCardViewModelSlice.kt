3445ff3e414dc83e6b32598c88c7d6c5a595af39
Thomas Horta <thomas.horta@automattic.com>
2023-12-01 17:11:43 -0300

    Update isEligible to target only users with EN language set

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/sotw2023/WpSotw2023NudgeCardViewModelSlice.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/sotw2023/WpSotw2023NudgeCardViewModelSlice.kt
@@ -11,6 +11,7 @@ import org.wordpress.android.ui.prefs.AppPrefsWrapper
 import org.wordpress.android.ui.utils.ListItemInteraction
 import org.wordpress.android.ui.utils.UiString.UiStringRes
 import org.wordpress.android.util.DateTimeUtilsWrapper
+import org.wordpress.android.util.LocaleManagerWrapper
 import org.wordpress.android.util.config.WpSotw2023NudgeFeatureConfig
 import org.wordpress.android.viewmodel.Event
 import java.time.Instant
@@ -20,6 +21,7 @@ class WpSotw2023NudgeCardViewModelSlice @Inject constructor(
     private val featureConfig: WpSotw2023NudgeFeatureConfig,
     private val appPrefsWrapper: AppPrefsWrapper,
     private val dateTimeUtilsWrapper: DateTimeUtilsWrapper,
+    private val localeManagerWrapper: LocaleManagerWrapper,
 ) {
     private val _onNavigation = MutableLiveData<Event<SiteNavigationAction>>()
     val onNavigation = _onNavigation as LiveData<Event<SiteNavigationAction>>
@@ -57,14 +59,19 @@ class WpSotw2023NudgeCardViewModelSlice @Inject constructor(
         val now = dateTimeUtilsWrapper.getInstantNow()
         val isDateEligible = now.isAfter(eventTime)
 
+        val currentLanguage = localeManagerWrapper.getLanguage()
+        val isLanguageEligible = currentLanguage.startsWith(TARGET_LANGUAGE, ignoreCase = true)
+
         return featureConfig.isEnabled() &&
                 !appPrefsWrapper.getShouldHideSotw2023NudgeCard() &&
-                isDateEligible
+                isDateEligible &&
+                isLanguageEligible
     }
 
     companion object {
         private const val URL = "https://wordpress.org/state-of-the-word/" +
                 "?utm_source=mobile&utm_medium=appnudge&utm_campaign=sotw2023"
         private const val EVENT_DATE = "2023-12-11T15:00:00.00Z"
+        private const val TARGET_LANGUAGE = "en"
     }
 }
