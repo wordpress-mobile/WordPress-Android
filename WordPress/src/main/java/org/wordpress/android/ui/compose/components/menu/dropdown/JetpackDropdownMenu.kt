d43ea0e01bb67da85bbe94f19ab8c9a7771089e2
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-12-12 16:30:51 -0300

    Fix dark mode divider color in JetpackDropdownMenu

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/dropdown/JetpackDropdownMenu.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/dropdown/JetpackDropdownMenu.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.saket.cascade.CascadeDropdownMenu
@@ -47,7 +46,9 @@ fun JetpackDropdownMenu(menuItems: List<MenuElementData>, defaultItem: MenuEleme
                     is MenuElementData.Item -> Item(element, onMenuItemClick)
                 }
                 if (element.hasDivider) {
-                    Divider()
+                    Divider(
+                        color = itemDividerColor(),
+                    )
                 }
             }
         }
--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/dropdown/MenuColors.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/menu/dropdown/MenuColors.kt
@@ -1,11 +1,12 @@
 package org.wordpress.android.ui.compose.components.menu.dropdown
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import org.wordpress.android.ui.compose.theme.AppColor
 
 @Composable
-fun isLightTheme(): Boolean = androidx.compose.material.MaterialTheme.colors.isLight
+fun isLightTheme(): Boolean = MaterialTheme.colors.isLight
 
 @Composable
 fun itemContentColor(): Color = if (isLightTheme()) {
@@ -19,4 +20,11 @@ fun itemBackgroundColor(): Color = if (isLightTheme()) {
     AppColor.White
 } else {
     AppColor.DarkGray90
+}
+
+@Composable
+fun itemDividerColor(): Color = if (isLightTheme()) {
+    MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
+} else {
+    AppColor.Gray50
 }
