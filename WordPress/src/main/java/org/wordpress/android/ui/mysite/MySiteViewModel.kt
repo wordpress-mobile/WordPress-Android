4e1e44d600433545356b226568ac9b0927143a26
Ravi <ravi.program@gmail.com>
2023-12-13 13:37:23 +1100

    Update MySiteViewModel.kt

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/MySiteViewModel.kt
@@ -65,7 +65,6 @@ import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker
 import org.wordpress.android.ui.mysite.cards.dashboard.activity.ActivityLogCardViewModelSlice
 import org.wordpress.android.ui.mysite.cards.dashboard.bloganuary.BloganuaryNudgeCardViewModelSlice
 import org.wordpress.android.ui.mysite.cards.dashboard.bloggingprompts.BloggingPromptCardViewModelSlice
-import org.wordpress.android.ui.mysite.cards.dashboard.domaintransfer.DomainTransferCardViewModel
 import org.wordpress.android.ui.mysite.cards.dashboard.pages.PagesCardViewModelSlice
 import org.wordpress.android.ui.mysite.cards.dashboard.plans.PlansCardUtils
 import org.wordpress.android.ui.mysite.cards.dashboard.posts.PostsCardViewModelSlice
@@ -156,7 +155,6 @@ class MySiteViewModel @Inject constructor(
     private val jetpackFeatureRemovalPhaseHelper: JetpackFeatureRemovalPhaseHelper,
     private val wpJetpackIndividualPluginHelper: WPJetpackIndividualPluginHelper,
     private val blazeCardViewModelSlice: BlazeCardViewModelSlice,
-    private val domainTransferCardViewModel: DomainTransferCardViewModel,
     private val pagesCardViewModelSlice: PagesCardViewModelSlice,
     private val todaysStatsViewModelSlice: TodaysStatsViewModelSlice,
     private val postsCardViewModelSlice: PostsCardViewModelSlice,
@@ -224,7 +222,6 @@ class MySiteViewModel @Inject constructor(
         siteStoriesHandler.onNavigation,
         blazeCardViewModelSlice.onNavigation,
         pagesCardViewModelSlice.onNavigation,
-        domainTransferCardViewModel.onNavigation,
         todaysStatsViewModelSlice.onNavigation,
         postsCardViewModelSlice.onNavigation,
         activityLogCardViewModelSlice.onNavigation,
@@ -252,7 +249,6 @@ class MySiteViewModel @Inject constructor(
             bloganuaryNudgeCardViewModelSlice.refresh,
             sotw2023NudgeCardViewModelSlice.refresh,
         )
-    val domainTransferCardRefresh = domainTransferCardViewModel.refresh
 
     private var shouldMarkUpdateSiteTitleTaskComplete = false
 
@@ -305,8 +301,6 @@ class MySiteViewModel @Inject constructor(
             bloggingPromptCardViewModelSlice.onSiteChanged(site?.id, state as? SiteSelected)
 
             dashboardCardPlansUtils.onSiteChanged(site?.id, state as? SiteSelected)
-
-            domainTransferCardViewModel.onSiteChanged(site?.id, state as? SiteSelected)
 
             state
         }
@@ -491,10 +485,6 @@ class MySiteViewModel @Inject constructor(
                 bloganuaryNudgeCardBuilderParams = bloganuaryNudgeCardViewModelSlice.getBuilderParams(),
                 bloggingPromptCardBuilderParams = bloggingPromptCardViewModelSlice.getBuilderParams(
                     bloggingPromptUpdate
-                ),
-                domainTransferCardBuilderParams = domainTransferCardViewModel.buildDomainTransferCardParams(
-                    site,
-                    uiModel.value as? SiteSelected
                 ),
                 blazeCardBuilderParams = blazeCardViewModelSlice.getBlazeCardBuilderParams(blazeCardUpdate),
                 dashboardCardPlansBuilderParams = DashboardCardPlansBuilderParams(
