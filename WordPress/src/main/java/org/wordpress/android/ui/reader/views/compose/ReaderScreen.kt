ec8754151f2f0832ebefe4a4e9ac834684beb3dc
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-12-18 13:07:24 -0300

    Fix detekt

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/ReaderScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/ReaderScreen.kt
@@ -1,8 +1,6 @@
 package org.wordpress.android.ui.reader.views.compose
 
-import android.annotation.SuppressLint
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,12 +9,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
