796b7e7d8b573eda9defad16ebc393fa34996f77
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-12-26 13:24:56 +0530

    - Removes: unused methods from CommentList.java

--- WordPress/src/main/java/org/wordpress/android/models/CommentList.java
+++ WordPress/src/main/java/org/wordpress/android/models/CommentList.java
@@ -1,8 +1,6 @@
 package org.wordpress.android.models;
 
 import org.wordpress.android.fluxc.model.CommentModel;
-import org.wordpress.android.fluxc.model.CommentStatus;
-import org.wordpress.android.util.StringUtils;
 
 import java.util.ArrayList;
 
@@ -14,93 +12,5 @@ public class CommentList extends ArrayList<CommentModel> {
             }
         }
         return -1;
-    }
-
-    /*
-     * replace comments in this list that match the passed list
-     */
-    public void replaceComments(final CommentList comments) {
-        if (comments == null || comments.size() == 0) {
-            return;
-        }
-        for (CommentModel comment : comments) {
-            int index = indexOfCommentId(comment.getRemoteCommentId());
-            if (index > -1) {
-                set(index, comment);
-            }
-        }
-    }
-
-    /*
-     * delete comments in this list that match the passed list
-     */
-    public void deleteComments(final CommentList comments) {
-        if (comments == null || comments.size() == 0) {
-            return;
-        }
-        for (CommentModel comment : comments) {
-            int index = indexOfCommentId(comment.getRemoteCommentId());
-            if (index > -1) {
-                remove(index);
-            }
-        }
-    }
-
-    /*
-     * returns true if any comments in this list have the passed status
-     */
-    public boolean hasAnyWithStatus(CommentStatus status) {
-        for (CommentModel comment : this) {
-            if (status.toString().equals(comment.getStatus())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /*
-     * returns true if any comments in this list do NOT have the passed status
-     */
-    public boolean hasAnyWithoutStatus(CommentStatus status) {
-        for (CommentModel comment : this) {
-            if (!status.toString().equals(comment.getStatus())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /*
-     * does passed list contain the same comments as this list?
-     */
-    public boolean isSameList(CommentList comments) {
-        if (comments == null || comments.size() != this.size()) {
-            return false;
-        }
-
-        for (final CommentModel comment : comments) {
-            int index = this.indexOfCommentId(comment.getRemoteCommentId());
-            if (index == -1) {
-                return false;
-            }
-            final CommentModel thisComment = this.get(index);
-            if (!StringUtils.equals(thisComment.getStatus(), comment.getStatus())) {
-                return false;
-            }
-            if (!StringUtils.equals(thisComment.getContent(), comment.getContent())) {
-                return false;
-            }
-            if (!StringUtils.equals(thisComment.getAuthorName(), comment.getAuthorName())) {
-                return false;
-            }
-            if (!StringUtils.equals(thisComment.getAuthorEmail(), comment.getAuthorEmail())) {
-                return false;
-            }
-            if (!StringUtils.equals(thisComment.getAuthorUrl(), comment.getAuthorUrl())) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
