32a02d15fa1b560eade2096bb0cfac25bc08971e
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-12-26 12:47:40 +0530

    - Removes: unused methods from WPPrefUtils.java

--- WordPress/src/main/java/org/wordpress/android/util/WPPrefUtils.java
+++ WordPress/src/main/java/org/wordpress/android/util/WPPrefUtils.java
@@ -4,10 +4,6 @@ import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceGroup;
-import android.util.TypedValue;
-import android.widget.TextView;
-
-import org.wordpress.android.R;
 
 /**
  * Design guidelines for Calypso-styled Site Settings (and likely other screens)
@@ -52,18 +48,5 @@ public class WPPrefUtils {
         if (parent != null && child != null) {
             parent.removePreference(child);
         }
-    }
-
-    /**
-     * Styles a {@link TextView} to display text in a button.
-     */
-    public static void layoutAsFlatButton(TextView view) {
-        int size = view.getResources().getDimensionPixelSize(R.dimen.text_sz_medium);
-        setTextViewAttributes(view, size, R.color.primary_40);
-    }
-
-    public static void setTextViewAttributes(TextView textView, int size, int colorRes) {
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
-        textView.setTextColor(textView.getResources().getColor(colorRes));
     }
 }
