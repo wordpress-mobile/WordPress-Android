1391214de879b4928b2e8765b86bc55d105dc599
Thomas Horta <thomas.horta@automattic.com>
2023-12-29 11:11:19 -0300

    Remove unused imports (fix detekt issues)

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloganuary/BloganuaryNudgeCardBuilder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/dashboard/bloganuary/BloganuaryNudgeCardBuilder.kt
@@ -1,10 +1,8 @@
 package org.wordpress.android.ui.mysite.cards.dashboard.bloganuary
 
-import org.wordpress.android.R
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.BloganuaryNudgeCardModel
 import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.BloganuaryNudgeCardBuilderParams
 import org.wordpress.android.ui.utils.ListItemInteraction
-import org.wordpress.android.ui.utils.UiString.UiStringRes
 import javax.inject.Inject
 
 class BloganuaryNudgeCardBuilder @Inject constructor() {
