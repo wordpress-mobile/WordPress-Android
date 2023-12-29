e871a4f4f694e6325d6fef972fcd57ba6cff5b5c
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-12-29 16:24:33 +0530

    ↑ [WIP] Updates: the AccountDataSource.kt to be a viewmodel slice
    Removes the site source dependency
    Adds the logic to build the card in the viewmodel slice

---
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/AccountDataViewModelSlice.kt
@@ -0,0 +1,43 @@
+package org.wordpress.android.ui.mysite
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.wordpress.android.fluxc.store.AccountStore
+import org.wordpress.android.ui.mysite.MySiteUiState.PartialState.AccountData
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AccountDataViewModelSlice @Inject constructor(
+    private val accountStore: AccountStore
+) {
+    private lateinit var scope: CoroutineScope
+
+    private val _isRefreshing = MutableLiveData<Boolean>()
+    val isRefreshing: LiveData<Boolean> = _isRefreshing
+
+    private val _uiModel = MutableLiveData<AccountData>()
+    val uiModel: LiveData<AccountData> = _uiModel
+
+    fun initialize(scope: CoroutineScope) {
+        this.scope = scope
+    }
+
+    val refresh = MutableLiveData(false)
+
+    fun getAccountData() {
+        scope.launch {
+            _isRefreshing.postValue(true)
+            val url = accountStore.account?.avatarUrl.orEmpty()
+            val name = accountStore.account?.displayName?.ifEmpty { accountStore.account?.userName.orEmpty() }.orEmpty()
+            _uiModel.postValue(AccountData(url, name))
+            _isRefreshing.postValue(false)
+        }
+    }
+
+    fun refresh() {
+        getAccountData()
+    }
+}
