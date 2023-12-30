8832e56c1c0ef25a7542abf7388b91cdf7f32131
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-12-30 11:42:47 +0530

    ↑ [WIP] Updates: the PersonalizeCardViewModelSlice to build card

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/personalize/PersonalizeCardViewModelSlice.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/personalize/PersonalizeCardViewModelSlice.kt
@@ -2,20 +2,35 @@ package org.wordpress.android.ui.mysite.cards.personalize
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
 import org.wordpress.android.ui.mysite.MySiteCardAndItem
-import org.wordpress.android.ui.mysite.SiteNavigationAction.OpenDashboardPersonalization
 import org.wordpress.android.ui.mysite.MySiteCardAndItemBuilderParams.PersonalizeCardBuilderParams
 import org.wordpress.android.ui.mysite.SiteNavigationAction
+import org.wordpress.android.ui.mysite.SiteNavigationAction.OpenDashboardPersonalization
 import org.wordpress.android.ui.mysite.cards.dashboard.CardsTracker
 import org.wordpress.android.viewmodel.Event
 import javax.inject.Inject
 
 class PersonalizeCardViewModelSlice @Inject constructor(
     private val cardsTracker: CardsTracker,
-    private val personalizeCardShownTracker: PersonalizeCardShownTracker
+    private val personalizeCardShownTracker: PersonalizeCardShownTracker,
+    private val personalizeCardBuilder: PersonalizeCardBuilder
 )  {
+    private lateinit var scope: CoroutineScope
+
     private val _onNavigation = MutableLiveData<Event<SiteNavigationAction>>()
     val onNavigation = _onNavigation as LiveData<Event<SiteNavigationAction>>
+
+    private val _uiModel = MutableLiveData<MySiteCardAndItem.Card.PersonalizeCardModel>()
+    val uiModel = _uiModel as LiveData<MySiteCardAndItem.Card.PersonalizeCardModel>
+
+    fun initialize(scope: CoroutineScope) {
+        this.scope = scope
+    }
+
+    fun buildCard() {
+        _uiModel.postValue(personalizeCardBuilder.build(getBuilderParams()))
+    }
 
     fun getBuilderParams() = PersonalizeCardBuilderParams(onClick = this::onCardClick)
 
