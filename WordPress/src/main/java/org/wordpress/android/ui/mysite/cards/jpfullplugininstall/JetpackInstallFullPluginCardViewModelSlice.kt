5306ba5de90f792b6a8dc3f310f800e6c752882d
Ajesh R Pai <ajeshr2k1@gmail.com>
2024-01-01 15:28:00 +0530

    ↑ Updates: the JetpackInstallFullPluginCardViewModelSlice logic
    Updates the logic to remove refresh live data
    Updates the live data to use distinctuntill changed

--- WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/jpfullplugininstall/JetpackInstallFullPluginCardViewModelSlice.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mysite/cards/jpfullplugininstall/JetpackInstallFullPluginCardViewModelSlice.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.ui.mysite.cards.jpfullplugininstall
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.distinctUntilChanged
 import org.wordpress.android.analytics.AnalyticsTracker
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.mysite.MySiteCardAndItem.Card.JetpackInstallFullPluginCard
@@ -20,20 +21,17 @@ class JetpackInstallFullPluginCardViewModelSlice @Inject constructor(
     private val selectedSiteRepository: SelectedSiteRepository,
     private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
 ) {
-    private val _refresh = MutableLiveData<Event<Boolean>>()
-    val refresh = _refresh
-
     private val _onOpenJetpackInstallFullPluginOnboarding = SingleLiveEvent<Event<Unit>>()
     val onOpenJetpackInstallFullPluginOnboarding = _onOpenJetpackInstallFullPluginOnboarding
 
-    private val _uiModel = MutableLiveData<JetpackInstallFullPluginCard>()
-    val uiModel = _uiModel
+    private val _uiModel = MutableLiveData<JetpackInstallFullPluginCard?>()
+    val uiModel = _uiModel.distinctUntilChanged()
 
     private fun onJetpackInstallFullPluginHideMenuItemClick() {
         selectedSiteRepository.getSelectedSite()?.localId()?.value?.let {
             analyticsTrackerWrapper.track(AnalyticsTracker.Stat.JETPACK_INSTALL_FULL_PLUGIN_CARD_DISMISSED)
             appPrefsWrapper.setShouldHideJetpackInstallFullPluginCard(it, true)
-            _refresh.postValue(Event(true))
+            _uiModel.postValue(null)
         }
     }
 
@@ -56,7 +54,7 @@ class JetpackInstallFullPluginCardViewModelSlice @Inject constructor(
         params: JetpackInstallFullPluginCardBuilderParams
     ) {
         if (shouldShowCard(params.site)) {
-            uiModel.postValue(
+            _uiModel.postValue(
                 JetpackInstallFullPluginCard(
                     siteName = params.site.name,
                     pluginNames = params.site.activeIndividualJetpackPluginNames().orEmpty(),
@@ -64,7 +62,7 @@ class JetpackInstallFullPluginCardViewModelSlice @Inject constructor(
                     onHideMenuItemClick = ListItemInteraction.create(params.onHideMenuItemClick),
                 )
             )
-        }
+        } else _uiModel.postValue(null)
     }
 
     private fun shouldShowCard(site: SiteModel): Boolean {
