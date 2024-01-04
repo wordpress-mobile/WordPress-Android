d8a7bffc6a35c54f55540ca63da0afede4611ec0
David Calhoun <github@davidcalhoun.me>
2024-01-04 09:31:39 -0500

    refactor: Fix parameter name typo

--- libs/editor/src/main/java/org/wordpress/android/editor/EditorMediaUploadListener.java
+++ libs/editor/src/main/java/org/wordpress/android/editor/EditorMediaUploadListener.java
@@ -9,5 +9,5 @@ public interface EditorMediaUploadListener {
     void onMediaUploadProgress(String localId, float progress);
     void onMediaUploadFailed(String localId, String errorType);
     void onGalleryMediaUploadSucceeded(long galleryId, long remoteId, int remaining);
-    void onMediaUploadPaused(String toString);
+    void onMediaUploadPaused(String localId);
 }
