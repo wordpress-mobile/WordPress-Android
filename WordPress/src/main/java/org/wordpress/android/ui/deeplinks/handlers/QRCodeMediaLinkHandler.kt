ca89cbe6ed685c5c453fcefd6ba8e9776089032e
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-01-04 19:39:16 -0500

    Track deep link failed when a match for the blog embedded in the qr code is not in the list of sites for the current user. Add associated test

--- WordPress/src/main/java/org/wordpress/android/ui/deeplinks/handlers/QRCodeMediaLinkHandler.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/deeplinks/handlers/QRCodeMediaLinkHandler.kt
@@ -1,12 +1,15 @@
 package org.wordpress.android.ui.deeplinks.handlers
 
+import org.wordpress.android.analytics.AnalyticsTracker
 import org.wordpress.android.ui.deeplinks.DeepLinkNavigator.NavigateAction
 import org.wordpress.android.ui.deeplinks.DeepLinkUriUtils
 import org.wordpress.android.util.UriWrapper
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 import javax.inject.Inject
 
 class QRCodeMediaLinkHandler @Inject constructor(
-    private val deepLinkUriUtils: DeepLinkUriUtils
+    private val deepLinkUriUtils: DeepLinkUriUtils,
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
 ) : DeepLinkHandler {
     /**
      * Returns true if the URI looks like `apps.wordpress.com/get`
@@ -22,6 +25,9 @@ class QRCodeMediaLinkHandler @Inject constructor(
         val extractedSiteId = extractSiteIdFromUrl(uri)
         return when (val siteModel = extractedSiteId?.let { siteId -> deepLinkUriUtils.blogIdToSite(siteId) }) {
             null -> {
+                analyticsTrackerWrapper.track(AnalyticsTracker.Stat.DEEP_LINK_FAILED,
+                    mapOf(ERROR to INVALID_SITE_ID,
+                        CAMPAIGN to uri.getQueryParameter(CAMPAIGN)?.replace("-", "_")))
                 NavigateAction.OpenMySite
             }
             else -> {
@@ -51,6 +57,8 @@ class QRCodeMediaLinkHandler @Inject constructor(
         private const val GET_PATH = "get"
         private const val HOST_APPS_WORDPRESS_COM = "apps.wordpress.com"
         private const val CAMPAIGN = "campaign"
+        private const val ERROR = "error"
+        private const val INVALID_SITE_ID = "invalid_site_id"
         const val CAMPAIGN_TYPE = "qr-code-media"
     }
 }
--- WordPress/src/test/java/org/wordpress/android/ui/deeplinks/handlers/QRCodeMediaLinkHandlerTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/deeplinks/handlers/QRCodeMediaLinkHandlerTest.kt
@@ -6,11 +6,14 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import org.wordpress.android.analytics.AnalyticsTracker
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.deeplinks.DeepLinkNavigator.NavigateAction
 import org.wordpress.android.ui.deeplinks.DeepLinkUriUtils
 import org.wordpress.android.ui.deeplinks.buildUri
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
 
 @RunWith(MockitoJUnitRunner::class)
 class QRCodeMediaLinkHandlerTest {
@@ -18,12 +21,15 @@ class QRCodeMediaLinkHandlerTest {
     lateinit var deepLinkUriUtils: DeepLinkUriUtils
 
     @Mock
+    lateinit var analyticsTrackerWrapper: AnalyticsTrackerWrapper
+
+    @Mock
     lateinit var site: SiteModel
     private lateinit var qrCodeMediaLinkHandler: QRCodeMediaLinkHandler
 
     @Before
     fun setUp() {
-        qrCodeMediaLinkHandler = QRCodeMediaLinkHandler(deepLinkUriUtils)
+        qrCodeMediaLinkHandler = QRCodeMediaLinkHandler(deepLinkUriUtils, analyticsTrackerWrapper)
     }
 
     // https://apps.wordpress.com/get/?campaign=qr-code-media#/media/225903215
@@ -93,5 +99,21 @@ class QRCodeMediaLinkHandlerTest {
         val navigateAction = qrCodeMediaLinkHandler.buildNavigateAction(mediaUri)
 
         assertThat(navigateAction).isEqualTo(NavigateAction.OpenMediaPickerForSite(site))
+    }
+
+    @Test
+    fun `given unrecognized siteId, when deep linked, then event is tracked`() {
+        val mediaUri = buildUri(host = "apps.wordpress.com",
+            queryParams = mapOf("campaign" to "qr-code-media"),
+            path = arrayOf("get"), )
+
+        whenever(mediaUri.fragment).thenReturn(null)
+
+        qrCodeMediaLinkHandler.buildNavigateAction(mediaUri)
+
+        verify(analyticsTrackerWrapper).track(
+            AnalyticsTracker.Stat.DEEP_LINK_FAILED,
+            mapOf("error" to "invalid_site_id",
+                "campaign" to "qr_code_media"))
     }
 }
