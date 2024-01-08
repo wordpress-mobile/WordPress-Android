a93fa5a473d366b8d51663c978fef989730d6b39
jhnstn <jhnstn@pm.me>
2024-01-08 18:06:35 -0500

    Add site info to editor upload retry event

--- WordPress/src/main/java/org/wordpress/android/ui/posts/editor/media/RetryFailedMediaUploadUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/editor/media/RetryFailedMediaUploadUseCase.kt
@@ -3,7 +3,11 @@ package org.wordpress.android.ui.posts.editor.media
 import dagger.Reusable
 import org.wordpress.android.analytics.AnalyticsTracker.Stat
 import org.wordpress.android.fluxc.model.MediaModel.MediaUploadState.QUEUED
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.ui.CommentFullScreenDialogFragment
 import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
+import org.wordpress.android.util.analytics.AnalyticsUtils
 import javax.inject.Inject
 
 @Reusable
@@ -13,6 +17,8 @@ class RetryFailedMediaUploadUseCase @Inject constructor(
     private val uploadMediaUseCase: UploadMediaUseCase,
     private val tracker: AnalyticsTrackerWrapper
 ) {
+    @Inject
+    lateinit var siteStore: SiteStore
     suspend fun retryFailedMediaAsync(
         editorMediaListener: EditorMediaListener,
         failedMediaLocalIds: List<Int>
@@ -33,7 +39,8 @@ class RetryFailedMediaUploadUseCase @Inject constructor(
                         editorMediaListener,
                         mediaModels
                     )
-                    tracker.track(Stat.EDITOR_UPLOAD_MEDIA_RETRIED)
+                    val site: SiteModel? = siteStore.getSiteByLocalId(editorMediaListener.getImmutablePost().localSiteId)
+                    AnalyticsUtils.trackWithSiteDetails(tracker, Stat.EDITOR_UPLOAD_MEDIA_RETRIED, site, null);
                 }
             }
     }
