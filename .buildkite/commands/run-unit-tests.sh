2c1f5d8d23155728077578e436a0526ff4f435c0
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-01-12 13:07:10 +0100

    temp: ls WordPress code coverage reports dir

--- .buildkite/commands/run-unit-tests.sh
+++ .buildkite/commands/run-unit-tests.sh
@@ -47,6 +47,7 @@ mkdir buildkite-test-analytics
 cp $test_log_dir buildkite-test-analytics
 
 echo "--- ⚒️ Uploading code coverage"
+ls WordPress/build/reports/kover
 .buildkite/commands/upload-code-coverage.sh
 
 exit $TESTS_EXIT_STATUS
