493682dda9421aa3ead1327a3d9a8bbb7f1e59ba
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2024-01-16 18:41:56 -0300

    Replace reader_discover_no_posts_subtitle with reader_discover_no_posts_subscribe_subtitle

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderDiscoverViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderDiscoverViewModel.kt
@@ -559,7 +559,7 @@ class ReaderDiscoverViewModel @Inject constructor(
             data class ShowNoPostsUiState(override val action: () -> Unit) : EmptyUiState() {
                 override val titleResId = R.string.reader_discover_no_posts_title
                 override val buttonResId = R.string.reader_discover_no_posts_button_text
-                override val subTitleRes = R.string.reader_discover_no_posts_subtitle
+                override val subTitleRes = R.string.reader_discover_no_posts_subscribe_subtitle
                 override val illustrationResId = R.drawable.img_illustration_empty_results_216dp
             }
         }
