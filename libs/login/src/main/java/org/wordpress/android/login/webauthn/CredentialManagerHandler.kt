fe859fc716498c4a8c1bb96f28316dadd243fd59
ThomazFB <thomazfcortez@gmail.com>
2024-01-18 18:04:58 -0300

    Define CredentialManagerHandler type

---
+++ libs/login/src/main/java/org/wordpress/android/login/webauthn/CredentialManagerHandler.kt
@@ -0,0 +1,14 @@
+package org.wordpress.android.login.webauthn
+
+import android.content.Context
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.GetPasswordOption
+import androidx.credentials.GetPublicKeyCredentialOption
+import androidx.credentials.exceptions.GetCredentialException
+
+class CredentialManagerHandler {
+}
