23910cd08800006a2c8e18404de31fd509cae059
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2024-01-19 19:46:55 -0300

    Replace reader_toast_err_remove_tag  with reader_toast_err_removing_tag

--- WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderPostAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderPostAdapter.java
@@ -339,7 +339,7 @@ public class ReaderPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         ReaderActions.ActionListener listener = succeeded -> {
             if (!succeeded) {
                 int errResId = isAskingToFollow ? R.string.reader_toast_err_adding_tag
-                        : R.string.reader_toast_err_remove_tag;
+                        : R.string.reader_toast_err_removing_tag;
                 ToastUtils.showToast(context, errResId);
             } else {
                 if (isAskingToFollow) {
