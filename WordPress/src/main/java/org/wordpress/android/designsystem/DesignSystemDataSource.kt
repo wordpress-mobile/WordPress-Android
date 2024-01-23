723f87763141760b6058593ec03b30f7106f7532
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-01-23 14:07:52 -0800

    Add new screen names

--- WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemDataSource.kt
+++ WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemDataSource.kt
@@ -3,8 +3,16 @@ package org.wordpress.android.designsystem
 import org.wordpress.android.R
 
 object DesignSystemDataSource {
-    val buttonOptions = listOf(
+    val startScreenButtonOptions = listOf(
         R.string.design_system_foundation,
         R.string.design_system_components
+    )
+    val foundationScreenButtonOptions = listOf(
+        R.string.design_system_foundation_colors,
+        R.string.design_system_foundation_fonts,
+        R.string.design_system_foundation_Lengths
+    )
+    val componentsScreenButtonOptions = listOf(
+        R.string.design_system_components_dsbutton
     )
 }
