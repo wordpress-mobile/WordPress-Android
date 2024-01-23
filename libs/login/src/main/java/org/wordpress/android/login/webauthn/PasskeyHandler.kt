8e7610770d08138d56c20280d63bf4fa3bad7627
ThomazFB <thomazfcortez@gmail.com>
2024-01-22 22:20:27 -0300

    Add FIDO2 handling inside PasskeyHandler

--- libs/login/src/main/java/org/wordpress/android/login/webauthn/PasskeyHandler.kt
+++ libs/login/src/main/java/org/wordpress/android/login/webauthn/PasskeyHandler.kt
@@ -1,12 +1,21 @@
 package org.wordpress.android.login.webauthn
 
 import android.app.Activity
+import android.content.Intent
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts.StartIntentSenderForResult
 import androidx.fragment.app.Fragment
+import com.google.android.gms.fido.Fido
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredential
+import org.wordpress.android.fluxc.network.rest.wpcom.auth.webauthn.WebauthnChallengeInfo
 
 class PasskeyHandler {
-    fun fetch(requestingFragment: Fragment, onResult: (PasskeyDataResult) -> Unit) {
+    fun fetch(
+        requestingFragment: Fragment,
+        userId: String,
+        challengeInfo: WebauthnChallengeInfo,
+        onResult: (PasskeyDataResult) -> Unit
+    ) {
         requestingFragment.registerForActivityResult(
                 StartIntentSenderForResult()
         )
@@ -19,7 +28,27 @@ class PasskeyHandler {
         }
     }
 
+    private fun parseFIDO2IntentData(
+        resultData: Intent,
+        userId: String,
+        challengeInfo: WebauthnChallengeInfo
+    ): PasskeyDataResult? =
+            resultData.takeIf { it.hasExtra(Fido.FIDO2_KEY_CREDENTIAL_EXTRA) }
+                    ?.getByteArrayExtra(Fido.FIDO2_KEY_CREDENTIAL_EXTRA)
+                    ?.let { PublicKeyCredential.deserializeFromBytes(it).toJson() }
+                    ?.let { clientData ->
+                        PasskeyDataResult(
+                                isFailure = false,
+                                userId = userId,
+                                twoStepNonce = challengeInfo.twoStepNonce,
+                                clientData = clientData
+                        )
+                    }
+
     data class PasskeyDataResult(
-        val isFailure: Boolean
+        val isFailure: Boolean,
+        val userId: String,
+        val twoStepNonce: String,
+        val clientData: String
     )
 }
