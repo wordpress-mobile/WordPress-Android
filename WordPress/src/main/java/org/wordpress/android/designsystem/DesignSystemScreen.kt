155fae064b536dd1988be4010f6a121896cfe99b
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-01-24 12:33:42 -0800

    Add top bar and onBackPressed navigation

--- WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemScreen.kt
@@ -1,55 +1,39 @@
 package org.wordpress.android.designsystem
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import org.wordpress.android.R
-import org.wordpress.android.designsystem.DesignSystemDataSource.buttonOptions
+import org.wordpress.android.ui.compose.components.MainTopAppBar
+import org.wordpress.android.ui.compose.components.NavigationIcons
 
 enum class DesignSystemScreen {
-    Start
-}
-@Composable
-fun DesignSystemStartScreen(
-    modifier: Modifier = Modifier
-){
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        Row(modifier = Modifier.weight(1f, false)) {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(
-                    dimensionResource(id = R.dimen.button_container_shadow_height)
-                )
-            ) {
-                buttonOptions.forEach { item ->
-                    SelectOptionButton(
-                        labelResourceId = item,
-                        onClick = {}
-                    )
-                }
-            }
-        }
-    }
+    Start,
+    Foundation,
+    Components
 }
 
 @Composable
@@ -69,18 +53,80 @@ fun SelectOptionButton(
 
 @Preview
 @Composable
-fun StartDesignSystemPreview() {
-    DesignSystemStartScreen(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(dimensionResource(R.dimen.button_container_shadow_height))
+fun StartDesignSystemPreview(){
+    DesignSystem {}
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DesignSystemAppBar(
+//    currentScreen: DesignSystemScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.backdated_for)
+                    )
+                }
+            }
+        }
     )
 }
 @Composable
-fun DesignSystem() {
-    DesignSystemStartScreen(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(dimensionResource(R.dimen.button_container_shadow_height))
-    )
+fun DesignSystem(
+    onBackTapped: () -> Unit
+    ) {
+    val navController: NavHostController = rememberNavController()
+    Scaffold(
+        topBar = {
+            MainTopAppBar(
+                title = stringResource(R.string.preference_design_system),
+                navigationIcon = NavigationIcons.BackIcon,
+                onNavigationIconClick = { onBackTapped() },
+                backgroundColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onSurface,
+            )
+        },
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = DesignSystemScreen.Start.name
+        ) {
+            composable(route = DesignSystemScreen.Start.name) {
+                DesignSystemStartScreen(
+                    onNextButtonClicked = {
+                        navController.navigate(DesignSystemScreen.Foundation.name)
+                    },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                )
+            }
+            composable(route = DesignSystemScreen.Foundation.name) {
+                DesignSystemFoundationScreen(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(R.dimen.button_container_shadow_height))
+                )
+            }
+            composable(route = DesignSystemScreen.Components.name) {
+                DesignSystemFoundationScreen(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(R.dimen.button_container_shadow_height))
+                )
+            }
+        }
+    }
 }
