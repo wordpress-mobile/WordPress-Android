130a534f9a32f8c1fc6613cdfe3c2c88f47d4314
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-01-23 16:44:21 -0800

    Add start, component, and foundation screens

---
+++ WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemFoundationScreen.kt
@@ -0,0 +1,52 @@
+package org.wordpress.android.designsystem
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import org.wordpress.android.R
+
+@Composable
+fun DesignSystemFoundationScreen(
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(modifier = Modifier.weight(1f, false)) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(
+                    dimensionResource(id = R.dimen.button_container_shadow_height)
+                )
+            ) {
+                DesignSystemDataSource.foundationScreenButtonOptions.forEach { item ->
+                    SelectOptionButton(
+                        labelResourceId = item,
+                        onClick = {}
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun StartDesignSystemFoundationScreenPreview(){
+    DesignSystemFoundationScreen(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(R.dimen.button_container_shadow_height))
+    )
+}
+
