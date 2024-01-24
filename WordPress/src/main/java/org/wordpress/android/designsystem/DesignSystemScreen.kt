965fe2a82cfed5e709b10e6ae1162a4b1190947c
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-01-24 13:28:15 -0800

    Unused import

--- WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
