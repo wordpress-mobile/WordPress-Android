767d7c15f12aefbadd7525d3185b3c9eb84450ef
Ajesh R Pai <ajeshr2k1@gmail.com>
2024-01-25 15:37:02 +0530

    + Adds: Tabs to SiteMonitorParentActivity and resources

--- WordPress/src/main/java/org/wordpress/android/ui/sitemonitor/SiteMonitorParentActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitemonitor/SiteMonitorParentActivity.kt
@@ -4,8 +4,18 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
+import androidx.compose.material3.Tab
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import org.wordpress.android.R
@@ -41,6 +51,38 @@ class SiteMonitorParentActivity: AppCompatActivity() {
     }
 
     @Composable
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     fun TabScreen(modifier: Modifier = Modifier) {
+        var tabIndex by remember { mutableStateOf(0) }
+
+        val tabs = listOf(
+            R.string.site_monitoring_tab_title_metrics,
+            R.string.site_monitoring_tab_title_php_logs,
+            R.string.site_monitoring_tab_title_web_server_logs
+        )
+
+        Column(modifier = modifier.fillMaxWidth()) {
+            TabRow(
+                selectedTabIndex = tabIndex,
+                backgroundColor = MaterialTheme.colors.surface,
+                contentColor = MaterialTheme.colors.onSurface,
+            ) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(text = { Text(stringResource(id = title)) },
+                        selected = tabIndex == index,
+                        onClick = { tabIndex = index }
+                    )
+                }
+            }
+            when (tabIndex) {
+                0 -> SiteMonitoringWebView()
+                1 -> SiteMonitoringWebView()
+                2 -> SiteMonitoringWebView()
+            }
+        }
+    }
+
+    @Composable
+    fun SiteMonitoringWebView(){
     }
 }
