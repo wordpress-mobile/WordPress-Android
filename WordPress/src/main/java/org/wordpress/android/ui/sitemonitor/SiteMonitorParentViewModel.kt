2a15978c86328a22202c1bb739ae0fec6624b9a2
Ajesh R Pai <ajeshr2k1@gmail.com>
2024-01-25 17:30:20 +0530

    + Adds: SiteMonitorParentViewModel class for handling ui logic

---
+++ WordPress/src/main/java/org/wordpress/android/ui/sitemonitor/SiteMonitorParentViewModel.kt
@@ -0,0 +1,26 @@
+package org.wordpress.android.ui.sitemonitor
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.modules.BG_THREAD
+import org.wordpress.android.util.analytics.AnalyticsTrackerWrapper
+import org.wordpress.android.viewmodel.ScopedViewModel
+import javax.inject.Inject
+import javax.inject.Named
+
+@HiltViewModel
+class SiteMonitorParentViewModel @Inject constructor(
+    @param:Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
+    private val analyticsTrackerWrapper: AnalyticsTrackerWrapper
+) : ScopedViewModel(bgDispatcher) {
+    private lateinit var site: SiteModel
+
+    fun start(site: SiteModel) {
+        this.site = site
+        trackActivityLaunched()
+    }
+
+    private fun trackActivityLaunched() {
+    }
+}
