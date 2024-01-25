a0e91725132ab5a2d013ef0b76e68acea1408d19
Ajesh R Pai <ajeshr2k1@gmail.com>
2024-01-25 17:30:49 +0530

    + Adds: Viewmodel to activity and calls to start function

--- WordPress/src/main/java/org/wordpress/android/ui/sitemonitor/SiteMonitorParentActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitemonitor/SiteMonitorParentActivity.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.ui.sitemonitor
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,19 +19,31 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import dagger.hilt.android.AndroidEntryPoint
 import org.wordpress.android.R
+import org.wordpress.android.WordPress
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.compose.components.MainTopAppBar
 import org.wordpress.android.ui.compose.components.NavigationIcons
 import org.wordpress.android.ui.compose.theme.AppTheme
+import org.wordpress.android.util.extensions.getSerializableExtraCompat
 
+@AndroidEntryPoint
 class SiteMonitorParentActivity: AppCompatActivity() {
+    val viewModel:SiteMonitorParentViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
+                viewModel.start(getSite())
                 SiteMonitorScreen()
             }
         }
+    }
+
+    private fun getSite(): SiteModel {
+        return requireNotNull(intent.getSerializableExtraCompat(WordPress.SITE)) as SiteModel
     }
 
     @Composable
