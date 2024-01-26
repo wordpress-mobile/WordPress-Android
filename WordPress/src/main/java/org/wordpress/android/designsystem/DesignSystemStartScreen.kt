08f82a72bb6753d70d3379468e05c4d1f3d9e9ae
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-01-25 16:53:23 -0800

    Cleanup

--- WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemStartScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/designsystem/DesignSystemStartScreen.kt
@@ -1,10 +1,7 @@
 package org.wordpress.android.designsystem
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
