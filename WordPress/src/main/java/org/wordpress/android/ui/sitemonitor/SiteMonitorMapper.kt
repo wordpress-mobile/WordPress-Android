a57a6d81da1c80d5f8dbc8e087dc70e2edd381f9
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-01-26 11:41:26 -0500

    Refactor: remove SiteMonitorURL list. Replace with values specific to that type

--- WordPress/src/main/java/org/wordpress/android/ui/sitemonitor/SiteMonitorMapper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/sitemonitor/SiteMonitorMapper.kt
@@ -5,13 +5,15 @@ import javax.inject.Inject
 class SiteMonitorMapper @Inject constructor(
     private val siteMonitorUtils: SiteMonitorUtils
 ) {
-    fun toPrepared(urls: List<SiteMonitorUrl>) = SiteMonitorUiState.Prepared(
+    fun toPrepared(url: String, addressToLoad: String, siteMonitorType: SiteMonitorType) = SiteMonitorUiState.Prepared(
         model = SiteMonitorModel(
             enableJavascript = true,
             enableDomStorage = true,
             userAgent = siteMonitorUtils.getUserAgent(),
             enableChromeClient = true,
-            urls = urls
+            url = url,
+            addressToLoad = addressToLoad,
+            siteMonitorType = siteMonitorType
         )
     )
 
