16cae761c49ee4f3775d7760ed73f1693cb175f1
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2024-02-06 12:04:57 -0300

    Remove unused import from ReaderBlogAdapter

--- WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderBlogAdapter.java
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/adapters/ReaderBlogAdapter.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.wordpress.android.R;
