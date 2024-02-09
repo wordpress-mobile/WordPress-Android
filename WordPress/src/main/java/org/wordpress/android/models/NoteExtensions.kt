4c53c96a2c51a22dfd848e7422a90593ce0b1d0f
Matthew Kevins <mmkevins@yahoo.com>
2024-02-09 18:38:25 +1000

    Use static map for raw type to note type conversion

--- WordPress/src/main/java/org/wordpress/android/models/NoteExtensions.kt
+++ WordPress/src/main/java/org/wordpress/android/models/NoteExtensions.kt
@@ -1,9 +1,9 @@
 package org.wordpress.android.models
 
 val Note.type
-    get() = runCatching { NoteType.valueOf(rawType) }.getOrDefault(NoteType.Unknown)
+    get() = NoteType.from(rawType)
 
-enum class NoteType(val value: String) {
+enum class NoteType(val rawType: String) {
     Follow(Note.NOTE_FOLLOW_TYPE),
     Like(Note.NOTE_LIKE_TYPE),
     Comment(Note.NOTE_COMMENT_TYPE),
@@ -12,5 +12,10 @@ enum class NoteType(val value: String) {
     Reblog(Note.NOTE_REBLOG_TYPE),
     NewPost(Note.NOTE_NEW_POST_TYPE),
     ViewMilestone(Note.NOTE_VIEW_MILESTONE),
-    Unknown(Note.NOTE_UNKNOWN_TYPE),
+    Unknown(Note.NOTE_UNKNOWN_TYPE);
+
+    companion object {
+        private val map = entries.associateBy(NoteType::rawType)
+        fun from(rawType: String) = map.get(rawType) ?: Unknown
+    }
 }
