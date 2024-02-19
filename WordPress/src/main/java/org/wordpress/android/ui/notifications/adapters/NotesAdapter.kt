3d8433659a3a046a167cb24231ec72b2a58ee4c2
Jarvis Lin <jarvis.lin@automattic.com>
2024-02-19 19:39:54 +0800

    Extract NoteViewHolder from NoteAdapter

--- WordPress/src/main/java/org/wordpress/android/ui/notifications/adapters/NotesAdapter.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/notifications/adapters/NotesAdapter.kt
@@ -1,21 +1,8 @@
 package org.wordpress.android.ui.notifications.adapters
 
 import android.content.Context
-import android.content.res.ColorStateList
-import android.text.Spanned
-import android.text.TextUtils
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.MarginLayoutParams
-import android.view.ViewTreeObserver
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.annotation.StringRes
-import androidx.core.text.BidiFormatter
-import androidx.core.view.ViewCompat
-import androidx.core.view.isVisible
-import androidx.core.widget.ImageViewCompat
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.RecyclerView
@@ -25,28 +12,12 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.wordpress.android.R
 import org.wordpress.android.WordPress
 import org.wordpress.android.databinding.NotificationsListItemBinding
 import org.wordpress.android.datasets.NotificationsTable
 import org.wordpress.android.models.Note
-import org.wordpress.android.models.Note.NoteTimeGroup
-import org.wordpress.android.models.Notification
-import org.wordpress.android.models.Notification.Comment
-import org.wordpress.android.models.Notification.PostNotification
-import org.wordpress.android.models.Notification.Unknown
-import org.wordpress.android.ui.comments.CommentUtils
 import org.wordpress.android.ui.notifications.NotificationsListViewModel.InlineActionEvent
-import org.wordpress.android.ui.notifications.blocks.NoteBlockClickableSpan
-import org.wordpress.android.ui.notifications.utils.NotificationsUtilsWrapper
-import org.wordpress.android.util.GravatarUtils
-import org.wordpress.android.util.RtlUtils
-import org.wordpress.android.util.extensions.getColorFromAttribute
 import org.wordpress.android.util.extensions.indexOrNull
-import org.wordpress.android.util.image.ImageManager
-import org.wordpress.android.util.image.ImageType
-import javax.inject.Inject
-import kotlin.math.roundToInt
 
 class NotesAdapter(context: Context, private val inlineActionEvents: MutableSharedFlow<InlineActionEvent>) :
     RecyclerView.Adapter<NoteViewHolder>() {
@@ -58,12 +29,6 @@ class NotesAdapter(context: Context, private val inlineActionEvents: MutableShar
     var onScrolledToBottom = { _: Long -> }
     var currentFilter = Filter.ALL
         private set
-
-    @Inject
-    lateinit var imageManager: ImageManager
-
-    @Inject
-    lateinit var notificationsUtilsWrapper: NotificationsUtilsWrapper
 
     init {
         (context.applicationContext as WordPress).component().inject(this)
@@ -84,7 +49,7 @@ class NotesAdapter(context: Context, private val inlineActionEvents: MutableShar
      */
     fun addAll(notes: List<Note>) = coroutineScope.launch {
         val newNotes = buildFilteredNotesList(notes, currentFilter)
-        val differ = AsyncListDiffer(this@NotesAdapter, object: ItemCallback<Note>(){
+        val differ = AsyncListDiffer(this@NotesAdapter, object : ItemCallback<Note>() {
             override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean =
                 oldItem.id == newItem.id
 
@@ -109,157 +74,25 @@ class NotesAdapter(context: Context, private val inlineActionEvents: MutableShar
 
     override fun getItemCount(): Int = filteredNotes.size
 
-    private val Note.timeGroup
-        get() = Note.getTimeGroupForTimestamp(timestamp)
 
-    @StringRes
-    private fun timeGroupHeaderText(note: Note, previousNote: Note?) =
-        previousNote?.timeGroup.let { previousTimeGroup ->
-            val timeGroup = note.timeGroup
-            if (previousTimeGroup?.let { it == timeGroup } == true) {
-                // If the previous time group exists and is the same, we don't need a new one
-                null
-            } else {
-                // Otherwise, we create a new one
-                when (timeGroup) {
-                    NoteTimeGroup.GROUP_TODAY -> R.string.stats_timeframe_today
-                    NoteTimeGroup.GROUP_YESTERDAY -> R.string.stats_timeframe_yesterday
-                    NoteTimeGroup.GROUP_OLDER_TWO_DAYS -> R.string.older_two_days
-                    NoteTimeGroup.GROUP_OLDER_WEEK -> R.string.older_last_week
-                    NoteTimeGroup.GROUP_OLDER_MONTH -> R.string.older_month
-                }
-            }
-        }
-
-    @Suppress("CyclomaticComplexMethod", "LongMethod")
     override fun onBindViewHolder(noteViewHolder: NoteViewHolder, position: Int) {
         val note = filteredNotes.getOrNull(position) ?: return
         val previousNote = filteredNotes.getOrNull(position - 1)
-        noteViewHolder.binding.noteContentContainer.setOnClickListener { onNoteClicked(note.id) }
 
-        // Display time group header
-        timeGroupHeaderText(note, previousNote)?.let { timeGroupText ->
-            with(noteViewHolder.binding.headerText) {
-                visibility = View.VISIBLE
-                setText(timeGroupText)
-            }
-        } ?: run {
-            noteViewHolder.binding.headerText.visibility = View.GONE
-        }
 
-        // Subject is stored in db as html to preserve text formatting
-        var noteSubjectSpanned: Spanned = note.getFormattedSubject(notificationsUtilsWrapper)
-        // Trim the '\n\n' added by HtmlCompat.fromHtml(...)
-        noteSubjectSpanned = noteSubjectSpanned.subSequence(
-            0,
-            TextUtils.getTrimmedLength(noteSubjectSpanned)
-        ) as Spanned
-        val spans = noteSubjectSpanned.getSpans(
-            0,
-            noteSubjectSpanned.length,
-            NoteBlockClickableSpan::class.java
-        )
-        for (span in spans) {
-            span.enableColors(noteViewHolder.itemView.context)
-        }
-        noteViewHolder.binding.noteSubject.text = noteSubjectSpanned
-        val noteSubjectNoticon = note.commentSubjectNoticon
-        if (!TextUtils.isEmpty(noteSubjectNoticon)) {
-            val parent = noteViewHolder.binding.noteSubject.parent
-            // Fix position of the subject noticon in the RtL mode
-            if (parent is ViewGroup) {
-                val textDirection = if (BidiFormatter.getInstance()
-                        .isRtl(noteViewHolder.binding.noteSubject.text)
-                ) ViewCompat.LAYOUT_DIRECTION_RTL else ViewCompat.LAYOUT_DIRECTION_LTR
-                ViewCompat.setLayoutDirection(parent, textDirection)
-            }
-            // mirror noticon in the rtl mode
-            if (RtlUtils.isRtl(noteViewHolder.itemView.context)) {
-                noteViewHolder.binding.noteSubjectNoticon.scaleX = -1f
-            }
-            val textIndentSize = noteViewHolder.itemView.context.resources
-                .getDimensionPixelSize(R.dimen.notifications_text_indent_sz)
-            CommentUtils.indentTextViewFirstLine(noteViewHolder.binding.noteSubject, textIndentSize)
-            noteViewHolder.binding.noteSubjectNoticon.text = noteSubjectNoticon
-            noteViewHolder.binding.noteSubjectNoticon.visibility = View.VISIBLE
-        } else {
-            noteViewHolder.binding.noteSubjectNoticon.visibility = View.GONE
-        }
-        val noteSnippet = note.commentSubject
-        if (!TextUtils.isEmpty(noteSnippet)) {
-            handleMaxLines(noteViewHolder.binding.noteSubject, noteViewHolder.binding.noteDetail)
-            noteViewHolder.binding.noteDetail.text = noteSnippet
-            noteViewHolder.binding.noteDetail.visibility = View.VISIBLE
-        } else {
-            noteViewHolder.binding.noteDetail.visibility = View.GONE
-        }
-        noteViewHolder.loadAvatars(note)
-        noteViewHolder.bindInlineActionIconsForNote(note)
-        noteViewHolder.binding.notificationUnread.isVisible = note.isUnread
+        noteViewHolder.bindTimeGroupHeader(note, previousNote, position)
+        noteViewHolder.bindSubject(note)
+        noteViewHolder.bindSubjectNoticon(note)
+        noteViewHolder.bindContent(note)
+        noteViewHolder.bindAvatars(note)
+        noteViewHolder.bindInlineActions(note)
+        noteViewHolder.bindOthers(note, onNoteClicked)
+
 
         // request to load more comments when we near the end
         if (position >= itemCount - 1) {
             onScrolledToBottom(note.timestamp)
         }
-        val headerMarginTop: Int
-        val context = noteViewHolder.itemView.context
-        headerMarginTop = if (position == 0) {
-            context.resources
-                .getDimensionPixelSize(R.dimen.notifications_header_margin_top_position_0)
-        } else {
-            context.resources
-                .getDimensionPixelSize(R.dimen.notifications_header_margin_top_position_n)
-        }
-        val layoutParams = noteViewHolder.binding.headerText.layoutParams as MarginLayoutParams
-        layoutParams.topMargin = headerMarginTop
-        noteViewHolder.binding.headerText.layoutParams = layoutParams
-    }
-
-    private fun NoteViewHolder.loadAvatars(note: Note) {
-        if (note.shouldShowMultipleAvatars() && note.iconURLs != null && note.iconURLs!!.size > 1) {
-            val avatars = note.iconURLs!!.toList()
-            if (avatars.size == 2) {
-                binding.noteAvatar.visibility = View.INVISIBLE
-                binding.twoAvatarsView.root.visibility = View.VISIBLE
-                binding.threeAvatarsView.root.visibility = View.INVISIBLE
-                loadAvatar(binding.twoAvatarsView.twoAvatars1, avatars[1])
-                loadAvatar(binding.twoAvatarsView.twoAvatars2, avatars[0])
-            } else { // size > 3
-                binding.noteAvatar.visibility = View.INVISIBLE
-                binding.twoAvatarsView.root.visibility = View.INVISIBLE
-                binding.threeAvatarsView.root.visibility = View.VISIBLE
-                loadAvatar(binding.threeAvatarsView.threeAvatars1, avatars[2])
-                loadAvatar(binding.threeAvatarsView.threeAvatars2, avatars[1])
-                loadAvatar(binding.threeAvatarsView.threeAvatars3, avatars[0])
-            }
-        } else { // single avatar
-            binding.noteAvatar.visibility = View.VISIBLE
-            binding.twoAvatarsView.root.visibility = View.INVISIBLE
-            binding.threeAvatarsView.root.visibility = View.INVISIBLE
-            loadAvatar(binding.noteAvatar, note.iconURL)
-        }
-    }
-
-    private fun loadAvatar(imageView: ImageView, avatarUrl: String) {
-        val avatarSize = imageView.context.resources.getDimension(R.dimen.notifications_avatar_sz).roundToInt()
-        val url = GravatarUtils.fixGravatarUrl(avatarUrl, avatarSize)
-        imageManager.loadIntoCircle(imageView, ImageType.AVATAR_WITH_BACKGROUND, url)
-    }
-
-    private fun Note.shouldShowMultipleAvatars() = isFollowType || isLikeType || isCommentLikeType
-
-    private fun handleMaxLines(subject: TextView, detail: TextView) {
-        subject.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
-            override fun onPreDraw(): Boolean {
-                subject.viewTreeObserver.removeOnPreDrawListener(this)
-                if (subject.lineCount == 2) {
-                    detail.maxLines = 1
-                } else {
-                    detail.maxLines = 2
-                }
-                return false
-            }
-        })
     }
 
     fun cancelReloadLocalNotes() {
@@ -302,73 +135,6 @@ class NotesAdapter(context: Context, private val inlineActionEvents: MutableShar
                 Filter.LIKE -> note.isLikeType
             }
         }.sortedByDescending { it.timestamp }.let { result -> ArrayList(result) }
-    }
-}
-
-class NoteViewHolder(
-    val binding: NotificationsListItemBinding,
-    private val inlineActionEvents: MutableSharedFlow<InlineActionEvent>,
-    private val coroutineScope: CoroutineScope
-) : RecyclerView.ViewHolder(binding.root) {
-    fun bindInlineActionIconsForNote(note: Note) = Notification.from(note).let { notification ->
-        when (notification) {
-            Comment -> bindLikeCommentAction(note)
-            is PostNotification.NewPost -> bindLikePostAction(note)
-            is PostNotification -> bindShareAction(notification)
-            is Unknown -> {
-                binding.action.isVisible = false
-            }
-        }
-    }
-
-    private fun bindShareAction(notification: PostNotification) {
-        binding.action.setImageResource(R.drawable.block_share)
-        val color = binding.root.context.getColorFromAttribute(R.attr.wpColorOnSurfaceMedium)
-        ImageViewCompat.setImageTintList(binding.action, ColorStateList.valueOf(color))
-        binding.action.isVisible = true
-        binding.action.setOnClickListener {
-            coroutineScope.launch {
-                inlineActionEvents.emit(
-                    InlineActionEvent.SharePostButtonTapped(notification)
-                )
-            }
-        }
-    }
-
-    private fun bindLikePostAction(note: Note) {
-        if (note.canLikePost().not()) return
-        setupLikeIcon(note.hasLikedPost())
-        binding.action.setOnClickListener {
-            val liked = note.hasLikedPost().not()
-            setupLikeIcon(liked)
-            coroutineScope.launch {
-                inlineActionEvents.emit(
-                    InlineActionEvent.LikePostButtonTapped(note, liked)
-                )
-            }
-        }
-    }
-
-    private fun bindLikeCommentAction(note: Note) {
-        if (note.canLikeComment().not()) return
-        setupLikeIcon(note.hasLikedComment())
-        binding.action.setOnClickListener {
-            val liked = note.hasLikedComment().not()
-            setupLikeIcon(liked)
-            coroutineScope.launch {
-                inlineActionEvents.emit(
-                    InlineActionEvent.LikeCommentButtonTapped(note, liked)
-                )
-            }
-        }
-    }
-
-    private fun setupLikeIcon(liked: Boolean) {
-        binding.action.isVisible = true
-        binding.action.setImageResource(if (liked) R.drawable.star_filled else R.drawable.star_empty)
-        val color = if (liked) binding.root.context.getColor(R.color.inline_action_filled)
-        else binding.root.context.getColorFromAttribute(R.attr.wpColorOnSurfaceMedium)
-        ImageViewCompat.setImageTintList(binding.action, ColorStateList.valueOf(color))
     }
 }
 
