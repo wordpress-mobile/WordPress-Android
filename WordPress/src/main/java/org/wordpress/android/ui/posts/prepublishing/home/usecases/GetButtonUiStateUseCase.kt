b3b4355e620b3c9c59ecf8528537fa20c62293b6
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-02-19 17:48:59 -0500

    Add a bunch of my insane thoughts - with the final thought of using a hot observable

--- WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/home/usecases/GetButtonUiStateUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/prepublishing/home/usecases/GetButtonUiStateUseCase.kt
@@ -28,6 +28,25 @@ class GetButtonUiStateUseCase @Inject constructor(
             uploadUtilsWrapper.userCanPublish(site)
         )
 
+        // todo: annmarie @ajesh - notice that the button states are based on the editorAction - should this only be used
+        // post the first time the user opens the editor, or if it should be used every time the user opens the editor.
+        // I think only the first time - and maybe then again - we also need to show loading if media is in progress. hmmm
+        // Maybe something else goes after this ... ugh, but what if the user is offline? Or what if they are not offline
+        // and media is uploading? Can the user tap the publish button when media is uploading? If we are only
+        // getting the media state once something happens?????
+
+        // So you can't press the publish button immediately when media is uploading; but you can press it while media is
+        // actually uploading. At which time the bottom sheet doesn't know anything about the media upload state until the
+        // next event comes in or it may never come in.
+
+        // How can we check the media state? Is this even possible?? Who can we poll? The media store?
+
+        // Are we moving media upload to sync??? I don't think so
+
+        // None of these states should show if media is uploading. We need to show a loading state if media is uploading.
+
+        // Okay, so what if we used MutableStateFlow- which, as a hot observable, will emit the last value to new subscribers???
+        // This could work!
         return when (editorAction) {
             PrimaryEditorAction.PUBLISH_NOW -> PublishButtonUiState(onButtonClicked)
             PrimaryEditorAction.SCHEDULE -> ScheduleButtonUiState(onButtonClicked)
