8c2c169093cc5cec65aa57e26c90817fb3a440f2
Ravi <ravi.program@gmail.com>
2024-02-22 13:15:15 +1100

    Update InsightsManagementMapper.kt
    Remove Today Stats from Insights when TrafficTab is enabled

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/insights/management/InsightsManagementMapper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/insights/management/InsightsManagementMapper.kt
@@ -33,6 +33,7 @@ import org.wordpress.android.ui.stats.refresh.lists.sections.insights.management
 import org.wordpress.android.ui.stats.refresh.lists.sections.insights.management.InsightsManagementViewModel.InsightListItem.InsightModel.Status.ADDED
 import org.wordpress.android.ui.stats.refresh.lists.sections.insights.management.InsightsManagementViewModel.InsightListItem.InsightModel.Status.REMOVED
 import org.wordpress.android.ui.utils.ListItemInteraction
+import org.wordpress.android.util.config.StatsTrafficTabFeatureConfig
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -47,17 +48,23 @@ private val ACTIVITY_INSIGHTS = mutableListOf(
 private val GENERAL_INSIGHTS = mutableListOf(
     ALL_TIME_STATS,
     MOST_POPULAR_DAY_AND_HOUR,
-    ANNUAL_SITE_STATS,
-    TODAY_STATS
+    ANNUAL_SITE_STATS
 )
 
 class InsightsManagementMapper @Inject constructor(
-    @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher
+    @Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
+    private val trafficTabFeatureConfig: StatsTrafficTabFeatureConfig
 ) {
     suspend fun buildUIModel(addedTypes: Set<InsightType>, onClick: (InsightType) -> Unit) =
         withContext(bgDispatcher) {
             val insightListItems = mutableListOf<InsightListItem>()
             insightListItems += Header(R.string.stats_insights_management_general)
+            if (BuildConfig.IS_JETPACK_APP &&
+                !trafficTabFeatureConfig.isEnabled() &&
+                !GENERAL_INSIGHTS.contains(TODAY_STATS)
+            ) {
+                GENERAL_INSIGHTS.add(TODAY_STATS)
+            }
             if (BuildConfig.IS_JETPACK_APP && !GENERAL_INSIGHTS.contains(VIEWS_AND_VISITORS)) {
                 GENERAL_INSIGHTS.add(0, VIEWS_AND_VISITORS)
             }
