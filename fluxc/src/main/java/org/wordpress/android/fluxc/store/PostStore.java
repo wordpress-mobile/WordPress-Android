a42f1df6ef9db56cd882b8b5a1ca477caa3c28f5
Pantelis Stampoulis <pantelis.stampoulis@gmail.com>
2024-02-23 12:40:17 +0200

    Fix checkstyle issues

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/PostStore.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/PostStore.java
@@ -1055,7 +1055,12 @@ public class PostStore extends Store {
 
         if (payload.isError()) {
             OnPostChanged event = new OnPostChanged(
-                    new CauseOfOnPostChanged.UpdatePost(payload.post.getId(), payload.post.getRemotePostId(), false), 0);
+                    new CauseOfOnPostChanged.UpdatePost(
+                            payload.post.getId(),
+                            payload.post.getRemotePostId(),
+                            false),
+                    0
+            );
             event.error = payload.error;
             emitChange(event);
         } else {
@@ -1127,7 +1132,11 @@ public class PostStore extends Store {
             post.setDateLocallyChanged((DateTimeUtils.iso8601UTCFromDate(new Date())));
         }
         int rowsAffected = mPostSqlUtils.insertOrUpdatePostOverwritingLocalChanges(post);
-        CauseOfOnPostChanged causeOfChange = new CauseOfOnPostChanged.UpdatePost(post.getId(), post.getRemotePostId(), isLocalUpdate);
+        CauseOfOnPostChanged causeOfChange = new CauseOfOnPostChanged.UpdatePost(
+                post.getId(),
+                post.getRemotePostId(),
+                isLocalUpdate
+        );
         OnPostChanged onPostChanged = new OnPostChanged(causeOfChange, rowsAffected);
         emitChange(onPostChanged);
 
