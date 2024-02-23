cd4a73663f27d0668df3955c4a1544871c7a8b7f
Ravi <ravi.program@gmail.com>
2024-02-23 13:46:17 +1100

    Update BarChartLabelFormatter.kt
    update range to start at 0

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/BarChartLabelFormatter.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/BarChartLabelFormatter.kt
@@ -10,7 +10,7 @@ class BarChartLabelFormatter @Inject constructor(
 ) : ValueFormatter() {
     override fun getAxisLabel(value: Float, axis: AxisBase?): String {
         val index = value.toInt()
-        return if (index in 1..entries.size) {
+        return if (index in 0..entries.size) {
             entries[index].label
         } else {
             ""
