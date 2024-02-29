f846a607fad3f220b16ee10ce66f02461b314f65
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-02-29 18:37:03 +0100

    Simplify new Blaze campaigns rest client

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/blaze/BlazeCampaignsRestClient.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/blaze/BlazeCampaignsRestClient.kt
@@ -1,35 +1,14 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.blaze
 
-import android.content.Context
-import com.android.volley.RequestQueue
-import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.endpoint.WPCOMV2
-import org.wordpress.android.fluxc.network.AcceptHeaderStrategy
-import org.wordpress.android.fluxc.network.UserAgent
-import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
-import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Error
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Success
-import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
+import org.wordpress.android.fluxc.network.rest.wpcom.WPComNetwork
 import org.wordpress.android.fluxc.utils.extensions.putIfNotNull
 import javax.inject.Inject
-import javax.inject.Named
 
 class BlazeCampaignsRestClient @Inject constructor(
-    private val wpComGsonRequestBuilder: WPComGsonRequestBuilder,
-    dispatcher: Dispatcher,
-    appContext: Context?,
-    @Named("regular") requestQueue: RequestQueue,
-    accessToken: AccessToken,
-    userAgent: UserAgent,
-    acceptStrategy: AcceptHeaderStrategy.JsonAcceptHeader
-) : BaseWPComRestClient(
-    appContext,
-    dispatcher,
-    requestQueue,
-    accessToken,
-    userAgent,
-    acceptStrategy
+    private val wpComNetwork: WPComNetwork
 ) {
     companion object {
         const val DEFAULT_ITEMS_LIMIT = 25 //Number of items to fetch in a single request
@@ -42,17 +21,16 @@ class BlazeCampaignsRestClient @Inject constructor(
         locale: String,
         status: String? = null,
     ): BlazeCampaignsFetchedPayload<BlazeCampaignListResponse> {
-        val url = "${WPCOMV2.sites.site(siteId).wordads.dsp.api.v1_1.campaigns}"
-        val response = wpComGsonRequestBuilder.syncGetRequest(
-            this,
-            url,
-            mutableMapOf(
+        val url = WPCOMV2.sites.site(siteId).wordads.dsp.api.v1_1.campaigns.url
+        val response = wpComNetwork.executeGetGsonRequest(
+            url = url,
+            params = mutableMapOf(
                 "site_id" to siteId.toString(),
                 "skip" to skip.toString(),
                 "limit" to limit.toString(),
                 "locale" to locale
             ).putIfNotNull("status" to status),
-            BlazeCampaignListResponse::class.java
+            clazz = BlazeCampaignListResponse::class.java
         )
         return when (response) {
             is Success -> BlazeCampaignsFetchedPayload(response.data)
