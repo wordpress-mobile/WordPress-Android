a38e2a72ca6ac3a20b478de84f7e1978369873a8
jorgemucientesfayos <jorge.mucientes@automattic.com>
2024-02-29 16:49:07 +0100

    Remove old models and rest client for fetching Blaze campaigns

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/blaze/BlazeCampaignsModel.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/blaze/BlazeCampaignsModel.kt
@@ -4,31 +4,11 @@ import java.util.Date
 
 data class BlazeCampaignsModel(
     val campaigns: List<BlazeCampaignModel> = emptyList(),
-    val page: Int,
-    val totalItems: Int,
-    val totalPages: Int
-)
-
-data class BlazeCampaignModel(
-    val campaignId: Int,
-    val title: String,
-    val imageUrl: String?,
-    val createdAt: Date,
-    val endDate: Date?,
-    val uiStatus: String,
-    val budgetCents: Long,
-    val impressions: Long,
-    val clicks: Long,
-    val targetUrn: String?
-)
-
-data class BlazeCampaignsModelNew(
-    val campaigns: List<BlazeCampaignModelNew> = emptyList(),
     val skipped: Int,
     val totalItems: Int,
 )
 
-data class BlazeCampaignModelNew(
+data class BlazeCampaignModel(
     val campaignId: Int,
     val title: String,
     val imageUrl: String?,
--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/blaze/BlazeCampaignsRestClient.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/blaze/BlazeCampaignsRestClient.kt
@@ -3,24 +3,19 @@ package org.wordpress.android.fluxc.network.rest.wpcom.blaze
 import android.content.Context
 import com.android.volley.RequestQueue
 import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.Payload
 import org.wordpress.android.fluxc.generated.endpoint.WPCOMV2
-import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.AcceptHeaderStrategy
-import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType
 import org.wordpress.android.fluxc.network.UserAgent
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
-import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequest.WPComGsonNetworkError
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Error
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Success
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
-import org.wordpress.android.fluxc.store.Store.OnChangedError
+import org.wordpress.android.fluxc.utils.extensions.putIfNotNull
+import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Named
-import javax.inject.Singleton
 
-@Singleton
 class BlazeCampaignsRestClient @Inject constructor(
     private val wpComGsonRequestBuilder: WPComGsonRequestBuilder,
     dispatcher: Dispatcher,
@@ -29,70 +24,40 @@ class BlazeCampaignsRestClient @Inject constructor(
     accessToken: AccessToken,
     userAgent: UserAgent,
     acceptStrategy: AcceptHeaderStrategy.JsonAcceptHeader
-) : BaseWPComRestClient(appContext, dispatcher, requestQueue, accessToken, userAgent, acceptStrategy) {
+) : BaseWPComRestClient(
+    appContext,
+    dispatcher,
+    requestQueue,
+    accessToken,
+    userAgent,
+    acceptStrategy
+) {
+    companion object {
+        const val DEFAULT_LIMIT = 25
+    }
+
     suspend fun fetchBlazeCampaigns(
-        site: SiteModel,
-        page: Int = 1
-    ): BlazeCampaignsFetchedPayload<BlazeCampaignsResponse> {
-        val url = "${WPCOMV2.sites.site(site.siteId).wordads.dsp.api.v1.search.campaigns.site.url}${site.siteId}"
+        siteId: Long,
+        skip: Int = 0,
+        limit: Int = DEFAULT_LIMIT,
+        locale: String = Locale.getDefault().country,
+        status: String? = null,
+    ): BlazeCampaignsFetchedPayload<BlazeCampaignListResponse> {
+        val url = "${WPCOMV2.sites.site(siteId).wordads.dsp.api.v1_1.campaigns}"
         val response = wpComGsonRequestBuilder.syncGetRequest(
             this,
             url,
-            mapOf(
-                "page" to page.toString(),
-                "order" to "start_date",
-                "sort" to "desc"
-            ),
-            BlazeCampaignsResponse::class.java
+            mutableMapOf(
+                "site_id" to siteId.toString(),
+                "skip" to skip.toString(),
+                "limit" to limit.toString(),
+                "locale" to locale
+            ).putIfNotNull("status" to status),
+            BlazeCampaignListResponse::class.java
         )
         return when (response) {
             is Success -> BlazeCampaignsFetchedPayload(response.data)
             is Error -> BlazeCampaignsFetchedPayload(response.error.toBlazeCampaignsError())
         }
     }
-}
-
-data class BlazeCampaignsFetchedPayload<T>(
-    val response: T? = null
-) : Payload<BlazeCampaignsError>() {
-    constructor(error: BlazeCampaignsError) : this() {
-        this.error = error
-    }
-}
-
-class BlazeCampaignsError
-@JvmOverloads constructor(
-    val type: BlazeCampaignsErrorType,
-    val message: String? = null
-) : OnChangedError
-
-enum class BlazeCampaignsErrorType {
-    GENERIC_ERROR,
-    AUTHORIZATION_REQUIRED,
-    INVALID_RESPONSE,
-    API_ERROR,
-    TIMEOUT
-}
-
-fun WPComGsonNetworkError.toBlazeCampaignsError(): BlazeCampaignsError {
-    val type = when (type) {
-        GenericErrorType.TIMEOUT -> BlazeCampaignsErrorType.TIMEOUT
-        GenericErrorType.NO_CONNECTION,
-        GenericErrorType.SERVER_ERROR,
-        GenericErrorType.INVALID_SSL_CERTIFICATE,
-        GenericErrorType.NETWORK_ERROR -> BlazeCampaignsErrorType.API_ERROR
-
-        GenericErrorType.PARSE_ERROR,
-        GenericErrorType.NOT_FOUND,
-        GenericErrorType.CENSORED,
-        GenericErrorType.INVALID_RESPONSE -> BlazeCampaignsErrorType.INVALID_RESPONSE
-
-        GenericErrorType.HTTP_AUTH_ERROR,
-        GenericErrorType.AUTHORIZATION_REQUIRED,
-        GenericErrorType.NOT_AUTHENTICATED -> BlazeCampaignsErrorType.AUTHORIZATION_REQUIRED
-
-        GenericErrorType.UNKNOWN,
-        null -> BlazeCampaignsErrorType.GENERIC_ERROR
-    }
-    return BlazeCampaignsError(type, message)
 }
