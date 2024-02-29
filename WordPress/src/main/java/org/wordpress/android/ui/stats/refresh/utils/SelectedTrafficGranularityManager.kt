ce36e30f7c1c9a25f8d39aa473ccde15920e5d1d
Irfan Omur <irfan.omur@automattic.com>
2024-02-29 13:53:54 +0300

    Make `SelectedTrafficGranularityManager` Singleton
    This will help all classes use the same instance of `SelectedTrafficGranularityManager`

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/SelectedTrafficGranularityManager.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/utils/SelectedTrafficGranularityManager.kt
@@ -4,11 +4,12 @@ import android.content.SharedPreferences
 import org.wordpress.android.fluxc.network.utils.StatsGranularity
 import org.wordpress.android.fluxc.network.utils.StatsGranularity.DAYS
 import javax.inject.Inject
+import javax.inject.Singleton
 
 const val SELECTED_TRAFFIC_GRANULARITY_KEY = "SELECTED_TRAFFIC_GRANULARITY_KEY"
 
-class SelectedTrafficGranularityManager
-@Inject constructor(private val sharedPrefs: SharedPreferences) {
+@Singleton
+class SelectedTrafficGranularityManager @Inject constructor(private val sharedPrefs: SharedPreferences) {
     fun getSelectedTrafficGranularity(): StatsGranularity {
         val value = sharedPrefs.getString(SELECTED_TRAFFIC_GRANULARITY_KEY, DAYS.name)
         return value?.let { StatsGranularity.valueOf(value) } ?: DAYS
