e0fb47b02ba5c4f9d241336d30538a1bdf8ac22a
Antonis Lilis <antonis.lilis@automattic.com>
2024-03-05 16:41:09 +0200

    Only check the US country code if Clarity analytics are enabled

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/privacy/GeoRepository.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/privacy/GeoRepository.kt
@@ -4,12 +4,14 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.network.rest.wpcom.geo.WpComGeoRestClient
 import org.wordpress.android.modules.IO_THREAD
+import org.wordpress.android.util.config.ClarityAnalyticsTrackingConfig
 import javax.inject.Inject
 import javax.inject.Named
 
 class GeoRepository @Inject constructor(
     @Named(IO_THREAD) val ioDispatcher: CoroutineDispatcher,
     private val wpComGeoRestClient: WpComGeoRestClient,
+    private val clarityAnalyticsTrackingConfig: ClarityAnalyticsTrackingConfig,
 ) {
     private suspend fun fetchCountryCode() = withContext(ioDispatcher) {
         wpComGeoRestClient.fetchCountryCode().map { it.orEmpty() }
@@ -17,11 +19,15 @@ class GeoRepository @Inject constructor(
 
     /**
      * @return true if the user is in a country where privacy policy compliance is required.
-     * Includes GDPR countries and the US.
+     * Includes GDPR countries and the US if Clarity analytics tracking is enabled.
      */
     suspend fun isPrivacyPolicyComplianceRequired() = fetchCountryCode().fold(
         onSuccess = { countryCode ->
-            countryCode.uppercase() in (GDPR_COUNTRY_CODES + US_COUNTRY_CODE)
+            if (clarityAnalyticsTrackingConfig.isEnabled()) {
+                countryCode.uppercase() in (GDPR_COUNTRY_CODES + US_COUNTRY_CODE)
+            } else {
+                countryCode.uppercase() in GDPR_COUNTRY_CODES
+            }
         },
         onFailure = {
             false
