ec806f3e63903664e8be5c9d7ecc3865c4bae29d
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-03-07 14:04:23 -0800

    Update coloring of bar

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/traffic/TrafficBarChartViewHolder.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/traffic/TrafficBarChartViewHolder.kt
@@ -178,6 +178,15 @@ class TrafficBarChartViewHolder(parent: ViewGroup) : BlockListItemViewHolder(
     private fun buildDataSet(context: Context, cut: List<BarEntry>): BarDataSet {
         val dataSet = BarDataSet(cut, "Data")
         dataSet.color = ContextCompat.getColor(context, R.color.blue_50)
+        dataSet.setGradientColor(
+            ContextCompat.getColor(
+                context,
+                R.color.blue_50
+            ), ContextCompat.getColor(
+                context,
+                R.color.blue_50
+            )
+        )
         dataSet.formLineWidth = 0f
         dataSet.setDrawValues(false)
 
