6291d17ceb8e6480f51c4adb7d11364ffb0f6aa2
Carlos Garcia <fluiddot@gmail.com>
2024-03-07 14:29:22 +0100

    Add missing import in `GutenbergEditorFragment`

--- libs/editor/src/main/java/org/wordpress/android/editor/gutenberg/GutenbergEditorFragment.java
+++ libs/editor/src/main/java/org/wordpress/android/editor/gutenberg/GutenbergEditorFragment.java
@@ -94,6 +94,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
