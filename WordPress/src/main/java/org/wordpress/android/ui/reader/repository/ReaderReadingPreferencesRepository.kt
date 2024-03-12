418ae7eef9eb130409b5a717fb457e4707566abf
Thomas Horta <thomas.horta@automattic.com>
2024-03-12 18:43:14 -0300

    Remove unused variables

--- WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderReadingPreferencesRepository.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/repository/ReaderReadingPreferencesRepository.kt
@@ -4,7 +4,6 @@ import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import org.wordpress.android.modules.IO_THREAD
-import org.wordpress.android.modules.UI_THREAD
 import org.wordpress.android.ui.prefs.AppPrefsWrapper
 import org.wordpress.android.ui.reader.models.ReaderReadingPreferences
 import javax.inject.Inject
@@ -13,7 +12,6 @@ import javax.inject.Named
 class ReaderReadingPreferencesRepository @Inject constructor(
     private val appPrefsWrapper: AppPrefsWrapper,
     @Named(IO_THREAD) private val ioDispatcher: CoroutineDispatcher,
-    @Named(UI_THREAD) private val mainDispatcher: CoroutineDispatcher,
 ) {
     private val gson = Gson()
 
@@ -27,9 +25,5 @@ class ReaderReadingPreferencesRepository @Inject constructor(
 
     suspend fun saveReadingPreferences(preferences: ReaderReadingPreferences): Unit = withContext(ioDispatcher) {
         appPrefsWrapper.readerReadingPreferencesJson = gson.toJson(preferences)
-    }
-
-    companion object {
-        private const val READER_READING_PREFERENCES = "reader_reading_preferences"
     }
 }
