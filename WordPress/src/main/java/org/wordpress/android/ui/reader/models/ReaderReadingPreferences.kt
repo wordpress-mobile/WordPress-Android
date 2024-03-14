48f186dc08daa809db331221bdfb4a2f0891f422
Thomas Horta <thomas.horta@automattic.com>
2024-03-14 19:28:19 -0300

    Add style to Reading Preferences and fix color parsing

--- WordPress/src/main/java/org/wordpress/android/ui/reader/models/ReaderReadingPreferences.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/models/ReaderReadingPreferences.kt
@@ -4,38 +4,44 @@ package org.wordpress.android.ui.reader.models
 
 import android.content.Context
 import android.graphics.Color
+import androidx.annotation.StyleRes
 import androidx.core.content.res.ResourcesCompat
 import org.wordpress.android.R
-import org.wordpress.android.util.HtmlUtils
 import org.wordpress.android.util.extensions.getColorFromAttributeOrRes
+import java.util.Locale
 import com.google.android.material.R as MaterialR
 
-data class ReaderReadingPreferences(
+data class ReaderReadingPreferences @JvmOverloads constructor(
     val theme: Theme,
     val fontFamily: FontFamily = FontFamily.SANS_SERIF,
     val textSizeMultiplier: Float = 1.0f,
 ) {
     enum class Theme(
+        @StyleRes val style: Int,
         val backgroundColorRes: Int,
         val baseTextColorRes: Int,
         val linkColorRes: Int,
     ) {
         PAPER(
+            style = R.style.ReaderTheme_Paper,
             backgroundColorRes = R.color.reader_theme_paper_background,
             baseTextColorRes = R.color.reader_theme_paper_text,
             linkColorRes = R.color.reader_post_body_link,
         ),
         OLED(
+            style = R.style.ReaderTheme_OLED,
             backgroundColorRes = R.color.reader_theme_oled_background,
             baseTextColorRes = R.color.reader_theme_oled_text,
             linkColorRes = R.color.reader_post_body_link,
         ),
         SEPIA(
+            style = R.style.ReaderTheme_Sepia,
             backgroundColorRes = R.color.reader_theme_sepia_background,
             baseTextColorRes = R.color.reader_theme_sepia_text,
             linkColorRes = R.color.reader_post_body_link,
         ),
         SYSTEM(
+            style = R.style.ReaderTheme_System,
             backgroundColorRes = MaterialR.attr.colorSurface,
             baseTextColorRes = MaterialR.attr.colorOnSurface,
             linkColorRes = R.color.reader_post_body_link,
@@ -64,8 +70,11 @@ data class ReaderReadingPreferences(
             val emphasisLow = ResourcesCompat.getFloat(resources, R.dimen.emphasis_low)
             val emphasisDisabled = ResourcesCompat.getFloat(resources, MaterialR.dimen.material_emphasis_disabled)
 
-            cssBackgroundColor = HtmlUtils.colorResToHtmlColor(context, theme.backgroundColorRes)
-            cssLinkColor = HtmlUtils.colorResToHtmlColor(context, theme.linkColorRes)
+            val backgroundColor = context.getColorFromAttributeOrRes(theme.backgroundColorRes)
+            cssBackgroundColor = colorToHtmlColor(backgroundColor)
+
+            val linkColor = context.getColorFromAttributeOrRes(theme.linkColorRes)
+            cssLinkColor = colorToHtmlColor(linkColor)
 
             val baseTextColor = context.getColorFromAttributeOrRes(theme.baseTextColorRes)
             val baseTextColorR = Color.red(baseTextColor)
@@ -83,8 +92,13 @@ data class ReaderReadingPreferences(
         companion object {
             private const val HTML_RGBA_TEMPLATE = "rgba(%d, %d, %d, %.2f)"
 
+            @JvmStatic
             fun from(context: Context, theme: Theme): ThemeValues {
                 return ThemeValues(context, theme)
+            }
+
+            private fun colorToHtmlColor(color: Int): String {
+                return String.format(Locale.US, "#%06X", 0xFFFFFF and color)
             }
 
             private fun htmlRgbaColor(red: Int, green: Int, blue: Int, alpha: Float): String {
