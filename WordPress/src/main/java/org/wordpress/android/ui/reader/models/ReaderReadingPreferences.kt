5e5fc52937ecff693027dee9dbe76392c77f92cf
Thomas Horta <thomas.horta@automattic.com>
2024-03-15 18:07:59 -0300

    Prepare ReadingPreferences for the Preferences modal

--- WordPress/src/main/java/org/wordpress/android/ui/reader/models/ReaderReadingPreferences.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/models/ReaderReadingPreferences.kt
@@ -4,6 +4,7 @@ package org.wordpress.android.ui.reader.models
 
 import android.content.Context
 import android.graphics.Color
+import androidx.annotation.StringRes
 import androidx.annotation.StyleRes
 import androidx.core.content.res.ResourcesCompat
 import org.wordpress.android.R
@@ -13,34 +14,39 @@ import com.google.android.material.R as MaterialR
 
 data class ReaderReadingPreferences @JvmOverloads constructor(
     val theme: Theme = Theme.SYSTEM,
-    val fontFamily: FontFamily = FontFamily.SANS_SERIF,
+    val fontFamily: FontFamily = FontFamily.SANS,
     val fontSize: FontSize = FontSize.DEFAULT,
 ) {
     enum class Theme(
+        @StringRes val displayNameRes: Int,
         @StyleRes val style: Int,
         val backgroundColorRes: Int,
         val baseTextColorRes: Int,
         val linkColorRes: Int,
     ) {
         PAPER(
+            displayNameRes = R.string.reader_preferences_theme_paper,
             style = R.style.ReaderTheme_Paper,
             backgroundColorRes = R.color.reader_theme_paper_background,
             baseTextColorRes = R.color.reader_theme_paper_text,
             linkColorRes = R.color.reader_post_body_link,
         ),
         OLED(
+            displayNameRes = R.string.reader_preferences_theme_oled,
             style = R.style.ReaderTheme_OLED,
             backgroundColorRes = R.color.reader_theme_oled_background,
             baseTextColorRes = R.color.reader_theme_oled_text,
             linkColorRes = R.color.reader_post_body_link,
         ),
         SEPIA(
+            displayNameRes = R.string.reader_preferences_theme_sepia,
             style = R.style.ReaderTheme_Sepia,
             backgroundColorRes = R.color.reader_theme_sepia_background,
             baseTextColorRes = R.color.reader_theme_sepia_text,
             linkColorRes = R.color.reader_post_body_link,
         ),
         SYSTEM(
+            displayNameRes = R.string.reader_preferences_theme_system,
             style = R.style.ReaderTheme_System,
             backgroundColorRes = MaterialR.attr.colorSurface,
             baseTextColorRes = MaterialR.attr.colorOnSurface,
@@ -61,7 +67,11 @@ data class ReaderReadingPreferences @JvmOverloads constructor(
         val cssTextExtraLightColor: String
         val cssTextDisabledColor: String
 
-        // TODO int color values (to be used on Views/Compose)
+        // Int color values
+        val intBackgroundColor: Int
+        val intBaseTextColor: Int
+        val intTextColor: Int
+        val intLinkColor: Int
 
         init {
             val resources = context.resources
@@ -70,18 +80,24 @@ data class ReaderReadingPreferences @JvmOverloads constructor(
             val emphasisLow = ResourcesCompat.getFloat(resources, R.dimen.emphasis_low)
             val emphasisDisabled = ResourcesCompat.getFloat(resources, MaterialR.dimen.material_emphasis_disabled)
 
-            val backgroundColor = context.getColorFromAttributeOrRes(theme.backgroundColorRes)
-            cssBackgroundColor = colorToHtmlColor(backgroundColor)
+            intBackgroundColor = context.getColorFromAttributeOrRes(theme.backgroundColorRes)
+            cssBackgroundColor = colorToHtmlColor(intBackgroundColor)
 
-            val linkColor = context.getColorFromAttributeOrRes(theme.linkColorRes)
-            cssLinkColor = colorToHtmlColor(linkColor)
+            intLinkColor = context.getColorFromAttributeOrRes(theme.linkColorRes)
+            cssLinkColor = colorToHtmlColor(intLinkColor)
 
-            val baseTextColor = context.getColorFromAttributeOrRes(theme.baseTextColorRes)
-            val baseTextColorR = Color.red(baseTextColor)
-            val baseTextColorG = Color.green(baseTextColor)
-            val baseTextColorB = Color.blue(baseTextColor)
+            intBaseTextColor = context.getColorFromAttributeOrRes(theme.baseTextColorRes)
+            val baseTextColorR = Color.red(intBaseTextColor)
+            val baseTextColorG = Color.green(intBaseTextColor)
+            val baseTextColorB = Color.blue(intBaseTextColor)
 
             // same colors/emphasis as ReaderResourceVars class
+            intTextColor = Color.argb(
+                (emphasisHigh * 255 + 0.5f).toInt(),
+                baseTextColorR,
+                baseTextColorG,
+                baseTextColorB
+            )
             cssTextColor = htmlRgbaColor(baseTextColorR, baseTextColorG, baseTextColorB, emphasisHigh)
             cssTextMediumColor = htmlRgbaColor(baseTextColorR, baseTextColorG, baseTextColorB, emphasisMedium)
             cssTextLightColor = htmlRgbaColor(baseTextColorR, baseTextColorG, baseTextColorB, emphasisDisabled)
@@ -108,10 +124,22 @@ data class ReaderReadingPreferences @JvmOverloads constructor(
         }
     }
 
-    enum class FontFamily(val value: String) {
-        SERIF("serif"),
-        SANS_SERIF("sans-serif"),
-        MONOSPACE("monospace"),
+    enum class FontFamily(
+        @StringRes val displayNameRes: Int,
+        val value: String,
+    ) {
+        SERIF(
+            displayNameRes = R.string.reader_preferences_font_family_serif,
+            value = "serif",
+        ),
+        SANS(
+            displayNameRes = R.string.reader_preferences_font_family_sans,
+            value = "sans-serif",
+        ),
+        MONO(
+            displayNameRes = R.string.reader_preferences_font_family_mono,
+            value = "monospace",
+        ),
     }
 
     enum class FontSize(val value: Int) {
