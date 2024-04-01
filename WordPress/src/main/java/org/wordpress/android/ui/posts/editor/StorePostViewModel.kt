50fa72c7e1e39b25b62282b72d670389a400355f
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-04-01 13:57:42 -0400

    Fix detekt

--- WordPress/src/main/java/org/wordpress/android/ui/posts/editor/StorePostViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/editor/StorePostViewModel.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.ui.posts.editor
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.CoroutineDispatcher
