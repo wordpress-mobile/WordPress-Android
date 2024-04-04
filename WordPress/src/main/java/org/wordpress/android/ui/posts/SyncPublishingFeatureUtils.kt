80e9497fd3a8b5f512d9f0fc4fe68b835d3283aa
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-04-04 11:47:46 -0400

    Remove check for isJetpackApp and applied name change from isConflictionResolution to shouldSkipConflictResolutionCheck

--- WordPress/src/main/java/org/wordpress/android/ui/posts/SyncPublishingFeatureUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/SyncPublishingFeatureUtils.kt
@@ -1,17 +1,14 @@
 package org.wordpress.android.ui.posts
 
 import org.wordpress.android.fluxc.store.PostStore.RemotePostPayload
-import org.wordpress.android.util.BuildConfigWrapper
 import org.wordpress.android.util.config.SyncPublishingFeatureConfig
 import javax.inject.Inject
 
 class SyncPublishingFeatureUtils @Inject constructor(
-    private val syncPublishingFeatureConfig: SyncPublishingFeatureConfig,
-    private val buildConfigWrapper: BuildConfigWrapper,
+    private val syncPublishingFeatureConfig: SyncPublishingFeatureConfig
 ) {
     private fun isSyncPublishingEnabled(): Boolean {
-        return buildConfigWrapper.isJetpackApp &&
-                syncPublishingFeatureConfig.isEnabled()
+        return syncPublishingFeatureConfig.isEnabled()
     }
 
     /**
@@ -20,12 +17,12 @@ class SyncPublishingFeatureUtils @Inject constructor(
      * has already been uploaded from another device. This functionality should be encapsulated
      * by the SYNC_PUBLISHING feature flag. The function is used to generate the final RemotePostPayload
      * that is sent to the backend through PostActionBuilder.newPushPostAction(). By setting the
-     * isConflictResolution = true, "if_not_modified_since" is not sent to server and the post overwrites
+     * shouldSkipConflictResolutionCheck = true, "if_not_modified_since" is not sent to server and the post overwrites
      * the remote version.
      */
     fun getRemotePostPayloadForPush(payload: RemotePostPayload): RemotePostPayload {
         if (isSyncPublishingEnabled().not()) {
-            payload.isConflictResolution = true
+            payload.shouldSkipConflictResolutionCheck = true
         }
         return payload
     }
