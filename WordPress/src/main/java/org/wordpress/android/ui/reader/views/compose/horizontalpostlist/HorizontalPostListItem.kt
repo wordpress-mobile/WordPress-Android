522cc73cd508e0160e555258f3acd8f80e35aebb
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2024-04-08 11:41:28 -0300

    WIP HorizontalPostListItem

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/horizontalpostlist/HorizontalPostListItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/horizontalpostlist/HorizontalPostListItem.kt
@@ -1,7 +1,34 @@
 package org.wordpress.android.ui.reader.views.compose.horizontalpostlist
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.wordpress.android.ui.compose.theme.AppTheme
 
 @Composable
 fun HorizontalPostListItem() {
+    Column(modifier = Modifier.width(240.dp)) {
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HorizontalPostListItemPreview() {
+    AppTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            HorizontalPostListItem()
+        }
+    }
 }
