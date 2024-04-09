2f5c8883b44c7ecc7d3e7e0f4f83085ad8a333ba
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2024-04-09 20:42:07 -0300

    Implement post image UI element

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/horizontalpostlist/HorizontalPostListItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/horizontalpostlist/HorizontalPostListItem.kt
@@ -1,22 +1,32 @@
 package org.wordpress.android.ui.reader.views.compose.horizontalpostlist
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import org.wordpress.android.R
 import org.wordpress.android.ui.compose.theme.AppColor
 import org.wordpress.android.ui.compose.theme.AppTheme
 import org.wordpress.android.ui.compose.unit.Margin
@@ -24,12 +34,15 @@ import org.wordpress.android.ui.compose.unit.Margin
 @Composable
 fun HorizontalPostListItem(
     siteName: String,
+    siteImageUrl: String,
     postDateLine: String,
     postTitle: String,
     postExcerpt: String,
-    onPostSiteImageClick: () -> Unit,
+    postImageUrl: String,
+    onSiteImageClick: () -> Unit,
+    onPostImageClick: () -> Unit,
 ) {
-    val black87AlphaColor = AppColor.Black.copy(
+    val primaryElementColor = AppColor.Black.copy(
         alpha = 0.87F
     )
     Column(modifier = Modifier.width(240.dp)) {
@@ -37,25 +50,25 @@ fun HorizontalPostListItem(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            val secondaryElementColor = MaterialTheme.colorScheme.onSurface.copy(
-                alpha = 0.6F
-            )
             // Site image
-            HorizontalPostListItemSiteImage(
+            SiteImage(
                 modifier = Modifier.padding(
-                    end = Margin.Small.value
+                    end = Margin.Small.value,
                 ),
-                imageUrl = "",
-                onClick = { onPostSiteImageClick() },
+                imageUrl = siteImageUrl,
+                onClick = { onSiteImageClick() },
             )
             // Site name
             Text(
                 modifier = Modifier.weight(1F),
                 text = siteName,
                 style = MaterialTheme.typography.labelLarge,
-                color = black87AlphaColor,
+                color = primaryElementColor,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
+            )
+            val secondaryElementColor = MaterialTheme.colorScheme.onSurface.copy(
+                alpha = 0.6F
             )
             // "•" separator
             Text(
@@ -87,11 +100,58 @@ fun HorizontalPostListItem(
             modifier = Modifier.padding(top = Margin.Small.value),
             text = postExcerpt,
             style = MaterialTheme.typography.bodySmall,
-            color = black87AlphaColor,
+            color = primaryElementColor,
             maxLines = 2,
             overflow = TextOverflow.Ellipsis,
         )
+        // Post image
+        PostImage(
+            imageUrl = postImageUrl,
+            onClick = onPostImageClick,
+        )
     }
+}
+
+@Composable
+fun SiteImage(
+    imageUrl: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    AsyncImage(
+        modifier = modifier
+            .size(20.dp)
+            .clip(CircleShape)
+            .clickable { onClick() },
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .error(R.drawable.bg_oval_placeholder_image_32dp)
+            .crossfade(true)
+            .build(),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun PostImage(
+    imageUrl: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    AsyncImage(
+        modifier = modifier
+            .width(240.dp)
+            .height(150.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(8.dp)))
+            .clickable { onClick() },
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            // TODO RenanLukas: placeholder
+            // .error(R.drawable.bg_oval_placeholder_image_32dp)
+            .crossfade(true)
+            .build(),
+        contentDescription = null
+    )
 }
 
 @Preview
@@ -107,12 +167,15 @@ fun HorizontalPostListItemPreview() {
             HorizontalPostListItem(
                 siteName = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque sapien sed" +
                         " urna fermentum posuere. Vivamus in pretium nisl.",
+                siteImageUrl = "",
                 postDateLine = "1h",
                 postTitle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque sapien " +
                         "sed urna fermentum posuere. Vivamus in pretium nisl.",
                 postExcerpt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque sapien" +
                         " sed urna fermentum posuere. Vivamus in pretium nisl.",
-                onPostSiteImageClick = {},
+                postImageUrl = "",
+                onSiteImageClick = {},
+                onPostImageClick = {},
             )
         }
     }
