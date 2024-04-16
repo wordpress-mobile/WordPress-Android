212400e55d30b522b0fff74b43e5b8917c866cb0
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2024-04-16 18:12:02 -0300

    Move shimmer color to generic component

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/shimmer/ShimmerBox.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/shimmer/ShimmerBox.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.compose.components.shimmer
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,6 +16,7 @@ fun ShimmerBox(
 ) {
     Box(
         modifier = modifier
+            .background(Shimmer.color)
             .shimmerLoadingAnimation(
                 isLoadingCompleted = isLoadingCompleted,
                 isLightModeActive = isLightModeActive,
