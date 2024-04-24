184a5e9e10341093898fc16cdce3d09aaec156b3
Irfan Omur <irfan.omur@automattic.com>
2024-04-24 15:02:46 +0300

    Add getStatsDateFromPeriodDay() function to StatsDateFormatter

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/subscribers/usecases/SubscribersMapper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/lists/sections/subscribers/usecases/SubscribersMapper.kt
@@ -2,13 +2,11 @@ package org.wordpress.android.ui.stats.refresh.lists.sections.subscribers.usecas
 
 import org.wordpress.android.R
 import org.wordpress.android.fluxc.model.stats.subscribers.SubscribersModel.PeriodData
-import org.wordpress.android.fluxc.network.utils.StatsGranularity
 import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem
 import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem.SubscribersChartItem
 import org.wordpress.android.ui.stats.refresh.lists.sections.BlockListItem.SubscribersChartItem.Line
 import org.wordpress.android.ui.stats.refresh.utils.StatsDateFormatter
 import org.wordpress.android.ui.stats.refresh.utils.StatsUtils
-import org.wordpress.android.util.extensions.enforceWesternArabicNumerals
 import javax.inject.Inject
 
 class SubscribersMapper @Inject constructor(
@@ -22,12 +20,7 @@ class SubscribersMapper @Inject constructor(
         selectedItemPeriod: String
     ): BlockListItem {
         val chartItems = dates.map {
-            val date = statsDateFormatter.parseStatsDate(StatsGranularity.DAYS, it.period)
-            Line(
-                statsDateFormatter.printDayWithoutYear(date).enforceWesternArabicNumerals() as String,
-                it.period,
-                it.subscribers.toInt()
-            )
+            Line(statsDateFormatter.getStatsDateFromPeriodDay(it.period), it.period, it.subscribers.toInt())
         }
 
         val contentDescriptions = statsUtils.getSubscribersChartEntryContentDescriptions(
