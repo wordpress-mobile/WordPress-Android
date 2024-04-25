66b6743c0e753c4bca5b73299f7c5973dfa103c8
Pantelis Stampoulis <pantelis.stampoulis@gmail.com>
2024-04-25 14:01:37 +0300

    Updates setLastModifiedForConflictResolution()

--- WordPress/src/main/java/org/wordpress/android/ui/posts/PostConflictResolutionFeatureUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/PostConflictResolutionFeatureUtils.kt
@@ -33,12 +33,20 @@ class PostConflictResolutionFeatureUtils @Inject constructor(
         return payload
     }
 
+    /**
+     * Resolves post-conflict issues for scheduled posts by adjusting the `if_not_modified_since` field. Normally, when
+     * a post is scheduled for the future, both the `dateCreated` and `lastModified` fields are set to the future
+     * publication date, as returned by the backend. This setup can prevent effective post-conflict resolution.
+     * This function introduces an additional field in the payload that is used to override the `lastModified` value.
+     * By setting a past date in `if_not_modified_since`, it ensures that the conflict resolution mechanism can operate
+     * correctly.
+     */
     private fun setLastModifiedForConflictResolution(payload: RemotePostPayload) {
         payload.post?.let { post ->
-            payload.lastModifiedForConflictResolution = if (shouldUpdateLastModified(post)) {
+            payload.lastModifiedForConflictResolution = if (shouldUpdateLastModifiedForConflictResolution(post)) {
                 dateTimeUtilsWrapper.dateStringFromIso8601MinusMillis(
-                    payload.post.lastModified,
-                    MILLISECONDS_IN_A_MINUTE
+                    dateTimeUtilsWrapper.currentTimeInIso8601(),
+                    MILLISECONDS_IN_A_HOUR
                 )
             } else {
                 null
@@ -46,13 +54,13 @@ class PostConflictResolutionFeatureUtils @Inject constructor(
         }
     }
 
-    private fun shouldUpdateLastModified(post: PostModel): Boolean {
+    private fun shouldUpdateLastModifiedForConflictResolution(post: PostModel): Boolean {
         return post.status == PostStatus.SCHEDULED.toString()
                 && post.remotePostId > 0
                 && post.lastModified == post.dateCreated
     }
 
     companion object {
-        const val MILLISECONDS_IN_A_MINUTE = 60L
+        const val MILLISECONDS_IN_A_HOUR = 1000*60*60L
     }
 }
