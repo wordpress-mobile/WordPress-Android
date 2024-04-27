698b72fe59ec67fc847729af876448772aa8872b
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-04-26 17:33:25 -0700

    Add total followers to SubscriberType

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/StatsStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/StatsStore.kt
@@ -272,7 +272,7 @@ class StatsStore
         FILE_DOWNLOADS
     }
 
-    enum class SubscriberType : StatsType { SUBSCRIBERS }
+    enum class SubscriberType : StatsType { SUBSCRIBERS, TOTAL_FOLLOWERS }
 
     enum class PostDetailType : StatsType {
         POST_HEADER,
