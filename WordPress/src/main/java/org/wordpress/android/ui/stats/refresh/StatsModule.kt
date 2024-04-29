daf80ad50623606757b973e9408105e5eabe016b
Irfan Omur <irfan.omur@automattic.com>
2024-04-29 17:41:14 +0300

    Add email card to the Subscribers tab

--- WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsModule.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/stats/refresh/StatsModule.kt
@@ -62,6 +62,7 @@ import org.wordpress.android.ui.stats.refresh.lists.sections.insights.usecases.T
 import org.wordpress.android.ui.stats.refresh.lists.sections.insights.usecases.TotalFollowersUseCase.TotalFollowersUseCaseFactory
 import org.wordpress.android.ui.stats.refresh.lists.sections.insights.usecases.TotalLikesUseCase.TotalLikesUseCaseFactory
 import org.wordpress.android.ui.stats.refresh.lists.sections.insights.usecases.ViewsAndVisitorsUseCase.ViewsAndVisitorsUseCaseFactory
+import org.wordpress.android.ui.stats.refresh.lists.sections.subscribers.usecases.EmailsUseCase.EmailsUseCaseFactory
 import org.wordpress.android.ui.stats.refresh.lists.sections.subscribers.usecases.SubscribersUseCase
 import org.wordpress.android.ui.stats.refresh.utils.SelectedTrafficGranularityManager
 import org.wordpress.android.ui.stats.refresh.utils.StatsSiteProvider
@@ -255,8 +256,10 @@ class StatsModule {
     @Named(BLOCK_SUBSCRIBERS_USE_CASES)
     @Suppress("LongParameterList")
     fun provideBlockSubscribersUseCases(
-        subscribersUseCase: SubscribersUseCase
-    ): List<@JvmSuppressWildcards BaseStatsUseCase<*, *>> = listOf(subscribersUseCase)
+        subscribersUseCase: SubscribersUseCase,
+        emailsUseCaseFactory: EmailsUseCaseFactory
+    ): List<@JvmSuppressWildcards BaseStatsUseCase<*, *>> =
+        listOf(subscribersUseCase, emailsUseCaseFactory.build(BLOCK))
 
     /**
      * Provides a singleton usecase that represents the Insights screen. It consists of list of use cases that build
