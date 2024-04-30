ef003a79114137a04d832faf5ffd43e4a755eb92
Irfan Omur <irfan.omur@automattic.com>
2024-04-30 16:35:41 +0300

    Add SubscriberType.SUBSCRIBERS

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/StatsStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/StatsStore.kt
@@ -272,7 +272,7 @@ class StatsStore
         FILE_DOWNLOADS
     }
 
-    enum class SubscriberType : StatsType { SUBSCRIBERS_CHART, EMAILS }
+    enum class SubscriberType : StatsType { SUBSCRIBERS_CHART, SUBSCRIBERS, EMAILS }
 
     enum class PostDetailType : StatsType {
         POST_HEADER,
