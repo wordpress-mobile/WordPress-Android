441ce6f210b017444ea40698fad665987666d633
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-05-03 17:05:05 -0700

    Update people_dropdown_item_followers usages

--- WordPress/src/main/java/org/wordpress/android/models/PeopleListFilter.java
+++ WordPress/src/main/java/org/wordpress/android/models/PeopleListFilter.java
@@ -7,7 +7,7 @@ import org.wordpress.android.WordPress;
 
 public enum PeopleListFilter implements FilterCriteria {
     TEAM(R.string.people_dropdown_item_team),
-    FOLLOWERS(R.string.people_dropdown_item_followers),
+    FOLLOWERS(R.string.people_dropdown_item_subscribers),
     EMAIL_FOLLOWERS(R.string.people_dropdown_item_email_followers),
     VIEWERS(R.string.people_dropdown_item_viewers);
 
