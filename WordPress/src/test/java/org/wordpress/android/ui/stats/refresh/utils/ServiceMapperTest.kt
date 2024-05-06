9d1e80846d67d18bc1b1ce7f863cea2438304f8f
Aditi Bhatia <aditi.bhatia@automattic.com>
2024-05-06 13:24:59 -0700

    Update stats_publicize_followers_label usages

--- WordPress/src/test/java/org/wordpress/android/ui/stats/refresh/utils/ServiceMapperTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/stats/refresh/utils/ServiceMapperTest.kt
@@ -46,7 +46,7 @@ class ServiceMapperTest : BaseUnitTest() {
 
         val result = serviceMapper.map(
             listOf(service),
-            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_followers_label)
+            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_subscribers_label)
         )
 
         assertThat(result).hasSize(1)
@@ -69,7 +69,7 @@ class ServiceMapperTest : BaseUnitTest() {
 
         val result = serviceMapper.map(
             listOf(service),
-            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_followers_label)
+            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_subscribers_label)
         )
 
         assertThat(result).hasSize(1)
@@ -92,7 +92,7 @@ class ServiceMapperTest : BaseUnitTest() {
 
         val result = serviceMapper.map(
             listOf(service),
-            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_followers_label)
+            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_subscribers_label)
         )
 
         assertThat(result).hasSize(1)
@@ -115,7 +115,7 @@ class ServiceMapperTest : BaseUnitTest() {
 
         val result = serviceMapper.map(
             listOf(service),
-            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_followers_label)
+            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_subscribers_label)
         )
 
         assertThat(result).hasSize(1)
@@ -138,7 +138,7 @@ class ServiceMapperTest : BaseUnitTest() {
 
         val result = serviceMapper.map(
             listOf(service),
-            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_followers_label)
+            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_subscribers_label)
         )
 
         assertThat(result).hasSize(1)
@@ -161,7 +161,7 @@ class ServiceMapperTest : BaseUnitTest() {
 
         val result = serviceMapper.map(
             listOf(service),
-            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_followers_label)
+            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_subscribers_label)
         )
 
         assertThat(result).hasSize(1)
@@ -185,7 +185,7 @@ class ServiceMapperTest : BaseUnitTest() {
 
         val result = serviceMapper.map(
             listOf(service),
-            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_followers_label)
+            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_subscribers_label)
         )
 
         assertThat(result).hasSize(1)
@@ -209,7 +209,7 @@ class ServiceMapperTest : BaseUnitTest() {
 
         val result = serviceMapper.map(
             listOf(service1, service2, service3),
-            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_followers_label)
+            Header(R.string.stats_publicize_service_label, R.string.stats_publicize_subscribers_label)
         )
 
         assertThat(result).hasSize(3)
