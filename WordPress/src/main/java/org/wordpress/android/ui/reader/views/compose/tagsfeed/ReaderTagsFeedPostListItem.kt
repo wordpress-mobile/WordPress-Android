e5603d4ebd9ca6957814196d56a17f83f187be46
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2024-05-08 23:39:36 -0300

    WIP implement tags feed overflow menu

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/tagsfeed/ReaderTagsFeedPostListItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/tagsfeed/ReaderTagsFeedPostListItem.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.ui.reader.views.compose.tagsfeed
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,10 +27,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -218,16 +224,25 @@ fun ReaderTagsFeedPostListItem(
             }
             Spacer(Modifier.weight(1f))
             // More menu ("…")
-            IconButton(
-                modifier = Modifier.size(24.dp),
-                onClick = {
-                    onPostMoreMenuClick()
-                },
+            Column(
+                horizontalAlignment = Alignment.End,
             ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_more_ellipsis_horizontal_squares),
-                    contentDescription = stringResource(R.string.show_more_desc),
-                    tint = secondaryElementColor,
+                var isMenuVisible by remember { mutableStateOf(false) }
+                IconButton(
+                    modifier = Modifier.size(24.dp),
+                    onClick = {
+                        onPostMoreMenuClick()
+                        isMenuVisible = !isMenuVisible
+                    },
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_more_ellipsis_horizontal_squares),
+                        contentDescription = stringResource(R.string.show_more_desc),
+                        tint = secondaryElementColor,
+                    )
+                }
+                ReaderTagsFeedMoreMenu(
+                    expanded = isMenuVisible,
                 )
             }
         }
