3813f81c5b47ad0a76964d9975a2a68591361bc8
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-05-22 09:10:46 -0400

    Refactor: use object instead of class

--- fluxc/src/main/java/org/wordpress/android/fluxc/utils/WPComRestClientUtils.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/utils/WPComRestClientUtils.kt
@@ -5,32 +5,30 @@ import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.wordpress.android.util.LanguageUtils
 
-class WPComRestClientUtils {
-    companion object {
-        private const val WPCOM_V2_PREFIX = "/wpcom/v2"
-        private const val WPCOM_V3_PREFIX = "/wpcom/v3"
-        private const val LOCALE_PARAM = "locale"
-        private const val UNDERSCORE_LOCALE_PARAM = "_locale"
+object WPComRestClientUtils {
+    private const val WPCOM_V2_PREFIX = "/wpcom/v2"
+    private const val WPCOM_V3_PREFIX = "/wpcom/v3"
+    private const val LOCALE_PARAM = "locale"
+    private const val UNDERSCORE_LOCALE_PARAM = "_locale"
 
-        @JvmStatic
-        fun getLocaleParamName(url: String): String {
-            return if (url.contains(WPCOM_V2_PREFIX) || url.contains(WPCOM_V3_PREFIX))
-                UNDERSCORE_LOCALE_PARAM
-            else LOCALE_PARAM
+    @JvmStatic
+    fun getLocaleParamName(url: String): String {
+        return if (url.contains(WPCOM_V2_PREFIX) || url.contains(WPCOM_V3_PREFIX))
+            UNDERSCORE_LOCALE_PARAM
+        else LOCALE_PARAM
+    }
+
+    @JvmStatic
+    fun getHttpUrlWithLocale(context: Context, url: String): HttpUrl? {
+        var httpUrl = url.toHttpUrlOrNull()
+
+        if (null != httpUrl) {
+            httpUrl = httpUrl.newBuilder().addQueryParameter(
+                getLocaleParamName(url),
+                LanguageUtils.getPatchedCurrentDeviceLanguage(context)
+            ).build()
         }
 
-        @JvmStatic
-        fun getHttpUrlWithLocale(context: Context, url: String): HttpUrl? {
-            var httpUrl = url.toHttpUrlOrNull()
-
-            if (null != httpUrl) {
-                httpUrl = httpUrl.newBuilder().addQueryParameter(
-                    getLocaleParamName(url),
-                    LanguageUtils.getPatchedCurrentDeviceLanguage(context)
-                ).build()
-            }
-
-            return httpUrl
-        }
+        return httpUrl
     }
 }
