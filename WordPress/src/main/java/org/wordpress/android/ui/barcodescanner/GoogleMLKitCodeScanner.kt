3329d74168b433df69d6e2f9a0438b5dc53f2257
Andy Valdez <avaldez31890@gmail.com>
2024-05-23 16:04:59 -0400

    [Bug] Remove flow and use callbacks instead. I know, pain.

--- WordPress/src/main/java/org/wordpress/android/ui/barcodescanner/GoogleMLKitCodeScanner.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/barcodescanner/GoogleMLKitCodeScanner.kt
@@ -3,10 +3,6 @@ package org.wordpress.android.ui.barcodescanner
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.common.Barcode
-import kotlinx.coroutines.channels.ProducerScope
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import javax.inject.Inject
 
 class GoogleMLKitCodeScanner @Inject constructor(
@@ -17,53 +13,41 @@ class GoogleMLKitCodeScanner @Inject constructor(
 ) : CodeScanner {
     private var barcodeFound = false
     @androidx.camera.core.ExperimentalGetImage
-    override fun startScan(imageProxy: ImageProxy): Flow<CodeScannerStatus> {
-        return callbackFlow {
-            val barcodeTask = barcodeScanner.process(inputImageProvider.provideImage(imageProxy))
-            barcodeTask.addOnCompleteListener {
-                // We must call image.close() on received images when finished using them.
-                // Otherwise, new images may not be received or the camera may stall.
-                imageProxy.close()
+    override fun startScan(imageProxy: ImageProxy, callback: CodeScannerCallback) {
+        val barcodeTask = barcodeScanner.process(inputImageProvider.provideImage(imageProxy))
+        barcodeTask.addOnCompleteListener {
+            // We must call image.close() on received images when finished using them.
+            // Otherwise, new images may not be received or the camera may stall.
+            imageProxy.close()
+        }
+        barcodeTask.addOnSuccessListener { barcodeList ->
+            // The check for barcodeFound is done because the startScan method will be called
+            // continuously by the library as long as we are in the scanning screen.
+            // There will be a good chance that the same barcode gets identified multiple times and as a result
+            // success callback will be called multiple times.
+            if (!barcodeList.isNullOrEmpty() && !barcodeFound) {
+                barcodeFound = true
+                callback.run(handleScanSuccess(barcodeList.firstOrNull()))
             }
-            barcodeTask.addOnSuccessListener { barcodeList ->
-                // The check for barcodeFound is done because the startScan method will be called
-                // continuously by the library as long as we are in the scanning screen.
-                // There will be a good chance that the same barcode gets identified multiple times and as a result
-                // success callback will be called multiple times.
-                if (!barcodeList.isNullOrEmpty() && !barcodeFound) {
-                    barcodeFound = true
-                    handleScanSuccess(barcodeList.firstOrNull())
-                    this@callbackFlow.close()
-                }
-            }
-            barcodeTask.addOnFailureListener { exception ->
-                this@callbackFlow.trySend(
-                    CodeScannerStatus.Failure(
-                        error = exception.message,
-                        type = errorMapper.mapGoogleMLKitScanningErrors(exception)
-                    )
-                )
-                this@callbackFlow.close()
-            }
-
-            awaitClose()
+        }
+        barcodeTask.addOnFailureListener { exception ->
+            callback.run(CodeScannerStatus.Failure(
+                error = exception.message,
+                type = errorMapper.mapGoogleMLKitScanningErrors(exception)
+            ))
         }
     }
 
-    private fun ProducerScope<CodeScannerStatus>.handleScanSuccess(code: Barcode?) {
-        code?.rawValue?.let {
-            trySend(
-                CodeScannerStatus.Success(
-                    it,
-                    barcodeFormatMapper.mapBarcodeFormat(code.format)
-                )
+    private fun handleScanSuccess(code: Barcode?): CodeScannerStatus {
+        return code?.rawValue?.let {
+            CodeScannerStatus.Success(
+                it,
+                barcodeFormatMapper.mapBarcodeFormat(code.format)
             )
         } ?: run {
-            trySend(
-                CodeScannerStatus.Failure(
-                    error = "Failed to find a valid raw value!",
-                    type = CodeScanningErrorType.Other(Throwable("Empty raw value"))
-                )
+            CodeScannerStatus.Failure(
+                error = "Failed to find a valid raw value!",
+                type = CodeScanningErrorType.Other(Throwable("Empty raw value"))
             )
         }
     }
