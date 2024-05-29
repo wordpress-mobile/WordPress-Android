1cdf268a6d191793982fa9ee0ada89900b4c257d
Jarvis Lin <jarvis.lin@automattic.com>
2024-05-29 09:19:06 +0200

    Define the peekHeight to avoid our bottom sheet expanding partially

--- WordPress/src/main/java/org/wordpress/android/ui/comments/ModerationBottomSheetDialogFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/comments/ModerationBottomSheetDialogFragment.kt
@@ -2,7 +2,12 @@ package org.wordpress.android.ui.comments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.google.android.material.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.wordpress.android.databinding.CommentModerationBinding
 
@@ -13,6 +18,25 @@ class ModerationBottomSheetDialogFragment : BottomSheetDialogFragment() {
         CommentModerationBinding.inflate(inflater, container, false).apply {
             binding = this
         }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // define the peekHeight to avoid it expanding partially
+        dialog?.setOnShowListener { dialogInterface ->
+            val sheetDialog = dialogInterface as? BottomSheetDialog
+
+            val bottomSheet = sheetDialog?.findViewById<View>(
+                R.id.design_bottom_sheet
+            ) as? FrameLayout
+
+            bottomSheet?.let {
+                val behavior = BottomSheetBehavior.from(it)
+                val metrics = resources.displayMetrics
+                behavior.peekHeight = metrics.heightPixels
+            }
+        }
+    }
 
     override fun onDestroyView() {
         binding = null
