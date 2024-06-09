ee7ae6ca9026ba38c97a60b2f8e0332f4c4ad9a0
Irfan Omur <irfan.omur@automattic.com>
2024-06-09 16:14:44 +0300

    Fix detekt errors in `CoverBlockProcessor`

--- WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/CoverBlockProcessor.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/CoverBlockProcessor.kt
@@ -3,6 +3,7 @@ package org.wordpress.android.ui.posts.mediauploadcompletionprocessors
 import com.google.gson.JsonObject
 import org.jsoup.nodes.Document
 import org.wordpress.android.util.helpers.MediaFile
+import java.util.Locale
 import java.util.regex.Pattern
 
 class CoverBlockProcessor(
@@ -22,7 +23,7 @@ class CoverBlockProcessor(
             return StringBuilder()
                 .append(innerMatcher.group(1))
                 .append(innerProcessed)
-                .append(innerMatcher.group(3))
+                .append(innerMatcher.group(GROUP_3))
                 .toString()
         }
 
@@ -31,7 +32,7 @@ class CoverBlockProcessor(
 
     override fun processBlockJsonAttributes(jsonAttributes: JsonObject): Boolean {
         val id = jsonAttributes["id"]
-        if (id != null && !id.isJsonNull && id.asInt == localId.toInt(10)) {
+        if (id != null && !id.isJsonNull && id.asInt == localId.toInt(RADIX)) {
             addIntPropertySafely(jsonAttributes, "id", remoteId)
 
             jsonAttributes.addProperty("url", remoteUrl)
@@ -52,43 +53,40 @@ class CoverBlockProcessor(
         val targetDiv = document.selectFirst(".wp-block-cover")
 
         // if a match is found, proceed with replacement
-        if (targetDiv != null) {
+        return targetDiv?.let { targetDivElement ->
             if (hasVideoBackground) {
-                val videoElement = targetDiv.selectFirst("video")
-                if (videoElement != null) {
-                    videoElement.attr("src", remoteUrl)
-                } else {
-                    return false
-                }
+                val videoElement = targetDivElement.selectFirst("video")
+                videoElement?.attr("src", remoteUrl) ?: return false
             } else {
                 // replace background-image url in style attribute
-                val style = PATTERN_BACKGROUND_IMAGE_URL.matcher(targetDiv.attr("style"))
-                    .replaceFirst(String.format("background-image:url(%1\$s)", remoteUrl))
-                targetDiv.attr("style", style)
+                val style = PATTERN_BACKGROUND_IMAGE_URL.matcher(targetDivElement.attr("style"))
+                    .replaceFirst(String.format(Locale.getDefault(), "background-image:url(%1\$s)", remoteUrl))
+                targetDivElement.attr("style", style)
             }
 
             // return injected block
-            return true
-        }
-
-        return false
+            true
+        } ?: false
     }
 
     companion object {
         /**
          * Template pattern used to match and splice cover inner blocks
          */
-        private val PATTERN_COVER_INNER: Pattern = Pattern.compile(
+        private val PATTERN_COVER_INNER = Pattern.compile(
             StringBuilder()
                 .append("(^.*?<div class=\"wp-block-cover__inner-container\">\\s*)")
                 .append("(.*)") // inner block contents
-                .append("(\\s*</div>\\s*</div>\\s*<!-- /wp:cover -->.*)").toString(),
+                .append("(\\s*</div>\\s*</div>\\s*<!-- /wp:cover -->.*)")
+                .toString(),
             Pattern.DOTALL
         )
 
         /**
          * Pattern to match background-image url in cover block html content
          */
-        private val PATTERN_BACKGROUND_IMAGE_URL: Pattern = Pattern.compile("background-image:\\s*url\\([^)]+\\)")
+        private val PATTERN_BACKGROUND_IMAGE_URL = Pattern.compile("background-image:\\s*url\\([^)]+\\)")
+        private const val GROUP_3 = 3
+        private const val RADIX = 10
     }
 }
