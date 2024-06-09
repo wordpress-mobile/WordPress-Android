923677354bc5537b9d82b2cfd4a9bbafc99d0b0d
Irfan Omur <irfan.omur@automattic.com>
2024-06-09 15:29:09 +0300

    Reformat CoverBlockProcessor after conversion

--- WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/CoverBlockProcessor.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/CoverBlockProcessor.kt
@@ -6,10 +6,11 @@ import org.wordpress.android.util.helpers.MediaFile
 import java.util.regex.Pattern
 
 class CoverBlockProcessor(
-    localId: String, mediaFile: MediaFile,
-    private val mMediaUploadCompletionProcessor: MediaUploadCompletionProcessor
+    localId: String,
+    mediaFile: MediaFile,
+    private val mediaUploadCompletionProcessor: MediaUploadCompletionProcessor
 ) : BlockProcessor(localId, mediaFile) {
-    private var mHasVideoBackground = false
+    private var hasVideoBackground = false
 
     override fun processInnerBlock(block: String): String {
         val innerMatcher = PATTERN_COVER_INNER.matcher(block)
@@ -17,8 +18,7 @@ class CoverBlockProcessor(
 
         // process inner contents recursively
         if (innerCapturesFound) {
-            val innerProcessed =
-                mMediaUploadCompletionProcessor.processContent(innerMatcher.group(2)) //
+            val innerProcessed = mediaUploadCompletionProcessor.processContent(innerMatcher.group(2))
             return StringBuilder()
                 .append(innerMatcher.group(1))
                 .append(innerProcessed)
@@ -38,8 +38,9 @@ class CoverBlockProcessor(
 
             // check if background type is video
             val backgroundType = jsonAttributes["backgroundType"]
-            mHasVideoBackground =
-                backgroundType != null && !backgroundType.isJsonNull && "video" == backgroundType.asString
+            hasVideoBackground = backgroundType != null &&
+                !backgroundType.isJsonNull &&
+                "video" == backgroundType.asString
             return true
         }
 
@@ -52,7 +53,7 @@ class CoverBlockProcessor(
 
         // if a match is found, proceed with replacement
         if (targetDiv != null) {
-            if (mHasVideoBackground) {
+            if (hasVideoBackground) {
                 val videoElement = targetDiv.selectFirst("video")
                 if (videoElement != null) {
                     videoElement.attr("src", remoteUrl)
@@ -61,10 +62,8 @@ class CoverBlockProcessor(
                 }
             } else {
                 // replace background-image url in style attribute
-                val style =
-                    PATTERN_BACKGROUND_IMAGE_URL.matcher(targetDiv.attr("style")).replaceFirst(
-                        String.format("background-image:url(%1\$s)", remoteUrl)
-                    )
+                val style = PATTERN_BACKGROUND_IMAGE_URL.matcher(targetDiv.attr("style"))
+                    .replaceFirst(String.format("background-image:url(%1\$s)", remoteUrl))
                 targetDiv.attr("style", style)
             }
 
@@ -83,14 +82,13 @@ class CoverBlockProcessor(
             StringBuilder()
                 .append("(^.*?<div class=\"wp-block-cover__inner-container\">\\s*)")
                 .append("(.*)") // inner block contents
-                .append("(\\s*</div>\\s*</div>\\s*<!-- /wp:cover -->.*)").toString(), Pattern.DOTALL
+                .append("(\\s*</div>\\s*</div>\\s*<!-- /wp:cover -->.*)").toString(),
+            Pattern.DOTALL
         )
 
         /**
          * Pattern to match background-image url in cover block html content
          */
-        private val PATTERN_BACKGROUND_IMAGE_URL: Pattern = Pattern.compile(
-            "background-image:\\s*url\\([^\\)]+\\)"
-        )
+        private val PATTERN_BACKGROUND_IMAGE_URL: Pattern = Pattern.compile("background-image:\\s*url\\([^)]+\\)")
     }
 }
