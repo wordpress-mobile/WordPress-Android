a9949f039c0ed8e2a5db4b327fd7eb95530b8acb
Irfan Omur <irfan.omur@automattic.com>
2024-06-09 12:24:18 +0300

    Convert `BlockProcessorFactory` to Kotlin

--- WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/BlockProcessorFactory.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/BlockProcessorFactory.kt
@@ -1,34 +1,14 @@
-package org.wordpress.android.ui.posts.mediauploadcompletionprocessors;
+package org.wordpress.android.ui.posts.mediauploadcompletionprocessors
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import org.wordpress.android.util.helpers.MediaFile
 
-import org.wordpress.android.util.helpers.MediaFile;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.wordpress.android.ui.posts.mediauploadcompletionprocessors.MediaBlockType.AUDIO;
-import static org.wordpress.android.ui.posts.mediauploadcompletionprocessors.MediaBlockType.COVER;
-import static org.wordpress.android.ui.posts.mediauploadcompletionprocessors.MediaBlockType.FILE;
-import static org.wordpress.android.ui.posts.mediauploadcompletionprocessors.MediaBlockType.GALLERY;
-import static org.wordpress.android.ui.posts.mediauploadcompletionprocessors.MediaBlockType.IMAGE;
-import static org.wordpress.android.ui.posts.mediauploadcompletionprocessors.MediaBlockType.MEDIA_TEXT;
-import static org.wordpress.android.ui.posts.mediauploadcompletionprocessors.MediaBlockType.VIDEO;
-import static org.wordpress.android.ui.posts.mediauploadcompletionprocessors.MediaBlockType.VIDEOPRESS;
-
-class BlockProcessorFactory {
-    @NonNull
-    private final MediaUploadCompletionProcessor mMediaUploadCompletionProcessor;
-    private final Map<MediaBlockType, BlockProcessor> mMediaBlockTypeBlockProcessorMap = new HashMap<>();
-
-    /**
-     * This factory initializes block processors for all media block types and provides a method to retrieve a block
-     * processor instance for a given block type.
-     */
-    BlockProcessorFactory(@NonNull MediaUploadCompletionProcessor mediaUploadCompletionProcessor) {
-        mMediaUploadCompletionProcessor = mediaUploadCompletionProcessor;
-    }
+internal class BlockProcessorFactory
+/**
+ * This factory initializes block processors for all media block types and provides a method to retrieve a block
+ * processor instance for a given block type.
+ */(private val mMediaUploadCompletionProcessor: MediaUploadCompletionProcessor) {
+    private val mMediaBlockTypeBlockProcessorMap: MutableMap<MediaBlockType, BlockProcessor> =
+        HashMap()
 
     /**
      * @param localId The local media id that needs replacement
@@ -36,30 +16,38 @@ class BlockProcessorFactory {
      * @param siteUrl The site url - used to generate the attachmentPage url
      * @return The factory instance - useful for chaining this method upon instantiation
      */
-    @NonNull
-    BlockProcessorFactory init(@NonNull String localId, @NonNull MediaFile mediaFile, @NonNull String siteUrl) {
-        mMediaBlockTypeBlockProcessorMap.put(IMAGE, new ImageBlockProcessor(localId, mediaFile));
-        mMediaBlockTypeBlockProcessorMap.put(VIDEOPRESS, new VideoPressBlockProcessor(localId, mediaFile));
-        mMediaBlockTypeBlockProcessorMap.put(VIDEO, new VideoBlockProcessor(localId, mediaFile));
-        mMediaBlockTypeBlockProcessorMap.put(MEDIA_TEXT, new MediaTextBlockProcessor(localId, mediaFile));
-        mMediaBlockTypeBlockProcessorMap.put(GALLERY, new GalleryBlockProcessor(localId, mediaFile, siteUrl,
-                mMediaUploadCompletionProcessor));
-        mMediaBlockTypeBlockProcessorMap.put(COVER, new CoverBlockProcessor(localId, mediaFile,
-                mMediaUploadCompletionProcessor));
-        mMediaBlockTypeBlockProcessorMap.put(FILE, new FileBlockProcessor(localId, mediaFile));
-        mMediaBlockTypeBlockProcessorMap.put(AUDIO, new AudioBlockProcessor(localId, mediaFile));
+    fun init(localId: String, mediaFile: MediaFile, siteUrl: String): BlockProcessorFactory {
+        mMediaBlockTypeBlockProcessorMap[MediaBlockType.IMAGE] =
+            ImageBlockProcessor(localId, mediaFile)
+        mMediaBlockTypeBlockProcessorMap[MediaBlockType.VIDEOPRESS] =
+            VideoPressBlockProcessor(localId, mediaFile)
+        mMediaBlockTypeBlockProcessorMap[MediaBlockType.VIDEO] =
+            VideoBlockProcessor(localId, mediaFile)
+        mMediaBlockTypeBlockProcessorMap[MediaBlockType.MEDIA_TEXT] =
+            MediaTextBlockProcessor(localId, mediaFile)
+        mMediaBlockTypeBlockProcessorMap[MediaBlockType.GALLERY] = GalleryBlockProcessor(
+            localId, mediaFile, siteUrl,
+            mMediaUploadCompletionProcessor
+        )
+        mMediaBlockTypeBlockProcessorMap[MediaBlockType.COVER] = CoverBlockProcessor(
+            localId, mediaFile,
+            mMediaUploadCompletionProcessor
+        )
+        mMediaBlockTypeBlockProcessorMap[MediaBlockType.FILE] =
+            FileBlockProcessor(localId, mediaFile)
+        mMediaBlockTypeBlockProcessorMap[MediaBlockType.AUDIO] =
+            AudioBlockProcessor(localId, mediaFile)
 
-        return this;
+        return this
     }
 
     /**
      * Retrieves the block processor instance for the given media block type.
      *
-     * @param blockType The media block type for which to provide a {@link BlockProcessor}
-     * @return The {@link BlockProcessor} for the given media block type
+     * @param blockType The media block type for which to provide a [BlockProcessor]
+     * @return The [BlockProcessor] for the given media block type
      */
-    @Nullable
-    BlockProcessor getProcessorForMediaBlockType(@NonNull MediaBlockType blockType) {
-        return mMediaBlockTypeBlockProcessorMap.get(blockType);
+    fun getProcessorForMediaBlockType(blockType: MediaBlockType): BlockProcessor? {
+        return mMediaBlockTypeBlockProcessorMap[blockType]
     }
 }
