5d5048dea310395e1a2416417f95ccde66dd386b
Irfan Omur <irfan.omur@automattic.com>
2024-06-10 13:10:45 +0300

    Reformat ImageBlockProcessor after conversion

--- WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/ImageBlockProcessor.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/ImageBlockProcessor.kt
@@ -4,33 +4,31 @@ import com.google.gson.JsonObject
 import org.jsoup.nodes.Document
 import org.wordpress.android.util.helpers.MediaFile
 
-class ImageBlockProcessor(localId: String, mediaFile: MediaFile) :
-    BlockProcessor(localId, mediaFile) {
+class ImageBlockProcessor(localId: String, mediaFile: MediaFile) : BlockProcessor(localId, mediaFile) {
     override fun processBlockContentDocument(document: Document): Boolean {
         // select image element with our local id
         val targetImg = document.select("img").first()
 
         // if a match is found, proceed with replacement
-        if (targetImg != null) {
+        return targetImg?.let {
             // replace attributes
-            targetImg.attr("src", remoteUrl)
+            it.attr("src", remoteUrl)
 
             // replace class
-            targetImg.removeClass("wp-image-$localId")
-            targetImg.addClass("wp-image-$remoteId")
+            it.removeClass("wp-image-$localId")
+            it.addClass("wp-image-$remoteId")
 
-            return true
-        }
-
-        return false
+            true
+        } ?: false
     }
 
     override fun processBlockJsonAttributes(jsonAttributes: JsonObject): Boolean {
         val id = jsonAttributes["id"]
-        if (id != null && !id.isJsonNull && id.asString == localId) {
+        return if (id != null && !id.isJsonNull && id.asString == localId) {
             addIntPropertySafely(jsonAttributes, "id", remoteId)
-            return true
+            true
+        } else {
+            false
         }
-        return false
     }
 }
