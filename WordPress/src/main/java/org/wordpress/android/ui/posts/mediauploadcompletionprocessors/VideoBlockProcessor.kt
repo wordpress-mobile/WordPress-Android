4d4abeb1eda291e92389dbea8e019b1470acfdaa
Irfan Omur <irfan.omur@automattic.com>
2024-06-10 19:24:00 +0300

    Reformat VideoBlockProcessor after conversion

--- WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/VideoBlockProcessor.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/VideoBlockProcessor.kt
@@ -4,30 +4,28 @@ import com.google.gson.JsonObject
 import org.jsoup.nodes.Document
 import org.wordpress.android.util.helpers.MediaFile
 
-class VideoBlockProcessor(localId: String, mediaFile: MediaFile) :
-    BlockProcessor(localId, mediaFile) {
+class VideoBlockProcessor(localId: String, mediaFile: MediaFile) : BlockProcessor(localId, mediaFile) {
     override fun processBlockContentDocument(document: Document): Boolean {
         // select video element with our local id
         val targetVideo = document.select("video").first()
 
         // if a match is found for video, proceed with replacement
-        if (targetVideo != null) {
+        return targetVideo?.let {
             // replace attribute
             targetVideo.attr("src", remoteUrl)
 
             // return injected block
-            return true
-        }
-
-        return false
+            true
+        } ?: false
     }
 
     override fun processBlockJsonAttributes(jsonAttributes: JsonObject): Boolean {
         val id = jsonAttributes["id"]
-        if (id != null && !id.isJsonNull && id.asString == localId) {
+        return if (id != null && !id.isJsonNull && id.asString == localId) {
             addIntPropertySafely(jsonAttributes, "id", remoteId)
-            return true
+            true
+        } else {
+            false
         }
-        return false
     }
 }
