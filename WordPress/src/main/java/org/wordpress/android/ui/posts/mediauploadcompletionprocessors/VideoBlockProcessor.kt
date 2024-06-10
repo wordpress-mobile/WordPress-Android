e639e666d0273d7d8ced5313fe474e57dc8045cf
Irfan Omur <irfan.omur@automattic.com>
2024-06-10 19:22:54 +0300

    Convert VideoBlockProcessor to Kotlin

--- WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/VideoBlockProcessor.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/posts/mediauploadcompletionprocessors/VideoBlockProcessor.kt
@@ -1,43 +1,33 @@
-package org.wordpress.android.ui.posts.mediauploadcompletionprocessors;
+package org.wordpress.android.ui.posts.mediauploadcompletionprocessors
 
-import androidx.annotation.NonNull;
+import com.google.gson.JsonObject
+import org.jsoup.nodes.Document
+import org.wordpress.android.util.helpers.MediaFile
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.wordpress.android.util.helpers.MediaFile;
-
-public class VideoBlockProcessor extends BlockProcessor {
-    public VideoBlockProcessor(@NonNull String localId, @NonNull MediaFile mediaFile) {
-        super(localId, mediaFile);
-    }
-
-    @Override
-    public boolean processBlockContentDocument(@NonNull Document document) {
+class VideoBlockProcessor(localId: String, mediaFile: MediaFile) :
+    BlockProcessor(localId, mediaFile) {
+    override fun processBlockContentDocument(document: Document): Boolean {
         // select video element with our local id
-        Element targetVideo = document.select("video").first();
+        val targetVideo = document.select("video").first()
 
         // if a match is found for video, proceed with replacement
         if (targetVideo != null) {
             // replace attribute
-            targetVideo.attr("src", remoteUrl);
+            targetVideo.attr("src", remoteUrl)
 
             // return injected block
-            return true;
+            return true
         }
 
-        return false;
+        return false
     }
 
-    @Override
-    public boolean processBlockJsonAttributes(@NonNull JsonObject jsonAttributes) {
-        JsonElement id = jsonAttributes.get("id");
-        if (id != null && !id.isJsonNull() && id.getAsString().equals(localId)) {
-            addIntPropertySafely(jsonAttributes, "id", remoteId);
-            return true;
+    override fun processBlockJsonAttributes(jsonAttributes: JsonObject): Boolean {
+        val id = jsonAttributes["id"]
+        if (id != null && !id.isJsonNull && id.asString == localId) {
+            addIntPropertySafely(jsonAttributes, "id", remoteId)
+            return true
         }
-        return false;
+        return false
     }
 }
