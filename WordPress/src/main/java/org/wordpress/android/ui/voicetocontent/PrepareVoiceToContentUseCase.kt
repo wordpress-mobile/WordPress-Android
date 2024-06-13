225a3f02597d772822e565dd9a510daf44957f89
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-06-13 11:05:26 -0400

    Capture and log errors; return result failure

--- WordPress/src/main/java/org/wordpress/android/ui/voicetocontent/PrepareVoiceToContentUseCase.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/voicetocontent/PrepareVoiceToContentUseCase.kt
@@ -6,19 +6,29 @@ import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.jetpackai.JetpackAIAssistantFeature
 import org.wordpress.android.fluxc.network.rest.wpcom.jetpackai.JetpackAIAssistantFeatureResponse
 import org.wordpress.android.fluxc.store.jetpackai.JetpackAIStore
+import org.wordpress.android.ui.voicetocontent.PrepareVoiceToContentResult.Success
+import org.wordpress.android.ui.voicetocontent.PrepareVoiceToContentResult.Failure.NetworkUnavailable
+import org.wordpress.android.ui.voicetocontent.PrepareVoiceToContentResult.Failure.RemoteRequestFailure
+import org.wordpress.android.util.NetworkUtilsWrapper
 import javax.inject.Inject
 
 class PrepareVoiceToContentUseCase @Inject constructor(
-    private val jetpackAIStore: JetpackAIStore
+    private val jetpackAIStore: JetpackAIStore,
+    private val networkUtilsWrapper: NetworkUtilsWrapper,
+    private val logger: VoiceToContentLogger
 ) {
     suspend fun execute(site: SiteModel): PrepareVoiceToContentResult =
         withContext(Dispatchers.IO) {
+            if (!networkUtilsWrapper.isNetworkAvailable()) {
+                return@withContext NetworkUnavailable
+            }
             when (val response = jetpackAIStore.fetchJetpackAIAssistantFeature(site)) {
                 is JetpackAIAssistantFeatureResponse.Success -> {
-                    PrepareVoiceToContentResult.Success(model = response.model)
+                    Success(model = response.model)
                 }
                 is JetpackAIAssistantFeatureResponse.Error -> {
-                    PrepareVoiceToContentResult.Error
+                    logger.logError("${response.type.name} - ${response.message}")
+                    RemoteRequestFailure
                 }
             }
         }
@@ -26,5 +36,8 @@ class PrepareVoiceToContentUseCase @Inject constructor(
 
 sealed class PrepareVoiceToContentResult {
     data class Success(val model: JetpackAIAssistantFeature) : PrepareVoiceToContentResult()
-    data object Error : PrepareVoiceToContentResult()
+    sealed class Failure: PrepareVoiceToContentResult() {
+        data object NetworkUnavailable: Failure()
+        data object RemoteRequestFailure: Failure()
+    }
 }
