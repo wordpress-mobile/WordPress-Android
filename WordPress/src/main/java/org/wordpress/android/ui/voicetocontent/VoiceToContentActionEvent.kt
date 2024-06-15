5a648b3873b135967cdd50db26b0623cb52b48b9
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-06-15 11:33:24 -0400

    Address checkstlye be removing empty line

--- WordPress/src/main/java/org/wordpress/android/ui/voicetocontent/VoiceToContentActionEvent.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/voicetocontent/VoiceToContentActionEvent.kt
@@ -7,5 +7,4 @@ sealed class VoiceToContentActionEvent {
     data class LaunchEditPost(val site: SiteModel, val content: String) : VoiceToContentActionEvent()
     data class LaunchExternalBrowser(val url: String) : VoiceToContentActionEvent()
     data object RequestPermission : VoiceToContentActionEvent()
-
 }
