70634a5420bf2e086999a4198edb0a1980b5f50a
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-07-22 13:54:37 +0300

    Build: Replace kotlin android extensions with parcelize for fluxc
    As part of this, the deprecated parcelize import got also replaced.

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/transactions/SupportedDomainCountry.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/transactions/SupportedDomainCountry.kt
@@ -1,7 +1,7 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.transactions
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class SupportedDomainCountry(
