eef6037f306edde010fc4515aa5a5fe35390585a
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-07-25 18:03:54 +0100

    Update UserAgent to handle initialization on a background thread

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.kt
@@ -2,13 +2,43 @@ package org.wordpress.android.fluxc.network
 
 import android.content.Context
 import android.webkit.WebSettings
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.PackageUtils
 
-@SuppressWarnings("SwallowedException", "TooGenericExceptionCaught", "MemberNameEqualsClassName")
-class UserAgent(appContext: Context?, appName: String) {
-    val userAgent: String
+@Suppress("MemberNameEqualsClassName")
+class UserAgent(
+    private val appContext: Context?,
+    private val appName: String
+) {
+    /**
+     * User-Agent string when making HTTP connections, for both API traffic and WebViews.
+     * Appends "[appName]/version" to WebView's default User-Agent string for the webservers
+     * to get the full feature list of the browser and serve content accordingly, e.g.:
+     *    "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
+     *    AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36
+     *    wp-android/4.7"
+     */
+    var userAgent: String = getAppNameVersion()
+        private set
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     init {
+        coroutineScope.launch {
+            initUserAgent()
+        }
+    }
+
+    /**
+     * Initializes the User-Agent string.
+     * This method will be called asynchronously to avoid blocking the main thread,
+     * because `WebSettings.getDefaultUserAgent()` can be slow.
+     */
+    @Suppress("TooGenericExceptionCaught", "SwallowedException")
+    private fun initUserAgent() {
         // Device's default User-Agent string.
         // E.g.:
         //   "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
@@ -18,17 +48,17 @@ class UserAgent(appContext: Context?, appName: String) {
         } catch (e: RuntimeException) {
             // `getDefaultUserAgent()` can throw an Exception
             // see: https://github.com/wordpress-mobile/WordPress-Android/issues/20147#issuecomment-1961238187
-            ""
+            AppLog.e(
+                AppLog.T.UTILS,
+                "Error getting the user's default User-Agent, ${e.stackTraceToString()}"
+            )
+            return
         }
-        // User-Agent string when making HTTP connections, for both API traffic and WebViews.
-        // Appends "wp-android/version" to WebView's default User-Agent string for the webservers
-        // to get the full feature list of the browser and serve content accordingly, e.g.:
-        //    "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
-        //    AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36
-        //    wp-android/4.7"
-        val appWithVersion = "$appName/${PackageUtils.getVersionName(appContext)}"
-        userAgent = if (defaultUserAgent.isNotEmpty()) "$defaultUserAgent $appWithVersion" else appWithVersion
+
+        userAgent = "$defaultUserAgent ${getAppNameVersion()}"
     }
+
+    private fun getAppNameVersion() = "$appName/${PackageUtils.getVersionName(appContext)}"
 
     override fun toString(): String = userAgent
 }
