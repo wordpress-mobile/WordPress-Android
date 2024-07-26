d9c569d94d56377873c73fd04312559ad4c850e5
Nick Bradbury <nick.bradbury@gmail.com>
2024-07-26 10:20:13 -0400

    Second pass at AppThemeMaterial3

--- WordPress/src/main/java/org/wordpress/android/ui/main/feedbackform/FeedbackFormScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/feedbackform/FeedbackFormScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -130,7 +129,7 @@ private fun Screen(
     content: @Composable () -> Unit,
     onCloseClick: () -> Unit
 ) {
-    MaterialTheme {
+    AppThemeMaterial3 {
         Scaffold(
             topBar = {
                 TopAppBar(
