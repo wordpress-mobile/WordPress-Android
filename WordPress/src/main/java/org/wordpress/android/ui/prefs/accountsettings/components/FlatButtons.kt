3c8c4935859e61f2b28a65d0a3267dbd315e297f
Nick Bradbury <nick.bradbury@gmail.com>
2024-07-29 15:49:25 -0400

    Reverted flat button color changes

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/components/FlatButtons.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/accountsettings/components/FlatButtons.kt
@@ -3,13 +3,14 @@ package org.wordpress.android.ui.prefs.accountsettings.components
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.wordpress.android.ui.domains.management.M3ThemeWithoutBackground
@@ -19,16 +20,14 @@ fun FlatButton(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        contentColor = Color.White,
+    ),
     enabled: Boolean = true,
 ): Unit = Button(
     modifier = modifier,
     onClick = onClick,
-    colors = ButtonDefaults.buttonColors(
-        contentColor =  MaterialTheme.colorScheme.onSecondary,
-        containerColor = MaterialTheme.colorScheme.secondary,
-        disabledContentColor = MaterialTheme.colorScheme.onSurface,
-        disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLow,
-    ),
+    colors = colors,
     elevation = ButtonDefaults.buttonElevation(
         defaultElevation = 0.dp,
         pressedElevation = 0.dp,
@@ -43,17 +42,13 @@ fun FlatOutlinedButton(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
     enabled: Boolean = true,
     isPending: Boolean = false,
 ): Unit = OutlinedButton(
     modifier = modifier,
     onClick = onClick,
-    colors = ButtonDefaults.buttonColors(
-        contentColor =  MaterialTheme.colorScheme.onSecondary,
-        containerColor = MaterialTheme.colorScheme.secondary,
-        disabledContentColor = MaterialTheme.colorScheme.onSurface,
-        disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLow,
-    ),
+    colors = colors,
     elevation = ButtonDefaults.buttonElevation(
         defaultElevation = 0.dp,
         pressedElevation = 0.dp,
