424a4680ddc79bb027adea895e83fdc168694987
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-07-30 13:46:48 +0200

    Add more debug calls

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -16,7 +16,9 @@ echo "--- :microscope: Linting"
 if [ "$1" = "wordpress" ]; then
 	./gradlew lintWordpressVanillaRelease
 
-  gzip -c '**/build/reports/lint-results*.sarif' | base64 > sarif_base64.tmp
+	cat 'WordPress/build/reports/lint-results-wordpressVanillaRelease.sarif'
+
+  gzip -c 'WordPress/build/reports/lint-results-wordpressVanillaRelease.sarif' | base64 > sarif_base64.tmp
 
   ls "WordPress/build/reports/"
 
