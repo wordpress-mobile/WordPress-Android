cc8356450df2c5a52b3d5129e4b98999a297a678
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-08-01 12:41:43 +0200

    Fix path for instrumented-tests result file
    This should fix annotating test failures.

--- .buildkite/commands/run-instrumented-tests.sh
+++ .buildkite/commands/run-instrumented-tests.sh
@@ -19,7 +19,7 @@ if [[ "$TESTS_EXIT_STATUS" -ne 0 ]]; then
 fi
 
 echo "--- 🚦 Report Tests Status"
-results_file=$(find "build/instrumented-tests" -type f -name "*.xml" -print -quit)
+results_file="build/instrumented-tests/JUnitReport.xml"
 
 if [[ $BUILDKITE_BRANCH == trunk ]] || [[ $BUILDKITE_BRANCH == release/* ]]; then
     annotate_test_failures "$results_file" --slack "build-and-ship"
