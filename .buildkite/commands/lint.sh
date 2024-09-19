cc99dd3353a625183a8ee55a617d7173368ec7a1
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-19 12:01:22 +0200

    Send Jetpack lint report, instead of WordPress
    As WordPress is a subset of Jetpack

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -12,18 +12,18 @@ bundle exec fastlane check_declared_locales_consistency app:"$1"
 echo "--- :microscope: Linting"
 
 if [ "$1" = "wordpress" ]; then
-  set +e
   ./gradlew lintWordpressVanillaRelease
-  lint_exit_code=$?
-  set -e
-
-  upload_sarif_to_github "WordPress/build/reports/lint-results-wordpressVanillaRelease.sarif"
-  exit $lint_exit_code
+  exit 0
 fi
 
 if [ "$1" = "jetpack" ]; then
+  set +e
   ./gradlew lintJetpackVanillaRelease
-  exit 0
+  lint_exit_code=$?
+  set -e
+
+  upload_sarif_to_github "WordPress/build/reports/lint-results-jetpackVanillaRelease.sarif"
+  exit $lint_exit_code
 fi
 
 echo "No target provided – unable to lint"
