e59606680f3c955486a0f2cb8f75ab133d2b560f
Nick Bradbury <nick.bradbury@gmail.com>
2024-09-25 15:27:38 -0400

    Renamed variable

--- WordPress/src/main/java/org/wordpress/android/util/config/SelfHostedUsersFeatureConfig.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/SelfHostedUsersFeatureConfig.kt
@@ -7,12 +7,12 @@ import javax.inject.Inject
 /**
  * Configuration of the self-hosted users feature
  */
-private const val ENABLE_SELF_HOSTED_USERS = "enable_self_hosted_users"
+private const val ENABLE_SELF_HOSTED_USERS_REMOTE_FIELD = "enable_self_hosted_users"
 
-@Feature(ENABLE_SELF_HOSTED_USERS, false)
+@Feature(ENABLE_SELF_HOSTED_USERS_REMOTE_FIELD, false)
 class SelfHostedUsersFeatureConfig
 @Inject constructor(appConfig: AppConfig) : FeatureConfig(
     appConfig,
     BuildConfig.ENABLE_SELF_HOSTED_USERS,
-    ENABLE_SELF_HOSTED_USERS
+    ENABLE_SELF_HOSTED_USERS_REMOTE_FIELD
 )
