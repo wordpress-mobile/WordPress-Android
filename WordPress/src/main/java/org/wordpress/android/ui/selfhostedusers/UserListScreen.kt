6feee8bad709e61cf908b43ceaf8c67b867e8333
Nick Bradbury <nick.bradbury@gmail.com>
2024-09-26 14:47:16 -0400

    Added dividers

--- WordPress/src/main/java/org/wordpress/android/ui/selfhostedusers/UserListScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/selfhostedusers/UserListScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -74,6 +75,7 @@ fun UserListScreen(
 private fun UserList(users: List<UserWithEditContext>) {
     for (user in users) {
         UserRow(user)
+        HorizontalDivider(thickness = 1.dp, modifier = Modifier.padding(start = 80.dp))
     }
 }
 
@@ -81,7 +83,7 @@ private fun UserList(users: List<UserWithEditContext>) {
 fun UserRow(user: UserWithEditContext) {
     Row(
         modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 12.dp)
+            .padding(all = 16.dp)
             .fillMaxWidth()
     ) {
         Column(
