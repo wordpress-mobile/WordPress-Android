1f87535eed9820755d2952ebac9f9e1b295f918c
Nick Bradbury <nick.bradbury@gmail.com>
2024-09-27 15:31:14 -0400

    Created UserComposables for re-using Composables

---
+++ WordPress/src/main/java/org/wordpress/android/ui/selfhostedusers/UserComposables.kt
@@ -0,0 +1,123 @@
+package org.wordpress.android.ui.selfhostedusers
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import org.wordpress.android.R
+import org.wordpress.android.ui.compose.theme.M3Theme
+
+@Composable
+fun UserAvatar(avatarUrl: String?) {
+    if (avatarUrl.isNullOrEmpty()) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_user_placeholder_primary_24),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .size(48.dp)
+        )
+    } else {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(avatarUrl)
+                .error(R.drawable.ic_user_placeholder_primary_24)
+                .crossfade(true)
+                .build(),
+            contentScale = ContentScale.Fit,
+            contentDescription = null,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(48.dp),
+            )
+    }
+}
+
+@Composable
+fun UserEmptyView(emptyText: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_people_white_24dp),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .size(85.dp)
+        )
+        Text(
+            text = emptyText,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserScreen(
+    title: String,
+    content: @Composable () -> Unit,
+    onCloseClick: () -> Unit,
+    isScrollable: Boolean,
+) {
+    M3Theme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(title) },
+                    navigationIcon = {
+                        IconButton(onClick = onCloseClick) {
+                            Icon(Icons.Filled.Close, stringResource(R.string.close))
+                        }
+                    },
+                )
+            },
+        ) { contentPadding ->
+            Column(
+                modifier = if (isScrollable) {
+                    Modifier
+                        .fillMaxSize()
+                        .imePadding()
+                        .padding(contentPadding)
+                        .verticalScroll(rememberScrollState())
+                } else {
+                    Modifier
+                        .fillMaxSize()
+                        .imePadding()
+                        .padding(contentPadding)
+                }
+            ) {
+                content()
+            }
+        }
+    }
+}
