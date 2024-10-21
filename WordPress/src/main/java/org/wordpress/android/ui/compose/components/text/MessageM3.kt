a5acde5e4bd9c064bb64612aa92634ba1aa4e6db
Nick Bradbury <nick.bradbury@gmail.com>
2024-10-21 13:56:17 -0400

    Removed unused imports

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/text/MessageM3.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/text/MessageM3.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/text/SubtitleM3.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/text/SubtitleM3.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
