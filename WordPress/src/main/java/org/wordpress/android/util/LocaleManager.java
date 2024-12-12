f647806c425882fa738b3cf85d2905e59eca04c9
Nick Bradbury <nick.bradbury@gmail.com>
2024-12-12 15:17:35 -0500

    Removed unused code from LocaleManager

--- WordPress/src/main/java/org/wordpress/android/util/LocaleManager.java
+++ WordPress/src/main/java/org/wordpress/android/util/LocaleManager.java
@@ -49,24 +49,6 @@ public class LocaleManager {
     }
 
     /**
-     * Apply locale to the provided configuration.
-     *
-     * @param context current context used to access Shared Preferences.
-     * @param configuration configuration that the locale should be applied to.
-     */
-    @SuppressLint("AppBundleLocaleChanges")
-    public static Configuration updatedConfigLocale(Context context, Configuration configuration) {
-        Locale locale = languageLocale(getLanguage(context));
-        Locale.setDefault(locale);
-
-        // NOTE: Earlier versions of Android require both of these to be set, otherwise
-        // RTL may not be implemented properly.
-        configuration.setLocale(locale);
-
-        return configuration;
-    }
-
-    /**
      * Change the active locale to the language provided. Save the updated language
      * settings to sharedPreferences.
      *
@@ -261,25 +243,6 @@ public class LocaleManager {
         }
 
         return new Triple<>(sortedEntries, sortedValues, detailStrings);
-    }
-
-    /**
-     * Generates detail display strings in the currently selected locale. Used as detail text
-     * in language preference dialog.
-     */
-    @Nullable
-    public static String[] createLanguageDetailDisplayStrings(CharSequence[] languageCodes) {
-        if (languageCodes == null || languageCodes.length < 1) {
-            return null;
-        }
-
-        String[] detailStrings = new String[languageCodes.length];
-        for (int i = 0; i < languageCodes.length; ++i) {
-            detailStrings[i] = StringUtils.capitalize(getLanguageString(
-                    languageCodes[i].toString(), languageLocale(languageCodes[i].toString())));
-        }
-
-        return detailStrings;
     }
 
     /**
