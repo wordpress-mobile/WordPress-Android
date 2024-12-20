e6af8c738376ed851f6decbf2364acfaddf86b60
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-12-20 17:48:06 +0200

    CI: Add a scheduled dependency cache job
    FYI: This job will be then used by 'buildkite-ci' and configured as a
    'buildkite_pipeline_schedule' with a weekly frequency.

---
+++ .buildkite/commands/save-cache.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :closed_lock_with_key: Installing Secrets"
+bundle exec fastlane run configure_apply
+
+echo "--- 🛠 Download Mobile App Dependencies [Assemble Jetpack App]"
+./gradlew assembleJetpackJalapenoDebug
+echo ""
+
+echo "--- 🧹 Download Lint Dependencies [Lint Jetpack App]"
+./gradlew lintJetpackJalapenoDebug
+echo ""
+
+echo "--- 🧹 Download Detekt Dependencies [Run Detekt]"
+./gradlew detekt
+echo ""
+
+echo "--- 🧹 Download Checkstyle Dependencies [Run Checkstyle]"
+./gradlew checkstyle
+echo ""
+
+echo "--- 🧪 Download Unit Test Dependencies [Assemble Unit Tests]"
+./gradlew assembleJetpackJalapenoDebugUnitTest libs:processors:testClasses libs:image-editor:assembleDebugUnitTest libs:fluxc:assembleDebugUnitTest libs:login:assembleDebugUnitTest
+echo ""
+
+echo "--- 🧪 Download Android Test Dependencies [Assemble Android Tests]"
+./gradlew assembleJetpackJalapenoDebugAndroidTest
+echo ""
+
+echo "--- 💾 Save Cache"
+save_gradle_dependency_cache
