a7737fedf1b1effe8ab11058db0514279687d2d7
Akshay Komar <akshaykomar732@gmail.com>
2025-01-11 21:04:55 +0530

    Revert "Merge branch 'trunk' of https://github.com/Akshaykomar890/WordPress-Android into trunk"
    This reverts commit 1cb521c2e6013b7b363de9ca099f27924d6efe11, reversing
    changes made to a0b9df15577a537fab3aa2e89c6325012b4e2c43.

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/SiteSettingsInterface.java
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/SiteSettingsInterface.java
@@ -701,6 +701,14 @@ public abstract class SiteSettingsInterface {
         return mJpSettings.serveStaticFilesFromOurServers;
     }
 
+    void enableLazyLoadImages(boolean enabled) {
+        mJpSettings.lazyLoadImages = enabled;
+    }
+
+    boolean isLazyLoadImagesEnabled() {
+        return mJpSettings.lazyLoadImages;
+    }
+
     void enableAdFreeHosting(boolean enabled) {
         mJpSettings.adFreeVideoHosting = enabled;
     }
