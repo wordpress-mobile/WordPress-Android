591ec7915662b046f9cdd18e6ca4e5b56989503c
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-01-16 13:52:45 -0700

    Fix merge issue

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/LoginActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
