1144f78233ef0eedd26248bc53feb3f752665ec3
Nick Bradbury <nick.bradbury@gmail.com>
2025-01-24 09:36:22 -0500

    Use actual classes to avoid fragility (renaming, etc)

--- WordPress/src/main/java/org/wordpress/android/ui/main/BaseAppCompatActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/BaseAppCompatActivity.kt
@@ -5,6 +5,28 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
+import org.wordpress.android.designsystem.DesignSystemActivity
+import org.wordpress.android.ui.blaze.blazecampaigns.BlazeCampaignParentActivity
+import org.wordpress.android.ui.bloggingprompts.promptslist.BloggingPromptsListActivity
+import org.wordpress.android.ui.debug.preferences.DebugSharedPreferenceFlagsActivity
+import org.wordpress.android.ui.domains.management.DomainManagementActivity
+import org.wordpress.android.ui.domains.management.newdomainsearch.NewDomainSearchActivity
+import org.wordpress.android.ui.domains.management.purchasedomain.PurchaseDomainActivity
+import org.wordpress.android.ui.jetpackoverlay.JetpackStaticPosterActivity
+import org.wordpress.android.ui.jetpackplugininstall.fullplugin.install.JetpackFullPluginInstallActivity
+import org.wordpress.android.ui.jetpackplugininstall.remoteplugin.JetpackRemoteInstallActivity
+import org.wordpress.android.ui.main.feedbackform.FeedbackFormActivity
+import org.wordpress.android.ui.media.MediaPreviewActivity
+import org.wordpress.android.ui.media.MediaSettingsActivity
+import org.wordpress.android.ui.mysite.menu.MenuActivity
+import org.wordpress.android.ui.mysite.personalization.PersonalizationActivity
+import org.wordpress.android.ui.pages.PagesActivity
+import org.wordpress.android.ui.posts.PostsListActivity
+import org.wordpress.android.ui.posts.sharemessage.EditJetpackSocialShareMessageActivity
+import org.wordpress.android.ui.prefs.ExperimentalFeaturesActivity
+import org.wordpress.android.ui.selfhostedusers.SelfHostedUsersActivity
+import org.wordpress.android.ui.sitemonitor.SiteMonitorParentActivity
+import org.wordpress.android.ui.stats.refresh.StatsActivity
 
 /**
  * Base class for all activities - initially created to handle insets for Android 15's edge-to-edge support,
@@ -23,8 +45,8 @@ open class BaseAppCompatActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.R)
     private fun applyInsetOffsets() {
-        val activityName = this.localClassName.substringAfterLast(".")
-        val excludedActivity = excludedActivities[activityName]
+        // val activityName = this.localClassName.substringAfterLast(".")
+        val excludedActivity = excludedActivities[this.localClassName]
         val applyTopOffset = excludedActivity?.applyTopOffset ?: true
         val applyBottomOffset = excludedActivity?.applyBottomOffset ?: true
 
@@ -57,37 +79,37 @@ open class BaseAppCompatActivity : AppCompatActivity() {
     /**
      * Activities that are excluded from the edge-to-edge top offset, bottom offset, or both. Activities not listed
      * here will have both offsets applied. Note that many of these excluded activities are Compose-based because
-     * Compose automatically adjusts for edge-to-edge insets. We may want to revisit this approach as more
-     * Compose-based activities are added to the project.
+     * Compose automatically adjusts for edge-to-edge insets. We may want to revisit this approach as we add more
+     * Compose-based activities to the project.
      */
     private val excludedActivities: HashMap<String, ActivityOffsets> = hashMapOf(
         // apply neither top nor bottom offset
-        "BlazeCampaignParentActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "BloggingPromptsListActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "DebugSharedPreferenceFlagsActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "DesignSystemActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "DomainManagementActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "EditJetpackSocialShareMessageActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "ExperimentalFeaturesActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "FeedbackFormActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "JetpackFullPluginInstallActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "JetpackRemoteInstallActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "JetpackStaticPosterActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "MediaPreviewActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "MenuActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "NewDomainSearchActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "PersonalizationActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "PurchaseDomainActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "SelfHostedUsersActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
-        "SiteMonitorParentActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        BlazeCampaignParentActivity::class.java.name  to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        BloggingPromptsListActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        DebugSharedPreferenceFlagsActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        DesignSystemActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        DomainManagementActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        EditJetpackSocialShareMessageActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        ExperimentalFeaturesActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        FeedbackFormActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        JetpackFullPluginInstallActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        JetpackRemoteInstallActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        JetpackStaticPosterActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        MediaPreviewActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        MenuActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        NewDomainSearchActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        PersonalizationActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        PurchaseDomainActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        SelfHostedUsersActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
+        SiteMonitorParentActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = false),
 
         // apply bottom offset only
-        "MediaSettingsActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = true),
-        "PagesActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = true),
-        "PostsListActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = true),
-        "StatsActivity" to ActivityOffsets(applyTopOffset = false, applyBottomOffset = true),
+        MediaSettingsActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = true),
+        PagesActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = true),
+        PostsListActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = true),
+        StatsActivity::class.java.name to ActivityOffsets(applyTopOffset = false, applyBottomOffset = true),
 
         // apply top offset only
-        "WPMainActivity" to ActivityOffsets(applyTopOffset = true, applyBottomOffset = false),
+        WPMainActivity::class.java.name to ActivityOffsets(applyTopOffset = true, applyBottomOffset = false),
     )
 }
