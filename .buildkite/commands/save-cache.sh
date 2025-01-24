4b6dde0c6d10280c6819f7145b299e123cf0e304
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-01-24 17:38:45 +0200

    CI: Replace all individual assemble tasks with download dependencies
    This custom 'downloadDependencies' task is part of the Android AMI,
    that is utilized by the 'android' agents, and it is becoming available
    to all project via the 'init.gradle.kts' script that is included in it.

--- .buildkite/commands/save-cache.sh
+++ .buildkite/commands/save-cache.sh
@@ -2,38 +2,8 @@
 
 set -euo pipefail
 
-echo "--- :rubygems: Setting up Gems"
-install_gems
-
-echo "--- :closed_lock_with_key: Installing Secrets"
-bundle exec fastlane run configure_apply
-
-# .buildkite/commands/prototype-build.sh -> build_and_upload_$1_prototype_build
-# -> PROTOTYPE_BUILD_FLAVOR = 'Jalapeno'
-# -> PROTOTYPE_BUILD_TYPE = 'Debug'
-echo "--- 🛠 Download Mobile App Dependencies [Assemble Jetpack App]"
-./gradlew assembleJetpackJalapenoDebug
-echo ""
-
-# .buildkite/commands/lint.sh -> ./gradlew lintJetpackVanillaRelease
-echo "--- 🧹 Download Lint Dependencies [Lint Jetpack App]"
-./gradlew lintJetpackJalapenoDebug
-echo ""
-
-# .buildkite/commands/run-unit-tests.sh -> ./gradlew $test_suite
-# -> test_suite="testWordpressVanillaRelease koverXmlReportWordpressVanillaRelease"
-# -> test_suite=":libs:processors:test :libs:processors:koverXmlReport"
-# -> test_suite=":libs:image-editor:testReleaseUnitTest :libs:image-editor:koverXmlReportRelease"
-# -> test_suite=":libs:fluxc:testReleaseUnitTest :libs:fluxc:koverXmlReportRelease"
-# -> test_suite=":libs:login:testReleaseUnitTest :libs:login:koverXmlReportRelease"
-echo "--- 🧪 Download Unit Test Dependencies [Assemble Unit Tests]"
-./gradlew assembleJetpackJalapenoDebugUnitTest assembleDebugUnitTest testClasses
-echo ""
-
-# .buildkite/commands/run-instrumented-tests.sh -> build_and_run_instrumented_test
-# -> gradle(tasks: ["WordPress:assemble#{app.to_s.capitalize}VanillaDebug", "WordPress:assemble#{app.to_s.capitalize}VanillaDebugAndroidTest"])
-echo "--- 🧪 Download Android Test Dependencies [Assemble Android Tests]"
-./gradlew assembleJetpackJalapenoDebugAndroidTest
+echo "--- 📦 Download Dependencies"
+./gradlew downloadDependencies
 echo ""
 
 echo "--- 💾 Save Cache"
