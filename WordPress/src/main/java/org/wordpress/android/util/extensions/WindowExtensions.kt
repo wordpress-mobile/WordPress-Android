9ebc1162ea4ae0a4d7be66f6f18e4f586a00df1c
Nick Bradbury <nick.bradbury@gmail.com>
2025-01-27 08:40:51 -0500

    Ignore changing status or navbar color on SDK 35+

--- WordPress/src/main/java/org/wordpress/android/util/extensions/WindowExtensions.kt
+++ WordPress/src/main/java/org/wordpress/android/util/extensions/WindowExtensions.kt
@@ -2,49 +2,39 @@ package org.wordpress.android.util.extensions
 
 import android.os.Build
 import android.view.Window
-import android.view.WindowInsets
 import androidx.core.view.WindowInsetsControllerCompat
 import org.wordpress.android.util.ColorUtils
 
+/**
+ * Note these are skipped on SDK 35+ because they conflict with API 15's edge-to-edge insets
+ */
 fun Window.setWindowStatusBarColor(color: Int) {
-    setWindowBarColor(color, InsetsType.STATUS_BAR)
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+        setWindowBarColor(color, InsetsType.STATUS_BAR)
+    }
 }
 
 fun Window.setWindowNavigationBarColor(color: Int) {
-    setWindowBarColor(color, InsetsType.NAVIGATION_BAR)
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+        setWindowBarColor(color, InsetsType.NAVIGATION_BAR)
+    }
 }
 
 /**
  * Sets the status bar or navigation bar color
- * TODO Setting both the status bar color and navigation bar color causes the insets to be set twice
  */
 @Suppress("DEPRECATION")
 private fun Window.setWindowBarColor(color: Int, insetsType: InsetsType) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-        // Android 15+
-        decorView.setOnApplyWindowInsetsListener { view, insets ->
-            view.setBackgroundColor(color)
-            val barInsets = insets.getInsets(
-                when (insetsType) {
-                    InsetsType.STATUS_BAR -> WindowInsets.Type.statusBars()
-                    InsetsType.NAVIGATION_BAR -> WindowInsets.Type.navigationBars()
-                }
-            )
-            // Adjust padding to avoid overlap
-            view.setPadding(0, barInsets.top, 0, 0)
-            insets
-        }
-    } else {
-        when(insetsType) {
-            InsetsType.STATUS_BAR -> statusBarColor = color
-            InsetsType.NAVIGATION_BAR -> navigationBarColor = color
-        }
-        val windowInsetsController = WindowInsetsControllerCompat(this, decorView)
-        if (insetsType == InsetsType.STATUS_BAR) {
-            windowInsetsController.isAppearanceLightStatusBars = ColorUtils.isColorLight(statusBarColor)
-        }
-        windowInsetsController.isAppearanceLightNavigationBars = ColorUtils.isColorLight(navigationBarColor)
+    when (insetsType) {
+        InsetsType.STATUS_BAR -> statusBarColor = color
+        InsetsType.NAVIGATION_BAR -> navigationBarColor = color
     }
+    val windowInsetsController = WindowInsetsControllerCompat(this, decorView)
+    if (insetsType == InsetsType.STATUS_BAR) {
+        windowInsetsController.isAppearanceLightStatusBars = ColorUtils.isColorLight(statusBarColor)
+    }
+    windowInsetsController.isAppearanceLightNavigationBars =
+        ColorUtils.isColorLight(navigationBarColor)
 }
 
 private enum class InsetsType {
