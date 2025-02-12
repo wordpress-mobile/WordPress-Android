5b333ff85d0012ba363a560abd6c8ed7e8c576d4
Nick Bradbury <nick.bradbury@gmail.com>
2025-02-12 14:04:32 -0500

    First pass at getting default user agent in separate thread

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.kt
@@ -2,32 +2,40 @@ package org.wordpress.android.fluxc.network
 
 import android.content.Context
 import android.webkit.WebSettings
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.wordpress.android.util.PackageUtils
 
-@SuppressWarnings("SwallowedException", "TooGenericExceptionCaught", "MemberNameEqualsClassName")
-class UserAgent(appContext: Context?, appName: String) {
-    val userAgent: String
+@SuppressWarnings("SwallowedException", "TooGenericExceptionCaught")
+class UserAgent(appContext: Context, appName: String) {
+    var userAgent: String
 
     init {
-        // Device's default User-Agent string.
-        // E.g.:
-        //   "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
-        //   AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36"
-        val defaultUserAgent = try {
-            WebSettings.getDefaultUserAgent(appContext)
-        } catch (e: RuntimeException) {
-            // `getDefaultUserAgent()` can throw an Exception
-            // see: https://github.com/wordpress-mobile/WordPress-Android/issues/20147#issuecomment-1961238187
-            ""
-        }
-        // User-Agent string when making HTTP connections, for both API traffic and WebViews.
-        // Appends "wp-android/version" to WebView's default User-Agent string for the webservers
-        // to get the full feature list of the browser and serve content accordingly, e.g.:
-        //    "Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv)
-        //    AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36
-        //    wp-android/4.7"
+        // default to using just the app name and version
         val appWithVersion = "$appName/${PackageUtils.getVersionName(appContext)}"
-        userAgent = if (defaultUserAgent.isNotEmpty()) "$defaultUserAgent $appWithVersion" else appWithVersion
+        userAgent = appWithVersion
+
+        // then get the default user agent on a separate thread - this can cause an ANR on the main thread
+        // see: https://github.com/wordpress-mobile/WordPress-Android/issues/21679#issuecomment-2654510229
+        CoroutineScope(Dispatchers.Default).launch {
+            getDefaultUserAgent(appContext)?.let {
+                userAgent = "$it $appWithVersion"
+            }
+        }
+    }
+
+    private suspend fun getDefaultUserAgent(appContext: Context): String? {
+        withContext(Dispatchers.IO) {
+            try {
+                return@withContext WebSettings.getDefaultUserAgent(appContext)
+            } catch (e: RuntimeException) {
+                // `getDefaultUserAgent()` can throw an Exception
+                // see: https://github.com/wordpress-mobile/WordPress-Android/issues/20147#issuecomment-1961238187
+            }
+        }
+        return null
     }
 
     override fun toString(): String = userAgent
