a620d05af9287995f6c35016d5c451cc1a6c24ef
Nick Bradbury <nick.bradbury@gmail.com>
2025-03-21 10:33:47 -0400

    Added initiallyVisible parameter so previews aren't dimmed

--- WordPress/src/main/java/org/wordpress/android/ui/main/emailverificationbanner/EmailVerificationViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/main/emailverificationbanner/EmailVerificationViewModel.kt
@@ -59,7 +59,7 @@ class EmailVerificationViewModel
         _emailAddress.value = accountStore.account.email
 
         _verificationState.value = if (accountStore.account.emailVerified) {
-            VerificationState.VERIFIED
+            VerificationState.UNVERIFIED // TODO
         } else if (_emailAddress.value.isNotEmpty()) {
             VerificationState.UNVERIFIED
         } else {
