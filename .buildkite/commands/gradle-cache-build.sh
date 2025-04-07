80e0158900dcced6fb43017288777232638a9ead
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-04-07 16:47:17 +0200

    Add "populate cache" job to the pipeline

---
+++ .buildkite/commands/gradle-cache-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eu
+
+"$(dirname "${BASH_SOURCE[0]}")/restore-cache.sh"
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :closed_lock_with_key: Installing Secrets"
+bundle exec fastlane run configure_apply
+
+echo "--- :hammer_and_wrench: Building"
+./gradlew assembleWordpressWasabiDebug assembleJetpackWasabiDebug
