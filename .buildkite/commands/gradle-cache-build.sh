10d39825c3954ae7ec2ede69ee698b6fb858112a
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-04-07 17:21:07 +0200

    Split populating Gradle build cache in two jobs

--- .buildkite/commands/gradle-cache-build.sh
+++ .buildkite/commands/gradle-cache-build.sh
@@ -9,4 +9,10 @@ echo "--- :closed_lock_with_key: Installing Secrets"
 bundle exec fastlane run configure_apply
 
 echo "--- :hammer_and_wrench: Building"
-./gradlew assembleWordpressWasabiDebug assembleJetpackWasabiDebug
+if [ "$1" = "wordpress" ]; then
+  ./gradlew assembleWordpressWasabiDebug
+fi
+
+if [ "$1" = "jetpack" ]; then
+  ./gradlew assembleJetpackWasabiDebug
+fi
