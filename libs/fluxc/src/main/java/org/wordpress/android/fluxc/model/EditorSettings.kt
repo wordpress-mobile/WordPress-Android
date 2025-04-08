a9710cbcea856863f94a92f80c52262193a0c096
David Calhoun <github@davidcalhoun.me>
2025-04-08 14:54:43 -0400

    refactor: Simplify EditorSettings

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/EditorSettings.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/model/EditorSettings.kt
@@ -2,61 +2,24 @@ package org.wordpress.android.fluxc.model
 
 import android.os.Bundle
 import com.google.gson.JsonObject
-import com.google.gson.JsonArray
-import com.google.gson.JsonPrimitive
+import com.google.gson.JsonParser
+import org.json.JSONObject
 
 data class EditorSettings(
     val settings: JsonObject
 ) {
-    fun toBundle(): Bundle {
-        val bundle = Bundle()
-        processJsonObject(settings, bundle)
-        return bundle
-    }
-
-    private fun processJsonObject(jsonObject: JsonObject, bundle: Bundle) {
-        jsonObject.entrySet().forEach { entry ->
-            when (val value = entry.value) {
-                is JsonPrimitive -> processJsonPrimitive(entry.key, value, bundle)
-                is JsonArray -> processJsonArray(entry.key, value, bundle)
-                is JsonObject -> {
-                    val nestedBundle = Bundle()
-                    processJsonObject(value, nestedBundle)
-                    bundle.putBundle(entry.key, nestedBundle)
-                }
-            }
+    companion object {
+        fun fromJsonString(jsonString: String): EditorSettings {
+            val jsonObject = JsonParser.parseString(jsonString).asJsonObject
+            return EditorSettings(jsonObject)
         }
     }
 
-    private fun processJsonPrimitive(key: String, value: JsonPrimitive, bundle: Bundle) {
-        when {
-            value.isBoolean -> bundle.putBoolean(key, value.asBoolean)
-            value.isNumber -> bundle.putDouble(key, value.asDouble)
-            value.isString -> bundle.putString(key, value.asString)
-        }
+    fun toJsonString(): String {
+        return settings.toString()
     }
 
-    private fun processJsonArray(key: String, array: JsonArray, bundle: Bundle) {
-        val arrayList = ArrayList<Bundle>()
-        array.forEach { element ->
-            when (element) {
-                is JsonObject -> {
-                    val nestedBundle = Bundle()
-                    processJsonObject(element, nestedBundle)
-                    arrayList.add(nestedBundle)
-                }
-                is JsonPrimitive -> {
-                    val primitiveBundle = Bundle()
-                    processJsonPrimitive("value", element, primitiveBundle)
-                    arrayList.add(primitiveBundle)
-                }
-                is JsonArray -> {
-                    val arrayBundle = Bundle()
-                    processJsonArray("nested", element, arrayBundle)
-                    arrayList.add(arrayBundle)
-                }
-            }
-        }
-        bundle.putParcelableArrayList(key, arrayList)
+    fun toJSONObject(): JSONObject {
+        return JSONObject(settings.toString())
     }
 }
