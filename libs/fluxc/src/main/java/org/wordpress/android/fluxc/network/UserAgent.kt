8c801f78446af34faafda060147e1bd067f4ba67
Nick Bradbury <nick.bradbury@gmail.com>
2025-04-10 09:12:39 -0400

    Use a private setter

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/UserAgent.kt
@@ -8,7 +8,8 @@ import javax.inject.Singleton
 @Singleton
 @SuppressWarnings("SwallowedException", "TooGenericExceptionCaught", "MemberNameEqualsClassName")
 class UserAgent(appContext: Context?, appName: String) {
-    val userAgent: String
+    var userAgent: String
+        private set
 
     init {
         // Device's default User-Agent string.
