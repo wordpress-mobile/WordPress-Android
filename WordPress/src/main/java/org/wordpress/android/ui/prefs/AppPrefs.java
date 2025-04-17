3f907f7c48d7106c33e6f166577556bb32fe9710
Nick Bradbury <nick.bradbury@gmail.com>
2025-04-17 10:20:52 -0400

    Fixed warnings in MeGravatarLoader

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/AppPrefs.java
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/AppPrefs.java
@@ -1055,6 +1055,7 @@ public class AppPrefs {
         return getBoolean(UndeletablePrefKey.IS_INSTALLATION_REFERRER_OBTAINED, false);
     }
 
+    // Default to 1 rather than 0 because a signature of 0 will not affect the cache key
     public static int getAvatarVersion() {
         return getInt(DeletablePrefKey.AVATAR_VERSION, 1);
     }
