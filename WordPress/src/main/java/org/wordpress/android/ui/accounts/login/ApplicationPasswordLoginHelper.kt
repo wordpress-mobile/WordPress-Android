8b510b26556453d4c774f0ede178b77baacf97e4
adalpari <adalpari@gmail.com>
2025-05-08 18:04:18 +0200

    Adding tests to the ApplicationPasswordLoginHelper

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/ApplicationPasswordLoginHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/ApplicationPasswordLoginHelper.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.accounts.login
 
+import android.net.Uri
 import android.util.Log
 import androidx.core.net.toUri
 import kotlinx.coroutines.runBlocking
@@ -7,37 +8,37 @@ import org.wordpress.android.fluxc.persistence.SiteSqlUtils
 import javax.inject.Inject
 
 class ApplicationPasswordLoginHelper @Inject constructor(
-    private val siteSqlUtils: SiteSqlUtils
+    private val siteSqlUtils: SiteSqlUtils,
+    private val uriLoginWrapper: UriLoginWrapper
 ) {
     private var processedAppPasswordData: String? = null
 
     @Suppress("ReturnCount")
-    fun storeApplicationPasswordCredentialsFrom(data: String?): Boolean {
-        if (data == null || data == processedAppPasswordData) {
+    suspend fun storeApplicationPasswordCredentialsFrom(url: String): Boolean {
+        if (url.isEmpty() || url == processedAppPasswordData) {
             return false
         }
 
-        val siteUrl = data.toUri().getQueryParameter("site_url")
-        val user = data.toUri().getQueryParameter("user_login")
-        val password = data.toUri().getQueryParameter("password")
+        // Check if the url can be parsed
+        val uriLogin = uriLoginWrapper.parseUriLogin(url)
 
-        if (user.isNullOrEmpty() || password.isNullOrEmpty() ) {
+        if (uriLogin.user.isNullOrEmpty() || uriLogin.password.isNullOrEmpty() ) {
             return false
         }
 
         runBlocking {
-            val site = siteSqlUtils.getSites().firstOrNull { it.url == siteUrl }
+            val site = siteSqlUtils.getSites().firstOrNull { it.url == uriLogin.siteUrl }
             if (site != null) {
-                site.apiRestUsername = user
-                site.apiRestPassword = password
+                site.apiRestUsername = uriLogin.user
+                site.apiRestPassword = uriLogin.password
                 siteSqlUtils.insertOrUpdateSite(site)
-                Log.d("WP_RS", "Saved application password credentials for: $siteUrl")
+                Log.d("WP_RS", "Saved application password credentials for: ${uriLogin.siteUrl}")
             } else {
-                Log.e("WP_RS", "Cannot save application password credentials for: $siteUrl")
+                Log.e("WP_RS", "Cannot save application password credentials for: ${uriLogin.siteUrl}")
             }
         }
 
-        processedAppPasswordData = data
+        processedAppPasswordData = url
         return true
     }
 
@@ -52,4 +53,24 @@ class ApplicationPasswordLoginHelper @Inject constructor(
             }.build().toString()
         }
     }
+
+    /**
+     * This class is created to wrap the Uri calls and let us unit test the login helper
+     */
+    class UriLoginWrapper @Inject constructor() {
+        fun parseUriLogin(url: String): UriLogin {
+            val uri = url.toUri()
+            return UriLogin(
+                uri.getQueryParameter("site_url"),
+                uri.getQueryParameter("user_login"),
+                uri.getQueryParameter("password")
+            )
+        }
+    }
+
+    data class UriLogin(
+        val siteUrl: String?,
+        val user: String?,
+        val password: String?
+    )
 }
