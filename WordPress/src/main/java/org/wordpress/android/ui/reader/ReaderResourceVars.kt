d753d34f5663cddfd5ef66b149b080550f68a5c4
Nick Bradbury <nick.bradbury@gmail.com>
2025-05-13 15:25:10 -0400

    Auto conversion of ReaderResourceVars

--- WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderResourceVars.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/ReaderResourceVars.kt
@@ -1,94 +1,100 @@
-package org.wordpress.android.ui.reader;
+package org.wordpress.android.ui.reader
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Color;
-
-import androidx.core.content.res.ResourcesCompat;
-
-import org.wordpress.android.R;
-import org.wordpress.android.util.extensions.ContextExtensionsKt;
-import org.wordpress.android.util.DisplayUtils;
-import org.wordpress.android.util.HtmlUtils;
+import android.content.Context
+import android.graphics.Color
+import androidx.core.content.res.ResourcesCompat
+import org.wordpress.android.R
+import org.wordpress.android.util.DisplayUtils
+import org.wordpress.android.util.HtmlUtils
+import org.wordpress.android.util.extensions.getColorFromAttribute
 
 /*
- * class which holds all resource-based variables used when rendering post detail
- */
-class ReaderResourceVars {
-    final int mMarginMediumPx;
+* class which holds all resource-based variables used when rendering post detail
+*/
+internal class ReaderResourceVars(context: Context) {
+    val mMarginMediumPx: Int
 
-    final boolean mIsWideDisplay;
+    val mIsWideDisplay: Boolean
 
-    final int mFullSizeImageWidthPx;
-    final int mFeaturedImageHeightPx;
+    val mFullSizeImageWidthPx: Int
+    val mFeaturedImageHeightPx: Int
 
-    final int mVideoWidthPx;
-    final int mVideoHeightPx;
+    val mVideoWidthPx: Int
+    val mVideoHeightPx: Int
 
-    final String mLinkColorStr;
-    final String mGreyMediumDarkStr;
-    final String mGreyLightStr;
-    final String mGreyExtraLightStr;
-    final String mTextColor;
-    final String mGreyDisabledStr;
+    val mLinkColorStr: String
+    val mGreyMediumDarkStr: String
+    val mGreyLightStr: String
+    val mGreyExtraLightStr: String
+    val mTextColor: String
+    val mGreyDisabledStr: String
 
-    ReaderResourceVars(Context context) {
-        Resources resources = context.getResources();
+    init {
+        val resources = context.resources
 
-        int displayWidthPx = DisplayUtils.getWindowPixelWidth(context);
+        val displayWidthPx = DisplayUtils.getWindowPixelWidth(context)
 
-        mIsWideDisplay = DisplayUtils.pxToDp(context, displayWidthPx) > 640;
+        mIsWideDisplay = DisplayUtils.pxToDp(context, displayWidthPx) > 640
 
-        int marginLargePx = resources.getDimensionPixelSize(R.dimen.margin_large);
-        int detailMarginWidthPx = resources.getDimensionPixelOffset(R.dimen.reader_detail_margin);
+        val marginLargePx = resources.getDimensionPixelSize(R.dimen.margin_large)
+        val detailMarginWidthPx = resources.getDimensionPixelOffset(R.dimen.reader_detail_margin)
 
-        mFeaturedImageHeightPx = resources.getDimensionPixelSize(R.dimen.reader_featured_image_height);
-        mMarginMediumPx = resources.getDimensionPixelSize(R.dimen.margin_medium);
+        mFeaturedImageHeightPx =
+            resources.getDimensionPixelSize(R.dimen.reader_featured_image_height)
+        mMarginMediumPx = resources.getDimensionPixelSize(R.dimen.margin_medium)
 
-        int onSurfaceColor = ContextExtensionsKt
-                .getColorFromAttribute(context, com.google.android.material.R.attr.colorOnSurface);
+        val onSurfaceColor =
+            context.getColorFromAttribute(com.google.android.material.R.attr.colorOnSurface)
 
-        String onSurfaceHighType = "rgba(" + Color.red(onSurfaceColor) + ", "
-                                   + Color.green(onSurfaceColor) + ", "
-                                   + Color.blue(onSurfaceColor) + ", "
-                                   + ResourcesCompat.getFloat(resources,
-                com.google.android.material.R.dimen.material_emphasis_high_type)
-                                   + ")";
+        val onSurfaceHighType = ("rgba(" + Color.red(onSurfaceColor) + ", "
+                + Color.green(onSurfaceColor) + ", "
+                + Color.blue(onSurfaceColor) + ", "
+                + ResourcesCompat.getFloat(
+            resources,
+            com.google.android.material.R.dimen.material_emphasis_high_type
+        )
+                + ")")
 
-        mGreyMediumDarkStr = "rgba(" + Color.red(onSurfaceColor) + ", "
-                             + Color.green(onSurfaceColor) + ", "
-                             + Color.blue(onSurfaceColor) + ", "
-                             + ResourcesCompat.getFloat(resources,
-                com.google.android.material.R.dimen.material_emphasis_medium)
-                             + ")";
+        mGreyMediumDarkStr = ("rgba(" + Color.red(onSurfaceColor) + ", "
+                + Color.green(onSurfaceColor) + ", "
+                + Color.blue(onSurfaceColor) + ", "
+                + ResourcesCompat.getFloat(
+            resources,
+            com.google.android.material.R.dimen.material_emphasis_medium
+        )
+                + ")")
 
-        mGreyLightStr = "rgba(" + Color.red(onSurfaceColor) + ", "
-                        + Color.green(onSurfaceColor) + ", "
-                        + Color.blue(onSurfaceColor) + ", "
-                        + ResourcesCompat.getFloat(resources,
-                com.google.android.material.R.dimen.material_emphasis_disabled)
-                        + ")";
+        mGreyLightStr = ("rgba(" + Color.red(onSurfaceColor) + ", "
+                + Color.green(onSurfaceColor) + ", "
+                + Color.blue(onSurfaceColor) + ", "
+                + ResourcesCompat.getFloat(
+            resources,
+            com.google.android.material.R.dimen.material_emphasis_disabled
+        )
+                + ")")
 
-        mGreyExtraLightStr = "rgba(" + Color.red(onSurfaceColor) + ", "
-                             + Color.green(onSurfaceColor) + ", "
-                             + Color.blue(onSurfaceColor) + ", "
-                             + ResourcesCompat.getFloat(resources, R.dimen.emphasis_low) + ")";
+        mGreyExtraLightStr = ("rgba(" + Color.red(onSurfaceColor) + ", "
+                + Color.green(onSurfaceColor) + ", "
+                + Color.blue(onSurfaceColor) + ", "
+                + ResourcesCompat.getFloat(resources, R.dimen.emphasis_low) + ")")
 
-        mGreyDisabledStr = "rgba(" + Color.red(onSurfaceColor) + ", "
-                           + Color.green(onSurfaceColor) + ", "
-                           + Color.blue(onSurfaceColor) + ", "
-                           + ResourcesCompat.getFloat(resources,
-                com.google.android.material.R.dimen.material_emphasis_disabled)
-                           + ")";
+        mGreyDisabledStr = ("rgba(" + Color.red(onSurfaceColor) + ", "
+                + Color.green(onSurfaceColor) + ", "
+                + Color.blue(onSurfaceColor) + ", "
+                + ResourcesCompat.getFloat(
+            resources,
+            com.google.android.material.R.dimen.material_emphasis_disabled
+        )
+                + ")")
 
-        mTextColor = onSurfaceHighType;
-        mLinkColorStr = HtmlUtils.colorResToHtmlColor(context, R.color.reader_post_body_link);
+        mTextColor = onSurfaceHighType
+        mLinkColorStr = HtmlUtils.colorResToHtmlColor(context, R.color.reader_post_body_link)
 
         // full-size image width must take margin into account
-        mFullSizeImageWidthPx = displayWidthPx - (detailMarginWidthPx * 2);
+        mFullSizeImageWidthPx = displayWidthPx - (detailMarginWidthPx * 2)
 
         // 16:9 ratio (YouTube standard)
-        mVideoWidthPx = mFullSizeImageWidthPx - (marginLargePx * 2);
-        mVideoHeightPx = (int) (mVideoWidthPx * 0.5625f);
+        mVideoWidthPx = mFullSizeImageWidthPx - (marginLargePx * 2)
+        mVideoHeightPx = (mVideoWidthPx * 0.5625f).toInt()
     }
 }
