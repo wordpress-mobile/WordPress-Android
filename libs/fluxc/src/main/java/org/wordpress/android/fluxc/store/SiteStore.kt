c686520e2cd618644711ed90ce2357b10359df01
Nick Bradbury <nick.bradbury@gmail.com>
2025-05-13 08:54:43 -0400

    Removed unused visibleSitesAccessedViaWPCom

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/SiteStore.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/store/SiteStore.kt
@@ -1236,13 +1236,6 @@ open class SiteStore @Inject constructor(
     }
 
     /**
-     * Returns all visible .COM sites as [SiteModel]s.
-     */
-    @Deprecated("Site visibility is no longer a feature", ReplaceWith(""))
-    val visibleSitesAccessedViaWPCom: List<SiteModel>
-        get() = siteSqlUtils.sitesAccessedViaWPComRest.asModel
-
-    /**
      * Given a (remote) site id, returns the corresponding (local) id.
      */
     fun getLocalIdForRemoteSiteId(siteId: Long): Int {
