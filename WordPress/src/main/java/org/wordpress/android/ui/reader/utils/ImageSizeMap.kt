56a4f03bf6f9dc16ca2cd97712e52fb878f63c44
Nick Bradbury <nick.bradbury@gmail.com>
2025-05-16 07:44:20 -0400

    Fixed issues with auto-conversion

--- WordPress/src/main/java/org/wordpress/android/ui/reader/utils/ImageSizeMap.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/utils/ImageSizeMap.kt
@@ -1,70 +1,34 @@
 package org.wordpress.android.ui.reader.utils
 
-import android.net.Uri
-import android.text.TextUtils
 import org.json.JSONException
 import org.json.JSONObject
 import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.JSONUtils
 import org.wordpress.android.util.UrlUtils
+import androidx.core.net.toUri
 
 /**
  * hash map of sizes of attachments in a reader post - created from the json "attachments" section
  * of the post endpoints
  */
-class ImageSizeMap(postContent: String, jsonString: String) :
-    HashMap<String?, ImageSizeMap.ImageSize?>() {
+class ImageSizeMap(private val postContent: String, private val jsonString: String) :
+    HashMap<String, ImageSizeMap.ImageSize>() {
     init {
-        if (TextUtils.isEmpty(jsonString) || jsonString == EMPTY_JSON) {
-            return
+        if (jsonString.isNotEmpty() && jsonString != EMPTY_JSON) {
+            parseJson()
         }
+    }
 
+    @Suppress("NestedBlockDepth")
+    private fun parseJson() {
         try {
             val json = JSONObject(jsonString)
-            val it = json.keys()
-            if (!it.hasNext()) {
-                return
-            }
-
-            while (it.hasNext()) {
-                val jsonAttach = json.optJSONObject(it.next())
-                if (jsonAttach != null && JSONUtils.getString(jsonAttach, "mime_type")
-                        .startsWith("image")
-                ) {
-                    val normUrl =
-                        UrlUtils.normalizeUrl(
-                            UrlUtils.removeQuery(
-                                JSONUtils.getString(
-                                    jsonAttach,
-                                    "URL"
-                                )
-                            )
-                        )
-
-                    // make sure this image actually appears in the post content - it's possible for
-                    // an image to be in the attachments but not in the post itself
-                    val path = Uri.parse(normUrl).path
-                    if (postContent.contains(path!!)) {
-                        var width = jsonAttach.optInt("width")
-                        var height = jsonAttach.optInt("height")
-
-                        // chech if data-orig-size is present and use it
-                        val originalSize = jsonAttach.optString("data-orig-size", null)
-                        if (originalSize != null) {
-                            val sizes =
-                                originalSize.split(",".toRegex()).dropLastWhile { it.isEmpty() }
-                                    .toTypedArray()
-                            if (sizes != null && sizes.size == 2) {
-                                width = sizes[0].toInt()
-                                height = sizes[1].toInt()
-                            }
-                        }
-
-                        this[normUrl] =
-                            ImageSize(
-                                width,
-                                height
-                            )
+            val keys = json.keys()
+            while (keys.hasNext()) {
+                json.optJSONObject(keys.next())?.let { jsonAttach ->
+                    val mimeType = JSONUtils.getString(jsonAttach, "mime_type")
+                    if (mimeType.startsWith("image")) {
+                        parseJsonImage(jsonAttach)
                     }
                 }
             }
@@ -73,28 +37,47 @@ class ImageSizeMap(postContent: String, jsonString: String) :
         }
     }
 
-    fun getImageSize(imageUrl: String?): ImageSize? {
-        return if (imageUrl == null) {
-            null
-        } else {
-            super.get(UrlUtils.normalizeUrl(UrlUtils.removeQuery(imageUrl)))
+    private fun parseJsonImage(jsonImage: JSONObject) {
+        val normUrl =
+            UrlUtils.normalizeUrl(
+                UrlUtils.removeQuery(
+                    JSONUtils.getString(
+                        jsonImage,
+                        "URL"
+                    )
+                )
+            )
+
+        // make sure this image actually appears in the post content - it's possible for
+        // an image to be in the attachments but not in the post itself
+        val path = normUrl.toUri().path
+        if (postContent.contains(path!!)) {
+            var width = jsonImage.optInt("width")
+            var height = jsonImage.optInt("height")
+
+            // check if data-orig-size is present and use it
+            val originalSize = jsonImage.optString("data-orig-size")
+            val sizes = originalSize.split(",".toRegex()).dropLastWhile { it.isEmpty() }
+                .toTypedArray()
+            if (sizes.size == 2) {
+                width = sizes[0].toInt()
+                height = sizes[1].toInt()
+            }
+
+            this[normUrl] =
+                ImageSize(
+                    width,
+                    height
+                )
         }
     }
 
-    fun getLargestImageUrl(minImageWidth: Int): String? {
-        var currentImageUrl: String? = null
-        var currentMaxWidth = minImageWidth
-        for ((key, value) in this) {
-            if (value!!.width > currentMaxWidth) {
-                currentImageUrl = key
-                currentMaxWidth = value.width
-            }
-        }
-
-        return currentImageUrl
+    fun getImageSize(imageUrl: String): ImageSize? {
+        return super.get(UrlUtils.normalizeUrl(UrlUtils.removeQuery(imageUrl)))
     }
 
     class ImageSize(val width: Int, val height: Int)
+
     companion object {
         private const val EMPTY_JSON = "{}"
     }
