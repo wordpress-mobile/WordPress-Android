3be7ddd86056de05a5ebab157dc2e753ce1ab0ad
Nick Bradbury <nick.bradbury@gmail.com>
2025-05-16 07:43:55 -0400

    Auto-conversion of ImageSizeMap to Kotlin

--- WordPress/src/main/java/org/wordpress/android/ui/reader/utils/ImageSizeMap.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/utils/ImageSizeMap.kt
@@ -1,100 +1,101 @@
-package org.wordpress.android.ui.reader.utils;
+package org.wordpress.android.ui.reader.utils
 
-import android.net.Uri;
-import android.text.TextUtils;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.wordpress.android.util.AppLog;
-import org.wordpress.android.util.JSONUtils;
-import org.wordpress.android.util.UrlUtils;
-
-import java.util.HashMap;
-import java.util.Iterator;
+import android.net.Uri
+import android.text.TextUtils
+import org.json.JSONException
+import org.json.JSONObject
+import org.wordpress.android.util.AppLog
+import org.wordpress.android.util.JSONUtils
+import org.wordpress.android.util.UrlUtils
 
 /**
  * hash map of sizes of attachments in a reader post - created from the json "attachments" section
  * of the post endpoints
  */
-public class ImageSizeMap extends HashMap<String, ImageSizeMap.ImageSize> {
-    private static final String EMPTY_JSON = "{}";
-
-    public ImageSizeMap(@NonNull String postContent, String jsonString) {
-        if (TextUtils.isEmpty(jsonString) || jsonString.equals(EMPTY_JSON)) {
-            return;
+class ImageSizeMap(postContent: String, jsonString: String) :
+    HashMap<String?, ImageSizeMap.ImageSize?>() {
+    init {
+        if (TextUtils.isEmpty(jsonString) || jsonString == EMPTY_JSON) {
+            return
         }
 
         try {
-            JSONObject json = new JSONObject(jsonString);
-            Iterator<String> it = json.keys();
+            val json = JSONObject(jsonString)
+            val it = json.keys()
             if (!it.hasNext()) {
-                return;
+                return
             }
 
             while (it.hasNext()) {
-                JSONObject jsonAttach = json.optJSONObject(it.next());
-                if (jsonAttach != null && JSONUtils.getString(jsonAttach, "mime_type").startsWith("image")) {
-                    String normUrl =
-                            UrlUtils.normalizeUrl(UrlUtils.removeQuery(JSONUtils.getString(jsonAttach, "URL")));
+                val jsonAttach = json.optJSONObject(it.next())
+                if (jsonAttach != null && JSONUtils.getString(jsonAttach, "mime_type")
+                        .startsWith("image")
+                ) {
+                    val normUrl =
+                        UrlUtils.normalizeUrl(
+                            UrlUtils.removeQuery(
+                                JSONUtils.getString(
+                                    jsonAttach,
+                                    "URL"
+                                )
+                            )
+                        )
 
                     // make sure this image actually appears in the post content - it's possible for
                     // an image to be in the attachments but not in the post itself
-                    String path = Uri.parse(normUrl).getPath();
-                    if (postContent.contains(path)) {
-                        int width = jsonAttach.optInt("width");
-                        int height = jsonAttach.optInt("height");
+                    val path = Uri.parse(normUrl).path
+                    if (postContent.contains(path!!)) {
+                        var width = jsonAttach.optInt("width")
+                        var height = jsonAttach.optInt("height")
 
                         // chech if data-orig-size is present and use it
-                        String originalSize = jsonAttach.optString("data-orig-size", null);
+                        val originalSize = jsonAttach.optString("data-orig-size", null)
                         if (originalSize != null) {
-                            String[] sizes = originalSize.split(",");
-                            if (sizes != null && sizes.length == 2) {
-                                width = Integer.parseInt(sizes[0]);
-                                height = Integer.parseInt(sizes[1]);
+                            val sizes =
+                                originalSize.split(",".toRegex()).dropLastWhile { it.isEmpty() }
+                                    .toTypedArray()
+                            if (sizes != null && sizes.size == 2) {
+                                width = sizes[0].toInt()
+                                height = sizes[1].toInt()
                             }
                         }
 
-                        this.put(normUrl, new ImageSize(width, height));
+                        this[normUrl] =
+                            ImageSize(
+                                width,
+                                height
+                            )
                     }
                 }
             }
-        } catch (JSONException e) {
-            AppLog.e(AppLog.T.READER, e);
+        } catch (e: JSONException) {
+            AppLog.e(AppLog.T.READER, e)
         }
     }
 
-    @Nullable
-    public ImageSize getImageSize(final String imageUrl) {
-        if (imageUrl == null) {
-            return null;
+    fun getImageSize(imageUrl: String?): ImageSize? {
+        return if (imageUrl == null) {
+            null
         } else {
-            return super.get(UrlUtils.normalizeUrl(UrlUtils.removeQuery(imageUrl)));
+            super.get(UrlUtils.normalizeUrl(UrlUtils.removeQuery(imageUrl)))
         }
     }
 
-    public String getLargestImageUrl(int minImageWidth) {
-        String currentImageUrl = null;
-        int currentMaxWidth = minImageWidth;
-        for (Entry<String, ImageSize> attach : this.entrySet()) {
-            if (attach.getValue().width > currentMaxWidth) {
-                currentImageUrl = attach.getKey();
-                currentMaxWidth = attach.getValue().width;
+    fun getLargestImageUrl(minImageWidth: Int): String? {
+        var currentImageUrl: String? = null
+        var currentMaxWidth = minImageWidth
+        for ((key, value) in this) {
+            if (value!!.width > currentMaxWidth) {
+                currentImageUrl = key
+                currentMaxWidth = value.width
             }
         }
 
-        return currentImageUrl;
+        return currentImageUrl
     }
 
-    public static class ImageSize {
-        public final int width;
-        public final int height;
-
-        public ImageSize(int width, int height) {
-            this.width = width;
-            this.height = height;
-        }
+    class ImageSize(val width: Int, val height: Int)
+    companion object {
+        private const val EMPTY_JSON = "{}"
     }
 }
