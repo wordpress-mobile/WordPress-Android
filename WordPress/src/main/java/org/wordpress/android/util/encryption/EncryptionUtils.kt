e361cd661064db85e5b19e1643ff5bff9538195b
adalpari <adalpari@gmail.com>
2025-05-21 13:04:25 +0200

    Changing a bit the helper

--- WordPress/src/main/java/org/wordpress/android/util/encryption/EncryptionUtils.kt
+++ WordPress/src/main/java/org/wordpress/android/util/encryption/EncryptionUtils.kt
@@ -2,11 +2,15 @@ package org.wordpress.android.util.encryption
 
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import org.wordpress.android.modules.BG_THREAD
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.GCMParameterSpec
 import javax.inject.Inject
+import javax.inject.Named
 import javax.inject.Singleton
 
 
@@ -15,13 +19,15 @@ private const val PROVIDER_NAME = "AndroidKeyStore"
 private const val CIPHER_TRANSFORMATION_TYPE = "AES/GCM/NoPadding"
 
 @Singleton
-class EncryptionUtils @Inject constructor() {
+class EncryptionUtils @Inject constructor(
+    @param:Named(BG_THREAD) private val bgDispatcher: CoroutineDispatcher,
+) {
     private var iv: ByteArray? = null
     private val secretKey by lazy {
-        getSecretKey()
+        initSecretKey()
     }
 
-    private fun getSecretKey(): SecretKey {
+    private fun initSecretKey(): SecretKey {
         val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, PROVIDER_NAME)
         keyGenerator.init(
             KeyGenParameterSpec.Builder(
@@ -35,21 +41,18 @@ class EncryptionUtils @Inject constructor() {
         return keyGenerator.generateKey()
     }
 
-    fun encrypt(data: String): String {
-        val cipher = Cipher.getInstance(CIPHER_TRANSFORMATION_TYPE)
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
-        iv = cipher.iv // Initialization vector
-        return String(cipher.doFinal(data.toByteArray()))
+   suspend fun encrypt(data: String): ByteArray = withContext(bgDispatcher) {
+       val cipher = Cipher.getInstance(CIPHER_TRANSFORMATION_TYPE)
+       cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+       iv = cipher.iv // Initialization vector
+       cipher.doFinal(data.toByteArray(Charsets.UTF_8))
     }
 
-    fun decrypt(encryptedData: String): String? {
-        if (iv == null) {
-            return null
-        }
+    suspend fun decrypt(encryptedData: ByteArray): String = withContext(bgDispatcher) {
         val cipher = Cipher.getInstance(CIPHER_TRANSFORMATION_TYPE)
         val spec = GCMParameterSpec(128, iv)
         cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
-        val decryptedData: ByteArray = cipher.doFinal(encryptedData.toByteArray())
-        return String(decryptedData)
+        val decryptedData = cipher.doFinal(encryptedData)
+        String(decryptedData)
     }
 }
