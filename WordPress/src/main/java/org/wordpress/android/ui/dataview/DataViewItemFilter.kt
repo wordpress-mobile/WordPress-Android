c0fd06861799055e15b0630314cf3f3c5c288597
Nick Bradbury <nick.bradbury@gmail.com>
2025-06-11 08:51:59 -0400

    Moved filter to its own file

---
+++ WordPress/src/main/java/org/wordpress/android/ui/dataview/DataViewItemFilter.kt
@@ -0,0 +1,10 @@
+package org.wordpress.android.ui.dataview
+
+/**
+ * Represents a single filter for displaying in a [DataViewItemCard].
+ */
+data class DataViewFilterItem(
+    val id: Long,
+    val title: String,
+)
+
