e2356e31ec56079f36af38002e1789e5c859b751
Nick Bradbury <nick.bradbury@gmail.com>
2025-06-16 11:42:52 -0400

    Changed open class to data class

--- WordPress/src/main/java/org/wordpress/android/ui/dataview/DataViewItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/dataview/DataViewItem.kt
@@ -4,7 +4,7 @@ package org.wordpress.android.ui.dataview
  * Represents a basic model for data to be displayed in a [DataViewItemCard]. The optional image
  * will appear to the left of the fields.
  */
-open class DataViewItem(
+data class DataViewItem(
     val id: Long,
     val image: DataViewItemImage?,
     val fields: List<DataViewItemField>,
