a7fda9cf7f5824985c47f7a807ff0dd2a8825d7e
adalpari <adalpari@gmail.com>
2025-06-18 14:19:48 +0200

    Adding methods

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordLoginActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordLoginActivity.kt
@@ -2,30 +2,17 @@ package org.wordpress.android.ui.accounts.applicationpassword
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
-import org.wordpress.android.R
-import org.wordpress.android.ui.accounts.LoginNavigationEvents
-import org.wordpress.android.ui.accounts.LoginNavigationEvents.ShowNoJetpackSites
-import org.wordpress.android.ui.accounts.LoginNavigationEvents.ShowSiteAddressError
-import org.wordpress.android.ui.accounts.LoginViewModel
-import org.wordpress.android.ui.accounts.login.ApplicationPasswordLoginHelper
 import org.wordpress.android.ui.main.BaseAppCompatActivity
 import org.wordpress.android.ui.main.WPMainActivity
-import org.wordpress.android.util.ToastUtils
-import org.wordpress.android.viewmodel.Event
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class ApplicationPasswordLoginActivity: BaseAppCompatActivity() {
-    @Inject
-    lateinit var applicationPasswordLoginHelper: ApplicationPasswordLoginHelper
-
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
@@ -34,55 +21,27 @@ class ApplicationPasswordLoginActivity: BaseAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initViewModel()
-        tryToSaveCredentialsAndRunMain()
+        viewModel?.setupSite(intent.dataString.orEmpty())
     }
 
     private fun initViewModel() {
         viewModel = ViewModelProvider(this, viewModelFactory)[ApplicationPasswordLoginViewModel::class.java]
 
         // initObservers
-        viewModel!!.runMain.onEach(this::handleActionEvents).launchIn(lifecycleScope)
+        viewModel!!.onFinishedEvent.onEach(this::runMainIdNecessary).launchIn(lifecycleScope)
     }
 
-    private fun tryToSaveCredentialsAndRunMain() {
-        lifecycleScope.launch {
-            val dataString = intent.dataString.orEmpty()
-            val credentialsStored =
-                applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(dataString)
-
-            if (credentialsStored) {
-                ToastUtils.showToast(
-                    this@ApplicationPasswordLoginActivity,
-                    getString(
-                        R.string.application_password_credentials_stored,
-                        applicationPasswordLoginHelper.getSiteUrlFromUrl(dataString)
-                    )
-                )
-                intent.setData(null)
-            }
-
-            val mainActivityIntent =
-                Intent(this@ApplicationPasswordLoginActivity, WPMainActivity::class.java)
-            mainActivityIntent.setFlags(
-                (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
-                        or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            )
-            startActivity(mainActivityIntent)
-            finish()
+    private fun runMainIdNecessary(credentialsStored: Boolean) {
+        if (credentialsStored) {
+            intent.setData(null)
         }
-    }
-
-    private fun runMainIdNecessary(runMain: Boolean) {
-        if (!runMain) {
-            return
-        }
-        viewModel?.setupSite()
-        viewModel?.runMain?.let { runMainFlow ->
-            runMainFlow.onEach { runMain ->
-                if (runMain) {
-                    tryToSaveCredentialsAndRunMain()
-                }
-            }.launchIn(lifecycleScope)
-        }
+        val mainActivityIntent =
+            Intent(this@ApplicationPasswordLoginActivity, WPMainActivity::class.java)
+        mainActivityIntent.setFlags(
+            (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+                    or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        )
+        startActivity(mainActivityIntent)
+        finish()
     }
 }
--- WordPress/src/main/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordLoginViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordLoginViewModel.kt
@@ -1,19 +1,45 @@
 package org.wordpress.android.ui.accounts.applicationpassword
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import org.wordpress.android.ui.mysite.menu.MenuViewModel.SnackbarMessage
-import org.wordpress.android.util.BuildConfigWrapper
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.wordpress.android.modules.IO_THREAD
+import org.wordpress.android.ui.accounts.login.ApplicationPasswordLoginHelper
 import javax.inject.Inject
+import javax.inject.Named
 
-class ApplicationPasswordLoginViewModel @Inject constructor() : ViewModel() {
+class ApplicationPasswordLoginViewModel @Inject constructor(
+    private val applicationPasswordLoginHelper: ApplicationPasswordLoginHelper,
+    @Named(IO_THREAD) private val ioDispatcher: CoroutineDispatcher,
+) : ViewModel() {
+    private val _onFinishedEvent = MutableSharedFlow<Boolean>()
+    val onFinishedEvent = _onFinishedEvent.asSharedFlow()
 
-    private val _runMain = MutableSharedFlow<Boolean>()
-    val runMain = _runMain.asSharedFlow()
-
-    fun setupSite() {
+    fun setupSite(rawCredentials: String) {
         // TODO: Init site
         // TODO store credentials
+        viewModelScope.launch {
+            saveCredentials(rawCredentials)
+        }
+    }
+
+    suspend private fun saveCredentials(rawCredentials: String): Boolean = withContext(ioDispatcher) {
+        if (rawCredentials.isEmpty()) {
+            false
+        } else {
+            val credentialsStoredResult = async { applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(rawCredentials) }
+            val credentialsStored = credentialsStoredResult.await()
+            Log.d(
+                "ApplicationPasswordLoginViewModel",
+                "Credentials stored: $credentialsStored"
+            )
+            credentialsStored
+        }
     }
 }
