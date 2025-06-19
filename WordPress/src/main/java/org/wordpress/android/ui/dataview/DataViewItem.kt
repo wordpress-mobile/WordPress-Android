56f9c111f43f0c098c2638fb1f396706048e7c58
Nick Bradbury <nick.bradbury@gmail.com>
2025-06-19 10:25:40 -0400

    Shortened comment

--- WordPress/src/main/java/org/wordpress/android/ui/dataview/DataViewItem.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/dataview/DataViewItem.kt
@@ -1,9 +1,8 @@
 package org.wordpress.android.ui.dataview
 
 /**
- * Represents a basic model for data to be displayed in a [DataViewItemCard]. The optional image
- * will appear to the left of the fields. Note that [data] is optional but is intended to store
- * the actual data object associated with the item..
+ * Represents a basic model for data to be displayed in a [DataViewItemCard]. Note that [data] is
+ * optional but is intended to store the actual data object associated with the item..
  */
 data class DataViewItem(
     val id: Long,
