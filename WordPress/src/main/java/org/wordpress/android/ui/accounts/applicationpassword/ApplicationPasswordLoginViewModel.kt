4b65418fbb4aa16e53241a555b43aca199482247
adalpari <adalpari@gmail.com>
2025-06-19 20:25:13 +0200

    Re adding the site store calls

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordLoginViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordLoginViewModel.kt
@@ -8,6 +8,11 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.wordpress.android.fluxc.generated.SiteActionBuilder
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.network.discovery.SelfHostedEndpointFinder
+import org.wordpress.android.fluxc.network.xmlrpc.site.SiteXMLRPCClient
+import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.modules.IO_THREAD
 import org.wordpress.android.ui.accounts.login.ApplicationPasswordLoginHelper
 import javax.inject.Inject
@@ -18,6 +23,9 @@ private const val TAG = "ApplicationPasswordLoginViewModel"
 class ApplicationPasswordLoginViewModel @Inject constructor(
     @Named(IO_THREAD) private val ioDispatcher: CoroutineDispatcher,
     private val applicationPasswordLoginHelper: ApplicationPasswordLoginHelper,
+    private val selfHostedEndpointFinder: SelfHostedEndpointFinder,
+    private val siteStore: SiteStore,
+    private val siteXMLRPCClient: SiteXMLRPCClient
 ) : ViewModel() {
     private val _onFinishedEvent = MutableSharedFlow<Boolean>()
     val onFinishedEvent = _onFinishedEvent.asSharedFlow()
@@ -29,10 +37,41 @@ class ApplicationPasswordLoginViewModel @Inject constructor(
      */
     fun setupSite(rawData: String) {
         viewModelScope.launch {
-            val stored = storeCredentials(rawData)
-            _onFinishedEvent.emit(stored)
+            val urlLogin = applicationPasswordLoginHelper.getSiteUrlLoginFromRawData(rawData)
+            val siteStored = storeSite(urlLogin)
+            _onFinishedEvent.emit(true)
         }
     }
+
+    suspend private fun storeSite(urlLogin: ApplicationPasswordLoginHelper.UriLogin): Boolean = withContext(ioDispatcher) {
+        try {
+            if (urlLogin.user.isNullOrEmpty() || urlLogin.password.isNullOrEmpty() || urlLogin.siteUrl.isNullOrEmpty()) {
+                Log.e(TAG, "Cannot store site: rawData is empty")
+                false
+            } else {
+                val xmlRpcEndpoint =
+                    selfHostedEndpointFinder.verifyOrDiscoverXMLRPCEndpoint(urlLogin.siteUrl)
+                Log.d(TAG, "Endpoint: $xmlRpcEndpoint")
+
+                siteStore.onAction(
+                    SiteActionBuilder.newFetchSiteAction(
+                        SiteModel().apply {
+                            xmlRpcUrl = xmlRpcEndpoint
+                            url = urlLogin.siteUrl
+                            apiRestUsernamePlain = urlLogin.user
+                            apiRestPasswordPlain = urlLogin.password
+                        }
+                    )
+                )
+
+                true
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "Error getting XML-RPC endpoint", e)
+            false
+        }
+    }
+
 
     @Suppress("TooGenericExceptionCaught")
     private suspend fun storeCredentials(rawData: String): Boolean = withContext(ioDispatcher) {
