cdeec59d78ba7c95dc199bddaf443367faf25f81
Nick Bradbury <nick.bradbury@gmail.com>
2025-06-20 11:41:05 -0400

    Make detail screen scrollable

--- WordPress/src/main/java/org/wordpress/android/ui/subscribers/SubscriberDetailScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/subscribers/SubscriberDetailScreen.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Person
@@ -53,7 +55,8 @@ fun SubscriberDetailScreen(
         modifier = modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // Profile Photo
