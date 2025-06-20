a90c3ca968b297a9ae6da7456f6d70b3422eaab4
adalpari <adalpari@gmail.com>
2025-06-20 10:50:42 +0200

    Adding tests

--- WordPress/src/test/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordLoginViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordLoginViewModelTest.kt
@@ -14,26 +14,47 @@ import org.wordpress.android.BaseUnitTest
 import org.wordpress.android.ui.accounts.login.ApplicationPasswordLoginHelper
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
+import org.wordpress.android.fluxc.network.discovery.SelfHostedEndpointFinder
+import org.wordpress.android.fluxc.store.SiteStore
+import org.wordpress.android.fluxc.generated.SiteActionBuilder
+import org.wordpress.android.fluxc.store.SiteStore.RefreshSitesXMLRPCPayload
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.doThrow
+import org.mockito.kotlin.doReturn
+import kotlin.test.assertEquals
 
 @ExperimentalCoroutinesApi
 class ApplicationPasswordLoginViewModelTest : BaseUnitTest() {
     @Mock
     lateinit var applicationPasswordLoginHelper: ApplicationPasswordLoginHelper
+    @Mock
+    lateinit var selfHostedEndpointFinder: SelfHostedEndpointFinder
+    @Mock
+    lateinit var siteStore: SiteStore
 
     private lateinit var viewModel: ApplicationPasswordLoginViewModel
+
+    private val rawData = "url=callback?site_url=https://example.com&user_login=user&password=pass"
+    private val uriLogin = ApplicationPasswordLoginHelper.UriLogin("https://example.com", "user", "pass")
 
     @Before
     fun setUp() {
         MockitoAnnotations.openMocks(this)
-        viewModel = ApplicationPasswordLoginViewModel(testDispatcher(), applicationPasswordLoginHelper)
+        viewModel = ApplicationPasswordLoginViewModel(
+            testDispatcher(),
+            applicationPasswordLoginHelper,
+            selfHostedEndpointFinder,
+            siteStore
+        )
+        whenever(applicationPasswordLoginHelper.getSiteUrlLoginFromRawData(rawData)).thenReturn(uriLogin)
     }
 
     @Test
-    fun `valid rawData stores credentials and emits true`() = runTest {
+    fun `valid rawData stores credentials and emits siteUrl`() = runTest {
         // Given
-        val rawData = "some valid raw data"
-        whenever(applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(rawData))
-            .thenReturn(true)
+        whenever(applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(rawData)).thenReturn(true)
 
         // When
         viewModel.onFinishedEvent.test {
@@ -41,57 +62,38 @@ class ApplicationPasswordLoginViewModelTest : BaseUnitTest() {
 
             // Then
             val finishedEvent = awaitItem()
-            assertTrue(finishedEvent, "onFinishedEvent should emit true")
+            assertEquals(uriLogin.siteUrl, finishedEvent, "onFinishedEvent should emit the siteUrl")
             verify(applicationPasswordLoginHelper, times(1))
                 .storeApplicationPasswordCredentialsFrom(rawData)
+            verify(selfHostedEndpointFinder, times(0)).verifyOrDiscoverXMLRPCEndpoint(any())
             cancelAndIgnoreRemainingEvents()
         }
     }
 
     @Test
-    fun `valid rawData storing fails emits true`() = runTest {
+    fun `empty rawData does not store credentials nor site and emits null`()= runTest {
         // Given
-        val rawData = "some invalid raw data"
-        whenever(applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(rawData))
-            .thenReturn(false)
+        val emptyRawData = ""
 
         // When
         viewModel.onFinishedEvent.test {
-            viewModel.setupSite(rawData)
+            viewModel.setupSite(emptyRawData)
 
             // Then
             val finishedEvent = awaitItem()
-            assertFalse(finishedEvent, "onFinishedEvent should emit false")
-            verify(applicationPasswordLoginHelper, times(1))
-                .storeApplicationPasswordCredentialsFrom(rawData)
-            cancelAndIgnoreRemainingEvents()
-        }
-    }
-
-    @Test
-    fun `empty rawData does not store credentials and emits true`()= runTest {
-        // Given
-        val rawData = ""
-
-        // When
-        viewModel.onFinishedEvent.test {
-            viewModel.setupSite(rawData)
-
-            // Then
-            val finishedEvent = awaitItem()
-            assertFalse(finishedEvent, "onFinishedEvent should emit false")
+            assertEquals(finishedEvent, null, "onFinishedEvent should emit null")
             verify(applicationPasswordLoginHelper, times(0))
                 .storeApplicationPasswordCredentialsFrom(rawData)
+            verify(selfHostedEndpointFinder, times(0)).verifyOrDiscoverXMLRPCEndpoint(any())
             cancelAndIgnoreRemainingEvents()
         }
     }
 
     @Test
-    fun `storeCredentials throws exception emits true and logs error`() = runTest {
+    fun `when credentials not stored, fetchSites succeeds, emits siteUrl`() = runTest {
         // Given
-        val rawData = "some invalid raw data"
-        whenever(applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(rawData))
-            .thenThrow(RuntimeException())
+        whenever(applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(rawData)).thenReturn(false)
+        whenever(selfHostedEndpointFinder.verifyOrDiscoverXMLRPCEndpoint(uriLogin.siteUrl!!)).thenReturn("https://example.com/xmlrpc.php")
 
         // When
         viewModel.onFinishedEvent.test {
@@ -99,9 +101,65 @@ class ApplicationPasswordLoginViewModelTest : BaseUnitTest() {
 
             // Then
             val finishedEvent = awaitItem()
-            assertFalse(finishedEvent, "onFinishedEvent should emit false")
-            verify(applicationPasswordLoginHelper, times(1))
-                .storeApplicationPasswordCredentialsFrom(rawData)
+            assertEquals(uriLogin.siteUrl, finishedEvent, "onFinishedEvent should emit the siteUrl")
+            verify(selfHostedEndpointFinder, times(1)).verifyOrDiscoverXMLRPCEndpoint(uriLogin.siteUrl!!)
+            verify(siteStore, times(1)).onAction(any())
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when credentials not stored due to exception, fetchSites succeeds, emits siteUrl`() = runTest {
+        // Given
+        whenever(applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(rawData)).thenThrow(RuntimeException())
+        whenever(selfHostedEndpointFinder.verifyOrDiscoverXMLRPCEndpoint(uriLogin.siteUrl!!)).thenReturn("https://example.com/xmlrpc.php")
+
+        // When
+        viewModel.onFinishedEvent.test {
+            viewModel.setupSite(rawData)
+
+            // Then
+            val finishedEvent = awaitItem()
+            assertEquals(uriLogin.siteUrl, finishedEvent, "onFinishedEvent should emit the siteUrl")
+            verify(selfHostedEndpointFinder, times(1)).verifyOrDiscoverXMLRPCEndpoint(uriLogin.siteUrl!!)
+            verify(siteStore, times(1)).onAction(any())
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when credentials not stored, fetchSites fails, emits null`() = runTest {
+        // Given
+        whenever(applicationPasswordLoginHelper.storeApplicationPasswordCredentialsFrom(rawData)).thenReturn(false)
+        whenever(selfHostedEndpointFinder.verifyOrDiscoverXMLRPCEndpoint(uriLogin.siteUrl!!)).thenThrow(RuntimeException())
+
+        // When
+        viewModel.onFinishedEvent.test {
+            viewModel.setupSite(rawData)
+
+            // Then
+            val finishedEvent = awaitItem()
+            assertEquals(null, finishedEvent, "onFinishedEvent should emit null on fetchSites failure")
+            verify(selfHostedEndpointFinder, times(1)).verifyOrDiscoverXMLRPCEndpoint(uriLogin.siteUrl!!)
+            verify(siteStore, times(0)).onAction(any())
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when credentials not stored, fetchSites with missing user or password emits null`() = runTest {
+        // Given
+        val malformedRawData = "malformed ray data"
+
+        // When
+        viewModel.onFinishedEvent.test {
+            viewModel.setupSite(malformedRawData)
+
+            // Then
+            val finishedEvent = awaitItem()
+            assertEquals(null, finishedEvent, "onFinishedEvent should emit null if user or password is missing")
+            verify(selfHostedEndpointFinder, times(0)).verifyOrDiscoverXMLRPCEndpoint(any())
+            verify(siteStore, times(0)).onAction(any())
             cancelAndIgnoreRemainingEvents()
         }
     }
