25224317f349e427bf9068bcfc75ed0f9638734b
Nick Bradbury <nick.bradbury@gmail.com>
2025-06-25 14:44:25 -0400

    Changed sort order

--- WordPress/src/main/java/org/wordpress/android/ui/subscribers/SubscribersViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/subscribers/SubscribersViewModel.kt
@@ -54,16 +54,16 @@ class SubscribersViewModel @Inject constructor(
                 titleRes = R.string.subscribers_sort_date
             ),
             DataViewItemFilter(
-                id = ID_SORT_PLAN,
-                titleRes = R.string.subscribers_sort_plan
+                id = ID_SORT_DISPLAY_NAME,
+                titleRes = R.string.subscribers_sort_display_name
             ),
             DataViewItemFilter(
                 id = ID_SORT_EMAIL,
                 titleRes = R.string.subscribers_sort_email
             ),
             DataViewItemFilter(
-                id = ID_SORT_NAME,
-                titleRes = R.string.subscribers_sort_name
+                id = ID_SORT_PLAN,
+                titleRes = R.string.subscribers_sort_plan
             ),
         )
     }
@@ -110,7 +110,7 @@ class SubscribersViewModel @Inject constructor(
             when (it.id) {
                 ID_SORT_DATE -> ListSubscribersSortField.DATE_SUBSCRIBED
                 ID_SORT_PLAN -> ListSubscribersSortField.PLAN
-                ID_SORT_NAME -> ListSubscribersSortField.DISPLAY_NAME
+                ID_SORT_DISPLAY_NAME -> ListSubscribersSortField.DISPLAY_NAME
                 ID_SORT_EMAIL -> ListSubscribersSortField.EMAIL_ADDRESS
                 else -> null
             }
@@ -193,7 +193,7 @@ class SubscribersViewModel @Inject constructor(
         private const val ID_FILTER_READER = 2L
 
         private const val ID_SORT_DATE = 1L
-        private const val ID_SORT_NAME = 2L
+        private const val ID_SORT_DISPLAY_NAME = 2L
         private const val ID_SORT_EMAIL = 3L
         private const val ID_SORT_PLAN = 4L
 
