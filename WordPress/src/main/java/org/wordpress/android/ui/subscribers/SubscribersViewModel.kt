5edf421922b6f1042818b2e6cd42d732d07fbe1b
Nick Bradbury <nick.bradbury@gmail.com>
2025-07-07 10:38:07 -0400

    Moved clearing the event to its own fun (again)

--- WordPress/src/main/java/org/wordpress/android/ui/subscribers/SubscribersViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/subscribers/SubscribersViewModel.kt
@@ -275,7 +275,7 @@ class SubscribersViewModel @Inject constructor(
     fun onDeleteSubscriberClick(subscriber: Subscriber) {
         appLogWrapper.d(AppLog.T.MAIN, "Clicked on delete subscriber ${subscriber.displayNameOrEmail()}")
         _uiEvent.value = UiEvent.ShowDeleteConfirmationDialog(subscriber)
-        _uiEvent.value = null
+        clearUiEvent()
     }
 
     /**
@@ -292,9 +292,13 @@ class SubscribersViewModel @Inject constructor(
                 } else {
                     _uiEvent.value = UiEvent.ShowToast(R.string.subscribers_delete_failed)
                 }
-                _uiEvent.value = null
+                clearUiEvent()
             }
         }
+    }
+
+    private fun clearUiEvent() {
+        _uiEvent.value = null
     }
 
     companion object {
