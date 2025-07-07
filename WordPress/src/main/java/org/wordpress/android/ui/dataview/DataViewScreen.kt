0db230e1b2765a6ff78e6f61ac16fddfeb8aea9d
Nick Bradbury <nick.bradbury@gmail.com>
2025-07-07 14:54:25 -0400

    Added divider

--- WordPress/src/main/java/org/wordpress/android/ui/dataview/DataViewScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/dataview/DataViewScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -287,6 +288,7 @@ private fun SortDropdownMenuButton(
                     menuExpanded = false
                 }
             )
+            HorizontalDivider()
             DropdownMenuItem(
                 text = { Text(stringResource(id = R.string.ascending)) },
                 trailingIcon = {
