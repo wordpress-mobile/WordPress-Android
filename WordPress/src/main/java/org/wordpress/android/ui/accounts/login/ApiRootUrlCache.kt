f37d718699b1a52af16f7a9af1894230fce6c31d
adalpari <adalpari@gmail.com>
2025-07-11 11:22:16 +0200

    Adding an api_root_url cache

---
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/ApiRootUrlCache.kt
@@ -0,0 +1,22 @@
+package org.wordpress.android.ui.accounts.login
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * In-memory cache to store and retrieve the ApiRootUrl between the login discovery process and the final store step
+ * This cache is necessary because between those states we are interrupting the code flow by calling external intents
+ * so we need a place to safely store the data
+ */
+@Singleton
+class ApiRootUrlCache @Inject constructor() {
+    private val cache = mutableMapOf<String, String>()
+
+    fun put(key: String, value: String) {
+        cache[key] = value
+    }
+
+    fun get(key: String): String? {
+        return cache[key]
+    }
+}
