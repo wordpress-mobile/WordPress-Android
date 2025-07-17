c7587c3141399ea28ee21e061098a7a2854195ef
Nick Bradbury <nick.bradbury@gmail.com>
2025-07-17 14:55:22 -0400

    Further reduced size of dummy profile photos

--- WordPress/src/main/java/org/wordpress/android/ui/dataview/compose/RemoteImage.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/dataview/compose/RemoteImage.kt
@@ -20,6 +20,23 @@ fun RemoteImage(
             contentDescription = null,
             modifier = modifier
         )
+    } else if (imageUrl.startsWith("android.resource://")) {
+        // Handle local drawable resources
+        val resourceId = extractResourceId(imageUrl)
+        if (resourceId != null) {
+            Image(
+                painter = painterResource(id = resourceId),
+                contentDescription = null,
+                modifier = modifier
+            )
+        } else {
+            // Fallback if resource can't be found
+            Image(
+                painter = painterResource(id = fallbackImageRes),
+                contentDescription = null,
+                modifier = modifier
+            )
+        }
     } else {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
@@ -30,5 +47,24 @@ fun RemoteImage(
             contentDescription = null,
             modifier = modifier
         )
+    }
+}
+
+private fun extractResourceId(resourceUrl: String): Int? {
+    return try {
+        val resourceName = resourceUrl.substringAfterLast("/")
+        when (resourceName) {
+            "dummy_profile_1" -> org.wordpress.android.R.drawable.dummy_profile_1
+            "dummy_profile_2" -> org.wordpress.android.R.drawable.dummy_profile_2
+            "dummy_profile_3" -> org.wordpress.android.R.drawable.dummy_profile_3
+            "dummy_profile_4" -> org.wordpress.android.R.drawable.dummy_profile_4
+            "dummy_profile_5" -> org.wordpress.android.R.drawable.dummy_profile_5
+            "dummy_profile_6" -> org.wordpress.android.R.drawable.dummy_profile_6
+            "dummy_profile_7" -> org.wordpress.android.R.drawable.dummy_profile_7
+            "dummy_profile_8" -> org.wordpress.android.R.drawable.dummy_profile_8
+            else -> null
+        }
+    } catch (e: Exception) {
+        null
     }
 }
