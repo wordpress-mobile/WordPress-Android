cf59ef2fb33aafc68c3d9cc10b9ece05ced6bc7e
Nick Bradbury <nick.bradbury@gmail.com>
2025-07-17 16:11:57 -0400

    Validate the drawable resource ID

--- WordPress/src/main/java/org/wordpress/android/ui/dataview/compose/RemoteImage.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/dataview/compose/RemoteImage.kt
@@ -1,5 +1,7 @@
 package org.wordpress.android.ui.dataview.compose
 
+import android.content.Context
+import android.util.TypedValue
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -7,6 +9,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import android.content.res.Resources
+
 
 @Composable
 fun RemoteImage(
@@ -23,14 +27,14 @@ fun RemoteImage(
     } else if (imageUrl.startsWith("drawable:")) {
         // Handle drawable resource ID passed as string
         val resourceId = imageUrl.removePrefix("drawable:").toIntOrNull()
-        if (resourceId != null) {
+        if (resourceId != null && isValidDrawableId(LocalContext.current, resourceId)) {
             Image(
                 painter = painterResource(id = resourceId),
                 contentDescription = null,
                 modifier = modifier
             )
         } else {
-            // Fallback if resource ID can't be parsed
+            // Fallback if resource ID can't be parsed or is invalid
             Image(
                 painter = painterResource(id = fallbackImageRes),
                 contentDescription = null,
@@ -49,3 +53,11 @@ fun RemoteImage(
         )
     }
 }
+
+private fun isValidDrawableId(context: Context, drawableId: Int): Boolean = try {
+    context.resources.getValue(drawableId, TypedValue(), true)
+    true
+} catch (e: Resources.NotFoundException) {
+    false
+}
+
