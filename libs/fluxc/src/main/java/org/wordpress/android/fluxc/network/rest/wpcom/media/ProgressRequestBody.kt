6076e225db96f216cd10e3a4d23f9d5b137e25a1
adalpari <adalpari@gmail.com>
2025-07-23 15:58:54 +0200

    Throttle progress update

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/ProgressRequestBody.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/media/ProgressRequestBody.kt
@@ -37,12 +37,24 @@ class ProgressRequestBody(
         private val listener: ProgressListener
     ) : ForwardingSink(delegate) {
         private var totalBytesWritten = 0L
+        private var lastProgressTime = 0L
+        
+        // Throttle to 2 calls per second (500ms minimum between calls)
+        private val progressThrottleMs = 500L
 
         @Throws(IOException::class)
         override fun write(source: Buffer, byteCount: Long) {
             super.write(source, byteCount)
             totalBytesWritten += byteCount
-            listener.onProgress(totalBytesWritten, contentLength)
+            
+            val currentTime = System.currentTimeMillis()
+            val isComplete = totalBytesWritten >= contentLength
+            
+            // Always send progress for completion, or if enough time has passed
+            if (isComplete || currentTime - lastProgressTime >= progressThrottleMs) {
+                listener.onProgress(totalBytesWritten, contentLength)
+                lastProgressTime = currentTime
+            }
         }
     }
 }
