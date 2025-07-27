6d996c7df5df8011de65ef68a7be41721adaf0cc
Oguz Kocer <oguz.kocer@automattic.com>
2025-07-27 00:40:47 -0400

    Revert ResolverUtility to trunk version
    Reverts database transaction KTX changes that broke existing tests.
    The KTX conversion changed exception handling behavior and return values,
    breaking test expectations for database failure scenarios.

--- WordPress/src/main/java/org/wordpress/android/resolver/ResolverUtility.kt
+++ WordPress/src/main/java/org/wordpress/android/resolver/ResolverUtility.kt
@@ -1,9 +1,6 @@
 package org.wordpress.android.resolver
 
-import android.database.SQLException
-import androidx.core.database.sqlite.transaction
 import com.wellsql.generated.QuickStartStatusModelMapper
-import org.wordpress.android.util.AppLog
 import com.wellsql.generated.QuickStartTaskModelMapper
 import com.wellsql.generated.SiteModelMapper
 import com.yarolegovich.wellsql.core.Identifiable
@@ -25,53 +22,66 @@ class ResolverUtility @Inject constructor(
     ): Boolean {
         if (items.isEmpty()) return true
 
-        return try {
-            val db = dbWrapper.giveMeWritableDb()
-            db.transaction {
-                db.delete(tableName, null, null)
-                db.delete("sqlite_sequence", "name='$tableName'", null)
-                val orderedItems = items.sortedBy { it.id }
+        var wasSuccessful: Boolean
 
-                for ((index, item) in orderedItems.withIndex()) {
-                    val sqlStatement = if (index == 0) {
-                        db.compileStatement("INSERT INTO SQLITE_SEQUENCE (name,seq) VALUES ('$tableName', ?)")
-                    } else {
-                        db.compileStatement("UPDATE SQLITE_SEQUENCE SET seq=? WHERE name='$tableName'")
-                    }
+        val db = dbWrapper.giveMeWritableDb()
+        db.beginTransaction()
+        try {
+            db.delete(tableName, null, null)
+            db.delete("sqlite_sequence", "name='$tableName'", null)
+            val orderedItems = items.sortedBy { it.id }
 
-                    sqlStatement.bindLong(1, (item.id - 1).toLong())
-                    sqlStatement.execute()
-
-                    db.insert(tableName, null, mapperAdapter.toCv(item))
+            for ((index, item) in orderedItems.withIndex()) {
+                val sqlStatement = if (index == 0) {
+                    db.compileStatement("INSERT INTO SQLITE_SEQUENCE (name,seq) VALUES ('$tableName', ?)")
+                } else {
+                    db.compileStatement("UPDATE SQLITE_SEQUENCE SET seq=? WHERE name='$tableName'")
                 }
+
+                sqlStatement.bindLong(1, (item.id - 1).toLong())
+                sqlStatement.execute()
+
+                db.insert(tableName, null, mapperAdapter.toCv(item))
             }
-            true
-        } catch (e: SQLException) {
-            AppLog.e(AppLog.T.DB, "Database transaction failed in copyWithIndexes", e)
-            false
+
+            db.setTransactionSuccessful()
+            wasSuccessful = true
+        } finally {
+            db.endTransaction()
         }
+        return wasSuccessful
     }
 
     fun copySitesWithIndexes(sites: List<SiteModel>) {
         copyWithIndexes("SiteModel", MapperAdapter(SiteModelMapper()), sites)
     }
 
-    fun copyQsDataWithIndexes(statusList: List<QuickStartStatusModel>, taskList: List<QuickStartTaskModel>): Boolean =
+    fun copyQsDataWithIndexes(statusList: List<QuickStartStatusModel>, taskList: List<QuickStartTaskModel>): Boolean {
+        var wasSuccessful = false
+
+        val db = dbWrapper.giveMeWritableDb()
+        db.beginTransaction()
+
         try {
-            dbWrapper.giveMeWritableDb().transaction {
+            if (
                 copyWithIndexes(
                     "QuickStartStatusModel",
                     MapperAdapter(QuickStartStatusModelMapper()),
                     statusList
-                ) && copyWithIndexes(
+                )
+                && copyWithIndexes(
                     "QuickStartTaskModel",
                     MapperAdapter(QuickStartTaskModelMapper()),
                     taskList
                 )
+            ) {
+                db.setTransactionSuccessful()
+                wasSuccessful = true
             }
-            true
-        } catch (e: SQLException) {
-            AppLog.e(AppLog.T.DB, "Database transaction failed in copyQsDataWithIndexes", e)
-            false
+        } finally {
+            db.endTransaction()
         }
+
+        return wasSuccessful
+    }
 }
