f89fbdd15cd21c8a77cf42c6b1085286a13e167a
Nick Bradbury <nick.bradbury@gmail.com>
2025-07-29 14:02:01 -0400

    Added shell for JetpackConnectionViewModel

---
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackconnection/JetpackConnectionViewModel.kt
@@ -0,0 +1,23 @@
+package org.wordpress.android.ui.jetpackconnection
+
+import kotlinx.coroutines.CoroutineDispatcher
+import org.wordpress.android.fluxc.utils.AppLogWrapper
+import org.wordpress.android.modules.UI_THREAD
+import org.wordpress.android.viewmodel.ScopedViewModel
+import javax.inject.Inject
+import javax.inject.Named
+
+class JetpackConnectionViewModel @Inject constructor(
+    @Named(UI_THREAD) mainDispatcher: CoroutineDispatcher,
+    private val appLogWrapper: AppLogWrapper,
+) : ScopedViewModel(mainDispatcher) {
+    // TODO
+
+    sealed class ConnectionStep {
+        data object LoginWpCom : ConnectionStep()
+        data object InstallJetpack : ConnectionStep()
+        data object ConnectSite : ConnectionStep()
+        data object ConnectWpCom : ConnectionStep()
+        data object Finalize : ConnectionStep()
+    }
+}
