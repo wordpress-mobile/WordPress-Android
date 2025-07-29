f109c500ee0324114b224ffe0f1d55105956eab4
Nick Bradbury <nick.bradbury@gmail.com>
2025-07-29 15:13:50 -0400

    Minor tweaks

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackconnection/JetpackConnectionViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackconnection/JetpackConnectionViewModel.kt
@@ -2,16 +2,28 @@ package org.wordpress.android.ui.jetpackconnection
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
+import org.wordpress.android.fluxc.store.AccountStore
 import org.wordpress.android.fluxc.utils.AppLogWrapper
 import org.wordpress.android.modules.UI_THREAD
+import org.wordpress.android.ui.mysite.SelectedSiteRepository
+import org.wordpress.android.util.AppLog
 import org.wordpress.android.viewmodel.ScopedViewModel
+import rs.wordpress.api.kotlin.WpComApiClient
+import uniffi.wp_api.JetpackConnectionClient
+import uniffi.wp_api.WpAuthentication
+import uniffi.wp_api.WpAuthenticationProvider
 import javax.inject.Inject
 import javax.inject.Named
 
 class JetpackConnectionViewModel @Inject constructor(
     @Named(UI_THREAD) mainDispatcher: CoroutineDispatcher,
+    private val selectedSiteRepository: SelectedSiteRepository,
+    private val accountStore: AccountStore,
     private val appLogWrapper: AppLogWrapper,
 ) : ScopedViewModel(mainDispatcher) {
+    @Inject
+    private lateinit var jetpackConnectionClient: JetpackConnectionClient
+
     private val _currentStep = MutableStateFlow<ConnectionStep?>(null)
     val currentStep = _currentStep
 
@@ -26,14 +38,39 @@ class JetpackConnectionViewModel @Inject constructor(
     )
     val stepStatuses = _stepStatuses
 
-    fun updateStepStatus(step: ConnectionStep, status: ConnectionStatus) {
+    private val wpComApiClient: WpComApiClient by lazy {
+        WpComApiClient(
+            WpAuthenticationProvider.staticWithAuth(
+                WpAuthentication.Bearer(token = accountStore.accessToken!!)
+            )
+        )
+    }
+
+    private fun updateStepStatus(step: ConnectionStep, status: ConnectionStatus) {
         _stepStatuses.value = _stepStatuses.value.toMutableMap().apply {
             this[step] = status
         }
     }
 
     fun setCurrentStep(step: ConnectionStep) {
+        appLogWrapper.d(AppLog.T.API, "$TAG: Setting current step to $step")
         _currentStep.value = step
+        updateStepStatus(step, ConnectionStatus.InProgress)
+    }
+
+    private suspend fun networkRequest() {
+        when (currentStep.value) {
+            ConnectionStep.LoginWpCom -> {
+                jetpackConnectionClient.connectSite(getSiteId().toString())
+            }
+            ConnectionStep.ConnectSite -> {
+                jetpackConnectionClient.connectUser(accountStore.accessToken)
+            }
+        }
+    }
+
+    private fun getSiteId(): Long {
+        return selectedSiteRepository.getSelectedSite()!!.siteId
     }
 
     sealed class ConnectionStep {
@@ -48,5 +85,9 @@ class JetpackConnectionViewModel @Inject constructor(
         data object NotStarted : ConnectionStatus()
         data object InProgress : ConnectionStatus()
         data object Completed : ConnectionStatus()
+    }
+
+    companion object {
+        private const val TAG = "JetpackConnectionViewModel"
     }
 }
