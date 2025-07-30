688cd865501cc13f50165f7818de0c2e17053098
adalpari <adalpari@gmail.com>
2025-07-30 16:40:04 +0200

    Keeping the state when rotating

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/experimentalfeatures/ExperimentalFeaturesScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/experimentalfeatures/ExperimentalFeaturesScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -233,7 +234,7 @@ fun ApplicationPasswordInfoDialog(
     onDismiss: () -> Unit,
     onConfirm: () -> Unit,
 ) {
-    var showMore by remember { mutableStateOf(false) }
+    var showMore by rememberSaveable { mutableStateOf(false) }
 
     AlertDialog(
         onDismissRequest = {},
