5779ac602fb24754117c61a9ee90dc12ca41bef4
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-01 15:10:00 -0400

    Replaced throwing exception with error() to resolve Detekt warning

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackconnection/JetpackConnectionViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackconnection/JetpackConnectionViewModel.kt
@@ -290,7 +290,7 @@ class JetpackConnectionViewModel @Inject constructor(
     private fun getSiteId() = getSite().siteId
 
     private fun getSite() = selectedSiteRepository.getSelectedSite()
-        ?: throw IllegalStateException("No site is currently selected in SelectedSiteRepository")
+        ?: error("No site is currently selected in SelectedSiteRepository")
 
     sealed class ConnectionStep {
         data object LoginWpCom : ConnectionStep()
