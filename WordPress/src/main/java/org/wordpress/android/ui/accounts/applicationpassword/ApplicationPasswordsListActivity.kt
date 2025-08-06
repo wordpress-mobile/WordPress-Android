d1f7ad5458843f92c520574f01ed48e968af6983
adalpari <adalpari@gmail.com>
2025-08-06 11:14:49 +0200

    Some adjustments

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordsListActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/applicationpassword/ApplicationPasswordsListActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -69,7 +68,7 @@ fun ApplicationPasswordsScreen(
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = stringResource(R.string.application_passwords))
+                    Text(text = stringResource(R.string.application_password_info_title))
                 },
                 navigationIcon = {
                     IconButton(onClick = onNavigateBack) {
@@ -86,8 +85,8 @@ fun ApplicationPasswordsScreen(
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(applicationPasswords) { ApplicationPassword ->
-                    ApplicationPasswordItem(applicationPassword = ApplicationPassword)
+                items(applicationPasswords) { applicationPassword ->
+                    ApplicationPasswordItem(applicationPassword = applicationPassword)
                     HorizontalDivider(
                         modifier = Modifier.padding(horizontal = 16.dp)
                     )
