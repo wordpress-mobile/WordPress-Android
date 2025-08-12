1237bc95ff99500affded0e30497db862aa271ba
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-12 14:42:47 -0400

    Simplified JetpackInstaller to always install the plugin

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackInstaller.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackInstaller.kt
@@ -6,11 +6,8 @@ import org.wordpress.android.util.AppLog
 import rs.wordpress.api.kotlin.WpApiClient
 import rs.wordpress.api.kotlin.WpRequestResult
 import uniffi.wp_api.PluginCreateParams
-import uniffi.wp_api.PluginSlug
 import uniffi.wp_api.PluginStatus
-import uniffi.wp_api.PluginUpdateParams
 import uniffi.wp_api.PluginWpOrgDirectorySlug
-import uniffi.wp_api.SparsePluginFieldWithEditContext
 import uniffi.wp_api.WpAuthenticationProvider
 import java.net.URL
 import javax.inject.Inject
@@ -22,6 +19,11 @@ class JetpackInstaller @Inject constructor(
     private val appLogWrapper: AppLogWrapper,
 ) {
     suspend fun installJetpack(site: SiteModel): InstallJetpackResult {
+        if (site.isJetpackInstalled && site.isJetpackConnected) {
+            appLogWrapper.d(AppLog.T.API, "$TAG: Jetpack is already installed and connected")
+            return InstallJetpackResult(InstallJetpackStatus.ACTIVE, HTTP_SUCCESS)
+        }
+
         val wpApiClient = WpApiClient(
             wpOrgSiteApiRootUrl = URL(site.wpApiRestUrl),
             authProvider = WpAuthenticationProvider.staticWithUsernameAndPassword(
@@ -30,30 +32,6 @@ class JetpackInstaller @Inject constructor(
             ),
         )
 
-        // Check if the Jetpack plugin is already installed and whether it's active
-        fetchJetpackPluginInfo(wpApiClient)?.let { pluginInfo ->
-            when (pluginInfo.status) {
-                PluginStatus.ACTIVE,
-                PluginStatus.NETWORK_ACTIVE -> {
-                    appLogWrapper.d(AppLog.T.API, "$TAG: Jetpack is already installed and active")
-                    return InstallJetpackResult(InstallJetpackStatus.ACTIVE, HTTP_SUCCESS)
-                }
-
-                PluginStatus.INACTIVE -> {
-                    appLogWrapper.d(AppLog.T.API, "$TAG: Jetpack is already installed but inactive")
-                    return activateJetpackPlugin(wpApiClient)
-                }
-            }
-        }
-
-        // Jetpack plugin isn't installed so install it now
-        return createJetpackPlugin(wpApiClient)
-    }
-
-    /**
-     * Called when the Jetpack plugin doesn't already exist to install it
-     */
-    private suspend fun createJetpackPlugin(wpApiClient: WpApiClient): InstallJetpackResult {
         val params = PluginCreateParams(
             slug = PluginWpOrgDirectorySlug(PLUGIN_SLUG),
             status = PluginStatus.ACTIVE,
@@ -94,84 +72,6 @@ class JetpackInstaller @Inject constructor(
         }
     }
 
-    /**
-     * Called when the Jetpack plugin already exists but is inactive to activate it
-     */
-    private suspend fun activateJetpackPlugin(wpApiClient: WpApiClient): InstallJetpackResult {
-        val params = PluginUpdateParams(
-            status = PluginStatus.ACTIVE,
-        )
-        val response = wpApiClient.request { requestBuilder ->
-            requestBuilder.plugins().update(
-                pluginSlug = PluginSlug(PLUGIN_SLUG),
-                params = params
-            )
-        }
-        when (response) {
-            is WpRequestResult.Success -> {
-                return when (response.response.data.status) {
-                    PluginStatus.ACTIVE,
-                    PluginStatus.NETWORK_ACTIVE -> {
-                        InstallJetpackResult(InstallJetpackStatus.ACTIVE, HTTP_SUCCESS)
-                    }
-
-                    PluginStatus.INACTIVE -> {
-                        InstallJetpackResult(InstallJetpackStatus.INACTIVE, HTTP_SUCCESS)
-                    }
-                }
-            }
-
-            is WpRequestResult.WpError<*> -> {
-                appLogWrapper.d(AppLog.T.API, "$TAG: Jetpack install error = ${response.statusCode}")
-                return InstallJetpackResult(InstallJetpackStatus.FAILED, response.statusCode)
-            }
-
-            is WpRequestResult.UnknownError<*> -> {
-                appLogWrapper.d(AppLog.T.API, "$TAG: Jetpack install error = ${response.statusCode}")
-                return InstallJetpackResult(InstallJetpackStatus.FAILED, response.statusCode)
-            }
-
-            else -> {
-                appLogWrapper.d(AppLog.T.API, "$TAG: Jetpack install error = $response")
-                return InstallJetpackResult(InstallJetpackStatus.FAILED, 0u)
-            }
-        }
-    }
-
-    /**
-     * Fetch the status and version of the Jetpack plugin, returns null if not installed
-     */
-    private suspend fun fetchJetpackPluginInfo(wpApiClient: WpApiClient): JetpackPluginInfo? {
-        val response = wpApiClient.request { requestBuilder ->
-            requestBuilder.plugins().filterRetrieveWithEditContext(
-                pluginSlug = PluginSlug(PLUGIN_SLUG),
-                fields = listOf(
-                    SparsePluginFieldWithEditContext.STATUS,
-                    SparsePluginFieldWithEditContext.VERSION
-                )
-            )
-        }
-        return when (response) {
-            is WpRequestResult.Success -> {
-                val status = response.response.data.status
-                val version = response.response.data.version
-                if (status != null && version != null) {
-                    JetpackPluginInfo(
-                        status = status,
-                        version = version,
-                    )
-                } else {
-                    null
-                }
-            }
-
-            else -> {
-                appLogWrapper.d(AppLog.T.API, "$TAG: Jetpack fetchPluginStatus error = $response")
-                null
-            }
-        }
-    }
-
     data class InstallJetpackResult(
         val status: InstallJetpackStatus,
         val httpStatusCode: UShort
@@ -182,11 +82,6 @@ class JetpackInstaller @Inject constructor(
         INACTIVE,
         FAILED,
     }
-
-    private data class JetpackPluginInfo(
-        val status: PluginStatus,
-        val version: String,
-    )
 
     companion object {
         private const val TAG = "JetpackInstaller"
