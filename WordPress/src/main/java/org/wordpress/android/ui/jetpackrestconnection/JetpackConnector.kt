3f2aa5a43a337a6b7c60b773569a081b875c1f6b
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-13 14:01:05 -0400

    Added fun to connect user

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackConnector.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackConnector.kt
@@ -2,23 +2,46 @@ package org.wordpress.android.ui.jetpackrestconnection
 
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.ui.jetpackrestconnection.JetpackConnectionHelper.Companion.CONNECT_FROM
+import uniffi.wp_api.WpAuthentication
+import uniffi.wp_api.WpComSiteId
 import javax.inject.Inject
 
 class JetpackConnector @Inject constructor(
     private val jetpackConnectionHelper: JetpackConnectionHelper
 ) {
     @Suppress("TooGenericExceptionCaught")
-    suspend fun connectSite(site: SiteModel): Result<ULong> {
+    suspend fun connectSite(site: SiteModel): Result<WpComSiteId> {
         try {
             val client = jetpackConnectionHelper.initJetpackConnectionClient(site)
             val wpComSiteId = client.connectSite(CONNECT_FROM)
             return if (wpComSiteId > 0u) {
                 return Result.success(wpComSiteId)
-            } else (
+            } else {
                 Result.failure(
-                    Exception("Jetpack connection failed, no site ID returned")
+                    Exception("Jetpacks site connection failed, no site ID returned")
                 )
+            }
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+    }
+
+    @Suppress("TooGenericExceptionCaught")
+    suspend fun connectUser(site: SiteModel, accessToken: String): Result<WpComSiteId> {
+        try {
+            val client = jetpackConnectionHelper.initJetpackConnectionClient(site)
+            val wpComAuthentication = WpAuthentication.Bearer(token = accessToken)
+            val wpComSiteId = client.connectUser(
+                wpComAuthentication = wpComAuthentication,
+                from = CONNECT_FROM
             )
+            return if (wpComSiteId > 0u) {
+                return Result.success(wpComSiteId)
+            } else {
+                Result.failure(
+                    Exception("Jetpack user connection failed, no site ID returned")
+                )
+            }
         } catch (e: Exception) {
             return Result.failure(e)
         }
