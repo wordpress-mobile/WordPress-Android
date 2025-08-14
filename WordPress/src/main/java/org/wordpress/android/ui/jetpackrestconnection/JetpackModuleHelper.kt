163941aa16a0e0359744e8cc788d7ded3ac3351b
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-14 11:50:25 -0400

    Fixed JetpackModuleHelper, p2

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackModuleHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackModuleHelper.kt
@@ -1,16 +1,12 @@
 package org.wordpress.android.ui.jetpackrestconnection
 
 import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.network.rest.wpcom.site.SiteRestClient
 import org.wordpress.android.fluxc.store.JetpackStore
 import org.wordpress.android.fluxc.store.JetpackStore.ActivateStatsModulePayload
-import org.wordpress.android.fluxc.store.SiteStore
 import javax.inject.Inject
 
 class JetpackModuleHelper @Inject constructor(
     private val jetpackStore: JetpackStore,
-    private val siteRestClient: SiteRestClient,
-    private val siteStore: SiteStore,
 ){
     /**
      * Activates the stats module for the passed site if not already activated.
@@ -32,24 +28,9 @@ class JetpackModuleHelper @Inject constructor(
         }
     }
 
-    /**
-     * Returns true if the passed module is activated for the passed site. If not, we
-     * fetch the site and check again, and if it's now activated then we update the
-     * site in the store.
-     */
-    private suspend fun isModuleActivated(site: SiteModel, moduleName: String): Boolean {
-        return if (site.isActiveModuleEnabled(moduleName)) {
-            true
-        } else {
-            val updatedSite = siteRestClient.fetchSite(site)
-            if (updatedSite.isActiveModuleEnabled(moduleName)) {
-                siteStore.fetchSite(updatedSite)
-                true
-            } else {
-                false
-            }
-        }
-    }
+    @Suppress("SameParameterValue")
+    private fun isModuleActivated(site: SiteModel, moduleName: String) =
+        site.isActiveModuleEnabled(moduleName)
 
     companion object {
         private const val STATS_MODULE_NAME = "stats"
