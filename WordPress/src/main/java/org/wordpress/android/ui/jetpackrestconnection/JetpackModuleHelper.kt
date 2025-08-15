e55e551afb25a9d1ae5c49a3f54bd64e6e57a02e
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-15 06:47:54 -0400

    Simplified JetpackModuleHelper

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackModuleHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackModuleHelper.kt
@@ -24,16 +24,25 @@ class JetpackModuleHelper @Inject constructor(
             }
         }
 
-        // Ignore the above response as it's unreliable, instead just fetch Jetpack sites filtering for this site
-        // and check if the module is now activated. Note that fetching the single site fails because of changes
-        // to it during the connection flow.
+        // ignore the activation response as it's unreliable, instead just check if it's activated now
+        return verifyModuleActivation(site, module)
+    }
+
+    /**
+     * Fetch Jetpack sites filtering for this site and check if the module is now activated. Note that fetching
+     * the single site fails because of changes to it during the connection flow.
+     */
+    private suspend fun verifyModuleActivation(
+        site: SiteModel,
+        module: Module
+    ): Result<Unit> {
         val payload = SiteStore.FetchSitesPayload(
             filters = listOf(SiteStore.SiteFilter.JETPACK)
         )
         return siteStore.fetchSites(payload).updatedSites.firstOrNull {
             it.siteId == site.siteId
         }?.let { updatedSite ->
-            // the updated site will have lost the REST passwords, so we need to copy them from the original site
+            // the updated site won't have the REST passwords, so we need to copy them from the original site
             copyRestPasswords(site, updatedSite)
             if (isModuleActivated(updatedSite, module)) {
                 Result.success(Unit)
@@ -48,18 +57,21 @@ class JetpackModuleHelper @Inject constructor(
      */
     private fun copyRestPasswords(fromSite: SiteModel, toSite: SiteModel) {
         siteStore.updateSite(toSite.apply {
-            apiRestUsernameEncrypted = fromSite.apiRestUsernameEncrypted
-            apiRestPasswordEncrypted = fromSite.apiRestPasswordEncrypted
-            apiRestUsernamePlain = fromSite.apiRestUsernamePlain
-            apiRestPasswordPlain = fromSite.apiRestPasswordPlain
-            apiRestUsernameIV = fromSite.apiRestUsernameIV
-            apiRestPasswordIV = fromSite.apiRestPasswordIV
+            copyRestCredentialsFrom(fromSite)
         })
     }
 
-    private fun isModuleActivated(site: SiteModel, module: Module): Boolean {
-        return site.isActiveModuleEnabled(module.moduleName)
+    private fun SiteModel.copyRestCredentialsFrom(source: SiteModel) {
+        apiRestUsernameEncrypted = source.apiRestUsernameEncrypted
+        apiRestPasswordEncrypted = source.apiRestPasswordEncrypted
+        apiRestUsernamePlain = source.apiRestUsernamePlain
+        apiRestPasswordPlain = source.apiRestPasswordPlain
+        apiRestUsernameIV = source.apiRestUsernameIV
+        apiRestPasswordIV = source.apiRestPasswordIV
     }
+
+    private fun isModuleActivated(site: SiteModel, module: Module) =
+        site.isActiveModuleEnabled(module.moduleName)
 
     private enum class Module(val moduleName: String) {
         STATS("stats"),
