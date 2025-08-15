ec1eafa9bebdc7aff3ccc1cfb5134a4d326925ef
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-15 06:48:23 -0400

    Defer copying passwords to a future PR

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackModuleHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackModuleHelper.kt
@@ -42,32 +42,12 @@ class JetpackModuleHelper @Inject constructor(
         return siteStore.fetchSites(payload).updatedSites.firstOrNull {
             it.siteId == site.siteId
         }?.let { updatedSite ->
-            // the updated site won't have the REST passwords, so we need to copy them from the original site
-            copyRestPasswords(site, updatedSite)
             if (isModuleActivated(updatedSite, module)) {
                 Result.success(Unit)
             } else {
                 Result.failure(Exception("${module.moduleName} module not activated"))
             }
         } ?: Result.failure(Exception("Site not found"))
-    }
-
-    /**
-     * Copy the REST passwords from one site to another and save the updated site
-     */
-    private fun copyRestPasswords(fromSite: SiteModel, toSite: SiteModel) {
-        siteStore.updateSite(toSite.apply {
-            copyRestCredentialsFrom(fromSite)
-        })
-    }
-
-    private fun SiteModel.copyRestCredentialsFrom(source: SiteModel) {
-        apiRestUsernameEncrypted = source.apiRestUsernameEncrypted
-        apiRestPasswordEncrypted = source.apiRestPasswordEncrypted
-        apiRestUsernamePlain = source.apiRestUsernamePlain
-        apiRestPasswordPlain = source.apiRestPasswordPlain
-        apiRestUsernameIV = source.apiRestUsernameIV
-        apiRestPasswordIV = source.apiRestPasswordIV
     }
 
     private fun isModuleActivated(site: SiteModel, module: Module) =
