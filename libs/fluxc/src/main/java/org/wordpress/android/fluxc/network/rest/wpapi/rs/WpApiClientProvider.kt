c7737031e1263717c2d345a8c804d6749acc62c9
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-19 07:30:17 -0400

    Removed test code

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpapi/rs/WpApiClientProvider.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpapi/rs/WpApiClientProvider.kt
@@ -17,7 +17,7 @@ class WpApiClientProvider @Inject constructor(
         uploadListener: WpRequestExecutor.UploadListener? = null
     ): WpApiClient {
         val authProvider = WpAuthenticationProvider.staticWithUsernameAndPassword(
-            username = site.apiRestUsernamePlain, password = site.apiRestUsernamePlain // TODO apiRestPasswordPlain
+            username = site.apiRestUsernamePlain, password = site.apiRestPasswordPlain
         )
         val apiRootUrl = URL(site.buildUrl())
         val client = WpApiClient(
