7dd5e2e2b1b01063b3a357644d8b9458943abe9d
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-21 14:02:41 -0400

    Fixed additional failing tests

--- WordPress/src/test/java/org/wordpress/android/ui/jetpackrestconnection/JetpackRestConnectionViewModelTest.kt
+++ WordPress/src/test/java/org/wordpress/android/ui/jetpackrestconnection/JetpackRestConnectionViewModelTest.kt
@@ -79,6 +79,8 @@ class JetpackRestConnectionViewModelTest : BaseUnitTest() {
 
     @Before
     fun setup() {
+        // Configure the site model to be using WpCom REST API
+        whenever(siteModel.isUsingWpComRestApi).thenReturn(true)
         whenever(selectedSiteRepository.getSelectedSite()).thenReturn(siteModel)
 
         viewModel = JetpackRestConnectionViewModel(
@@ -155,6 +157,7 @@ class JetpackRestConnectionViewModelTest : BaseUnitTest() {
         whenever(jetpackInstaller.installJetpack(any())).thenReturn(Result.success(PluginStatus.ACTIVE))
 
         viewModel.onRetryClick()
+        advanceTimeBy(TEST_ADVANCE_TIME_MS) // Need to advance time for retry to complete
 
         assertThat(viewModel.stepStates.value[ConnectionStep.InstallJetpack]?.status)
             .isEqualTo(ConnectionStatus.Completed)
@@ -165,7 +168,7 @@ class JetpackRestConnectionViewModelTest : BaseUnitTest() {
         whenever(accountStore.hasAccessToken()).thenReturn(true)
 
         viewModel.onStartClick()
-        advanceUntilIdle()
+        advanceTimeBy(TEST_ADVANCE_TIME_MS)
 
         assertThat(viewModel.stepStates.value[ConnectionStep.LoginWpCom]?.status)
             .isEqualTo(ConnectionStatus.Completed)
@@ -212,7 +215,7 @@ class JetpackRestConnectionViewModelTest : BaseUnitTest() {
         whenever(jetpackInstaller.installJetpack(any())).thenReturn(Result.success(PluginStatus.ACTIVE))
 
         viewModel.onStartClick()
-        advanceUntilIdle()
+        advanceTimeBy(TEST_ADVANCE_TIME_MS)
 
         assertStepStatus(ConnectionStep.InstallJetpack, ConnectionStatus.Completed)
     }
@@ -237,7 +240,7 @@ class JetpackRestConnectionViewModelTest : BaseUnitTest() {
         whenever(jetpackConnector.connectSite(any())).thenReturn(Result.success(TEST_SITE_ID))
 
         viewModel.onStartClick()
-        advanceUntilIdle()
+        advanceTimeBy(TEST_ADVANCE_TIME_MS)
 
         verify(siteModel).siteId = TEST_SITE_ID.toLong()
         assertStepStatus(ConnectionStep.ConnectSite, ConnectionStatus.Completed)
@@ -253,7 +256,7 @@ class JetpackRestConnectionViewModelTest : BaseUnitTest() {
         whenever(jetpackConnector.connectSite(any())).thenReturn(Result.success(TEST_SITE_ID))
 
         viewModel.onStartClick()
-        advanceUntilIdle()
+        advanceTimeBy(TEST_ADVANCE_TIME_MS)
 
         assertStepStatus(ConnectionStep.ConnectUser, ConnectionStatus.Failed, ErrorType.MissingAccessToken)
     }
@@ -267,7 +270,7 @@ class JetpackRestConnectionViewModelTest : BaseUnitTest() {
         whenever(jetpackConnector.connectUser(any(), any())).thenReturn(Result.success(TEST_USER_ID))
 
         viewModel.onStartClick()
-        advanceUntilIdle()
+        advanceTimeBy(TEST_ADVANCE_TIME_MS)
 
         verify(jetpackConnector).connectUser(eq(siteModel), eq(TEST_ACCESS_TOKEN))
         assertStepStatus(ConnectionStep.ConnectUser, ConnectionStatus.Completed)
@@ -301,7 +304,7 @@ class JetpackRestConnectionViewModelTest : BaseUnitTest() {
         viewModel.onStartClick()
         advanceTimeBy(TEST_ADVANCE_TIME_MS)
 
-        assertStepStatus(ConnectionStep.Finalize, ConnectionStatus.Failed, ErrorType.ActivateStatsFailed())
+        assertStepStatus(ConnectionStep.Finalize, ConnectionStatus.Failed, ErrorType.ActivateStatsFailed("Stats failed"))
     }
 
     @Test
