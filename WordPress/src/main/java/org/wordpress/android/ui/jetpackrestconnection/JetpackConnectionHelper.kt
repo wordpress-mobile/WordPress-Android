054a1c59a069b9e63544de190043aa3cfaa33891
Nick Bradbury <nick.bradbury@gmail.com>
2025-08-21 08:33:03 -0400

    Restored requiring app password already entered

--- WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackConnectionHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/jetpackrestconnection/JetpackConnectionHelper.kt
@@ -4,8 +4,6 @@ import org.wordpress.android.BuildConfig
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.rest.wpapi.rs.WpApiClientProvider
 import org.wordpress.android.fluxc.utils.AppLogWrapper
-import org.wordpress.android.ui.prefs.experimentalfeatures.ExperimentalFeatures
-import org.wordpress.android.ui.prefs.experimentalfeatures.ExperimentalFeatures.Feature
 import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.VersionUtils.checkMinimalVersion
 import rs.wordpress.api.kotlin.WpApiClient
@@ -20,7 +18,6 @@ import javax.inject.Inject
 
 class JetpackConnectionHelper @Inject constructor(
     private val wpApiClientProvider: WpApiClientProvider,
-    private val experimentalFeatures: ExperimentalFeatures,
     private val appLogWrapper: AppLogWrapper
 ) {
     fun initWpApiClient(site: SiteModel): WpApiClient {
@@ -73,14 +70,14 @@ class JetpackConnectionHelper @Inject constructor(
      * Returns true if the Jetpack REST Connection flow is available and this site is able to use it.
      * Requirements:
      * - Jetpack app
-     * - Experimental application password feature enabled
-     * - Self-hosted site
+     * - Self-hosted site authenticated with application password,
      * - Site isn't already connected to Jetpack
      * - Jetpack is not installed or the installed jetpack version is 14.2 or above
      */
     fun canInitiateJetpackRestConnection(site: SiteModel): Boolean {
         return BuildConfig.IS_JETPACK_APP
-                && experimentalFeatures.isEnabled(Feature.EXPERIMENTAL_APPLICATION_PASSWORD_FEATURE)
+                && site.isUsingSelfHostedRestApi
+                && !site.wpApiRestUrl.isNullOrEmpty()
                 && !site.isJetpackConnected
                 && (!site.isJetpackInstalled || checkMinimalVersion(site.jetpackVersion, JETPACK_LIMIT_VERSION))
     }
