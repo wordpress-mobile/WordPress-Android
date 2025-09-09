b6b7b11277bd13eb2b813f0abd999614e7994ede
adalpari <adalpari@gmail.com>
2025-09-09 15:04:48 +0200

    Navigation and typo

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/applicationpassword/ApplicationPasswordRequiredDialogActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/applicationpassword/ApplicationPasswordRequiredDialogActivity.kt
@@ -4,7 +4,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import org.wordpress.android.R
 
 @AndroidEntryPoint
-class ApplicationPasswordRequieredDialogActivity : ApplicationPasswordDialogActivity() {
+class ApplicationPasswordRequiredDialogActivity : ApplicationPasswordDialogActivity() {
     override fun getTitleResource(): Int = R.string.application_password_required
     override fun getDescriptionResource(): Int = R.string.application_password_required_description
     override fun getButtonTextResource(): Int = R.string.get_started
