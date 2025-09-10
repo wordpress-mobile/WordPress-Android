e6670bd7bd8b78e74317bc35df987ec54bd0641a
adalpari <adalpari@gmail.com>
2025-09-10 16:05:17 +0200

    Adding an extra note

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/applicationpassword/ApplicationPasswordRequiredDialogActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/applicationpassword/ApplicationPasswordRequiredDialogActivity.kt
@@ -6,10 +6,13 @@ import org.wordpress.android.R
 @AndroidEntryPoint
 class ApplicationPasswordRequiredDialogActivity : ApplicationPasswordDialogActivity() {
     override fun getTitleResource(): Int = R.string.application_password_required
-    override fun getDescriptionString(): String =
-        intent.getStringExtra(EXTRA_FEATURE_NAME)?.let {
+    override fun getDescriptionString(): String {
+        val baseDescription = intent.getStringExtra(EXTRA_FEATURE_NAME)?.let {
             resources.getString(R.string.application_password_required_description, it)
-        } ?: resources.getString(R.string.application_password_required_description_default)
+        } ?: resources.getString(R.string.application_password_info_description_1)
+
+        return baseDescription + resources.getString(R.string.application_password_experimental_feature_note)
+    }
     override fun getButtonTextResource(): Int = R.string.get_started
 
     companion object {
