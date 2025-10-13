2b6c494c4b59dd3b77a6268a798723e379b2df0f
adalpari <adalpari@gmail.com>
2025-10-13 16:30:01 +0200

    Removing unused func

--- libs/support/src/main/java/org/wordpress/android/support/feature/aibot/ConversationUtils.kt
+++ libs/support/src/main/java/org/wordpress/android/support/feature/aibot/ConversationUtils.kt
@@ -69,27 +69,6 @@ fun formatRelativeTime(date: Date): String {
     }
 }
 
-@Composable
-fun formatRelativeTimePreview(date: Date): String {
-    val now = Date()
-    val diffMillis = now.time - date.time
-    val diffMinutes = TimeUnit.MILLISECONDS.toMinutes(diffMillis)
-    val diffHours = TimeUnit.MILLISECONDS.toHours(diffMillis)
-    val diffDays = TimeUnit.MILLISECONDS.toDays(diffMillis)
-
-    return when {
-        diffMinutes < 1 -> "Just now"
-        diffMinutes < 60 -> "$diffMinutes minute${if (diffMinutes == 1L) "" else "s"} ago"
-        diffHours < 24 -> "$diffHours hour${if (diffHours == 1L) "" else "s"} ago"
-        diffDays < 7 -> "$diffDays day${if (diffDays == 1L) "" else "s"} ago"
-        diffDays < 30 -> "${diffDays / 7} week${if (diffDays / 7 == 1L) "" else "s"} ago"
-        else -> {
-            val formatter = SimpleDateFormat("MMM d, yyyy", Locale.getDefault())
-            formatter.format(date)
-        }
-    }
-}
-
 fun generateSampleBotConversations(): List<BotConversation> {
     val now = Date()
     return listOf(
