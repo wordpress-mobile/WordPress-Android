6c83ae3da2ca8a1969457ddd7c1d1dea1d63c385
adalpari <adalpari@gmail.com>
2025-10-17 12:46:59 +0200

    Using proper "new conversation icon"

--- WordPress/src/main/java/org/wordpress/android/support/he/ui/HEConversationsListScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/support/he/ui/HEConversationsListScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -61,7 +61,7 @@ fun HEConversationsListScreen(
                 actions = {
                     IconButton(onClick = { onCreateNewConversationClick() }) {
                         Icon(
-                            imageVector = Icons.Default.Add,
+                            imageVector = Icons.Default.Edit,
                             contentDescription = stringResource(
                                 R.string.he_support_new_conversation_content_description
                             )
