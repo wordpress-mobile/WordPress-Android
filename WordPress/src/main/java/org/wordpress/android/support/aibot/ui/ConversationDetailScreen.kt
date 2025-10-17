a22e42ed3cd89be8b6664cbd8a58576c2ed5a1ca
adalpari <adalpari@gmail.com>
2025-10-17 10:49:01 +0200

    Potential fix for scrllong issue

--- WordPress/src/main/java/org/wordpress/android/support/aibot/ui/ConversationDetailScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/support/aibot/ui/ConversationDetailScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
@@ -117,6 +118,7 @@ fun ConversationDetailScreen(
             LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
+                    .imePadding()
                     .padding(horizontal = 16.dp),
                 state = listState,
                 verticalArrangement = Arrangement.spacedBy(12.dp)
