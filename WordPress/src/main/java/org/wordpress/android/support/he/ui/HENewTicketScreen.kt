d328103fa3b6bfa59f4ce8924b8f6579bae1f43a
adalpari <adalpari@gmail.com>
2025-10-22 15:06:44 +0200

    Apply padding to create ticket screen when the keyboard is opened

--- WordPress/src/main/java/org/wordpress/android/support/he/ui/HENewTicketScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/support/he/ui/HENewTicketScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -214,6 +215,7 @@ private fun SendButton(
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .imePadding()
             .padding(16.dp)
     ) {
         Button(
