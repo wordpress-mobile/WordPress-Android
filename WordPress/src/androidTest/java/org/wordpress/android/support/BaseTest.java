95397fe9fce7a94974d96e1e4f8ba032a0612fc7
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-10-23 09:44:33 -0600

    Remove Wiremock (#22305)
    * Bump wiremock to 2.35.0
    
    * Entirely remove wiremock for now
    
    * Remove wiremock references

--- WordPress/src/androidTest/java/org/wordpress/android/support/BaseTest.java
+++ WordPress/src/androidTest/java/org/wordpress/android/support/BaseTest.java
@@ -1,26 +1,15 @@
 package org.wordpress.android.support;
 
-import android.app.Instrumentation;
-import android.util.Log;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.compose.ui.test.junit4.ComposeTestRule;
 import androidx.test.espresso.accessibility.AccessibilityChecks;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.fasterxml.jackson.databind.util.ISO8601Utils;
-import com.github.jknack.handlebars.Helper;
-import com.github.jknack.handlebars.Options;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.DateOffset;
-import com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.HandlebarsHelper;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResult;
 import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResult.AccessibilityCheckResultType;
+import com.google.gson.internal.bind.util.ISO8601Utils;
 
-import org.apache.commons.lang3.LocaleUtils;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -31,25 +20,17 @@ import org.wordpress.android.R;
 import org.wordpress.android.e2e.flows.LoginFlow;
 import org.wordpress.android.e2e.pages.MePage;
 import org.wordpress.android.e2e.pages.MySitesPage;
-import org.wordpress.android.editor.Utils;
-import org.wordpress.android.mocks.AndroidNotifier;
-import org.wordpress.android.mocks.AssetFileSource;
 import org.wordpress.android.rules.RetryTestRule;
 import org.wordpress.android.ui.WPLaunchActivity;
-import org.wordpress.android.wiremock.WireMockStub;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
 import dagger.hilt.android.testing.HiltAndroidRule;
 
 import static androidx.compose.ui.test.junit4.AndroidComposeTestRule_androidKt.createComposeRule;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResultUtils.matchesTypes;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.is;
@@ -60,7 +41,6 @@ import static org.wordpress.android.support.WPSupportUtils.isElementDisplayed;
 
 public class BaseTest {
     static final String TAG = BaseTest.class.getSimpleName();
-    public static final int WIREMOCK_PORT = 8080;
 
     @Rule(order = 0)
     public HiltAndroidRule mHiltRule = new HiltAndroidRule(this);
@@ -76,51 +56,7 @@ public class BaseTest {
             = new ActivityScenarioRule<>(WPLaunchActivity.class);
 
     @Rule(order = 4)
-    public WireMockRule wireMockRule;
-
-    @Rule(order = 5)
     public RetryTestRule retryTestRule = new RetryTestRule();
-
-    public BaseTest() {
-        this(null);
-    }
-
-    /**
-     * Constructor
-     *
-     * @param wireMockStubs the wiremock stubs to use for this specific test.
-     */
-    public BaseTest(@Nullable final List<WireMockStub> wireMockStubs) {
-        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
-        wireMockRule = new WireMockRule(
-                options().port(WIREMOCK_PORT)
-                         .fileSource(
-                             new AssetFileSource(instrumentation.getContext().getAssets())
-                         )
-
-                         .extensions(new ResponseTemplateTransformer(true, new HashMap<String, Helper>() {
-                             {
-                                 put("fnow", new UnlocalizedDateHelper());
-                             }
-                         }))
-                         .notifier(new AndroidNotifier()));
-        if (wireMockStubs != null && !wireMockStubs.isEmpty()) {
-            for (WireMockStub wireMockStub : wireMockStubs) {
-                try {
-                    final String result = Utils.getStringFromInputStream(
-                            instrumentation.getContext().getClassLoader().getResourceAsStream(
-                                    wireMockStub.getFileName()
-                            )
-                    );
-                    // This is where we can stub out
-                    wireMockRule.stubFor(WireMock.get(WireMock.urlPathMatching(wireMockStub.getUrlPath().getPath()))
-                                                 .willReturn(WireMock.aResponse().withBody(result)));
-                } catch (final Exception exception) {
-                    Log.e(TAG, "Problem stubbing endpoint", exception);
-                }
-            }
-        }
-    }
 
     @Before
     public void setup() {
@@ -172,27 +108,6 @@ public class BaseTest {
     }
 }
 
-class UnlocalizedDateHelper extends HandlebarsHelper<Object> {
-    @Override public Object apply(Object context, Options options) throws IOException {
-        String format = options.hash("format", null);
-        String offset = options.hash("offset", null);
-        String timezone = options.hash("timezone", null);
-        String localeCode = options.hash("locale", "en_US_POSIX");
-
-        Date date = new Date();
-        if (offset != null) {
-            date = new DateOffset(offset).shift(date);
-        }
-
-        Locale locale = Locale.getDefault();
-        if (localeCode != null) {
-            locale = LocaleUtils.toLocale(localeCode);
-        }
-
-        return new LocaleAwareRenderableDate(date, format, timezone, locale);
-    }
-}
-
 class LocaleAwareRenderableDate {
     private static final long DIVIDE_MILLISECONDS_TO_SECONDS = 1000L;
 
@@ -208,7 +123,7 @@ class LocaleAwareRenderableDate {
         this.mLocale = locale;
     }
 
-    @Override
+    @NonNull @Override
     public String toString() {
         if (mFormat != null) {
             if (mFormat.equals("epoch")) {
