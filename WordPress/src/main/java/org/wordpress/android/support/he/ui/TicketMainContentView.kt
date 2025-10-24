19fcdf63ef23c601cb8c11e96dafce536653a634
adalpari <adalpari@gmail.com>
2025-10-24 14:05:43 +0200

    detekt

--- WordPress/src/main/java/org/wordpress/android/support/he/ui/TicketMainContentView.kt
+++ WordPress/src/main/java/org/wordpress/android/support/he/ui/TicketMainContentView.kt
@@ -12,8 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CameraAlt
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
