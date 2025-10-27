9016a82be972daf50dc86436977a49a382ef25bb
adalpari <adalpari@gmail.com>
2025-10-27 14:06:25 +0100

    Claude PR suggestions
    Mutex and constant

--- WordPress/src/main/java/org/wordpress/android/support/aibot/ui/AIBotConversationDetailScreen.kt
+++ WordPress/src/main/java/org/wordpress/android/support/aibot/ui/AIBotConversationDetailScreen.kt
@@ -58,6 +58,8 @@ import org.wordpress.android.support.aibot.util.formatRelativeTime
 import org.wordpress.android.support.aibot.util.generateSampleBotConversations
 import org.wordpress.android.ui.compose.theme.AppThemeM3
 
+private const val PAGINATION_TRIGGER_THRESHOLD = 4
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AIBotConversationDetailScreen(
@@ -91,10 +93,7 @@ fun AIBotConversationDetailScreen(
     LaunchedEffect(listState, isLoadingOlderMessages, isLoading, hasMorePages) {
         snapshotFlow { listState.firstVisibleItemIndex }
             .collect { firstVisibleIndex ->
-                // Trigger pagination when user reaches the 4th message from the top
-                val threshold = 4
-
-                val shouldLoadMore = !isLoadingOlderMessages && firstVisibleIndex <= threshold
+                val shouldLoadMore = !isLoadingOlderMessages && firstVisibleIndex <= PAGINATION_TRIGGER_THRESHOLD
 
                 if (shouldLoadMore && !isLoading && hasMorePages) {
                     onLoadOlderMessages()
