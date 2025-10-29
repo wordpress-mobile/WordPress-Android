8223d9e01ee0f44a00e6a15380a73e79381e0beb
adalpari <adalpari@gmail.com>
2025-10-29 10:48:41 +0100

    Add attachments to the message data class

--- WordPress/src/main/java/org/wordpress/android/support/he/model/SupportMessage.kt
+++ WordPress/src/main/java/org/wordpress/android/support/he/model/SupportMessage.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.support.he.model
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.AnnotatedString
+import uniffi.wp_api.AttachmentMetadataValue
 import java.util.Date
 
 @Immutable
@@ -11,5 +12,15 @@ data class SupportMessage(
     val formattedText: AnnotatedString,
     val createdAt: Date,
     val authorName: String,
-    val authorIsUser: Boolean
+    val authorIsUser: Boolean,
+    val attachments: List<SupportAttachment>,
 )
+
+data class SupportAttachment (
+    val id: Long,
+    val filename: String,
+    val url: String,
+    val type: AttachmentType,
+)
+
+enum class AttachmentType { Image, Video, Other }
