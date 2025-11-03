80bedeb8584856a2181123bc9cece10f8392f9d5
adalpari <adalpari@gmail.com>
2025-11-03 16:23:41 +0100

    Tests

--- WordPress/src/main/java/org/wordpress/android/support/he/ui/HEConversationReplyBottomSheet.kt
+++ WordPress/src/main/java/org/wordpress/android/support/he/ui/HEConversationReplyBottomSheet.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
