03c6f49bc7e5ac51d944a4d62238c02f5cf55522
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-11-18 13:24:04 +0100

    Provide `AppLogWrapper#e` with `throwable` directly
    Otherwise unit tests fail because Mockito has a problem with the synthethic Kotlin method (doesn't see it)

--- libs/fluxc/src/main/java/org/wordpress/android/fluxc/utils/AppLogWrapper.kt
+++ libs/fluxc/src/main/java/org/wordpress/android/fluxc/utils/AppLogWrapper.kt
@@ -3,10 +3,9 @@ package org.wordpress.android.fluxc.utils
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
 
-class AppLogWrapper
-@Inject constructor() {
+class AppLogWrapper @Inject constructor() {
     fun d(tag: AppLog.T, message: String) = AppLog.d(tag, message)
-    fun e(tag: AppLog.T, message: String, throwable: Throwable? = null) =
-        if (throwable != null) AppLog.e(tag, message, throwable) else AppLog.e(tag, message)
+    fun e(tag: AppLog.T, message: String) = AppLog.e(tag, message)
+    fun e(tag: AppLog.T, message: String, throwable: Throwable? = null) = AppLog.e(tag, message, throwable)
     fun w(tag: AppLog.T, message: String) = AppLog.w(tag, message)
 }
