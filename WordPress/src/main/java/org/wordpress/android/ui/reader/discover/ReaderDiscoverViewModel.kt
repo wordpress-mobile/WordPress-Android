288ff57f0b20734e101b1fce5fad9040054ca4b6
adalpari <adalpari@gmail.com>
2025-11-27 16:47:09 +0100

    Fixing the loading wrong state

--- WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderDiscoverViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/discover/ReaderDiscoverViewModel.kt
@@ -157,8 +157,11 @@ class ReaderDiscoverViewModel @Inject constructor(
                         emptyList()
                     }
 
+                    val newCards = convertCardsToUiStates(posts)
+                    val cardsWithPreservedState = preserveFollowActionRunningState(newCards)
+
                     _uiState.value = DiscoverUiState.ContentUiState(
-                        announcement + convertCardsToUiStates(posts),
+                        announcement + cardsWithPreservedState,
                         reloadProgressVisibility = false,
                         loadMoreProgressVisibility = false,
                     )
@@ -171,6 +174,30 @@ class ReaderDiscoverViewModel @Inject constructor(
                         _navigationEvents.value = Event(ShowReaderSubs)
                     }
                 }
+            }
+        }
+    }
+
+    private fun preserveFollowActionRunningState(newCards: List<ReaderCardUiState>): List<ReaderCardUiState> {
+        val currentUiState = _uiState.value as? DiscoverUiState.ContentUiState ?: return newCards
+
+        return newCards.map { card ->
+            if (card is ReaderCardUiState.ReaderRecommendedBlogsCardUiState) {
+                val updatedBlogs = card.blogs.map { blog ->
+                    val currentBlog = currentUiState.cards
+                        .filterIsInstance<ReaderCardUiState.ReaderRecommendedBlogsCardUiState>()
+                        .flatMap { it.blogs }
+                        .find { it.blogId == blog.blogId && it.feedId == blog.feedId }
+
+                    if (currentBlog != null && currentBlog.isFollowActionRunning) {
+                        blog.copy(isFollowActionRunning = true)
+                    } else {
+                        blog
+                    }
+                }
+                card.copy(blogs = updatedBlogs)
+            } else {
+                card
             }
         }
     }
