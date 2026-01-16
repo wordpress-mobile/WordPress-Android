335002f113777ded579dd3f237d2909107cabff3
adalpari <adalpari@gmail.com>
2026-01-16 13:05:04 +0100

    Making the screen tabbed

--- WordPress/src/main/java/org/wordpress/android/ui/newstats/NewStatsActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/newstats/NewStatsActivity.kt
@@ -5,22 +5,29 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import org.wordpress.android.R
 import org.wordpress.android.ui.compose.theme.AppThemeM3
 import org.wordpress.android.ui.main.BaseAppCompatActivity
@@ -45,11 +52,21 @@ class NewStatsActivity : BaseAppCompatActivity() {
     }
 }
 
+private enum class StatsTab(val titleResId: Int) {
+    TRAFFIC(R.string.stats_traffic),
+    INSIGHTS(R.string.stats_insights),
+    SUBSCRIBERS(R.string.subscribers)
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewStatsScreen(
+private fun NewStatsScreen(
     onBackPressed: () -> Unit
 ) {
+    val tabs = StatsTab.entries
+    val pagerState = rememberPagerState(pageCount = { tabs.size })
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -67,14 +84,42 @@ fun NewStatsScreen(
             )
         }
     ) { contentPadding ->
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(contentPadding),
-            contentAlignment = Alignment.Center
+                .padding(contentPadding)
         ) {
-            Text(text = "New Stats Screen - Coming Soon")
+            PrimaryTabRow(selectedTabIndex = pagerState.currentPage) {
+                tabs.forEachIndexed { index, tab ->
+                    Tab(
+                        selected = pagerState.currentPage == index,
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                        },
+                        text = { Text(text = stringResource(id = tab.titleResId)) }
+                    )
+                }
+            }
+
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier.fillMaxSize()
+            ) { page ->
+                StatsTabContent(tab = tabs[page])
+            }
         }
+    }
+}
+
+@Composable
+private fun StatsTabContent(tab: StatsTab) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "${stringResource(id = tab.titleResId)} - Coming Soon")
     }
 }
 
