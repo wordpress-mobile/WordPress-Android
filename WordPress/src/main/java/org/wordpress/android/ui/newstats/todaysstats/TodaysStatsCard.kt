0429fdf097a3823f7d1fc15f472465f42ebf530c
Adalberto Plaza <adalpari@gmail.com>
2026-01-23 15:51:24 +0100

    Add StatsDataSource and refactor StatsRepository (#22513)
    * Add StatsDataSource and refactor StatsRepository
    
    - Create StatsDataSource interface and implementation to abstract data fetching
    - Add StatsModule for dependency injection
    - Refactor StatsRepository to use the new data source
    - Rename todaysstat -> todaystats package in newstats
    - Add StatsRepositoryTest with comprehensive test coverage
    
    * Add StatsDataSource and refactor StatsRepository
    
    - Create StatsDataSource interface and implementation to abstract data fetching
    - Add StatsModule for dependency injection
    - Refactor StatsRepository to use the new data source
    - Rename todaysstat -> todaystats package in newstats
    - Add StatsRepositoryTest with comprehensive test coverage
    
    * detekt
    
    * rename
    
    * PR suggestions

--- WordPress/src/main/java/org/wordpress/android/ui/newstats/todaysstats/TodaysStatsCard.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/newstats/todaysstats/TodaysStatsCard.kt
@@ -1,4 +1,4 @@
-package org.wordpress.android.ui.newstats.todaysstat
+package org.wordpress.android.ui.newstats.todaysstats
 
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
--- WordPress/src/main/java/org/wordpress/android/ui/newstats/todaysstats/TodaysStatsViewModel.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/newstats/todaysstats/TodaysStatsViewModel.kt
@@ -1,4 +1,4 @@
-package org.wordpress.android.ui.newstats.todaysstat
+package org.wordpress.android.ui.newstats.todaysstats
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
