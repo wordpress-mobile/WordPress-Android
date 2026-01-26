48dd1d3c5e4904aa22aafd02cf52656073584c5c
Nick Bradbury <nick.bradbury@gmail.com>
2026-01-26 14:31:34 -0500

    Don't hide keyboard on search

--- WordPress/src/main/java/org/wordpress/android/ui/mediapicker/MediaPickerFragment.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/mediapicker/MediaPickerFragment.kt
@@ -434,7 +434,6 @@ class MediaPickerFragment : Fragment(), MenuProvider {
 
             override fun onQueryTextChange(query: String): Boolean {
                 if (!isExpanding) {
-                    binding?.actionableEmptyView?.visibility = View.GONE
                     viewModel.onSearch(query)
                 }
                 isExpanding = false
