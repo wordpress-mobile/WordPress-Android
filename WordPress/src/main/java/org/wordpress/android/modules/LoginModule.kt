3c255cd4638875d666783416b20e7865c95ddeae
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2026-02-03 18:13:35 -0700

    Remove orphaned files

--- WordPress/src/main/java/org/wordpress/android/modules/LoginModule.kt
+++ WordPress/src/main/java/org/wordpress/android/modules/LoginModule.kt
@@ -4,8 +4,7 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.wordpress.android.login.di.LoginFragmentModule
-import org.wordpress.android.login.di.LoginServiceModule
 
 @InstallIn(SingletonComponent::class)
-@Module(includes = [LoginFragmentModule::class, LoginServiceModule::class])
+@Module(includes = [LoginFragmentModule::class])
 interface LoginModule
