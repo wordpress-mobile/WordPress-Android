bd5899fa049ed0f10ffea4317fbff63c03e88e3a
thirumani-vihaan <your-github-email@example.com>
2026-07-16 18:37:31 +0530

    Fix missing AccountAction import

--- WordPress/src/main/java/org/wordpress/android/ui/main/WPMainActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/main/WPMainActivity.java
@@ -39,6 +39,7 @@ import org.wordpress.android.WordPress;
 import org.wordpress.android.analytics.AnalyticsTracker;
 import org.wordpress.android.analytics.AnalyticsTracker.Stat;
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.action.AccountAction;
 import org.wordpress.android.fluxc.generated.AccountActionBuilder;
 import org.wordpress.android.fluxc.generated.SiteActionBuilder;
 import org.wordpress.android.fluxc.model.PostModel;
