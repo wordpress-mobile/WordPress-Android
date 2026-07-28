342643a360cf36b6036b26151340a7b4d765a9fd
OneBitSailor <aryanjagtap06@gmail.com>
2026-07-28 14:24:51 +0530

    Update UnifiedAboutActivity.kt
    Added check for possible invalid URLs or missing web browsers.

--- WordPress/src/main/java/org/wordpress/android/ui/about/UnifiedAboutActivity.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/about/UnifiedAboutActivity.kt
@@ -1,6 +1,8 @@
 package org.wordpress.android.ui.about
 
+import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.widget.Toast
 import com.automattic.about.model.AboutConfigProvider
 import org.wordpress.android.R
 import org.wordpress.android.WordPress
@@ -19,14 +21,20 @@ class UnifiedAboutActivity : BaseAppCompatActivity(), AboutConfigProvider {
         super.onCreate(savedInstanceState)
         (application as WordPress).component().inject(this)
         setContentView(R.layout.unified_about_activity)
-
-        viewModel.onNavigation.observeEvent(this) {
-            when (it) {
+        viewModel.onNavigation.observeEvent(this) { action -> 
+            when (action) {
                 is Dismiss -> finish()
-                is OpenBlog -> ActivityLauncher.openUrlExternal(this, it.url)
+                is OpenBlog -> openExternalBlogUrl(action.url)
             }
         }
     }
 
+    private fun openExternalBlogUrl(url: String){
+        try{
+            ActivityLauncher.openUrlExternal(this, url)
+        }catch(e: ActivityNotFoundException){
+            Toast.makeText(this, "External Activity does not exists or is invalid.", Toast.LENGTH_SHORT).show()
+        }
+    }
     override fun getAboutConfig() = viewModel.getAboutConfig()
 }
