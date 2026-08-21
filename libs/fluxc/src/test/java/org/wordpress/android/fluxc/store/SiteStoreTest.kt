be791e65acc2a3c677c61f2bb3945350455f8f0f
Nick Bradbury <nick.bradbury@gmail.com>
2026-08-21 13:17:13 -0400

    Fix SiteStoreTest stubs for the new fetchWPAPISite parameter
    fetchWPAPISite gained a previousSite parameter with a default, so Kotlin routes
    these calls through fetchWPAPISite$default and Mockito sees two arguments where
    the stubs recorded one matcher. This was the unit-tests CI failure.

--- libs/fluxc/src/test/java/org/wordpress/android/fluxc/store/SiteStoreTest.kt
+++ libs/fluxc/src/test/java/org/wordpress/android/fluxc/store/SiteStoreTest.kt
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.any
+import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.argWhere
 import org.mockito.kotlin.inOrder
 import org.mockito.kotlin.mock
@@ -494,7 +495,7 @@ class SiteStoreTest {
                 apiRestUsernamePlain = "appUser"
                 apiRestPasswordPlain = "appPass"
             }
-            whenever(siteWPAPIClient.fetchWPAPISite(any<SiteStore.FetchWPAPISitePayload>()))
+            whenever(siteWPAPIClient.fetchWPAPISite(any<SiteStore.FetchWPAPISitePayload>(), anyOrNull()))
                 .thenReturn(fetchedSite)
             whenever(siteSqlUtils.insertOrUpdateSite(fetchedSite))
                 .thenReturn(1)
@@ -556,7 +557,7 @@ class SiteStoreTest {
                     origin = SiteModel.ORIGIN_WPAPI
                     url = "https://example.com"
                 }
-                whenever(siteWPAPIClient.fetchWPAPISite(any<SiteStore.FetchWPAPISitePayload>()))
+                whenever(siteWPAPIClient.fetchWPAPISite(any<SiteStore.FetchWPAPISitePayload>(), anyOrNull()))
                     .thenReturn(fetchedSite)
                 whenever(siteSqlUtils.insertOrUpdateSite(fetchedSite)).thenReturn(1)
 
