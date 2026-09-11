c8b263cbef515c55a19477173bc1691d99a4949f
Oguz Kocer <oguz.kocer@automattic.com>
2026-09-11 18:44:32 -0400

    Add `RedeemCartUseCase`
    Pays for a created cart with the account's WordPress.com credits through
    `me().redeemCart()`, registering the domain the cart holds.
    
    Changes:
    - `redeemCartParams` builds the request body, split out because the mocked
      `WpComApiClient` discards the lambda `execute` hands it
    - `DomainContactField` maps the error code a rejection names to the contact
      field the form shows. Codes naming nothing on the form map to null
    - `apiErrorCode()` joins `apiErrorMessage()` and `isDeviceOffline()` on
      `WpRequestResult`
    - `testTransactionReceipt()` builds a receipt for the tests

--- WordPress/src/main/java/org/wordpress/android/ui/domains/usecases/WpRequestResultExtensions.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/domains/usecases/WpRequestResultExtensions.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.ui.domains.usecases
 
 import rs.wordpress.api.kotlin.WpRequestResult
 import uniffi.wp_api.RequestExecutionErrorReason
+import uniffi.wp_api.WpErrorCode
 
 /**
  * The message WordPress.com sent with a refusal, for the screens that put it in
@@ -9,6 +10,14 @@ import uniffi.wp_api.RequestExecutionErrorReason
  */
 internal fun WpRequestResult<*>.apiErrorMessage(): String? =
     (this as? WpRequestResult.WpError)?.errorMessage
+
+/**
+ * The code WordPress.com sent with a refusal, for the endpoints that define
+ * codes of their own. Codes the WordPress REST API also uses are modelled as
+ * [WpErrorCode] variants and come back null here.
+ */
+internal fun WpRequestResult<*>.apiErrorCode(): String? =
+    ((this as? WpRequestResult.WpError)?.errorCode as? WpErrorCode.CustomException)?.v1
 
 internal fun WpRequestResult<*>.isDeviceOffline(): Boolean =
     this is WpRequestResult.RequestExecutionFailed &&
